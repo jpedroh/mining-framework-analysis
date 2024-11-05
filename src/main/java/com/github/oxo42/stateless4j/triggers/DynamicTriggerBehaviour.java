f069e70da00dead529b23f3ce37cc924338cb037
jphsd <jphsd@inesserver2.cin.ufpe.br>
2024-11-05 13:21:14 -0300

    Mining Framework Analysis

--- src/main/java/com/github/oxo42/stateless4j/triggers/DynamicTriggerBehaviour.java
+++ src/main/java/com/github/oxo42/stateless4j/triggers/DynamicTriggerBehaviour.java
@@ -1,22 +1,19 @@
 package com.github.oxo42.stateless4j.triggers;
-
 import com.github.oxo42.stateless4j.OutVar;
 import com.github.oxo42.stateless4j.delegates.Func2;
 import com.github.oxo42.stateless4j.delegates.FuncBoolean;
-import com.github.oxo42.stateless4j.validation.Enforce;
 
-public class DynamicTriggerBehaviour<TState, TTrigger> extends TriggerBehaviour<TState, TTrigger> {
+public class DynamicTriggerBehaviour<TState extends java.lang.Object, TTrigger extends java.lang.Object> extends TriggerBehaviour<TState, TTrigger> {
+  private final Func2<Object[], TState> destination;
 
-    private final Func2<Object[], TState> destination;
+  public DynamicTriggerBehaviour(final TTrigger trigger, final Func2<Object[], TState> destination, final FuncBoolean guard) {
+    super(trigger, guard);
+    assert destination != null : "destination is null";
+    this.destination = destination;
+  }
 
-    public DynamicTriggerBehaviour(TTrigger trigger, Func2<Object[], TState> destination, FuncBoolean guard) {
-        super(trigger, guard);
-        assert destination != null : "destination is null";
-        this.destination = destination;
-    }
-
-    public boolean resultsInTransitionFrom(TState source, Object[] args, OutVar<TState> dest) {
-        dest.set(destination.call(args));
-        return true;
-    }
+  public boolean resultsInTransitionFrom(TState source, Object[] args, OutVar<TState> dest) {
+    dest.set(destination.call(args));
+    return true;
+  }
 }
