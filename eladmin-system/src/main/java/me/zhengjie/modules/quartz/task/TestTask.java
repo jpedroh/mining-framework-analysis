cbcc16046b35f1cc304aebb0dbe8a1af15d37b70
Joao Duarte <jphsd@cin.ufpe.br>
2024-11-05 10:51:34 -0300

    Mining Framework Analysis

--- eladmin-system/src/main/java/me/zhengjie/modules/quartz/task/TestTask.java
+++ eladmin-system/src/main/java/me/zhengjie/modules/quartz/task/TestTask.java
@@ -1,58 +1,27 @@
-/*
- *  Copyright 2019-2020 Zheng Jie
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- */
 package me.zhengjie.modules.quartz.task;
-
 import lombok.extern.slf4j.Slf4j;
 import me.zhengjie.exception.TaskException;
 import org.springframework.scheduling.annotation.Async;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-/**
- * 测试用
- * @author Zheng Jie
- * @date 2019-01-08
- */
-@Slf4j
-@Async
-@Component
-@SuppressWarnings({"unused"})
-public class TestTask {
+@Slf4j @Async @SuppressWarnings(value = { "unused" }) public @Service class TestTask {
+  @SuppressWarnings(value = { "unused" }) public void run() {
+    log.info("run \u6267\u884c\u6210\u529f");
+  }
 
-    @SuppressWarnings({"unused"})
-    public void run(){
-        log.info("run 执行成功");
-    }
+  @SuppressWarnings(value = { "unused" }) public void run1(String str) {
+    log.info("run1 \u6267\u884c\u6210\u529f\uff0c\u53c2\u6570\u4e3a\uff1a {}" + str);
+  }
 
-    @SuppressWarnings({"unused"})
-    public void run1(String str){
-        log.info("run1 执行成功，参数为： {}" + str);
-    }
+  @SuppressWarnings(value = { "unused" }) public void run2() {
+    log.info("run2 \u6267\u884c\u6210\u529f");
+  }
 
-    @SuppressWarnings({"unused"})
-    public void run2(){
-        log.info("run2 执行成功");
-    }
+  @SuppressWarnings(value = { "unused" }) public void runWithException() {
+    throw new TaskException("\u8fd4\u56de\u4e00\u4e2a\u6d4b\u8bd5\u9519\u8bef");
+  }
 
-    @SuppressWarnings({"unused"})
-    public void runWithException() {
-        throw new TaskException("返回一个测试错误");
-    }
-
-    @SuppressWarnings({"unused"})
-    public void runWithException(String param) {
-        throw new TaskException("返回一个带参数的测试错误", param);
-    }
+  @SuppressWarnings(value = { "unused" }) public void runWithException(String param) {
+    throw new TaskException("\u8fd4\u56de\u4e00\u4e2a\u5e26\u53c2\u6570\u7684\u6d4b\u8bd5\u9519\u8bef", param);
+  }
 }
--- eladmin-system/src/main/java/me/zhengjie/modules/system/service/impl/DataServiceImpl.java
+++ eladmin-system/src/main/java/me/zhengjie/modules/system/service/impl/DataServiceImpl.java
@@ -1,20 +1,4 @@
-/*
- *  Copyright 2019-2020 Zheng Jie
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- */
 package me.zhengjie.modules.system.service.impl;
-
 import lombok.RequiredArgsConstructor;
 import me.zhengjie.modules.system.domain.Dept;
 import me.zhengjie.modules.system.service.DataService;
@@ -28,67 +12,41 @@ import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import java.util.*;
 
-/**
- * @author Zheng Jie
- * @website https://el-admin.vip
- * @description 数据权限服务实现
- * @date 2020-05-07
- **/
-@Service
-@RequiredArgsConstructor
-@CacheConfig(cacheNames = "data")
-public class DataServiceImpl implements DataService {
+@Service @RequiredArgsConstructor @CacheConfig(cacheNames = "data") public class DataServiceImpl implements DataService {
+  private final RoleService roleService;
 
-    private final RoleService roleService;
-    private final DeptService deptService;
+  private final DeptService deptService;
 
-    /**
-     * 用户角色和用户部门改变时需清理缓存
-     * @param user /
-     * @return /
-     */
-    @Override
-    @Cacheable(key = "'user:' + #p0.id")
-    public List<Long> getDeptIds(UserDto user) {
-        // 用于存储部门id
-        Set<Long> deptIds = new HashSet<>();
-        // 查询用户角色
-        List<RoleSmallDto> roleSet = roleService.findByUsersId(user.getId());
-        // 获取对应的部门ID
-        for (RoleSmallDto role : roleSet) {
-            DataScopeEnum dataScopeEnum = DataScopeEnum.find(role.getDataScope());
-            switch (Objects.requireNonNull(dataScopeEnum)) {
-                case THIS_LEVEL:
-                    deptIds.add(user.getDept().getId());
-                    break;
-                case CUSTOMIZE:
-                    deptIds.addAll(getCustomize(deptIds, role));
-                    break;
-                case ALL:
-                    // 增加如果是数据权限包含有全部，则直接返回空
-                    return new ArrayList<>();
-                default:
-                    return new ArrayList<>(deptIds);
-            }
-        }
+  @Override @Cacheable(key = "\'user:\' + #p0.id") public List<Long> getDeptIds(UserDto user) {
+    Set<Long> deptIds = new HashSet<>();
+    List<RoleSmallDto> roleSet = roleService.findByUsersId(user.getId());
+    for (RoleSmallDto role : roleSet) {
+      DataScopeEnum dataScopeEnum = DataScopeEnum.find(role.getDataScope());
+      switch (Objects.requireNonNull(dataScopeEnum)) {
+        case THIS_LEVEL:
+        deptIds.add(user.getDept().getId());
+        break;
+        case CUSTOMIZE:
+        deptIds.addAll(getCustomize(deptIds, role));
+        break;
+        case ALL:
+        return new ArrayList<>();
+        default:
         return new ArrayList<>(deptIds);
+      }
     }
+    return new ArrayList<>(deptIds);
+  }
 
-    /**
-     * 获取自定义的数据权限
-     * @param deptIds 部门ID
-     * @param role 角色
-     * @return 数据权限ID
-     */
-    public Set<Long> getCustomize(Set<Long> deptIds, RoleSmallDto role){
-        Set<Dept> depts = deptService.findByRoleId(role.getId());
-        for (Dept dept : depts) {
-            deptIds.add(dept.getId());
-            List<Dept> deptChildren = deptService.findByPid(dept.getId());
-            if (deptChildren != null && deptChildren.size() != 0) {
-                deptIds.addAll(deptService.getDeptChildren(deptChildren));
-            }
-        }
-        return deptIds;
+  public Set<Long> getCustomize(Set<Long> deptIds, RoleSmallDto role) {
+    Set<Dept> depts = deptService.findByRoleId(role.getId());
+    for (Dept dept : depts) {
+      deptIds.add(dept.getId());
+      List<Dept> deptChildren = deptService.findByPid(dept.getId());
+      if (deptChildren != null && deptChildren.size() != 0) {
+        deptIds.addAll(deptService.getDeptChildren(deptChildren));
+      }
     }
+    return deptIds;
+  }
 }
