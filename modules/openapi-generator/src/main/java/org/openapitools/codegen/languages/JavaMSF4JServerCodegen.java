31ed7e658ac1177c1210735b4ba252978b583229
Joao Duarte <jphsd@cin.ufpe.br>
2024-11-05 10:27:10 -0300

    Mining Framework Analysis

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/JavaMSF4JServerCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/JavaMSF4JServerCodegen.java
@@ -1,22 +1,4 @@
-/*
- * Copyright 2018 OpenAPI-Generator Contributors (https://openapi-generator.tech)
- * Copyright 2018 SmartBear Software
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.openapitools.codegen.languages;
-
 import io.swagger.models.Path;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.Operation;
@@ -25,147 +7,122 @@ import org.apache.commons.lang3.BooleanUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.openapitools.codegen.*;
 import org.openapitools.codegen.meta.features.DocumentationFeature;
-import org.openapitools.codegen.model.ModelMap;
-import org.openapitools.codegen.model.ModelsMap;
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import org.openapitools.codegen.model.ModelMap;
+import org.openapitools.codegen.model.ModelsMap;
 
 public class JavaMSF4JServerCodegen extends AbstractJavaJAXRSServerCodegen {
+  protected static final String LIBRARY_JERSEY1 = "jersey1";
 
-    protected static final String LIBRARY_JERSEY1 = "jersey1";
-    protected static final String LIBRARY_JERSEY2 = "jersey2";
+  protected static final String LIBRARY_JERSEY2 = "jersey2";
 
+  public static final String DEFAULT_MSF4J_LIBRARY = LIBRARY_JERSEY2;
 
-    public static final String DEFAULT_MSF4J_LIBRARY = LIBRARY_JERSEY2;
+  public JavaMSF4JServerCodegen() {
+    super();
+    modifyFeatureSet((features) -> features.includeDocumentationFeatures(DocumentationFeature.Readme));
+    outputFolder = "generated-code/JavaJaxRS-MSF4J";
+    apiTemplateFiles.put("apiService.mustache", ".java");
+    apiTemplateFiles.put("apiServiceImpl.mustache", ".java");
+    apiTemplateFiles.put("apiServiceFactory.mustache", ".java");
+    apiTestTemplateFiles.clear();
+    modelDocTemplateFiles.remove("model_doc.mustache");
+    apiDocTemplateFiles.remove("api_doc.mustache");
+    embeddedTemplateDir = templateDir = "java-msf4j-server";
+    CliOption library = new CliOption(CodegenConstants.LIBRARY, CodegenConstants.LIBRARY_DESC);
+    supportedLibraries.put(LIBRARY_JERSEY1, "Jersey core 1.x");
+    supportedLibraries.put(LIBRARY_JERSEY2, "Jersey core 2.x");
+    library.setEnum(supportedLibraries);
+    library.setDefault(DEFAULT_MSF4J_LIBRARY);
+    cliOptions.add(library);
+  }
 
-    public JavaMSF4JServerCodegen() {
-        super();
+  @Override public String getName() {
+    return "java-msf4j";
+  }
 
-        modifyFeatureSet(features -> features.includeDocumentationFeatures(DocumentationFeature.Readme));
+  @Override public String getHelp() {
+    return "Generates a Java Micro Service based on WSO2 Microservices Framework for Java (MSF4J)";
+  }
 
-        outputFolder = "generated-code/JavaJaxRS-MSF4J";
-        apiTemplateFiles.put("apiService.mustache", ".java");
-        apiTemplateFiles.put("apiServiceImpl.mustache", ".java");
-        apiTemplateFiles.put("apiServiceFactory.mustache", ".java");
-        apiTestTemplateFiles.clear();
-        modelDocTemplateFiles.remove("model_doc.mustache");
-        apiDocTemplateFiles.remove("api_doc.mustache");
-        embeddedTemplateDir = templateDir = "java-msf4j-server";
-        CliOption library = new CliOption(CodegenConstants.LIBRARY, CodegenConstants.LIBRARY_DESC);
-        supportedLibraries.put(LIBRARY_JERSEY1, "Jersey core 1.x");
-        supportedLibraries.put(LIBRARY_JERSEY2, "Jersey core 2.x");
-        library.setEnum(supportedLibraries);
-        library.setDefault(DEFAULT_MSF4J_LIBRARY);
-        cliOptions.add(library);
-
+  @Override public void postProcessModelProperty(CodegenModel model, CodegenProperty property) {
+    super.postProcessModelProperty(model, property);
+    if ("null".equals(property.example)) {
+      property.example = null;
     }
-
-    @Override
-    public String getName() {
-        return "java-msf4j";
+    if (!BooleanUtils.toBoolean(model.isEnum)) {
+      model.imports.add("JsonProperty");
+      if (BooleanUtils.toBoolean(model.hasEnums)) {
+        model.imports.add("JsonValue");
+      }
     }
+  }
 
-    @Override
-    public String getHelp() {
-        return "Generates a Java Micro Service based on WSO2 Microservices Framework for Java (MSF4J)";
+  @Override public void processOpts() {
+    super.processOpts();
+    if (StringUtils.isEmpty(library)) {
+      setLibrary(DEFAULT_MSF4J_LIBRARY);
     }
+    if (additionalProperties.containsKey(CodegenConstants.IMPL_FOLDER)) {
+      implFolder = (String) additionalProperties.get(CodegenConstants.IMPL_FOLDER);
+    }
+    if ("joda".equals(dateLibrary)) {
+      supportingFiles.add(new SupportingFile("JodaDateTimeProvider.mustache", (sourceFolder + '/' + apiPackage).replace(".", "/"), "JodaDateTimeProvider.java"));
+      supportingFiles.add(new SupportingFile("JodaLocalDateProvider.mustache", (sourceFolder + '/' + apiPackage).replace(".", "/"), "JodaLocalDateProvider.java"));
+    } else {
+      if (dateLibrary.startsWith("java8")) {
+        supportingFiles.add(new SupportingFile("OffsetDateTimeProvider.mustache", (sourceFolder + '/' + apiPackage).replace(".", "/"), "OffsetDateTimeProvider.java"));
+        supportingFiles.add(new SupportingFile("LocalDateProvider.mustache", (sourceFolder + '/' + apiPackage).replace(".", "/"), "LocalDateProvider.java"));
+      }
+    }
+    supportingFiles.add(new SupportingFile("pom.mustache", "", "pom.xml").doNotOverwrite());
+    supportingFiles.add(new SupportingFile("README.mustache", "", "README.md").doNotOverwrite());
+    supportingFiles.add(new SupportingFile("ApiException.mustache", (sourceFolder + '/' + apiPackage).replace(".", "/"), "ApiException.java"));
+    supportingFiles.add(new SupportingFile("ApiOriginFilter.mustache", (sourceFolder + '/' + apiPackage).replace(".", "/"), "ApiOriginFilter.java"));
+    supportingFiles.add(new SupportingFile("ApiResponseMessage.mustache", (sourceFolder + '/' + apiPackage).replace(".", "/"), "ApiResponseMessage.java"));
+    supportingFiles.add(new SupportingFile("NotFoundException.mustache", (sourceFolder + '/' + apiPackage).replace(".", "/"), "NotFoundException.java"));
+    supportingFiles.add(new SupportingFile("jacksonJsonProvider.mustache", (sourceFolder + '/' + apiPackage).replace(".", "/"), "JacksonJsonProvider.java"));
+    supportingFiles.add(new SupportingFile("RFC3339DateFormat.mustache", (sourceFolder + '/' + apiPackage).replace(".", "/"), "RFC3339DateFormat.java"));
+    supportingFiles.add(new SupportingFile("StringUtil.mustache", (sourceFolder + '/' + apiPackage).replace(".", "/"), "StringUtil.java"));
+    supportingFiles.add(new SupportingFile("Application.mustache", (sourceFolder + '/' + apiPackage).replace(".", "/"), "Application.java"));
+  }
 
-    @Override
-    public void postProcessModelProperty(CodegenModel model, CodegenProperty property) {
-        super.postProcessModelProperty(model, property);
-        if ("null".equals(property.example)) {
-            property.example = null;
-        }
-
-        //Add imports for Jackson
-        if (!BooleanUtils.toBoolean(model.isEnum)) {
-            model.imports.add("JsonProperty");
-
-            if (BooleanUtils.toBoolean(model.hasEnums)) {
-                model.imports.add("JsonValue");
+  @Override public void preprocessOpenAPI(OpenAPI openAPI) {
+    super.preprocessOpenAPI(openAPI);
+    if (openAPI.getPaths() != null) {
+      for (String pathname : openAPI.getPaths().keySet()) {
+        boolean dependencies = false;
+        PathItem path = openAPI.getPaths().get(pathname);
+        if (path.readOperations() != null) {
+          for (Operation operation : path.readOperations()) {
+            if (operation.getExtensions() != null && operation.getExtensions().containsKey("x-dependencies")) {
+              supportingFiles.add(new SupportingFile("DependencyUtil.mustache", (sourceFolder + '/' + apiPackage).replace(".", "/"), "DependencyUtil.java"));
+              dependencies = true;
+              break;
             }
+          }
         }
+        if (dependencies) {
+          break;
+        }
+      }
     }
+  }
 
-    @Override
-    public void processOpts() {
-        super.processOpts();
-
-        // use default library if unset
-        if (StringUtils.isEmpty(library)) {
-            setLibrary(DEFAULT_MSF4J_LIBRARY);
-        }
-
-        if (additionalProperties.containsKey(CodegenConstants.IMPL_FOLDER)) {
-            implFolder = (String) additionalProperties.get(CodegenConstants.IMPL_FOLDER);
-        }
-
-        if ("joda".equals(dateLibrary)) {
-            supportingFiles.add(new SupportingFile("JodaDateTimeProvider.mustache", (sourceFolder + '/' + apiPackage).replace(".", "/"), "JodaDateTimeProvider.java"));
-            supportingFiles.add(new SupportingFile("JodaLocalDateProvider.mustache", (sourceFolder + '/' + apiPackage).replace(".", "/"), "JodaLocalDateProvider.java"));
-        } else if (dateLibrary.startsWith("java8")) {
-            supportingFiles.add(new SupportingFile("OffsetDateTimeProvider.mustache", (sourceFolder + '/' + apiPackage).replace(".", "/"), "OffsetDateTimeProvider.java"));
-            supportingFiles.add(new SupportingFile("LocalDateProvider.mustache", (sourceFolder + '/' + apiPackage).replace(".", "/"), "LocalDateProvider.java"));
-        }
-
-        supportingFiles.add(new SupportingFile("pom.mustache", "", "pom.xml")
-                .doNotOverwrite());
-        supportingFiles.add(new SupportingFile("README.mustache", "", "README.md")
-                .doNotOverwrite());
-        supportingFiles.add(new SupportingFile("ApiException.mustache", (sourceFolder + '/' + apiPackage).replace(".", "/"), "ApiException.java"));
-        supportingFiles.add(new SupportingFile("ApiOriginFilter.mustache", (sourceFolder + '/' + apiPackage).replace(".", "/"), "ApiOriginFilter.java"));
-        supportingFiles.add(new SupportingFile("ApiResponseMessage.mustache", (sourceFolder + '/' + apiPackage).replace(".", "/"), "ApiResponseMessage.java"));
-        supportingFiles.add(new SupportingFile("NotFoundException.mustache", (sourceFolder + '/' + apiPackage).replace(".", "/"), "NotFoundException.java"));
-        supportingFiles.add(new SupportingFile("jacksonJsonProvider.mustache", (sourceFolder + '/' + apiPackage).replace(".", "/"), "JacksonJsonProvider.java"));
-        supportingFiles.add(new SupportingFile("RFC3339DateFormat.mustache", (sourceFolder + '/' + apiPackage).replace(".", "/"), "RFC3339DateFormat.java"));
-        // supportingFiles.add(new SupportingFile("bootstrap.mustache", (implFolder + '/' + apiPackage).replace(".", "/"), "Bootstrap.java").doNotOverwrite());
-        // supportingFiles.add(new SupportingFile("web.mustache", ("src/main/webapp/WEB-INF"), "web.xml").doNotOverwrite());
-        supportingFiles.add(new SupportingFile("StringUtil.mustache", (sourceFolder + '/' + apiPackage).replace(".", "/"), "StringUtil.java"));
-        supportingFiles.add(new SupportingFile("Application.mustache", (sourceFolder + '/' + apiPackage).replace(".", "/"), "Application.java"));
+  @Override public ModelsMap postProcessModelsEnum(ModelsMap objs) {
+    objs = super.postProcessModelsEnum(objs);
+    List<Map<String, String>> imports = objs.getImports();
+    for (ModelMap mo : objs.getModels()) {
+      CodegenModel cm = mo.getModel();
+      if (Boolean.TRUE.equals(cm.isEnum) && cm.allowableValues != null) {
+        cm.imports.add(importMapping.get("JsonValue"));
+        Map<String, String> item = new HashMap<String, String>();
+        item.put("import", importMapping.get("JsonValue"));
+        imports.add(item);
+      }
     }
-
-    @Override
-    public void preprocessOpenAPI(OpenAPI openAPI) {
-        super.preprocessOpenAPI(openAPI);
-
-        if (openAPI.getPaths() != null){
-            for (String pathname : openAPI.getPaths().keySet()) {
-                boolean dependencies = false;
-                PathItem path = openAPI.getPaths().get(pathname);
-                if (path.readOperations() != null) {
-                    for(Operation operation : path.readOperations()){
-                        if (operation.getExtensions()!=null && operation.getExtensions().containsKey("x-dependencies")){
-                            supportingFiles.add(new SupportingFile("DependencyUtil.mustache", (sourceFolder + '/' + apiPackage).replace(".", "/"), "DependencyUtil.java"));
-                            dependencies = true;
-                            break;
-                        }
-                    }
-                }
-                if (dependencies)
-                    break;
-            }
-        }
-    }
-
-    public ModelsMap postProcessModelsEnum(ModelsMap objs) {
-        objs = super.postProcessModelsEnum(objs);
-
-        //Add imports for Jackson
-        List<Map<String, String>> imports = objs.getImports();
-        for (ModelMap mo : objs.getModels()) {
-            CodegenModel cm = mo.getModel();
-            // for enum model
-            if (Boolean.TRUE.equals(cm.isEnum) && cm.allowableValues != null) {
-                cm.imports.add(importMapping.get("JsonValue"));
-                Map<String, String> item = new HashMap<String, String>();
-                item.put("import", importMapping.get("JsonValue"));
-                imports.add(item);
-            }
-        }
-
-        return objs;
-    }
-
+    return objs;
+  }
 }
--- samples/client/petstore/java/resttemplate-withXml/src/main/java/org/openapitools/client/api/AnotherFakeApi.java
+++ samples/client/petstore/java/resttemplate-withXml/src/main/java/org/openapitools/client/api/AnotherFakeApi.java
@@ -1,16 +1,12 @@
 package org.openapitools.client.api;
-
 import org.openapitools.client.ApiClient;
-
 import org.openapitools.client.model.Client;
-
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.util.LinkedMultiValueMap;
@@ -25,74 +21,44 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen")
-@Component("org.openapitools.client.api.AnotherFakeApi")
-public class AnotherFakeApi {
-    private ApiClient apiClient;
+@javax.annotation.Generated(value = { "org.openapitools.codegen.languages.JavaClientCodegen" }) @Component(value = "org.openapitools.client.api.AnotherFakeApi") public class AnotherFakeApi {
+  private ApiClient apiClient;
 
-    public AnotherFakeApi() {
-        this(new ApiClient());
+  public AnotherFakeApi() {
+    this(new ApiClient());
+  }
+
+  @Autowired public AnotherFakeApi(ApiClient apiClient) {
+    this.apiClient = apiClient;
+  }
+
+  public ApiClient getApiClient() {
+    return apiClient;
+  }
+
+  public void setApiClient(ApiClient apiClient) {
+    this.apiClient = apiClient;
+  }
+
+  public Client call123testSpecialTags(Client body) throws RestClientException {
+    return call123testSpecialTagsWithHttpInfo(body).getBody();
+  }
+
+  public ResponseEntity<Client> call123testSpecialTagsWithHttpInfo(Client body) throws RestClientException {
+    Object localVarPostBody = body;
+    if (body == null) {
+      throw new HttpClientErrorException(HttpStatus.BAD_REQUEST, "Missing the required parameter \'body\' when calling call123testSpecialTags");
     }
-
-    @Autowired
-    public AnotherFakeApi(ApiClient apiClient) {
-        this.apiClient = apiClient;
-    }
-
-    public ApiClient getApiClient() {
-        return apiClient;
-    }
-
-    public void setApiClient(ApiClient apiClient) {
-        this.apiClient = apiClient;
-    }
-
-    /**
-     * To test special tags
-     * To test special tags and operation ID starting with number
-     * <p><b>200</b> - successful operation
-     * @param body client model (required)
-     * @return Client
-     * @throws RestClientException if an error occurs while attempting to invoke the API
-     */
-    public Client call123testSpecialTags(Client body) throws RestClientException {
-        return call123testSpecialTagsWithHttpInfo(body).getBody();
-    }
-
-    /**
-     * To test special tags
-     * To test special tags and operation ID starting with number
-     * <p><b>200</b> - successful operation
-     * @param body client model (required)
-     * @return ResponseEntity&lt;Client&gt;
-     * @throws RestClientException if an error occurs while attempting to invoke the API
-     */
-    public ResponseEntity<Client> call123testSpecialTagsWithHttpInfo(Client body) throws RestClientException {
-        Object localVarPostBody = body;
-        
-        // verify the required parameter 'body' is set
-        if (body == null) {
-            throw new HttpClientErrorException(HttpStatus.BAD_REQUEST, "Missing the required parameter 'body' when calling call123testSpecialTags");
-        }
-        
-
-        final MultiValueMap<String, String> localVarQueryParams = new LinkedMultiValueMap<String, String>();
-        final HttpHeaders localVarHeaderParams = new HttpHeaders();
-        final MultiValueMap<String, String> localVarCookieParams = new LinkedMultiValueMap<String, String>();
-        final MultiValueMap<String, Object> localVarFormParams = new LinkedMultiValueMap<String, Object>();
-
-        final String[] localVarAccepts = { 
-            "application/json"
-         };
-        final List<MediaType> localVarAccept = apiClient.selectHeaderAccept(localVarAccepts);
-        final String[] localVarContentTypes = { 
-            "application/json"
-         };
-        final MediaType localVarContentType = apiClient.selectHeaderContentType(localVarContentTypes);
-
-        String[] localVarAuthNames = new String[] {  };
-
-        ParameterizedTypeReference<Client> localReturnType = new ParameterizedTypeReference<Client>() {};
-        return apiClient.invokeAPI("/another-fake/dummy", HttpMethod.PATCH, Collections.<String, Object>emptyMap(), localVarQueryParams, localVarPostBody, localVarHeaderParams, localVarCookieParams, localVarFormParams, localVarAccept, localVarContentType, localVarAuthNames, localReturnType);
-    }
+    final MultiValueMap<String, String> localVarQueryParams = new LinkedMultiValueMap<String, String>();
+    final HttpHeaders localVarHeaderParams = new HttpHeaders();
+    final MultiValueMap<String, String> localVarCookieParams = new LinkedMultiValueMap<String, String>();
+    final MultiValueMap<String, Object> localVarFormParams = new LinkedMultiValueMap<String, Object>();
+    final String[] localVarAccepts = { "application/json" };
+    final List<MediaType> localVarAccept = apiClient.selectHeaderAccept(localVarAccepts);
+    final String[] localVarContentTypes = { "application/json" };
+    final MediaType localVarContentType = apiClient.selectHeaderContentType(localVarContentTypes);
+    String[] localVarAuthNames = new String[] {  };
+    ParameterizedTypeReference<Client> localReturnType = new ParameterizedTypeReference<Client>() { };
+    return apiClient.invokeAPI("/another-fake/dummy", HttpMethod.PATCH, Collections.<String, Object>emptyMap(), localVarQueryParams, localVarPostBody, localVarHeaderParams, localVarCookieParams, localVarFormParams, localVarAccept, localVarContentType, localVarAuthNames, localReturnType);
+  }
 }
