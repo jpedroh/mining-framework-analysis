ae735f75beb45b7f7ea5ca572ff95836e76502d4
Joao Duarte <jphsd@cin.ufpe.br>
2024-11-05 10:48:47 -0300

    Mining Framework Analysis

--- src/main/java/org/vafer/jdeb/maven/Data.java
+++ src/main/java/org/vafer/jdeb/maven/Data.java
@@ -1,151 +1,98 @@
-/*
- * Copyright 2012 The Apache Software Foundation.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.vafer.jdeb.maven;
-
-import static org.vafer.jdeb.maven.MissingSourceBehavior.FAIL;
-import static org.vafer.jdeb.maven.MissingSourceBehavior.IGNORE;
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
-
 import org.vafer.jdeb.DataConsumer;
 import org.vafer.jdeb.DataProducer;
 import org.vafer.jdeb.producers.DataProducerArchive;
 import org.vafer.jdeb.producers.DataProducerDirectory;
 import org.vafer.jdeb.producers.DataProducerFile;
-/**
- * Maven "data" elment acting as a factory for DataProducers. So far Archive and
- * Directory producers are supported. Both support the usual ant pattern set
- * matching.
- * 
- * @author Bryan Sant <bryan.sant@gmail.com>
- */
+import static org.vafer.jdeb.maven.MissingSourceBehavior.FAIL;
+import static org.vafer.jdeb.maven.MissingSourceBehavior.IGNORE;
+
 public final class Data implements DataProducer {
+  private File src;
 
-    private File src;
-    
-    /**
-     * @parameter expression="${src}"
-     * @required
-     */
-    public void setSrc( File src ) {
-        this.src = src;
+  public void setSrc(File src) {
+    this.src = src;
+  }
+
+  private String destinationName;
+
+  public void setDestinationName(String destinationName) {
+    this.destinationName = destinationName;
+  }
+
+  private String type;
+
+  public void setType(String type) {
+    this.type = type;
+  }
+
+  public void setIncludes(String includes) {
+    includePatterns = splitPatterns(includes);
+  }
+
+  private String[] includePatterns;
+
+  public void setExcludes(String excludes) {
+    excludePatterns = splitPatterns(excludes);
+  }
+
+  private String[] excludePatterns;
+
+  private Mapper mapper;
+
+  public String[] splitPatterns(String patterns) {
+    String[] result = null;
+    if (patterns != null && patterns.length() > 0) {
+      List tokens = new ArrayList();
+      StringTokenizer tok = new StringTokenizer(patterns, ", ", false);
+      while (tok.hasMoreTokens()) {
+        tokens.add(tok.nextToken());
+      }
+      result = (String[]) tokens.toArray(new String[tokens.size()]);
     }
+    return result;
+  }
 
-    private String destinationName;
-
-    /**
-     * @parameter expression="${dst}"
-     * @required
-     */
-    public void setDestinationName( String destinationName ) {
-        this.destinationName = destinationName;
+  @Override public void produce(final DataConsumer pReceiver) throws IOException {
+    if (src != null && !src.exists()) {
+      if (missingSrc == IGNORE) {
+        return;
+      } else {
+        throw new FileNotFoundException("Data source not found : " + src);
+      }
     }
-
-    private String type;
-
-    /**
-     * @parameter expression="${type}"
-     */
-    public void setType( String type ) {
-        this.type = type;
+    org.vafer.jdeb.mapping.Mapper[] mappers = null;
+    if (mapper != null) {
+      mappers = new org.vafer.jdeb.mapping.Mapper[] { mapper.createMapper() };
     }
-
-    private MissingSourceBehavior missingSrc = FAIL;
-
-    /**
-     * @parameter expression="${missingSrc}"
-     */
-    public void setMissingSrc( String missingSrc ) {
-        MissingSourceBehavior value = MissingSourceBehavior.valueOf(missingSrc.trim().toUpperCase());
-        if (value == null) {
-            throw new IllegalArgumentException("Unknown " + MissingSourceBehavior.class.getSimpleName() + ": " + missingSrc);
-        }
-        this.missingSrc = value;
+    if ("file".equalsIgnoreCase(type)) {
+      new DataProducerFile(src, destinationName, includePatterns, excludePatterns, mappers).produce(pReceiver);
+      return;
     }
-
-    /**
-     * @parameter expression="${includes}" alias="includes"
-     */
-    public void setIncludes( String includes ) {
-        includePatterns = splitPatterns(includes);
+    if ("archive".equalsIgnoreCase(type)) {
+      new DataProducerArchive(src, includePatterns, excludePatterns, mappers).produce(pReceiver);
+      return;
     }
-
-    private String[] includePatterns;
-
-    /**
-     * @parameter expression="${excludes}" alias="excludes"
-     */
-    public void setExcludes( String excludes ) {
-        excludePatterns = splitPatterns(excludes);
+    if ("directory".equalsIgnoreCase(type)) {
+      new DataProducerDirectory(src, includePatterns, excludePatterns, mappers).produce(pReceiver);
+      return;
     }
+    throw new IOException("Unknown type \'" + type + "\' (file|directory|archive) for " + src);
+  }
 
-    private String[] excludePatterns;
-    /**
-     * @parameter expression="${mapper}"
-     */
-    private Mapper mapper;
+  private MissingSourceBehavior missingSrc = FAIL;
 
-    public String[] splitPatterns( String patterns ) {
-        String[] result = null;
-        if (patterns != null && patterns.length() > 0) {
-            List tokens = new ArrayList();
-            StringTokenizer tok = new StringTokenizer(patterns, ", ", false);
-            while (tok.hasMoreTokens()) {
-                tokens.add(tok.nextToken());
-            }
-            result = (String[])tokens.toArray(new String[tokens.size()]);
-        }
-        return result;
+  public void setMissingSrc(String missingSrc) {
+    MissingSourceBehavior value = MissingSourceBehavior.valueOf(missingSrc.trim().toUpperCase());
+    if (value == null) {
+      throw new IllegalArgumentException("Unknown " + MissingSourceBehavior.class.getSimpleName() + ": " + missingSrc);
     }
-
-    public void produce( final DataConsumer pReceiver ) throws IOException {
-        if (src != null && !src.exists()) {
-            if (missingSrc == IGNORE) {
-                return;
-            } else {
-                throw new FileNotFoundException("Data source not found : " + src);
-            }
-        }
-
-        org.vafer.jdeb.mapping.Mapper[] mappers = null;
-        if (mapper != null) {
-            mappers = new org.vafer.jdeb.mapping.Mapper[] { mapper.createMapper() };
-        }
-
-        if ("file".equalsIgnoreCase(type)) {
-            new DataProducerFile(src, destinationName, includePatterns, excludePatterns, mappers).produce(pReceiver);
-            return;
-        }
-
-        if ("archive".equalsIgnoreCase(type)) {
-            new DataProducerArchive(src, includePatterns, excludePatterns, mappers).produce(pReceiver);
-            return;
-        }
-
-        if ("directory".equalsIgnoreCase(type)) {
-            new DataProducerDirectory(src, includePatterns, excludePatterns, mappers).produce(pReceiver);
-            return;
-        }
-
-        throw new IOException("Unknown type '" + type + "' (file|directory|archive) for " + src);
-    }
-    
+    this.missingSrc = value;
+  }
 }
