7ab9d9be4dedce2c3f516d6d434f2944754c1d7f
jphsd <jphsd@inesserver2.cin.ufpe.br>
2024-11-05 13:34:16 -0300

    Mining Framework Analysis

--- src/main/java/com/jcabi/github/RtHooks.java
+++ src/main/java/com/jcabi/github/RtHooks.java
@@ -1,135 +1,55 @@
-/**
- * Copyright (c) 2012-2013, JCabi.com
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met: 1) Redistributions of source code must retain the above
- * copyright notice, this list of conditions and the following
- * disclaimer. 2) Redistributions in binary form must reproduce the above
- * copyright notice, this list of conditions and the following
- * disclaimer in the documentation and/or other materials provided
- * with the distribution. 3) Neither the name of the jcabi.com nor
- * the names of its contributors may be used to endorse or promote
- * products derived from this software without specific prior written
- * permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
- * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
- * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
- * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- * OF THE POSSIBILITY OF SUCH DAMAGE.
- */
 package com.jcabi.github;
-
 import com.jcabi.aspects.Immutable;
 import com.jcabi.aspects.Loggable;
 import com.rexsl.test.Request;
-import com.rexsl.test.response.JsonResponse;
 import com.rexsl.test.response.RestResponse;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.util.Collections;
+import lombok.EqualsAndHashCode;
+import com.rexsl.test.response.JsonResponse;
 import java.util.Map;
 import javax.json.Json;
 import javax.json.JsonObjectBuilder;
 import javax.json.JsonStructure;
 import javax.validation.constraints.NotNull;
-import lombok.EqualsAndHashCode;
 
-/**
- * Github hooks.
- * @author Paul Polishchuk (ppol@ua.fm)
- * @version $Id$
- * @since 0.8
- */
-@Immutable
-@Loggable(Loggable.DEBUG)
-@EqualsAndHashCode(of = { "entry", "owner", "request" })
-public final class RtHooks implements Hooks {
-    /**
-     * API entry point.
-     */
-    private final transient Request entry;
+@Immutable @Loggable(value = Loggable.DEBUG) @EqualsAndHashCode(of = { "entry", "owner", "request" }) public final class RtHooks implements Hooks {
+  private final transient Request entry;
 
-    /**
-     * RESTful request.
-     */
-    private final transient Request request;
+  private final transient Request request;
 
-    /**
-     * Repository.
-     */
-    private final transient Repo owner;
+  private final transient Repo owner;
 
-    /**
-     * Public ctor.
-     * @param req Request
-     * @param repo Repository
-     */
-    public RtHooks(final Request req, final Repo repo) {
-        this.entry = req;
-        final Coordinates coords = repo.coordinates();
-        this.request = this.entry.uri()
-            .path("/repos")
-            .path(coords.user())
-            .path(coords.repo())
-            .path("/hooks")
-            .back();
-        this.owner = repo;
+  public RtHooks(final Request req, final Repo repo) {
+    this.entry = req;
+    final Coordinates coords = repo.coordinates();
+    this.request = this.entry.uri().path("/repos").path(coords.user()).path(coords.repo()).path("/hooks").back();
+    this.owner = repo;
+  }
+
+  @Override public Repo repo() {
+    return this.owner;
+  }
+
+  @Override public Iterable<Hook> iterate() {
+    return Collections.emptyList();
+  }
+
+  @Override public void remove(final int number) throws IOException {
+    this.request.method(Request.DELETE).uri().path(Integer.toString(number)).back().fetch().as(RestResponse.class).assertStatus(HttpURLConnection.HTTP_NO_CONTENT);
+  }
+
+  @Override public Hook get(final int number) {
+    return new RtHook(this.entry, this.owner, number);
+  }
+
+  @Override public Hook create(@NotNull(message = "name can\'t be NULL") final String name, @NotNull(message = "config can\'t be NULL") final Map<String, String> config) throws IOException {
+    final JsonObjectBuilder builder = Json.createObjectBuilder();
+    for (final Map.Entry<String, String> entr : config.entrySet()) {
+      builder.add(entr.getKey(), entr.getValue());
     }
-
-    @Override
-    public Repo repo() {
-        return this.owner;
-    }
-
-    @Override
-    public Iterable<Hook> iterate() {
-        return Collections.emptyList();
-    }
-
-    @Override
-    public void remove(final int number) throws IOException {
-        this.request.method(Request.DELETE)
-            .uri().path(Integer.toString(number)).back()
-            .fetch()
-            .as(RestResponse.class)
-            .assertStatus(HttpURLConnection.HTTP_NO_CONTENT);
-    }
-
-    @Override
-    public Hook get(final int number) {
-        return new RtHook(this.entry, this.owner, number);
-    }
-
-    @Override
-    public Hook create(
-        @NotNull(message = "name can't be NULL") final String name,
-        @NotNull(message = "config can't be NULL")
-        final Map<String, String> config) throws IOException {
-        final JsonObjectBuilder builder = Json.createObjectBuilder();
-        for (final Map.Entry<String, String> entr : config.entrySet()) {
-            builder.add(entr.getKey(), entr.getValue());
-        }
-        final JsonStructure json = Json.createObjectBuilder()
-            .add("name", name)
-            .add("config", builder)
-            .build();
-        return this.get(
-            this.request.method(Request.POST)
-                .body().set(json).back()
-                .fetch().as(RestResponse.class)
-                .assertStatus(HttpURLConnection.HTTP_CREATED)
-                .as(JsonResponse.class)
-                .json().readObject().getInt("id")
-        );
-    }
+    final JsonStructure json = Json.createObjectBuilder().add("name", name).add("config", builder).build();
+    return this.get(this.request.method(Request.POST).body().set(json).back().fetch().as(RestResponse.class).assertStatus(HttpURLConnection.HTTP_CREATED).as(JsonResponse.class).json().readObject().getInt("id"));
+  }
 }
--- src/test/java/com/jcabi/github/RtHooksTest.java
+++ src/test/java/com/jcabi/github/RtHooksTest.java
@@ -1,34 +1,4 @@
-/**
- * Copyright (c) 2012-2013, JCabi.com
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met: 1) Redistributions of source code must retain the above
- * copyright notice, this list of conditions and the following
- * disclaimer. 2) Redistributions in binary form must reproduce the above
- * copyright notice, this list of conditions and the following
- * disclaimer in the documentation and/or other materials provided
- * with the distribution. 3) Neither the name of the jcabi.com nor
- * the names of its contributors may be used to endorse or promote
- * products derived from this software without specific prior written
- * permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
- * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
- * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
- * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- * OF THE POSSIBILITY OF SUCH DAMAGE.
- */
 package com.jcabi.github;
-
 import com.jcabi.aspects.Immutable;
 import com.rexsl.test.Request;
 import com.rexsl.test.mock.MkAnswer;
@@ -37,187 +7,86 @@ import com.rexsl.test.mock.MkGrizzlyContainer;
 import com.rexsl.test.mock.MkQuery;
 import com.rexsl.test.request.JdkRequest;
 import java.net.HttpURLConnection;
-import java.util.Collections;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 import javax.json.Json;
 import javax.json.JsonObject;
-import javax.json.JsonObjectBuilder;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.mockito.Mockito;
+import java.util.Collections;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import javax.json.JsonObjectBuilder;
 
-/**
- * Test case for {@link RtHooks}.
- * @author Paul Polishchuk (ppol@ua.fm)
- * @version $Id$
- * @since 0.8
- * @checkstyle MultipleStringLiterals (500 lines)
- */
-@Immutable
-public final class RtHooksTest {
-
-    /**
-     * RtHooks can fetch empty list of hooks.
-     * @throws Exception if some problem inside
-     */
-    @Test
-    public void canFetchEmptyListOfHooks() throws Exception {
-        final MkContainer container = new MkGrizzlyContainer().next(
-            new MkAnswer.Simple(HttpURLConnection.HTTP_OK, "[]")
-        ).start();
-        final Hooks hooks = new RtHooks(
-            new JdkRequest(container.home()),
-            RtHooksTest.repo()
-        );
-        try {
-            MatcherAssert.assertThat(
-                hooks.iterate(),
-                Matchers.emptyIterable()
-            );
-        } finally {
-            container.stop();
-        }
+@Immutable public final class RtHooksTest {
+  @Test public void canFetchEmptyListOfHooks() throws Exception {
+    final MkContainer container = new MkGrizzlyContainer().next(new MkAnswer.Simple(HttpURLConnection.HTTP_OK, "[]")).start();
+    final Hooks hooks = new RtHooks(new JdkRequest(container.home()), RtHooksTest.repo());
+    try {
+      MatcherAssert.assertThat(hooks.iterate(), Matchers.emptyIterable());
+    }  finally {
+      container.stop();
     }
+  }
 
-    /**
-     * RtHooks can fetch non empty list of hooks.
-     *
-     * @todo #122 RtHooks should iterate multiple hooks. Let's implement
-     *  a test here and a method of RtHooks. The method should iterate
-     *  multiple hooks. See how it's done in other classes with GhPagination.
-     *  When done, remove this puzzle and Ignore annotation from the method.
-     */
-    @Test
-    @Ignore
-    public void canFetchNonEmptyListOfHooks() {
-        // to be implemented
-    }
+  @Test @Ignore public void canFetchNonEmptyListOfHooks() {
+  }
 
-    /**
-     * RtHooks can fetch single hook.
-     * @throws Exception if some problem inside
-     */
-    @Test
-    public void canFetchSingleHook() throws Exception {
-        final String name = "hook name";
-        final MkContainer container = new MkGrizzlyContainer().next(
-            new MkAnswer.Simple(
-                HttpURLConnection.HTTP_OK,
-                RtHooksTest.hook(
-                    name,
-                    Collections.<String, String>emptyMap()
-                ).toString()
-            )
-        ).start();
-        final Hooks hooks = new RtHooks(
-            new JdkRequest(container.home()),
-            RtHooksTest.repo()
-        );
-        final Hook hook = hooks.get(1);
-        MatcherAssert.assertThat(
-            new Hook.Smart(hook).name(),
-            Matchers.equalTo(name)
-        );
-        container.stop();
-    }
+  @Test public void canFetchSingleHook() throws Exception {
+    final String name = "hook name";
+    final MkContainer container = new MkGrizzlyContainer().next(new MkAnswer.Simple(HttpURLConnection.HTTP_OK, RtHooksTest.hook(name, Collections.<String, String>emptyMap()).toString())).start();
+    final Hooks hooks = new RtHooks(new JdkRequest(container.home()), RtHooksTest.repo());
+    final Hook hook = hooks.get(1);
+    MatcherAssert.assertThat(new Hook.Smart(hook).name(), Matchers.equalTo(name));
+    container.stop();
+  }
 
-    /**
-     * RtHooks can create a hook.
-     *
-     * @throws Exception if something goes wrong.
-     */
-    @Test
-    public void canCreateHook() throws Exception {
-        final String name = "hook name";
-        final ConcurrentHashMap<String, String> config =
-            new ConcurrentHashMap<String, String>(2);
-        config.put("url", "http://example.com");
-        config.put("content_type", "json");
-        final String body = RtHooksTest.hook(name, config).toString();
-        final MkContainer container = new MkGrizzlyContainer().next(
-            new MkAnswer.Simple(HttpURLConnection.HTTP_CREATED, body)
-        ).next(new MkAnswer.Simple(HttpURLConnection.HTTP_OK, body)).start();
-        final Hooks hooks = new RtHooks(
-            new JdkRequest(container.home()),
-            RtHooksTest.repo()
-        );
-        try {
-            final Hook hook = hooks.create(name, config);
-            MatcherAssert.assertThat(
-                container.take().method(),
-                Matchers.equalTo(Request.POST)
-            );
-            MatcherAssert.assertThat(
-                new Hook.Smart(hook).name(),
-                Matchers.equalTo(name)
-            );
-        } finally {
-            container.stop();
-        }
+  @Test public void canCreateHook() throws Exception {
+    final String name = "hook name";
+    final ConcurrentHashMap<String, String> config = new ConcurrentHashMap<String, String>(2);
+    config.put("url", "http://example.com");
+    config.put("content_type", "json");
+    final String body = RtHooksTest.hook(name, config).toString();
+    final MkContainer container = new MkGrizzlyContainer().next(new MkAnswer.Simple(HttpURLConnection.HTTP_CREATED, body)).next(new MkAnswer.Simple(HttpURLConnection.HTTP_OK, body)).start();
+    final Hooks hooks = new RtHooks(new JdkRequest(container.home()), RtHooksTest.repo());
+    try {
+      final Hook hook = hooks.create(name, config);
+      MatcherAssert.assertThat(container.take().method(), Matchers.equalTo(Request.POST));
+      MatcherAssert.assertThat(new Hook.Smart(hook).name(), Matchers.equalTo(name));
+    }  finally {
+      container.stop();
     }
+  }
 
-    /**
-     * RtHooks can delete a hook.
-     *
-     * @throws Exception if something goes wrong.
-     */
-    @Test
-    public void canDeleteHook() throws Exception {
-        final MkContainer container = new MkGrizzlyContainer().next(
-            new MkAnswer.Simple(HttpURLConnection.HTTP_NO_CONTENT, "")
-        ).start();
-        final Hooks hooks = new RtHooks(
-            new JdkRequest(container.home()),
-            RtHooksTest.repo()
-        );
-        hooks.remove(1);
-        try {
-            final MkQuery query = container.take();
-            MatcherAssert.assertThat(
-                query.method(),
-                Matchers.equalTo(Request.DELETE)
-            );
-            MatcherAssert.assertThat(
-                query.body(),
-                Matchers.isEmptyString()
-            );
-        } finally {
-            container.stop();
-        }
+  @Test public void canDeleteHook() throws Exception {
+    final MkContainer container = new MkGrizzlyContainer().next(new MkAnswer.Simple(HttpURLConnection.HTTP_NO_CONTENT, "")).start();
+    final Hooks hooks = new RtHooks(new JdkRequest(container.home()), RtHooksTest.repo());
+    hooks.remove(1);
+    try {
+      final MkQuery query = container.take();
+      MatcherAssert.assertThat(query.method(), Matchers.equalTo(Request.DELETE));
+      MatcherAssert.assertThat(query.body(), Matchers.isEmptyString());
+    }  finally {
+      container.stop();
     }
+  }
 
-    /**
-     * Create and return JsonObject to test.
-     * @param name Name of the hook
-     * @param config Config of hook
-     * @return JsonObject
-     * @throws Exception If some problem inside
-     */
-    private static JsonObject hook(final String name,
-        final Map<String, String> config)
-        throws Exception {
-        final JsonObjectBuilder builder = Json.createObjectBuilder();
-        for (final Map.Entry<String, String> entry : config.entrySet()) {
-            builder.add(entry.getKey(), entry.getValue());
-        }
-        return Json.createObjectBuilder()
-            .add("id", 1)
-            .add("name", name)
-            .add("config", builder)
-            .build();
-    }
+  private static JsonObject hook(final String name) throws Exception {
+    return Json.createObjectBuilder().add("name", name).build();
+  }
 
-    /**
-     * Create and return repo for testing.
-     * @return Repo
-     */
-    private static Repo repo() {
-        final Repo repo = Mockito.mock(Repo.class);
-        Mockito.doReturn(new Coordinates.Simple("test", "hooks"))
-            .when(repo).coordinates();
-        return repo;
+  private static Repo repo() {
+    final Repo repo = Mockito.mock(Repo.class);
+    Mockito.doReturn(new Coordinates.Simple("test", "hooks")).when(repo).coordinates();
+    return repo;
+  }
+
+  private static JsonObject hook(final String name, final Map<String, String> config) throws Exception {
+    final JsonObjectBuilder builder = Json.createObjectBuilder();
+    for (final Map.Entry<String, String> entry : config.entrySet()) {
+      builder.add(entry.getKey(), entry.getValue());
     }
+    return Json.createObjectBuilder().add("id", 1).add("name", name).add("config", builder).build();
+  }
 }
