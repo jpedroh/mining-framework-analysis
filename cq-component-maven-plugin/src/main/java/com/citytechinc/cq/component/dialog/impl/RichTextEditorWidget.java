69336076043a5ba6033a5d1a6718e80530a4e2ff
jphsd <jphsd@inesserver2.cin.ufpe.br>
2024-11-05 13:36:53 -0300

    Mining Framework Analysis

--- cq-component-maven-plugin/src/main/java/com/citytechinc/cq/component/dialog/impl/RichTextEditorWidget.java
+++ cq-component-maven-plugin/src/main/java/com/citytechinc/cq/component/dialog/impl/RichTextEditorWidget.java
@@ -1,44 +1,17 @@
 package com.citytechinc.cq.component.dialog.impl;
-
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-
 import com.citytechinc.cq.component.annotations.config.Widget;
 import com.citytechinc.cq.component.dialog.AbstractWidget;
 import com.citytechinc.cq.component.dialog.DialogElement;
 
-@Widget(annotationClass="com.citytechinc.cq.component.annotations.widgets.RichTextEditor", makerClass = "com.citytechinc.cq.component.dialog.maker.impl.RichTextEditorMaker", xtypes = "richtext")
-public class RichTextEditorWidget extends AbstractWidget {
+@Widget(annotationClass = "com.citytechinc.cq.component.annotations.widgets.RichTextEditor", makerClass = "com.citytechinc.cq.component.dialog.maker.impl.RichTextEditorMaker", xtypes = "richtext") public class RichTextEditorWidget extends AbstractWidget {
+  public static final String XTYPE = "richtext";
 
-	public static final String XTYPE = "richtext";
-	private static final String PRIMARY_TYPE = "cq:Widget";
+  private static final String PRIMARY_TYPE = "cq:Widget";
 
-	public RichTextEditorWidget(
-			String fieldLabel,
-			String fieldDescription,
-			boolean allowBlank,
-			boolean hideLabel,
-			String defaultValue,
-			String name,
-			String fieldName,
-			Map<String, String> additionalProperties,
-			List<DialogElement> rtePlugins) {
-
-		super(
-				XTYPE,
-				fieldLabel,
-				fieldDescription,
-				allowBlank,
-				hideLabel,
-				defaultValue,
-				name,
-				PRIMARY_TYPE,
-				null,
-				fieldName,
-				additionalProperties,
-				Arrays.asList(new DialogElement[]{new RtePlugins(rtePlugins)}));
-
-	}
-
+  public RichTextEditorWidget(String fieldLabel, String fieldDescription, boolean allowBlank, boolean hideLabel, String defaultValue, String name, String fieldName, Map<String, String> additionalProperties, List<DialogElement> rtePlugins) {
+    super(XTYPE, fieldLabel, fieldDescription, allowBlank, hideLabel, defaultValue, name, PRIMARY_TYPE, null, fieldName, additionalProperties, Arrays.asList(new DialogElement[] { new RtePlugins(rtePlugins) }));
+  }
 }
