9ccf0c385d16695fd515b56f633c9bc2e77405a7
Joao Duarte <jphsd@cin.ufpe.br>
2024-11-05 10:17:19 -0300

    Mining Framework Analysis

--- yoga-demos/yoga-demo-springmvc/src/main/java/org/skyscreamer/yoga/demo/controller/AbstractController.java
+++ yoga-demos/yoga-demo-springmvc/src/main/java/org/skyscreamer/yoga/demo/controller/AbstractController.java
@@ -1,41 +1,33 @@
 package org.skyscreamer.yoga.demo.controller;
-
-import org.hibernate.ObjectNotFoundException;
 import org.skyscreamer.yoga.demo.dao.GenericDao;
+import java.lang.reflect.ParameterizedType;
+import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import java.lang.reflect.ParameterizedType;
-
 /**
  * Created by IntelliJ IDEA.
  * User: Carter Page
  */
-public abstract class AbstractController<T>
-{
-	@Autowired GenericDao _genericDao;
-	
-	Class<T> _entityClass = returnedClass();
+public abstract class AbstractController<T extends java.lang.Object> {
+  @Autowired GenericDao _genericDao;
 
-	@RequestMapping("/{id}")
-    public T get( @PathVariable long id )
-    {
-		return _genericDao.find( _entityClass, id);
-    }
+  Class<T> _entityClass = returnedClass();
 
-    // http://blog.xebia.com/2009/02/acessing-generic-types-at-runtime-in-java/
-    @SuppressWarnings("unchecked")
-	private Class<T> returnedClass()
-    {
-        ParameterizedType parameterizedType = (ParameterizedType) getClass().getGenericSuperclass();
-        return (Class<T>) parameterizedType.getActualTypeArguments()[0];
-    }
-    
-    @ExceptionHandler(ObjectNotFoundException.class)
-    @ResponseStatus(value=HttpStatus.NOT_FOUND,reason="No such resource")
-    public void notFound() { }
+  @RequestMapping(value = "/{id}") public T get(@PathVariable long id) {
+    return _genericDao.find(_entityClass, id);
+  }
+
+  @SuppressWarnings(value = { "unchecked" }) private Class<T> returnedClass() {
+    ParameterizedType parameterizedType = (ParameterizedType) getClass().getGenericSuperclass();
+    return (Class<T>) parameterizedType.getActualTypeArguments()[0];
+  }
+
+  @ExceptionHandler(value = ObjectNotFoundException.class) @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "No such resource") public void notFound() {
+  }
 }
