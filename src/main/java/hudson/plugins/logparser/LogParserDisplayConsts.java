f477d566f68dafff8f7e9caae60323a929a52c51
Joao Duarte <jphsd@cin.ufpe.br>
2024-11-04 23:55:21 -0300

    Mining Framework Analysis

--- src/main/java/hudson/plugins/logparser/LogParserDisplayConsts.java
+++ src/main/java/hudson/plugins/logparser/LogParserDisplayConsts.java
@@ -1,66 +1,61 @@
 package hudson.plugins.logparser;
-
 import java.util.HashMap;
 import org.apache.commons.lang.WordUtils;
 
 public class LogParserDisplayConsts {
+  final private HashMap<String, String> colorTable = new HashMap<>();
 
-    final private HashMap<String, String> colorTable = new HashMap<>();
-    final private HashMap<String, String> iconTable = new HashMap<>();
-    final private HashMap<String, String> linkListDisplay = new HashMap<>();
-    final private HashMap<String, String> linkListDisplayPlural = new HashMap<>();
+  final private HashMap<String, String> iconTable = new HashMap<>();
 
-    public static final String DEFAULT_COLOR = "blue";
-    public static final String DEFAULT_ICON = "blue.gif";
+  final private HashMap<String, String> linkListDisplay = new HashMap<>();
 
-    public static String getDefaultLinkListDisplay(String status) {
-        return WordUtils.capitalize(status);
-    }
+  final private HashMap<String, String> linkListDisplayPlural = new HashMap<>();
 
-    public static String getDefaultLinkListDisplayPlural(String status) {
-        return getDefaultLinkListDisplay(status) + "s";
-    }
+  public static final String DEFAULT_COLOR = "blue";
 
-    public LogParserDisplayConsts() {
-        // Color of each status
-        colorTable.put(LogParserConsts.ERROR, "red");
-        colorTable.put(LogParserConsts.WARNING, "orange");
-        colorTable.put(LogParserConsts.INFO, "blue");
-        colorTable.put(LogParserConsts.START, "blue");
-        colorTable.put(LogParserConsts.DEBUG, "grey");
+  public static final String DEFAULT_ICON = "blue.gif";
 
-        // Icon for each status in the summary
-        iconTable.put(LogParserConsts.ERROR, "red.gif");
-        iconTable.put(LogParserConsts.WARNING, "yellow.gif");
-        iconTable.put(LogParserConsts.INFO, "blue.gif");
-        iconTable.put(LogParserConsts.DEBUG, "grey.gif");
+  public static String getDefaultLinkListDisplay(String status) {
+    return WordUtils.capitalize(status);
+  }
 
-        // How to display in link summary html
-        linkListDisplay.put(LogParserConsts.ERROR, "Error");
-        linkListDisplay.put(LogParserConsts.WARNING, "Warning");
-        linkListDisplay.put(LogParserConsts.INFO, "Info");
-        linkListDisplay.put(LogParserConsts.DEBUG, "Debug");
+  public static String getDefaultLinkListDisplayPlural(String status) {
+    return getDefaultLinkListDisplay(status) + "s";
+  }
 
-        linkListDisplayPlural.put(LogParserConsts.ERROR, "Errors");
-        linkListDisplayPlural.put(LogParserConsts.WARNING, "Warnings");
-        linkListDisplayPlural.put(LogParserConsts.INFO, "Infos");
-        linkListDisplayPlural.put(LogParserConsts.DEBUG, "Debugs");
-    }
+  public LogParserDisplayConsts() {
+    colorTable.put(LogParserConsts.ERROR, "red");
+    colorTable.put(LogParserConsts.WARNING, "orange");
+    colorTable.put(LogParserConsts.INFO, "blue");
+    colorTable.put(LogParserConsts.START, "blue");
+    colorTable.put(LogParserConsts.DEBUG, "grey");
+    iconTable.put(LogParserConsts.ERROR, "red.gif");
+    iconTable.put(LogParserConsts.WARNING, "yellow.gif");
+    iconTable.put(LogParserConsts.INFO, "blue.gif");
+    iconTable.put(LogParserConsts.DEBUG, "grey.gif");
+    linkListDisplay.put(LogParserConsts.ERROR, "Error");
+    linkListDisplay.put(LogParserConsts.WARNING, "Warning");
+    linkListDisplay.put(LogParserConsts.INFO, "Info");
+    linkListDisplay.put(LogParserConsts.DEBUG, "Debug");
+    linkListDisplayPlural.put(LogParserConsts.ERROR, "Errors");
+    linkListDisplayPlural.put(LogParserConsts.WARNING, "Warnings");
+    linkListDisplayPlural.put(LogParserConsts.INFO, "Infos");
+    linkListDisplayPlural.put(LogParserConsts.DEBUG, "Debugs");
+  }
 
-    public HashMap<String, String> getColorTable() {
-        return colorTable;
-    }
+  public HashMap<String, String> getColorTable() {
+    return colorTable;
+  }
 
-    public HashMap<String, String> getIconTable() {
-        return iconTable;
-    }
+  public HashMap<String, String> getIconTable() {
+    return iconTable;
+  }
 
-    public HashMap<String, String> getLinkListDisplay() {
-        return linkListDisplay;
-    }
+  public HashMap<String, String> getLinkListDisplay() {
+    return linkListDisplay;
+  }
 
-    public HashMap<String, String> getLinkListDisplayPlural() {
-        return linkListDisplayPlural;
-    }
-
+  public HashMap<String, String> getLinkListDisplayPlural() {
+    return linkListDisplayPlural;
+  }
 }
--- src/main/java/hudson/plugins/logparser/LogParserStatusComputer.java
+++ src/main/java/hudson/plugins/logparser/LogParserStatusComputer.java
@@ -1,10 +1,7 @@
 package hudson.plugins.logparser;
-
 import hudson.FilePath;
-import hudson.remoting.VirtualChannel;
 import hudson.remoting.RemoteInputStream;
 import jenkins.security.MasterToSlaveCallable;
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
@@ -20,135 +17,94 @@ import java.util.logging.Logger;
 import java.util.regex.Pattern;
 
 public class LogParserStatusComputer extends MasterToSlaveCallable<HashMap<String, String>, RuntimeException> {
+  private static final long serialVersionUID = 1L;
 
-    private static final long serialVersionUID = 1L;
-    final private String[] parsingRulesArray;
-    final private Pattern[] compiledPatterns;
-    private final InputStream remoteLog;
-    private final String signature;
+  final private String[] parsingRulesArray;
 
-    public LogParserStatusComputer(
-            final InputStream log, final String[] parsingRulesArray,
-            final Pattern[] compiledPatterns,
-            final String signature) throws IOException, InterruptedException {
-        this.parsingRulesArray = parsingRulesArray;
-        this.compiledPatterns = compiledPatterns;
-        this.remoteLog = new RemoteInputStream(log, RemoteInputStream.Flag.GREEDY);
-        this.signature = signature;
+  final private Pattern[] compiledPatterns;
+
+  final private InputStream remoteLog;
+
+  private final String signature;
+
+  public LogParserStatusComputer(final InputStream log, final String[] parsingRulesArray, final Pattern[] compiledPatterns, final String signature) throws IOException, InterruptedException {
+    this.parsingRulesArray = parsingRulesArray;
+    this.compiledPatterns = compiledPatterns;
+    this.remoteLog = new RemoteInputStream(log, RemoteInputStream.Flag.GREEDY);
+    this.signature = signature;
+  }
+
+  public HashMap<String, String> call() {
+    try {
+      return computeStatusMatches(remoteLog, signature);
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
     }
+  }
 
-    public HashMap<String, String> call() {
-        try {
-            return computeStatusMatches(remoteLog, signature);
-            // rethrow any exception here to report why the
-            // parsing failed
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+  private HashMap<String, String> computeStatusMatches(final InputStream log, final String signature) throws IOException, InterruptedException {
+    final Logger logger = Logger.getLogger(this.getClass().getName());
+    String tempDir = System.getProperty("java.io.tmpdir");
+    if (!tempDir.endsWith(File.separator)) {
+      final StringBuffer tempDirBuffer = new StringBuffer(tempDir);
+      tempDirBuffer.append(File.separator);
+      tempDir = tempDirBuffer.toString();
     }
-
-    private HashMap<String, String> computeStatusMatches(
-            final InputStream log,
-            final String signature) throws IOException, InterruptedException {
-        // SLAVE PART START
-
-        final Logger logger = Logger.getLogger(this.getClass().getName());
-
-        // Copy remote file to temp local location
-        String tempDir = System.getProperty("java.io.tmpdir");
-        if (!tempDir.endsWith(File.separator)) {
-            final StringBuffer tempDirBuffer = new StringBuffer(tempDir);
-            tempDirBuffer.append(File.separator);
-            tempDir = tempDirBuffer.toString();
-        }
-
-        final String tempFileLocation = tempDir + "log-parser_" + signature;
-        final File tempFile = new File(tempFileLocation);
-        final FilePath tempFilePath = new FilePath(tempFile);
-        tempFilePath.copyFrom(log);
-
-        logger.log(Level.INFO, "Local temp file:" + tempFileLocation);
-
-        final BufferedReader reader = new BufferedReader(new InputStreamReader(
-                tempFilePath.read()));
-        int threadCounter = 0;
-
-        final ArrayList<LogParserThread> runners = new ArrayList<>();
-        final LogParserReader logParserReader = new LogParserReader(reader);
-
-        //ExecutorService execSvc = Executors.newFixedThreadPool(
-        //    LogParserUtils.getNumThreads() );
-        final ExecutorService execSvc = Executors.newCachedThreadPool();
-        int linesInLog = LogParserUtils.countLines(tempFileLocation);
-        final int threadsNeeded = linesInLog
-                / LogParserUtils.getLinesPerThread() + 1;
-
-        // Read and parse the log parts.  Keep the threads and results in an
-        // array for future reference when writing
-        for (int i = 0; i < threadsNeeded; i++) {
-            //logger.log(Level.INFO,"LogParserParser: Open thread #"+threadCounter);
-            final LogParserThread logParserThread = new LogParserThread(
-                    logParserReader, parsingRulesArray, compiledPatterns,
-                    threadCounter);
-            //logParserThread.start();
-            runners.add(logParserThread);
-            execSvc.execute(logParserThread);
-            threadCounter++;
-        }
-
-        // Wait for all threads to finish before sequentially writing the
-        // outcome
-        execSvc.shutdown();
-        execSvc.awaitTermination(3600, TimeUnit.SECONDS);
-
-        // Sort the threads in the order of the log parts they read
-        // It could be that thread #1 read log part #2 and thread #2 read log
-        // part #1
-
-        final int runnersSize = runners.size();
-        LogParserThread[] sortedRunners = new LogParserThread[runnersSize];
-        for (LogParserThread logParserThread : runners) {
-            final LogParserLogPart logPart = logParserThread.getLogPart();
-            if (logPart != null) {
-                final int logPartNum = logPart.getLogPartNum();
-                sortedRunners[logPartNum] = logParserThread;
-            }
-        }
-
-        final HashMap<String, String> result = new HashMap<>();
-        HashMap<String, String> moreLineStatusMatches;
-        for (int i = 0; i < runnersSize; i++) {
-            final LogParserThread logParserThread = sortedRunners[i];
-            if (logParserThread != null) {
-                moreLineStatusMatches = getLineStatusMatches(
-                        logParserThread.getLineStatuses(), i);
-                result.putAll(moreLineStatusMatches);
-            }
-        }
-
-        reader.close(); // Close to unlock.
-
-        // Delete temp file
-        tempFilePath.delete();
-
-        return result;
-        // SLAVE PART END
+    final String tempFileLocation = tempDir + "log-parser_" + signature;
+    final File tempFile = new File(tempFileLocation);
+    final FilePath tempFilePath = new FilePath(tempFile);
+    tempFilePath.copyFrom(log);
+    logger.log(Level.INFO, "Local temp file:" + tempFileLocation);
+    final BufferedReader reader = new BufferedReader(new InputStreamReader(tempFilePath.read()));
+    int threadCounter = 0;
+    final ArrayList<LogParserThread> runners = new ArrayList<>();
+    final LogParserReader logParserReader = new LogParserReader(reader);
+    final ExecutorService execSvc = Executors.newCachedThreadPool();
+    int linesInLog = LogParserUtils.countLines(tempFileLocation);
+    final int threadsNeeded = linesInLog / LogParserUtils.getLinesPerThread() + 1;
+    for (int i = 0; i < threadsNeeded; i++) {
+      final LogParserThread logParserThread = new LogParserThread(logParserReader, parsingRulesArray, compiledPatterns, threadCounter);
+      runners.add(logParserThread);
+      execSvc.execute(logParserThread);
+      threadCounter++;
     }
-
-    private HashMap<String, String> getLineStatusMatches(
-            final String[] statuses, final int logPart) {
-        final HashMap<String, String> result = new HashMap<>();
-        String status;
-        int line_num;
-        final int linesPerThread = LogParserUtils.getLinesPerThread();
-        for (int i = 0; i < statuses.length; i++) {
-            status = statuses[i];
-            line_num = i + logPart * linesPerThread;
-            result.put(String.valueOf(line_num), status);
-        }
-        return result;
+    execSvc.shutdown();
+    execSvc.awaitTermination(3600, TimeUnit.SECONDS);
+    final int runnersSize = runners.size();
+    LogParserThread[] sortedRunners = new LogParserThread[runnersSize];
+    for (LogParserThread logParserThread : runners) {
+      final LogParserLogPart logPart = logParserThread.getLogPart();
+      if (logPart != null) {
+        final int logPartNum = logPart.getLogPartNum();
+        sortedRunners[logPartNum] = logParserThread;
+      }
     }
+    final HashMap<String, String> result = new HashMap<>();
+    HashMap<String, String> moreLineStatusMatches;
+    for (int i = 0; i < runnersSize; i++) {
+      final LogParserThread logParserThread = sortedRunners[i];
+      if (logParserThread != null) {
+        moreLineStatusMatches = getLineStatusMatches(logParserThread.getLineStatuses(), i);
+        result.putAll(moreLineStatusMatches);
+      }
+    }
+    reader.close();
+    tempFilePath.delete();
+    return result;
+  }
 
+  private HashMap<String, String> getLineStatusMatches(final String[] statuses, final int logPart) {
+    final HashMap<String, String> result = new HashMap<>();
+    String status;
+    int line_num;
+    final int linesPerThread = LogParserUtils.getLinesPerThread();
+    for (int i = 0; i < statuses.length; i++) {
+      status = statuses[i];
+      line_num = i + logPart * linesPerThread;
+      result.put(String.valueOf(line_num), status);
+    }
+    return result;
+  }
 }
