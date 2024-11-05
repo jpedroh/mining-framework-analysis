ea67cfe25be46f5eca3ef0b54acfa39b20322f51
Joao Duarte <jphsd@cin.ufpe.br>
2024-11-04 23:41:57 -0300

    Mining Framework Analysis

--- src/main/java/com/ning/billing/recurly/model/Coupon.java
+++ src/main/java/com/ning/billing/recurly/model/Coupon.java
@@ -1,271 +1,172 @@
-/*
- * Copyright 2010-2013 Ning, Inc.
- *
- * Ning licenses this file to you under the Apache License, version 2.0
- * (the "License"); you may not use this file except in compliance with the
- * License.  You may obtain a copy of the License at:
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
- * License for the specific language governing permissions and limitations
- * under the License.
- */
-
 package com.ning.billing.recurly.model;
-
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
-
 import org.joda.time.DateTime;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-/**
- * Class that represents the Concept of a Coupon within the Recurly API.
- */
-@XmlRootElement(name = "coupon")
-public class Coupon extends RecurlyObject {
+@XmlRootElement(name = "coupon") public class Coupon extends RecurlyObject {
+  @XmlTransient public static final String COUPON_RESOURCE = "/coupons";
 
-    @XmlTransient
-    public static final String COUPON_RESOURCE = "/coupons";
-    @XmlTransient
-    private String href;
+  @XmlElement(name = "name") private String name;
 
-    @XmlElement(name = "name")
-    private String name;
+  @XmlElement(name = "coupon_code") private String couponCode;
 
-    @XmlElement(name = "coupon_code")
-    private String couponCode;
+  @XmlElement(name = "discount_type") private String discountType;
 
-    /**
-     * Last date to redeem the coupon, defaults to no date
-     */
-    @XmlElement(name = "redeem_by_date")
-    private DateTime redeemByDate;
+  @XmlElement(name = "discount_percent") private Integer discountPercent;
 
-    /**
-     * Number of months after redemption that the coupon is valid, defaults to no date
-     */
-    @XmlElement(name = "applies_for_months")
-    private Integer appliesForMonths;
+  public String getName() {
+    return name;
+  }
 
-    /**
-     * Maximum number of accounts that may use the coupon before it can no longer be redeemed
-     */
-    @XmlElement(name = "max_redemptions")
-    private Integer maxRedemptions;
+  public void setName(final Object name) {
+    this.name = stringOrNull(name);
+  }
 
-    /**
-     * The coupon is valid for all plans if true, defaults to true
-     */
-    @XmlElement(name = "applies_to_all_plans")
-    private Boolean appliesToAllPlans;
+  public String getCouponCode() {
+    return couponCode;
+  }
 
-    /**
-     * If true, the coupon applies to the first invoice only
-     */
-    @XmlElement(name = "single_use")
-    private Boolean singleUse;
+  public void setCouponCode(final Object couponCode) {
+    this.couponCode = stringOrNull(couponCode);
+  }
 
-    /**
-     * "percent" or "dollars"
-     */
-    @XmlElement(name = "discount_type")
-    private String discountType;
+  public void setDiscountType(final Object discountType) {
+    this.discountType = stringOrNull(discountType);
+  }
 
-    /**
-     * Discount percentage if discount_type is "percent"
-     */
-    @XmlElement(name = "discount_percent")
-    private Integer discountPercent;
+  public String getDiscountType() {
+    return discountType;
+  }
 
-    @XmlElement(name = "discount_in_cents")
-    private Integer discountInCents;
+  public Integer getDiscountPercent() {
+    return discountPercent;
+  }
 
-    @XmlElement(name = "state")
-    private String state;
+  public void setDiscountPercent(final Object discountPercent) {
+    this.discountPercent = integerOrNull(discountPercent);
+  }
 
-    @JsonIgnore
-    public String getHref() {
-        return href;
+  @Override public String toString() {
+    final StringBuilder sb = new StringBuilder();
+    sb.append("Coupon");
+    sb.append("{name=\'").append(name).append('\'');
+    sb.append(", couponCode=\'").append(couponCode).append('\'');
+    sb.append(", discountType=\'").append(discountType).append('\'');
+    sb.append(", discountPercent=\'").append(discountPercent).append('\'');
+    sb.append('}');
+    return sb.toString();
+  }
+
+  @Override public boolean equals(final Object o) {
+    if (this == o) {
+      return true;
     }
-
-    public void setHref(final Object href) {
-        this.href = stringOrNull(href);
+    if (o == null || getClass() != o.getClass()) {
+      return false;
     }
-
-    public String getState() {
-        return state;
+    final Coupon coupon = (Coupon) o;
+    if (couponCode != null ? !couponCode.equals(coupon.couponCode) : coupon.couponCode != null) {
+      return false;
     }
-
-    public void setState(final Object state) {
-        this.state = stringOrNull(state);
+    if (discountPercent != null ? !discountPercent.equals(coupon.discountPercent) : coupon.discountPercent != null) {
+      return false;
     }
-
-    /**
-     * Gets the name of the {@link Coupon}
-     *
-     * @return The {@link Coupon} name
-     */
-    public String getName() {
-        return name;
+    if (discountType != null ? !discountType.equals(coupon.discountType) : coupon.discountType != null) {
+      return false;
     }
-
-    /**
-     * Sets the name of the {@link Coupon}
-     *
-     * @param name The Name that is to be given to the {@link Coupon}
-     */
-    public void setName(final Object name) {
-        this.name = stringOrNull(name);
+    if (name != null ? !name.equals(coupon.name) : coupon.name != null) {
+      return false;
     }
+    return true;
+  }
 
-    /**
-     * Gets the coupon code for a {@link Coupon}
-     *
-     * @return The coupon code for the {@link Coupon}
-     */
-    public String getCouponCode() {
-        return couponCode;
-    }
+  @Override public int hashCode() {
+    int result = name != null ? name.hashCode() : 0;
+    result = 31 * result + (couponCode != null ? couponCode.hashCode() : 0);
+    result = 31 * result + (discountType != null ? discountType.hashCode() : 0);
+    result = 31 * result + (discountPercent != null ? discountPercent.hashCode() : 0);
+    return result;
+  }
 
-    /**
-     * Sets the coupon code for the {@link Coupon}
-     *
-     * @param couponCode The coupon code
-     */
-    public void setCouponCode(final Object couponCode) {
-        this.couponCode = stringOrNull(couponCode);
-    }
+  @XmlTransient private String href;
 
-    /**
-     * Sets the discount type for a {@link Coupon}
-     *
-     * @param discountType A String of: 'percent'; 'dollars';
-     */
-    public void setDiscountType(final Object discountType) {
-        this.discountType = stringOrNull(discountType);
-    }
+  @XmlElement(name = "redeem_by_date") private DateTime redeemByDate;
 
-    /**
-     * Gets the discount type associated with the {@link Coupon}
-     *
-     * @return A String defining the discount type: 'percent' or 'dollars'.
-     */
-    public String getDiscountType() {
-        return discountType;
-    }
+  @XmlElement(name = "applies_for_months") private Integer appliesForMonths;
 
-    /**
-     * Gets the percentage discount for a coupon
-     *
-     * @return The percentage
-     */
-    public Integer getDiscountPercent() {
-        return discountPercent;
-    }
+  @XmlElement(name = "max_redemptions") private Integer maxRedemptions;
 
-    public void setDiscountPercent(final Object discountPercent) {
-        this.discountPercent = integerOrNull(discountPercent);
-    }
+  @XmlElement(name = "applies_to_all_plans") private Boolean appliesToAllPlans;
 
-    public DateTime getRedeemByDate() {
-        return redeemByDate;
-    }
+  @XmlElement(name = "single_use") private Boolean singleUse;
 
-    public void setRedeemByDate(final Object redeemByDate) {
-        this.redeemByDate = dateTimeOrNull(redeemByDate);
-    }
+  @XmlElement(name = "discount_in_cents") private Integer discountInCents;
 
-    public Integer getAppliesForMonths() {
-        return appliesForMonths;
-    }
+  @XmlElement(name = "state") private String state;
 
-    public void setAppliesForMonths(final Object appliesForMonths) {
-        this.appliesForMonths = integerOrNull(appliesForMonths);
-    }
+  @JsonIgnore public String getHref() {
+    return href;
+  }
 
-    public Integer getMaxRedemptions() {
-        return maxRedemptions;
-    }
+  public void setHref(final Object href) {
+    this.href = stringOrNull(href);
+  }
 
-    public void setMaxRedemptions(final Object maxRedemptions) {
-        this.maxRedemptions = integerOrNull(maxRedemptions);
-    }
+  public String getState() {
+    return state;
+  }
 
-    public Boolean getSingleUse() {
-        return singleUse;
-    }
+  public void setState(final Object state) {
+    this.state = stringOrNull(state);
+  }
 
-    public void setSingleUse(final Object singleUse) {
-        this.singleUse = booleanOrNull(singleUse);
-    }
+  public DateTime getRedeemByDate() {
+    return redeemByDate;
+  }
 
-    public Integer getDiscountInCents() {
-        return discountInCents;
-    }
+  public void setRedeemByDate(final Object redeemByDate) {
+    this.redeemByDate = dateTimeOrNull(redeemByDate);
+  }
 
-    public void setDiscountInCents(final Object discountInCents) {
-        this.discountInCents = integerOrNull(discountInCents);
-    }
+  public Integer getAppliesForMonths() {
+    return appliesForMonths;
+  }
 
-    public Boolean getAppliesToAllPlans() {
-        return appliesToAllPlans;
-    }
+  public void setAppliesForMonths(final Object appliesForMonths) {
+    this.appliesForMonths = integerOrNull(appliesForMonths);
+  }
 
-    public void setAppliesToAllPlans(final Object appliesToAllPlans) {
-        this.appliesToAllPlans = booleanOrNull(appliesToAllPlans);
-    }
+  public Integer getMaxRedemptions() {
+    return maxRedemptions;
+  }
 
-    @Override
-    public String toString() {
-        final StringBuilder sb = new StringBuilder();
-        sb.append("Coupon");
-        sb.append("{name='").append(name).append('\'');
-        sb.append(", couponCode='").append(couponCode).append('\'');
-        sb.append(", discountType='").append(discountType).append('\'');
-        sb.append(", discountPercent='").append(discountPercent).append('\'');
-        sb.append('}');
-        return sb.toString();
-    }
+  public void setMaxRedemptions(final Object maxRedemptions) {
+    this.maxRedemptions = integerOrNull(maxRedemptions);
+  }
 
-    @Override
-    public boolean equals(final Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
+  public Boolean getSingleUse() {
+    return singleUse;
+  }
 
-        final Coupon coupon = (Coupon) o;
+  public void setSingleUse(final Object singleUse) {
+    this.singleUse = booleanOrNull(singleUse);
+  }
 
-        if (couponCode != null ? !couponCode.equals(coupon.couponCode) : coupon.couponCode != null) {
-            return false;
-        }
-        if (discountPercent != null ? !discountPercent.equals(coupon.discountPercent) : coupon.discountPercent != null) {
-            return false;
-        }
-        if (discountType != null ? !discountType.equals(coupon.discountType) : coupon.discountType != null) {
-            return false;
-        }
-        if (name != null ? !name.equals(coupon.name) : coupon.name != null) {
-            return false;
-        }
+  public Integer getDiscountInCents() {
+    return discountInCents;
+  }
 
-        return true;
-    }
+  public void setDiscountInCents(final Object discountInCents) {
+    this.discountInCents = integerOrNull(discountInCents);
+  }
 
-    @Override
-    public int hashCode() {
-        int result = name != null ? name.hashCode() : 0;
-        result = 31 * result + (couponCode != null ? couponCode.hashCode() : 0);
-        result = 31 * result + (discountType != null ? discountType.hashCode() : 0);
-        result = 31 * result + (discountPercent != null ? discountPercent.hashCode() : 0);
-        return result;
-    }
+  public Boolean getAppliesToAllPlans() {
+    return appliesToAllPlans;
+  }
+
+  public void setAppliesToAllPlans(final Object appliesToAllPlans) {
+    this.appliesToAllPlans = booleanOrNull(appliesToAllPlans);
+  }
 }
--- src/main/java/com/ning/billing/recurly/model/Transaction.java
+++ src/main/java/com/ning/billing/recurly/model/Transaction.java
@@ -1,177 +1,144 @@
-/*
- * Copyright 2010-2013 Ning, Inc.
- *
- * Ning licenses this file to you under the Apache License, version 2.0
- * (the "License"); you may not use this file except in compliance with the
- * License.  You may obtain a copy of the License at:
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
- * License for the specific language governing permissions and limitations
- * under the License.
- */
-
 package com.ning.billing.recurly.model;
-
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
-
 import org.joda.time.DateTime;
 
-@XmlRootElement(name = "transaction")
-public class Transaction extends AbstractTransaction {
+@XmlRootElement(name = "transaction") public class Transaction extends AbstractTransaction {
+  @XmlElement(name = "account") private Account account;
 
-    @XmlElement(name = "account")
-    private Account account;
+  @XmlElement(name = "invoice") private Invoice invoice;
 
-    @XmlElement(name = "invoice")
-    private Invoice invoice;
+  @XmlElement(name = "subscription") private String subscription;
 
-    @XmlElement(name = "subscription")
-    private String subscription;
+  @XmlElement(name = "uuid") private String uuid;
 
-    @XmlElement(name = "uuid")
-    private String uuid;
+  @XmlElement(name = "tax_in_cents") private Integer taxInCents;
 
-    @XmlElement(name = "tax_in_cents")
-    private Integer taxInCents;
+  @XmlElement(name = "currency") private String currency;
 
-    @XmlElement(name = "currency")
-    private String currency;
+  @XmlElement(name = "created_at") private DateTime createdAt;
 
-    @XmlElement(name = "created_at")
-    private DateTime createdAt;
+  public Account getAccount() {
+    return account;
+  }
 
-    public Account getAccount() {
-        return account;
+  public void setAccount(final Account account) {
+    this.account = account;
+  }
+
+  public Invoice getInvoice() {
+    return invoice;
+  }
+
+  public void setInvoice(final Invoice invoice) {
+    this.invoice = invoice;
+  }
+
+  public String getSubscription() {
+    return subscription;
+  }
+
+  public void setSubscription(final Object subscription) {
+    this.subscription = stringOrNull(subscription);
+  }
+
+  public String getUuid() {
+    return uuid;
+  }
+
+  public void setUuid(final Object uuid) {
+    this.uuid = stringOrNull(uuid);
+  }
+
+  public Integer getTaxInCents() {
+    return taxInCents;
+  }
+
+  public void setTaxInCents(final Object taxInCents) {
+    this.taxInCents = integerOrNull(taxInCents);
+  }
+
+  public String getCurrency() {
+    return currency;
+  }
+
+  public void setCurrency(final Object currency) {
+    this.currency = stringOrNull(currency);
+  }
+
+  public DateTime getCreatedAt() {
+    return createdAt;
+  }
+
+  public void setCreatedAt(final Object createdAt) {
+    this.createdAt = dateTimeOrNull(createdAt);
+  }
+
+  @Override public String toString() {
+    final StringBuilder sb = new StringBuilder();
+    sb.append("Transaction");
+    sb.append("{account=").append(account);
+    sb.append(", invoice=").append(invoice);
+    sb.append(", subscription=\'").append(subscription).append('\'');
+    sb.append(", uuid=\'").append(uuid).append('\'');
+    sb.append(", action=\'").append(action).append('\'');
+    sb.append(", amountInCents=").append(amountInCents);
+    sb.append(", taxInCents=").append(taxInCents);
+    sb.append(", currency=\'").append(currency).append('\'');
+    sb.append(", status=\'").append(status).append('\'');
+    sb.append(", reference=\'").append(reference).append('\'');
+    sb.append(", test=").append(test);
+    sb.append(", voidable=").append(voidable);
+    sb.append(", refundable=").append(refundable);
+    sb.append(", createdAt=").append(createdAt);
+    sb.append('}');
+    return sb.toString();
+  }
+
+  @Override public boolean equals(final Object o) {
+    if (this == o) {
+      return true;
     }
-
-    public void setAccount(final Account account) {
-        this.account = account;
+    if (!(o instanceof Transaction)) {
+      return false;
     }
-
-    public Invoice getInvoice() {
-        return invoice;
+    if (!super.equals(o)) {
+      return false;
     }
-
-    public void setInvoice(final Invoice invoice) {
-        this.invoice = invoice;
+    final Transaction that = (Transaction) o;
+    if (account != null ? !account.equals(that.account) : that.account != null) {
+      return false;
     }
-
-    public String getSubscription() {
-        return subscription;
+    if (createdAt != null ? !createdAt.equals(that.createdAt) : that.createdAt != null) {
+      return false;
     }
-
-    public void setSubscription(final Object subscription) {
-        this.subscription = stringOrNull(subscription);
+    if (currency != null ? !currency.equals(that.currency) : that.currency != null) {
+      return false;
     }
-
-    public String getUuid() {
-        return uuid;
+    if (invoice != null ? !invoice.equals(that.invoice) : that.invoice != null) {
+      return false;
     }
-
-    public void setUuid(final Object uuid) {
-        this.uuid = stringOrNull(uuid);
+    if (subscription != null ? !subscription.equals(that.subscription) : that.subscription != null) {
+      return false;
     }
-
-    public Integer getTaxInCents() {
-        return taxInCents;
+    if (taxInCents != null ? !taxInCents.equals(that.taxInCents) : that.taxInCents != null) {
+      return false;
     }
-
-    public void setTaxInCents(final Object taxInCents) {
-        this.taxInCents = integerOrNull(taxInCents);
+    if (uuid != null ? !uuid.equals(that.uuid) : that.uuid != null) {
+      return false;
     }
+    return true;
+  }
 
-    public String getCurrency() {
-        return currency;
-    }
-
-    public void setCurrency(final Object currency) {
-        this.currency = stringOrNull(currency);
-    }
-
-    public DateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(final Object createdAt) {
-        this.createdAt = dateTimeOrNull(createdAt);
-    }
-
-    @Override
-    public String toString() {
-        final StringBuilder sb = new StringBuilder();
-        sb.append("Transaction");
-        sb.append("{account=").append(account);
-        sb.append(", invoice=").append(invoice);
-        sb.append(", subscription='").append(subscription).append('\'');
-        sb.append(", uuid='").append(uuid).append('\'');
-        sb.append(", action='").append(action).append('\'');
-        sb.append(", amountInCents=").append(amountInCents);
-        sb.append(", taxInCents=").append(taxInCents);
-        sb.append(", currency='").append(currency).append('\'');
-        sb.append(", status='").append(status).append('\'');
-        sb.append(", reference='").append(reference).append('\'');
-        sb.append(", test=").append(test);
-        sb.append(", voidable=").append(voidable);
-        sb.append(", refundable=").append(refundable);
-        sb.append(", createdAt=").append(createdAt);
-        sb.append('}');
-        return sb.toString();
-    }
-
-    @Override
-    public boolean equals(final Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof Transaction)) {
-            return false;
-        }
-        if (!super.equals(o)) {
-            return false;
-        }
-
-        final Transaction that = (Transaction) o;
-
-        if (account != null ? !account.equals(that.account) : that.account != null) {
-            return false;
-        }
-        if (createdAt != null ? !createdAt.equals(that.createdAt) : that.createdAt != null) {
-            return false;
-        }
-        if (currency != null ? !currency.equals(that.currency) : that.currency != null) {
-            return false;
-        }
-        if (invoice != null ? !invoice.equals(that.invoice) : that.invoice != null) {
-            return false;
-        }
-        if (subscription != null ? !subscription.equals(that.subscription) : that.subscription != null) {
-            return false;
-        }
-        if (taxInCents != null ? !taxInCents.equals(that.taxInCents) : that.taxInCents != null) {
-            return false;
-        }
-        if (uuid != null ? !uuid.equals(that.uuid) : that.uuid != null) {
-            return false;
-        }
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = super.hashCode();
-        result = 31 * result + (account != null ? account.hashCode() : 0);
-        result = 31 * result + (invoice != null ? invoice.hashCode() : 0);
-        result = 31 * result + (subscription != null ? subscription.hashCode() : 0);
-        result = 31 * result + (uuid != null ? uuid.hashCode() : 0);
-        result = 31 * result + (taxInCents != null ? taxInCents.hashCode() : 0);
-        result = 31 * result + (currency != null ? currency.hashCode() : 0);
-        result = 31 * result + (createdAt != null ? createdAt.hashCode() : 0);
-        return result;
-    }
+  @Override public int hashCode() {
+    int result = super.hashCode();
+    result = 31 * result + (account != null ? account.hashCode() : 0);
+    result = 31 * result + (invoice != null ? invoice.hashCode() : 0);
+    result = 31 * result + (subscription != null ? subscription.hashCode() : 0);
+    result = 31 * result + (uuid != null ? uuid.hashCode() : 0);
+    result = 31 * result + (taxInCents != null ? taxInCents.hashCode() : 0);
+    result = 31 * result + (currency != null ? currency.hashCode() : 0);
+    result = 31 * result + (createdAt != null ? createdAt.hashCode() : 0);
+    return result;
+  }
 }
