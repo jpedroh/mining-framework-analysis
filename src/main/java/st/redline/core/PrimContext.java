9c355dca67c8f83f15cf91e759e4c950fef06e94
Joao Duarte <jphsd@cin.ufpe.br>
2024-11-05 09:59:12 -0300

    Mining Framework Analysis

--- src/main/java/st/redline/core/PrimContext.java
+++ src/main/java/st/redline/core/PrimContext.java
@@ -1,85 +1,92 @@
-/* Redline Smalltalk, Copyright (c) James C. Ladd. All rights reserved. See LICENSE in the root of this distribution. */
 package st.redline.core;
-
 import st.redline.classloader.*;
 
 public class PrimContext {
+  private final PrimObject receiver;
 
-    private final PrimObject receiver;
-    private final PrimObject lookupClass;
-    private final String selector;
-    private final PrimObject[] arguments;
-    private PrimObject[] temporaries;
-    private PrimContext homeContext;
+  private final PrimObject lookupClass;
 
-    public PrimContext(PrimObject receiver) {
-        this(receiver, null, null, null);
+  private final String selector;
+
+  private final PrimObject[] arguments;
+
+  private PrimObject[] temporaries;
+
+  private PrimContext homeContext;
+
+  public PrimContext(PrimObject receiver) {
+    this(receiver, null, null, null);
+  }
+
+  public PrimContext(PrimObject receiver, PrimObject lookupClass, String selector, PrimObject[] arguments) {
+    this.receiver = receiver;
+    this.lookupClass = lookupClass;
+    this.selector = selector;
+    this.arguments = arguments;
+  }
+
+  public PrimObject receiver() {
+    return receiver;
+  }
+
+  public String selector() {
+    return selector;
+  }
+
+  public PrimObject[] arguments() {
+    return arguments;
+  }
+
+  public void initTemporaries(int count) {
+    temporaries = new PrimObject[count];
+    PrimObject nil = nil();
+    for (int i = 0; i < count; i++) {
+      temporaries[i] = nil;
     }
+  }
 
-    public PrimContext(PrimObject receiver, PrimObject lookupClass, String selector, PrimObject[] arguments) {
-        this.receiver = receiver;
-        this.lookupClass = lookupClass;
-        this.selector = selector;
-        this.arguments = arguments;
-    }
+  private PrimObject nil() {
+    return classLoader().nilInstance();
+  }
 
-    public PrimObject receiver() {
-        return receiver;
-    }
+  private SmalltalkClassLoader classLoader() {
+    return (SmalltalkClassLoader) Thread.currentThread().getContextClassLoader();
+  }
 
-    public String selector() {
-        return selector;
-    }
+  public PrimObject[] selectorAndArguments() {
+    PrimObject selectorObject = new PrimObject();
+    selectorObject.javaValue(selector);
+    PrimObject[] selectorAndArguments = new PrimObject[arguments.length + 1];
+    selectorAndArguments[0] = selectorObject;
+    System.arraycopy(arguments, 0, selectorAndArguments, 1, arguments.length);
+    return selectorAndArguments;
+  }
 
-    public PrimObject[] arguments() {
-        return arguments;
-    }
+  public PrimObject argumentAt(int index) {
+    return arguments[index];
+  }
 
-    public void initTemporaries(int count) {
-        temporaries = new PrimObject[count];
-        PrimObject nil = nil();
-        for (int i = 0; i < count; i++)
-            temporaries[i] = nil;
-    }
+  public Object argumentJavaValueAt(int index) {
+    return argumentAt(index).javaValue();
+  }
 
-    private PrimObject nil() {
-        return classLoader().nilInstance();
-    }
+  public PrimObject temporaryAt(int index) {
+    return temporaries[index];
+  }
 
-    private SmalltalkClassLoader classLoader() {
-        return (SmalltalkClassLoader) Thread.currentThread().getContextClassLoader();
-    }
+  public void temporaryAtPut(int index, PrimObject object) {
+    temporaries[index] = object;
+  }
 
-    public PrimObject[] selectorAndArguments() {
-        PrimObject selectorObject = new PrimObject();
-        selectorObject.javaValue(selector);
-        PrimObject[] selectorAndArguments = new PrimObject[arguments.length + 1];
-        selectorAndArguments[0] = selectorObject;
-        System.arraycopy(arguments, 0, selectorAndArguments, 1, arguments.length);
-        return selectorAndArguments;
-    }
+  public static void temporaryPutAt(PrimObject object, int index, PrimContext context) {
+    context.temporaryAtPut(index, object);
+  }
 
-    public PrimObject argumentAt(int index) {
-        return arguments[index];
-    }
+  public void homeContext(PrimContext homeContext) {
+    this.homeContext = homeContext;
+  }
 
-    public Object argumentJavaValueAt(int index) {
-        return argumentAt(index).javaValue();
-    }
-
-    public PrimObject temporaryAt(int index) {
-        return temporaries[index];
-    }
-
-    public void temporaryAtPut(int index, PrimObject object) {
-        temporaries[index] = object;
-    }
-
-    public static void temporaryPutAt(PrimObject object, int index, PrimContext context) {
-        context.temporaryAtPut(index, object);
-    }
-
-    public void homeContext(PrimContext homeContext) {
-        this.homeContext = homeContext;
-    }
+  public PrimContext homeContext() {
+    return (homeContext != null) ? homeContext : this;
+  }
 }
--- src/main/java/st/redline/core/PrimObject.java
+++ src/main/java/st/redline/core/PrimObject.java
@@ -1,287 +1,248 @@
-/* Redline Smalltalk, Copyright (c) James C. Ladd. All rights reserved. See LICENSE in the root of this distribution. */
 package st.redline.core;
-
 import st.redline.classloader.*;
-
 import static st.redline.compiler.SmalltalkGeneratingVisitor.*;
 import static st.redline.core.PrimDoesNotUnderstand.*;
 import static st.redline.core.PrimSubclass.*;
 
 public class PrimObject {
+  private PrimObject selfClass;
 
-    private PrimObject selfClass;
-    private Object javaValue;
+  private Object javaValue;
 
-    public String toString() {
-        if (javaValue != null)
-            return javaValue.toString();
-        if (selfClass != null && selfClass != this)
-            return selfClass.toString();
-        return super.toString();
+  public String toString() {
+    if (javaValue != null) {
+      return javaValue.toString();
     }
-
-    public void javaValue(Object object) {
-        javaValue = object;
+    if (selfClass != null && selfClass != this) {
+      return selfClass.toString();
     }
+    return super.toString();
+  }
 
-    public Object javaValue() {
-        return javaValue;
+  public void javaValue(Object object) {
+    javaValue = object;
+  }
+
+  public Object javaValue() {
+    return javaValue;
+  }
+
+  public void selfClass(PrimObject primObject) {
+    selfClass = primObject;
+  }
+
+  public PrimObject selfClass() {
+    return selfClass;
+  }
+
+  public PrimObject referenceNil() {
+    return classLoader().nilInstance();
+  }
+
+  public PrimObject referenceTrue() {
+    return classLoader().trueInstance();
+  }
+
+  public PrimObject referenceFalse() {
+    return classLoader().falseInstance();
+  }
+
+  public PrimObject reference(String name) {
+    return resolveObject(name);
+  }
+
+  public PrimObject resolveObject(String name) {
+    return findObject(importFor(name));
+  }
+
+  public PrimObject smalltalkBlock(Object value, PrimContext homeContext) {
+    return instanceOfWith("BlockClosure", new Object[] { value, homeContext });
+  }
+
+  public PrimObject smalltalkBlockAnswer(Object value, PrimContext homeContext, String answerClassName) {
+    return instanceOfWith("BlockClosure", new Object[] { throwingAnswer(value, answerClassName), homeContext });
+  }
+
+  private LambdaBlock throwingAnswer(Object value, String answerClassName) {
+    return (self, receiver, context) -> {
+      PrimObject answer = ((LambdaBlock) value).apply(self, receiver, context);
+      PrimBlockAnswer blockAnswer;
+      try {
+        blockAnswer = (PrimBlockAnswer) classLoader().loadClass(answerClassName).getConstructor(PrimObject.class).newInstance(answer);
+      } catch (Exception e) {
+        throw new RuntimeException(e);
+      }
+      throw blockAnswer;
+    };
+  }
+
+  public PrimObject smalltalkMethod(Object value, PrimContext homeContext) {
+    return instanceOfWith("CompiledMethod", value);
+  }
+
+  public PrimObject smalltalkString(Object value) {
+    return instanceOfWith("String", value);
+  }
+
+  public PrimObject smalltalkSymbol(Object value) {
+    return instanceOfWith("Symbol", value);
+  }
+
+  protected PrimObject instanceOfWith(String type, Object value) {
+    PrimObject instance = instanceOf(type);
+    instance.javaValue(value);
+    return instance;
+  }
+
+  protected PrimObject instanceOf(String type) {
+    return isBootstrapping() ? new PrimObject() : resolveObject(type).perform("new");
+  }
+
+  protected boolean isBootstrapping() {
+    return classLoader().isBootstrapping();
+  }
+
+  protected PrimObject findObject(String name) {
+    return classLoader().findObject(name);
+  }
+
+  protected String importFor(String name) {
+    return packageName() + "." + name;
+  }
+
+  protected void importAll(String packageName) {
+    classLoader().importAll(packageName);
+  }
+
+  protected SmalltalkClassLoader classLoader() {
+    return (SmalltalkClassLoader) Thread.currentThread().getContextClassLoader();
+  }
+
+  protected PrimObject sendMessages(PrimObject receiver, PrimContext context) {
+    return receiver;
+  }
+
+  protected String packageName() {
+    return DEFAULT_IMPORTED_PACKAGE;
+  }
+
+  public PrimObject perform(String selector) {
+    return perform0(selector);
+  }
+
+  public PrimObject superPerform(String selector) {
+    return perform0s(selector);
+  }
+
+  public PrimObject perform(PrimObject arg1, String selector) {
+    return perform0(selector, arg1);
+  }
+
+  public PrimObject superPerform(PrimObject arg1, String selector) {
+    return perform0s(selector, arg1);
+  }
+
+  public PrimObject perform(PrimObject arg1, PrimObject arg2, String selector) {
+    return perform0(selector, arg1, arg2);
+  }
+
+  public PrimObject superPerform(PrimObject arg1, PrimObject arg2, String selector) {
+    return perform0s(selector, arg1, arg2);
+  }
+
+  public PrimObject perform(PrimObject arg1, PrimObject arg2, PrimObject arg3, String selector) {
+    return perform0(selector, arg1, arg2);
+  }
+
+  public PrimObject superPerform(PrimObject arg1, PrimObject arg2, PrimObject arg3, String selector) {
+    return perform0s(selector, arg1, arg2);
+  }
+
+  public PrimObject perform(PrimObject arg1, PrimObject arg2, PrimObject arg3, PrimObject arg4, String selector) {
+    return perform0(selector, arg1, arg2);
+  }
+
+  public PrimObject superPerform(PrimObject arg1, PrimObject arg2, PrimObject arg3, PrimObject arg4, String selector) {
+    return perform0s(selector, arg1, arg2);
+  }
+
+  public PrimObject perform(PrimObject arg1, PrimObject arg2, PrimObject arg3, PrimObject arg4, PrimObject arg5, String selector) {
+    return perform0(selector, arg1, arg2);
+  }
+
+  public PrimObject superPerform(PrimObject arg1, PrimObject arg2, PrimObject arg3, PrimObject arg4, PrimObject arg5, String selector) {
+    return perform0s(selector, arg1, arg2);
+  }
+
+  protected PrimObject perform0(String selector, PrimObject... arguments) {
+    return perform0(selfClass, selector, arguments);
+  }
+
+  protected PrimObject perform0s(String selector, PrimObject... arguments) {
+    return perform0(selfClass.superclass(), selector, arguments);
+  }
+
+  protected PrimObject perform0(PrimObject foundInClass, String selector, PrimObject... arguments) {
+    PrimObject cls = foundInClass;
+    while (!cls.includesSelector(selector)) {
+      cls = cls.superclass();
     }
+    return apply(cls.methodFor(selector), cls, selector, arguments);
+  }
 
-    public void selfClass(PrimObject primObject) {
-        selfClass = primObject;
+  protected PrimObject apply(PrimObject method, PrimObject foundInClass, String selector, PrimObject... arguments) {
+    PrimObject result = method.invoke(this, new PrimContext(this, foundInClass, selector, arguments));
+    return result;
+  }
+
+  protected PrimObject invoke(PrimObject receiver, PrimContext context) {
+    return this;
+  }
+
+  protected PrimObject methodFor(String selector) {
+    if ("subclass:".equals(selector)) {
+      return PRIM_SUBCLASS;
     }
+    return PRIM_DOES_NOT_UNDERSTAND;
+  }
 
-    public PrimObject selfClass() {
-        return selfClass;
+  protected PrimObject superclass() {
+    throw new IllegalStateException("This receiver should not have received this message.");
+  }
+
+  protected boolean includesSelector(String selector) {
+    return true;
+  }
+
+  public boolean isMeta() {
+    return false;
+  }
+
+  public PrimObject primitiveSubclass(PrimContext primContext) {
+    return PRIM_SUBCLASS.invoke(this, primContext);
+  }
+
+  public PrimObject primitiveNew() {
+    PrimObject object = new PrimObject();
+    object.selfClass(this);
+    return object;
+  }
+
+  public PrimObject primitiveEval(PrimContext context) {
+    Object[] values = (Object[]) javaValue();
+    context.homeContext((PrimContext) values[1]);
+    return ((LambdaBlock) values[0]).apply(this, this, context);
+  }
+
+  public PrimObject primitive110(PrimContext context) {
+    if (this.equals(context.argumentAt(0))) {
+      return classLoader().trueInstance();
+    } else {
+      return classLoader().falseInstance();
     }
+  }
 
-    public PrimObject referenceNil() {
-        //System.out.println("** referenceNil");
-        return classLoader().nilInstance();
-    }
-
-    public PrimObject referenceTrue() {
-        //System.out.println("** referenceTrue");
-        return classLoader().trueInstance();
-    }
-
-    public PrimObject referenceFalse() {
-        //System.out.println("** referenceFalse");
-        return classLoader().falseInstance();
-    }
-
-    public PrimObject reference(String name) {
-        //System.out.println("** reference " + name);
-        return resolveObject(name);
-    }
-
-    public PrimObject resolveObject(String name) {
-        //System.out.println("** resolveObject " + name + " " + importFor(name));
-        return findObject(importFor(name));
-    }
-
-    public PrimObject smalltalkBlock(Object value, PrimContext homeContext) {
-//        System.out.println("** smalltalkBlock " + value);
-        return instanceOfWith("BlockClosure", new Object[] { value, homeContext });
-    }
-
-    public PrimObject smalltalkBlockAnswer(Object value, PrimContext homeContext, String answerClassName) {
-//        System.out.println("** smalltalkBlockAnswer " + value + " from " + answerClassName);
-        return instanceOfWith("BlockClosure", new Object[] { throwingAnswer(value, answerClassName), homeContext });
-    }
-
-    private LambdaBlock throwingAnswer(Object value, String answerClassName) {
-        return (self, receiver, context) -> {
-            PrimObject answer = ((LambdaBlock) value).apply(self, receiver, context);
-            PrimBlockAnswer blockAnswer;
-            try {
-                blockAnswer = (PrimBlockAnswer)
-                        classLoader()
-                                .loadClass(answerClassName)
-                                .getConstructor(PrimObject.class)
-                                .newInstance(answer);
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-            throw blockAnswer;
-        };
-    }
-
-    public PrimObject smalltalkMethod(Object value) {
-        //System.out.println("** smalltalkMethod " + value);
-        return instanceOfWith("CompiledMethod", value);
-    }
-
-    public PrimObject smalltalkString(Object value) {
-        return instanceOfWith("String", value);
-    }
-
-    public PrimObject smalltalkSymbol(Object value) {
-        return instanceOfWith("Symbol", value);
-
-//        String symbol = (String) javaValue;
-//        SmalltalkClassLoader smalltalkClassLoader = classLoader();
-//        if (smalltalkClassLoader.isInternedSymbol(symbol))
-//            return smalltalkClassLoader.internedSymbolAt(symbol);
-//        PrimObject symbolObject = instanceOfWith("Symbol", symbol);
-//        smalltalkClassLoader.internSymbolAtPut(symbol, symbolObject);
-//        return symbolObject;
-    }
-
-    protected PrimObject instanceOfWith(String type, Object value) {
-        PrimObject instance = instanceOf(type);
-        instance.javaValue(value);
-        return instance;
-    }
-
-    protected PrimObject instanceOf(String type) {
-        return isBootstrapping() ? new PrimObject() : resolveObject(type).perform("new");
-    }
-
-    protected boolean isBootstrapping() {
-        return classLoader().isBootstrapping();
-    }
-
-    protected PrimObject findObject(String name) {
-        return classLoader().findObject(name);
-    }
-
-    protected String importFor(String name) {
-        // Subclass should override.
-        return packageName() + "." + name;
-    }
-
-    protected void importAll(String packageName) {
-        classLoader().importAll(packageName);
-    }
-
-    protected SmalltalkClassLoader classLoader() {
-        return (SmalltalkClassLoader) Thread.currentThread().getContextClassLoader();
-    }
-
-    protected PrimObject sendMessages(PrimObject receiver, PrimContext context) {
-        // Subclass should override.
-        return receiver;
-    }
-
-    protected String packageName() {
-        // Subclass should override.
-        return DEFAULT_IMPORTED_PACKAGE;
-    }
-
-    public PrimObject perform(String selector) {
-//        System.out.println("** perform(" + selector + ") " + this);
-        return perform0(selector);
-    }
-
-    public PrimObject superPerform(String selector) {
-//        System.out.println("** superPerform(" + selector + ") " + this);
-        return perform0s(selector);
-    }
-
-    public PrimObject perform(PrimObject arg1, String selector) {
-//        System.out.println("** perform(" + arg1 + "," + selector + ") " + this);
-        return perform0(selector, arg1);
-    }
-
-    public PrimObject superPerform(PrimObject arg1, String selector) {
-//        System.out.println("** superPerform(" + arg1 + "," + selector + ") " + this);
-        return perform0s(selector, arg1);
-    }
-
-    public PrimObject perform(PrimObject arg1, PrimObject arg2, String selector) {
-        //System.out.println("** perform(" + arg1 + "," + arg2 + "," + selector + ") " + this);
-        return perform0(selector, arg1, arg2);
-    }
-
-    public PrimObject superPerform(PrimObject arg1, PrimObject arg2, String selector) {
-        //System.out.println("** superPerform(" + arg1 + "," + arg2 + "," + selector + ") " + this);
-        return perform0s(selector, arg1, arg2);
-    }
-
-    public PrimObject perform(PrimObject arg1, PrimObject arg2, PrimObject arg3, String selector) {
-//        System.out.println("** perform(" + arg1 + "," + arg2 + "," + arg3 + "," + selector + ") " + this);
-        return perform0(selector, arg1, arg2);
-    }
-
-    public PrimObject superPerform(PrimObject arg1, PrimObject arg2, PrimObject arg3, String selector) {
-//        System.out.println("** superPerform(" + arg1 + "," + arg2 + "," + arg3 + "," + selector + ") " + this);
-        return perform0s(selector, arg1, arg2);
-    }
-
-    public PrimObject perform(PrimObject arg1, PrimObject arg2, PrimObject arg3, PrimObject arg4, String selector) {
-//        System.out.println("** perform(" + arg1 + "," + arg2 + "," + arg3 + "," + arg4 + "," + selector + ") " + this);
-        return perform0(selector, arg1, arg2);
-    }
-
-    public PrimObject superPerform(PrimObject arg1, PrimObject arg2, PrimObject arg3, PrimObject arg4, String selector) {
-//        System.out.println("** superPerform(" + arg1 + "," + arg2 + "," + arg3 + "," + arg4 + "," + selector + ") " + this);
-        return perform0s(selector, arg1, arg2);
-    }
-
-    public PrimObject perform(PrimObject arg1, PrimObject arg2, PrimObject arg3, PrimObject arg4, PrimObject arg5, String selector) {
-//        System.out.println("** perform(" + arg1 + "," + arg2 + "," + arg3 + "," + arg4 + "," + arg5 + "," + selector + ") " + this);
-        return perform0(selector, arg1, arg2);
-    }
-
-    public PrimObject superPerform(PrimObject arg1, PrimObject arg2, PrimObject arg3, PrimObject arg4, PrimObject arg5, String selector) {
-//        System.out.println("** superPerform(" + arg1 + "," + arg2 + "," + arg3 + "," + arg4 + "," + arg5 + "," + selector + ") " + this);
-        return perform0s(selector, arg1, arg2);
-    }
-
-    protected PrimObject perform0(String selector, PrimObject ... arguments) {
-        return perform0(selfClass, selector, arguments);
-    }
-
-    protected PrimObject perform0s(String selector, PrimObject ... arguments) {
-        return perform0(selfClass.superclass(), selector, arguments);
-    }
-
-    protected PrimObject perform0(PrimObject foundInClass, String selector, PrimObject ... arguments) {
-        PrimObject cls = foundInClass;
-        while (!cls.includesSelector(selector))
-            cls = cls.superclass();
-        return apply(cls.methodFor(selector), cls, selector, arguments);
-    }
-
-    protected PrimObject apply(PrimObject method, PrimObject foundInClass, String selector, PrimObject ... arguments) {
-//        System.out.println("** apply: #" + selector + " found in " + foundInClass + " to " + this);
-        PrimObject result = method.invoke(this, new PrimContext(this, foundInClass, selector, arguments));
-        //System.out.println("** result: " + String.valueOf(result));
-        return result; // method.invoke(this, new PrimContext(this, foundInClass, outerContext, selector, arguments));
-    }
-
-    protected PrimObject invoke(PrimObject receiver, PrimContext context) {
-        return this;
-    }
-
-    protected PrimObject methodFor(String selector) {
-        if ("subclass:".equals(selector))
-            return PRIM_SUBCLASS;
-        return PRIM_DOES_NOT_UNDERSTAND;
-    }
-
-    protected PrimObject superclass() {
-        throw new IllegalStateException("This receiver should not have received this message.");
-    }
-
-    protected boolean includesSelector(String selector) {
-        return true;
-    }
-
-    public boolean isMeta() {
-        return false;
-    }
-
-    public PrimObject primitiveSubclass(PrimContext primContext) {
-//        System.out.println("primitiveSubclass: " + primContext.argumentJavaValueAt(0));
-        return PRIM_SUBCLASS.invoke(this, primContext);
-    }
-
-    public PrimObject primitiveNew() {
-        PrimObject object = new PrimObject();
-        object.selfClass(this);
-        return object;
-    }
-
-    public PrimObject primitiveEval(PrimContext context) {
-//        System.out.println("primitiveEval: " + context + " " + ((Object[]) javaValue())[1]);
-        Object[] values = (Object[]) javaValue();
-        context.homeContext((PrimContext) values[1]);
-        return ((LambdaBlock) values[0]).apply(this, this, context);
-    }
-
-    public PrimObject primitive110(PrimContext context) {
-//        System.out.println("primitive110: " + context);
-        if (this.equals(context.argumentAt(0)))
-            return classLoader().trueInstance();
-        else
-            return classLoader().falseInstance();
-    }
-
-    public PrimObject primitive111(PrimContext context) {
-//        System.out.println("primitive111: " + context);
-        return this.selfClass();
-    }
+  public PrimObject primitive111(PrimContext context) {
+    return this.selfClass();
+  }
 }
