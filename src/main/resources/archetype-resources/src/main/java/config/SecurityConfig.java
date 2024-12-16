e9bf33b5e99a63345284972bfcd3ee709c8526d9
Joao Pedro <joao.pedro.hsd@gmail.com>
2024-12-16 18:20:27 +0000

    Mining Framework Analysis

--- src/main/resources/archetype-resources/src/main/java/config/SecurityConfig.java
+++ src/main/resources/archetype-resources/src/main/java/config/SecurityConfig.java
@@ -1,10 +1,9 @@
-package ${package}.config;
+package $;
 
 import java.util.regex.Pattern;
-
 import javax.servlet.http.HttpServletRequest;
-
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,88 +11,92 @@ import org.springframework.security.config.annotation.web.servlet.configuration.
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.StandardPasswordEncoder;
 import org.springframework.security.web.authentication.rememberme.TokenBasedRememberMeServices;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+import pl.codeleak.arch.account.UserService;
 
-import ${package}.account.UserService;
 
 @Configuration
 @EnableWebMvcSecurity
 class SecurityConfig extends WebSecurityConfigurerAdapter {
+	@Bean
+	public UserService userService() {
+		return new UserService();
+	}
 
 	@Bean
 	public TokenBasedRememberMeServices rememberMeServices() {
 		return new TokenBasedRememberMeServices("remember-me-key", userService());
 	}
-	
+
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new StandardPasswordEncoder();
 	}
 
-  @Profile("test")
-  @Bean(name = "csrfMatcher")
-  public RequestMatcher testCsrfMatcher() {
-    return new RequestMatcher() {
+				@Override
+				protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+				    auth
+				            .eraseCredentials(true)
+				            .userDetailsService(userService())
+				            .passwordEncoder(passwordEncoder());
+				}
 
-      @Override
-      public boolean matches(HttpServletRequest request) {
-        return false;
-      }
-    };
-  }
+				@Override
+				protected void configure(HttpSecurity http) throws Exception {
+				    http
+				        .authorizeRequests()
+				            .antMatchers("/", "/favicon.ico", "/resources/**", "/signup").permitAll()
+				            .anyRequest().authenticated()
+				            .and()
+				        .formLogin()
+				            .loginPage("/signin")
+				            .permitAll()
+				            .failureUrl("/signin?error=1")
+				            .loginProcessingUrl("/authenticate")
+				            .and()
+				        .logout()
+				            .logoutUrl("/logout")
+				            .permitAll()
+				            .logoutSuccessUrl("/signin?logout")
+				            .and()
+				        .rememberMe()
+				            .rememberMeServices(rememberMeServices())
+				            .key("remember-me-key");
+				}
 
-  @Profile("!test")
-  @Bean(name = "csrfMatcher")
-  public RequestMatcher csrfMatcher() {
-    /**
-     * Copy of default request matcher from
-     * CsrfFilter$DefaultRequiresCsrfMatcher
-     */
-    return new RequestMatcher() {
-      private Pattern allowedMethods = Pattern
-        .compile("^(GET|HEAD|TRACE|OPTIONS)$");
+		@Profile("test")
+		@Bean(name = "csrfMatcher")
+		public RequestMatcher testCsrfMatcher() {
+		  return new RequestMatcher() {
 
-      /*
-       * (non-Javadoc)
-       *
-       * @see
-       * org.springframework.security.web.util.matcher.RequestMatcher#
-       * matches(javax.servlet.http.HttpServletRequest)
-       */
-      public boolean matches(HttpServletRequest request) {
-        return !allowedMethods.matcher(request.getMethod()).matches();
-      }
-    };
-  }
-    
+		    @Override
+		    public boolean matches(HttpServletRequest request) {
+		      return false;
+		    }
+		  };
+		}
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .eraseCredentials(true)
-                .userDetailsService(userService())
-                .passwordEncoder(passwordEncoder());
-    }
+		@Profile("!test")
+		@Bean(name = "csrfMatcher")
+		public RequestMatcher csrfMatcher() {
+		  /**
+		   * Copy of default request matcher from
+		   * CsrfFilter$DefaultRequiresCsrfMatcher
+		   */
+		  return new RequestMatcher() {
+		    private Pattern allowedMethods = Pattern
+		      .compile("^(GET|HEAD|TRACE|OPTIONS)$");
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-            .authorizeRequests()
-                .antMatchers("/", "/favicon.ico", "/resources/**", "/signup").permitAll()
-                .anyRequest().authenticated()
-                .and()
-            .formLogin()
-                .loginPage("/signin")
-                .permitAll()
-                .failureUrl("/signin?error=1")
-                .loginProcessingUrl("/authenticate")
-                .and()
-            .logout()
-                .logoutUrl("/logout")
-                .permitAll()
-                .logoutSuccessUrl("/signin?logout")
-                .and()
-            .rememberMe()
-                .rememberMeServices(rememberMeServices())
-                .key("remember-me-key");
-    }
+		    /*
+		     * (non-Javadoc)
+		     *
+		     * @see
+		     * org.springframework.security.web.util.matcher.RequestMatcher#
+		     * matches(javax.servlet.http.HttpServletRequest)
+		     */
+		    public boolean matches(HttpServletRequest request) {
+		      return !allowedMethods.matcher(request.getMethod()).matches();
+		    }
+		  };
+		}
 }
