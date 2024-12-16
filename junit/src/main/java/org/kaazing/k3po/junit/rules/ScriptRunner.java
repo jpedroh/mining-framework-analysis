5ea2811eef735962f5cab3bf837ad1a10fb303aa
Joao Pedro <joao.pedro.hsd@gmail.com>
2024-12-16 17:58:25 +0000

    Mining Framework Analysis

--- junit/src/main/java/org/kaazing/k3po/junit/rules/ScriptRunner.java
+++ junit/src/main/java/org/kaazing/k3po/junit/rules/ScriptRunner.java
@@ -18,14 +18,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-
 package org.kaazing.k3po.junit.rules;
-
-import static java.lang.String.format;
-import static java.util.concurrent.TimeUnit.MILLISECONDS;
-import static org.kaazing.k3po.junit.rules.ScriptRunner.BarrierState.INITIAL;
-import static org.kaazing.k3po.junit.rules.ScriptRunner.BarrierState.NOTIFIED;
-import static org.kaazing.k3po.junit.rules.ScriptRunner.BarrierState.NOTIFYING;
 
 import java.lang.management.ManagementFactory;
 import java.net.ConnectException;
@@ -37,7 +30,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.Callable;
 import java.util.concurrent.CountDownLatch;
-
 import org.kaazing.k3po.control.internal.Control;
 import org.kaazing.k3po.control.internal.command.AbortCommand;
 import org.kaazing.k3po.control.internal.command.PrepareCommand;
@@ -48,27 +40,33 @@ import org.kaazing.k3po.control.internal.event.FinishedEvent;
 import org.kaazing.k3po.control.internal.event.NotifiedEvent;
 import org.kaazing.k3po.control.internal.event.PreparedEvent;
 import org.kaazing.k3po.junit.rules.internal.ScriptPair;
+import static java.lang.String.format;
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static org.kaazing.k3po.junit.rules.ScriptRunner.BarrierState.INITIAL;
+import static org.kaazing.k3po.junit.rules.ScriptRunner.BarrierState.NOTIFIED;
+import static org.kaazing.k3po.junit.rules.ScriptRunner.BarrierState.NOTIFYING;
+
 
 final class ScriptRunner implements Callable<ScriptPair> {
-
     private final Control controller;
+
     private final List<String> names;
+
     private final Latch latch;
 
     private volatile boolean abortScheduled;
+
     private volatile Map<String, BarrierStateMachine> barriers;
+
     private static final int DISPOSE_TIMEOUT = isDebugging() ? 0: 5000;
 
     ScriptRunner(URL controlURL, List<String> names, Latch latch) {
-
         if (names == null) {
             throw new NullPointerException("names");
         }
-
         if (latch == null) {
             throw new NullPointerException("latch");
         }
-
         this.controller = new Control(controlURL);
         this.names = names;
         this.latch = latch;
@@ -245,26 +243,28 @@ final class ScriptRunner implements Callable<ScriptPair> {
     }
 
     private interface BarrierStateListener {
-        void initial();
+        public abstract void initial();
 
-        void notified();
+        public abstract void notified();
 
-        void notifying();
+        public abstract void notifying();
     }
 
     enum BarrierState {
-        INITIAL, NOTIFYING, NOTIFIED;
-    }
+
+        INITIAL,
+        NOTIFYING,
+        NOTIFIED;}
 
     private class BarrierStateMachine implements BarrierStateListener {
-
         private BarrierState state = INITIAL;
+
         private List<BarrierStateListener> stateListeners = new ArrayList<>();
 
         @Override
         public void initial() {
             System.out.println("Hello");
-            synchronized (this) {
+            synchronized(this) {
                 this.state = NOTIFYING;
                 for (BarrierStateListener listener : stateListeners) {
                     listener.initial();
@@ -274,7 +274,7 @@ final class ScriptRunner implements Callable<ScriptPair> {
 
         @Override
         public void notifying() {
-            synchronized (this) {
+            synchronized(this) {
                 this.state = NOTIFYING;
                 for (BarrierStateListener listener : stateListeners) {
                     listener.notifying();
@@ -284,7 +284,7 @@ final class ScriptRunner implements Callable<ScriptPair> {
 
         @Override
         public void notified() {
-            synchronized (this) {
+            synchronized(this) {
                 this.state = NOTIFIED;
                 for (BarrierStateListener listener : stateListeners) {
                     listener.notified();
@@ -293,20 +293,20 @@ final class ScriptRunner implements Callable<ScriptPair> {
         }
 
         public void addListener(BarrierStateListener stateListener) {
-            synchronized (this) {
+            synchronized(this) {
                 switch (this.state) {
-                // notify right away if waiting on state
-                case INITIAL:
-                    stateListener.initial();
-                    break;
-                case NOTIFYING:
-                    stateListener.notify();
-                    break;
-                case NOTIFIED:
-                    stateListener.notified();
-                    break;
-                default:
-                    break;
+                    // notify right away if waiting on state
+                    case INITIAL :
+                        stateListener.initial();
+                        break;
+                    case NOTIFYING :
+                        stateListener.notify();
+                        break;
+                    case NOTIFIED :
+                        stateListener.notified();
+                        break;
+                    default :
+                        break;
                 }
                 stateListeners.add(stateListener);
             }
@@ -316,15 +316,19 @@ final class ScriptRunner implements Callable<ScriptPair> {
     public void dispose() throws Exception {
         controller.dispose();
         try {
-            CommandEvent event = controller.readEvent();
-
+            CommandEvent event = 
+<<<<<<< LEFT
+            controller.readEvent(DISPOSE_TIMEOUT, MILLISECONDS)
+=======
+            controller.readEvent()
+>>>>>>> RIGHT;
             // ensure it is the correct event
             switch (event.getKind()) {
-            case DISPOSED:
-                latch.notifyDisposed();
-                break;
-            default:
-                throw new IllegalArgumentException("Unrecognized event kind: " + event.getKind());
+                case DISPOSED :
+                    latch.notifyDisposed();
+                    break;
+                default :
+                    throw new IllegalArgumentException("Unrecognized event kind: " + event.getKind());
             }
         } finally {
             controller.disconnect();
