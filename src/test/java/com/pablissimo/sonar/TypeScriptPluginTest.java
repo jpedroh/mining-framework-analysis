adf6a1da9d27c1706ff1611931cef5e31d9591d5
Joao Pedro <joao.pedro.hsd@gmail.com>
2024-12-16 17:57:58 +0000

    Mining Framework Analysis

--- src/test/java/com/pablissimo/sonar/TypeScriptPluginTest.java
+++ src/test/java/com/pablissimo/sonar/TypeScriptPluginTest.java
@@ -1,11 +1,8 @@
 package com.pablissimo.sonar;
 
-import static org.junit.Assert.*;
-
 import java.lang.annotation.Annotation;
 import java.util.Arrays;
 import java.util.List;
-
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.collections.Predicate;
 import org.junit.After;
@@ -16,6 +13,8 @@ import org.sonar.api.Properties;
 import org.sonar.api.Property;
 import org.sonar.api.PropertyType;
 import org.sonar.api.SonarQubeVersion;
+import static org.junit.Assert.*;
+
 
 public class TypeScriptPluginTest {
     TypeScriptPlugin plugin;
@@ -32,11 +31,8 @@ public class TypeScriptPluginTest {
     @Test
     public void advertisesAppropriateExtensions() {
         Plugin.Context context = new Plugin.Context(SonarQubeVersion.V5_6);
-
         this.plugin.define(context);
-        
         List extensions = context.getExtensions();
-
         assertTrue(extensions.contains(TypeScriptRuleProfile.class));
         assertTrue(extensions.contains(TypeScriptLanguage.class));
         assertTrue(extensions.contains(TsLintSensor.class));
@@ -55,29 +51,18 @@ public class TypeScriptPluginTest {
     @Test
     public void definesExpectedProperties() {
         Annotation annotation = plugin.getClass().getAnnotations()[0];
-        Properties propertiesAnnotation = (Properties) annotation;
-
+        Properties propertiesAnnotation = ((Properties) (annotation));
         assertEquals(9, propertiesAnnotation.value().length);
-
         Property[] properties = propertiesAnnotation.value();
-        assertNotNull(findPropertyByName(properties,
-                TypeScriptPlugin.SETTING_EXCLUDE_TYPE_DEFINITION_FILES));
-        assertNotNull(findPropertyByName(properties,
-                TypeScriptPlugin.SETTING_FORCE_ZERO_COVERAGE));
-        assertNotNull(findPropertyByName(properties,
-                TypeScriptPlugin.SETTING_LCOV_REPORT_PATH));
-        assertNotNull(findPropertyByName(properties,
-            TypeScriptPlugin.SETTING_TS_LINT_ENABLED));
-        assertNotNull(findPropertyByName(properties,
-                TypeScriptPlugin.SETTING_TS_LINT_PATH));
-        assertNotNull(findPropertyByName(properties,
-                TypeScriptPlugin.SETTING_TS_LINT_CONFIG_PATH));
-        assertNotNull(findPropertyByName(properties,
-                TypeScriptPlugin.SETTING_TS_LINT_TIMEOUT));
-        assertNotNull(findPropertyByName(properties,
-                TypeScriptPlugin.SETTING_TS_LINT_RULES_DIR));
-        assertNotNull(findPropertyByName(properties,
-                TypeScriptPlugin.SETTING_TS_RULE_CONFIGS));
+        assertNotNull(findPropertyByName(properties, TypeScriptPlugin.SETTING_EXCLUDE_TYPE_DEFINITION_FILES));
+        assertNotNull(findPropertyByName(properties, TypeScriptPlugin.SETTING_FORCE_ZERO_COVERAGE));
+        assertNotNull(findPropertyByName(properties, TypeScriptPlugin.SETTING_LCOV_REPORT_PATH));
+        assertNotNull(findPropertyByName(properties, TypeScriptPlugin.SETTING_TS_LINT_ENABLED));
+        assertNotNull(findPropertyByName(properties, TypeScriptPlugin.SETTING_TS_LINT_PATH));
+        assertNotNull(findPropertyByName(properties, TypeScriptPlugin.SETTING_TS_LINT_CONFIG_PATH));
+        assertNotNull(findPropertyByName(properties, TypeScriptPlugin.SETTING_TS_LINT_TIMEOUT));
+        assertNotNull(findPropertyByName(properties, TypeScriptPlugin.SETTING_TS_LINT_RULES_DIR));
+        assertNotNull(findPropertyByName(properties, TypeScriptPlugin.SETTING_TS_RULE_CONFIGS));
     }
 
     @Test
