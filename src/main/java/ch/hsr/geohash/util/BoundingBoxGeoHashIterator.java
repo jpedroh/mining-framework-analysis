d67143af074df456e7590217cfbb23202b272e40
Joao Pedro <joao.pedro.hsd@gmail.com>
2024-12-16 18:12:25 +0000

    Mining Framework Analysis

--- src/main/java/ch/hsr/geohash/util/BoundingBoxGeoHashIterator.java
+++ src/main/java/ch/hsr/geohash/util/BoundingBoxGeoHashIterator.java
@@ -1,10 +1,9 @@
 package ch.hsr.geohash.util;
 
 import ch.hsr.geohash.GeoHash;
-import ch.hsr.geohash.util.TwoGeoHashBoundingBox;
-
 import java.util.Iterator;
 import java.util.NoSuchElementException;
+
 
 /**
  * Iterate over all of the values within a bounding box at a particular
@@ -12,6 +11,7 @@ import java.util.NoSuchElementException;
  */
 public class BoundingBoxGeoHashIterator implements Iterator<GeoHash> {
 	private TwoGeoHashBoundingBox boundingBox;
+
 	private GeoHash current;
 
 	public BoundingBoxGeoHashIterator(TwoGeoHashBoundingBox bbox) {
@@ -33,17 +33,15 @@ public class BoundingBoxGeoHashIterator implements Iterator<GeoHash> {
 		if (!hasNext()) {
 			throw new NoSuchElementException();
 		}
-
 		GeoHash rv = current;
 		if (rv.equals(boundingBox.getNorthEastCorner())) {
-		    current = null;
-        } else {
+			current = null;
+		} else {
 			current = rv.next();
-            while (hasNext() && !boundingBox.getBoundingBox().contains(current.getOriginatingPoint())) {
-                current = current.next();
-            }
-        }
-
+			while (hasNext() && (!boundingBox.getBoundingBox().contains(current.getOriginatingPoint()))) {
+				current = current.next();
+			} 
+		}
 		return rv;
 	}
 
--- src/test/java/ch/hsr/geohash/util/BoundingBoxGeoHashIteratorTest.java
+++ src/test/java/ch/hsr/geohash/util/BoundingBoxGeoHashIteratorTest.java
@@ -2,18 +2,17 @@ package ch.hsr.geohash.util;
 
 import ch.hsr.geohash.BoundingBox;
 import ch.hsr.geohash.GeoHash;
-import org.junit.Assert;
-import org.junit.Test;
-
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-
+import org.junit.Assert;
+import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsCollectionContaining.hasItem;
 import static org.hamcrest.core.IsNot.not;
 import static org.junit.Assert.assertThat;
+
 
 /**
  * Created by IntelliJ IDEA. User: kevin Date: Jan 6, 2011 Time: 3:05:43 PM
@@ -22,40 +21,36 @@ public class BoundingBoxGeoHashIteratorTest {
 	@Test
 	public void testIter() {
 		BoundingBox box = new BoundingBox(37.7, 37.84, -122.52, -122.35);
-		BoundingBoxGeoHashIterator iter = new BoundingBoxGeoHashIterator(
-				TwoGeoHashBoundingBox.withBitPrecision(box, 10));
+		BoundingBoxGeoHashIterator iter = new BoundingBoxGeoHashIterator(TwoGeoHashBoundingBox.withBitPrecision(box, 10));
 		checkIterator(iter);
 	}
 
 	@Test
 	public void testIter2() {
 		BoundingBox box = new BoundingBox(37.7, 37.84, -122.52, -122.35);
-		BoundingBoxGeoHashIterator iter = new BoundingBoxGeoHashIterator(
-				TwoGeoHashBoundingBox.withBitPrecision(box, 35));
+		BoundingBoxGeoHashIterator iter = new BoundingBoxGeoHashIterator(TwoGeoHashBoundingBox.withBitPrecision(box, 35));
 		checkIterator(iter);
 	}
 
 	@Test
 	public void testIter3() {
 		BoundingBox box = new BoundingBox(28.5, 67.15, -33.2, 44.5);
-		BoundingBoxGeoHashIterator iter = new BoundingBoxGeoHashIterator(
-				TwoGeoHashBoundingBox.withCharacterPrecision(box, 2));
+		BoundingBoxGeoHashIterator iter = new BoundingBoxGeoHashIterator(TwoGeoHashBoundingBox.withCharacterPrecision(box, 2));
 		List<GeoHash> hashes = checkIterator(iter);
 		assertThat(hashes.size(), is(49));
 	}
 
 	@Test
 	public void testEndlessIterator() {
-		BoundingBox box = new BoundingBox(72.28907f, 88.62655f, -50.976562f, 170.50781f);
+		BoundingBox box = new BoundingBox(72.28907F, 88.62655F, -50.976562F, 170.50781F);
 		TwoGeoHashBoundingBox twoGeoHashBoundingBox = TwoGeoHashBoundingBox.withCharacterPrecision(box, 2);
 		BoundingBoxGeoHashIterator iterator = new BoundingBoxGeoHashIterator(twoGeoHashBoundingBox);
-
 		Set<GeoHash> hashes = new HashSet<>();
 		while (iterator.hasNext()) {
 			GeoHash hash = iterator.next();
 			assertThat("Hash has been already produced by the iterator once: " + hash, hashes, not(hasItem(hash)));
 			hashes.add(hash);
-		}
+		} 
 	}
 
 	@Test
@@ -63,28 +58,26 @@ public class BoundingBoxGeoHashIteratorTest {
 		BoundingBox box = new BoundingBox(-90, 90, -180, 180);
 		TwoGeoHashBoundingBox twoGeoHashBoundingBox = TwoGeoHashBoundingBox.withCharacterPrecision(box, 2);
 		BoundingBoxGeoHashIterator iterator = new BoundingBoxGeoHashIterator(twoGeoHashBoundingBox);
-
 		Set<GeoHash> hashes = new HashSet<>();
 		while (iterator.hasNext()) {
 			GeoHash hash = iterator.next();
 			hashes.add(hash);
-		}
-
+		} 
 		assertThat(hashes.size(), is(1024));
 	}
 
 	@Test
 	public void testTopRightCorner() {
-		BoundingBox box = new BoundingBox(84.4, 84.9, 169.3, 179.6); // all in ZZ cell
+		BoundingBox box = new BoundingBox(84.4, 84.9, 169.3, 179.6);// all in ZZ cell
+
 		TwoGeoHashBoundingBox twoGeoHashBoundingBox = TwoGeoHashBoundingBox.withCharacterPrecision(box, 2);
 		BoundingBoxGeoHashIterator iterator = new BoundingBoxGeoHashIterator(twoGeoHashBoundingBox);
-
 		Set<GeoHash> hashes = new HashSet<>();
 		while (iterator.hasNext()) {
 			GeoHash hash = iterator.next();
 			assertThat("Hash has been already produced by the iterator once: " + hash, hashes, not(hasItem(hash)));
 			hashes.add(hash);
-		}
+		} 
 	}
 
 	private List<GeoHash> checkIterator(BoundingBoxGeoHashIterator iter) {
@@ -92,8 +85,7 @@ public class BoundingBoxGeoHashIteratorTest {
 		List<GeoHash> hashes = new ArrayList<>();
 		while (iter.hasNext()) {
 			hashes.add(iter.next());
-		}
-
+		} 
 		GeoHash prev = null;
 		for (GeoHash gh : hashes) {
 			if (prev != null) {
@@ -102,7 +94,6 @@ public class BoundingBoxGeoHashIteratorTest {
 			Assert.assertTrue(newBox.contains(gh.getOriginatingPoint()));
 			prev = gh;
 		}
-
 		return hashes;
 	}
 }
