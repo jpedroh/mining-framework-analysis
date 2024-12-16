14f7cc8c1dc3ce86107990a89144687db385a94e
Joao Pedro <joao.pedro.hsd@gmail.com>
2024-12-16 18:09:37 +0000

    Mining Framework Analysis

--- example/src/main/java/org/wicketopia/example/web/page/HomePage.java
+++ example/src/main/java/org/wicketopia/example/web/page/HomePage.java
@@ -1,47 +1,11 @@
-/*
- * Copyright (c) 2011 Carman Consulting, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-package org.wicketopia.example.web.page;
-
-import org.apache.wicket.spring.injection.annot.SpringBean;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.wicketopia.persistence.PersistenceProvider;
-
-/**
- * Homepage
- */
-public class HomePage extends BasePage
-{
-//----------------------------------------------------------------------------------------------------------------------
-// Fields
-//----------------------------------------------------------------------------------------------------------------------
-
-    private static final long serialVersionUID = 1L;
-
-    @SpringBean
-    private PersistenceProvider persistenceProvider;
-
-    @SpringBean
-    private AuthenticationManager authenticationManager;
-
-//----------------------------------------------------------------------------------------------------------------------
-// Constructors
-//----------------------------------------------------------------------------------------------------------------------
-
-    public HomePage()
-    {
-    }
-}
+  package     org . wicketopia . example . web . page ;   import       org . apache . wicket . spring . injection . annot . SpringBean ;  import     org . springframework . security . authentication . AuthenticationManager ;  import    org . wicketopia . persistence . PersistenceProvider ;   public class HomePage  extends BasePage  {   private static final  long  serialVersionUID = 1L ;    @ SpringBean private PersistenceProvider  persistenceProvider ;    @ SpringBean private AuthenticationManager  authenticationManager ;   public HomePage  ( )  { 
+<<<<<<<
+  add  (  new Link  ( "loginAdmin" )  {    @ Override public void onClick  ( )  {   final UsernamePasswordAuthenticationToken  tok =  new UsernamePasswordAuthenticationToken  ( "admin" , "admin" ) ;    SecurityContextHolder . getContext  ( ) . setAuthentication  (  authenticationManager . authenticate  ( tok ) ) ;   setResponsePage  (  HomePage . class ) ; }    @ Override public boolean isVisible  ( )  {  return    SecurityContextHolder . getContext  ( ) . getAuthentication  ( ) == null ; } } ) ;
+=======
+>>>>>>>
+ 
+<<<<<<<
+  add  (  new Link  ( "logout" )  {    @ Override public void onClick  ( )  {   SecurityContextHolder . clearContext  ( ) ;   setResponsePage  (  HomePage . class ) ; }    @ Override public boolean isVisible  ( )  {  return    SecurityContextHolder . getContext  ( ) . getAuthentication  ( ) != null ; } } ) ;
+=======
+>>>>>>>
+ } }
