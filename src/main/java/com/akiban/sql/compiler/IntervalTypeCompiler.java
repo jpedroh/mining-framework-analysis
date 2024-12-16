89967f280de28f3e7207e04e2c8f17b564df9b77
Joao Pedro <joao.pedro.hsd@gmail.com>
2024-12-16 17:57:45 +0000

    Mining Framework Analysis

--- src/main/java/com/akiban/sql/compiler/IntervalTypeCompiler.java
+++ src/main/java/com/akiban/sql/compiler/IntervalTypeCompiler.java
@@ -12,19 +12,16 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see http://www.gnu.org/licenses.
  */
-
 package com.akiban.sql.compiler;
 
-import com.akiban.sql.parser.*;
-
 import com.akiban.sql.StandardException;
+import com.akiban.sql.parser.*;
 import com.akiban.sql.types.DataTypeDescriptor;
 import com.akiban.sql.types.TypeId;
-
 import java.sql.Types;
 
-public class IntervalTypeCompiler extends TypeCompiler
-{
+
+public class IntervalTypeCompiler extends TypeCompiler {
     protected IntervalTypeCompiler(TypeId typeId) {
         super(typeId);
     }
@@ -47,7 +44,7 @@ public class IntervalTypeCompiler extends TypeCompiler
     public boolean compatible(TypeId otherType) {
         return convertible(otherType, false);
     }
-            
+
     /**
      * @see TypeCompiler#getCorrespondingPrimitiveTypeName
      */
@@ -132,56 +129,42 @@ public class IntervalTypeCompiler extends TypeCompiler
      *
      * @exception StandardException     Thrown on error
      */
-    public DataTypeDescriptor resolveArithmeticOperation(DataTypeDescriptor leftType,
-                                                         DataTypeDescriptor rightType,
-                                                         String operator)
-            throws StandardException {
+    public DataTypeDescriptor resolveArithmeticOperation(DataTypeDescriptor leftType, DataTypeDescriptor rightType, String operator) throws StandardException {
         TypeId rightTypeId = rightType.getTypeId();
         TypeId leftTypeId = leftType.getTypeId();
         boolean nullable = leftType.isNullable() || rightType.isNullable();
-
-        if (operator.equals(PLUS_OP) || operator.equals(MINUS_OP))
-        {
+        if (operator.equals(PLUS_OP) || operator.equals(MINUS_OP)) {
             // date/time and interval
             TypeId datetimeType;
-            if ((datetimeType = rightTypeId).isDateTimeTimeStampTypeId() && leftTypeId.isIntervalTypeId() ||
-                  (datetimeType = leftTypeId).isDateTimeTimeStampTypeID() && rightTypeId.isIntervalTypeId())
-                // Let specific datetime type resolve it.
+            // Let specific datetime type resolve it.
+            if (((datetimeType = rightTypeId).isDateTimeTimeStampTypeId() && leftTypeId.isIntervalTypeId()) || ((datetimeType = leftTypeId).isDateTimeTimeStampTypeID() && rightTypeId.isIntervalTypeId())) {
                 return getTypeCompiler(datetimeType).resolveArithmeticOperation(rightType, leftType, operator);
-        
+            }
             // interval and interval
             int typeFormatId = 0;
-            if (leftTypeId.isIntervalTypeId() && rightTypeId.isIntervalTypeId())
                 // two intervals are exactly the same
-                if (leftTypeId == rightTypeId)                    
+            if (leftTypeId.isIntervalTypeId() && rightTypeId.isIntervalTypeId()) {
+                if (leftTypeId == rightTypeId) {
                     return leftType.getNullabilityType(nullable);
-                // two intervals are of the same *type*
-                else if ((typeFormatId = leftTypeId.getTypeFormatId()) == rightTypeId.getTypeFormatId())
-                    return new DataTypeDescriptor(typeFormatId == TypeId.FormatIds.INTERVAL_DAY_SECOND_ID ?
-                                                    TypeId.INTERVAL_SECOND_ID : TypeId.INTERVAL_MONTH_ID,
-                                                    nullable);
-                        
+                } else if ((typeFormatId = leftTypeId.getTypeFormatId()) == rightTypeId.getTypeFormatId()) {
+                    return new DataTypeDescriptor(typeFormatId == TypeId.FormatIds.INTERVAL_DAY_SECOND_ID ? TypeId.INTERVAL_SECOND_ID : TypeId.INTERVAL_MONTH_ID, nullable);
+                }
+            }
             // varchar
-             DataTypeDescriptor varcharType;
-             if ((varcharType = leftType).getTypeId().isStringTypeId() && rightTypeId.isIntervalTypeId()||
-                 (varcharType = rightType).getTypeId().isStringTypeId() && leftTypeId.isIntervalTypeId()
-                    && operator.equals(PLUS_OP)) // when left is interval, only + is legal
+            DataTypeDescriptor varcharType;
+            // when left is interval, only + is legal
+            if (((varcharType = leftType).getTypeId().isStringTypeId() && rightTypeId.isIntervalTypeId()) || (((varcharType = rightType).getTypeId().isStringTypeId() && leftTypeId.isIntervalTypeId()) && operator.equals(PLUS_OP))) {
                 return new DataTypeDescriptor(varcharType.getPrecision() > 10 ? TypeId.DATETIME_ID : TypeId.DATE_ID, nullable);
-        }
-        else if (operator.equals(TIMES_OP) || operator.equals(DIVIDE_OP))
-        {   
+            }
+        } else if (operator.equals(TIMES_OP) || operator.equals(DIVIDE_OP)) {
             // numeric / varchar and interval
             TypeId intervalId = null;
-            if ((intervalId = leftTypeId).isIntervalTypeId() && 
-                    (rightTypeId.isNumericTypeId() || rightTypeId.isStringTypeId())||
-                (intervalId = rightTypeId).isIntervalTypeId() && 
-                    (leftTypeId.isNumericTypeId() || leftTypeId.isStringTypeId()) &&
-                    operator.equals(TIMES_OP)) // when right is interval, only * is legal
-                return new DataTypeDescriptor(intervalId, nullable);            
-        }        
-
+                    // when right is interval, only * is legal
+            if (((intervalId = leftTypeId).isIntervalTypeId() && (rightTypeId.isNumericTypeId() || rightTypeId.isStringTypeId())) || (((intervalId = rightTypeId).isIntervalTypeId() && (leftTypeId.isNumericTypeId() || leftTypeId.isStringTypeId())) && operator.equals(TIMES_OP))) {
+                return new DataTypeDescriptor(intervalId, nullable);
+            }
+        }
         // Unsupported
         return super.resolveArithmeticOperation(leftType, rightType, operator);
     }
-
 }
