978b6562c8c224017e86a32eef81ce17e3ae4b96
Joao Pedro <joao.pedro.hsd@gmail.com>
2024-12-16 18:06:10 +0000

    Mining Framework Analysis

--- src/main/java/com/github/dakusui/jcunit/generators/PairwiseTestArrayGenerator.java
+++ src/main/java/com/github/dakusui/jcunit/generators/PairwiseTestArrayGenerator.java
@@ -1,14 +1,14 @@
 package com.github.dakusui.jcunit.generators;
 
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
-
 import com.github.dakusui.jcunit.core.GeneratorParameters;
 import com.github.dakusui.jcunit.generators.ipo.IPO;
 import com.github.dakusui.jcunit.generators.ipo.TestRun;
 import com.github.dakusui.jcunit.generators.ipo.TestRunSet;
 import com.github.dakusui.jcunit.generators.ipo.TestSpace;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 
 /**
  * This class doesn't guarantee that the generated covering array is the smallest one.
@@ -26,16 +26,15 @@ public class PairwiseTestArrayGenerator<T, U> extends BaseTestArrayGenerator<T, 
 	 * A set of test runs.
 	 */
 	private TestRunSet testRunSet;
-	
+
 	/**
 	 * A map which associates IPO's indices (1-origin) to keys.
 	 */
 	private Map<Integer, T> indexToKeyMap = new HashMap<Integer, T>();
-	
+
 	@Override
-  public void init(GeneratorParameters.Value[] params,
-      LinkedHashMap<T, U[]> domains) {
-    super.init(params, domains);
+	public void init(Map<T, U[]> domains) {
+		super.init(params, domains);
 		this.testRunSet = this.composeTestRunSet(indexToKeyMap);
 		this.size = this.testRunSet.size();
 		this.cur = 0;
@@ -47,46 +46,47 @@ public class PairwiseTestArrayGenerator<T, U> extends BaseTestArrayGenerator<T, 
 	protected TestRunSet composeTestRunSet(Map<Integer, T> indexToKeyMap) {
 		Object[][] testSpaceDomains = new Object[this.domains.size()][];
 		int i = 0;
-		for (T cur: this.domains.keySet()) {
+		for (T cur : this.domains.keySet()) {
 			testSpaceDomains[i++] = this.domains.get(cur);
-			indexToKeyMap.put(i, cur); // since i is already incremented, put it as is. 
+			// since i is already incremented, put it as
+			indexToKeyMap.put(i, cur);
+																																	// is.
 		}
-	    TestSpace space = new TestSpace(testSpaceDomains);
-	    IPO ipo = new IPO(space);
-	    return ipo.ipo();
-  }
+		TestSpace space = new TestSpace(testSpaceDomains);
+		IPO ipo = new IPO(space);
+		return ipo.ipo();
+	}
 
-  @SuppressWarnings("unchecked")
-  @Override
-  public int getIndex(T key, long cur) {
-    TestRun run = this.testRunSet.get((int) cur);
-    // IPO classes provide 1-origin methods!
-    for (int i = 1; i <= run.width(); i++) {
-      T k = indexToKeyMap.get(i);
-      if (key.equals(k)) {
-        U[] domainOf_i = findDomain(k);
-        return indexOf((U) run.get(i), domainOf_i);
-      }
-    }
-    assert false;
-    return -1;
-  }
+	@SuppressWarnings("unchecked")
+	@Override
+	public int getIndex(T key, long cur) {
+		TestRun run = this.testRunSet.get(((int) (cur)));
+		// IPO classes provide 1-origin methods!
+		for (int i = 1; i <= run.width(); i++) {
+			T k = indexToKeyMap.get(i);
+			if (key.equals(k)) {
+				U[] domainOf_i = findDomain(k);
+				return indexOf(((U) (run.get(i))), domainOf_i);
+			}
+		}
+		assert false;
+		return -1;
+	}
 
-  private U[] findDomain(T key) {
-    return this.domains.get(key);
-  }
+	private U[] findDomain(T key) {
+		return this.domains.get(key);
+	}
 
-  /*
-   * returns an index of specified value by using '==' operator not by using
-   * 'equals' method.
-   */
-  private int indexOf(U u, U[] domain) {
-    int i = 0;
-    for (Object obj : domain) {
-      if (obj == u)
-        return i;
-      i++;
-    }
-    return -1;
-  }
+	/*
+	 * returns an index of specified value by using '==' operator not by using
+	 * 'equals' method.
+	 */
+	private int indexOf(U u, U[] domain) {
+		int i = 0;
+		for (Object obj : domain) {
+			if (obj == u) return i;
+			i++;
+		}
+		return -1;
+	}
 }
