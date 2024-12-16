4e99e25dbab8d7a3496e1605a614db24df4c7991
Joao Pedro <joao.pedro.hsd@gmail.com>
2024-12-16 18:11:04 +0000

    Mining Framework Analysis

--- src/main/java/com/citysearch/webwidget/action/NearbyPlacesAction.java
+++ src/main/java/com/citysearch/webwidget/action/NearbyPlacesAction.java
@@ -1,14 +1,5 @@
 package com.citysearch.webwidget.action;
 
-import java.io.UnsupportedEncodingException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLEncoder;
-import java.util.ArrayList;
-import java.util.List;
-
-import org.apache.log4j.Logger;
-
 import com.citysearch.webwidget.bean.HouseAd;
 import com.citysearch.webwidget.bean.NearbyPlace;
 import com.citysearch.webwidget.bean.NearbyPlacesRequest;
@@ -19,15 +10,22 @@ import com.citysearch.webwidget.helper.NearbyPlacesHelper;
 import com.citysearch.webwidget.util.CommonConstants;
 import com.opensymphony.xwork2.Action;
 import com.opensymphony.xwork2.ModelDriven;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.log4j.Logger;
 
-public class NearbyPlacesAction extends AbstractCitySearchAction implements
-        ModelDriven<NearbyPlacesRequest> {
 
+public class NearbyPlacesAction extends AbstractCitySearchAction implements ModelDriven<NearbyPlacesRequest> {
     private Logger log = Logger.getLogger(getClass());
 
     private static final String ACTION_FORWARD_CONQUEST = "conquest";
 
     private NearbyPlacesRequest nearbyPlacesRequest = new NearbyPlacesRequest();
+
     private NearbyPlacesResponse nearbyPlacesResponse;
 
     public NearbyPlacesRequest getModel() {
@@ -64,40 +62,30 @@ public class NearbyPlacesAction extends AbstractCitySearchAction implements
     }
 
     public String execute() throws CitysearchException {
-
         log.info("Begin NearbyPlacesAction");
         NearbyPlacesHelper helper = new NearbyPlacesHelper(getResourceRootPath());
         String adUnitSize = nearbyPlacesRequest.getAdUnitSize();
-
         try {
             nearbyPlacesResponse = helper.getNearbyPlaces(nearbyPlacesRequest);
-            if (nearbyPlacesResponse.getNearbyPlaces() != null
-                    && !nearbyPlacesResponse.getNearbyPlaces().isEmpty()) {
+            if ((nearbyPlacesResponse.getNearbyPlaces() != null) && (!nearbyPlacesResponse.getNearbyPlaces().isEmpty())) {
                 // For all nearby places found, set the listingUrl and callback function
                 for (NearbyPlace alb : nearbyPlacesResponse.getNearbyPlaces()) {
                     alb.setCallBackFunction(nearbyPlacesRequest.getCallBackFunction());
                     alb.setCallBackUrl(nearbyPlacesRequest.getCallBackUrl());
                     String listingUrl = null;
                     String callBackUrl = nearbyPlacesRequest.getCallBackUrl();
-                    if (callBackUrl != null && callBackUrl.trim().length() > 0) {
+                    if ((callBackUrl != null) && (callBackUrl.trim().length() > 0)) {
                         callBackUrl = callBackUrl.replace("$l", alb.getListingId());
                         callBackUrl = callBackUrl.replace("$p", alb.getPhone());
-                        listingUrl = getTrackingUrl(callBackUrl,
-                                "http://ad.doubleclick.net/clk;225291110;48835962;h?",
-                                alb.getListingId(), nearbyPlacesRequest.getPublisher(),
-                                nearbyPlacesRequest.getAdUnitName(), nearbyPlacesRequest.getAdUnitSize());
+                        listingUrl = getTrackingUrl(callBackUrl, "http://ad.doubleclick.net/clk;225291110;48835962;h?", alb.getListingId(), nearbyPlacesRequest.getPublisher(), nearbyPlacesRequest.getAdUnitName(), nearbyPlacesRequest.getAdUnitSize());
                     } else {
-                        listingUrl = getTrackingUrl(alb.getAdDisplayURL(),
-                                "http://ad.doubleclick.net/clk;225291110;48835962;h?",
-                                alb.getListingId(), nearbyPlacesRequest.getPublisher(),
-                                nearbyPlacesRequest.getAdUnitName(), nearbyPlacesRequest.getAdUnitSize());
+                        listingUrl = getTrackingUrl(alb.getAdDisplayURL(), "http://ad.doubleclick.net/clk;225291110;48835962;h?", alb.getListingId(), nearbyPlacesRequest.getPublisher(), nearbyPlacesRequest.getAdUnitName(), nearbyPlacesRequest.getAdUnitSize());
                     }
                     alb.setListingUrl(listingUrl);
-
                     // Set the call back function JS function here.
                     // Its messy to build the string in the JSP.
                     String callBackFn = alb.getCallBackFunction();
-                    if (callBackFn != null && callBackFn.trim().length() > 0) {
+                    if ((callBackFn != null) && (callBackFn.trim().length() > 0)) {
                         // Should produce javascript:fnName('param1','param2')
                         StringBuilder strBuilder = new StringBuilder("javascript:");
                         strBuilder.append(callBackFn);
@@ -106,9 +94,7 @@ public class NearbyPlacesAction extends AbstractCitySearchAction implements
                         strBuilder.append("\",\"");
                         strBuilder.append(alb.getPhone());
                         strBuilder.append("\")");
-
-                        log.info("CallBackFunction: "+strBuilder.toString());
-
+                        log.info("CallBackFunction: " + strBuilder.toString());
                         alb.setCallBackFunction(strBuilder.toString());
                     }
                 }
@@ -121,31 +107,29 @@ public class NearbyPlacesAction extends AbstractCitySearchAction implements
             log.error(cse.getMessage());
             throw cse;
         }
-
-        if (adUnitSize != null && adUnitSize.equals(CommonConstants.CONQUEST_AD_SIZE))
+        if ((adUnitSize != null) && adUnitSize.equals(CommonConstants.CONQUEST_AD_SIZE)) {
             return ACTION_FORWARD_CONQUEST;
-        else
+        } else {
             return Action.SUCCESS;
+        }
     }
 
-    private String getTrackingUrl(String adDisplayURL, String dartTrackingUrl, String listingId,
-            String publisher, String adUnitName, String adUnitSize) throws CitysearchException {
+    private String getTrackingUrl(String adDisplayURL, String dartTrackingUrl, String listingId, String publisher, String adUnitName, String adUnitSize) throws CitysearchException {
         try {
-            if (!adDisplayURL.startsWith("http://"))
-            {
+            if (!adDisplayURL.startsWith("http://")) {
                 StringBuilder strb = new StringBuilder("http://");
                 strb.append(adDisplayURL);
                 adDisplayURL = strb.toString();
             }
             URL url = new URL(adDisplayURL);
-            int prodDetId = 12; // Click outside Citysearch
+            int prodDetId = 12;// Click outside Citysearch
+
             String host = url.getHost();
-            if (host.indexOf("citysearch.com") != -1) {
+            if (host.indexOf("citysearch.com") != (-1)) {
                 prodDetId = 16;
             }
             StringBuilder dartUrl = new StringBuilder(dartTrackingUrl);
             dartUrl.append(adDisplayURL);
-
             StringBuilder strBuilder = new StringBuilder("http://pfpc.citysearch.com/pfp/ad?");
             strBuilder.append("directUrl=");
             strBuilder.append(URLEncoder.encode(dartUrl.toString(), "UTF-8"));
@@ -156,7 +140,7 @@ public class NearbyPlacesAction extends AbstractCitySearchAction implements
             strBuilder.append("&prodDetId=");
             strBuilder.append(prodDetId);
             strBuilder.append("&placement=");
-            strBuilder.append(URLEncoder.encode(publisher+"_"+adUnitName+"_"+adUnitSize, "UTF-8"));
+            strBuilder.append(URLEncoder.encode((((publisher + "_") + adUnitName) + "_") + adUnitSize, "UTF-8"));
             return strBuilder.toString();
         } catch (MalformedURLException mue) {
             throw new CitysearchException("NearbyPlacesAction", "getTrackingUrl", mue);
