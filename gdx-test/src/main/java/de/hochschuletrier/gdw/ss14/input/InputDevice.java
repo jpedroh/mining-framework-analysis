538844188224232bff48572239145929ca3800e5
Joao Pedro <joao.pedro.hsd@gmail.com>
2024-12-16 18:04:11 +0000

    Mining Framework Analysis

--- gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/input/InputDevice.java
+++ gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/input/InputDevice.java
@@ -1,28 +1,31 @@
 package de.hochschuletrier.gdw.ss14.input;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 
-public abstract class InputDevice {
 
+public abstract class InputDevice {
     public static enum DeviceType {
+
         MOUSE("mouse"),
         KEYBOARD("keyboard"),
         GAMEPAD("gamepad");
-        
         private String deviceName;
 
         private DeviceType(String deviceName) {
             this.deviceName = deviceName;
         }
-        
+
         @Override
         public String toString() {
-            return deviceName; 
+            return deviceName;
         }
-    };
-    
-	protected LinkedList<GameInputAdapter> listener = new LinkedList<>(); 
-	
+    }
+
+    private HashMap<String, InputAction> buttonMap = new HashMap<>();
+
+protected LinkedList<GameInputAdapter> listener = new LinkedList<>();
+
     /**
      * 
      * @param gia @ GameInputAdapter to the Listener
@@ -30,48 +33,48 @@ public abstract class InputDevice {
     public void addGameInputAdapter(GameInputAdapter gia) {
     	listener.add(gia);
     }
-    
+
     /**
      * 
      * @param gia remove GameInputAdapter gia from the listener
      */
     public void removeGameInputAdapter(GameInputAdapter gia) {
     	listener.remove(gia);
-	}
-    
+    	}
+
     /**
-	 *  Laser on / off
-	 */
-	protected void fireLaserButtonPressed() {	
-		for(GameInputAdapter inp: listener) {
-			inp.laserButtonPressed();
-		}
+    	 *  Laser on / off
+    	 */
+    	protected void fireLaserButtonPressed() {	
+    		for(GameInputAdapter inp: listener) {
+    			inp.laserButtonPressed();
+    		}
+    	}
+
+/**
+ * water pistol on
+ */
+protected void fireWaterPistolButtonDown() {
+	for(GameInputAdapter inp: listener) {
+		inp.waterPistolButtonDown();
 	}
-	
-	/**
-	 * water pistol on
-	 */
-	protected void fireWaterPistolButtonDown() {
-		for(GameInputAdapter inp: listener) {
-			inp.waterPistolButtonDown();
-		}
+}
+
+/**
+ * water pistol off
+ */
+protected void fireWaterPistolButtonUp() {
+	for(GameInputAdapter inp: listener) {
+		inp.waterPistolButtonUp();
 	}
-	
-	/**
-	 * water pistol off
-	 */
-	protected void fireWaterPistolButtonUp() {
-		for(GameInputAdapter inp: listener) {
-			inp.waterPistolButtonUp();
-		}
+}
+
+/**
+ * go to the menu or back
+ */
+protected void fireMenuButtonPressed() {
+	for(GameInputAdapter inp: listener) {
+		inp.menueButtonPressed();
 	}
-	
-	/**
-	 * go to the menu or back
-	 */
-	protected void fireMenuButtonPressed() {
-		for(GameInputAdapter inp: listener) {
-			inp.menueButtonPressed();
-		}
-	}
+}
 }
--- gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/input/InputManager.java
+++ gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/input/InputManager.java
@@ -1,47 +1,47 @@
 package de.hochschuletrier.gdw.ss14.input;
 
+import de.hochschuletrier.gdw.ss14.game.GameSettings;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+
 public class InputManager {
     private static final Logger logger = LoggerFactory.getLogger(InputManager.class);
+
     private static InputManager instance;
-    
-    public static InputManager getInstance () {
+
+    public static InputManager getInstance() {
         if (instance == null) {
             logger.error("InputManager not initialized!");
         }
         return instance;
     }
-    
-    private InputManager(){
-        
-        /*
+
+    private InputManager() {
         switch (GameSettings.getInstance().getInputDevice()) {
-        case MOUSE:
-            break;
-        case KEYBOARD:
-            break;
-        case GAMEPAD:
-            break;
+            case MOUSE :
+                break;
+            case KEYBOARD :
+                break;
+            case GAMEPAD :
+                break;
         }
-        */
     }
-    
+
     private InputDevice inputDevice = new InputMouse();
-    
+
     public void addGameInputAdapter(GameInputAdapter gia) {
         inputDevice.addGameInputAdapter(gia);
     }
-    
+
     public void removeGameInputAdapter(GameInputAdapter gia) {
         inputDevice.addGameInputAdapter(gia);
     }
-    
+
     public void update() {
         
     }
-    
+
     public static void init() {
         if (instance != null) {
             logger.info("InputManager already initialized!");
@@ -49,5 +49,4 @@ public class InputManager {
         }
         instance = new InputManager();
     }
-    
 }
