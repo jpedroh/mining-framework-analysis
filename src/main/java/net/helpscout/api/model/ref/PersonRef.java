d846923573e50197eb7d9c7092a09e08964d9048
Joao Pedro <joao.pedro.hsd@gmail.com>
2024-12-16 18:25:00 +0000

    Mining Framework Analysis

--- src/main/java/net/helpscout/api/model/ref/PersonRef.java
+++ src/main/java/net/helpscout/api/model/ref/PersonRef.java
@@ -1,9 +1,1 @@
-package net.helpscout.api.model.ref;
-
-public interface PersonRef {
-	public Long getId();
-	public String getFirstName();
-	public String getLastName();
-	public String getEmail();
-	public String getType();
-}
+  package     net . helpscout . api . model . ref ;   public interface PersonRef  {   public Long getId  ( ) ;   public String getFirstName  ( ) ;   public String getLastName  ( ) ;   public String getEmail  ( ) ;   public void setId  (  Long id ) ;   public void setFirstName  (  String firstName ) ;   public void setLastName  (  String lastName ) ;   public void setEmail  (  String email ) ;   public String getType  ( ) ; }
