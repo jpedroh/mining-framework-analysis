64e54b17811533c4d4f8640b638ac1f757df58f4
Joao Pedro <joao.pedro.hsd@gmail.com>
2024-12-16 18:08:06 +0000

    Mining Framework Analysis

--- distributed/src/main/java/com/github/dozedoff/similarImage/messaging/HasherNode.java
+++ distributed/src/main/java/com/github/dozedoff/similarImage/messaging/HasherNode.java
@@ -17,12 +17,15 @@
  */
 package com.github.dozedoff.similarImage.messaging;
 
+import com.codahale.metrics.Meter;
+import com.codahale.metrics.MetricRegistry;
+import com.github.dozedoff.commonj.hash.ImagePHash;
+import com.github.dozedoff.similarImage.io.ByteBufferInputstream;
+import com.github.dozedoff.similarImage.util.MessagingUtil;
 import java.awt.image.BufferedImage;
 import java.nio.ByteBuffer;
 import java.util.UUID;
-
 import javax.imageio.ImageIO;
-
 import org.apache.activemq.artemis.api.core.ActiveMQException;
 import org.apache.activemq.artemis.api.core.client.ClientConsumer;
 import org.apache.activemq.artemis.api.core.client.ClientMessage;
@@ -32,11 +35,6 @@ import org.apache.activemq.artemis.api.core.client.MessageHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.codahale.metrics.Meter;
-import com.codahale.metrics.MetricRegistry;
-import com.github.dozedoff.commonj.hash.ImagePHash;
-import com.github.dozedoff.similarImage.io.ByteBufferInputstream;
-import com.github.dozedoff.similarImage.util.MessagingUtil;
 
 /**
  * Consumes resize messages, hashes them and produces result messages.
@@ -50,10 +48,15 @@ public class HasherNode implements MessageHandler {
 	private static final int INITIAL_BUFFER_SIZE = 4096;
 
 	private final ClientConsumer consumer;
+
 	private final ClientProducer producer;
+
 	private final ClientSession session;
+
 	private final ImagePHash hasher;
+
 	private ByteBuffer buffer;
+
 	private MessageFactory messageFactory;
 
 	private final Meter hashRequests;
@@ -62,46 +65,48 @@ public class HasherNode implements MessageHandler {
 
 	/**
 	 * Create a hash consumer that listens and responds on the given addresses.
-	 * 
+	 *
 	 * @param session
-	 *            of the client
+	 * 		of the client
 	 * @param hasher
-	 *            to use for hashing files
+	 * 		to use for hashing files
 	 * @param requestAddress
-	 *            to listen to
+	 * 		to listen to
 	 * @param resultAddress
-	 *            where to send the results of hashing
-	 * @param metrics
-	 *            registry for tracking metrics
+	 * 		where to send the results of hashing
 	 * @throws ActiveMQException
-	 *             if there is an error with the queue
+	 * 		if there is an error with the queue
+	 * @throws ActiveMQException
+	 * 		if there is an error with the queue
 	 */
-	public HasherNode(ClientSession session, ImagePHash hasher, String requestAddress, String resultAddress,
-			MetricRegistry metrics) throws ActiveMQException {
+	public HasherNode(ClientSession session, ImagePHash hasher, String requestAddress, String resultAddress, MetricRegistry metrics) throws ActiveMQException {
 		this.hasher = hasher;
 		this.session = session;
 		this.consumer = session.createConsumer(requestAddress);
 		this.producer = session.createProducer(resultAddress);
 		this.consumer.setMessageHandler(this);
 		this.messageFactory = new MessageFactory(session);
-
+<<<<<<< LEFT
 		this.hashRequests = metrics.meter(METRIC_NAME_HASH_MESSAGES);
+=======
 		this.buffer = ByteBuffer.allocate(INITIAL_BUFFER_SIZE);
+>>>>>>> RIGHT
+
 	}
 
 	/**
 	 * Create a hash consumer that listens and responds on the given addresses.
-	 * 
+	 *
 	 * @param session
-	 *            of the client
+	 * 		of the client
 	 * @param hasher
-	 *            to use for hashing files
+	 * 		to use for hashing files
 	 * @param requestAddress
-	 *            to listen to
+	 * 		to listen to
 	 * @param resultAddress
-	 *            where to send the results of hashing
+	 * 		where to send the results of hashing
 	 * @throws ActiveMQException
-	 *             if there is an error with the queue
+	 * 		if there is an error with the queue
 	 * @deprecated Use the constructor with {@link MetricRegistry}
 	 */
 	@Deprecated
@@ -132,23 +137,20 @@ public class HasherNode implements MessageHandler {
 		try {
 			long most = message.getBodyBuffer().readLong();
 			long least = message.getBodyBuffer().readLong();
-
 			if (LOGGER.isTraceEnabled()) {
 				LOGGER.trace("Got hash request with UUID {}, size {}", new UUID(most, least), message.getBodySize());
 			}
-
 			checkBufferCapacity(message.getBodySize());
 			buffer.limit(message.getBodySize());
 			buffer.rewind();
 			message.getBodyBuffer().readBytes(buffer);
 			buffer.rewind();
-
 			long hash = doHash(ImageIO.read(new ByteBufferInputstream(buffer)));
 			ClientMessage response = messageFactory.resultMessage(hash, most, least);
 			producer.send(response);
 		} catch (ActiveMQException e) {
 			LOGGER.error("Failed to process message: {}", e.toString());
-		} catch (Exception e) {
+		} catch (java.lang.Exception e) {
 			LOGGER.error("Failed to process image: {}", e.toString());
 		}
 	}
