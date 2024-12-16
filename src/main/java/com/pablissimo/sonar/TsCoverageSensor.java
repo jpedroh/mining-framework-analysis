b441860c4d1c2a3f5b86ec3e80d4a09133fa117c
Joao Pedro <joao.pedro.hsd@gmail.com>
2024-12-16 17:58:01 +0000

    Mining Framework Analysis

--- src/main/java/com/pablissimo/sonar/TsCoverageSensor.java
+++ src/main/java/com/pablissimo/sonar/TsCoverageSensor.java
@@ -2,10 +2,10 @@ package com.pablissimo.sonar;
 
 import java.util.Map;
 import java.util.Set;
-
 import org.sonar.api.batch.BatchSide;
 import org.sonar.api.batch.fs.InputFile;
 import org.sonar.api.batch.sensor.SensorContext;
+
 
 @BatchSide
 public interface TsCoverageSensor {
