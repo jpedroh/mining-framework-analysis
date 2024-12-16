0af1c1de50b2c709ea8e41ab91c050ad106ae860
Joao Pedro <joao.pedro.hsd@gmail.com>
2024-12-16 17:57:42 +0000

    Mining Framework Analysis

--- core/src/main/java/tech/tablesaw/aggregate/BooleanAggregateFunction.java
+++ core/src/main/java/tech/tablesaw/aggregate/BooleanAggregateFunction.java
@@ -1,26 +1,7 @@
-package tech.tablesaw.aggregate;
-
-import tech.tablesaw.api.BooleanColumn;
-import tech.tablesaw.api.ColumnType;
-
-/**
- * A partial implementation of aggregate functions to summarize over a boolean column
- */
-public abstract class BooleanAggregateFunction extends AggregateFunction<Boolean, BooleanColumn> {
-
-    public BooleanAggregateFunction(String name) {
-        super(name);
-    }
-
-    abstract public Boolean summarize(BooleanColumn column);
-
-    @Override
-    public boolean isCompatableColumn(ColumnType type) {
-        return type == ColumnType.BOOLEAN;
-    }
-
-    @Override
-    public ColumnType returnType() {
-        return ColumnType.BOOLEAN;
-    }
-}
+  package   tech . tablesaw . aggregate ;   import    tech . tablesaw . api . ColumnType ;  import    tech . tablesaw . api . BooleanColumn ;   public abstract class BooleanAggregateFunction  <  C  extends  Column  <  ? > >  extends  AggregateFunction  < Boolean , 
+<<<<<<<
+C
+=======
+BooleanColumn
+>>>>>>>
+ >  {   public BooleanAggregateFunction  (  String name )  {  super  ( name ) ; }    @ Override public boolean isCompatableColumn  (  ColumnType type )  {  return  type ==  ColumnType . BOOLEAN ; }    @ Override public ColumnType returnType  ( )  {  return  ColumnType . BOOLEAN ; }   abstract public Boolean summarize  (  BooleanColumn column ) ; }
