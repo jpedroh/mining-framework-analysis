149e86a15c1404a610ece33e7d7929ab4bb52c75
Joao Pedro <joao.pedro.hsd@gmail.com>
2024-12-16 18:12:02 +0000

    Mining Framework Analysis

--- src/main/java/com/jadventure/game/navigation/LocationType.java
+++ src/main/java/com/jadventure/game/navigation/LocationType.java
@@ -1,15 +1,1 @@
-package com.jadventure.game.navigation;
-
-/*
- * A enum list of all the locationtypes.
- * For now, it only includes environments
- * and not safezones or city areas.
- */
-public enum LocationType {
-	FOREST,
-	SWAMP,
-	MOUNTAIN,
-	PLAINS,
-    WALL,
-	CAVE;
-}
+  package    com . jadventure . game . navigation ;   public enum LocationType  {  FOREST ,  SWAMP ,  MOUNTAIN ,  PLAINS ,  WALL ,  WALL  ; }
