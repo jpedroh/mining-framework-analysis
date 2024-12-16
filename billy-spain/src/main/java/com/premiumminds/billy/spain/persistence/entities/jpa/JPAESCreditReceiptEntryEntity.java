c1e52be695991d9eab045016a9e854fafd3f382d
Joao Pedro <joao.pedro.hsd@gmail.com>
2024-12-16 18:06:52 +0000

    Mining Framework Analysis

--- billy-spain/src/main/java/com/premiumminds/billy/spain/persistence/entities/jpa/JPAESCreditReceiptEntryEntity.java
+++ billy-spain/src/main/java/com/premiumminds/billy/spain/persistence/entities/jpa/JPAESCreditReceiptEntryEntity.java
@@ -1,73 +1,11 @@
-/*
- * Copyright (C) 2017 Premium Minds.
- *
- * This file is part of billy spain (ES Pack).
- *
- * billy spain (ES Pack) is free software: you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation, either version 3 of the License, or (at your option) any
- * later version.
- *
- * billy spain (ES Pack) is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with billy spain (ES Pack). If not, see <http://www.gnu.org/licenses/>.
- */
-package com.premiumminds.billy.spain.persistence.entities.jpa;
-
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
-
-import org.hibernate.envers.Audited;
-
-import com.premiumminds.billy.spain.Config;
-import com.premiumminds.billy.spain.persistence.entities.ESCreditReceiptEntryEntity;
-import com.premiumminds.billy.spain.services.entities.ESReceipt;
-
-@Entity
-@Audited
-@Table(name = Config.TABLE_PREFIX + "CREDIT_RECEIPT_ENTRY")
-public class JPAESCreditReceiptEntryEntity extends JPAESGenericInvoiceEntryEntity
-        implements ESCreditReceiptEntryEntity {
-
-    /**
-     *
-     */
-    private static final long serialVersionUID = 1L;
-
-    @OneToOne(fetch = FetchType.EAGER, targetEntity = JPAESReceiptEntity.class,
-            cascade = { CascadeType.PERSIST, CascadeType.MERGE })
-    @JoinColumn(name = "ID_ESRECEIPT", referencedColumnName = "ID")
-    protected ESReceipt receiptReference;
-
-    @Column(name = "REASON")
-    protected String reason;
-
-    @Override
-    public String getReason() {
-        return this.reason;
-    }
-
-    @Override
-    public ESReceipt getReference() {
-        return this.receiptReference;
-    }
-
-    @Override
-    public void setReference(ESReceipt reference) {
-        this.receiptReference = reference;
-    }
-
-    @Override
-    public void setReason(String reason) {
-        this.reason = reason;
-    }
-}
+  package       com . premiumminds . billy . spain . persistence . entities . jpa ;   import       com . premiumminds . billy . core . persistence . entities . GenericInvoiceEntity ;  import   javax . persistence . CascadeType ;  import   javax . persistence . Column ;  import   javax . persistence . Entity ;  import   javax . persistence . FetchType ;  import   javax . persistence . JoinColumn ;  import   javax . persistence . OneToOne ;  import   javax . persistence . Table ;  import    org . hibernate . envers . Audited ;  import     com . premiumminds . billy . spain . Config ;  import       com . premiumminds . billy . spain . persistence . entities . ESCreditReceiptEntryEntity ;    @ Entity  @ Audited  @ Table  (  name =   Config . TABLE_PREFIX + "CREDIT_RECEIPT_ENTRY" ) public class JPAESCreditReceiptEntryEntity  extends JPAESGenericInvoiceEntryEntity  implements  ESCreditReceiptEntryEntity  {   private static final  long  serialVersionUID = 1L ; 
+<<<<<<<
+   @ OneToOne  (  fetch =  FetchType . EAGER ,  targetEntity =  JPAESReceiptEntity . class ,  cascade =  {  CascadeType . PERSIST ,  CascadeType . MERGE } )  @ JoinColumn  (  name = "ID_ESRECEIPT" ,  referencedColumnName = "ID" ) protected GenericInvoiceEntity  reference ;
+=======
+>>>>>>>
+    @ Column  (  name = "REASON" ) protected String  reason ;    @ Override public String getReason  ( )  {  return  this . reason ; }    @ Override public GenericInvoiceEntity getReference  ( )  {  return  this . receiptReference ; }    @ Override public void setReference  (  GenericInvoiceEntity reference )  {    this . reference = reference ; }    @ Override public void setReason  (  String reason )  {    this . reason = reason ; }    @ OneToOne  (  fetch =  FetchType . EAGER ,  targetEntity =  JPAESReceiptEntity . class ,  cascade =  {  CascadeType . PERSIST ,  CascadeType . MERGE } )  @ JoinColumn  (  name = "ID_ESRECEIPT" ,  referencedColumnName = "ID" ) protected ESReceipt  receiptReference ; 
+<<<<<<<
+=======
+   @ Override public void setReference  (  ESReceipt reference )  {    this . receiptReference = reference ; }
+>>>>>>>
+ }
