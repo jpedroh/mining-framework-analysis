e55a603e95d1360cefb4d7f912b7337a1c149e96
Joao Pedro <joao.pedro.hsd@gmail.com>
2024-12-16 18:18:05 +0000

    Mining Framework Analysis

--- src/de/uni_koblenz/jgralab/eca/Condition.java
+++ src/de/uni_koblenz/jgralab/eca/Condition.java
@@ -1,15 +1,16 @@
 package de.uni_koblenz.jgralab.eca;
 
 import de.uni_koblenz.jgralab.eca.events.Event;
+import de.uni_koblenz.jgralab.greql2.types.Types;
+
 
 public interface Condition {
-
 	/**
 	 * Evaluates the condition
-	 *
+	 * 
 	 * @param event
 	 *            an Event containing the element to check the condition for
 	 * @return if the condition is evaluated to true
 	 */
-	public boolean evaluate(Event event);
+	public abstract boolean evaluate(Event event);
 }
