d3392a65d1b9518b96c0fa1cce0231c3087da0a3
Joao Pedro <joao.pedro.hsd@gmail.com>
2024-12-16 18:11:33 +0000

    Mining Framework Analysis

--- src/main/java/org/jvnet/libpam/PAM.java
+++ src/main/java/org/jvnet/libpam/PAM.java
@@ -23,17 +23,17 @@
  */
 package org.jvnet.libpam;
 
-import org.jvnet.libpam.impl.PAMLibrary.*;
-import static org.jvnet.libpam.impl.PAMLibrary.*;
-import org.jvnet.libpam.impl.PAMLibrary.pam_conv.PamCallback;
-import org.jvnet.libpam.impl.CLibrary.passwd;
-import static org.jvnet.libpam.impl.CLibrary.libc;
 import com.sun.jna.Pointer;
-import static com.sun.jna.Native.POINTER_SIZE;
 import com.sun.jna.ptr.PointerByReference;
 import java.util.Set;
-
 import java.util.logging.Logger;
+import org.jvnet.libpam.impl.CLibrary.passwd;
+import org.jvnet.libpam.impl.PAMLibrary.*;
+import org.jvnet.libpam.impl.PAMLibrary.pam_conv.PamCallback;
+import static com.sun.jna.Native.POINTER_SIZE;
+import static org.jvnet.libpam.impl.CLibrary.libc;
+import static org.jvnet.libpam.impl.PAMLibrary.*;
+
 
 /**
  * PAM authenticator.
@@ -54,10 +54,9 @@ import java.util.logging.Logger;
  */
 public class PAM {
     private pam_handle_t pht;
-    /**
-     * This structure needs to survive for the entire duration of authenticate invocations.
-     */
+
     private pam_conv conv;
+
     private int ret;
 
     /**
@@ -76,30 +75,28 @@ public class PAM {
     public PAM(String serviceName) throws PAMException {
         conv = new pam_conv(new PamCallback() {
             public int callback(int num_msg, Pointer msg, Pointer resp, Pointer __) {
-                LOGGER.fine("pam_conv num_msg="+num_msg);
-                if(password==null)
+                LOGGER.fine("pam_conv num_msg=" + num_msg);
+                if (password == null) {
                     return PAM_CONV_ERR;
-
+                }
                 // allocates pam_response[num_msg]. the caller will free this
-                Pointer m = libc.calloc(pam_response.SIZE,num_msg);
-                resp.setPointer(0,m);
-
-                for( int i=0; i<num_msg; i++ ) {
-                    pam_message pm = new pam_message(msg.getPointer(POINTER_SIZE*i));
-                    LOGGER.fine(pm.msg_style+":"+pm.msg);
-                    if(pm.msg_style==PAM_PROMPT_ECHO_OFF) {
-                        pam_response r = new pam_response(m.share(pam_response.SIZE*i));
+                Pointer m = libc.calloc(pam_response.SIZE, num_msg);
+                resp.setPointer(0, m);
+                for (int i = 0; i < num_msg; i++) {
+                    pam_message pm = new pam_message(msg.getPointer(POINTER_SIZE * i));
+                    LOGGER.fine((pm.msg_style + ":") + pm.msg);
+                    if (pm.msg_style == PAM_PROMPT_ECHO_OFF) {
+                        pam_response r = new pam_response(m.share(pam_response.SIZE * i));
                         r.setResp(password);
-                        r.write(); // write to (*resp)[i]
+                        r.write();// write to (*resp)[i]
+
                     }
                 }
-
                 return PAM_SUCCESS;
             }
         });
-
         PointerByReference phtr = new PointerByReference();
-        check(libpam.pam_start(serviceName,null,conv,phtr), "pam_start failed");
+        check(libpam.pam_start(serviceName, null, conv, phtr), "pam_start failed");
         pht = new pam_handle_t(phtr.getValue());
     }
 
@@ -124,19 +121,19 @@ public class PAM {
     public UnixUser authenticate(String username, String password) throws PAMException {
         this.password = password;
         try {
-            check(libpam.pam_set_item(pht,PAM_USER,username),"pam_set_item failed");
-            check(libpam.pam_authenticate(pht,0),"pam_authenticate failed");
-            check(libpam.pam_setcred(pht,0),"pam_setcred failed");
+            check(libpam.pam_set_item(pht, PAM_USER, username), "pam_set_item failed");
+            check(libpam.pam_authenticate(pht, 0), "pam_authenticate failed");
+            check(libpam.pam_setcred(pht, 0), "pam_setcred failed");
             // several different error code seem to be used to represent authentication failures
-            check(libpam.pam_acct_mgmt(pht,0),"pam_acct_mgmt failed");
-
+            check(libpam.pam_acct_mgmt(pht, 0), "pam_acct_mgmt failed");
             PointerByReference r = new PointerByReference();
-            check(libpam.pam_get_item(pht,PAM_USER,r),"pam_get_item failed");
+            check(libpam.pam_get_item(pht, PAM_USER, r), "pam_get_item failed");
             String userName = r.getValue().getString(0);
             passwd pwd = libc.getpwnam(userName);
-            if(pwd==null)
+            if (pwd == null) {
                 throw new PAMException("Authentication succeeded but no user information is available");
-            return new UnixUser(userName,pwd);
+            }
+            return new UnixUser(userName, pwd);
         } finally {
             this.password = null;
         }
@@ -175,7 +172,6 @@ public class PAM {
             pht=null;
         }
     }
-
 
     @Override
     protected void finalize() throws Throwable {
