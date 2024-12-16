c75a2a8652d2976cddcf6db471e0201985f7546e
Joao Pedro <joao.pedro.hsd@gmail.com>
2024-12-16 18:04:40 +0000

    Mining Framework Analysis

--- src/main/java/org/spoutcraft/launcher/GUI/LoginForm.java
+++ src/main/java/org/spoutcraft/launcher/GUI/LoginForm.java
@@ -6,18 +6,20 @@ import java.awt.Font;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
-import java.io.EOFException;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.Stack;
-
 import javax.crypto.Cipher;
 import javax.crypto.CipherInputStream;
 import javax.crypto.CipherOutputStream;
@@ -39,20 +41,20 @@ import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 import javax.swing.event.HyperlinkEvent;
 import javax.swing.event.HyperlinkListener;
-
-import org.spoutcraft.launcher.GameUpdater;
-import org.spoutcraft.launcher.MinecraftUtils;
-import org.spoutcraft.launcher.PlatformUtils;
 import org.spoutcraft.launcher.Exceptions.BadLoginException;
 import org.spoutcraft.launcher.Exceptions.MCNetworkException;
 import org.spoutcraft.launcher.Exceptions.OutdatedMCLauncherException;
+import org.spoutcraft.launcher.GameUpdater;
+import org.spoutcraft.launcher.MinecraftUtils;
+import org.spoutcraft.launcher.PlatformUtils;
+
 
 public class LoginForm extends JFrame implements ActionListener {
-
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -192904429165686059L;
+
 	private JPanel contentPane;
 
 	/**
@@ -71,40 +73,42 @@ public class LoginForm extends JFrame implements ActionListener {
 		});
 	}
 
-	  private Cipher getCipher(int mode, String password) throws Exception {
-	    Random random = new Random(43287234L);
-	    byte[] salt = new byte[8];
-	    random.nextBytes(salt);
-	    PBEParameterSpec pbeParamSpec = new PBEParameterSpec(salt, 5);
+			private Cipher getCipher(int mode, String password) throws Exception {
+			  Random random = new Random(43287234L);
+			  byte[] salt = new byte[8];
+			  random.nextBytes(salt);
+			  PBEParameterSpec pbeParamSpec = new PBEParameterSpec(salt, 5);
 
-	    SecretKey pbeKey = SecretKeyFactory.getInstance("PBEWithMD5AndDES").generateSecret(new PBEKeySpec(password.toCharArray()));
-	    Cipher cipher = Cipher.getInstance("PBEWithMD5AndDES");
-	    cipher.init(mode, pbeKey, pbeParamSpec);
-	    return cipher;
-	  }
-	
+			  SecretKey pbeKey = SecretKeyFactory.getInstance("PBEWithMD5AndDES").generateSecret(new PBEKeySpec(password.toCharArray()));
+			  Cipher cipher = Cipher.getInstance("PBEWithMD5AndDES");
+			  cipher.init(mode, pbeKey, pbeParamSpec);
+			  return cipher;
+			}
+
 	/**
 	 * Create the frame.
 	 */
 	JEditorPane jedHTML = new JEditorPane();
+
 	@SuppressWarnings("rawtypes")
 	private Stack urlStack = new Stack();
+
 	private JPasswordField txtPassword;
+
 	private JComboBox cmbUsername = new JComboBox();
-	private JButton btnLogin = new JButton("Login");		
+
+	private JButton btnLogin = new JButton("Login");
+
 	private JCheckBox cbRemember = new JCheckBox("Remember");
-	private JButton btnOptions = new JButton("Options");;
-	
+
+	private JButton btnOptions = new JButton("Options");
+
 	public LoginForm() {
-		
 		btnLogin.setBounds(761, 390, 86, 23);
 		btnLogin.addActionListener(this);
 		btnOptions.addActionListener(this);
-		cmbUsername.addActionListener(this);
-		
 		setIconImage(Toolkit.getDefaultToolkit().getImage(LoginForm.class.getResource("/org/spoutcraft/launcher/favicon.png")));
 		setResizable(false);
-		
 		try {
 			jedHTML = new JEditorPane("http://updates.getspout.org/");
 			jedHTML.setEditable(false);
@@ -113,64 +117,47 @@ public class LoginForm extends JFrame implements ActionListener {
 			jedHTML.setEditable(false);
 			jedHTML.setText("Could not connect to the Spoutcraft Updates Page. Don't worry about it though :P");
 		}
-		
 		jedHTML.setBounds(0, 0, 855, 381);
+		jedHTML.setEditable(false);
 		jedHTML.setForeground(new Color(255, 255, 255));
-		
-		jedHTML.addHyperlinkListener(new HyperlinkListener()
-        {
-             @SuppressWarnings("unchecked")
-			public void hyperlinkUpdate(HyperlinkEvent event)
-             {
-                  if(event.getEventType() == HyperlinkEvent.EventType.ACTIVATED)
-                  {
-                       try {
-                            urlStack.push(event.getURL().toString());
-
-                            jedHTML.setPage(event.getURL());
-                       } catch(IOException e) {
-                            jedHTML.setText("Error: " + e);
-                       }
-                  }
-             }
-        });
-		
+		jedHTML.addHyperlinkListener(new HyperlinkListener() {
+			@SuppressWarnings("unchecked")
+			public void hyperlinkUpdate(HyperlinkEvent event) {
+				if (event.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
+					try {
+						urlStack.push(event.getURL().toString());
+						jedHTML.setPage(event.getURL());
+					} catch (IOException e) {
+						jedHTML.setText("Error: " + e);
+					}
+				}
+			}
+		});
 		setTitle("Spoutcraft Launcher");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 861, 500);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		
 		JLabel lblLogo = new JLabel("");
 		lblLogo.setBounds(8, 375, 294, 99);
 		lblLogo.setIcon(new ImageIcon(LoginForm.class.getResource("/org/spoutcraft/launcher/spoutcraft.png")));
-		
-		
 		jedHTML.setBackground(Color.DARK_GRAY);
-		
 		JLabel lblMinecraftUsername = new JLabel("Minecraft Username: ");
 		lblMinecraftUsername.setHorizontalAlignment(SwingConstants.RIGHT);
 		lblMinecraftUsername.setBounds(472, 394, 150, 14);
-		
 		JLabel lblPassword = new JLabel("Password: ");
 		lblPassword.setHorizontalAlignment(SwingConstants.RIGHT);
 		lblPassword.setBounds(522, 419, 100, 20);
-		
 		txtPassword = new JPasswordField();
 		txtPassword.setBounds(633, 419, 119, 20);
-		
 		readUsedUsernames();
-		
 		JLabel lblNewLabel = new HyperlinkJLabel("<html><u>Need a minecraft account?</u></html>", "http://www.minecraft.net/register.jsp");
 		lblNewLabel.setBounds(757, 447, 86, 14);
-
 		lblNewLabel.setText("<html><u>Need an account?</u></html>");
 		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 11));
 		lblNewLabel.setForeground(new Color(0, 0, 255));
 		cmbUsername.setBounds(632, 391, 119, 20);
-		
-		
 		cmbUsername.setEditable(true);
 		contentPane.setLayout(null);
 		cbRemember.setBounds(633, 443, 93, 23);
@@ -186,57 +173,69 @@ public class LoginForm extends JFrame implements ActionListener {
 		contentPane.add(lblNewLabel);
 		contentPane.add(jedHTML);
 	}
-	
-	
-	HashMap<String, String> usernames = new HashMap<String, String>();
+
+	ArrayList<String> usernames = new ArrayList<String>();
+
 	private void readUsedUsernames() {
+		File recentsU = new File(PlatformUtils.getWorkingDirectory(), "recentUsernames");
+		if (!recentsU.exists()) {
+			return;
+		}
+		try {
+			FileInputStream fstream = new FileInputStream(recentsU);
+			DataInputStream in = new DataInputStream(fstream);
+			BufferedReader br = new BufferedReader(new InputStreamReader(in));
+			String strLine;
+			while ((strLine = br.readLine()) != null) {
+				this.cmbUsername.addItem(strLine);
+				usernames.add(strLine);
+			} 
+			in.close();
+		} catch (java.lang.Exception e) {
+		}
 		try {
 			File lastLogin = new File(PlatformUtils.getWorkingDirectory(), "lastlogin");
-			if (!lastLogin.exists()) return;
 			Cipher cipher = getCipher(2, "passwordfile");
-			
 			DataInputStream dis;
-			if (cipher != null)
+			if (cipher != null) {
 				dis = new DataInputStream(new CipherInputStream(new FileInputStream(lastLogin), cipher));
-			else {
+			} else {
 				dis = new DataInputStream(new FileInputStream(lastLogin));
 			}
-			
-			try {
-				while (true) {
-					String user = dis.readUTF();
-					String pass = dis.readUTF();
-					usernames.put(user, pass);
-					this.cmbUsername.addItem(user);
-				}
-			} catch (EOFException e) { }
+			String username = dis.readUTF();
+			this.cmbUsername.addItem(username);
+			usernames.add(username);
+			this.txtPassword.setText(dis.readUTF());
+			this.cbRemember.setSelected(this.txtPassword.getPassword().length > 0);
 			dis.close();
-			
-		} catch (Exception e) {
+		} catch (java.lang.Exception e) {
 			e.printStackTrace();
 		}
-		this.txtPassword.setText(usernames.get(this.cmbUsername.getSelectedItem().toString()));
-		this.cbRemember.setSelected(this.txtPassword.getPassword().length > 0);
 	}
-	
 
-	private void writeUsernameList() {
+	private void writeUsernameList(String user) {
+		File recentsU = new File(PlatformUtils.getWorkingDirectory(), "recentUsernames");
+		try {
+			FileWriter fstream = new FileWriter(recentsU, true);
+			BufferedWriter out = new BufferedWriter(fstream);
+			out.write(user);
+			out.write("\n");
+			out.close();
+		} catch (java.lang.Exception e) {
+		}
 		try {
 			File lastLogin = new File(PlatformUtils.getWorkingDirectory(), "lastlogin");
-
 			Cipher cipher = getCipher(1, "passwordfile");
 			DataOutputStream dos;
-			if (cipher != null)
-				dos = new DataOutputStream(new CipherOutputStream(new FileOutputStream(lastLogin), cipher));
-			else {
+			if (cipher != null) {
+				dos = new DataOutputStream(new CipherOutputStream(new FileOutputStream(lastLogin, true), cipher));
+			} else {
 				dos = new DataOutputStream(new FileOutputStream(lastLogin, true));
 			}
-			for (String user : usernames.keySet()) {
-				dos.writeUTF(user);
-				dos.writeUTF(usernames.get(user));
-			}
+			dos.writeUTF(user);
+			dos.writeUTF(this.cbRemember.isSelected() ? new String(this.txtPassword.getPassword()) : "");
 			dos.close();
-		} catch (Exception e) {
+		} catch (java.lang.Exception e) {
 			e.printStackTrace();
 		}
 	}
@@ -246,36 +245,32 @@ public class LoginForm extends JFrame implements ActionListener {
 		if (btnID.equals("Login")) {
 			try {
 				String[] values = MinecraftUtils.doLogin(this.cmbUsername.getSelectedItem().toString(), new String(this.txtPassword.getPassword()));
-				usernames.put(this.cmbUsername.getSelectedItem().toString(), this.cbRemember.isSelected() ? new String(this.txtPassword.getPassword()) : "");
-				writeUsernameList();
+				if (!usernames.contains(this.cmbUsername.getSelectedItem().toString())) {
+					this.writeUsernameList(this.cmbUsername.getSelectedItem().toString());
+				}
+				if (cbRemember.isSelected()) {
+				}
 				GameUpdater gu = new GameUpdater(values[2].trim(), values[1].trim(), values[0].trim());
 				gu.updateMC();
 				gu.updateSpout(false);
-				
 				LauncherFrame launcher = new LauncherFrame();
-				
-				launcher.runGame(values[2].trim(), values[3].trim(), values[1].trim(), new String(this.txtPassword.getPassword()));	
+				launcher.runGame(values[2].trim(), values[3].trim(), values[1].trim(), new String(this.txtPassword.getPassword()));
 				this.setVisible(false);
-				
 			} catch (BadLoginException e) {
-				JOptionPane.showMessageDialog(this,"Incorrect username/password combination");
+				JOptionPane.showMessageDialog(this, "Incorrect username/password combination");
 			} catch (MCNetworkException e) {
-				JOptionPane.showMessageDialog(this,"Cannot connect to minecraft.net");
+				JOptionPane.showMessageDialog(this, "Cannot connect to minecraft.net");
 			} catch (OutdatedMCLauncherException e) {
-				JOptionPane.showMessageDialog(this,"The unthinkable has happened, alert alta189@getsport.org!!!!");
+				JOptionPane.showMessageDialog(this, "The unthinkable has happened, alert alta189@getsport.org!!!!");
 			} catch (UnsupportedEncodingException e) {
-				JOptionPane.showMessageDialog(this,"Incorrect username/password combination");
+				JOptionPane.showMessageDialog(this, "Incorrect username/password combination");
 				e.printStackTrace();
-			} catch (Exception e) {
+			} catch (java.lang.Exception e) {
 				e.printStackTrace();
 			}
-			
 		} else if (btnID.equals("Options")) {
 			OptionDialog options = new OptionDialog();
 			options.setVisible(true);
-		} else if (btnID.equals("comboBoxChanged")) {
-			this.txtPassword.setText(usernames.get(this.cmbUsername.getSelectedItem().toString()));
-			this.cbRemember.setSelected(this.txtPassword.getPassword().length > 0);
 		}
 	}
 }
