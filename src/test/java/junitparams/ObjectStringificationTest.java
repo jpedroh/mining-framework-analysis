31f36679de62eca6515187ade4067ece98d30863
Joao Pedro <joao.pedro.hsd@gmail.com>
2024-12-16 18:00:11 +0000

    Mining Framework Analysis

--- src/test/java/junitparams/ObjectStringificationTest.java
+++ src/test/java/junitparams/ObjectStringificationTest.java
@@ -1,19 +1,16 @@
 package junitparams;
 
+import junitparams.internal.Utils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import junitparams.internal.Utils;
-
-import static junitparams.JUnitParamsRunner.*;
-import static junitparams.internal.Utils.*;
+import static junitparams.JUnitParamsRunner.$;
+import static junitparams.internal.Utils.stringify;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.fail;
 
 
 @RunWith(JUnitParamsRunner.class)
 public class ObjectStringificationTest {
-
     @Test
     public void stringifyString() throws Exception {
         assertThat(stringify("exampleName", 0)).isEqualTo("[0] exampleName");
@@ -22,7 +19,6 @@ public class ObjectStringificationTest {
     @Test
     public void stringifyClassWithToStringAndOneParam() throws Exception {
         ClassWithToString obj = new ClassWithToString("exampleName");
-
         assertThat(stringify(obj, 0)).isEqualTo("[0] exampleName");
     }
 
@@ -30,14 +26,12 @@ public class ObjectStringificationTest {
     public void stringifyClassWithToStringAndManyParams() throws Exception {
         ClassWithToString obj1 = new ClassWithToString("one");
         ClassWithToString obj2 = new ClassWithToString("two");
-
-        assertThat(stringify(new Object[]{obj1, obj2}, 0)).isEqualTo("[0] one, two");
+        assertThat(stringify(new Object[]{ obj1, obj2 }, 0)).isEqualTo("[0] one, two");
     }
 
     @Test
     public void stringifyClassWithToStringInSuperclass() throws Exception {
         ClassWithToString obj = new ClassWithToString("exampleName");
-
         assertThat(Utils.stringify(obj, 0)).isEqualTo("[0] exampleName");
     }
 
@@ -69,19 +63,20 @@ public class ObjectStringificationTest {
     @Test
     @Parameters
     public void shouldCreateParameterObjectsOnce(Object object) {
-        assertThat(object).isInstanceOf(A.class);
+        assertThat(object).isInstanceOf(ObjectStringificationTest.A.class);
     }
 
     public Object[] parametersForShouldCreateParameterObjectsOnce() {
-        return new Object[]{new A()};
+        return new Object[] { new A() };
     }
 
     static class A {
         static int instances = 0;
+
         String test = "test";
 
         A() {
-            if (++instances > 1) {
+            if ((++instances) > 1) {
                 fail();
             }
         }
