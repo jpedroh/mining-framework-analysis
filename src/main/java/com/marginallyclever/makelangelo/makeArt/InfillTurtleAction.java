79be4369e3b117a7ef09188802d90264b02829b4
Joao Pedro <joao.pedro.hsd@gmail.com>
2024-12-16 18:09:05 +0000

    Mining Framework Analysis

--- src/main/java/com/marginallyclever/makelangelo/makeArt/InfillTurtleAction.java
+++ src/main/java/com/marginallyclever/makelangelo/makeArt/InfillTurtleAction.java
@@ -5,25 +5,27 @@ import com.marginallyclever.makelangelo.turtle.Turtle;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+
 public class InfillTurtleAction extends TurtleModifierAction {
 	private static final Logger logger = LoggerFactory.getLogger(InfillTurtleAction.class);
 
 	private static final long serialVersionUID = -8653065260609614796L;
-	
+
 	public InfillTurtleAction() {
 		super(Translator.get("InfillTurtleAction.title"));
 	}
-	
+
 	@Override
 	public Turtle run(Turtle t) {
 		InfillTurtle infill = new InfillTurtle();
 		Turtle result = new Turtle(t);
 		try {
 			result.add(infill.run(t));
-		} catch (Exception ex) {
+			// PPAC : to indirectly update the slider last max limit (or else max limit is not updated and no view update on infill menu item clic)
+			myMakelangelo.setTurtle(t);
+		} catch (java.lang.Exception ex) {
 			logger.error("Failed to infill", ex);
 		}
 		return result;
 	}
-
 }
