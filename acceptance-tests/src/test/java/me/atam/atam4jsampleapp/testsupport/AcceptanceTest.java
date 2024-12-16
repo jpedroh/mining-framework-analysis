9a7d2f4ed2e4234274435b92c30930b3c6652bfd
Joao Pedro <joao.pedro.hsd@gmail.com>
2024-12-16 17:58:21 +0000

    Mining Framework Analysis

--- acceptance-tests/src/test/java/me/atam/atam4jsampleapp/testsupport/AcceptanceTest.java
+++ acceptance-tests/src/test/java/me/atam/atam4jsampleapp/testsupport/AcceptanceTest.java
@@ -1,19 +1,20 @@
 package me.atam.atam4jsampleapp.testsupport;
 
 import io.dropwizard.testing.DropwizardTestSupport;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.core.Response;
 import me.atam.atam4j.PollingPredicate;
 import me.atam.atam4jdomain.TestsRunResult;
 import me.atam.atam4jsampleapp.ApplicationConfiguration;
 import org.glassfish.jersey.client.JerseyClientBuilder;
 import org.junit.After;
-
-import javax.ws.rs.core.Response;
-
 import static me.atam.atam4jsampleapp.testsupport.AcceptanceTestTimeouts.MAX_ATTEMPTS;
 import static me.atam.atam4jsampleapp.testsupport.AcceptanceTestTimeouts.RETRY_POLL_INTERVAL;
 import static org.junit.Assert.assertTrue;
 
+
 public abstract class AcceptanceTest {
+    private static final Client client = new JerseyClientBuilder().build();
 
     protected DropwizardTestSupport<ApplicationConfiguration> dropwizardTestSupportAppConfig;
 
@@ -53,4 +54,9 @@ public abstract class AcceptanceTest {
         assertTrue(responsePollingPredicate.pollUntilPassedOrMaxAttemptsExceeded());
     }
 
+    public Response getCriticalTestRunResultFromServer() {
+        return client.target(String.format("http://localhost:%d/tests/priority-1", dropwizardTestSupportAppConfig.getLocalPort()))
+                .request()
+                .get();
+    }
 }
