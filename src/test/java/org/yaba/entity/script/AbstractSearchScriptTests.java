aa2f53ee11b3d51fb9aea1342eca5374b516cd67
Joao Pedro <joao.pedro.hsd@gmail.com>
2024-12-16 18:04:59 +0000

    Mining Framework Analysis

--- src/test/java/org/yaba/entity/script/AbstractSearchScriptTests.java
+++ src/test/java/org/yaba/entity/script/AbstractSearchScriptTests.java
@@ -1,26 +1,7 @@
-package org.yaba.entity.script;
-
-import org.elasticsearch.common.settings.ImmutableSettings;
-import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.plugins.PluginsService;
-import org.elasticsearch.test.ElasticsearchIntegrationTest;
-import org.elasticsearch.test.ElasticsearchIntegrationTest.ClusterScope;
-import org.elasticsearch.test.ElasticsearchIntegrationTest.Scope;
-
-
-/**
- */
-@ClusterScope(scope = Scope.TEST, numDataNodes = 0)
-public class AbstractSearchScriptTests extends ElasticsearchIntegrationTest {
-
-    @Override
-    protected final Settings nodeSettings(int nodeOrdinal) {
-        return ImmutableSettings.settingsBuilder()
-                .put("gateway.type", "none")
-                .put("index.number_of_shards", 1)
-                .put("index.number_of_replicas", 0)
-                .put(super.nodeSettings(nodeOrdinal))
-                .put("plugins." + PluginsService.LOAD_PLUGIN_FROM_CLASSPATH, true)
-                .build();
-    }
-}
+  package    org . yaba . entity . script ;   import     org . elasticsearch . common . settings . ImmutableSettings ;  import     org . elasticsearch . common . settings . Settings ;  import    org . elasticsearch . plugins . PluginsService ;  import    org . elasticsearch . test . ElasticsearchIntegrationTest ;  import     org . elasticsearch . test . ElasticsearchIntegrationTest . ClusterScope ;  import     org . elasticsearch . test . ElasticsearchIntegrationTest . Scope ;    @ ClusterScope  (  scope =  Scope . TEST ,  numDataNodes = 0 ) public class AbstractSearchScriptTests  extends ElasticsearchIntegrationTest  {    @ Override protected final Settings nodeSettings  (   int nodeOrdinal )  {  return        ImmutableSettings . settingsBuilder  ( ) . put  ( "gateway.type" , "none" ) . put  ( "index.number_of_shards" , 1 ) . put  ( "index.number_of_replicas" , 0 ) . put  ( 
+<<<<<<<
+ "plugins." +  PluginsService . LOAD_PLUGIN_FROM_CLASSPATH
+=======
+ super . nodeSettings  ( nodeOrdinal )
+>>>>>>>
+ true ) . put  (  "plugins." +  PluginsService . LOAD_PLUGIN_FROM_CLASSPATH , true ) . build  ( ) ; } }
