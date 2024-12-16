993de954f2cda6bf8a1093541fab37fd6582a9aa
Joao Pedro <joao.pedro.hsd@gmail.com>
2024-12-16 18:23:07 +0000

    Mining Framework Analysis

--- sm-shop/src/main/java/com/salesmanager/shop/store/api/v1/product/ProductApi.java
+++ sm-shop/src/main/java/com/salesmanager/shop/store/api/v1/product/ProductApi.java
@@ -1,36 +1,5 @@
 package com.salesmanager.shop.store.api.v1.product;
 
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.inject.Inject;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.validation.Valid;
-
-import org.apache.commons.collections4.CollectionUtils;
-import org.apache.commons.lang3.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
 import com.salesmanager.core.business.services.catalog.category.CategoryService;
 import com.salesmanager.core.business.services.catalog.product.ProductService;
 import com.salesmanager.core.model.catalog.category.Category;
@@ -50,7 +19,6 @@ import com.salesmanager.shop.store.api.exception.UnauthorizedException;
 import com.salesmanager.shop.store.controller.product.facade.ProductCommonFacade;
 import com.salesmanager.shop.store.controller.product.facade.ProductFacade;
 import com.salesmanager.shop.utils.ImageFilePath;
-
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
@@ -59,7 +27,35 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import io.swagger.annotations.SwaggerDefinition;
 import io.swagger.annotations.Tag;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import springfox.documentation.annotations.ApiIgnore;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
 
 /**
  * API to create, read, update and delete a Product API.
@@ -68,12 +64,9 @@ import springfox.documentation.annotations.ApiIgnore;
  */
 @Controller
 @RequestMapping("/api/v1")
-@Api(tags = {
-		"Product definition resource (Create udtate and delete product definition. Serves api v1 and v2 with backward compatibility)" })
-@SwaggerDefinition(tags = {
-		@Tag(name = "Product definition  resource, add product to category", description = "View product, Add product, edit product and delete product") })
+@Api(tags = { "Product definition resource (Create udtate and delete product definition. Serves api v1 and v2 with backward compatibility)" })
+@SwaggerDefinition(tags = { @Tag(name = "Product definition  resource, add product to category", description = "View product, Add product, edit product and delete product") })
 public class ProductApi {
-
 	@Inject
 	private CategoryService categoryService;
 
@@ -109,7 +102,7 @@ public class ProductApi {
 			@ApiImplicitParam(name = "lang", dataType = "String", defaultValue = "en") })
 	public @ResponseBody Entity create(@Valid @RequestBody PersistableProduct product,
 			@ApiIgnore MerchantStore merchantStore, @ApiIgnore Language language) {
-	
+
 		Long id = productCommonFacade.saveProduct(merchantStore, product, language);
 		Entity returnEntity = new Entity();
 		returnEntity.setId(id);
@@ -184,37 +177,36 @@ public class ProductApi {
 	 */
 	@RequestMapping(value = "/products", method = RequestMethod.GET)
 	@ResponseBody
-	@ApiImplicitParams({ @ApiImplicitParam(name = "store", dataType = "String", defaultValue = "DEFAULT"),
-			@ApiImplicitParam(name = "lang", dataType = "String", defaultValue = "en") })
-	public ReadableProductList list(
-			@RequestParam(value = "lang", required = false) String lang,
-			@RequestParam(value = "category", required = false) Long category,
-			@RequestParam(value = "name", required = false) String name,
-			@RequestParam(value = "sku", required = false) String sku,
-			@RequestParam(value = "manufacturer", required = false) Long manufacturer,
-			@RequestParam(value = "optionValues", required = false) List<Long> optionValueIds,
-			@RequestParam(value = "status", required = false) String status,
-			@RequestParam(value = "owner", required = false) Long owner,
-			@RequestParam(value = "page", required = false, defaultValue = "0") Integer page, // current
-			@RequestParam(value = "origin", required = false, defaultValue = ProductCriteria.ORIGIN_SHOP) String origin,
-			// page
-			// 0
-			// ..
-			// n
-			// allowing
-			// navigation
-			@RequestParam(value = "count", required = false, defaultValue = "100") Integer count, // count
-			@RequestParam(value = "slug", required = false) String slug, // category slug
-			@RequestParam(value = "available", required = false) Boolean available,
-			// per
-			// page
-			@ApiIgnore MerchantStore merchantStore, @ApiIgnore Language language, HttpServletRequest request,
-			HttpServletResponse response) throws Exception {
-
+	@ApiImplicitParams({ @ApiImplicitParam(name = "store", dataType = "String", defaultValue = "DEFAULT"), @ApiImplicitParam(name = "lang", dataType = "String", defaultValue = "en") })
+	public ReadableProductList list(@RequestParam(value = "lang", required = false)
+	String lang, @RequestParam(value = "category", required = false)
+	Long category, @RequestParam(value = "name", required = false)
+	String name, @RequestParam(value = "sku", required = false)
+	String sku, @RequestParam(value = "manufacturer", required = false)
+	Long manufacturer, @RequestParam(value = "optionValues", required = false)
+	List<Long> optionValueIds, @RequestParam(value = "status", required = false)
+	String status, @RequestParam(value = "owner", required = false)
+	Long owner, @RequestParam(value = "page", required = false, defaultValue = "0")
+	Integer page, // current
+	@RequestParam(value = "origin", required = false, defaultValue = ProductCriteria.ORIGIN_SHOP)
+	String origin, // page
+	// 0
+	// ..
+	// n
+	// allowing
+	// navigation
+	@RequestParam(value = "count", required = false, defaultValue = "100")
+	Integer count, // count
+	@RequestParam(value = "slug", required = false)
+	String slug, // category slug
+	@RequestParam(value = "available", required = false)
+	Boolean available, // per
+	// page
+	@ApiIgnore
+	MerchantStore merchantStore, @ApiIgnore
+	Language language, HttpServletRequest request, HttpServletResponse response) throws Exception {
 		ProductCriteria criteria = new ProductCriteria();
-
 		criteria.setOrigin(origin);
-
 		// do not use legacy pagination anymore
 		if (lang != null) {
 			criteria.setLanguage(lang);
@@ -237,54 +229,41 @@ public class ProductApi {
 			criteria.setCategoryIds(categoryIds);
 		}
 		// End Category handling
-
-		if (available != null && available) {
+		if ((available != null) && available) {
 			criteria.setAvailable(available);
 		}
-
 		if (manufacturer != null) {
 			criteria.setManufacturerId(manufacturer);
 		}
-
 		if (CollectionUtils.isNotEmpty(optionValueIds)) {
 			criteria.setOptionValueIds(optionValueIds);
 		}
-
 		if (owner != null) {
 			criteria.setOwnerId(owner);
 		}
-
 		if (page != null) {
 			criteria.setStartPage(page);
 		}
-
 		if (count != null) {
 			criteria.setMaxCount(count);
 		}
-
 		if (!StringUtils.isBlank(name)) {
 			criteria.setProductName(name);
 		}
-
 		if (!StringUtils.isBlank(sku)) {
 			criteria.setCode(sku);
 		}
-
 		// TODO
 		// RENTAL add filter by owner
 		// REPOSITORY to use the new filters
-
 		try {
 			return productFacade.getProductListsByCriterias(merchantStore, language, criteria);
-
-		} catch (Exception e) {
-
+		} catch (java.lang.Exception e) {
 			LOGGER.error("Error while filtering products product", e);
 			try {
 				response.sendError(503, "Error while filtering products " + e.getMessage());
-			} catch (Exception ignore) {
+			} catch (java.lang.Exception ignore) {
 			}
-
 			return null;
 		}
 	}
@@ -301,8 +280,18 @@ public class ProductApi {
 	 *                   <p>
 	 *                   /api/product/123
 	 */
-   
-	@RequestMapping(value = {"/product/{id}","/products/{id}"}, method = RequestMethod.GET)
+	/**
+	 * use v2 api with product sku
+	 * @param id
+	 * @param lang
+	 * @param merchantStore
+	 * @param language
+	 * @param response
+	 * @return
+	 * @throws Exception
+	 */
+	/**
+	@RequestMapping(value = "/product/{id}", method = RequestMethod.GET)
 	@ApiOperation(httpMethod = "GET", value = "Get a product by id", notes = "For administration and shop purpose. Specifying ?merchant is required otherwise it falls back to DEFAULT")
 	@ApiResponses(value = {
 			@ApiResponse(code = 200, message = "Single product found", response = ReadableProduct.class) })
@@ -322,7 +311,6 @@ public class ProductApi {
 		return product;
 	}
 	**/
-
 	/**
 	 * Price calculation
 	 * @param id
@@ -347,7 +335,6 @@ public class ProductApi {
 
 	}
 	**/
-
 	/**
 	 * API for getting a product
 	 *
@@ -359,25 +346,21 @@ public class ProductApi {
 	 *                   <p>
 	 *                   /api/product/123
 	 */
-	@RequestMapping(value = { "/product/{friendlyUrl}",
-			"/product/friendly/{friendlyUrl}" }, method = RequestMethod.GET)
-	@ApiOperation(httpMethod = "GET", value = "Get a product by friendlyUrl (slug)", notes = "For administration and shop purpose. Specifying ?merchant is "
-			+ "required otherwise it falls back to DEFAULT")
-	@ApiResponses(value = {
-			@ApiResponse(code = 200, message = "Single product found", response = ReadableProduct.class) })
+	@RequestMapping(value = { "/product/{friendlyUrl}", "/product/friendly/{friendlyUrl}" }, method = RequestMethod.GET)
+	@ApiOperation(httpMethod = "GET", value = "Get a product by friendlyUrl (slug)", notes = "For administration and shop purpose. Specifying ?merchant is " + "required otherwise it falls back to DEFAULT")
+	@ApiResponses({ @ApiResponse(code = 200, message = "Single product found", response = ReadableProduct.class) })
 	@ResponseBody
-	@ApiImplicitParams({ @ApiImplicitParam(name = "store", dataType = "String", defaultValue = "DEFAULT"),
-			@ApiImplicitParam(name = "lang", dataType = "String", defaultValue = "en") })
-	public ReadableProduct getByfriendlyUrl(@PathVariable final String friendlyUrl,
-			@RequestParam(value = "lang", required = false) String lang, @ApiIgnore MerchantStore merchantStore,
-			@ApiIgnore Language language, HttpServletResponse response) throws Exception {
+	@ApiImplicitParams({ @ApiImplicitParam(name = "store", dataType = "String", defaultValue = "DEFAULT"), @ApiImplicitParam(name = "lang", dataType = "String", defaultValue = "en") })
+	public ReadableProduct getByfriendlyUrl(@PathVariable
+	final String friendlyUrl, @RequestParam(value = "lang", required = false)
+	String lang, @ApiIgnore
+	MerchantStore merchantStore, @ApiIgnore
+	Language language, HttpServletResponse response) throws Exception {
 		ReadableProduct product = productFacade.getProductBySeUrl(merchantStore, friendlyUrl, language);
-
 		if (product == null) {
 			response.sendError(404, "Product not fount for id " + friendlyUrl);
 			return null;
 		}
-
 		return product;
 	}
 
@@ -512,5 +495,4 @@ public class ProductApi {
 			throw new ServiceRuntimeException("Product [" + id + "] cannot be edited");
 		}
 	}
-
 }
