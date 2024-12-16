3a25b36419a8ca7353acb74a39a09579c1d4b3a1
Joao Pedro <joao.pedro.hsd@gmail.com>
2024-12-16 18:25:07 +0000

    Mining Framework Analysis

--- fluentlenium-core/src/main/java/org/fluentlenium/core/filter/AttributeFilterPredicate.java
+++ fluentlenium-core/src/main/java/org/fluentlenium/core/filter/AttributeFilterPredicate.java
@@ -1,39 +1,7 @@
-package org.fluentlenium.core.filter;
-
-import org.fluentlenium.core.domain.FluentWebElement;
-
-import java.util.function.Predicate;
-
-/**
- * Filter a FluentWebElement collection to return only the elements with the same text
- */
-public class AttributeFilterPredicate implements Predicate<FluentWebElement> {
-    private final AttributeFilter filter;
-
-    /**
-     * Creates a new Attribute Filter Predicated, from an attribute filter
-     *
-     * @param filter attribute filter
-     */
-    public AttributeFilterPredicate(AttributeFilter filter) {
-        this.filter = filter;
-    }
-
-    @Override
-    public boolean test(FluentWebElement element) {
-
-        String attribute = getAttributeValue(element);
-        return filter != null && filter.getMatcher().isSatisfiedBy(attribute);
-    }
-
-    private String getAttributeValue(FluentWebElement element) {
-        if("text".equalsIgnoreCase(filter.getAttribute())) {
-            return element.text();
-        } else if("textContent".equalsIgnoreCase(filter.getAttribute())) {
-            return element.textContent();
-        } else {
-            return element.attribute(filter.getAttribute());
-        }
-    }
-
-}
+  package    org . fluentlenium . core . filter ;   import     org . fluentlenium . core . domain . FluentWebElement ;  import    java . util . function . Predicate ;   public class AttributeFilterPredicate  implements   Predicate  < FluentWebElement >  {   private final AttributeFilter  filter ;   public AttributeFilterPredicate  (  AttributeFilter filter )  {    this . filter = filter ; }    @ Override public boolean test  (  FluentWebElement element )  {  String  attribute =  getAttributeValue  ( element ) ;  return   filter != null &&   filter . getMatcher  ( ) . isSatisfiedBy  ( attribute ) ; }   private String getAttributeValue  (  FluentWebElement element )  { 
+<<<<<<<
+ return   "text" . equalsIgnoreCase  (  filter . getAttribute  ( ) ) ?  element . text  ( ) :  element . attribute  (  filter . getAttribute  ( ) ) ;
+=======
+ if  (  "text" . equalsIgnoreCase  (  filter . getAttribut  ( ) ) )  {  return  element . text  ( ) ; } else  if  (  "textContent" . equalsIgnoreCase  (  filter . getAttribut  ( ) ) )  {  return  element . textContent  ( ) ; } else  {  return  element . attribute  (  filter . getAttribut  ( ) ) ; }
+>>>>>>>
+ } }
