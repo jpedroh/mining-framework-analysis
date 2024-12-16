30424eae2c3c95816190a5fad6e2f7f54fb239c0
Joao Pedro <joao.pedro.hsd@gmail.com>
2024-12-16 18:19:02 +0000

    Mining Framework Analysis

--- cloudfoundry-client-reactor/src/main/java/org/cloudfoundry/reactor/_DefaultConnectionContext.java
+++ cloudfoundry-client-reactor/src/main/java/org/cloudfoundry/reactor/_DefaultConnectionContext.java
@@ -13,8 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-package org.cloudfoundry.reactor;
+package io.netty.channel;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -22,6 +21,10 @@ import com.fasterxml.jackson.databind.deser.DeserializationProblemHandler;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import io.netty.buffer.ByteBufAllocator;
 import io.netty.buffer.PooledByteBufAllocator;
+import java.time.Duration;
+import java.util.List;
+import java.util.Optional;
+import javax.annotation.PreDestroy;
 import org.cloudfoundry.Nullable;
 import org.cloudfoundry.reactor.util.DefaultSslCertificateTruster;
 import org.cloudfoundry.reactor.util.SslCertificateTruster;
@@ -31,192 +34,8 @@ import reactor.core.publisher.Mono;
 import reactor.ipc.netty.http.client.HttpClient;
 import reactor.ipc.netty.resources.LoopResources;
 import reactor.ipc.netty.resources.PoolResources;
-
-import javax.annotation.PreDestroy;
-import java.time.Duration;
-import java.util.List;
-import java.util.Optional;
-
 import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
 import static com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES;
-import static io.netty.channel.ChannelOption.ALLOCATOR;
-import static io.netty.channel.ChannelOption.CONNECT_TIMEOUT_MILLIS;
-import static io.netty.channel.ChannelOption.SO_KEEPALIVE;
-import static io.netty.channel.ChannelOption.SO_RCVBUF;
-import static io.netty.channel.ChannelOption.SO_SNDBUF;
 
-/**
- * The default implementation of the {@link ConnectionContext} interface.  This is the implementation that should be used for most non-testing cases.
- */
-@Value.Immutable
-abstract class _DefaultConnectionContext implements ConnectionContext {
 
-    private static final int RECEIVE_BUFFER_SIZE = 10 * 1024 * 1024;
-
-    private static final int SEND_BUFFER_SIZE = 10 * 1024 * 1024;
-
-    /**
-     * Disposes resources created to service this connection context
-     */
-    @PreDestroy
-    public final void dispose() {
-        getConnectionPool().ifPresent(PoolResources::dispose);
-        getThreadPool().dispose();
-    }
-
-    /**
-     * The number of connections to use when processing requests and responses.  Setting this to `null` disables connection pooling.
-     */
-    @Nullable
-    @Value.Default
-    public Integer getConnectionPoolSize() {
-        return PoolResources.DEFAULT_POOL_MAX_CONNECTION;
-    }
-
-    @Override
-    @Value.Default
-    public HttpClient getHttpClient() {
-        return HttpClient.create(options -> {
-            options
-                .compression(true)
-                .loopResources(getThreadPool())
-                .option(ALLOCATOR, getAllocator())
-                .option(SO_SNDBUF, SEND_BUFFER_SIZE)
-                .option(SO_RCVBUF, RECEIVE_BUFFER_SIZE)
-                .disablePool();
-
-            options.sslSupport(ssl -> getSslCertificateTruster().ifPresent(trustManager -> ssl.trustManager(new StaticTrustManagerFactory(trustManager))));
-
-            getConnectionPool().ifPresent(options::poolResources);
-            getConnectTimeout().ifPresent(socketTimeout -> options.option(CONNECT_TIMEOUT_MILLIS, (int) socketTimeout.toMillis()));
-            getKeepAlive().ifPresent(keepAlive -> options.option(SO_KEEPALIVE, keepAlive));
-            getSslHandshakeTimeout().ifPresent(options::sslHandshakeTimeout);
-// TODO: Add back once these options show up in 0.7.0
-//            getSslCloseNotifyFlushTimeout().ifPresent(options::sslCloseNotifyFlushTimeout);
-//            getSslCloseNotifyReadTimeout().ifPresent(options::sslCloseNotifyReadTimeout);
-            getProxyConfiguration().ifPresent(c -> c.configure(options));
-        });
-    }
-
-    @Override
-    @Value.Default
-    public ObjectMapper getObjectMapper() {
-        ObjectMapper objectMapper = new ObjectMapper()
-            .disable(FAIL_ON_UNKNOWN_PROPERTIES)
-            .enable(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY)
-            .registerModule(new Jdk8Module())
-            .setSerializationInclusion(NON_NULL);
-
-        getProblemHandlers().forEach(objectMapper::addHandler);
-
-        return objectMapper;
-    }
-
-    @Override
-    @Value.Default
-    public RootProvider getRootProvider() {
-        return DelegatingRootProvider.builder()
-            .apiHost(getApiHost())
-            .objectMapper(getObjectMapper())
-            .port(getPort())
-            .secure(getSecure())
-            .build();
-    }
-
-    /**
-     * The number of worker threads to use when processing requests and responses
-     */
-    @Value.Default
-    public Integer getThreadPoolSize() {
-        return LoopResources.DEFAULT_IO_WORKER_COUNT;
-    }
-
-    @Override
-    public Mono<Void> trust(String host, int port) {
-        return getSslCertificateTruster()
-            .map(t -> t.trust(host, port, Duration.ofSeconds(30)))
-            .orElse(Mono.empty());
-    }
-
-    @Value.Derived
-    ByteBufAllocator getAllocator() {
-        return new PooledByteBufAllocator(false, 16, 16, 8192, 7, 512, 256, 64, true, 0);
-    }
-
-    /**
-     * The hostname of the API root.  Typically something like {@code api.run.pivotal.io}.
-     */
-    @Nullable
-    abstract String getApiHost();
-
-    /**
-     * The {@code CONNECT_TIMEOUT_MILLIS} value
-     */
-    abstract Optional<Duration> getConnectTimeout();
-
-    @Value.Derived
-    Optional<PoolResources> getConnectionPool() {
-        return Optional.ofNullable(getConnectionPoolSize())
-            .map(connectionPoolSize -> PoolResources.fixed("cloudfoundry-client", connectionPoolSize));
-    }
-
-    /**
-     * The {@code SO_KEEPALIVE} value
-     */
-    abstract Optional<Boolean> getKeepAlive();
-
-    /**
-     * The port for the Cloud Foundry instance. Defaults to {@code 443}.
-     */
-    abstract Optional<Integer> getPort();
-
-    /**
-     * Jackson deserialization problem handlers.  Typically only used for testing.
-     */
-    abstract List<DeserializationProblemHandler> getProblemHandlers();
-
-    /**
-     * The (optional) proxy configuration
-     */
-    abstract Optional<ProxyConfiguration> getProxyConfiguration();
-
-    /**
-     * Whether the connection to the root API should be secure (i.e. using HTTPS).  Defaults to {@code true}.
-     */
-    abstract Optional<Boolean> getSecure();
-
-    /**
-     * Whether to skip SSL certificate validation for all hosts reachable from the API host.  Defaults to {@code false}.
-     */
-    abstract Optional<Boolean> getSkipSslValidation();
-
-    @Value.Derived
-    Optional<SslCertificateTruster> getSslCertificateTruster() {
-        if (getSkipSslValidation().orElse(false)) {
-            return Optional.of(new DefaultSslCertificateTruster(getAllocator(), getProxyConfiguration(), getThreadPool()));
-        } else {
-            return Optional.empty();
-        }
-    }
-
-    /**
-     * The timeout for the SSL close notify flush
-     */
-    abstract Optional<Duration> getSslCloseNotifyFlushTimeout();
-
-    /**
-     * THe timeout for the SSL close notify read
-     */
-    abstract Optional<Duration> getSslCloseNotifyReadTimeout();
-
-    /**
-     * The timeout for the SSL handshake negotiation
-     */
-    abstract Optional<Duration> getSslHandshakeTimeout();
-
-    @Value.Derived
-    LoopResources getThreadPool() {
-        return LoopResources.create("cloudfoundry-client", getThreadPoolSize(), true);
-    }
-
-}
+class ChannelOption {}
--- cloudfoundry-client-reactor/src/main/java/org/cloudfoundry/reactor/util/DefaultSslCertificateTruster.java
+++ cloudfoundry-client-reactor/src/main/java/org/cloudfoundry/reactor/util/DefaultSslCertificateTruster.java
@@ -13,22 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-package org.cloudfoundry.reactor.util;
+package io.netty.channel;
 
 import io.netty.buffer.ByteBufAllocator;
-import org.cloudfoundry.reactor.ProxyConfiguration;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import reactor.core.publisher.Mono;
-import reactor.ipc.netty.resources.LoopResources;
-import reactor.ipc.netty.tcp.TcpClient;
-import reactor.util.function.Tuple2;
-import reactor.util.function.Tuples;
-
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.TrustManagerFactory;
-import javax.net.ssl.X509TrustManager;
 import java.io.IOException;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
@@ -41,145 +28,17 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicReference;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.X509TrustManager;
+import org.cloudfoundry.reactor.ProxyConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Mono;
+import reactor.ipc.netty.resources.LoopResources;
+import reactor.ipc.netty.tcp.TcpClient;
+import reactor.util.function.Tuple2;
+import reactor.util.function.Tuples;
 
-import static io.netty.channel.ChannelOption.ALLOCATOR;
 
-public final class DefaultSslCertificateTruster implements SslCertificateTruster {
-
-    private final Logger logger = LoggerFactory.getLogger("cloudfoundry-client.trust");
-
-    private final ByteBufAllocator allocator;
-
-    private final AtomicReference<X509TrustManager> delegate;
-
-    private final Optional<ProxyConfiguration> proxyConfiguration;
-
-    private final LoopResources threadPool;
-
-    private final Set<Tuple2<String, Integer>> trustedHostsAndPorts;
-
-    public DefaultSslCertificateTruster(ByteBufAllocator allocator, Optional<ProxyConfiguration> proxyConfiguration, LoopResources threadPool) {
-        this.allocator = allocator;
-        this.proxyConfiguration = proxyConfiguration;
-        this.threadPool = threadPool;
-        this.delegate = new AtomicReference<>(getTrustManager(getTrustManagerFactory(null)));
-        this.trustedHostsAndPorts = Collections.newSetFromMap(new ConcurrentHashMap<>());
-    }
-
-    @Override
-    public void checkClientTrusted(X509Certificate[] x509Certificates, String authType) throws CertificateException {
-        this.delegate.get().checkClientTrusted(x509Certificates, authType);
-    }
-
-    @Override
-    public void checkServerTrusted(X509Certificate[] x509Certificates, String authType) throws CertificateException {
-        this.delegate.get().checkServerTrusted(x509Certificates, authType);
-    }
-
-    @Override
-    public X509Certificate[] getAcceptedIssuers() {
-        return this.delegate.get().getAcceptedIssuers();
-    }
-
-    @Override
-    public Mono<Void> trust(String host, int port, Duration duration) {
-        Tuple2<String, Integer> hostAndPort = Tuples.of(host, port);
-        if (this.trustedHostsAndPorts.contains(hostAndPort)) {
-            return Mono.empty();
-        }
-
-        this.logger.warn("Trusting SSL Certificate for {}:{}", host, port);
-
-        X509TrustManager trustManager = this.delegate.get();
-
-        return getUntrustedCertificates(duration, host, port, this.allocator, this.proxyConfiguration, this.threadPool, trustManager)
-            .doOnNext(untrustedCertificates -> {
-                KeyStore trustStore = addToTrustStore(untrustedCertificates, trustManager);
-                this.delegate.set(getTrustManager(getTrustManagerFactory(trustStore)));
-
-                this.trustedHostsAndPorts.add(hostAndPort);
-                this.logger.debug("Trusted SSL Certificate for {}:{}", host, port);
-            })
-            .then();
-    }
-
-    private static KeyStore addToTrustStore(X509Certificate[] untrustedCertificates, X509TrustManager trustManager) {
-        try {
-            KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
-            trustStore.load(null);
-
-            int count = 0;
-            for (X509Certificate certificate : untrustedCertificates) {
-                trustStore.setCertificateEntry(String.valueOf(count++), certificate);
-            }
-            for (X509Certificate certificate : trustManager.getAcceptedIssuers()) {
-                trustStore.setCertificateEntry(String.valueOf(count++), certificate);
-            }
-
-            return trustStore;
-        } catch (CertificateException | NoSuchAlgorithmException | IOException | KeyStoreException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private static TcpClient getTcpClient(ByteBufAllocator allocator, Optional<ProxyConfiguration> proxyConfiguration, LoopResources threadPool, CertificateCollectingTrustManager collector,
-                                          String host, int port) {
-
-        return TcpClient.create(options -> {
-            options
-                .host(host)
-                .port(port)
-                .loopResources(threadPool)
-                .option(ALLOCATOR, allocator)
-                .disablePool()
-                .sslSupport(ssl -> ssl.trustManager(new StaticTrustManagerFactory(collector)));
-
-            proxyConfiguration.ifPresent(c -> c.configure(options));
-        });
-    }
-
-    private static X509TrustManager getTrustManager(TrustManagerFactory trustManagerFactory) {
-        for (TrustManager trustManager : trustManagerFactory.getTrustManagers()) {
-            if (trustManager instanceof X509TrustManager) {
-                return (X509TrustManager) trustManager;
-            }
-        }
-
-        throw new IllegalStateException("No X509TrustManager in TrustManagerFactory");
-    }
-
-    private static TrustManagerFactory getTrustManagerFactory(KeyStore trustStore) {
-        try {
-            TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
-            trustManagerFactory.init(trustStore);
-
-            return trustManagerFactory;
-        } catch (KeyStoreException | NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private static Mono<X509Certificate[]> getUntrustedCertificates(Duration duration, String host, int port, ByteBufAllocator allocator, Optional<ProxyConfiguration> proxyConfiguration,
-                                                                    LoopResources threadPool, X509TrustManager delegate) {
-
-        CertificateCollectingTrustManager collector = new CertificateCollectingTrustManager(delegate);
-
-        return getTcpClient(allocator, proxyConfiguration, threadPool, collector, host, port)
-            .newHandler((inbound, outbound) -> inbound.receive().then())
-            .timeout(duration)
-            .handle((c, sink) -> {
-                X509Certificate[] chain = collector.getCollectedCertificateChain();
-
-                if (chain == null) {
-                    sink.error(new IllegalStateException("Could not obtain server certificate chain"));
-                }
-
-                if (collector.isTrusted()) {
-                    sink.complete();
-                } else {
-                    sink.next(chain);
-                }
-            });
-    }
-
-}
+class ChannelOption {}
