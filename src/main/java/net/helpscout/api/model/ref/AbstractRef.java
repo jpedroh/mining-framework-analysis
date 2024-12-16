c10ab8151f9eabba75a22e081045fdd8112ba32a
Joao Pedro <joao.pedro.hsd@gmail.com>
2024-12-16 18:24:56 +0000

    Mining Framework Analysis

--- src/main/java/net/helpscout/api/model/ref/AbstractRef.java
+++ src/main/java/net/helpscout/api/model/ref/AbstractRef.java
@@ -1,31 +1,11 @@
-package net.helpscout.api.model.ref;
-
-import net.helpscout.api.cbo.PersonType;
-
-public abstract class AbstractRef implements PersonRef {
-	private int id;
-	private String firstName;
-	private String lastName;
-	private String email;
-	private PersonType type;
-
-	public int getId() {
-		return id;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public PersonType getType() {
-		return type;
-	}
-}
+  package     net . helpscout . api . model . ref ;   import     net . helpscout . api . cbo . PersonType ;   public abstract class AbstractRef  implements  PersonRef  {   private  int  id ;   private String  firstName ;   private String  lastName ;   private String  email ; 
+<<<<<<<
+  private PersonType  type ;
+=======
+>>>>>>>
+   public  int getId  ( )  {  return id ; }   public String getFirstName  ( )  {  return firstName ; }   public String getLastName  ( )  {  return lastName ; }   public String getEmail  ( )  {  return email ; } 
+<<<<<<<
+  public PersonType getType  ( )  {  return type ; }
+=======
+>>>>>>>
+ }
