69bc20cec0fdac43457f06346fb1c0740c6dccec
Joao Pedro <joao.pedro.hsd@gmail.com>
2024-12-16 18:02:48 +0000

    Mining Framework Analysis

--- redpen-core/src/main/java/cc/redpen/validator/ValidatorFactory.java
+++ redpen-core/src/main/java/cc/redpen/validator/ValidatorFactory.java
@@ -3,11 +3,11 @@ package cc.redpen.validator;
 import cc.redpen.DocumentValidatorException;
 import cc.redpen.config.SymbolTable;
 import cc.redpen.config.ValidatorConfiguration;
-
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
+
 
 /**
  * Factory class of validators.
@@ -26,18 +26,16 @@ public class ValidatorFactory {
     VALIDATOR_PACKAGES.add(packageToAdd);
   }
 
-  public static Validator<?> getInstance(ValidatorConfiguration config,
-                                         SymbolTable symbolTable)
-      throws DocumentValidatorException {
+  public static Validator<?> getInstance(ValidatorConfiguration config, SymbolTable symbolTable) throws DocumentValidatorException {
     try {
       for (String validatorPackage : VALIDATOR_PACKAGES) {
-        String validatorClassName = validatorPackage + "." + config.getConfigurationName() + "Validator";
+        String validatorClassName = ((validatorPackage + ".") + config.getConfigurationName()) + "Validator";
         try {
           Class<?> clazz = Class.forName(validatorClassName);
           // ensure the class implements Validator
           boolean implementsValidator = false;
           for (Class<?> aClass : clazz.getInterfaces()) {
-            if (aClass.equals(cc.redpen.validator.Validator.class)) {
+            if (aClass.equals(Validator.class)) {
               implementsValidator = true;
               break;
             }
@@ -45,16 +43,14 @@ public class ValidatorFactory {
           if (!implementsValidator) {
             throw new RuntimeException(validatorClassName + " doesn't implement cc.redpen.validator.Validator");
           }
-
-          Constructor<?> constructor = clazz.getConstructor(ValidatorConfiguration.class, SymbolTable.class);
-          return (Validator<?>) constructor.newInstance(config, symbolTable);
-        } catch (ClassNotFoundException ignore) {
+          Constructor<?> constructor = clazz.getConstructor(ValidatorConfiguration.class, cc.redpen.config.CharacterTable.class);
+          return ((Validator<?>) (constructor.newInstance(config, characterTable)));
+        } catch (java.lang.ClassNotFoundException ignore) {
         }
       }
-    } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException | InstantiationException e) {
+    } catch (java.lang.NoSuchMethodException | InvocationTargetException | java.lang.IllegalAccessException | java.lang.InstantiationException e) {
       throw new RuntimeException(e);
     }
-    throw new DocumentValidatorException(
-        "There is no such Validator: " + config.getConfigurationName());
+    throw new DocumentValidatorException("There is no such Validator: " + config.getConfigurationName());
   }
 }
