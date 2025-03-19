a55e6c527916ad340148f414f8501cf64cb7e6fd
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 16:10:26 +0000

    Mining Framework Analysis

--- src/org/enderstone/server/packet/play/PacketInPlayerPosition.java
+++ src/org/enderstone/server/packet/play/PacketInPlayerPosition.java
@@ -1,105 +1,55 @@
-package org.enderstone.server.packet.play;
-
-import org.enderstone.server.EnderLogger;
-
-import io.netty.buffer.ByteBuf;
-import org.enderstone.server.Location;
-import org.enderstone.server.Main;
-import org.enderstone.server.chat.SimpleMessage;
-import org.enderstone.server.packet.NetworkManager;
-import org.enderstone.server.packet.Packet;
-
-public class PacketInPlayerPosition extends Packet {
-
-	private double x;
-	private double feetY;
-	private double headY;
-	private double z;
-	private boolean onGround;
-
-	public PacketInPlayerPosition() {
-	}
-
-	public PacketInPlayerPosition(double x, double feetY, double headY, double z, boolean onGround) {
-		this.x = x;
-		this.feetY = feetY;
-		this.headY = headY;
-		this.z = z;
-		this.onGround = onGround;
-	}
-
-	@Override
-	public void read(ByteBuf buf) throws Exception {
-		this.x = buf.readDouble();
-		this.feetY = buf.readDouble();
-		this.headY = buf.readDouble();
-		this.z = buf.readDouble();
-		this.onGround = buf.readBoolean();
-	}
-
-	@Override
-	public void write(ByteBuf buf) throws Exception {
-		throw new RuntimeException("Packet " + this.getClass().getSimpleName() + " with ID 0x" + Integer.toHexString(getId()) + " cannot be written.");
-	}
-
-	@Override
-	public int getSize() throws Exception {
-		return (getDoubleSize() * 4) + 1 + getVarIntSize(getId());
-	}
-
-	@Override
-	public byte getId() {
-		return 0x04;
-	}
-
-	@Override
-	public void onRecieve(final NetworkManager networkManager) {
-		Main.getInstance().sendToMainThread(new Runnable() {
-
-			@Override
-			public void run() {
-				if (networkManager.player != null) {
-					Location loc = networkManager.player.getLocation();
-					if (networkManager.player.waitingForValidMoveAfterTeleport > 0) {
-						if (Math.max(Math.max(
-								getX() < loc.getX() ? loc.getX() - getX() : getX() - loc.getX(),
-								getHeadY() < loc.getY() ? loc.getY() - getHeadY() : getHeadY() - loc.getY()),
-								getZ() < loc.getZ() ? loc.getZ() - getZ() : getZ() - loc.getZ()
-						) > 0.1) {
-							if (networkManager.player.waitingForValidMoveAfterTeleport++ > 100) {
-								networkManager.player.teleport(loc);
-							}
-							return;
-						}
-						networkManager.player.sendMessage(new SimpleMessage("Position corrected!"));
-						networkManager.player.waitingForValidMoveAfterTeleport = 0;
-					}
-					networkManager.player.broadcastLocation(new Location("", getX(), getFeetY(), getZ(), networkManager.player.getLocation().getYaw(), networkManager.player.getLocation().getPitch()));
-					loc.setX(getX());
-					loc.setY(getFeetY());
-					loc.setZ(getZ());
-				}
-			}
-		});
-	}
-
-	public double getX() {
-		return x;
-	}
-
-	public double getFeetY() {
-		return feetY;
-	}
-
-	public double getHeadY() {
-		return headY;
-	}
-
-	public double getZ() {
-		return z;
-	}
-
-	public boolean isOnGround() {
-		return onGround;
-	}
-}
+  package     org . enderstone . server . packet . play ;   import    io . netty . buffer . ByteBuf ;  import    org . enderstone . server . Location ;  import    org . enderstone . server . Main ;  import     org . enderstone . server . packet . NetworkManager ;  import     org . enderstone . server . packet . Packet ;  import    org . enderstone . server . EnderLogger ;  import     org . enderstone . server . chat . SimpleMessage ;   public class PacketInPlayerPosition  extends Packet  {   private  double  x ;   private  double  feetY ;   private  double  headY ;   private  double  z ;   private boolean  onGround ;   public PacketInPlayerPosition  ( )  { }   public PacketInPlayerPosition  (   double x ,   double feetY ,   double headY ,   double z ,  boolean onGround )  {    this . x = x ;    this . feetY = feetY ;    this . headY = headY ;    this . z = z ;    this . onGround = onGround ; }    @ Override public void read  (  ByteBuf buf )  throws Exception  {    this . x =  buf . readDouble  ( ) ;    this . feetY =  buf . readDouble  ( ) ;    this . headY =  buf . readDouble  ( ) ;    this . z =  buf . readDouble  ( ) ;    this . onGround =  buf . readBoolean  ( ) ; }    @ Override public void write  (  ByteBuf buf )  throws Exception  {  throw  new RuntimeException  (     "Packet " +   this . getClass  ( ) . getSimpleName  ( ) + " with ID 0x" +  Integer . toHexString  (  getId  ( ) ) + " cannot be written." ) ; }    @ Override public  int getSize  ( )  throws Exception  {  return    (   getDoubleSize  ( ) * 4 ) + 1 +  getVarIntSize  (  getId  ( ) ) ; }    @ Override public  byte getId  ( )  {  return 0x04 ; }    @ Override public void onRecieve  (   final NetworkManager networkManager )  {    Main . getInstance  ( ) . sendToMainThread  (  new Runnable  ( )  {    @ Override public void run  ( )  {  if  (   
+<<<<<<<
+networkManager
+=======
+ networkManager . player
+>>>>>>>
+ . 
+<<<<<<<
+player
+=======
+waitingForValidMoveAfterTeleport
+>>>>>>>
+ 
+<<<<<<<
+!=
+=======
+>
+>>>>>>>
+ 
+<<<<<<<
+null
+=======
+0
+>>>>>>>
+ )  { 
+<<<<<<<
+ Location  loc =   networkManager . player . getLocation  ( ) ;
+=======
+ if  (   Math . max  (  Math . max  (    getX  ( ) <  loc . getX  ( ) ?   loc . getX  ( ) -  getX  ( ) :   getX  ( ) -  loc . getX  ( ) ,    getHeadY  ( ) <  loc . getY  ( ) ?   loc . getY  ( ) -  getHeadY  ( ) :   getHeadY  ( ) -  loc . getY  ( ) ) ,    getZ  ( ) <  loc . getZ  ( ) ?   loc . getZ  ( ) -  getZ  ( ) :   getZ  ( ) -  loc . getZ  ( ) ) > 0.1 )  {  if  (     networkManager . player . waitingForValidMoveAfterTeleport ++ > 100 )  {    networkManager . player . teleport  ( loc ) ; }  return ; }
+>>>>>>>
+  try  {    networkManager . player . broadcastLocation  (  new Location  ( "" ,  getX  ( ) ,  getFeetY  ( ) ,  getZ  ( ) ,    networkManager . player . getLocation  ( ) . getYaw  ( ) ,    networkManager . player . getLocation  ( ) . getPitch  ( ) ) ) ; }  catch (   Exception e )  {   e . printStackTrace  ( ) ; }   loc . setX  (  getX  ( ) ) ;   
+<<<<<<<
+loc
+=======
+ networkManager . player
+>>>>>>>
+ . 
+<<<<<<<
+setY
+=======
+sendMessage
+>>>>>>>
+  ( 
+<<<<<<<
+ getFeetY  ( )
+=======
+ new SimpleMessage  ( "Position corrected!" )
+>>>>>>>
+ ) ;  
+<<<<<<<
+ loc . setZ  (  getZ  ( ) )
+=======
+   networkManager . player . waitingForValidMoveAfterTeleport = 0
+>>>>>>>
+ ; }    networkManager . player . broadcastLocation  (  new Location  ( "" ,  getX  ( ) ,  getFeetY  ( ) ,  getZ  ( ) ,    networkManager . player . getLocation  ( ) . getYaw  ( ) ,    networkManager . player . getLocation  ( ) . getPitch  ( ) ) ) ; } } ) ; }   public  double getX  ( )  {  return x ; }   public  double getFeetY  ( )  {  return feetY ; }   public  double getHeadY  ( )  {  return headY ; }   public  double getZ  ( )  {  return z ; }   public boolean isOnGround  ( )  {  return onGround ; } }
