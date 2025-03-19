2be65e2bcfe65ab84f94a90c2fdc741c21720a0b
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 14:57:22 +0000

    Mining Framework Analysis

--- code/core/src/main/java/org/apache/tamaya/core/internal/converters/DoubleConverter.java
+++ code/core/src/main/java/org/apache/tamaya/core/internal/converters/DoubleConverter.java
@@ -1,27 +1,7 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 package org.apache.tamaya.core.internal.converters;
-
 import org.apache.tamaya.spi.ConversionContext;
 import org.apache.tamaya.spi.PropertyConverter;
 import org.osgi.service.component.annotations.Component;
-
 import java.util.Locale;
 import java.util.Objects;
 import java.util.logging.Logger;
@@ -40,58 +20,52 @@ import java.util.logging.Logger;
  * <li>MAX (ignoring case)</li>
  * </ul>
  */
-@Component(service = PropertyConverter.class)
-public class DoubleConverter implements PropertyConverter<Double> {
-    /**
+@Component(service = PropertyConverter.class) public class DoubleConverter implements PropertyConverter<Double> {
+  /**
      * The logger.
      */
-    private static final Logger LOG = Logger.getLogger(DoubleConverter.class.getName());
-    /**
+  private static final Logger LOG = Logger.getLogger(DoubleConverter.class.getName());
+
+  /**
      * The converter used, when floating point parse failed.
      */
-    private final LongConverter integerConverter = new LongConverter();
+  private final LongConverter integerConverter = new LongConverter();
 
-    @Override
-    public Double convert(String value, ConversionContext context) {
-        context.addSupportedFormats(getClass(), "<double>", "MIN", "MIN_VALUE", "MAX", "MAX_VALUE", "POSITIVE_INFINITY", "NEGATIVE_INFINITY", "NAN");
-        String trimmed = Objects.requireNonNull(value).trim();
-        switch (trimmed.toUpperCase(Locale.ENGLISH)) {
-            case "POSITIVE_INFINITY":
-                return Double.POSITIVE_INFINITY;
-            case "NEGATIVE_INFINITY":
-                return Double.NEGATIVE_INFINITY;
-            case "NAN":
-                return Double.NaN;
-            case "MIN_VALUE":
-            case "MIN":
-                return Double.MIN_VALUE;
-            case "MAX_VALUE":
-            case "MAX":
-                return Double.MAX_VALUE;
-            default:
-                try {
-                    return Double.valueOf(trimmed);
-                } catch (Exception e) {
-                    // OK perhaps we have an integral number that must be converted to the double type...
-                    LOG.finest("Parsing of double as floating number failed, trying parsing integral" +
-                            " number/hex instead...");
-                }
-                Long val = integerConverter.convert(trimmed, context);
-                if(val!=null){
-                    return val.doubleValue();
-                }
-                return null;
-        }
-
+  @Override public Double convert(String value, ConversionContext context) {
+    context.addSupportedFormats(getClass(), "<double>", "MIN", "MIN_VALUE", "MAX", "MAX_VALUE", "POSITIVE_INFINITY", "NEGATIVE_INFINITY", "NAN");
+    String trimmed = Objects.requireNonNull(value).trim();
+    switch (trimmed.toUpperCase(Locale.ENGLISH)) {
+      case "POSITIVE_INFINITY":
+      return Double.POSITIVE_INFINITY;
+      case "NEGATIVE_INFINITY":
+      return Double.NEGATIVE_INFINITY;
+      case "NAN":
+      return Double.NaN;
+      case "MIN_VALUE":
+      case "MIN":
+      return Double.MIN_VALUE;
+      case "MAX_VALUE":
+      case "MAX":
+      return Double.MAX_VALUE;
+      default:
+      try {
+        return Double.valueOf(trimmed);
+      } catch (Exception e) {
+        LOG.finest("Parsing of double as floating number failed, trying parsing integral" + " number/hex instead...");
+      }
+      Long val = integerConverter.convert(trimmed, context);
+      if (val != null) {
+        return val.doubleValue();
+      }
+      return null;
     }
+  }
 
-    @Override
-    public boolean equals(Object o){
-        return Objects.nonNull(o) && getClass().equals(o.getClass());
-    }
+  @Override public boolean equals(Object o) {
+    return Objects.nonNull(o) && getClass().equals(o.getClass());
+  }
 
-    @Override
-    public int hashCode(){
-        return getClass().hashCode();
-    }
+  @Override public int hashCode() {
+    return getClass().hashCode();
+  }
 }
--- code/core/src/main/java/org/apache/tamaya/core/internal/converters/FloatConverter.java
+++ code/core/src/main/java/org/apache/tamaya/core/internal/converters/FloatConverter.java
@@ -1,27 +1,7 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 package org.apache.tamaya.core.internal.converters;
-
 import org.apache.tamaya.spi.ConversionContext;
 import org.apache.tamaya.spi.PropertyConverter;
 import org.osgi.service.component.annotations.Component;
-
 import java.util.Locale;
 import java.util.Objects;
 import java.util.logging.Logger;
@@ -40,58 +20,53 @@ import java.util.logging.Logger;
  * <li>MAX (ignoring case)</li>
  * </ul>
  */
-@Component(service = PropertyConverter.class)
-public class FloatConverter implements PropertyConverter<Float> {
-    /**
+@Component(service = PropertyConverter.class) public class FloatConverter implements PropertyConverter<Float> {
+  /**
      * The logger.
      */
-    private static final Logger LOG = Logger.getLogger(FloatConverter.class.getName());
-    /**
+  private static final Logger LOG = Logger.getLogger(FloatConverter.class.getName());
+
+  /**
      * The converter used, when floating point parse failed.
      */
-    private final IntegerConverter integerConverter = new IntegerConverter();
+  private final IntegerConverter integerConverter = new IntegerConverter();
 
-    @Override
-    public Float convert(String value, ConversionContext context) {
-        context.addSupportedFormats(getClass(), "<float>", "MIN", "MIN_VALUE", "MAX", "MAX_VALUE", "POSITIVE_INFINITY", "NEGATIVE_INFINITY", "NAN");
-        String trimmed = Objects.requireNonNull(value).trim();
-        switch(trimmed.toUpperCase(Locale.ENGLISH)){
-            case "POSITIVE_INFINITY":
-                return Float.POSITIVE_INFINITY;
-            case "NEGATIVE_INFINITY":
-                return Float.NEGATIVE_INFINITY;
-            case "NAN":
-                return Float.NaN;
-            case "MIN_VALUE":
-            case "MIN":
-                return Float.MIN_VALUE;
-            case "MAX_VALUE":
-            case "MAX":
-                return Float.MAX_VALUE;
-            default:
-                try {
-                    return Float.valueOf(trimmed);
-                } catch(Exception e){
-                    // OK perhaps we have an integral number that must be converted to the double type...
-                    LOG.finest("Parsing of float as floating number failed, trying parsing integral" +
-                            " number/hex instead...");
-                }
-                Integer val = integerConverter.convert(trimmed, context);
-                if(val!=null) {
-                    return val.floatValue();
-                }
-                LOG.finest("Unparseable float value: " + trimmed);
-                return null;
-        }
+  @Override public Float convert(String value, ConversionContext context) {
+    context.addSupportedFormats(getClass(), "<float>", "MIN", "MIN_VALUE", "MAX", "MAX_VALUE", "POSITIVE_INFINITY", "NEGATIVE_INFINITY", "NAN");
+    String trimmed = Objects.requireNonNull(value).trim();
+    switch (trimmed.toUpperCase(Locale.ENGLISH)) {
+      case "POSITIVE_INFINITY":
+      return Float.POSITIVE_INFINITY;
+      case "NEGATIVE_INFINITY":
+      return Float.NEGATIVE_INFINITY;
+      case "NAN":
+      return Float.NaN;
+      case "MIN_VALUE":
+      case "MIN":
+      return Float.MIN_VALUE;
+      case "MAX_VALUE":
+      case "MAX":
+      return Float.MAX_VALUE;
+      default:
+      try {
+        return Float.valueOf(trimmed);
+      } catch (Exception e) {
+        LOG.finest("Parsing of float as floating number failed, trying parsing integral" + " number/hex instead...");
+      }
+      Integer val = integerConverter.convert(trimmed, context);
+      if (val != null) {
+        return val.floatValue();
+      }
+      LOG.finest("Unparseable float value: " + trimmed);
+      return null;
     }
+  }
 
-    @Override
-    public boolean equals(Object o){
-        return Objects.nonNull(o) && getClass().equals(o.getClass());
-    }
+  @Override public boolean equals(Object o) {
+    return Objects.nonNull(o) && getClass().equals(o.getClass());
+  }
 
-    @Override
-    public int hashCode(){
-        return getClass().hashCode();
-    }
+  @Override public int hashCode() {
+    return getClass().hashCode();
+  }
 }
