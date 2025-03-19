0aaa6b444e16834e558de8d1a4d82831846ace75
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 17:38:04 +0000

    Mining Framework Analysis

--- src/main/java/com/tufsd/drugwars/Game.java
+++ src/main/java/com/tufsd/drugwars/Game.java
@@ -1,75 +1,64 @@
 package com.tufsd.drugwars;
 import java.util.Random;
 import java.util.EnumMap;
+
 /**
  * A class to represent a game session.
  *
  * @author SHHS Coding Club
  * @version 20141105
  */
-public class Game
-{
-    /**
+public class Game {
+  /**
      * Constructor for objects of class Game
      */
-    public Player player;
+  public Player player;
 
-    public final double RATE = .1;
-    public int turns;
-    
-    Location currentLocation;
-    
-    public void turn()
-    {
-        addInterest();
+  public final double RATE = .1;
+
+  public int turns;
+
+  Location currentLocation;
+
+  public void turn() {
+    addInterest();
+  }
+
+  public Game(String name) {
+    EnumMap<Drug, Integer> inventory = new EnumMap<Drug, Integer>(Drug.class);
+    player = new Player(name);
+    player.inv = inventory;
+    player.inv.put(Drug.POT, 3);
+    player.sellDrugs(Drug.POT, 2);
+    turns = 0;
+  }
+
+  public double addInterest() {
+    player.debt = (player.debt * Math.pow(Math.E, (RATE * turns)));
+    return player.debt;
+  }
+
+  public void run() {
+    initPrices();
+    UI.playerInfo(player);
+  }
+
+  public boolean policeEncounter() {
+    double random = Math.random() * 10 + 1;
+    if (random == 1) {
+      return true;
+    } else {
+      return false;
     }
+  }
 
-    public Game(String name)
-    {
-        EnumMap<Drug, Integer> inventory = new EnumMap<Drug, Integer>(Drug.class);
-        // initialise instance variables
-        player = new Player(name);
-        player.inv = inventory;
-        player.inv.put(Drug.POT, 3);
-        player.sellDrugs (Drug.POT, 2);
+  private void initPrices() {
+    Location.BLOCK.prices.put(Drug.POT, 10.0);
+    System.out.println(Location.BLOCK.prices);
+  }
 
-        turns = 0;
-
-    }
-
-    public double addInterest()
-    {
-        player.debt = (player.debt*Math.pow(Math.E, (RATE*turns)));
-        return player.debt;
-    }
-
-    public void run()
-    {
-        initPrices();
-        UI.playerInfo(player);
-    }
-     
-    public boolean policeEncounter ()
-    {
-        double random = Math.random() * 10 + 1;
-        if (random == 1)
-            return true;
-        else
-            return false;
-
-    }
-
-    private void initPrices()
-    {
-        Location.BLOCK.prices.put(Drug.POT, 10.0);
-        System.out.println(Location.BLOCK.prices);
-    }
-    
-    public void setLocation (Location newLocation)
-    {
-        currentLocation = newLocation;
-        
-        turns += 1;
-    }
-    
+  public void setLocation(Location newLocation) {
+    currentLocation = newLocation;
+    turns += 1;
+  }
 }
