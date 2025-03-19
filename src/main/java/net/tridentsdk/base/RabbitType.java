aaed787440b28daf0038cf3dfe513b73fff1c7c1
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 18:22:00 +0000

    Mining Framework Analysis

--- src/main/java/net/tridentsdk/base/RabbitType.java
+++ src/main/java/net/tridentsdk/base/RabbitType.java
@@ -1,61 +1,40 @@
-/*
- * Trident - A Multithreaded Server Alternative
- * Copyright 2016 The TridentSDK Team
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package net.tridentsdk.base;
-
 import lombok.Getter;
-
 import java.util.HashMap;
 import java.util.Map;
 
 /**
  * @author TridentSDK
  * @since 0.5-alpha
- */
-public enum RabbitType {
+ */public enum RabbitType {
+  BROWN(0),
+  WHITE(1),
+  BLACK(2),
+  BLACK_AND_WHITE(3),
+  GOLD(4),
+  SALT_AND_PEPPER(5),
+  KILLER_BUNNY(99)
+  ;
 
-    BROWN(0),
-    WHITE(1),
-    BLACK(2),
-    BLACK_AND_WHITE(3),
-    GOLD(4),
-    SALT_AND_PEPPER(5),
-    KILLER_BUNNY(99);
+  @Getter private final int data;
 
-    @Getter
-    private final int data;
+  RabbitType(int data) {
+    this.data = data;
+  }
 
-    RabbitType(int data) {
-        this.data = data;
+  private static final Map<Integer, RabbitType> dataToType = new HashMap<>();
+
+  static {
+    for (RabbitType type : values()) {
+      dataToType.put(type.data, type);
     }
+  }
 
-    private static final Map<Integer, RabbitType> dataToType = new HashMap<>();
-
-    static {
-        for (RabbitType type : values()) {
-            dataToType.put(type.data, type);
-        }
+  public static RabbitType of(int data) {
+    RabbitType type = dataToType.get(data);
+    if (type == null) {
+      throw new IllegalArgumentException("no rabbit type with id = " + data);
     }
-
-    public static RabbitType of(int data) {
-        RabbitType type = dataToType.get(data);
-        if (type == null) {
-            throw new IllegalArgumentException("no rabbit type with id = " + data);
-        }
-        return type;
-    }
-
+    return type;
+  }
 }
--- src/main/java/net/tridentsdk/base/SkeletonType.java
+++ src/main/java/net/tridentsdk/base/SkeletonType.java
@@ -1,57 +1,36 @@
-/*
- * Trident - A Multithreaded Server Alternative
- * Copyright 2016 The TridentSDK Team
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package net.tridentsdk.base;
-
 import lombok.Getter;
-
 import java.util.HashMap;
 import java.util.Map;
 
 /**
  * @author TridentSDK
  * @since 0.5-alpha
- */
-public enum SkeletonType {
+ */public enum SkeletonType {
+  NORMAL(0),
+  WITHER(1),
+  STRAY(2)
+  ;
 
-    NORMAL(0),
-    WITHER(1),
-    STRAY(2);
+  @Getter private final int data;
 
-    @Getter
-    private final int data;
+  SkeletonType(int data) {
+    this.data = data;
+  }
 
-    SkeletonType(int data) {
-        this.data = data;
+  private static final Map<Integer, SkeletonType> dataToType = new HashMap<>();
+
+  public static SkeletonType of(int data) {
+    SkeletonType type = dataToType.get(data);
+    if (type == null) {
+      throw new IllegalArgumentException("no skeleton type with id = " + data);
     }
+    return type;
+  }
 
-    private static final Map<Integer, SkeletonType> dataToType = new HashMap<>();
-
-    public static SkeletonType of(int data) {
-        SkeletonType type = dataToType.get(data);
-        if (type == null) {
-            throw new IllegalArgumentException("no skeleton type with id = " + data);
-        }
-        return type;
+  static {
+    for (SkeletonType type : values()) {
+      dataToType.put(type.data, type);
     }
-
-    static {
-        for (SkeletonType type : values()) {
-            dataToType.put(type.data, type);
-        }
-    }
-
+  }
 }
