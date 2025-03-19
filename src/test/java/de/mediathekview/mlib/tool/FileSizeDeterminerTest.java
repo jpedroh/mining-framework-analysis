e1bd6c018ffed30b53342edc22523b21e79615cc
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 18:23:09 +0000

    Mining Framework Analysis

--- src/test/java/de/mediathekview/mlib/tool/FileSizeDeterminerTest.java
+++ src/test/java/de/mediathekview/mlib/tool/FileSizeDeterminerTest.java
@@ -1,11 +1,9 @@
 package de.mediathekview.mlib.tool;
-
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.head;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
-
 import com.github.tomakehurst.wiremock.WireMockServer;
 import javax.ws.rs.core.HttpHeaders;
 import org.junit.jupiter.api.AfterEach;
@@ -15,40 +13,40 @@ import org.junit.jupiter.api.Test;
 
 public class FileSizeDeterminerTest {
   public static final String MOCK_URL_BASE = "http://localhost:8589";
+
   private static final String TEST_FILE_NAME = "FileSizeDeterminerTest.txt";
+
   private static final String TEST_FILE_URL = MOCK_URL_BASE + "/" + TEST_FILE_NAME;
+
   private static WireMockServer wireMockServer = new WireMockServer(options().port(8589));
 
-  @BeforeEach
-  public void startWireMock() {
+  @BeforeEach public void startWireMock() {
     wireMockServer.start();
   }
 
-  @AfterEach
-  public void stopWireMock() {
+  @AfterEach public void stopWireMock() {
     wireMockServer.stop();
   }
 
-  @BeforeAll
-  public static void setUpWiremock() {
-    wireMockServer.stubFor(
-        head(urlEqualTo("/" + TEST_FILE_NAME))
-            .willReturn(
-                aResponse().withStatus(200).withHeader(HttpHeaders.CONTENT_LENGTH, "5643")));
+  @BeforeAll public static void setUpWiremock() {
+    wireMockServer.stubFor(head(urlEqualTo("/" + TEST_FILE_NAME)).willReturn(aResponse().withStatus(200).withHeader(HttpHeaders.CONTENT_LENGTH, "5643")));
   }
 
-  @Test
-  public void testGetFileSize() {
-    assertThat(new FileSizeDeterminer(TEST_FILE_URL).getFileSizeForBuilder()).isEqualTo(5643l);
+  @Test public void testGetFileSize() {
+
+<<<<<<< /usr/src/app/output/xaverw/msearch/89f696aad5e1bb5b729a747dee346381a944b4e5/src/test/java/de/mediathekview/mlib/tool/FileSizeDeterminerTest.java/left.java
+    assertThat(new FileSizeDeterminer(TEST_FILE_URL).getFileSize()).isEqualTo(5643l)
+=======
+    assertThat(new FileSizeDeterminer(TEST_FILE_URL).getFileSizeForBuilder(), is(5643l))
+>>>>>>> /usr/src/app/output/xaverw/msearch/89f696aad5e1bb5b729a747dee346381a944b4e5/src/test/java/de/mediathekview/mlib/tool/FileSizeDeterminerTest.java/right.java
+    ;
   }
 
-  @Test
-  public void testGetFileSizeMiB() {
+  @Test public void testGetFileSizeMiB() {
     assertThat(new FileSizeDeterminer(TEST_FILE_URL).getFileSizeInMiB()).isEqualTo(5l);
   }
 
-  @Test
-  public void testGetFileSizeMB() {
+  @Test public void testGetFileSizeMB() {
     assertThat(new FileSizeDeterminer(TEST_FILE_URL).getFileSizeInMB()).isEqualTo(5l);
   }
 }
