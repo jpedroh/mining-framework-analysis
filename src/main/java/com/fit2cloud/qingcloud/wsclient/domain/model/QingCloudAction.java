82005d328cc00c3b4c59a5757da3207beb298862
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 17:15:13 +0000

    Mining Framework Analysis

--- src/main/java/com/fit2cloud/qingcloud/wsclient/domain/model/QingCloudAction.java
+++ src/main/java/com/fit2cloud/qingcloud/wsclient/domain/model/QingCloudAction.java
@@ -1,164 +1,263 @@
 package com.fit2cloud.qingcloud.wsclient.domain.model;
 
 public class QingCloudAction {
-	//images
-	public static String DESCRIBE_IMAGES = "DescribeImages";
-	public static String CAPTURE_INSTANCE = "CaptureInstance";
-	public static String DELETE_IMAGES = "DeleteImages";
-    public static String MODIFY_IMAGE_ATTRIBUTES = "ModifyImageAttributes";
-    public static String GRANT_IMAGE_TO_USERS = "GrantImageToUsers";
-    public static String REVOKE_IMAGE_FROM_USERS = "RevokeImageFromUsers";
-    public static String CREATE_BROKERS = "CreateBrokers";
-    public static String DELETE_BROKERS = "DeleteBrokers";
-    public static String CLONE_INSTANCES = "CloneInstances";
-    
-    // instances
-    public static String DESCRIBE_INSTANCETYPES = "DescribeInstanceTypes";
-    public static String DESCRIBE_INSTANCES = "DescribeInstances";
-    public static String RUN_INSTANCES = "RunInstances";
-    public static String TERMINATE_INSTANCES = "TerminateInstances";
-    public static String START_INSTANCES = "StartInstances";
-    public static String RESTART_INSTANCES = "RestartInstances";
-    public static String STOP_INSTANCES = "StopInstances";
-    public static String RESIZE_INSTANCES = "ResizeInstances";
-    public static String RESET_INSTANCES = "ResetInstances";
-    public static String MODIFY_INSTANCE_ATTRIBUTES = "ModifyInstanceAttributes";
+  public static String DESCRIBE_IMAGES = "DescribeImages";
 
-    // user data
-    public static String UPLOAD_USERDATA_ATTACHMENT = "UploadUserDataAttachment";
+  public static String CAPTURE_INSTANCE = "CaptureInstance";
 
-    // volumes
-    public static String DESCRIBE_VOLUMES = "DescribeVolumes";
-    public static String CREATE_VOLUMES = "CreateVolumes";
-    public static String DELETE_VOLUMES = "DeleteVolumes";
-    public static String ATTACH_VOLUMES = "AttachVolumes";
-    public static String DETACH_VOLUMES = "DetachVolumes";
-    public static String RESIZE_VOLUMES = "ResizeVolumes";
-    public static String MODIFY_VOLUME_ATTRIBUTES = "ModifyVolumeAttributes";
+  public static String DELETE_IMAGES = "DeleteImages";
 
-    // key pair
-    public static String DESCRIBE_KEY_PAIRS = "DescribeKeyPairs";
-    public static String CREATE_KEY_PAIR = "CreateKeyPair";
-    public static String DELETE_KEY_PAIRS = "DeleteKeyPairs";
-    public static String ATTACH_KEY_PAIRS = "AttachKeyPairs";
-    public static String DETACH_KEY_PAIRS = "DetachKeyPairs";
-    public static String MODIFY_KEYPAIR_ATTRIBUTES = "ModifyKeyPairAttributes";
+  public static String MODIFY_IMAGE_ATTRIBUTES = "ModifyImageAttributes";
 
-    // security group
-    public static String DESCRIBE_SECURITY_GROUPS = "DescribeSecurityGroups";
-    public static String CREATE_SECURITY_GROUP = "CreateSecurityGroup";
-    public static String MODIFY_SECURITY_GROUP_ATTRIBUTES = "ModifySecurityGroupAttributes";
-    public static String APPLY_SECURITY_GROUP = "ApplySecurityGroup";
-    public static String DELETE_SECURITY_GROUPS = "DeleteSecurityGroups";
-    public static String DESCRIBE_SECURITY_GROUP_RULES = "DescribeSecurityGroupRules";
-    public static String ADD_SECURITY_GROUP_RULES = "AddSecurityGroupRules";
-    public static String DELETE_SECURITY_GROUP_RULES = "DeleteSecurityGroupRules";
-    public static String MODIFY_SECURITY_GROUP_RULE_ATTRIBUTES = "ModifySecurityGroupRuleAttributes";
-    public static String DESCRIBE_SECURIRY_GROUP_IP_SETS = "DescribeSecurityGroupIPSets";
-    public static String CREATE_SECURIRY_GROUP_IP_SETS = "CreateSecurityGroupIPSet";
-    public static String DELETE_SECURIRY_GROUP_IP_SETS = "DeleteSecurityGroupIPSets";
-    public static String MODIFY_SECURIRY_GROUP_IP_SETS_ATTRIBUTE = "ModifySecurityGroupIPSetAttributes";
-    public static String APPLY_SECURIRY_GROUP_IP_SETS = "ApplySecurityGroupIPSets";
+  public static String GRANT_IMAGE_TO_USERS = "GrantImageToUsers";
 
-    //network acl
-    public static String DESCRIBE_NETWORK_ACLS = "DescribeNetworkACLs";
-    public static String CREATE_NETWORK_ACL = "CreateNetworkACL";
-    public static String REMOVE_NETWORK_ACL = "RemoveNetworkACL";
-    public static String DELETE_NETWORK_ACL_ENTRIES = "DeleteNetworkACLEntries";
-    public static String APPLY_NETWORK_ACL = "ApplyNetworkACL";
-    public static String MODIFY_NETWORK_ACL_ENTRY_ATTRIBUTES = "ModifyNetworkACLEntryAttributes";
-    public static String ADD_NETWORK_ACL_ENTRIES = "AddNetworkACLEntries";
-    public static String DELETE_NETWORK_ACLS = "DeleteNetworkACLs";
-    public static String MODIFY_NETWORK_ACL_ATTRIBUTES = "ModifyNetworkACLAttributes";
+  public static String REVOKE_IMAGE_FROM_USERS = "RevokeImageFromUsers";
 
-    // vxnets
-    public static String DESCRIBE_VXNETS = "DescribeVxnets";
-    public static String CREATE_VXNETS = "CreateVxnets";
-    public static String DELETE_VXNETS = "DeleteVxnets";
-    public static String JOIN_VXNET = "JoinVxnet";
-    public static String LEAVE_VXNET = "LeaveVxnet";
-    public static String MODIFY_VXNET_ATTRIBUTES = "ModifyVxnetAttributes";
-    public static String DESCRIBE_VXNET_INSTANCES = "DescribeVxnetInstances";
+  public static String CREATE_BROKERS = "CreateBrokers";
 
-    // router
-    public static String CREATE_ROUTERS = "CreateRouters";
-    public static String UPDATE_ROUTERS = "UpdateRouters";
-    public static String DELETE_ROUTERS = "DeleteRouters";
-    public static String JOIN_ROUTER = "JoinRouter";
-    public static String LEAVE_ROUTER = "LeaveRouter";
-    public static String POWEROFF_ROUTERS = "PowerOffRouters";
-    public static String POWERON_ROUTERS = "PowerOnRouters";
-    public static String DESCRIBE_ROUTERS = "DescribeRouters";
-    public static String DESCRIBE_ROUTER_VXNETS = "DescribeRouterVxnets";
-    public static String MODIFY_ROUTER_ATTRIBUTES = "ModifyRouterAttributes";
-    public static String DESCRIBE_ROUTER_STATICS = "DescribeRouterStatics";
-    public static String ADD_ROUTER_STATICS = "AddRouterStatics";
-    public static String DELETE_ROUTER_STATICS = "DeleteRouterStatics";
-    public static String MODIFY_ROUTER_STATIC_ATTRIBUTES = "ModifyRouterStaticAttributes";
-    public static String ADD_ROUTER_STATICS_ENTRIES = "AddRouterStaticEntries";
-    public static String DELETE_ROUTER_STATICS_ENTRIES = "DeleteRouterStaticEntries";
-    public static String MODIFY_ROUTER_STATIC_ATTRIBUTES_ENTRIES = "ModifyRouterStaticEntryAttributes";
-    public static String RESIZE_ROUTERS = "ResizeRouters";
-    public static String DELETE_ROUTER_STATIC_ENTRIES = "DeleteRouterStaticEntries";
-    public static String ADD_ROUTER_STATIC_ENTRIES = "AddRouterStaticEntries";
+  public static String DELETE_BROKERS = "DeleteBrokers";
 
-    // eip
-    public static String ASSOCIATE_EIP = "AssociateEip";
-    public static String DISSOCIATE_EIPS = "DissociateEips";
-    public static String ALLOCATE_EIPS = "AllocateEips";
-    public static String RELEASE_EIPS = "ReleaseEips";
-    public static String DESCRIBE_EIPS = "DescribeEips";
-    public static String MODIFY_EIP_ATTRIBUTES = "ModifyEipAttributes";
-    public static String CHANGE_EIPS_BANDWIDTH = "ChangeEipsBandwidth";
-    public static String CHANGE_EIPS_BILLINGMODE = "ChangeEipsBillingMode";
-    public static String DESCRIBE_EIPS_GROUPS = "DescribeEipGroups";
+  public static String CLONE_INSTANCES = "CloneInstances";
 
-    // lb
-    public static String DESCRIBE_LOADBALANCERS = "DescribeLoadBalancers";
-    public static String CREATE_LOADBALANCER = "CreateLoadBalancer";
-    public static String DELETE_LOADBALANCERS = "DeleteLoadBalancers";
-    public static String ASSOCIATE_EIPS_TO_LOADBALANCER = "AssociateEipsToLoadBalancer";
-    public static String DISSOCIATE_EIPS_FROM_LOADBALANCER = "DissociateEipsFromLoadBalancer";
-    public static String UPDATE_LOADBALANCERS = "UpdateLoadBalancers";
-    public static String STOP_LOADBALANCERS = "StopLoadBalancers";
-    public static String START_LOADBALANCERS = "StartLoadBalancers";
-    public static String MODIFY_LOADBALANCER_ATTRIBUTES = "ModifyLoadBalancerAttributes";
-    public static String RESIZE_LOADBALANCERS = "ResizeLoadBalancers";
+  public static String DESCRIBE_INSTANCETYPES = "DescribeInstanceTypes";
 
-    public static String DESCRIBE_LOADBALANCER_LISTENERS = "DescribeLoadBalancerListeners";
-    public static String ADD_LOADBALANCER_LISTENERS = "AddLoadBalancerListeners";
-    public static String DELETE_LOADBALANCER_LISTENERS = "DeleteLoadBalancerListeners";
-    public static String MODIFY_LOADBALANCER_LISTENER_ATTRIBUTES = "ModifyLoadBalancerListenerAttributes";
-    public static String ADD_LOADBALANCER_BACKENDS = "AddLoadBalancerBackends";
-    public static String DELETE_LOADBALANCER_BACKENDS = "DeleteLoadBalancerBackends";
-    public static String MODIFY_LOADBALANCER_BACKEND_ATTRIBUTES = "ModifyLoadBalancerBackendAttributes";
-    public static String DESCRIBE_LOADBALANCER_BACKENDS = "DescribeLoadBalancerBackends";
+  public static String DESCRIBE_INSTANCES = "DescribeInstances";
 
-    // monitor
-    public static String GET_MONITOR = "GetMonitor";
-    public static String GET_LOADBALANCER_MONITOR = "GetLoadBalancerMonitor";
+  public static String RUN_INSTANCES = "RunInstances";
 
-    // snapshot
-    public static String CREATE_SNAPSHOTS = "CreateSnapshots";
-    public static String DELETE_SNAPSHOTS = "DeleteSnapshots";
-    public static String APPLY_SNAPSHOTS = "ApplySnapshots";
-    public static String DESCRIBE_SNAPSHOTS = "DescribeSnapshots";
-    public static String MODIFY_SNAPSHOT_ATTRIBUTES = "ModifySnapshotAttributes";
-    public static String CAPTURE_INSTANCE_FROM_SNAPSHOT = "CaptureInstanceFromSnapshot";
-    public static String CREATE_VOLUME_FROM_SNAPSHOT = "CreateVolumeFromSnapshot";
-    
-    // job
-    public static String DESCRIBE_JOBS = "DescribeJobs";
-    
-    // zone
-    public static String DESCRIBE_ZONES = "DescribeZones";
+  public static String TERMINATE_INSTANCES = "TerminateInstances";
 
-    //app
-    public static final String DESCRIBE_USERS = "DescribeUsers";
-    public static final String LEASE_APP = "LeaseApp";
-    public static final String UNLEASE_APP = "UnLeaseApp";
+  public static String START_INSTANCES = "StartInstances";
 
-    //Physical resource
-    public static final String DESCRIBE_HUPERNODES = "DescribeHyperNodes";
-    public static final String DESCRIBE_BOTS="DescribeBots";
+  public static String RESTART_INSTANCES = "RestartInstances";
+
+  public static String STOP_INSTANCES = "StopInstances";
+
+  public static String RESIZE_INSTANCES = "ResizeInstances";
+
+  public static String RESET_INSTANCES = "ResetInstances";
+
+  public static String MODIFY_INSTANCE_ATTRIBUTES = "ModifyInstanceAttributes";
+
+  public static String UPLOAD_USERDATA_ATTACHMENT = "UploadUserDataAttachment";
+
+  public static String DESCRIBE_VOLUMES = "DescribeVolumes";
+
+  public static String CREATE_VOLUMES = "CreateVolumes";
+
+  public static String DELETE_VOLUMES = "DeleteVolumes";
+
+  public static String ATTACH_VOLUMES = "AttachVolumes";
+
+  public static String DETACH_VOLUMES = "DetachVolumes";
+
+  public static String RESIZE_VOLUMES = "ResizeVolumes";
+
+  public static String MODIFY_VOLUME_ATTRIBUTES = "ModifyVolumeAttributes";
+
+  public static String DESCRIBE_KEY_PAIRS = "DescribeKeyPairs";
+
+  public static String CREATE_KEY_PAIR = "CreateKeyPair";
+
+  public static String DELETE_KEY_PAIRS = "DeleteKeyPairs";
+
+  public static String ATTACH_KEY_PAIRS = "AttachKeyPairs";
+
+  public static String DETACH_KEY_PAIRS = "DetachKeyPairs";
+
+  public static String MODIFY_KEYPAIR_ATTRIBUTES = "ModifyKeyPairAttributes";
+
+  public static String DESCRIBE_SECURITY_GROUPS = "DescribeSecurityGroups";
+
+  public static String CREATE_SECURITY_GROUP = "CreateSecurityGroup";
+
+  public static String MODIFY_SECURITY_GROUP_ATTRIBUTES = "ModifySecurityGroupAttributes";
+
+  public static String APPLY_SECURITY_GROUP = "ApplySecurityGroup";
+
+  public static String DELETE_SECURITY_GROUPS = "DeleteSecurityGroups";
+
+  public static String DESCRIBE_SECURITY_GROUP_RULES = "DescribeSecurityGroupRules";
+
+  public static String ADD_SECURITY_GROUP_RULES = "AddSecurityGroupRules";
+
+  public static String DELETE_SECURITY_GROUP_RULES = "DeleteSecurityGroupRules";
+
+  public static String MODIFY_SECURITY_GROUP_RULE_ATTRIBUTES = "ModifySecurityGroupRuleAttributes";
+
+  public static String DESCRIBE_SECURIRY_GROUP_IP_SETS = "DescribeSecurityGroupIPSets";
+
+  public static String CREATE_SECURIRY_GROUP_IP_SETS = "CreateSecurityGroupIPSet";
+
+  public static String DELETE_SECURIRY_GROUP_IP_SETS = "DeleteSecurityGroupIPSets";
+
+  public static String MODIFY_SECURIRY_GROUP_IP_SETS_ATTRIBUTE = "ModifySecurityGroupIPSetAttributes";
+
+  public static String APPLY_SECURIRY_GROUP_IP_SETS = "ApplySecurityGroupIPSets";
+
+  public static String DESCRIBE_NETWORK_ACLS = "DescribeNetworkACLs";
+
+  public static String CREATE_NETWORK_ACL = "CreateNetworkACL";
+
+  public static String REMOVE_NETWORK_ACL = "RemoveNetworkACL";
+
+  public static String DELETE_NETWORK_ACL_ENTRIES = "DeleteNetworkACLEntries";
+
+  public static String APPLY_NETWORK_ACL = "ApplyNetworkACL";
+
+  public static String MODIFY_NETWORK_ACL_ENTRY_ATTRIBUTES = "ModifyNetworkACLEntryAttributes";
+
+  public static String ADD_NETWORK_ACL_ENTRIES = "AddNetworkACLEntries";
+
+  public static String DELETE_NETWORK_ACLS = "DeleteNetworkACLs";
+
+  public static String MODIFY_NETWORK_ACL_ATTRIBUTES = "ModifyNetworkACLAttributes";
+
+  public static String DESCRIBE_VXNETS = "DescribeVxnets";
+
+  public static String CREATE_VXNETS = "CreateVxnets";
+
+  public static String DELETE_VXNETS = "DeleteVxnets";
+
+  public static String JOIN_VXNET = "JoinVxnet";
+
+  public static String LEAVE_VXNET = "LeaveVxnet";
+
+  public static String MODIFY_VXNET_ATTRIBUTES = "ModifyVxnetAttributes";
+
+  public static String DESCRIBE_VXNET_INSTANCES = "DescribeVxnetInstances";
+
+  public static String CREATE_ROUTERS = "CreateRouters";
+
+  public static String UPDATE_ROUTERS = "UpdateRouters";
+
+  public static String DELETE_ROUTERS = "DeleteRouters";
+
+  public static String JOIN_ROUTER = "JoinRouter";
+
+  public static String LEAVE_ROUTER = "LeaveRouter";
+
+  public static String POWEROFF_ROUTERS = "PowerOffRouters";
+
+  public static String POWERON_ROUTERS = "PowerOnRouters";
+
+  public static String DESCRIBE_ROUTERS = "DescribeRouters";
+
+  public static String DESCRIBE_ROUTER_VXNETS = "DescribeRouterVxnets";
+
+  public static String MODIFY_ROUTER_ATTRIBUTES = "ModifyRouterAttributes";
+
+  public static String DESCRIBE_ROUTER_STATICS = "DescribeRouterStatics";
+
+  public static String ADD_ROUTER_STATICS = "AddRouterStatics";
+
+  public static String DELETE_ROUTER_STATICS = "DeleteRouterStatics";
+
+  public static String MODIFY_ROUTER_STATIC_ATTRIBUTES = "ModifyRouterStaticAttributes";
+
+  public static String 
+<<<<<<< /usr/src/app/output/fit2cloud/qingcloud-api-java-wrapper/f96e05486c8dd41084214f4b838a87f178be0aaf/src/main/java/com/fit2cloud/qingcloud/wsclient/domain/model/QingCloudAction.java/left.java
+  ADD_ROUTER_STATICS_ENTRIES = "AddRouterStaticEntries"
+=======
+  DELETE_ROUTER_STATIC_ENTRIES = "DeleteRouterStaticEntries"
+>>>>>>> /usr/src/app/output/fit2cloud/qingcloud-api-java-wrapper/f96e05486c8dd41084214f4b838a87f178be0aaf/src/main/java/com/fit2cloud/qingcloud/wsclient/domain/model/QingCloudAction.java/right.java
+  ;
+
+  public static String RESIZE_ROUTERS = "ResizeRouters";
+
+  public static String 
+<<<<<<< /usr/src/app/output/fit2cloud/qingcloud-api-java-wrapper/f96e05486c8dd41084214f4b838a87f178be0aaf/src/main/java/com/fit2cloud/qingcloud/wsclient/domain/model/QingCloudAction.java/left.java
+  DELETE_ROUTER_STATICS_ENTRIES = "DeleteRouterStaticEntries"
+=======
+  ADD_ROUTER_STATIC_ENTRIES = "AddRouterStaticEntries"
+>>>>>>> /usr/src/app/output/fit2cloud/qingcloud-api-java-wrapper/f96e05486c8dd41084214f4b838a87f178be0aaf/src/main/java/com/fit2cloud/qingcloud/wsclient/domain/model/QingCloudAction.java/right.java
+  ;
+
+  public static String MODIFY_ROUTER_STATIC_ATTRIBUTES_ENTRIES = "ModifyRouterStaticEntryAttributes";
+
+  public static String ASSOCIATE_EIP = "AssociateEip";
+
+  public static String DISSOCIATE_EIPS = "DissociateEips";
+
+  public static String ALLOCATE_EIPS = "AllocateEips";
+
+  public static String RELEASE_EIPS = "ReleaseEips";
+
+  public static String DESCRIBE_EIPS = "DescribeEips";
+
+  public static String MODIFY_EIP_ATTRIBUTES = "ModifyEipAttributes";
+
+  public static String CHANGE_EIPS_BANDWIDTH = "ChangeEipsBandwidth";
+
+  public static String CHANGE_EIPS_BILLINGMODE = "ChangeEipsBillingMode";
+
+  public static String DESCRIBE_EIPS_GROUPS = "DescribeEipGroups";
+
+  public static String DESCRIBE_LOADBALANCERS = "DescribeLoadBalancers";
+
+  public static String CREATE_LOADBALANCER = "CreateLoadBalancer";
+
+  public static String DELETE_LOADBALANCERS = "DeleteLoadBalancers";
+
+  public static String ASSOCIATE_EIPS_TO_LOADBALANCER = "AssociateEipsToLoadBalancer";
+
+  public static String DISSOCIATE_EIPS_FROM_LOADBALANCER = "DissociateEipsFromLoadBalancer";
+
+  public static String UPDATE_LOADBALANCERS = "UpdateLoadBalancers";
+
+  public static String STOP_LOADBALANCERS = "StopLoadBalancers";
+
+  public static String START_LOADBALANCERS = "StartLoadBalancers";
+
+  public static String MODIFY_LOADBALANCER_ATTRIBUTES = "ModifyLoadBalancerAttributes";
+
+  public static String RESIZE_LOADBALANCERS = "ResizeLoadBalancers";
+
+  public static String DESCRIBE_LOADBALANCER_LISTENERS = "DescribeLoadBalancerListeners";
+
+  public static String ADD_LOADBALANCER_LISTENERS = "AddLoadBalancerListeners";
+
+  public static String DELETE_LOADBALANCER_LISTENERS = "DeleteLoadBalancerListeners";
+
+  public static String MODIFY_LOADBALANCER_LISTENER_ATTRIBUTES = "ModifyLoadBalancerListenerAttributes";
+
+  public static String ADD_LOADBALANCER_BACKENDS = "AddLoadBalancerBackends";
+
+  public static String DELETE_LOADBALANCER_BACKENDS = "DeleteLoadBalancerBackends";
+
+  public static String MODIFY_LOADBALANCER_BACKEND_ATTRIBUTES = "ModifyLoadBalancerBackendAttributes";
+
+  public static String DESCRIBE_LOADBALANCER_BACKENDS = "DescribeLoadBalancerBackends";
+
+  public static String GET_MONITOR = "GetMonitor";
+
+  public static String GET_LOADBALANCER_MONITOR = "GetLoadBalancerMonitor";
+
+  public static String CREATE_SNAPSHOTS = "CreateSnapshots";
+
+  public static String DELETE_SNAPSHOTS = "DeleteSnapshots";
+
+  public static String APPLY_SNAPSHOTS = "ApplySnapshots";
+
+  public static String DESCRIBE_SNAPSHOTS = "DescribeSnapshots";
+
+  public static String MODIFY_SNAPSHOT_ATTRIBUTES = "ModifySnapshotAttributes";
+
+  public static String CAPTURE_INSTANCE_FROM_SNAPSHOT = "CaptureInstanceFromSnapshot";
+
+  public static String CREATE_VOLUME_FROM_SNAPSHOT = "CreateVolumeFromSnapshot";
+
+  public static String DESCRIBE_JOBS = "DescribeJobs";
+
+  public static String DESCRIBE_ZONES = "DescribeZones";
+
+  public static final String DESCRIBE_USERS = "DescribeUsers";
+
+  public static final String LEASE_APP = "LeaseApp";
+
+  public static final String UNLEASE_APP = "UnLeaseApp";
+
+  public static final String DESCRIBE_HUPERNODES = "DescribeHyperNodes";
+
+  public static final String DESCRIBE_BOTS = "DescribeBots";
 }
--- src/main/java/com/fit2cloud/qingcloud/wsclient/domain/model/QingCloudRouterStatics.java
+++ src/main/java/com/fit2cloud/qingcloud/wsclient/domain/model/QingCloudRouterStatics.java
@@ -1,119 +1,124 @@
 package com.fit2cloud.qingcloud.wsclient.domain.model;
-
-
 import java.util.List;
 
 public class QingCloudRouterStatics {
-	private String router_id;
-	private String vxnet_id;
-	private String router_static_name;
-	private Integer static_type;
-	private String router_static_id;
-	private String create_time;
-	private String val1;
-	private String val2;
-	private String val3;
-	private String val4;
-	private String val5;
+  private String router_id;
 
-    private List<RouterStaticEntry> entry_set;
+  private String vxnet_id;
 
-	public String getRouter_id() {
-		return router_id;
-	}
+  private String router_static_name;
 
-	public void setRouter_id(String router_id) {
-		this.router_id = router_id;
-	}
+  private Integer static_type;
 
-	public String getVxnet_id() {
-		return vxnet_id;
-	}
+  private String router_static_id;
 
-	public void setVxnet_id(String vxnet_id) {
-		this.vxnet_id = vxnet_id;
-	}
+  private String create_time;
 
-	public String getRouter_static_id() {
-		return router_static_id;
-	}
+  private String val1;
 
-	public void setRouter_static_id(String router_static_id) {
-		this.router_static_id = router_static_id;
-	}
+  private String val2;
 
-	public String getCreate_time() {
-		return create_time;
-	}
+  private String val3;
 
-	public void setCreate_time(String create_time) {
-		this.create_time = create_time;
-	}
+  private String val4;
 
+  private String val5;
 
+  private List<RouterStaticEntry> entry_set;
 
-	public String getVal2() {
-		return val2;
-	}
+  public String getRouter_id() {
+    return router_id;
+  }
 
-	public void setVal2(String val2) {
-		this.val2 = val2;
-	}
+  public void setRouter_id(String router_id) {
+    this.router_id = router_id;
+  }
 
-	public String getVal1() {
-		return val1;
-	}
+  public String getVxnet_id() {
+    return vxnet_id;
+  }
 
-	public void setVal1(String val1) {
-		this.val1 = val1;
-	}
+  public void setVxnet_id(String vxnet_id) {
+    this.vxnet_id = vxnet_id;
+  }
 
-	public String getVal3() {
-		return val3;
-	}
+  public String getRouter_static_id() {
+    return router_static_id;
+  }
 
-	public void setVal3(String val3) {
-		this.val3 = val3;
-	}
+  public void setRouter_static_id(String router_static_id) {
+    this.router_static_id = router_static_id;
+  }
 
-	public String getVal4() {
-		return val4;
-	}
+  public String getCreate_time() {
+    return create_time;
+  }
 
-	public void setVal4(String val4) {
-		this.val4 = val4;
-	}
+  public void setCreate_time(String create_time) {
+    this.create_time = create_time;
+  }
 
-	public String getVal5() {
-		return val5;
-	}
+  public String getVal2() {
+    return val2;
+  }
 
-	public void setVal5(String val5) {
-		this.val5 = val5;
-	}
+  public void setVal2(String val2) {
+    this.val2 = val2;
+  }
 
-	public Integer getStatic_type() {
-		return static_type;
-	}
+  public String getVal1() {
+    return val1;
+  }
 
-	public void setStatic_type(Integer static_type) {
-		this.static_type = static_type;
-	}
+  public void setVal1(String val1) {
+    this.val1 = val1;
+  }
 
-	public String getRouter_static_name() {
-		return router_static_name;
-	}
+  public String getVal3() {
+    return val3;
+  }
 
-	public void setRouter_static_name(String router_static_name) {
-		this.router_static_name = router_static_name;
-	}
+  public void setVal3(String val3) {
+    this.val3 = val3;
+  }
 
+  public String getVal4() {
+    return val4;
+  }
 
-	public List<RouterStaticEntry> getEntry_set() {
-		return entry_set;
-	}
+  public void setVal4(String val4) {
+    this.val4 = val4;
+  }
 
-	public void setEntry_set(List<RouterStaticEntry> entry_set) {
-		this.entry_set = entry_set;
-	}
+  public String getVal5() {
+    return val5;
+  }
+
+  public void setVal5(String val5) {
+    this.val5 = val5;
+  }
+
+  public Integer getStatic_type() {
+    return static_type;
+  }
+
+  public void setStatic_type(Integer static_type) {
+    this.static_type = static_type;
+  }
+
+  public String getRouter_static_name() {
+    return router_static_name;
+  }
+
+  public void setRouter_static_name(String router_static_name) {
+    this.router_static_name = router_static_name;
+  }
+
+  public List<RouterStaticEntry> getEntry_set() {
+    return entry_set;
+  }
+
+  public void setEntry_set(List<RouterStaticEntry> entry_set) {
+    this.entry_set = entry_set;
+  }
 }
