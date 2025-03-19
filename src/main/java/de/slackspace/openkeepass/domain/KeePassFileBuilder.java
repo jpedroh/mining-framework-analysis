4bad8dc0879ce297f5d5bf050cf47971dc347cae
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 14:49:59 +0000

    Mining Framework Analysis

--- src/main/java/de/slackspace/openkeepass/domain/KeePassFileBuilder.java
+++ src/main/java/de/slackspace/openkeepass/domain/KeePassFileBuilder.java
@@ -1,5 +1,4 @@
 package de.slackspace.openkeepass.domain;
-
 import de.slackspace.openkeepass.domain.zipper.GroupZipper;
 
 /**
@@ -12,107 +11,121 @@ import de.slackspace.openkeepass.domain.zipper.GroupZipper;
  *
  */
 public class KeePassFileBuilder implements KeePassFileContract {
+  Meta meta;
 
-	Meta meta;
-	Group root;
-	private GroupBuilder rootBuilder = new GroupBuilder();
-	private GroupBuilder topGroupBuilder = new GroupBuilder();
+  Group root;
 
-	/**
+  private GroupBuilder rootBuilder = new GroupBuilder();
+
+  private GroupBuilder topGroupBuilder = new GroupBuilder();
+
+  /**
 	 * Creates a builder and initializes it with the structure from the given
 	 * KeePass file.
 	 *
 	 * @param keePassFile
 	 *            the KeePass file which will be used to initialize the builder
 	 */
-	public KeePassFileBuilder(KeePassFile keePassFile) {
-		this.meta = keePassFile.getMeta();
+  public KeePassFileBuilder(KeePassFile keePassFile) {
+    this.meta = keePassFile.getMeta();
+    rootBuilder = new GroupBuilder(keePassFile.getRoot());
+  }
 
-		rootBuilder = new GroupBuilder(keePassFile.getRoot());
-	}
-
-	/**
+  /**
 	 * Creates a builder with the given databasename.
 	 *
 	 * @param databaseName
 	 *            the name of the database
 	 */
-	public KeePassFileBuilder(String databaseName) {
-		meta = new MetaBuilder(databaseName).historyMaxItems(10).build();
-	}
+  public KeePassFileBuilder(String databaseName) {
+    meta = new MetaBuilder(databaseName).historyMaxItems(10).build();
+  }
 
-	/**
+  /**
 	 * Creates a builder with the given meta object.
 	 *
 	 * @param meta
 	 *            the meta object to initialize the builder meta
 	 */
-	public KeePassFileBuilder(Meta meta) {
-		this.meta = meta;
-	}
+  public KeePassFileBuilder(Meta meta) {
+    this.meta = meta;
+  }
 
-	public KeePassFileBuilder withMeta(Meta meta) {
-		this.meta = meta;
-		return this;
-	}
+  public KeePassFileBuilder withMeta(Meta meta) {
+    this.meta = meta;
+    return this;
+  }
 
-	/**
+  /**
 	 * Adds the given groups right under the root node.
 	 *
 	 * @param groups
 	 *            the groups which should be added
 	 * @return the builder with added groups
 	 */
-	public KeePassFileBuilder addTopGroups(Group... groups) {
-		for (Group group : groups) {
-			rootBuilder.addGroup(group);
-		}
+  public KeePassFileBuilder addTopGroups(Group... groups) {
+    for (Group group : groups) {
+      rootBuilder.addGroup(group);
+    }
+    return this;
+  }
 
-		return this;
-	}
-
-	/**
+  /**
 	 * Add the given entries right under the root node.
 	 *
 	 * @param entries
 	 *            the entries which should be added
 	 * @return the builder with added entries
 	 */
-	public KeePassFileBuilder addTopEntries(Entry... entries) {
-		for (Entry entry : entries) {
-			topGroupBuilder.addEntry(entry);
-		}
+  public KeePassFileBuilder addTopEntries(Entry... entries) {
+    for (Entry entry : entries) {
+      topGroupBuilder.addEntry(entry);
+    }
+    return this;
+  }
 
-		return this;
-	}
-
-	/**
+  /**
 	 * Builds a new KeePass file.
 	 *
 	 * @return a new KeePass file
 	 * @see KeePassFile
 	 */
-	public KeePassFile build() {
-		setTopGroupNameIfNotExisting();
+  public KeePassFile build() {
+    setTopGroupNameIfNotExisting();
+    root = rootBuilder.build();
+    return new KeePassFile(this);
+  }
 
-		root = rootBuilder.build();
 
-		return new KeePassFile(this);
-	}
+<<<<<<< Unknown file: This is a bug in JDime.
+=======
+  /**
+	 * Returns a {@link GroupZipper} with the structure of the builders
+	 * {@link KeePassFile} as underlying data.
+	 * <p>
+	 * A GroupZipper can be used to easily modify existing KeePass files.
+	 *
+	 * @return a new group zipper
+	 * @deprecated use {@link GroupZipper} directly instead
+	 * @see GroupZipper
+	 */
+  @Deprecated public GroupZipper getZipper() {
+    return new GroupZipper(keePassFile);
+  }
+>>>>>>> /usr/src/app/output/cternes/openkeepass/6218ade3f2e50f22e5e263714197f176e856c5a2/src/main/java/de/slackspace/openkeepass/domain/KeePassFileBuilder.java/right.java
 
-	private void setTopGroupNameIfNotExisting() {
-		if (rootBuilder.getGroups().isEmpty()) {
-			rootBuilder.addGroup(topGroupBuilder.name(meta.getDatabaseName()).build());
-		}
-	}
 
-	@Override
-	public Meta getMeta() {
-		return meta;
-	}
+  private void setTopGroupNameIfNotExisting() {
+    if (rootBuilder.getGroups().isEmpty()) {
+      rootBuilder.addGroup(topGroupBuilder.name(meta.getDatabaseName()).build());
+    }
+  }
 
-	@Override
-	public Group getRoot() {
-		return root;
-	}
+  @Override public Meta getMeta() {
+    return meta;
+  }
+
+  @Override public Group getRoot() {
+    return root;
+  }
 }
