2b15dc133ce6671a2d7783552638a515fa331202
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 17:27:26 +0000

    Mining Framework Analysis

--- src/main/java/endpoint/utils/EntityUtils.java
+++ src/main/java/endpoint/utils/EntityUtils.java
@@ -4,8 +4,8 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
-import java.util.ArrayList;
 import java.util.Date;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.lang3.ArrayUtils;
