1da7dc83ab8c44adcbd564f7f0f74db5fecc2e02
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 18:19:48 +0000

    Mining Framework Analysis

--- link-move-json/src/main/java/com/nhl/link/move/runtime/json/JsonExtractorFactory.java
+++ link-move-json/src/main/java/com/nhl/link/move/runtime/json/JsonExtractorFactory.java
@@ -1,5 +1,4 @@
 package com.nhl.link.move.runtime.json;
-
 import com.nhl.link.move.RowAttribute;
 import com.nhl.link.move.connect.StreamConnector;
 import com.nhl.link.move.extractor.Extractor;
@@ -9,55 +8,45 @@ import com.nhl.link.move.runtime.json.query.JsonQuery;
 import com.nhl.link.move.runtime.json.query.QueryCompiler;
 
 public class JsonExtractorFactory implements IExtractorFactory<StreamConnector> {
+  public static final String JSON_QUERY_PROPERTY = "extractor.json.path";
 
-    public static final String JSON_QUERY_PROPERTY = "extractor.json.path";
-    private static final String JSON_EXTRACTOR_TYPE = "json";
+  private static final String JSON_EXTRACTOR_TYPE = "json";
 
-    private IJacksonService jacksonService;
-    private QueryCompiler compiler;
+  private IJacksonService jacksonService;
 
-    public JsonExtractorFactory() {
-        jacksonService = new JacksonService();
-        compiler = new QueryCompiler();
+  private QueryCompiler compiler;
+
+  public JsonExtractorFactory() {
+    jacksonService = new JacksonService();
+    compiler = new QueryCompiler();
+  }
+
+  @Override public String getExtractorType() {
+    return JSON_EXTRACTOR_TYPE;
+  }
+
+  @Override public Class<StreamConnector> getConnectorType() {
+    return StreamConnector.class;
+  }
+
+  @Override public Extractor createExtractor(StreamConnector connector, ExtractorModel model) {
+    return new JsonExtractor(jacksonService, connector, mapToJsonAttributes(model.getAttributes()), getRootQuery(model));
+  }
+
+  private JsonQuery getRootQuery(ExtractorModel model) {
+    String query = model.getSingletonProperty(JSON_QUERY_PROPERTY);
+    if (query == null) {
+      throw new IllegalArgumentException(String.format("Missing required property for key \'%s\'", JSON_QUERY_PROPERTY));
     }
+    return compiler.compile(query);
+  }
 
-    @Override
-    public String getExtractorType() {
-        return JSON_EXTRACTOR_TYPE;
+  private JsonRowAttribute[] mapToJsonAttributes(RowAttribute[] attributes) {
+    int len = attributes.length;
+    JsonRowAttribute[] jsonAttributes = new JsonRowAttribute[len];
+    for (int i = 0; i < len; i++) {
+      jsonAttributes[i] = new JsonRowAttribute(attributes[i], compiler);
     }
-
-    @Override
-    public Class<StreamConnector> getConnectorType() {
-        return StreamConnector.class;
-    }
-
-    @Override
-    public Extractor createExtractor(StreamConnector connector, ExtractorModel model) {
-        return new JsonExtractor(
-                jacksonService,
-                connector,
-                mapToJsonAttributes(model.getAttributes()),
-                getRootQuery(model));
-    }
-
-    private JsonQuery getRootQuery(ExtractorModel model) {
-        String query = model.getSingletonProperty(JSON_QUERY_PROPERTY);
-
-        // TODO: should we just use "$" for root instead of throwing?
-        if (query == null) {
-            throw new IllegalArgumentException(String.format(
-                    "Missing required property for key '%s'", JSON_QUERY_PROPERTY));
-        }
-        return compiler.compile(query);
-    }
-
-    private JsonRowAttribute[] mapToJsonAttributes(RowAttribute[] attributes) {
-        int len = attributes.length;
-        JsonRowAttribute[] jsonAttributes = new JsonRowAttribute[len];
-
-        for (int i = 0; i < len; i++) {
-            jsonAttributes[i] = new JsonRowAttribute(attributes[i], compiler);
-        }
-        return jsonAttributes;
-    }
+    return jsonAttributes;
+  }
 }
