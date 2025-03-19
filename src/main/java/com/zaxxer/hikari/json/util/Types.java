3070a2024f60d9fea8b7b96563e9d50a8714e4e5
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 17:11:30 +0000

    Mining Framework Analysis

--- src/main/java/com/zaxxer/hikari/json/util/Types.java
+++ src/main/java/com/zaxxer/hikari/json/util/Types.java
@@ -1,25 +1,13 @@
-package com.zaxxer.hikari.json.util;
-
-
-public class Types
-{
-   // Constants, where for numerics the LSB indicates whether it is an
-   // integer or decimal type.
-   //
-   // ONLY SET THIS BIT FOR INT TYPES ------v
-   public static final int BYTE    = 0b0000_1;
-   public static final int INT     = 0b0001_1;
-   public static final int SHORT   = 0b0010_1;
-   public static final int LONG    = 0b0011_1;
-   public static final int CHAR    = 0b0100_1;
-
-   public static final int FLOAT   = 0b0000_0;
-   public static final int DOUBLE  = 0b0001_0;
-   public static final int BOOLEAN = 0b0010_0;
-   public static final int STRING  = 0b0011_0;
-   public static final int OBJECT  = 0b0100_0;
-   public static final int DATE    = 0b0101_0;
-   public static final int ENUM    = 0b0110_0;
-
-   public static final int INTEGRAL_TYPE = 0b1;
-}
+  package     com . zaxxer . hikari . json . util ;   public class Types  {   public static final  int  BYTE = 0b0000_1 ;   public static final  int  INT = 0b0001_1 ;   public static final  int  SHORT = 0b0010_1 ;   public static final  int  LONG = 0b0011_1 ;   public static final  int  CHAR = 0b0100_1 ;   public static final  int  FLOAT = 0b0000_0 ;   public static final  int  DOUBLE = 0b0001_0 ;   public static final  int  BOOLEAN = 0b0010_0 ;   public static final  int  STRING = 0b0011_0 ;   public static final  int  OBJECT = 0b0100_0 ;   public static final  int  ENUM = 
+<<<<<<<
+0b1010
+=======
+0b0110_0
+>>>>>>>
+ ;   public static final  int  DATE = 
+<<<<<<<
+0b1100
+=======
+0b0101_0
+>>>>>>>
+ ;   public static final  int  INTEGRAL_TYPE = 0b1 ; }
