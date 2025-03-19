cb8cb705eda04f23f87cc309ac8beb32daefbb76
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 12:39:14 +0000

    Mining Framework Analysis

--- score-tests/src/test/java/io/cloudslang/schema/EngineTest.java
+++ score-tests/src/test/java/io/cloudslang/schema/EngineTest.java
@@ -1,21 +1,4 @@
-/*
- * Copyright © 2014-2017 EntIT Software LLC, a Micro Focus company (L.P.)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package io.cloudslang.schema;
-
 import io.cloudslang.engine.data.SimpleHiloIdentifierGenerator;
 import io.cloudslang.engine.node.services.QueueConfigurationDataService;
 import io.cloudslang.engine.node.services.WorkerNodeService;
@@ -54,164 +37,127 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.support.TransactionTemplate;
-
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 import java.util.List;
 import java.util.Properties;
-
 import static org.fest.assertions.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 
 /**
  * Date: 1/21/14
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = EngineTest.Context.class)
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
-public class EngineTest {
+@RunWith(value = SpringJUnit4ClassRunner.class) @ContextConfiguration(classes = EngineTest.Context.class) @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS) public class EngineTest {
+  private static final int WORKER_FREE_MEMORY = 200000000;
 
-    private static final int WORKER_FREE_MEMORY = 200000000; //bytes
+  @Autowired private Score score;
 
-    @Autowired
-    private Score score;
+  @Autowired private WorkerNodeService workerNodeService;
 
-    @Autowired
-    private WorkerNodeService workerNodeService;
+  @Autowired private QueueDispatcherService dispatcherService;
 
-    @Autowired
-    private QueueDispatcherService dispatcherService;
+  @Test public void baseEngineTest() throws InterruptedException {
+    workerNodeService.create("uuid", "password", "host", "dir");
+    workerNodeService.activate("uuid");
+    workerNodeService.up("uuid", "", "");
+    ExecutionPlan executionPlan = createExecutionPlan();
+    TriggeringProperties triggeringProperties = TriggeringProperties.create(executionPlan);
+    score.trigger(triggeringProperties);
+    Thread.sleep(300);
+    List<ExecutionMessage> messages = dispatcherService.poll("uuid", 10, WORKER_FREE_MEMORY);
+    assertThat(messages).hasSize(1);
+  }
 
-    @Test
-    public void baseEngineTest() throws InterruptedException {
-        // register worker
-        workerNodeService.create("uuid", "password", "host", "dir");
-        workerNodeService.activate("uuid");
-        workerNodeService.up("uuid", "", "");
+  private ExecutionPlan createExecutionPlan() {
+    return new ExecutionPlan().setFlowUuid("flowUUID").addStep(new ExecutionStep(1L)).setBeginStep(1L);
+  }
 
-        ExecutionPlan executionPlan = createExecutionPlan();
-        TriggeringProperties triggeringProperties = TriggeringProperties.create(executionPlan);
-        score.trigger(triggeringProperties);
-        Thread.sleep(300);
-        List<ExecutionMessage> messages = dispatcherService.poll("uuid", 10, WORKER_FREE_MEMORY);
-
-        assertThat(messages).hasSize(1);
+  @Configuration @EnableTransactionManagement @ImportResource(value = "META-INF/spring/schema/schemaEngineTestContext.xml") static class Context {
+    @Bean DataSource dataSource() {
+      return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).build();
     }
 
-    private ExecutionPlan createExecutionPlan() {
-        return new ExecutionPlan()
-                .setFlowUuid("flowUUID")
-                .addStep(new ExecutionStep(1L))
-                .setBeginStep(1L);
+    @Bean JdbcTemplate jdbcTemplate(DataSource dataSource) {
+      return new JdbcTemplate(dataSource);
     }
 
-    @Configuration
-    @EnableTransactionManagement
-    @ImportResource("META-INF/spring/schema/schemaEngineTestContext.xml")
-    static class Context {
-        @Bean
-        DataSource dataSource() {
-            return new EmbeddedDatabaseBuilder()
-                    .setType(EmbeddedDatabaseType.H2)
-                    .build();
-        }
-
-        @Bean
-        JdbcTemplate jdbcTemplate(DataSource dataSource) {
-            return new JdbcTemplate(dataSource);
-        }
-
-        @Bean
-        SpringLiquibase liquibase(DataSource dataSource) {
-            SpringLiquibase liquibase = new SpringLiquibase();
-            liquibase.setDataSource(dataSource);
-            liquibase.setChangeLog("classpath:/META-INF/database/test.changes.xml");
-            SimpleHiloIdentifierGenerator.setDataSource(dataSource);
-            return liquibase;
-        }
-
-        @Bean
-        Properties jpaProperties() {
-            return new Properties() {{
-                setProperty("hibernate.format_sql", "true");
-                setProperty("hibernate.hbm2ddl.auto", "create");
-                setProperty("hibernate.cache.use_query_cache", "false");
-                setProperty("hibernate.generate_statistics", "false");
-                setProperty("hibernate.cache.use_second_level_cache", "false");
-                setProperty("hibernate.order_updates", "true");
-                setProperty("hibernate.order_inserts", "true");
-            }};
-        }
-
-        @Bean
-        JpaVendorAdapter jpaVendorAdapter() {
-            HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
-            adapter.setShowSql(false);
-            adapter.setGenerateDdl(true);
-            return adapter;
-        }
-
-        @Bean(name = "entityManagerFactory")
-        @DependsOn("liquibase")
-        LocalContainerEntityManagerFactoryBean emf(JpaVendorAdapter jpaVendorAdapter, Properties jpaProperties) {
-            LocalContainerEntityManagerFactoryBean fb = new LocalContainerEntityManagerFactoryBean();
-            fb.setDataSource(dataSource());
-            fb.setJpaProperties(jpaProperties);
-            fb.setPersistenceProviderClass(HibernatePersistenceProvider.class);
-            fb.setPackagesToScan("io.cloudslang");
-            fb.setJpaVendorAdapter(jpaVendorAdapter);
-            return fb;
-        }
-
-        @Bean
-        PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
-            return new JpaTransactionManager(emf);
-        }
-
-        @Bean
-        TransactionTemplate transactionTemplate(PlatformTransactionManager transactionManager) {
-            return new TransactionTemplate(transactionManager);
-        }
-
-        @Bean
-        EventBus eventBus() {
-            return mock(EventBus.class);
-        }
-
-        @Bean(name = "consumptionFastEventBus")
-        FastEventBus fastEventBus() {
-            return mock(FastEventBus.class);
-        }
-
-        @Bean
-        public ExecutionServiceImpl executionService() {
-            return new ExecutionServiceImpl();
-        }
-
-        @Bean
-        public ReflectionAdapter getReflectionAdapter() {
-            return mock(ReflectionAdapter.class);
-        }
-
-        @Bean
-        public WorkerConfigurationService getWorkerConfigurationService() {
-            return mock(WorkerConfigurationService.class);
-        }
-
-        @Bean
-        public RobotAvailabilityService robotAvailabilityService() {
-            return mock(RobotAvailabilityService.class);
-        }
-
-        @Bean
-        public AplsLicensingService aplsLicensingService() {
-            return mock(AplsLicensingService.class);
-        }
-
-        @Bean
-        public QueueConfigurationDataService queueConfigurationDataService() {
-            return mock(QueueConfigurationDataService.class);
-        }
+    @Bean SpringLiquibase liquibase(DataSource dataSource) {
+      SpringLiquibase liquibase = new SpringLiquibase();
+      liquibase.setDataSource(dataSource);
+      liquibase.setChangeLog("classpath:/META-INF/database/test.changes.xml");
+      SimpleHiloIdentifierGenerator.setDataSource(dataSource);
+      return liquibase;
     }
 
+    @Bean Properties jpaProperties() {
+      return new Properties() {
+        {
+          setProperty("hibernate.format_sql", "true");
+          setProperty("hibernate.hbm2ddl.auto", "create");
+          setProperty("hibernate.cache.use_query_cache", "false");
+          setProperty("hibernate.generate_statistics", "false");
+          setProperty("hibernate.cache.use_second_level_cache", "false");
+          setProperty("hibernate.order_updates", "true");
+          setProperty("hibernate.order_inserts", "true");
+        }
+      };
+    }
+
+    @Bean JpaVendorAdapter jpaVendorAdapter() {
+      HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+      adapter.setShowSql(false);
+      adapter.setGenerateDdl(true);
+      return adapter;
+    }
+
+    @Bean(name = "entityManagerFactory") @DependsOn(value = "liquibase") LocalContainerEntityManagerFactoryBean emf(JpaVendorAdapter jpaVendorAdapter, Properties jpaProperties) {
+      LocalContainerEntityManagerFactoryBean fb = new LocalContainerEntityManagerFactoryBean();
+      fb.setDataSource(dataSource());
+      fb.setJpaProperties(jpaProperties);
+      fb.setPersistenceProviderClass(HibernatePersistenceProvider.class);
+      fb.setPackagesToScan("io.cloudslang");
+      fb.setJpaVendorAdapter(jpaVendorAdapter);
+      return fb;
+    }
+
+    @Bean PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+      return new JpaTransactionManager(emf);
+    }
+
+    @Bean TransactionTemplate transactionTemplate(PlatformTransactionManager transactionManager) {
+      return new TransactionTemplate(transactionManager);
+    }
+
+    @Bean EventBus eventBus() {
+      return mock(EventBus.class);
+    }
+
+    @Bean(name = "consumptionFastEventBus") FastEventBus fastEventBus() {
+      return mock(FastEventBus.class);
+    }
+
+    @Bean public ExecutionServiceImpl executionService() {
+      return new ExecutionServiceImpl();
+    }
+
+    @Bean public ReflectionAdapter getReflectionAdapter() {
+      return mock(ReflectionAdapter.class);
+    }
+
+    @Bean public WorkerConfigurationService getWorkerConfigurationService() {
+      return mock(WorkerConfigurationService.class);
+    }
+
+    @Bean public RobotAvailabilityService robotAvailabilityService() {
+      return mock(RobotAvailabilityService.class);
+    }
+
+    @Bean public AplsLicensingService aplsLicensingService() {
+      return mock(AplsLicensingService.class);
+    }
+
+    @Bean public QueueConfigurationDataService queueConfigurationDataService() {
+      return mock(QueueConfigurationDataService.class);
+    }
+  }
 }
--- score-tests/src/test/java/io/cloudslang/schema/WorkerTest.java
+++ score-tests/src/test/java/io/cloudslang/schema/WorkerTest.java
@@ -1,21 +1,4 @@
-/*
- * Copyright © 2014-2017 EntIT Software LLC, a Micro Focus company (L.P.)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package io.cloudslang.schema;
-
 import io.cloudslang.engine.queue.services.ExecutionQueueService;
 import io.cloudslang.orchestrator.services.*;
 import io.cloudslang.worker.management.queue.WorkerQueueDetailsContainer;
@@ -28,7 +11,6 @@ import org.springframework.context.annotation.ImportResource;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
 import io.cloudslang.engine.node.services.WorkerNodeService;
 import io.cloudslang.engine.queue.entities.ExecutionMessageConverter;
 import io.cloudslang.engine.queue.services.QueueDispatcherService;
@@ -36,104 +18,76 @@ import io.cloudslang.engine.queue.services.QueueStateIdGeneratorService;
 import io.cloudslang.engine.queue.services.ScoreEventFactory;
 import io.cloudslang.worker.management.services.WorkerManager;
 import io.cloudslang.worker.management.services.dbsupport.WorkerDbSupportService;
-
 import static org.mockito.Mockito.mock;
 
 /**
  * Date: 1/21/14
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
-public class WorkerTest {
+@RunWith(value = SpringJUnit4ClassRunner.class) @ContextConfiguration @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS) public class WorkerTest {
+  @Autowired private WorkerManager workerManager;
 
-	@Autowired
-	private WorkerManager workerManager;
+  @Test public void baseEngineTest() throws InterruptedException {
+  }
 
-	@Test
-    //This test starts an engine process, uncomment the line if you want the process to run, please note it is endless...
-	public void baseEngineTest() throws InterruptedException {
-//		Thread.currentThread().join();
-	}
+  @Configuration @ImportResource(value = "classpath:META-INF/spring/schema/schemaWorkerTestContext.xml") static class Context {
+    @Bean EngineVersionService versionService() {
+      return mock(EngineVersionService.class);
+    }
 
-	@Configuration
-	@ImportResource("classpath:META-INF/spring/schema/schemaWorkerTestContext.xml")
-	static class Context{
+    @Bean OrchestratorDispatcherService orchestratorDispatcherService() {
+      return mock(OrchestratorDispatcherService.class);
+    }
 
-		@Bean
-		EngineVersionService versionService(){
-			return mock(EngineVersionService.class);
-		}
+    @Bean QueueDispatcherService queueDispatcherService() {
+      return mock(QueueDispatcherService.class);
+    }
 
-		@Bean
-		OrchestratorDispatcherService orchestratorDispatcherService(){
-			return mock(OrchestratorDispatcherService.class);
-		}
+    @Bean PauseResumeService pauseResumeService() {
+      return mock(PauseResumeService.class);
+    }
 
-		@Bean
-		QueueDispatcherService queueDispatcherService(){
-			return mock(QueueDispatcherService.class);
-		}
+    @Bean WorkerNodeService workerNodeService() {
+      return mock(WorkerNodeService.class);
+    }
 
-		@Bean
-		PauseResumeService pauseResumeService(){
-			return mock(PauseResumeService.class);
-		}
+    @Bean WorkerDbSupportService workerDbSupportService() {
+      return mock(WorkerDbSupportService.class);
+    }
 
-		@Bean
-         WorkerNodeService workerNodeService(){
-            return mock(WorkerNodeService.class);
-        }
+    @Bean CancelExecutionService cancelExecutionService() {
+      return mock(CancelExecutionService.class);
+    }
 
-		@Bean
-		WorkerDbSupportService workerDbSupportService(){
-			return mock(WorkerDbSupportService.class);
-		}
+    @Bean MergedConfigurationService mergedConfigurationService() {
+      return mock(MergedConfigurationService.class);
+    }
 
-		@Bean
-		CancelExecutionService cancelExecutionService(){
-			return mock(CancelExecutionService.class);
-		}
+    @Bean ExecutionMessageConverter executionMessageConverter() {
+      return mock(ExecutionMessageConverter.class);
+    }
 
-		@Bean
-		MergedConfigurationService mergedConfigurationService(){
-			return mock(MergedConfigurationService.class);
-		}
+    @Bean ScoreEventFactory scoreEventFactory() {
+      return mock(ScoreEventFactory.class);
+    }
 
-		@Bean
-		ExecutionMessageConverter executionMessageConverter(){
-			return mock(ExecutionMessageConverter.class);
-		}
+    @Bean QueueStateIdGeneratorService queueStateIdGeneratorService() {
+      return mock(QueueStateIdGeneratorService.class);
+    }
 
-		@Bean
-		ScoreEventFactory scoreEventFactory() {
-			return mock(ScoreEventFactory.class);
-		}
+    @Bean ExecutionQueueService executionQueueService() {
+      return mock(ExecutionQueueService.class);
+    }
 
-		@Bean
-		QueueStateIdGeneratorService queueStateIdGeneratorService(){
-			return mock(QueueStateIdGeneratorService.class);
-		}
+    @Bean SuspendedExecutionService suspendedExecution() {
+      return mock(SuspendedExecutionService.class);
+    }
 
-		@Bean
-		ExecutionQueueService executionQueueService() {
-			return mock(ExecutionQueueService.class);
-		}
+    @Bean AplsLicensingService aplsLicensingService() {
+      return mock(AplsLicensingService.class);
+    }
 
-		@Bean
-		SuspendedExecutionService suspendedExecution() {
-			return mock(SuspendedExecutionService.class);
-		}
-
-		@Bean
-		AplsLicensingService aplsLicensingService() {
-			return mock(AplsLicensingService.class);
-		}
-
-		@Bean
-		public WorkerQueueDetailsContainer workerQueueDetailsContainer() {
-			return mock(WorkerQueueDetailsContainer.class);
-		}
-	}
-
+    @Bean public WorkerQueueDetailsContainer workerQueueDetailsContainer() {
+      return mock(WorkerQueueDetailsContainer.class);
+    }
+  }
 }
