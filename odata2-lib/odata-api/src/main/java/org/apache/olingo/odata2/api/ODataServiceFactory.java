532c92168f7c9398098007d7909959c36afd3c59
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 20:36:38 +0000

    Mining Framework Analysis

--- odata2-lib/odata-api/src/main/java/org/apache/olingo/odata2/api/ODataServiceFactory.java
+++ odata2-lib/odata-api/src/main/java/org/apache/olingo/odata2/api/ODataServiceFactory.java
@@ -1,23 +1,4 @@
-/*******************************************************************************
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements. See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership. The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at
- * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations
- * under the License.
- ******************************************************************************/
 package org.apache.olingo.odata2.api;
-
 import org.apache.olingo.odata2.api.edm.provider.EdmProvider;
 import org.apache.olingo.odata2.api.exception.ODataException;
 import org.apache.olingo.odata2.api.processor.ODataContext;
@@ -30,7 +11,6 @@ import org.apache.olingo.odata2.api.rt.RuntimeDelegate;
  * 
  */
 public abstract class ODataServiceFactory {
-
   /**
    * Label used in web.xml to assign servlet init parameter to factory class instance.
    */
@@ -65,8 +45,7 @@ public abstract class ODataServiceFactory {
    * @param processor A custom processor implementation derived from <code>ODataSingleProcessor</code> .
    * @return A new default <code>ODataSingleProcessorService</code> instance.
    */
-  public ODataService createODataSingleProcessorService(final EdmProvider provider,
-      final ODataSingleProcessor processor) {
+  public ODataService createODataSingleProcessorService(final EdmProvider provider, final ODataSingleProcessor processor) {
     return RuntimeDelegate.createODataSingleProcessorService(provider, processor);
   }
 
@@ -78,5 +57,4 @@ public abstract class ODataServiceFactory {
   public <T extends ODataCallback> T getCallback(final Class<T> callbackInterface) {
     return null;
   }
-
 }
