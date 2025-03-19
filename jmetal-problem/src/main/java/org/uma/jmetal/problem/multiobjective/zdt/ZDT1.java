7884a5205ac29b93c8e580757ff72bd30791bd6a
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 22:32:50 +0000

    Mining Framework Analysis

--- jmetal-problem/src/main/java/org/uma/jmetal/problem/multiobjective/zdt/ZDT1.java
+++ jmetal-problem/src/main/java/org/uma/jmetal/problem/multiobjective/zdt/ZDT1.java
@@ -1,35 +1,11 @@
-//  ZDT1.java
-//
-//  Author:
-//       Antonio J. Nebro <antonio@lcc.uma.es>
-//       Juan J. Durillo <durillo@lcc.uma.es>
-//
-//  Copyright (c) 2011 Antonio J. Nebro, Juan J. Durillo
-//
-//  This program is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU Lesser General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
-//
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU Lesser General Public License for more details.
-// 
-//  You should have received a copy of the GNU Lesser General Public License
-//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 package org.uma.jmetal.problem.multiobjective.zdt;
-
 import org.uma.jmetal.problem.impl.AbstractDoubleProblem;
 import org.uma.jmetal.solution.DoubleSolution;
-
 import java.util.ArrayList;
 import java.util.List;
 
 /** Class representing problem ZDT1 */
 public class ZDT1 extends AbstractDoubleProblem {
-
   /** Constructor. Creates default instance of problem ZDT1 (30 decision variables) */
   public ZDT1() {
     this(30);
@@ -44,15 +20,12 @@ public class ZDT1 extends AbstractDoubleProblem {
     setNumberOfVariables(numberOfVariables);
     setNumberOfObjectives(2);
     setName("ZDT1");
-
-    List<Double> lowerLimit = new ArrayList<>(getNumberOfVariables()) ;
-    List<Double> upperLimit = new ArrayList<>(getNumberOfVariables()) ;
-
+    List<Double> lowerLimit = new ArrayList<>(getNumberOfVariables());
+    List<Double> upperLimit = new ArrayList<>(getNumberOfVariables());
     for (int i = 0; i < getNumberOfVariables(); i++) {
       lowerLimit.add(0.0);
       upperLimit.add(1.0);
     }
-
     setLowerLimit(lowerLimit);
     setUpperLimit(upperLimit);
   }
@@ -60,12 +33,10 @@ public class ZDT1 extends AbstractDoubleProblem {
   /** Evaluate() method */
   public void evaluate(DoubleSolution solution) {
     double[] f = new double[getNumberOfObjectives()];
-
     f[0] = solution.getVariableValue(0);
     double g = this.evalG(solution);
     double h = this.evalH(f[0], g);
     f[1] = h * g;
-
     solution.setObjective(0, f[0]);
     solution.setObjective(1, f[1]);
   }
@@ -93,7 +64,7 @@ public class ZDT1 extends AbstractDoubleProblem {
    * @param g Second argument of the function H.
    */
   public double evalH(double f, double g) {
-    double h ;
+    double h;
     h = 1.0 - Math.sqrt(f / g);
     return h;
   }
