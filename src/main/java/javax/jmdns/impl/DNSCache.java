1332bb2164aa37bf4292b9fc89963e40f748113d
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 20:03:06 +0000

    Mining Framework Analysis

--- src/main/java/javax/jmdns/impl/DNSCache.java
+++ src/main/java/javax/jmdns/impl/DNSCache.java
@@ -1,9 +1,4 @@
-// Copyright 2003-2005 Arthur van Hoff Rick Blair
-// Licensed under Apache License version 2.0
-// Original license LGPL
-
 package javax.jmdns.impl;
-
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
@@ -11,10 +6,8 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-
 import javax.jmdns.impl.constants.DNSRecordClass;
 import javax.jmdns.impl.constants.DNSRecordType;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -44,114 +37,107 @@ import org.slf4j.LoggerFactory;
  * @author Arthur van Hoff, Werner Randelshofer, Rick Blair, Pierre Frisch
  */
 public class DNSCache {
+  private static Logger logger = LoggerFactory.getLogger(DNSCache.class.getName());
 
-    private static Logger       logger              = LoggerFactory.getLogger(DNSCache.class.getName());
+  private static final long serialVersionUID = 3024739453186759259L;
 
-    private static final long   serialVersionUID    = 3024739453186759259L;
+  private ConcurrentHashMap<String, List<DNSEntry>> cacheMap;
 
-    private ConcurrentHashMap<String, List<DNSEntry>> cacheMap;
-
-    /**
+  /**
      *
      */
-    public DNSCache() {
-        this(1024);
-    }
+  public DNSCache() {
+    this(1024);
+  }
 
-    /**
+  /**
      * @param other Other DNSCache instance
      */
-    public DNSCache(DNSCache other) {
-        this(other != null ? other.cacheMap.size() : 1024);
-        if (other != null) {
-            cacheMap.putAll(other.cacheMap);
-        }
+  public DNSCache(DNSCache other) {
+    this(other != null ? other.cacheMap.size() : 1024);
+    if (other != null) {
+      cacheMap.putAll(other.cacheMap);
     }
+  }
 
-    /**
+  /**
      * Create a table with a given initial size.
      *
      * @param initialCapacity
      */
-    public DNSCache(int initialCapacity) {
-        cacheMap = new ConcurrentHashMap<String, List<DNSEntry>>(initialCapacity);
-    }
+  public DNSCache(int initialCapacity) {
+    cacheMap = new ConcurrentHashMap<String, List<DNSEntry>>(initialCapacity);
+  }
 
-    // ====================================================================
-    // Map
-
-    /**
+  /**
      * {@inheritDoc}
      */
-    @Override
-    protected Object clone() throws CloneNotSupportedException {
-        return new DNSCache(this);
-    }
+  @Override protected Object clone() throws CloneNotSupportedException {
+    return new DNSCache(this);
+  }
 
-    // ====================================================================
-
-    /**
+  /**
      * Returns all entries in the cache
      *
      * @return all entries in the cache
      */
-    public Collection<DNSEntry> allValues() {
-        List<DNSEntry> allValues = new ArrayList<DNSEntry>();
-        for (List<? extends DNSEntry> entry : cacheMap.values()) {
-            if (entry != null) {
-                allValues.addAll(entry);
-            }
-        }
-        return allValues;
+  public Collection<DNSEntry> allValues() {
+    List<DNSEntry> allValues = new ArrayList<DNSEntry>();
+    for (List<? extends DNSEntry> entry : cacheMap.values()) {
+      if (entry != null) {
+        allValues.addAll(entry);
+      }
     }
+    return allValues;
+  }
 
-    /**
+  /**
      * Iterate only over items with matching name. Returns an list of DNSEntry or null. To retrieve all entries, one must iterate over this linked list.
      *
      * @param name
      * @return list of DNSEntries
      */
-    public Collection<? extends DNSEntry> getDNSEntryList(String name) {
-        Collection<? extends DNSEntry> entryList = _getDNSEntryList(name);
-        if (entryList != null) {
-            synchronized (entryList) {
-                entryList = new ArrayList<DNSEntry>(entryList);
-            }
-        } else {
-            entryList = Collections.emptyList();
-        }
-        return entryList;
+  public Collection<? extends DNSEntry> getDNSEntryList(String name) {
+    Collection<? extends DNSEntry> entryList = _getDNSEntryList(name);
+    if (entryList != null) {
+      synchronized (entryList) {
+        entryList = new ArrayList<DNSEntry>(entryList);
+      }
+    } else {
+      entryList = Collections.emptyList();
     }
+    return entryList;
+  }
 
-    private Collection<? extends DNSEntry> _getDNSEntryList(String name) {
-        return cacheMap.get(name != null ? name.toLowerCase() : null);
-    }
+  private Collection<? extends DNSEntry> _getDNSEntryList(String name) {
+    return cacheMap.get(name != null ? name.toLowerCase() : null);
+  }
 
-    /**
+  /**
      * Get a matching DNS entry from the table (using isSameEntry). Returns the entry that was found.
      *
      * @param dnsEntry
      * @return DNSEntry
      */
-    public DNSEntry getDNSEntry(DNSEntry dnsEntry) {
-        DNSEntry result = null;
-        if (dnsEntry != null) {
-            Collection<? extends DNSEntry> entryList = _getDNSEntryList(dnsEntry.getKey());
-            if (entryList != null) {
-                synchronized (entryList) {
-                    for (DNSEntry testDNSEntry : entryList) {
-                        if (testDNSEntry.isSameEntry(dnsEntry)) {
-                            result = testDNSEntry;
-                            break;
-                        }
-                    }
-                }
+  public DNSEntry getDNSEntry(DNSEntry dnsEntry) {
+    DNSEntry result = null;
+    if (dnsEntry != null) {
+      Collection<? extends DNSEntry> entryList = _getDNSEntryList(dnsEntry.getKey());
+      if (entryList != null) {
+        synchronized (entryList) {
+          for (DNSEntry testDNSEntry : entryList) {
+            if (testDNSEntry.isSameEntry(dnsEntry)) {
+              result = testDNSEntry;
+              break;
             }
+          }
         }
-        return result;
+      }
     }
+    return result;
+  }
 
-    /**
+  /**
      * Get a matching DNS entry from the table.
      *
      * @param name
@@ -159,23 +145,23 @@ public class DNSCache {
      * @param recordClass
      * @return DNSEntry
      */
-    public DNSEntry getDNSEntry(String name, DNSRecordType type, DNSRecordClass recordClass) {
-        DNSEntry result = null;
-        Collection<? extends DNSEntry> entryList = _getDNSEntryList(name);
-        if (entryList != null) {
-            synchronized (entryList) {
-                for (DNSEntry testDNSEntry : entryList) {
-                    if (testDNSEntry.matchRecordType(type) && testDNSEntry.matchRecordClass(recordClass)) {
-                        result = testDNSEntry;
-                        break;
-                    }
-                }
-            }
+  public DNSEntry getDNSEntry(String name, DNSRecordType type, DNSRecordClass recordClass) {
+    DNSEntry result = null;
+    Collection<? extends DNSEntry> entryList = _getDNSEntryList(name);
+    if (entryList != null) {
+      synchronized (entryList) {
+        for (DNSEntry testDNSEntry : entryList) {
+          if (testDNSEntry.matchRecordType(type) && testDNSEntry.matchRecordClass(recordClass)) {
+            result = testDNSEntry;
+            break;
+          }
         }
-        return result;
+      }
     }
+    return result;
+  }
 
-    /**
+  /**
      * Get all matching DNS entries from the table.
      *
      * @param name
@@ -183,71 +169,79 @@ public class DNSCache {
      * @param recordClass
      * @return list of entries
      */
-    public Collection<? extends DNSEntry> getDNSEntryList(String name, DNSRecordType type, DNSRecordClass recordClass) {
-        Collection<? extends DNSEntry> entryList = _getDNSEntryList(name);
-        if (entryList != null) {
-            synchronized (entryList) {
-                entryList = new ArrayList<DNSEntry>(entryList);
-                for (Iterator<? extends DNSEntry> i = entryList.iterator(); i.hasNext();) {
-                    DNSEntry testDNSEntry = i.next();
-                    if (!testDNSEntry.matchRecordType(type) || (!testDNSEntry.matchRecordClass(recordClass))) {
-                        i.remove();
-                    }
-                }
-            }
-        } else {
-            entryList = Collections.emptyList();
+  public Collection<? extends DNSEntry> getDNSEntryList(String name, DNSRecordType type, DNSRecordClass recordClass) {
+    Collection<? extends DNSEntry> entryList = _getDNSEntryList(name);
+    if (entryList != null) {
+      synchronized (entryList) {
+        entryList = new ArrayList<DNSEntry>(entryList);
+        for (Iterator<? extends DNSEntry> i = entryList.iterator(); i.hasNext(); ) {
+          DNSEntry testDNSEntry = i.next();
+          if (!testDNSEntry.matchRecordType(type) || (!testDNSEntry.matchRecordClass(recordClass))) {
+            i.remove();
+          }
         }
-        return entryList;
+      }
+    } else {
+      entryList = Collections.emptyList();
     }
+    return entryList;
+  }
 
-    /**
+  /**
      * Adds an entry to the table.
      *
      * @param dnsEntry
      * @return true if the entry was added
      */
-    public boolean addDNSEntry(final DNSEntry dnsEntry) {
-        boolean result = false;
-        if (dnsEntry != null) {
-            synchronized (cacheMap) {
-                List<DNSEntry> entryList = cacheMap.get(dnsEntry.getKey());
-                if (entryList == null) {
-                    entryList = new ArrayList<DNSEntry>();
-                }
-                entryList.add(dnsEntry);
-                cacheMap.putIfAbsent(dnsEntry.getKey(), entryList);
-            }
-            // This is probably not very informative
-            result = true;
+  public boolean addDNSEntry(final DNSEntry dnsEntry) {
+    boolean result = false;
+    if (dnsEntry != null) {
+      synchronized (cacheMap) {
+        List<DNSEntry> entryList = cacheMap.get(dnsEntry.getKey());
+        if (entryList == null) {
+          entryList = new ArrayList<DNSEntry>();
         }
-        return result;
+        entryList.add(dnsEntry);
+        cacheMap.putIfAbsent(dnsEntry.getKey(), entryList);
+      }
+      result = true;
     }
+    return result;
+  }
 
-    /**
+  /**
      * Removes a specific entry from the table. Returns true if the entry was found.
      *
      * @param dnsEntry
      * @return true if the entry was removed
      */
-    public boolean removeDNSEntry(DNSEntry dnsEntry) {
-        boolean result = false;
-        if (dnsEntry != null) {
-            synchronized (cacheMap) {
-                List<DNSEntry> entryList = cacheMap.get(dnsEntry.getKey());
-                if (entryList != null) {    
-                    result = entryList.remove(dnsEntry);
-                }
-                /* Remove from DNS cache when no records remain with this key */
-                if (result && entryList.isEmpty()) {
-                    cacheMap.remove(dnsEntry.getKey());
-                }
-            }
+  public boolean removeDNSEntry(DNSEntry dnsEntry) {
+    boolean result = false;
+    if (dnsEntry != null) {
+      synchronized (cacheMap) {
+        List<DNSEntry> entryList = cacheMap.get(dnsEntry.getKey());
+        if (entryList != null) {
+          entryList.remove(dnsEntry);
         }
-        return result;
-    }
+      }
 
-    /**
+<<<<<<< Unknown file: This is a bug in JDime.
+=======
+      if (entryList != null) {
+        synchronized (entryList) {
+          result = entryList.remove(dnsEntry);
+        }
+      }
+>>>>>>> /usr/src/app/output/openhab/jmdns/1ea70c3c7b12342a04644c9261369d4a416f3ea1/src/main/java/javax/jmdns/impl/DNSCache.java/right.java
+
+      if (result && entryList.isEmpty()) {
+        this.remove(dnsEntry.getKey());
+      }
+    }
+    return result;
+  }
+
+  /**
      * Replace an existing entry by a new one.<br/>
      * <b>Note:</b> the 2 entries must have the same key.
      *
@@ -255,69 +249,59 @@ public class DNSCache {
      * @param existingDNSEntry
      * @return <code>true</code> if the entry has been replace, <code>false</code> otherwise.
      */
-    public boolean replaceDNSEntry(DNSEntry newDNSEntry, DNSEntry existingDNSEntry) {
-        boolean result = false;
-        if ((newDNSEntry != null) && (existingDNSEntry != null) && (newDNSEntry.getKey().equals(existingDNSEntry.getKey()))) {
-            synchronized (cacheMap) {
-                List<DNSEntry> entryList = cacheMap.get(newDNSEntry.getKey());
-                if (entryList == null) {
-                    entryList = new ArrayList<DNSEntry>();
-                }
-                entryList.remove(existingDNSEntry);
-                entryList.add(newDNSEntry);
-                cacheMap.putIfAbsent(newDNSEntry.getKey(), entryList);
-            }
-            // This is probably not very informative
-            result = true;
+  public boolean replaceDNSEntry(DNSEntry newDNSEntry, DNSEntry existingDNSEntry) {
+    boolean result = false;
+    if ((newDNSEntry != null) && (existingDNSEntry != null) && (newDNSEntry.getKey().equals(existingDNSEntry.getKey()))) {
+      synchronized (cacheMap) {
+        List<DNSEntry> entryList = cacheMap.get(newDNSEntry.getKey());
+        if (entryList == null) {
+          entryList = new ArrayList<DNSEntry>();
         }
-        return result;
+        entryList.remove(existingDNSEntry);
+        entryList.add(newDNSEntry);
+        cacheMap.putIfAbsent(newDNSEntry.getKey(), entryList);
+      }
+      result = true;
     }
+    return result;
+  }
 
-    /**
+  /**
      * {@inheritDoc}
      */
-    @Override
-    public synchronized String toString() {
-        final StringBuilder sb = new StringBuilder(2000);
-        sb.append("\n\t---- cache ----");
-        for(final Map.Entry<String, List<DNSEntry>> entry : cacheMap.entrySet()) {
-            sb.append("\n\n\t\tname '").append(entry.getKey()).append('\'');
-            final List<DNSEntry> entryList = entry.getValue();
-            if ((entryList != null) && (!entryList.isEmpty())) {
-                synchronized (entryList) {
-                    for (DNSEntry dnsEntry : entryList) {
-                        sb.append("\n\t\t\t").append(dnsEntry.toString());
-                    }
-                }
-            } else {
-                sb.append(" : no entries");
-            }
+  @Override public synchronized String toString() {
+    final StringBuilder sb = new StringBuilder(2000);
+    sb.append("\n\t---- cache ----");
+    for (final Map.Entry<String, List<DNSEntry>> entry : cacheMap.entrySet()) {
+      sb.append("\n\n\t\tname \'").append(entry.getKey()).append('\'');
+      final List<DNSEntry> entryList = entry.getValue();
+      if ((entryList != null) && (!entryList.isEmpty())) {
+        synchronized (entryList) {
+          for (DNSEntry dnsEntry : entryList) {
+            sb.append("\n\t\t\t").append(dnsEntry.toString());
+          }
         }
-        return sb.toString();
+      } else {
+        sb.append(" : no entries");
+      }
     }
+    return sb.toString();
+  }
 
-    /**
+  /**
      * Prints the content of the cache to the {@link #logger}.
      */
-    public void logCachedContent() {
-        if (!logger.isTraceEnabled()) {
-            return;
-        }
-
-        logger.trace("Cached DNSEntries: {}", toString());
+  public void logCachedContent() {
+    if (!logger.isTraceEnabled()) {
+      return;
     }
+    logger.trace("Cached DNSEntries: {}", toString());
+  }
 
-    /**
+  /**
      * Clears the contents of the internal cacheMap
      */
-    public void clear() {
-        cacheMap.clear();
-    }
-
-    /**
-     * Get List of DNSEntry entries for provided key.
-     */
-    List<DNSEntry> get(String key) {
-        return cacheMap.get(key);
-    } 
+  public void clear() {
+    cacheMap.clear();
+  }
 }
