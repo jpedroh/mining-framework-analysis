f4ca24efe11c94f043923655fb6be3962e43e127
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 23:17:12 +0000

    Mining Framework Analysis

--- src/de/typology/executables/GoogleNGramBuilder.java
+++ src/de/typology/executables/GoogleNGramBuilder.java
@@ -1,16 +1,12 @@
 package de.typology.executables;
-
 import java.io.File;
 import java.io.IOException;
-
-import de.typology.googleNGrams.NGramMergerMain;
-import de.typology.googleNGrams.NGramParserMain;
+import de.typology.nGramBuilder.NGramFromGoogleBuilder;
 import de.typology.utils.Config;
 import de.typology.utils.IOHelper;
 
 public class GoogleNGramBuilder {
-
-	/**
+  /**
 	 * executes the following steps:
 	 * <p>
 	 * 1) parse and normalize google ngram data
@@ -19,29 +15,33 @@ public class GoogleNGramBuilder {
 	 * @author Rene Pickhardt, Martin Koerner
 	 * @throws IOException
 	 */
-	public static void main(String[] args) throws IOException {
-		// parse and normalize google ngram data:
-		IOHelper.log("start building ngrams");
-		// NGramNormalizer ngn = new NGramNormalizer();
-		File dir = new File(Config.get().googleInputDirectory);
-		new File(Config.get().outputDirectory).mkdirs();
-		for (File f : dir.listFiles()) {
-			IOHelper.log(f.getAbsolutePath() + ":");
-			// PARSE NGRAMS!
-			String googleTyp = f.getName();
+  public static void main(String[] args) throws IOException {
+    IOHelper.log("start building ngrams");
+    File dir = new File(Config.get().googleInputDirectory);
+    new File(Config.get().outputDirectory).mkdirs();
+    for (File f : dir.listFiles()) {
+      IOHelper.log(f.getAbsolutePath() + ":");
+      String googleTyp = f.getName();
+      NGramMergerMain.run(f.getAbsolutePath(), mergedGoogle);
 
-			String outPath = Config.get().outputDirectory + "google/"
-					+ googleTyp + "/";
-			String mergedGoogle = outPath + "merged/";
-			String finalGoogle = outPath + "final/";
-			new File(mergedGoogle).mkdirs();
-			new File(finalGoogle).mkdirs();
-			if (Config.get().parseData) {
-				// TODO combine GoogleNGramNormalizer and GoogleNGramBuilder
-				NGramMergerMain.run(f.getAbsolutePath(), mergedGoogle);
-				NGramParserMain.run(mergedGoogle, outPath);
-				// TODO: extract typology edges
-			}
-		}
-	}
+<<<<<<< /usr/src/app/output/renepickhardt/generalized-language-modeling-toolkit/b2b90c58b5c20fc9e7565e4336c575ab6f27684d/src/de/typology/executables/GoogleNGramBuilder.java/left.java
+      NGramParserMain
+=======
+      NGramFromGoogleBuilder
+>>>>>>> /usr/src/app/output/renepickhardt/generalized-language-modeling-toolkit/b2b90c58b5c20fc9e7565e4336c575ab6f27684d/src/de/typology/executables/GoogleNGramBuilder.java/right.java
+      .run(
+<<<<<<< /usr/src/app/output/renepickhardt/generalized-language-modeling-toolkit/b2b90c58b5c20fc9e7565e4336c575ab6f27684d/src/de/typology/executables/GoogleNGramBuilder.java/left.java
+      mergedGoogle
+=======
+      Config.get().outputDirectory + "/google/" + googleTyp + "/"
+>>>>>>> /usr/src/app/output/renepickhardt/generalized-language-modeling-toolkit/b2b90c58b5c20fc9e7565e4336c575ab6f27684d/src/de/typology/executables/GoogleNGramBuilder.java/right.java
+      , 
+<<<<<<< /usr/src/app/output/renepickhardt/generalized-language-modeling-toolkit/b2b90c58b5c20fc9e7565e4336c575ab6f27684d/src/de/typology/executables/GoogleNGramBuilder.java/left.java
+      outPath
+=======
+      Config.get().outputDirectory + "/google/" + googleTyp + "/" + "normalized/1/1gram-normalized.txt"
+>>>>>>> /usr/src/app/output/renepickhardt/generalized-language-modeling-toolkit/b2b90c58b5c20fc9e7565e4336c575ab6f27684d/src/de/typology/executables/GoogleNGramBuilder.java/right.java
+      );
+    }
+  }
 }
--- src/de/typology/nGramBuilder/NGramBuilder.java
+++ src/de/typology/nGramBuilder/NGramBuilder.java
@@ -1,5 +1,4 @@
 package de.typology.nGramBuilder;
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
@@ -8,7 +7,6 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.HashMap;
-
 import de.typology.stats.NGramDistribution;
 import de.typology.utils.Config;
 import de.typology.utils.IOHelper;
@@ -92,10 +90,8 @@ import de.typology.utils.IOHelper;
  * @author rpickhardt
  * 
  */
-
 public class NGramBuilder {
-
-	/**
+  /**
 	 * 
 	 * TODO:
 	 * 
@@ -112,258 +108,183 @@ public class NGramBuilder {
 	 * 
 	 * @param args
 	 */
-	public static void main(String[] args) {
+  public static void main(String[] args) {
+  }
 
-	}
+  public static void run(String trainingPath, String trainingFile) {
+    long startTime = System.currentTimeMillis();
+    long endTime = 0;
+    long sek = 0;
+    String[] letters = countMostFrequentStartingLetters(62, trainingFile, trainingPath);
+    endTime = System.currentTimeMillis();
+    sek = (endTime - startTime) / 1000;
+    IOHelper.strongLog(sek + " seconds to: count most frequent letters");
+    for (int n = Config.get().nGramLength; n >= 1; n--) {
+      new File(trainingPath + Config.get().nGramsNotAggregatedPath + "/" + n).mkdirs();
+      NGramChunkCreator ngcc = new NGramChunkCreator(letters);
+      if (Config.get().createNGramChunks) {
+        ngcc.createNGramChunks(trainingFile, n, trainingPath);
+        endTime = System.currentTimeMillis();
+        sek = (endTime - startTime) / 1000;
+        IOHelper.strongLog(sek + " seconds to: finnish creating first chunks");
+      }
+      if (Config.get().createSecondLevelNGramChunks) {
+        ngcc.createSecondLevelChunks(trainingPath + Config.get().nGramsNotAggregatedPath + "/" + n, "." + n + "gc");
+        endTime = System.currentTimeMillis();
+        sek = (endTime - startTime) / 1000;
+        IOHelper.strongLog(sek + " seconds to: finnish creating detailed ngram chunks");
+      }
+      if (Config.get().aggregateNGramChunks) {
+        Aggregator a = new Aggregator();
+        a.aggregateNGrams(trainingPath + Config.get().nGramsNotAggregatedPath + "/" + n, "." + n + "gc", 3);
+        endTime = System.currentTimeMillis();
+        sek = (endTime - startTime) / 1000;
+        IOHelper.strongLog(sek + " seconds to: finnish aggregating ngrams");
+      }
+      if (Config.get().sortNGrams) {
+        Sorter s = new Sorter();
+        s.sort(trainingPath + Config.get().nGramsNotAggregatedPath + "/" + n, "." + n + "ga");
+        endTime = System.currentTimeMillis();
+        sek = (endTime - startTime) / 1000;
+        IOHelper.strongLog(sek + " seconds to: finnish sorting aggregated ngrams");
+      }
+      if (Config.get().generateNGramDistribution) {
+        NGramDistribution ngd = new NGramDistribution();
+        ngd.countDistribution(trainingPath + Config.get().nGramsNotAggregatedPath + "/" + n, "." + n + "gs");
+        endTime = System.currentTimeMillis();
+        sek = (endTime - startTime) / 1000;
+        IOHelper.strongLog(sek + " seconds to: finnish creating distribution for ngrams");
+      }
+      new File(trainingPath + Config.get().typologyEdgesPathNotAggregated + "/" + (n - 1)).mkdirs();
+      if (Config.get().createTypologyEdgeChunks) {
+        TypologyChunkCreator tcc = new TypologyChunkCreator(letters);
+        tcc.createTypoelogyEdgeChunks(trainingFile, trainingPath + Config.get().typologyEdgesPathNotAggregated, n - 1);
+        endTime = System.currentTimeMillis();
+        sek = (endTime - startTime) / 1000;
+        IOHelper.strongLog(sek + " seconds to: finnish creating typology edge chunks of type: " + (n - 1));
+      }
+      if (Config.get().createSecondLevelTypologyEdgeChunks) {
+        TypologyChunkCreator tcc = new TypologyChunkCreator(letters);
+        tcc.createSecondLevelChunks(trainingPath + Config.get().typologyEdgesPathNotAggregated + "/" + (n - 1), "." + (n - 1) + "ec");
+        endTime = System.currentTimeMillis();
+        sek = (endTime - startTime) / 1000;
+        IOHelper.strongLog(sek + " seconds to: finnish putting typology edge chunks of type: " + (n - 1) + " into smaller chunks");
+      }
+      if (Config.get().aggregateTypologyEdgeChunks) {
+        Aggregator a = new Aggregator();
+        a.aggregateNGrams(trainingPath + Config.get().typologyEdgesPathNotAggregated + "/" + (n - 1), "." + (n - 1) + "ec", 3);
+        endTime = System.currentTimeMillis();
+        sek = (endTime - startTime) / 1000;
+        IOHelper.strongLog(sek + " seconds to: finnish aggregating ngrams");
+      }
+      if (Config.get().sortTypologyEdges) {
+        Sorter s = new Sorter();
+        s.sort(trainingPath + Config.get().typologyEdgesPathNotAggregated + "/" + (n - 1), "." + (n - 1) + "ea");
+        endTime = System.currentTimeMillis();
+        sek = (endTime - startTime) / 1000;
+        IOHelper.strongLog(sek + " seconds to: finnish sorting aggregated ngrams");
+      }
+      if (Config.get().generateTypologyEdgeDistribution) {
+        NGramDistribution ngd = new NGramDistribution();
+        ngd.countDistribution(trainingPath + Config.get().typologyEdgesPathNotAggregated + "/" + (n - 1), "." + (n - 1) + "es");
+        endTime = System.currentTimeMillis();
+        sek = (endTime - startTime) / 1000;
+        IOHelper.strongLog(sek + " seconds to: finnish creating distribution for ngrams");
+      }
+    }
+    if (Config.get().normalizeNGrams) {
+      try {
+        String input = trainingPath + Config.get().nGramsNotAggregatedPath + "/";
+        String output = trainingPath + Config.get().nGramsNotAggregatedPath + "Normalized/";
+        new File(output).mkdirs();
+        NGramNormalizer ngn = new NGramNormalizer();
+        IOHelper.strongLog("normalizing ngrams from " + input + " and storing updated ngrams at " + output);
+        double time = ngn.normalize(input, output);
+        IOHelper.strongLog("time for normalizing ngrams from " + input + " : " + time);
+      } catch (NumberFormatException e) {
+        e.printStackTrace();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+  }
 
-	public static void run(String trainingPath, String trainingFile) {
-		long startTime = System.currentTimeMillis();
-		long endTime = 0;
-		long sek = 0;
-
-		String[] letters = countMostFrequentStartingLetters(62, trainingFile,
-				trainingPath);
-		endTime = System.currentTimeMillis();
-		sek = (endTime - startTime) / 1000;
-		IOHelper.strongLog(sek + " seconds to: count most frequent letters");
-
-		// wrap everything into a cool program that runs through all the data
-		// sets that we have an creates the appropriate output
-
-		for (int n = Config.get().nGramLength; n >= 1; n--) {
-			new File(trainingPath + Config.get().nGramsNotAggregatedPath + "/"
-					+ n).mkdirs();
-
-			NGramChunkCreator ngcc = new NGramChunkCreator(letters);
-			if (Config.get().createNGramChunks) {
-
-				ngcc.createNGramChunks(trainingFile, n, trainingPath);
-				endTime = System.currentTimeMillis();
-				sek = (endTime - startTime) / 1000;
-				IOHelper.strongLog(sek
-						+ " seconds to: finnish creating first chunks");
-			}
-			if (Config.get().createSecondLevelNGramChunks) {
-				ngcc.createSecondLevelChunks(trainingPath
-						+ Config.get().nGramsNotAggregatedPath + "/" + n, "."
-								+ n + "gc");
-				endTime = System.currentTimeMillis();
-				sek = (endTime - startTime) / 1000;
-				IOHelper.strongLog(sek
-						+ " seconds to: finnish creating detailed ngram chunks");
-			}
-			if (Config.get().aggregateNGramChunks) {
-				Aggregator a = new Aggregator();
-				a.aggregateNGrams(trainingPath
-						+ Config.get().nGramsNotAggregatedPath + "/" + n, "."
-								+ n + "gc", 3);
-				endTime = System.currentTimeMillis();
-				sek = (endTime - startTime) / 1000;
-				IOHelper.strongLog(sek
-						+ " seconds to: finnish aggregating ngrams");
-			}
-
-			if (Config.get().sortNGrams) {
-				Sorter s = new Sorter();
-				s.sort(trainingPath + Config.get().nGramsNotAggregatedPath
-						+ "/" + n, "." + n + "ga");
-				endTime = System.currentTimeMillis();
-				sek = (endTime - startTime) / 1000;
-				IOHelper.strongLog(sek
-						+ " seconds to: finnish sorting aggregated ngrams");
-			}
-
-			if (Config.get().generateNGramDistribution) {
-				NGramDistribution ngd = new NGramDistribution();
-				ngd.countDistribution(trainingPath
-						+ Config.get().nGramsNotAggregatedPath + "/" + n, "."
-								+ n + "gs");
-				endTime = System.currentTimeMillis();
-				sek = (endTime - startTime) / 1000;
-				IOHelper.strongLog(sek
-						+ " seconds to: finnish creating distribution for ngrams");
-			}
-
-			new File(trainingPath + Config.get().typologyEdgesPathNotAggregated
-					+ "/" + (n - 1)).mkdirs();
-			if (Config.get().createTypologyEdgeChunks) {
-				TypologyChunkCreator tcc = new TypologyChunkCreator(letters);
-				tcc.createTypoelogyEdgeChunks(trainingFile, trainingPath
-						+ Config.get().typologyEdgesPathNotAggregated, n - 1);
-				endTime = System.currentTimeMillis();
-				sek = (endTime - startTime) / 1000;
-				IOHelper.strongLog(sek
-						+ " seconds to: finnish creating typology edge chunks of type: "
-						+ (n - 1));
-			}
-
-			if (Config.get().createSecondLevelTypologyEdgeChunks) {
-				TypologyChunkCreator tcc = new TypologyChunkCreator(letters);
-				tcc.createSecondLevelChunks(trainingPath
-						+ Config.get().typologyEdgesPathNotAggregated + "/"
-						+ (n - 1), "." + (n - 1) + "ec");
-				endTime = System.currentTimeMillis();
-				sek = (endTime - startTime) / 1000;
-				IOHelper.strongLog(sek
-						+ " seconds to: finnish putting typology edge chunks of type: "
-						+ (n - 1) + " into smaller chunks");
-			}
-
-			if (Config.get().aggregateTypologyEdgeChunks) {
-				Aggregator a = new Aggregator();
-				a.aggregateNGrams(trainingPath
-						+ Config.get().typologyEdgesPathNotAggregated + "/"
-						+ (n - 1), "." + (n - 1) + "ec", 3);
-				endTime = System.currentTimeMillis();
-				sek = (endTime - startTime) / 1000;
-				IOHelper.strongLog(sek
-						+ " seconds to: finnish aggregating ngrams");
-			}
-
-			if (Config.get().sortTypologyEdges) {
-				Sorter s = new Sorter();
-				s.sort(trainingPath
-						+ Config.get().typologyEdgesPathNotAggregated + "/"
-						+ (n - 1), "." + (n - 1) + "ea");
-				endTime = System.currentTimeMillis();
-				sek = (endTime - startTime) / 1000;
-				IOHelper.strongLog(sek
-						+ " seconds to: finnish sorting aggregated ngrams");
-			}
-
-			if (Config.get().generateTypologyEdgeDistribution) {
-				NGramDistribution ngd = new NGramDistribution();
-				ngd.countDistribution(trainingPath
-						+ Config.get().typologyEdgesPathNotAggregated + "/"
-						+ (n - 1), "." + (n - 1) + "es");
-				endTime = System.currentTimeMillis();
-				sek = (endTime - startTime) / 1000;
-				IOHelper.strongLog(sek
-						+ " seconds to: finnish creating distribution for ngrams");
-			}
-
-		}
-
-		if (Config.get().normalizeNGrams) {
-			try {
-
-				String input = trainingPath
-						+ Config.get().nGramsNotAggregatedPath + "/";
-				String output = trainingPath
-						+ Config.get().nGramsNotAggregatedPath + "Normalized/";
-				new File(output).mkdirs();
-				NGramNormalizer ngn = new NGramNormalizer();
-				IOHelper.strongLog("normalizing ngrams from " + input
-						+ " and storing updated ngrams at " + output);
-				double time = ngn.normalize(input, output);
-				IOHelper.strongLog("time for normalizing ngrams from " + input
-						+ " : " + time);
-			} catch (NumberFormatException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-	}
-
-	// TODO: need to generalize this function if it should fit in more general
-	// tests! need to take away the pointing to trainingFile and
-	// put it as a parameter sourceFile. also the output file should be saved to
-	// a different spot
-
-	private static String[] countMostFrequentStartingLetters(int k,
-			String trainingFile, String trainingPath) {
-		IOHelper.log("Counting the " + k + " most frequent letters in: "
-				+ trainingFile);
-		String[] result = new String[k];
-		try {
-			IOHelper.log("Check if most frequent letters have been counted");
-			File file = new File(trainingPath
-					+ Config.get().nGramsNotAggregatedPath
-					+ "mostFrequentLetters.txt");
-			if (file.exists()) {
-				IOHelper.log("Yes! load from file");
-				FileInputStream f = new FileInputStream(file);
-				ObjectInputStream s;
-				s = new ObjectInputStream(f);
-				result = (String[]) s.readObject();
-				s.close();
-				return result;
-			}
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		IOHelper.log("No! just start counting now");
-
-		BufferedReader br = IOHelper.openReadFile(trainingFile);// Config.get().nGramKeyFile);
-		String line = "";
-		int cnt = 0;
-
-		HashMap<String, Integer> letters = new HashMap<String, Integer>();
-
-		try {
-			while ((line = br.readLine()) != null) {
-				cnt++;
-				String[] tokens = line.split(" ");
-				for (String token : tokens) {
-					if (token == null || token.length() < 1) {
-						continue;
-					}
-					String key = token.substring(0, 1);
-					Integer c = letters.get(key);
-					if (c == null) {
-						letters.put(key, 1);
-					} else {
-						letters.put(key, c + 1);
-					}
-				}
-				if (cnt % 50000 == 0) {
-					System.out.println(cnt);
-				}
-			}
-
-			int done = 0;
-			while (letters.size() > 0 && done < k) {
-				int max = 0;
-				String maxLetter = null;
-				for (String key : letters.keySet()) {
-					int tmp = letters.get(key);
-					if (tmp > max) {
-						maxLetter = key;
-						max = tmp;
-					}
-				}
-				letters.remove(maxLetter);
-				if (maxLetter.equals("-")) {
-					continue;
-				}
-				result[done] = maxLetter;
-				done++;
-			}
-			br.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		File file = new File(trainingPath
-				+ Config.get().nGramsNotAggregatedPath
-				+ "mostFrequentLetters.txt");
-		try {
-			IOHelper.log("Counting done! save results to file");
-
-			FileOutputStream f = new FileOutputStream(file);
-			ObjectOutputStream s = new ObjectOutputStream(f);
-
-			s.writeObject(result);
-			s.flush();
-			s.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return result;
-	}
+  private static String[] countMostFrequentStartingLetters(int k, String trainingFile, String trainingPath) {
+    IOHelper.log("Counting the " + k + " most frequent letters in: " + trainingFile);
+    String[] result = new String[k];
+    try {
+      IOHelper.log("Check if most frequent letters have been counted");
+      File file = new File(trainingPath + Config.get().nGramsNotAggregatedPath + "mostFrequentLetters.txt");
+      if (file.exists()) {
+        IOHelper.log("Yes! load from file");
+        FileInputStream f = new FileInputStream(file);
+        ObjectInputStream s;
+        s = new ObjectInputStream(f);
+        result = (String[]) s.readObject();
+        s.close();
+        return result;
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    IOHelper.log("No! just start counting now");
+    BufferedReader br = IOHelper.openReadFile(trainingFile);
+    String line = "";
+    int cnt = 0;
+    HashMap<String, Integer> letters = new HashMap<String, Integer>();
+    try {
+      while ((line = br.readLine()) != null) {
+        cnt++;
+        String[] tokens = line.split(" ");
+        for (String token : tokens) {
+          if (token == null || token.length() < 1) {
+            continue;
+          }
+          String key = token.substring(0, 1);
+          Integer c = letters.get(key);
+          if (c == null) {
+            letters.put(key, 1);
+          } else {
+            letters.put(key, c + 1);
+          }
+        }
+        if (cnt % 50000 == 0) {
+          System.out.println(cnt);
+        }
+      }
+      int done = 0;
+      while (letters.size() > 0 && done < k) {
+        int max = 0;
+        String maxLetter = null;
+        for (String key : letters.keySet()) {
+          int tmp = letters.get(key);
+          if (tmp > max) {
+            maxLetter = key;
+            max = tmp;
+          }
+        }
+        letters.remove(maxLetter);
+        if (maxLetter.equals("-")) {
+          continue;
+        }
+        result[done] = maxLetter;
+        done++;
+      }
+      br.close();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    File file = new File(trainingPath + Config.get().nGramsNotAggregatedPath + "mostFrequentLetters.txt");
+    try {
+      IOHelper.log("Counting done! save results to file");
+      FileOutputStream f = new FileOutputStream(file);
+      ObjectOutputStream s = new ObjectOutputStream(f);
+      s.writeObject(result);
+      s.flush();
+      s.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return result;
+  }
 }
