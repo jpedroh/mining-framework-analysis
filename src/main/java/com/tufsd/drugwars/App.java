c2a87d4eb552c9831531744573d3d28eb001e914
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 17:42:18 +0000

    Mining Framework Analysis

--- src/main/java/com/tufsd/drugwars/App.java
+++ src/main/java/com/tufsd/drugwars/App.java
@@ -1,10 +1,20 @@
+
+<<<<<<< /usr/src/app/output/shhs-coding-club/drugwars/7ce53da76c6863fc2ce54ea903bc34cf4e910080/src/main/java/com/tufsd/drugwars/App.java/left.java
+package com.tufsd.drugwars;
+import java.util.Scanner;
+
+public class App {
+  public static void main(String[] args) {
+    UI.splashscreen();
+  }
+}
+=======
 package src.main.java.com.tufsd.drugwars;
 import java.util.Scanner;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        UI.splashscreen();
-    }
+public class App {
+  public static void main(String[] args) {
+    UI.splashscreen();
+  }
 }
+>>>>>>> /usr/src/app/output/shhs-coding-club/drugwars/7ce53da76c6863fc2ce54ea903bc34cf4e910080/src/main/java/com/tufsd/drugwars/App.java/right.java
