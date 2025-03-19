c04f818bb2c95b0e291fac4f574701982b41f0db
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 18:11:22 +0000

    Mining Framework Analysis

--- core/src/test/java/com/google/errorprone/bugpatterns/testdata/ThisEscapesConstructorPositiveCases.java
+++ core/src/test/java/com/google/errorprone/bugpatterns/testdata/ThisEscapesConstructorPositiveCases.java
@@ -6,7 +6,7 @@ public class ThisEscapesConstructorPositiveCases {
         public AssignsThis at;
         AssignsThis() {
             // BUG: Diagnostic contains:
-            at = this;
+            this.at = this;
         }
     }
 
