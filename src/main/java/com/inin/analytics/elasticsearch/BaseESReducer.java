dfc65d4f3fcaf874fdaa2567b0063ea22742d93a
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 13:26:47 +0000

    Mining Framework Analysis

--- src/main/java/com/inin/analytics/elasticsearch/BaseESReducer.java
+++ src/main/java/com/inin/analytics/elasticsearch/BaseESReducer.java
@@ -1,12 +1,9 @@
 package com.inin.analytics.elasticsearch;
-
 import static org.elasticsearch.common.settings.Settings.builder;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Iterator;
-
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.hadoop.io.NullWritable;
@@ -20,98 +17,92 @@ import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
 import org.elasticsearch.action.admin.indices.delete.DeleteIndexResponse;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.rest.RestStatus;
+import org.elasticsearch.cluster.metadata.IndexMetaData;
 import com.inin.analytics.elasticsearch.transport.SnapshotTransportStrategy;
 
 public abstract class BaseESReducer implements Reducer<Text, Text, NullWritable, Text> {
-	public static final char TUPLE_SEPARATOR = '|';
-	public static final char DIR_SEPARATOR = '/';
-	
-	public static enum JOB_COUNTER {
-		TIME_SPENT_INDEXING_MS, TIME_SPENT_FLUSHING_MS, TIME_SPENT_MERGING_MS, TIME_SPENT_SNAPSHOTTING_MS, TIME_SPENT_TRANSPORTING_SNAPSHOT_MS, INDEXING_DOC_FAIL, INDEX_DOC_CREATED, INDEX_DOC_NOT_CREATED
-	}
-	
-	// We prefix all snapshots with the word snapshot
-	public static final String SNAPSHOT_NAME = "snapshot";
-	
-	// The local filesystem location that ES will write the snapshot out to
-	private String snapshotWorkingLocation;
-	
-	// Where the snapshot will be moved to. Typical use case would be to throw it onto S3
-	private String snapshotFinalDestination;
-	
-	// The name of a snapshot repo. We'll enumerate that on each job run so that the repo names are unique across rebuilds
-	private String snapshotRepoName;
-	
-	// Local filesystem location where index data is built
-	private String esWorkingDir;
-	
-	// The partition of data this reducer is serving. Useful for making directories unique if running multiple reducers on a task tracker 
-	private String partition;
-	
-	// The container handles spinning up our embedded elasticsearch instance
-	private ESEmbededContainer esEmbededContainer;
-	
-	private ShardConfig shardConfig;
-		
-	// Hold onto some frequently generated objects to cut down on GC overhead 
-	private String indexType;
-	private String docId;
-	private String pre;
-	private String json;
-   
-	@Override
-	public void configure(JobConf job) {
-		partition = job.get("mapred.task.partition");
-        String attemptId = job.get("mapred.task.id");
-		
-		// If running multiple reducers on a node, the node needs a unique name & data directory hence the random number we append 
-		snapshotWorkingLocation = job.get(ConfigParams.SNAPSHOT_WORKING_LOCATION_CONFIG_KEY.toString()) + partition + attemptId + DIR_SEPARATOR;
-		snapshotFinalDestination = job.get(ConfigParams.SNAPSHOT_FINAL_DESTINATION.toString());
-		snapshotRepoName = job.get(ConfigParams.SNAPSHOT_REPO_NAME_CONFIG_KEY.toString());
-		esWorkingDir = job.get(ConfigParams.ES_WORKING_DIR.toString()) + partition + attemptId + DIR_SEPARATOR;
-		if(shardConfig == null) {
-		    shardConfig = getShardConfig(job);    
-		}
-	}
-	
-	public void setShardConfig(ShardConfig shardConfig) {
-        this.shardConfig = shardConfig;
+  public static final char TUPLE_SEPARATOR = '|';
+
+  public static final char DIR_SEPARATOR = '/';
+
+  public static enum JOB_COUNTER {
+    TIME_SPENT_INDEXING_MS,
+    TIME_SPENT_FLUSHING_MS,
+    TIME_SPENT_MERGING_MS,
+    TIME_SPENT_SNAPSHOTTING_MS,
+    TIME_SPENT_TRANSPORTING_SNAPSHOT_MS,
+    INDEXING_DOC_FAIL,
+    INDEX_DOC_CREATED,
+    INDEX_DOC_NOT_CREATED
+  }
+
+  public static final String SNAPSHOT_NAME = "snapshot";
+
+  private String snapshotWorkingLocation;
+
+  private String snapshotFinalDestination;
+
+  private String snapshotRepoName;
+
+  private String esWorkingDir;
+
+  private String partition;
+
+  private ShardConfig shardConfig;
+
+  private ESEmbededContainer esEmbededContainer;
+
+  private String indexType;
+
+  private String docId;
+
+  private String pre;
+
+  private String json;
+
+  @Override public void configure(JobConf job) {
+    partition = job.get("mapred.task.partition");
+    String attemptId = job.get("mapred.task.id");
+    snapshotWorkingLocation = job.get(ConfigParams.SNAPSHOT_WORKING_LOCATION_CONFIG_KEY.toString()) + partition + attemptId + DIR_SEPARATOR;
+    snapshotFinalDestination = job.get(ConfigParams.SNAPSHOT_FINAL_DESTINATION.toString());
+    snapshotRepoName = job.get(ConfigParams.SNAPSHOT_REPO_NAME_CONFIG_KEY.toString());
+    esWorkingDir = job.get(ConfigParams.ES_WORKING_DIR.toString()) + partition + attemptId + DIR_SEPARATOR;
+    if (shardConfig == null) {
+      shardConfig = getShardConfig(job);
     }
+  }
 
-    private void init(String index) {
-		String templateName = getTemplateName();
-		String templateJson = getTemplate();
+  public void setShardConfig(ShardConfig shardConfig) {
+    this.shardConfig = shardConfig;
+  }
 
-		ESEmbededContainer.Builder builder = new ESEmbededContainer.Builder()
-		.withNodeName("embededESTempLoaderNode" + partition)
-		.withWorkingDir(esWorkingDir)
-		.withClusterName("bulkLoadPartition:" + partition)
-		.withSnapshotWorkingLocation(snapshotWorkingLocation)
-		.withSnapshotRepoName(snapshotRepoName);
-		
-		if(templateName != null && templateJson != null) {
-			builder.withTemplate(templateName, templateJson);	
-		}
-		
-		if(esEmbededContainer == null) {
-			esEmbededContainer = builder.build();	
-		} 
+  private void init(String index) {
+    String templateName = getTemplateName();
+    String templateJson = getTemplate();
+    ESEmbededContainer.Builder builder = new ESEmbededContainer.Builder().withNodeName("embededESTempLoaderNode" + partition).withWorkingDir(esWorkingDir).withClusterName("bulkLoadPartition:" + partition).withSnapshotWorkingLocation(snapshotWorkingLocation).withSnapshotRepoName(snapshotRepoName);
+    if (templateName != null && templateJson != null) {
+      builder.withTemplate(templateName, templateJson);
+    }
+    if (esEmbededContainer == null) {
+      esEmbededContainer = builder.build();
+    }
+    esEmbededContainer.getNode().client().admin().indices().prepareCreate(index).setSettings(
+<<<<<<< /usr/src/app/output/mypurecloud/elasticsearch-lambda/9ad6ef3cc4463ad5e7c71ae057264ad3ba7b2f86/src/main/java/com/inin/analytics/elasticsearch/BaseESReducer.java/left.java
+    builder().put("index.number_of_replicas", 0)
+=======
+    settingsBuilder().put("index.number_of_replicas", 0).put(IndexMetaData.SETTING_NUMBER_OF_SHARDS, shardConfig.getShardsForIndex(index))
+>>>>>>> /usr/src/app/output/mypurecloud/elasticsearch-lambda/9ad6ef3cc4463ad5e7c71ae057264ad3ba7b2f86/src/main/java/com/inin/analytics/elasticsearch/BaseESReducer.java/right.java
+    ).get();
+  }
 
-		// Create index
-		esEmbededContainer.getNode().client().admin().indices().prepareCreate(index).setSettings(builder().put("index.number_of_replicas", 0)).get();
-	}
-	
-	/**
+  /**
 	 * Provide the JSON contents of the index template. This is your hook for configuring ElasticSearch.
 	 * 
 	 * http://www.elasticsearch.org/guide/en/elasticsearch/reference/current/indices-templates.html
-	 * 
-     * @return String
 	 */
-	public abstract String getTemplate();
-	
-	
-	/**
+  public abstract String getTemplate();
+
+  /**
 	 * Provide a ShardConfig which provides the number of shards per index and the number of 
 	 * shards to split organizations across. The number can be uniform across indices or a mapping
 	 * can be provided to enable per-index configuration values.
@@ -119,70 +110,62 @@ public abstract class BaseESReducer implements Reducer<Text, Text, NullWritable,
 	 * @param job
 	 * @return ShardConfig
 	 */
-	public abstract ShardConfig getShardConfig(JobConf job);
-	
-	/**
+  public abstract ShardConfig getShardConfig(JobConf job);
+
+  /**
 	 * Provide an all lower case template name
 	 *  
-	 * @return String
+	 * @return
 	 */
-	public abstract String getTemplateName();
+  public abstract String getTemplateName();
 
-	@Override
-	public void reduce(Text docMetaData, Iterator<Text> documentPayloads, OutputCollector<NullWritable, Text> output, final Reporter reporter) throws IOException {
-		String[] pieces = StringUtils.split(docMetaData.toString(), TUPLE_SEPARATOR);
-		String indexName = pieces[0];
-		String routing = pieces[1]; 
-		init(indexName);
+  @Override public void reduce(Text docMetaData, Iterator<Text> documentPayloads, OutputCollector<NullWritable, Text> output, final Reporter reporter) throws IOException {
+    String[] pieces = StringUtils.split(docMetaData.toString(), TUPLE_SEPARATOR);
+    String indexName = pieces[0];
+    String routing = pieces[1];
+    init(indexName);
+    long start = System.currentTimeMillis();
+    while (documentPayloads.hasNext()) {
+      Text line = documentPayloads.next();
+      if (line == null) {
+        continue;
+      }
+      pieces = StringUtils.split(line.toString(), TUPLE_SEPARATOR);
+      indexType = pieces[0];
+      docId = pieces[1];
+      pre = indexType + TUPLE_SEPARATOR + docId + TUPLE_SEPARATOR;
+      json = line.toString().substring(pre.length());
+      IndexResponse response = esEmbededContainer.getNode().client().prepareIndex(indexName, indexType).setId(docId).setRouting(routing).setSource(json).execute().actionGet();
+      if (response.status() == RestStatus.CREATED) {
+        reporter.incrCounter(JOB_COUNTER.INDEX_DOC_CREATED, 1l);
+      } else {
+        reporter.incrCounter(JOB_COUNTER.INDEX_DOC_NOT_CREATED, 1l);
+      }
+    }
+    reporter.incrCounter(JOB_COUNTER.TIME_SPENT_INDEXING_MS, System.currentTimeMillis() - start);
+    snapshot(indexName, reporter);
+    output.collect(NullWritable.get(), new Text(indexName));
+  }
 
-		long start = System.currentTimeMillis();
-		while(documentPayloads.hasNext()) {
-			Text line = documentPayloads.next();
-			if(line == null) {
-				continue;
-			}
-			
-			pieces = StringUtils.split(line.toString(), TUPLE_SEPARATOR);
-			indexType = pieces[0];
-			docId = pieces[1];
-			pre = indexType + TUPLE_SEPARATOR + docId + TUPLE_SEPARATOR;
-			json = line.toString().substring(pre.length());
+  @Override public void close() throws IOException {
+    if (esEmbededContainer != null) {
+      esEmbededContainer.getNode().close();
+      while (!esEmbededContainer.getNode().isClosed()) {
+        ;
+      }
+      FileUtils.deleteDirectory(new File(snapshotWorkingLocation));
+    }
+  }
 
-			IndexResponse response = esEmbededContainer.getNode().client().prepareIndex(indexName, indexType).setId(docId).setRouting(routing).setSource(json).execute().actionGet();
-			if(response.status() == RestStatus.CREATED) {
-				reporter.incrCounter(JOB_COUNTER.INDEX_DOC_CREATED, 1l);
-			} else {
-				reporter.incrCounter(JOB_COUNTER.INDEX_DOC_NOT_CREATED, 1l);
-			}
-		}
-
-		reporter.incrCounter(JOB_COUNTER.TIME_SPENT_INDEXING_MS, System.currentTimeMillis() - start);
-        
-		snapshot(indexName, reporter);
-		output.collect(NullWritable.get(), new Text(indexName));
-	}
-
-	@Override
-	public void close() throws IOException {
-		if(esEmbededContainer != null) {
-			esEmbededContainer.getNode().close();
-			while(!esEmbededContainer.getNode().isClosed());
-			FileUtils.deleteDirectory(new File(snapshotWorkingLocation));
-		}
-	}
-
-	public void snapshot(String index, Reporter reporter) throws IOException {
-	    esEmbededContainer.snapshot(Arrays.asList(index), SNAPSHOT_NAME, snapshotRepoName, reporter);
-
-		// Delete the index to free up that space
-		ActionFuture<DeleteIndexResponse> response = esEmbededContainer.getNode().client().admin().indices().delete(new DeleteIndexRequest(index));
-		while(!response.isDone());
-		
-		// Move the shard snapshot to the destination
-		long start = System.currentTimeMillis();
-		SnapshotTransportStrategy.get(snapshotWorkingLocation, snapshotFinalDestination).execute(SNAPSHOT_NAME);
-        reporter.incrCounter(JOB_COUNTER.TIME_SPENT_TRANSPORTING_SNAPSHOT_MS, System.currentTimeMillis() - start);
-		
-		esEmbededContainer.deleteSnapshot(SNAPSHOT_NAME, snapshotRepoName);
-	}
+  public void snapshot(String index, Reporter reporter) throws IOException {
+    esEmbededContainer.snapshot(Arrays.asList(index), SNAPSHOT_NAME, snapshotRepoName, reporter);
+    ActionFuture<DeleteIndexResponse> response = esEmbededContainer.getNode().client().admin().indices().delete(new DeleteIndexRequest(index));
+    while (!response.isDone()) {
+      ;
+    }
+    long start = System.currentTimeMillis();
+    SnapshotTransportStrategy.get(snapshotWorkingLocation, snapshotFinalDestination).execute(SNAPSHOT_NAME);
+    reporter.incrCounter(JOB_COUNTER.TIME_SPENT_TRANSPORTING_SNAPSHOT_MS, System.currentTimeMillis() - start);
+    esEmbededContainer.deleteSnapshot(SNAPSHOT_NAME, snapshotRepoName);
+  }
 }
--- src/main/java/com/inin/analytics/elasticsearch/example/ExampleIndexingJob.java
+++ src/main/java/com/inin/analytics/elasticsearch/example/ExampleIndexingJob.java
@@ -1,7 +1,5 @@
 package com.inin.analytics.elasticsearch.example;
-
 import java.io.File;
-
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.hadoop.conf.Configuration;
@@ -14,88 +12,72 @@ import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapred.SequenceFileInputFormat;
 import org.apache.hadoop.mapred.TextOutputFormat;
 import org.apache.hadoop.util.Tool;
-
 import com.inin.analytics.elasticsearch.BaseESMapper;
 import com.inin.analytics.elasticsearch.ConfigParams;
 import com.inin.analytics.elasticsearch.IndexingPostProcessor;
 import com.inin.analytics.elasticsearch.ShardConfig;
 
 public class ExampleIndexingJob implements Tool {
+  private static Configuration conf;
 
-	private static Configuration conf;
-	public static int main(String[] args) throws Exception {
-		if(args.length != 9) {
-			System.err.println("Invalid # arguments. EG: loadES [pipe separated input] [snapshot working directory (fs/nfs)] [snapshot final destination (s3/nfs/hdfs)] [snapshot repo name] [elasticsearch working data location] [num reducers] [num shards per index] [num shards per organization] [manifest location]");
-			return -1;
-		}
+  public static int main(String[] args) throws Exception {
+    if (args.length != 9) {
+      System.err.println("Invalid # arguments. EG: loadES [pipe separated input] [snapshot working directory (fs/nfs)] [snapshot final destination (s3/nfs/hdfs)] [snapshot repo name] [elasticsearch working data location] [num reducers] [num shards per index] [num shards per organization] [manifest location]");
+      return -1;
+    }
+    String inputPath = args[0];
+    String snapshotWorkingLocation = args[1];
+    String snapshotFinalDestination = args[2];
+    String snapshotRepoName = args[3];
+    String esWorkingDir = args[4];
+    Integer numReducers = new Integer(args[5]);
+    Long numShardsPerIndex = new Long(args[6]);
+    Long numShardsPerOrganization = new Long(args[7]);
+    String manifestLocation = args[8];
+    snapshotFinalDestination = StringUtils.stripEnd(snapshotFinalDestination, "/");
+    conf = new Configuration();
+    conf.set(ConfigParams.SNAPSHOT_WORKING_LOCATION_CONFIG_KEY.toString(), snapshotWorkingLocation);
+    conf.set(ConfigParams.SNAPSHOT_FINAL_DESTINATION.toString(), snapshotFinalDestination);
+    conf.set(ConfigParams.SNAPSHOT_REPO_NAME_CONFIG_KEY.toString(), snapshotRepoName);
+    conf.set(ConfigParams.ES_WORKING_DIR.toString(), esWorkingDir);
+    conf.set(ConfigParams.NUM_SHARDS_PER_INDEX.toString(), numShardsPerIndex.toString());
+    conf.set(ConfigParams.NUM_SHARDS_PER_ORGANIZATION.toString(), numShardsPerOrganization.toString());
+    JobConf job = new JobConf(conf, ExampleIndexingJob.class);
+    job.setJobName("Elastic Search Offline Index Generator");
+    job.setInputFormat(SequenceFileInputFormat.class);
+    job.setOutputFormat(TextOutputFormat.class);
+    job.setMapperClass(BaseESMapper.class);
+    job.setReducerClass(ExampleIndexingReducerImpl.class);
+    job.setMapOutputValueClass(Text.class);
+    job.setMapOutputKeyClass(Text.class);
+    job.setNumReduceTasks(numReducers);
+    job.setSpeculativeExecution(false);
+    Path jobOutput = new Path(manifestLocation + "/raw/");
+    Path manifestFile = new Path(manifestLocation + "manifest");
+    File outputfile = new File(jobOutput.toUri().toString());
+    if (outputfile.exists()) {
+      FileUtils.deleteDirectory(outputfile);
+    }
+    FileOutputFormat.setOutputPath(job, jobOutput);
+    String[] inputFolders = StringUtils.split(inputPath, "|");
+    for (String input : inputFolders) {
+      FileInputFormat.addInputPath(job, new Path(input));
+    }
+    JobClient.runJob(job);
+    IndexingPostProcessor postProcessor = new IndexingPostProcessor();
+    postProcessor.execute(jobOutput, manifestFile, esWorkingDir, new ShardConfig(numShardsPerIndex, numShardsPerOrganization), conf, ExampleIndexingReducerImpl.class);
+    return 0;
+  }
 
-		String inputPath = args[0];
-		String snapshotWorkingLocation = args[1];
-		String snapshotFinalDestination = args[2];
-		String snapshotRepoName = args[3];
-		String esWorkingDir = args[4];
-		Integer numReducers = new Integer(args[5]);
-		Long numShardsPerIndex = new Long(args[6]);
-		Long numShardsPerOrganization = new Long(args[7]);
-		String manifestLocation = args[8];
+  @Override public void setConf(Configuration conf) {
+    this.conf = conf;
+  }
 
-		// Remove trailing slashes from the destination 
-		snapshotFinalDestination = StringUtils.stripEnd(snapshotFinalDestination, "/");
+  @Override public Configuration getConf() {
+    return conf;
+  }
 
-		conf = new Configuration();
-		conf.set(ConfigParams.SNAPSHOT_WORKING_LOCATION_CONFIG_KEY.toString(), snapshotWorkingLocation);
-		conf.set(ConfigParams.SNAPSHOT_FINAL_DESTINATION.toString(), snapshotFinalDestination);
-		conf.set(ConfigParams.SNAPSHOT_REPO_NAME_CONFIG_KEY.toString(), snapshotRepoName);
-		conf.set(ConfigParams.ES_WORKING_DIR.toString(), esWorkingDir);
-		conf.set(ConfigParams.NUM_SHARDS_PER_INDEX.toString(), numShardsPerIndex.toString());
-		conf.set(ConfigParams.NUM_SHARDS_PER_ORGANIZATION.toString(), numShardsPerOrganization.toString());
-
-		JobConf job = new JobConf(conf, ExampleIndexingJob.class);
-		job.setJobName("Elastic Search Offline Index Generator");
-		job.setInputFormat(SequenceFileInputFormat.class);
-		job.setOutputFormat(TextOutputFormat.class);
-		job.setMapperClass(BaseESMapper.class);
-		job.setReducerClass(ExampleIndexingReducerImpl.class);
-		job.setMapOutputValueClass(Text.class);
-		job.setMapOutputKeyClass(Text.class);
-		job.setNumReduceTasks(numReducers);
-		job.setSpeculativeExecution(false);
-
-		Path jobOutput = new Path(manifestLocation + "/raw/");
-		Path manifestFile = new Path(manifestLocation + "manifest");
-
-		File outputfile = new File(jobOutput.toUri().toString());
-		if (outputfile.exists()) {
-	        FileUtils.deleteDirectory(outputfile);
-		}
-
-		FileOutputFormat.setOutputPath(job, jobOutput);
-
-		// Set up inputs
-		String[]inputFolders = StringUtils.split(inputPath, "|");
-		for(String input : inputFolders) {
-			FileInputFormat.addInputPath(job, new Path(input));
-		}
-
-		JobClient.runJob(job);
-		IndexingPostProcessor postProcessor = new IndexingPostProcessor();
-		postProcessor.execute(jobOutput, manifestFile, esWorkingDir,  new ShardConfig(numShardsPerIndex, numShardsPerOrganization), conf, ExampleIndexingReducerImpl.class);
-		return 0;
-	}
-
-	@Override
-	public void setConf(Configuration conf) {
-		this.conf = conf;
-	}
-
-	@Override
-	public Configuration getConf() {
-		return conf;
-	}
-
-	@Override
-	public int run(String[] args) throws Exception {
-		return ExampleIndexingJob.main(args);
-	}
-
+  @Override public int run(String[] args) throws Exception {
+    return ExampleIndexingJob.main(args);
+  }
 }
