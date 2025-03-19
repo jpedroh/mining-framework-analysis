71786c074f15f965daf6dd6a426eaf8bfaea34a3
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 18:30:55 +0000

    Mining Framework Analysis

--- src/main/java/hudson/tasks/junit/TestResultSummary.java
+++ src/main/java/hudson/tasks/junit/TestResultSummary.java
@@ -1,7 +1,5 @@
 package hudson.tasks.junit;
-
 import org.jenkinsci.plugins.scriptsecurity.sandbox.whitelists.Whitelisted;
-
 import java.io.Serializable;
 import org.kohsuke.accmod.Restricted;
 import org.kohsuke.accmod.restrictions.DoNotUse;
@@ -10,55 +8,51 @@ import org.kohsuke.accmod.restrictions.DoNotUse;
  * Summary of test results that can be used in Pipeline scripts.
  */
 public class TestResultSummary implements Serializable {
-    private int failCount;
-    private int skipCount;
-    private int passCount;
-    private int totalCount;
+  private int failCount;
 
-    @Deprecated
-    @Restricted(DoNotUse.class)
-    public TestResultSummary() {
-    }
+  private int skipCount;
 
-    public TestResultSummary(int failCount, int skipCount, int passCount, int totalCount) {
-        this.failCount = failCount;
-        this.skipCount = skipCount;
-        this.passCount = passCount;
-        this.totalCount = totalCount;
-    }
+  private int passCount;
 
-    public TestResultSummary(TestResult result) {
-        this.failCount = result.getFailCount();
-        this.skipCount = result.getSkipCount();
-        this.passCount = result.getPassCount();
-        this.totalCount = result.getTotalCount();
-        if (totalCount == 0) {
-            for (SuiteResult suite : result.getSuites()) {
-                if (!suite.getCases().isEmpty()) {
-                    throw new IllegalArgumentException("Attempt to construct TestResultSummary from TestResult without calling tally/freeze");
-                }
-            }
+  private int totalCount;
+
+  @Deprecated @Restricted(value = DoNotUse.class) public TestResultSummary() {
+  }
+
+  public TestResultSummary(int failCount, int skipCount, int passCount, int totalCount) {
+    this.failCount = failCount;
+    this.skipCount = skipCount;
+    this.passCount = passCount;
+    this.totalCount = totalCount;
+  }
+
+  public TestResultSummary(TestResult result) {
+    this.failCount = result.getFailCount();
+    this.skipCount = result.getSkipCount();
+    this.passCount = result.getPassCount();
+    this.totalCount = result.getTotalCount();
+    if (totalCount == 0) {
+      for (SuiteResult suite : result.getSuites()) {
+        if (!suite.getCases().isEmpty()) {
+          throw new IllegalArgumentException("Attempt to construct TestResultSummary from TestResult without calling tally/freeze");
         }
+      }
     }
+  }
 
-    @Whitelisted
-    public int getFailCount() {
-        return failCount;
-    }
+  @Whitelisted public int getFailCount() {
+    return failCount;
+  }
 
-    @Whitelisted
-    public int getSkipCount() {
-        return skipCount;
-    }
+  @Whitelisted public int getSkipCount() {
+    return skipCount;
+  }
 
-    @Whitelisted
-    public int getPassCount() {
-        return passCount;
-    }
+  @Whitelisted public int getPassCount() {
+    return passCount;
+  }
 
-    @Whitelisted
-    public int getTotalCount() {
-        return totalCount;
-    }
+  @Whitelisted public int getTotalCount() {
+    return totalCount;
+  }
 }
-
