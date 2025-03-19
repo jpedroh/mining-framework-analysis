d01627ca3b540071fe65e8c5459f67440ce0b552
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 13:12:47 +0000

    Mining Framework Analysis

--- src/main/java/net/openhft/chronicle/threads/internal/EventLoopThreadHolder.java
+++ src/main/java/net/openhft/chronicle/threads/internal/EventLoopThreadHolder.java
@@ -1,76 +1,62 @@
 package net.openhft.chronicle.threads.internal;
-
 import net.openhft.chronicle.core.Jvm;
 import net.openhft.chronicle.threads.CoreEventLoop;
 import net.openhft.chronicle.threads.ThreadHolder;
 
 public class EventLoopThreadHolder implements ThreadHolder {
-    private final CoreEventLoop eventLoop;
-    private final long monitorIntervalNS;
-    private long intervalToAddNS;
-    private long printBlockTimeNS;
+  private final CoreEventLoop eventLoop;
 
-    public EventLoopThreadHolder(long monitorIntervalNS, CoreEventLoop eventLoop) {
-        this.monitorIntervalNS = intervalToAddNS = printBlockTimeNS = monitorIntervalNS;
-        this.eventLoop = eventLoop;
-    }
+  private final long monitorIntervalNS;
 
-    @Override
-    public boolean isAlive() {
-        return eventLoop.isAlive();
-    }
+  private long intervalToAddNS;
 
-    @Override
-    public void reportFinished() {
-        Jvm.warn().on(getClass(), "Monitoring a task which has finished " + eventLoop);
-    }
+  private long printBlockTimeNS;
 
-    @Override
-    public long startedNS() {
-        return eventLoop.loopStartNS();
-    }
+  public EventLoopThreadHolder(long monitorIntervalNS, CoreEventLoop eventLoop) {
+    this.monitorIntervalNS = intervalToAddNS = printBlockTimeNS = monitorIntervalNS;
+    this.eventLoop = eventLoop;
+  }
 
-    @Override
-    public void resetTimers() {
-        intervalToAddNS =
-                printBlockTimeNS = monitorIntervalNS;
-    }
+  @Override public boolean isAlive() {
+    return eventLoop.isAlive();
+  }
 
-    @Override
-    public boolean shouldLog(long nowNS) {
-        long blockingTimeNS = nowNS - startedNS();
-        return blockingTimeNS >= printBlockTimeNS;
-    }
+  @Override public void reportFinished() {
+    Jvm.warn().on(getClass(), "Monitoring a task which has finished " + eventLoop);
+  }
 
-    @Override
-    public void dumpThread(long startedNS, long nowNS) {
-        long blockingTimeNS = nowNS - startedNS;
+  @Override public long startedNS() {
+    return eventLoop.loopStartNS();
+  }
 
-        eventLoop.dumpRunningState(eventLoop.name() + " thread has blocked for "
-                        + blockingTimeNS / 100_000 / 10.0 + " ms.",
-                // check we are still in the loop.
-                () -> eventLoop.loopStartNS() == startedNS);
+  @Override public void resetTimers() {
+    intervalToAddNS = printBlockTimeNS = monitorIntervalNS;
+  }
 
-        printBlockTimeNS += intervalToAddNS;
-        intervalToAddNS = (long) Math.min(1.41d * intervalToAddNS, 20d * monitorIntervalNS);
-    }
+  @Override public boolean shouldLog(long nowNS) {
+    long blockingTimeNS = nowNS - startedNS();
+    return blockingTimeNS >= printBlockTimeNS;
+  }
 
-    @Override
-    public long timingTolerance() {
-        return monitorIntervalNS + timingError();
-    }
+  @Override public void dumpThread(long startedNS, long nowNS) {
+    long blockingTimeNS = nowNS - startedNS;
+    eventLoop.dumpRunningState(eventLoop.name() + " thread has blocked for " + blockingTimeNS / 100_000 / 10.0 + " ms.", () -> eventLoop.loopStartNS() == startedNS);
+    printBlockTimeNS += intervalToAddNS;
+    intervalToAddNS = (long) Math.min(1.41d * intervalToAddNS, 20d * monitorIntervalNS);
+  }
 
-    protected long timingError() {
-        return TIMING_ERROR;
-    }
+  @Override public long timingTolerance() {
+    return monitorIntervalNS + timingError();
+  }
 
-    @Override
-    public String getName() {
-        return eventLoop.name();
-    }
+  protected long timingError() {
+    return TIMING_ERROR;
+  }
 
-    @Override
-    public void monitorThreadDelayed(long actionCallDelayNS) {
-        // report it??
-    }
+  @Override public String getName() {
+    return eventLoop.name();
+  }
+
+  @Override public void monitorThreadDelayed(long actionCallDelayNS) {
+  }
 }
