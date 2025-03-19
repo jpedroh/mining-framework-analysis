c87d984292966c5b1862b082cc9cb65097fe292a
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 18:45:06 +0000

    Mining Framework Analysis

--- src/test/java/org/sqlite/ErrorMessageTest.java
+++ src/test/java/org/sqlite/ErrorMessageTest.java
@@ -1,150 +1,150 @@
 package org.sqlite;
-
 import org.hamcrest.BaseMatcher;
 import org.hamcrest.Description;
 import org.junit.jupiter.api.Test;
-
 import java.io.File;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
-
+import static org.hamcrest.CoreMatchers.anyOf;
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 public class ErrorMessageTest {
-    static class VendorCodeMatcher extends BaseMatcher<Object> {
-        final SQLiteErrorCode expected;
+  static class VendorCodeMatcher extends BaseMatcher<Object> {
+    final SQLiteErrorCode expected;
 
-        VendorCodeMatcher(SQLiteErrorCode expected) {
-            this.expected = expected;
-        }
-
-        public boolean matches(Object o) {
-            if (!(o instanceof SQLException)) {
-                return false;
-            }
-            SQLException e = (SQLException) o;
-            SQLiteErrorCode ec = SQLiteErrorCode.getErrorCode(e.getErrorCode());
-            return ec == expected;
-        }
-
-        public void describeTo(Description description) {
-            description
-                .appendText("SQLException with error code ")
-                .appendText(expected.name())
-                .appendText(" (")
-                .appendValue(expected.code)
-                .appendText(")");
-        }
+    VendorCodeMatcher(SQLiteErrorCode expected) {
+      this.expected = expected;
     }
 
-    static class ResultCodeMatcher extends BaseMatcher<Object> {
-        final SQLiteErrorCode expected;
-
-        ResultCodeMatcher(SQLiteErrorCode expected) {
-            this.expected = expected;
-        }
-
-        public boolean matches(Object o) {
-            if (!(o instanceof SQLiteException)) {
-                return false;
-            }
-            SQLiteException e = (SQLiteException) o;
-            return e.getResultCode() == expected;
-        }
-
-        public void describeTo(Description description) {
-            description
-                .appendText("SQLiteException with error code ")
-                .appendText(expected.name())
-                .appendText(" (")
-                .appendValue(expected.code)
-                .appendText(")");
-        }
+    public boolean matches(Object o) {
+      if (!(o instanceof SQLException)) {
+        return false;
+      }
+      SQLException e = (SQLException) o;
+      SQLiteErrorCode ec = SQLiteErrorCode.getErrorCode(e.getErrorCode());
+      return ec == expected;
     }
 
-    @Test
-    public void moved() throws SQLException, IOException {
-        File from = File.createTempFile("error-message-test-moved-from", ".sqlite");
-        from.deleteOnExit();
+    public void describeTo(Description description) {
+      description.appendText("SQLException with error code ").appendText(expected.name()).appendText(" (").appendValue(expected.code).appendText(")");
+    }
+  }
 
-        Connection conn = DriverManager.getConnection("jdbc:sqlite:" + from.getAbsolutePath());
-        Statement stmt = conn.createStatement();
-        stmt.executeUpdate("create table sample(id, name)");
-        stmt.executeUpdate("insert into sample values(1, \"foo\")");
+  static class ResultCodeMatcher extends BaseMatcher<Object> {
+    final SQLiteErrorCode expected;
 
-        File to = File.createTempFile("error-message-test-moved-from", ".sqlite");
-        assumeTrue(to.delete());
-        assumeTrue(from.renameTo(to));
-
-        Exception exception = assertThrows(SQLException.class, () -> stmt.executeUpdate("insert into sample values(2, \"bar\")"));
-        assertTrue(exception.getMessage().contains("[SQLITE_READONLY_DBMOVED]"));
-
-        stmt.close();
-        conn.close();
+    ResultCodeMatcher(SQLiteErrorCode expected) {
+      this.expected = expected;
     }
 
-    @Test
-    public void writeProtected() throws SQLException, IOException {
-        File file = File.createTempFile("error-message-test-write-protected", ".sqlite");
-        file.deleteOnExit();
-
-        Connection conn = DriverManager.getConnection("jdbc:sqlite:" + file.getAbsolutePath());
-        Statement stmt = conn.createStatement();
-        stmt.executeUpdate("create table sample(id, name)");
-        stmt.executeUpdate("insert into sample values(1, \"foo\")");
-        stmt.close();
-        conn.close();
-
-        assumeTrue(file.setReadOnly());
-
-        conn = DriverManager.getConnection("jdbc:sqlite:" + file.getAbsolutePath());
-        stmt = conn.createStatement();
-        Statement finalStmt = stmt;
-        Exception exception = assertThrows(SQLException.class, () -> finalStmt.executeUpdate("insert into sample values(2, \"bar\")"));
-        assertTrue(exception.getMessage().contains("[SQLITE_READONLY]"));
-        stmt.close();
-        conn.close();
+    public boolean matches(Object o) {
+      if (!(o instanceof SQLiteException)) {
+        return false;
+      }
+      SQLiteException e = (SQLiteException) o;
+      return e.getResultCode() == expected;
     }
 
-    @Test
-    public void cantOpenDir() throws SQLException, IOException {
-        File dir = File.createTempFile("error-message-test-cant-open-dir", "");
-        assumeTrue(dir.delete());
-        assumeTrue(dir.mkdir());
-        dir.deleteOnExit();
-
-        Exception exception = assertThrows(SQLException.class, () -> DriverManager.getConnection("jdbc:sqlite:" + dir.getAbsolutePath()));
-        assertTrue(exception.getMessage().contains("[SQLITE_CANTOPEN]")
-            || exception.getMessage().contains("[SQLITE_CANTOPEN_ISDIR]"));
+    public void describeTo(Description description) {
+      description.appendText("SQLiteException with error code ").appendText(expected.name()).appendText(" (").appendValue(expected.code).appendText(")");
     }
+  }
 
-    @Test
-    public void shouldUsePlainErrorCodeAsVendorCodeAndExtendedAsResultCode() throws SQLException, IOException {
-        File from = File.createTempFile("error-message-test-plain-1", ".sqlite");
-        from.deleteOnExit();
+  @Test public void moved() throws SQLException, IOException {
+    File from = File.createTempFile("error-message-test-moved-from", ".sqlite");
+    from.deleteOnExit();
+    Connection conn = DriverManager.getConnection("jdbc:sqlite:" + from.getAbsolutePath());
+    Statement stmt = conn.createStatement();
+    stmt.executeUpdate("create table sample(id, name)");
+    stmt.executeUpdate("insert into sample values(1, \"foo\")");
+    File to = File.createTempFile("error-message-test-moved-from", ".sqlite");
+    assumeTrue(to.delete());
+    assumeTrue(from.renameTo(to));
+    Exception exception = assertThrows(SQLException.class, () -> stmt.executeUpdate("insert into sample values(2, \"bar\")"));
 
-        Connection conn = DriverManager.getConnection("jdbc:sqlite:" + from.getAbsolutePath());
-        Statement stmt = conn.createStatement();
-        stmt.executeUpdate("create table sample(id, name)");
-        stmt.executeUpdate("insert into sample values(1, \"foo\")");
+<<<<<<< /usr/src/app/output/xerial/sqlite-jdbc/4787e24110c952e1241fe5c44b862906bd40292b/src/test/java/org/sqlite/ErrorMessageTest.java/left.java
+    assertTrue(exception.getMessage().contains("[SQLITE_READONLY_DBMOVED]"))
+=======
+    assertThat(exception.getMessage(), containsString("[SQLITE_READONLY_DBMOVED]"))
+>>>>>>> /usr/src/app/output/xerial/sqlite-jdbc/4787e24110c952e1241fe5c44b862906bd40292b/src/test/java/org/sqlite/ErrorMessageTest.java/right.java
+    ;
+    stmt.close();
+    conn.close();
+  }
 
-        File to = File.createTempFile("error-message-test-plain-2", ".sqlite");
-        assumeTrue(to.delete());
-        assumeTrue(from.renameTo(to));
+  @Test public void writeProtected() throws SQLException, IOException {
+    File file = File.createTempFile("error-message-test-write-protected", ".sqlite");
+    file.deleteOnExit();
+    Connection conn = DriverManager.getConnection("jdbc:sqlite:" + file.getAbsolutePath());
+    Statement stmt = conn.createStatement();
+    stmt.executeUpdate("create table sample(id, name)");
+    stmt.executeUpdate("insert into sample values(1, \"foo\")");
+    stmt.close();
+    conn.close();
+    assumeTrue(file.setReadOnly());
+    conn = DriverManager.getConnection("jdbc:sqlite:" + file.getAbsolutePath());
+    stmt = conn.createStatement();
+    Statement finalStmt = stmt;
+    Exception exception = assertThrows(SQLException.class, () -> finalStmt.executeUpdate("insert into sample values(2, \"bar\")"));
 
-        Exception exception = assertThrows(SQLException.class, () -> stmt.executeUpdate("insert into sample values(2, \"bar\")"));
-        assertTrue(exception.getMessage().contains("[SQLITE_READONLY_DBMOVED]"));
-        assertThat(exception, new VendorCodeMatcher(SQLiteErrorCode.SQLITE_READONLY));
-        assertThat(exception, new ResultCodeMatcher(SQLiteErrorCode.SQLITE_READONLY_DBMOVED));
+<<<<<<< /usr/src/app/output/xerial/sqlite-jdbc/4787e24110c952e1241fe5c44b862906bd40292b/src/test/java/org/sqlite/ErrorMessageTest.java/left.java
+    assertTrue(exception.getMessage().contains("[SQLITE_READONLY]"))
+=======
+    assertThat(exception.getMessage(), containsString("[SQLITE_READONLY]"))
+>>>>>>> /usr/src/app/output/xerial/sqlite-jdbc/4787e24110c952e1241fe5c44b862906bd40292b/src/test/java/org/sqlite/ErrorMessageTest.java/right.java
+    ;
+    stmt.close();
+    conn.close();
+  }
 
+  @Test public void cantOpenDir() throws SQLException, IOException {
+    File dir = File.createTempFile("error-message-test-cant-open-dir", "");
+    assumeTrue(dir.delete());
+    assumeTrue(dir.mkdir());
+    dir.deleteOnExit();
+    Exception exception = assertThrows(SQLException.class, () -> DriverManager.getConnection("jdbc:sqlite:" + dir.getAbsolutePath()));
 
-        stmt.close();
-        conn.close();
-    }
+<<<<<<< /usr/src/app/output/xerial/sqlite-jdbc/4787e24110c952e1241fe5c44b862906bd40292b/src/test/java/org/sqlite/ErrorMessageTest.java/left.java
+    assertTrue(exception.getMessage().contains("[SQLITE_CANTOPEN]") || exception.getMessage().contains("[SQLITE_CANTOPEN_ISDIR]"))
+=======
+    assertThat(exception.getMessage(), anyOf(containsString("[SQLITE_CANTOPEN]"), containsString("[SQLITE_CANTOPEN_ISDIR]")))
+>>>>>>> /usr/src/app/output/xerial/sqlite-jdbc/4787e24110c952e1241fe5c44b862906bd40292b/src/test/java/org/sqlite/ErrorMessageTest.java/right.java
+    ;
+  }
+
+  @Test public void shouldUsePlainErrorCodeAsVendorCodeAndExtendedAsResultCode() throws SQLException, IOException {
+    File from = File.createTempFile("error-message-test-plain-1", ".sqlite");
+    from.deleteOnExit();
+    Connection conn = DriverManager.getConnection("jdbc:sqlite:" + from.getAbsolutePath());
+    Statement stmt = conn.createStatement();
+    stmt.executeUpdate("create table sample(id, name)");
+    stmt.executeUpdate("insert into sample values(1, \"foo\")");
+    File to = File.createTempFile("error-message-test-plain-2", ".sqlite");
+    assumeTrue(to.delete());
+
+<<<<<<< /usr/src/app/output/xerial/sqlite-jdbc/4787e24110c952e1241fe5c44b862906bd40292b/src/test/java/org/sqlite/ErrorMessageTest.java/left.java
+    from.renameTo(to)
+=======
+>>>>>>> Unknown file: This is a bug in JDime.
+    ;
+    Exception exception = assertThrows(SQLException.class, () -> stmt.executeUpdate("insert into sample values(2, \"bar\")"));
+
+<<<<<<< /usr/src/app/output/xerial/sqlite-jdbc/4787e24110c952e1241fe5c44b862906bd40292b/src/test/java/org/sqlite/ErrorMessageTest.java/left.java
+    assertTrue(exception.getMessage().contains("[SQLITE_READONLY_DBMOVED]"))
+=======
+    assertThat(exception.getMessage(), containsString("[SQLITE_READONLY_DBMOVED]"))
+>>>>>>> /usr/src/app/output/xerial/sqlite-jdbc/4787e24110c952e1241fe5c44b862906bd40292b/src/test/java/org/sqlite/ErrorMessageTest.java/right.java
+    ;
+    assertThat(exception, new VendorCodeMatcher(SQLiteErrorCode.SQLITE_READONLY));
+    assertThat(exception, new ResultCodeMatcher(SQLiteErrorCode.SQLITE_READONLY_DBMOVED));
+    stmt.close();
+    conn.close();
+  }
 }
