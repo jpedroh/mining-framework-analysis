c72d8a21ed7687893fb2b26e5e8ece638b670e1c
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 19:44:46 +0000

    Mining Framework Analysis

--- src/main/java/org/jfaster/mango/descriptor/MethodDescriptor.java
+++ src/main/java/org/jfaster/mango/descriptor/MethodDescriptor.java
@@ -1,21 +1,4 @@
-/*
- * Copyright 2014 mango.jfaster.org
- *
- * The Mango Project licenses this file to you under the Apache License,
- * version 2.0 (the "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at:
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations
- * under the License.
- */
-
 package org.jfaster.mango.descriptor;
-
 import org.jfaster.mango.annotation.*;
 import org.jfaster.mango.exception.DescriptionException;
 import org.jfaster.mango.util.Joiner;
@@ -23,7 +6,6 @@ import org.jfaster.mango.util.Strings;
 import org.jfaster.mango.util.logging.InternalLogger;
 import org.jfaster.mango.util.logging.InternalLoggerFactory;
 import org.jfaster.mango.util.reflect.Reflection;
-
 import javax.annotation.Nullable;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
@@ -37,28 +19,26 @@ import java.util.List;
  * @author ash
  */
 public class MethodDescriptor {
-
   private final static InternalLogger logger = InternalLoggerFactory.getInstance(MethodDescriptor.class);
 
   private final String name;
+
   private final Class<?> daoClass;
+
   private final ReturnDescriptor returnDescriptor;
+
   private final List<ParameterDescriptor> parameterDescriptors;
 
   private String cachedSQL;
 
-  private MethodDescriptor(
-      String name, Class<?> daoClass, ReturnDescriptor returnDescriptor,
-      List<ParameterDescriptor> parameterDescriptors) {
+  private MethodDescriptor(String name, Class<?> daoClass, ReturnDescriptor returnDescriptor, List<ParameterDescriptor> parameterDescriptors) {
     this.name = name;
     this.daoClass = daoClass;
     this.returnDescriptor = returnDescriptor;
     this.parameterDescriptors = Collections.unmodifiableList(parameterDescriptors);
   }
 
-  public static MethodDescriptor create(
-      String name, Class<?> daoClass, ReturnDescriptor returnDescriptor,
-      List<ParameterDescriptor> parameterDescriptors) {
+  public static MethodDescriptor create(String name, Class<?> daoClass, ReturnDescriptor returnDescriptor, List<ParameterDescriptor> parameterDescriptors) {
     return new MethodDescriptor(name, daoClass, returnDescriptor, parameterDescriptors);
   }
 
@@ -74,8 +54,7 @@ public class MethodDescriptor {
     return getAnnotation(annotationType) != null;
   }
 
-  @Nullable
-  public <A extends Annotation> A getAnnotation(Class<A> annotationType) {
+  @Nullable public <A extends Annotation> A getAnnotation(Class<A> annotationType) {
     return returnDescriptor.getAnnotation(annotationType);
   }
 
@@ -123,19 +102,16 @@ public class MethodDescriptor {
       throw new DescriptionException("sql is null or empty");
     }
     if (logger.isDebugEnabled()) {
-      // TODO 补全日志
       logger.debug(sql);
     }
     cachedSQL = sql;
     return cachedSQL;
   }
 
-  @Nullable
-  public String getGlobalTable() {
+  @Nullable public String getGlobalTable() {
     DB dbAnno = getAnnotation(DB.class);
     if (dbAnno == null) {
-      throw new DescriptionException("dao interface expected one @DB " +
-          "annotation but not found");
+      throw new DescriptionException("dao interface expected one @DB " + "annotation but not found");
     }
     String table = null;
     if (Strings.isNotEmpty(dbAnno.table())) {
@@ -147,14 +123,12 @@ public class MethodDescriptor {
   public String getDataSourceFactoryName() {
     DB dbAnno = getAnnotation(DB.class);
     if (dbAnno == null) {
-      throw new DescriptionException("dao interface expected one @DB " +
-          "annotation but not found");
+      throw new DescriptionException("dao interface expected one @DB " + "annotation but not found");
     }
     return dbAnno.name();
   }
 
-  @Nullable
-  public Sharding getShardingAnno() {
+  @Nullable public Sharding getShardingAnno() {
     return getAnnotation(Sharding.class);
   }
 
@@ -165,8 +139,6 @@ public class MethodDescriptor {
   }
 
   public boolean isReturnGeneratedId() {
-    return isAnnotationPresent(ReturnGeneratedId.class) ||
-        (name != null && name.contains("ReturnGeneratedId"));
+    return isAnnotationPresent(ReturnGeneratedId.class) || (name != null && name.contains("ReturnGeneratedId"));
   }
-
 }
--- src/main/java/org/jfaster/mango/plugin/stats/ExtendStat.java
+++ src/main/java/org/jfaster/mango/plugin/stats/ExtendStat.java
@@ -1,30 +1,11 @@
-/*
- * Copyright 2014 mango.jfaster.org
- *
- * The Mango Project licenses this file to you under the Apache License,
- * version 2.0 (the "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at:
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations
- * under the License.
- */
-
 package org.jfaster.mango.plugin.stats;
-
 import org.jfaster.mango.annotation.DB;
-import org.jfaster.mango.annotation.SQL;
 import org.jfaster.mango.annotation.Sharding;
 import org.jfaster.mango.sharding.NotUseTableShardingStrategy;
 import org.jfaster.mango.stat.OperatorStat;
 import org.jfaster.mango.util.Joiner;
 import org.jfaster.mango.util.Strings;
 import org.jfaster.mango.util.ToStringHelper;
-
 import java.lang.reflect.Method;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
@@ -35,7 +16,6 @@ import java.util.List;
  * @author ash
  */
 public class ExtendStat {
-
   private OperatorStat operatorStat;
 
   private Method method;
@@ -54,16 +34,21 @@ public class ExtendStat {
   }
 
   public String getSql() {
-    String sql = Joiner.on(' ').join(Arrays.asList(method.getAnnotation(SQL.class).value()));
-    DB dbAnno = method.getDeclaringClass().getAnnotation(DB.class);
+    String sql = 
+<<<<<<< /usr/src/app/output/jfaster/mango/6c28ffc629752987623e939e30b2ae66e1952a2f/src/main/java/org/jfaster/mango/plugin/stats/ExtendStat.java/left.java
+    Joiner.on(' ').join(Arrays.asList(method.getAnnotation(SQL.class).value()))
+=======
+    operatorStat.getSql()
+>>>>>>> /usr/src/app/output/jfaster/mango/6c28ffc629752987623e939e30b2ae66e1952a2f/src/main/java/org/jfaster/mango/plugin/stats/ExtendStat.java/right.java
+    ;
+    DB dbAnno = operatorStat.getDaoClass().getAnnotation(DB.class);
     String table = dbAnno.table();
     if (Strings.isNotEmpty(table)) {
       Sharding shardingAnno = method.getAnnotation(Sharding.class);
       if (shardingAnno == null) {
         shardingAnno = operatorStat.getDaoClass().getAnnotation(Sharding.class);
       }
-      if (shardingAnno != null &&
-          !NotUseTableShardingStrategy.class.equals(shardingAnno.tableShardingStrategy())) {
+      if (shardingAnno != null && !NotUseTableShardingStrategy.class.equals(shardingAnno.tableShardingStrategy())) {
         table = table + "_#";
       }
       sql = sql.replaceAll("#table", table);
@@ -82,5 +67,4 @@ public class ExtendStat {
   public String getType() {
     return operatorStat.getOperatorType().name().toLowerCase();
   }
-
 }
