b30b467114583ad0348634b9cc9181d8a41be9a7
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 18:16:55 +0000

    Mining Framework Analysis

--- bob-data/src/main/java/com/dnastack/bob/processor/BeaconizerBeaconProcessor.java
+++ bob-data/src/main/java/com/dnastack/bob/processor/BeaconizerBeaconProcessor.java
@@ -1,34 +1,7 @@
-/*
- * The MIT License
- *
- * Copyright 2014 Miroslav Cupak (mirocupak@gmail.com).
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
 package com.dnastack.bob.processor;
-
 import com.dnastack.bob.persistence.entity.Beacon;
 import com.dnastack.bob.persistence.entity.Query;
 import com.dnastack.bob.persistence.enumerated.Reference;
-import com.dnastack.bob.util.HttpUtils;
-import com.dnastack.bob.util.ParsingUtils;
-import com.dnastack.bob.util.QueryUtils;
 import com.google.common.collect.ImmutableSet;
 import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
@@ -37,7 +10,6 @@ import java.util.concurrent.Future;
 import javax.ejb.AsyncResult;
 import javax.ejb.Asynchronous;
 import org.apache.http.client.methods.HttpRequestBase;
-
 import static com.dnastack.bob.util.HttpUtils.createRequest;
 import static com.dnastack.bob.util.HttpUtils.executeRequest;
 import static com.dnastack.bob.util.ParsingUtils.parseBooleanFromJson;
@@ -49,47 +21,36 @@ import static com.dnastack.bob.util.ParsingUtils.parseBooleanFromJson;
  * @version 1.0
  */
 public abstract class BeaconizerBeaconProcessor extends AbstractBeaconProcessor {
+  private static final long serialVersionUID = 112L;
 
-    private static final long serialVersionUID = 112L;
-    private static final Set<Reference> SUPPORTED_REFS = ImmutableSet.of(Reference.HG19);
+  private static final Set<Reference> SUPPORTED_REFS = ImmutableSet.of(Reference.HG19);
 
-    protected abstract String getParamTemplate();
+  protected abstract String getParamTemplate();
 
-    protected abstract String getBaseUrl();
+  protected abstract String getBaseUrl();
 
-    private String getQueryUrl(String beacon, String chrom, Long pos, String allele) throws MalformedURLException {
-        String params = String.format(getParamTemplate(), beacon, chrom, pos, allele);
+  private String getQueryUrl(String beacon, String chrom, Long pos, String allele) throws MalformedURLException {
+    String params = String.format(getParamTemplate(), beacon, chrom, pos, allele);
+    return getBaseUrl() + params;
+  }
 
-        return getBaseUrl() + params;
+  @Override @Asynchronous public Future<String> getQueryResponse(Beacon beacon, Query query) {
+    String res = null;
+    try {
+      HttpRequestBase request = createRequest(getQueryUrl(beacon.getId(), query.getChromosome().toString(), query.getPosition(), query.getAllele()), false, null);
+      request.setHeader("Accept", "application/json");
+      res = executeRequest(request);
+    } catch (MalformedURLException | UnsupportedEncodingException ex) {
     }
+    return new AsyncResult<>(res);
+  }
 
-    @Override
-    @Asynchronous
-    public Future<String> getQueryResponse(Beacon beacon, Query query) {
-        String res = null;
+  @Override @Asynchronous public Future<Boolean> parseQueryResponse(Beacon b, String response) {
+    Boolean res = parseBooleanFromJson(response, "exists");
+    return new AsyncResult<>(res);
+  }
 
-        // should be POST, but the server accepts GET as well
-        try {
-            HttpRequestBase request = createRequest(getQueryUrl(beacon.getId(), query.getChromosome().toString(), query.getPosition(), query.getAllele()), false, null);
-            request.setHeader("Accept", "application/json");
-            res = executeRequest(request);
-        } catch (MalformedURLException | UnsupportedEncodingException ex) {
-            // ignore, already null
-        }
-
-        return new AsyncResult<>(res);
-    }
-
-    @Override
-    @Asynchronous
-    public Future<Boolean> parseQueryResponse(Beacon b, String response) {
-        Boolean res = parseBooleanFromJson(response, "exists");
-
-        return new AsyncResult<>(res);
-    }
-
-    @Override
-    public Set<Reference> getSupportedReferences() {
-        return SUPPORTED_REFS;
-    }
+  @Override public Set<Reference> getSupportedReferences() {
+    return SUPPORTED_REFS;
+  }
 }
