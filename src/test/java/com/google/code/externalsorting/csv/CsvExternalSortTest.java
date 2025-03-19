81f2e89a21fa47721f1a4b1ee9adcbfbeaf09166
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 22:55:50 +0000

    Mining Framework Analysis

--- src/test/java/com/google/code/externalsorting/csv/CsvExternalSortTest.java
+++ src/test/java/com/google/code/externalsorting/csv/CsvExternalSortTest.java
@@ -1,7 +1,11 @@
 package com.google.code.externalsorting.csv;
-
+import org.apache.commons.csv.CSVFormat;
+import static org.junit.Assert.assertEquals;
+import org.apache.commons.csv.CSVRecord;
 import java.io.BufferedReader;
+import org.junit.After;
 import java.io.File;
+import org.junit.Test;
 import java.io.FileReader;
 import java.io.IOException;
 import java.lang.reflect.Field;
@@ -10,186 +14,124 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
-
-import static org.junit.Assert.assertEquals;
-
+import java.util.Map;
 
 public class CsvExternalSortTest {
-	private static final String FILE_CSV = "externalSorting.csv";
-	private static final String FILE_UNICODE_CSV = "nonLatinSorting.csv";
+  private static final String FILE_CSV = "externalSorting.csv";
 
-	private static final String FILE_CSV_WITH_TABS = "externalSortingTabs.csv";
-	private static final String FILE_CSV_WITH_SEMICOOLONS = "externalSortingSemicolon.csv";
-	private static final char SEMICOLON = ';';
+  private static final String FILE_UNICODE_CSV = "nonLatinSorting.csv";
 
-	File outputfile;
+  private static final String FILE_CSV_WITH_TABS = "externalSortingTabs.csv";
 
-	@Test
-	public void testMultiLineFile() throws IOException, ClassNotFoundException {
-		String path = this.getClass().getClassLoader().getResource(FILE_CSV).getPath();
-		
-		File file = new File(path);
-		
-		outputfile = new File("outputSort1.csv");
-		
-		Comparator<CSVRecord> comparator = (op1, op2) -> op1.get(0)
-				.compareTo(op2.get(0));
+  private static final String FILE_CSV_WITH_SEMICOOLONS = "externalSortingSemicolon.csv";
 
-		CsvSortOptions sortOptions = new CsvSortOptions
-				.Builder(CsvExternalSort.DEFAULTMAXTEMPFILES, comparator, 1, CsvExternalSort.estimateAvailableMemory())
-				.charset(Charset.defaultCharset())
-				.distinct(false)
-				.numHeader(1, CSVFormat.DEFAULT)
-				.build();
+  private static final char SEMICOLON = ';';
 
-		List<File> sortInBatch = CsvExternalSort.sortInBatch(file, null, sortOptions);
-		
-		assertEquals(sortInBatch.size(), 1);
-		
-		int mergeSortedFiles = CsvExternalSort.mergeSortedFiles(sortInBatch, outputfile, sortOptions, true, CSVFormat.DEFAULT);
-		
-		assertEquals(mergeSortedFiles, 4);
-		
-		BufferedReader reader = new BufferedReader(new FileReader(outputfile));
-		String readLine = reader.readLine();
+  File outputfile;
 
-		assertEquals(readLine, "6,this wont work in other systems,3");
-		reader.close();
-	}
+  @Test public void testMultiLineFile() throws IOException, ClassNotFoundException {
+    String path = this.getClass().getClassLoader().getResource(FILE_CSV).getPath();
+    File file = new File(path);
+    outputfile = new File("outputSort1.csv");
+    Comparator<CSVRecord> comparator = (op1, op2) -> op1.get(0).compareTo(op2.get(0));
+    CsvSortOptions sortOptions = new CsvSortOptions.Builder(CsvExternalSort.DEFAULTMAXTEMPFILES, comparator, 1, CsvExternalSort.estimateAvailableMemory()).charset(Charset.defaultCharset()).distinct(false).numHeader(1).build();
+    List<File> sortInBatch = CsvExternalSort.sortInBatch(file, null, sortOptions, CSVFormat.DEFAULT);
+    assertEquals(sortInBatch.size(), 1);
+    int mergeSortedFiles = CsvExternalSort.mergeSortedFiles(sortInBatch, outputfile, sortOptions, true, CSVFormat.DEFAULT);
+    assertEquals(mergeSortedFiles, 4);
+    BufferedReader reader = new BufferedReader(new FileReader(outputfile));
+    String readLine = reader.readLine();
+    assertEquals(readLine, "6,this wont work in other systems,3");
+    reader.close();
+  }
 
-	@Test
-	public void testNonLatin() throws Exception {
-		Field cs = Charset.class.getDeclaredField("defaultCharset");
-		cs.setAccessible(true);
-		cs.set(null, Charset.forName("windows-1251"));
+  @Test public void testNonLatin() throws Exception {
+    Field cs = Charset.class.getDeclaredField("defaultCharset");
+    cs.setAccessible(true);
+    cs.set(null, Charset.forName("windows-1251"));
+    String path = this.getClass().getClassLoader().getResource(FILE_UNICODE_CSV).getPath();
+    File file = new File(path);
+    outputfile = new File("unicode_output.csv");
+    Comparator<CSVRecord> comparator = (op1, op2) -> op1.get(0).compareTo(op2.get(0));
+    CsvSortOptions sortOptions = new CsvSortOptions.Builder(CsvExternalSort.DEFAULTMAXTEMPFILES, comparator, 1, CsvExternalSort.estimateAvailableMemory()).charset(StandardCharsets.UTF_8).distinct(false).numHeader(1).build();
+    List<File> sortInBatch = CsvExternalSort.sortInBatch(file, null, sortOptions, CSVFormat.DEFAULT);
+    assertEquals(sortInBatch.size(), 1);
+    int mergeSortedFiles = CsvExternalSort.mergeSortedFiles(sortInBatch, outputfile, sortOptions, true, CSVFormat.DEFAULT);
+    assertEquals(mergeSortedFiles, 5);
+    List<String> lines = Files.readAllLines(Paths.get(outputfile.getPath()), StandardCharsets.UTF_8);
+    assertEquals(lines.get(0), "2,\u05d6\u05d4 \u05e8\u05e7 \u05d8\u05e7\u05e1\u05d8 \u05d0\u05d7\u05d9 \u05dc\u05e7\u05e8\u05d9\u05d0\u05d4 \u05e7\u05e9\u05d4,8");
+    assertEquals(lines.get(1), "5,\u0647\u0630\u0627 \u0647\u0648 \u0627\u0644\u0646\u0635 \u0625\u062e\u0648\u0627\u0646\u0647 \u0641\u0642\u0637 \u0645\u0646 \u0627\u0644\u0635\u0639\u0628 \u0627\u0644\u0642\u0631\u0627\u0621\u0629,3");
+    assertEquals(lines.get(2), "6,\u044d\u0442\u043e \u043d\u0435 \u0431\u0443\u0434\u0435\u0442 \u0440\u0430\u0431\u043e\u0442\u0430\u0442\u044c \u0432 \u0434\u0440\u0443\u0433\u0438\u0445 \u0441\u0438\u0441\u0442\u0435\u043c\u0430\u0445,3");
+  }
 
-		String path = this.getClass().getClassLoader().getResource(FILE_UNICODE_CSV).getPath();
+  @Test public void testMultiLineFileWthHeader() throws IOException, ClassNotFoundException {
+    String path = this.getClass().getClassLoader().getResource(FILE_CSV).getPath();
+    File file = new File(path);
+    outputfile = new File("outputSort1.csv");
+    Comparator<CSVRecord> comparator = (op1, op2) -> op1.get(0).compareTo(op2.get(0));
+    CsvSortOptions sortOptions = new CsvSortOptions.Builder(CsvExternalSort.DEFAULTMAXTEMPFILES, comparator, 1, CsvExternalSort.estimateAvailableMemory()).charset(Charset.defaultCharset()).distinct(false).numHeader(1).skipHeader(false).build();
+    List<File> sortInBatch = CsvExternalSort.sortInBatch(file, null, sortOptions);
+    assertEquals(sortInBatch.size(), 1);
+    int mergeSortedFiles = CsvExternalSort.mergeSortedFiles(sortInBatch, outputfile, sortOptions, true);
+    assertEquals(mergeSortedFiles, 5);
+    List<String> lines = Files.readAllLines(outputfile.toPath(), sortOptions.getCharset());
+    assertEquals(lines.get(0), "personId,text,ishired");
+    assertEquals(lines.get(1), "6,this wont work in other systems,3");
+  }
 
-		File file = new File(path);
+  @Test public void testCVSFormat() throws Exception {
+    Map<CSVFormat, Pair> map = new HashMap<CSVFormat, Pair>() {
+      {
+        put(CSVFormat.MYSQL, new Pair(FILE_CSV_WITH_TABS, "6   \"this wont work in other systems\"   3"));
+        put(CSVFormat.EXCEL.withDelimiter(SEMICOLON), new Pair(FILE_CSV_WITH_SEMICOOLONS, "6;this wont work in other systems;3"));
+      }
+    };
+    for (Map.Entry<CSVFormat, Pair> format : map.entrySet()) {
+      String path = this.getClass().getClassLoader().getResource(format.getValue().getFileName()).getPath();
+      File file = new File(path);
+      outputfile = new File("outputSort1.csv");
+      Comparator<CSVRecord> comparator = (op1, op2) -> op1.get(0).compareTo(op2.get(0));
+      List<File> sortInBatch = CsvExternalSort.sortInBatch(file, comparator, CsvExternalSort.DEFAULTMAXTEMPFILES, Charset.defaultCharset(), null, false, 1, format.getKey());
+      assertEquals(sortInBatch.size(), 1);
+      int mergeSortedFiles = CsvExternalSort.mergeSortedFiles(sortInBatch, outputfile, comparator, Charset.defaultCharset(), false, false, format.getKey());
+      assertEquals(mergeSortedFiles, 4);
+      List<String> lines = Files.readAllLines(outputfile.toPath());
+      assertEquals(format.getValue().getExpected(), lines.get(0));
+    }
+  }
 
-		outputfile = new File("unicode_output.csv");
+  @After public void onTearDown() {
+    if (outputfile.exists()) {
+      outputfile.delete();
+    }
+  }
 
-		Comparator<CSVRecord> comparator = (op1, op2) -> op1.get(0)
-				.compareTo(op2.get(0));
+  private class Pair {
+    private String fileName;
 
-		CsvSortOptions sortOptions = new CsvSortOptions
-				.Builder(CsvExternalSort.DEFAULTMAXTEMPFILES, comparator, 1, CsvExternalSort.estimateAvailableMemory())
-				.charset(StandardCharsets.UTF_8)
-				.distinct(false)
-				.numHeader(1, CSVFormat.DEFAULT)
-				.build();
+    private String expected;
 
-		List<File> sortInBatch = CsvExternalSort.sortInBatch(file, null, sortOptions);
+    public Pair(String fileName, String expected) {
+      this.fileName = fileName;
+      this.expected = expected;
+    }
 
-		assertEquals(sortInBatch.size(), 1);
+    public String getFileName() {
+      return fileName;
+    }
 
-		int mergeSortedFiles = CsvExternalSort.mergeSortedFiles(sortInBatch, outputfile, sortOptions, true, CSVFormat.DEFAULT);
+    public void setFileName(String fileName) {
+      this.fileName = fileName;
+    }
 
-		assertEquals(mergeSortedFiles, 5);
+    public String getExpected() {
+      return expected;
+    }
 
-		List<String> lines = Files.readAllLines(Paths.get(outputfile.getPath()), StandardCharsets.UTF_8);
-
-		assertEquals(lines.get(0), "2,זה רק טקסט אחי לקריאה קשה,8");
-		assertEquals(lines.get(1), "5,هذا هو النص إخوانه فقط من الصعب القراءة,3");
-		assertEquals(lines.get(2), "6,это не будет работать в других системах,3");
-	}
-
-
-	@Test
-	public void testCVSFormat() throws Exception {
-		Map<CSVFormat, Pair> map = new HashMap<CSVFormat, Pair>(){{
-			put(CSVFormat.MYSQL, new Pair(FILE_CSV_WITH_TABS, "6   \"this wont work in other systems\"   3"));
-			put(CSVFormat.EXCEL.withDelimiter(SEMICOLON), new Pair(FILE_CSV_WITH_SEMICOOLONS, "6;this wont work in other systems;3"));
-		}};
-
-		for (Map.Entry<CSVFormat, Pair> format : map.entrySet()){
-			String path = this.getClass().getClassLoader().getResource(format.getValue().getFileName()).getPath();
-
-			File file = new File(path);
-
-			outputfile = new File("outputSort1.csv");
-
-			Comparator<CSVRecord> comparator = (op1, op2) -> op1.get(0)
-					.compareTo(op2.get(0));
-
-			List<File> sortInBatch = CsvExternalSort.sortInBatch(file, comparator, CsvExternalSort.DEFAULTMAXTEMPFILES, Charset.defaultCharset(), null, false, 1, format.getKey());
-
-			assertEquals(sortInBatch.size(), 1);
-
-			int mergeSortedFiles = CsvExternalSort.mergeSortedFiles(sortInBatch, outputfile, comparator, Charset.defaultCharset(), false, false, format.getKey());
-
-			assertEquals(mergeSortedFiles, 4);
-
-			List<String> lines = Files.readAllLines(outputfile.toPath());
-
-			assertEquals(format.getValue().getExpected(), lines.get(0));
-		}
-	}
-
-	@Test
-	public void testMultiLineFileWthHeader() throws IOException, ClassNotFoundException {
-		String path = this.getClass().getClassLoader().getResource(FILE_CSV).getPath();
-
-		File file = new File(path);
-
-		outputfile = new File("outputSort1.csv");
-
-		Comparator<CSVRecord> comparator = (op1, op2) -> op1.get(0)
-				.compareTo(op2.get(0));
-
-		CsvSortOptions sortOptions = new CsvSortOptions
-				.Builder(CsvExternalSort.DEFAULTMAXTEMPFILES, comparator, 1, CsvExternalSort.estimateAvailableMemory())
-				.charset(Charset.defaultCharset())
-				.distinct(false)
-				.numHeader(1)
-				.skipHeader(false)
-				.build();
-
-		List<File> sortInBatch = CsvExternalSort.sortInBatch(file, null, sortOptions);
-
-		assertEquals(sortInBatch.size(), 1);
-
-		int mergeSortedFiles = CsvExternalSort.mergeSortedFiles(sortInBatch, outputfile, sortOptions, true);
-
-		assertEquals(mergeSortedFiles, 5);
-
-		List<String> lines = Files.readAllLines(outputfile.toPath(), sortOptions.getCharset());
-
-		assertEquals(lines.get(0), "personId,text,ishired");
-		assertEquals(lines.get(1), "6,this wont work in other systems,3");
-	}
-
-	@After
-	public void onTearDown() {
-		if(outputfile.exists()) {
-			outputfile.delete();
-		}
-	}
-
-	private class Pair {
-		private String fileName;
-		private String expected;
-
-		public Pair(String fileName, String expected) {
-			this.fileName = fileName;
-			this.expected = expected;
-		}
-
-		public String getFileName() {
-			return fileName;
-		}
-
-		public void setFileName(String fileName) {
-			this.fileName = fileName;
-		}
-
-		public String getExpected() {
-			return expected;
-		}
-
-		public void setExpected(String expected) {
-			this.expected = expected;
-		}
-	}
+    public void setExpected(String expected) {
+      this.expected = expected;
+    }
+  }
 }
