c7ba67ec2cccf4b620efe2056fdeafed7a019d04
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 13:08:55 +0000

    Mining Framework Analysis

--- core/src/main/java/tech/tablesaw/columns/times/TimeColumnType.java
+++ core/src/main/java/tech/tablesaw/columns/times/TimeColumnType.java
@@ -1,5 +1,4 @@
 package tech.tablesaw.columns.times;
-
 import it.unimi.dsi.fastutil.ints.IntArrayList;
 import tech.tablesaw.api.ColumnType;
 import tech.tablesaw.api.Row;
@@ -8,58 +7,49 @@ import tech.tablesaw.columns.AbstractColumnType;
 import tech.tablesaw.columns.Column;
 import tech.tablesaw.columns.StringParser;
 import tech.tablesaw.io.csv.CsvReadOptions;
-
 import java.time.LocalTime;
 
 public class TimeColumnType extends AbstractColumnType<LocalTime> {
+  public static final TimeStringParser DEFAULT_PARSER = new TimeStringParser(ColumnType.LOCAL_TIME);
 
-    public static final TimeStringParser DEFAULT_PARSER = new TimeStringParser(ColumnType.LOCAL_TIME);
-    public static final TimeColumnType INSTANCE =
-            new TimeColumnType(Integer.MIN_VALUE, 4, "LOCAL_TIME", "Time");
+  public static final TimeColumnType INSTANCE = new TimeColumnType(Integer.MIN_VALUE, 4, "LOCAL_TIME", "Time");
 
-    private TimeColumnType(Comparable<?> missingValue, int byteSize, String name, String printerFriendlyName) {
-        super(missingValue, byteSize, name, printerFriendlyName);
+  private TimeColumnType(Comparable<?> missingValue, int byteSize, String name, String printerFriendlyName) {
+    super(missingValue, byteSize, name, printerFriendlyName);
+  }
+
+  @Override public TimeColumn create(String name) {
+    return TimeColumn.create(name);
+  }
+
+  @Override public StringParser<LocalTime> defaultParser() {
+    return new TimeStringParser(this);
+  }
+
+  @Override public StringParser<LocalTime> customParser(CsvReadOptions options) {
+    return new TimeStringParser(this, options);
+  }
+
+  @Override public void copy(IntArrayList rows, Column<LocalTime> oldColumn, Column<LocalTime> newColumn) {
+    TimeColumn oldTime = (TimeColumn) oldColumn;
+    TimeColumn newTime = (TimeColumn) newColumn;
+    for (int index : rows) {
+      newTime.appendInternal(oldTime.getIntInternal(index));
     }
+  }
 
-    @Override
-    public TimeColumn create(String name) {
-        return TimeColumn.create(name);
+  @Override public void copyFromRows(IntArrayList rows, Column<LocalTime> newColumn, Row row) {
+    TimeColumn newTime = (TimeColumn) newColumn;
+    for (int index : rows) {
+      row.at(index);
+      int packedTime = row.getPackedTime(newColumn.name());
+      newTime.appendInternal(packedTime);
     }
+  }
 
-    @Override
-    public StringParser<LocalTime> defaultParser() {
-        return new TimeStringParser(this);
-    }
-
-    @Override
-    public StringParser<LocalTime> customParser(CsvReadOptions options) {
-        return new TimeStringParser(this, options);
-    }
-
-    @Override
-    public void copy(IntArrayList rows, Column<LocalTime> oldColumn, Column<LocalTime> newColumn) {
-        TimeColumn oldTime = (TimeColumn) oldColumn;
-        TimeColumn newTime = (TimeColumn) newColumn;
-        for (int index : rows) {
-            newTime.appendInternal(oldTime.getIntInternal(index));
-        }
-    }
-
-    @Override
-    public void copyFromRows(IntArrayList rows, Column<LocalTime> newColumn, Row row) {
-        TimeColumn newTime = (TimeColumn) newColumn;
-        for (int index : rows) {
-            row.at(index);
-            int packedTime = row.getPackedTime(newColumn.name());
-            newTime.appendInternal(packedTime);
-        }
-    }
-
-    @Override
-    public boolean compare(int rowNumber, Column<LocalTime> temp, Column<LocalTime> original) {
-        TimeColumn tempTime = (TimeColumn) temp;
-        TimeColumn originalTime = (TimeColumn) original;
-        return originalTime.getIntInternal(rowNumber) == tempTime.getIntInternal(tempTime.size() - 1);
-    }
-
+  @Override public boolean compare(int rowNumber, Column<LocalTime> temp, Column<LocalTime> original) {
+    TimeColumn tempTime = (TimeColumn) temp;
+    TimeColumn originalTime = (TimeColumn) original;
+    return originalTime.getIntInternal(rowNumber) == tempTime.getIntInternal(tempTime.size() - 1);
+  }
 }
