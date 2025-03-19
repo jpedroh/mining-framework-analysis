12871147866acc90d521c4211aba716d3c782a5c
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 13:29:53 +0000

    Mining Framework Analysis

--- src/main/java/net/openhft/chronicle/bytes/BytesUtil.java
+++ src/main/java/net/openhft/chronicle/bytes/BytesUtil.java
@@ -1,24 +1,6 @@
-/*
- *     Copyright (C) 2015  higherfrequencytrading.com
- *
- *     This program is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU Lesser General Public License as published by
- *     the Free Software Foundation, either version 3 of the License.
- *
- *     This program is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU Lesser General Public License for more details.
- *
- *     You should have received a copy of the GNU Lesser General Public License
- *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 package net.openhft.chronicle.bytes;
-
 import net.openhft.chronicle.core.annotation.NotNull;
 import net.openhft.chronicle.core.util.StringUtils;
-
 import java.nio.BufferUnderflowException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
@@ -26,75 +8,77 @@ import java.nio.charset.StandardCharsets;
 
 /**
  * Created by peter on 30/08/15.
- */
-public enum BytesUtil {
-    ;
+ */public enum BytesUtil {
 
-    public static boolean bytesEqual(
-            @NotNull RandomDataInput a, long offset,
-            @NotNull RandomDataInput second, long secondOffset, long len)
-            throws IORuntimeException, BufferUnderflowException {
-        long i = 0;
-        while (len - i >= 8L) {
-            if (a.readLong(offset + i) != second.readLong(secondOffset + i))
-                return false;
-            i += 8L;
-        }
-        if (len - i >= 4L) {
-            if (a.readInt(offset + i) != second.readInt(secondOffset + i))
-                return false;
-            i += 4L;
-        }
-        if (len - i >= 2L) {
-            if (a.readShort(offset + i) != second.readShort(secondOffset + i))
-                return false;
-            i += 2L;
-        }
-        if (i < len)
-            if (a.readByte(offset + i) != second.readByte(secondOffset + i))
-                return false;
-        return true;
+  ;
+
+  public static boolean bytesEqual(@NotNull RandomDataInput a, long offset, @NotNull RandomDataInput second, long secondOffset, long len) throws IORuntimeException, BufferUnderflowException {
+    long i = 0;
+    while (len - i >= 8L) {
+      if (a.readLong(offset + i) != second.readLong(secondOffset + i)) {
+        return false;
+      }
+      i += 8L;
     }
-
-    public static boolean equals(Object o1, Object o2) {
-        if (o1 == o2) return true;
-        if (o1 instanceof CharSequence && o2 instanceof CharSequence)
-            return StringUtils.isEqual((CharSequence) o1, (CharSequence) o2);
-        return o1 != null && o1.equals(o2);
+    if (len - i >= 4L) {
+      if (a.readInt(offset + i) != second.readInt(secondOffset + i)) {
+        return false;
+      }
+      i += 4L;
     }
-
-    public static int asInt(@NotNull String str) {
-        ByteBuffer bb = ByteBuffer.wrap(str.getBytes(StandardCharsets.ISO_8859_1)).order(ByteOrder.nativeOrder());
-        return bb.getInt();
+    if (len - i >= 2L) {
+      if (a.readShort(offset + i) != second.readShort(secondOffset + i)) {
+        return false;
+      }
+      i += 2L;
     }
-
-    public static int stopBitLength(long n) {
-        if ((n & ~0x7F) == 0) {
-            return 1;
-        }
-        if ((n & ~0x3FFF) == 0) {
-            return 2;
-        }
-        return BytesInternal.stopBitlength0(n);
+    if (i < len) {
+      if (a.readByte(offset + i) != second.readByte(secondOffset + i)) {
+        return false;
+      }
     }
+    return true;
+  }
 
-    public static char[] toCharArray(Bytes bytes) {
-        final char[] chars = new char[(int) bytes.readRemaining()];
-
-        for (int i = 0; i < bytes.readRemaining(); i++) {
-            chars[i] = (char) bytes.readUnsignedByte(i + bytes.readPosition());
-        }
-        return chars;
+  public static boolean equals(Object o1, Object o2) {
+    if (o1 == o2) {
+      return true;
     }
-
-
-    public static char[] toCharArray(Bytes bytes, long position, long limit) {
-        final char[] chars = new char[(int) bytes.readRemaining()];
-
-        int j = 0;
-        for (long i = position; i < limit; i++) {
-            chars[j++] = (char) bytes.readUnsignedByte(i);
-        }
-        return chars;
+    if (o1 instanceof CharSequence && o2 instanceof CharSequence) {
+      return StringUtils.isEqual((CharSequence) o1, (CharSequence) o2);
     }
+    return o1 != null && o1.equals(o2);
+  }
+
+  public static int asInt(@NotNull String str) {
+    ByteBuffer bb = ByteBuffer.wrap(str.getBytes(StandardCharsets.ISO_8859_1)).order(ByteOrder.nativeOrder());
+    return bb.getInt();
+  }
+
+  public static int stopBitLength(long n) {
+    if ((n & ~0x7F) == 0) {
+      return 1;
+    }
+    if ((n & ~0x3FFF) == 0) {
+      return 2;
+    }
+    return BytesInternal.stopBitlength0(n);
+  }
+
+  public static char[] toCharArray(Bytes bytes) {
+    final char[] chars = new char[(int) bytes.readRemaining()];
+    for (int i = 0; i < bytes.readRemaining(); i++) {
+      chars[i] = (char) bytes.readUnsignedByte(i + bytes.readPosition());
+    }
+    return chars;
+  }
+
+  public static char[] toCharArray(Bytes bytes, long position, long limit) {
+    final char[] chars = new char[(int) bytes.readRemaining()];
+    int j = 0;
+    for (long i = position; i < limit; i++) {
+      chars[j++] = (char) bytes.readUnsignedByte(i);
+    }
+    return chars;
+  }
 }
