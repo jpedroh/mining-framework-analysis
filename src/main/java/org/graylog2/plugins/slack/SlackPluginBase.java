0e9611bf5e9134fecfcdd5357cedd20affdce02a
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 17:07:39 +0000

    Mining Framework Analysis

--- src/main/java/org/graylog2/plugins/slack/SlackPluginBase.java
+++ src/main/java/org/graylog2/plugins/slack/SlackPluginBase.java
@@ -1,5 +1,4 @@
 package org.graylog2.plugins.slack;
-
 import org.graylog2.plugin.configuration.Configuration;
 import org.graylog2.plugin.configuration.ConfigurationException;
 import org.graylog2.plugin.configuration.ConfigurationRequest;
@@ -8,140 +7,115 @@ import org.graylog2.plugin.configuration.fields.ConfigurationField;
 import org.graylog2.plugin.configuration.fields.NumberField;
 import org.graylog2.plugin.configuration.fields.TextField;
 import org.graylog2.plugin.streams.Stream;
-
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Arrays;
 
 public class SlackPluginBase {
-    public static final String CK_WEBHOOK_URL = "webhook_url";
-    public static final String CK_CHANNEL = "channel";
-    public static final String CK_USER_NAME = "user_name";
-    public static final String CK_NOTIFY_CHANNEL = "notify_channel";
-    public static final String CK_ADD_ATTACHMENT = "add_attachment";
-    public static final String CK_SHORT_MODE = "short_mode";
-    public static final String CK_LINK_NAMES = "link_names";
-    public static final String CK_ICON_URL = "icon_url";
-    public static final String CK_ICON_EMOJI = "icon_emoji";
-    public static final String CK_GRAYLOG2_URL = "graylog2_url";
-    public static final String CK_PROXY_ADDRESS = "proxy_address";
-    public static final String CK_COLOR = "color";
-    public static final String CK_FIELDS = "custom_fields";
-    public static final String CK_ADD_BLITEMS = "backlog_items";
+  public static final String CK_ADD_BLITEMS = "backlog_items";
 
-    protected static ConfigurationRequest configuration() {
-        final ConfigurationRequest configurationRequest = new ConfigurationRequest();
+  public static final String CK_WEBHOOK_URL = "webhook_url";
 
-        configurationRequest.addField(new TextField(
-                CK_WEBHOOK_URL, "Webhook URL", "", "Slack \"Incoming Webhook\" URL",
-                ConfigurationField.Optional.NOT_OPTIONAL)
-        );
-        configurationRequest.addField(new TextField(
-                CK_CHANNEL, "Channel", "#channel", "Name of Slack #channel or @user for a direct message.",
-                ConfigurationField.Optional.NOT_OPTIONAL)
-        );
-        configurationRequest.addField(new TextField(
-                CK_USER_NAME, "User name", "Graylog",
-                "User name of the sender in Slack",
-                ConfigurationField.Optional.OPTIONAL)
-        );
-        configurationRequest.addField(new TextField(
-                CK_COLOR, "Color", "#FF0000",
-                "Color to use for Slack message",
-                ConfigurationField.Optional.NOT_OPTIONAL)
-        );
-        configurationRequest.addField(new BooleanField(
-                CK_ADD_ATTACHMENT, "Include more information", true,
-                "Add structured information as message attachment")
-        );
-        configurationRequest.addField(new NumberField(
-                CK_ADD_BLITEMS, "Backlog items", 5,
-                "Number of backlog item descriptions to attach")
-        );
+  public static final String CK_CHANNEL = "channel";
 
-        configurationRequest.addField(new BooleanField(
-                CK_SHORT_MODE, "Short mode", false,
-                "Enable short mode? This strips down the Slack message to the bare minimum to take less space in the chat room. " +
-                        "Not used in alarm callback but only in the message output module.")
-        );
-        configurationRequest.addField(new BooleanField(
-                CK_NOTIFY_CHANNEL, "Notify Channel", false,
-                "Notify all users in channel by adding @channel to the message.")
-        );
-        configurationRequest.addField(new BooleanField(
-                CK_LINK_NAMES, "Link names", true,
-                "Find and link channel names and user names")
-        );
-        configurationRequest.addField(new TextField(
-                CK_ICON_URL, "Icon URL", null,
-                "Image to use as the icon for this message",
-                ConfigurationField.Optional.OPTIONAL)
-        );
-        configurationRequest.addField(new TextField(
-                CK_ICON_EMOJI, "Icon Emoji", null,
-                "Emoji to use as the icon for this message (overrides Icon URL)",
-                ConfigurationField.Optional.OPTIONAL)
-        );
-        configurationRequest.addField(new TextField(
-                CK_GRAYLOG2_URL, "Graylog URL", null,
-                "URL to your Graylog web interface. Used to build links in alarm notification.",
-                ConfigurationField.Optional.OPTIONAL)
-        );
-        configurationRequest.addField(new TextField(
-                CK_PROXY_ADDRESS, "Proxy", null,
-                "Please insert the proxy information in the follwoing format: <ProxyAddress>:<Port>",
-                ConfigurationField.Optional.OPTIONAL)
-        );
-        configurationRequest.addField(new TextField(
-                CK_FIELDS, "Custom fields", null,
-                "Add fields into alert (field1, field2...)",
-                ConfigurationField.Optional.OPTIONAL)
-        );
+  public static final String CK_USER_NAME = "user_name";
 
-        return configurationRequest;
+  public static final String CK_NOTIFY_CHANNEL = "notify_channel";
+
+  public static final String CK_ADD_ATTACHMENT = "add_attachment";
+
+  public static final String CK_SHORT_MODE = "short_mode";
+
+  public static final String CK_LINK_NAMES = "link_names";
+
+  public static final String CK_ICON_URL = "icon_url";
+
+  public static final String CK_ICON_EMOJI = "icon_emoji";
+
+  public static final String CK_GRAYLOG2_URL = "graylog2_url";
+
+  public static final String CK_COLOR = "color";
+
+  public static final String 
+<<<<<<< /usr/src/app/output/graylog2/graylog2-alarmcallback-slack/c9a4912adaef3596c44b1fb7f501dcdb96190977/src/main/java/org/graylog2/plugins/slack/SlackPluginBase.java/left.java
+  CK_FIELDS = "custom_fields"
+=======
+  CK_PROXY_ADDRESS = "proxy_address"
+>>>>>>> /usr/src/app/output/graylog2/graylog2-alarmcallback-slack/c9a4912adaef3596c44b1fb7f501dcdb96190977/src/main/java/org/graylog2/plugins/slack/SlackPluginBase.java/right.java
+  ;
+
+  protected static ConfigurationRequest configuration() {
+    final ConfigurationRequest configurationRequest = new ConfigurationRequest();
+    configurationRequest.addField(new TextField(CK_WEBHOOK_URL, "Webhook URL", "", "Slack \"Incoming Webhook\" URL", ConfigurationField.Optional.NOT_OPTIONAL));
+    configurationRequest.addField(new TextField(CK_CHANNEL, "Channel", "#channel", "Name of Slack #channel or @user for a direct message.", ConfigurationField.Optional.NOT_OPTIONAL));
+    configurationRequest.addField(new TextField(CK_USER_NAME, "User name", "Graylog", "User name of the sender in Slack", ConfigurationField.Optional.OPTIONAL));
+    configurationRequest.addField(new TextField(CK_COLOR, "Color", "#FF0000", "Color to use for Slack message", ConfigurationField.Optional.NOT_OPTIONAL));
+    configurationRequest.addField(new BooleanField(CK_ADD_ATTACHMENT, "Include more information", true, "Add structured information as message attachment"));
+    configurationRequest.addField(new NumberField(CK_ADD_BLITEMS, "Backlog items", 5, "Number of backlog item descriptions to attach"));
+    configurationRequest.addField(new BooleanField(CK_SHORT_MODE, "Short mode", false, "Enable short mode? This strips down the Slack message to the bare minimum to take less space in the chat room. " + "Not used in alarm callback but only in the message output module."));
+    configurationRequest.addField(new BooleanField(CK_NOTIFY_CHANNEL, "Notify Channel", false, "Notify all users in channel by adding @channel to the message."));
+    configurationRequest.addField(new BooleanField(CK_LINK_NAMES, "Link names", true, "Find and link channel names and user names"));
+    configurationRequest.addField(new TextField(CK_ICON_URL, "Icon URL", null, "Image to use as the icon for this message", ConfigurationField.Optional.OPTIONAL));
+    configurationRequest.addField(new TextField(CK_ICON_EMOJI, "Icon Emoji", null, "Emoji to use as the icon for this message (overrides Icon URL)", ConfigurationField.Optional.OPTIONAL));
+    configurationRequest.addField(new TextField(CK_GRAYLOG2_URL, "Graylog URL", null, "URL to your Graylog web interface. Used to build links in alarm notification.", ConfigurationField.Optional.OPTIONAL));
+    configurationRequest.addField(new TextField(
+<<<<<<< /usr/src/app/output/graylog2/graylog2-alarmcallback-slack/c9a4912adaef3596c44b1fb7f501dcdb96190977/src/main/java/org/graylog2/plugins/slack/SlackPluginBase.java/left.java
+    CK_FIELDS
+=======
+    CK_PROXY_ADDRESS
+>>>>>>> /usr/src/app/output/graylog2/graylog2-alarmcallback-slack/c9a4912adaef3596c44b1fb7f501dcdb96190977/src/main/java/org/graylog2/plugins/slack/SlackPluginBase.java/right.java
+    , 
+<<<<<<< /usr/src/app/output/graylog2/graylog2-alarmcallback-slack/c9a4912adaef3596c44b1fb7f501dcdb96190977/src/main/java/org/graylog2/plugins/slack/SlackPluginBase.java/left.java
+    "Custom fields"
+=======
+    "Proxy"
+>>>>>>> /usr/src/app/output/graylog2/graylog2-alarmcallback-slack/c9a4912adaef3596c44b1fb7f501dcdb96190977/src/main/java/org/graylog2/plugins/slack/SlackPluginBase.java/right.java
+    , null, 
+<<<<<<< /usr/src/app/output/graylog2/graylog2-alarmcallback-slack/c9a4912adaef3596c44b1fb7f501dcdb96190977/src/main/java/org/graylog2/plugins/slack/SlackPluginBase.java/left.java
+    "Add fields into alert (field1, field2...)"
+=======
+    "Please insert the proxy information in the follwoing format: <ProxyAddress>:<Port>"
+>>>>>>> /usr/src/app/output/graylog2/graylog2-alarmcallback-slack/c9a4912adaef3596c44b1fb7f501dcdb96190977/src/main/java/org/graylog2/plugins/slack/SlackPluginBase.java/right.java
+    , ConfigurationField.Optional.OPTIONAL));
+    return configurationRequest;
+  }
+
+  protected static void checkConfiguration(Configuration configuration) throws ConfigurationException {
+    if (!configuration.stringIsSet(CK_WEBHOOK_URL)) {
+      throw new ConfigurationException(CK_WEBHOOK_URL + " is mandatory and must not be empty.");
     }
-
-    protected static void checkConfiguration(Configuration configuration) throws ConfigurationException {
-        if (!configuration.stringIsSet(CK_WEBHOOK_URL)) {
-            throw new ConfigurationException(CK_WEBHOOK_URL + " is mandatory and must not be empty.");
-        }
-
-        if (!configuration.stringIsSet(CK_CHANNEL)) {
-            throw new ConfigurationException(CK_CHANNEL + " is mandatory and must not be empty.");
-        }
-
-        if (!configuration.stringIsSet(CK_COLOR)) {
-            throw new ConfigurationException(CK_COLOR + " is mandatory and must not be empty.");
-        }
-
-        checkUri(configuration, CK_PROXY_ADDRESS);
-        checkUri(configuration, CK_ICON_URL);
-        checkUri(configuration, CK_GRAYLOG2_URL);
+    if (!configuration.stringIsSet(CK_CHANNEL)) {
+      throw new ConfigurationException(CK_CHANNEL + " is mandatory and must not be empty.");
     }
-
-    private static boolean isValidUriScheme(URI uri, String... validSchemes) {
-        return uri.getScheme() != null && Arrays.binarySearch(validSchemes, uri.getScheme(), null) >= 0;
+    if (!configuration.stringIsSet(CK_COLOR)) {
+      throw new ConfigurationException(CK_COLOR + " is mandatory and must not be empty.");
     }
+    checkUri(configuration, CK_PROXY_ADDRESS);
+    checkUri(configuration, CK_ICON_URL);
+    checkUri(configuration, CK_GRAYLOG2_URL);
+  }
 
-    private static void checkUri(Configuration configuration, String settingName) throws ConfigurationException {
-        if (configuration.stringIsSet(settingName)) {
-            try {
-                final URI uri = new URI(configuration.getString(settingName));
-                if (!isValidUriScheme(uri, "http", "https")) {
-                    throw new ConfigurationException(settingName + " must be a valid HTTP or HTTPS URL.");
-                }
-            } catch (URISyntaxException e) {
-                throw new ConfigurationException("Couldn't parse " + settingName + " correctly.", e);
-            }
+  private static boolean isValidUriScheme(URI uri, String... validSchemes) {
+    return uri.getScheme() != null && Arrays.binarySearch(validSchemes, uri.getScheme(), null) >= 0;
+  }
+
+  private static void checkUri(Configuration configuration, String settingName) throws ConfigurationException {
+    if (configuration.stringIsSet(settingName)) {
+      try {
+        final URI uri = new URI(configuration.getString(settingName));
+        if (!isValidUriScheme(uri, "http", "https")) {
+          throw new ConfigurationException(settingName + " must be a valid HTTP or HTTPS URL.");
         }
+      } catch (URISyntaxException e) {
+        throw new ConfigurationException("Couldn\'t parse " + settingName + " correctly.", e);
+      }
     }
+  }
 
-    protected String buildStreamLink(String baseUrl, Stream stream) {
-        if (!baseUrl.endsWith("/")) {
-            baseUrl = baseUrl + "/";
-        }
-
-        return baseUrl + "streams/" + stream.getId() + "/messages?q=*&rangetype=relative&relative=3600";
+  protected String buildStreamLink(String baseUrl, Stream stream) {
+    if (!baseUrl.endsWith("/")) {
+      baseUrl = baseUrl + "/";
     }
-
+    return baseUrl + "streams/" + stream.getId() + "/messages?q=*&rangetype=relative&relative=3600";
+  }
 }
--- src/main/java/org/graylog2/plugins/slack/callback/SlackAlarmCallback.java
+++ src/main/java/org/graylog2/plugins/slack/callback/SlackAlarmCallback.java
@@ -1,10 +1,12 @@
 package org.graylog2.plugins.slack.callback;
-
 import com.google.common.collect.Lists;
 import org.graylog2.plugin.Message;
 import org.graylog2.plugin.MessageSummary;
+import org.graylog2.plugins.slack.SlackClient;
 import org.graylog2.plugin.alarms.AlertCondition;
+import org.graylog2.plugins.slack.SlackMessage;
 import org.graylog2.plugin.alarms.callbacks.AlarmCallback;
+import org.graylog2.plugins.slack.SlackPluginBase;
 import org.graylog2.plugin.alarms.callbacks.AlarmCallbackConfigurationException;
 import org.graylog2.plugin.alarms.callbacks.AlarmCallbackException;
 import org.graylog2.plugin.configuration.Configuration;
@@ -12,147 +14,109 @@ import org.graylog2.plugin.configuration.ConfigurationException;
 import org.graylog2.plugin.configuration.ConfigurationRequest;
 import org.graylog2.plugin.streams.Stream;
 import org.graylog2.plugins.slack.SlackClient;
+import java.util.Collections;
 import org.graylog2.plugins.slack.SlackMessage;
+import java.util.List;
 import org.graylog2.plugins.slack.SlackPluginBase;
-
-import java.util.*;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
-
 import static com.google.common.base.Strings.isNullOrEmpty;
 
 public class SlackAlarmCallback extends SlackPluginBase implements AlarmCallback {
+  public static final String DELIMITER = " | ";
 
-    public static final String DELIMITER = " | ";
-    private Configuration configuration;
+  private Configuration configuration;
 
-    @Override
-    public void initialize(final Configuration config) throws AlarmCallbackConfigurationException {
-        this.configuration = config;
-
-        try {
-            checkConfiguration(config);
-        } catch (ConfigurationException e) {
-            throw new AlarmCallbackConfigurationException("Configuration error. " + e.getMessage());
-        }
+  @Override public void initialize(final Configuration config) throws AlarmCallbackConfigurationException {
+    this.configuration = config;
+    try {
+      checkConfiguration(config);
+    } catch (ConfigurationException e) {
+      throw new AlarmCallbackConfigurationException("Configuration error. " + e.getMessage());
     }
+  }
 
-    @Override
-    public void call(Stream stream, AlertCondition.CheckResult result) throws AlarmCallbackException {
-        final SlackClient client = new SlackClient(configuration);
-
-        SlackMessage message = new SlackMessage(
-                configuration.getString(CK_COLOR),
-                configuration.getString(CK_ICON_EMOJI),
-                configuration.getString(CK_ICON_URL),
-                buildMessage(stream, result),
-                configuration.getString(CK_USER_NAME),
-                configuration.getString(CK_CHANNEL),
-                configuration.getBoolean(CK_LINK_NAMES)
-        );
-
-        // Add attachments if requested.
-        final List<Message> backlogItems = getAlarmBacklog(result);
-
-        if (configuration.getBoolean(CK_ADD_ATTACHMENT)) {
-            message.addAttachment(new SlackMessage.AttachmentField("Stream ID", stream.getId(), true));
-            message.addAttachment(new SlackMessage.AttachmentField("Stream Title", stream.getTitle(), false));
-            message.addAttachment(new SlackMessage.AttachmentField("Stream Description", stream.getDescription(), false));
-
-            int count = configuration.getInt(CK_ADD_BLITEMS);
-            if (count < 1) {
-                count = 5; //Default items to show
-            }
-            final int blSize = backlogItems.size();
-            if (blSize < count) {
-                count = blSize;
-            }
-            final StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < count; i++) {
-                sb.append(backlogItems.get(i).getMessage()).append("\n\n");
-            }
-            String attachmentName = "Backlog Items (" + Integer.toString(count) + ")";
-            message.addAttachment(new SlackMessage.AttachmentField(attachmentName, sb.toString(), false));
-        }
-
-        // Add custom fields
-        String customFields = configuration.getString(SlackPluginBase.CK_FIELDS);
-        if(!customFields.isEmpty()){
-
-            String[] fields = customFields.split(",");
-
-            for (MessageSummary messageSummary : result.getMatchingMessages()) {
-
-                String value = Arrays
-                        .stream(fields)
-                        .map(field -> Optional.ofNullable(messageSummary.getField(field)).map(Object::toString).orElse("???"))
-                        .collect(Collectors.joining(DELIMITER));
-
-                String title = String.join(DELIMITER, fields);
-
-                SlackMessage.AttachmentField attachment = new SlackMessage.AttachmentField(title, value, false);
-                message.addAttachment(attachment);
-            }
-        }
-
-        try {
-            client.send(message);
-        } catch (SlackClient.SlackClientException e) {
-            throw new RuntimeException("Could not send message to Slack.", e);
-        }
+  @Override public void call(Stream stream, AlertCondition.CheckResult result) throws AlarmCallbackException {
+    final SlackClient client = new SlackClient(configuration);
+    SlackMessage message = new SlackMessage(configuration.getString(CK_COLOR), configuration.getString(CK_ICON_EMOJI), configuration.getString(CK_ICON_URL), buildMessage(stream, result), configuration.getString(CK_USER_NAME), configuration.getString(CK_CHANNEL), configuration.getBoolean(CK_LINK_NAMES));
+    final List<Message> backlogItems = getAlarmBacklog(result);
+    if (configuration.getBoolean(CK_ADD_ATTACHMENT)) {
+      message.addAttachment(new SlackMessage.AttachmentField("Stream ID", stream.getId(), true));
+      message.addAttachment(new SlackMessage.AttachmentField("Stream Title", stream.getTitle(), false));
+      message.addAttachment(new SlackMessage.AttachmentField("Stream Description", stream.getDescription(), false));
+      int count = configuration.getInt(CK_ADD_BLITEMS);
+      if (count < 1) {
+        count = 5;
+      }
+      final int blSize = backlogItems.size();
+      if (blSize < count) {
+        count = blSize;
+      }
+      final StringBuilder sb = new StringBuilder();
+      for (int i = 0; i < count; i++) {
+        sb.append(backlogItems.get(i).getMessage()).append("\n\n");
+      }
+      String attachmentName = "Backlog Items (" + Integer.toString(count) + ")";
+      message.addAttachment(new SlackMessage.AttachmentField(attachmentName, sb.toString(), false));
     }
-
-    protected List<Message> getAlarmBacklog(AlertCondition.CheckResult result) {
-        final AlertCondition alertCondition = result.getTriggeredCondition();
-        final List<MessageSummary> matchingMessages = result.getMatchingMessages();
-
-        final int effectiveBacklogSize = Math.min(alertCondition.getBacklog(), matchingMessages.size());
-
-        if (effectiveBacklogSize == 0) {
-            return Collections.emptyList();
-        }
-
-        final List<MessageSummary> backlogSummaries = matchingMessages.subList(0, effectiveBacklogSize);
-
-        final List<Message> backlog = Lists.newArrayListWithCapacity(effectiveBacklogSize);
-
-        for (MessageSummary messageSummary : backlogSummaries) {
-            backlog.add(messageSummary.getRawMessage());
-        }
-
-        return backlog;
+    String customFields = configuration.getString(SlackPluginBase.CK_FIELDS);
+    if (!customFields.isEmpty()) {
+      String[] fields = customFields.split(",");
+      for (MessageSummary messageSummary : result.getMatchingMessages()) {
+        String value = Arrays.stream(fields).map((field) -> Optional.ofNullable(messageSummary.getField(field)).map(Object::toString).orElse("???")).collect(Collectors.joining(DELIMITER));
+        String title = String.join(DELIMITER, fields);
+        SlackMessage.AttachmentField attachment = new SlackMessage.AttachmentField(title, value, false);
+        message.addAttachment(attachment);
+      }
     }
-
-    public String buildMessage(Stream stream, AlertCondition.CheckResult result) {
-        String graylogUri = configuration.getString(CK_GRAYLOG2_URL);
-        boolean notifyChannel = configuration.getBoolean(CK_NOTIFY_CHANNEL);
-
-        String titleLink;
-        if (!isNullOrEmpty(graylogUri)) {
-            titleLink = "<" + buildStreamLink(graylogUri, stream) + "|" + stream.getTitle() + ">";
-        } else {
-            titleLink = "_" + stream.getTitle() + "_";
-        }
-
-        return notifyChannel ? "@channel " : "" + "*Alert for Graylog stream " + titleLink + "*:\n" + "> " + result.getResultDescription();
+    try {
+      client.send(message);
+    } catch (SlackClient.SlackClientException e) {
+      throw new RuntimeException("Could not send message to Slack.", e);
     }
+  }
 
-    @Override
-    public Map<String, Object> getAttributes() {
-        return configuration.getSource();
+  protected List<Message> getAlarmBacklog(AlertCondition.CheckResult result) {
+    final AlertCondition alertCondition = result.getTriggeredCondition();
+    final List<MessageSummary> matchingMessages = result.getMatchingMessages();
+    final int effectiveBacklogSize = Math.min(alertCondition.getBacklog(), matchingMessages.size());
+    if (effectiveBacklogSize == 0) {
+      return Collections.emptyList();
     }
+    final List<MessageSummary> backlogSummaries = matchingMessages.subList(0, effectiveBacklogSize);
+    final List<Message> backlog = Lists.newArrayListWithCapacity(effectiveBacklogSize);
+    for (MessageSummary messageSummary : backlogSummaries) {
+      backlog.add(messageSummary.getRawMessage());
+    }
+    return backlog;
+  }
 
-    @Override
-    public void checkConfiguration() throws ConfigurationException {
-        /* Never actually called by graylog-server */
+  public String buildMessage(Stream stream, AlertCondition.CheckResult result) {
+    String graylogUri = configuration.getString(CK_GRAYLOG2_URL);
+    boolean notifyChannel = configuration.getBoolean(CK_NOTIFY_CHANNEL);
+    String titleLink;
+    if (!isNullOrEmpty(graylogUri)) {
+      titleLink = "<" + buildStreamLink(graylogUri, stream) + "|" + stream.getTitle() + ">";
+    } else {
+      titleLink = "_" + stream.getTitle() + "_";
     }
+    return notifyChannel ? "@channel " : "" + "*Alert for Graylog stream " + titleLink + "*:\n" + "> " + result.getResultDescription();
+  }
 
-    @Override
-    public ConfigurationRequest getRequestedConfiguration() {
-        return configuration();
-    }
+  @Override public Map<String, Object> getAttributes() {
+    return configuration.getSource();
+  }
 
-    @Override
-    public String getName() {
-        return "Slack alarm callback";
-    }
+  @Override public void checkConfiguration() throws ConfigurationException {
+  }
+
+  @Override public ConfigurationRequest getRequestedConfiguration() {
+    return configuration();
+  }
+
+  @Override public String getName() {
+    return "Slack alarm callback";
+  }
 }
