1703dd38c428daab1e7c5daa57d3c21d12e41706
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 15:52:06 +0000

    Mining Framework Analysis

--- src/org/enderstone/server/regions/generators/FlatLandGenerator.java
+++ src/org/enderstone/server/regions/generators/FlatLandGenerator.java
@@ -1,8 +1,6 @@
 package org.enderstone.server.regions.generators;
-
 import java.util.List;
 import java.util.Random;
-
 import org.enderstone.server.regions.BlockId;
 import org.enderstone.server.regions.BlockPopulator;
 import org.enderstone.server.regions.ChunkGenerator;
@@ -10,29 +8,22 @@ import org.enderstone.server.regions.EnderChunk;
 import org.enderstone.server.regions.EnderWorld;
 
 public class FlatLandGenerator extends ChunkGenerator {
-	@Override
-	public BlockId[][] generateExtBlockSections(EnderWorld world, Random random, int x, int z, ChunkGenerator.BiomeGrid biomes) {
-		BlockId[][] r = new BlockId[16][];
-		
-		r[0] = new BlockId[4096];
-		for (int i = 0; i < r[0].length; i++) {
-			r[0][i] = BlockId.DIRT;
-		}
-		return r;
-	}
+  @Override public BlockId[][] generateExtBlockSections(EnderWorld world, Random random, int x, int z, ChunkGenerator.BiomeGrid biomes) {
+    BlockId[][] r = new BlockId[16][];
+    r[0] = new BlockId[4096];
+    for (int i = 0; i < r[0].length; i++) {
+      r[0][i] = BlockId.DIRT;
+    }
+    return r;
+  }
 
-	@Override
-	public List<BlockPopulator> getDefaultPopulators(EnderWorld world) {
-		List<BlockPopulator> p = super.getDefaultPopulators(world);
-		p.add(new BlockPopulator() {
-
-			@Override
-			public void populate(EnderWorld world, Random random, EnderChunk source) {
-				source.setBlock(7, 16, 7, BlockId.COBBLESTONE,(byte) 0);
-			}
-		});
-		return p;
-	}
-	
-	
+  @Override public List<BlockPopulator> getDefaultPopulators(EnderWorld world) {
+    List<BlockPopulator> p = super.getDefaultPopulators(world);
+    p.add(new BlockPopulator() {
+      @Override public void populate(EnderWorld world, Random random, EnderChunk source) {
+        source.setBlock(7, 16, 7, BlockId.COBBLESTONE, (byte) 0);
+      }
+    });
+    return p;
+  }
 }
