d56bec3e24683be24083e2ef31e5fbc25c330da7
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 13:11:17 +0000

    Mining Framework Analysis

--- core/src/main/java/tech/tablesaw/aggregate/BooleanAggregateFunction.java
+++ core/src/main/java/tech/tablesaw/aggregate/BooleanAggregateFunction.java
@@ -6,14 +6,26 @@ import tech.tablesaw.api.ColumnType;
 /**
  * A partial implementation of aggregate functions to summarize over a boolean column
  */
-public abstract class BooleanAggregateFunction extends AggregateFunction<Boolean, BooleanColumn> {
+<<<<<<< /usr/src/app/output/lwhite1/tablesaw/436f7cfe5ecfe763e7a45ce6745f1d1c52133d22/core/src/main/java/tech/tablesaw/aggregate/BooleanAggregateFunction.java/left.java
+public abstract class BooleanAggregateFunction<C extends Column<?>> extends AggregateFunction<Boolean, C> {
+||||||| /usr/src/app/output/lwhite1/tablesaw/436f7cfe5ecfe763e7a45ce6745f1d1c52133d22/core/src/main/java/tech/tablesaw/aggregate/BooleanAggregateFunction.java/base.java
+public abstract class BooleanAggregateFunction<C extends Column<?>> extends AggregateFunction<Boolean, Column> {
+=======
+public abstract class BooleanAggregateFunction<C extends Column<?>> extends AggregateFunction<Boolean, BooleanColumn> {
+>>>>>>> /usr/src/app/output/lwhite1/tablesaw/436f7cfe5ecfe763e7a45ce6745f1d1c52133d22/core/src/main/java/tech/tablesaw/aggregate/BooleanAggregateFunction.java/right.java
 
     public BooleanAggregateFunction(String name) {
         super(name);
     }
 
+<<<<<<< /usr/src/app/output/lwhite1/tablesaw/436f7cfe5ecfe763e7a45ce6745f1d1c52133d22/core/src/main/java/tech/tablesaw/aggregate/BooleanAggregateFunction.java/left.java
+||||||| /usr/src/app/output/lwhite1/tablesaw/436f7cfe5ecfe763e7a45ce6745f1d1c52133d22/core/src/main/java/tech/tablesaw/aggregate/BooleanAggregateFunction.java/base.java
+    abstract public Boolean summarize(Column column);
+
+=======
     abstract public Boolean summarize(BooleanColumn column);
 
+>>>>>>> /usr/src/app/output/lwhite1/tablesaw/436f7cfe5ecfe763e7a45ce6745f1d1c52133d22/core/src/main/java/tech/tablesaw/aggregate/BooleanAggregateFunction.java/right.java
     @Override
     public boolean isCompatableColumn(ColumnType type) {
         return type == ColumnType.BOOLEAN;
