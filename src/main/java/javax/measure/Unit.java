67940480818c3547f155696c93c92b6c2afa1de5
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 23:03:34 +0000

    Mining Framework Analysis

--- src/main/java/javax/measure/Unit.java
+++ src/main/java/javax/measure/Unit.java
@@ -1,39 +1,4 @@
-/*
- * Units of Measurement API
- * Copyright (c) 2014-2018, Jean-Marie Dautelle, Werner Keil, Otavio Santana.
- *
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions
- *    and the following disclaimer in the documentation and/or other materials provided with the distribution.
- *
- * 3. Neither the name of JSR-385 nor the names of its contributors may be used to endorse or promote products
- *    derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
- * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
- * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-//
-// This source code implements specifications defined by the Java
-// Community Process. In order to remain compliant with the specification
-// DO NOT add / change / or delete method signatures!
-//
 package javax.measure;
-
 import java.util.Map;
 
 /**
@@ -69,15 +34,10 @@ import java.util.Map;
  * @author <a href="mailto:werner@uom.technology">Werner Keil</a>
  * @version 1.0.2, December 25, 2017
  * @since 1.0
- *
+ * 
  * @see <a href="http://en.wikipedia.org/wiki/Units_of_measurement">Wikipedia: Units of measurement</a>
  */
 public interface Unit<Q extends Quantity<Q>> {
-
-  /*******************/
-  /** Units Queries **/
-  /*******************/
-
   /**
    * Returns the symbol (if any) of this unit. This method returns {@code null} if this unit has no specific symbol associated with.
    *
@@ -203,10 +163,6 @@ public interface Unit<Q extends Quantity<Q>> {
    * @see #isCompatible(Unit)
    */
   UnitConverter getConverterToAny(Unit<?> that) throws IncommensurableException, UnconvertibleException;
-
-  /**********************/
-  /** Units Operations **/
-  /**********************/
 
   /**
    * Returns a system unit equivalent to this unscaled standard unit but used in expressions to distinguish between quantities of a different nature
@@ -350,6 +306,5 @@ public interface Unit<Q extends Quantity<Q>> {
    * @see #getSymbol()
    * @see javax.measure.format.UnitFormat
    */
-  @Override
-  String toString();
+  @Override String toString();
 }
