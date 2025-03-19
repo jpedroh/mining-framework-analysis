1e69f8c612133dd8e385d06db3edd10b69e9d631
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 18:16:31 +0000

    Mining Framework Analysis

--- src/main/java/com/pengyifan/leetcode/commons/TreeNode.java
+++ src/main/java/com/pengyifan/leetcode/commons/TreeNode.java
@@ -6,7 +6,14 @@ public class TreeNode {
   public TreeNode left;
   public TreeNode right;
 
+<<<<<<< /usr/src/app/output/yfpeng/pengyifan-leetcode/bb9b165e9b6f5db5725394be827250f61e87b71a/src/main/java/com/pengyifan/leetcode/commons/TreeNode.java/left.java
+  TreeNode(int x) {
+    val = x;
+  }
+||||||| /usr/src/app/output/yfpeng/pengyifan-leetcode/bb9b165e9b6f5db5725394be827250f61e87b71a/src/main/java/com/pengyifan/leetcode/commons/TreeNode.java/base.java
+=======
   public TreeNode(int x) {
     val = x;
   }
+>>>>>>> /usr/src/app/output/yfpeng/pengyifan-leetcode/bb9b165e9b6f5db5725394be827250f61e87b71a/src/main/java/com/pengyifan/leetcode/commons/TreeNode.java/right.java
 }
