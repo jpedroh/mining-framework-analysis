1b13265e8ac58556c4fb20a5881bb5ad2ab55cb6
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 14:50:42 +0000

    Mining Framework Analysis

--- src/main/java/net/iponweb/disthene/reader/service/index/IndexService.java
+++ src/main/java/net/iponweb/disthene/reader/service/index/IndexService.java
@@ -1,5 +1,4 @@
 package net.iponweb.disthene.reader.service.index;
-
 import com.google.common.base.Joiner;
 import net.iponweb.disthene.reader.config.IndexConfiguration;
 import net.iponweb.disthene.reader.exceptions.TooMuchDataExpectedException;
@@ -14,7 +13,6 @@ import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.index.query.FilterBuilders;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -26,121 +24,83 @@ import java.util.List;
  * @author Andrei Ivanov
  */
 public class IndexService {
-    final static Logger logger = Logger.getLogger(IndexService.class);
+  final static Logger logger = Logger.getLogger(IndexService.class);
 
-    private IndexConfiguration indexConfiguration;
-    private TransportClient client;
-    private Joiner joiner = Joiner.on(",").skipNulls();
+  private IndexConfiguration indexConfiguration;
 
-    public IndexService(IndexConfiguration indexConfiguration) {
-        this.indexConfiguration = indexConfiguration;
+  private TransportClient client;
 
-        Settings settings = ImmutableSettings.settingsBuilder()
-                .put("cluster.name", indexConfiguration.getName())
-                .build();
-        client = new TransportClient(settings);
+  private Joiner joiner = Joiner.on(",").skipNulls();
 
-        for (String node : indexConfiguration.getCluster()) {
-            client.addTransportAddress(new InetSocketTransportAddress(node, indexConfiguration.getPort()));
-        }
+  public IndexService(IndexConfiguration indexConfiguration) {
+    this.indexConfiguration = indexConfiguration;
+    Settings settings = ImmutableSettings.settingsBuilder().put("cluster.name", indexConfiguration.getName()).build();
+    client = new TransportClient(settings);
+    for (String node : indexConfiguration.getCluster()) {
+      client.addTransportAddress(new InetSocketTransportAddress(node, indexConfiguration.getPort()));
     }
+  }
 
-    public Map<String, String> getPaths(String tenant, List<String> wildcards) throws TooMuchDataExpectedException {
-        List<String> regExs = new ArrayList<>();
-        for(String wildcard : wildcards) {
-            regExs.add(WildcardUtil.getPathsRegExFromWildcard(wildcard));
-        }
-        String regEx = Joiner.on("|").skipNulls().join(regExs);
-
-        Map<String, String> result = new HashMap<>();
-
-        SearchResponse response = client.prepareSearch(indexConfiguration.getIndex())
-                .setScroll(new TimeValue(indexConfiguration.getTimeout()))
-                .setSize(indexConfiguration.getScroll())
-                .setQuery(QueryBuilders.filteredQuery(QueryBuilders.regexpQuery("path", regEx),
-                        FilterBuilders.termFilter("tenant", tenant)))
-                .addField("path")
-                .addField("origin")
-                .execute().actionGet();
-
-        // if total hits exceeds maximum - abort right away returning empty array
-        if (response.getHits().totalHits() > indexConfiguration.getMaxPaths()) {
-            return result;
-        }
-
-        while (response.getHits().getHits().length > 0) {
-            // if total hits exceeds maximum - abort right away returning empty array
-            if (response.getHits().totalHits() > indexConfiguration.getMaxPaths()) {
-                logger.debug("Total number of paths exceeds the limit: " + response.getHits().totalHits());
-                throw new TooMuchDataExpectedException("Total number of paths exceeds the limit: " + response.getHits().totalHits() + " (the limit is " + indexConfiguration.getMaxPaths() + ")");
-            }
-
-            for (SearchHit hit : response.getHits()) {
-                String path = (String) hit.field("path").getValue();
-
-                if (hit.getFields().containsKey("origin")) {
-                    result.put(path, (String) hit.field("origin").getValue());
-                } else {
-                    result.put(path, path);
-                }
-            }
-            response = client.prepareSearchScroll(response.getScrollId())
-                    .setScroll(new TimeValue(indexConfiguration.getTimeout()))
-                    .execute().actionGet();
-        }
-
-        return result;
+  public Map<String, String> getPaths(String tenant, List<String> wildcards) throws TooMuchDataExpectedException {
+    List<String> regExs = new ArrayList<>();
+    for (String wildcard : wildcards) {
+      regExs.add(WildcardUtil.getPathsRegExFromWildcard(wildcard));
     }
+    String regEx = Joiner.on("|").skipNulls().join(regExs);
+    Map<String, String> result = new HashMap<>();
+    SearchResponse response = client.prepareSearch(indexConfiguration.getIndex()).setScroll(new TimeValue(indexConfiguration.getTimeout())).setSize(indexConfiguration.getScroll()).setQuery(QueryBuilders.filteredQuery(QueryBuilders.regexpQuery("path", regEx), FilterBuilders.termFilter("tenant", tenant))).addField("path").addField("origin").execute().actionGet();
+    if (response.getHits().totalHits() > indexConfiguration.getMaxPaths()) {
+      return result;
 
-    public String getPathsAsJsonArray(String tenant, String wildcard) throws TooMuchDataExpectedException {
-        String regEx = WildcardUtil.getPathsRegExFromWildcard(wildcard);
-
-        SearchResponse response = client.prepareSearch(indexConfiguration.getIndex())
-                .setScroll(new TimeValue(indexConfiguration.getTimeout()))
-                .setSize(indexConfiguration.getScroll())
-                .setQuery(QueryBuilders.filteredQuery(
-                        QueryBuilders.regexpQuery("path", regEx),
-                        FilterBuilders.termFilter("tenant", tenant)))
-                .execute().actionGet();
-
-        // if total hits exceeds maximum - abort right away returning empty array
-        if (response.getHits().totalHits() > indexConfiguration.getMaxPaths()) {
-            logger.debug("Total number of paths exceeds the limit: " + response.getHits().totalHits());
-            throw new TooMuchDataExpectedException("Total number of paths exceeds the limit: " + response.getHits().totalHits() + " (the limit is " + indexConfiguration.getMaxPaths() + ")");
-        }
-
-        List<String> paths = new ArrayList<>();
-        while (response.getHits().getHits().length > 0) {
-            for (SearchHit hit : response.getHits()) {
-                paths.add(hit.getSourceAsString());
-            }
-            response = client.prepareSearchScroll(response.getScrollId())
-                    .setScroll(new TimeValue(indexConfiguration.getTimeout()))
-                    .execute().actionGet();
-        }
-
-        return "[" + joiner.join(paths) + "]";
+<<<<<<< Unknown file: This is a bug in JDime.
+=======
+      if (response.getHits().totalHits() > indexConfiguration.getMaxPaths()) {
+        logger.debug("Total number of paths exceeds the limit: " + response.getHits().totalHits());
+        throw new TooMuchDataExpectedException("Total number of paths exceeds the limit: " + response.getHits().totalHits() + " (the limit is " + indexConfiguration.getMaxPaths() + ")");
+      }
+>>>>>>> /usr/src/app/output/einsamhauer/disthene-reader/28a8ab2adb083fdca0b512255193aeb06dc1726a/src/main/java/net/iponweb/disthene/reader/service/index/IndexService.java/right.java
     }
-
-    public String getSearchPathsAsString(String tenant, String regEx, int limit) {
-        SearchResponse response = client.prepareSearch(indexConfiguration.getIndex())
-                .setScroll(new TimeValue(indexConfiguration.getTimeout()))
-                .setSize(limit)
-                .setQuery(QueryBuilders.filteredQuery(
-                        QueryBuilders.regexpQuery("path", regEx),
-                        FilterBuilders.termFilter("tenant", tenant)))
-                .addField("path")
-                .execute().actionGet();
-
-        List<String> paths = new ArrayList<>();
-        for (SearchHit hit : response.getHits()) {
-            paths.add((String) hit.field("path").getValue());
+    while (response.getHits().getHits().length > 0) {
+      for (SearchHit hit : response.getHits()) {
+        String path = (String) hit.field("path").getValue();
+        if (hit.getFields().containsKey("origin")) {
+          result.put(path, (String) hit.field("origin").getValue());
+        } else {
+          result.put(path, path);
         }
-
-        return Joiner.on(",").skipNulls().join(paths);
+      }
+      response = client.prepareSearchScroll(response.getScrollId()).setScroll(new TimeValue(indexConfiguration.getTimeout())).execute().actionGet();
     }
+    return result;
+  }
 
-    public void shutdown() {
-        client.close();
+  public String getPathsAsJsonArray(String tenant, String wildcard) throws TooMuchDataExpectedException {
+    String regEx = WildcardUtil.getPathsRegExFromWildcard(wildcard);
+    SearchResponse response = client.prepareSearch(indexConfiguration.getIndex()).setScroll(new TimeValue(indexConfiguration.getTimeout())).setSize(indexConfiguration.getScroll()).setQuery(QueryBuilders.filteredQuery(QueryBuilders.regexpQuery("path", regEx), FilterBuilders.termFilter("tenant", tenant))).execute().actionGet();
+    if (response.getHits().totalHits() > indexConfiguration.getMaxPaths()) {
+      logger.debug("Total number of paths exceeds the limit: " + response.getHits().totalHits());
+      throw new TooMuchDataExpectedException("Total number of paths exceeds the limit: " + response.getHits().totalHits() + " (the limit is " + indexConfiguration.getMaxPaths() + ")");
     }
+    List<String> paths = new ArrayList<>();
+    while (response.getHits().getHits().length > 0) {
+      for (SearchHit hit : response.getHits()) {
+        paths.add(hit.getSourceAsString());
+      }
+      response = client.prepareSearchScroll(response.getScrollId()).setScroll(new TimeValue(indexConfiguration.getTimeout())).execute().actionGet();
+    }
+    return "[" + joiner.join(paths) + "]";
+  }
+
+  public String getSearchPathsAsString(String tenant, String regEx, int limit) {
+    SearchResponse response = client.prepareSearch(indexConfiguration.getIndex()).setScroll(new TimeValue(indexConfiguration.getTimeout())).setSize(limit).setQuery(QueryBuilders.filteredQuery(QueryBuilders.regexpQuery("path", regEx), FilterBuilders.termFilter("tenant", tenant))).addField("path").execute().actionGet();
+    List<String> paths = new ArrayList<>();
+    for (SearchHit hit : response.getHits()) {
+      paths.add((String) hit.field("path").getValue());
+    }
+    return Joiner.on(",").skipNulls().join(paths);
+  }
+
+  public void shutdown() {
+    client.close();
+  }
 }
