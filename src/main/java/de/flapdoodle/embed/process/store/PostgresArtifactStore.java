0d166393d17470de596f12fd374c7b5e96c5717c
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 20:02:25 +0000

    Mining Framework Analysis

--- src/main/java/de/flapdoodle/embed/process/store/PostgresArtifactStore.java
+++ src/main/java/de/flapdoodle/embed/process/store/PostgresArtifactStore.java
@@ -1,5 +1,4 @@
 package de.flapdoodle.embed.process.store;
-
 import de.flapdoodle.embed.process.config.store.FileSet;
 import de.flapdoodle.embed.process.config.store.FileType;
 import de.flapdoodle.embed.process.config.store.IDownloadConfig;
@@ -13,11 +12,9 @@ import de.flapdoodle.embed.process.extract.ITempNaming;
 import de.flapdoodle.embed.process.io.directories.IDirectory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.EnumSet;
-
 import static org.apache.commons.io.FileUtils.deleteQuietly;
 
 /**
@@ -25,95 +22,96 @@ import static org.apache.commons.io.FileUtils.deleteQuietly;
  *         Hacky ArtifactStore. Just to override the default FilesToExtract with PostgresFilesToExtract
  */
 public class PostgresArtifactStore implements IMutableArtifactStore {
-    private static final Logger LOGGER = LoggerFactory.getLogger(PostgresArtifactStore.class);
-    private IDownloadConfig downloadConfig;
-    private IDirectory tempDirFactory;
-    private ITempNaming executableNaming;
-    private IDownloader downloader;
+  private static final Logger LOGGER = LoggerFactory.getLogger(PostgresArtifactStore.class);
 
-    PostgresArtifactStore(IDownloadConfig downloadConfig, IDirectory tempDirFactory, ITempNaming executableNaming, IDownloader downloader) {
-        this.downloadConfig = downloadConfig;
-        this.tempDirFactory = tempDirFactory;
-        this.executableNaming = executableNaming;
-        this.downloader = downloader;
-    }
+  private IDownloadConfig downloadConfig;
 
-    public IDirectory getTempDir() {
-        return tempDirFactory;
-    }
+  private IDirectory tempDirFactory;
 
-    @Override
-    public void removeFileSet(Distribution distribution, IExtractedFileSet all) {
-        for (FileType type : EnumSet.complementOf(EnumSet.of(FileType.Executable))) {
-            for (File file : all.files(type)) {
-                if (file.exists() && !deleteQuietly(file))
-                    LOGGER.trace("Could not delete {} NOW: {}", type, file);
-            }
+  private ITempNaming executableNaming;
+
+  private IDownloader downloader;
+
+  PostgresArtifactStore(IDownloadConfig downloadConfig, IDirectory tempDirFactory, ITempNaming executableNaming, IDownloader downloader) {
+    this.downloadConfig = downloadConfig;
+    this.tempDirFactory = tempDirFactory;
+    this.executableNaming = executableNaming;
+    this.downloader = downloader;
+  }
+
+  public IDirectory getTempDir() {
+    return tempDirFactory;
+  }
+
+  @Override public void removeFileSet(Distribution distribution, IExtractedFileSet all) {
+    for (FileType type : EnumSet.complementOf(EnumSet.of(FileType.Executable))) {
+      for (File file : all.files(type)) {
+        if (file.exists() && !deleteQuietly(file)) {
+          LOGGER.trace("Could not delete {} NOW: {}", type, file);
         }
-        File exe = all.executable();
-        if (exe.exists() && !deleteQuietly(exe)) {
-            LOGGER.trace("Could not delete executable NOW: {}", exe);
-        }
-
-        if (all.baseDirIsGenerated()) {
-            if (!deleteQuietly(all.baseDir())) {
-                LOGGER.trace("Could not delete generatedBaseDir: {}", all.baseDir());
-            }
-        }
+      }
     }
-
-    @Override
-    public boolean checkDistribution(Distribution distribution) throws IOException {
-        if (!LocalArtifactStore.checkArtifact(downloadConfig, distribution)) {
-            return LocalArtifactStore.store(downloadConfig, distribution, downloader.download(downloadConfig, distribution));
-        }
-        return true;
+    File exe = all.executable();
+    if (exe.exists() && !deleteQuietly(exe)) {
+      LOGGER.trace("Could not delete executable NOW: {}", exe);
     }
-
-    public IDownloadConfig getDownloadConfig() {
-        return downloadConfig;
+    if (all.baseDirIsGenerated()) {
+      if (!deleteQuietly(all.baseDir())) {
+        LOGGER.trace("Could not delete generatedBaseDir: {}", all.baseDir());
+      }
     }
+  }
 
-    @Override
-    public void setDownloadConfig(IDownloadConfig downloadConfig) {
-        this.downloadConfig = downloadConfig;
+  @Override public boolean checkDistribution(Distribution distribution) throws IOException {
+    if (!LocalArtifactStore.checkArtifact(downloadConfig, distribution)) {
+      return LocalArtifactStore.store(downloadConfig, distribution, downloader.download(downloadConfig, distribution));
     }
+    return true;
+  }
 
-    @Override
-    public IExtractedFileSet extractFileSet(Distribution distribution) throws IOException {
-        IPackageResolver packageResolver = downloadConfig.getPackageResolver();
-        File artifact = getArtifact(downloadConfig, distribution);
-        final ArchiveType archiveType = packageResolver.getArchiveType(distribution);
-        IExtractor extractor = Extractors.getExtractor(archiveType);
-        try {
-            final FileSet fileSet = packageResolver.getFileSet(distribution);
-            return extractor.extract(downloadConfig, artifact,
-                    new PostgresFilesToExtract(tempDirFactory, executableNaming, fileSet, distribution));
-        } catch (Exception e) {
-            LOGGER.error("Failed to extract file set:", e);
-            return new EmptyFileSet();
-        }
-    }
+  public IDownloadConfig getDownloadConfig() {
+    return downloadConfig;
+  }
 
-    private File getArtifact(IDownloadConfig runtime, Distribution distribution) {
-        File dir = createOrGetBaseDir(runtime);
-        File artifactFile = new File(dir, runtime.getPackageResolver().getPath(distribution));
-        if ((artifactFile.exists()) && (artifactFile.isFile()))
-            return artifactFile;
-        return null;
-    }
+  @Override public void setDownloadConfig(IDownloadConfig downloadConfig) {
+    this.downloadConfig = downloadConfig;
+  }
 
-    private File createOrGetBaseDir(IDownloadConfig runtime) {
-        File dir = runtime.getArtifactStorePath().asFile();
-        createOrCheckDir(dir);
-        return dir;
+  @Override public IExtractedFileSet extractFileSet(Distribution distribution) throws IOException {
+    IPackageResolver packageResolver = downloadConfig.getPackageResolver();
+    File artifact = getArtifact(downloadConfig, distribution);
+    final ArchiveType archiveType = packageResolver.getArchiveType(distribution);
+    IExtractor extractor = Extractors.getExtractor(archiveType);
+    try {
+      final FileSet fileSet = packageResolver.getFileSet(distribution);
+      return extractor.extract(downloadConfig, artifact, new PostgresFilesToExtract(tempDirFactory, executableNaming, fileSet, distribution));
+    } catch (Exception e) {
+      LOGGER.error("Failed to extract file set:", e);
+      return new EmptyFileSet();
     }
+  }
 
-    private void createOrCheckDir(File dir) {
-        if (!dir.exists() && !dir.mkdirs()) {
-            throw new IllegalArgumentException("Could NOT create Directory " + dir);
-        }
-        if (!dir.isDirectory())
-            throw new IllegalArgumentException("" + dir + " is not a Directory");
+  private File getArtifact(IDownloadConfig runtime, Distribution distribution) {
+    File dir = createOrGetBaseDir(runtime);
+    File artifactFile = new File(dir, runtime.getPackageResolver().getPath(distribution));
+    if ((artifactFile.exists()) && (artifactFile.isFile())) {
+      return artifactFile;
     }
+    return null;
+  }
+
+  private File createOrGetBaseDir(IDownloadConfig runtime) {
+    File dir = runtime.getArtifactStorePath().asFile();
+    createOrCheckDir(dir);
+    return dir;
+  }
+
+  private void createOrCheckDir(File dir) {
+    if (!dir.exists() && !dir.mkdirs()) {
+      throw new IllegalArgumentException("Could NOT create Directory " + dir);
+    }
+    if (!dir.isDirectory()) {
+      throw new IllegalArgumentException("" + dir + " is not a Directory");
+    }
+  }
 }
--- src/main/java/ru/yandex/qatools/embed/postgresql/PackagePaths.java
+++ src/main/java/ru/yandex/qatools/embed/postgresql/PackagePaths.java
@@ -1,5 +1,4 @@
 package ru.yandex.qatools.embed.postgresql;
-
 import de.flapdoodle.embed.process.config.store.FileSet;
 import de.flapdoodle.embed.process.config.store.FileType;
 import de.flapdoodle.embed.process.config.store.IPackageResolver;
@@ -12,134 +11,114 @@ import de.flapdoodle.embed.process.io.directories.IDirectory;
  * Paths builder
  */
 public class PackagePaths implements IPackageResolver {
+  private final Command command;
 
-    private final Command command;
-    private final IDirectory tempDir;
+  private final IDirectory tempDir;
 
-    public PackagePaths(Command command, IDirectory tempDir) {
-        this.command = command;
-        this.tempDir = tempDir;
+  public PackagePaths(Command command, IDirectory tempDir) {
+    this.command = command;
+    this.tempDir = tempDir;
+  }
+
+  protected static String getVersionPart(IVersion version) {
+    return version.asInDownloadPath();
+  }
+
+  public IDirectory getTempDir() {
+    return tempDir;
+  }
+
+  @Override public FileSet getFileSet(Distribution distribution) {
+    String cmdPattern;
+    switch (distribution.getPlatform()) {
+      case Linux:
+      case OS_X:
+      cmdPattern = command.commandName();
+      break;
+      case Windows:
+      cmdPattern = command.commandName() + ".exe";
+      break;
+      default:
+      throw new IllegalArgumentException("Unknown Platform " + distribution.getPlatform());
     }
-
-    protected static String getVersionPart(IVersion version) {
-        return version.asInDownloadPath();
+    try {
+      return FileSet.builder().addEntry(FileType.Executable, tempDir.asFile().getPath(), "^.*pgsql\\\\bin\\\\" + cmdPattern + "$").addEntry(FileType.Executable, tempDir.asFile().getPath(), "^.*pgsql/bin/" + cmdPattern + "$").build();
+    } catch (Exception e) {
+      throw new RuntimeException(e);
     }
+  }
 
-    public IDirectory getTempDir() {
-        return tempDir;
+  @Override public ArchiveType getArchiveType(Distribution distribution) {
+    ArchiveType archiveType;
+    switch (distribution.getPlatform()) {
+      case Linux:
+      archiveType = ArchiveType.TGZ;
+      break;
+      case OS_X:
+      case Windows:
+      archiveType = ArchiveType.ZIP;
+      break;
+      default:
+      throw new IllegalArgumentException("Unknown Platform " + distribution.getPlatform());
     }
+    return archiveType;
+  }
 
-    @Override
-    public FileSet getFileSet(Distribution distribution) {
-        String cmdPattern;
-        switch (distribution.getPlatform()) {
-            case Linux:
-            case OS_X:
-                cmdPattern = command.commandName();
-                break;
-            case Windows:
-                cmdPattern = command.commandName() + ".exe";
-                break;
-            default:
-                throw new IllegalArgumentException("Unknown Platform "
-                        + distribution.getPlatform());
-        }
-        try {
-            return FileSet.builder()
-                    .addEntry(FileType.Executable, tempDir.asFile().getPath(),
-                            "^.*pgsql\\\\bin\\\\"+ cmdPattern+ "$")
-                                    .addEntry(FileType.Executable, tempDir.asFile().getPath(),
-                            "^.*pgsql/bin/" + cmdPattern + "$")
-                    .build();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+  @Override public String getPath(Distribution distribution) {
+    String sversion = getVersionPart(distribution.getVersion());
+    ArchiveType archiveType = getArchiveType(distribution);
+    String sarchiveType;
+    switch (archiveType) {
+      case TGZ:
+      sarchiveType = "tar.gz";
+      break;
+      case ZIP:
+      sarchiveType = "zip";
+      break;
+      default:
+      throw new IllegalArgumentException("Unknown ArchiveType " + archiveType);
     }
-
-    @Override
-    public ArchiveType getArchiveType(Distribution distribution) {
-        ArchiveType archiveType;
-        switch (distribution.getPlatform()) {
-            case Linux:
-                archiveType = ArchiveType.TGZ;
-                break;
-            case OS_X:
-            case Windows:
-                archiveType = ArchiveType.ZIP;
-                break;
-            default:
-                throw new IllegalArgumentException("Unknown Platform "
-                        + distribution.getPlatform());
-        }
-        return archiveType;
+    String splatform;
+    switch (distribution.getPlatform()) {
+      case Linux:
+      splatform = "linux";
+      break;
+      case Windows:
+      splatform = "windows";
+      break;
+      case OS_X:
+      splatform = "osx";
+      break;
+      default:
+      throw new IllegalArgumentException("Unknown Platform " + distribution.getPlatform());
     }
-
-    @Override
-    public String getPath(Distribution distribution) {
-        String sversion = getVersionPart(distribution.getVersion());
-
-        ArchiveType archiveType = getArchiveType(distribution);
-        String sarchiveType;
-        switch (archiveType) {
-            case TGZ:
-                sarchiveType = "tar.gz";
-                break;
-            case ZIP:
-                sarchiveType = "zip";
-                break;
-            default:
-                throw new IllegalArgumentException("Unknown ArchiveType "
-                        + archiveType);
-        }
-
-        String splatform;
-        switch (distribution.getPlatform()) {
-            case Linux:
-                splatform = "linux";
-                break;
-            case Windows:
-                splatform = "windows";
-                break;
-            case OS_X:
-                splatform = "osx";
-                break;
-            default:
-                throw new IllegalArgumentException("Unknown Platform "
-                        + distribution.getPlatform());
-        }
-
-        String bitsize = "";
-        switch (distribution.getBitsize()) {
-            case B32:
-                switch (distribution.getPlatform()) {
-                    case Windows:
-                    case Linux:
-                    case OS_X:
-                        break;
-                    default:
-                        throw new IllegalArgumentException(
-                                "32 bit supported only on Windows, MacOS, Linux, platform is "
-                                        + distribution.getPlatform());
-                }
-                break;
-            case B64:
-                switch (distribution.getPlatform()) {
-                    case Linux:
-                    case Windows:
-                        bitsize = "-x64";
-                        break;
-                    case OS_X:
-                        break;
-                    default:
-                        throw new IllegalArgumentException(
-                                "64 bit supported only on Linux and Windows, platform is "
-                                        + distribution.getPlatform());
-                }
-                break;
-            default:
-                throw new IllegalArgumentException("Unknown BitSize " + distribution.getBitsize());
-        }
-
-        return "postgresql-" + sversion + "-" + splatform + bitsize + "-binaries" + "." + sarchiveType;
+    String bitsize = "";
+    switch (distribution.getBitsize()) {
+      case B32:
+      switch (distribution.getPlatform()) {
+        case Windows:
+        case Linux:
+        case OS_X:
+        break;
+        default:
+        throw new IllegalArgumentException("32 bit supported only on Windows, MacOS, Linux, platform is " + distribution.getPlatform());
+      }
+      break;
+      case B64:
+      switch (distribution.getPlatform()) {
+        case Linux:
+        case Windows:
+        bitsize = "-x64";
+        break;
+        case OS_X:
+        break;
+        default:
+        throw new IllegalArgumentException("64 bit supported only on Linux and Windows, platform is " + distribution.getPlatform());
+      }
+      break;
+      default:
+      throw new IllegalArgumentException("Unknown BitSize " + distribution.getBitsize());
     }
+    return "postgresql-" + sversion + "-" + splatform + bitsize + "-binaries" + "." + sarchiveType;
+  }
 }
