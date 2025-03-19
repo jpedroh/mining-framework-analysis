350d1d9b15f8f86091175f73dd27b5a9c63be773
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 21:07:37 +0000

    Mining Framework Analysis

--- cdi-test-core/src/main/java/de/hilling/junit/cdi/CdiTestJunitExtension.java
+++ cdi-test-core/src/main/java/de/hilling/junit/cdi/CdiTestJunitExtension.java
@@ -1,10 +1,6 @@
 package de.hilling.junit.cdi;
-
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
-import java.util.List;
-
-import org.jboss.weld.proxy.WeldClientProxy;
 import org.junit.jupiter.api.extension.AfterAllCallback;
 import org.junit.jupiter.api.extension.AfterEachCallback;
 import org.junit.jupiter.api.extension.BeforeAllCallback;
@@ -16,7 +12,6 @@ import org.junit.jupiter.api.extension.TestInstanceFactory;
 import org.junit.jupiter.api.extension.TestInstanceFactoryContext;
 import org.junit.jupiter.api.extension.TestInstantiationException;
 import org.mockito.Mockito;
-
 import de.hilling.junit.cdi.annotations.ActivatableTestImplementation;
 import de.hilling.junit.cdi.lifecycle.LifecycleNotifier;
 import de.hilling.junit.cdi.scope.InvocationTargetManager;
@@ -34,82 +29,89 @@ import de.hilling.junit.cdi.util.ReflectionsUtils;
  * </p>
  */
 public class CdiTestJunitExtension implements TestInstanceFactory, InvocationInterceptor, BeforeAllCallback, AfterAllCallback, BeforeEachCallback, AfterEachCallback {
+  static {
+    LoggerConfigurator.configure();
+  }
 
-    static {
-        LoggerConfigurator.configure();
+  private final InvocationTargetManager invocationTargetManager;
+
+  private final ContextControlWrapper contextControl = ContextControlWrapper.getInstance();
+
+  private final LifecycleNotifier lifecycleNotifier;
+
+  private TestEnvironment testEnvironment;
+
+  public CdiTestJunitExtension() {
+    invocationTargetManager = contextControl.getContextualReference(InvocationTargetManager.class);
+    lifecycleNotifier = contextControl.getContextualReference(LifecycleNotifier.class);
+  }
+
+  @Override public Object createTestInstance(TestInstanceFactoryContext factoryContext, ExtensionContext extensionContext) throws TestInstantiationException {
+    Class<?> testClass = factoryContext.getTestClass();
+    TestContext.activate();
+    contextControl.startContexts();
+
+<<<<<<< /usr/src/app/output/guhilling/cdi-test/d8cb85a75cdde55aed2e44322ba1e458a51ff41a/cdi-test-core/src/main/java/de/hilling/junit/cdi/CdiTestJunitExtension.java/left.java
+    Object
+=======
+    WeldClientProxy
+>>>>>>> /usr/src/app/output/guhilling/cdi-test/d8cb85a75cdde55aed2e44322ba1e458a51ff41a/cdi-test-core/src/main/java/de/hilling/junit/cdi/CdiTestJunitExtension.java/right.java
+     testInstance = 
+<<<<<<< /usr/src/app/output/guhilling/cdi-test/d8cb85a75cdde55aed2e44322ba1e458a51ff41a/cdi-test-core/src/main/java/de/hilling/junit/cdi/CdiTestJunitExtension.java/left.java
+    contextControl.getContextualReference(testClass)
+=======
+    (WeldClientProxy) contextControl.getContextualReference(testClass)
+>>>>>>> /usr/src/app/output/guhilling/cdi-test/d8cb85a75cdde55aed2e44322ba1e458a51ff41a/cdi-test-core/src/main/java/de/hilling/junit/cdi/CdiTestJunitExtension.java/right.java
+    ;
+    testEnvironment = contextControl.getContextualReference(TestEnvironment.class);
+    testEnvironment.setTestInstance(testInstance);
+    return 
+<<<<<<< /usr/src/app/output/guhilling/cdi-test/d8cb85a75cdde55aed2e44322ba1e458a51ff41a/cdi-test-core/src/main/java/de/hilling/junit/cdi/CdiTestJunitExtension.java/left.java
+    testInstance
+=======
+    testInstance.getMetadata().getContextualInstance()
+>>>>>>> /usr/src/app/output/guhilling/cdi-test/d8cb85a75cdde55aed2e44322ba1e458a51ff41a/cdi-test-core/src/main/java/de/hilling/junit/cdi/CdiTestJunitExtension.java/right.java
+    ;
+  }
+
+  @Override public void interceptTestMethod(Invocation<Void> invocation, ReflectiveInvocationContext<Method> invocationContext, ExtensionContext extensionContext) throws Throwable {
+    Object testProxy = testEnvironment.getTestInstance();
+    Method testMethod = testEnvironment.getTestMethod();
+    List<Object> arguments = invocationContext.getArguments();
+    testMethod.setAccessible(true);
+    testMethod.invoke(testProxy, arguments.toArray());
+    invocation.skip();
+  }
+
+  @Override public void beforeEach(ExtensionContext context) {
+    testEnvironment.setTestMethod(context.getRequiredTestMethod());
+    testEnvironment.setTestName(context.getDisplayName());
+    lifecycleNotifier.notify(TestState.STARTING, context);
+    for (Field field : ReflectionsUtils.getAllFields(testEnvironment.getTestClass())) {
+      if (isTestActivatable(field)) {
+        invocationTargetManager.activateAlternative(field.getType());
+      }
     }
+    lifecycleNotifier.notify(TestState.STARTED, context);
+  }
 
-    private final InvocationTargetManager invocationTargetManager;
-    private final ContextControlWrapper   contextControl = ContextControlWrapper.getInstance();
+  @Override public void afterEach(ExtensionContext context) {
+    lifecycleNotifier.notify(TestState.FINISHING, context);
+    contextControl.stopContexts();
+    lifecycleNotifier.notify(TestState.FINISHED, context);
+    TestContext.deactivate();
+  }
 
-    private final LifecycleNotifier lifecycleNotifier;
-    private       TestEnvironment   testEnvironment;
+  @Override public void beforeAll(ExtensionContext context) {
+    Mockito.framework().addListener(invocationTargetManager);
+  }
 
-    public CdiTestJunitExtension() {
-        invocationTargetManager = contextControl.getContextualReference(InvocationTargetManager.class);
-        lifecycleNotifier = contextControl.getContextualReference(LifecycleNotifier.class);
-    }
+  @Override public void afterAll(ExtensionContext context) {
+    Mockito.framework().removeListener(invocationTargetManager);
+  }
 
-    @Override
-    public Object createTestInstance(TestInstanceFactoryContext factoryContext,
-                                     ExtensionContext extensionContext) throws TestInstantiationException {
-        Class<?> testClass = factoryContext.getTestClass();
-        TestContext.activate();
-        contextControl.startContexts();
-        WeldClientProxy testInstance = (WeldClientProxy) contextControl.getContextualReference(testClass);
-        testEnvironment = contextControl.getContextualReference(TestEnvironment.class);
-        testEnvironment.setTestInstance(testInstance);
-        return testInstance.getMetadata().getContextualInstance();
-    }
-
-    @Override
-    public void interceptTestMethod(Invocation<Void> invocation, ReflectiveInvocationContext<Method> invocationContext,
-                                     ExtensionContext extensionContext) throws Throwable {
-        Object testProxy = testEnvironment.getTestInstance();
-        Method testMethod = testEnvironment.getTestMethod();
-        List<Object> arguments = invocationContext.getArguments();
-        testMethod.setAccessible(true);
-        testMethod.invoke(testProxy, arguments.toArray());
-        invocation.skip();
-    }
-
-
-    @Override
-    public void beforeEach(ExtensionContext context) {
-        testEnvironment.setTestMethod(context.getRequiredTestMethod());
-        testEnvironment.setTestName(context.getDisplayName());
-        lifecycleNotifier.notify(TestState.STARTING, context);
-        for (Field field : ReflectionsUtils.getAllFields(testEnvironment.getTestClass())) {
-            if (isTestActivatable(field)) {
-                invocationTargetManager.activateAlternative(field.getType());
-            }
-        }
-        lifecycleNotifier.notify(TestState.STARTED, context);
-    }
-
-    @Override
-    public void afterEach(ExtensionContext context) {
-        lifecycleNotifier.notify(TestState.FINISHING, context);
-        contextControl.stopContexts();
-        lifecycleNotifier.notify(TestState.FINISHED, context);
-        TestContext.deactivate();
-    }
-
-    @Override
-    public void beforeAll(ExtensionContext context) {
-        Mockito.framework()
-               .addListener(invocationTargetManager);
-    }
-
-    @Override
-    public void afterAll(ExtensionContext context) {
-        Mockito.framework()
-               .removeListener(invocationTargetManager);
-    }
-
-    private boolean isTestActivatable(Field field) {
-        Class<?> type = field.getType();
-        return type.isAnnotationPresent(ActivatableTestImplementation.class);
-    }
-
+  private boolean isTestActivatable(Field field) {
+    Class<?> type = field.getType();
+    return type.isAnnotationPresent(ActivatableTestImplementation.class);
+  }
 }
