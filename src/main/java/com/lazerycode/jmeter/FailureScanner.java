cce188d693e157c9988dfde74b21439203df67cb
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 19:50:29 +0000

    Mining Framework Analysis

--- src/main/java/com/lazerycode/jmeter/FailureScanner.java
+++ src/main/java/com/lazerycode/jmeter/FailureScanner.java
@@ -1,5 +1,4 @@
 package com.lazerycode.jmeter;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
@@ -11,66 +10,68 @@ import java.util.regex.Pattern;
  * @author Jon Roberts
  */
 class FailureScanner {
+  private static final String REQUEST_FAILURE_PATTERN = "s=\"false\"";
 
-	private static final String REQUEST_FAILURE_PATTERN = "s=\"false\"";
-	private static final String REQUEST_SUCCESS_PATTERN = "s=\"true\"";
-	private final boolean ignoreFailures;
-	private int failureCount;
-	private int successCount;
+  private static final String REQUEST_SUCCESS_PATTERN = "s=\"true\"";
 
-	public FailureScanner(boolean ignoreFailures) {
-		this.ignoreFailures = ignoreFailures;
-	}
+  private final boolean ignoreFailures;
 
-	/**
+  private int failureCount;
+
+  private int successCount;
+
+  public FailureScanner(boolean ignoreFailures) {
+    this.ignoreFailures = ignoreFailures;
+  }
+
+  /**
 	 * Check file for errors
 	 *
 	 * @return false if file doesn't contain failures
 	 */
-	public boolean hasTestFailed() {
-		return !this.ignoreFailures && this.failureCount > 0;
-	}
+  public boolean hasTestFailed() {
+    return !this.ignoreFailures && this.failureCount > 0;
+  }
 
-	/**
+  /**
 	 * Parse given results file
 	 *
 	 * @param file File to parse for results
 	 * @throws IOException
 	 */
-	public void parseResults(File file) throws IOException {
+  public void parseResults(File file) throws IOException {
+    failureCount = 0;
+    successCount = 0;
+    Scanner resultFileScanner;
+    Pattern errorPattern = Pattern.compile(REQUEST_FAILURE_PATTERN);
+    Pattern successPattern = Pattern.compile(REQUEST_SUCCESS_PATTERN);
+    resultFileScanner = new Scanner(file);
+    while (resultFileScanner.findWithinHorizon(errorPattern, 0) != null) {
+      failureCount++;
+    }
+    resultFileScanner.close();
+    resultFileScanner = new Scanner(file);
+    while (resultFileScanner.findWithinHorizon(successPattern, 0) != null) {
+      successCount++;
+    }
+    resultFileScanner.close();
+  }
 
-		failureCount = 0;
-		successCount = 0;
-
-		Scanner resultFileScanner;
-		Pattern errorPattern = Pattern.compile(REQUEST_FAILURE_PATTERN);
-		Pattern successPattern = Pattern.compile(REQUEST_SUCCESS_PATTERN);
-
-		resultFileScanner = new Scanner(file);
-		while (resultFileScanner.findWithinHorizon(errorPattern, 0) != null) {
-			failureCount++;
-		}
-		resultFileScanner.close();
-
-		resultFileScanner = new Scanner(file);
-		while (resultFileScanner.findWithinHorizon(successPattern, 0) != null) {
-			successCount++;
-		}
-		resultFileScanner.close();
-	}
-
-	/**
+  /**
 	 * @return failureCount
 	 */
-	public int getFailureCount() {
-		if (this.ignoreFailures) {
-			return 0;
-		} else {
-			return this.failureCount;
-		}
-	}
-	
-	public int getRequestCount() {
-		return this.failureCount + this.successCount;
-	}
+  public int getFailureCount() {
+    if (this.ignoreFailures) {
+      return 0;
+    } else {
+      return this.failureCount;
+    }
+  }
+
+  /**
+	 * @return requestCount
+	 */
+  public int getRequestCount() {
+    return this.failureCount + this.successCount;
+  }
 }
