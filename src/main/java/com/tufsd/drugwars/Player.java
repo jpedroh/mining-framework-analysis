b9f1f19c40a0682be4a151314562e78926e633b8
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 17:40:13 +0000

    Mining Framework Analysis

--- src/main/java/com/tufsd/drugwars/Player.java
+++ src/main/java/com/tufsd/drugwars/Player.java
@@ -1,54 +1,6 @@
-package com.tufsd.drugwars;
-
-
-/**
- * Write a description of class Player here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
-public class Player
-{
-    // instance variables - replace the example below with your own
-    private int y;
-
-    /**
-     * Constructor for objects of class Player
-     */
-    public int[] inventory;
-    public String name;
-   
-    public Player(String nameIn, int[] inventoryIn)
-    {
-        // initialise instance variables
-        y = 0;
-        name = nameIn;
-        inventory = inventoryIn;
-        //Inv:
-       
-        //stats
-        
-        int health = 100; //it's health, what do you think
-        int dunkness = 0; //the more you drink, the higher it is
-        int highness = 0; //you know what it is
-        int stamina = 100; //how long you run
-        int tiredness = 10; //get sleep
-        
-        //Goals
-
-        int days = 31; //days until the loan shark comes
-        int bounty = 0; //illigal things = bigger bounty
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return y + y;
-    }
-}
+  package   com . tufsd . drugwars ;   public class Player  {   private  int  y ; 
+<<<<<<<
+  public Player  ( )  {   y = 0 ;   int  money = 250 ;   int  ammo = 18 ;   int  beer = 0 ;   int  pot = 0 ;   int  coke = 0 ;   int  meth = 0 ;   int  bong = 0 ;   int  lighter = 0 ;   int  health = 100 ;   int  dunkness = 0 ;   int  highness = 0 ;   int  stamina = 100 ;   int  tiredness = 10 ;   int  days = 31 ;   int  bounty = 0 ; }
+=======
+>>>>>>>
+   public  int sampleMethod  (   int y )  {  return  y + y ; }   public   int  [ ]  inventory ;   public String  name ;   public Player  (  String nameIn ,    int  [ ] inventoryIn )  {   y = 0 ;   name = nameIn ;   inventory = inventoryIn ;   int  health = 100 ;   int  dunkness = 0 ;   int  highness = 0 ;   int  stanima = 100 ;   int  tiredness = 10 ;   int  days = 31 ;   int  bounty = 0 ; } }
