18f38e9cea8e26d3d9133b125670ed816d0b2608
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 21:00:41 +0000

    Mining Framework Analysis

--- src/main/java/strat/mining/stratum/proxy/worker/WorkerConnection.java
+++ src/main/java/strat/mining/stratum/proxy/worker/WorkerConnection.java
@@ -4,14 +4,96 @@ import java.util.Date;
 import java.util.Set;
 
 import strat.mining.stratum.proxy.exception.ChangeExtranonceNotSupportedException;
+
 import strat.mining.stratum.proxy.exception.TooManyWorkersException;
+
 import strat.mining.stratum.proxy.json.MiningNotifyNotification;
+
 import strat.mining.stratum.proxy.json.MiningSetDifficultyNotification;
+
 import strat.mining.stratum.proxy.json.MiningSubmitRequest;
+
 import strat.mining.stratum.proxy.json.MiningSubmitResponse;
+
 import strat.mining.stratum.proxy.model.Share;
+
 import strat.mining.stratum.proxy.network.Connection;
+
 import strat.mining.stratum.proxy.pool.Pool;
+
+import strat.mining.stratum.proxy.utils.Timer;
+
+import strat.mining.stratum.proxy.utils.Timer.Task;
+
+public class WorkerConnection extends StratumConnection {
+
+	private Task subscribeTimeoutTask;
+
+	@Override
+	public void startReading() {
+		super.startReading();
+		subscribeTimeoutTask = new Task() {
+			public void run() {
+				LOGGER.warn("No subscribe request received from {} in {} ms. Closing connection.", getConnectionName(), subscribeReceiveTimeout);
+				// Close the connection if subscribe request is not received at
+				// time.
+				close();
+			}
+		};
+		Timer.getInstance().schedule(subscribeTimeoutTask, subscribeReceiveTimeout);
+	}
+
+	@Override
+	protected void onSubscribeRequest(MiningSubscribeRequest request) {
+		// Once the subscribe request is received, cancel the timeout timer.
+		if (subscribeTimeoutTask != null) {
+			subscribeTimeoutTask.cancel();
+		}
+
+		JsonRpcError error = null;
+		try {
+			pool = manager.onSubscribeRequest(this, request);
+		} catch (NoPoolAvailableException e) {
+			LOGGER.error("No pool available for the connection {}. Sending error and close the connection.", getConnectionName());
+			error = new JsonRpcError();
+			error.setCode(JsonRpcError.ErrorCode.UNKNOWN.getCode());
+			error.setMessage("No pool available on this proxy.");
+		}
+
+		if (error == null) {
+			try {
+				extranonce1Tail = pool.getFreeTail();
+				extranonce2Size = pool.getWorkerExtranonce2Size();
+			} catch (TooManyWorkersException e) {
+				LOGGER.error("Too many connections on pool {} for the connection {}. Sending error and close the connection.", pool.getName(),
+						getConnectionName(), e);
+				error = new JsonRpcError();
+				error.setCode(JsonRpcError.ErrorCode.UNKNOWN.getCode());
+				error.setMessage("Too many connection on the pool.");
+			}
+		}
+
+		// Send the subscribe response
+		MiningSubscribeResponse response = new MiningSubscribeResponse();
+		response.setId(request.getId());
+		if (error != null) {
+			response.setErrorRpc(error);
+		} else {
+			response.setExtranonce1(pool.getExtranonce1() + extranonce1Tail);
+			response.setExtranonce2Size(extranonce2Size);
+			response.setSubscriptionDetails(getSubscibtionDetails());
+			isActiveSince = new Date();
+		}
+
+		sendResponse(response);
+
+		// If the subscribe succeed, send the initial notifications (difficulty
+		// and notify).
+		if (error == null) {
+			sendInitialNotifications();
+		}
+	}
+}
 
 public interface WorkerConnection extends Connection {
 
