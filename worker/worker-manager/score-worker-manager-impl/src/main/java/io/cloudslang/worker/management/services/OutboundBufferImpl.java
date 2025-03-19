1c2f0c39eebc8718040f7edd906cc5332dab1fdc
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 14:19:02 +0000

    Mining Framework Analysis

--- worker/worker-manager/score-worker-manager-impl/src/main/java/io/cloudslang/worker/management/services/OutboundBufferImpl.java
+++ worker/worker-manager/score-worker-manager-impl/src/main/java/io/cloudslang/worker/management/services/OutboundBufferImpl.java
@@ -20,7 +20,6 @@ import io.cloudslang.engine.queue.entities.ExecutionMessage;
 import io.cloudslang.orchestrator.entities.Message;
 import io.cloudslang.orchestrator.services.OrchestratorDispatcherService;
 import io.cloudslang.worker.management.ExecutionsActivityListener;
-import org.apache.commons.lang3.ArrayUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,7 +27,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
