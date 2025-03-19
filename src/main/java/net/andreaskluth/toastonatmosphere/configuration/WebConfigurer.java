d718b445ecb0d2da86aaabfc3b4af55b7d645ed1
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 18:54:09 +0000

    Mining Framework Analysis

--- src/main/java/net/andreaskluth/toastonatmosphere/configuration/WebConfigurer.java
+++ src/main/java/net/andreaskluth/toastonatmosphere/configuration/WebConfigurer.java
@@ -1,62 +1,6 @@
-package net.andreaskluth.toastonatmosphere.configuration;
-
-
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration;
-
-import net.andreaskluth.toastonatmosphere.websocket.ToastService;
-
-import org.atmosphere.cache.UUIDBroadcasterCache;
-import org.atmosphere.cpr.ApplicationConfig;
-import org.atmosphere.cpr.AtmosphereFramework;
-import org.atmosphere.cpr.AtmosphereServlet;
-import org.atmosphere.cpr.MetaBroadcaster;
-import org.springframework.boot.context.embedded.ServletContextInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-/**
- * Configures an {@link AtmosphereServlet} and announces it to the
- * {@link ServletContext}.
- * 
- * @author Andreas Kluth
- */
-@Configuration
-public class WebConfigurer implements ServletContextInitializer {
-
-  @Bean
-  public AtmosphereServlet atmosphereServlet(){
-    return new AtmosphereServlet();
-  }
-
-  @Bean
-  public AtmosphereFramework atmosphereFramework() {
-    return atmosphereServlet().framework();
-  }
-
-  @Bean
-  public MetaBroadcaster metaBroadcaster() {
-    AtmosphereFramework framework = atmosphereFramework();
-    return framework.metaBroadcaster();
-  }
-  
-  @Override
-  public void onStartup(ServletContext servletContext) throws ServletException {
-    configureAthmosphere(atmosphereServlet(), servletContext);
-  }
-
-  private void configureAthmosphere(AtmosphereServlet servlet, ServletContext servletContext) {
-    ServletRegistration.Dynamic atmosphereServlet = servletContext.addServlet("atmosphereServlet", servlet);
-    atmosphereServlet.setInitParameter(ApplicationConfig.ANNOTATION_PACKAGE, ToastService.class.getPackage().getName());
-    atmosphereServlet.setInitParameter(ApplicationConfig.BROADCASTER_CACHE, UUIDBroadcasterCache.class.getName());
-    atmosphereServlet.setInitParameter(ApplicationConfig.BROADCASTER_SHARABLE_THREAD_POOLS, "true");
-    atmosphereServlet.setInitParameter(ApplicationConfig.BROADCASTER_MESSAGE_PROCESSING_THREADPOOL_MAXSIZE, "10");
-    atmosphereServlet.setInitParameter(ApplicationConfig.BROADCASTER_ASYNC_WRITE_THREADPOOL_MAXSIZE, "10");
-    servletContext.addListener(new org.atmosphere.cpr.SessionSupport());
-    atmosphereServlet.addMapping("/websocket/*");
-    atmosphereServlet.setLoadOnStartup(0);
-    atmosphereServlet.setAsyncSupported(true);
-  }
-
-}
+  package    net . andreaskluth . toastonatmosphere . configuration ;   import   javax . servlet . ServletContext ;  import   javax . servlet . ServletException ;  import   javax . servlet . ServletRegistration ;  import     net . andreaskluth . toastonatmosphere . websocket . ToastService ;  import    org . atmosphere . cache . UUIDBroadcasterCache ;  import    org . atmosphere . cpr . ApplicationConfig ;  import    org . atmosphere . cpr . AtmosphereFramework ;  import    org . atmosphere . cpr . AtmosphereServlet ;  import    org . atmosphere . cpr . MetaBroadcaster ;  import      org . springframework . boot . context . embedded . ServletContextInitializer ;  import     org . springframework . context . annotation . Bean ;  import     org . springframework . context . annotation . Configuration ;  import   java . util . Arrays ;  import    org . apache . catalina . Context ;  import      org . apache . tomcat . websocket . server . WsSci ;  import       org . springframework . boot . context . embedded . tomcat . TomcatContextCustomizer ;  import       org . springframework . boot . context . embedded . tomcat . TomcatEmbeddedServletContainerFactory ;    @ Configuration public class WebConfigurer  implements  ServletContextInitializer  {    @ Bean public AtmosphereServlet atmosphereServlet  ( )  {  return  new AtmosphereServlet  ( ) ; }    @ Bean public AtmosphereFramework atmosphereFramework  ( )  {  return   atmosphereServlet  ( ) . framework  ( ) ; }    @ Bean public MetaBroadcaster metaBroadcaster  ( )  {  AtmosphereFramework  framework =  atmosphereFramework  ( ) ;  return  framework . metaBroadcaster  ( ) ; }    @ Override public void onStartup  (  ServletContext servletContext )  throws ServletException  {   configureAthmosphere  (  atmosphereServlet  ( ) , servletContext ) ; }   private void configureAthmosphere  (  AtmosphereServlet servlet ,  ServletContext servletContext )  {   ServletRegistration . Dynamic  atmosphereServlet =  servletContext . addServlet  ( "atmosphereServlet" , servlet ) ;   atmosphereServlet . setInitParameter  (  ApplicationConfig . ANNOTATION_PACKAGE ,    ToastService . class . getPackage  ( ) . getName  ( ) ) ;   atmosphereServlet . setInitParameter  (  ApplicationConfig . BROADCASTER_CACHE ,   UUIDBroadcasterCache . class . getName  ( ) ) ;   atmosphereServlet . setInitParameter  (  ApplicationConfig . BROADCASTER_SHARABLE_THREAD_POOLS , "true" ) ;   atmosphereServlet . setInitParameter  (  ApplicationConfig . BROADCASTER_MESSAGE_PROCESSING_THREADPOOL_MAXSIZE , "10" ) ;   atmosphereServlet . setInitParameter  (  ApplicationConfig . BROADCASTER_ASYNC_WRITE_THREADPOOL_MAXSIZE , "10" ) ;   atmosphereServlet . setInitParameter  (  ApplicationConfig . JSR356_MAPPING_PATH , "/websocket/*" ) ;   servletContext . addListener  (  new    org . atmosphere . cpr . SessionSupport  ( ) ) ;   atmosphereServlet . addMapping  ( "/websocket/*" ) ;   atmosphereServlet . setLoadOnStartup  ( 0 ) ;   atmosphereServlet . setAsyncSupported  ( true ) ; }    @ Bean public TomcatEmbeddedServletContainerFactory tomcatContainerFactory  ( )  {  TomcatEmbeddedServletContainerFactory  factory =  new TomcatEmbeddedServletContainerFactory  ( ) ;   factory . setTomcatContextCustomizers  (  Arrays . asList  (  new TomcatContextCustomizer  [ ]  {  tomcatContextCustomizer  ( ) } ) ) ;  return factory ; }    @ Bean public TomcatContextCustomizer tomcatContextCustomizer  ( )  {  return  new TomcatContextCustomizer  ( )  {    @ Override public void customize  (  Context context )  {   context . addServletContainerInitializer  (  new WsSci  ( ) , null ) ; } } ; } 
+<<<<<<<
+=======
+  private void configureAthmosphere  (  AtmosphereFramework framework ,  ServletContext servletContext )  {  AtmosphereServlet  servlet =  new AtmosphereServlet  ( ) ;  Field  frameworkField =  ReflectionUtils . findField  (  AtmosphereServlet . class , "framework" ) ;   ReflectionUtils . makeAccessible  ( frameworkField ) ;   ReflectionUtils . setField  ( frameworkField , servlet , framework ) ;   ServletRegistration . Dynamic  atmosphereServlet =  servletContext . addServlet  ( "atmosphereServlet" , servlet ) ;   atmosphereServlet . setInitParameter  (  ApplicationConfig . ANNOTATION_PACKAGE ,    ToastService . class . getPackage  ( ) . getName  ( ) ) ;   atmosphereServlet . setInitParameter  (  ApplicationConfig . BROADCASTER_CACHE ,   UUIDBroadcasterCache . class . getName  ( ) ) ;   atmosphereServlet . setInitParameter  (  ApplicationConfig . BROADCASTER_SHARABLE_THREAD_POOLS , "true" ) ;   atmosphereServlet . setInitParameter  (  ApplicationConfig . BROADCASTER_MESSAGE_PROCESSING_THREADPOOL_MAXSIZE , "10" ) ;   atmosphereServlet . setInitParameter  (  ApplicationConfig . BROADCASTER_ASYNC_WRITE_THREADPOOL_MAXSIZE , "10" ) ;   servletContext . addListener  (  new    org . atmosphere . cpr . SessionSupport  ( ) ) ;   atmosphereServlet . addMapping  ( "/websocket/*" ) ;   atmosphereServlet . setLoadOnStartup  ( 0 ) ;   atmosphereServlet . setAsyncSupported  ( true ) ; }
+>>>>>>>
+ }
--- src/test/java/net/andreaskluth/toastonatmosphere/security/FakeAuthenticationProviderTest.java
+++ src/test/java/net/andreaskluth/toastonatmosphere/security/FakeAuthenticationProviderTest.java
@@ -1,51 +1,7 @@
-package net.andreaskluth.toastonatmosphere.security;
-
-import org.junit.Assert;
-import org.junit.Test;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.TestingAuthenticationToken;
-import org.springframework.security.core.Authentication;
-
-/**
- * Test suite for {@link FakeAuthenticationProvider}.
- * 
- * @author Andreas Kluth
- */
-public class FakeAuthenticationProviderTest {
-
-  @Test
-  public void arbitraryCasedNameReturnsValidAuthentication() {
-    AuthenticationProvider provider = createProvider();
-    Authentication authentication = provider.authenticate(createAuthenticationTokenWithUserAndPw("AdMiN", "adm1n"));
-    Assert.assertNotNull(authentication);
-  }
-
-  @Test
-  public void validPasswordReturnsValidAuthentication() {
-    AuthenticationProvider provider = createProvider();
-    Authentication authentication = provider.authenticate(createAuthenticationTokenWithUserAndPw("admin", "adm1n"));
-    Assert.assertNotNull(authentication);
-  }
-
-  @Test(expected = BadCredentialsException.class)
-  public void invalidPasswordRaises() {
-    AuthenticationProvider provider = createProvider();
-    provider.authenticate(createAuthenticationTokenWithUserAndPw("admin", "admin"));
-  }
-
-  @Test(expected = BadCredentialsException.class)
-  public void invalidUserRaises() {
-    AuthenticationProvider provider = createProvider();
-    provider.authenticate(createAuthenticationTokenWithUserAndPw("admon", "adm1n"));
-  }
-
-  private AuthenticationProvider createProvider() {
-    return new FakeAuthenticationProvider();
-  }
-
-  private TestingAuthenticationToken createAuthenticationTokenWithUserAndPw(String username, String password) {
-    return new TestingAuthenticationToken(username, password);
-  }
-
-}
+  fatal :  path  'src/test/java/net/andreaskluth/toastonatmosphere/security/FakeAuthenticationProviderTest.java' exists on  disk ,  but  
+<<<<<<<
+  import   org . junit . Assert ;  import   org . junit . Test ;  import     org . springframework . security . authentication . AuthenticationProvider ;  import     org . springframework . security . authentication . BadCredentialsException ;  import     org . springframework . security . authentication . TestingAuthenticationToken ;  import     org . springframework . security . core . Authentication ;
+=======
+ not in 'ca84722195aac40f62ec2db5eed3eea4273a6ff8'
+>>>>>>>
+   public class FakeAuthenticationProviderTest  {    @ Test public void arbitraryCasedNameReturnsValidAuthentication  ( )  {  AuthenticationProvider  provider =  createProvider  ( ) ;  Authentication  authentication =  provider . authenticate  (  createAuthenticationTokenWithUserAndPw  ( "AdMiN" , "adm1n" ) ) ;   Assert . assertNotNull  ( authentication ) ; }    @ Test public void validPasswordReturnsValidAuthentication  ( )  {  AuthenticationProvider  provider =  createProvider  ( ) ;  Authentication  authentication =  provider . authenticate  (  createAuthenticationTokenWithUserAndPw  ( "admin" , "adm1n" ) ) ;   Assert . assertNotNull  ( authentication ) ; }    @ Test  (  expected =  BadCredentialsException . class ) public void invalidPasswordRaises  ( )  {  AuthenticationProvider  provider =  createProvider  ( ) ;   provider . authenticate  (  createAuthenticationTokenWithUserAndPw  ( "admin" , "admin" ) ) ; }    @ Test  (  expected =  BadCredentialsException . class ) public void invalidUserRaises  ( )  {  AuthenticationProvider  provider =  createProvider  ( ) ;   provider . authenticate  (  createAuthenticationTokenWithUserAndPw  ( "admon" , "adm1n" ) ) ; }   private AuthenticationProvider createProvider  ( )  {  return  new FakeAuthenticationProvider  ( ) ; }   private TestingAuthenticationToken createAuthenticationTokenWithUserAndPw  (  String username ,  String password )  {  return  new TestingAuthenticationToken  ( username , password ) ; } }
