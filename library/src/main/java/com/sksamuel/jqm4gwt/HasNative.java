f7979bccf92d8b44e46de160d0f123dfc623be10
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 23:30:59 +0000

    Mining Framework Analysis

--- library/src/main/java/com/sksamuel/jqm4gwt/HasNative.java
+++ library/src/main/java/com/sksamuel/jqm4gwt/HasNative.java
@@ -1,15 +1,1 @@
-package com.sksamuel.jqm4gwt;
-
-/**
- * @author Stephen K Samuel samspade79@gmail.com 5 May 2011 10:59:06
- * <br>
- * Interface for classes that enable switching between native and jqm rendering modes.
- */
-public interface HasNative<T> {
-
-    boolean isNative();
-
-    void setNative(boolean b);
-
-    T withNative(boolean b);
-}
+  package   com . sksamuel . jqm4gwt ;   public interface HasNative  <  T >  {  boolean isNative  ( ) ;  void setNative  (  boolean b ) ;  T withNative  (  boolean b ) ; }
