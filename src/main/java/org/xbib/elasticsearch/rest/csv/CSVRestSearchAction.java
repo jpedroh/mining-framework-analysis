961b848b787c3a80b88599d90b0e05dd0176cf1f
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 18:52:43 +0000

    Mining Framework Analysis

--- src/main/java/org/xbib/elasticsearch/rest/csv/CSVRestSearchAction.java
+++ src/main/java/org/xbib/elasticsearch/rest/csv/CSVRestSearchAction.java
@@ -34,7 +34,8 @@ public class CSVRestSearchAction extends BaseRestHandler {
 
     @Override
     public void handleRequest(final RestRequest request, final RestChannel channel, final Client client) {
-        SearchRequest searchRequest = RestSearchAction.parseSearchRequest(request);
+        SearchRequest searchRequest;
+        searchRequest = RestSearchAction.parseSearchRequest(request);
         searchRequest.listenerThreaded(false);
         client.search(searchRequest, new CSVToXContentListener(channel, request.paramAsStringArray("keys", null)));
     }
