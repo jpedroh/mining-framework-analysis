87d7afc7a5733f184e4dfe379213ebbf623ca2d8
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 19:54:23 +0000

    Mining Framework Analysis

--- jmetal-core/src/main/java/org/uma/jmetal/util/JMetalLogger.java
+++ jmetal-core/src/main/java/org/uma/jmetal/util/JMetalLogger.java
@@ -1,13 +1,1 @@
-package org.uma.jmetal.util;
-
-import java.io.Serializable;
-import java.util.logging.Logger;
-
-/**
- * @author Antonio J. Nebro <antonio@lcc.uma.es>
- */
-public class JMetalLogger implements Serializable {
-
-	public static final Logger logger = Logger.getLogger(JMetalLogger.class
-			.getName());
-}
+  package    org . uma . jmetal . util ;   import   java . io . Serializable ;  import    java . util . logging . Logger ;   public class JMetalLogger  implements  Serializable  {   public static final Logger  logger =  Logger . getLogger  (   JMetalLogger . class . getName  ( ) ) ; }
