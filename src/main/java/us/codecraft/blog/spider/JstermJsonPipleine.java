acac374aeef76c8544aa3abbc2bdc1b1f1497ded
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 23:10:49 +0000

    Mining Framework Analysis

--- src/main/java/us/codecraft/blog/spider/JstermJsonPipleine.java
+++ src/main/java/us/codecraft/blog/spider/JstermJsonPipleine.java
@@ -1,5 +1,4 @@
 package us.codecraft.blog.spider;
-
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.google.common.collect.ArrayListMultimap;
@@ -9,7 +8,6 @@ import us.codecraft.blog.jsterm.File;
 import us.codecraft.blog.jsterm.Text;
 import us.codecraft.webmagic.Task;
 import us.codecraft.webmagic.pipeline.PageModelPipeline;
-
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -20,42 +18,40 @@ import java.util.List;
  * @author code4crafter@gmail.com
  */
 public class JstermJsonPipleine implements PageModelPipeline<OschinaBlog> {
+  private ArrayListMultimap<String, OschinaBlog> blogCategoryMap = ArrayListMultimap.create();
 
-    private ArrayListMultimap<String, OschinaBlog> blogCategoryMap = ArrayListMultimap.create();
+  private String filePath;
 
-    private String filePath;
+  public JstermJsonPipleine(String filePath) {
+    this.filePath = filePath;
+  }
 
-    public JstermJsonPipleine(String filePath) {
-        this.filePath = filePath;
+  @Override public void process(OschinaBlog oschinaBlog, Task task) {
+    blogCategoryMap.put(oschinaBlog.getCategory(), oschinaBlog);
+  }
+
+  public void flush() {
+    Dir root = new Dir("blog");
+    List<File> blogCategoryDirs = new ArrayList<File>();
+    root.setContents(blogCategoryDirs);
+    for (String key : blogCategoryMap.keySet()) {
+      Dir categoryDir = new Dir(key);
+      blogCategoryDirs.add(categoryDir);
+      List<OschinaBlog> oschinaBlogList = blogCategoryMap.get(key);
+      List<Text> blogList = new ArrayList<Text>();
+      Collections.sort(oschinaBlogList);
+      categoryDir.setContents(blogList);
+      for (OschinaBlog oschinaBlog : oschinaBlogList) {
+        Text blog = new Text(oschinaBlog.getTitle());
+        blog.setContents("<h2>" + oschinaBlog.getTitle() + "</h2>" + oschinaBlog.getContent());
+        blog.setDescription(DateFormatUtils.format(oschinaBlog.getDate(), "yyyy-MM-dd HH:mm"));
+        blogList.add(blog);
+      }
     }
-
-    @Override
-    public void process(OschinaBlog oschinaBlog, Task task) {
-        blogCategoryMap.put(oschinaBlog.getCategory(), oschinaBlog);
+    try {
+      JSON.writeJSONStringTo(root, new FileWriter(filePath + java.io.File.separator + "oschina.json"), SerializerFeature.PrettyFormat, SerializerFeature.BrowserCompatible);
+    } catch (IOException e) {
+      e.printStackTrace();
     }
-
-    public void flush() {
-        Dir root = new Dir("blog");
-        List<File> blogCategoryDirs = new ArrayList<File>();
-        root.setContents(blogCategoryDirs);
-        for (String key : blogCategoryMap.keySet()) {
-            Dir categoryDir = new Dir(key);
-            blogCategoryDirs.add(categoryDir);
-            List<OschinaBlog> oschinaBlogList = blogCategoryMap.get(key);
-            List<Text> blogList = new ArrayList<Text>();
-            Collections.sort(oschinaBlogList);
-            categoryDir.setContents(blogList);
-            for (OschinaBlog oschinaBlog : oschinaBlogList) {
-                Text blog = new Text(oschinaBlog.getTitle());
-                blog.setContents("<h2>" + oschinaBlog.getTitle() + "</h2>" + oschinaBlog.getContent());
-                blog.setDescription(DateFormatUtils.format(oschinaBlog.getDate(), "yyyy-MM-dd HH:mm"));
-                blogList.add(blog);
-            }
-        }
-        try {
-            JSON.writeJSONStringTo(root, new FileWriter(filePath + java.io.File.separator + "oschina.json"), SerializerFeature.PrettyFormat,SerializerFeature.BrowserCompatible);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+  }
 }
--- src/main/java/us/codecraft/blog/spider/OschinaBlog.java
+++ src/main/java/us/codecraft/blog/spider/OschinaBlog.java
@@ -1,5 +1,4 @@
 package us.codecraft.blog.spider;
-
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Site;
 import us.codecraft.webmagic.model.AfterExtractor;
@@ -8,61 +7,54 @@ import us.codecraft.webmagic.model.annotation.ExtractBy;
 import us.codecraft.webmagic.model.annotation.Formatter;
 import us.codecraft.webmagic.model.annotation.HelpUrl;
 import us.codecraft.webmagic.model.annotation.TargetUrl;
-
 import java.util.Date;
 
 /**
  * @author code4crafter@gmail.com
  */
-@TargetUrl("http://my.oschina.net/flashsword/blog/\\d+")
-@HelpUrl("http://my.oschina.net/flashsword/blog\\?disp=1&catalog=0&sort=time&p=\\d+")
-public class OschinaBlog implements Comparable<OschinaBlog>,AfterExtractor{
+@TargetUrl(value = "http://my.oschina.net/flashsword/blog/\\d+") @HelpUrl(value = "http://my.oschina.net/flashsword/blog\\?disp=1&catalog=0&sort=time&p=\\d+") public class OschinaBlog implements Comparable<OschinaBlog>, AfterExtractor {
+  @ExtractBy(value = "//title/regex(\'>(.+?)\\s+\\-\',1)") private String title;
 
-    @ExtractBy("//title/regex('>(.+?)\\s+\\-',1)")
-    private String title;
+  @ExtractBy(value = "//div[@class=BlogContent]") private String content;
 
-    @ExtractBy("//div[@class=BlogContent]")
-    private String content;
+  @ExtractBy(value = "//span[@class=\"catalogs\"]//a/text()") private String category;
 
-    @ExtractBy("//span[@class=\"catalogs\"]//a/text()")
-    private String category;
+  @Formatter(value = "yyyy-MM-dd HH:mm") @ExtractBy(value = "//div[@class=\'BlogStat\']/regex(\'\\d+-\\d+-\\d+\\s+\\d+:\\d+\')") private Date date;
 
-    @Formatter("yyyy-MM-dd HH:mm")
-    @ExtractBy("//div[@class='BlogStat']/regex('\\d+-\\d+-\\d+\\s+\\d+:\\d+')")
-    private Date date;
+  public static void main(String[] args) {
+    JstermJsonPipleine jstermJsonPipleine = new JstermJsonPipleine(
+<<<<<<< /usr/src/app/output/code4craft/termblog/794189f4401f172745bbc2ab1e17edf70a476b66/src/main/java/us/codecraft/blog/spider/OschinaBlog.java/left.java
+    "/data/oschinablog/"
+=======
+    "/Users/yihua/codecraft/blog/json"
+>>>>>>> /usr/src/app/output/code4craft/termblog/794189f4401f172745bbc2ab1e17edf70a476b66/src/main/java/us/codecraft/blog/spider/OschinaBlog.java/right.java
+    );
+    OOSpider.create(Site.me().setSleepTime(100).setUserAgent("Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)"), jstermJsonPipleine, OschinaBlog.class).addUrl("http://my.oschina.net/flashsword/blog").thread(1).run();
+    jstermJsonPipleine.flush();
+  }
 
-    public static void main(String[] args) {
-        JstermJsonPipleine jstermJsonPipleine = new JstermJsonPipleine("/Users/yihua/codecraft/blog/json");
-        OOSpider.create(Site.me().setSleepTime(100).setUserAgent("Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)"),
-                jstermJsonPipleine, OschinaBlog.class)
-                .addUrl("http://my.oschina.net/flashsword/blog").thread(1).run();
-        jstermJsonPipleine.flush();
-    }
+  public String getTitle() {
+    return title;
+  }
 
-    public String getTitle() {
-        return title;
-    }
+  public String getContent() {
+    return content;
+  }
 
-    public String getContent() {
-        return content;
-    }
+  public String getCategory() {
+    return category;
+  }
 
-    public String getCategory() {
-        return category;
-    }
+  public Date getDate() {
+    return date;
+  }
 
-    public Date getDate() {
-        return date;
-    }
+  @Override public int compareTo(OschinaBlog o) {
+    return date.compareTo(o.date);
+  }
 
-    @Override
-    public int compareTo(OschinaBlog o) {
-        return date.compareTo(o.date);
-    }
-
-    @Override
-    public void afterProcess(Page page) {
-        title = title.replaceAll("\\s+","_");
-        category = category.replaceAll("\\s+","_");
-    }
+  @Override public void afterProcess(Page page) {
+    title = title.replaceAll("\\s+", "_");
+    category = category.replaceAll("\\s+", "_");
+  }
 }
