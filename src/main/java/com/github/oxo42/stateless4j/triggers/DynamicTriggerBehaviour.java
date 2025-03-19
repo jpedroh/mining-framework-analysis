8f411d2211cce6c0bb39776d6bd3f57e4c7da12b
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 22:42:40 +0000

    Mining Framework Analysis

--- src/main/java/com/github/oxo42/stateless4j/triggers/DynamicTriggerBehaviour.java
+++ src/main/java/com/github/oxo42/stateless4j/triggers/DynamicTriggerBehaviour.java
@@ -1,22 +1,6 @@
-package com.github.oxo42.stateless4j.triggers;
-
-import com.github.oxo42.stateless4j.OutVar;
-import com.github.oxo42.stateless4j.delegates.Func2;
-import com.github.oxo42.stateless4j.delegates.FuncBoolean;
-import com.github.oxo42.stateless4j.validation.Enforce;
-
-public class DynamicTriggerBehaviour<TState, TTrigger> extends TriggerBehaviour<TState, TTrigger> {
-
-    private final Func2<Object[], TState> destination;
-
-    public DynamicTriggerBehaviour(TTrigger trigger, Func2<Object[], TState> destination, FuncBoolean guard) {
-        super(trigger, guard);
-        assert destination != null : "destination is null";
-        this.destination = destination;
-    }
-
-    public boolean resultsInTransitionFrom(TState source, Object[] args, OutVar<TState> dest) {
-        dest.set(destination.call(args));
-        return true;
-    }
-}
+  package     com . github . oxo42 . stateless4j . triggers ;   import     com . github . oxo42 . stateless4j . OutVar ;  import      com . github . oxo42 . stateless4j . delegates . Func2 ;  import      com . github . oxo42 . stateless4j . delegates . FuncBoolean ;   public class DynamicTriggerBehaviour  <  TState ,  TTrigger >  extends  TriggerBehaviour  < TState , TTrigger >  {   private final  Func2  <  Object  [ ] , TState >  destination ;   public DynamicTriggerBehaviour  (  TTrigger trigger ,   Func2  <  Object  [ ] , TState > destination ,  FuncBoolean guard )  {  super  ( trigger , guard ) ;    this . destination =  Enforce . argumentNotNull  ( destination , "destination" ) ; }   public boolean resultsInTransitionFrom  (  TState source ,   Object  [ ] args ,   OutVar  < TState > dest )  {   dest . set  (  destination . call  ( args ) ) ;  return true ; } 
+<<<<<<<
+=======
+  public DynamicTriggerBehaviour  (   final TTrigger trigger ,   final  Func2  <  Object  [ ] , TState > destination ,   final  Func  < Boolean > guard )  {  super  ( trigger , guard ) ;  assert  destination != null : "destination is null" ;    this . destination = destination ; }
+>>>>>>>
+ }
