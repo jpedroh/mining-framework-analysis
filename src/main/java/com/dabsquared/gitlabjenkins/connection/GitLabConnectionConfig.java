b81ec58c0166971114fb4aef7eddc6eec5caed66
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 19:06:13 +0000

    Mining Framework Analysis

--- src/main/java/com/dabsquared/gitlabjenkins/connection/GitLabConnectionConfig.java
+++ src/main/java/com/dabsquared/gitlabjenkins/connection/GitLabConnectionConfig.java
@@ -1,6 +1,4 @@
 package com.dabsquared.gitlabjenkins.connection;
-
-
 import com.cloudbees.plugins.credentials.Credentials;
 import com.cloudbees.plugins.credentials.CredentialsMatcher;
 import com.cloudbees.plugins.credentials.common.AbstractIdCredentialsListBoxModel;
@@ -25,178 +23,161 @@ import org.kohsuke.stapler.StaplerRequest;
 import org.kohsuke.stapler.interceptor.RequirePOST;
 import org.kohsuke.accmod.Restricted;
 import org.kohsuke.accmod.restrictions.DoNotUse;
-
 import javax.ws.rs.ProcessingException;
 import javax.ws.rs.WebApplicationException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import static com.dabsquared.gitlabjenkins.gitlab.api.GitLabClientBuilder.getAllGitLabClientBuilders;
-
 
 /**
  * @author Robin Müller
  */
-@Extension
-public class GitLabConnectionConfig extends GlobalConfiguration {
+@Extension public class GitLabConnectionConfig extends GlobalConfiguration {
+  private Boolean useAuthenticatedEndpoint = true;
 
-    private Boolean useAuthenticatedEndpoint = true;
-    private List<GitLabConnection> connections = new ArrayList<>();
-    private transient Map<String, GitLabConnection> connectionMap = new HashMap<>();
+  private List<GitLabConnection> connections = new ArrayList<>();
 
-    public GitLabConnectionConfig() {
-        load();
-        refreshConnectionMap();
+  private transient Map<String, GitLabConnection> connectionMap = new HashMap<>();
+
+  public GitLabConnectionConfig() {
+    load();
+    refreshConnectionMap();
+  }
+
+  @Override public boolean configure(StaplerRequest req, JSONObject json) throws FormException {
+    connections = req.bindJSONToList(GitLabConnection.class, json.get("connections"));
+    useAuthenticatedEndpoint = json.getBoolean("useAuthenticatedEndpoint");
+    refreshConnectionMap();
+    save();
+    return super.configure(req, json);
+  }
+
+  public boolean isUseAuthenticatedEndpoint() {
+    return useAuthenticatedEndpoint;
+  }
+
+  void setUseAuthenticatedEndpoint(boolean useAuthenticatedEndpoint) {
+    this.useAuthenticatedEndpoint = useAuthenticatedEndpoint;
+  }
+
+  public List<GitLabConnection> getConnections() {
+    return connections;
+  }
+
+  public void addConnection(GitLabConnection connection) {
+    connections.add(connection);
+    connectionMap.put(connection.getName(), connection);
+  }
+
+  public void setConnections(List<GitLabConnection> newConnections) {
+    connections = new ArrayList<>();
+    connectionMap = new HashMap<>();
+    for (GitLabConnection connection : newConnections) {
+      addConnection(connection);
     }
+  }
 
-    @Override
-    public boolean configure(StaplerRequest req, JSONObject json) throws FormException {
-        connections = req.bindJSONToList(GitLabConnection.class, json.get("connections"));
-        useAuthenticatedEndpoint = json.getBoolean("useAuthenticatedEndpoint");
-        refreshConnectionMap();
-        save();
-        return super.configure(req, json);
+  public GitLabClient getClient(String connectionName, Item item, String jobCredentialId) {
+    if (!connectionMap.containsKey(connectionName)) {
+      return null;
     }
+    return connectionMap.get(connectionName).getClient(item, jobCredentialId);
+  }
 
-    public boolean isUseAuthenticatedEndpoint() {
-        return useAuthenticatedEndpoint;
+  public FormValidation doCheckName(@QueryParameter String id, @QueryParameter String value) {
+    if (StringUtils.isEmptyOrNull(value)) {
+      return FormValidation.error(Messages.name_required());
+    } else {
+      if (connectionMap.containsKey(value) && !connectionMap.get(value).toString().equals(id)) {
+        return FormValidation.error(Messages.name_exists(value));
+      } else {
+        return FormValidation.ok();
+      }
     }
+  }
 
-    void setUseAuthenticatedEndpoint(boolean useAuthenticatedEndpoint) {
-        this.useAuthenticatedEndpoint = useAuthenticatedEndpoint;
+  public FormValidation doCheckUrl(@QueryParameter String value) {
+    if (StringUtils.isEmptyOrNull(value)) {
+      return FormValidation.error(Messages.url_required());
+    } else {
+      return FormValidation.ok();
     }
+  }
 
-    public List<GitLabConnection> getConnections() {
-        return connections;
+  public FormValidation doCheckApiTokenId(@QueryParameter String value) {
+    if (StringUtils.isEmptyOrNull(value)) {
+      return FormValidation.error(Messages.apiToken_required());
+    } else {
+      return FormValidation.ok();
     }
+  }
 
-    public void addConnection(GitLabConnection connection) {
-        connections.add(connection);
-        connectionMap.put(connection.getName(), connection);
+  public FormValidation doCheckConnectionTimeout(@QueryParameter Integer value) {
+    if (value == null) {
+      return FormValidation.error(Messages.connectionTimeout_required());
+    } else {
+      return FormValidation.ok();
     }
+  }
 
-    public void setConnections(List<GitLabConnection> newConnections) {
-        connections = new ArrayList<>();
-        connectionMap = new HashMap<>();
-        for (GitLabConnection connection: newConnections){
-            addConnection(connection);
+  public FormValidation doCheckReadTimeout(@QueryParameter Integer value) {
+    if (value == null) {
+      return FormValidation.error(Messages.readTimeout_required());
+    } else {
+      return FormValidation.ok();
+    }
+  }
+
+  @RequirePOST @Restricted(value = DoNotUse.class) public FormValidation doTestConnection(@QueryParameter String url, @QueryParameter String apiTokenId, @QueryParameter String clientBuilderId, @QueryParameter boolean ignoreCertificateErrors, @QueryParameter int connectionTimeout, @QueryParameter int readTimeout) {
+    Jenkins.getActiveInstance().checkPermission(Jenkins.ADMINISTER);
+    try {
+      new GitLabConnection("", url, apiTokenId, clientBuilderId, ignoreCertificateErrors, connectionTimeout, readTimeout).getClient(null, null).getCurrentUser();
+      return FormValidation.ok(Messages.connection_success());
+    } catch (WebApplicationException e) {
+      return FormValidation.error(Messages.connection_error(e.getMessage()));
+    } catch (ProcessingException e) {
+      return FormValidation.error(Messages.connection_error(e.getCause().getMessage()));
+    }
+  }
+
+  public ListBoxModel doFillApiTokenIdItems(@QueryParameter String name, @QueryParameter String url) {
+    if (Jenkins.getInstance().hasPermission(Item.CONFIGURE)) {
+      AbstractIdCredentialsListBoxModel<StandardListBoxModel, StandardCredentials> options = new StandardListBoxModel().includeEmptyValue().includeMatchingAs(ACL.SYSTEM, Jenkins.getActiveInstance(), StandardCredentials.class, URIRequirementBuilder.fromUri(url).build(), new GitLabCredentialMatcher());
+      if (name != null && connectionMap.containsKey(name)) {
+        String apiTokenId = connectionMap.get(name).getApiTokenId();
+        options.includeCurrentValue(apiTokenId);
+        for (ListBoxModel.Option option : options) {
+          if (option.value.equals(apiTokenId)) {
+            option.selected = true;
+          }
         }
+      }
+      return options;
     }
+    return new StandardListBoxModel();
+  }
 
-    public GitLabClient getClient(String connectionName, Item item, String jobCredentialId) {
-        if (!connectionMap.containsKey(connectionName)) {
-            return null;
-        }
-        return connectionMap.get(connectionName).getClient(item, jobCredentialId);
+  public ListBoxModel doFillClientBuilderIdItems() {
+    ListBoxModel model = new ListBoxModel();
+    for (GitLabClientBuilder builder : getAllGitLabClientBuilders()) {
+      model.add(builder.id());
     }
+    return model;
+  }
 
-    public FormValidation doCheckName(@QueryParameter String id, @QueryParameter String value) {
-        if (StringUtils.isEmptyOrNull(value)) {
-            return FormValidation.error(Messages.name_required());
-        } else if (connectionMap.containsKey(value) && !connectionMap.get(value).toString().equals(id)) {
-            return FormValidation.error(Messages.name_exists(value));
-        } else {
-            return FormValidation.ok();
-        }
+  private void refreshConnectionMap() {
+    connectionMap.clear();
+    for (GitLabConnection connection : connections) {
+      connectionMap.put(connection.getName(), connection);
     }
+  }
 
-    public FormValidation doCheckUrl(@QueryParameter String value) {
-        if (StringUtils.isEmptyOrNull(value)) {
-            return FormValidation.error(Messages.url_required());
-        } else {
-            return FormValidation.ok();
-        }
+  protected GitLabConnectionConfig readResolve() {
+    if (useAuthenticatedEndpoint == null) {
+      setUseAuthenticatedEndpoint(false);
     }
-
-    public FormValidation doCheckApiTokenId(@QueryParameter String value) {
-        if (StringUtils.isEmptyOrNull(value)) {
-            return FormValidation.error(Messages.apiToken_required());
-        } else {
-            return FormValidation.ok();
-        }
-    }
-
-    public FormValidation doCheckConnectionTimeout(@QueryParameter Integer value) {
-        if (value == null) {
-            return FormValidation.error(Messages.connectionTimeout_required());
-        } else {
-            return FormValidation.ok();
-        }
-    }
-
-    public FormValidation doCheckReadTimeout(@QueryParameter Integer value) {
-        if (value == null) {
-            return FormValidation.error(Messages.readTimeout_required());
-        } else {
-            return FormValidation.ok();
-        }
-    }
-
-    @RequirePOST
-    @Restricted(DoNotUse.class) // WebOnly
-    public FormValidation doTestConnection(@QueryParameter String url,
-                                           @QueryParameter String apiTokenId,
-                                           @QueryParameter String clientBuilderId,
-                                           @QueryParameter boolean ignoreCertificateErrors,
-                                           @QueryParameter int connectionTimeout,
-                                           @QueryParameter int readTimeout) {
-	Jenkins.getActiveInstance().checkPermission(Jenkins.ADMINISTER);
-        try {
-            new GitLabConnection("", url, apiTokenId, clientBuilderId, ignoreCertificateErrors, connectionTimeout, readTimeout).getClient(null, null).getCurrentUser();
-            return FormValidation.ok(Messages.connection_success());
-        } catch (WebApplicationException e) {
-            return FormValidation.error(Messages.connection_error(e.getMessage()));
-        } catch (ProcessingException e) {
-            return FormValidation.error(Messages.connection_error(e.getCause().getMessage()));
-        }
-    }
-
-    public ListBoxModel doFillApiTokenIdItems(@QueryParameter String name, @QueryParameter String url) {
-        if (Jenkins.getInstance().hasPermission(Item.CONFIGURE)) {
-            AbstractIdCredentialsListBoxModel<StandardListBoxModel, StandardCredentials> options = new StandardListBoxModel()
-                .includeEmptyValue()
-                .includeMatchingAs(ACL.SYSTEM,
-                                   Jenkins.getActiveInstance(),
-                                   StandardCredentials.class,
-                                   URIRequirementBuilder.fromUri(url).build(),
-                                   new GitLabCredentialMatcher());
-            if (name != null && connectionMap.containsKey(name)) {
-                String apiTokenId = connectionMap.get(name).getApiTokenId();
-                options.includeCurrentValue(apiTokenId);
-                for (ListBoxModel.Option option : options) {
-                    if (option.value.equals(apiTokenId)) {
-                        option.selected = true;
-                    }
-                }
-            }
-            return options;
-        }
-        return new StandardListBoxModel();
-    }
-
-    public ListBoxModel doFillClientBuilderIdItems() {
-        ListBoxModel model = new ListBoxModel();
-        for (GitLabClientBuilder builder : getAllGitLabClientBuilders()) {
-            model.add(builder.id());
-        }
-
-        return model;
-    }
-
-    private void refreshConnectionMap() {
-        connectionMap.clear();
-        for (GitLabConnection connection : connections) {
-            connectionMap.put(connection.getName(), connection);
-        }
-    }
-
-    //For backwards compatibility. ReadResolve is called on startup
-    protected GitLabConnectionConfig readResolve() {
-        if (useAuthenticatedEndpoint == null) {
-            setUseAuthenticatedEndpoint(false);
-        }
-        return this;
-    }
+    return this;
+  }
 }
