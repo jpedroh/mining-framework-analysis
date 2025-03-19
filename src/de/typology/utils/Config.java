a83c2008d6afc528f6ea8d5f9643ae5aa1b7cd31
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 23:11:37 +0000

    Mining Framework Analysis

--- src/de/typology/utils/Config.java
+++ src/de/typology/utils/Config.java
@@ -1,5 +1,4 @@
 package de.typology.utils;
-
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -21,162 +20,208 @@ import java.util.Properties;
  * 
  */
 public class Config extends Properties {
+  public String dbUser;
 
-	// CONTROLL PARAMETERS
-	public String dbUser;
+  public String rawLogDir;
 
-	public String rawLogDir;
+  public String trainedOnDataSet;
 
-	public String trainedOnDataSet;
-	public String trainedOnLang;
-	public String testedOnDataSet;
-	public String testedOnLang;
-	public String dataBaseType;
-	public String resultLogDirName;
+  public String trainedOnLang;
 
-	public String weight;
-	public boolean useWeights;
+  public String testedOnDataSet;
 
-	public boolean parseData;
-	public boolean splitData;
-	public boolean buildStats;
-	public boolean buildIndex;
-	public boolean buildNGrams;
-	public boolean buildGLM;
-	public boolean build_absoluteGLM;
-	public boolean buildTypoEdges;
-	public boolean revertSort;
-	public boolean aggregateAbsolute_;
+  public String testedOnLang;
 
-	public boolean deleteTempFiles;
+  public String dataBaseType;
 
-	public boolean addSentenceTags;
-	public boolean addFakeStartTag;
+  public String resultLogDirName;
 
-	public boolean resultLog10;
-	// DEBUGGING
-	public String inputDataSet;
+  public String weight;
 
-	public String trainingPath;
-	public String testingPath;
-	public String learningPath;
+  public boolean useWeights;
 
-	// STEP 0 GLOBAL CONFIGS
-	public boolean weightedPredictions;
+  public boolean parseData;
 
-	public String acquisInputDirectory;
-	public String enronInputDirectory;
-	public String googleInputDirectory;
-	public String reutersInputDirectory;
-	public String wikiInputDirectory;
-	public String acquisLanguages;
+  public boolean splitData;
 
-	public boolean splitPunctuation;
+  public boolean buildStats;
 
-	public int fileSizeThreashhold;
-	public String outputDirectory;
-	public int memoryLimitForWritingFiles;
-	public int memoryLimitForReadingFiles;
-	public int maxCountDivider;
-	public int minCountPerFile;
-	public int modelLength;
+  public boolean buildIndex;
 
-	public int numberOfQueries;
-	public int subResultSize;
+  public boolean buildNGrams;
 
-	// STEP 2 SAMPLING AND MAKE TRAININGS DATA SPLIT
-	public int sampleRate; // \in [0, 100] 0 means no data from input will be
-	// used. 100 means all input data will be used
-	public int splitDataRatio; // \in [0, 100] 0 means no training data. 100
-	// means only training data
-	public int splitTestRatio; // \in [0, 100] 0 means all data is stored in
-	// test file. 100 means all data is stored in (smaller) learning file
+  public boolean buildGLM;
 
-	// OTHER VARIABLES...
-	public String wikiLinksOutputPath;
-	public String wikiLinksHead;
+  public boolean build_absoluteGLM;
 
-	// used in de.typology.utils/WordCounter
-	public String wordCountInput;
-	public String wordCountStats;
-	// used in de.typology.utils/LineCounter
-	public String lineCountInput;
-	public String lineCountStats;
-	public String dataSet;
+  public boolean buildTypoEdges;
 
-	private static final long serialVersionUID = -4439565094382127683L;
+  public boolean revertSort;
 
-	static Config instance = null;
+  public boolean aggregateAbsolute_;
 
-	public static String ngramDownloadPath;
-	public static String ngramDownloadOutputPath;
+  public boolean deleteTempFiles;
 
-	public Config() {
-		String file = "config.txt";
-		try {
-			BufferedInputStream stream = new BufferedInputStream(
-					new FileInputStream(file));
-			this.load(stream);
-			stream.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		try {
-			this.initialize();
-		} catch (IllegalArgumentException e) {
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		}
-	}
+  public boolean addSentenceTags;
 
-	/**
+  public boolean addFakeStartTag;
+
+  public 
+<<<<<<< /usr/src/app/output/renepickhardt/generalized-language-modeling-toolkit/f8105ddc44d47cbadcf250db7f2e6240addce1de/src/de/typology/utils/Config.java/left.java
+  boolean
+=======
+  int
+>>>>>>> /usr/src/app/output/renepickhardt/generalized-language-modeling-toolkit/f8105ddc44d47cbadcf250db7f2e6240addce1de/src/de/typology/utils/Config.java/right.java
+   
+<<<<<<< /usr/src/app/output/renepickhardt/generalized-language-modeling-toolkit/f8105ddc44d47cbadcf250db7f2e6240addce1de/src/de/typology/utils/Config.java/left.java
+  resultLog10
+=======
+  subResultSize
+>>>>>>> /usr/src/app/output/renepickhardt/generalized-language-modeling-toolkit/f8105ddc44d47cbadcf250db7f2e6240addce1de/src/de/typology/utils/Config.java/right.java
+  ;
+
+  public String inputDataSet;
+
+  public String trainingPath;
+
+  public String testingPath;
+
+  public String learningPath;
+
+  public boolean weightedPredictions;
+
+  public String acquisInputDirectory;
+
+  public String enronInputDirectory;
+
+  public String googleInputDirectory;
+
+  public String reutersInputDirectory;
+
+  public String wikiInputDirectory;
+
+  public String acquisLanguages;
+
+  public boolean splitPunctuation;
+
+  public int fileSizeThreashhold;
+
+  public String outputDirectory;
+
+  public int memoryLimitForWritingFiles;
+
+  public int memoryLimitForReadingFiles;
+
+  public int maxCountDivider;
+
+  public int minCountPerFile;
+
+  public int modelLength;
+
+  public int numberOfQueries;
+
+  public int sampleRate;
+
+  public int splitDataRatio;
+
+  public int splitTestRatio;
+
+  public String wikiLinksOutputPath;
+
+  public String wikiLinksHead;
+
+  public String wordCountInput;
+
+  public String wordCountStats;
+
+  public String lineCountInput;
+
+  public String lineCountStats;
+
+  public String dataSet;
+
+  private static final long serialVersionUID = -4439565094382127683L;
+
+  static Config instance = null;
+
+  public static String ngramDownloadPath;
+
+  public static String ngramDownloadOutputPath;
+
+  public Config() {
+    String file = "config.txt";
+    try {
+      BufferedInputStream stream = new BufferedInputStream(new FileInputStream(file));
+      this.load(stream);
+      stream.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    try {
+      this.initialize();
+    } catch (IllegalArgumentException e) {
+      e.printStackTrace();
+    } catch (IllegalAccessException e) {
+      e.printStackTrace();
+    }
+  }
+
+  /**
 	 * Fills all fields with the data defined in the config file.
 	 * 
 	 * @throws IllegalArgumentException
 	 * @throws IllegalAccessException
 	 */
-	private void initialize() throws IllegalArgumentException,
-			IllegalAccessException {
-		Field[] fields = this.getClass().getFields();
-		for (Field f : fields) {
-			if (this.getProperty(f.getName()) == null) {
-				System.err.print("Property '" + f.getName()
-						+ "' not defined in config file");
-			}
-			if (f.getType().equals(String.class)) {
-				f.set(this, this.getProperty(f.getName()));
-			} else if (f.getType().equals(long.class)) {
-				f.setLong(this, Long.valueOf(this.getProperty(f.getName())));
-			} else if (f.getType().equals(int.class)) {
-				f.setInt(this, Integer.valueOf(this.getProperty(f.getName())));
-			} else if (f.getType().equals(boolean.class)) {
-				f.setBoolean(this,
-						Boolean.valueOf(this.getProperty(f.getName())));
-			} else if (f.getType().equals(String[].class)) {
-				f.set(this, this.getProperty(f.getName()).split(";"));
-			} else if (f.getType().equals(int[].class)) {
-				String[] tmp = this.getProperty(f.getName()).split(";");
-				int[] ints = new int[tmp.length];
-				for (int i = 0; i < tmp.length; i++) {
-					ints[i] = Integer.parseInt(tmp[i]);
-				}
-				f.set(this, ints);
-			} else if (f.getType().equals(long[].class)) {
-				String[] tmp = this.getProperty(f.getName()).split(";");
-				long[] longs = new long[tmp.length];
-				for (int i = 0; i < tmp.length; i++) {
-					longs[i] = Long.parseLong(tmp[i]);
-				}
-				f.set(this, longs);
-			}
-		}
-	}
+  private void initialize() throws IllegalArgumentException, IllegalAccessException {
+    Field[] fields = this.getClass().getFields();
+    for (Field f : fields) {
+      if (this.getProperty(f.getName()) == null) {
+        System.err.print("Property \'" + f.getName() + "\' not defined in config file");
+      }
+      if (f.getType().equals(String.class)) {
+        f.set(this, this.getProperty(f.getName()));
+      } else {
+        if (f.getType().equals(long.class)) {
+          f.setLong(this, Long.valueOf(this.getProperty(f.getName())));
+        } else {
+          if (f.getType().equals(int.class)) {
+            f.setInt(this, Integer.valueOf(this.getProperty(f.getName())));
+          } else {
+            if (f.getType().equals(boolean.class)) {
+              f.setBoolean(this, Boolean.valueOf(this.getProperty(f.getName())));
+            } else {
+              if (f.getType().equals(String[].class)) {
+                f.set(this, this.getProperty(f.getName()).split(";"));
+              } else {
+                if (f.getType().equals(int[].class)) {
+                  String[] tmp = this.getProperty(f.getName()).split(";");
+                  int[] ints = new int[tmp.length];
+                  for (int i = 0; i < tmp.length; i++) {
+                    ints[i] = Integer.parseInt(tmp[i]);
+                  }
+                  f.set(this, ints);
+                } else {
+                  if (f.getType().equals(long[].class)) {
+                    String[] tmp = this.getProperty(f.getName()).split(";");
+                    long[] longs = new long[tmp.length];
+                    for (int i = 0; i < tmp.length; i++) {
+                      longs[i] = Long.parseLong(tmp[i]);
+                    }
+                    f.set(this, longs);
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
 
-	public static Config get() {
-		if (instance == null) {
-			instance = new Config();
-		}
-		return instance;
-	}
+  public static Config get() {
+    if (instance == null) {
+      instance = new Config();
+    }
+    return instance;
+  }
 }
