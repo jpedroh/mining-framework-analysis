66c063b5b8bb4d6b9a3aa30a0ce0457f16af96ff
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 17:31:30 +0000

    Mining Framework Analysis

--- src/main/java/com/github/sd4324530/fastweixin/servlet/WeixinControllerSupport.java
+++ src/main/java/com/github/sd4324530/fastweixin/servlet/WeixinControllerSupport.java
@@ -1,10 +1,8 @@
 package com.github.sd4324530.fastweixin.servlet;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,45 +15,49 @@ import java.io.PrintWriter;
  *
  * @author peiyu
  */
-@Controller
-public abstract class WeixinControllerSupport extends WeixinSupport {
-	
-    /**
+@Controller public abstract class WeixinControllerSupport extends WeixinSupport {
+  /**
      * 绑定微信服务器
      *
      * @param request 请求
      * @return 响应内容
      */
-    @RequestMapping(method = RequestMethod.GET)
-    @ResponseBody
-    protected final String bind(HttpServletRequest request) {
-        if (isLegal(request)) {
-            //绑定微信服务器成功
-            return request.getParameter("echostr");
-        } else {
-            //绑定微信服务器失败
-            return "";
-        }
+  @RequestMapping(method = RequestMethod.GET) @ResponseBody protected final String bind(HttpServletRequest request) {
+    if (isLegal(request)) {
+      return request.getParameter("echostr");
+    } else {
+      return "";
     }
+  }
 
-    /**
+  /**
      * 微信消息交互处理
      *
      * @param request http 请求对象
-     * @param response http 响应对象
      * @return 响应给微信服务器的消息报文
      * @throws ServletException 异常
      * @throws IOException      IO异常
      */
-    @RequestMapping(method = RequestMethod.POST)
-    protected final void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        if (isLegal(request)) {
-            String result = processRequest(request);
-            //设置正确的 content-type 以防止中文乱码
-            response.setContentType("text/xml;charset=UTF-8");
-            PrintWriter writer = response.getWriter();
-            writer.write(result);
-            writer.close();
-        }
+  @RequestMapping(method = RequestMethod.POST) protected final void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+<<<<<<< /usr/src/app/output/sd4324530/fastweixin/b9c911aea2bd0710b54afc94fb09c55e813ed9b6/src/main/java/com/github/sd4324530/fastweixin/servlet/WeixinControllerSupport.java/left.java
+    String result = processRequest(request);
+=======
+    if (isLegal(request)) {
+      String result = processRequest(request);
+      response.setContentType("text/xml;charset=UTF-8");
+      PrintWriter writer = response.getWriter();
+      writer.write(result);
+      writer.close();
     }
+>>>>>>> /usr/src/app/output/sd4324530/fastweixin/b9c911aea2bd0710b54afc94fb09c55e813ed9b6/src/main/java/com/github/sd4324530/fastweixin/servlet/WeixinControllerSupport.java/right.java
+
+    response.setContentType("text/xml;charset=UTF-8");
+    response.getWriter().write(result);
+
+<<<<<<< /usr/src/app/output/sd4324530/fastweixin/b9c911aea2bd0710b54afc94fb09c55e813ed9b6/src/main/java/com/github/sd4324530/fastweixin/servlet/WeixinControllerSupport.java/left.java
+    return null;
+=======
+>>>>>>> Unknown file: This is a bug in JDime.
+  }
 }
