20d07b6f352a4bdeffcfe28dbfd509ab335a71b3
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 16:29:30 +0000

    Mining Framework Analysis

--- modules/swagger-parser-core/src/main/java/io/swagger/v3/parser/core/models/ParseOptions.java
+++ modules/swagger-parser-core/src/main/java/io/swagger/v3/parser/core/models/ParseOptions.java
@@ -1,114 +1,134 @@
 package io.swagger.v3.parser.core.models;
 
 public class ParseOptions {
-    private boolean resolve;
-    private boolean resolveCombinators = true;
-    private boolean resolveFully;
-    private boolean flatten;
-    private boolean flattenComposedSchemas;
-    private boolean camelCaseFlattenNaming;
-    private boolean skipMatches;
-    private boolean allowEmptyStrings = true;
-    private boolean validateExternalRefs = false;
-    private boolean validateInternalRefs = false;
-    private boolean legacyYamlDeserialization = false;
-    private boolean resolveRequestBody = false;
-    
-    public boolean isResolve() {
-        return resolve;
-    }
+  private boolean resolve;
 
-    public void setResolve(boolean resolve) {
-        this.resolve = resolve;
-    }
+  private boolean resolveCombinators = true;
 
-    public boolean isResolveCombinators() {
-        return resolveCombinators;
-    }
+  private boolean resolveFully;
 
-    public void setResolveCombinators(boolean resolveCombinators) {
-        this.resolveCombinators = resolveCombinators;
-    }
+  private boolean flatten;
 
-    public boolean isResolveFully() {
-        return resolveFully;
-    }
+  private boolean flattenComposedSchemas;
 
-    public void setResolveFully(boolean resolveFully) {
-        this.resolveFully = resolveFully;
-    }
-    
-    public boolean isResolveRequestBody() {
-		return resolveRequestBody;
-	}
-    
-    /**
+  private boolean camelCaseFlattenNaming;
+
+  private boolean skipMatches;
+
+  private boolean allowEmptyStrings = true;
+
+  private boolean validateExternalRefs = false;
+
+  private boolean legacyYamlDeserialization = false;
+
+  private boolean 
+<<<<<<< /usr/src/app/output/swagger-api/swagger-parser/7328eba1dd55fa4f9d3aee4a6d35d976d3396ca6/modules/swagger-parser-core/src/main/java/io/swagger/v3/parser/core/models/ParseOptions.java/left.java
+  resolveRequestBody = false
+=======
+  validateInternalRefs = false
+>>>>>>> /usr/src/app/output/swagger-api/swagger-parser/7328eba1dd55fa4f9d3aee4a6d35d976d3396ca6/modules/swagger-parser-core/src/main/java/io/swagger/v3/parser/core/models/ParseOptions.java/right.java
+  ;
+
+  public boolean isResolve() {
+    return resolve;
+  }
+
+  public void setResolve(boolean resolve) {
+    this.resolve = resolve;
+  }
+
+  public boolean isResolveCombinators() {
+    return resolveCombinators;
+  }
+
+  public void setResolveCombinators(boolean resolveCombinators) {
+    this.resolveCombinators = resolveCombinators;
+  }
+
+  public boolean isResolveFully() {
+    return resolveFully;
+  }
+
+  public void setResolveFully(boolean resolveFully) {
+    this.resolveFully = resolveFully;
+  }
+
+  public boolean isResolveRequestBody() {
+    return resolveRequestBody;
+  }
+
+  /**
      * If set to true, will help resolving the requestBody as inline, provided resolve is also set to true.
      * Default is false because of the existing behaviour.
      */
-	public void setResolveRequestBody(boolean resolveRequestBody) {
-		this.resolveRequestBody = resolveRequestBody;
-	}
+  public void setResolveRequestBody(boolean resolveRequestBody) {
+    this.resolveRequestBody = resolveRequestBody;
+  }
 
-    public boolean isFlatten() { return flatten; }
+  public boolean isFlatten() {
+    return flatten;
+  }
 
-    public void setFlatten(boolean flatten) { this.flatten = flatten; }
+  public void setFlatten(boolean flatten) {
+    this.flatten = flatten;
+  }
 
-    public boolean isSkipMatches() {
-        return skipMatches;
-    }
+  public boolean isSkipMatches() {
+    return skipMatches;
+  }
 
-    public void setSkipMatches(boolean skipMatches) {
-        this.skipMatches = skipMatches;
-    }
+  public void setSkipMatches(boolean skipMatches) {
+    this.skipMatches = skipMatches;
+  }
 
-    public boolean isFlattenComposedSchemas() {
-        return flattenComposedSchemas;
-    }
+  public boolean isFlattenComposedSchemas() {
+    return flattenComposedSchemas;
+  }
 
-    public void setFlattenComposedSchemas(boolean flattenComposedSchemas) {
-        this.flattenComposedSchemas = flattenComposedSchemas;
-    }
-    public boolean isCamelCaseFlattenNaming() {
-        return camelCaseFlattenNaming;
-    }
+  public void setFlattenComposedSchemas(boolean flattenComposedSchemas) {
+    this.flattenComposedSchemas = flattenComposedSchemas;
+  }
 
-    public void setCamelCaseFlattenNaming(boolean camelCaseFlattenNaming) {
-        this.camelCaseFlattenNaming = camelCaseFlattenNaming;
-    }
+  public boolean isCamelCaseFlattenNaming() {
+    return camelCaseFlattenNaming;
+  }
 
-    public boolean isAllowEmptyString() {
-        return allowEmptyStrings;
-    }
+  public void setCamelCaseFlattenNaming(boolean camelCaseFlattenNaming) {
+    this.camelCaseFlattenNaming = camelCaseFlattenNaming;
+  }
 
-    public void setAllowEmptyString(boolean allowEmptyStrings) {
-        this.allowEmptyStrings = allowEmptyStrings;
-    }
+  public boolean isAllowEmptyString() {
+    return allowEmptyStrings;
+  }
 
-    public boolean isValidateExternalRefs() {
-        return validateExternalRefs;
-    }
+  public void setAllowEmptyString(boolean allowEmptyStrings) {
+    this.allowEmptyStrings = allowEmptyStrings;
+  }
 
-    public void setValidateExternalRefs(boolean validateExternalRefs) {
-        this.validateExternalRefs = validateExternalRefs;
-    }
+  public boolean isValidateExternalRefs() {
+    return validateExternalRefs;
+  }
 
-    /**
+  public void setValidateExternalRefs(boolean validateExternalRefs) {
+    this.validateExternalRefs = validateExternalRefs;
+  }
+
+  /**
      * if set to true, triggers YAML deserialization as done up to 2.0.30, not supporting YAML Anchors safe resolution.
      */
-    public boolean isLegacyYamlDeserialization() {
-        return legacyYamlDeserialization;
-    }
+  public boolean isLegacyYamlDeserialization() {
+    return legacyYamlDeserialization;
+  }
 
-    public void setLegacyYamlDeserialization(boolean legacyYamlDeserialization) {
-        this.legacyYamlDeserialization = legacyYamlDeserialization;
-    }
+  public void setLegacyYamlDeserialization(boolean legacyYamlDeserialization) {
+    this.legacyYamlDeserialization = legacyYamlDeserialization;
+  }
 
-    public void setValidateInternalRefs(boolean validateInternalRefs) {
-        this.validateInternalRefs = validateInternalRefs;
-    }
+  public void setValidateInternalRefs(boolean validateInternalRefs) {
+    this.validateInternalRefs = validateInternalRefs;
+  }
 
-    public boolean isValidateInternalRefs() {
-        return validateInternalRefs;
-    }
+  public boolean isValidateInternalRefs() {
+    return validateInternalRefs;
+  }
 }
