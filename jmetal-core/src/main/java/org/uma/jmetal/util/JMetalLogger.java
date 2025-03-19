9b7a59a7cef4e67f17e3ecae82da01742a1686cf
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 19:54:44 +0000

    Mining Framework Analysis

--- jmetal-core/src/main/java/org/uma/jmetal/util/JMetalLogger.java
+++ jmetal-core/src/main/java/org/uma/jmetal/util/JMetalLogger.java
@@ -1,13 +1,12 @@
 package org.uma.jmetal.util;
-
 import java.io.Serializable;
 import java.util.logging.Logger;
 
 /**
+ * This class provides a logger.
+ * 
  * @author Antonio J. Nebro <antonio@lcc.uma.es>
  */
 public class JMetalLogger implements Serializable {
-
-	public static final Logger logger = Logger.getLogger(JMetalLogger.class
-			.getName());
+  public static final Logger logger = Logger.getLogger(JMetalLogger.class.getName());
 }
