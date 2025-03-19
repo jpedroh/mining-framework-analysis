dfe3133e8a67dac1faa2aa30d9f82a36f3ec153b
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 17:18:28 +0000

    Mining Framework Analysis

--- src/main/java/org/khaleesi/carfield/tools/sparkjobserver/api/SparkJobBaseInfo.java
+++ src/main/java/org/khaleesi/carfield/tools/sparkjobserver/api/SparkJobBaseInfo.java
@@ -1,5 +1,4 @@
 package org.khaleesi.carfield.tools.sparkjobserver.api;
-
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
@@ -13,129 +12,145 @@ import java.util.Set;
  *
  */
 class SparkJobBaseInfo {
-	static final String INFO_EMPTY_VALUE = "empty value";
-	
-	/**
+  static final String INFO_EMPTY_VALUE = "empty value";
+
+  /**
 	 * Status value in a global job information (a <code>SparkJobInfo</code>
 	 * instance) or a job result/status information 
 	 * (a <code>SparkJobResult</code> instance)
 	 */
-	static final String INFO_STATUS_ERROR = "ERROR";
-	
-	/**
+  static final String INFO_STATUS_ERROR = "ERROR";
+
+  /**
 	 * Status value in a global job information (a <code>SparkJobInfo</code>
 	 * instance)
 	 */
-	static final String INFO_STATUS_FINISHED = "FINISHED";
-	
-	/**
+  static final String INFO_STATUS_FINISHED = "FINISHED";
+
+  /**
 	 * Status value in a job status/result information (a <code>SparkJobResult</code> 
 	 * instance)
 	 */
-	static final String INFO_STATUS_OK = "OK";
-	
-	/**
+  static final String INFO_STATUS_OK = "OK";
+
+  /**
 	 * Status value in a job status/result information (a <code>SparkJobResult</code> instance)
 	 */
-	static final String INFO_STATUS_STARTED = "STARTED";
+  static final String INFO_STATUS_STARTED = "STARTED";
 
-	/**
+  /**
 	 * Status value in a job status/result information (a <code>SparkJobResult</code> instance)
 	 */
-	static final String INFO_STATUS_RUNNING = "RUNNING";
+  static final String INFO_STATUS_RUNNING = "RUNNING";
 
-	/**
+  /**
 	 * Key of status information in the Spark Job Server's json response.
 	 */
-	static final String INFO_KEY_STATUS = "status";
-	
-	/**
+  static final String INFO_KEY_STATUS = "status";
+
+  /**
 	 * Key of result information in the Spark Job Server's json response.
 	 */
-	static final String INFO_KEY_RESULT = "result";
-	
-	/**
+  static final String INFO_KEY_RESULT = "result";
+
+  /**
 	 * Key of message information of a result item in the Spark Job Server's json response.
 	 * <p>
 	 * It presents the global description of the error
 	 */
-	static final String INFO_KEY_RESULT_MESSAGE = "message";
-	
-	/**
+  static final String INFO_KEY_RESULT_MESSAGE = "message";
+
+  /**
 	 * Key of error class information of a result item in the Spark Job Server's json response.
 	 * <p>
 	 * It indicates the error class of current error message
 	 */
-	static final String INFO_KEY_RESULT_ERROR_CLASS = "errorClass";
-	
-	/**
+  static final String INFO_KEY_RESULT_ERROR_CLASS = "errorClass";
+
+  /**
 	 * Key of stack class information of a result item in the Spark Job Server's json response.
 	 * <p>
 	 * It shows the information of java/scala exception stack
 	 */
-	static final String INFO_KEY_RESULT_STACK = "stack";
+  static final String INFO_KEY_RESULT_STACK = "stack";
 
-	/**
+  /**
 	 * Key of context information of a result item in the Spark Job Server's json response.
 	 * <p>
 	 * It's the context name.
 	 */
-	static final String INFO_KEY_CONTEXT = "context";
-	
-	/**
+  static final String INFO_KEY_CONTEXT = "context";
+
+  /**
 	 * Key of context information of a result item in the Spark Job Server's json response.
 	 * <p>
 	 * It shows the job id of the target spark job
 	 */
-	static final String INFO_KEY_JOB_ID = "jobId";
+  static final String INFO_KEY_JOB_ID = "jobId";
 
-	static final Set<String> ASYNC_STATUS = new HashSet<String>(Arrays.asList(new String[]{INFO_STATUS_STARTED, INFO_STATUS_RUNNING}));
-	static final Set<String> COMPLETED = new HashSet<String>(Arrays.asList(new String[]{INFO_STATUS_FINISHED, INFO_STATUS_OK}));
+  static final Set<String> ASYNC_STATUS = new HashSet<String>(Arrays.asList(new String[] { INFO_STATUS_STARTED, INFO_STATUS_RUNNING }));
 
-	private String status;
-	private String message;
-	private String errorClass;
-	private String[] stack;
-	private String context;
-	private String jobId;
-	protected String contents;
-	
-	public String getStatus() {
-		return status;
-	}
-	void setStatus(String status) {
-		this.status = status;
-	}
-	public String getMessage() {
-		return message;
-	}
-	void setMessage(String message) {
-		this.message = message;
-	}
-	public String getErrorClass() {
-		return errorClass;
-	}
-	void setErrorClass(String errorClass) {
-		this.errorClass = errorClass;
-	}
-	public String[] getStack() {
-		return stack;
-	}
-	void setStack(String[] stack) {
-		this.stack = stack;
-	}
-	
-	public String getContext() {
-		return context;
-	}
-	void setContext(String context) {
-		this.context = context;
-	}
+  static final Set<String> COMPLETED = new HashSet<String>(Arrays.asList(new String[] { INFO_STATUS_FINISHED, INFO_STATUS_OK }));
 
-	public String getJobId() {
-		return jobId;
-	}
-	void setJobId(String jobId) {
-		this.jobId = jobId;
-	}
+  private String status;
+
+  private String message;
+
+  private String errorClass;
+
+  private String[] stack;
+
+  private String context;
+
+  private String jobId;
+
+  protected String contents;
+
+  public String getStatus() {
+    return status;
+  }
+
+  void setStatus(String status) {
+    this.status = status;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  void setMessage(String message) {
+    this.message = message;
+  }
+
+  public String getErrorClass() {
+    return errorClass;
+  }
+
+  void setErrorClass(String errorClass) {
+    this.errorClass = errorClass;
+  }
+
+  public String[] getStack() {
+    return stack;
+  }
+
+  void setStack(String[] stack) {
+    this.stack = stack;
+  }
+
+  public String getContext() {
+    return context;
+  }
+
+  void setContext(String context) {
+    this.context = context;
+  }
+
+  public String getJobId() {
+    return jobId;
+  }
+
+  void setJobId(String jobId) {
+    this.jobId = jobId;
+  }
 }
--- src/main/java/org/khaleesi/carfield/tools/sparkjobserver/api/SparkJobResult.java
+++ src/main/java/org/khaleesi/carfield/tools/sparkjobserver/api/SparkJobResult.java
@@ -1,5 +1,4 @@
 package org.khaleesi.carfield.tools.sparkjobserver.api;
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,148 +11,81 @@ import java.util.Map;
  *
  */
 public class SparkJobResult extends SparkJobBaseInfo {
+  private String result;
 
-	private String result;
-	private Map<String, Object> extendAttributes = new HashMap<String, Object>();
+  private Map<String, Object> extendAttributes = new HashMap<String, Object>();
 
-	SparkJobResult(String contents, String jobId) {
-		this.contents = contents;
-		setJobId(jobId);
-	}
+  SparkJobResult(String contents, String jobId) {
+    this.contents = contents;
+    setJobId(jobId);
+  }
 
-	SparkJobResult(String contents) {
-		this(contents, null);
-	}
+  SparkJobResult(String contents) {
+    this(contents, null);
+  }
 
-	public String getResult() {
-		return result;
-	}
-	
-	void setResult(String result) {
-		this.result = result;
-	}
-	
-	void putExtendAttribute(String key, Object value) {
-		this.extendAttributes.put(key, value);
-	}
-	
-	public Map<String, Object> getExtendAttributes() {
-		return new HashMap<String, Object>(this.extendAttributes);
-	}
-	
-	/**
+  public String getResult() {
+    return result;
+  }
+
+  void setResult(String result) {
+    this.result = result;
+  }
+
+  void putExtendAttribute(String key, Object value) {
+    this.extendAttributes.put(key, value);
+  }
+
+  public Map<String, Object> getExtendAttributes() {
+    return new HashMap<String, Object>(this.extendAttributes);
+  }
+
+  /**
 	 * {@inheritDoc}
 	 */
-	@Override
-	public String toString() {
-		StringBuffer buff = new StringBuffer("SparkJobResult\n");
-		buff.append(contents);
-		return buff.toString();
-	}
-	
-	/**
+  @Override public String toString() {
+    StringBuffer buff = new StringBuffer("SparkJobResult\n");
+    buff.append(contents);
+    return buff.toString();
+  }
+
+  /**
 	 * Judges current <code>SparkJobResult</code> instance represents the 
 	 * status information of a asynchronous running spark job or not.
 	 * 
 	 * @return true indicates it contains asynchronous running status of a
 	 *         spark job, false otherwise
 	 */
-	public boolean containsAsynStatus() {
-		return SparkJobBaseInfo.ASYNC_STATUS.contains(getStatus());
-	}
+  public boolean containsAsynStatus() {
+    return SparkJobBaseInfo.ASYNC_STATUS.contains(getStatus());
+  }
 
-	/**
+  /**
 	 * Judges the queried target job doesn't exist or not.
 	 * 
 	 * @return true indicates the related job doesn't exist, false otherwise
 	 */
-	public boolean jobNotExists() {
-		return SparkJobBaseInfo.INFO_STATUS_ERROR.equals(getStatus()) 
-		    && getResult() != null && getResult().contains("No such job ID");
-	}
-	
-	/**
+  public boolean jobNotExists() {
+    return SparkJobBaseInfo.INFO_STATUS_ERROR.equals(getStatus()) && getResult() != null && getResult().contains("No such job ID");
+  }
+
+  /**
 	 * Judges current <code>SparkJobResult</code> instance contains 
 	 * error information of a failed spark job or not.
 	 * 
 	 * @return true indicates it contains error message, false otherwise
 	 */
-	public boolean containsErrorInfo() {
-		return SparkJobBaseInfo.INFO_STATUS_ERROR.equals(getStatus()) 
-			&& getMessage() != null;
-	}
-	
-	/**
+  public boolean containsErrorInfo() {
+    return SparkJobBaseInfo.INFO_STATUS_ERROR.equals(getStatus()) && getMessage() != null;
+  }
+
+  /**
 	 * Judges current <code>SparkJobResult</code> instance contains 
 	 * custom-defined extend attributes of result or not
 	 * 
 	 * @return true indicates it contains custom-defined extend attributes, false otherwise
 	 */
-	public boolean containsExtendAttributes() {
-		return !extendAttributes.isEmpty();
-	}
-	
-//	/**
-//	 * Sets the status information of the asynchronous running job.
-//	 * 
-//	 * @param buff the existing contents
-//	 */
-//	private void setAsynStatusInfo(StringBuffer buff) {
-//		if (buff != null) {
-//			buff.append(" ").append(SparkJobBaseInfo.INFO_KEY_RESULT).append(": {\n")
-//			    .append("  ").append(SparkJobBaseInfo.INFO_KEY_JOB_ID).append(": ").append(getJobId()).append(",\n")
-//			    .append("  ").append(SparkJobBaseInfo.INFO_KEY_CONTEXT).append(": ").append(getContext()).append(",\n")
-//			    .append(" }\n");
-//		}
-//	}
-//	
-//	/**
-//	 * Sets the information for non-existence job.
-//	 * 
-//	 * @param buff the existing contents
-//	 */
-//	private void setNotExistsInfo(StringBuffer buff) {
-//		if (buff != null) {
-//			buff.append(" ").append(SparkJobBaseInfo.INFO_KEY_RESULT).append(": ")
-//				.append(getResult()).append(",\n");
-//		}
-//	}
-//	
-//	/**
-//	 * Sets the error information of the target failed job.
-//	 * 
-//	 * @param buff the existing contents
-//	 */
-//	private void setErrorInfo(StringBuffer buff) {
-//		if (buff != null) {
-//			buff.append(" ").append(SparkJobBaseInfo.INFO_STATUS_ERROR).append(": {\n")
-//				.append("  ").append(SparkJobBaseInfo.INFO_KEY_RESULT_MESSAGE).append(": ").append(getMessage()).append(",\n");
-//			if (getErrorClass() != null) {
-//				buff.append("  ").append(SparkJobBaseInfo.INFO_KEY_RESULT_ERROR_CLASS).append(": ")
-//				.append(getErrorClass()).append(",\n");
-//			}
-//			if (getStack() != null && getStack().length > 0) {
-//				buff.append("  ").append(SparkJobBaseInfo.INFO_KEY_RESULT_STACK).append(": [");
-//				for (String stackItem : getStack()) {
-//					buff.append(stackItem).append(", ");
-//				}
-//				buff.append("]\n");
-//			}
-//		}
-//	}
-//	
-//	/**
-//	 * Sets the information of the extend attributes.
-//	 * 
-//	 * @param buff the existing contents
-//	 */
-//	private void setExtendAttributesInfo(StringBuffer buff) {
-//		if (buff != null) {
-//			Set<Entry<String, Object>> contents = extendAttributes.entrySet();
-//			for (Entry<String, Object> item : contents) {
-//				buff.append(" ").append(item.getKey()).append(": ")
-//					.append(item.getValue().toString()).append(",\n");
-//			}
-//		}
-//	}
+  public boolean containsExtendAttributes() {
+    return !extendAttributes.isEmpty();
+  }
 }
