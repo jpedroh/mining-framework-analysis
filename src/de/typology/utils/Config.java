acdbc620b9df5a3751919afc7cccd3d747db0b9d
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 23:13:19 +0000

    Mining Framework Analysis

--- src/de/typology/utils/Config.java
+++ src/de/typology/utils/Config.java
@@ -1,5 +1,4 @@
 package de.typology.utils;
-
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -21,196 +20,236 @@ import java.util.Properties;
  * 
  */
 public class Config extends Properties {
-	// new:
-	public int GLMType;
-	public int maxCountDivider;
-	public int minCountPerFile;
-	public String inputDataSet;
-	public int fileSizeThreashhold;
-	public boolean splitData;
-	public boolean buildStats;
-	public boolean buildIndex;
-	public boolean buildNGrams;
-	public boolean buildGLM;
-	public boolean buildContinuationGLM;
-	public boolean buildZeroGLM;
-	public boolean buildTypoEdges;
-	public boolean aggregateContinuationDelta;
+  public int GLMType;
 
-	public int modelLength;
-	public int numberOfQueries;
+  public int maxCountDivider;
 
-	// old:
+  public int minCountPerFile;
 
-	// CONTROLL PARAMETERS
-	public String dbUser;
+  public String inputDataSet;
 
-	public String rawLogDir;
+  public int fileSizeThreashhold;
 
-	public String trainedOnDataSet;
-	public String trainedOnLang;
-	public String testedOnDataSet;
-	public String testedOnLang;
+  public boolean splitData;
 
-	public String weight;
+  public boolean buildStats;
 
-	public boolean parseData;
-	public boolean sampleSplitData;
-	public boolean useWeights;
+  public boolean buildIndex;
 
-	// STEP 0 GLOBAL CONFIGS
-	public boolean loadIndexToRAM;
-	public boolean weightedPredictions;
+  public boolean buildNGrams;
 
-	public String acquisInputDirectory;
-	public String enronInputDirectory;
-	public String googleInputDirectory;
-	public String reutersInputDirectory;
-	public String wikiInputDirectory;
-	public String acquisLanguages;
+  public boolean buildGLM;
 
-	public String outputDirectory;
-	public int memoryLimitForWritingFiles;
-	public int memoryLimitForReadingFiles;
-	public int nGramLength;
+  public boolean buildContinuationGLM;
 
-	// STEP 2 SAMPLING AND MAKE TRAININGS DATA SPLIT
-	public int sampleRate; // \in [0, 100] 0 means no data from input will be
-	// used. 100 means all input data will be used
-	public int splitDataRatio; // \in [0, 100] 0 means no training data. 100
-	// means only training data
-	public int splitTestRatio; // \in [0, 100] 0 means all data is stored in
-	// test file. 100 means all data is stored in (smaller) learning file
+  public boolean buildZeroGLM;
 
-	// OPTIONAL only used in standalone scripts
-	public String trainingPath;
-	public String testingPath;
-	public String learningPath;
+  public boolean buildTypoEdges;
 
-	// STEP3 ngrams & typology edges
-	public boolean createNGramChunks;
-	public boolean createSecondLevelNGramChunks;
-	public boolean aggregateNGramChunks;
-	public boolean sortNGrams;
-	public boolean generateNGramDistribution;
-	public boolean normalizeNGrams;
-	// same flags for typology edge creating as for ngram building
-	public boolean createTypologyEdgeChunks;
-	public boolean createSecondLevelTypologyEdgeChunks;
-	public boolean aggregateTypologyEdgeChunks;
-	public boolean sortTypologyEdges;
-	public boolean generateTypologyEdgeDistribution;
-	public boolean normalizeEdges;
+  public boolean aggregateContinuationDelta;
 
-	// INPUT
-	// should be the output of step1
+  public int modelLength;
 
-	// temporary files
-	public int fileChunkThreashhold;
-	public String nGramKeyFile;
-	public String nGramsNotAggregatedPath;
-	public String typologyEdgesPathNotAggregated;
+  public int numberOfQueries;
 
-	// COULD NOT REALLY MATCH
-	public String wikiLinksOutputPath;
-	public String wikiLinksHead;
+  public String dbUser;
 
-	// STEP 3 BUILDING NGRAMS TYPOEDGES AND SPLIT THE DATA SET
-	// used in de.typology.nGramBuilder
-	// OUTPUT
-	public String nGramsAggregatedPath;
+  public String rawLogDir;
 
-	// used in de.typology.trainers
-	public String edgeInput;
-	public String normalizedEdges;
+  public String trainedOnDataSet;
 
-	public String nGramsInput;
-	public String normalizedNGrams;
+  public String trainedOnLang;
 
-	public String indexPath;
-	public String nGramIndexPath;
+  public String testedOnDataSet;
 
-	// used in de.typology.utils/WordCounter
-	public String wordCountInput;
-	public String wordCountStats;
-	// used in de.typology.utils/LineCounter
-	public String lineCountInput;
-	public String lineCountStats;
-	public String dataSet;
+  public String testedOnLang;
 
-	private static final long serialVersionUID = -4439565094382127683L;
+  public String weight;
 
-	static Config instance = null;
+  public boolean parseData;
 
-	public static String ngramDownloadPath;
-	public static String ngramDownloadOutputPath;
+  public boolean sampleSplitData;
 
-	public Config() {
-		String file = "config.txt";
-		try {
-			BufferedInputStream stream = new BufferedInputStream(
-					new FileInputStream(file));
-			this.load(stream);
-			stream.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		try {
-			this.initialize();
-		} catch (IllegalArgumentException e) {
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		}
-	}
+  public boolean useWeights;
 
-	/**
+  public boolean loadIndexToRAM;
+
+  public boolean weightedPredictions;
+
+  public String acquisInputDirectory;
+
+  public String enronInputDirectory;
+
+  public String googleInputDirectory;
+
+  public String reutersInputDirectory;
+
+  public String wikiInputDirectory;
+
+  public String acquisLanguages;
+
+  public String outputDirectory;
+
+  public int memoryLimitForWritingFiles;
+
+  public int memoryLimitForReadingFiles;
+
+  public int nGramLength;
+
+  public int sampleRate;
+
+  public int splitDataRatio;
+
+  public int splitTestRatio;
+
+  public String trainingPath;
+
+  public String testingPath;
+
+  public String learningPath;
+
+  public boolean createNGramChunks;
+
+  public boolean createSecondLevelNGramChunks;
+
+  public boolean aggregateNGramChunks;
+
+  public boolean sortNGrams;
+
+  public boolean generateNGramDistribution;
+
+  public boolean normalizeNGrams;
+
+  public boolean createTypologyEdgeChunks;
+
+  public boolean createSecondLevelTypologyEdgeChunks;
+
+  public boolean aggregateTypologyEdgeChunks;
+
+  public boolean sortTypologyEdges;
+
+  public boolean generateTypologyEdgeDistribution;
+
+  public boolean normalizeEdges;
+
+  public int fileChunkThreashhold;
+
+  public String nGramKeyFile;
+
+  public String nGramsNotAggregatedPath;
+
+  public String typologyEdgesPathNotAggregated;
+
+  public String wikiLinksOutputPath;
+
+  public String wikiLinksHead;
+
+  public String nGramsAggregatedPath;
+
+  public String edgeInput;
+
+  public String normalizedEdges;
+
+  public String nGramsInput;
+
+  public String normalizedNGrams;
+
+  public String indexPath;
+
+  public String nGramIndexPath;
+
+  public String wordCountInput;
+
+  public String wordCountStats;
+
+  public String lineCountInput;
+
+  public String lineCountStats;
+
+  public String dataSet;
+
+  private static final long serialVersionUID = -4439565094382127683L;
+
+  static Config instance = null;
+
+  public static String ngramDownloadPath;
+
+  public static String ngramDownloadOutputPath;
+
+  public Config() {
+    String file = "config.txt";
+    try {
+      BufferedInputStream stream = new BufferedInputStream(new FileInputStream(file));
+      this.load(stream);
+      stream.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    try {
+      this.initialize();
+    } catch (IllegalArgumentException e) {
+      e.printStackTrace();
+    } catch (IllegalAccessException e) {
+      e.printStackTrace();
+    }
+  }
+
+  /**
 	 * Fills all fields with the data defined in the config file.
 	 * 
 	 * @throws IllegalArgumentException
 	 * @throws IllegalAccessException
 	 */
-	private void initialize() throws IllegalArgumentException,
-			IllegalAccessException {
-		Field[] fields = this.getClass().getFields();
-		for (Field f : fields) {
-			if (this.getProperty(f.getName()) == null) {
-				System.err.print("Property '" + f.getName()
-						+ "' not defined in config file");
-			}
-			if (f.getType().equals(String.class)) {
-				f.set(this, this.getProperty(f.getName()));
-			} else if (f.getType().equals(long.class)) {
-				f.setLong(this, Long.valueOf(this.getProperty(f.getName())));
-			} else if (f.getType().equals(int.class)) {
-				f.setInt(this, Integer.valueOf(this.getProperty(f.getName())));
-			} else if (f.getType().equals(boolean.class)) {
-				f.setBoolean(this,
-						Boolean.valueOf(this.getProperty(f.getName())));
-			} else if (f.getType().equals(String[].class)) {
-				f.set(this, this.getProperty(f.getName()).split(";"));
-			} else if (f.getType().equals(int[].class)) {
-				String[] tmp = this.getProperty(f.getName()).split(";");
-				int[] ints = new int[tmp.length];
-				for (int i = 0; i < tmp.length; i++) {
-					ints[i] = Integer.parseInt(tmp[i]);
-				}
-				f.set(this, ints);
-			} else if (f.getType().equals(long[].class)) {
-				String[] tmp = this.getProperty(f.getName()).split(";");
-				long[] longs = new long[tmp.length];
-				for (int i = 0; i < tmp.length; i++) {
-					longs[i] = Long.parseLong(tmp[i]);
-				}
-				f.set(this, longs);
-			}
-		}
-	}
+  private void initialize() throws IllegalArgumentException, IllegalAccessException {
+    Field[] fields = this.getClass().getFields();
+    for (Field f : fields) {
+      if (this.getProperty(f.getName()) == null) {
+        System.err.print("Property \'" + f.getName() + "\' not defined in config file");
+      }
+      if (f.getType().equals(String.class)) {
+        f.set(this, this.getProperty(f.getName()));
+      } else {
+        if (f.getType().equals(long.class)) {
+          f.setLong(this, Long.valueOf(this.getProperty(f.getName())));
+        } else {
+          if (f.getType().equals(int.class)) {
+            f.setInt(this, Integer.valueOf(this.getProperty(f.getName())));
+          } else {
+            if (f.getType().equals(boolean.class)) {
+              f.setBoolean(this, Boolean.valueOf(this.getProperty(f.getName())));
+            } else {
+              if (f.getType().equals(String[].class)) {
+                f.set(this, this.getProperty(f.getName()).split(";"));
+              } else {
+                if (f.getType().equals(int[].class)) {
+                  String[] tmp = this.getProperty(f.getName()).split(";");
+                  int[] ints = new int[tmp.length];
+                  for (int i = 0; i < tmp.length; i++) {
+                    ints[i] = Integer.parseInt(tmp[i]);
+                  }
+                  f.set(this, ints);
+                } else {
+                  if (f.getType().equals(long[].class)) {
+                    String[] tmp = this.getProperty(f.getName()).split(";");
+                    long[] longs = new long[tmp.length];
+                    for (int i = 0; i < tmp.length; i++) {
+                      longs[i] = Long.parseLong(tmp[i]);
+                    }
+                    f.set(this, longs);
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
 
-	public static Config get() {
-		if (instance == null) {
-			instance = new Config();
-		}
-		return instance;
-	}
+  public static Config get() {
+    if (instance == null) {
+      instance = new Config();
+    }
+    return instance;
+  }
 }
