47ee878186d2217473db3517335dc5ee6dc15e14
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 23:13:02 +0000

    Mining Framework Analysis

--- src/test/java/net/imagej/ops/copy/CopyRAITest.java
+++ src/test/java/net/imagej/ops/copy/CopyRAITest.java
@@ -1,38 +1,7 @@
-/*
- * #%L
- * ImageJ2 software for multidimensional image processing and analysis.
- * %%
- * Copyright (C) 2014 - 2023 ImageJ2 developers.
- * %%
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- * 
- * 1. Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- * #L%
- */
-
 package net.imagej.ops.copy;
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
-
 import net.imagej.ops.AbstractOpTest;
 import net.imagej.ops.special.hybrid.Hybrids;
 import net.imagej.ops.special.hybrid.UnaryHybridCF;
@@ -49,7 +18,6 @@ import net.imglib2.type.logic.NativeBoolType;
 import net.imglib2.type.numeric.integer.UnsignedByteType;
 import net.imglib2.view.IntervalView;
 import net.imglib2.view.Views;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.scijava.util.MersenneTwisterFast;
@@ -58,139 +26,87 @@ import org.scijava.util.MersenneTwisterFast;
  * Test {@link CopyRAI}.
  *
  * @author Tim-Oliver Buchholz (University of Konstanz)
- * @author Gabriel Selzer
  */
 public class CopyRAITest extends AbstractOpTest {
+  private Img<UnsignedByteType> input;
 
-	private Img<UnsignedByteType> input;
+  Img<UnsignedByteType> input2;
 
-	Img<UnsignedByteType> input2;
-	Img<UnsignedByteType> inputPlanar;
-	IntervalView<UnsignedByteType> view;
-	IntervalView<UnsignedByteType> viewPlanar;
+  Img<UnsignedByteType> inputPlanar;
 
-	int[] size1 = new int[] { 64, 64, 64 };
-	int[] size2 = new int[] { 32, 32, 32 };
+  IntervalView<UnsignedByteType> view;
 
-	double delta = 0.0000001;
+  IntervalView<UnsignedByteType> viewPlanar;
 
-	@Before
-	public void createData() {
-		input = new ArrayImgFactory<UnsignedByteType>().create(new int[] { 120,
-			100 }, new UnsignedByteType());
+  int[] size1 = new int[] { 64, 64, 64 };
 
-		final MersenneTwisterFast r = new MersenneTwisterFast(System.currentTimeMillis());
+  int[] size2 = new int[] { 32, 32, 32 };
 
-		final Cursor<UnsignedByteType> inc = input.cursor();
+  double delta = 0.0000001;
 
-		while (inc.hasNext()) {
-			inc.next().setReal(r.nextDouble() * 255);
-		}
+  @Before public void createData() {
+    input = new ArrayImgFactory<UnsignedByteType>().create(new int[] { 120, 100 }, new UnsignedByteType());
+    final MersenneTwisterFast r = new MersenneTwisterFast(System.currentTimeMillis());
+    final Cursor<UnsignedByteType> inc = input.cursor();
+    while (inc.hasNext()) {
+      inc.next().setReal(r.nextDouble() * 255);
+    }
+    final long[] start = new long[] { 16, 16, 16 };
+    final long[] end = new long[] { 47, 47, 47 };
+    input2 = ops.create().img(new FinalDimensions(size1), new UnsignedByteType());
+    inputPlanar = ops.create().img(new FinalDimensions(size1), new UnsignedByteType(), new PlanarImgFactory<UnsignedByteType>());
+    view = Views.interval(input2, new FinalInterval(start, end));
+    viewPlanar = Views.interval(inputPlanar, new FinalInterval(start, end));
+    final Cursor<UnsignedByteType> cursor = view.cursor();
+    final Cursor<UnsignedByteType> cursorPlanar = viewPlanar.cursor();
+    while (cursor.hasNext()) {
+      cursor.fwd();
+      cursorPlanar.fwd();
+      cursor.get().setReal(100.0);
+      cursorPlanar.get().setReal(100.0);
+    }
+  }
 
-		// create
-		final long[] start = new long[] { 16, 16, 16 };
-		final long[] end = new long[] { 47, 47, 47 };
+  @Test public void copyRAINoOutputTest() {
+    @SuppressWarnings(value = { "unchecked" }) final RandomAccessibleInterval<UnsignedByteType> output = (RandomAccessibleInterval<UnsignedByteType>) ops.run(CopyRAI.class, input);
+    final Cursor<UnsignedByteType> inc = input.localizingCursor();
+    final RandomAccess<UnsignedByteType> outRA = output.randomAccess();
+    while (inc.hasNext()) {
+      inc.fwd();
+      outRA.setPosition(inc);
+      assertEquals(inc.get().get(), outRA.get().get());
+    }
+  }
 
-		// create an input with a cube at the center
-		input2 = ops.create().img(new FinalDimensions(size1),
-			new UnsignedByteType());
+  @Test public void copyRAIWithOutputTest() {
+    final Img<UnsignedByteType> output = input.factory().create(input, input.firstElement());
+    ops.run(CopyRAI.class, output, input);
+    final Cursor<UnsignedByteType> inc = input.cursor();
+    final Cursor<UnsignedByteType> outc = output.cursor();
+    while (inc.hasNext()) {
+      assertEquals(inc.next().get(), outc.next().get());
+    }
+  }
 
-		// create the same input but force it to be a planar image
-		inputPlanar = ops.create().img(new FinalDimensions(size1),
-			new UnsignedByteType(), new PlanarImgFactory<UnsignedByteType>());
+  @Test @SuppressWarnings(value = { "unchecked", "rawtypes" }) public void copyRAIDifferentSizeTest() {
+    final UnaryHybridCF<IntervalView<UnsignedByteType>, RandomAccessibleInterval<UnsignedByteType>> copy = (UnaryHybridCF) Hybrids.unaryCF(ops, CopyRAI.class, RandomAccessibleInterval.class, IntervalView.class);
+    assertNotNull(copy);
+    final Img<UnsignedByteType> out = ops.create().img(new FinalDimensions(size2), new UnsignedByteType());
+    copy.compute(view, out);
+    assertEquals(ops.stats().mean(out).getRealDouble(), 100.0, delta);
+    final Img<UnsignedByteType> outFromPlanar = ops.create().img(new FinalDimensions(size2), new UnsignedByteType());
+    copy.compute(viewPlanar, outFromPlanar);
+    assertEquals(ops.stats().mean(outFromPlanar).getRealDouble(), 100.0, delta);
+  }
 
-		// get centered views
-		view = Views.interval(input2, new FinalInterval(start, end));
-		viewPlanar = Views.interval(inputPlanar, new FinalInterval(start, end));
-
-		final Cursor<UnsignedByteType> cursor = view.cursor();
-		final Cursor<UnsignedByteType> cursorPlanar = viewPlanar.cursor();
-
-		// set every pixel in the view to 100
-		while (cursor.hasNext()) {
-			cursor.fwd();
-			cursorPlanar.fwd();
-			cursor.get().setReal(100.0);
-			cursorPlanar.get().setReal(100.0);
-
-		}
-	}
-
-	@Test
-	public void copyRAINoOutputTest() {
-		@SuppressWarnings("unchecked")
-		final RandomAccessibleInterval<UnsignedByteType> output =
-			(RandomAccessibleInterval<UnsignedByteType>) ops.run(CopyRAI.class,
-				input);
-
-		final Cursor<UnsignedByteType> inc = input.localizingCursor();
-		final RandomAccess<UnsignedByteType> outRA = output.randomAccess();
-
-		while (inc.hasNext()) {
-			inc.fwd();
-			outRA.setPosition(inc);
-			assertEquals(inc.get().get(), outRA.get().get());
-		}
-	}
-
-	@Test
-	public void copyRAIWithOutputTest() {
-		final Img<UnsignedByteType> output = input.factory().create(input, input
-			.firstElement());
-
-		ops.run(CopyRAI.class, output, input);
-
-		final Cursor<UnsignedByteType> inc = input.cursor();
-		final Cursor<UnsignedByteType> outc = output.cursor();
-
-		while (inc.hasNext()) {
-			assertEquals(inc.next().get(), outc.next().get());
-		}
-	}
-
-	@Test
-	@SuppressWarnings({ "unchecked", "rawtypes" })
-	public void copyRAIDifferentSizeTest() {
-
-		// create a copy op
-		final UnaryHybridCF<IntervalView<UnsignedByteType>, RandomAccessibleInterval<UnsignedByteType>> copy =
-			(UnaryHybridCF) Hybrids.unaryCF(ops, CopyRAI.class,
-				RandomAccessibleInterval.class, IntervalView.class);
-
-		assertNotNull(copy);
-
-		final Img<UnsignedByteType> out = ops.create().img(new FinalDimensions(
-			size2), new UnsignedByteType());
-
-		// copy view to output and assert that is equal to the mean of the view
-		copy.compute(view, out);
-		assertEquals(ops.stats().mean(out).getRealDouble(), 100.0, delta);
-
-		// also try with a planar image
-		final Img<UnsignedByteType> outFromPlanar = ops.create().img(
-			new FinalDimensions(size2), new UnsignedByteType());
-
-		copy.compute(viewPlanar, outFromPlanar);
-		assertEquals(ops.stats().mean(outFromPlanar).getRealDouble(), 100.0, delta);
-
-	}
-
-	@Test
-	public void copyBooleanTypesTest() {
-
-		final Img<NativeBoolType> in = ops.create().img(new FinalDimensions(size2),
-			new NativeBoolType());
-		Cursor<NativeBoolType> cursor = in.cursor();
-		while (cursor.hasNext()) {
-			cursor.next().set(true);
-		}
-
-		final Img<BitType> out = ops.create().img(new FinalDimensions(size2),
-			new BitType());
-
-		ops.run("copy.rai", out, in);
-
-		assertTrue(out.firstElement().get());
-
-	}
+  @Test public void copyBooleanTypesTest() {
+    final Img<NativeBoolType> in = ops.create().img(new FinalDimensions(size2), new NativeBoolType());
+    Cursor<NativeBoolType> cursor = in.cursor();
+    while (cursor.hasNext()) {
+      cursor.next().set(true);
+    }
+    final Img<BitType> out = ops.create().img(new FinalDimensions(size2), new BitType());
+    ops.run("copy.rai", out, in);
+    assertTrue(out.firstElement().get());
+  }
 }
