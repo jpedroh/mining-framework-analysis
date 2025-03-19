5561c0fe3b4b7115caee0f23f420072a513e7430
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 17:26:05 +0000

    Mining Framework Analysis

--- src/main/java/endpoint/DatastoreServlet.java
+++ src/main/java/endpoint/DatastoreServlet.java
@@ -1,5 +1,4 @@
 package endpoint;
-
 import java.io.IOException;
 import java.util.Enumeration;
 import java.util.HashMap;
@@ -7,15 +6,12 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.logging.Logger;
-
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import org.reflections.Reflections;
-
 import endpoint.actions.RepositoryActions;
 import endpoint.hooks.RepositoryHooks;
 import endpoint.query.DatastoreQuery;
@@ -28,182 +24,158 @@ import endpoint.transformers.RepositoryTransformers;
 import endpoint.utils.JsonUtils;
 
 public class DatastoreServlet extends HttpServlet {
+  private static Logger logger = Logger.getLogger(DatastoreServlet.class.getSimpleName());
 
-	private static Logger logger = Logger.getLogger(DatastoreServlet.class.getSimpleName());
+  private static final long serialVersionUID = 8155293897299089610L;
 
-	private static final long serialVersionUID = 8155293897299089610L;
+  private Map<String, Class<?>> endpoints = new HashMap<String, Class<?>>();
 
-	private Map<String, Class<?>> endpoints = new HashMap<String, Class<?>>();
+  @Override public void init(ServletConfig config) throws ServletException {
+    super.init(config);
+    String packagePrefix = config.getInitParameter("packagePrefix");
+    bootEndpoint(packagePrefix);
+    scanEndpoints(packagePrefix);
+  }
 
-	@Override
-	public void init(ServletConfig config) throws ServletException {
-		super.init(config);
-		String packagePrefix = config.getInitParameter("packagePrefix");
-		bootEndpoint(packagePrefix);
-		scanEndpoints(packagePrefix);
-	}
+  public DatastoreServlet() {
+  }
 
-	public DatastoreServlet() {
-	}
+  protected DatastoreServlet(String packagePrefix) {
+    scanEndpoints(packagePrefix);
+  }
 
-	protected DatastoreServlet(String packagePrefix) {
-		scanEndpoints(packagePrefix);
-	}
+  private void bootEndpoint(String packagePrefix) {
+    RepositoryActions.scan(packagePrefix);
+    RepositoryHooks.scan(packagePrefix);
+    RepositoryTransformers.scan(packagePrefix);
+  }
 
-	private void bootEndpoint(String packagePrefix) {
-		RepositoryActions.scan(packagePrefix);
-		RepositoryHooks.scan(packagePrefix);
-		RepositoryTransformers.scan(packagePrefix);
-	}
+  private void scanEndpoints(String packagePrefix) {
+    Reflections reflections = new Reflections(packagePrefix);
+    Set<Class<?>> clazzes = reflections.getTypesAnnotatedWith(Endpoint.class);
+    for (Class<?> endpoint : clazzes) {
+      Endpoint annotation = endpoint.getAnnotation(Endpoint.class);
+      endpoints.put(annotation.path(), (Class<?>) endpoint);
+    }
+  }
 
-	private void scanEndpoints(String packagePrefix) {
-		Reflections reflections = new Reflections(packagePrefix);
-		Set<Class<?>> clazzes = reflections.getTypesAnnotatedWith(Endpoint.class);
+  private void response(HttpServletResponse resp, HttpResponse httpResponse) throws IOException {
+    if (httpResponse == null) {
+      JsonResponse jsonResponse = new JsonResponse("{\"status\":\"ok\"}");
+      jsonResponse.execute(resp);
+      return;
+    }
+    httpResponse.execute(resp);
+  }
 
-		for (Class<?> endpoint : clazzes) {
-			Endpoint annotation = endpoint.getAnnotation(Endpoint.class);
-			endpoints.put(annotation.path(), (Class<?>) endpoint);
-		}
-	}
+  @Override protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    HttpResponse httpResponse;
+    try {
+      httpResponse = execute(req.getMethod(), getPath(req), JsonUtils.readJson(req.getReader()), makeParams(req));
+    } catch (HttpException e) {
+      httpResponse = new ErrorResponse(e.getHttpStatus(), e.getText());
+    } catch (DatastoreException e) {
+      httpResponse = new ErrorResponse(403);
+    }
+    response(resp, httpResponse);
+  }
 
-	private void response(HttpServletResponse resp, HttpResponse httpResponse) throws IOException {
-		if (httpResponse == null) {
-			JsonResponse jsonResponse = new JsonResponse("{\"status\":\"ok\"}");
-			jsonResponse.execute(resp);
-			return;
-		}
-		httpResponse.execute(resp);
-	}
+  @SuppressWarnings(value = { "rawtypes" }) private Map<String, String> makeParams(HttpServletRequest req) {
+    Map<String, String> map = new HashMap<String, String>();
+    Enumeration e = req.getParameterNames();
+    while (e.hasMoreElements()) {
+      String name = (String) e.nextElement();
+      map.put(name, req.getParameter(name));
+    }
+    return map;
+  }
 
-	@Override
-	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+  private String getPath(HttpServletRequest req) {
+    return req.getRequestURI().substring(req.getServletPath().length());
+  }
 
-		HttpResponse httpResponse;
-		try {
+  protected HttpResponse execute(String method, String path, String requestJson, Map<String, String> params) throws HttpException {
+    DatastoreRouter router = new DatastoreRouter(method, path);
+    Class<?> clazz = endpoints.get(router.getEndpointPath());
+    Endpoint endpoint = clazz.getAnnotation(Endpoint.class);
+    Repository r = getRepository(params);
+    switch (router.getAction()) {
+      case INDEX:
+      if (!endpoint.index()) {
+        throw new HttpException(403);
+      }
+      return new JsonResponse(index(r, clazz, q(params), t(params)));
+      case SHOW:
+      try {
+        return new JsonResponse(get(r, clazz, router.getId(), t(params)));
+      } catch (NoResultException e) {
+        throw new HttpException(404);
+      }
+      case CREATE:
+      return new JsonResponse(save(r, clazz, requestJson));
+      case UPDATE:
+      if (!endpoint.update()) {
+        throw new HttpException(403);
+      }
+      return new JsonResponse(save(r, clazz, requestJson));
+      case CUSTOM:
+      return action(r, clazz, router.getMethod(), router.getCustomAction(), router.getId(), params);
+    }
+    throw new IllegalArgumentException("Invalid datastore action");
+  }
 
-			httpResponse = execute(req.getMethod(), getPath(req), JsonUtils.readJson(req.getReader()), makeParams(req));
+  private String q(Map<String, String> params) {
+    return params == null ? null : params.get("q");
+  }
 
-		} catch (HttpException e) {
-			httpResponse = new ErrorResponse(e.getHttpStatus(), e.getText());
-		} catch (DatastoreException e) {
-			httpResponse = new ErrorResponse(403);
-		}
+  private String t(Map<String, String> params) {
+    return params == null ? null : params.get("t");
+  }
 
-		response(resp, httpResponse);
-	}
+  protected Repository getRepository(Map<String, String> params) {
+    return Repository.r();
+  }
 
-	@SuppressWarnings("rawtypes")
-	private Map<String, String> makeParams(HttpServletRequest req) {
-		Map<String, String> map = new HashMap<String, String>();
+  private HttpResponse action(Repository r, Class<?> clazz, String method, String customAction, Long id, Map<String, String> params) throws HttpException {
+    return r.action(clazz, method, customAction, id, params);
+  }
 
-		Enumeration e = req.getParameterNames();
-		while (e.hasMoreElements()) {
-			String name = (String) e.nextElement();
-			map.put(name, req.getParameter(name));
-		}
+  private String save(Repository r, Class<?> clazz, String json) throws HttpException {
+    logger.warning("JSON: " + json);
+    if (JsonUtils.isJsonArray(json)) {
+      return saveFromArray(r, clazz, json);
+    } else {
+      return saveFromObject(r, clazz, json);
+    }
+  }
 
-		return map;
-	}
+  private String saveFromObject(Repository r, Class<?> clazz, String json) throws HttpException {
+    Object object = JsonUtils.from(r, json, clazz);
+    r.save(object);
+    return JsonUtils.to(object);
+  }
 
-	private String getPath(HttpServletRequest req) {
-		return req.getRequestURI().substring(req.getServletPath().length());
-	}
+  private String saveFromArray(Repository r, Class<?> clazz, String json) throws HttpException {
+    List<?> objects = JsonUtils.fromList(r, json, clazz);
+    for (Object object : objects) {
+      r.save(object);
+    }
+    return JsonUtils.to(objects);
+  }
 
-	protected HttpResponse execute(String method, String path, String requestJson, Map<String, String> params) throws HttpException {
-		DatastoreRouter router = new DatastoreRouter(method, path);
-		Class<?> clazz = endpoints.get(router.getEndpointPath());
+  private String index(Repository r, Class<?> clazz, String q, String t) throws HttpException {
+    DatastoreQuery<?> query = r.queryWithHooks(clazz);
+    if (q != null) {
+      query.options(DatastoreQueryOptions.parse(q));
+    }
+    if (t != null) {
+      return JsonUtils.to(query.transform(t).list());
+    }
+    return JsonUtils.to(query.list());
+  }
 
-		Endpoint endpoint = clazz.getAnnotation(Endpoint.class);
-
-		Repository r = getRepository(params);
-
-		switch (router.getAction()) {
-			case INDEX:
-				if (!endpoint.index()) {
-					throw new HttpException(403);
-				}
-				return new JsonResponse(index(r, clazz, q(params), t(params)));
-			case SHOW:
-				try {
-					return new JsonResponse(get(r, clazz, router.getId(), t(params)));
-				} catch (NoResultException e) {
-					throw new HttpException(404);
-				}
-			case CREATE:
-				return new JsonResponse(save(r, clazz, requestJson));
-			case UPDATE:
-				if (!endpoint.update()) {
-					throw new HttpException(403);
-				}
-				return new JsonResponse(save(r, clazz, requestJson));
-			case CUSTOM:
-				return action(r, clazz, router.getMethod(), router.getCustomAction(), router.getId(), params);
-		}
-
-		throw new IllegalArgumentException("Invalid datastore action");
-	}
-
-	private String q(Map<String, String> params) {
-		return params == null ? null : params.get("q");
-	}
-
-	private String t(Map<String, String> params) {
-		return params == null ? null : params.get("t");
-	}
-
-	protected Repository getRepository(Map<String, String> params) {
-		return Repository.r();
-	}
-
-	private HttpResponse action(Repository r, Class<?> clazz, String method, String customAction, Long id, Map<String, String> params) throws HttpException {
-		return r.action(clazz, method, customAction, id, params);
-	}
-
-	private String save(Repository r, Class<?> clazz, String json) throws HttpException {
-		logger.warning("JSON: " + json);
-
-		if (JsonUtils.isJsonArray(json)) {
-			return saveFromArray(r, clazz, json);
-		} else {
-			return saveFromObject(r, clazz, json);
-		}
-	}
-
-	private String saveFromObject(Repository r, Class<?> clazz, String json) throws HttpException {
-		Object object = JsonUtils.from(r, json, clazz);
-
-		r.save(object);
-
-		return JsonUtils.to(object);
-	}
-
-	private String saveFromArray(Repository r, Class<?> clazz, String json) throws HttpException {
-		List<?> objects = JsonUtils.fromList(r, json, clazz);
-
-		for (Object object : objects) {
-			r.save(object);
-		}
-
-		return JsonUtils.to(objects);
-	}
-
-	private String index(Repository r, Class<?> clazz, String q, String t) throws HttpException {
-		DatastoreQuery<?> query = r.queryWithHooks(clazz);
-
-		if (q != null) {
-			query.options(DatastoreQueryOptions.parse(q));
-		}
-
-		if (t != null) {
-			return JsonUtils.to(query.transform(t).list());
-		}
-
-		return JsonUtils.to(query.list());
-	}
-
-	private String get(Repository r, Class<?> clazz, Long id, String t) throws HttpException {
-		DatastoreQuery<?> query = r.queryWithHooks(clazz).whereById("=", id);
-		return JsonUtils.to(t == null ? query.only() : query.transform(t).only());
-	}
-
+  private String get(Repository r, Class<?> clazz, Long id, String t) throws HttpException {
+    DatastoreQuery<?> query = r.queryWithHooks(clazz).whereById("=", id);
+    return JsonUtils.to(t == null ? query.only() : query.transform(t).only());
+  }
 }
--- src/main/java/endpoint/Repository.java
+++ src/main/java/endpoint/Repository.java
@@ -1,14 +1,11 @@
 package endpoint;
-
 import java.util.Map;
 import java.util.logging.Logger;
-
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
-
 import endpoint.actions.RepositoryActions;
 import endpoint.hooks.RepositoryHooks;
 import endpoint.query.DatastoreQuery;
@@ -16,110 +13,101 @@ import endpoint.response.HttpResponse;
 import endpoint.utils.EntityUtils;
 
 public class Repository {
+  @SuppressWarnings(value = { "unused" }) private static Logger logger = Logger.getLogger(Repository.class.getSimpleName());
 
-	@SuppressWarnings("unused")
-	private static Logger logger = Logger.getLogger(Repository.class.getSimpleName());
+  private Namespace namespace;
 
-	private Namespace namespace;
+  public static Repository r() {
+    return new Repository();
+  }
 
-	public static Repository r() {
-		return new Repository();
-	}
+  public static Repository r(String ns) {
+    return new Repository(ns);
+  }
 
-	public static Repository r(String ns) {
-		return new Repository(ns);
-	}
+  private Repository() {
+    this.namespace = new Namespace();
+  }
 
-	private Repository() {
-		this.namespace = new Namespace();
-	}
+  private Repository(String ns) {
+    this.namespace = new Namespace(ns);
+  }
 
-	private Repository(String ns) {
-		this.namespace = new Namespace(ns);
-	}
+  public Repository namespace(String ns) {
+    namespace.setNs(ns);
+    return this;
+  }
 
-	public Repository namespace(String ns) {
-		namespace.setNs(ns);
-		return this;
-	}
+  public Namespace namespace() {
+    return namespace;
+  }
 
-	public Namespace namespace() {
-		return namespace;
-	}
+  public String currentNamespace() {
+    return namespace.getNs();
+  }
 
-	public String currentNamespace() {
-		return namespace.getNs();
-	}
+  public void save(Object object) throws HttpException {
+    namespace.set(object.getClass());
+    try {
+      RepositoryHooks.beforeSave(this, object);
+      Entity entity = createEntity(object);
+      EntityUtils.toEntity(object, entity);
+      saveEntity(object, entity, null);
+      RepositoryHooks.afterSave(this, object);
+    }  finally {
+      namespace.reset();
+    }
+  }
 
-	public void save(Object object) throws HttpException {
-		namespace.set(object.getClass());
-		try {
-			RepositoryHooks.beforeSave(this, object);
+  public HttpResponse action(Class<?> clazz, String method, String action, Long id, Map<String, String> params) throws HttpException {
+    namespace.set(clazz);
+    try {
+      return RepositoryActions.execute(this, clazz, method, action, id, params);
+    }  finally {
+      namespace.reset();
+    }
+  }
 
-			Entity entity = createEntity(object);
-			EntityUtils.toEntity(object, entity);
-			saveEntity(object, entity, null);
+  public <T extends java.lang.Object> DatastoreQuery<T> queryWithHooks(Class<T> clazz) throws HttpException {
+    DatastoreQuery<T> q = DatastoreQuery.q(clazz, this);
+    RepositoryHooks.beforeQuery(this, q, clazz);
+    return q;
+  }
 
-			RepositoryHooks.afterSave(this, object);
-		} finally {
-			namespace.reset();
-		}
-	}
+  public <T extends java.lang.Object> DatastoreQuery<T> query(Class<T> clazz) {
+    return DatastoreQuery.q(clazz, this);
+  }
 
-	public HttpResponse action(Class<?> clazz, String method, String action, Long id, Map<String, String> params) throws HttpException {
-		namespace.set(clazz);
-		try {
-			return RepositoryActions.execute(this, clazz, method, action, id, params);
-		} finally {
-			namespace.reset();
-		}
-	}
+  public void delete(Object object) {
+    delete(EntityUtils.getId(object), object.getClass());
+  }
 
-	public <T> DatastoreQuery<T> queryWithHooks(Class<T> clazz) throws HttpException {
-		DatastoreQuery<T> q = DatastoreQuery.q(clazz, this);
-		RepositoryHooks.beforeQuery(this, q, clazz);
-		return q;
-	}
+  public void delete(Long id, Class<?> clazz) {
+    namespace.set(clazz);
+    try {
+      DatastoreService datastoreService = DatastoreServiceFactory.getDatastoreService();
+      Key key = EntityUtils.createKey(id, clazz);
+      datastoreService.delete(key);
+    }  finally {
+      namespace.reset();
+    }
+  }
 
-	public <T> DatastoreQuery<T> query(Class<T> clazz) {
-		return DatastoreQuery.q(clazz, this);
-	}
+  private void saveEntity(Object object, Entity entity, String action) {
+    DatastoreService datastoreService = DatastoreServiceFactory.getDatastoreService();
+    Key key = datastoreService.put(entity);
+    EntityUtils.setKey(this, object, key);
+  }
 
-	public void delete(Object object) {
-		delete(EntityUtils.getId(object), object.getClass());
-	}
-
-	public void delete(Long id, Class<?> clazz) {
-		namespace.set(clazz);
-		try {
-			DatastoreService datastoreService = DatastoreServiceFactory.getDatastoreService();
-
-			Key key = EntityUtils.createKey(id, clazz);
-			datastoreService.delete(key);
-
-		} finally {
-			namespace.reset();
-		}
-
-	}
-
-	private void saveEntity(Object object, Entity entity, String action) {
-		DatastoreService datastoreService = DatastoreServiceFactory.getDatastoreService();
-		Key key = datastoreService.put(entity);
-		EntityUtils.setKey(this, object, key);
-	}
-
-	private Entity createEntity(Object object) {
-		Entity entity = null;
-
-		Key currentKey = EntityUtils.getKey(object);
-
-		if (currentKey == null) {
-			entity = new Entity(EntityUtils.getKind(object.getClass()));
-		} else {
-			Key key = KeyFactory.createKey(currentKey.getKind(), currentKey.getId());
-			entity = new Entity(key);
-		}
-		return entity;
-	}
+  private Entity createEntity(Object object) {
+    Entity entity = null;
+    Key currentKey = EntityUtils.getKey(object);
+    if (currentKey == null) {
+      entity = new Entity(EntityUtils.getKind(object.getClass()));
+    } else {
+      Key key = KeyFactory.createKey(currentKey.getKind(), currentKey.getId());
+      entity = new Entity(key);
+    }
+    return entity;
+  }
 }
