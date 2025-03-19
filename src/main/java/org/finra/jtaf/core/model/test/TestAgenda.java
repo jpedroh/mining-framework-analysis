e2aca870eb41da4b82f22d1abe05115fb0506ed6
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 15:00:07 +0000

    Mining Framework Analysis

--- src/main/java/org/finra/jtaf/core/model/test/TestAgenda.java
+++ src/main/java/org/finra/jtaf/core/model/test/TestAgenda.java
@@ -1,21 +1,4 @@
-/*
- * (C) Copyright 2014 Java Test Automation Framework Contributors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * 
- */
 package org.finra.jtaf.core.model.test;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
@@ -27,74 +10,73 @@ import java.util.Set;
  * 
  */
 public class TestAgenda {
+  private final List<TestScript> testScripts;
 
-	private final List<TestScript> testScripts;
+  private Set<String> automationValues;
 
-	private Set<String> automationValues;
-	private int maxThreads = 1;
+  private int maxThreads = 1;
 
-	public TestAgenda() {
-		testScripts = new ArrayList<TestScript>();
-		automationValues = new HashSet<String>();
-	}
+  public TestAgenda() {
+    testScripts = new ArrayList<TestScript>();
+    automationValues = new HashSet<String>();
+  }
 
-	public List<TestScript> getTestScripts() {
-		return testScripts;
-	}
+  public List<TestScript> getTestScripts() {
+    return testScripts;
+  }
 
-	/**
+  /**
 	 * This is a set operation and values are converted to lowercase first
 	 * 
 	 * @param automationValue
 	 * @return 
 	 */
-	public boolean addAutomationValue(String automationValue) {
-		return this.automationValues.add(automationValue.toLowerCase());
-	}
-	
-	/**
+  public boolean addAutomationValue(String automationValue) {
+    return this.automationValues.add(automationValue.toLowerCase());
+  }
+
+  /**
 	 * returns a read only version of the set of automation values for the test agenda
 	 * 
 	 * @return read only set of automation values
 	 */
-	public Set<String> getAutomationValues() {
-		if (automationValues == null) {
-			return Collections.emptySet();
-		} else {
-			return Collections.unmodifiableSet(automationValues);
-		}
-	}
-	
-	/**
+  public Set<String> getAutomationValues() {
+    if (automationValues == null) {
+      return Collections.emptySet();
+    } else {
+      return Collections.unmodifiableSet(automationValues);
+    }
+  }
+
+  /**
 	 * Checks whether the automationValues set contains the given value.
 	 * Ensures correct case sensitivity.
 	 * 
 	 * @param value to check for
 	 * @return true if value exists, case insensitive
 	 */
-	public boolean containsAutomationValue(String value) {
-		if (value == null || automationValues == null) {
-			return false;
-		} else {
-			return automationValues.contains(value.toLowerCase());
-		}
-	}
-	
-	/**
+  public boolean containsAutomationValue(String value) {
+    if (value == null || automationValues == null) {
+      return false;
+    } else {
+      return automationValues.contains(value.toLowerCase());
+    }
+  }
+
+  /**
 	 * pass through of is empty for the automationValues set .
 	 * 
 	 * @return true if null or empty
 	 */
-	public boolean isAutomationValuesEmpty() {
-		return automationValues == null || automationValues.isEmpty();
-	}
+  public boolean isAutomationValuesEmpty() {
+    return automationValues == null || automationValues.isEmpty();
+  }
 
-	public void setThreadCount(String threads) {
-		maxThreads = Integer.parseInt(threads);
-	}
-	
-	public int getThreadCount() {
-		return maxThreads;
-	}
+  public void setThreadCount(String threads) {
+    maxThreads = Integer.parseInt(threads);
+  }
 
+  public int getThreadCount() {
+    return maxThreads;
+  }
 }
