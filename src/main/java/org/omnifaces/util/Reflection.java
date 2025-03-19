91c9ad9e36dff5aed12b0f415769fd4329a78249
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 22:57:27 +0000

    Mining Framework Analysis

--- src/main/java/org/omnifaces/util/Reflection.java
+++ src/main/java/org/omnifaces/util/Reflection.java
@@ -1,20 +1,6 @@
-/*
- * Copyright 2014 OmniFaces.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License.
- */
 package org.omnifaces.util;
-
 import static java.beans.Introspector.getBeanInfo;
 import static java.beans.PropertyEditorManager.findEditor;
-
 import java.beans.IntrospectionException;
 import java.beans.PropertyDescriptor;
 import java.beans.PropertyEditor;
@@ -27,7 +13,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-
 import javax.enterprise.inject.Typed;
 
 /**
@@ -37,19 +22,19 @@ import javax.enterprise.inject.Typed;
  * @author Arjan Tijms
  *
  */
-@Typed
-public final class Reflection {
+@Typed public final class Reflection {
+  private static final String ERROR_LOAD_CLASS = "Cannot load class \'%s\'.";
 
-	private static final String ERROR_LOAD_CLASS = "Cannot load class '%s'.";
-	private static final String ERROR_CREATE_INSTANCE = "Cannot create instance of class '%s'.";
-	private static final String ERROR_ACCESS_FIELD = "Cannot access field '%s' of class '%s'.";
-	private static final String ERROR_INVOKE_METHOD = "Cannot invoke method '%s' of class '%s' with arguments %s.";
+  private static final String ERROR_CREATE_INSTANCE = "Cannot create instance of class \'%s\'.";
 
-	private Reflection() {
-		// Hide constructor.
-	}
+  private static final String ERROR_ACCESS_FIELD = "Cannot access field \'%s\' of class \'%s\'.";
 
-	/**
+  private static final String ERROR_INVOKE_METHOD = "Cannot invoke method \'%s\' of class \'%s\' with arguments %s.";
+
+  private Reflection() {
+  }
+
+  /**
 	 * Sets a collection of properties of a given object to the values associated with those properties.
 	 * <p>
 	 * In the map that represents these properties, each key represents the name of the property, with the value
@@ -67,24 +52,21 @@ public final class Reflection {
 	 * @param propertiesToSet
 	 *            the map containing properties and their values to be set on the object
 	 */
-	public static void setProperties(Object object, Map<String, Object> propertiesToSet) {
+  public static void setProperties(Object object, Map<String, Object> propertiesToSet) {
+    try {
+      Map<String, PropertyDescriptor> availableProperties = new HashMap<>();
+      for (PropertyDescriptor propertyDescriptor : getBeanInfo(object.getClass()).getPropertyDescriptors()) {
+        availableProperties.put(propertyDescriptor.getName(), propertyDescriptor);
+      }
+      for (Entry<String, Object> propertyToSet : propertiesToSet.entrySet()) {
+        availableProperties.get(propertyToSet.getKey()).getWriteMethod().invoke(object, propertyToSet.getValue());
+      }
+    } catch (IntrospectionException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+      throw new IllegalStateException(e);
+    }
+  }
 
-	  try {
-			Map<String, PropertyDescriptor> availableProperties = new HashMap<>();
-			for (PropertyDescriptor propertyDescriptor : getBeanInfo(object.getClass()).getPropertyDescriptors()) {
-				availableProperties.put(propertyDescriptor.getName(), propertyDescriptor);
-			}
-
-			for (Entry<String, Object> propertyToSet : propertiesToSet.entrySet()) {
-				availableProperties.get(propertyToSet.getKey()).getWriteMethod().invoke(object, propertyToSet.getValue());
-			}
-
-		} catch (IntrospectionException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
-			throw new IllegalStateException(e);
-		}
-	}
-
-	/**
+  /**
 	 * Sets a collection of properties of a given object to the (optionally coerced) values associated with those properties.
 	 * <p>
 	 * In the map that represents these properties, each key represents the name of the property, with the value
@@ -113,37 +95,29 @@ public final class Reflection {
 	 * @param propertiesToSet
 	 *            the map containing properties and their values to be set on the object
 	 */
-	public static void setPropertiesWithCoercion(Object object, Map<String, Object> propertiesToSet) {
-		try {
-			for (PropertyDescriptor property : getBeanInfo(object.getClass()).getPropertyDescriptors()) {
-				Method setter = property.getWriteMethod();
+  public static void setPropertiesWithCoercion(Object object, Map<String, Object> propertiesToSet) {
+    try {
+      for (PropertyDescriptor property : getBeanInfo(object.getClass()).getPropertyDescriptors()) {
+        Method setter = property.getWriteMethod();
+        if (setter == null) {
+          continue;
+        }
+        if (propertiesToSet.containsKey(property.getName())) {
+          Object value = propertiesToSet.get(property.getName());
+          if (value instanceof String && !property.getPropertyType().equals(String.class)) {
+            PropertyEditor editor = findEditor(property.getPropertyType());
+            editor.setAsText((String) value);
+            value = editor.getValue();
+          }
+          property.getWriteMethod().invoke(object, value);
+        }
+      }
+    } catch (Exception e) {
+      throw new IllegalStateException(e);
+    }
+  }
 
-				if (setter == null) {
-					continue;
-				}
-
-				if (propertiesToSet.containsKey(property.getName())) {
-
-					Object value = propertiesToSet.get(property.getName());
-					if (value instanceof String && !property.getPropertyType().equals(String.class)) {
-
-						// Try to convert Strings to the type expected by the converter
-
-						PropertyEditor editor = findEditor(property.getPropertyType());
-						editor.setAsText((String) value);
-						value = editor.getValue();
-					}
-
-					property.getWriteMethod().invoke(object, value);
-				}
-
-			}
-		} catch (Exception e) {
-			throw new IllegalStateException(e);
-		}
-	}
-
-	/**
+  /**
 	 * Finds a method based on the method name, amount of parameters and limited typing.
 	 * <p>
 	 * Note that this supports overloading, but a limited one. Given an actual parameter of type Long, this will select
@@ -155,45 +129,35 @@ public final class Reflection {
 	 * @param params the method parameters
 	 * @return a method if one is found, null otherwise
 	 */
-	public static Method findMethod(Object base, String methodName, Object... params) {
+  public static Method findMethod(Object base, String methodName, Object... params) {
+    List<Method> methods = new ArrayList<>();
+    for (Class<?> cls = base.getClass(); cls != null; cls = cls.getSuperclass()) {
+      for (Method method : cls.getDeclaredMethods()) {
+        if (method.getName().equals(methodName) && method.getParameterTypes().length == params.length) {
+          methods.add(method);
+        }
+      }
+    }
+    if (methods.size() == 1) {
+      return methods.get(0);
+    }
+    for (Method method : methods) {
+      boolean match = true;
+      Class<?>[] candidateParams = method.getParameterTypes();
+      for (int i = 0; i < params.length; i++) {
+        if (!candidateParams[i].isInstance(params[i])) {
+          match = false;
+          break;
+        }
+      }
+      if (match) {
+        return method;
+      }
+    }
+    return null;
+  }
 
-		List<Method> methods = new ArrayList<>();
-		for (Class<?> cls = base.getClass(); cls != null; cls = cls.getSuperclass()) {
-			for (Method method : cls.getDeclaredMethods()) {
-				if (method.getName().equals(methodName) && method.getParameterTypes().length == params.length) {
-					methods.add(method);
-				}
-			}
-		}
-
-		if (methods.size() == 1) {
-			return methods.get(0);
-		}
-
-		for (Method method : methods) { // Overloaded methods were found. Try to get a match
-			boolean match = true;
-			Class<?>[] candidateParams = method.getParameterTypes();
-			for (int i = 0; i < params.length; i++) {
-				if (!candidateParams[i].isInstance(params[i])) {
-					match = false;
-					break;
-				}
-			}
-
-			// If all candidate parameters were expected and for none of them the actual
-			// parameter was NOT an instance, we have a match
-			if (match) {
-				return method;
-			}
-
-			// Else, at least one parameter was not an instance
-			// Go ahead a test then next methods
-		}
-
-		return null;
-	}
-
-	/**
+  /**
 	 * Returns the class object associated with the given class name, using the context class loader and if
 	 * that fails the defining class loader of the current class.
 	 * @param <T> The expected class type.
@@ -202,54 +166,48 @@ public final class Reflection {
 	 * @throws IllegalStateException If the class cannot be loaded.
 	 * @throws ClassCastException When <code>T</code> is of wrong type.
 	 */
-	@SuppressWarnings("unchecked")
-	public static <T> Class<T> toClass(String className) {
-		try {
-			return (Class<T>) (Class.forName(className, true, Thread.currentThread().getContextClassLoader()));
-		}
-		catch (Exception e) {
-			try {
-				return (Class<T>) Class.forName(className);
-			}
-			catch (Exception ignore) {
-				ignore = null; // Just continue to IllegalStateException on original ClassNotFoundException.
-			}
+  @SuppressWarnings(value = { "unchecked" }) public static <T extends java.lang.Object> Class<T> toClass(String className) {
+    try {
+      return (Class<T>) (Class.forName(className, true, Thread.currentThread().getContextClassLoader()));
+    } catch (Exception e) {
+      try {
+        return (Class<T>) Class.forName(className);
+      } catch (Exception ignore) {
+        ignore = null;
+      }
+      throw new IllegalStateException(String.format(ERROR_LOAD_CLASS, className), e);
+    }
+  }
 
-			throw new IllegalStateException(String.format(ERROR_LOAD_CLASS, className), e);
-		}
-	}
-
-	/**
-	 * Returns a new instance of the given class name using the default constructor.
-	 * @param <T> The expected return type.
-	 * @param className Fully qualified class name of the class for which an instance needs to be created.
-	 * @return A new instance of the given class name using the default constructor.
-	 * @throws IllegalStateException If the class cannot be loaded.
-	 * @throws ClassCastException When <code>T</code> is of wrong type.
-	 */
-	@SuppressWarnings("unchecked")
-	public static <T> T instance(String className) {
-		return (T) instance(toClass(className));
-	}
-
-	/**
-	 * Returns a new instance of the given class object using the default constructor.
+  /**
+	 * Creates an instance of a class with the given fully qualified class name.
+	 *
 	 * @param <T> The generic object type.
-	 * @param clazz The class object for which an instance needs to be created.
-	 * @return A new instance of the given class object using the default constructor.
-	 * @throws IllegalStateException If the class cannot be found, or cannot be instantiated, or when a security manager
-	 * prevents this operation.
+	 * @param className fully qualified class name of the class for which an instance needs to be created
+	 * @return an instance of the class denoted by className
+	 * @throws IllegalStateException if the class cannot be found
 	 */
-	public static <T> T instance(Class<T> clazz) {
-		try {
-			return clazz.newInstance();
-		}
-		catch (Exception e) {
-			throw new IllegalStateException(String.format(ERROR_CREATE_INSTANCE, clazz), e);
-		}
-	}
+  @SuppressWarnings(value = { "unchecked" }) public static <T extends java.lang.Object> T instance(String className) {
+    return (T) instance(toClass(className));
+  }
 
-	/**
+  /**
+	 * Creates a new instance of the class represented by the given Class object
+	 *
+	 * @param <T> The generic object type.
+	 * @param clazz the Class object for which an instance needs to be created
+	 * @return an instance of the class as given by the clazz parameter
+	 * @throws IllegalStateException if the class cannot be found, or cannot be instantiated or when a security manager prevents this operation
+	 */
+  public static <T extends java.lang.Object> T instance(Class<T> clazz) {
+    try {
+      return clazz.newInstance();
+    } catch (Exception e) {
+      throw new IllegalStateException(String.format(ERROR_CREATE_INSTANCE, clazz), e);
+    }
+  }
+
+  /**
 	 * Returns the value of the field of the given instance on the given field name.
 	 * @param <T> The expected return type.
 	 * @param instance The instance to access the given field on.
@@ -259,19 +217,17 @@ public final class Reflection {
 	 * @throws IllegalStateException If the field cannot be accessed.
 	 * @since 2.5
 	 */
-	@SuppressWarnings("unchecked")
-	public static <T> T accessField(Object instance, String fieldName) {
-		try {
-			Field field = instance.getClass().getDeclaredField(fieldName);
-			field.setAccessible(true);
-			return (T) field.get(instance);
-		}
-		catch (Exception e) {
-			throw new IllegalStateException(String.format(ERROR_ACCESS_FIELD, fieldName, instance.getClass()), e);
-		}
-	}
+  @SuppressWarnings(value = { "unchecked" }) public static <T extends java.lang.Object> T accessField(Object instance, String fieldName) {
+    try {
+      Field field = instance.getClass().getDeclaredField(fieldName);
+      field.setAccessible(true);
+      return (T) field.get(instance);
+    } catch (Exception e) {
+      throw new IllegalStateException(String.format(ERROR_ACCESS_FIELD, fieldName, instance.getClass()), e);
+    }
+  }
 
-	/**
+  /**
 	 * Invoke a method of the given instance on the given method name with the given parameters and return the result.
 	 * <p>
 	 * Note: the current implementation assumes for simplicity that no one of the given parameters is null. If one of
@@ -286,17 +242,13 @@ public final class Reflection {
 	 * @throws ClassCastException When <code>T</code> is of wrong type.
 	 * @since 2.5
 	 */
-	@SuppressWarnings("unchecked")
-	public static <T> T invokeMethod(Object instance, String methodName, Object... parameters) {
-		try {
-			Method method = findMethod(instance, methodName, parameters);
-			method.setAccessible(true);
-			return (T) method.invoke(instance, parameters);
-		}
-		catch (Exception e) {
-			throw new IllegalStateException(
-				String.format(ERROR_INVOKE_METHOD, methodName, instance.getClass(), Arrays.toString(parameters)), e);
-		}
-	}
-
+  @SuppressWarnings(value = { "unchecked" }) public static <T extends java.lang.Object> T invokeMethod(Object instance, String methodName, Object... parameters) {
+    try {
+      Method method = findMethod(instance, methodName, parameters);
+      method.setAccessible(true);
+      return (T) method.invoke(instance, parameters);
+    } catch (Exception e) {
+      throw new IllegalStateException(String.format(ERROR_INVOKE_METHOD, methodName, instance.getClass(), Arrays.toString(parameters)), e);
+    }
+  }
 }
