69656c2851ebd44bf1efc091462168172d2ce7fe
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 16:46:31 +0000

    Mining Framework Analysis

--- src/main/java/stockagent/StockAgent.java
+++ src/main/java/stockagent/StockAgent.java
@@ -1,8 +1,6 @@
 package stockagent;
-
 import yahoofinance.Stock;
 import yahoofinance.YahooFinance;
-
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.HashMap;
@@ -10,45 +8,11 @@ import java.util.List;
 import java.util.Map;
 
 public interface StockAgent {
+  public void buyStock(LocalSensor sensor, Stock symbol) throws IOException;
 
-    //watch list of stocks
-    //List of stocks it owns
-    //value of the stocks
-    //amount of stocks per company it owns
+  public void sellStock();
 
-    //getPortfolio of stocks //returns list of stocks we own
+  public HashMap<Stock, Integer> getPorfolio();
 
-    //start buying power "balance" (money it starts with) - cannot buy if balance is less than zero
-
-    //function that buys stock - cannot buy stock if it can't afford it  // void
-    //amount of shares bought is a percentage of our buying power "balance"
-
-    //function that sells stock //void
-
-    //if stock goes up to a certain percentage from bought date
-    //if stock plumets pass buying price (certain percentage)
-
-
-    //agent should only get a list of stock that it can buy/sell from sensor 
-        //pass back out what stocks it bought and the change in price as actuator
-    //have a default action (buy a standard amount of shares) (some percentage of your buying power)
-
-
-    public void buyStock(LocalSensor sensor, Stock symbol) throws IOException;
-
-
-    public void sellStock();
-
-
-    public HashMap<Stock, Integer> getPorfolio();
-
-
-    public double getBuyingPower();
-
-
-
-
-
-
-
+  public double getBuyingPower();
 }
