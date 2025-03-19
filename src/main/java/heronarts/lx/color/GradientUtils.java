a8e0935ea0530cffe99bb0d4319111d7741e02e0
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 23:02:00 +0000

    Mining Framework Analysis

--- src/main/java/heronarts/lx/color/GradientUtils.java
+++ src/main/java/heronarts/lx/color/GradientUtils.java
@@ -1,34 +1,15 @@
-/**
- * Copyright 2016- Mark C. Slee, Heron Arts LLC
- *
- * This file is part of the LX Studio software library. By using
- * LX, you agree to the terms of the LX Studio Software License
- * and Distribution Agreement, available at: http://lx.studio/license
- *
- * Please note that the LX license is not open-source. The license
- * allows for free, non-commercial use.
- *
- * HERON ARTS MAKES NO WARRANTY, EXPRESS, IMPLIED, STATUTORY, OR
- * OTHERWISE, AND SPECIFICALLY DISCLAIMS ANY WARRANTY OF
- * MERCHANTABILITY, NON-INFRINGEMENT, OR FITNESS FOR A PARTICULAR
- * PURPOSE, WITH RESPECT TO THE SOFTWARE.
- *
- * @author Mark C. Slee <mark@heronarts.com>
- */
-
 package heronarts.lx.color;
-
 import heronarts.lx.parameter.LXNormalizedParameter;
 import heronarts.lx.utils.LXUtils;
 
 public class GradientUtils {
-
   public static class GrayTable {
-
     private final static int SIZE = 256;
 
     private final LXNormalizedParameter invert;
+
     private double previousValue = -1;
+
     private boolean dirty = true;
 
     /**
@@ -57,7 +38,7 @@ public class GradientUtils {
           this.lut[i] = 0xff000000 | (b << 16) | (b << 8) | b;
         }
         for (int i = SIZE; i < this.lut.length; ++i) {
-          this.lut[i] = this.lut[SIZE-1];
+          this.lut[i] = this.lut[SIZE - 1];
         }
         this.dirty = false;
       }
@@ -80,10 +61,15 @@ public class GradientUtils {
 
   public static class ColorStop {
     private float hue;
+
     private float saturation;
+
     private float brightness;
+
     private int r;
+
     private int g;
+
     private int b;
 
     public void set(ColorParameter color) {
@@ -140,14 +126,14 @@ public class GradientUtils {
       return this.brightness == 0;
     }
 
-    @Override
-    public String toString() {
+    @Override public String toString() {
       return String.format("rgb(%d,%d,%d) hsb(%f,%f,%f)", r, g, b, hue, saturation, brightness);
     }
   }
 
   public static class ColorStops {
     public final ColorStop[] stops = new ColorStop[LXSwatch.MAX_COLORS + 1];
+
     public int numStops = 1;
 
     public ColorStops() {
@@ -173,14 +159,14 @@ public class GradientUtils {
     public void setNumStops(int numStops) {
       this.numStops = numStops;
       if (this.numStops > 0) {
-        this.stops[numStops].set(this.stops[numStops-1]);
+        this.stops[numStops].set(this.stops[numStops - 1]);
       }
     }
 
     public int getColor(float lerp, BlendFunction blendFunction) {
       lerp *= (this.numStops - 1);
       int stop = (int) Math.floor(lerp);
-      return blendFunction.blend(this.stops[stop], this.stops[stop+1], lerp - stop);
+      return blendFunction.blend(this.stops[stop], this.stops[stop + 1], lerp - stop);
     }
   }
 
@@ -195,7 +181,6 @@ public class GradientUtils {
       int b = LXUtils.lerpi(c1.b, c2.b, lerp);
       return LXColor.rgba(r, g, b, 255);
     }),
-
     HSV((c1, c2, lerp) -> {
       float hue1 = c1.hue;
       float hue2 = c2.hue;
@@ -206,17 +191,14 @@ public class GradientUtils {
       if (c1.isBlack()) {
         hue1 = hue2;
         sat1 = 0;
-      } else if (c2.isBlack()) {
-        hue2 = hue1;
-        sat2 = 0;
+      } else {
+        if (c2.isBlack()) {
+          hue2 = hue1;
+          sat2 = 0;
+        }
       }
-      return LXColor.hsb(
-        LXUtils.lerpf(hue1, hue2, lerp),
-        LXUtils.lerpf(sat1, sat2, lerp),
-        LXUtils.lerpf(bright1, bright2, lerp)
-      );
+      return LXColor.hsb(LXUtils.lerpf(hue1, hue2, lerp), LXUtils.lerpf(sat1, sat2, lerp), LXUtils.lerpf(bright1, bright2, lerp));
     }),
-
     HSV2((c1, c2, lerp) -> {
       float hue1 = c1.hue;
       float hue2 = c2.hue;
@@ -226,28 +208,29 @@ public class GradientUtils {
       float bright2 = c2.brightness;
       if (hue2 - hue1 > 180) {
         hue1 += 360;
-      } else if (hue1 - hue2 > 180) {
-        hue2 += 360;
+      } else {
+        if (hue1 - hue2 > 180) {
+          hue2 += 360;
+        }
       }
       if (c1.isBlack()) {
         hue1 = hue2;
         sat1 = 0;
-      } else if (c2.isBlack()) {
-        hue2 = hue1;
-        sat2 = 0;
+      } else {
+        if (c2.isBlack()) {
+          hue2 = hue1;
+          sat2 = 0;
+        }
       }
-      return LXColor.hsb(
-        LXUtils.lerpf(hue1, hue2, lerp),
-        LXUtils.lerpf(sat1, sat2, lerp),
-        LXUtils.lerpf(bright1, bright2, lerp)
-      );
-    });
+      return LXColor.hsb(LXUtils.lerpf(hue1, hue2, lerp), LXUtils.lerpf(sat1, sat2, lerp), LXUtils.lerpf(bright1, bright2, lerp));
+    })
+    ;
 
     public final BlendFunction function;
 
     private BlendMode(BlendFunction function) {
       this.function = function;
     }
-  };
+  }
 
 }
