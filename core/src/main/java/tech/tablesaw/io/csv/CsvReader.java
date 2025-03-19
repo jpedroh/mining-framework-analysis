a9928dcba4cf30fb79530bf8bb2fd91dacf02caa
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 12:53:16 +0000

    Mining Framework Analysis

--- core/src/main/java/tech/tablesaw/io/csv/CsvReader.java
+++ core/src/main/java/tech/tablesaw/io/csv/CsvReader.java
@@ -398,7 +398,7 @@ public class CsvReader {
             while ((nextLine = csvParser.parseNext()) != null) {
                 // initialize the arrays to hold the strings. we don't know how many we need until we read the first row
                 if (rowCount == 0) {
-                    for (int i = 0; i < nextLine.length; i++) {
+                    for (String aNextLine : nextLine) {
                         columnData.add(new ArrayList<>());
                     }
                 }
