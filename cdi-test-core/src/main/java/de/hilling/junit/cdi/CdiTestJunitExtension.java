b10bb4a075482176eb35015996f617eecce65a67
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 21:04:31 +0000

    Mining Framework Analysis

--- cdi-test-core/src/main/java/de/hilling/junit/cdi/CdiTestJunitExtension.java
+++ cdi-test-core/src/main/java/de/hilling/junit/cdi/CdiTestJunitExtension.java
@@ -1,8 +1,6 @@
 package de.hilling.junit.cdi;
-
 import jakarta.inject.Inject;
 import jakarta.inject.Qualifier;
-
 import de.hilling.junit.cdi.annotations.ActivatableTestImplementation;
 import de.hilling.junit.cdi.lifecycle.LifecycleNotifier;
 import de.hilling.junit.cdi.scope.TestState;
@@ -10,10 +8,10 @@ import de.hilling.junit.cdi.scope.InvocationTargetManager;
 import de.hilling.junit.cdi.scope.context.TestContext;
 import de.hilling.junit.cdi.util.LoggerConfigurator;
 import de.hilling.junit.cdi.util.ReflectionsUtils;
-
 import org.junit.jupiter.api.extension.*;
 import org.mockito.Mockito;
-
+import javax.enterprise.inject.spi.Bean;
+import javax.enterprise.inject.spi.BeanManager;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.util.Arrays;
@@ -31,93 +29,84 @@ import java.util.logging.Logger;
  * </p>
  */
 public class CdiTestJunitExtension implements TestInstancePostProcessor, BeforeAllCallback, AfterAllCallback, BeforeEachCallback, AfterEachCallback {
+  private static final Logger LOG;
 
-    private static final Logger LOG;
+  static {
+    LoggerConfigurator.configure();
+    LOG = Logger.getLogger(CdiTestJunitExtension.class.getName());
+  }
 
-    static {
-        LoggerConfigurator.configure();
-        LOG = Logger.getLogger(CdiTestJunitExtension.class.getName());
-    }
+  private final InvocationTargetManager invocationTargetManager;
 
-    private final InvocationTargetManager invocationTargetManager;
-    private final ContextControlWrapper   contextControl = ContextControlWrapper.getInstance();
+  private final ContextControlWrapper contextControl = ContextControlWrapper.getInstance();
 
-    private final LifecycleNotifier lifecycleNotifier;
-    private       TestEnvironment   testEnvironment;
+  private final LifecycleNotifier lifecycleNotifier;
 
-    public CdiTestJunitExtension() {
-        invocationTargetManager = contextControl.getContextualReference(InvocationTargetManager.class);
-        lifecycleNotifier = contextControl.getContextualReference(LifecycleNotifier.class);
-    }
+  private TestEnvironment testEnvironment;
 
-    @Override
-    public void beforeAll(ExtensionContext context) {
-        Mockito.framework()
-                .addListener(invocationTargetManager);
-    }
+  public CdiTestJunitExtension() {
+    invocationTargetManager = contextControl.getContextualReference(InvocationTargetManager.class);
+    lifecycleNotifier = contextControl.getContextualReference(LifecycleNotifier.class);
+  }
 
-    @Override
-    public void afterAll(ExtensionContext context) {
-        Mockito.framework()
-                .removeListener(invocationTargetManager);
-    }
+  @Override public void beforeAll(ExtensionContext context) {
+    Mockito.framework().addListener(invocationTargetManager);
+  }
 
-    @Override
-    public void postProcessTestInstance(Object testInstance, ExtensionContext context) {
-        testEnvironment = contextControl.getContextualReference(TestEnvironment.class);
-        testEnvironment.setTestInstance(testInstance);
-        TestContext.activate();
-        for (Field field : ReflectionsUtils.getAllFields(testInstance.getClass())) {
-            if (field.isAnnotationPresent(Inject.class)) {
-                Optional<Annotation> qualifier = Arrays.stream(field.getDeclaredAnnotations()).filter(this::isQualifier).findFirst();
-                if(qualifier.isPresent()) {
-                    setField(testInstance, field, new Annotation[]{qualifier.get()});
-                } else {
-                    setField(testInstance, field, new Annotation[0]);
-                }
-            }
+  @Override public void afterAll(ExtensionContext context) {
+    Mockito.framework().removeListener(invocationTargetManager);
+  }
+
+  @Override public void postProcessTestInstance(Object testInstance, ExtensionContext context) {
+    testEnvironment = contextControl.getContextualReference(TestEnvironment.class);
+    testEnvironment.setTestInstance(testInstance);
+    TestContext.activate();
+    for (Field field : ReflectionsUtils.getAllFields(testInstance.getClass())) {
+      if (field.isAnnotationPresent(Inject.class)) {
+        Optional<Annotation> qualifier = Arrays.stream(field.getDeclaredAnnotations()).filter(this::isQualifier).findFirst();
+        if (qualifier.isPresent()) {
+          setField(testInstance, field, new Annotation[] { qualifier.get() });
+        } else {
+          setField(testInstance, field, new Annotation[0]);
         }
+      }
     }
+  }
 
-    private boolean isQualifier(Annotation annotation) {
-        return annotation.annotationType().isAnnotationPresent(Qualifier.class);
+  private boolean isQualifier(Annotation annotation) {
+    return annotation.annotationType().isAnnotationPresent(Qualifier.class);
+  }
+
+  private void setField(Object testInstance, Field field, Annotation[] annotations) {
+    Object bean = contextControl.getContextualReference(field.getType(), annotations);
+    if (!contextControl.hasNormalScope(field.getType())) {
+      LOG.warning("injecting bean with scope \'dependant\' \'" + bean.getClass().getCanonicalName() + "\' into test class \'" + testInstance.getClass().getCanonicalName() + "\' which might lead to unexpected behaviour");
     }
+    ReflectionsUtils.setField(testInstance, bean, field);
+  }
 
-    private void setField(Object testInstance, Field field, Annotation[] annotations) {
-        Object bean = contextControl.getContextualReference(field.getType(), annotations);
-        if(!contextControl.hasNormalScope(field.getType())) {
-            LOG.warning("injecting bean with scope 'dependant' '" + bean.getClass().getCanonicalName()
-                        + "' into test class '" + testInstance.getClass().getCanonicalName()
-                        + "' which might lead to unexpected behaviour");
-        }
-        ReflectionsUtils.setField(testInstance, bean, field);
+  @Override public void beforeEach(ExtensionContext context) {
+    testEnvironment.setTestMethod(context.getRequiredTestMethod());
+    testEnvironment.setTestName(context.getDisplayName());
+    lifecycleNotifier.notify(TestState.STARTING, context);
+    contextControl.startContexts();
+    lifecycleNotifier.notify(TestState.STARTED, context);
+    for (Field field : ReflectionsUtils.getAllFields(testEnvironment.getTestClass())) {
+      if (isTestActivatable(field)) {
+        invocationTargetManager.activateAlternative(field.getType());
+      }
     }
+  }
 
-    @Override
-    public void beforeEach(ExtensionContext context) {
-        testEnvironment.setTestMethod(context.getRequiredTestMethod());
-        testEnvironment.setTestName(context.getDisplayName());
-        lifecycleNotifier.notify(TestState.STARTING, context);
-        contextControl.startContexts();
-        lifecycleNotifier.notify(TestState.STARTED, context);
-        for (Field field : ReflectionsUtils.getAllFields(testEnvironment.getTestClass())) {
-            if (isTestActivatable(field)) {
-                invocationTargetManager.activateAlternative(field.getType());
-            }
-        }
-    }
+  @Override public void afterEach(ExtensionContext context) {
+    lifecycleNotifier.notify(TestState.FINISHING, context);
+    contextControl.stopContexts();
+    lifecycleNotifier.notify(TestState.FINISHED, context);
+    TestContext.deactivate();
+  }
 
-    @Override
-    public void afterEach(ExtensionContext context) {
-        lifecycleNotifier.notify(TestState.FINISHING, context);
-        contextControl.stopContexts();
-        lifecycleNotifier.notify(TestState.FINISHED, context);
-        TestContext.deactivate();
-    }
-
-    private boolean isTestActivatable(Field field) {
-        Class<?> type = field.getType();
-        return type.isAnnotationPresent(ActivatableTestImplementation.class);
-    }
-
+  private boolean isTestActivatable(Field field) {
+    Class<?> type = field.getType();
+    return type.isAnnotationPresent(ActivatableTestImplementation.class);
+  }
 }
--- integration-tests/test-jee/src/main/java/de/hilling/junit/cdi/jpa/RequestScopedUserService.java
+++ integration-tests/test-jee/src/main/java/de/hilling/junit/cdi/jpa/RequestScopedUserService.java
@@ -1,17 +1,13 @@
 package de.hilling.junit.cdi.jpa;
-
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
 import jakarta.persistence.EntityManager;
 
-@RequestScoped
-public class RequestScopedUserService {
+@RequestScoped public class RequestScopedUserService {
+  @Inject private EntityManager entityManager;
 
-    @Inject
-    private EntityManager entityManager;
-
-    public long addUser(UserEntity userEntity) {
-        entityManager.persist(userEntity);
-        return userEntity.getId();
-    }
+  public long addUser(UserEntity userEntity) {
+    entityManager.persist(userEntity);
+    return userEntity.getId();
+  }
 }
