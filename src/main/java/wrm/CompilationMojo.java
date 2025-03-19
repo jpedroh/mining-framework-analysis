c1d30c7428cc678313df342bf6f71efe6ec5be6d
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 22:55:47 +0000

    Mining Framework Analysis

--- src/main/java/wrm/CompilationMojo.java
+++ src/main/java/wrm/CompilationMojo.java
@@ -1,5 +1,4 @@
 package wrm;
-
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.sonatype.plexus.build.incremental.BuildContext;
@@ -12,43 +11,36 @@ import org.sonatype.plexus.build.incremental.BuildContext;
  * @requiresDependencyResolution compile
  */
 public class CompilationMojo extends AbstractSassMojo {
-
-	/**
+  /**
 	 * @component
 	 */
-	private BuildContext buildContext;
+  private BuildContext buildContext;
 
-	/**
+  /**
 	 * Returns project relative input path for sass files. If input path is absolute, remove base dir from string.
 	 * @return project relative input path. 
 	 */
-	private String getRelativeInputPath() {
-		String relativeInputPath = inputPath;
-		String baseDirPath = project.getBasedir().getPath();
-		if (relativeInputPath.startsWith(baseDirPath)) {
-			relativeInputPath = relativeInputPath.substring(baseDirPath.length() + 1);
-		}
-		return relativeInputPath;
-	}
-	
-	public void execute() throws MojoExecutionException, MojoFailureException {
-		validateConfig();
-		if ((buildContext!=null) && 
-				(!buildContext.isIncremental() || (buildContext.hasDelta(getRelativeInputPath())))) {
+  private String getRelativeInputPath() {
+    String relativeInputPath = inputPath;
+    String baseDirPath = project.getBasedir().getPath();
+    if (relativeInputPath.startsWith(baseDirPath)) {
+      relativeInputPath = relativeInputPath.substring(baseDirPath.length() + 1);
+    }
+    return relativeInputPath;
+  }
 
-			compiler = initCompiler();
-
-			inputPath = inputPath.replaceAll("\\\\", "/");
-
-			getLog().debug("Input Path=" + inputPath);
-			getLog().debug("Output Path=" + outputPath);
-		
-			try {
-				compile();
-			} catch (Exception e) {
-				throw new MojoExecutionException("Failed", e);
-			}
-		}
-	}
-
+  public void execute() throws MojoExecutionException, MojoFailureException {
+    validateConfig();
+    if ((buildContext != null) && (!buildContext.isIncremental() || (buildContext.hasDelta(getRelativeInputPath())))) {
+      compiler = initCompiler();
+      inputPath = inputPath.replaceAll("\\\\", "/");
+      getLog().debug("Input Path=" + inputPath);
+      getLog().debug("Output Path=" + outputPath);
+      try {
+        compile();
+      } catch (Exception e) {
+        throw new MojoExecutionException("Failed", e);
+      }
+    }
+  }
 }
