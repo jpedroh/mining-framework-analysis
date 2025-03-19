4f2a4ca57785f9fdd292df7b9156df544526425c
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 13:38:26 +0000

    Mining Framework Analysis

--- src/test/java/net/openhft/performance/tests/network/SimpleServerAndClientTest.java
+++ src/test/java/net/openhft/performance/tests/network/SimpleServerAndClientTest.java
@@ -1,28 +1,9 @@
-/*
- * Copyright 2016 higherfrequencytrading.com
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package net.openhft.performance.tests.network;
-
 import net.openhft.chronicle.bytes.Bytes;
 import net.openhft.chronicle.core.threads.EventLoop;
 import net.openhft.chronicle.core.threads.HandlerPriority;
 import net.openhft.chronicle.core.threads.ThreadDump;
-import net.openhft.chronicle.network.AcceptorEventHandler;
-import net.openhft.chronicle.network.TCPRegistry;
-import net.openhft.chronicle.network.VanillaNetworkContext;
+import net.openhft.chronicle.network.*;
 import net.openhft.chronicle.network.connection.TcpChannelHub;
 import net.openhft.chronicle.network.connection.TryLock;
 import net.openhft.chronicle.threads.EventGroup;
@@ -34,108 +15,96 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-
 import java.io.IOException;
 import java.nio.channels.SocketChannel;
 import java.util.concurrent.TimeoutException;
-
+import java.util.function.Function;
 import static net.openhft.chronicle.network.connection.SocketAddressSupplier.uri;
+import static net.openhft.performance.tests.network.LegacyHanderFactory.simpleTcpEventHandlerFactory;
 
 /**
  * Created by rob on 26/08/2015.
  */
 public class SimpleServerAndClientTest {
-    private ThreadDump threadDump;
+  private ThreadDump threadDump;
 
-    @Before
-    public void threadDump() {
-        threadDump = new ThreadDump();
-    }
+  @Before public void threadDump() {
+    threadDump = new ThreadDump();
+  }
 
-    @After
-    public void checkThreadDump() {
+  @After public void checkThreadDump() {
+    threadDump.assertNoNewThreads();
+  }
 
-        threadDump.assertNoNewThreads();
-    }
-
-    @Test
-    // @Ignore("Fails on Teamcity ")
-    public void test() throws IOException, TimeoutException, InterruptedException {
-
-
-        for (; ; ) {
-            // this the name of a reference to the host name and port,
-            // allocated automatically when to a free port on localhost
-            final String desc = "host.port";
-            TCPRegistry.createServerSocketChannelFor(desc);
-
-            // we use an event loop rather than lots of threads
-            try (EventLoop eg = new EventGroup(true)) {
-                eg.start();
-
-                // an example message that we are going to send from the server to the client and back
-                final String expectedMessage = "<my message>";
-                createServer(desc, eg);
-
-                try (TcpChannelHub tcpChannelHub = createClient(eg, desc)) {
-
-                    // create the message the client sends to the server
-
-                    // the tid must be unique, its reflected back by the server, it must be at the start
-                    // of each message sent from the server to the client. Its use by the client to identify which
-                    // thread will handle this message
-                    final long tid = tcpChannelHub.nextUniqueTransaction(System.currentTimeMillis());
-
-                    // we will use a text wire backed by a elasticByteBuffer
-                    final Wire wire = new TextWire(Bytes.elasticByteBuffer());
-
-                    wire.writeDocument(true, w -> w.write(() -> "tid").int64(tid));
-                    wire.writeDocument(false, w -> w.write(() -> "payload").text(expectedMessage));
-
-                    // write the data to the socket
-                    tcpChannelHub.lock2(() -> tcpChannelHub.writeSocket(wire, true),
-                            true, TryLock.TRY_LOCK_WARN);
-
-                    // read the reply from the socket ( timeout after 5 second ), note: we have to pass
-                    // the tid
-                    try {
-                        Wire reply = tcpChannelHub.proxyReply(500, tid);
-
-                        // read the reply and check the result
-                        reply.readDocument(null, data -> {
-                            final String text = data.read(() -> "payloadResponse").text();
-                            Assert.assertEquals(expectedMessage, text);
-                        });
-
-                    } catch (TimeoutException e) {
-                        // retry, you will get this is the client attempts to send a message to
-                        // the server and the server is not running or ready
-                        continue;
-                    }
-                    break;
-                }
-
-            } finally {
-                TcpChannelHub.closeAllHubs();
-                TCPRegistry.reset();
-            }
+  @Test public void test() throws IOException, TimeoutException, InterruptedException {
+    for ( ; ; ) {
+      final String desc = "host.port";
+      TCPRegistry.createServerSocketChannelFor(desc);
+      try (EventLoop eg = new EventGroup(true)) {
+        eg.start();
+        final String expectedMessage = "<my message>";
+        createServer(desc, eg);
+        try (TcpChannelHub tcpChannelHub = createClient(eg, desc)) {
+          final long tid = tcpChannelHub.nextUniqueTransaction(System.currentTimeMillis());
+          final Wire wire = new TextWire(Bytes.elasticByteBuffer());
+          wire.writeDocument(true, (w) -> w.write(() -> "tid").int64(tid));
+          wire.writeDocument(false, (w) -> w.write(() -> "payload").text(expectedMessage));
+          tcpChannelHub.lock2(() -> tcpChannelHub.writeSocket(wire, true), true, TryLock.TRY_LOCK_WARN);
+          try {
+            Wire reply = tcpChannelHub.proxyReply(500, tid);
+            reply.readDocument(null, (data) -> {
+              final String text = data.read(() -> "payloadResponse").text();
+              Assert.assertEquals(expectedMessage, text);
+            });
+          } catch (TimeoutException e) {
+            continue;
+          }
+          break;
         }
-
+      } finally {
+        TcpChannelHub.closeAllHubs();
+        TCPRegistry.reset();
+      }
     }
 
-    @NotNull
-    private TcpChannelHub createClient(EventLoop eg, String desc) {
-        return new TcpChannelHub(null, eg, WireType.TEXT, "/", uri(desc), false, null, HandlerPriority.TIMER);
+<<<<<<< /usr/src/app/output/openhft/chronicle-network/0866a74fd59691c127717ca1d8b1864866486a71/src/test/java/net/openhft/performance/tests/network/SimpleServerAndClientTest.java/left.java
+    try (EventLoop eg = new EventGroup(true)) {
+      eg.start();
+      final String expectedMessage = "<my message>";
+      createServer(desc, eg);
+      Thread.sleep(1);
+      try (TcpChannelHub tcpChannelHub = createClient(eg, desc)) {
+        final long tid = tcpChannelHub.nextUniqueTransaction(System.currentTimeMillis());
+        final Wire wire = new TextWire(Bytes.elasticByteBuffer());
+        wire.writeDocument(true, (w) -> w.write(() -> "tid").int64(tid));
+        wire.writeDocument(false, (w) -> w.write(() -> "payload").text(expectedMessage));
+        tcpChannelHub.lock2(() -> tcpChannelHub.writeSocket(wire, true), true, TryLock.TRY_LOCK_WARN);
+        Wire reply = tcpChannelHub.proxyReply(TimeUnit.SECONDS.toMillis(5), tid);
+        reply.readDocument(null, (data) -> {
+          final String text = data.read(() -> "payloadResponse").text();
+          Assert.assertEquals(expectedMessage, text);
+        });
+      }
+    } finally {
+      TcpChannelHub.closeAllHubs();
+      TCPRegistry.reset();
     }
+=======
+>>>>>>> Unknown file: This is a bug in JDime.
+  }
 
-    private void createServer(String desc, EventLoop eg) throws IOException {
-        AcceptorEventHandler eah = new AcceptorEventHandler(desc,
+  @NotNull private TcpChannelHub createClient(EventLoop eg, String desc) {
+    return new TcpChannelHub(null, eg, WireType.TEXT, "/", uri(desc), false, null, HandlerPriority.TIMER);
+  }
 
-                LegacyHanderFactory.simpleTcpEventHandlerFactory(WireEchoRequestHandler::new, WireType.TEXT),
-                VanillaNetworkContext::new);
+  private void createServer(String desc, EventLoop eg) throws IOException {
+    AcceptorEventHandler eah = new AcceptorEventHandler(desc, handlerFactory(), VanillaNetworkContext::new);
+    eg.addHandler(eah);
+    SocketChannel sc = TCPRegistry.createSocketChannel(desc);
+    sc.configureBlocking(false);
+  }
 
-        eg.addHandler(eah);
-        SocketChannel sc = TCPRegistry.createSocketChannel(desc);
-        sc.configureBlocking(false);
-    }
+  @NotNull private Function<NetworkContext, TcpEventHandler> handlerFactory() {
+    return simpleTcpEventHandlerFactory(WireEchoRequestHandler::new, WireType.TEXT);
+  }
 }
