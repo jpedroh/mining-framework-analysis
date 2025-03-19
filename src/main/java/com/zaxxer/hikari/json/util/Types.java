ce3c04e0823f9e823dbd866dc119388e397bf060
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 17:11:35 +0000

    Mining Framework Analysis

--- src/main/java/com/zaxxer/hikari/json/util/Types.java
+++ src/main/java/com/zaxxer/hikari/json/util/Types.java
@@ -6,20 +6,30 @@ public class Types
    // Constants, where for numerics the LSB indicates whether it is an
    // integer or decimal type.
    //
+<<<<<<< /usr/src/app/output/brettwooldridge/hikarijson/f1b45352cf9c8510a94fd97c943d4fe526cc94ad/src/main/java/com/zaxxer/hikari/json/util/Types.java/left.java
+   public static final int ENUM    = 0b1010;
+||||||| /usr/src/app/output/brettwooldridge/hikarijson/f1b45352cf9c8510a94fd97c943d4fe526cc94ad/src/main/java/com/zaxxer/hikari/json/util/Types.java/base.java
+   public static final int ENUM    = 0b1100;
+=======
+   public static final int ENUM    = 0b0110_0;
+>>>>>>> /usr/src/app/output/brettwooldridge/hikarijson/f1b45352cf9c8510a94fd97c943d4fe526cc94ad/src/main/java/com/zaxxer/hikari/json/util/Types.java/right.java
+<<<<<<< /usr/src/app/output/brettwooldridge/hikarijson/f1b45352cf9c8510a94fd97c943d4fe526cc94ad/src/main/java/com/zaxxer/hikari/json/util/Types.java/left.java
+   public static final int DATE    = 0b1100;
+||||||| /usr/src/app/output/brettwooldridge/hikarijson/f1b45352cf9c8510a94fd97c943d4fe526cc94ad/src/main/java/com/zaxxer/hikari/json/util/Types.java/base.java
+=======
+   public static final int DATE    = 0b0101_0;
+>>>>>>> /usr/src/app/output/brettwooldridge/hikarijson/f1b45352cf9c8510a94fd97c943d4fe526cc94ad/src/main/java/com/zaxxer/hikari/json/util/Types.java/right.java
    // ONLY SET THIS BIT FOR INT TYPES ------v
    public static final int BYTE    = 0b0000_1;
    public static final int INT     = 0b0001_1;
    public static final int SHORT   = 0b0010_1;
    public static final int LONG    = 0b0011_1;
    public static final int CHAR    = 0b0100_1;
-
    public static final int FLOAT   = 0b0000_0;
    public static final int DOUBLE  = 0b0001_0;
    public static final int BOOLEAN = 0b0010_0;
    public static final int STRING  = 0b0011_0;
    public static final int OBJECT  = 0b0100_0;
-   public static final int DATE    = 0b0101_0;
-   public static final int ENUM    = 0b0110_0;
 
    public static final int INTEGRAL_TYPE = 0b1;
 }
