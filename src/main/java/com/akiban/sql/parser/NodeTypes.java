18ce1359f39b188b3b9d5c1f26108d6cabdcbc53
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 18:28:28 +0000

    Mining Framework Analysis

--- src/main/java/com/akiban/sql/parser/NodeTypes.java
+++ src/main/java/com/akiban/sql/parser/NodeTypes.java
@@ -218,11 +218,16 @@ public interface NodeTypes
     public static final int LEFT_FN_NODE = 158;
     public static final int RIGHT_FN_NODE = 159;
     public static final int ROWS_RESULT_SET_NODE = 160;
+<<<<<<< /usr/src/app/output/brunoribeiro/sql-parser/307d28df56eaaf3913404fbde2b3f0053acd1c6f/src/main/java/com/akiban/sql/parser/NodeTypes.java/left.java
+    public static final int AT_DROP_INDEX_NODE = 161;
+    public static final int AT_ADD_INDEX_NODE = 162;
+    // 163 - 182 available
+||||||| /usr/src/app/output/brunoribeiro/sql-parser/307d28df56eaaf3913404fbde2b3f0053acd1c6f/src/main/java/com/akiban/sql/parser/NodeTypes.java/base.java
+    // 161 - 182 available
+=======
     public static final int SPECIAL_INDEX_FUNC_NODE = 161;
-    public static final int AT_DROP_INDEX_NODE = 162;
-    public static final int AT_ADD_INDEX_NODE = 163;
-    // 164 - 182 available
-
+    // 162 - 182 available
+>>>>>>> /usr/src/app/output/brunoribeiro/sql-parser/307d28df56eaaf3913404fbde2b3f0053acd1c6f/src/main/java/com/akiban/sql/parser/NodeTypes.java/right.java
     public static final int ALTER_SERVER_NODE = 183;
     public static final int TIMESTAMP_ADD_FN_NODE = 184;
     public static final int TIMESTAMP_DIFF_FN_NODE = 185;
