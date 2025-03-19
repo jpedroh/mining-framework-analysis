4057c8dcf511569814b1f22d7d8f11893a9f6745
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 21:04:28 +0000

    Mining Framework Analysis

--- integration-tests/test-jee/src/main/java/de/hilling/junit/cdi/jpa/RequestScopedUserService.java
+++ integration-tests/test-jee/src/main/java/de/hilling/junit/cdi/jpa/RequestScopedUserService.java
@@ -1,17 +1,1 @@
-package de.hilling.junit.cdi.jpa;
-
-import jakarta.enterprise.context.RequestScoped;
-import jakarta.inject.Inject;
-import jakarta.persistence.EntityManager;
-
-@RequestScoped
-public class RequestScopedUserService {
-
-    @Inject
-    private EntityManager entityManager;
-
-    public long addUser(UserEntity userEntity) {
-        entityManager.persist(userEntity);
-        return userEntity.getId();
-    }
-}
+  package     de . hilling . junit . cdi . jpa ;   import    jakarta . enterprise . context . RequestScoped ;  import   jakarta . inject . Inject ;  import   jakarta . persistence . EntityManager ;    @ RequestScoped public class RequestScopedUserService  {    @ Inject private EntityManager  entityManager ;   public  long addUser  (  UserEntity userEntity )  {   entityManager . persist  ( userEntity ) ;  return  userEntity . getId  ( ) ; } }
