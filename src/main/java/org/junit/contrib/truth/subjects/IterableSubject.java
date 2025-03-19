57055d94e116cdbeaacf9aeb0ce3d121d5aa154e
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 19:27:48 +0000

    Mining Framework Analysis

--- src/main/java/org/junit/contrib/truth/subjects/IterableSubject.java
+++ src/main/java/org/junit/contrib/truth/subjects/IterableSubject.java
@@ -1,41 +1,19 @@
-/*
- * Copyright (C) 2011 Google, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.junit.contrib.truth.subjects;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import org.junit.contrib.truth.FailureStrategy;
 
 /**
  * @author Kevin Bourrillion
  */
-public class IterableSubject<S extends IterableSubject<S, T, C>, T, C extends Iterable<T>> extends Subject<S, C> {
-
-  @SuppressWarnings({ "unchecked", "rawtypes" })
-  public static <T, C extends Iterable<T>> IterableSubject<? extends IterableSubject<?, T, C>, T, C> create(
-      FailureStrategy failureStrategy, Iterable<T> list) {
+public class IterableSubject<S extends IterableSubject<S, T, C>, T extends java.lang.Object, C extends Iterable<T>> extends Subject<S, C> {
+  @SuppressWarnings(value = { "unchecked", "rawtypes" }) public static <T extends java.lang.Object, C extends Iterable<T>> IterableSubject<? extends IterableSubject<?, T, C>, T, C> create(FailureStrategy failureStrategy, Iterable<T> list) {
     return new IterableSubject(failureStrategy, list);
   }
 
-  // TODO: Arguably this should even be package private
   protected IterableSubject(FailureStrategy failureStrategy, C list) {
     super(failureStrategy, list);
   }
@@ -61,7 +39,6 @@ public class IterableSubject<S extends IterableSubject<S, T, C>, T, C extends It
   }
 
   public And<S> hasContentsInOrder(Object... expected) {
-    // TODO(kevinb): prettier error message
     List<Object> target = new ArrayList<Object>();
     for (Object t : getSubject()) {
       target.add(t);
@@ -71,8 +48,7 @@ public class IterableSubject<S extends IterableSubject<S, T, C>, T, C extends It
   }
 
   public And<S> hasContentsAnyOrder(Object... expected) {
-    check().that(createFakeMultiset(getSubject()))
-        .isEqualTo(createFakeMultiset(Arrays.asList(expected)));
+    check().that(createFakeMultiset(getSubject())).isEqualTo(createFakeMultiset(Arrays.asList(expected)));
     return nextChain();
   }
 
