22a56be083a6470290990b948ba083f88426200f
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 19:25:04 +0000

    Mining Framework Analysis

--- src/main/java/org/junit/contrib/truth/subjects/IterableSubject.java
+++ src/main/java/org/junit/contrib/truth/subjects/IterableSubject.java
@@ -1,39 +1,17 @@
-/*
- * Copyright (C) 2011 Google, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.junit.contrib.truth.subjects;
-
 import java.util.Arrays;
 import java.util.Iterator;
-
 import org.junit.contrib.truth.FailureStrategy;
 import org.junit.contrib.truth.util.GwtCompatible;
 
 /**
  * @author Kevin Bourrillion
  */
-@GwtCompatible
-public class IterableSubject<S extends IterableSubject<S, T, C>, T, C extends Iterable<T>> extends Subject<S, C> {
-
-  @SuppressWarnings({ "unchecked", "rawtypes" })
-  public static <T, C extends Iterable<T>> IterableSubject<? extends IterableSubject<?, T, C>, T, C> create(
-      FailureStrategy failureStrategy, Iterable<T> list) {
+@GwtCompatible public class IterableSubject<S extends IterableSubject<S, T, C>, T extends java.lang.Object, C extends Iterable<T>> extends Subject<S, C> {
+  @SuppressWarnings(value = { "unchecked", "rawtypes" }) public static <T extends java.lang.Object, C extends Iterable<T>> IterableSubject<? extends IterableSubject<?, T, C>, T, C> create(FailureStrategy failureStrategy, Iterable<T> list) {
     return new IterableSubject(failureStrategy, list);
   }
 
-  // TODO: Arguably this should even be package private
   protected IterableSubject(FailureStrategy failureStrategy, C list) {
     super(failureStrategy, list);
   }
