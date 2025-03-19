8eda0ee7ba60207c51eeb5586492bc8d8f836a33
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 20:20:30 +0000

    Mining Framework Analysis

--- src/test/java/com/cronutils/parser/CronParserTest.java
+++ src/test/java/com/cronutils/parser/CronParserTest.java
@@ -145,7 +145,6 @@ public class CronParserTest {
         parser = new CronParser(CronDefinitions.quartzWithDayOfYearExtension());
         parser.parse("0 0 0 * * ? 2017 1/14");
     }
-
     //@Test TODO: issue #180
     public void testThatEveryMinuteIsPreserved() {
         CronDefinition quartzDefinition = CronDefinitionBuilder.instanceDefinitionFor(CronType.QUARTZ);
