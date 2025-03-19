f8a92e928a461022fb144cb75441f4728dacbb4e
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 15:32:10 +0000

    Mining Framework Analysis

--- binding/servlet/src/main/java/io/tracee/binding/servlet/TraceeServletRequestListener.java
+++ binding/servlet/src/main/java/io/tracee/binding/servlet/TraceeServletRequestListener.java
@@ -1,5 +1,4 @@
 package io.tracee.binding.servlet;
-
 import io.tracee.Tracee;
 import io.tracee.TraceeBackend;
 import io.tracee.TraceeConstants;
@@ -7,7 +6,6 @@ import io.tracee.Utilities;
 import io.tracee.configuration.TraceeFilterConfiguration;
 import io.tracee.transport.HttpHeaderTransport;
 import io.tracee.transport.HttpRequestParameterTransport;
-
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletRequestEvent;
 import javax.servlet.ServletRequestListener;
@@ -16,7 +14,6 @@ import javax.servlet.http.HttpSession;
 import java.util.Collections;
 import java.util.Enumeration;
 import java.util.Map;
-
 import static io.tracee.configuration.TraceeFilterConfiguration.Channel.IncomingRequest;
 import static io.tracee.configuration.TraceeFilterConfiguration.Channel.IncomingResponse;
 
@@ -24,61 +21,51 @@ import static io.tracee.configuration.TraceeFilterConfiguration.Channel.Incoming
  * Manages the TracEE lifecycle.
  */
 public final class TraceeServletRequestListener implements ServletRequestListener {
+  private static final String HTTP_HEADER_NAME = TraceeConstants.HTTP_HEADER_NAME;
 
-	private static final String HTTP_HEADER_NAME = TraceeConstants.HTTP_HEADER_NAME;
+  private final TraceeBackend backend;
 
-	private final TraceeBackend backend;
+  private final HttpHeaderTransport transportSerialization;
 
-	private final HttpHeaderTransport transportSerialization;
-	private final HttpRequestParameterTransport httpRequestParameterTransport;
+  private final HttpRequestParameterTransport httpRequestParameterTransport;
 
-	protected TraceeServletRequestListener(TraceeBackend backend, HttpHeaderTransport transportSerialization, HttpRequestParameterTransport httpRequestParameterTransport) {
-		this.backend = backend;
-		this.transportSerialization = transportSerialization;
-		this.httpRequestParameterTransport = httpRequestParameterTransport;
-	}
+  protected TraceeServletRequestListener(TraceeBackend backend, HttpHeaderTransport transportSerialization, HttpRequestParameterTransport httpRequestParameterTransport) {
+    this.backend = backend;
+    this.transportSerialization = transportSerialization;
+    this.httpRequestParameterTransport = httpRequestParameterTransport;
+  }
 
-	public TraceeServletRequestListener() {
-		this(Tracee.getBackend(), new HttpHeaderTransport(Tracee.getBackend().getLoggerFactory()), new HttpRequestParameterTransport(Tracee.getBackend().getLoggerFactory()));
-	}
+  public TraceeServletRequestListener() {
+    this(Tracee.getBackend(), new HttpHeaderTransport(Tracee.getBackend().getLoggerFactory()), new HttpRequestParameterTransport(Tracee.getBackend().getLoggerFactory()));
+  }
 
-	@Override
-	public void requestDestroyed(final ServletRequestEvent sre) {
-		backend.clear();
-	}
+  @Override public void requestDestroyed(final ServletRequestEvent sre) {
+    backend.clear();
+  }
 
-	@Override
-	public void requestInitialized(final ServletRequestEvent sre) {
-		final ServletRequest servletRequest = sre.getServletRequest();
-		if (servletRequest instanceof HttpServletRequest) {
-			httpRequestInitialized((HttpServletRequest) servletRequest);
-		}
-	}
+  @Override public void requestInitialized(final ServletRequestEvent sre) {
+    final ServletRequest servletRequest = sre.getServletRequest();
+    if (servletRequest instanceof HttpServletRequest) {
+      httpRequestInitialized((HttpServletRequest) servletRequest);
+    }
+  }
 
-	private void httpRequestInitialized(final HttpServletRequest request) {
-		final TraceeFilterConfiguration configuration = backend.getConfiguration();
-
-		if (configuration.shouldProcessContext(IncomingRequest)) {
-			// process request parameter first
-			final Map<String, String> parameterParsedContext = httpRequestParameterTransport.parse((Map<String, String[]>)request.getParameterMap());
-			backend.putAll(configuration.filterDeniedParams(parameterParsedContext, IncomingResponse));
-
-			// overwrite values defined by request parameter with values from header
-			@SuppressWarnings("unchecked")
-			final Enumeration headers = request.getHeaders(HTTP_HEADER_NAME);
-
-			if (headers != null && headers.hasMoreElements()) {
-				final Map<String, String> contextMap = transportSerialization.parse(Collections.list(headers));
-				backend.putAll(backend.getConfiguration().filterDeniedParams(contextMap, IncomingRequest));
-			}
-		}
-
-		Utilities.generateRequestIdIfNecessary(backend);
-		Utilities.generateConversationIdIfNecessary(backend);
-
-		final HttpSession session = request.getSession(false);
-		if (session != null) {
-			Utilities.generateSessionIdIfNecessary(backend, session.getId());
-		}
-	}
+  private void httpRequestInitialized(final HttpServletRequest request) {
+    final TraceeFilterConfiguration configuration = backend.getConfiguration();
+    if (configuration.shouldProcessContext(IncomingRequest)) {
+      final Map<String, String> parameterParsedContext = httpRequestParameterTransport.parse((Map<String, String[]>) request.getParameterMap());
+      backend.putAll(configuration.filterDeniedParams(parameterParsedContext, IncomingResponse));
+      @SuppressWarnings(value = { "unchecked" }) final Enumeration<String> headers = request.getHeaders(HTTP_HEADER_NAME);
+      if (headers != null && headers.hasMoreElements()) {
+        final Map<String, String> contextMap = transportSerialization.parse(Collections.list(headers));
+        backend.putAll(backend.getConfiguration().filterDeniedParams(contextMap, IncomingRequest));
+      }
+    }
+    Utilities.generateRequestIdIfNecessary(backend);
+    Utilities.generateConversationIdIfNecessary(backend);
+    final HttpSession session = request.getSession(false);
+    if (session != null) {
+      Utilities.generateSessionIdIfNecessary(backend, session.getId());
+    }
+  }
 }
--- binding/springmvc/src/main/java/io/tracee/binding/springmvc/TraceeInterceptor.java
+++ binding/springmvc/src/main/java/io/tracee/binding/springmvc/TraceeInterceptor.java
@@ -1,105 +1,91 @@
 package io.tracee.binding.springmvc;
-
 import io.tracee.Tracee;
 import io.tracee.TraceeBackend;
 import io.tracee.TraceeConstants;
 import io.tracee.Utilities;
 import io.tracee.configuration.TraceeFilterConfiguration;
 import io.tracee.transport.HttpHeaderTransport;
-
 import io.tracee.transport.HttpRequestParameterTransport;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.util.Collections;
 import java.util.Enumeration;
 import java.util.Map;
-
 import static io.tracee.configuration.TraceeFilterConfiguration.Channel.IncomingRequest;
 import static io.tracee.configuration.TraceeFilterConfiguration.Channel.IncomingResponse;
 import static io.tracee.configuration.TraceeFilterConfiguration.Channel.OutgoingResponse;
 
 public final class TraceeInterceptor implements HandlerInterceptor {
+  private final TraceeBackend backend;
 
-	private final TraceeBackend backend;
-	private final HttpHeaderTransport httpHeaderSerialization;
-	private final HttpRequestParameterTransport httpRequestParameterTransport;
-	private String outgoingHeaderName = TraceeConstants.HTTP_HEADER_NAME;
-	private String incomingHeaderName = TraceeConstants.HTTP_HEADER_NAME;
-	private String profileName;
+  private final HttpHeaderTransport httpHeaderSerialization;
 
-	public TraceeInterceptor() {
-		this(Tracee.getBackend());
-	}
+  private final HttpRequestParameterTransport httpRequestParameterTransport;
 
-	protected TraceeInterceptor(TraceeBackend backend) {
-		this.backend = backend;
-		httpHeaderSerialization = new HttpHeaderTransport(backend.getLoggerFactory());
-		httpRequestParameterTransport = new HttpRequestParameterTransport(backend.getLoggerFactory());
-	}
+  private String outgoingHeaderName = TraceeConstants.HTTP_HEADER_NAME;
 
-	@Override
-	public boolean preHandle(final HttpServletRequest request, final HttpServletResponse response, final Object o) throws Exception {
+  private String incomingHeaderName = TraceeConstants.HTTP_HEADER_NAME;
 
-		final TraceeFilterConfiguration configuration = backend.getConfiguration(profileName);
+  private String profileName;
 
-		if (configuration.shouldProcessContext(IncomingRequest)) {
-			// process request parameter first
-			final Map<String, String> parameterParsedContext = httpRequestParameterTransport.parse((Map<String, String[]>)request.getParameterMap());
-			backend.putAll(configuration.filterDeniedParams(parameterParsedContext, IncomingResponse));
+  public TraceeInterceptor() {
+    this(Tracee.getBackend());
+  }
 
-			// overwrite values defined by request parameter with values from header
-			@SuppressWarnings("unchecked")
-			final Enumeration<String> headers = request.getHeaders(incomingHeaderName);
-			if (headers != null && headers.hasMoreElements()) {
-				final Map<String, String> parsedContext = httpHeaderSerialization.parse(Collections.list(headers));
-				backend.putAll(configuration.filterDeniedParams(parsedContext, IncomingResponse));
-			}
-		}
+  protected TraceeInterceptor(TraceeBackend backend) {
+    this.backend = backend;
+    httpHeaderSerialization = new HttpHeaderTransport(backend.getLoggerFactory());
+    httpRequestParameterTransport = new HttpRequestParameterTransport(backend.getLoggerFactory());
+  }
 
-		Utilities.generateRequestIdIfNecessary(backend);
-		Utilities.generateConversationIdIfNecessary(backend);
+  @Override public boolean preHandle(final HttpServletRequest request, final HttpServletResponse response, final Object o) throws Exception {
+    final TraceeFilterConfiguration configuration = backend.getConfiguration(profileName);
+    if (configuration.shouldProcessContext(IncomingRequest)) {
+      final Map<String, String> parameterParsedContext = httpRequestParameterTransport.parse((Map<String, String[]>) request.getParameterMap());
+      backend.putAll(configuration.filterDeniedParams(parameterParsedContext, IncomingResponse));
+      @SuppressWarnings(value = { "unchecked" }) final Enumeration<String> headers = request.getHeaders(incomingHeaderName);
+      if (headers != null && headers.hasMoreElements()) {
+        final Map<String, String> parsedContext = httpHeaderSerialization.parse(Collections.list(headers));
+        backend.putAll(configuration.filterDeniedParams(parsedContext, IncomingResponse));
+      }
+    }
+    Utilities.generateRequestIdIfNecessary(backend);
+    Utilities.generateConversationIdIfNecessary(backend);
+    final HttpSession session = request.getSession(false);
+    if (session != null) {
+      Utilities.generateSessionIdIfNecessary(backend, session.getId());
+    }
+    return true;
+  }
 
-		final HttpSession session = request.getSession(false);
-		if (session != null) {
-			Utilities.generateSessionIdIfNecessary(backend, session.getId());
-		}
+  @Override public void postHandle(HttpServletRequest request, HttpServletResponse response, Object o, ModelAndView modelAndView) throws Exception {
+    final TraceeFilterConfiguration configuration = backend.getConfiguration(profileName);
+    if (!backend.isEmpty() && configuration.shouldProcessContext(OutgoingResponse)) {
+      final Map<String, String> filteredContext = configuration.filterDeniedParams(backend.copyToMap(), OutgoingResponse);
+      response.setHeader(outgoingHeaderName, httpHeaderSerialization.render(filteredContext));
+    }
+    backend.clear();
+  }
 
-		return true;
-	}
+  @Override public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+  }
 
-	@Override
-	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object o, ModelAndView modelAndView) throws Exception {
-		final TraceeFilterConfiguration configuration = backend.getConfiguration(profileName);
+  public void setOutgoingHeaderName(String outgoingHeaderName) {
+    this.outgoingHeaderName = outgoingHeaderName;
+  }
 
-		if (!backend.isEmpty() && configuration.shouldProcessContext(OutgoingResponse)) {
-			final Map<String, String> filteredContext = configuration.filterDeniedParams(backend.copyToMap(), OutgoingResponse);
-			response.setHeader(outgoingHeaderName, httpHeaderSerialization.render(filteredContext));
-		}
-		backend.clear();
-	}
+  public void setIncomingHeaderName(String incomingHeaderName) {
+    this.incomingHeaderName = incomingHeaderName;
+  }
 
-	@Override
-	public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+  public String getProfileName() {
+    return profileName;
+  }
 
-	}
-
-	public void setOutgoingHeaderName(String outgoingHeaderName) {
-		this.outgoingHeaderName = outgoingHeaderName;
-	}
-
-	public void setIncomingHeaderName(String incomingHeaderName) {
-		this.incomingHeaderName = incomingHeaderName;
-	}
-
-	public String getProfileName() {
-		return profileName;
-	}
-
-	public void setProfileName(String profileName) {
-		this.profileName = profileName;
-	}
+  public void setProfileName(String profileName) {
+    this.profileName = profileName;
+  }
 }
