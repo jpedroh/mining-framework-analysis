a70d77ef9cdb81af5da046687c543f44d51c70fe
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 15:07:03 +0000

    Mining Framework Analysis

--- src/main/java/fr/techad/sonar/GerritInitializer.java
+++ src/main/java/fr/techad/sonar/GerritInitializer.java
@@ -1,50 +1,43 @@
 package fr.techad.sonar;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.sonar.api.batch.Initializer;
 import org.sonar.api.resources.Project;
-
 import fr.techad.sonar.GerritConfiguration;
 import fr.techad.sonar.gerrit.GerritFacade;
 
 public class GerritInitializer extends Initializer {
-    private static final Logger LOG = LoggerFactory.getLogger(GerritInitializer.class);
-    private final GerritConfiguration gerritConfiguration;
-    private GerritFacade gerritFacade;
+  private static final Logger LOG = LoggerFactory.getLogger(GerritInitializer.class);
 
-    public GerritInitializer(GerritConfiguration gerritConfiguration, GerritFacade gerritFacade) {
-        LOG.debug("[GERRIT PLUGIN] Instanciating GerritInitializer");
-        this.gerritConfiguration = gerritConfiguration;
-        this.gerritFacade = gerritFacade;
+  private final GerritConfiguration gerritConfiguration;
+
+  private GerritFacade gerritFacade;
+
+  public GerritInitializer(GerritConfiguration gerritConfiguration, GerritFacade gerritFacade) {
+    LOG.debug("[GERRIT PLUGIN] Instanciating GerritInitializer");
+    this.gerritConfiguration = gerritConfiguration;
+    this.gerritFacade = gerritFacade;
+  }
+
+  @Override public void execute(Project project) {
+    if (gerritConfiguration.forceBranch()) {
+      LOG.debug("[GERRIT PLUGIN] Force project branch to {}", gerritConfiguration.getBranchName());
+      project.setBranch(gerritConfiguration.getBranchName());
     }
-
-    @Override
-    public void execute(Project project) {
-        if (gerritConfiguration.forceBranch()) {
-            LOG.debug("[GERRIT PLUGIN] Force project branch to {}", gerritConfiguration.getBranchName());
-            project.setBranch(gerritConfiguration.getBranchName());
-        }
-        
-        try {
-            gerritFacade.listFiles();
-        } catch (GerritPluginException e) {
-            LOG.error("[GERRIT PLUGIN] Error getting Gerrit datas", e);
-        }
+    try {
+      gerritFacade.listFiles();
+    } catch (GerritPluginException e) {
+      LOG.error("[GERRIT PLUGIN] Error getting Gerrit datas", e);
     }
+  }
 
-    @Override
-    public boolean shouldExecuteOnProject(Project project) {
-        boolean willRun = gerritConfiguration.isEnabled();
-        
-        LOG.info("[GERRIT PLUGIN] Initializer : will{}execute plugin on project \'{}\'.", willRun ? " " : " NOT ",
-                project.getName());
-
-        if (!gerritConfiguration.isValid()) {
-            LOG.info("[GERRIT PLUGIN] Initializer : Configuration is not valid, will not execute.");
-            willRun = false;
-        }
-
-        return willRun;
+  @Override public boolean shouldExecuteOnProject(Project project) {
+    boolean willRun = gerritConfiguration.isEnabled();
+    LOG.info("[GERRIT PLUGIN] Initializer : will{}execute plugin on project \'{}\'.", willRun ? " " : " NOT ", project.getName());
+    if (!gerritConfiguration.isValid()) {
+      LOG.info("[GERRIT PLUGIN] Initializer : Configuration is not valid, will not execute.");
+      willRun = false;
     }
+    return willRun;
+  }
 }
