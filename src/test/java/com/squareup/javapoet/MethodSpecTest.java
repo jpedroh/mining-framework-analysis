553ea7a7c6ff8b7190b3ad3c10da5e3891991df5
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 14:15:51 +0000

    Mining Framework Analysis

--- src/test/java/com/squareup/javapoet/MethodSpecTest.java
+++ src/test/java/com/squareup/javapoet/MethodSpecTest.java
@@ -37,8 +37,8 @@ import org.junit.Test;
 
 import static com.google.common.collect.Iterables.getOnlyElement;
 import static com.google.common.truth.Truth.assertThat;
-import static com.squareup.javapoet.MethodSpec.CONSTRUCTOR;
 import static com.squareup.javapoet.TestUtil.findFirst;
+import static com.squareup.javapoet.MethodSpec.CONSTRUCTOR;
 import static javax.lang.model.util.ElementFilter.methodsIn;
 import static org.junit.Assert.fail;
 
@@ -179,9 +179,7 @@ public final class MethodSpecTest {
     TypeElement classElement = getElement(ExtendsIterableWithDefaultMethods.class);
     DeclaredType classType = (DeclaredType) classElement.asType();
     List<ExecutableElement> methods = methodsIn(elements.getAllMembers(classElement));
-    ExecutableElement exec = 
-      
-      (methods, "spliterator");
+    ExecutableElement exec = findFirst(methods, "spliterator");
     MethodSpec method = MethodSpec.overriding(exec, classType, types).build();
     assertThat(method.toString()).isEqualTo(""
         + "@java.lang.Override\n"
