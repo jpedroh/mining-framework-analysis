4d4b3982825cb0e1a9f114b018dcafaef1886e7e
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 16:15:12 +0000

    Mining Framework Analysis

--- modules/swagger-parser/src/main/java/io/swagger/parser/util/SwaggerDeserializer.java
+++ modules/swagger-parser/src/main/java/io/swagger/parser/util/SwaggerDeserializer.java
@@ -420,7 +420,7 @@ public class SwaggerDeserializer {
         List<SecurityRequirement> security = securityRequirements(array, location, result);
         if (security != null) {
             List<Map<String, List<String>>> ss = new ArrayList<>();
-            for(SecurityRequirement s : security) {
+            for (SecurityRequirement s : security) {
                 if(s.getRequirements() != null) {
                     if (s.getRequirements().size() > 0) {
                         ss.add(s.getRequirements());
