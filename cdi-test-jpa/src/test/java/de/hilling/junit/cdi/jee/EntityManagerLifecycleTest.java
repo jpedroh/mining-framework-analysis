8a823bde02bc6c3dcd1ef58f12d6b37b11588aae
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 21:08:23 +0000

    Mining Framework Analysis

--- cdi-test-jpa/src/test/java/de/hilling/junit/cdi/jee/EntityManagerLifecycleTest.java
+++ cdi-test-jpa/src/test/java/de/hilling/junit/cdi/jee/EntityManagerLifecycleTest.java
@@ -1,71 +1,57 @@
 package de.hilling.junit.cdi.jee;
-
 import jakarta.inject.Inject;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.PersistenceUnit;
 import jakarta.transaction.Transactional;
-
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
+import static org.junit.jupiter.api.Assertions.*;
 import org.jboss.weld.transaction.spi.TransactionServices;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-
 import de.hilling.junit.cdi.CdiTestJunitExtension;
 
-@ExtendWith(CdiTestJunitExtension.class)
-@DisplayName("EntityManager Lifecycle")
-@Transactional(Transactional.TxType.REQUIRES_NEW)
-class EntityManagerLifecycleTest {
+@ExtendWith(value = CdiTestJunitExtension.class) @DisplayName(value = "EntityManager Lifecycle") @Transactional(value = Transactional.TxType.REQUIRES_NEW) class EntityManagerLifecycleTest {
+  @Inject private TransactionServices transactionServices;
 
-    @Inject
-    private TransactionServices transactionServices;
+  @PersistenceContext(unitName = "cdi-test") private EntityManager entityManagerJta;
 
-    @PersistenceContext(unitName = "cdi-test")
-    private EntityManager entityManagerJta;
+  @PersistenceUnit(unitName = "cdi-test") private EntityManagerFactory entityManagerFactoryJta;
 
-    @PersistenceUnit(unitName = "cdi-test")
-    private EntityManagerFactory entityManagerFactoryJta;
+  @PersistenceContext(unitName = "cdi-test-local") private EntityManager entityManagerLocal;
 
-    @PersistenceContext(unitName = "cdi-test-local")
-    private EntityManager entityManagerLocal;
+  @PersistenceUnit(unitName = "cdi-test-local") private EntityManagerFactory entityManagerFactoryLocal;
 
-    @PersistenceUnit(unitName = "cdi-test-local")
-    private EntityManagerFactory entityManagerFactoryLocal;
+  @Test void jtaInjected() {
+    assertNotNull(entityManagerJta);
+  }
 
-    @Test
-    void jtaInjected() {
-        assertNotNull(entityManagerJta);
-    }
-    @Test
-    void localInjected() {
-        assertNotNull(entityManagerLocal);
-    }
+  @Test void localInjected() {
+    assertNotNull(entityManagerLocal);
+  }
 
-    @Test
-    void localFactoryInjected() {
-        assertNotNull(entityManagerFactoryLocal);
-    }
-    @Test
-    void jtaFactoryInjected() {
-        assertNotNull(entityManagerFactoryJta);
-    }
+  @Test void localFactoryInjected() {
+    assertNotNull(entityManagerFactoryLocal);
+  }
 
-    @Test
-    void localTransactionNotActive() {
-        assertFalse(entityManagerLocal.getTransaction().isActive());
-    }
+  @Test void jtaFactoryInjected() {
+    assertNotNull(entityManagerFactoryJta);
+  }
 
-    @Test
-    @Transactional(Transactional.TxType.REQUIRES_NEW)
-    void globalTransactionActive() {
-        assertFalse(transactionServices.isTransactionActive());
-        entityManagerJta.persist(new UserEntity());
-        assertFalse(transactionServices.isTransactionActive());
-    }
+  @Test void localTransactionNotActive() {
+    assertFalse(entityManagerLocal.getTransaction().isActive());
+  }
 
+  @Test @Transactional(value = Transactional.TxType.REQUIRES_NEW) void globalTransactionActive() {
+    assertFalse(transactionServices.isTransactionActive());
+
+<<<<<<< /usr/src/app/output/guhilling/cdi-test/f3f8094c67a74f9c6cb44fa4ec0a8a365a79f40c/cdi-test-jpa/src/test/java/de/hilling/junit/cdi/jee/EntityManagerLifecycleTest.java/left.java
+    entityManagerJta.persist(new UserEntity());
+=======
+    assertTrue(transactionServices.isTransactionActive());
+>>>>>>> /usr/src/app/output/guhilling/cdi-test/f3f8094c67a74f9c6cb44fa4ec0a8a365a79f40c/cdi-test-jpa/src/test/java/de/hilling/junit/cdi/jee/EntityManagerLifecycleTest.java/right.java
+
+    assertFalse(transactionServices.isTransactionActive());
+  }
 }
