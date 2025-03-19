c0661ca4d8d42d9c43dac029acc95c9abcd8fdbc
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 13:05:00 +0000

    Mining Framework Analysis

--- src/main/java/org/mdkt/compiler/InMemoryJavaCompiler.java
+++ src/main/java/org/mdkt/compiler/InMemoryJavaCompiler.java
@@ -1,92 +1,16 @@
-package org.mdkt.compiler;
-
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-
-import javax.tools.JavaCompiler;
-import javax.tools.ToolProvider;
-
-/**
- * Complile Java sources in-memory
- */
-public class InMemoryJavaCompiler {
-	private JavaCompiler javac;
-	private DynamicClassLoader classLoader;
-
-	private Map<String, SourceCode> sourceCodes = new HashMap<String, SourceCode>();
-
-	public static InMemoryJavaCompiler newInstance() {
-		return new InMemoryJavaCompiler();
-	}
-
-	private InMemoryJavaCompiler() {
-		this.javac = ToolProvider.getSystemJavaCompiler();
-		this.classLoader = new DynamicClassLoader(ClassLoader.getSystemClassLoader());
-	}
-
-	public InMemoryJavaCompiler useParentClassLoader(ClassLoader parent) {
-		this.classLoader = new DynamicClassLoader(parent);
-		return this;
-	}
-
-	/**
-	 * Compile all sources
-	 *
-	 * @return
-	 * @throws Exception
-	 */
-	public Map<String, Class<?>> compileAll() throws Exception {
-		if (sourceCodes.size() == 0) {
-			throw new Exception("No source code to compile");
-		}
-		Collection<SourceCode> compilationUnits = sourceCodes.values();
-		CompiledCode[] code;
-
-		code = new CompiledCode[compilationUnits.size()];
-		Iterator<SourceCode> iter = compilationUnits.iterator();
-		for (int i = 0; i < code.length; i++) {
-			code[i] = new CompiledCode(iter.next().getClassName());
-		}
-
-		ExtendedStandardJavaFileManager fileManager = new ExtendedStandardJavaFileManager(javac.getStandardFileManager(null, null, null), classLoader);
-		JavaCompiler.CompilationTask task = javac.getTask(null, fileManager, null, null, null, compilationUnits);
-		boolean result = task.call();
-		if (!result) {
-			throw new RuntimeException("Unknown error during compilation.");
-		}
-
-		Map<String, Class<?>> classes = new HashMap<String, Class<?>>();
-		for (String className : sourceCodes.keySet()) {
-			classes.put(className, classLoader.loadClass(className));
-		}
-		return classes;
-	}
-
-	/**
-	 * Compile single source
-	 *
-	 * @param className
-	 * @param sourceCode
-	 * @return
-	 * @throws Exception
-	 */
-	public Class<?> compile(String className, String sourceCode) throws Exception {
-		return addSource(className, sourceCode).compileAll().get(className);
-	}
-
-	/**
-	 * Add source code to the compiler
-	 *
-	 * @param className
-	 * @param sourceCode
-	 * @return
-	 * @throws Exception
-	 * @see {@link #compileAll()}
-	 */
-	public InMemoryJavaCompiler addSource(String className, String sourceCode) throws Exception {
-		sourceCodes.put(className, new SourceCode(className, sourceCode));
-		return this;
-	}
-}
+  package   org . mdkt . compiler ;   import   java . util . Collection ;  import   java . util . HashMap ;  import   java . util . Iterator ;  import   java . util . Map ;  import  javax . tools .  * ;  import   java . io . IOException ;  import   java . util . Collections ;  import   java . util . List ;   public class InMemoryJavaCompiler  {   private JavaCompiler  javac =  ToolProvider . getSystemJavaCompiler  ( ) ;   private DynamicClassLoader  classLoader ;   private  Map  < String , SourceCode >  sourceCodes =  new  HashMap  < String , SourceCode >  ( ) ;   public static InMemoryJavaCompiler newInstance  ( )  {  return  new InMemoryJavaCompiler  ( ) ; }   private InMemoryJavaCompiler  ( )  {    this . javac =  ToolProvider . getSystemJavaCompiler  ( ) ;    this . classLoader =  new DynamicClassLoader  (  ClassLoader . getSystemClassLoader  ( ) ) ; }   public InMemoryJavaCompiler useParentClassLoader  (  ClassLoader parent )  {    this . classLoader =  new DynamicClassLoader  ( parent ) ;  return this ; }   public  Map  < String ,  Class  <  ? > > compileAll  ( )  throws Exception  {  if  (   sourceCodes . size  ( ) == 0 )  {  throw  new Exception  ( "No source code to compile" ) ; }   Collection  < SourceCode >  compilationUnits =  sourceCodes . values  ( ) ;   CompiledCode  [ ]  code ;   code =  new CompiledCode  [  compilationUnits . size  ( ) ] ;   Iterator  < SourceCode >  iter =  compilationUnits . iterator  ( ) ;  for (   int  i = 0 ;  i <  code . length ;  i ++ )  {    code [ i ] =  new CompiledCode  (   iter . next  ( ) . getClassName  ( ) ) ; }  ExtendedStandardJavaFileManager  fileManager =  new ExtendedStandardJavaFileManager  (  javac . getStandardFileManager  ( null , null , null ) , classLoader ) ;   JavaCompiler . CompilationTask  task =  javac . getTask  ( null , fileManager , null , null , null , compilationUnits ) ;  boolean  result =  task . call  ( ) ;  if  (  ! result )  {  throw  new RuntimeException  ( "Unknown error during compilation." ) ; }   Map  < String ,  Class  <  ? > >  classes =  new  HashMap  < String ,  Class  <  ? > >  ( ) ;  for ( String className :  sourceCodes . keySet  ( ) )  {   classes . put  ( className ,  classLoader . loadClass  ( className ) ) ; }  return classes ; }   public  Class  <  ? > compile  (  String className ,  String sourceCode )  throws Exception  {   final  DiagnosticCollector  < JavaFileObject >  diagnosticsCollector =  new  DiagnosticCollector  < >  ( ) ; 
+<<<<<<<
+=======
+  Iterable  <  ? extends JavaFileObject >  compilationUnits =  Collections . singletonList  ( sourceCode ) ;
+>>>>>>>
+ 
+<<<<<<<
+=======
+ ExtendedStandardJavaFileManager  fileManager =  new ExtendedStandardJavaFileManager  (  javac . getStandardFileManager  ( diagnosticsCollector , null , null ) , compiledCode , cl ) ;
+>>>>>>>
+ 
+<<<<<<<
+=======
+  JavaCompiler . CompilationTask  task =  javac . getTask  ( null , fileManager , diagnosticsCollector , null , null , compilationUnits ) ;
+>>>>>>>
+   fileManager . close  ( ) ;  if  (  ! result )  {  StringBuilder  sb =  new StringBuilder  ( ) ;   List  <  Diagnostic  <  ? extends JavaFileObject > >  diagnostics =  diagnosticsCollector . getDiagnostics  ( ) ;  for (  Diagnostic  <  ? extends JavaFileObject > diagnostic : diagnostics )  {     sb . append  ( "=> " ) . append  (  diagnostic . getMessage  ( null ) ) . append  ( "\n" ) ; }  throw  new CompileException  (  sb . toString  ( ) ) ; }  return    addSource  ( className , sourceCode ) . compileAll  ( ) . get  ( className ) ; }   public InMemoryJavaCompiler addSource  (  String className ,  String sourceCode )  throws Exception  {   sourceCodes . put  ( className ,  new SourceCode  ( className , sourceCode ) ) ;  return this ; } }
