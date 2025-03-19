26d2658ee1fce8b522ec851505e4566d0b171ada
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 22:39:49 +0000

    Mining Framework Analysis

--- src/main/java/com/microsoft/hadoop/azure/AzureTablePartitioner.java
+++ src/main/java/com/microsoft/hadoop/azure/AzureTablePartitioner.java
@@ -1,10 +1,1 @@
-package com.microsoft.hadoop.azure;
-
-import java.util.*;
-
-import com.microsoft.windowsazure.storage.StorageException;
-import com.microsoft.windowsazure.storage.table.*;
-
-public interface AzureTablePartitioner {
-	public List<AzureTableInputSplit> getSplits(CloudTable table) throws StorageException;
-}
+  package    com . microsoft . hadoop . azure ;   import  java . util .  * ;  import     com . microsoft . windowsazure . storage . table .  * ;  import     com . microsoft . windowsazure . storage . StorageException ;   public interface AzureTablePartitioner  {   public  List  < AzureTableInputSplit > getSplits  (  CloudTable table )  throws StorageException ; }
