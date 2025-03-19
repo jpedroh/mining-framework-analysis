bdccf84c172a4b0d127192d4920a30c31ee7a622
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 17:42:15 +0000

    Mining Framework Analysis

--- src/main/java/com/tufsd/drugwars/App.java
+++ src/main/java/com/tufsd/drugwars/App.java
@@ -1,10 +1,1 @@
-package src.main.java.com.tufsd.drugwars;
-import java.util.Scanner;
-
-public class App 
-{
-    public static void main( String[] args )
-    {
-        UI.splashscreen();
-    }
-}
+  package      src . main . java . com . tufsd . drugwars ;   import   java . util . Scanner ;   public class App  {   public static void main  (   String  [ ] args )  {   UI . splashscreen  ( ) ; } }
