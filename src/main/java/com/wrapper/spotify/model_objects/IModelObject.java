1f5b27088fdcaf2f192b1450d0640a7395c7950c
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 21:33:30 +0000

    Mining Framework Analysis

--- src/main/java/com/wrapper/spotify/model_objects/IModelObject.java
+++ src/main/java/com/wrapper/spotify/model_objects/IModelObject.java
@@ -1,35 +1,25 @@
 package com.wrapper.spotify.model_objects;
-
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.wrapper.spotify.model_objects.specification.Paging;
 import com.wrapper.spotify.model_objects.specification.PagingCursorbased;
-
 import java.text.SimpleDateFormat;
 
 /**
  * Interface with methods used in model objects.
  */
 public interface IModelObject {
+  SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd\'T\'hh:mm:ss");
 
   /**
-   * The date format used by the Spotify Web API. It uses the following pattern: {@code yyyy-MM-dd'T'hh:mm:ss}
-   */
-  SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss");
-
-  /**
-   * Create a builder for building an instance of a model object. <br>
+   * Create a builder for building an instance of a model object.<br>
    * The type of the builder and its methods depend on its corresponding implementation.
    *
    * @return A builder object.
    */
   Builder builder();
 
-  /**
-   * Interface with methods used in builder classes of model objects.
-   */
   interface Builder {
-
     /**
      * Build a model object with the information set in the builder object. <br>
      * The type of the model object and its methods depend on its corresponding implementation.
@@ -39,13 +29,7 @@ public interface IModelObject {
     IModelObject build();
   }
 
-  /**
-   * Interface with methods used in JsonUtil classes of model objects.
-   *
-   * @param <T> Type of the corresponding model object.
-   */
-  interface IJsonUtil<T> {
-
+  interface IJsonUtil<T extends java.lang.Object> {
     boolean hasAndNotNull(final JsonObject jsonObject, final String memberName);
 
     /**
@@ -65,7 +49,7 @@ public interface IModelObject {
 
     T[] createModelObjectArray(final String json, final String key);
 
-    <X> X[] createModelObjectArray(final JsonArray jsonArray, final Class<X> clazz);
+    <X extends java.lang.Object> X[] createModelObjectArray(final JsonArray jsonArray, final Class<X> clazz);
 
     Paging<T> createModelObjectPaging(final JsonObject jsonObject);
 
