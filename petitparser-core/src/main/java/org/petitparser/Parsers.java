663dbc8729a15a67bdd44db31014273e37351ec6
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 15:30:33 +0000

    Mining Framework Analysis

--- petitparser-core/src/main/java/org/petitparser/Parsers.java
+++ petitparser-core/src/main/java/org/petitparser/Parsers.java
@@ -1,5 +1,4 @@
 package org.petitparser;
-
 import org.petitparser.parser.Parser;
 import org.petitparser.parser.primitive.EpsilonParser;
 import org.petitparser.parser.primitive.FailureParser;
@@ -8,56 +7,49 @@ import org.petitparser.parser.primitive.StringParser;
 /**
  * Deprecated factory for generic parsers.
  */
-@Deprecated
-public class Parsers {
-
-  private Parsers() { }
+@Deprecated public class Parsers {
+  private Parsers() {
+  }
 
   /**
    * Replace this deprecated call with {@link EpsilonParser}.
    */
-  @Deprecated
-  public static Parser epsilon() {
+  @Deprecated public static Parser epsilon() {
     return new EpsilonParser();
   }
 
   /**
    * Replace this deprecated call with {@link FailureParser#withMessage(String)}.
    */
-  @Deprecated
-  public static Parser failure(String message) {
+  @Deprecated public static Parser failure(String message) {
     return FailureParser.withMessage(message);
   }
 
   /**
    * Replace this deprecated call with {@link StringParser#of(String)}.
    */
-  @Deprecated
-  public static Parser string(String string) {
+  @Deprecated public static Parser string(String string) {
     return StringParser.of(string);
   }
 
   /**
    * Replace this deprecated call with {@link StringParser#of(String, String)}.
    */
-  @Deprecated
-  public static Parser string(String string, String message) {
+  @Deprecated public static Parser string(String string, String message) {
     return StringParser.of(string, message);
   }
 
   /**
    * Replace this deprecated call with {@link StringParser#ofIgnoringCase(String)}.
    */
-  @Deprecated
-  public static Parser stringIgnoreCase(String string) {
+  @Deprecated public static Parser stringIgnoreCase(String string) {
     return StringParser.ofIgnoringCase(string);
   }
 
   /**
    * Replace this deprecated call with {@link StringParser#ofIgnoringCase(String, String)}.
    */
-  @Deprecated
-  public static Parser stringIgnoreCase(String string, String message) {
+  @Deprecated public static Parser stringIgnoreCase(String string, String message) {
     return StringParser.ofIgnoringCase(string, message);
   }
 }
