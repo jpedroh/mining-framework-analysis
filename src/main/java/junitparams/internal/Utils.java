8bb3d7d5954f904fec09c1c673bc6113db07698b
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 18:44:11 +0000

    Mining Framework Analysis

--- src/main/java/junitparams/internal/Utils.java
+++ src/main/java/junitparams/internal/Utils.java
@@ -1,5 +1,4 @@
 package junitparams.internal;
-
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 
@@ -9,122 +8,119 @@ import java.util.ArrayList;
  * @author Pawel Lipinski
  */
 public class Utils {
-    public static final String REGEX_ALL_NEWLINES = "(\\r\\n|\\n|\\r)";
+  public static final String REGEX_ALL_NEWLINES = "(\\r\\n|\\n|\\r)";
 
-    public static String stringify(Object paramSet, int paramIdx) {
-        String result = "[" + paramIdx + "] ";
+  public static String stringify(Object paramSet, int paramIdx) {
+    String result = "[" + paramIdx + "] ";
+    return result + stringify(paramSet);
+  }
 
-        return result + stringify(paramSet);
+  public static String stringify(Object paramSet) {
+    String result;
+    if (paramSet == null) {
+      result = "null";
+    } else {
+      if (paramSet instanceof String) {
+        result = paramSet.toString();
+      } else {
+
+<<<<<<< /usr/src/app/output/pragmatists/junitparams/8f6c13ae04b9f525f36e55c1dd881be14a246b59/src/main/java/junitparams/internal/Utils.java/left.java
+        result += asCsvString(safelyCastParamsToArray(paramSet))
+=======
+        result = asCsvString(safelyCastParamsToArray(paramSet))
+>>>>>>> /usr/src/app/output/pragmatists/junitparams/8f6c13ae04b9f525f36e55c1dd881be14a246b59/src/main/java/junitparams/internal/Utils.java/right.java
+        ;
+      }
     }
+    return trimSpecialChars(result);
+  }
 
-    public static String stringify(Object paramSet) {
-        String result;
-        if (paramSet == null)
-            result = "null";
-        else if (paramSet instanceof String)
-            result = paramSet.toString();
-        else
-            result = asCsvString(safelyCastParamsToArray(paramSet));
-
-        return trimSpecialChars(result);
+  public static String getParameterStringByIndexOrEmpty(Object paramSet, int parameterIndex) {
+    Object[] params = safelyCastParamsToArray(paramSet);
+    if (paramSet instanceof String) {
+      params = splitAtCommaOrPipe((String) paramSet);
     }
-
-    public static String getParameterStringByIndexOrEmpty(Object paramSet, int parameterIndex) {
-        Object[] params = safelyCastParamsToArray(paramSet);
-        if (paramSet instanceof String) {
-            params = splitAtCommaOrPipe((String) paramSet);
-        }
-        if (parameterIndex >= 0 && parameterIndex < params.length) {
-            return addParamToResult("", params[parameterIndex]);
-        }
-
-        return "";
+    if (parameterIndex >= 0 && parameterIndex < params.length) {
+      return addParamToResult("", params[parameterIndex]);
     }
+    return "";
+  }
 
-    public static String[] splitAtCommaOrPipe(String input) {
-        ArrayList<String> result = new ArrayList<String>();
-
-        char character = '\0';
-        char previousCharacter;
-
-        StringBuilder value = new StringBuilder();
-        for (int i = 0; i < input.length(); i++) {
-            previousCharacter = character;
-            character = input.charAt(i);
-
-            if (character == ',' || character == '|') {
-                if (previousCharacter == '\\') {
-                    value.setCharAt(value.length() - 1, character);
-                    continue;
-                }
-                result.add(value.toString().trim());
-                value = new StringBuilder();
-                continue;
-            }
-
-            value.append(character);
+  public static String[] splitAtCommaOrPipe(String input) {
+    ArrayList<String> result = new ArrayList<String>();
+    char character = '\u0000';
+    char previousCharacter;
+    StringBuilder value = new StringBuilder();
+    for (int i = 0; i < input.length(); i++) {
+      previousCharacter = character;
+      character = input.charAt(i);
+      if (character == ',' || character == '|') {
+        if (previousCharacter == '\\') {
+          value.setCharAt(value.length() - 1, character);
+          continue;
         }
         result.add(value.toString().trim());
-
-        return result.toArray(new String[]{});
+        value = new StringBuilder();
+        continue;
+      }
+      value.append(character);
     }
+    result.add(value.toString().trim());
+    return result.toArray(new String[] {  });
+  }
 
-    private static String trimSpecialChars(String result) {
-        return result.replace('(', '[').replace(')', ']').replaceAll(REGEX_ALL_NEWLINES, " ");
+  private static String trimSpecialChars(String result) {
+    return result.replace('(', '[').replace(')', ']').replaceAll(REGEX_ALL_NEWLINES, " ");
+  }
+
+  static Object[] safelyCastParamsToArray(Object paramSet) {
+    final Object[] params;
+    if (paramSet instanceof Object[]) {
+      params = (Object[]) paramSet;
+    } else {
+      params = new Object[] { paramSet };
     }
+    return params;
+  }
 
-    static Object[] safelyCastParamsToArray(Object paramSet) {
-        final Object[] params;
-        if (paramSet instanceof Object[]) {
-            params = (Object[]) paramSet;
-        } else {
-            params = new Object[]{paramSet};
-        }
-        return params;
+  private static String asCsvString(Object[] params) {
+    if (params == null) {
+      return "null";
     }
-
-    private static String asCsvString(Object[] params) {
-        if (params == null)
-            return "null";
-
-        if (params.length == 0)
-            return "";
-
-        String result = "";
-
-        for (int i = 0; i < params.length - 1; i++) {
-            Object param = params[i];
-            result = addParamToResult(result, param) + ", ";
-        }
-        result = addParamToResult(result, params[params.length - 1]);
-
-        return result;
+    if (params.length == 0) {
+      return "";
     }
-
-    private static String addParamToResult(String result, Object param) {
-        if (param == null)
-            result += "null";
-        else {
-            try {
-                tryFindingOverridenToString(param);
-                result += param.toString();
-            } catch (Exception e) {
-                result += param.getClass().getSimpleName();
-            }
-        }
-        return result;
+    String result = "";
+    for (int i = 0; i < params.length - 1; i++) {
+      Object param = params[i];
+      result = addParamToResult(result, param) + ", ";
     }
+    result = addParamToResult(result, params[params.length - 1]);
+    return result;
+  }
 
-    private static void tryFindingOverridenToString(Object param)
-            throws NoSuchMethodException {
-        final Method toString = param.getClass().getMethod("toString");
-
-        if (toString.getDeclaringClass().equals(Object.class)) {
-            throw new NoSuchMethodException();
-        }
+  private static String addParamToResult(String result, Object param) {
+    if (param == null) {
+      result += "null";
+    } else {
+      try {
+        tryFindingOverridenToString(param);
+        result += param.toString();
+      } catch (Exception e) {
+        result += param.getClass().getSimpleName();
+      }
     }
+    return result;
+  }
 
-    static String uniqueMethodId(int index, Object paramSet, String methodName) {
-        return stringify(paramSet, index) + " (" + methodName + ")";
+  private static void tryFindingOverridenToString(Object param) throws NoSuchMethodException {
+    final Method toString = param.getClass().getMethod("toString");
+    if (toString.getDeclaringClass().equals(Object.class)) {
+      throw new NoSuchMethodException();
     }
+  }
+
+  static String uniqueMethodId(int index, Object paramSet, String methodName) {
+    return stringify(paramSet, index) + " (" + methodName + ")";
+  }
 }
