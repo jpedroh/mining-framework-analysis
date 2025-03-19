b6d330e847386d3db5088403eac749dc2f39d175
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 18:39:18 +0000

    Mining Framework Analysis

--- src/main/java/org/apache/commons/rdf/Quad.java
+++ src/main/java/org/apache/commons/rdf/Quad.java
@@ -1,69 +1,24 @@
 package org.apache.commons.rdf;
-
 import java.util.Optional;
 
-/**
- * A Quad is an abstraction to specify that a <a href=
- * "http://www.w3.org/TR/rdf11-concepts/#dfn-rdf-triple" >RDF-1.1 Triple</a> is
- * in an <a href="http://www.w3.org/TR/rdf11-concepts/#dfn-rdf-graph" >RDF-1.1
- * Graph</a>.
- */
 public interface Quad {
+  Resource getSubject();
 
-    /**
-     * The subject of this quad, which may be either a {@link BlankNode} or an
-     * {@link IRI}, which are represented in Commons RDF by the interface
-     * {@link BlankNodeOrIRI}.
-     *
-     * @return The subject {@link BlankNodeOrIRI} of this quad.
-     * @see <a href="http://www.w3.org/TR/rdf11-concepts/#dfn-subject">RDF-1.1
-     * Triple subject</a>
+  /**
+	 * The predicate {@link IRI} of this quad.
+	 * 
+	 * @return The predicate {@link IRI} of this quad.
+	 * @see <a href="http://www.w3.org/TR/rdf11-concepts/#dfn-predicate">RDF-1.1
+	 *      Triple predicate</a>
+	 */
+  IRI getPredicate();
+
+  RDFTerm getObject();
+
+  Optional<BlankNodeOrIRI> getGraph();
+
+  /**
+     * Convert to a Triple
      */
-    Resource getSubject();
-
-    /**
-     * The predicate {@link IRI} of this quad.
-     *
-     * @return The predicate {@link IRI} of this quad.
-     * @see <a href="http://www.w3.org/TR/rdf11-concepts/#dfn-predicate">RDF-1.1
-     * Triple predicate</a>
-     */
-    IRI getPredicate();
-
-    /**
-     * The object of this quad, which may be either a {@link BlankNode}, an
-     * {@link IRI}, or a {@link Literal}, which are represented in Commons RDF
-     * by the interface {@link RDFTerm}.
-     *
-     * @return The object {@link RDFTerm} of this quad.
-     * @see <a href="http://www.w3.org/TR/rdf11-concepts/#dfn-object">RDF-1.1
-     * Triple object</a>
-     */
-    RDFTerm getObject();
-
-    /**
-     * The optional named graph of this quad, which may be either a
-     * {@link BlankNode} or an {@link IRI}, which are represented in Commons RDF
-     * by the interface {@link BlankNodeOrIRI}.<br>
-     * If the {@link Optional#isPresent()} method on the result returns
-     * <code>false</code>, this Quad is defined to be in the <a
-     * href="dfn-default-graph">default graph</a>
-     *
-     * @return The optional {@link BlankNodeOrIRI} representing the Named Graph
-     * of this quad if present, and the Default Graph if not present.
-     * @see <a
-     * href="http://www.w3.org/TR/rdf11-concepts/#dfn-named-graph">RDF-1.1
-     * Named Graph</a>
-     */
-    Optional<Resource> getGraph();
-
-    /**
-     * A helper method used to convert to a {@link Triple}, removing any Graph
-     * information that may be present.
-     *
-     * @return A {@link Triple} containing the Subject, Predicate, and Object
-     * from this Quad.
-     */
-    Triple toTriple();
-
+  Triple asTriple();
 }
