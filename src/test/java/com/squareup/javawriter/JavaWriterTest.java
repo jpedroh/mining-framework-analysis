7715a14aa81654afc2f5a2902ef0bfed8199f432
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 14:07:10 +0000

    Mining Framework Analysis

--- src/test/java/com/squareup/javawriter/JavaWriterTest.java
+++ src/test/java/com/squareup/javawriter/JavaWriterTest.java
@@ -821,11 +821,6 @@ public final class JavaWriterTest {
     javaWriter.emitPackage("com.blah");
     javaWriter.beginType("com.blah.Foo", "class", Collections.<Modifier>emptySet())
               .endType();
-    assertCode(""
-        + "package com.blah;\n"
-        + "\n"
-        + "class Foo {\n"
-        + "}\n");
   }
 
   private void assertCode(String expected) {
