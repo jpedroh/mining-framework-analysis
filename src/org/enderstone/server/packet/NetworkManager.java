c57744bc918d100a9fc0915cb9a15d256169982b
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 15:30:11 +0000

    Mining Framework Analysis

--- src/org/enderstone/server/packet/NetworkManager.java
+++ src/org/enderstone/server/packet/NetworkManager.java
@@ -1,22 +1,4 @@
-/* 
- * Enderstone
- * Copyright (C) 2014 Sander Gielisse and Fernando van Loenhout
- *
- *     This program is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
- *
- *     This program is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
- *
- *     You should have received a copy of the GNU General Public License
- *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 package org.enderstone.server.packet;
-
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerAdapter;
@@ -61,287 +43,280 @@ import org.enderstone.server.packet.play.PacketOutUpdateTime;
 import org.enderstone.server.regions.EnderWorld;
 
 public class NetworkManager extends ChannelHandlerAdapter {
+  public ChannelHandlerContext ctx;
 
-	public ChannelHandlerContext ctx;
-	public EnderPlayer player;
-	public String wantedName;
-	private EncryptionSettings encryptionSettings;
-	public UUID uuid;
-	public PlayerTextureStore skinBlob;
-	public int clientVersion;
-	
-	public PacketHandshake latestHandshakePacket;
-	public volatile int handShakeStatus = -1;
+  public EnderPlayer player;
 
-	private final Queue<Packet> packets = new LinkedList<>();
-	private volatile boolean isConnected = true;
+  public String wantedName;
 
-	public EncryptionSettings getEncryptionSettings() {
-		return encryptionSettings;
-	}
+  private EncryptionSettings encryptionSettings;
 
-	public void regenerateEncryptionSettings() {
-		encryptionSettings = new EncryptionSettings();
-		// encryptionSettings.serverid = new BigInteger(130,
-		// Main.random).toString(32).substring(0, 20);
-		encryptionSettings.serverid = "";
-		KeyPairGenerator keyPairGenerator;
-		try {
-			keyPairGenerator = KeyPairGenerator.getInstance("RSA");
-		} catch (NoSuchAlgorithmException ex) {
-			throw new RuntimeException("Server platfrom unsuported, cannot hash rsa", ex);
-		}
-		keyPairGenerator.initialize(1024);
-		encryptionSettings.keyPair = keyPairGenerator.genKeyPair();
-		encryptionSettings.verifyToken = new byte[16];
-		Main.random.nextBytes(encryptionSettings.verifyToken);
-	}
+  public UUID uuid;
 
-	public void forcePacketFlush() {
-		synchronized (packets) {
-			Packet p;
-			while ((p = packets.poll()) != null) {
-				ctx.write(p);
-				// EnderLogger.debug("Out: "+p.toString());
-				p.onSend(this);
-			}
-			ctx.flush();
-		}
-	}
+  public PlayerTextureStore skinBlob;
 
-	@Override
-	public void channelRead(ChannelHandlerContext ctx, Object msg) {
-		if (this.ctx == null)
-			this.ctx = ctx;
-		((Packet) msg).onRecieve(this);
-		// EnderLogger.debug("in: " + msg);
-		forcePacketFlush();
-	}
+  public int clientVersion;
 
-	@Override
-	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
-		EnderLogger.exception(cause);
-		ctx.close();
-		super.exceptionCaught(ctx, cause);
-	}
+  public PacketHandshake latestHandshakePacket;
 
-	@Override
-	public void channelInactive(ChannelHandlerContext ctx) throws Exception {
-		this.onDisconnect();
-		super.channelInactive(ctx);
-	}
+  public volatile int handShakeStatus = -1;
 
-	private void onDisconnect() {
-		if (this.isConnected == false) return;
-		synchronized (packets) {
-			if (this.isConnected == false) return;
-			this.isConnected = false;
-			this.packets.clear();
-			if (player != null && player.isOnline()) {
-				final EnderPlayer subPlayer = player;
-				Main.getInstance().sendToMainThread(new Runnable() {
+  private final Queue<Packet> packets = new LinkedList<>();
 
-					@Override
-					public void run() {
-						subPlayer.onDisconnect();
-					}
-				});
-				player.isOnline = false;
-			}
-		}
-	}
+  private volatile boolean isConnected = true;
 
-	private MinecraftServerCodex codex;
+  public EncryptionSettings getEncryptionSettings() {
+    return encryptionSettings;
+  }
 
-	public MinecraftServerCodex createCodex() {
-		return codex = new MinecraftServerCodex(this);
-	}
+  public void regenerateEncryptionSettings() {
+    encryptionSettings = new EncryptionSettings();
+    encryptionSettings.serverid = "";
+    KeyPairGenerator keyPairGenerator;
+    try {
+      keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+    } catch (NoSuchAlgorithmException ex) {
+      throw new RuntimeException("Server platfrom unsuported, cannot hash rsa", ex);
+    }
+    keyPairGenerator.initialize(1024);
+    encryptionSettings.keyPair = keyPairGenerator.genKeyPair();
+    encryptionSettings.verifyToken = new byte[16];
+    Main.random.nextBytes(encryptionSettings.verifyToken);
+  }
 
-	public MinecraftServerCodex getCodex() {
-		return codex;
-	}
+  public void forcePacketFlush() {
+    synchronized (packets) {
+      Packet p;
+      while ((p = packets.poll()) != null) {
+        ctx.write(p);
+        p.onSend(this);
+      }
+      ctx.flush();
+    }
+  }
 
-	public void sendPacket(Packet... packets) {
-		if (this.isConnected == false) return;
-		synchronized (packets) {
-			if (this.isConnected == false) return;
-			for (Packet packet : packets) {
-				this.packets.offer(packet);
-			}
-		}
-	}
+  @Override public void channelRead(ChannelHandlerContext ctx, Object msg) {
+    if (this.ctx == null) {
+      this.ctx = ctx;
+    }
+    ((Packet) msg).onRecieve(this);
+    forcePacketFlush();
+  }
 
-	public void setupEncryption(final SecretKey key) throws IOException {
-		final Cipher decrypter = generateKey(2, key);
-		final Cipher encrypter = generateKey(1, key);
-		ctx.pipeline().addBefore("packet_rw_converter", "decrypt", new MessageToMessageDecoder<ByteBuf>() {
+  @Override public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+    EnderLogger.exception(cause);
+    ctx.close();
+    super.exceptionCaught(ctx, cause);
+  }
 
-			NetworkEncrypter chipper = new NetworkEncrypter(decrypter);
+  @Override public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+    this.onDisconnect();
+    super.channelInactive(ctx);
+  }
 
-			@Override
-			protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws ShortBufferException {
-				System.out.println("Decrypting " + in.readableBytes());
-				this.chipper.decrypt(ctx, in);
-			}
+  private void onDisconnect() {
+    if (this.isConnected == false) {
+      return;
+    }
+    synchronized (packets) {
+      if (this.isConnected == false) {
+        return;
+      }
+      this.isConnected = false;
+      this.packets.clear();
+      if (player != null && player.isOnline()) {
+        final EnderPlayer subPlayer = player;
+        Main.getInstance().sendToMainThread(new Runnable() {
+          @Override public void run() {
+            subPlayer.onDisconnect();
+          }
+        });
+        player.isOnline = false;
+      }
+    }
+  }
 
-		});
-		ctx.pipeline().addBefore("packet_rw_converter", "encrypt", new MessageToByteEncoder<ByteBuf>() {
+  private MinecraftServerCodex codex;
 
-			NetworkEncrypter chipper = new NetworkEncrypter(encrypter);
+  public MinecraftServerCodex createCodex() {
+    return codex = new MinecraftServerCodex(this);
+  }
 
-			@Override
-			protected void encode(ChannelHandlerContext ctx, ByteBuf msg, ByteBuf out) throws Exception {
-				System.out.println("Encrypting " + msg.readableBytes());
-				this.chipper.encrypt(msg, out);
-			}
+  public MinecraftServerCodex getCodex() {
+    return codex;
+  }
 
-		});
-	}
+  public void sendPacket(Packet... packets) {
+    if (this.isConnected == false) {
+      return;
+    }
+    synchronized (packets) {
+      if (this.isConnected == false) {
+        return;
+      }
+      for (Packet packet : packets) {
+        this.packets.offer(packet);
+      }
+    }
+  }
 
-	private static Cipher generateKey(int keyType, Key secretKey) throws IOException {
-		try {
-			Cipher localCipher = Cipher.getInstance("AES/CFB8/NoPadding");
-			localCipher.init(keyType, secretKey, new IvParameterSpec(secretKey.getEncoded()));
-			return localCipher;
-		} catch (GeneralSecurityException localGeneralSecurityException) {
-			throw new IOException("Unable to generate a encryption key", localGeneralSecurityException);
-		}
-	}
+  public void setupEncryption(final SecretKey key) throws IOException {
+    final Cipher decrypter = generateKey(2, key);
+    final Cipher encrypter = generateKey(1, key);
+    ctx.pipeline().addBefore("packet_rw_converter", "decrypt", new MessageToMessageDecoder<ByteBuf>() {
+      NetworkEncrypter chipper = new NetworkEncrypter(decrypter);
 
-	public void spawnPlayer() {
-		if (player != null)
-			throw new IllegalStateException();
-		if (this.uuid == null) {
-			this.disconnect("Illegal uuid", true);
-			return;
-		}
-		if (this.skinBlob == null)
-			this.skinBlob = PlayerTextureStore.DEFAULT_STORE; // Null oject
-																// design
-																// pattern
+      @Override protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws ShortBufferException {
+        System.out.println("Decrypting " + in.readableBytes());
+        this.chipper.decrypt(ctx, in);
+      }
+    });
+    ctx.pipeline().addBefore("packet_rw_converter", "encrypt", new MessageToByteEncoder<ByteBuf>() {
+      NetworkEncrypter chipper = new NetworkEncrypter(encrypter);
 
-		final Object lock = new Object();
-		synchronized (lock) {
-			Main.getInstance().sendToMainThread(new Runnable() {
+      @Override protected void encode(ChannelHandlerContext ctx, ByteBuf msg, ByteBuf out) throws Exception {
+        System.out.println("Encrypting " + msg.readableBytes());
+        this.chipper.encrypt(msg, out);
+      }
+    });
+  }
 
-				@Override
-				public void run() {
-					EnderWorld world = Main.getInstance().worlds.get(0);
+  private static Cipher generateKey(int keyType, Key secretKey) throws IOException {
+    try {
+      Cipher localCipher = Cipher.getInstance("AES/CFB8/NoPadding");
+      localCipher.init(keyType, secretKey, new IvParameterSpec(secretKey.getEncoded()));
+      return localCipher;
+    } catch (GeneralSecurityException localGeneralSecurityException) {
+      throw new IOException("Unable to generate a encryption key", localGeneralSecurityException);
+    }
+  }
 
-					EnderLogger.debug("Player " + wantedName + " spawning in world: " + world.worldName);
-					player = new EnderPlayer(world, wantedName, NetworkManager.this, uuid, skinBlob);
-					world.players.add(player);
-					Main.getInstance().onlinePlayers.add(player);
-					try {
-						sendPacket(new PacketOutLoginSucces(player.uuid.toString(), player.getPlayerName()));
-						sendPacket(new PacketOutJoinGame(player.getEntityId(), (byte) GameMode.SURVIVAL.getId(), (byte) 0, (byte) 1, (byte) 60, "default", false));
-						sendPacket(new PacketOutUpdateTime(0, world.getTime()));
-						Location spawn = world.getSpawn();
-						Location loc = player.getLocation();
-						loc.setX(spawn.getX());
-						loc.setY(spawn.getY());
-						loc.setZ(spawn.getZ());
-						loc.setYaw(spawn.getYaw());
-						loc.setPitch(spawn.getPitch());
+  public void spawnPlayer() {
+    if (player != null) {
+      throw new IllegalStateException();
+    }
+    if (this.uuid == null) {
+      this.disconnect("Illegal uuid", true);
+      return;
+    }
+    if (this.skinBlob == null) {
+      this.skinBlob = PlayerTextureStore.DEFAULT_STORE;
+    }
+    final Object lock = new Object();
+    synchronized (lock) {
+      Main.getInstance().sendToMainThread(new Runnable() {
+        @Override public void run() {
+          EnderWorld world = Main.getInstance().worlds.get(0);
+          EnderLogger.debug("Player " + wantedName + " spawning in world: " + world.worldName);
+          player = new EnderPlayer(world, wantedName, NetworkManager.this, uuid, skinBlob);
+          world.players.add(player);
+          Main.getInstance().onlinePlayers.add(player);
+          try {
+            sendPacket(new PacketOutLoginSucces(player.uuid.toString(), player.getPlayerName()));
+            sendPacket(new PacketOutJoinGame(player.getEntityId(), (byte) GameMode.SURVIVAL.getId(), (byte) 0, (byte) 1, (byte) 60, "default", false));
+            sendPacket(new PacketOutUpdateTime(0, world.getTime()));
+            Location spawn = world.getSpawn();
+            Location loc = player.getLocation();
+            loc.setX(spawn.getX());
+            loc.setY(spawn.getY());
+            loc.setZ(spawn.getZ());
+            loc.setYaw(spawn.getYaw());
+            loc.setPitch(spawn.getPitch());
+            world.doChunkUpdatesForPlayer(player, player.chunkInformer, 1);
+            player.teleport(world.getSpawn());
+            player.onSpawn();
+            player.onRespawn();
+            sendPacket(new PacketOutSpawnPosition(spawn));
+            sendPacket(new PacketOutPlayerPositionLook(loc.getX(), loc.getY(), loc.getZ(), loc.getYaw(), loc.getPitch(), (byte) 0b00000));
+            sendPacket(new PacketOutUpdateHealth(player.getHealth(), player.clientSettings.food, player.clientSettings.foodSaturation));
+          } catch (Exception e) {
+            EnderLogger.exception(e);
+          } finally {
+            synchronized (lock) {
+              lock.notifyAll();
+            }
+          }
+        }
+      });
+      try {
+        lock.wait();
+      } catch (InterruptedException ex) {
+        Thread.currentThread().interrupt();
+      }
+    }
+  }
 
-						world.doChunkUpdatesForPlayer(player, player.chunkInformer, 1);
-						player.teleport(world.getSpawn());
-						player.onSpawn();
-						player.onRespawn();
-						sendPacket(new PacketOutSpawnPosition(spawn));
-						sendPacket(new PacketOutPlayerPositionLook(loc.getX(), loc.getY(), loc.getZ(), loc.getYaw(), loc.getPitch(), (byte) 0b00000));
-						sendPacket(new PacketOutUpdateHealth(player.getHealth(), player.clientSettings.food, player.clientSettings.foodSaturation));
-					} catch (Exception e) {
-						EnderLogger.exception(e);
-					} finally {
-						synchronized (lock) {
-							lock.notifyAll();
-						}
-					}
+  public class EncryptionSettings {
+    private String serverid;
 
-				}
-			});
-			try {
-				lock.wait();
-			} catch (InterruptedException ex) {
-				Thread.currentThread().interrupt();
-			}
-		}
+    private KeyPair keyPair;
 
-	}
+    private byte[] verifyToken;
 
-	public class EncryptionSettings {
+    public String getServerid() {
+      return serverid;
+    }
 
-		private String serverid;
-		private KeyPair keyPair;
-		private byte[] verifyToken;
+    public KeyPair getKeyPair() {
+      return keyPair;
+    }
 
-		public String getServerid() {
-			return serverid;
-		}
+    public byte[] getVerifyToken() {
+      return verifyToken;
+    }
+  }
 
-		public KeyPair getKeyPair() {
-			return keyPair;
-		}
-
-		public byte[] getVerifyToken() {
-			return verifyToken;
-		}
-	}
-
-	/**
+  /**
 	 * Disconnected the player with the specified warning
 	 * @param message the disconnect message
 	 * @deprecated This method doesn't know the real cause of the disconnect, use disconnect(Message, boolean) instead
 	 */
-	@Deprecated
-	public void disconnect(String message) {
-		this.disconnect(message, true);
-	}
-	
-	public void disconnect(String message, boolean byError) {
-		this.disconnect(new SimpleMessage(message), byError);
-	}
-	
-	@Deprecated
-	public void disconnect(Message message) {
-		this.disconnect(message, true);
-	}
+  @Deprecated public void disconnect(String message) {
+    this.disconnect(message, true);
+  }
 
-	/**
+  public void disconnect(String message, boolean byError) {
+    this.disconnect(new SimpleMessage(message), byError);
+  }
+
+  @Deprecated public void disconnect(Message message) {
+    this.disconnect(message, true);
+  }
+
+  /**
 	 * Disconnects the player
 	 * @param message the message to kick the player
 	 * @param byError if true, then the reason of the kick is printed in the console
 	 */
-	public void disconnect(Message message, boolean byError) {
-		try {
-			this.ctx.channel().pipeline().addFirst("packet_r_disconnected", new DiscardingReader());
-			Level level = byError ? Level.WARNING : Level.INFO;
-			if (this.player == null)
-				EnderLogger.logger.log(level, "Kicking unregistered channel " + this.digitalName() + ": " + message.toPlainText());
-			else
-				EnderLogger.logger.log(level, "Kicking " + this.digitalName() +": " + message.toPlainText());
-			Packet p = codex.getDisconnectionPacket(message);
-			EnderLogger.warn("Kicked ");
-			this.ctx.channel().pipeline().addFirst(new DiscardingReader());
-			if (p != null)
-				ctx.write(p).addListener(ChannelFutureListener.CLOSE);
-		} catch (Exception ex) {
-			EnderLogger.exception(ex);
-		}
-		finally
-		{
-			this.onDisconnect();
-		}
-	}
-	
-	public void enableCompression(){
-		this.ctx.pipeline().addBefore("packet_rw_converter", "packet_r_decompressor", new MinecraftDecompressionCodex(this));
-		this.ctx.pipeline().addBefore("packet_rw_converter", "packet_w_compressor", new MinecraftCompressionCodex(this));
-	}
-	public String digitalName()
-	{
-		InetSocketAddress address = (InetSocketAddress)ctx.channel().remoteAddress();
-		return "["+address.getAddress()+"|"+address.getPort()+"]-("+String.valueOf(this.wantedName)+"";
-	}
+  public void disconnect(Message message, boolean byError) {
+    try {
+      this.ctx.channel().pipeline().addFirst("packet_r_disconnected", new DiscardingReader());
+      Level level = byError ? Level.WARNING : Level.INFO;
+      if (this.player == null) {
+        EnderLogger.logger.log(level, "Kicking unregistered channel " + this.digitalName() + ": " + message.toPlainText());
+      } else {
+        EnderLogger.logger.log(level, "Kicking " + this.digitalName() + ": " + message.toPlainText());
+      }
+      Packet p = codex.getDisconnectionPacket(message);
+      EnderLogger.warn("Kicked ");
+      this.ctx.channel().pipeline().addFirst(new DiscardingReader());
+      if (p != null) {
+        ctx.write(p).addListener(ChannelFutureListener.CLOSE);
+      }
+    } catch (Exception ex) {
+      EnderLogger.exception(ex);
+    } finally {
+      this.onDisconnect();
+    }
+  }
+
+  public void enableCompression() {
+    this.ctx.pipeline().addBefore("packet_rw_converter", "packet_r_decompressor", new MinecraftDecompressionCodex(this));
+    this.ctx.pipeline().addBefore("packet_rw_converter", "packet_w_compressor", new MinecraftCompressionCodex(this));
+  }
+
+  public String digitalName() {
+    InetSocketAddress address = (InetSocketAddress) ctx.channel().remoteAddress();
+    return "[" + address.getAddress() + "|" + address.getPort() + "]-(" + String.valueOf(this.wantedName) + "";
+  }
 }
