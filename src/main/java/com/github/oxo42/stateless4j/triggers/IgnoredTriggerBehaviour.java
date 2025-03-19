164e1b0785dba1837bb8456b7538b0edb635d694
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 22:49:58 +0000

    Mining Framework Analysis

--- src/main/java/com/github/oxo42/stateless4j/triggers/IgnoredTriggerBehaviour.java
+++ src/main/java/com/github/oxo42/stateless4j/triggers/IgnoredTriggerBehaviour.java
@@ -1,15 +1,6 @@
-package com.github.oxo42.stateless4j.triggers;
-
-import com.github.oxo42.stateless4j.OutVar;
-import com.github.oxo42.stateless4j.delegates.FuncBoolean;
-
-public class IgnoredTriggerBehaviour<TState, TTrigger> extends TriggerBehaviour<TState, TTrigger> {
-    public IgnoredTriggerBehaviour(TTrigger trigger, FuncBoolean guard) {
-        super(trigger, guard);
-    }
-
-    @Override
-    public boolean resultsInTransitionFrom(TState source, Object[] args, OutVar<TState> dest) {
-        return false;
-    }
-}
+  package     com . github . oxo42 . stateless4j . triggers ;   import      com . github . oxo42 . stateless4j . delegates . FuncBoolean ;  import     com . github . oxo42 . stateless4j . OutVar ;   public class IgnoredTriggerBehaviour  <  TState ,  TTrigger >  extends  TriggerBehaviour  < TState , TTrigger >  {   public IgnoredTriggerBehaviour  (  TTrigger trigger ,  FuncBoolean guard )  {  super  ( trigger , guard ) ; } 
+<<<<<<<
+   @ Override public TState resultsInTransitionFrom  (  TState source ,  Object ...  args )  {  throw  new IllegalStateException  ( ) ; }
+=======
+>>>>>>>
+    @ Override public boolean resultsInTransitionFrom  (  TState source ,   Object  [ ] args ,   OutVar  < TState > dest )  {  return false ; } }
