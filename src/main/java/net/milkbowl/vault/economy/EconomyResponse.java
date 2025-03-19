956017b01ade8a4eaef38dfcbf901101966e1610
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 20:43:54 +0000

    Mining Framework Analysis

--- src/main/java/net/milkbowl/vault/economy/EconomyResponse.java
+++ src/main/java/net/milkbowl/vault/economy/EconomyResponse.java
@@ -1,18 +1,3 @@
-/* This file is part of Vault.
-
-    Vault is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Lesser General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    Vault is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Lesser General Public License for more details.
-
-    You should have received a copy of the GNU Lesser General Public License
-    along with Vault.  If not, see <http://www.gnu.org/licenses/>.
- */
 package net.milkbowl.vault.economy;
 
 /**
@@ -20,66 +5,65 @@ package net.milkbowl.vault.economy;
  * It includes a {@link ResponseType} indicating whether the plugin currently being used for Economy actually allows
  * the method, or if the operation was a success or failure.
  *
- * @deprecated in lieu of the modern Vault2. To update alter your import to net.milkbowl.vault2.economy.
  */
 public class EconomyResponse {
+  public static enum ResponseType {
+    SUCCESS(1),
+    FAILURE(2),
+    NOT_IMPLEMENTED(3)
+    ;
 
-    /**
-     * Enum for types of Responses indicating the status of a method call.
-     */
-    public static enum ResponseType {
-        SUCCESS(1),
-        FAILURE(2),
-        NOT_IMPLEMENTED(3);
+    private int id;
 
-        private int id;
-
-        ResponseType(int id) {
-            this.id = id;
-        }
-
-        int getId() {
-            return id;
-        }
+    ResponseType(int id) {
+      this.id = id;
     }
 
-    /**
+    int getId() {
+      return id;
+    }
+  }
+
+  /**
      * Amount modified by calling method
      */
-    public final double amount;
-    /**
+  public final double amount;
+
+  /**
      * New balance of account
      */
-    public final double balance;
-    /**
+  public final double balance;
+
+  /**
      * Success or failure of call. Using Enum of ResponseType to determine valid
      * outcomes
      */
-    public final ResponseType type;
-    /**
+  public final ResponseType type;
+
+  /**
      * Error message if the variable 'type' is ResponseType.FAILURE
      */
-    public final String errorMessage;
+  public final String errorMessage;
 
-    /**
+  /**
      * Constructor for EconomyResponse
      * @param amount Amount modified during operation
      * @param balance New balance of account
      * @param type Success or failure type of the operation
      * @param errorMessage Error message if necessary (commonly null)
      */
-    public EconomyResponse(double amount, double balance, ResponseType type, String errorMessage) {
-        this.amount = amount;
-        this.balance = balance;
-        this.type = type;
-        this.errorMessage = errorMessage;
-    }
+  public EconomyResponse(double amount, double balance, ResponseType type, String errorMessage) {
+    this.amount = amount;
+    this.balance = balance;
+    this.type = type;
+    this.errorMessage = errorMessage;
+  }
 
-    /**
+  /**
      * Checks if an operation was successful
      * @return Value
      */
-    public boolean transactionSuccess() {
-        return type == ResponseType.SUCCESS;
-    }
+  public boolean transactionSuccess() {
+    return type == ResponseType.SUCCESS;
+  }
 }
