cf5444883d444898d77ad932b960d4efab81c9af
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 21:28:24 +0000

    Mining Framework Analysis

--- jmetal-core/src/test/java/org/uma/jmetal/component/ranking/MergeNonDominatedSortingRankingTest.java
+++ jmetal-core/src/test/java/org/uma/jmetal/component/ranking/MergeNonDominatedSortingRankingTest.java
@@ -1,17 +1,1 @@
-package org.uma.jmetal.component.ranking;
-
-import org.uma.jmetal.component.ranking.impl.MergeSortNonDominatedSortRanking;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.IntStream;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-
-public class MergeNonDominatedSortingRankingTest extends NonDominanceRankingTestCases {
-  public MergeNonDominatedSortingRankingTest() {
-    this.ranking = new MergeSortNonDominatedSortRanking();
-  }
-}
+  package     org . uma . jmetal . component . ranking ;   import       org . uma . jmetal . component . ranking . impl . MergeSortNonDominatedSortRanking ;   public class MergeNonDominatedSortingRankingTest  extends NonDominanceRankingTestCases  {   public MergeNonDominatedSortingRankingTest  ( )  {    this . ranking =  new MergeSortNonDominatedSortRanking  ( ) ; } }
