7ed2160233de535aa126c0e8ec9503d5f8e6f73e
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 16:42:10 +0000

    Mining Framework Analysis

--- src/main/java/org/fenixedu/academic/domain/accounting/postingRules/AdministrativeOfficeFeePR.java
+++ src/main/java/org/fenixedu/academic/domain/accounting/postingRules/AdministrativeOfficeFeePR.java
@@ -1,92 +1,25 @@
-/**
- * Copyright © 2002 Instituto Superior Técnico
- *
- * This file is part of FenixEdu Academic.
- *
- * FenixEdu Academic is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * FenixEdu Academic is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with FenixEdu Academic.  If not, see <http://www.gnu.org/licenses/>.
- */
-package org.fenixedu.academic.domain.accounting.postingRules;
-
-import java.util.Optional;
-
-import org.fenixedu.academic.domain.accounting.EntryType;
-import org.fenixedu.academic.domain.accounting.Event;
-import org.fenixedu.academic.domain.accounting.EventType;
-import org.fenixedu.academic.domain.accounting.ServiceAgreementTemplate;
-import org.fenixedu.academic.domain.accounting.events.administrativeOfficeFee.IAdministrativeOfficeFeeEvent;
-import org.fenixedu.academic.domain.exceptions.DomainException;
-import org.fenixedu.academic.util.Money;
-import org.joda.time.DateTime;
-import org.joda.time.LocalDate;
-import org.joda.time.YearMonthDay;
-
-public class AdministrativeOfficeFeePR extends AdministrativeOfficeFeePR_Base implements IAdministrativeOfficeFeeAndInsurancePR {
-
-    protected AdministrativeOfficeFeePR() {
-        super();
-    }
-
-    public AdministrativeOfficeFeePR(DateTime startDate, DateTime endDate, ServiceAgreementTemplate serviceAgreementTemplate,
-            Money fixedAmount, Money fixedAmountPenalty, YearMonthDay whenToApplyFixedAmountPenalty) {
-        this();
-        init(EntryType.ADMINISTRATIVE_OFFICE_FEE, EventType.ADMINISTRATIVE_OFFICE_FEE, startDate, endDate,
-                serviceAgreementTemplate, fixedAmount, fixedAmountPenalty, whenToApplyFixedAmountPenalty);
-
-    }
-
-    @Override
-    protected Optional<LocalDate> getPenaltyDueDate(Event event) {
-        final IAdministrativeOfficeFeeEvent administrativeOfficeFeeEvent = (IAdministrativeOfficeFeeEvent) event;
-
-        final YearMonthDay paymentEndDate =
-            administrativeOfficeFeeEvent.getPaymentEndDate() != null ? administrativeOfficeFeeEvent
-                                                                                       .getPaymentEndDate() : getWhenToApplyFixedAmountPenalty();
-
-        return Optional.of(paymentEndDate.toLocalDate());
-    }
-
-    public AdministrativeOfficeFeePR edit(DateTime startDate, Money fixedAmount, Money penaltyAmount,
-            YearMonthDay whenToApplyFixedAmountPenalty) {
-
-        if (!startDate.isAfter(getStartDate())) {
-            throw new DomainException(
-                    "error.AdministrativeOfficeFeePR.startDate.is.before.then.start.date.of.previous.posting.rule");
-        }
-
-        deactivate(startDate);
-
-        return new AdministrativeOfficeFeePR(startDate.minus(1000), null, getServiceAgreementTemplate(), fixedAmount,
-                penaltyAmount, whenToApplyFixedAmountPenalty);
-    }
-
-    @Override
-    public YearMonthDay getAdministrativeOfficeFeePaymentLimitDate(DateTime startDate, DateTime endDate) {
-        return getWhenToApplyFixedAmountPenalty();
-    }
-
-    @Override
-    public Money getInsuranceAmount(DateTime startDate, DateTime endDate) {
-        return Money.ZERO;
-    }
-
-    @Override
-    public Money getAdministrativeOfficeFeeAmount(Event event, DateTime startDate, DateTime endDate) {
-        return getFixedAmount();
-    }
-
-    @Override
-    public Money getAdministrativeOfficeFeePenaltyAmount(Event event, DateTime startDate, DateTime endDate) {
-        return getFixedAmountPenalty();
-    }
-}
+  package      org . fenixedu . academic . domain . accounting . postingRules ;   import   java . util . Optional ;  import      org . fenixedu . academic . domain . accounting . EntryType ;  import      org . fenixedu . academic . domain . accounting . Event ;  import      org . fenixedu . academic . domain . accounting . EventType ;  import      org . fenixedu . academic . domain . accounting . ServiceAgreementTemplate ;  import        org . fenixedu . academic . domain . accounting . events . administrativeOfficeFee . IAdministrativeOfficeFeeEvent ;  import      org . fenixedu . academic . domain . exceptions . DomainException ;  import     org . fenixedu . academic . util . Money ;  import    org . joda . time . DateTime ;  import    org . joda . time . LocalDate ;  import    org . joda . time . YearMonthDay ;   public class AdministrativeOfficeFeePR  extends AdministrativeOfficeFeePR_Base  implements  IAdministrativeOfficeFeeAndInsurancePR  {   protected AdministrativeOfficeFeePR  ( )  {  super  ( ) ; }   public AdministrativeOfficeFeePR  (  DateTime startDate ,  DateTime endDate ,  ServiceAgreementTemplate serviceAgreementTemplate ,  Money fixedAmount ,  Money fixedAmountPenalty ,  YearMonthDay whenToApplyFixedAmountPenalty )  {  this  ( ) ;   init  (  EntryType . ADMINISTRATIVE_OFFICE_FEE ,  EventType . ADMINISTRATIVE_OFFICE_FEE , startDate , endDate , serviceAgreementTemplate , fixedAmount , fixedAmountPenalty , whenToApplyFixedAmountPenalty ) ; }    @ Override protected  Optional  < LocalDate > getPenaltyDueDate  (  Event event )  {   final 
+<<<<<<<
+IAdministrativeOfficeFeeEvent
+=======
+AdministrativeOfficeFeeAndInsuranceEvent
+>>>>>>>
+ 
+<<<<<<<
+ administrativeOfficeFeeEvent =  ( IAdministrativeOfficeFeeEvent ) event
+=======
+ administrativeOfficeFeeAndInsuranceEvent =  ( AdministrativeOfficeFeeAndInsuranceEvent ) event
+>>>>>>>
+ ;   final YearMonthDay  paymentEndDate =    
+<<<<<<<
+administrativeOfficeFeeEvent
+=======
+administrativeOfficeFeeAndInsuranceEvent
+>>>>>>>
+ . getPaymentEndDate  ( ) != null ?  
+<<<<<<<
+administrativeOfficeFeeEvent
+=======
+administrativeOfficeFeeAndInsuranceEvent
+>>>>>>>
+ . getPaymentEndDate  ( ) :  getWhenToApplyFixedAmountPenalty  ( ) ;  return  Optional . of  (  paymentEndDate . toLocalDate  ( ) ) ; }   public AdministrativeOfficeFeePR edit  (  DateTime startDate ,  Money fixedAmount ,  Money penaltyAmount ,  YearMonthDay whenToApplyFixedAmountPenalty )  {  if  (  !  startDate . isAfter  (  getStartDate  ( ) ) )  {  throw  new DomainException  ( "error.AdministrativeOfficeFeePR.startDate.is.before.then.start.date.of.previous.posting.rule" ) ; }   deactivate  ( startDate ) ;  return  new AdministrativeOfficeFeePR  (  startDate . minus  ( 1000 ) , null ,  getServiceAgreementTemplate  ( ) , fixedAmount , penaltyAmount , whenToApplyFixedAmountPenalty ) ; }    @ Override public YearMonthDay getAdministrativeOfficeFeePaymentLimitDate  (  DateTime startDate ,  DateTime endDate )  {  return  getWhenToApplyFixedAmountPenalty  ( ) ; }    @ Override public Money getInsuranceAmount  (  DateTime startDate ,  DateTime endDate )  {  return  Money . ZERO ; }    @ Override public Money getAdministrativeOfficeFeeAmount  (  Event event ,  DateTime startDate ,  DateTime endDate )  {  return  getFixedAmount  ( ) ; }    @ Override public Money getAdministrativeOfficeFeePenaltyAmount  (  Event event ,  DateTime startDate ,  DateTime endDate )  {  return  getFixedAmountPenalty  ( ) ; } }
--- src/main/java/org/fenixedu/academic/domain/accounting/postingRules/ResidencePR.java
+++ src/main/java/org/fenixedu/academic/domain/accounting/postingRules/ResidencePR.java
@@ -1,62 +1,19 @@
-/**
- * Copyright © 2002 Instituto Superior Técnico
- *
- * This file is part of FenixEdu Academic.
- *
- * FenixEdu Academic is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * FenixEdu Academic is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with FenixEdu Academic.  If not, see <http://www.gnu.org/licenses/>.
- */
-package org.fenixedu.academic.domain.accounting.postingRules;
-
-import java.math.BigDecimal;
-import java.util.Map;
-
-import org.fenixedu.academic.domain.accounting.EntryType;
-import org.fenixedu.academic.domain.accounting.Event;
-import org.fenixedu.academic.domain.accounting.EventType;
-import org.fenixedu.academic.domain.accounting.ResidenceEvent;
-import org.fenixedu.academic.domain.accounting.ServiceAgreementTemplate;
-import org.fenixedu.academic.util.Money;
-import org.joda.time.DateTime;
-import org.joda.time.Days;
-import org.joda.time.LocalDate;
-
-import edu.emory.mathcs.backport.java.util.Collections;
-
-public class ResidencePR extends ResidencePR_Base {
-
-    public ResidencePR(final DateTime startDate, final DateTime endDate, final ServiceAgreementTemplate serviceAgreementTemplate,
-            Money penaltyPerDay) {
-        super.init(EntryType.RESIDENCE_FEE, EventType.RESIDENCE_PAYMENT, startDate, endDate, serviceAgreementTemplate);
-        setPenaltyPerDay(penaltyPerDay);
-    }
-
-    @Override
-    public Map<LocalDate, Money> getDueDatePenaltyAmountMap(Event event, DateTime when) {
-        ResidenceEvent residenceEvent = (ResidenceEvent) event;
-        if (residenceEvent.getPaymentLimitDate().isAfter(when)) {
-            return Collections.emptyMap();
-        }
-
-        final BigDecimal daysBetween = BigDecimal.valueOf(Days.daysBetween(residenceEvent.getPaymentLimitDate().toLocalDate(),
-                when.toLocalDate()).getDays());
-        final Money amount = getPenaltyPerDay().multiply(daysBetween);
-        return Collections.singletonMap(residenceEvent.getPaymentLimitDate().toLocalDate(), amount);
-    }
-
-    @Override
-    protected Money doCalculationForAmountToPay(Event event, DateTime when) {
-        return ((ResidenceEvent) event).getRoomValue();
-    }
-
-}
+  package      org . fenixedu . academic . domain . accounting . postingRules ;   import   java . math . BigDecimal ;  import   java . util . Map ;  import      org . fenixedu . academic . domain . accounting . EntryType ;  import      org . fenixedu . academic . domain . accounting . Event ;  import      org . fenixedu . academic . domain . accounting . EventType ;  import      org . fenixedu . academic . domain . accounting . ResidenceEvent ;  import      org . fenixedu . academic . domain . accounting . ServiceAgreementTemplate ;  import     org . fenixedu . academic . util . Money ;  import    org . joda . time . DateTime ;  import    org . joda . time . Days ;  import    org . joda . time . LocalDate ;  import       edu . emory . mathcs . backport . java . util . Collections ;   public class ResidencePR  extends ResidencePR_Base  {   public ResidencePR  (   final DateTime startDate ,   final DateTime endDate ,   final ServiceAgreementTemplate serviceAgreementTemplate ,  Money penaltyPerDay )  {   super . init  (  EntryType . RESIDENCE_FEE ,  EventType . RESIDENCE_PAYMENT , startDate , endDate , serviceAgreementTemplate ) ;   setPenaltyPerDay  ( penaltyPerDay ) ; }    @ Override public  Map  < LocalDate , Money > getDueDatePenaltyAmountMap  (  Event event ,  DateTime when )  {  ResidenceEvent  residenceEvent =  ( ResidenceEvent ) event ;  if  (   residenceEvent . getPaymentLimitDate  ( ) . isAfter  ( when ) )  {  return  Collections . emptyMap  ( ) ; }   final BigDecimal  daysBetween =  BigDecimal . valueOf  (   Days . daysBetween  (  
+<<<<<<<
+ residenceEvent . getPaymentLimitDate  ( )
+=======
+residenceEvent
+>>>>>>>
+ . 
+<<<<<<<
+toLocalDate
+=======
+getPaymentLimitDate
+>>>>>>>
+  ( ) , 
+<<<<<<<
+ when . toLocalDate  ( )
+=======
+when
+>>>>>>>
+ ) . getDays  ( ) ) ;   final Money  amount =   getPenaltyPerDay  ( ) . multiply  ( daysBetween ) ;  return  Collections . singletonMap  (   residenceEvent . getPaymentLimitDate  ( ) . toLocalDate  ( ) , amount ) ; }    @ Override protected Money doCalculationForAmountToPay  (  Event event ,  DateTime when )  {  return   (  ( ResidenceEvent ) event ) . getRoomValue  ( ) ; } }
