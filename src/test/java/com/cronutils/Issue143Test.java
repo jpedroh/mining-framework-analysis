092be61d0e8ca0792c9e1d680dc3fe6fedd483a9
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 19:54:31 +0000

    Mining Framework Analysis

--- src/test/java/com/cronutils/Issue143Test.java
+++ src/test/java/com/cronutils/Issue143Test.java
@@ -1,96 +1,162 @@
-/*
- * Copyright 2015 jmrozanec
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.cronutils;
-
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.Optional;
-
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
-
 import com.cronutils.model.CronType;
 import com.cronutils.model.definition.CronDefinitionBuilder;
 import com.cronutils.model.time.ExecutionTime;
 import com.cronutils.parser.CronParser;
-
 import static org.junit.Assert.fail;
 
 public class Issue143Test {
+  private static final String LAST_EXECUTION_NOT_PRESENT_ERROR = "last execution was not present";
 
-    private static final String LAST_EXECUTION_NOT_PRESENT_ERROR = "last execution was not present";
-    private CronParser parser;
-    private ZonedDateTime currentDateTime;
+  private CronParser parser;
 
-    @Before
-    public void setUp() {
-        // Make sure that current date is before Dec-31
-        currentDateTime = ZonedDateTime.of(LocalDateTime.of(2016, 12, 20, 12, 0),
-                ZoneId.systemDefault());
+  private ZonedDateTime currentDateTime;
 
-        parser = new CronParser(CronDefinitionBuilder.instanceDefinitionFor(CronType.QUARTZ));
+  @Before public void setUp() {
+    currentDateTime = ZonedDateTime.of(LocalDateTime.of(2016, 12, 20, 12, 0), ZoneId.systemDefault());
+    parser = new CronParser(CronDefinitionBuilder.instanceDefinitionFor(CronType.QUARTZ));
+  }
+
+  @Test public void testCase1() {
+    final ExecutionTime et = ExecutionTime.forCron(parser.parse("0 0 12 31 12 ? *"));
+    final Optional<ZonedDateTime> 
+<<<<<<< /usr/src/app/output/jmrozanec/cron-utils/219633080b459601437e67784e60a42ca6d53e10/src/test/java/com/cronutils/Issue143Test.java/left.java
+    lastExecution = et.lastExecution(currentDateTime)
+=======
+    olast = et.lastExecution(currentDateTime)
+>>>>>>> /usr/src/app/output/jmrozanec/cron-utils/219633080b459601437e67784e60a42ca6d53e10/src/test/java/com/cronutils/Issue143Test.java/right.java
+    ;
+
+<<<<<<< /usr/src/app/output/jmrozanec/cron-utils/219633080b459601437e67784e60a42ca6d53e10/src/test/java/com/cronutils/Issue143Test.java/left.java
+    if (lastExecution.isPresent()) {
+      final ZonedDateTime actual = lastExecution.get();
+      final ZonedDateTime expected = ZonedDateTime.of(LocalDateTime.of(2015, 12, 31, 12, 00), ZoneId.systemDefault());
+      Assert.assertEquals(expected, actual);
     }
+=======
+    ZonedDateTime last = olast.orElse(null);
+>>>>>>> /usr/src/app/output/jmrozanec/cron-utils/219633080b459601437e67784e60a42ca6d53e10/src/test/java/com/cronutils/Issue143Test.java/right.java
 
-    @Test
-    public void testCase1() {
-        ExecutionTime et = ExecutionTime.forCron(parser.parse("0 0 12 31 12 ? *"));
-        Optional<ZonedDateTime> olast = et.lastExecution(currentDateTime);
-        ZonedDateTime last = olast.orElse(null);
 
-        ZonedDateTime expected = ZonedDateTime.of(LocalDateTime.of(2015, 12, 31, 12, 0),
-                ZoneId.systemDefault());
-        Assert.assertEquals(expected, last);
+<<<<<<< Unknown file: This is a bug in JDime.
+=======
+    ZonedDateTime expected = ZonedDateTime.of(LocalDateTime.of(2015, 12, 31, 12, 0), ZoneId.systemDefault());
+>>>>>>> /usr/src/app/output/jmrozanec/cron-utils/219633080b459601437e67784e60a42ca6d53e10/src/test/java/com/cronutils/Issue143Test.java/right.java
+
+
+<<<<<<< Unknown file: This is a bug in JDime.
+=======
+    Assert.assertEquals(expected, last);
+>>>>>>> /usr/src/app/output/jmrozanec/cron-utils/219633080b459601437e67784e60a42ca6d53e10/src/test/java/com/cronutils/Issue143Test.java/right.java
+  }
+
+  @Test public void testCase2() {
+    final ExecutionTime et = ExecutionTime.forCron(parser.parse("0 0 12 ? 12 SAT#5 *"));
+    final Optional<ZonedDateTime> 
+<<<<<<< /usr/src/app/output/jmrozanec/cron-utils/219633080b459601437e67784e60a42ca6d53e10/src/test/java/com/cronutils/Issue143Test.java/left.java
+    lastExecution = et.lastExecution(currentDateTime)
+=======
+    olast = et.lastExecution(currentDateTime)
+>>>>>>> /usr/src/app/output/jmrozanec/cron-utils/219633080b459601437e67784e60a42ca6d53e10/src/test/java/com/cronutils/Issue143Test.java/right.java
+    ;
+
+<<<<<<< /usr/src/app/output/jmrozanec/cron-utils/219633080b459601437e67784e60a42ca6d53e10/src/test/java/com/cronutils/Issue143Test.java/left.java
+    if (lastExecution.isPresent()) {
+      final ZonedDateTime expected = ZonedDateTime.of(LocalDateTime.of(2012, 12, 29, 12, 00), ZoneId.systemDefault());
+      Assert.assertEquals(expected, lastExecution.get());
+    } else {
+      fail(LAST_EXECUTION_NOT_PRESENT_ERROR);
     }
+=======
+    ZonedDateTime last = olast.orElse(null);
+>>>>>>> /usr/src/app/output/jmrozanec/cron-utils/219633080b459601437e67784e60a42ca6d53e10/src/test/java/com/cronutils/Issue143Test.java/right.java
 
-    @Test
-    public void testCase2() {
-        final ExecutionTime et = ExecutionTime.forCron(parser.parse("0 0 12 ? 12 SAT#5 *"));
-        final Optional<ZonedDateTime> lastExecution = et.lastExecution(currentDateTime);
-        if (lastExecution.isPresent()) {
-            final ZonedDateTime expected = ZonedDateTime.of(LocalDateTime.of(2012, 12, 29, 12, 00), ZoneId.systemDefault());
-            Assert.assertEquals(expected, lastExecution.get());
-        } else {
-            fail(LAST_EXECUTION_NOT_PRESENT_ERROR);
-        }
+
+<<<<<<< Unknown file: This is a bug in JDime.
+=======
+    ZonedDateTime expected = ZonedDateTime.of(LocalDateTime.of(2012, 12, 29, 12, 0), ZoneId.systemDefault());
+>>>>>>> /usr/src/app/output/jmrozanec/cron-utils/219633080b459601437e67784e60a42ca6d53e10/src/test/java/com/cronutils/Issue143Test.java/right.java
+
+
+<<<<<<< Unknown file: This is a bug in JDime.
+=======
+    Assert.assertEquals(expected, last);
+>>>>>>> /usr/src/app/output/jmrozanec/cron-utils/219633080b459601437e67784e60a42ca6d53e10/src/test/java/com/cronutils/Issue143Test.java/right.java
+  }
+
+  @Test @Ignore public void testCase3() {
+    final ExecutionTime et = ExecutionTime.forCron(parser.parse("0 0 12 31 1/1 ? *"));
+    final Optional<ZonedDateTime> 
+<<<<<<< /usr/src/app/output/jmrozanec/cron-utils/219633080b459601437e67784e60a42ca6d53e10/src/test/java/com/cronutils/Issue143Test.java/left.java
+    lastExecution = et.lastExecution(currentDateTime)
+=======
+    olast = et.lastExecution(currentDateTime)
+>>>>>>> /usr/src/app/output/jmrozanec/cron-utils/219633080b459601437e67784e60a42ca6d53e10/src/test/java/com/cronutils/Issue143Test.java/right.java
+    ;
+
+<<<<<<< /usr/src/app/output/jmrozanec/cron-utils/219633080b459601437e67784e60a42ca6d53e10/src/test/java/com/cronutils/Issue143Test.java/left.java
+    if (lastExecution.isPresent()) {
+      final ZonedDateTime expected = ZonedDateTime.of(LocalDateTime.of(2015, 12, 31, 12, 00), ZoneId.systemDefault());
+      Assert.assertEquals(expected, lastExecution.get());
+    } else {
+      fail(LAST_EXECUTION_NOT_PRESENT_ERROR);
     }
+=======
+    ZonedDateTime last = olast.orElse(null);
+>>>>>>> /usr/src/app/output/jmrozanec/cron-utils/219633080b459601437e67784e60a42ca6d53e10/src/test/java/com/cronutils/Issue143Test.java/right.java
 
-    @Test
-    @Ignore //FIXME Fix this test
-    public void testCase3() {
-        final ExecutionTime et = ExecutionTime.forCron(parser.parse("0 0 12 31 1/1 ? *"));
-        final Optional<ZonedDateTime> lastExecution = et.lastExecution(currentDateTime);
-        if (lastExecution.isPresent()) {
-            final ZonedDateTime expected = ZonedDateTime.of(LocalDateTime.of(2015, 12, 31, 12, 00), ZoneId.systemDefault());
-            Assert.assertEquals(expected, lastExecution.get());
-        } else {
-            fail(LAST_EXECUTION_NOT_PRESENT_ERROR);
-        }
+
+<<<<<<< Unknown file: This is a bug in JDime.
+=======
+    ZonedDateTime expected = ZonedDateTime.of(LocalDateTime.of(2015, 12, 31, 12, 0), ZoneId.systemDefault());
+>>>>>>> /usr/src/app/output/jmrozanec/cron-utils/219633080b459601437e67784e60a42ca6d53e10/src/test/java/com/cronutils/Issue143Test.java/right.java
+
+
+<<<<<<< Unknown file: This is a bug in JDime.
+=======
+    Assert.assertEquals(expected, last);
+>>>>>>> /usr/src/app/output/jmrozanec/cron-utils/219633080b459601437e67784e60a42ca6d53e10/src/test/java/com/cronutils/Issue143Test.java/right.java
+  }
+
+  @Test public void testCase4() {
+    final ExecutionTime et = ExecutionTime.forCron(parser.parse("0 0 12 ? 1/1 SAT#5 *"));
+    final Optional<ZonedDateTime> 
+<<<<<<< /usr/src/app/output/jmrozanec/cron-utils/219633080b459601437e67784e60a42ca6d53e10/src/test/java/com/cronutils/Issue143Test.java/left.java
+    lastExecution = et.lastExecution(currentDateTime)
+=======
+    olast = et.lastExecution(currentDateTime)
+>>>>>>> /usr/src/app/output/jmrozanec/cron-utils/219633080b459601437e67784e60a42ca6d53e10/src/test/java/com/cronutils/Issue143Test.java/right.java
+    ;
+
+<<<<<<< /usr/src/app/output/jmrozanec/cron-utils/219633080b459601437e67784e60a42ca6d53e10/src/test/java/com/cronutils/Issue143Test.java/left.java
+    if (lastExecution.isPresent()) {
+      final ZonedDateTime expected = ZonedDateTime.of(LocalDateTime.of(2016, 10, 29, 12, 00), ZoneId.systemDefault());
+      Assert.assertEquals(expected, lastExecution.get());
+    } else {
+      fail(LAST_EXECUTION_NOT_PRESENT_ERROR);
     }
+=======
+    ZonedDateTime last = olast.orElse(null);
+>>>>>>> /usr/src/app/output/jmrozanec/cron-utils/219633080b459601437e67784e60a42ca6d53e10/src/test/java/com/cronutils/Issue143Test.java/right.java
 
-    @Test
-    public void testCase4() {
-        final ExecutionTime et = ExecutionTime.forCron(parser.parse("0 0 12 ? 1/1 SAT#5 *"));
-        final Optional<ZonedDateTime> lastExecution = et.lastExecution(currentDateTime);
-        if (lastExecution.isPresent()) {
-            final ZonedDateTime expected = ZonedDateTime.of(LocalDateTime.of(2016, 10, 29, 12, 00), ZoneId.systemDefault());
-            Assert.assertEquals(expected, lastExecution.get());
-        } else {
-            fail(LAST_EXECUTION_NOT_PRESENT_ERROR);
-        }
-    }
 
+<<<<<<< Unknown file: This is a bug in JDime.
+=======
+    ZonedDateTime expected = ZonedDateTime.of(LocalDateTime.of(2016, 10, 29, 12, 0), ZoneId.systemDefault());
+>>>>>>> /usr/src/app/output/jmrozanec/cron-utils/219633080b459601437e67784e60a42ca6d53e10/src/test/java/com/cronutils/Issue143Test.java/right.java
+
+
+<<<<<<< Unknown file: This is a bug in JDime.
+=======
+    Assert.assertEquals(expected, last);
+>>>>>>> /usr/src/app/output/jmrozanec/cron-utils/219633080b459601437e67784e60a42ca6d53e10/src/test/java/com/cronutils/Issue143Test.java/right.java
+  }
 }
--- src/test/java/com/cronutils/utils/descriptor/CronDescriptorCron4jIntegrationZhTest.java
+++ src/test/java/com/cronutils/utils/descriptor/CronDescriptorCron4jIntegrationZhTest.java
@@ -1,28 +1,11 @@
-/*
- * Copyright 2015 jmrozanec
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.cronutils.utils.descriptor;
-
 import java.util.Locale;
-
 import org.junit.Before;
 import org.junit.Test;
-
 import com.cronutils.descriptor.CronDescriptor;
 import com.cronutils.model.CronType;
 import com.cronutils.model.definition.CronDefinitionBuilder;
 import com.cronutils.parser.CronParser;
-
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -31,36 +14,31 @@ import static org.junit.Assert.assertEquals;
  * add chinese local test
  */
 public class CronDescriptorCron4jIntegrationZhTest {
-    private CronDescriptor descriptor;
-    private CronParser parser;
+  private CronDescriptor descriptor;
 
-    @Before
-    public void setUp() {
-        descriptor = CronDescriptor.instance(Locale.CHINESE);
-        parser = new CronParser(CronDefinitionBuilder.instanceDefinitionFor(CronType.CRON4J));
-    }
+  private CronParser parser;
 
-    @Test
-    public void testEveryMinuteBetween1100And1110() {
-        assertEquals("每 分钟 在 11:00 和 11:10", descriptor.describe(parser.parse("0-10 11 * * *")));
-    }
+  @Before public void setUp() {
+    descriptor = CronDescriptor.instance(Locale.CHINESE);
+    parser = new CronParser(CronDefinitionBuilder.instanceDefinitionFor(CronType.CRON4J));
+  }
 
-    @Test
-    public void testEveryMinute() {
-        assertEquals("每 分钟", descriptor.describe(parser.parse("* * * * *")));
-        assertEquals("每 分钟", descriptor.describe(parser.parse("*/1 * * * *")));
-        assertEquals("每 分钟", descriptor.describe(parser.parse("0/1 * * * *")));
-    }
+  @Test public void testEveryMinuteBetween1100And1110() {
+    assertEquals("\u6bcf \u5206\u949f \u5728 11:00 \u548c 11:10", descriptor.describe(parser.parse("0-10 11 * * *")));
+  }
 
-    @Test
-    public void testEveryFiveMinutes() {
-        assertEquals("每 5 分钟", descriptor.describe(parser.parse("*/5 * * * *")));
-        assertEquals("每 5 分钟", descriptor.describe(parser.parse("0/5 * * * *")));
-    }
+  @Test public void testEveryMinute() {
+    assertEquals("\u6bcf \u5206\u949f", descriptor.describe(parser.parse("* * * * *")));
+    assertEquals("\u6bcf \u5206\u949f", descriptor.describe(parser.parse("*/1 * * * *")));
+    assertEquals("\u6bcf \u5206\u949f", descriptor.describe(parser.parse("0/1 * * * *")));
+  }
 
-    @Test
-    public void testAtElevenThirty() {
-        assertEquals("在 11:30", descriptor.describe(parser.parse("30 11 * * *")));
-    }
+  @Test public void testEveryFiveMinutes() {
+    assertEquals("\u6bcf 5 \u5206\u949f", descriptor.describe(parser.parse("*/5 * * * *")));
+    assertEquals("\u6bcf 5 \u5206\u949f", descriptor.describe(parser.parse("0/5 * * * *")));
+  }
 
+  @Test public void testAtElevenThirty() {
+    assertEquals("\u5728 11:30", descriptor.describe(parser.parse("30 11 * * *")));
+  }
 }
