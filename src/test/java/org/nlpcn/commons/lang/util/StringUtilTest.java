c63b53096c7da602f6772d460f59dacbc921ed37
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 22:50:39 +0000

    Mining Framework Analysis

--- src/test/java/org/nlpcn/commons/lang/util/StringUtilTest.java
+++ src/test/java/org/nlpcn/commons/lang/util/StringUtilTest.java
@@ -1,22 +1,7 @@
-package org.nlpcn.commons.lang.util;
-
-import org.junit.Test;
-
-public class StringUtilTest {
-
-	@Test
-	public void test() {
-		//顺路介绍下 字符串是否为空的判断
-		System.out.println(StringUtil.isBlank(" \t")); // result --> true
-		
-		//html清理
-		System.out.println(StringUtil.rmHtmlTag("<a>hello ansj</a>my name is ")); //result -->hello ansj
-		
-		System.out.println(StringUtil.rmHtmlTag("hello ansj hello kk "));
-		
-		 //将用都好隔开的字符转换为sql中的in查询
-		System.out.println(StringUtil.makeSqlInString("ansj,2134,123,123,123"));
-		//result --> 'ansj','2134','123','123','123'
-	}
-
-}
+  package     org . nlpcn . commons . lang . util ;   import   org . junit . Test ;   public class StringUtilTest  {    @ Test public void test  ( )  {    System . out . println  (  StringUtil . isBlank  ( " \t" ) ) ;    System . out . println  (  StringUtil . rmHtmlTag  ( 
+<<<<<<<
+"<a>hello ansj</a>my name is "
+=======
+"hello ansj hello kk "
+>>>>>>>
+ ) ) ;    System . out . println  (  StringUtil . makeSqlInString  ( "ansj,2134,123,123,123" ) ) ; } }
