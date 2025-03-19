205cb0b95d0ae9f50dc1e02f937c4966c8f12e5f
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-19 13:14:32 +0000

    Mining Framework Analysis

--- src/test/java/io/github/benas/easyproperties/processors/DBPropertyAnnotationProcessorTest.java
+++ src/test/java/io/github/benas/easyproperties/processors/DBPropertyAnnotationProcessorTest.java
@@ -1,5 +1,4 @@
 package io.github.benas.easyproperties.processors;
-
 import io.github.benas.easyproperties.annotations.DBProperty;
 import io.github.benas.easyproperties.api.PropertyInjectionException;
 import org.junit.After;
@@ -7,87 +6,70 @@ import org.junit.Before;
 import org.junit.Test;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class DBPropertyAnnotationProcessorTest extends AbstractAnnotationProcessorTest {
+  private EmbeddedDatabase embeddedDatabase;
 
-    private EmbeddedDatabase embeddedDatabase;
+  @Before public void setUp() throws Exception {
+    super.setUp();
+    embeddedDatabase = new EmbeddedDatabaseBuilder().setName("test").addScript("database.sql").build();
+  }
 
-    @Before
-    public void setUp() throws Exception {
-        super.setUp();
-        embeddedDatabase = new EmbeddedDatabaseBuilder()
-                .setName("test")
-                .addScript("database.sql")
-                .build();
+  @Test public void testPropertyInjectionFromDatabase() {
+    Bean bean = new Bean();
+    propertiesInjector.injectProperties(bean);
+    assertThat(bean.getName()).isEqualTo("Foo");
+  }
+
+  @Test(expected = PropertyInjectionException.class) public void whenConfigurationIsMissing_thenShouldThrowAnException() throws Exception {
+    BeanWithInvalidConfiguration bean = new BeanWithInvalidConfiguration();
+    propertiesInjector.injectProperties(bean);
+  }
+
+  @Test public void whenKeyIsMissing_thenShouldSilentlyIgnoreTheField() {
+    BeanWithInvalidKey bean = new BeanWithInvalidKey();
+    propertiesInjector.injectProperties(bean);
+    assertThat(bean.getName()).isNull();
+  }
+
+  @After public void shutdownEmbeddedDatabase() throws Exception {
+    embeddedDatabase.shutdown();
+  }
+
+  public class Bean {
+    @DBProperty(configuration = "database.properties", key = "name") private String name;
+
+    public String getName() {
+      return name;
     }
 
-    @Test
-    public void testPropertyInjectionFromDatabase() {
-        //given
-        Bean bean = new Bean();
+    public void setName(String name) {
+      this.name = name;
+    }
+  }
 
-        //when
-        propertiesInjector.injectProperties(bean);
+  public class BeanWithInvalidConfiguration {
+    @DBProperty(configuration = "blah.properties", key = "name") private String name;
 
-        //then
-        assertThat(bean.getName()).isEqualTo("Foo");
+    public String getName() {
+      return name;
     }
 
-    @Test(expected = PropertyInjectionException.class)
-    public void whenConfigurationIsMissing_thenShouldThrowAnException() throws Exception {
-        //given
-        BeanWithInvalidConfiguration bean = new BeanWithInvalidConfiguration();
+    public void setName(String name) {
+      this.name = name;
+    }
+  }
 
-        //when
-        propertiesInjector.injectProperties(bean);
+  public class BeanWithInvalidKey {
+    @DBProperty(configuration = "database.properties", key = "blah") private String name;
 
-        //then should throw exception
+    public String getName() {
+      return name;
     }
 
-    @Test
-    public void whenKeyIsMissing_thenShouldSilentlyIgnoreTheField() {
-        //given
-        BeanWithInvalidKey bean = new BeanWithInvalidKey();
-
-        //when
-        propertiesInjector.injectProperties(bean);
-
-        //then
-        assertThat(bean.getName()).isNull();
+    public void setName(String name) {
+      this.name = name;
     }
-
-    @After
-    public void shutdownEmbeddedDatabase() throws Exception {
-        embeddedDatabase.shutdown();
-    }
-
-    public class Bean {
-
-        @DBProperty(configuration = "database.properties", key = "name")
-        private String name;
-
-        public String getName() { return name; }
-        public void setName(String name) { this.name = name; }
-    }
-
-    public class BeanWithInvalidConfiguration {
-
-        @DBProperty(configuration = "blah.properties", key = "name")
-        private String name;
-
-        public String getName() { return name; }
-        public void setName(String name) { this.name = name; }
-    }
-
-    public class BeanWithInvalidKey {
-
-        @DBProperty(configuration = "database.properties", key = "blah")
-        private String name;
-
-        public String getName() { return name; }
-        public void setName(String name) { this.name = name; }
-    }
-
+  }
 }
