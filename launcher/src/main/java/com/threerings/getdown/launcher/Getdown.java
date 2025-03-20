f6d867ea2231348419e8895f4095201aa4699e90
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 06:07:03 +0000

    Mining Framework Analysis

--- launcher/src/main/java/com/threerings/getdown/launcher/Getdown.java
+++ launcher/src/main/java/com/threerings/getdown/launcher/Getdown.java
@@ -311,8 +311,9 @@ public abstract class Getdown extends Thread implements Application.StatusDispla
   {
     boolean proxySettingsDetected = false;
     // we may already have a proxy configured
-        if (System.getProperty("http.proxyHost") != null ||
-            System.getProperty("https.proxyHost") != null) {
+    if (System.getProperty("http.proxyHost") != null ||
+        System.getProperty("https.proxyHost") != null)
+    {
       proxySettingsDetected = true;
     }
 
@@ -893,10 +894,11 @@ public abstract class Getdown extends Thread implements Application.StatusDispla
       {
         ProgressObserver pobs = pragg.startElement(ii++);
         try
-                    // install the patch file (renaming them from _new)
-                    prsrc.install(false);
-                    // now apply the patch
-          Patcher patcher = new Patcher();
+        {
+            // install the patch file (renaming them from _new)
+            prsrc.install(false);
+            // now apply the patch
+            Patcher patcher = new Patcher();
           patcher.patch(prsrc.getLocal().getParentFile(), prsrc.getLocal(), pobs);
         }
         catch (Exception e)
