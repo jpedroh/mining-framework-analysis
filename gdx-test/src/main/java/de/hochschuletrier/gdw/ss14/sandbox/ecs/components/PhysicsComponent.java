8a4f498cfa46d6f9033fb24a0f2c62e279c6fada
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 00:15:44 +0000

    Mining Framework Analysis

--- gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/sandbox/ecs/components/PhysicsComponent.java
+++ gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/sandbox/ecs/components/PhysicsComponent.java
@@ -1,19 +1,12 @@
 package de.hochschuletrier.gdw.ss14.sandbox.ecs.components;
-
 import de.hochschuletrier.gdw.commons.gdx.physix.*;
 
 /**
  * Created by Dani on 29.09.2014.
  */
+public class PhysicsComponent extends PhysixEntity implements Component {
+  public PhysixBody physicsBody;
 
-// use this class as parent for other physicsComponents (e.g. CatPhysicsComponent, DogPhysicsComponent, ...)
-public class PhysicsComponent extends PhysixEntity implements Component
-{
-    public PhysixBody physicsBody;
-
-    @Override
-    public void initPhysics(PhysixManager manager)
-    {
-    	
-    }
+  @Override public void initPhysics(PhysixManager manager) {
+  }
 }
