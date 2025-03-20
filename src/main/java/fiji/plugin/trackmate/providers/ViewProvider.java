a2574c2ccb81615f8395073d33f682c758c50477
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 02:14:45 +0000

    Mining Framework Analysis

--- src/main/java/fiji/plugin/trackmate/providers/ViewProvider.java
+++ src/main/java/fiji/plugin/trackmate/providers/ViewProvider.java
@@ -1,18 +1,6 @@
-package fiji.plugin.trackmate.providers;
-
-import fiji.plugin.trackmate.visualization.ViewFactory;
-
-public class ViewProvider extends AbstractProvider< ViewFactory >
-{
-
-	public ViewProvider()
-	{
-		super( ViewFactory.class );
-	}
-
-	public static void main( final String[] args )
-	{
-		final ViewProvider provider = new ViewProvider();
-		System.out.println( provider.echo() );
-	}
-}
+  package    fiji . plugin . trackmate . providers ;   import     fiji . plugin . trackmate . visualization . ViewFactory ;   public class ViewProvider  extends  AbstractProvider  < ViewFactory >  {   public ViewProvider  ( )  {  super  (  ViewFactory . class ) ; }   public static void main  (   final  String  [ ] args )  {   final ViewProvider  provider =  new ViewProvider  ( ) ;    System . out . println  (  provider . echo  ( ) ) ; } 
+<<<<<<<
+=======
+  protected void registerViews  ( )  {   final Context  context =  TMUtils . getContext  ( ) ;   final LogService  log =  context . getService  (  LogService . class ) ;   final PluginService  pluginService =  context . getService  (  PluginService . class ) ;   final  List  <  PluginInfo  < ViewFactory > >  infos =  pluginService . getPluginsOfType  (  ViewFactory . class ) ;  for (  final  PluginInfo  < ViewFactory > info : infos )  {  try  {   final ViewFactory  view =  info . createInstance  ( ) ;   registerView  (  view . getKey  ( ) , view ,  info . isVisible  ( ) ) ; }  catch (   final  InstantiableException e )  {   log . error  (  "Could not instantiate " +  info . getClassName  ( ) , e ) ; } } }
+>>>>>>>
+ }
