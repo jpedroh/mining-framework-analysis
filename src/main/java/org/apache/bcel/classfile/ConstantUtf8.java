47abdc48d352999bac7bc9f7da687cff17f00ec0
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 05:34:30 +0000

    Mining Framework Analysis

--- src/main/java/org/apache/bcel/classfile/ConstantUtf8.java
+++ src/main/java/org/apache/bcel/classfile/ConstantUtf8.java
@@ -1,29 +1,10 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- *
- */
 package org.apache.bcel.classfile;
-
 import java.io.DataInput;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
-
 import org.apache.bcel.Const;
 
 /**
@@ -46,218 +27,198 @@ import org.apache.bcel.Const;
  *   is {@code 200}. </dd>
  * </dl>
  *
+ * @version $Id$
  * @see     Constant
  */
 public final class ConstantUtf8 extends Constant {
+  private final String bytes;
 
-    private final String bytes;
+  private static volatile int considered = 0;
 
-    // TODO these should perhaps be AtomicInt?
-    private static volatile int considered = 0;
-    private static volatile int hits = 0;
-    private static volatile int skipped = 0;
-    private static volatile int created = 0;
+  private static volatile int hits = 0;
 
-    // If this system property is more than zero, instances are reused for the same value.
-    // By default, the instances are not reused to avoid performance degradation (BCEL-186).
-    static final String MAX_CACHED_SIZE_KEY = "bcel.constant.cache.max.size";
-    // The maximum length of cached ConstantUtf8 string. Default: 200.
-    static final String MAX_CACHED_ENTRY_LENGTH_KEY = "bcel.constant.cache.entry.max.length";
+  private static volatile int skipped = 0;
 
-    private static final CachingGenerator cachingGenerator = new CachingGenerator();
-    private static final DefaultGenerator defaultGenerator = new DefaultGenerator();
+  private static volatile int created = 0;
 
-    private static final Generator generator = selectGenerator();
+  static final String MAX_CACHED_SIZE_KEY = "bcel.constant.cache.max.size";
 
-    static Generator selectGenerator() {
+  static final String MAX_CACHED_ENTRY_LENGTH_KEY = "bcel.constant.cache.entry.max.length";
 
-        // Not static so that unit test can change the value before instantiating them
-        int maxCachedSize =  Integer.getInteger(MAX_CACHED_SIZE_KEY, 0);
-        return maxCachedSize > 0 ? cachingGenerator : defaultGenerator;
+  private static final CachingGenerator cachingGenerator = new CachingGenerator();
+
+  private static final DefaultGenerator defaultGenerator = new DefaultGenerator();
+
+  private static final Generator generator = selectGenerator();
+
+  static Generator selectGenerator() {
+    int maxCachedSize = Integer.getInteger(MAX_CACHED_SIZE_KEY, 0);
+    return maxCachedSize > 0 ? cachingGenerator : defaultGenerator;
+  }
+
+  private static final boolean BCEL_STATISTICS = Boolean.getBoolean("bcel.statistics");
+
+  interface Generator {
+    ConstantUtf8 getInstance(String s);
+  }
+
+  static final class DefaultGenerator implements Generator {
+    @Override public ConstantUtf8 getInstance(String s) {
+      return new ConstantUtf8(s);
+    }
+  }
+
+  private static final class CachingGenerator implements Generator {
+    private final int maxCacheEntries = Integer.getInteger(MAX_CACHED_SIZE_KEY, 20000);
+
+    private final int maxCachedSize = Integer.getInteger(MAX_CACHED_ENTRY_LENGTH_KEY, 200);
+
+    private final HashMap<String, ConstantUtf8> cache;
+
+    private CachingGenerator() {
+      int initialCacheCapacity = (int) (maxCacheEntries / 0.75);
+      if (maxCacheEntries == -1) {
+        cache = new HashMap<>(initialCacheCapacity);
+      } else {
+        cache = new LinkedHashMap<String, ConstantUtf8>(initialCacheCapacity, 0.75f, true) {
+          @Override protected boolean removeEldestEntry(final Map.Entry<String, ConstantUtf8> eldest) {
+            return size() > maxCacheEntries;
+          }
+        };
+      }
     }
 
-    private static final boolean BCEL_STATISTICS = Boolean.getBoolean("bcel.statistics");
-
-    interface Generator {
-        ConstantUtf8 getInstance(String s);
-    }
-
-    static final class DefaultGenerator implements Generator {
-        @Override
-        public ConstantUtf8 getInstance(String s) {
-            return new ConstantUtf8(s);
+    public ConstantUtf8 getInstance(final String s) {
+      if (s.length() > maxCachedSize) {
+        skipped++;
+        return new ConstantUtf8(s);
+      }
+      considered++;
+      synchronized (cache) {
+        ConstantUtf8 result = cache.get(s);
+        if (result != null) {
+          hits++;
+          return result;
         }
+        result = new ConstantUtf8(s);
+        cache.put(s, result);
+        return result;
+      }
     }
+  }
 
-    private static final class CachingGenerator implements Generator {
-        // Not static so that unit test can change the value before instantiating them
-        private final int maxCacheEntries =
-                Integer.getInteger(MAX_CACHED_SIZE_KEY, 20000);// CHECKSTYLE IGNORE MagicNumber
+  static void printStats() {
+    System.err.println("Cache hit " + hits + "/" + considered + ", " + skipped + " skipped");
+    System.err.println("Total of " + created + " ConstantUtf8 objects created");
+  }
 
-        private final int maxCachedSize = Integer.getInteger(MAX_CACHED_ENTRY_LENGTH_KEY, 200);
+  static void clearStats() {
+    hits = considered = skipped = created = 0;
+  }
 
-        private final HashMap<String, ConstantUtf8> cache;
-
-        private CachingGenerator() {
-            int initialCacheCapacity = (int)(maxCacheEntries /0.75);
-            if (maxCacheEntries == -1) {
-                // No capacity limit
-                cache = new HashMap<>(initialCacheCapacity);
-            } else {
-                cache = new LinkedHashMap<String, ConstantUtf8>(initialCacheCapacity, 0.75f, true) {
-                    @Override
-                    protected boolean removeEldestEntry(final Map.Entry<String, ConstantUtf8> eldest) {
-                        return size() > maxCacheEntries;
-                    }
-                };
-            }
+  static {
+    if (BCEL_STATISTICS) {
+      Runtime.getRuntime().addShutdownHook(new Thread() {
+        @Override public void run() {
+          printStats();
         }
-
-        public ConstantUtf8 getInstance(final String s) {
-            if (s.length() > maxCachedSize) {
-                skipped++;
-                return  new ConstantUtf8(s);
-            }
-            considered++;
-            synchronized (cache) {
-                ConstantUtf8 result = cache.get(s);
-                if (result != null) {
-                    hits++;
-                    return result;
-                }
-                result = new ConstantUtf8(s);
-                cache.put(s, result);
-                return result;
-            }
-        }
+      });
     }
+  }
 
-    // for accesss by test code
-    static void printStats() {
-        System.err.println("Cache hit " + hits + "/" + considered +", " + skipped + " skipped");
-        System.err.println("Total of " + created + " ConstantUtf8 objects created");
-    }
-
-    // for accesss by test code
-    static void clearStats() {
-        hits = considered = skipped = created = 0;
-    }
-
-    static {
-        if (BCEL_STATISTICS) {
-            Runtime.getRuntime().addShutdownHook(new Thread() {
-                @Override
-                public void run() {
-                    printStats();
-                }
-            });
-        }
-    }
-
-    /**
+  /**
      * @since 6.0
      */
-    public static ConstantUtf8 getCachedInstance(final String s) {
-        return cachingGenerator.getInstance(s);
-    }
+  public static ConstantUtf8 getCachedInstance(final String s) {
+    return cachingGenerator.getInstance(s);
+  }
 
-    /**
+  /**
      * @since 6.0
      */
-    public static ConstantUtf8 getInstance(final String s) {
-        return generator.getInstance(s);
-    }
+  public static ConstantUtf8 getInstance(final String s) {
+    return generator.getInstance(s);
+  }
 
-    /**
+  /**
      * @since 6.0
      */
-    public static ConstantUtf8 getInstance(final DataInput input)  throws IOException {
-        return getInstance(input.readUTF());
-    }
+  public static ConstantUtf8 getInstance(final DataInput input) throws IOException {
+    return getInstance(input.readUTF());
+  }
 
-    /**
+  /**
      * Initialize from another object.
      */
-    public ConstantUtf8(final ConstantUtf8 c) {
-        this(c.getBytes());
-    }
+  public ConstantUtf8(final ConstantUtf8 c) {
+    this(c.getBytes());
+  }
 
-
-    /**
+  /**
      * Initialize instance from file data.
      *
      * @param file Input stream
      * @throws IOException
      */
-    ConstantUtf8(final DataInput file) throws IOException {
-        super(Const.CONSTANT_Utf8);
-        bytes = file.readUTF();
-        created++;
-    }
+  ConstantUtf8(final DataInput file) throws IOException {
+    super(Const.CONSTANT_Utf8);
+    bytes = file.readUTF();
+    created++;
+  }
 
-
-    /**
+  /**
      * @param bytes Data
      */
-    public ConstantUtf8(final String bytes) {
-        super(Const.CONSTANT_Utf8);
-        if (bytes == null) {
-            throw new IllegalArgumentException("bytes must not be null!");
-        }
-        this.bytes = bytes;
-        created++;
+  public ConstantUtf8(final String bytes) {
+    super(Const.CONSTANT_Utf8);
+    if (bytes == null) {
+      throw new IllegalArgumentException("bytes must not be null!");
     }
+    this.bytes = bytes;
+    created++;
+  }
 
-
-    /**
+  /**
      * Called by objects that are traversing the nodes of the tree implicitely
      * defined by the contents of a Java class. I.e., the hierarchy of methods,
      * fields, attributes, etc. spawns a tree of objects.
      *
      * @param v Visitor object
      */
-    @Override
-    public void accept( final Visitor v ) {
-        v.visitConstantUtf8(this);
-    }
+  @Override public void accept(final Visitor v) {
+    v.visitConstantUtf8(this);
+  }
 
-
-    /**
+  /**
      * Dump String in Utf8 format to file stream.
      *
      * @param file Output file stream
      * @throws IOException
      */
-    @Override
-    public final void dump( final DataOutputStream file ) throws IOException {
-        file.writeByte(super.getTag());
-        file.writeUTF(bytes);
-    }
+  @Override public final void dump(final DataOutputStream file) throws IOException {
+    file.writeByte(super.getTag());
+    file.writeUTF(bytes);
+  }
 
-
-    /**
+  /**
      * @return Data converted to string.
      */
-    public final String getBytes() {
-        return bytes;
-    }
+  public final String getBytes() {
+    return bytes;
+  }
 
-
-    /**
+  /**
      * @param bytes the raw bytes of this Utf-8
      * @deprecated (since 6.0)
      */
-    @java.lang.Deprecated
-    public final void setBytes( final String bytes ) {
-        throw new UnsupportedOperationException();
-    }
+  @java.lang.Deprecated public final void setBytes(final String bytes) {
+    throw new UnsupportedOperationException();
+  }
 
-
-    /**
+  /**
      * @return String representation
      */
-    @Override
-    public final String toString() {
-        return super.toString() + "(\"" + Utility.replace(bytes, "\n", "\\n") + "\")";
-    }
+  @Override public final String toString() {
+    return super.toString() + "(\"" + Utility.replace(bytes, "\n", "\\n") + "\")";
+  }
 }
