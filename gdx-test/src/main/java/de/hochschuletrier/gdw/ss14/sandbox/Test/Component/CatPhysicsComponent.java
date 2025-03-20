8c548299d7898736258592635524edc1a1abc0e8
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 00:15:12 +0000

    Mining Framework Analysis

--- gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/sandbox/Test/Component/CatPhysicsComponent.java
+++ gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/sandbox/Test/Component/CatPhysicsComponent.java
@@ -1,68 +1,74 @@
 package de.hochschuletrier.gdw.ss14.sandbox.Test.Component;
-
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 import com.badlogic.gdx.physics.box2d.Fixture;
-
+import com.badlogic.gdx.physics.box2d.FixtureDef;
 import de.hochschuletrier.gdw.commons.gdx.physix.PhysixBody;
 import de.hochschuletrier.gdw.commons.gdx.physix.PhysixBodyDef;
 import de.hochschuletrier.gdw.commons.gdx.physix.PhysixFixtureDef;
 import de.hochschuletrier.gdw.commons.gdx.physix.PhysixManager;
 import de.hochschuletrier.gdw.ss14.sandbox.ecs.components.PhysicsComponent;
 
-public class CatPhysicsComponent extends PhysicsComponent{
-	
-	// TODO: Für Physik Team
-	
-	public Vector2 			mPosition;
-	public float 			mWidth;
-	public float 			mHeight;
-	public float 			mFriction;
-	public float 			mRotation;
-	public float			mRestitution;
-	
-	private Fixture 	[]	mFixtures;
-	private PhysixBody		mBody;
-	
-	/**
-	 * 
-	 * @param position 		central position of the object
-	 * @param width			the width of the object
-	 * @param height		the height of the object
-	 * @param rotation		the rotation in radians [0 .. 2*PI]
-	 * @param friction  	the friction of the object 
-	 * @param restitution	the restitution (elastitcy)
-	 */
-	public CatPhysicsComponent(Vector2 position, float width, float height, float rotation, float friciton, float restitutioin) {
-		mPosition = position;
-		mWidth = width;
-		mHeight = height;
-		mRotation = rotation;
-		mFriction = friciton;
-		mRestitution = restitutioin;
-		
-		mFixtures = new Fixture[3];
-	}
-	
-	public CatPhysicsComponent(){
-		this(new Vector2(0,0), 100f, 100f, 0f, 1f, 0f);
-	}
-	
-	@Override
-    public void initPhysics(PhysixManager manager){
-		
-		PhysixFixtureDef fixturedef = new PhysixFixtureDef(manager).density(1)
-				.friction(mFriction).restitution(mRestitution);
-		
-		mBody = new PhysixBodyDef(BodyType.DynamicBody, manager).position(mPosition)
-				.fixedRotation(true).angle(mRotation)
-				.create();
-		
-		mFixtures[0] = mBody.createFixture(fixturedef.shapeBox(mWidth, mHeight-mWidth));
-		mFixtures[1] = mBody.createFixture(fixturedef.shapeCircle(mWidth, new Vector2(mPosition.x + mHeight-mWidth, mPosition.y) ));
-		mFixtures[2] = mBody.createFixture(fixturedef.shapeCircle(mWidth, new Vector2(mPosition.x - mHeight-mWidth, mPosition.y) ));
-		
-	}
+public class CatPhysicsComponent extends PhysicsComponent {
+  public Vector2 mPosition;
 
-	
+  public float mWidth;
+
+  public float mHeight;
+
+  public float mFriction;
+
+  public float mRotation;
+
+  public float mRestitution;
+
+  private Fixture[] mFixtures;
+
+  private PhysixBody mBody;
+
+  public CatPhysicsComponent(Vector2 position, float width, float height, float rotation, float friciton, float restitutioin) {
+    mPosition = position;
+    mWidth = width;
+    mHeight = height;
+    mRotation = rotation;
+    mFriction = friciton;
+    mRestitution = restitutioin;
+    mFixtures = new Fixture[3];
+  }
+
+  public CatPhysicsComponent() {
+    this(new Vector2(0, 0), 100f, 100f, 0f, 1f, 0f);
+  }
+
+  @Override public void initPhysics(PhysixManager manager) {
+
+<<<<<<< /usr/src/app/output/lusito/gamedevweek/b9d0b595eaae93747ce9081072973fa34c8b8764/gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/sandbox/Test/Component/CatPhysicsComponent.java/left.java
+    super.initPhysics(manager);
+=======
+    PhysixFixtureDef fixturedef = new PhysixFixtureDef(manager).density(1).friction(mFriction).restitution(mRestitution);
+>>>>>>> /usr/src/app/output/lusito/gamedevweek/b9d0b595eaae93747ce9081072973fa34c8b8764/gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/sandbox/Test/Component/CatPhysicsComponent.java/right.java
+
+
+<<<<<<< /usr/src/app/output/lusito/gamedevweek/b9d0b595eaae93747ce9081072973fa34c8b8764/gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/sandbox/Test/Component/CatPhysicsComponent.java/left.java
+    this.physicsBody = new PhysixBodyDef(BodyDef.BodyType.DynamicBody, manager).position(new Vector2(0, 0)).fixedRotation(false).create()
+=======
+    mBody = new PhysixBodyDef(BodyType.DynamicBody, manager).position(mPosition).fixedRotation(true).angle(mRotation).create()
+>>>>>>> /usr/src/app/output/lusito/gamedevweek/b9d0b595eaae93747ce9081072973fa34c8b8764/gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/sandbox/Test/Component/CatPhysicsComponent.java/right.java
+    ;
+    mFixtures[0] = mBody.createFixture(fixturedef.shapeBox(mWidth, mHeight - mWidth));
+
+<<<<<<< /usr/src/app/output/lusito/gamedevweek/b9d0b595eaae93747ce9081072973fa34c8b8764/gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/sandbox/Test/Component/CatPhysicsComponent.java/left.java
+    physicsBody.createFixture(new PhysixFixtureDef(manager).density(5).friction(0.2f).restitution(0.4f).shapeCircle(50));
+=======
+    mFixtures[1] = mBody.createFixture(fixturedef.shapeCircle(mWidth, new Vector2(mPosition.x + mHeight - mWidth, mPosition.y)));
+>>>>>>> /usr/src/app/output/lusito/gamedevweek/b9d0b595eaae93747ce9081072973fa34c8b8764/gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/sandbox/Test/Component/CatPhysicsComponent.java/right.java
+
+
+<<<<<<< /usr/src/app/output/lusito/gamedevweek/b9d0b595eaae93747ce9081072973fa34c8b8764/gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/sandbox/Test/Component/CatPhysicsComponent.java/left.java
+    setPhysicsBody(physicsBody);
+=======
+    mFixtures[2] = mBody.createFixture(fixturedef.shapeCircle(mWidth, new Vector2(mPosition.x - mHeight - mWidth, mPosition.y)));
+>>>>>>> /usr/src/app/output/lusito/gamedevweek/b9d0b595eaae93747ce9081072973fa34c8b8764/gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/sandbox/Test/Component/CatPhysicsComponent.java/right.java
+  }
 }
--- gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/sandbox/Test/Entity/EntityFactory.java
+++ gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/sandbox/Test/Entity/EntityFactory.java
@@ -1,135 +1,100 @@
 package de.hochschuletrier.gdw.ss14.sandbox.Test.Entity;
-
 import com.badlogic.gdx.math.Vector2;
-
 import de.hochschuletrier.gdw.commons.gdx.assets.AssetManagerX;
 import de.hochschuletrier.gdw.commons.gdx.physix.PhysixManager;
 import de.hochschuletrier.gdw.ss14.sandbox.Test.Component.CatPhysicsComponent;
+import de.hochschuletrier.gdw.ss14.sandbox.Test.Component.CatPropertyComponent;
 import de.hochschuletrier.gdw.ss14.sandbox.Test.Component.DogPhysicsComponent;
 import de.hochschuletrier.gdw.ss14.sandbox.Test.Component.HolePhysicsComponent;
 import de.hochschuletrier.gdw.ss14.sandbox.Test.Component.MovementComponent;
 import de.hochschuletrier.gdw.ss14.sandbox.Test.Component.PositionComponent;
 import de.hochschuletrier.gdw.ss14.sandbox.ecs.EntityManager;
+import de.hochschuletrier.gdw.ss14.sandbox.ecs.components.PhysicsComponent;
 
 public class EntityFactory {
-	
-	public static EntityManager manager;
-	public static PhysixManager phyManager;
-	public static AssetManagerX assetManager;
-	
-	public EntityFactory(EntityManager manager, PhysixManager phyManager, AssetManagerX assetManager){
-		this.manager = manager;
-		this.phyManager = phyManager;
-		this.assetManager = assetManager;
-	}
-	
-	public static void constructCat(Vector2 pos, float maxVelocity, float middleVelocity, float minVelocity, float acceleration){
-		int entity = manager.createEntity();
-	    CatPhysicsComponent catPhysix = new CatPhysicsComponent();
-	    //PositionComponent catPosition = new PositionComponent(new Vector2((int)pos.x,(int)pos.y));
-	    MovementComponent catMove = new MovementComponent(maxVelocity,middleVelocity,minVelocity,acceleration,new Vector2(1,1));
-	   //	PhysicsComponent catPhysix = new PhysicsComponent();
-	    catPhysix.initPhysics(phyManager);
-	    catPhysix.physicsBody.setX(pos.x);
-	    catPhysix.physicsBody.setY(pos.y);
-	    catPhysix.physicsBody.setLinearVelocity(catMove.velocity, catMove.velocity);
-	    manager.addComponent(entity, catPhysix);
-	    manager.addComponent(entity, catMove);
-	}
-	
-	public static void constructDog(Vector2 pos, float maxVelocity, float middleVelocity, float minVelocity, float acceleration){
-		int entity = manager.createEntity();
-		DogPhysicsComponent dogPhysix = new DogPhysicsComponent();
-		PositionComponent dogPosition = new PositionComponent(new Vector2((int)pos.x,(int)pos.y));
-	    MovementComponent dogMove = new MovementComponent(maxVelocity,middleVelocity,minVelocity,acceleration,new Vector2(0,0));
-		dogPhysix.initPhysics(phyManager);
-	    manager.addComponent(entity, dogPhysix);
-	    manager.addComponent(entity, dogPosition);
-	    manager.addComponent(entity, dogMove);
-	}
-	
-	public static void constructHole(Vector2 pos){
-		int entity = manager.createEntity();
-		HolePhysicsComponent holePhysix = new HolePhysicsComponent();
-		PositionComponent holePosition = new PositionComponent(new Vector2((int)pos.x,(int)pos.y));
-		holePhysix.initPhysics(phyManager);
-	    manager.addComponent(entity, holePhysix);
-	    manager.addComponent(entity, holePosition);
-	}
-	
-//	public static void constructDog(Vector2 pos, float maxVelocity, float middleVelocity, float minVelocity, float acceleration){
-//		int entity = manager.createEntity();
-//		DogPhysicsComponent dogPhysix = new DogPhysicsComponent();
-//		PositionComponent dogPosition = new PositionComponent(new Vector2((int)pos.x,(int)pos.y));
-//	    MovementComponent dogMove = new MovementComponent(maxVelocity,middleVelocity,minVelocity,acceleration,new Vector2(0,0));
-//		dogPhysix.initPhysics(phyManager);
-//	    manager.addComponent(entity, dogPhysix);
-//	    manager.addComponent(entity, dogPosition);
-//	    manager.addComponent(entity, dogMove);
-//	}
-	
-//=======
-//    public static void constructCat(Vector2 pos, float maxVelocity,
-//            float middleVelocity, float minVelocity, float acceleration) {
-//        final int entity = manager.createEntity();
-//        final CatPhysicsComponent catPhysix = new CatPhysicsComponent();
-//        final PositionComponent catPosition = new PositionComponent(
-//                new Vector2((int) pos.x, (int) pos.y));
-//        final MovementComponent catMove = new MovementComponent(maxVelocity,
-//                middleVelocity, minVelocity, acceleration, new Vector2(0, 0));
-//        final CatPropertyComponent catProperty = new CatPropertyComponent();
-//        catPhysix.initPhysics(phyManager);
-//        manager.addComponent(entity, catProperty);
-//        manager.addComponent(entity, catPhysix);
-//        manager.addComponent(entity, catPosition);
-//        manager.addComponent(entity, catMove);
-//    }
-//>>>>>>> eb05791cc2829b2ac4d6cc36dedcf0604c658cf3
-//
-//    public static void constructDog(Vector2 pos, float maxVelocity,
-//            float middleVelocity, float minVelocity, float acceleration) {
-//        final int entity = manager.createEntity();
-//        final DogPhysicsComponent dogPhysix = new DogPhysicsComponent();
-//        final PositionComponent dogPosition = new PositionComponent(
-//                new Vector2((int) pos.x, (int) pos.y));
-//        final MovementComponent dogMove = new MovementComponent(maxVelocity,
-//                middleVelocity, minVelocity, acceleration, new Vector2(0, 0));
-//        dogPhysix.initPhysics(phyManager);
-//        manager.addComponent(entity, dogPhysix);
-//        manager.addComponent(entity, dogPosition);
-//        manager.addComponent(entity, dogMove);
-//    }
-//
-//    public static void constructHole(Vector2 pos) {
-//        final int entity = manager.createEntity();
-//        final HolePhysicsComponent holePhysix = new HolePhysicsComponent();
-//        final PositionComponent holePosition = new PositionComponent(
-//                new Vector2((int) pos.x, (int) pos.y));
-//        holePhysix.initPhysics(phyManager);
-//        manager.addComponent(entity, holePhysix);
-//        manager.addComponent(entity, holePosition);
-//    }
-//
-//    public static EntityManager manager;
-//
-//    public static PhysixManager phyManager;
-//
-//    public EntityFactory() {
-//
-//    }
-//
-//    // public static void constructDog(Vector2 pos, float maxVelocity, float
-//    // middleVelocity, float minVelocity, float acceleration){
-//    // int entity = manager.createEntity();
-//    // DogPhysicsComponent dogPhysix = new DogPhysicsComponent();
-//    // PositionComponent dogPosition = new PositionComponent(new
-//    // Vector2((int)pos.x,(int)pos.y));
-//    // MovementComponent dogMove = new
-//    // MovementComponent(maxVelocity,middleVelocity,minVelocity,acceleration,new
-//    // Vector2(0,0));
-//    // dogPhysix.initPhysics(phyManager);
-//    // manager.addComponent(entity, dogPhysix);
-//    // manager.addComponent(entity, dogPosition);
-//    // manager.addComponent(entity, dogMove);
-//    // }
+  public static EntityManager manager;
+
+  public static void constructCat(Vector2 pos, float maxVelocity, float middleVelocity, float minVelocity, float acceleration) {
+    final int entity = manager.createEntity();
+    final CatPhysicsComponent catPhysix = new CatPhysicsComponent();
+
+<<<<<<< Unknown file: This is a bug in JDime.
+=======
+    final PositionComponent catPosition = new PositionComponent(new Vector2((int) pos.x, (int) pos.y));
+>>>>>>> /usr/src/app/output/lusito/gamedevweek/b9d0b595eaae93747ce9081072973fa34c8b8764/gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/sandbox/Test/Entity/EntityFactory.java/right.java
+
+    final MovementComponent catMove = new MovementComponent(maxVelocity, middleVelocity, minVelocity, acceleration, new Vector2(1, 1));
+    final CatPropertyComponent catProperty = new CatPropertyComponent();
+    catPhysix.initPhysics(phyManager);
+    catPhysix.physicsBody.setX(pos.x);
+
+<<<<<<< /usr/src/app/output/lusito/gamedevweek/b9d0b595eaae93747ce9081072973fa34c8b8764/gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/sandbox/Test/Entity/EntityFactory.java/left.java
+    catPhysix.physicsBody.setY(pos.y)
+=======
+    manager.addComponent(entity, catProperty)
+>>>>>>> /usr/src/app/output/lusito/gamedevweek/b9d0b595eaae93747ce9081072973fa34c8b8764/gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/sandbox/Test/Entity/EntityFactory.java/right.java
+    ;
+    catPhysix.physicsBody.setLinearVelocity(catMove.velocity, catMove.velocity);
+    manager.addComponent(entity, catPhysix);
+    manager.addComponent(entity, catMove);
+  }
+
+  public static PhysixManager phyManager;
+
+  public static void constructDog(Vector2 pos, float maxVelocity, float middleVelocity, float minVelocity, float acceleration) {
+    final int entity = manager.createEntity();
+    final DogPhysicsComponent dogPhysix = new DogPhysicsComponent();
+    final PositionComponent dogPosition = new PositionComponent(new Vector2((int) pos.x, (int) pos.y));
+    final MovementComponent dogMove = new MovementComponent(maxVelocity, middleVelocity, minVelocity, acceleration, new Vector2(0, 0));
+    dogPhysix.initPhysics(phyManager);
+    manager.addComponent(entity, dogPhysix);
+    manager.addComponent(entity, dogPosition);
+    manager.addComponent(entity, dogMove);
+  }
+
+  public static AssetManagerX assetManager;
+
+  public EntityFactory(
+<<<<<<< /usr/src/app/output/lusito/gamedevweek/b9d0b595eaae93747ce9081072973fa34c8b8764/gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/sandbox/Test/Entity/EntityFactory.java/left.java
+  EntityManager manager
+=======
+>>>>>>> Unknown file: This is a bug in JDime.
+  , 
+<<<<<<< /usr/src/app/output/lusito/gamedevweek/b9d0b595eaae93747ce9081072973fa34c8b8764/gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/sandbox/Test/Entity/EntityFactory.java/left.java
+  PhysixManager phyManager
+=======
+>>>>>>> Unknown file: This is a bug in JDime.
+  , 
+<<<<<<< /usr/src/app/output/lusito/gamedevweek/b9d0b595eaae93747ce9081072973fa34c8b8764/gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/sandbox/Test/Entity/EntityFactory.java/left.java
+  AssetManagerX assetManager
+=======
+>>>>>>> Unknown file: This is a bug in JDime.
+  ) {
+
+<<<<<<< /usr/src/app/output/lusito/gamedevweek/b9d0b595eaae93747ce9081072973fa34c8b8764/gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/sandbox/Test/Entity/EntityFactory.java/left.java
+    this.manager = manager;
+=======
+>>>>>>> Unknown file: This is a bug in JDime.
+
+
+<<<<<<< /usr/src/app/output/lusito/gamedevweek/b9d0b595eaae93747ce9081072973fa34c8b8764/gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/sandbox/Test/Entity/EntityFactory.java/left.java
+    this.phyManager = phyManager;
+=======
+>>>>>>> Unknown file: This is a bug in JDime.
+
+
+<<<<<<< /usr/src/app/output/lusito/gamedevweek/b9d0b595eaae93747ce9081072973fa34c8b8764/gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/sandbox/Test/Entity/EntityFactory.java/left.java
+    this.assetManager = assetManager;
+=======
+>>>>>>> Unknown file: This is a bug in JDime.
+  }
+
+  public static void constructHole(Vector2 pos) {
+    final int entity = manager.createEntity();
+    final HolePhysicsComponent holePhysix = new HolePhysicsComponent();
+    final PositionComponent holePosition = new PositionComponent(new Vector2((int) pos.x, (int) pos.y));
+    holePhysix.initPhysics(phyManager);
+    manager.addComponent(entity, holePhysix);
+    manager.addComponent(entity, holePosition);
+  }
 }
