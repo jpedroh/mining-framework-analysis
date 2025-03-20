a5e74a82bf4cd1f1b4bb764d2a229d34d77234cf
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 00:30:00 +0000

    Mining Framework Analysis

--- gdx-kitten-rescue/src/main/java/de/hochschuletrier/gdw/ss14/gamestates/GameStateEnum.java
+++ gdx-kitten-rescue/src/main/java/de/hochschuletrier/gdw/ss14/gamestates/GameStateEnum.java
@@ -1,59 +1,59 @@
 package de.hochschuletrier.gdw.ss14.gamestates;
-
 import com.badlogic.gdx.graphics.Color;
-
 import de.hochschuletrier.gdw.commons.gdx.assets.AssetManagerX;
 import de.hochschuletrier.gdw.commons.gdx.state.transition.FadeTransition;
 import de.hochschuletrier.gdw.commons.gdx.state.transition.Transition;
 import de.hochschuletrier.gdw.ss14.Main;
+import de.hochschuletrier.gdw.ss14.ui.StartScreen;
 
 public enum GameStateEnum {
+  LOADING(new LoadGameState()),
+  MAINMENU(new MainMenuState()),
+  GAMEPLAY(new GameplayState()),
+  LEVELMENU(new LevelMenuState()),
+  SANDBOX(new SandboxState()),
+  PAUSEGAME(new PauseGameState()),
+  CREDITS(new CreditsGameState()),
+  OPTIONSMENU(new OptionsMenuState()),
+  FINISH(new FinishGameState()),
+  STARTSCREEN(new StartScreenState())
+  ;
 
-    LOADING(new LoadGameState()),
-    MAINMENU(new MainMenuState()),
-    GAMEPLAY(new GameplayState()),
-    LEVELMENU(new LevelMenuState()),
-    SANDBOX(new SandboxState()),
-    PAUSEGAME(new PauseGameState()),
-    CREDITS(new CreditsGameState()),
-    OPTIONSMENU(new OptionsMenuState()),
-    FINISH(new FinishGameState()),
-    STARTSCREEN (new StartScreenState());
-    private final KittenGameState state;
+  private final KittenGameState state;
 
-    GameStateEnum(KittenGameState state) {
-        this.state = state;
-        state.gameStateEnum = this;
+  GameStateEnum(KittenGameState state) {
+    this.state = state;
+    state.gameStateEnum = this;
+  }
+
+  public void activate() {
+    Main.getInstance().changeState(state, null, null);
+  }
+
+  public void activate(int fadeTime) {
+    FadeTransition out = new FadeTransition(Color.BLACK, fadeTime);
+    Main.getInstance().changeState(state, out, null);
+  }
+
+  public void activate(Transition out, Transition in) {
+    Main.getInstance().changeState(state, out, in);
+  }
+
+  public KittenGameState get() {
+    return state;
+  }
+
+  public void init(AssetManagerX assetManager) {
+    state.init(assetManager);
+  }
+
+  public static void dispose() {
+    for (GameStateEnum entry : GameStateEnum.values()) {
+      entry.state.dispose();
     }
+  }
 
-    public void activate() {
-        Main.getInstance().changeState(state, null, null);
-    }
-
-    public void activate(int fadeTime) {
-        FadeTransition out = new FadeTransition(Color.BLACK, fadeTime);
-        Main.getInstance().changeState(state, out, null);
-    }
-
-    public void activate(Transition out, Transition in) {
-        Main.getInstance().changeState(state, out, in);
-    }
-
-    public KittenGameState get() {
-        return state;
-    }
-
-    public void init(AssetManagerX assetManager) {
-        state.init(assetManager);
-    }
-
-    public static void dispose() {
-        for (GameStateEnum entry : GameStateEnum.values()) {
-            entry.state.dispose();
-        }
-    }
-
-    public boolean isActive() {
-        return Main.getInstance().getCurrentState() == state;
-    }
+  public boolean isActive() {
+    return Main.getInstance().getCurrentState() == state;
+  }
 }
