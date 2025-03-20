94c68be79ae809557204088d3d197f9aaea122d2
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 15:31:24 +0000

    Mining Framework Analysis

--- protocol/src/main/java/net/md_5/bungee/protocol/ProtocolConstants.java
+++ protocol/src/main/java/net/md_5/bungee/protocol/ProtocolConstants.java
@@ -25,7 +25,6 @@ public class ProtocolConstants
     public static final int MINECRAFT_1_14_2 = 485;
     public static final int MINECRAFT_1_14_3 = 490;
     public static final int MINECRAFT_1_14_4 = 498;
-
     public static final List<String> SUPPORTED_VERSIONS = Arrays.asList(
             "1.8.x",
             "1.9.x",
