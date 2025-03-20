f53e618deec84f1f542ff43d3b8ce6deea84f1c6
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 21:18:27 +0000

    Mining Framework Analysis

--- simmetrics-core/src/main/java/org/simmetrics/metrics/SimonWhite.java
+++ simmetrics-core/src/main/java/org/simmetrics/metrics/SimonWhite.java
@@ -1,28 +1,6 @@
-/*
- * SimMetrics - SimMetrics is a java library of Similarity or Distance Metrics,
- * e.g. Levenshtein Distance, that provide float based similarity measures
- * between String Data. All metrics return consistent measures rather than
- * unbounded similarity scores.
- * 
- * Copyright (C) 2014 SimMetrics authors
- * 
- * This file is part of SimMetrics. This program is free software: you can
- * redistribute it and/or modify it under the terms of the GNU General Public
- * License as published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License along with
- * SimMetrics. If not, see <http://www.gnu.org/licenses/>.
- */
 package org.simmetrics.metrics;
-
 import java.util.ArrayList;
 import java.util.List;
-
 import org.simmetrics.ListMetric;
 
 /**
@@ -57,37 +35,23 @@ import org.simmetrics.ListMetric;
  *            type of the token
  * 
  */
-public class SimonWhite<T> implements ListMetric<T> {
+public class SimonWhite<T extends java.lang.Object> implements ListMetric<T> {
+  @Override public float compare(List<T> a, List<T> b) {
+    if (a.isEmpty() || b.isEmpty()) {
+      return 0.0f;
+    }
+    b = new ArrayList<>(b);
+    int union = a.size() + b.size();
+    int intersection = 0;
+    for (T token : a) {
+      if (b.remove(token)) {
+        intersection++;
+      }
+    }
+    return 2.0f * intersection / union;
+  }
 
-	@Override
-	public float compare(List<T> a, List<T> b) {
-
-		if (a.isEmpty() || b.isEmpty()) {
-			return 0.0f;
-		}
-
-		// Copy for destructive list difference
-		b = new ArrayList<>(b);
-		int union = a.size() + b.size();
-
-		// Count elements in the list intersection.
-		// Elements are counted only once in both lists.
-		// E.g. the intersection of [ab,ab,ab] and [ab,ab,ac,ad] is [ab,ab].
-		// Note: this is not the same as b.retainAll(a).size()
-		int intersection = 0;
-		for (T token : a) {
-			if (b.remove(token)) {
-				intersection++;
-			}
-		}
-
-		return 2.0f * intersection / union;
-
-	}
-
-	@Override
-	public String toString() {
-		return "SimonWhite";
-	}
-
+  @Override public String toString() {
+    return "SimonWhite";
+  }
 }
