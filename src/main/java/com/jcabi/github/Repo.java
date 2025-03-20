d300ba9716f26ca1fb0254b0acb1d2f962d9ecc9
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 04:22:54 +0000

    Mining Framework Analysis

--- src/main/java/com/jcabi/github/Repo.java
+++ src/main/java/com/jcabi/github/Repo.java
@@ -1,34 +1,4 @@
-/**
- * Copyright (c) 2012-2013, JCabi.com
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met: 1) Redistributions of source code must retain the above
- * copyright notice, this list of conditions and the following
- * disclaimer. 2) Redistributions in binary form must reproduce the above
- * copyright notice, this list of conditions and the following
- * disclaimer in the documentation and/or other materials provided
- * with the distribution. 3) Neither the name of the jcabi.com nor
- * the names of its contributors may be used to endorse or promote
- * products derived from this software without specific prior written
- * permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
- * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
- * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
- * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- * OF THE POSSIBILITY OF SUCH DAMAGE.
- */
 package com.jcabi.github;
-
 import com.jcabi.aspects.Immutable;
 import com.jcabi.aspects.Loggable;
 import java.io.IOException;
@@ -41,139 +11,130 @@ import lombok.ToString;
  * Github repository.
  * @author Yegor Bugayenko (yegor@tpc2.com)
  * @version $Id$
+ * @todo #1:1hr Assignees API should be implemented. Let's add a method
+ *  assignees() to this class returning an instance of interface Assignees.
+ *  This interface should have at least two methods: 1) iterate() returning
+ *  a list of Users and 2) check(String) returning TRUE if provided
+ *  login can be used as an assignee in repository. New interface should
+ *  be implemented by GhAssignees class and tested in unit and integration
+ *  tests. Moreover, we should implement MkAssignees class. See
+ *  http://developer.github.com/v3/issues/assignees/
  * @since 0.1
  */
-@Immutable
-@SuppressWarnings("PMD.TooManyMethods")
-public interface Repo extends JsonReadable, JsonPatchable {
-
-    /**
+@Immutable @SuppressWarnings(value = { "PMD.TooManyMethods" }) public interface Repo extends JsonReadable, JsonPatchable {
+  /**
      * Get its owner.
      * @return Github
      */
-    @NotNull(message = "github is never NULL")
-    Github github();
+  @NotNull(message = "github is never NULL") Github github();
 
-    /**
+  /**
      * Get its coordinates.
      * @return Coordinates
      */
-    @NotNull(message = "coordinates is never NULL")
-    Coordinates coordinates();
+  @NotNull(message = "coordinates is never NULL") Coordinates coordinates();
 
-    /**
+  /**
      * Iterate issues.
      * @return Issues
      */
-    @NotNull(message = "iterator of issues is never NULL")
-    Issues issues();
+  @NotNull(message = "iterator of issues is never NULL") Issues issues();
 
-    /**
+  /**
      * Iterate milestones.
      * @return Milestones
      * @since 0.7
      */
-    @NotNull(message = "iterator of milestones is never NULL")
-    Milestones milestones();
+  @NotNull(message = "iterator of milestones is never NULL") Milestones milestones();
 
-    /**
+  /**
      * Pull requests.
      * @return Pulls
      */
-    @NotNull(message = "iterator of pull requests is never NULL")
-    Pulls pulls();
+  @NotNull(message = "iterator of pull requests is never NULL") Pulls pulls();
 
-    /**
+  /**
      * Get all events for the repository.
      * @return Events
      * @see <a href="http://developer.github.com/v3/issues/events/#list-events-for-a-repository">List Events for a Repository</a>
      */
-    @NotNull(message = "iterable of events is never NULL")
-    Iterable<Event> events();
+  @NotNull(message = "iterable of events is never NULL") Iterable<Event> events();
 
-    /**
+  /**
      * Get all labels of the repo.
      * @return Labels
      * @see <a href="http://developer.github.com/v3/issues/labels/">Labels API</a>
      */
-    @NotNull(message = "labels are never NULL")
-    Labels labels();
+  @NotNull(message = "labels are never NULL") Labels labels();
 
-    /**
+  /**
      * Get all available assignees to which issues may be assigned.
      * @return Assignees
      * @see @see <a href="http://developer.github.com/v3/issues/assignees/">Assignees API</a>
      */
-    @NotNull(message = "labels are never NULL")
-    Assignees assignees();
+  @NotNull(message = "labels are never NULL") Assignees assignees();
 
+  @Immutable @ToString @Loggable(value = Loggable.DEBUG) @EqualsAndHashCode(of = "repo") final class Smart implements Repo {
     /**
-     * Smart Repo with extra features.
-     */
-    @Immutable
-    @ToString
-    @Loggable(Loggable.DEBUG)
-    @EqualsAndHashCode(of = "repo")
-    final class Smart implements Repo {
-        /**
          * Encapsulated Repo.
          */
-        private final transient Repo repo;
-        /**
+    private final transient Repo repo;
+
+    /**
          * Public ctor.
          * @param rep Repo
          */
-        public Smart(final Repo rep) {
-            this.repo = rep;
-        }
-        /**
+    public Smart(final Repo rep) {
+      this.repo = rep;
+    }
+
+    /**
          * Get its description.
          * @return Description
          * @throws IOException If there is any I/O problem
          */
-        public String description() throws IOException {
-            return new SmartJson(this).text("description");
-        }
-        @Override
-        public Github github() {
-            return this.repo.github();
-        }
-        @Override
-        public Coordinates coordinates() {
-            return this.repo.coordinates();
-        }
-        @Override
-        public Issues issues() {
-            return this.repo.issues();
-        }
-        @Override
-        public Milestones milestones() {
-            return this.repo.milestones();
-        }
-        @Override
-        public Pulls pulls() {
-            return this.repo.pulls();
-        }
-        @Override
-        public Iterable<Event> events() {
-            return this.repo.events();
-        }
-        @Override
-        public Labels labels() {
-            return this.repo.labels();
-        }
-        @Override
-        public Assignees assignees() {
-            return this.repo.assignees();
-        }
-        @Override
-        public void patch(final JsonObject json) throws IOException {
-            this.repo.patch(json);
-        }
-        @Override
-        public JsonObject json() throws IOException {
-            return this.repo.json();
-        }
+    public String description() throws IOException {
+      return new SmartJson(this).text("description");
     }
 
+    @Override public Github github() {
+      return this.repo.github();
+    }
+
+    @Override public Coordinates coordinates() {
+      return this.repo.coordinates();
+    }
+
+    @Override public Issues issues() {
+      return this.repo.issues();
+    }
+
+    @Override public Milestones milestones() {
+      return this.repo.milestones();
+    }
+
+    @Override public Pulls pulls() {
+      return this.repo.pulls();
+    }
+
+    @Override public Iterable<Event> events() {
+      return this.repo.events();
+    }
+
+    @Override public Labels labels() {
+      return this.repo.labels();
+    }
+
+    @Override public Assignees assignees() {
+      return this.repo.assignees();
+    }
+
+    @Override public void patch(final JsonObject json) throws IOException {
+      this.repo.patch(json);
+    }
+
+    @Override public JsonObject json() throws IOException {
+      return this.repo.json();
+    }
+  }
 }
--- src/main/java/com/jcabi/github/mock/MkRepo.java
+++ src/main/java/com/jcabi/github/mock/MkRepo.java
@@ -1,34 +1,4 @@
-/**
- * Copyright (c) 2012-2013, JCabi.com
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met: 1) Redistributions of source code must retain the above
- * copyright notice, this list of conditions and the following
- * disclaimer. 2) Redistributions in binary form must reproduce the above
- * copyright notice, this list of conditions and the following
- * disclaimer in the documentation and/or other materials provided
- * with the distribution. 3) Neither the name of the jcabi.com nor
- * the names of its contributors may be used to endorse or promote
- * products derived from this software without specific prior written
- * permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
- * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
- * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
- * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- * OF THE POSSIBILITY OF SUCH DAMAGE.
- */
 package com.jcabi.github.mock;
-
 import com.jcabi.aspects.Immutable;
 import com.jcabi.aspects.Loggable;
 import com.jcabi.github.Assignees;
@@ -54,112 +24,91 @@ import lombok.ToString;
  *  Please, implement milestones() method to return
  *  MkMilestones. Don't forget about unit tests
  */
-@Immutable
-@Loggable(Loggable.DEBUG)
-@ToString
-@EqualsAndHashCode(of = {"storage", "self", "coords" })
-final class MkRepo implements Repo {
-
-    /**
+@Immutable @Loggable(value = Loggable.DEBUG) @ToString @EqualsAndHashCode(of = { "storage", "self", "coords" }) final class MkRepo implements Repo {
+  /**
      * Storage.
      */
-    private final transient MkStorage storage;
+  private final transient MkStorage storage;
 
-    /**
+  /**
      * Login of the user logged in.
      */
-    private final transient String self;
+  private final transient String self;
 
-    /**
+  /**
      * Repo coordinates.
      */
-    private final transient Coordinates coords;
+  private final transient Coordinates coords;
 
-    /**
+  /**
      * Public ctor.
      * @param stg Storage
      * @param login User to login
      * @param repo Repo name
      */
-    MkRepo(final MkStorage stg, final String login,
-        final Coordinates repo) {
-        this.storage = stg;
-        this.self = login;
-        this.coords = repo;
-    }
+  MkRepo(final MkStorage stg, final String login, final Coordinates repo) {
+    this.storage = stg;
+    this.self = login;
+    this.coords = repo;
+  }
 
-    @Override
-    public Github github() {
-        return new MkGithub(this.storage, this.self);
-    }
+  @Override public Github github() {
+    return new MkGithub(this.storage, this.self);
+  }
 
-    @Override
-    public Coordinates coordinates() {
-        return this.coords;
-    }
+  @Override public Coordinates coordinates() {
+    return this.coords;
+  }
 
-    @Override
-    public Issues issues() {
-        try {
-            return new MkIssues(this.storage, this.self, this.coords);
-        } catch (IOException ex) {
-            throw new IllegalStateException(ex);
-        }
+  @Override public Issues issues() {
+    try {
+      return new MkIssues(this.storage, this.self, this.coords);
+    } catch (IOException ex) {
+      throw new IllegalStateException(ex);
     }
+  }
 
-    @Override
-    public Milestones milestones() {
-        return null;
+  @Override public Milestones milestones() {
+    return null;
+  }
+
+  @Override public Pulls pulls() {
+    try {
+      return new MkPulls(this.storage, this.self, this.coords);
+    } catch (IOException ex) {
+      throw new IllegalStateException(ex);
     }
+  }
 
-    @Override
-    public Pulls pulls() {
-        try {
-            return new MkPulls(this.storage, this.self, this.coords);
-        } catch (IOException ex) {
-            throw new IllegalStateException(ex);
-        }
+  @Override public Iterable<Event> events() {
+    return null;
+  }
+
+  @Override public Labels labels() {
+    try {
+      return new MkLabels(this.storage, this.self, this.coords);
+    } catch (IOException ex) {
+      throw new IllegalStateException(ex);
     }
+  }
 
-    @Override
-    public Iterable<Event> events() {
-        return null;
-    }
+  @Override public Assignees assignees() {
+    return null;
+  }
 
-    @Override
-    public Labels labels() {
-        try {
-            return new MkLabels(this.storage, this.self, this.coords);
-        } catch (IOException ex) {
-            throw new IllegalStateException(ex);
-        }
-    }
+  @Override public void patch(final JsonObject json) throws IOException {
+    new JsonPatch(this.storage).patch(this.xpath(), json);
+  }
 
-    @Override
-    public Assignees assignees() {
-        return null;
-    }
+  @Override public JsonObject json() throws IOException {
+    return new JsonNode(this.storage.xml().nodes(this.xpath()).get(0)).json();
+  }
 
-    @Override
-    public void patch(final JsonObject json) throws IOException {
-        new JsonPatch(this.storage).patch(this.xpath(), json);
-    }
-
-    @Override
-    public JsonObject json() throws IOException {
-        return new JsonNode(
-            this.storage.xml().nodes(this.xpath()).get(0)
-        ).json();
-    }
-
-    /**
+  /**
      * XPath of this element in XML tree.
      * @return XPath
      */
-    private String xpath() {
-        return String.format(
-            "/github/repos/repo[@coords='%s']",
-            this.coords
-        );
-    }
+  private String xpath() {
+    return String.format("/github/repos/repo[@coords=\'%s\']", this.coords);
+  }
 }
