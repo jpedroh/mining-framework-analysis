9aff516391b2f29dd3032983cbbee19de59ccc04
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 08:16:44 +0000

    Mining Framework Analysis

--- src/main/java/com/mitchellbosecke/pebble/extension/escaper/RawFilter.java
+++ src/main/java/com/mitchellbosecke/pebble/extension/escaper/RawFilter.java
@@ -1,28 +1,15 @@
-/*******************************************************************************
- * This file is part of Pebble.
- *
- * Copyright (c) 2014 by Mitchell Bösecke
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- ******************************************************************************/
 package com.mitchellbosecke.pebble.extension.escaper;
-
 import java.util.List;
 import java.util.Map;
-
 import com.mitchellbosecke.pebble.extension.Filter;
 import com.mitchellbosecke.pebble.template.PebbleTemplateImpl;
 
 public class RawFilter implements Filter {
+  public List<String> getArgumentNames() {
+    return null;
+  }
 
-    public List<String> getArgumentNames() {
-        return null;
-    }
-
-    @Override
-    public Object apply(Object inputObject, Map<String, Object> args, PebbleTemplateImpl self, int lineNumber) {
-        return inputObject == null ? null : new SafeString(inputObject.toString());
-    }
-
+  @Override public Object apply(Object inputObject, Map<String, Object> args, PebbleTemplateImpl self, int lineNumber) {
+    return inputObject == null ? null : new SafeString(inputObject.toString());
+  }
 }
