bc1d0eaf97cc8a9bf62fce6a26e2746cf9692588
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 23:00:19 +0000

    Mining Framework Analysis

--- src/main/java/org/assertj/core/error/ShouldContainOnly.java
+++ src/main/java/org/assertj/core/error/ShouldContainOnly.java
@@ -1,3 +1,4 @@
+<<<<<<< /usr/src/app/output/joel-costigliola/assertj-core/a7c0edb3b3015a109764cb2428f3d16ab231b604/src/main/java/org/assertj/core/error/ShouldContainOnly.java/left.java
 /**
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
@@ -10,6 +11,23 @@
  *
  * Copyright 2012-2014 the original author or authors.
  */
+||||||| /usr/src/app/output/joel-costigliola/assertj-core/a7c0edb3b3015a109764cb2428f3d16ab231b604/src/main/java/org/assertj/core/error/ShouldContainOnly.java/base.java
+/*
+ * Created on Oct 18, 2010
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ * 
+ * Copyright @2010-2011 the original author or authors.
+ */
+=======
+>>>>>>> /usr/src/app/output/joel-costigliola/assertj-core/a7c0edb3b3015a109764cb2428f3d16ab231b604/src/main/java/org/assertj/core/error/ShouldContainOnly.java/right.java
 package org.assertj.core.error;
 
 import static org.assertj.core.util.Iterables.isNullOrEmpty;
