df2e487e471329ff2a8dca139a4a7a3cb71773ef
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 00:47:08 +0000

    Mining Framework Analysis

--- src/main/java/org/omnifaces/component/tree/TreeNodeItem.java
+++ src/main/java/org/omnifaces/component/tree/TreeNodeItem.java
@@ -1,30 +1,14 @@
-/*
- * Copyright 2012 OmniFaces.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License.
- */
 package org.omnifaces.component.tree;
-
 import static org.omnifaces.util.Components.getClosestParent;
 import static org.omnifaces.util.Components.shouldVisitSkipIteration;
 import static org.omnifaces.util.Components.validateHasParent;
-
 import java.io.IOException;
-
 import javax.faces.component.FacesComponent;
 import javax.faces.component.UIComponent;
 import javax.faces.component.visit.VisitCallback;
 import javax.faces.component.visit.VisitContext;
 import javax.faces.context.FacesContext;
 import javax.faces.event.PhaseId;
-
 import org.omnifaces.model.tree.TreeModel;
 import org.omnifaces.util.Callback;
 
@@ -42,113 +26,88 @@ import org.omnifaces.util.Callback;
  * @see TreeNode
  * @see TreeInsertChildren
  */
-@FacesComponent(TreeNodeItem.COMPONENT_TYPE)
-public class TreeNodeItem extends TreeFamily {
+@FacesComponent(value = TreeNodeItem.COMPONENT_TYPE) public class TreeNodeItem extends TreeFamily {
+  /** The standard component type. */
+  public static final String COMPONENT_TYPE = "org.omnifaces.component.tree.TreeNodeItem";
 
-	// Public constants -----------------------------------------------------------------------------------------------
+  private static final String ERROR_NESTING_DISALLOWED = "Nesting TreeNodeItem components is disallowed. Use TreeNode instead to markup specific levels.";
 
-	/** The standard component type. */
-	public static final String COMPONENT_TYPE = "org.omnifaces.component.tree.TreeNodeItem";
-
-	// Private constants ----------------------------------------------------------------------------------------------
-
-	private static final String ERROR_NESTING_DISALLOWED =
-		"Nesting TreeNodeItem components is disallowed. Use TreeNode instead to markup specific levels.";
-
-	// Actions --------------------------------------------------------------------------------------------------------
-
-	/**
+  /**
 	 * Validate the component hierarchy.
 	 * @throws IllegalArgumentException When there is no parent of type {@link Tree}, or when this component is nested
 	 * in another {@link TreeNodeItem}.
 	 */
-	@Override
-	protected void validateHierarchy() {
-		validateHasParent(this, Tree.class);
+  @Override protected void validateHierarchy() {
+    validateHasParent(this, Tree.class);
+    if (getClosestParent(this, TreeNodeItem.class) != null) {
+      throw new IllegalArgumentException(ERROR_NESTING_DISALLOWED);
+    }
+  }
 
-		if (getClosestParent(this, TreeNodeItem.class) != null) {
-			throw new IllegalArgumentException(ERROR_NESTING_DISALLOWED);
-		}
-	}
-
-	/**
+  /**
 	 * Suppress default behavior of {@link #encodeAll(FacesContext)} (which also checks {@link #isRendered()}) by
 	 * delegating directly to {@link #encodeChildren(FacesContext)}.
 	 */
-	@Override
-	public void encodeAll(FacesContext context) throws IOException {
-		encodeChildren(context);
-	}
+  @Override public void encodeAll(FacesContext context) throws IOException {
+    encodeChildren(context);
+  }
 
-	/**
+  /**
 	 * Loop over children of the current model node, set the child as the current model node and continue processing
 	 * this component according to the rules of the given phase ID.
 	 * @param context The faces context to work with.
 	 * @param phaseId The current phase ID.
 	 * @see Tree#setCurrentModelNode(FacesContext, TreeModel)
 	 */
-	@Override
-	@SuppressWarnings({ "rawtypes", "unchecked" }) // For TreeModel. We don't care about its actual type anyway.
-	protected void process(final FacesContext context, final PhaseId phaseId) {
-		if (getChildCount() == 0) {
-			return;
-		}
+  @Override @SuppressWarnings(value = { "rawtypes", "unchecked" }) protected void process(final FacesContext context, final PhaseId phaseId) {
+    if (getChildCount() == 0) {
+      return;
+    }
+    process(context, new Callback.ReturningWithArgument<Void, Tree>() {
+      @Override public Void invoke(Tree tree) {
+        if (tree.getCurrentModelNode() != null) {
+          for (TreeModel childModelNode : (Iterable<TreeModel>) tree.getCurrentModelNode()) {
+            tree.setCurrentModelNode(context, childModelNode);
+            if (isRendered()) {
+              processSuper(context, phaseId);
+            }
+          }
+        }
+        return null;
+      }
+    });
+  }
 
-		process(context, new Callback.ReturningWithArgument<Void, Tree>() {
-			@Override
-			public Void invoke(Tree tree) {
-				if (tree.getCurrentModelNode() != null) {
-					for (TreeModel childModelNode : (Iterable<TreeModel>) tree.getCurrentModelNode()) {
-						tree.setCurrentModelNode(context, childModelNode);
-
-						if (isRendered()) {
-							processSuper(context, phaseId);
-						}
-					}
-				}
-
-				return null;
-			}
-		});
-	}
-
-	/**
+  /**
 	 * Loop over children of the current model node, set the child as the current model node and continue visiting
 	 * this component according to the given visit context and callback.
 	 * @param context The visit context to work with.
 	 * @param callback The visit callback to work with.
 	 * @see Tree#setCurrentModelNode(FacesContext, TreeModel)
 	 */
-	@Override
-	@SuppressWarnings({ "rawtypes", "unchecked" }) // For TreeModel. We don't care about its actual type anyway.
-	public boolean visitTree(final VisitContext context, final VisitCallback callback) {
-		if (shouldVisitSkipIteration(context)) {
-			return super.visitTree(context, callback);
-		}
+  @Override @SuppressWarnings(value = { "rawtypes", "unchecked" }) public boolean visitTree(final VisitContext context, final VisitCallback callback) {
+    if (shouldVisitSkipIteration(context)) {
+      return super.visitTree(context, callback);
+    }
+    if (!isVisitable(context) || getChildCount() == 0) {
+      return false;
+    }
+    return process(context.getFacesContext(), new Callback.ReturningWithArgument<Boolean, Tree>() {
+      @Override public Boolean invoke(Tree tree) {
+        if (tree.getCurrentModelNode() != null) {
+          for (TreeModel childModelNode : (Iterable<TreeModel>) tree.getCurrentModelNode()) {
+            tree.setCurrentModelNode(context.getFacesContext(), childModelNode);
+            if (TreeNodeItem.super.visitTree(context, callback)) {
+              return true;
+            }
+          }
+        }
+        return false;
+      }
+    });
+  }
 
-		if (!isVisitable(context) || getChildCount() == 0) {
-			return false;
-		}
-
-		return process(context.getFacesContext(), new Callback.ReturningWithArgument<Boolean, Tree>() {
-			@Override
-			public Boolean invoke(Tree tree) {
-				if (tree.getCurrentModelNode() != null) {
-					for (TreeModel childModelNode : (Iterable<TreeModel>) tree.getCurrentModelNode()) {
-						tree.setCurrentModelNode(context.getFacesContext(), childModelNode);
-
-						if (TreeNodeItem.super.visitTree(context, callback)) {
-							return true;
-						}
-					}
-				}
-
-				return false;
-			}
-		});
-	}
-
-	/**
+  /**
 	 * Convenience method to handle both {@link #process(FacesContext, PhaseId)} and
 	 * {@link #visitTree(VisitContext, VisitCallback)} without code duplication.
 	 * @param context The faces context to work with.
@@ -156,17 +115,13 @@ public class TreeNodeItem extends TreeFamily {
 	 * @param callback The callback to be invoked.
 	 * @return The callback result.
 	 */
-	@SuppressWarnings("rawtypes") // For TreeModel. We don't care about its actual type anyway.
-	private <R> R process(FacesContext context, Callback.ReturningWithArgument<R, Tree> callback) {
-		Tree tree = getClosestParent(this, Tree.class);
-		TreeModel originalModelNode = tree.getCurrentModelNode();
-
-		try {
-			return callback.invoke(tree);
-		}
-		finally {
-			tree.setCurrentModelNode(context, originalModelNode);
-		}
-	}
-
+  @SuppressWarnings(value = { "rawtypes" }) private <R extends java.lang.Object> R process(FacesContext context, Callback.ReturningWithArgument<R, Tree> callback) {
+    Tree tree = getClosestParent(this, Tree.class);
+    TreeModel originalModelNode = tree.getCurrentModelNode();
+    try {
+      return callback.invoke(tree);
+    }  finally {
+      tree.setCurrentModelNode(context, originalModelNode);
+    }
+  }
 }
--- src/main/java/org/omnifaces/component/validator/ValidateOrder.java
+++ src/main/java/org/omnifaces/component/validator/ValidateOrder.java
@@ -1,28 +1,12 @@
-/*
- * Copyright 2012 OmniFaces.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License.
- */
 package org.omnifaces.component.validator;
-
 import static java.util.Arrays.asList;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.TreeSet;
-
 import javax.faces.component.FacesComponent;
 import javax.faces.component.UIInput;
 import javax.faces.context.FacesContext;
-
 import org.omnifaces.util.Callback;
 import org.omnifaces.util.State;
 import org.omnifaces.validator.MultiFieldValidator;
@@ -55,117 +39,94 @@ import org.omnifaces.validator.MultiFieldValidator;
  * @see ValidatorFamily
  * @see MultiFieldValidator
  */
-@FacesComponent(ValidateOrder.COMPONENT_TYPE)
-@SuppressWarnings({ "unchecked", "rawtypes" }) // We don't care about the actual Comparable type.
-public class ValidateOrder extends ValidateMultipleFields {
+@FacesComponent(value = ValidateOrder.COMPONENT_TYPE) @SuppressWarnings(value = { "unchecked", "rawtypes" }) public class ValidateOrder extends ValidateMultipleFields {
+  /** The standard component type. */
+  public static final String COMPONENT_TYPE = "org.omnifaces.component.validator.ValidateOrder";
 
-	// Public constants -----------------------------------------------------------------------------------------------
+  private enum Type {
+    LT(new Callback.ReturningWithArgument<Boolean, List<Comparable>>() {
+      @Override public Boolean invoke(List<Comparable> values) {
+        return new ArrayList<Comparable>(new TreeSet<Comparable>(values)).equals(values);
+      }
+    }),
+    LTE(new Callback.ReturningWithArgument<Boolean, List<Comparable>>() {
+      @Override public Boolean invoke(List<Comparable> values) {
+        List<Comparable> sortedValues = new ArrayList<Comparable>(values);
+        Collections.sort(sortedValues);
+        return sortedValues.equals(values);
+      }
+    }),
+    GT(new Callback.ReturningWithArgument<Boolean, List<Comparable>>() {
+      @Override public Boolean invoke(List<Comparable> values) {
+        List<Comparable> sortedValues = new ArrayList<Comparable>(new TreeSet<Comparable>(values));
+        Collections.reverse(sortedValues);
+        return sortedValues.equals(values);
+      }
+    }),
+    GTE(new Callback.ReturningWithArgument<Boolean, List<Comparable>>() {
+      @Override public Boolean invoke(List<Comparable> values) {
+        List<Comparable> sortedValues = new ArrayList<Comparable>(values);
+        Collections.sort(sortedValues, Collections.reverseOrder());
+        return sortedValues.equals(values);
+      }
+    })
+    ;
 
-	/** The standard component type. */
-	public static final String COMPONENT_TYPE = "org.omnifaces.component.validator.ValidateOrder";
+    private Callback.ReturningWithArgument<Boolean, List<Comparable>> callback;
 
-	// Private constants ----------------------------------------------------------------------------------------------
+    private Type(Callback.ReturningWithArgument<Boolean, List<Comparable>> callback) {
+      this.callback = callback;
+    }
 
-	private enum Type {
-		LT(new Callback.ReturningWithArgument<Boolean, List<Comparable>>() {
-			@Override
-			public Boolean invoke(List<Comparable> values) {
-				return new ArrayList<Comparable>(new TreeSet<Comparable>(values)).equals(values);
-			}
-		}),
+    public boolean validateOrder(List<Comparable> values) {
+      return callback.invoke(values);
+    }
+  }
 
-		LTE(new Callback.ReturningWithArgument<Boolean, List<Comparable>>() {
-			@Override
-			public Boolean invoke(List<Comparable> values) {
-				List<Comparable> sortedValues = new ArrayList<Comparable>(values);
-				Collections.sort(sortedValues);
-				return sortedValues.equals(values);
-			}
-		}),
+  private static final String DEFAULT_TYPE = Type.LT.name();
 
-		GT(new Callback.ReturningWithArgument<Boolean, List<Comparable>>() {
-			@Override
-			public Boolean invoke(List<Comparable> values) {
-				List<Comparable> sortedValues = new ArrayList<Comparable>(new TreeSet<Comparable>(values));
-				Collections.reverse(sortedValues);
-				return sortedValues.equals(values);
-			}
-		}),
+  private static final String ERROR_INVALID_TYPE = "Invalid type \'%s\'. Only \'lt\', \'lte\', \'gt\' and \'gte\' are allowed.";
 
-		GTE(new Callback.ReturningWithArgument<Boolean, List<Comparable>>() {
-			@Override
-			public Boolean invoke(List<Comparable> values) {
-				List<Comparable> sortedValues = new ArrayList<Comparable>(values);
-				Collections.sort(sortedValues, Collections.reverseOrder());
-				return sortedValues.equals(values);
-			}
-		});
+  private static final String ERROR_VALUES_NOT_COMPARABLE = "All values must implement java.lang.Comparable.";
 
-		private Callback.ReturningWithArgument<Boolean, List<Comparable>> callback;
+  private enum PropertyKeys {
+    type
+  }
 
-		private Type(Callback.ReturningWithArgument<Boolean, List<Comparable>> callback) {
-			this.callback = callback;
-		}
+  private final State state = new State(getStateHelper());
 
-		public boolean validateOrder(List<Comparable> values) {
-			return callback.invoke(values);
-		}
-	}
-
-	private static final String DEFAULT_TYPE = Type.LT.name();
-	private static final String ERROR_INVALID_TYPE = "Invalid type '%s'. Only 'lt', 'lte', 'gt' and 'gte' are allowed.";
-	private static final String ERROR_VALUES_NOT_COMPARABLE = "All values must implement java.lang.Comparable.";
-
-	private enum PropertyKeys {
-		// Cannot be uppercased. They have to exactly match the attribute names.
-		type;
-	}
-
-	// Variables ------------------------------------------------------------------------------------------------------
-
-	private final State state = new State(getStateHelper());
-
-	// Actions --------------------------------------------------------------------------------------------------------
-
-	/**
+  /**
 	 * Validate if all values are in specified order.
 	 */
-	@Override
-	public boolean validateValues(FacesContext context, List<UIInput> components, List<Object> values) {
-		try {
-			Object tmp = values; // https://bugs.eclipse.org/bugs/show_bug.cgi?id=158870
-			List<Comparable> comparableValues = new ArrayList<Comparable>((List<Comparable>) tmp);
-			comparableValues.removeAll(asList(null, "")); // Empty checking job is up to required="true".
-			return Type.valueOf(getType().toUpperCase()).validateOrder(comparableValues);
-		}
-		catch (ClassCastException e) {
-			throw new IllegalArgumentException(ERROR_VALUES_NOT_COMPARABLE, e);
-		}
-	}
+  @Override public boolean validateValues(FacesContext context, List<UIInput> components, List<Object> values) {
+    try {
+      Object tmp = values;
+      List<Comparable> comparableValues = new ArrayList<Comparable>((List<Comparable>) tmp);
+      comparableValues.removeAll(asList(null, ""));
+      return Type.valueOf(getType().toUpperCase()).validateOrder(comparableValues);
+    } catch (ClassCastException e) {
+      throw new IllegalArgumentException(ERROR_VALUES_NOT_COMPARABLE, e);
+    }
+  }
 
-	// Getters/setters ------------------------------------------------------------------------------------------------
-
-	/**
+  /**
 	 * Returns the ordering type to be used.
 	 * @return The ordering type to be used.
 	 */
-	public String getType() {
-		return state.get(PropertyKeys.type, DEFAULT_TYPE);
-	}
+  public String getType() {
+    return state.get(PropertyKeys.type, DEFAULT_TYPE);
+  }
 
-	/**
+  /**
 	 * Sets the ordering type to be used.
 	 * @param type The ordering type to be used.
 	 */
-	public void setType(String type) {
-		try {
-			Type.valueOf(type.toUpperCase());
-		}
-		catch (IllegalArgumentException e) {
-			throw new IllegalArgumentException(String.format(ERROR_INVALID_TYPE, type), e);
-		}
-
-		state.put(PropertyKeys.type, type);
-	}
-
+  public void setType(String type) {
+    try {
+      Type.valueOf(type.toUpperCase());
+    } catch (IllegalArgumentException e) {
+      throw new IllegalArgumentException(String.format(ERROR_INVALID_TYPE, type), e);
+    }
+    state.put(PropertyKeys.type, type);
+  }
 }
