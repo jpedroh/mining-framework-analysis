d6e7dbb2439f02c2b7b8625e12d0409a9c9634ad
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 18:11:29 +0000

    Mining Framework Analysis

--- src/main/java/nl/kb/europeananewspaper/NerAnnotater/output/ResultHandlerFactory.java
+++ src/main/java/nl/kb/europeananewspaper/NerAnnotater/output/ResultHandlerFactory.java
@@ -1,10 +1,8 @@
 package nl.kb.europeananewspaper.NerAnnotater.output;
-
-import nl.kb.europeananewspaper.NerAnnotater.App;
-import nl.kb.europeananewspaper.NerAnnotater.container.ContainerContext;
-
 import java.sql.SQLException;
+import nl.kb.europeananewspaper.NerAnnotater.App;
 import java.util.ArrayList;
+import nl.kb.europeananewspaper.NerAnnotater.container.ContainerContext;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -16,75 +14,107 @@ import java.util.Map;
  * 
  */
 public class ResultHandlerFactory {
-	/**
+  /**
 	 */
-	
-	static Map<Class<? extends ResultHandler>, ResultHandler> registeredHandlers=new LinkedHashMap<Class<? extends ResultHandler>, ResultHandler>();
-	
-	
-	/**
+  static Map<Class<? extends ResultHandler>, ResultHandler> registeredHandlers = new LinkedHashMap<Class<? extends ResultHandler>, ResultHandler>();
+
+  /**
 	 * @param context
 	 * @param name 
 	 * @return array of ResultHandlers according to the configuration
 	 */
-	public static ResultHandler[] createResultHandlers(
-			final ContainerContext context, final String name) {
+  public static ResultHandler[] createResultHandlers(final ContainerContext context, final String name) {
+    String[] outputFormats = App.getOutputFormats();
+    ArrayList<ResultHandler> result = new ArrayList<ResultHandler>();
+    for (String outputFormat : outputFormats) {
 
-		String[] outputFormats = App.getOutputFormats();
+<<<<<<< /usr/src/app/output/kbnlresearch/europeananp-ner/02058bce7bad895e7b27640e4b1b3a4d68ca1cb2/src/main/java/nl/kb/europeananewspaper/NerAnnotater/output/ResultHandlerFactory.java/left.java
+      if (outputFormat.equals("log")) {
+        LogResultHandler logResultHandler = new LogResultHandler();
+        registeredHandlers.put(LogResultHandler.class, logResultHandler);
+        result.add(logResultHandler);
+      } else {
+        if (outputFormat.equals("csv")) {
+          CsvResultHandler csvResultHandler = new CsvResultHandler(context, name);
+          registeredHandlers.put(CsvResultHandler.class, csvResultHandler);
+          result.add(csvResultHandler);
+        } else {
+          if (outputFormat.equals("alto")) {
+            AnnotatedAltoResultHandler annotatedAltoResultHandler = new AnnotatedAltoResultHandler(context, name);
+            registeredHandlers.put(AnnotatedAltoResultHandler.class, annotatedAltoResultHandler);
+            result.add(annotatedAltoResultHandler);
+          } else {
+            if (outputFormat.equals("alto2_1")) {
+              Alto2_1ResultHandler alto2_1ResultHandler = new Alto2_1ResultHandler(context, name);
+              registeredHandlers.put(Alto2_1ResultHandler.class, alto2_1ResultHandler);
+              result.add(alto2_1ResultHandler);
+            } else {
+              if (outputFormat.equals("html")) {
+                HtmlResultHandler htmlResultHandler = new HtmlResultHandler(context, name);
+                registeredHandlers.put(HtmlResultHandler.class, htmlResultHandler);
+                result.add(htmlResultHandler);
+              } else {
+                if (outputFormat.equals("db")) {
+                  try {
+                    DbResultHandler dbResultHandler = new DbResultHandler(context, name);
+                    registeredHandlers.put(DbResultHandler.class, dbResultHandler);
+                    result.add(dbResultHandler);
+                  } catch (SQLException e) {
+                    e.printStackTrace();
+                  }
+                } else {
+                  throw new IllegalArgumentException("Unknown output format: " + outputFormat);
+                }
+              }
+            }
+          }
+        }
+      }
+=======
+      switch (outputFormat) {
+        case "log":
+        LogResultHandler logResultHandler = new LogResultHandler();
+        registeredHandlers.put(LogResultHandler.class, logResultHandler);
+        result.add(logResultHandler);
+        break;
+        case "csv":
+        CsvResultHandler csvResultHandler = new CsvResultHandler(context, name);
+        registeredHandlers.put(CsvResultHandler.class, csvResultHandler);
+        result.add(csvResultHandler);
+        break;
+        case "alto":
+        AnnotatedAltoResultHandler annotatedAltoResultHandler = new AnnotatedAltoResultHandler(context, name);
+        registeredHandlers.put(AnnotatedAltoResultHandler.class, annotatedAltoResultHandler);
+        result.add(annotatedAltoResultHandler);
+        break;
+        case "html":
+        HtmlResultHandler htmlResultHandler = new HtmlResultHandler(context, name);
+        registeredHandlers.put(HtmlResultHandler.class, htmlResultHandler);
+        result.add(htmlResultHandler);
+        break;
+        case "db":
+        try {
+          DbResultHandler dbResultHandler = new DbResultHandler(context, name);
+          registeredHandlers.put(DbResultHandler.class, dbResultHandler);
+          result.add(dbResultHandler);
+        } catch (SQLException e) {
+          e.printStackTrace();
+        }
+        break;
+        default:
+        throw new IllegalArgumentException("Unknown output format: " + outputFormat);
+      }
+>>>>>>> /usr/src/app/output/kbnlresearch/europeananp-ner/02058bce7bad895e7b27640e4b1b3a4d68ca1cb2/src/main/java/nl/kb/europeananewspaper/NerAnnotater/output/ResultHandlerFactory.java/right.java
+    }
+    return result.toArray(new ResultHandler[result.size()]);
+  }
 
-		ArrayList<ResultHandler> result = new ArrayList<ResultHandler>();
-
-		for (String outputFormat : outputFormats) {
-                    switch (outputFormat) {
-                        case "log":
-                            LogResultHandler logResultHandler = new LogResultHandler();
-                            registeredHandlers.put(LogResultHandler.class, logResultHandler);
-                            result.add(logResultHandler);
-                            break;
-                        case "csv":
-                            CsvResultHandler csvResultHandler = new CsvResultHandler(context, name);
-                            registeredHandlers.put(CsvResultHandler.class, csvResultHandler);
-                            result.add(csvResultHandler);
-                            break;
-                        case "alto":
-                            AnnotatedAltoResultHandler annotatedAltoResultHandler = new AnnotatedAltoResultHandler(context, name);
-                            registeredHandlers.put(AnnotatedAltoResultHandler.class, annotatedAltoResultHandler);
-                            result.add(annotatedAltoResultHandler);
-                            break;
-                        case "html":
-                            HtmlResultHandler htmlResultHandler = new HtmlResultHandler(context, name);
-                            registeredHandlers.put(HtmlResultHandler.class, htmlResultHandler);
-                            result.add(htmlResultHandler);
-                            break;
-                        case "alto2_1":
-                            Alto2_1ResultHandler alto2_1ResultHandler = new Alto2_1ResultHandler(context, name);
-                            registeredHandlers.put(Alto2_1ResultHandler.class, alto2_1ResultHandler);
-                            result.add(alto2_1ResultHandler);
-                            break;
-                        case "db":
-                            try {
-                                DbResultHandler dbResultHandler = new DbResultHandler(context, name);
-                                registeredHandlers.put(DbResultHandler.class, dbResultHandler);
-                                result.add(dbResultHandler);
-                            } catch (SQLException e) {
-                                // TODO Auto-generated catch block
-                                e.printStackTrace();
-                            }
-                            break;
-                        default:
-                            throw new IllegalArgumentException("Unknown output format: "
-                                    + outputFormat);
-                    }
-		}
-		return result.toArray(new ResultHandler[result.size()]);
-	}
-	
-	/**
+  /**
 	 * 
 	 */
-	public static void shutdownResultHandlers() {
-		for (@SuppressWarnings("rawtypes") Class c: registeredHandlers.keySet()) {
-			registeredHandlers.get(c).globalShutdown();
-		}
-	}
+  public static void shutdownResultHandlers() {
+    for (@SuppressWarnings(value = { "rawtypes" }) Class c : registeredHandlers.keySet()) {
+      registeredHandlers.get(c).globalShutdown();
+    }
+  }
 }
