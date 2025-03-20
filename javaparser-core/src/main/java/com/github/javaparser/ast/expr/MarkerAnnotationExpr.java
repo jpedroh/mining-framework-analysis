33faec8aaa69f468a3c29478b3ea7932e6a72eff
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 02:15:08 +0000

    Mining Framework Analysis

--- javaparser-core/src/main/java/com/github/javaparser/ast/expr/MarkerAnnotationExpr.java
+++ javaparser-core/src/main/java/com/github/javaparser/ast/expr/MarkerAnnotationExpr.java
@@ -1,25 +1,4 @@
-/*
- * Copyright (C) 2007-2010 Júlio Vilmar Gesser.
- * Copyright (C) 2011, 2013-2016 The JavaParser Team.
- *
- * This file is part of JavaParser.
- *
- * JavaParser can be used either under the terms of
- * a) the GNU Lesser General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
- * b) the terms of the Apache License
- *
- * You should have received a copy of both licenses in LICENCE.LGPL and
- * LICENCE.APACHE. Please refer to those files for details.
- *
- * JavaParser is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- */
 package com.github.javaparser.ast.expr;
-
 import com.github.javaparser.Range;
 import com.github.javaparser.ast.AllFieldsConstructor;
 import com.github.javaparser.ast.visitor.GenericVisitor;
@@ -30,8 +9,6 @@ import com.github.javaparser.metamodel.MarkerAnnotationExprMetaModel;
 import com.github.javaparser.metamodel.JavaParserMetaModel;
 import static com.github.javaparser.JavaParser.parseName;
 
-import static com.github.javaparser.JavaParser.parseName;
-
 /**
  * An annotation that uses only the annotation type name.
  * <br/><code>@Override</code>
@@ -39,49 +16,42 @@ import static com.github.javaparser.JavaParser.parseName;
  * @author Julio Vilmar Gesser
  */
 public final class MarkerAnnotationExpr extends AnnotationExpr {
+  public MarkerAnnotationExpr() {
+    this(null, new Name());
+  }
 
-    public MarkerAnnotationExpr() {
-        this(null, new Name());
-    }
+  public MarkerAnnotationExpr(final String name) {
+    this(null, parseName(name));
+  }
 
-    public MarkerAnnotationExpr(final String name) {
-        this(null, parseName(name));
-    }
+  @AllFieldsConstructor public MarkerAnnotationExpr(final Name name) {
+    this(null, name);
+  }
 
-    @AllFieldsConstructor
-    public MarkerAnnotationExpr(final Name name) {
-        this(null, name);
-    }
+  public MarkerAnnotationExpr(final Range range, final Name name) {
+    super(range, name);
+  }
 
-    public MarkerAnnotationExpr(final Range range, final Name name) {
-        super(range, name);
-    }
+  @Override public <R extends java.lang.Object, A extends java.lang.Object> R accept(final GenericVisitor<R, A> v, final A arg) {
+    return v.visit(this, arg);
+  }
 
-    @Override
-    public <R, A> R accept(final GenericVisitor<R, A> v, final A arg) {
-        return v.visit(this, arg);
-    }
+  @Override public <A extends java.lang.Object> void accept(final VoidVisitor<A> v, final A arg) {
+    v.visit(this, arg);
+  }
 
-    @Override
-    public <A> void accept(final VoidVisitor<A> v, final A arg) {
-        v.visit(this, arg);
+  @Override public boolean remove(Node node) {
+    if (node == null) {
+      return false;
     }
+    return super.remove(node);
+  }
 
-    @Override
-    public boolean remove(Node node) {
-        if (node == null)
-            return false;
-        return super.remove(node);
-    }
+  @Override public MarkerAnnotationExpr clone() {
+    return (MarkerAnnotationExpr) accept(new CloneVisitor(), null);
+  }
 
-    @Override
-    public MarkerAnnotationExpr clone() {
-        return (MarkerAnnotationExpr) accept(new CloneVisitor(), null);
-    }
-
-    @Override
-    public MarkerAnnotationExprMetaModel getMetaModel() {
-        return JavaParserMetaModel.markerAnnotationExprMetaModel;
-    }
+  @Override public MarkerAnnotationExprMetaModel getMetaModel() {
+    return JavaParserMetaModel.markerAnnotationExprMetaModel;
+  }
 }
-
