8b77f19c2ff60ccb7181bf5852d1f568314636b1
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 04:27:53 +0000

    Mining Framework Analysis

--- src/test/java/com/jcabi/github/RtContentsTest.java
+++ src/test/java/com/jcabi/github/RtContentsTest.java
@@ -1,39 +1,10 @@
-/**
- * Copyright (c) 2012-2013, JCabi.com
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met: 1) Redistributions of source code must retain the above
- * copyright notice, this list of conditions and the following
- * disclaimer. 2) Redistributions in binary form must reproduce the above
- * copyright notice, this list of conditions and the following
- * disclaimer in the documentation and/or other materials provided
- * with the distribution. 3) Neither the name of the jcabi.com nor
- * the names of its contributors may be used to endorse or promote
- * products derived from this software without specific prior written
- * permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
- * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
- * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
- * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- * OF THE POSSIBILITY OF SUCH DAMAGE.
- */
 package com.jcabi.github;
-
 import com.jcabi.aspects.Immutable;
 import com.rexsl.test.mock.MkAnswer;
 import com.rexsl.test.mock.MkContainer;
 import com.rexsl.test.mock.MkGrizzlyContainer;
 import com.rexsl.test.mock.MkQuery;
+import com.rexsl.test.request.FakeRequest;
 import com.rexsl.test.request.ApacheRequest;
 import java.net.HttpURLConnection;
 import javax.json.Json;
@@ -49,30 +20,25 @@ import org.mockito.Mockito;
  * @version $Id$
  * @since 0.8
  */
-@Immutable
-public final class RtContentsTest {
-
-    /**
+@Immutable public final class RtContentsTest {
+  /**
      * RtContents can fetch the default branch readme file.
      * @throws Exception if some problem inside.
      */
-    @Test
-    public void canFetchReadmeFile() throws Exception {
-        final MkContainer container = new MkGrizzlyContainer().next(
-            new MkAnswer.Simple(HttpURLConnection.HTTP_OK,
-                "{\"path\": \"README.md\"}")
-        ).start();
-        final Contents contents = new RtContents(
-            new ApacheRequest(container.home()), RtContentsTest.repo()
-        );
-        MatcherAssert.assertThat(
-            contents.readme(),
-            Matchers.notNullValue()
-        );
-        container.stop();
-    }
+  @Test public void canFetchReadmeFile() throws Exception {
+    final MkContainer container = new MkGrizzlyContainer().next(new MkAnswer.Simple(HttpURLConnection.HTTP_OK, "{\"path\": \"README.md\"}")).start();
+    final Contents contents = new RtContents(new 
+<<<<<<< /usr/src/app/output/jcabi/jcabi-github/25f907f6579e89ddd5f7aaccd2507a89a56b94c2/src/test/java/com/jcabi/github/RtContentsTest.java/left.java
+    ApacheRequest
+=======
+    FakeRequest
+>>>>>>> /usr/src/app/output/jcabi/jcabi-github/25f907f6579e89ddd5f7aaccd2507a89a56b94c2/src/test/java/com/jcabi/github/RtContentsTest.java/right.java
+    (container.home()), RtContentsTest.repo());
+    MatcherAssert.assertThat(contents.readme(), Matchers.notNullValue());
+    container.stop();
+  }
 
-    /**
+  /**
      * RtContents can fetch the readme file from the specified branch.
      *
      * @todo #119 RtContents should fetch the readme file for any branch.
@@ -80,106 +46,66 @@ public final class RtContentsTest {
      *  The method should receive the branch name as a parameter.
      *  When done, remove this puzzle and Ignore annotation from the method.
      */
-    @Test
-    @Ignore
-    public void canFetchReadmeFileFromSpecifiedBranch() {
-        // to be implemented
-    }
+  @Test @Ignore public void canFetchReadmeFileFromSpecifiedBranch() {
+  }
 
-    /**
+  /**
      * RtContents can fetch files from the repository.
      *
      * @todo #119 RtContents should be able to fetch files from the repository.
      *  Let's implement a test here and a method of RtContents.
      *  When done, remove this puzzle and Ignore annotation from the method.
      */
-    @Test
-    @Ignore
-    public void canFetchFilesFromRepository() {
-        // to be implemented
-    }
+  @Test @Ignore public void canFetchFilesFromRepository() {
+  }
 
-    /**
+  /**
      * RtContents can create a file in the repository.
      *
      * @todo #119 RtContents should be able to create files in the repository.
      *  Let's implement a test here and a method of RtContents.
      *  When done, remove this puzzle and Ignore annotation from the method.
      */
-    @Test
-    @Ignore
-    public void canCreateFilesFromRepository() {
-        // to be implemented
-    }
+  @Test @Ignore public void canCreateFilesFromRepository() {
+  }
 
-    /**
+  /**
      * RtContents can delete files from the repository.
      *
      * @throws Exception if a problem occurs.
      * @checkstyle MultipleStringLiteralsCheck (50 lines)
      */
-    @Test
-    public void canDeleteFilesFromRepository() throws Exception {
-        final MkContainer container = new MkGrizzlyContainer().next(
-            new MkAnswer.Simple(HttpURLConnection.HTTP_OK,
-                Json.createObjectBuilder().add(
-                    "commit",
-                    Json.createObjectBuilder()
-                        .add("sha", "commitSha")
-                        .build()
-                ).build().toString()
-            )
-        ).start();
-        final RtContents contents = new RtContents(
-            new ApacheRequest(container.home()),
-            repo()
-        );
-        try {
-            final Commit commit = contents.remove(
-                "to/remove", "Delete me", "fileSha"
-            );
-            MatcherAssert.assertThat(
-                commit.sha(),
-                Matchers.is("commitSha")
-            );
-            final MkQuery query = container.take();
-            MatcherAssert.assertThat(
-                query.body(),
-                Matchers.allOf(
-                    Matchers.containsString("\"message\":\"Delete me\""),
-                    Matchers.containsString("\"sha\":\"fileSha\"")
-                )
-            );
-            MatcherAssert.assertThat(
-                query.uri().toString(),
-                Matchers.endsWith("/repos/test/contents/contents/to/remove")
-            );
-        } finally {
-            container.stop();
-        }
+  @Test public void canDeleteFilesFromRepository() throws Exception {
+    final MkContainer container = new MkGrizzlyContainer().next(new MkAnswer.Simple(HttpURLConnection.HTTP_OK, Json.createObjectBuilder().add("commit", Json.createObjectBuilder().add("sha", "commitSha").build()).build().toString())).start();
+    final RtContents contents = new RtContents(new ApacheRequest(container.home()), repo());
+    try {
+      final Commit commit = contents.remove("to/remove", "Delete me", "fileSha");
+      MatcherAssert.assertThat(commit.sha(), Matchers.is("commitSha"));
+      final MkQuery query = container.take();
+      MatcherAssert.assertThat(query.body(), Matchers.allOf(Matchers.containsString("\"message\":\"Delete me\""), Matchers.containsString("\"sha\":\"fileSha\"")));
+      MatcherAssert.assertThat(query.uri().toString(), Matchers.endsWith("/repos/test/contents/contents/to/remove"));
+    }  finally {
+      container.stop();
     }
+  }
 
-    /**
+  /**
      * RtContents can update files into the repository.
      *
      * @todo #119 RtContents should be able to update files into the repository.
      *  Let's implement a test here and a method of RtContents.
      *  When done, remove this puzzle and Ignore annotation from the method.
      */
-    @Test
-    @Ignore
-    public void canUpdateFilesInRepository() {
-        // to be implemented
-    }
+  @Test @Ignore public void canUpdateFilesInRepository() {
+  }
 
-    /**
+  /**
      * Create and return repo for testing.
      * @return Repo
      */
-    private static Repo repo() {
-        final Repo repo = Mockito.mock(Repo.class);
-        Mockito.doReturn(new Coordinates.Simple("test", "contents"))
-            .when(repo).coordinates();
-        return repo;
-    }
+  private static Repo repo() {
+    final Repo repo = Mockito.mock(Repo.class);
+    Mockito.doReturn(new Coordinates.Simple("test", "contents")).when(repo).coordinates();
+    return repo;
+  }
 }
