8d3f56a3711b9ed6e9d08b71479a2ffde67cc912
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 17:36:57 +0000

    Mining Framework Analysis

--- javaparser-core/src/main/java/com/github/javaparser/ast/stmt/DoStmt.java
+++ javaparser-core/src/main/java/com/github/javaparser/ast/stmt/DoStmt.java
@@ -1,25 +1,4 @@
-/*
- * Copyright (C) 2007-2010 Júlio Vilmar Gesser.
- * Copyright (C) 2011, 2013-2016 The JavaParser Team.
- *
- * This file is part of JavaParser.
- *
- * JavaParser can be used either under the terms of
- * a) the GNU Lesser General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
- * b) the terms of the Apache License
- *
- * You should have received a copy of both licenses in LICENCE.LGPL and
- * LICENCE.APACHE. Please refer to those files for details.
- *
- * JavaParser is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- */
 package com.github.javaparser.ast.stmt;
-
 import com.github.javaparser.Range;
 import com.github.javaparser.ast.AllFieldsConstructor;
 import com.github.javaparser.ast.expr.BooleanLiteralExpr;
@@ -44,94 +23,82 @@ import com.github.javaparser.TokenRange;
  * @author Julio Vilmar Gesser
  */
 public final class DoStmt extends Statement implements NodeWithBody<DoStmt>, NodeWithCondition<DoStmt> {
+  private Statement body;
 
-    private Statement body;
+  private Expression condition;
 
-    private Expression condition;
+  public DoStmt() {
+    this(null, new ReturnStmt(), new BooleanLiteralExpr());
+  }
 
-    public DoStmt() {
-        this(null, new ReturnStmt(), new BooleanLiteralExpr());
+  @AllFieldsConstructor public DoStmt(final Statement body, final Expression condition) {
+    this(null, body, condition);
+  }
+
+  /**This constructor is used by the parser and is considered private.*/
+  @Generated(value = { "com.github.javaparser.generator.core.node.MainConstructorGenerator" }) public DoStmt(TokenRange tokenRange, Statement body, Expression condition) {
+    super(tokenRange);
+    setBody(body);
+    setCondition(condition);
+    customInitialization();
+  }
+
+  @Override public <R extends java.lang.Object, A extends java.lang.Object> R accept(final GenericVisitor<R, A> v, final A arg) {
+    return v.visit(this, arg);
+  }
+
+  @Override public <A extends java.lang.Object> void accept(final VoidVisitor<A> v, final A arg) {
+    v.visit(this, arg);
+  }
+
+  @Generated(value = { "com.github.javaparser.generator.core.node.PropertyGenerator" }) public Statement getBody() {
+    return body;
+  }
+
+  @Generated(value = { "com.github.javaparser.generator.core.node.PropertyGenerator" }) public Expression getCondition() {
+    return condition;
+  }
+
+  @Generated(value = { "com.github.javaparser.generator.core.node.PropertyGenerator" }) public DoStmt setBody(final Statement body) {
+    assertNotNull(body);
+    if (body == this.body) {
+      return (DoStmt) this;
     }
-
-    @AllFieldsConstructor
-    public DoStmt(final Statement body, final Expression condition) {
-        this(null, body, condition);
+    notifyPropertyChange(ObservableProperty.BODY, this.body, body);
+    if (this.body != null) {
+      this.body.setParentNode(null);
     }
+    this.body = body;
+    setAsParentNodeOf(body);
+    return this;
+  }
 
-    /**This constructor is used by the parser and is considered private.*/
-    @Generated("com.github.javaparser.generator.core.node.MainConstructorGenerator")
-    public DoStmt(TokenRange tokenRange, Statement body, Expression condition) {
-        super(tokenRange);
-        setBody(body);
-        setCondition(condition);
-        customInitialization();
+  @Generated(value = { "com.github.javaparser.generator.core.node.PropertyGenerator" }) public DoStmt setCondition(final Expression condition) {
+    assertNotNull(condition);
+    if (condition == this.condition) {
+      return (DoStmt) this;
     }
+    notifyPropertyChange(ObservableProperty.CONDITION, this.condition, condition);
+    if (this.condition != null) {
+      this.condition.setParentNode(null);
+    }
+    this.condition = condition;
+    setAsParentNodeOf(condition);
+    return this;
+  }
 
-    @Override
-    public <R, A> R accept(final GenericVisitor<R, A> v, final A arg) {
-        return v.visit(this, arg);
+  @Override @Generated(value = { "com.github.javaparser.generator.core.node.RemoveMethodGenerator" }) public boolean remove(Node node) {
+    if (node == null) {
+      return false;
     }
+    return super.remove(node);
+  }
 
-    @Override
-    public <A> void accept(final VoidVisitor<A> v, final A arg) {
-        v.visit(this, arg);
-    }
+  @Override @Generated(value = { "com.github.javaparser.generator.core.node.CloneGenerator" }) public DoStmt clone() {
+    return (DoStmt) accept(new CloneVisitor(), null);
+  }
 
-    @Generated("com.github.javaparser.generator.core.node.PropertyGenerator")
-    public Statement getBody() {
-        return body;
-    }
-
-    @Generated("com.github.javaparser.generator.core.node.PropertyGenerator")
-    public Expression getCondition() {
-        return condition;
-    }
-
-    @Generated("com.github.javaparser.generator.core.node.PropertyGenerator")
-    public DoStmt setBody(final Statement body) {
-        assertNotNull(body);
-        if (body == this.body) {
-            return (DoStmt) this;
-        }
-        notifyPropertyChange(ObservableProperty.BODY, this.body, body);
-        if (this.body != null)
-            this.body.setParentNode(null);
-        this.body = body;
-        setAsParentNodeOf(body);
-        return this;
-    }
-
-    @Generated("com.github.javaparser.generator.core.node.PropertyGenerator")
-    public DoStmt setCondition(final Expression condition) {
-        assertNotNull(condition);
-        if (condition == this.condition) {
-            return (DoStmt) this;
-        }
-        notifyPropertyChange(ObservableProperty.CONDITION, this.condition, condition);
-        if (this.condition != null)
-            this.condition.setParentNode(null);
-        this.condition = condition;
-        setAsParentNodeOf(condition);
-        return this;
-    }
-
-    @Override
-    @Generated("com.github.javaparser.generator.core.node.RemoveMethodGenerator")
-    public boolean remove(Node node) {
-        if (node == null)
-            return false;
-        return super.remove(node);
-    }
-
-    @Override
-    @Generated("com.github.javaparser.generator.core.node.CloneGenerator")
-    public DoStmt clone() {
-        return (DoStmt) accept(new CloneVisitor(), null);
-    }
-
-    @Override
-    @Generated("com.github.javaparser.generator.core.node.GetMetaModelGenerator")
-    public DoStmtMetaModel getMetaModel() {
-        return JavaParserMetaModel.doStmtMetaModel;
-    }
+  @Override @Generated(value = { "com.github.javaparser.generator.core.node.GetMetaModelGenerator" }) public DoStmtMetaModel getMetaModel() {
+    return JavaParserMetaModel.doStmtMetaModel;
+  }
 }
