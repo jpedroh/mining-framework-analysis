d360808bce4b46b8ccb81cc218bad3b56948011f
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 11:19:44 +0000

    Mining Framework Analysis

--- billy-france/src/test/java/com/premiumminds/billy/france/test/services/documents/handler/TestFRInvoiceIssuingHandler.java
+++ billy-france/src/test/java/com/premiumminds/billy/france/test/services/documents/handler/TestFRInvoiceIssuingHandler.java
@@ -1,134 +1,25 @@
-/*
- * Copyright (C) 2017 Premium Minds.
- *
- * This file is part of billy france (FR Pack).
- *
- * billy france (FR Pack) is free software: you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation, either version 3 of the License, or (at your option) any
- * later version.
- *
- * billy france (FR Pack) is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with billy france (FR Pack). If not, see <http://www.gnu.org/licenses/>.
- */
-package com.premiumminds.billy.france.test.services.documents.handler;
-
-import com.premiumminds.billy.core.exceptions.SeriesUniqueCodeNotFilled;
-import com.premiumminds.billy.core.services.exceptions.DocumentSeriesDoesNotExistException;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-import com.premiumminds.billy.core.services.UID;
-import com.premiumminds.billy.core.services.exceptions.DocumentIssuingException;
-import com.premiumminds.billy.france.persistence.dao.DAOFRInvoice;
-import com.premiumminds.billy.france.persistence.entities.FRInvoiceEntity;
-import com.premiumminds.billy.france.services.documents.FRInvoiceIssuingHandler;
-import com.premiumminds.billy.france.services.entities.FRInvoice;
-import com.premiumminds.billy.france.test.FRPersistencyAbstractTest;
-import com.premiumminds.billy.france.test.services.documents.FRDocumentAbstractTest;
-
-public class TestFRInvoiceIssuingHandler extends FRDocumentAbstractTest {
-
-    private FRInvoiceIssuingHandler handler;
-    private UID issuedInvoiceUID;
-
-    private String DEFAULT_SERIES = INVOICE_TYPE.FT + " " + FRPersistencyAbstractTest.DEFAULT_SERIES;
-
-    @BeforeEach
-    public void setUpNewInvoice() {
-        this.handler = this.getInstance(FRInvoiceIssuingHandler.class);
-
-        try {
-            FRInvoiceEntity invoice = this.newInvoice(INVOICE_TYPE.FT);
-
-            this.createSeries(invoice, this.DEFAULT_SERIES);
-
-            this.issueNewInvoice(this.handler, invoice, this.DEFAULT_SERIES);
-            this.issuedInvoiceUID = invoice.getUID();
-
-        } catch (DocumentIssuingException | DocumentSeriesDoesNotExistException | SeriesUniqueCodeNotFilled e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    @Test
-    public void testIssuedInvoiceSimple() {
-        FRInvoice issuedInvoice = this.getInstance(DAOFRInvoice.class).get(this.issuedInvoiceUID);
-
-        Assertions.assertEquals(this.DEFAULT_SERIES, issuedInvoice.getSeries());
-        Assertions.assertTrue(1 == issuedInvoice.getSeriesNumber());
-        String formatedNumber = this.DEFAULT_SERIES + "/1";
-        Assertions.assertEquals(formatedNumber, issuedInvoice.getNumber());
-    }
-
-    @Test
-    public void testIssuedInvoiceSameSeries()
-            throws DocumentIssuingException, DocumentSeriesDoesNotExistException, SeriesUniqueCodeNotFilled {
-        FRInvoice issuedInvoice = this.getInstance(DAOFRInvoice.class).get(this.issuedInvoiceUID);
-        Integer nextNumber = 2;
-
-        FRInvoiceEntity newInvoice = this.newInvoice(INVOICE_TYPE.FT);
-
-        UID newInvoiceUID = newInvoice.getUID();
-        newInvoice.setBusiness(issuedInvoice.getBusiness());
-
-        this.issueNewInvoice(this.handler, newInvoice, this.DEFAULT_SERIES);
-
-        FRInvoice lastInvoice = this.getInstance(DAOFRInvoice.class).get(newInvoiceUID);
-
-        Assertions.assertEquals(this.DEFAULT_SERIES, lastInvoice.getSeries());
-        Assertions.assertEquals(nextNumber, lastInvoice.getSeriesNumber());
-        String formatedNumber = this.DEFAULT_SERIES + "/" + nextNumber;
-        Assertions.assertEquals(formatedNumber, lastInvoice.getNumber());
-    }
-
-    @Test
-    public void testIssuedInvoiceDifferentSeries()
-            throws DocumentIssuingException, DocumentSeriesDoesNotExistException, SeriesUniqueCodeNotFilled {
-        Integer nextNumber = 1;
-        String newSeries = "FT NEW_SERIES";
-
-        FRInvoiceEntity newInvoice = this.newInvoice(INVOICE_TYPE.FT);
-
-        UID newInvoiceUID = newInvoice.getUID();
-        this.createSeries(newInvoice, newSeries);
-
-        this.issueNewInvoice(this.handler, newInvoice, newSeries);
-
-        FRInvoice issuedInvoice = this.getInstance(DAOFRInvoice.class).get(newInvoiceUID);
-
-        Assertions.assertEquals(newSeries, issuedInvoice.getSeries());
-        Assertions.assertEquals(nextNumber, issuedInvoice.getSeriesNumber());
-        String formatedNumber = newSeries + "/" + nextNumber;
-        Assertions.assertEquals(formatedNumber, issuedInvoice.getNumber());
-    }
-
-    @Test
-    public void testIssuedInvoiceSameSourceBilling()
-            throws DocumentIssuingException, DocumentSeriesDoesNotExistException, SeriesUniqueCodeNotFilled {
-        FRInvoiceEntity newInvoice = this.newInvoice(INVOICE_TYPE.FT);
-
-        UID newInvoiceUID = newInvoice.getUID();
-        this.createSeries(newInvoice, this.DEFAULT_SERIES);
-
-        this.issueNewInvoice(this.handler, newInvoice, this.DEFAULT_SERIES);
-
-        this.getInstance(DAOFRInvoice.class).get(newInvoiceUID);
-    }
-
-    @Test
-    public void testSeriesDoesNotExist() {
-        FRInvoiceEntity invoiceEntity = this.newInvoice(INVOICE_TYPE.FT);
-
-        Assertions.assertThrows(DocumentSeriesDoesNotExistException.class,
-                () -> this.issueNewInvoice(this.handler, invoiceEntity, "A RANDOM SERIES"));
-    }
-
-}
+  package        com . premiumminds . billy . france . test . services . documents . handler ;   import      com . premiumminds . billy . core . exceptions . SeriesUniqueCodeNotFilled ;  import     org . junit . jupiter . api . Assertions ;  import     org . junit . jupiter . api . BeforeEach ;  import     org . junit . jupiter . api . Test ;  import      com . premiumminds . billy . core . services . UID ;  import       com . premiumminds . billy . core . services . exceptions . DocumentIssuingException ;  import       com . premiumminds . billy . france . persistence . dao . DAOFRInvoice ;  import       com . premiumminds . billy . france . persistence . entities . FRInvoiceEntity ;  import       com . premiumminds . billy . france . services . documents . FRInvoiceIssuingHandler ;  import       com . premiumminds . billy . france . services . entities . FRInvoice ;  import      com . premiumminds . billy . france . test . FRPersistencyAbstractTest ;  import        com . premiumminds . billy . france . test . services . documents . FRDocumentAbstractTest ;  import       com . premiumminds . billy . core . services . exceptions . DocumentSeriesDoesNotExistException ;   public class TestFRInvoiceIssuingHandler  extends FRDocumentAbstractTest  {   private FRInvoiceIssuingHandler  handler ;   private UID  issuedInvoiceUID ;   private String  DEFAULT_SERIES =    INVOICE_TYPE . FT + " " +  FRPersistencyAbstractTest . DEFAULT_SERIES ;    @ BeforeEach public void setUpNewInvoice  ( )  {    this . handler =  this . getInstance  (  FRInvoiceIssuingHandler . class ) ;  try  {  FRInvoiceEntity  invoice =  this . newInvoice  (  INVOICE_TYPE . FT ) ;   this . createSeries  ( invoice ,  this . DEFAULT_SERIES ) ;   this . issueNewInvoice  (  this . handler , invoice ,  this . DEFAULT_SERIES ) ;    this . issuedInvoiceUID =  invoice . getUID  ( ) ; }  catch (   DocumentIssuingException | 
+<<<<<<<
+SeriesUniqueCodeNotFilled
+=======
+DocumentSeriesDoesNotExistException
+>>>>>>>
+ e )  {   e . printStackTrace  ( ) ; } }    @ Test public void testIssuedInvoiceSimple  ( )  {  FRInvoice  issuedInvoice =   this . getInstance  (  DAOFRInvoice . class ) . get  (  this . issuedInvoiceUID ) ;   Assertions . assertEquals  (  this . DEFAULT_SERIES ,  issuedInvoice . getSeries  ( ) ) ;   Assertions . assertTrue  (  1 ==  issuedInvoice . getSeriesNumber  ( ) ) ;  String  formatedNumber =   this . DEFAULT_SERIES + "/1" ;   Assertions . assertEquals  ( formatedNumber ,  issuedInvoice . getNumber  ( ) ) ; }    @ Test public void testIssuedInvoiceSameSeries  ( )  throws DocumentIssuingException , 
+<<<<<<<
+SeriesUniqueCodeNotFilled
+=======
+DocumentSeriesDoesNotExistException
+>>>>>>>
+  {  FRInvoice  issuedInvoice =   this . getInstance  (  DAOFRInvoice . class ) . get  (  this . issuedInvoiceUID ) ;  Integer  nextNumber = 2 ;  FRInvoiceEntity  newInvoice =  this . newInvoice  (  INVOICE_TYPE . FT ) ;  UID  newInvoiceUID =  newInvoice . getUID  ( ) ;   newInvoice . setBusiness  (  issuedInvoice . getBusiness  ( ) ) ;   this . issueNewInvoice  (  this . handler , newInvoice ,  this . DEFAULT_SERIES ) ;  FRInvoice  lastInvoice =   this . getInstance  (  DAOFRInvoice . class ) . get  ( newInvoiceUID ) ;   Assertions . assertEquals  (  this . DEFAULT_SERIES ,  lastInvoice . getSeries  ( ) ) ;   Assertions . assertEquals  ( nextNumber ,  lastInvoice . getSeriesNumber  ( ) ) ;  String  formatedNumber =    this . DEFAULT_SERIES + "/" + nextNumber ;   Assertions . assertEquals  ( formatedNumber ,  lastInvoice . getNumber  ( ) ) ; }    @ Test public void testIssuedInvoiceDifferentSeries  ( )  throws DocumentIssuingException , 
+<<<<<<<
+SeriesUniqueCodeNotFilled
+=======
+DocumentSeriesDoesNotExistException
+>>>>>>>
+  {  Integer  nextNumber = 1 ;  String  newSeries = "FT NEW_SERIES" ;  FRInvoiceEntity  newInvoice =  this . newInvoice  (  INVOICE_TYPE . FT ) ;  UID  newInvoiceUID =  newInvoice . getUID  ( ) ;   this . createSeries  ( newInvoice , newSeries ) ;   this . issueNewInvoice  (  this . handler , newInvoice , newSeries ) ;  FRInvoice  issuedInvoice =   this . getInstance  (  DAOFRInvoice . class ) . get  ( newInvoiceUID ) ;   Assertions . assertEquals  ( newSeries ,  issuedInvoice . getSeries  ( ) ) ;   Assertions . assertEquals  ( nextNumber ,  issuedInvoice . getSeriesNumber  ( ) ) ;  String  formatedNumber =   newSeries + "/" + nextNumber ;   Assertions . assertEquals  ( formatedNumber ,  issuedInvoice . getNumber  ( ) ) ; }    @ Test public void testIssuedInvoiceSameSourceBilling  ( )  throws DocumentIssuingException , 
+<<<<<<<
+SeriesUniqueCodeNotFilled
+=======
+DocumentSeriesDoesNotExistException
+>>>>>>>
+  {  FRInvoiceEntity  newInvoice =  this . newInvoice  (  INVOICE_TYPE . FT ) ;  UID  newInvoiceUID =  newInvoice . getUID  ( ) ;   this . createSeries  ( newInvoice ,  this . DEFAULT_SERIES ) ;   this . issueNewInvoice  (  this . handler , newInvoice ,  this . DEFAULT_SERIES ) ;    this . getInstance  (  DAOFRInvoice . class ) . get  ( newInvoiceUID ) ; }    @ Test public void testSeriesDoesNotExist  ( )  {  FRInvoiceEntity  invoiceEntity =  this . newInvoice  (  INVOICE_TYPE . FT ) ;   Assertions . assertThrows  (  DocumentSeriesDoesNotExistException . class ,   ( ) ->  this . issueNewInvoice  (  this . handler , invoiceEntity , "A RANDOM SERIES" ) ) ; } }
--- billy-spain/src/test/java/com/premiumminds/billy/spain/test/services/documents/handler/TestESReceiptIssuingHandler.java
+++ billy-spain/src/test/java/com/premiumminds/billy/spain/test/services/documents/handler/TestESReceiptIssuingHandler.java
@@ -1,68 +1,7 @@
-/*
- * Copyright (C) 2017 Premium Minds.
- *
- * This file is part of billy spain (ES Pack).
- *
- * billy spain (ES Pack) is free software: you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation, either version 3 of the License, or (at your option) any
- * later version.
- *
- * billy spain (ES Pack) is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with billy spain (ES Pack). If not, see <http://www.gnu.org/licenses/>.
- */
-package com.premiumminds.billy.spain.test.services.documents.handler;
-
-import com.premiumminds.billy.core.exceptions.SeriesUniqueCodeNotFilled;
-import com.premiumminds.billy.core.services.exceptions.DocumentSeriesDoesNotExistException;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-import com.premiumminds.billy.core.services.UID;
-import com.premiumminds.billy.core.services.exceptions.DocumentIssuingException;
-import com.premiumminds.billy.spain.persistence.dao.DAOESReceipt;
-import com.premiumminds.billy.spain.persistence.entities.ESReceiptEntity;
-import com.premiumminds.billy.spain.services.documents.ESReceiptIssuingHandler;
-import com.premiumminds.billy.spain.services.entities.ESReceipt;
-import com.premiumminds.billy.spain.test.ESPersistencyAbstractTest;
-import com.premiumminds.billy.spain.test.services.documents.ESDocumentAbstractTest;
-
-public class TestESReceiptIssuingHandler extends ESDocumentAbstractTest {
-
-    private ESReceiptIssuingHandler handler;
-    private UID issuedReceiptUID;
-
-    private String DEFAULT_SERIES = INVOICE_TYPE.RC + " " + ESPersistencyAbstractTest.DEFAULT_SERIES;
-
-    @BeforeEach
-    public void setUpNewReceipt() {
-        this.handler = this.getInstance(ESReceiptIssuingHandler.class);
-
-        ESReceiptEntity receipt = this.newInvoice(INVOICE_TYPE.RC);
-        try {
-            this.createSeries(receipt, this.DEFAULT_SERIES);
-
-            this.issueNewInvoice(this.handler, receipt, this.DEFAULT_SERIES);
-
-            this.issuedReceiptUID = receipt.getUID();
-        } catch (DocumentIssuingException | DocumentSeriesDoesNotExistException | SeriesUniqueCodeNotFilled e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Test
-    public void testIssueReceipt() {
-        ESReceipt issuedReceipt = this.getInstance(DAOESReceipt.class).get(this.issuedReceiptUID);
-
-        Assertions.assertEquals(this.DEFAULT_SERIES, issuedReceipt.getSeries());
-        Assertions.assertTrue(1 == issuedReceipt.getSeriesNumber());
-        String formatedNumber = this.DEFAULT_SERIES + "/1";
-        Assertions.assertEquals(formatedNumber, issuedReceipt.getNumber());
-    }
-}
+  package        com . premiumminds . billy . spain . test . services . documents . handler ;   import      com . premiumminds . billy . core . exceptions . SeriesUniqueCodeNotFilled ;  import     org . junit . jupiter . api . Assertions ;  import     org . junit . jupiter . api . BeforeEach ;  import     org . junit . jupiter . api . Test ;  import      com . premiumminds . billy . core . services . UID ;  import       com . premiumminds . billy . core . services . exceptions . DocumentIssuingException ;  import       com . premiumminds . billy . spain . persistence . dao . DAOESReceipt ;  import       com . premiumminds . billy . spain . persistence . entities . ESReceiptEntity ;  import       com . premiumminds . billy . spain . services . documents . ESReceiptIssuingHandler ;  import       com . premiumminds . billy . spain . services . entities . ESReceipt ;  import      com . premiumminds . billy . spain . test . ESPersistencyAbstractTest ;  import        com . premiumminds . billy . spain . test . services . documents . ESDocumentAbstractTest ;  import       com . premiumminds . billy . core . services . exceptions . DocumentSeriesDoesNotExistException ;   public class TestESReceiptIssuingHandler  extends ESDocumentAbstractTest  {   private ESReceiptIssuingHandler  handler ;   private UID  issuedReceiptUID ;   private String  DEFAULT_SERIES =    INVOICE_TYPE . RC + " " +  ESPersistencyAbstractTest . DEFAULT_SERIES ;    @ BeforeEach public void setUpNewReceipt  ( )  {    this . handler =  this . getInstance  (  ESReceiptIssuingHandler . class ) ;  ESReceiptEntity  receipt =  this . newInvoice  (  INVOICE_TYPE . RC ) ;  try  {   this . createSeries  ( receipt ,  this . DEFAULT_SERIES ) ;   this . issueNewInvoice  (  this . handler , receipt ,  this . DEFAULT_SERIES ) ;    this . issuedReceiptUID =  receipt . getUID  ( ) ; }  catch (   DocumentIssuingException | 
+<<<<<<<
+SeriesUniqueCodeNotFilled
+=======
+DocumentSeriesDoesNotExistException
+>>>>>>>
+ e )  {   e . printStackTrace  ( ) ; } }    @ Test public void testIssueReceipt  ( )  {  ESReceipt  issuedReceipt =   this . getInstance  (  DAOESReceipt . class ) . get  (  this . issuedReceiptUID ) ;   Assertions . assertEquals  (  this . DEFAULT_SERIES ,  issuedReceipt . getSeries  ( ) ) ;   Assertions . assertTrue  (  1 ==  issuedReceipt . getSeriesNumber  ( ) ) ;  String  formatedNumber =   this . DEFAULT_SERIES + "/1" ;   Assertions . assertEquals  ( formatedNumber ,  issuedReceipt . getNumber  ( ) ) ; } }
