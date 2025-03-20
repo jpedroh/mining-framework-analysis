31a15bf3b37060b82154658d46d39c4c8ebd1d5c
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 06:42:36 +0000

    Mining Framework Analysis

--- src/main/java/com/mitchellbosecke/pebble/extension/core/DateFilter.java
+++ src/main/java/com/mitchellbosecke/pebble/extension/core/DateFilter.java
@@ -1,98 +1,86 @@
-/*******************************************************************************
- * This file is part of Pebble.
- *
- * Copyright (c) 2014 by Mitchell Bösecke
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- ******************************************************************************/
 package com.mitchellbosecke.pebble.extension.core;
-
 import com.mitchellbosecke.pebble.error.PebbleException;
 import com.mitchellbosecke.pebble.extension.Filter;
 import com.mitchellbosecke.pebble.extension.escaper.SafeString;
 import com.mitchellbosecke.pebble.template.EvaluationContext;
 import com.mitchellbosecke.pebble.template.PebbleTemplate;
-
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.DateTimeException;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.TemporalAccessor;
 import java.util.ArrayList;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
+import java.time.temporal.Temporal;
 import java.util.List;
+import java.time.temporal.TemporalAccessor;
 import java.util.Locale;
 import java.util.Map;
-
 import static java.lang.String.format;
 
 public class DateFilter implements Filter {
+  private final List<String> argumentNames = new ArrayList<>();
 
-    private final List<String> argumentNames = new ArrayList<>();
+  public DateFilter() {
+    argumentNames.add("format");
+    argumentNames.add("existingFormat");
+  }
 
-    public DateFilter() {
-        argumentNames.add("format");
-        argumentNames.add("existingFormat");
+  @Override public List<String> getArgumentNames() {
+    return argumentNames;
+  }
+
+  @Override public Object apply(Object input, Map<String, Object> args, PebbleTemplate self, EvaluationContext context, int lineNumber) throws PebbleException {
+    if (input == null) {
+      return null;
     }
-
-    @Override
-    public List<String> getArgumentNames() {
-        return argumentNames;
-    }
-
-    @Override
-    public Object apply(Object input, Map<String, Object> args, PebbleTemplate self, EvaluationContext context, int lineNumber) throws PebbleException {
-        if (input == null) {
-            return null;
-        }
-        final Locale locale = context.getLocale();
-        final String format = (String) args.get("format");
-
-        if(TemporalAccessor.class.isAssignableFrom(input.getClass())) {
-            return applyTemporal((TemporalAccessor)input, self, locale, lineNumber, format);
-        }
-        return applyDate(input, self, locale, lineNumber, format, (String) args.get("existingFormat"));
-     }
-
-    private Object applyDate(Object dateOrString, final PebbleTemplate self, final Locale locale,
-        int lineNumber, final String format, final String existingFormatString) throws PebbleException {
-        Date date = null;
-        DateFormat existingFormat = null;
-        DateFormat intendedFormat = null;
-        if (existingFormatString != null) {
-            existingFormat = new SimpleDateFormat(existingFormatString, locale);
-            try {
-                date = existingFormat.parse(dateOrString.toString());
-            } catch (ParseException e) {
-                throw new PebbleException(e, String.format("Could not parse the string '%1' into a date.",
-                    dateOrString.toString()), lineNumber, self.getName());
-            }
+    final Locale locale = context.getLocale();
+    final String format = (String) args.get("format");
+    if (TemporalAccessor.class.isAssignableFrom(input.getClass())) {
+      return applyTemporal((TemporalAccessor) input, self, locale, lineNumber, format);
+    } else 
+<<<<<<< Unknown file: This is a bug in JDime.
+=======
+    {
+      if (input instanceof Date) {
+        date = (Date) input;
+      } else {
+        if (input instanceof Number) {
+          date = new Date(((Number) input).longValue());
         } else {
-            if (dateOrString instanceof Date) {
-                date = (Date) dateOrString;
-            } else if (dateOrString instanceof Number) {
-                date = new Date(((Number) dateOrString).longValue());
-            } else {
-                throw new IllegalArgumentException(format("Unsupported argument type: %s (value: %s)", dateOrString.getClass().getName(), dateOrString));
-            }
+          throw new IllegalArgumentException(format("Unsupported argument type: %s (value: %s)", input.getClass().getName(), input));
         }
-        intendedFormat = new SimpleDateFormat(format, locale);
-        return new SafeString(intendedFormat.format(date));
+      }
     }
+>>>>>>> /usr/src/app/output/mbosecke/pebble/1d6c48cea436f9c099a0ca56ba3cbad12c5d8db5/src/main/java/com/mitchellbosecke/pebble/extension/core/DateFilter.java/right.java
 
-    private Object applyTemporal(final TemporalAccessor input, PebbleTemplate self, final Locale locale,
-        int lineNumber, final String format) throws PebbleException {
-        final DateTimeFormatter formatter = format != null
-            ? DateTimeFormatter.ofPattern(format, locale)
-            : DateTimeFormatter.ISO_DATE_TIME;
-        try {
-            return new SafeString(formatter.format(input));
-        } catch (DateTimeException dte) {
-            throw new PebbleException(dte, String.format("Could not parse the string '%1' into a date.",
-                input.toString()), lineNumber, self.getName());
-        }
+    return applyDate(input, self, locale, lineNumber, format, (String) args.get("existingFormat"));
+  }
+
+  private Object applyDate(Object dateOrString, final PebbleTemplate self, final Locale locale, int lineNumber, final String format, final String existingFormatString) throws PebbleException {
+    Date date = null;
+    DateFormat existingFormat = null;
+    DateFormat intendedFormat = null;
+    if (existingFormatString != null) {
+      existingFormat = new SimpleDateFormat(existingFormatString, locale);
+      try {
+        date = existingFormat.parse(dateOrString.toString());
+      } catch (ParseException e) {
+        throw new PebbleException(e, String.format("Could not parse the string \'%1\' into a date.", dateOrString.toString()), lineNumber, self.getName());
+      }
+    } else {
+      date = (Date) dateOrString;
     }
+    intendedFormat = new SimpleDateFormat(format, locale);
+    return new SafeString(intendedFormat.format(date));
+  }
 
+  private Object applyTemporal(final TemporalAccessor input, PebbleTemplate self, final Locale locale, int lineNumber, final String format) throws PebbleException {
+    final DateTimeFormatter formatter = format != null ? DateTimeFormatter.ofPattern(format, locale) : DateTimeFormatter.ISO_DATE_TIME;
+    try {
+      return new SafeString(formatter.format(input));
+    } catch (DateTimeException dte) {
+      throw new PebbleException(dte, String.format("Could not parse the string \'%1\' into a date.", input.toString()), lineNumber, self.getName());
+    }
+  }
 }
