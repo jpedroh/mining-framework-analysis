9b6fe71fffa868fb649799962f152eb33e6516cb
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 19:45:20 +0000

    Mining Framework Analysis

--- src/test/java/org/selophane/elements/helpers/FormTestObject.java
+++ src/test/java/org/selophane/elements/helpers/FormTestObject.java
@@ -1,69 +1,56 @@
 package org.selophane.elements.helpers;
-
 import org.selophane.elements.*;
 import org.selophane.elements.impl.internal.ElementFactory;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-
 import java.util.List;
 
 /**
  * declare elements of a form.
  */
 public class FormTestObject {
+  private WebDriver driver;
 
-    private WebDriver driver;
+  public TextInput texta;
 
-    public TextInput texta;
+  @FindBy(id = "test1") public Element element;
 
-    @FindBy(id = "test1")
-    public Element element;
+  @FindBy(id = "test1") public WebElement webElement;
 
-    @FindBy(id = "test1")
-    public WebElement webElement;
+  @FindBy(id = "option1") public Select option1;
 
-    @FindBy(id = "option1")
-    public Select option1;
+  @FindBy(id = "checkbox") public CheckBox checkbox;
 
-    @FindBy(id = "checkbox")
-    public CheckBox checkbox;
-    
-    @FindBy(id = "table")
-    public Table table;
+  @FindBy(id = "table") public Table table;
 
-    @FindBy(tagName = "label")
-    public List<Label> labels;
+  @FindBy(tagName = "label") public List<Label> labels;
 
-    @FindBy(tagName = "label")
-    public List<Element> elementLabels;
+  @FindBy(tagName = "label") public List<Element> elementLabels;
 
-    @FindBy(tagName = "label")
-    public List<WebElement> webElementLabels;
+  @FindBy(tagName = "label") public List<WebElement> webElementLabels;
 
-    @FindBy(css = "label[for='textb']")
-    public WebElement labelForTextB;
+  @FindBy(css = "label[for=\'textb\']") public WebElement labelForTextB;
 
-    public FormTestObject(WebDriver driver) {
-        this.driver = driver;
-    }
+  public FormTestObject(WebDriver driver) {
+    this.driver = driver;
+  }
 
-    /**
+  /**
      * Static factory for generating the class.
      *
      * @param driver The WebDriver for the session.
      * @return FormTestObject.
      */
-    public static FormTestObject initialize(WebDriver driver) {
-        return ElementFactory.initElements(driver, FormTestObject.class);
-    }
+  public static FormTestObject initialize(WebDriver driver) {
+    return ElementFactory.initElements(driver, FormTestObject.class);
+  }
 
-    public void get() {
-        PageLoader.get(driver, "forms.html");
-    }
+  public void get() {
+    PageLoader.get(driver, "forms.html");
+  }
 
-    public void close() {
-        driver.close();
-    }
-
+  public void close() {
+    driver.close();
+  }
 }
