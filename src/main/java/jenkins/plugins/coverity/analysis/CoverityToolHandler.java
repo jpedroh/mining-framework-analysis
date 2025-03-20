a5c732bd7a90caebcdb6c5b42ba0b5e5f5cdb098
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 22:45:22 +0000

    Mining Framework Analysis

--- src/main/java/jenkins/plugins/coverity/analysis/CoverityToolHandler.java
+++ src/main/java/jenkins/plugins/coverity/analysis/CoverityToolHandler.java
@@ -1,15 +1,4 @@
-/*******************************************************************************
- * Copyright (c) 2016 Synopsys, Inc
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *    Synopsys, Inc - initial implementation and documentation
- *******************************************************************************/
 package jenkins.plugins.coverity.analysis;
-
 import com.coverity.ws.v6.*;
 import hudson.EnvVars;
 import hudson.FilePath;
@@ -19,7 +8,6 @@ import hudson.model.AbstractProject;
 import hudson.model.BuildListener;
 import hudson.util.ArgumentListBuilder;
 import jenkins.plugins.coverity.*;
-
 import java.io.File;
 import java.io.FilenameFilter;
 import java.io.IOException;
@@ -34,177 +22,142 @@ import java.util.Vector;
  * and {@link #getHandler(CoverityVersion)} to choose an appropriate subclass.}
  */
 public abstract class CoverityToolHandler {
-    /**
+  /**
      * Find a suitable {@link CoverityToolHandler} to run analysis.
      * @param version the version of Coverity analysis present on the {@link hudson.model.Node} where the analysis will
      *                run.
      * @return A {@link CoverityToolHandler} that can run the given version of analysis.
      */
-    public static CoverityToolHandler getHandler(CoverityVersion version) {
-        // A fail safe for testing so that when we have new versions of analysis and platform, the build steps will
-        // Automatically go to fresnotoolhandler
-        if (version.isCodeName() && !version.containsCodeName()){
-            return new FresnoToolHandler(version);
-        }
-
-        if(version.compareTo(CoverityVersion.VERSION_FRESNO) < 0) {
-            return new PreFresnoToolHandler();
-        } if(version.compareTo(CoverityVersion.VERSION_FRESNO) >= 0 && version.compareTo(CoverityVersion.VERSION_INDIO) < 0){
-            return new FresnoToolHandler(version);
-        } if(version.compareTo(CoverityVersion.VERSION_INDIO) >= 0 && version.compareTo(CoverityVersion.VERSION_JASPER) < 0){
-            return new IndioToolHandler(version);
-        } else {
-            return new JasperToolHandler(version);
-        }
+  public static CoverityToolHandler getHandler(CoverityVersion version) {
+    if (version.isCodeName() && !version.containsCodeName()) {
+      return new FresnoToolHandler(version);
     }
-
-    public static Collection<File> listFiles(
-            File directory,
-            FilenameFilter filter,
-            boolean recurse) {
-        Vector<File> files = new Vector<File>();
-        File[] entries = directory.listFiles();
-
-        for(File entry : entries) {
-            if(filter == null || filter.accept(directory, entry.getName())) {
-                files.add(entry);
-            }
-
-            if(recurse && entry.isDirectory()) {
-                files.addAll(listFiles(entry, filter, recurse));
-            }
-        }
-
-        return files;
+    if (version.compareTo(CoverityVersion.VERSION_FRESNO) < 0) {
+      return new PreFresnoToolHandler();
     }
-
-    public static File[] listFilesAsArray(
-            File directory,
-            FilenameFilter filter,
-            boolean recurse) {
-        Collection<File> files = listFiles(directory, filter, recurse);
-
-        File[] arr = new File[files.size()];
-        return files.toArray(arr);
+    if (version.compareTo(CoverityVersion.VERSION_FRESNO) >= 0 && version.compareTo(CoverityVersion.VERSION_INDIO) < 0) {
+      return new FresnoToolHandler(version);
     }
-
-    public abstract boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener, CoverityPublisher publisher) throws Exception;
-
-    public File[] findMsvscaOutputFiles(String dirName) {
-        File dir = new File(dirName);
-
-        return listFilesAsArray(dir, new FilenameFilter() {
-            public boolean accept(File dir, String filename) {
-                return filename.endsWith("CodeAnalysisLog.xml");
-            }
-        }, true);
+    if (version.compareTo(CoverityVersion.VERSION_INDIO) >= 0 && version.compareTo(CoverityVersion.VERSION_JASPER) < 0) {
+      return new IndioToolHandler(version);
+    } else {
+      return new JasperToolHandler(version);
     }
+  }
 
-    public File[] findAssemblies(String dirName) {
-        File dir = new File(dirName);
-
-        return listFilesAsArray(dir, new FilenameFilter() {
-            public boolean accept(File dir, String filename) {
-                if(filename.endsWith(".exe") || filename.endsWith(".dll")) {
-                    String pathname = dir.getAbsolutePath();
-                    pathname = pathname + File.separator + filename;
-                    ;
-                    String pdbFilename = pathname.replaceAll(".exe$", ".pdb");
-                    pdbFilename = pdbFilename.replaceAll(".dll$", ".pdb");
-
-                    File pdbFile = new File(pdbFilename);
-
-                    return pdbFile.exists();
-                }
-
-                return false;
-            }
-
-        }, true);
+  public static Collection<File> listFiles(File directory, FilenameFilter filter, boolean recurse) {
+    Vector<File> files = new Vector<File>();
+    File[] entries = directory.listFiles();
+    for (File entry : entries) {
+      if (filter == null || filter.accept(directory, entry.getName())) {
+        files.add(entry);
+      }
+      if (recurse && entry.isDirectory()) {
+        files.addAll(listFiles(entry, filter, recurse));
+      }
     }
+    return files;
+  }
 
-    public boolean covEmitWar(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener, String home, CoverityTempDir temp, List<String> javaWarFiles) throws IOException, InterruptedException {
-        String covEmitJava = "cov-emit-java";
-        covEmitJava = new FilePath(launcher.getChannel(), home).child("bin").child(covEmitJava).getRemote();
-        EnvVars envVars = build.getEnvironment(listener);
+  public static File[] listFilesAsArray(File directory, FilenameFilter filter, boolean recurse) {
+    Collection<File> files = listFiles(directory, filter, recurse);
+    File[] arr = new File[files.size()];
+    return files.toArray(arr);
+  }
 
-        List<String> cmd = new ArrayList<String>();
-        cmd.add(covEmitJava);
-        cmd.add("--dir");
-        cmd.add(temp.getTempDir().getRemote());
-        if(javaWarFiles != null && !javaWarFiles.isEmpty()){
-            for(String javaWarFile : javaWarFiles){
-                cmd.add("--webapp-archive");
-                cmd.add(javaWarFile);
-            }
+  public abstract boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener, CoverityPublisher publisher) throws Exception;
+
+  public File[] findMsvscaOutputFiles(String dirName) {
+    File dir = new File(dirName);
+    return listFilesAsArray(dir, new FilenameFilter() {
+      public boolean accept(File dir, String filename) {
+        return filename.endsWith("CodeAnalysisLog.xml");
+      }
+    }, true);
+  }
+
+  public File[] findAssemblies(String dirName) {
+    File dir = new File(dirName);
+    return listFilesAsArray(dir, new FilenameFilter() {
+      public boolean accept(File dir, String filename) {
+        if (filename.endsWith(".exe") || filename.endsWith(".dll")) {
+          String pathname = dir.getAbsolutePath();
+          pathname = pathname + File.separator + filename;
+          ;
+          String pdbFilename = pathname.replaceAll(".exe$", ".pdb");
+          pdbFilename = pdbFilename.replaceAll(".dll$", ".pdb");
+          File pdbFile = new File(pdbFilename);
+          return pdbFile.exists();
         }
+        return false;
+      }
+    }, true);
+  }
 
-        try {
-            CoverityLauncherDecorator.SKIP.set(true);
-
-            InvocationAssistance invocationAssistance = CoverityUtils.getInvocationAssistance(build);
-            boolean useAdvancedParser = false;
-
-            if(invocationAssistance != null && invocationAssistance.getUseAdvancedParser()){
-                useAdvancedParser = true;
-            }
-
-            int result = CoverityUtils.runCmd(cmd, build, launcher, listener, envVars, useAdvancedParser);
-
-            if(result != 0) {
-                listener.getLogger().println("[Coverity] " + covEmitJava + " returned " + result + ", aborting...");
-                return false;
-            }
-        } finally {
-            CoverityLauncherDecorator.SKIP.set(false);
-        }
-
-        return true;
+  public boolean covEmitWar(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener, String home, CoverityTempDir temp, List<String> javaWarFiles) throws IOException, InterruptedException {
+    String covEmitJava = "cov-emit-java";
+    covEmitJava = new FilePath(launcher.getChannel(), home).child("bin").child(covEmitJava).getRemote();
+    EnvVars envVars = build.getEnvironment(listener);
+    List<String> cmd = new ArrayList<String>();
+    cmd.add(covEmitJava);
+    cmd.add("--dir");
+    cmd.add(temp.getTempDir().getRemote());
+    if (javaWarFiles != null && !javaWarFiles.isEmpty()) {
+      for (String javaWarFile : javaWarFiles) {
+        cmd.add("--webapp-archive");
+        cmd.add(javaWarFile);
+      }
     }
-
-    public boolean importMsvsca(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener, String home,
-                                CoverityTempDir temp, boolean csharpMsvsca) throws IOException, InterruptedException {
-        String covImportMsvsca = "cov-import-msvsca";
-        covImportMsvsca = new FilePath(launcher.getChannel(), home).child("bin").child(covImportMsvsca).getRemote();
-        EnvVars envVars = build.getEnvironment(listener);
-
-        List<String> importCmd = new ArrayList<String>();
-        importCmd.add(covImportMsvsca);
-        importCmd.add("--dir");
-        importCmd.add(temp.getTempDir().getRemote());
-        importCmd.add("--append");
-
-        listener.getLogger().println("[Coverity] Searching for Microsoft Code Analysis results...");
-        File[] msvscaOutputFiles = {};
-
-        if(csharpMsvsca) {
-            msvscaOutputFiles = findMsvscaOutputFiles(build.getWorkspace().getRemote());
-        }
-
-        for(File outputFile : msvscaOutputFiles) {
-            importCmd.add(outputFile.getAbsolutePath());
-        }
-
-        if(msvscaOutputFiles.length == 0) {
-            listener.getLogger().println("[MSVSCA] MSVSCA No results found, skipping");
-        } else {
-            listener.getLogger().println("[MSVSCA] MSVSCA Import cmd so far is: " + importCmd.toString());
-
-            InvocationAssistance invocationAssistance = CoverityUtils.getInvocationAssistance(build);
-            boolean useAdvancedParser = false;
-
-            if(invocationAssistance != null && invocationAssistance.getUseAdvancedParser()){
-                useAdvancedParser = true;
-            }
-
-            int importResult = CoverityUtils.runCmd(importCmd, build, launcher, listener, envVars, useAdvancedParser);
-
-            if(importResult != 0) {
-                listener.getLogger().println("[Coverity] " + covImportMsvsca + " returned " + importResult + ", aborting...");
-                return false;
-            }
-        }
-
-        return true;
+    try {
+      CoverityLauncherDecorator.SKIP.set(true);
+      InvocationAssistance invocationAssistance = CoverityUtils.getInvocationAssistance(build);
+      boolean useAdvancedParser = false;
+      if (invocationAssistance != null && invocationAssistance.getUseAdvancedParser()) {
+        useAdvancedParser = true;
+      }
+      int result = CoverityUtils.runCmd(cmd, build, launcher, listener, envVars, useAdvancedParser);
+      if (result != 0) {
+        listener.getLogger().println("[Coverity] " + covEmitJava + " returned " + result + ", aborting...");
+        return false;
+      }
+    }  finally {
+      CoverityLauncherDecorator.SKIP.set(false);
     }
+    return true;
+  }
+
+  public boolean importMsvsca(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener, String home, CoverityTempDir temp, boolean csharpMsvsca) throws IOException, InterruptedException {
+    String covImportMsvsca = "cov-import-msvsca";
+    covImportMsvsca = new FilePath(launcher.getChannel(), home).child("bin").child(covImportMsvsca).getRemote();
+    EnvVars envVars = build.getEnvironment(listener);
+    List<String> importCmd = new ArrayList<String>();
+    importCmd.add(covImportMsvsca);
+    importCmd.add("--dir");
+    importCmd.add(temp.getTempDir().getRemote());
+    importCmd.add("--append");
+    listener.getLogger().println("[Coverity] Searching for Microsoft Code Analysis results...");
+    File[] msvscaOutputFiles = {  };
+    if (csharpMsvsca) {
+      msvscaOutputFiles = findMsvscaOutputFiles(build.getWorkspace().getRemote());
+    }
+    for (File outputFile : msvscaOutputFiles) {
+      importCmd.add(outputFile.getAbsolutePath());
+    }
+    if (msvscaOutputFiles.length == 0) {
+      listener.getLogger().println("[MSVSCA] MSVSCA No results found, skipping");
+    } else {
+      listener.getLogger().println("[MSVSCA] MSVSCA Import cmd so far is: " + importCmd.toString());
+      InvocationAssistance invocationAssistance = CoverityUtils.getInvocationAssistance(build);
+      boolean useAdvancedParser = false;
+      if (invocationAssistance != null && invocationAssistance.getUseAdvancedParser()) {
+        useAdvancedParser = true;
+      }
+      int importResult = CoverityUtils.runCmd(importCmd, build, launcher, listener, envVars, useAdvancedParser);
+      if (importResult != 0) {
+        listener.getLogger().println("[Coverity] " + covImportMsvsca + " returned " + importResult + ", aborting...");
+        return false;
+      }
+    }
+    return true;
+  }
 }
