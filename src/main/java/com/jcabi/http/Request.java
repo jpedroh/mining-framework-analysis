dab336424c1a02a701ffe501e559b370d7f1215b
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 01:04:35 +0000

    Mining Framework Analysis

--- src/main/java/com/jcabi/http/Request.java
+++ src/main/java/com/jcabi/http/Request.java
@@ -1,34 +1,4 @@
-/**
- * Copyright (c) 2011-2017, jcabi.com
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met: 1) Redistributions of source code must retain the above
- * copyright notice, this list of conditions and the following
- * disclaimer. 2) Redistributions in binary form must reproduce the above
- * copyright notice, this list of conditions and the following
- * disclaimer in the documentation and/or other materials provided
- * with the distribution. 3) Neither the name of the jcabi.com nor
- * the names of its contributors may be used to endorse or promote
- * products derived from this software without specific prior written
- * permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
- * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
- * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
- * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- * OF THE POSSIBILITY OF SUCH DAMAGE.
- */
 package com.jcabi.http;
-
 import com.jcabi.aspects.Immutable;
 import java.io.IOException;
 import java.io.InputStream;
@@ -78,110 +48,107 @@ import javax.net.ssl.SSLContext;
  * @see com.jcabi.http.request.JdkRequest
  * @see com.jcabi.http.request.ApacheRequest
  */
-@Immutable
-@SuppressWarnings({"PMD.AvoidDuplicateLiterals", "PMD.TooManyMethods"})
-public interface Request {
-
-    /**
+@Immutable @SuppressWarnings(value = { "PMD.AvoidDuplicateLiterals", "PMD.TooManyMethods" }) public interface Request {
+  /**
      * GET method name.
      */
-    String GET = "GET";
+  String GET = "GET";
 
-    /**
+  /**
      * POST method name.
      */
-    String POST = "POST";
+  String POST = "POST";
 
-    /**
+  /**
      * PUT method name.
      */
-    String PUT = "PUT";
+  String PUT = "PUT";
 
-    /**
+  /**
      * HEAD method name.
      */
-    String HEAD = "HEAD";
+  String HEAD = "HEAD";
 
-    /**
+  /**
      * DELETE method name.
      */
-    String DELETE = "DELETE";
+  String DELETE = "DELETE";
 
-    /**
+  /**
      * OPTIONS method name.
      */
-    String OPTIONS = "OPTIONS";
+  String OPTIONS = "OPTIONS";
 
-    /**
+  /**
      * PATCH method name.
      */
-    String PATCH = "PATCH";
+  String PATCH = "PATCH";
 
-    /**
+  /**
      * Get destination URI.
      * @return The destination it is currently pointing to
      */
-    RequestURI uri();
+  RequestURI uri();
 
-    /**
+  /**
      * Get request body.
      * @return New alternated request
      */
-    RequestBody body();
+  RequestBody body();
 
-    /**
+  /**
      * Get multipart form (multipart/form-data) body.
      * @return New altered request
      */
-    RequestBody multipartBody();
+  RequestBody multipartBody();
 
-    /**
+  /**
      * Set request header.
      * @param name ImmutableHeader name
      * @param value Value of the header to set
      * @return New alternated request
      */
-    Request header(String name, Object value);
+  Request header(String name, Object value);
 
-    /**
+  /**
      * Remove all headers with this name.
      * @param name ImmutableHeader name
      * @return New alternated request
      * @since 0.10
      */
-    Request reset(String name);
+  Request reset(String name);
 
-    /**
+  /**
      * Use this method.
      * @param method The method to use
      * @return New alternated request
      */
-    Request method(String method);
+  Request method(String method);
 
-    /**
+  /**
      * Use this timeout values.
      * @param connect The connect timeout to use in ms
      * @param read The read timeout to use in ms
      * @return New alternated request
      */
-    Request timeout(int connect, int read);
+  Request timeout(int connect, int read);
 
-    /**
+  /**
      * Use this SSL Context.
      *
      * @param context The SSL Context to use
      * @return New alternated request
      */
-    Request sslcontext(SSLContext context);
+  Request sslcontext(SSLContext context);
 
-    /**
+  /**
      * Execute it with a specified HTTP method.
      * @return Response
      * @throws IOException If fails to fetch HTTP request
      */
-    Response fetch() throws IOException;
+  Response fetch() throws IOException;
 
-    /**
+  /**
      * Execute this request using the content provided by the
      * {@link InputStream} being passed as the request body. Note that the
      * request MUST have an empty body when this method is being invoked, or
@@ -192,9 +159,9 @@ public interface Request {
      * @throws IOException If fails to fetch HTTP request
      * @since 1.8
      */
-    Response fetch(InputStream stream) throws IOException;
+  Response fetch(InputStream stream) throws IOException;
 
-    /**
+  /**
      * Send it through a decorating {@link Wire}.
      * @param type Type of wire to use
      * @param args Optional arguments for the wire constructor
@@ -202,13 +169,13 @@ public interface Request {
      * @return New request with a wire decorated
      * @since 0.10
      */
-    <T extends Wire> Request through(Class<T> type, Object... args);
+  <T extends Wire> Request through(Class<T> type, Object... args);
 
-    /**
+  /**
      * Send it through a decorating {@link Wire}.
      * @param wire Wire to use
      * @return New request with a wire decorated
      * @since 0.10
      */
-    Request through(Wire wire);
+  Request through(Wire wire);
 }
--- src/main/java/com/jcabi/http/request/ApacheRequest.java
+++ src/main/java/com/jcabi/http/request/ApacheRequest.java
@@ -1,34 +1,4 @@
-/**
- * Copyright (c) 2011-2017, jcabi.com
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met: 1) Redistributions of source code must retain the above
- * copyright notice, this list of conditions and the following
- * disclaimer. 2) Redistributions in binary form must reproduce the above
- * copyright notice, this list of conditions and the following
- * disclaimer in the documentation and/or other materials provided
- * with the distribution. 3) Neither the name of the jcabi.com nor
- * the names of its contributors may be used to endorse or promote
- * products derived from this software without specific prior written
- * permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
- * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
- * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
- * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- * OF THE POSSIBILITY OF SUCH DAMAGE.
- */
 package com.jcabi.http.request;
-
 import com.jcabi.aspects.Immutable;
 import com.jcabi.aspects.Loggable;
 import com.jcabi.http.ImmutableHeader;
@@ -70,47 +40,20 @@ import org.apache.http.util.EntityUtils;
  * @since 0.8
  * @checkstyle ClassDataAbstractionCoupling (500 lines)
  */
-@Immutable
-@EqualsAndHashCode(of = "base")
-@ToString(of = "base")
-@Loggable(Loggable.DEBUG)
-@SuppressWarnings("PMD.TooManyMethods")
-public final class ApacheRequest implements Request {
-
-    /**
+@Immutable @EqualsAndHashCode(of = "base") @ToString(of = "base") @Loggable(value = Loggable.DEBUG) @SuppressWarnings(value = { "PMD.TooManyMethods" }) public final class ApacheRequest implements Request {
+  /**
      * The wire to use.
      * @checkstyle AnonInnerLength (200 lines)
      */
-    private static final Wire WIRE = new Wire() {
-        // @checkstyle ParameterNumber (6 lines)
-        @Override
-        public Response send(final Request req, final String home,
-            final String method,
-            final Collection<Map.Entry<String, String>> headers,
-            final InputStream content,
-            final int connect,
-            final int read,
-            final SSLContext sslcontext) throws IOException {
-            final CloseableHttpClient client = HttpClientBuilder.create()
-                    .useSystemProperties()
-                    .setSSLContext(sslcontext)
-                    .build();
-            try (CloseableHttpResponse response = client.execute(
-                    this.httpRequest(
-                            home, method, headers, content,
-                            connect, read
-                    )
-            )) {
-                return new DefaultResponse(
-                        req,
-                        response.getStatusLine().getStatusCode(),
-                        response.getStatusLine().getReasonPhrase(),
-                        this.headers(response.getAllHeaders()),
-                        this.consume(response.getEntity())
-                );
-            }
-        }
-        /**
+  private static final Wire WIRE = new Wire() {
+    @Override public Response send(final Request req, final String home, final String method, final Collection<Map.Entry<String, String>> headers, final InputStream content, final int connect, final int read, final SSLContext sslcontext) throws IOException {
+      final CloseableHttpClient client = HttpClientBuilder.create().useSystemProperties().setSSLContext(sslcontext).build();
+      try (CloseableHttpResponse response = client.execute(this.httpRequest(home, method, headers, content, connect, read))) {
+        return new DefaultResponse(req, response.getStatusLine().getStatusCode(), response.getStatusLine().getReasonPhrase(), this.headers(response.getAllHeaders()), this.consume(response.getEntity()));
+      }
+    }
+
+    /**
          * Create request.
          * @param home Home URI
          * @param method Method to use
@@ -122,160 +65,126 @@ public final class ApacheRequest implements Request {
          * @throws IOException If an IO Exception occurs
          * @checkstyle ParameterNumber (6 lines)
          */
-        public HttpEntityEnclosingRequestBase httpRequest(final String home,
-            final String method,
-            final Collection<Map.Entry<String, String>> headers,
-            final InputStream content,
-            final int connect,
-            final int read) throws IOException {
-            final HttpEntityEnclosingRequestBase req =
-                new HttpEntityEnclosingRequestBase() {
-                    @Override
-                    public String getMethod() {
-                        return method;
-                    }
-                };
-            final URI uri = URI.create(home);
-            req.setConfig(
-                RequestConfig.custom()
-                    .setCircularRedirectsAllowed(false)
-                    .setRedirectsEnabled(false)
-                    .setConnectTimeout(connect)
-                    .setSocketTimeout(read)
-                    .build()
-            );
-            req.setURI(uri);
-            req.setEntity(
-                new BufferedHttpEntity(new InputStreamEntity(content))
-            );
-            for (final Map.Entry<String, String> header : headers) {
-                req.addHeader(header.getKey(), header.getValue());
-            }
-            return req;
+    public HttpEntityEnclosingRequestBase httpRequest(final String home, final String method, final Collection<Map.Entry<String, String>> headers, final InputStream content, final int connect, final int read) throws IOException {
+      final HttpEntityEnclosingRequestBase req = new HttpEntityEnclosingRequestBase() {
+        @Override public String getMethod() {
+          return method;
         }
-        /**
+      };
+      final URI uri = URI.create(home);
+      req.setConfig(RequestConfig.custom().setCircularRedirectsAllowed(false).setRedirectsEnabled(false).setConnectTimeout(connect).setSocketTimeout(read).build());
+      req.setURI(uri);
+      req.setEntity(new BufferedHttpEntity(new InputStreamEntity(content)));
+      for (final Map.Entry<String, String> header : headers) {
+        req.addHeader(header.getKey(), header.getValue());
+      }
+      return req;
+    }
+
+    /**
          * Fetch body from http entity.
          * @param entity HTTP entity
          * @return Body in UTF-8
          * @throws IOException If fails
          */
-        private byte[] consume(final HttpEntity entity) throws IOException {
-            final byte[] body;
-            if (entity == null) {
-                body = new byte[0];
-            } else {
-                body = EntityUtils.toByteArray(entity);
-            }
-            return body;
-        }
-        /**
+    private byte[] consume(final HttpEntity entity) throws IOException {
+      final byte[] body;
+      if (entity == null) {
+        body = new byte[0];
+      } else {
+        body = EntityUtils.toByteArray(entity);
+      }
+      return body;
+    }
+
+    /**
          * Make a list of all hdrs.
          * @param list Apache HTTP hdrs
          * @return Body in UTF-8
          */
-        @SuppressWarnings("PMD.AvoidInstantiatingObjectsInLoops")
-        private Array<Map.Entry<String, String>> headers(final Header... list) {
-            final Collection<Map.Entry<String, String>> headers =
-                new LinkedList<>();
-            for (final Header header : list) {
-                headers.add(
-                    new ImmutableHeader(
-                        header.getName(),
-                        header.getValue()
-                    )
-                );
-            }
-            return new Array<Map.Entry<String, String>>(headers);
-        }
-    };
+    @SuppressWarnings(value = { "PMD.AvoidInstantiatingObjectsInLoops" }) private Array<Map.Entry<String, String>> headers(final Header... list) {
+      final Collection<Map.Entry<String, String>> headers = new LinkedList<>();
+      for (final Header header : list) {
+        headers.add(new ImmutableHeader(header.getName(), header.getValue()));
+      }
+      return new Array<Map.Entry<String, String>>(headers);
+    }
+  };
 
-    /**
+  /**
      * Base request.
      */
-    private final transient Request base;
+  private final transient Request base;
 
-    /**
+  /**
      * Public ctor.
      * @param url The resource to work with
      */
-    public ApacheRequest(final URL url) {
-        this(url.toString());
-    }
+  public ApacheRequest(final URL url) {
+    this(url.toString());
+  }
 
-    /**
+  /**
      * Public ctor.
      * @param uri The resource to work with
      */
-    public ApacheRequest(final URI uri) {
-        this(uri.toString());
-    }
+  public ApacheRequest(final URI uri) {
+    this(uri.toString());
+  }
 
-    /**
+  /**
      * Public ctor.
      * @param uri The resource to work with
      */
-    public ApacheRequest(final String uri) {
-        this.base = new BaseRequest(ApacheRequest.WIRE, uri);
-    }
+  public ApacheRequest(final String uri) {
+    this.base = new BaseRequest(ApacheRequest.WIRE, uri);
+  }
 
-    @Override
-    public RequestURI uri() {
-        return this.base.uri();
-    }
+  @Override public RequestURI uri() {
+    return this.base.uri();
+  }
 
-    @Override
-    public Request header(final String name, final Object value) {
-        return this.base.header(name, value);
-    }
+  @Override public Request header(final String name, final Object value) {
+    return this.base.header(name, value);
+  }
 
-    @Override
-    public Request reset(final String name) {
-        return this.base.reset(name);
-    }
+  @Override public Request reset(final String name) {
+    return this.base.reset(name);
+  }
 
-    @Override
-    public RequestBody body() {
-        return this.base.body();
-    }
+  @Override public RequestBody body() {
+    return this.base.body();
+  }
 
-    @Override
-    public RequestBody multipartBody() {
-        return this.base.multipartBody();
-    }
+  @Override public RequestBody multipartBody() {
+    return this.base.multipartBody();
+  }
 
-    @Override
-    public Request method(final String method) {
-        return this.base.method(method);
-    }
+  @Override public Request method(final String method) {
+    return this.base.method(method);
+  }
 
-    @Override
-    public Request sslcontext(final SSLContext context) {
-        return this.base.sslcontext(context);
-    }
+  @Override public Request sslcontext(final SSLContext context) {
+    return this.base.sslcontext(context);
+  }
 
-    @Override
-    public Request timeout(final int connect, final int read) {
-        return this.base.timeout(connect, read);
-    }
+  @Override public Request timeout(final int connect, final int read) {
+    return this.base.timeout(connect, read);
+  }
 
-    @Override
-    public Response fetch() throws IOException {
-        return this.base.fetch();
-    }
+  @Override public Response fetch() throws IOException {
+    return this.base.fetch();
+  }
 
-    @Override
-    public Response fetch(final InputStream stream) throws IOException {
-        return this.base.fetch(stream);
-    }
+  @Override public Response fetch(final InputStream stream) throws IOException {
+    return this.base.fetch(stream);
+  }
 
-    @Override
-    public <T extends Wire> Request through(final Class<T> type,
-        final Object... args) {
-        return this.base.through(type, args);
-    }
+  @Override public <T extends Wire> Request through(final Class<T> type, final Object... args) {
+    return this.base.through(type, args);
+  }
 
-    @Override
-    public Request through(final Wire wire) {
-        return this.base.through(wire);
-    }
+  @Override public Request through(final Wire wire) {
+    return this.base.through(wire);
+  }
 }
