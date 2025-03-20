178a3236664c455914f0c1baaba48e6a9c624356
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 09:33:11 +0000

    Mining Framework Analysis

--- src/main/java/com/fincatto/documentofiscal/nfe400/classes/lote/envio/NFLoteEnvioRetorno.java
+++ src/main/java/com/fincatto/documentofiscal/nfe400/classes/lote/envio/NFLoteEnvioRetorno.java
@@ -1,5 +1,4 @@
 package com.fincatto.documentofiscal.nfe400.classes.lote.envio;
-
 import com.fincatto.documentofiscal.DFAmbiente;
 import com.fincatto.documentofiscal.DFBase;
 import com.fincatto.documentofiscal.DFUnidadeFederativa;
@@ -9,132 +8,118 @@ import org.simpleframework.xml.Attribute;
 import org.simpleframework.xml.Element;
 import org.simpleframework.xml.Namespace;
 import org.simpleframework.xml.Root;
-
 import java.time.ZonedDateTime;
 
-@Root(name = "retEnviNFe")
-@Namespace(reference = "http://www.portalfiscal.inf.br/nfe")
-public class NFLoteEnvioRetorno extends DFBase {
-    private static final long serialVersionUID = 7457383606779886158L;
-    
-    @Attribute(name = "versao")
-    private String versao;
-    
-    @Element(name = "tpAmb")
-    private DFAmbiente ambiente;
-    
-    @Element(name = "verAplic")
-    private String versaoAplicacao;
-    
-    @Element(name = "cStat")
-    private String status;
-    
-    @Element(name = "xMotivo")
-    private String motivo;
-    
-    @Element(name = "cUF", required = false)
-    private DFUnidadeFederativa uf;
-    
-    @Element(name = "dhRecbto")
-    private ZonedDateTime dataRecebimento;
+@Root(name = "retEnviNFe") @Namespace(reference = "http://www.portalfiscal.inf.br/nfe") public class NFLoteEnvioRetorno extends DFBase {
+  private static final long serialVersionUID = 7457383606779886158L;
 
-    @Element(name = "infRec", required = false)
-    private NFLoteEnvioRetornoRecebimentoInfo infoRecebimento;
+  @Attribute(name = "versao") private String versao;
 
-    @Element(name = "protNFe", required = false)
-    private String protocoloRecebimentoSincrono;
+  @Element(name = "tpAmb") private DFAmbiente ambiente;
 
-    @Element(name = "infProt", required = false)
-    private NFProtocoloInfo protocoloInfo;
+  @Element(name = "verAplic") private String versaoAplicacao;
 
-    @Element(name = "Signature", required = false)
-    private NFSignature assinatura;
+  @Element(name = "cStat") private String status;
 
-    public void setVersao(final String versao) {
-        this.versao = versao;
-    }
+  @Element(name = "xMotivo") private String motivo;
 
-    public void setAmbiente(final DFAmbiente ambiente) {
-        this.ambiente = ambiente;
-    }
+  @Element(name = "cUF", required = false) private DFUnidadeFederativa uf;
 
-    public void setVersaoAplicacao(final String versaoAplicacao) {
-        this.versaoAplicacao = versaoAplicacao;
-    }
+  @Element(name = "dhRecbto") private ZonedDateTime dataRecebimento;
 
-    public void setStatus(final String status) {
-        this.status = status;
-    }
+  @Element(name = "infRec", required = false) private NFLoteEnvioRetornoRecebimentoInfo infoRecebimento;
 
-    public void setMotivo(final String motivo) {
-        this.motivo = motivo;
-    }
+  @Element(name = "protNFe", required = false) private String protocoloRecebimentoSincrono;
 
-    public void setUf(final DFUnidadeFederativa uf) {
-        this.uf = uf;
-    }
-    
-    public void setDataRecebimento(final ZonedDateTime dataRecebimento) {
-        this.dataRecebimento = dataRecebimento;
-    }
+  @Element(name = "infProt", required = false) private NFProtocoloInfo protocoloInfo;
 
-    public NFLoteEnvioRetornoRecebimentoInfo getInfoRecebimento() {
-        return this.infoRecebimento;
-    }
+  @Element(name = "Signature", required = false) private NFSignature assinatura;
 
-    public void setInfoRecebimento(final NFLoteEnvioRetornoRecebimentoInfo infoRecebimento) {
-        this.infoRecebimento = infoRecebimento;
-    }
+  public void setVersao(final String versao) {
+    this.versao = versao;
+  }
 
-    public String getVersao() {
-        return this.versao;
-    }
+  public void setAmbiente(final DFAmbiente ambiente) {
+    this.ambiente = ambiente;
+  }
 
-    public DFAmbiente getAmbiente() {
-        return this.ambiente;
-    }
+  public void setVersaoAplicacao(final String versaoAplicacao) {
+    this.versaoAplicacao = versaoAplicacao;
+  }
 
-    public String getVersaoAplicacao() {
-        return this.versaoAplicacao;
-    }
+  public void setStatus(final String status) {
+    this.status = status;
+  }
 
-    public String getStatus() {
-        return this.status;
-    }
+  public void setMotivo(final String motivo) {
+    this.motivo = motivo;
+  }
 
-    public String getMotivo() {
-        return this.motivo;
-    }
+  public void setUf(final DFUnidadeFederativa uf) {
+    this.uf = uf;
+  }
 
-    public DFUnidadeFederativa getUf() {
-        return this.uf;
-    }
-    
-    public ZonedDateTime getDataRecebimento() {
-        return this.dataRecebimento;
-    }
+  public void setDataRecebimento(final ZonedDateTime dataRecebimento) {
+    this.dataRecebimento = dataRecebimento;
+  }
 
-    public String getProtocoloRecebimentoSincrono() {
-        return this.protocoloRecebimentoSincrono;
-    }
+  public NFLoteEnvioRetornoRecebimentoInfo getInfoRecebimento() {
+    return this.infoRecebimento;
+  }
 
-    public NFProtocoloInfo getProtocoloInfo() {
-        return this.protocoloInfo;
-    }
+  public void setInfoRecebimento(final NFLoteEnvioRetornoRecebimentoInfo infoRecebimento) {
+    this.infoRecebimento = infoRecebimento;
+  }
 
-    public void setProtocoloInfo(final NFProtocoloInfo protocoloInfo) {
-        this.protocoloInfo = protocoloInfo;
-    }
+  public String getVersao() {
+    return this.versao;
+  }
 
-    public void setProtocoloRecebimentoSincrono(final String protocoloRecebimentoSincrono) {
-        this.protocoloRecebimentoSincrono = protocoloRecebimentoSincrono;
-    }
+  public DFAmbiente getAmbiente() {
+    return this.ambiente;
+  }
 
-    public NFSignature getAssinatura() {
-        return assinatura;
-    }
+  public String getVersaoAplicacao() {
+    return this.versaoAplicacao;
+  }
 
-    public void setAssinatura(NFSignature assinatura) {
-        this.assinatura = assinatura;
-    }
+  public String getStatus() {
+    return this.status;
+  }
+
+  public String getMotivo() {
+    return this.motivo;
+  }
+
+  public DFUnidadeFederativa getUf() {
+    return this.uf;
+  }
+
+  public ZonedDateTime getDataRecebimento() {
+    return this.dataRecebimento;
+  }
+
+  public String getProtocoloRecebimentoSincrono() {
+    return this.protocoloRecebimentoSincrono;
+  }
+
+  public NFProtocoloInfo getProtocoloInfo() {
+    return this.protocoloInfo;
+  }
+
+  public void setProtocoloInfo(final NFProtocoloInfo protocoloInfo) {
+    this.protocoloInfo = protocoloInfo;
+  }
+
+  public void setProtocoloRecebimentoSincrono(final String protocoloRecebimentoSincrono) {
+    this.protocoloRecebimentoSincrono = protocoloRecebimentoSincrono;
+  }
+
+  public NFSignature getAssinatura() {
+    return assinatura;
+  }
+
+  public void setAssinatura(NFSignature assinatura) {
+    this.assinatura = assinatura;
+  }
 }
