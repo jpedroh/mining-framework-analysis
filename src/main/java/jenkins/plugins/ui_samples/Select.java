46ea12513453fa34abe0812a599ec3990214d456
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 04:59:35 +0000

    Mining Framework Analysis

--- src/main/java/jenkins/plugins/ui_samples/Select.java
+++ src/main/java/jenkins/plugins/ui_samples/Select.java
@@ -1,5 +1,4 @@
 package jenkins.plugins.ui_samples;
-
 import hudson.DescriptorExtensionList;
 import hudson.Extension;
 import hudson.ExtensionPoint;
@@ -11,7 +10,6 @@ import hudson.util.XStream2;
 import java.io.IOException;
 import java.util.List;
 import javax.servlet.ServletException;
-
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.StaplerRequest;
 import org.kohsuke.stapler.StaplerResponse;
@@ -19,85 +17,79 @@ import org.kohsuke.stapler.StaplerResponse;
 /**
  * @author Alan.Harder@oracle.com
  */
-@Extension
-public class Select extends UISample {
-    @Override
-    public String getIconFileName() {
-        return "symbol-select";
+@Extension public class Select extends UISample {
+  @Override public String getIconFileName() {
+    return "symbol-select";
+  }
+
+  @Override public String getDescription() {
+    return "Show different form elements based on choice in a &lt;select> control";
+  }
+
+  public Fruit getFruit() {
+    return null;
+  }
+
+  public DescriptorExtensionList<Fruit, Descriptor<Fruit>> getFruitDescriptors() {
+    return Jenkins.get().getDescriptorList(Fruit.class);
+  }
+
+  public void doSubmit(StaplerRequest req, StaplerResponse rsp) throws ServletException, IOException {
+    Fruit fruit = req.bindJSON(Fruit.class, req.getSubmittedForm().getJSONObject("fruit"));
+    rsp.setContentType("text/plain");
+    new XStream2().toXML(fruit, rsp.getWriter());
+  }
+
+  @Extension public static final class DescriptorImpl extends UISampleDescriptor {
+    public ListBoxModel doFillFruitItems() {
+      return new ListBoxModel(new ListBoxModel.Option("Apple"), new ListBoxModel.Option("Banana"));
+    }
+  }
+
+  public static abstract class Fruit implements ExtensionPoint, Describable<Fruit> {
+    protected String name;
+
+    protected Fruit(String name) {
+      this.name = name;
     }
 
-    @Override
-    public String getDescription() {
-        return "Show different form elements based on choice in a &lt;select> control";
+    public Descriptor<Fruit> getDescriptor() {
+      return Jenkins.get().getDescriptor(getClass());
+    }
+  }
+
+  public static class FruitDescriptor extends Descriptor<Fruit> {
+  }
+
+  public static class Apple extends Fruit {
+    private final int seeds;
+
+    @DataBoundConstructor public Apple(int seeds) {
+      super("Apple");
+      this.seeds = seeds;
     }
 
-    public Fruit getFruit() {
-        // Could return currently configured/saved item here to initialized form with this data
-        return null;
+    public int getSeeds() {
+      return seeds;
     }
 
-    public DescriptorExtensionList<Fruit,Descriptor<Fruit>> getFruitDescriptors() {
-        return Jenkins.get().getDescriptorList(Fruit.class);
+    @Extension public static final class DescriptorImpl extends FruitDescriptor {
+    }
+  }
+
+  public static class Banana extends Fruit {
+    private final boolean yellow;
+
+    @DataBoundConstructor public Banana(boolean yellow) {
+      super("Banana");
+      this.yellow = yellow;
     }
 
-    // Process form data and show it as serialized XML
-    public void doSubmit(StaplerRequest req, StaplerResponse rsp) throws ServletException, IOException {
-        // '$class' in form data tells Stapler which Fruit subclass to use,
-        // older versions of Jenkins/Stapler used 'stapler-class'
-        Fruit fruit = req.bindJSON(Fruit.class, req.getSubmittedForm().getJSONObject("fruit"));
-        rsp.setContentType("text/plain");
-        new XStream2().toXML(fruit, rsp.getWriter());
+    public boolean isYellow() {
+      return yellow;
     }
 
-    @Extension
-    public static final class DescriptorImpl extends UISampleDescriptor {
-
-        public ListBoxModel doFillFruitItems() {
-            return new ListBoxModel(
-                    new ListBoxModel.Option("Apple"),
-                    new ListBoxModel.Option("Banana")
-            );
-        }
+    @Extension public static final class DescriptorImpl extends FruitDescriptor {
     }
-
-    public static abstract class Fruit implements ExtensionPoint, Describable<Fruit> {
-        protected String name;
-        protected Fruit(String name) { this.name = name; }
-
-        public Descriptor<Fruit> getDescriptor() {
-            return Jenkins.get().getDescriptor(getClass());
-        }
-    }
-
-    public static class FruitDescriptor extends Descriptor<Fruit> {}
-
-    public static class Apple extends Fruit {
-        private final int seeds;
-        @DataBoundConstructor public Apple(int seeds) {
-            super("Apple");
-            this.seeds = seeds;
-        }
-
-        public int getSeeds() {
-            return seeds;
-        }
-
-        @Extension
-        public static final class DescriptorImpl extends FruitDescriptor {}
-    }
-
-    public static class Banana extends Fruit {
-        private final boolean yellow;
-        @DataBoundConstructor public Banana(boolean yellow) {
-            super("Banana");
-            this.yellow = yellow;
-        }
-
-        public boolean isYellow() {
-            return yellow;
-        }
-
-        @Extension
-        public static final class DescriptorImpl extends FruitDescriptor {}
-    }
+  }
 }
