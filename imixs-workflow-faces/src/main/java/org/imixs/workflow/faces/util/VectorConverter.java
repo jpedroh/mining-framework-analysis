0f0ba9f95f2cedf3ff22ee7386f85ef45f6a39c8
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 01:50:32 +0000

    Mining Framework Analysis

--- imixs-workflow-faces/src/main/java/org/imixs/workflow/faces/util/VectorConverter.java
+++ imixs-workflow-faces/src/main/java/org/imixs/workflow/faces/util/VectorConverter.java
@@ -1,37 +1,7 @@
-/*  
- *  Imixs-Workflow 
- *  
- *  Copyright (C) 2001-2020 Imixs Software Solutions GmbH,  
- *  http://www.imixs.com
- *  
- *  This program is free software; you can redistribute it and/or 
- *  modify it under the terms of the GNU General Public License 
- *  as published by the Free Software Foundation; either version 2 
- *  of the License, or (at your option) any later version.
- *  
- *  This program is distributed in the hope that it will be useful, 
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of 
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
- *  General Public License for more details.
- *  
- *  You can receive a copy of the GNU General Public
- *  License at http://www.gnu.org/licenses/gpl.html
- *  
- *  Project: 
- *      https://www.imixs.org
- *      https://github.com/imixs/imixs-workflow
- *  
- *  Contributors:  
- *      Imixs Software Solutions GmbH - Project Management
- *      Ralph Soika - Software Developer
- */
-
 package org.imixs.workflow.faces.util;
-
 import java.util.List;
 import java.util.ListIterator;
 import java.util.Vector;
-
 import jakarta.faces.component.UIComponent;
 import jakarta.faces.context.FacesContext;
 import jakarta.faces.convert.Converter;
@@ -39,50 +9,42 @@ import jakarta.faces.convert.ConverterException;
 import jakarta.faces.convert.FacesConverter;
 
 /**
- * The VectorConverter can be used to convert a new-line separated list into a
- * vecotr and vice versa.
- * <p>
- * usage:
- * <p>
- * <code><h:inputTextarea value="#{value}" converter="org.imixs.VectorConverter" /></code>
+ * für ConfigItem benutzter Converter, der einen Komma-separierten String in
+ * einen Vektor umwandelt und umgekehrt.
  * 
- *
+ * Noch dringend zu tun: - Dem Converter im Fehlerfall noch eine eigene
+ * Fehlermeldung mitgeben - müssen da nicht noch eine Menge try-catch blöcke und
+ * Typ-Prüfungen rein? Derzeit geht das alles sehr optimistisch davon aus, dass
+ * in dem Vektor wirklich auch Strings drin sind; was eigentlich auch der Fall
+ * ist. Interessant wird es, wenn man bestehende Felder umbiegt.
+ * 
+ * Schön wäre noch folgendes: - Den Separator im converter-tag der JSP Seite
+ * definieren. Das wird allerdings ein Act (Vorgehen beschrieben in Kap. 20.4 in
+ * "Kito Mann - JSF in Action")
  */
-@SuppressWarnings("rawtypes")
-@FacesConverter(value = "org.imixs.VectorConverter")
-public class VectorConverter implements Converter {
+@SuppressWarnings(value = { "rawtypes" }) @FacesConverter(value = "org.imixs.VectorConverter") public class VectorConverter implements Converter {
+  private String separator = "\n";
 
-    private String separator = "\n";
-
-    @SuppressWarnings({ "unchecked" })
-    public Object getAsObject(FacesContext context, UIComponent component, String value) throws ConverterException {
-        // for backward compatibility we leave it a Vector even if a ArrayList would
-        // make more sense here.
-        Vector v = new Vector();
-        String[] tokens = value.split(separator);
-        for (int i = 0; i < tokens.length; i++) {
-            v.addElement(tokens[i].trim());
-        }
-        return v;
+  @SuppressWarnings(value = { "unchecked" }) public Object getAsObject(FacesContext context, UIComponent component, String value) throws ConverterException {
+    Vector v = new Vector();
+    String[] tokens = value.split(separator);
+    for (int i = 0; i < tokens.length; i++) {
+      v.addElement(tokens[i].trim());
     }
+    return v;
+  }
 
-    /**
-     * Converts a List of objects into a comma separated String
-     */
-    public String getAsString(FacesContext context, UIComponent component, Object value) throws ConverterException {
-        String result = "";
-        // we only support List objects
-        if (value instanceof List) {
-            ListIterator interator = ((List) value).listIterator();
-            while (interator.hasNext()) {
-                result = result + interator.next();
-                // append separator?
-                if (interator.hasNext()) {
-                    result = result + separator;
-                }
-            }
+  public String getAsString(FacesContext context, UIComponent component, Object value) throws ConverterException {
+    String result = "";
+    if (value instanceof List) {
+      ListIterator interator = ((List) value).listIterator();
+      while (interator.hasNext()) {
+        result = result + interator.next();
+        if (interator.hasNext()) {
+          result = result + separator;
         }
-        return result;
+      }
     }
-
+    return result;
+  }
 }
