fbe16dbaf8baac47b15f26739b329a11a3f5f348
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 19:20:12 +0000

    Mining Framework Analysis

--- javaparser-core/src/main/java/com/github/javaparser/ast/body/EnumDeclaration.java
+++ javaparser-core/src/main/java/com/github/javaparser/ast/body/EnumDeclaration.java
@@ -1,26 +1,4 @@
-/*
- * Copyright (C) 2007-2010 Júlio Vilmar Gesser.
- * Copyright (C) 2011, 2013-2016 The JavaParser Team.
- *
- * This file is part of JavaParser.
- *
- * JavaParser can be used either under the terms of
- * a) the GNU Lesser General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
- * b) the terms of the Apache License
- *
- * You should have received a copy of both licenses in LICENCE.LGPL and
- * LICENCE.APACHE. Please refer to those files for details.
- *
- * JavaParser is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- */
-
 package com.github.javaparser.ast.body;
-
 import com.github.javaparser.Range;
 import com.github.javaparser.ast.Modifier;
 import com.github.javaparser.ast.NodeList;
@@ -31,11 +9,9 @@ import com.github.javaparser.ast.observer.ObservableProperty;
 import com.github.javaparser.ast.type.ClassOrInterfaceType;
 import com.github.javaparser.ast.visitor.GenericVisitor;
 import com.github.javaparser.ast.visitor.VoidVisitor;
-
 import java.util.EnumSet;
 import java.util.LinkedList;
 import java.util.List;
-
 import static com.github.javaparser.utils.Utils.assertNotNull;
 
 /**
@@ -43,104 +19,74 @@ import static com.github.javaparser.utils.Utils.assertNotNull;
  * The declaration of an enum. <code>enum X { ... }</code>
  * @author Julio Vilmar Gesser
  */
-public final class EnumDeclaration extends TypeDeclaration<EnumDeclaration> implements
-        NodeWithImplements<EnumDeclaration> {
+public final class EnumDeclaration extends TypeDeclaration<EnumDeclaration> implements NodeWithImplements<EnumDeclaration> {
+  private NodeList<ClassOrInterfaceType> implementedTypes;
 
-    private NodeList<ClassOrInterfaceType> implementedTypes;
+  private NodeList<EnumConstantDeclaration> entries;
 
-    private NodeList<EnumConstantDeclaration> entries;
+  public EnumDeclaration() {
+    this(null, EnumSet.noneOf(Modifier.class), new NodeList<>(), new SimpleName(), new NodeList<>(), new NodeList<>(), new NodeList<>());
+  }
 
-    public EnumDeclaration() {
-        this(null,
-                EnumSet.noneOf(Modifier.class),
-                new NodeList<>(),
-                new SimpleName(),
-                new NodeList<>(),
-                new NodeList<>(),
-                new NodeList<>());
-    }
+  public EnumDeclaration(EnumSet<Modifier> modifiers, String name) {
+    this(null, modifiers, new NodeList<>(), new SimpleName(name), new NodeList<>(), new NodeList<>(), new NodeList<>());
+  }
 
-    public EnumDeclaration(EnumSet<Modifier> modifiers, String name) {
-        this(null,
-                modifiers,
-                new NodeList<>(),
-                new SimpleName(name),
-                new NodeList<>(),
-                new NodeList<>(),
-                new NodeList<>());
-    }
+  public EnumDeclaration(EnumSet<Modifier> modifiers, NodeList<AnnotationExpr> annotations, SimpleName name, NodeList<ClassOrInterfaceType> implementedTypes, NodeList<EnumConstantDeclaration> entries, NodeList<BodyDeclaration<?>> members) {
+    this(null, modifiers, annotations, name, implementedTypes, entries, members);
+  }
 
-    public EnumDeclaration(EnumSet<Modifier> modifiers, NodeList<AnnotationExpr> annotations, SimpleName name,
-                           NodeList<ClassOrInterfaceType> implementedTypes, NodeList<EnumConstantDeclaration> entries,
-                           NodeList<BodyDeclaration<?>> members) {
-        this(null,
-                modifiers,
-                annotations,
-                name,
-                implementedTypes,
-                entries,
-                members);
-    }
+  public EnumDeclaration(Range range, EnumSet<Modifier> modifiers, NodeList<AnnotationExpr> annotations, SimpleName name, NodeList<ClassOrInterfaceType> implementedTypes, NodeList<EnumConstantDeclaration> entries, NodeList<BodyDeclaration<?>> members) {
+    super(range, annotations, modifiers, name, members);
+    setImplementedTypes(implementedTypes);
+    setEntries(entries);
+  }
 
-    public EnumDeclaration(Range range, EnumSet<Modifier> modifiers, NodeList<AnnotationExpr> annotations, SimpleName name,
-                           NodeList<ClassOrInterfaceType> implementedTypes, NodeList<EnumConstantDeclaration> entries,
-                           NodeList<BodyDeclaration<?>> members) {
-        super(range, annotations, modifiers, name, members);
-        setImplementedTypes(implementedTypes);
-        setEntries(entries);
-    }
+  @Override public <R extends java.lang.Object, A extends java.lang.Object> R accept(GenericVisitor<R, A> v, A arg) {
+    return v.visit(this, arg);
+  }
 
-    @Override
-    public <R, A> R accept(GenericVisitor<R, A> v, A arg) {
-        return v.visit(this, arg);
-    }
+  @Override public <A extends java.lang.Object> void accept(VoidVisitor<A> v, A arg) {
+    v.visit(this, arg);
+  }
 
+  public NodeList<EnumConstantDeclaration> getEntries() {
+    return entries;
+  }
 
-    @Override
-    public <A> void accept(VoidVisitor<A> v, A arg) {
-        v.visit(this, arg);
-    }
+  public EnumConstantDeclaration getEntry(int i) {
+    return getEntries().get(i);
+  }
 
-    public NodeList<EnumConstantDeclaration> getEntries() {
-        return entries;
-    }
+  @Override public NodeList<ClassOrInterfaceType> getImplementedTypes() {
+    return implementedTypes;
+  }
 
-    public EnumConstantDeclaration getEntry(int i) {
-        return getEntries().get(i);
-    }
+  public EnumDeclaration setEntries(NodeList<EnumConstantDeclaration> entries) {
+    notifyPropertyChange(ObservableProperty.ENTRIES, this.entries, entries);
+    this.entries = assertNotNull(entries);
+    setAsParentNodeOf(this.entries);
+    return this;
+  }
 
-    @Override
-    public NodeList<ClassOrInterfaceType> getImplementedTypes() {
-        return implementedTypes;
-    }
+  @Override public EnumDeclaration setImplementedTypes(NodeList<ClassOrInterfaceType> implementsList) {
+    notifyPropertyChange(ObservableProperty.IMPLEMENTED_TYPES, this.implementedTypes, implementsList);
+    this.implementedTypes = assertNotNull(implementsList);
+    setAsParentNodeOf(this.implementedTypes);
+    return this;
+  }
 
-    public EnumDeclaration setEntries(NodeList<EnumConstantDeclaration> entries) {
-        notifyPropertyChange(ObservableProperty.ENTRIES, this.entries, entries);
-        this.entries = assertNotNull(entries);
-        setAsParentNodeOf(this.entries);
-        return this;
-    }
+  public EnumConstantDeclaration addEnumConstant(String name) {
+    EnumConstantDeclaration enumConstant = new EnumConstantDeclaration(assertNotNull(name));
+    getEntries().add(enumConstant);
+    enumConstant.setParentNode(this);
+    return enumConstant;
+  }
 
-    @Override
-    public EnumDeclaration setImplementedTypes(NodeList<ClassOrInterfaceType> implementsList) {
-        notifyPropertyChange(ObservableProperty.IMPLEMENTED_TYPES, this.implementedTypes, implementsList);
-        this.implementedTypes = assertNotNull(implementsList);
-        setAsParentNodeOf(this.implementedTypes);
-        return this;
-    }
-
-    public EnumConstantDeclaration addEnumConstant(String name) {
-        EnumConstantDeclaration enumConstant = new EnumConstantDeclaration(assertNotNull(name));
-        getEntries().add(enumConstant);
-        enumConstant.setParentNode(this);
-        return enumConstant;
-    }
-
-    @Override
-    public List<NodeList<?>> getNodeLists() {
-        List<NodeList<?>> res = new LinkedList<>(super.getNodeLists());
-        res.add(implementedTypes);
-        res.add(entries);
-        return res;
-    }
+  @Override public List<NodeList<?>> getNodeLists() {
+    List<NodeList<?>> res = new LinkedList<>(super.getNodeLists());
+    res.add(implementedTypes);
+    res.add(entries);
+    return res;
+  }
 }
