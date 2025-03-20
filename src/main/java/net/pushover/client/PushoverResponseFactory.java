0a62233b64d8367de62135e3c517aae70c40f004
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 23:30:30 +0000

    Mining Framework Analysis

--- src/main/java/net/pushover/client/PushoverResponseFactory.java
+++ src/main/java/net/pushover/client/PushoverResponseFactory.java
@@ -1,14 +1,10 @@
 package net.pushover.client;
-
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
-
-import org.apache.http.Header;
 import org.apache.http.HttpResponse;
 import org.apache.http.util.EntityUtils;
-
 import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
 import java.util.ArrayList;
@@ -18,105 +14,115 @@ import java.util.List;
  * encapsulate service response parsing / building
  */
 public class PushoverResponseFactory {
+  private static final Gson GSON = new Gson();
 
-    private static final Gson GSON = new Gson();
 
-    public static final String REQUEST_REMAINING_HEADER = "X-Limit-App-Remaining";
+<<<<<<< Unknown file: This is a bug in JDime.
+=======
+  public static final String REQUEST_REMAINING_HEADER = "X-Limit-App-Remaining";
+>>>>>>> /usr/src/app/output/sps/pushover4j/88cd636fabef766f358a3a81d802a07e940a5ceb/src/main/java/net/pushover/client/PushoverResponseFactory.java/right.java
 
-    public static Status createStatus(HttpResponse response) throws IOException {
 
-        if (response == null || response.getEntity() == null) {
-            throw new IOException("unreadable response!");
-        }
-
-        final String body = EntityUtils.toString(response.getEntity());
-
-        final Status toReturn;
-
-        try {
-            toReturn = GSON.fromJson(body, Status.class);
-        } catch (JsonSyntaxException e) {
-            throw new IOException(e.getCause());
-        }
-
-        return toReturn;
+  public static Status createStatus(HttpResponse response) throws IOException {
+    if (response == null || response.getEntity() == null) {
+      throw new IOException("unreadable response!");
     }
-    
-    public static Response createResponse(HttpResponse response) throws IOException {
-          if (response == null || response.getEntity() == null) {
-            throw new IOException("unreadable response!");
-        }
-
-        final String body = EntityUtils.toString(response.getEntity());
-
-        final Response toReturn;
-
-        try {
-            toReturn = GSON.fromJson(body, Response.class);
-        } catch (JsonSyntaxException e) {
-            throw new IOException(e.getCause());
-        }
-        
-        final Header responseId = response.getFirstHeader(REQUEST_REMAINING_HEADER);
-
-        if (responseId != null) {
-              try
-              {
-                    toReturn.setRemaining(Integer.parseInt(responseId.getValue()));
-              }
-              catch (Exception ex)
-              {
-                    toReturn.setRemaining(Integer.MIN_VALUE);  //on failure to parse just set to minimum value. easy to check for. You should never be that far in the hole.
-              }
-        }
-
-        return toReturn;
-    }
-    
-    public static Set<PushOverSound> createSoundSet(HttpResponse response) throws IOException {
-
-        if (response == null || response.getEntity() == null) {
-            throw new IOException("unreadable response!");
-        }
-
-        final String body = EntityUtils.toString(response.getEntity());
-
-        SoundResponse r;
-        try {
-            r = GSON.fromJson(body, SoundResponse.class);
-        } catch (JsonSyntaxException e) {
-            throw new IOException(e.getCause());
-        }
-
-        final Set<PushOverSound> sounds = new HashSet<PushOverSound>();
-        if (r.sounds != null) {
-            for (Map.Entry<String, String> e : r.sounds.entrySet()) {
-                sounds.add(new PushOverSound(e.getKey(), e.getValue()));
-            }
-        }
-        return sounds;
-    }
-    
-    public static Verification createVerification(HttpResponse response) throws IOException {
-        if (response == null || response.getEntity() == null) {
-            throw new IOException("unreadable response!");
-        }
-
-        final String body = EntityUtils.toString(response.getEntity());
-
-        Verification v;
-
-        try {
-            v = GSON.fromJson(body, Verification.class);
-        } catch (JsonSyntaxException e) {
-            throw new IOException(e.getCause());
-        }
-
-        return v;    
+    final String body = EntityUtils.toString(response.getEntity());
+    final Status toReturn;
+    try {
+      toReturn = GSON.fromJson(body, Status.class);
+    } catch (JsonSyntaxException e) {
+      throw new IOException(e.getCause());
     }
 
-    // {"sounds":{"id":"name",...},"status":1}
-    private static class SoundResponse {
-        Map<String, String> sounds;
+<<<<<<< /usr/src/app/output/sps/pushover4j/88cd636fabef766f358a3a81d802a07e940a5ceb/src/main/java/net/pushover/client/PushoverResponseFactory.java/left.java
+    if (m.request != null) {
+      toReturn.setRequestId(m.request);
     }
+=======
+>>>>>>> Unknown file: This is a bug in JDime.
+
+    return toReturn;
+  }
+
+  public static Response createResponse(HttpResponse response) throws IOException {
+    if (response == null || response.getEntity() == null) {
+      throw new IOException("unreadable response!");
+    }
+    final String body = EntityUtils.toString(response.getEntity());
+    final Response toReturn;
+    try {
+      toReturn = GSON.fromJson(body, Response.class);
+    } catch (JsonSyntaxException e) {
+      throw new IOException(e.getCause());
+    }
+    final Header responseId = response.getFirstHeader(REQUEST_REMAINING_HEADER);
+    if (responseId != null) {
+      try {
+        toReturn.setRemaining(Integer.parseInt(responseId.getValue()));
+      } catch (Exception ex) {
+        toReturn.setRemaining(Integer.MIN_VALUE);
+      }
+    }
+    return toReturn;
+  }
+
+  public static Set<PushOverSound> createSoundSet(HttpResponse response) throws IOException {
+    if (response == null || response.getEntity() == null) {
+      throw new IOException("unreadable response!");
+    }
+    final String body = EntityUtils.toString(response.getEntity());
+    SoundResponse r;
+    try {
+      r = GSON.fromJson(body, SoundResponse.class);
+    } catch (JsonSyntaxException e) {
+      throw new IOException(e.getCause());
+    }
+    final Set<PushOverSound> sounds = new HashSet<PushOverSound>();
+    if (r.sounds != null) {
+      for (Map.Entry<String, String> e : r.sounds.entrySet()) {
+        sounds.add(new PushOverSound(e.getKey(), e.getValue()));
+      }
+    }
+    return sounds;
+  }
+
+  public static Verification createVerification(HttpResponse response) throws IOException {
+    if (response == null || response.getEntity() == null) {
+      throw new IOException("unreadable response!");
+    }
+    final String body = EntityUtils.toString(response.getEntity());
+    Verification v;
+    try {
+      v = GSON.fromJson(body, Verification.class);
+    } catch (JsonSyntaxException e) {
+      throw new IOException(e.getCause());
+    }
+    return v;
+  }
+
+
+<<<<<<< /usr/src/app/output/sps/pushover4j/88cd636fabef766f358a3a81d802a07e940a5ceb/src/main/java/net/pushover/client/PushoverResponseFactory.java/left.java
+  private static class ResponseModel {
+    int status;
+
+    String request;
+
+    String user;
+
+    List<String> errors = new ArrayList<String>();
+
+    List<String> devices = new ArrayList<String>();
+
+    String receipt;
+
+    int remaining_messages;
+  }
+=======
+>>>>>>> Unknown file: This is a bug in JDime.
+
+
+  private static class SoundResponse {
+    Map<String, String> sounds;
+  }
 }
--- src/test/java/net/pushover/client/PushoverResponseFactoryTest.java
+++ src/test/java/net/pushover/client/PushoverResponseFactoryTest.java
@@ -1,5 +1,4 @@
 package net.pushover.client;
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
@@ -7,12 +6,10 @@ import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
-
 import org.apache.http.HttpResponse;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.message.BasicHeader;
@@ -20,161 +17,129 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class PushoverResponseFactoryTest {
+  private HttpResponse response;
 
-    private HttpResponse response;
+  @Before public void setUp() throws Exception {
+    assertNotNull(new PushoverResponseFactory());
+    response = mock(HttpResponse.class);
+  }
 
-    @Before
-    public void setUp() throws Exception {
-        assertNotNull(new PushoverResponseFactory());
-        response = mock(HttpResponse.class);
-    }
+  @Test(expected = IOException.class) public void testNullStausResponse() throws Exception {
+    assertNull(PushoverResponseFactory.createStatus(null));
+  }
 
-    @Test(expected = IOException.class)
-    public void testNullStausResponse() throws Exception {
-        assertNull(PushoverResponseFactory.createStatus(null));
-    }
+  @Test(expected = IOException.class) public void testNullEntityStatusResponse() throws Exception {
+    assertNull(PushoverResponseFactory.createStatus(response));
+  }
 
-    @Test(expected = IOException.class)
-    public void testNullEntityStatusResponse() throws Exception {
-        assertNull(PushoverResponseFactory.createStatus(response));
-    }
+  @Test(expected = IOException.class) public void testMalformedStatusResponse() throws IOException {
+    when(response.getEntity()).thenReturn(new StringEntity("{"));
+    PushoverResponseFactory.createStatus(response);
+  }
 
-    @Test(expected = IOException.class)
-    public void testMalformedStatusResponse() throws IOException {
-        when(response.getEntity()).thenReturn(new StringEntity("{"));
-        PushoverResponseFactory.createStatus(response);        
-    }
-    
-    @Test
-    public void testEmptyStatus() throws IOException {
-        when(response.getEntity()).thenReturn(new StringEntity("{}"));
-        Status status = PushoverResponseFactory.createStatus(response);
-        assertNotNull(status);
-    }
+  @Test public void testEmptyStatus() throws IOException {
+    when(response.getEntity()).thenReturn(new StringEntity("{}"));
+    Status status = PushoverResponseFactory.createStatus(response);
+    assertNotNull(status);
+  }
 
-    @Test
-    public void testOKStatus() throws IOException {
-        final String expectedRequestId = "1234";
+  @Test public void testOKStatus() throws IOException {
+    final String expectedRequestId = "1234";
+    when(response.getEntity()).thenReturn(new StringEntity("{\"status\":1, \"request\":\"" + expectedRequestId + "\"}"));
+    final Status status = PushoverResponseFactory.createStatus(response);
+    assertNotNull(status);
+    assertEquals(status.getStatus(), 1);
+    assertEquals(status.getRequestId(), expectedRequestId);
+  }
 
-        when(response.getEntity()).thenReturn(new StringEntity("{\"status\":1, \"request\":\"" + expectedRequestId +"\"}"));
+  @Test public void testOKVerification() throws IOException {
+    final String expectedRequestId = "1234";
+    final List<String> expectedDevices = new ArrayList<String>() {
+      {
+        add("abc");
+        add("efg");
+        add("123");
+      }
+    };
+    when(response.getEntity()).thenReturn(new StringEntity("{\"status\":1, \"request\":\"" + expectedRequestId + "\",\"devices\": " + expectedDevices.toString() + "}"));
+    final Verification status = PushoverResponseFactory.createVerification(response);
+    assertNotNull(status);
+    assertEquals(status.getStatus(), 1);
+    assertEquals(status.getRequest(), expectedRequestId);
+    assertEquals(status.getDevices(), expectedDevices);
+  }
 
-        final Status status = PushoverResponseFactory.createStatus(response);
-        
-        assertNotNull(status);
-        assertEquals(status.getStatus(), 1);
-        assertEquals(status.getRequestId(), expectedRequestId);
-    }
-    
-    @Test
-    public void testOKResponse() throws IOException {
-        final String expectedRequestId = "1234";
-        final int expectedRemaining = 4321;
+  @Test public void testOKResponse() throws IOException {
+    final String expectedRequestId = "1234";
+    final int expectedRemaining = 4321;
+    when(response.getEntity()).thenReturn(new StringEntity("{\"status\":1, \"request\":\"" + expectedRequestId + "\"}"));
+    when(response.getFirstHeader(PushoverResponseFactory.REQUEST_REMAINING_HEADER)).thenReturn(new BasicHeader(PushoverResponseFactory.REQUEST_REMAINING_HEADER, String.valueOf(expectedRemaining)));
+    final Response status = PushoverResponseFactory.createResponse(response);
+    assertNotNull(status);
+    assertEquals(status.getStatus(), 1);
+    assertEquals(status.getRequest(), expectedRequestId);
+    assertEquals(status.getRemaining(), expectedRemaining);
+  }
 
-        when(response.getEntity()).thenReturn(new StringEntity("{\"status\":1, \"request\":\"" + expectedRequestId +"\"}"));
+  @Test public void testFailedVerification() throws IOException {
+    final String expectedRequestId = "1234";
+    final List<String> expectedDevices = new ArrayList<String>() {
+      {
+        add("requesteddevice");
+      }
+    };
+    final List<String> reportedErrors = new ArrayList<String>() {
+      {
+        add("User not found");
+      }
+    };
+    when(response.getEntity()).thenReturn(new StringEntity("{\"status\":0, \"request\":\"" + expectedRequestId + "\",\"devices\": " + expectedDevices.toString() + ", \"errors\":[\"User not found\"]}"));
+    final Verification status = PushoverResponseFactory.createVerification(response);
+    assertNotNull(status);
+    assertEquals(status.getStatus(), 0);
+    assertEquals(status.getRequest(), expectedRequestId);
+    assertEquals(status.getDevices(), expectedDevices);
+    assertEquals(status.getErrors(), reportedErrors);
+  }
 
-        when(response.getFirstHeader(PushoverResponseFactory.REQUEST_REMAINING_HEADER)).thenReturn(new BasicHeader(PushoverResponseFactory.REQUEST_REMAINING_HEADER,
-                String.valueOf(expectedRemaining)));
+  @Test public void testOKEmergencyResponse() throws IOException {
+    final String expectedRequestId = "1234";
+    final String expectedReceipt = "qwertyuiop";
+    when(response.getEntity()).thenReturn(new StringEntity("{\"status\":1, \"request\":\"" + expectedRequestId + "\",\"receipt\":\"" + expectedReceipt + "\"}"));
+    final Response resp = PushoverResponseFactory.createResponse(response);
+    assertNotNull(resp);
+    assertEquals(resp.getStatus(), 1);
+    assertEquals(resp.getRequest(), expectedRequestId);
+    assertEquals(resp.getReceipt(), expectedReceipt);
+  }
 
-        final Response status = PushoverResponseFactory.createResponse(response);
-        assertNotNull(status);
-        assertEquals(status.getStatus(), 1);
-        assertEquals(status.getRequest(), expectedRequestId);
-        assertEquals(status.getRemaining(), expectedRemaining);
-    }
-    
-    @Test
-    public void testOKEmergencyResponse() throws IOException {
-        final String expectedRequestId = "1234";
-        final String expectedReceipt = "qwertyuiop";
+  @Test public void testCreateSoundResponse() throws IOException {
+    when(response.getEntity()).thenReturn(new StringEntity("{\"sounds\":{\"id\":\"name\"},\"status\":1}"));
+    final Set<PushOverSound> sounds = PushoverResponseFactory.createSoundSet(response);
+    assertNotNull(sounds);
+    assertFalse(sounds.isEmpty());
+    PushOverSound sound = sounds.iterator().next();
+    assertEquals(sound.getId(), "id");
+    assertEquals(sound.getName(), "name");
+  }
 
-        when(response.getEntity()).thenReturn(new StringEntity("{\"status\":1, \"request\":\"" + expectedRequestId +"\",\"receipt\":\"" + expectedReceipt +"\"}"));
+  @Test(expected = IOException.class) public void testMalformedCreateSoundResponse() throws IOException {
+    when(response.getEntity()).thenReturn(new StringEntity("{"));
+    PushoverResponseFactory.createSoundSet(response);
+  }
 
-        final Response resp = PushoverResponseFactory.createResponse(response);
-        assertNotNull(resp);
-        assertEquals(resp.getStatus(), 1);
-        assertEquals(resp.getRequest(), expectedRequestId);
-        assertEquals(resp.getReceipt(), expectedReceipt);
-    }
+  @Test public void testEmptyCreateSoundResponse() throws IOException {
+    when(response.getEntity()).thenReturn(new StringEntity("{}"));
+    final Set<PushOverSound> sounds = PushoverResponseFactory.createSoundSet(response);
+    assertNotNull(sounds);
+    assertTrue(sounds.isEmpty());
+  }
 
-    @Test
-    public void testOKVerification() throws IOException {
-        final String expectedRequestId = "1234";
-        final List<String> expectedDevices = new ArrayList<String>(){
-                {
-                      add("abc");
-                      add("efg");
-                      add("123");
-                }
-        };
+  @Test(expected = IOException.class) public void testNullCreateSoundResponse() throws Exception {
+    assertNull(PushoverResponseFactory.createSoundSet(null));
+  }
 
-        when(response.getEntity()).thenReturn(new StringEntity("{\"status\":1, \"request\":\"" + expectedRequestId +"\",\"devices\": "+expectedDevices.toString()+"}"));
-
-        final Verification status = PushoverResponseFactory.createVerification(response);
-        assertNotNull(status);
-        assertEquals(status.getStatus(), 1);
-        assertEquals(status.getRequest(), expectedRequestId);
-        assertEquals(status.getDevices(), expectedDevices);
-    }
-    
-    @Test
-    public void testFailedVerification() throws IOException {
-        final String expectedRequestId = "1234";
-        final List<String> expectedDevices = new ArrayList<String>(){
-                {
-                      add("requesteddevice");
-                }
-        };
-        final List<String> reportedErrors = new ArrayList<String>(){
-                {
-                      add("User not found");
-                }
-        };
-        
-
-        when(response.getEntity()).thenReturn(new StringEntity("{\"status\":0, \"request\":\"" + expectedRequestId +"\",\"devices\": "+expectedDevices.toString()+", \"errors\":[\"User not found\"]}"));
-
-        final Verification status = PushoverResponseFactory.createVerification(response);
-        assertNotNull(status);
-        assertEquals(status.getStatus(), 0);
-        assertEquals(status.getRequest(), expectedRequestId);
-        assertEquals(status.getDevices(), expectedDevices);
-        assertEquals(status.getErrors(), reportedErrors);
-    }
-    
-    @Test
-    public void testCreateSoundResponse() throws IOException {
-
-        when(response.getEntity()).thenReturn(new StringEntity("{\"sounds\":{\"id\":\"name\"},\"status\":1}"));
-
-        final Set<PushOverSound> sounds = PushoverResponseFactory.createSoundSet(response);
-        assertNotNull(sounds);
-        assertFalse(sounds.isEmpty());
-        PushOverSound sound = sounds.iterator().next(); 
-        assertEquals(sound.getId(), "id");
-        assertEquals(sound.getName(), "name");
-    }
-
-    @Test(expected = IOException.class)
-    public void testMalformedCreateSoundResponse() throws IOException {
-        when(response.getEntity()).thenReturn(new StringEntity("{"));
-        PushoverResponseFactory.createSoundSet(response);        
-    }
-
-    @Test
-    public void testEmptyCreateSoundResponse() throws IOException {
-        when(response.getEntity()).thenReturn(new StringEntity("{}"));
-        final Set<PushOverSound> sounds = PushoverResponseFactory.createSoundSet(response);
-        assertNotNull(sounds);
-        assertTrue(sounds.isEmpty());
-    }
-    
-    @Test(expected = IOException.class)
-    public void testNullCreateSoundResponse() throws Exception {
-        assertNull(PushoverResponseFactory.createSoundSet(null));
-    }
-
-    @Test(expected = IOException.class)
-    public void testNullEntityCreateSoundResponse() throws Exception {
-        assertNull(PushoverResponseFactory.createSoundSet(response));
-    }
+  @Test(expected = IOException.class) public void testNullEntityCreateSoundResponse() throws Exception {
+    assertNull(PushoverResponseFactory.createSoundSet(response));
+  }
 }
