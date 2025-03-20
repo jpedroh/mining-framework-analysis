925ef7d19c534c81e209a237e2bcc06931fb88ef
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 14:03:23 +0000

    Mining Framework Analysis

--- jsprit-core/src/main/java/algorithms/BestInsertionBuilder.java
+++ jsprit-core/src/main/java/algorithms/BestInsertionBuilder.java
@@ -1,110 +1,113 @@
 package algorithms;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
-
 import basics.VehicleRoutingProblem;
 import basics.algo.InsertionListener;
 import basics.algo.VehicleRoutingAlgorithmListeners.PrioritizedVRAListener;
 import basics.route.VehicleFleetManager;
 
 public class BestInsertionBuilder {
+  private VehicleRoutingProblem vrp;
 
-	private VehicleRoutingProblem vrp;
-	
-	private StateManager stateManager;
-	
-	private boolean local = true;
-	
-	private ConstraintManager constraintManager;
+  private StateManager stateManager;
 
-	private VehicleFleetManager fleetManager;
+  private boolean local = true;
 
-	private double weightOfFixedCosts;
+  private ConstraintManager constraintManager;
 
-	private boolean considerFixedCosts = false;
+  private VehicleFleetManager fleetManager;
 
-	private ActivityInsertionCostsCalculator actInsertionCostsCalculator = null;
+  private double weightOfFixedCosts;
 
-	private int forwaredLooking;
+  private boolean considerFixedCosts = false;
 
-	private int memory;
+  private ActivityInsertionCostsCalculator actInsertionCostsCalculator = null;
 
-	private ExecutorService executor;
+  private int forwaredLooking;
 
-	private int nuOfThreads;
-	
-	public BestInsertionBuilder(VehicleRoutingProblem vrp, VehicleFleetManager vehicleFleetManager, StateManager stateManager, ConstraintManager constraintManager) {
-		super();
-		this.vrp = vrp;
-		this.stateManager = stateManager;
-		this.constraintManager = constraintManager;
-		this.fleetManager = vehicleFleetManager;
-	}
-		
-	public BestInsertionBuilder setRouteLevel(int forwardLooking, int memory){
+  private int memory;
 
-		local = false;
-		this.forwaredLooking = forwardLooking;
-		this.memory = memory;
-		return this;
-	};
-	
-	public BestInsertionBuilder setLocalLevel(){
-		local = true;
-		return this;
-	};
-	
-	public BestInsertionBuilder considerFixedCosts(double weightOfFixedCosts){
-		this.weightOfFixedCosts = weightOfFixedCosts;
-		this.considerFixedCosts  = true;
-		return this;
-	}
-	
-	public BestInsertionBuilder setActivityInsertionCostCalculator(ActivityInsertionCostsCalculator activityInsertionCostsCalculator){
-		this.actInsertionCostsCalculator = activityInsertionCostsCalculator;
-		return this;
-	};
-	
-	public BestInsertionBuilder setConcurrentMode(ExecutorService executor, int nuOfThreads){
-		this.executor = executor;
-		this.nuOfThreads = nuOfThreads;
-		return this;
-	}
-	
-	
-	public InsertionStrategy build() {
-		List<InsertionListener> iListeners = new ArrayList<InsertionListener>();
-		List<PrioritizedVRAListener> algorithmListeners = new ArrayList<PrioritizedVRAListener>();
-		CalculatorBuilder calcBuilder = new CalculatorBuilder(iListeners, algorithmListeners);
-		if(local){
-			calcBuilder.setLocalLevel();
-		}
-		else {
-			calcBuilder.setRouteLevel(forwaredLooking, memory);
-		}
-		calcBuilder.setConstraintManager(constraintManager);
-		calcBuilder.setStates(stateManager);
-		calcBuilder.setVehicleRoutingProblem(vrp);
-		calcBuilder.setVehicleFleetManager(fleetManager);
-		calcBuilder.setActivityInsertionCostsCalculator(actInsertionCostsCalculator);
-		if(considerFixedCosts) {
-			calcBuilder.considerFixedCosts(weightOfFixedCosts);
-		}
-		JobInsertionCostsCalculator jobInsertions = calcBuilder.build();
-		InsertionStrategy bestInsertion;
-		if(executor == null){
-			bestInsertion = new BestInsertion(jobInsertions);
-			
-		}
-		else{
+  private ExecutorService executor;
 
-			bestInsertion = new BestInsertionConcurrent(jobInsertions,executor,nuOfThreads);
+  private int nuOfThreads;
 
-		}
-		for(InsertionListener l : iListeners) bestInsertion.addListener(l);
-		return bestInsertion;
-	}
+  public BestInsertionBuilder(VehicleRoutingProblem vrp, VehicleFleetManager vehicleFleetManager, StateManager stateManager, ConstraintManager constraintManager) {
+    super();
+    this.vrp = vrp;
+    this.stateManager = stateManager;
+    this.constraintManager = constraintManager;
+    this.fleetManager = vehicleFleetManager;
+  }
 
+  public BestInsertionBuilder setRouteLevel(int forwardLooking, int memory) {
+    local = false;
+    this.forwaredLooking = forwardLooking;
+    this.memory = memory;
+    return this;
+  }
+
+
+
+  public BestInsertionBuilder setLocalLevel() {
+    local = true;
+    return this;
+  }
+
+
+
+  public BestInsertionBuilder considerFixedCosts(double weightOfFixedCosts) {
+    this.weightOfFixedCosts = weightOfFixedCosts;
+    this.considerFixedCosts = true;
+    return this;
+  }
+
+  public BestInsertionBuilder setActivityInsertionCostCalculator(ActivityInsertionCostsCalculator activityInsertionCostsCalculator) {
+    this.actInsertionCostsCalculator = activityInsertionCostsCalculator;
+    return this;
+  }
+
+
+
+  public BestInsertionBuilder setConcurrentMode(ExecutorService executor, int nuOfThreads) {
+    this.executor = executor;
+    this.nuOfThreads = nuOfThreads;
+    return this;
+  }
+
+  public InsertionStrategy build() {
+    List<InsertionListener> iListeners = new ArrayList<InsertionListener>();
+    List<PrioritizedVRAListener> algorithmListeners = new ArrayList<PrioritizedVRAListener>();
+    CalculatorBuilder calcBuilder = new CalculatorBuilder(iListeners, algorithmListeners);
+    if (local) {
+      calcBuilder.setLocalLevel();
+    } else {
+      calcBuilder.setRouteLevel(forwaredLooking, memory);
+    }
+    calcBuilder.setConstraintManager(constraintManager);
+    calcBuilder.setStates(stateManager);
+    calcBuilder.setVehicleRoutingProblem(vrp);
+    calcBuilder.setVehicleFleetManager(fleetManager);
+    calcBuilder.setActivityInsertionCostsCalculator(actInsertionCostsCalculator);
+    if (considerFixedCosts) {
+      calcBuilder.considerFixedCosts(weightOfFixedCosts);
+    }
+    JobInsertionCostsCalculator jobInsertions = calcBuilder.build();
+    InsertionStrategy bestInsertion;
+    if (executor == null) {
+      bestInsertion = new BestInsertion(jobInsertions);
+    } else {
+      bestInsertion = new 
+<<<<<<< /usr/src/app/output/jsprit/jsprit/d1dac2d622e978ac839e3ba49cc8037b40ae5762/jsprit-core/src/main/java/algorithms/BestInsertionBuilder.java/left.java
+      BestInsertionConc
+=======
+      BestInsertionConcurrent
+>>>>>>> /usr/src/app/output/jsprit/jsprit/d1dac2d622e978ac839e3ba49cc8037b40ae5762/jsprit-core/src/main/java/algorithms/BestInsertionBuilder.java/right.java
+      (jobInsertions, executor, nuOfThreads);
+    }
+    for (InsertionListener l : iListeners) {
+      bestInsertion.addListener(l);
+    }
+    return bestInsertion;
+  }
 }
--- jsprit-core/src/test/java/algorithms/GendreauPostOptTest.java
+++ jsprit-core/src/test/java/algorithms/GendreauPostOptTest.java
@@ -1,31 +1,11 @@
-/*******************************************************************************
- * Copyright (C) 2013  Stefan Schroeder
- * 
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either 
- * version 3.0 of the License, or (at your option) any later version.
- * 
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public 
- * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
- ******************************************************************************/
 package algorithms;
-
 import static org.junit.Assert.assertEquals;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
-
 import org.junit.Before;
 import org.junit.Test;
-
 import util.Coordinate;
 import util.ManhattanDistanceCalculator;
 import util.RouteUtils;
@@ -48,243 +28,170 @@ import basics.route.VehicleRoute;
 import basics.route.VehicleTypeImpl;
 
 public class GendreauPostOptTest {
-	
-	TourActivities tour;
+  TourActivities tour;
 
-	Vehicle heavyVehicle;
+  Vehicle heavyVehicle;
 
-	Vehicle lightVehicle1;
-	
-	Vehicle lightVehicle2;
-	
-	VehicleRoutingTransportCosts cost;
-	
-	VehicleRoutingActivityCosts activityCosts;
-	
-	VehicleRoutingProblem vrp;
-	
-	Service job1;
-	
-	Service job2;
-	
-	Service job3;
+  Vehicle lightVehicle1;
 
-	private StateManager states;
+  Vehicle lightVehicle2;
 
-	private List<Vehicle> vehicles;
+  VehicleRoutingTransportCosts cost;
 
-	private VehicleFleetManager fleetManager;
-	
-	private JobInsertionCostsCalculator insertionCalc;
+  VehicleRoutingActivityCosts activityCosts;
 
-	@Before
-	public void setUp(){
-		
-		cost = new VehicleRoutingTransportCosts() {
-			
-			@Override
-			public double getBackwardTransportTime(String fromId, String toId,
-					double arrivalTime, Driver driver, Vehicle vehicle) {
-				// TODO Auto-generated method stub
-				return 0;
-			}
-			
-			@Override
-			public double getBackwardTransportCost(String fromId, String toId,
-					double arrivalTime, Driver driver, Vehicle vehicle) {
-				// TODO Auto-generated method stub
-				return 0;
-			}
-			
-			@Override
-			public double getTransportCost(String fromId, String toId, double departureTime, Driver driver, Vehicle vehicle) {
-				
-				String[] fromTokens = fromId.split(",");
-				String[] toTokens = toId.split(",");
-				double fromX = Double.parseDouble(fromTokens[0]);
-				double fromY = Double.parseDouble(fromTokens[1]);
-				
-				double toX = Double.parseDouble(toTokens[0]);
-				double toY = Double.parseDouble(toTokens[1]);
-				
-				double costPerDistanceUnit;
-				if(vehicle != null){
-					costPerDistanceUnit = vehicle.getType().getVehicleCostParams().perDistanceUnit;
-				}
-				else{
-					costPerDistanceUnit = 1;
-				}
-				
-				return costPerDistanceUnit*ManhattanDistanceCalculator.calculateDistance(new Coordinate(fromX, fromY), new Coordinate(toX, toY));
-			}
-			
-			@Override
-			public double getTransportTime(String fromId, String toId, double departureTime, Driver driver, Vehicle vehicle) {		
-				return 0;
-			}
-		};
-		
-		VehicleTypeImpl lightType = VehicleTypeImpl.Builder.newInstance("light", 10).setFixedCost(10).setCostPerDistance(1.0).build();
-		VehicleTypeImpl heavyType = VehicleTypeImpl.Builder.newInstance("heavy", 10).setFixedCost(30).setCostPerDistance(2.0).build();
-		
-		lightVehicle1 = VehicleImpl.Builder.newInstance("light").setLocationId("0,0").setType(lightType).build(); 
-		lightVehicle2 = VehicleImpl.Builder.newInstance("light2").setLocationId("0,0").setType(lightType).build(); 
-		heavyVehicle = VehicleImpl.Builder.newInstance("heavy").setLocationId("0,0").setType(heavyType).build(); 
-			
-		
-		job1 = getService("10,0");
-		job2 = getService("10,10");
-		job3 = getService("0,10");
-		
-		Collection<Job> jobs = new ArrayList<Job>();
-		jobs.add(job1);
-		jobs.add(job2);
-		jobs.add(job3);
-		
-		vehicles = Arrays.asList(lightVehicle1,lightVehicle2, heavyVehicle);
-		
-//		Collection<Vehicle> vehicles = Arrays.asList(lightVehicle1,lightVehicle2, heavyVehicle);
-		fleetManager = new FiniteFleetManagerFactory(vehicles).createFleetManager();
-		states = new StateManager();
-		
-		activityCosts = new ExampleActivityCostFunction();
-		
+  VehicleRoutingProblem vrp;
 
-		ServiceInsertionCalculator standardServiceInsertion = new ServiceInsertionCalculator(cost, new LocalActivityInsertionCostsCalculator(cost, activityCosts), new LoadConstraint(states), new TimeWindowConstraint(states, cost));
+  Service job1;
 
+  Service job2;
 
+  Service job3;
 
-		
-		JobInsertionConsideringFixCostsCalculator withFixCost = new JobInsertionConsideringFixCostsCalculator(standardServiceInsertion, states);
-		withFixCost.setWeightOfFixCost(1.2);
-		
-		insertionCalc = new VehicleTypeDependentJobInsertionCalculator(fleetManager, withFixCost);
-		
-//		updater = new TourStateUpdater(states, cost, activityCosts);
-		
-	}
-	
-	@Test
-	public void whenPostOpt_splitsTour_oneActiveTourBecomeTwoSeperateActiveTours(){
-		Collection<Job> jobs = new ArrayList<Job>();
-		jobs.add(job1);
-		jobs.add(job2);
-		
-		vrp = VehicleRoutingProblem.Builder.newInstance().addAllJobs(jobs).addAllVehicles(vehicles).setRoutingCost(cost).build();
-				
-		TourActivities tour = new TourActivities();
-		tour.addActivity(ServiceActivity.newInstance(job1));
-		tour.addActivity(ServiceActivity.newInstance(job2));
-		
-		VehicleRoute route = VehicleRoute.newInstance(tour,DriverImpl.noDriver(),heavyVehicle);
-		
-		fleetManager.lock(heavyVehicle);
-		
-		UpdateStates stateUpdater = new UpdateStates(states, vrp.getTransportCosts(), vrp.getActivityCosts());
-		stateUpdater.update(route);
-		
-		Collection<VehicleRoute> routes = new ArrayList<VehicleRoute>();
-		routes.add(route);
-//		routes.add(new VehicleRoute(getEmptyTour(),getDriver(),getNoVehicle()));
-//		routes.add(new VehicleRoute(getEmptyTour(),getDriver(),getNoVehicle()));
+  private StateManager states;
 
+  private List<Vehicle> vehicles;
 
-		VehicleRoutingProblemSolution sol = new VehicleRoutingProblemSolution(routes, states.getRouteState(route, StateFactory.COSTS).toDouble() + getFixedCosts(routes));
+  private VehicleFleetManager fleetManager;
 
-		
-		assertEquals(110.0, sol.getCost(), 0.5);
-		
-		
-		RuinRadial radialRuin = new RuinRadial(vrp, 0.2, new JobDistanceAvgCosts(vrp.getTransportCosts()));
-//		radialRuin.addListener(stateUpdater);
-		
-		InsertionStrategy insertionStrategy = new BestInsertion(insertionCalc);
-		insertionStrategy.addListener(stateUpdater);
-		insertionStrategy.addListener(new VehicleSwitched(fleetManager));
-		Gendreau postOpt = new Gendreau(vrp, radialRuin, insertionStrategy, fleetManager);
-		
-		VehicleRoutingProblemSolution newSolution = postOpt.runAndGetSolution(sol);
-		newSolution.setCost(getCosts(newSolution,states));
-		
-		assertEquals(2,RouteUtils.getNuOfActiveRoutes(newSolution.getRoutes()));
-		assertEquals(2,newSolution.getRoutes().size());
-		assertEquals(80.0,newSolution.getCost(),0.5);
-	}
-	
-	private double getFixedCosts(Collection<VehicleRoute> routes) {
-		double c = 0.0;
-		for(VehicleRoute r : routes){ c += r.getVehicle().getType().getVehicleCostParams().fix; }
-		return c;
-	}
+  private JobInsertionCostsCalculator insertionCalc;
 
-	private double getCosts(VehicleRoutingProblemSolution newSolution, StateManager states) {
-		double c = 0.0;
-		for(VehicleRoute r : newSolution.getRoutes()){
+  @Before public void setUp() {
+    cost = new VehicleRoutingTransportCosts() {
+      @Override public double getBackwardTransportTime(String fromId, String toId, double arrivalTime, Driver driver, Vehicle vehicle) {
+        return 0;
+      }
 
-			c += states.getRouteState(r, StateFactory.COSTS).toDouble() + r.getVehicle().getType().getVehicleCostParams().fix;
+      @Override public double getBackwardTransportCost(String fromId, String toId, double arrivalTime, Driver driver, Vehicle vehicle) {
+        return 0;
+      }
 
-		}
-		return c;
-	}
+      @Override public double getTransportCost(String fromId, String toId, double departureTime, Driver driver, Vehicle vehicle) {
+        String[] fromTokens = fromId.split(",");
+        String[] toTokens = toId.split(",");
+        double fromX = Double.parseDouble(fromTokens[0]);
+        double fromY = Double.parseDouble(fromTokens[1]);
+        double toX = Double.parseDouble(toTokens[0]);
+        double toY = Double.parseDouble(toTokens[1]);
+        double costPerDistanceUnit;
+        if (vehicle != null) {
+          costPerDistanceUnit = vehicle.getType().getVehicleCostParams().perDistanceUnit;
+        } else {
+          costPerDistanceUnit = 1;
+        }
+        return costPerDistanceUnit * ManhattanDistanceCalculator.calculateDistance(new Coordinate(fromX, fromY), new Coordinate(toX, toY));
+      }
 
-	@Test
-	public void whenPostOpt_optsRoutesWithMoreThanTwoJobs_oneRouteBecomesTwoRoutes(){
-		Collection<Job> jobs = new ArrayList<Job>();
-		jobs.add(job1);
-		jobs.add(job2);
-		jobs.add(job3);
-		
-		vrp = VehicleRoutingProblem.Builder.newInstance().addAllJobs(jobs).addAllVehicles(vehicles).setRoutingCost(cost).build();
-		
-		TourActivities tour = new TourActivities();
-		tour.addActivity(ServiceActivity.newInstance(job1));
-		tour.addActivity(ServiceActivity.newInstance(job2));
-		tour.addActivity(ServiceActivity.newInstance(job3));
-		
-		VehicleRoute route = VehicleRoute.newInstance(tour,DriverImpl.noDriver(),heavyVehicle);
-		
-		UpdateStates stateUpdater = new UpdateStates(states, vrp.getTransportCosts(), vrp.getActivityCosts());
-		stateUpdater.update(route);
-		
-		fleetManager.lock(heavyVehicle);
-		
-		Collection<VehicleRoute> routes = new ArrayList<VehicleRoute>();
-		routes.add(route);
+      @Override public double getTransportTime(String fromId, String toId, double departureTime, Driver driver, Vehicle vehicle) {
+        return 0;
+      }
+    };
+    VehicleTypeImpl lightType = VehicleTypeImpl.Builder.newInstance("light", 10).setFixedCost(10).setCostPerDistance(1.0).build();
+    VehicleTypeImpl heavyType = VehicleTypeImpl.Builder.newInstance("heavy", 10).setFixedCost(30).setCostPerDistance(2.0).build();
+    lightVehicle1 = VehicleImpl.Builder.newInstance("light").setLocationId("0,0").setType(lightType).build();
+    lightVehicle2 = VehicleImpl.Builder.newInstance("light2").setLocationId("0,0").setType(lightType).build();
+    heavyVehicle = VehicleImpl.Builder.newInstance("heavy").setLocationId("0,0").setType(heavyType).build();
+    job1 = getService("10,0");
+    job2 = getService("10,10");
+    job3 = getService("0,10");
+    Collection<Job> jobs = new ArrayList<Job>();
+    jobs.add(job1);
+    jobs.add(job2);
+    jobs.add(job3);
+    vehicles = Arrays.asList(lightVehicle1, lightVehicle2, heavyVehicle);
+    fleetManager = new FiniteFleetManagerFactory(vehicles).createFleetManager();
+    states = new StateManager();
+    activityCosts = new ExampleActivityCostFunction();
+    ServiceInsertionCalculator standardServiceInsertion = new ServiceInsertionCalculator(cost, new LocalActivityInsertionCostsCalculator(cost, activityCosts), new LoadConstraint(states), new TimeWindowConstraint(states, cost));
+    JobInsertionConsideringFixCostsCalculator withFixCost = new JobInsertionConsideringFixCostsCalculator(standardServiceInsertion, states);
+    withFixCost.setWeightOfFixCost(1.2);
+    insertionCalc = new VehicleTypeDependentJobInsertionCalculator(fleetManager, withFixCost);
+  }
 
-		VehicleRoutingProblemSolution sol = new VehicleRoutingProblemSolution(routes, route.getCost());
-		sol.setCost(getCosts(sol,states));
-		
-		assertEquals(110.0, sol.getCost(), 0.5);
-		
-		RuinRadial radialRuin = new RuinRadial(vrp, 0.2, new JobDistanceAvgCosts(vrp.getTransportCosts()));
-		InsertionStrategy insertionStrategy = new BestInsertion(insertionCalc);
-		insertionStrategy.addListener(stateUpdater);
-		insertionStrategy.addListener(new VehicleSwitched(fleetManager));
-		Gendreau postOpt = new Gendreau(vrp, radialRuin, insertionStrategy, fleetManager);
-		postOpt.setShareOfJobsToRuin(1.0);
-		postOpt.setNuOfIterations(1);
+  @Test public void whenPostOpt_splitsTour_oneActiveTourBecomeTwoSeperateActiveTours() {
+    Collection<Job> jobs = new ArrayList<Job>();
+    jobs.add(job1);
+    jobs.add(job2);
+    vrp = VehicleRoutingProblem.Builder.newInstance().addAllJobs(jobs).addAllVehicles(vehicles).setRoutingCost(cost).build();
+    TourActivities tour = new TourActivities();
+    tour.addActivity(ServiceActivity.newInstance(job1));
+    tour.addActivity(ServiceActivity.newInstance(job2));
+    VehicleRoute route = VehicleRoute.newInstance(tour, DriverImpl.noDriver(), heavyVehicle);
+    fleetManager.lock(heavyVehicle);
+    UpdateStates stateUpdater = new UpdateStates(states, vrp.getTransportCosts(), vrp.getActivityCosts());
+    stateUpdater.update(route);
+    Collection<VehicleRoute> routes = new ArrayList<VehicleRoute>();
+    routes.add(route);
+    VehicleRoutingProblemSolution sol = new VehicleRoutingProblemSolution(routes, states.getRouteState(route, StateFactory.COSTS).toDouble() + getFixedCosts(routes));
+    assertEquals(110.0, sol.getCost(), 0.5);
+    RuinRadial radialRuin = new RuinRadial(vrp, 0.2, new JobDistanceAvgCosts(vrp.getTransportCosts()));
+    InsertionStrategy insertionStrategy = new BestInsertion(insertionCalc);
+    insertionStrategy.addListener(stateUpdater);
+    insertionStrategy.addListener(new VehicleSwitched(fleetManager));
+    Gendreau postOpt = new Gendreau(vrp, radialRuin, insertionStrategy, fleetManager);
+    VehicleRoutingProblemSolution newSolution = postOpt.runAndGetSolution(sol);
+    newSolution.setCost(getCosts(newSolution, states));
+    assertEquals(2, RouteUtils.getNuOfActiveRoutes(newSolution.getRoutes()));
+    assertEquals(2, newSolution.getRoutes().size());
+    assertEquals(80.0, newSolution.getCost(), 0.5);
+  }
 
-//		postOpt.setWithFix(withFixCost);
-		VehicleRoutingProblemSolution newSolution = postOpt.runAndGetSolution(sol);
-		newSolution.setCost(getCosts(newSolution,states));
-		
-		assertEquals(2,RouteUtils.getNuOfActiveRoutes(newSolution.getRoutes()));
-		assertEquals(2,newSolution.getRoutes().size());
-		assertEquals(80.0,newSolution.getCost(),0.5);
-	}
+  private double getFixedCosts(Collection<VehicleRoute> routes) {
+    double c = 0.0;
+    for (VehicleRoute r : routes) {
+      c += r.getVehicle().getType().getVehicleCostParams().fix;
+    }
+    return c;
+  }
 
-	private Service getService(String to, double serviceTime) {
-		Service s = Service.Builder.newInstance(to, 0).setLocationId(to).setServiceTime(serviceTime).setTimeWindow(TimeWindow.newInstance(0.0, 20.0)).build(); 
-			
-		return s;
-	}
-	
-	private Service getService(String to) {
-		Service s = getService(to, 0.0);
-		return s;
-	}
-	
-	
-		
+  private double getCosts(VehicleRoutingProblemSolution newSolution, StateManager states) {
+    double c = 0.0;
+    for (VehicleRoute r : newSolution.getRoutes()) {
+      c += states.getRouteState(r, StateFactory.COSTS).toDouble() + r.getVehicle().getType().getVehicleCostParams().fix;
+    }
+    return c;
+  }
 
+  @Test public void whenPostOpt_optsRoutesWithMoreThanTwoJobs_oneRouteBecomesTwoRoutes() {
+    Collection<Job> jobs = new ArrayList<Job>();
+    jobs.add(job1);
+    jobs.add(job2);
+    jobs.add(job3);
+    vrp = VehicleRoutingProblem.Builder.newInstance().addAllJobs(jobs).addAllVehicles(vehicles).setRoutingCost(cost).build();
+    TourActivities tour = new TourActivities();
+    tour.addActivity(ServiceActivity.newInstance(job1));
+    tour.addActivity(ServiceActivity.newInstance(job2));
+    tour.addActivity(ServiceActivity.newInstance(job3));
+    VehicleRoute route = VehicleRoute.newInstance(tour, DriverImpl.noDriver(), heavyVehicle);
+    UpdateStates stateUpdater = new UpdateStates(states, vrp.getTransportCosts(), vrp.getActivityCosts());
+    stateUpdater.update(route);
+    fleetManager.lock(heavyVehicle);
+    Collection<VehicleRoute> routes = new ArrayList<VehicleRoute>();
+    routes.add(route);
+    VehicleRoutingProblemSolution sol = new VehicleRoutingProblemSolution(routes, route.getCost());
+    sol.setCost(getCosts(sol, states));
+    assertEquals(110.0, sol.getCost(), 0.5);
+    RuinRadial radialRuin = new RuinRadial(vrp, 0.2, new JobDistanceAvgCosts(vrp.getTransportCosts()));
+    InsertionStrategy insertionStrategy = new BestInsertion(insertionCalc);
+    insertionStrategy.addListener(stateUpdater);
+    insertionStrategy.addListener(new VehicleSwitched(fleetManager));
+    Gendreau postOpt = new Gendreau(vrp, radialRuin, insertionStrategy, fleetManager);
+    postOpt.setShareOfJobsToRuin(1.0);
+    postOpt.setNuOfIterations(1);
+    VehicleRoutingProblemSolution newSolution = postOpt.runAndGetSolution(sol);
+    newSolution.setCost(getCosts(newSolution, states));
+    assertEquals(2, RouteUtils.getNuOfActiveRoutes(newSolution.getRoutes()));
+    assertEquals(2, newSolution.getRoutes().size());
+    assertEquals(80.0, newSolution.getCost(), 0.5);
+  }
+
+  private Service getService(String to, double serviceTime) {
+    Service s = Service.Builder.newInstance(to, 0).setLocationId(to).setServiceTime(serviceTime).setTimeWindow(TimeWindow.newInstance(0.0, 20.0)).build();
+    return s;
+  }
+
+  private Service getService(String to) {
+    Service s = getService(to, 0.0);
+    return s;
+  }
 }
