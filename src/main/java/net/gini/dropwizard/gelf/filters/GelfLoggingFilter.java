f7b833a1481d690ca27ef7d121276135b7b777ad
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 11:34:03 +0000

    Mining Framework Analysis

--- src/main/java/net/gini/dropwizard/gelf/filters/GelfLoggingFilter.java
+++ src/main/java/net/gini/dropwizard/gelf/filters/GelfLoggingFilter.java
@@ -1,17 +1,13 @@
 package net.gini.dropwizard.gelf.filters;
-
 import com.google.common.base.Optional;
 import com.google.common.base.Stopwatch;
 import com.google.common.io.CountingOutputStream;
 import com.google.common.net.HttpHeaders;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
-
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
-
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -27,10 +23,9 @@ import javax.servlet.http.HttpServletResponseWrapper;
  * A {@link Filter} which logs requests and adds some data about it to the logger's {@link MDC}.
  */
 public class GelfLoggingFilter implements Filter {
+  private static final Logger LOG = LoggerFactory.getLogger(GelfLoggingFilter.class);
 
-    private static final Logger LOG = LoggerFactory.getLogger(GelfLoggingFilter.class);
-
-    /**
+  /**
      * Called by the web container to indicate to a filter that it is
      * being placed into service.
      * <p/>
@@ -49,12 +44,10 @@ public class GelfLoggingFilter implements Filter {
      * @param filterConfig the {@link FilterChain} for this {@link Filter}
      * @throws ServletException if something goes wrong
      */
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-        // Do nothing
-    }
+  @Override public void init(FilterConfig filterConfig) throws ServletException {
+  }
 
-    /**
+  /**
      * The <code>doFilter</code> method of the Filter is called by the
      * container each time a request/response pair is passed through the
      * chain due to a client request for a resource at the end of the chain.
@@ -82,79 +75,63 @@ public class GelfLoggingFilter implements Filter {
      * next entity in the filter chain.
      * </ol>
      */
-    @Override
-    public void doFilter(final ServletRequest request, final ServletResponse response, final FilterChain chain)
-            throws IOException, ServletException {
-        // It's quite safe to assume that we only receive HTTP requests
-        final HttpServletRequest httpRequest = (HttpServletRequest) request;
-        final HttpServletResponse httpResponse = (HttpServletResponse) response;
-
-        final StringBuilder buf = new StringBuilder(256);
-
-        final Optional<String> address = Optional.fromNullable(httpRequest.getHeader(HttpHeaders.X_FORWARDED_FOR));
-        final String clientAddress = address.or(request.getRemoteAddr());
-
-        buf.append(clientAddress);
-        buf.append(" - ");
-
-        final String authType = httpRequest.getAuthType();
-        if (authType != null) {
-            buf.append(httpRequest.getUserPrincipal().getName());
-        } else {
-            buf.append("-");
-        }
-        buf.append(" \"");
-        buf.append(httpRequest.getMethod());
-        buf.append(' ');
-        buf.append(httpRequest.getRequestURI());
-        buf.append(' ');
-        buf.append(request.getProtocol());
-        buf.append("\" ");
-
-        final CountingHttpServletResponseWrapper responseWrapper = new CountingHttpServletResponseWrapper(httpResponse);
-
-        final Stopwatch stopwatch = Stopwatch.createUnstarted();
-        stopwatch.start();
-
-        try {
-            chain.doFilter(request, responseWrapper);
-        } finally {
-            stopwatch.stop();
-
-            buf.append(responseWrapper.getStatus());
-            buf.append(" ");
-            buf.append(responseWrapper.getCount());
-
-            final String userAgent = httpRequest.getHeader(HttpHeaders.USER_AGENT);
-            if (userAgent != null) {
-                MDC.put(AdditionalKeys.USER_AGENT, userAgent);
-            }
-
-            if (authType != null) {
-                MDC.put(AdditionalKeys.REQ_AUTH, authType);
-                MDC.put(AdditionalKeys.PRINCIPAL, httpRequest.getUserPrincipal().getName());
-            }
-
-            MDC.put(AdditionalKeys.REMOTE_ADDRESS, clientAddress);
-            MDC.put(AdditionalKeys.HTTP_METHOD, httpRequest.getMethod());
-            MDC.put(AdditionalKeys.PROTCOL, httpRequest.getProtocol());
-            MDC.put(AdditionalKeys.REQ_URI, httpRequest.getRequestURI());
-            MDC.put(AdditionalKeys.REQ_LENGTH, String.valueOf(httpRequest.getContentLength()));
-            MDC.put(AdditionalKeys.REQ_CONTENT_TYPE, httpRequest.getContentType());
-            MDC.put(AdditionalKeys.REQ_ENCODING, httpRequest.getCharacterEncoding());
-            MDC.put(AdditionalKeys.REQ_STATUS, String.valueOf(responseWrapper.getStatus()));
-            MDC.put(AdditionalKeys.RESP_CONTENT_TYPE, responseWrapper.getContentType());
-            MDC.put(AdditionalKeys.RESP_ENCODING, responseWrapper.getCharacterEncoding());
-            MDC.put(AdditionalKeys.RESP_TIME, String.valueOf(stopwatch.elapsed(TimeUnit.NANOSECONDS)));
-            MDC.put(AdditionalKeys.RESP_LENGTH, String.valueOf(responseWrapper.getCount()));
-
-            LOG.info(buf.toString());
-
-            clearMDC();
-        }
+  @Override public void doFilter(final ServletRequest request, final ServletResponse response, final FilterChain chain) throws IOException, ServletException {
+    final HttpServletRequest httpRequest = (HttpServletRequest) request;
+    final HttpServletResponse httpResponse = (HttpServletResponse) response;
+    final StringBuilder buf = new StringBuilder(256);
+    final Optional<String> address = Optional.fromNullable(httpRequest.getHeader(HttpHeaders.X_FORWARDED_FOR));
+    final String clientAddress = address.or(request.getRemoteAddr());
+    buf.append(clientAddress);
+    buf.append(" - ");
+    final String authType = httpRequest.getAuthType();
+    if (authType != null) {
+      buf.append(httpRequest.getUserPrincipal().getName());
+    } else {
+      buf.append("-");
     }
+    buf.append(" \"");
+    buf.append(httpRequest.getMethod());
+    buf.append(' ');
+    buf.append(httpRequest.getRequestURI());
+    buf.append(' ');
+    buf.append(request.getProtocol());
+    buf.append("\" ");
+    final CountingHttpServletResponseWrapper responseWrapper = new CountingHttpServletResponseWrapper(httpResponse);
+    final Stopwatch stopwatch = Stopwatch.createUnstarted();
+    stopwatch.start();
+    try {
+      chain.doFilter(request, responseWrapper);
+    }  finally {
+      stopwatch.stop();
+      buf.append(responseWrapper.getStatus());
+      buf.append(" ");
+      buf.append(responseWrapper.getCount());
+      final String userAgent = httpRequest.getHeader(HttpHeaders.USER_AGENT);
+      if (userAgent != null) {
+        MDC.put(AdditionalKeys.USER_AGENT, userAgent);
+      }
+      if (authType != null) {
+        MDC.put(AdditionalKeys.REQ_AUTH, authType);
+        MDC.put(AdditionalKeys.PRINCIPAL, httpRequest.getUserPrincipal().getName());
+      }
+      MDC.put(AdditionalKeys.REMOTE_ADDRESS, clientAddress);
+      MDC.put(AdditionalKeys.HTTP_METHOD, httpRequest.getMethod());
+      MDC.put(AdditionalKeys.PROTCOL, httpRequest.getProtocol());
+      MDC.put(AdditionalKeys.REQ_URI, httpRequest.getRequestURI());
+      MDC.put(AdditionalKeys.REQ_LENGTH, String.valueOf(httpRequest.getContentLength()));
+      MDC.put(AdditionalKeys.REQ_CONTENT_TYPE, httpRequest.getContentType());
+      MDC.put(AdditionalKeys.REQ_ENCODING, httpRequest.getCharacterEncoding());
+      MDC.put(AdditionalKeys.REQ_STATUS, String.valueOf(responseWrapper.getStatus()));
+      MDC.put(AdditionalKeys.RESP_CONTENT_TYPE, responseWrapper.getContentType());
+      MDC.put(AdditionalKeys.RESP_ENCODING, responseWrapper.getCharacterEncoding());
+      MDC.put(AdditionalKeys.RESP_TIME, String.valueOf(stopwatch.elapsed(TimeUnit.NANOSECONDS)));
+      MDC.put(AdditionalKeys.RESP_LENGTH, String.valueOf(responseWrapper.getCount()));
+      LOG.info(buf.toString());
+      clearMDC();
+    }
+  }
 
-    /**
+  /**
      * Called by the web container to indicate to a filter that it is being
      * taken out of service.
      * <p/>
@@ -168,42 +145,35 @@ public class GelfLoggingFilter implements Filter {
      * threads) and make sure that any persistent state is synchronized
      * with the filter's current state in memory.
      */
-    @Override
-    public void destroy() {
-        // Do nothing
-    }
+  @Override public void destroy() {
+  }
 
-    private void clearMDC() {
-        MDC.remove(AdditionalKeys.USER_AGENT);
-        MDC.remove(AdditionalKeys.REQ_AUTH);
-        MDC.remove(AdditionalKeys.PRINCIPAL);
-        MDC.remove(AdditionalKeys.REMOTE_ADDRESS);
-        MDC.remove(AdditionalKeys.HTTP_METHOD);
-        MDC.remove(AdditionalKeys.PROTCOL);
-        MDC.remove(AdditionalKeys.REQ_URI);
-        MDC.remove(AdditionalKeys.REQ_LENGTH);
-        MDC.remove(AdditionalKeys.REQ_CONTENT_TYPE);
-        MDC.remove(AdditionalKeys.REQ_ENCODING);
-        MDC.remove(AdditionalKeys.REQ_STATUS);
-        MDC.remove(AdditionalKeys.RESP_CONTENT_TYPE);
-        MDC.remove(AdditionalKeys.RESP_ENCODING);
-        MDC.remove(AdditionalKeys.RESP_TIME);
-        MDC.remove(AdditionalKeys.RESP_LENGTH);
+  private void clearMDC() {
+    MDC.remove(AdditionalKeys.USER_AGENT);
+    MDC.remove(AdditionalKeys.REQ_AUTH);
+    MDC.remove(AdditionalKeys.PRINCIPAL);
+    MDC.remove(AdditionalKeys.REMOTE_ADDRESS);
+    MDC.remove(AdditionalKeys.HTTP_METHOD);
+    MDC.remove(AdditionalKeys.PROTCOL);
+    MDC.remove(AdditionalKeys.REQ_URI);
+    MDC.remove(AdditionalKeys.REQ_LENGTH);
+    MDC.remove(AdditionalKeys.REQ_CONTENT_TYPE);
+    MDC.remove(AdditionalKeys.REQ_ENCODING);
+    MDC.remove(AdditionalKeys.REQ_STATUS);
+    MDC.remove(AdditionalKeys.RESP_CONTENT_TYPE);
+    MDC.remove(AdditionalKeys.RESP_ENCODING);
+    MDC.remove(AdditionalKeys.RESP_TIME);
+    MDC.remove(AdditionalKeys.RESP_LENGTH);
+  }
+
+  private static final class CountingServletOutputStream extends ServletOutputStream {
+    private final CountingOutputStream outputStream;
+
+    private CountingServletOutputStream(ServletOutputStream servletOutputStream) {
+      this.outputStream = new CountingOutputStream(servletOutputStream);
     }
 
     /**
-     * An implementation of {@link ServletOutputStream} which counts the bytes being
-     * written using a {@link CountingOutputStream}.
-     */
-    private static final class CountingServletOutputStream extends ServletOutputStream {
-
-        private final CountingOutputStream outputStream;
-
-        private CountingServletOutputStream(ServletOutputStream servletOutputStream) {
-            this.outputStream = new CountingOutputStream(servletOutputStream);
-        }
-
-        /**
          * Writes the specified byte to this output stream. The general
          * contract for <code>write</code> is that one byte is written
          * to the output stream. The byte to be written is the eight
@@ -218,87 +188,92 @@ public class GelfLoggingFilter implements Filter {
          *                             an <code>IOException</code> may be thrown if the
          *                             output stream has been closed.
          */
-        @Override
-        public void write(int b) throws IOException {
-            outputStream.write(b);
-        }
+    @Override public void write(int b) throws IOException {
+      outputStream.write(b);
+    }
 
-        public long getCount() {
-            return outputStream.getCount();
-        }
+    public long getCount() {
+      return outputStream.getCount();
+    }
+  }
+
+  private static final class CountingHttpServletResponseWrapper extends HttpServletResponseWrapper {
+    private CountingServletOutputStream outputStream;
+
+    private CountingHttpServletResponseWrapper(HttpServletResponse response) throws IOException {
+      super(response);
     }
 
     /**
-     * An implementation of {@link HttpServletResponseWrapper} which counts the bytes being written as the response
-     * body using a {@link CountingServletOutputStream}.
-     */
-    private static final class CountingHttpServletResponseWrapper extends HttpServletResponseWrapper {
-        private CountingServletOutputStream outputStream;
-
-        private CountingHttpServletResponseWrapper(HttpServletResponse response) throws IOException {
-            super(response);
-        }
-
-        /**
          * The default behavior of this method is to return getOutputStream()
          * on the wrapped response object.
          */
-        @Override
-        public ServletOutputStream getOutputStream() throws IOException {
-            if (outputStream == null) {
-                outputStream = new CountingServletOutputStream(getResponse().getOutputStream());
-            }
-            return outputStream;
-        }
+    @Override public ServletOutputStream getOutputStream() throws IOException {
+      if (outputStream == null) {
+        outputStream = new CountingServletOutputStream(getResponse().getOutputStream());
+      }
+      return outputStream;
+    }
 
-        /**
+    /**
          * Get the number of bytes written to the response output stream.
          *
          * @return the number of bytes written to the response output stream
          */
-        public long getCount() {
-            return outputStream == null ? 0L : outputStream.getCount();
-        }
+    public long getCount() {
+      return outputStream == null ? 0L : outputStream.getCount();
+    }
 
-        /**
+    /**
          * The default behavior of this method is to call resetBuffer() on the wrapped response object.
          *
          * @see javax.servlet.http.HttpServletResponseWrapper#resetBuffer()
          */
-        @Override
-        public void resetBuffer() {
-            super.resetBuffer();
-            outputStream = null;
-        }
+    @Override public void resetBuffer() {
+      super.resetBuffer();
+      outputStream = null;
+    }
 
-        /**
+    /**
          * The default behavior of this method is to call reset() on the wrapped response object.
          *
          * @see javax.servlet.http.HttpServletResponseWrapper#reset()
          */
-        @Override
-        public void reset() {
-            super.reset();
-            outputStream = null;
-        }
+    @Override public void reset() {
+      super.reset();
+      outputStream = null;
     }
+  }
 
-    private final class AdditionalKeys {
+  private final class AdditionalKeys {
+    public static final String USER_AGENT = "userAgent";
 
-        public static final String USER_AGENT = "userAgent";
-        public static final String REQ_AUTH = "requestAuth";
-        public static final String PRINCIPAL = "userPrincipal";
-        public static final String REMOTE_ADDRESS = "remoteAddress";
-        public static final String HTTP_METHOD = "httpMethod";
-        public static final String PROTCOL = "protocol";
-        public static final String REQ_URI = "requestUri";
-        public static final String REQ_LENGTH = "requestLength";
-        public static final String REQ_CONTENT_TYPE = "requestContentType";
-        public static final String REQ_ENCODING = "requestEncoding";
-        public static final String REQ_STATUS = "responseStatus";
-        public static final String RESP_CONTENT_TYPE = "responseContentType";
-        public static final String RESP_ENCODING = "responseEncoding";
-        public static final String RESP_TIME = "responseTimeNanos";
-        public static final String RESP_LENGTH = "responseLength";
-    }
+    public static final String REQ_AUTH = "requestAuth";
+
+    public static final String PRINCIPAL = "userPrincipal";
+
+    public static final String REMOTE_ADDRESS = "remoteAddress";
+
+    public static final String HTTP_METHOD = "httpMethod";
+
+    public static final String PROTCOL = "protocol";
+
+    public static final String REQ_URI = "requestUri";
+
+    public static final String REQ_LENGTH = "requestLength";
+
+    public static final String REQ_CONTENT_TYPE = "requestContentType";
+
+    public static final String REQ_ENCODING = "requestEncoding";
+
+    public static final String REQ_STATUS = "responseStatus";
+
+    public static final String RESP_CONTENT_TYPE = "responseContentType";
+
+    public static final String RESP_ENCODING = "responseEncoding";
+
+    public static final String RESP_TIME = "responseTimeNanos";
+
+    public static final String RESP_LENGTH = "responseLength";
+  }
 }
