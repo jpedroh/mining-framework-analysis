413607c95119a93a80168226d9ad1733d7a57af8
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 03:43:02 +0000

    Mining Framework Analysis

--- src/main/java/com/jcabi/github/mock/MkRepoCommit.java
+++ src/main/java/com/jcabi/github/mock/MkRepoCommit.java
@@ -1,34 +1,4 @@
-/**
- * Copyright (c) 2013-2014, JCabi.com
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met: 1) Redistributions of source code must retain the above
- * copyright notice, this list of conditions and the following
- * disclaimer. 2) Redistributions in binary form must reproduce the above
- * copyright notice, this list of conditions and the following
- * disclaimer in the documentation and/or other materials provided
- * with the distribution. 3) Neither the name of the jcabi.com nor
- * the names of its contributors may be used to endorse or promote
- * products derived from this software without specific prior written
- * permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
- * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
- * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
- * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- * OF THE POSSIBILITY OF SUCH DAMAGE.
- */
 package com.jcabi.github.mock;
-
 import com.jcabi.aspects.Immutable;
 import com.jcabi.aspects.Loggable;
 import com.jcabi.github.Repo;
@@ -48,68 +18,47 @@ import lombok.ToString;
  * @todo #166 Should create test class for MkRepoCommit.
  *  Once created please remove this puzzle.
  */
-@Immutable
-@Loggable(Loggable.DEBUG)
-@ToString
-@EqualsAndHashCode(of = { "repository", "hash" })
-final class MkRepoCommit implements RepoCommit {
-
-    /**
+@Immutable @Loggable(value = Loggable.DEBUG) @ToString @EqualsAndHashCode(of = { "repository", "hash" }) final class MkRepoCommit implements RepoCommit {
+  /**
      * Commit SHA.
      */
-    private final transient String hash;
+  private final transient String hash;
 
-    /**
+  /**
      * The storage.
      */
-    private final transient MkStorage storage;
-    /**
+  private final transient MkStorage storage;
+
+  /**
      * The repository.
      */
-    private final transient Repo repository;
+  private final transient Repo repository;
 
-    /**
+  /**
      * Public ctor.
      * @param stg The storage
      * @param repo The repository
      * @param sha Commit SHA
      */
-    MkRepoCommit(final MkStorage stg, final Repo repo, final String sha) {
-        this.storage = stg;
-        this.repository = repo;
-        this.hash = sha;
-    }
+  MkRepoCommit(final MkStorage stg, @NotNull(message = "repo is never NULL") final Repo repo, @NotNull(message = "sha is never NULL") final String sha) {
+    this.storage = stg;
+    this.repository = repo;
+    this.hash = sha;
+  }
 
-    @Override
-    public int compareTo(
-        @NotNull(message = "other can't be NULL") final RepoCommit other
-    ) {
-        throw new UnsupportedOperationException("MkRepoCommit#compareTo()");
-    }
+  @Override public int compareTo(@NotNull(message = "other can\'t be NULL") final RepoCommit other) {
+    throw new UnsupportedOperationException("MkRepoCommit#compareTo()");
+  }
 
-    @Override
-    @NotNull(message = "JSON is never NULL")
-    public JsonObject json() throws IOException {
-        return new JsonNode(
-            this.storage.xml().nodes(
-                String.format(
-                    "/github/repos/repo[@coords='%s']/commits/commit[sha='%s']",
-                        this.repo().coordinates(), this.hash
-                )
-            ).get(0)
-        ).json();
-    }
+  @Override @NotNull(message = "JSON is never NULL") public JsonObject json() throws IOException {
+    return new JsonNode(this.storage.xml().nodes(String.format("/github/repos/repo[@coords=\'%s\']/commits/commit[sha=\'%s\']", this.repo().coordinates(), this.hash)).get(0)).json();
+  }
 
-    @Override
-    @NotNull(message = "repository is never NULL")
-    public Repo repo() {
-        return this.repository;
-    }
+  @Override @NotNull(message = "repository is never NULL") public Repo repo() {
+    return this.repository;
+  }
 
-    @Override
-    @NotNull(message = "sha should no be NULL")
-    public String sha() {
-        return this.hash;
-    }
-
+  @Override @NotNull(message = "sha should no be NULL") public String sha() {
+    return this.hash;
+  }
 }
