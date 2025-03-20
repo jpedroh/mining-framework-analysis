1e2052a6bc9b1bda08a182d852ada8641a4dae48
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 04:48:42 +0000

    Mining Framework Analysis

--- cq-component-maven-plugin/src/main/java/com/citytechinc/cq/component/dialog/html5smartimage/Html5SmartImageWidget.java
+++ cq-component-maven-plugin/src/main/java/com/citytechinc/cq/component/dialog/html5smartimage/Html5SmartImageWidget.java
@@ -1,173 +1,168 @@
-/**
- *    Copyright 2013 CITYTECH, Inc.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- */
 package com.citytechinc.cq.component.dialog.html5smartimage;
-
 import org.codehaus.plexus.util.StringUtils;
-
 import com.citytechinc.cq.component.annotations.config.Widget;
 import com.citytechinc.cq.component.annotations.widgets.Html5SmartImage;
 import com.citytechinc.cq.component.dialog.AbstractWidget;
 import com.citytechinc.cq.component.dialog.TabbableDialogElement;
 
-@Widget(annotationClass = Html5SmartImage.class, makerClass = Html5SmartImageWidgetMaker.class, xtype = Html5SmartImageWidget.XTYPE)
-public class Html5SmartImageWidget extends AbstractWidget implements TabbableDialogElement {
-	public static final String XTYPE = "html5smartimage";
-	private String originalName;
-	private final boolean disableFlush;
-	private final boolean disableInfo;
-	private final boolean disableZoom;
-	private final String cropParameter;
-	private final String fileNameParameter;
-	private final String fileReferenceParameter;
-	private final String mapParameter;
-	private final String rotateParameter;
-	private final String uploadUrl;
-	private final String ddGroups;
-	private final boolean allowUpload;
-	private final Integer height;
-	private final boolean tab;
-	private String title;
+@Widget(annotationClass = Html5SmartImage.class, makerClass = Html5SmartImageWidgetMaker.class, xtype = Html5SmartImageWidget.XTYPE) public class Html5SmartImageWidget extends AbstractWidget implements TabbableDialogElement {
+  public static final String XTYPE = "html5smartimage";
 
-	public Html5SmartImageWidget(Html5SmartImageWidgetParameters parameters) {
-		super(parameters);
-		originalName = parameters.getName();
-		this.disableFlush = parameters.isDisableFlush();
-		this.disableInfo = parameters.isDisableInfo();
-		this.disableZoom = parameters.isDisableZoom();
-		this.cropParameter = parameters.getCropParameter();
-		this.fileNameParameter = parameters.getFileNameParameter();
-		this.fileReferenceParameter = parameters.getFileReferenceParameter();
-		this.mapParameter = parameters.getMapParameter();
-		this.rotateParameter = parameters.getRotateParameter();
-		this.uploadUrl = parameters.getUploadUrl();
-		this.ddGroups = parameters.getDdGroups();
-		this.allowUpload = parameters.isAllowUpload();
-		this.height = parameters.getHeight();
-		this.tab = parameters.isTab();
-	}
+  private String originalName;
 
-	private static String getNameAsPrefix(String name) {
-		if (StringUtils.isEmpty(name)) {
-			return "./";
-		} else {
-			return "./" + name + "/";
-		}
-	}
+  private final boolean disableFlush;
 
-	public String getTitle() {
-		return title;
-	}
+  private final boolean disableInfo;
 
-	public void setTitle(String title) {
-		this.title = title;
-	}
+  private final boolean disableZoom;
 
-	public boolean isDisableFlush() {
-		return disableFlush;
-	}
+  private final String cropParameter;
 
-	public boolean isDisableInfo() {
-		return disableInfo;
-	}
+  private final String fileNameParameter;
 
-	public boolean isDisableZoom() {
-		return disableZoom;
-	}
+  private final String fileReferenceParameter;
 
-	public String getCropParameter() {
-		if (!StringUtils.isEmpty(cropParameter)) {
-			return getNameAsPrefix(originalName) + cropParameter;
-		}
-		return cropParameter;
-	}
+  private final String mapParameter;
 
-	public String getFileNameParameter() {
-		if (!StringUtils.isEmpty(fileNameParameter)) {
-			return getNameAsPrefix(originalName) + fileNameParameter;
-		}
-		return fileNameParameter;
-	}
+  private final String rotateParameter;
 
-	public String getFileReferenceParameter() {
-		if (!StringUtils.isEmpty(fileReferenceParameter)) {
-			return getNameAsPrefix(originalName) + fileReferenceParameter;
-		}
-		return fileReferenceParameter;
-	}
+  private final String uploadUrl;
 
-	public String getMapParameter() {
-		if (!StringUtils.isEmpty(mapParameter)) {
-			return getNameAsPrefix(originalName) + mapParameter;
-		}
-		return mapParameter;
-	}
+  private final String ddGroups;
 
-	public String getRotateParameter() {
-		if (!StringUtils.isEmpty(rotateParameter)) {
-			return getNameAsPrefix(originalName) + rotateParameter;
-		}
-		return rotateParameter;
-	}
+  private final boolean allowUpload;
 
-	public String getUploadUrl() {
-		return uploadUrl;
-	}
+  private final Integer height;
 
-	public String getDdGroups() {
-		return ddGroups;
-	}
+  private final boolean tab;
 
-	public boolean isAllowUpload() {
-		return allowUpload;
-	}
+  private String title;
 
-	public Boolean isTab() {
-		return tab;
-	}
+  public Html5SmartImageWidget(Html5SmartImageWidgetParameters parameters) {
+    super(parameters);
+    originalName = parameters.getName();
+    this.disableFlush = parameters.isDisableFlush();
+    this.disableInfo = parameters.isDisableInfo();
+    this.disableZoom = parameters.isDisableZoom();
+    this.cropParameter = parameters.getCropParameter();
+    this.fileNameParameter = parameters.getFileNameParameter();
+    this.fileReferenceParameter = parameters.getFileReferenceParameter();
+    this.mapParameter = parameters.getMapParameter();
+    this.rotateParameter = parameters.getRotateParameter();
+    this.uploadUrl = parameters.getUploadUrl();
+    this.ddGroups = parameters.getDdGroups();
+    this.allowUpload = parameters.isAllowUpload();
+    this.height = parameters.getHeight();
+    this.tab = parameters.isTab();
+  }
 
-	public String getLabel() {
-		return getFieldLabel();
-	}
+  private static String getNameAsPrefix(String name) {
+    if (StringUtils.isEmpty(name)) {
+      return "./";
+    } else {
+      return "./" + name + "/";
+    }
+  }
 
-	public Integer getHeight() {
-		return height;
-	}
+  public String getTitle() {
+    return title;
+  }
 
-	public String getRequestSuffix() {
-		if (StringUtils.isEmpty(originalName)) {
-			return ".img.png";
-		} else {
-			return ".img." + originalName + ".png";
-		}
-	}
+  public void setTitle(String title) {
+    this.title = title;
+  }
 
-	@Override
-	public String getName() {
-		return getNameAsPrefix(originalName);
-	}
+  public boolean isDisableFlush() {
+    return disableFlush;
+  }
 
-	@Override
-	public void setName(String name) {
-		String newName = name;
-		if (name.startsWith("./")) {
-			newName = newName.substring(2);
-		}
-		if (name.endsWith("/")) {
-			newName = newName.substring(0, newName.length() - 1);
-		}
-		originalName = newName;
-	}
+  public boolean isDisableInfo() {
+    return disableInfo;
+  }
+
+  public boolean isDisableZoom() {
+    return disableZoom;
+  }
+
+  public String getCropParameter() {
+    if (!StringUtils.isEmpty(cropParameter)) {
+      return getNameAsPrefix(originalName) + cropParameter;
+    }
+    return cropParameter;
+  }
+
+  public String getFileNameParameter() {
+    if (!StringUtils.isEmpty(fileNameParameter)) {
+      return getNameAsPrefix(originalName) + fileNameParameter;
+    }
+    return fileNameParameter;
+  }
+
+  public String getFileReferenceParameter() {
+    if (!StringUtils.isEmpty(fileReferenceParameter)) {
+      return getNameAsPrefix(originalName) + fileReferenceParameter;
+    }
+    return fileReferenceParameter;
+  }
+
+  public String getMapParameter() {
+    if (!StringUtils.isEmpty(mapParameter)) {
+      return getNameAsPrefix(originalName) + mapParameter;
+    }
+    return mapParameter;
+  }
+
+  public String getRotateParameter() {
+    if (!StringUtils.isEmpty(rotateParameter)) {
+      return getNameAsPrefix(originalName) + rotateParameter;
+    }
+    return rotateParameter;
+  }
+
+  public String getUploadUrl() {
+    return uploadUrl;
+  }
+
+  public String getDdGroups() {
+    return ddGroups;
+  }
+
+  public boolean isAllowUpload() {
+    return allowUpload;
+  }
+
+  public Boolean isTab() {
+    return tab;
+  }
+
+  public String getLabel() {
+    return getFieldLabel();
+  }
+
+  public Integer getHeight() {
+    return height;
+  }
+
+  public String getRequestSuffix() {
+    if (StringUtils.isEmpty(originalName)) {
+      return ".img.png";
+    } else {
+      return ".img." + originalName + ".png";
+    }
+  }
+
+  @Override public String getName() {
+    return getNameAsPrefix(originalName);
+  }
+
+  @Override public void setName(String name) {
+    String newName = name;
+    if (name.startsWith("./")) {
+      newName = newName.substring(2);
+    }
+    if (name.endsWith("/")) {
+      newName = newName.substring(0, newName.length() - 1);
+    }
+    originalName = newName;
+  }
 }
