038489f9d781dd70489583e9c1538be3278b29c5
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 10:18:50 +0000

    Mining Framework Analysis

--- src/main/java/org/geojson/Point.java
+++ src/main/java/org/geojson/Point.java
@@ -1,56 +1,52 @@
 package org.geojson;
 
 public class Point extends GeoJsonObject {
+  private LngLatAlt coordinates;
 
-	private LngLatAlt coordinates;
+  public Point() {
+  }
 
-	public Point() {
-	}
+  public Point(LngLatAlt coordinates) {
+    this.coordinates = coordinates;
+  }
 
-	public Point(LngLatAlt coordinates) {
-		this.coordinates = coordinates;
-	}
+  public Point(double longitude, double latitude) {
+    coordinates = new LngLatAlt(longitude, latitude);
+  }
 
-	public Point(double longitude, double latitude) {
-		coordinates = new LngLatAlt(longitude, latitude);
-	}
+  public Point(double longitude, double latitude, double altitude) {
+    coordinates = new LngLatAlt(longitude, latitude, altitude);
+  }
 
-	public Point(double longitude, double latitude, double altitude) {
-		coordinates = new LngLatAlt(longitude, latitude, altitude);
-	}
+  public LngLatAlt getCoordinates() {
+    return coordinates;
+  }
 
-	public LngLatAlt getCoordinates() {
-		return coordinates;
-	}
+  public void setCoordinates(LngLatAlt coordinates) {
+    this.coordinates = coordinates;
+  }
 
-	public void setCoordinates(LngLatAlt coordinates) {
-		this.coordinates = coordinates;
-	}
+  @Override public <T extends java.lang.Object> T accept(GeoJsonObjectVisitor<T> geoJsonObjectVisitor) {
+    return geoJsonObjectVisitor.visit(this);
+  }
 
-	@Override
-	public <T> T accept(GeoJsonObjectVisitor<T> geoJsonObjectVisitor) {
-		return geoJsonObjectVisitor.visit(this);
-	}
+  @Override public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof Point)) {
+      return false;
+    }
+    if (!super.equals(o)) {
+      return false;
+    }
+    Point point = (Point) o;
+    return !(coordinates != null ? !coordinates.equals(point.coordinates) : point.coordinates != null);
+  }
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) {
-			return true;
-		}
-		if (!(o instanceof Point)) {
-			return false;
-		}
-		if (!super.equals(o)) {
-			return false;
-		}
-		Point point = (Point)o;
-		return !(coordinates != null ? !coordinates.equals(point.coordinates) : point.coordinates != null);
-	}
-
-	@Override
-	public int hashCode() {
-		int result = super.hashCode();
-		result = 31 * result + (coordinates != null ? coordinates.hashCode() : 0);
-		return result;
-	}
+  @Override public int hashCode() {
+    int result = super.hashCode();
+    result = 31 * result + (coordinates != null ? coordinates.hashCode() : 0);
+    return result;
+  }
 }
