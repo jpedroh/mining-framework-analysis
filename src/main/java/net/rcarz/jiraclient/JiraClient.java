74bdbda870e2c1eab9a9c51a820a7e486fa531e2
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 12:35:24 +0000

    Mining Framework Analysis

--- src/main/java/net/rcarz/jiraclient/JiraClient.java
+++ src/main/java/net/rcarz/jiraclient/JiraClient.java
@@ -123,7 +123,7 @@ public class JiraClient {
         );
         return customFieldOptions;
     }
-    
+	
     /**
      * Get a list of options for a components
      *
@@ -134,16 +134,16 @@ public class JiraClient {
      *
      * @throws JiraException when the search fails
      */
-    public List<Component> getComponentsAllowedValues(String project, String issueType) throws JiraException {
-        JSONObject createMetadata = (JSONObject) Issue.getCreateMetadata(restclient, project, issueType);
-        JSONObject fieldMetadata = (JSONObject) createMetadata.get(Field.COMPONENTS);
-        List<Component> componentOptions = Field.getResourceArray(
-            Component.class,
-            fieldMetadata.get("allowedValues"),
-            restclient
+	public List<Component> getComponentsAllowedValues(String project, String issueType) throws JiraException {
+		JSONObject createMetadata = (JSONObject) Issue.getCreateMetadata(restclient, project, issueType);
+ 		JSONObject fieldMetadata = (JSONObject) createMetadata.get(Field.COMPONENTS);
+ 		List<Component> componentOptions = Field.getResourceArray(
+			Component.class,
+			fieldMetadata.get("allowedValues"),
+			restclient
         );
-        return componentOptions;
-    }
+ 		return componentOptions;
+	}
 
     public RestClient getRestClient() {
         return restclient;
