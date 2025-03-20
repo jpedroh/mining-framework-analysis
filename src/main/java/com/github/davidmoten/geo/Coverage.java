364df38bb4d0ce6af4282813316cca2f985f34ad
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 11:55:04 +0000

    Mining Framework Analysis

--- src/main/java/com/github/davidmoten/geo/Coverage.java
+++ src/main/java/com/github/davidmoten/geo/Coverage.java
@@ -1,52 +1,30 @@
 package com.github.davidmoten.geo;
-
 import java.util.Set;
 
-/**
- * A set of hashes and a measure of how well those hashes cover a region.
- * 
- * @author dave
- * 
- */
 public class Coverage {
+  private final Set<String> hashes;
 
-	private final Set<String> hashes;
-	private final double ratio;
+  private final double ratio;
 
-	/**
-	 * Constructor.
-	 * 
-	 * @param hashes
-	 * @param ratio
-	 */
-	public Coverage(Set<String> hashes, double ratio) {
-		super();
-		this.hashes = hashes;
-		this.ratio = ratio;
-	}
+  public Coverage(Set<String> hashes, double ratio) {
+    super();
+    this.hashes = hashes;
+    this.ratio = ratio;
+  }
 
-	/**
-	 * Returns the hashes.
-	 * 
-	 * @return
-	 */
-	public Set<String> getHashes() {
-		return hashes;
-	}
+  public Set<String> getHashes() {
+    return hashes;
+  }
 
-	/**
-	 * Returns the measure of how well the hashes cover a region.
-	 * 
-	 * @return
-	 */
-	public double getRatio() {
-		return ratio;
-	}
+  public double getRatio() {
+    return ratio;
+  }
 
-	public int getHashLength() {
-		if (hashes.size() == 0)
-			return 0;
-		else
-			return hashes.iterator().next().length();
-	}
+  public int getHashLength() {
+    if (hashes.size() == 0) {
+      return 0;
+    } else {
+      return hashes.iterator().next().length();
+    }
+  }
 }
