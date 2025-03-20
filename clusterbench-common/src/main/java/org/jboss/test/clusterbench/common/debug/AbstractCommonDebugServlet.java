9e475b7e2f335630fa2b289d6754943d79a0f1dd
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 20:31:33 +0000

    Mining Framework Analysis

--- clusterbench-common/src/main/java/org/jboss/test/clusterbench/common/debug/AbstractCommonDebugServlet.java
+++ clusterbench-common/src/main/java/org/jboss/test/clusterbench/common/debug/AbstractCommonDebugServlet.java
@@ -1,21 +1,4 @@
-/*
- * Copyright 2013 Radoslav Husár
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.jboss.test.clusterbench.common.debug;
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.URLDecoder;
@@ -26,12 +9,10 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-
 import org.jboss.test.clusterbench.common.ClusterBenchConstants;
 import org.jboss.test.clusterbench.common.SerialBean;
 
@@ -42,115 +23,87 @@ import org.jboss.test.clusterbench.common.SerialBean;
  * @version April 2012
  */
 public abstract class AbstractCommonDebugServlet extends HttpServlet {
+  private static final Logger log = Logger.getLogger(AbstractCommonDebugServlet.class.getName());
 
-    private static final Logger log = Logger.getLogger(AbstractCommonDebugServlet.class.getName());
-    public static final String KEY = AbstractCommonDebugServlet.class.getName();
+  public static final String KEY = AbstractCommonDebugServlet.class.getName();
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        HttpSession session = req.getSession(true);
-        resp.setContentType("text/plain");
-        resp.setCharacterEncoding(StandardCharsets.UTF_8.name());
-        PrintWriter out = resp.getWriter();
-
-        if (session.isNew()) {
-            log.log(Level.INFO, "New session created: {0}", session.getId());
-            session.setAttribute(KEY, new SerialBean());
-        } else if (session.getAttribute(KEY) == null) {
-            log.log(Level.INFO, "Session is not new, creating SerialBean: {0}", session.getId());
-            session.setAttribute(KEY, new SerialBean());
-        }
-
-        SerialBean bean = (SerialBean) session.getAttribute(KEY);
-
-        resp.setContentType("text/plain");
-
-        // Readonly?
-        if (req.getParameter(ClusterBenchConstants.READONLY) != null) {
-            out.print(bean.getSerial());
-            out.println(this.getContainerSpecificDebugInfo(req));
-            return;
-        }
-
-        int serial = bean.getSerial();
-        bean.setSerial(serial + 1);
-
-        // Now store bean in the session
-        session.setAttribute(KEY, bean);
-
-        // Write out request headers
-        Enumeration<String> headers = req.getHeaderNames();
-        while (headers.hasMoreElements()) {
-            String header = headers.nextElement();
-            out.println("Request header: " + header + "=" + req.getHeader(header));
-        }
-
-        // Request URI
-        out.println("Request URI: " + req.getRequestURI());
-        // Query path of the URL
-        out.println("Query string: " + req.getQueryString());
-        // Query path of the URL decoded
-        out.println("Query string UTF-8 decoded: " + ((req.getQueryString() == null) ? "null" :
-                                                    URLDecoder.decode(req.getQueryString(), StandardCharsets.UTF_8)));
-        // Extra path information
-        out.println("Path info: " + req.getPathInfo());
-        // Common debug info
-        out.println("Serial: " + serial);
-        // Get the session ID with the route (if present)
-        out.println("Session ID: " + req.getSession().getId());
-        // Current invocation time; useful for testing session timeouts
-        out.println("Current time: " + new Date());
-        // Display Server/Local ports
-        out.println("ServletRequest.getServerPort(): " + req.getServerPort());
-        out.println("ServletRequest.getLocalPort(): " + req.getLocalPort());
-        // Fetch just the node name for now
-        out.println("Node name: " + System.getProperty("jboss.node.name"));
-        // All parameters from current request
-        out.println(printRequestParameters(req));
-
-        // Container/EE-specific debug info
-        out.println(this.getContainerSpecificDebugInfo(req));
-
-        // Invalidate?
-        if (req.getParameter(ClusterBenchConstants.INVALIDATE) != null) {
-            log.log(Level.INFO, "Invalidating: {0}", session.getId());
-            session.invalidate();
-        }
+  @Override protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    HttpSession session = req.getSession(true);
+    resp.setContentType("text/plain");
+    resp.setCharacterEncoding(StandardCharsets.UTF_8.name());
+    PrintWriter out = resp.getWriter();
+    if (session.isNew()) {
+      log.log(Level.INFO, "New session created: {0}", session.getId());
+      session.setAttribute(KEY, new SerialBean());
+    } else {
+      if (session.getAttribute(KEY) == null) {
+        log.log(Level.INFO, "Session is not new, creating SerialBean: {0}", session.getId());
+        session.setAttribute(KEY, new SerialBean());
+      }
     }
-
-    private String printRequestParameters(HttpServletRequest request) {
-        final StringBuilder responseText = new StringBuilder();
-
-        responseText.append("Parameters [key=value]: {");
-
-        final Map<String, String[]> params = request.getParameterMap();
-
-        final Iterator<String> i = params.keySet().iterator();
-        while (i.hasNext()) {
-            String key = i.next();
-            String value = (params.get(key))[0];
-            responseText.append("[");
-            responseText.append(key);
-            responseText.append("=");
-            responseText.append(value);
-            responseText.append("]");
-            if (i.hasNext()) responseText.append(" ");
-        }
-        responseText.append("}");
-
-        return responseText.toString();
+    SerialBean bean = (SerialBean) session.getAttribute(KEY);
+    resp.setContentType("text/plain");
+    if (req.getParameter(ClusterBenchConstants.READONLY) != null) {
+      out.print(bean.getSerial());
+      out.println(this.getContainerSpecificDebugInfo(req));
+      return;
     }
-
-    @Override
-    public String getServletInfo() {
-        return "Debug servlet.";
+    int serial = bean.getSerial();
+    bean.setSerial(serial + 1);
+    session.setAttribute(KEY, bean);
+    Enumeration<String> headers = req.getHeaderNames();
+    while (headers.hasMoreElements()) {
+      String header = headers.nextElement();
+      out.println("Request header: " + header + "=" + req.getHeader(header));
     }
+    out.println("Request URI: " + req.getRequestURI());
+    out.println("Query string: " + req.getQueryString());
+    out.println("Query string UTF-8 decoded: " + ((req.getQueryString() == null) ? "null" : URLDecoder.decode(req.getQueryString(), StandardCharsets.UTF_8)));
+    out.println("Path info: " + req.getPathInfo());
+    out.println("Serial: " + serial);
+    out.println("Session ID: " + req.getSession().getId());
+    out.println("Current time: " + new Date());
+    out.println("ServletRequest.getServerPort(): " + req.getServerPort());
+    out.println("ServletRequest.getLocalPort(): " + req.getLocalPort());
+    out.println("Node name: " + System.getProperty("jboss.node.name"));
+    out.println(printRequestParameters(req));
+    out.println(this.getContainerSpecificDebugInfo(req));
+    if (req.getParameter(ClusterBenchConstants.INVALIDATE) != null) {
+      log.log(Level.INFO, "Invalidating: {0}", session.getId());
+      session.invalidate();
+    }
+  }
 
-    /**
+  private String printRequestParameters(HttpServletRequest request) {
+    final StringBuilder responseText = new StringBuilder();
+    responseText.append("Parameters [key=value]: {");
+    final Map<String, String[]> params = request.getParameterMap();
+    final Iterator<String> i = params.keySet().iterator();
+    while (i.hasNext()) {
+      String key = i.next();
+      String value = (params.get(key))[0];
+      responseText.append("[");
+      responseText.append(key);
+      responseText.append("=");
+      responseText.append(value);
+      responseText.append("]");
+      if (i.hasNext()) {
+        responseText.append(" ");
+      }
+    }
+    responseText.append("}");
+    return responseText.toString();
+  }
+
+  @Override public String getServletInfo() {
+    return "Debug servlet.";
+  }
+
+  /**
      * Implement this method to print out any debug info specific to the container or EE version.
      *
      * @param req HttpServletRequest
      * @return debug info String
      */
-    abstract public String getContainerSpecificDebugInfo(HttpServletRequest req);
+  abstract public String getContainerSpecificDebugInfo(HttpServletRequest req);
 }
