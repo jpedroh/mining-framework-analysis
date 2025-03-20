d0a3f7714abe9f08d695e8db1c33cf11b89f07e2
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 00:06:03 +0000

    Mining Framework Analysis

--- src/main/java/com/hmsonline/trident/cql/CqlClientFactory.java
+++ src/main/java/com/hmsonline/trident/cql/CqlClientFactory.java
@@ -1,113 +1,30 @@
-package com.hmsonline.trident.cql;
-
-import java.io.Serializable;
-import java.net.InetSocketAddress;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.apache.commons.lang.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.datastax.driver.core.Cluster;
-import com.datastax.driver.core.ConsistencyLevel;
-import com.datastax.driver.core.ProtocolOptions;
-import com.datastax.driver.core.QueryOptions;
-import com.datastax.driver.core.Session;
-import com.datastax.driver.core.exceptions.NoHostAvailableException;
-
-/**
- * @author boneill
- */
-public class CqlClientFactory implements Serializable {
-    private static final long serialVersionUID = 1L;
-    private static final Logger LOG = LoggerFactory.getLogger(CqlClientFactory.class);
-    private Map<String, Session> sessions = new HashMap<String, Session>();
-    private Session defaultSession = null;
-    private String[] hosts;
-    private String clusterName = null;
-    private ConsistencyLevel clusterConsistencyLevel= null;
-    private ConsistencyLevel serialConsistencyLevel = null;
-
-    protected static Cluster cluster;
-    private final ProtocolOptions.Compression compression;
-
-    public CqlClientFactory(String hosts) {
-        this(hosts, null, ConsistencyLevel.QUORUM, QueryOptions.DEFAULT_SERIAL_CONSISTENCY_LEVEL, ProtocolOptions.Compression.NONE);
-    }
-
-    public CqlClientFactory(String hosts, ConsistencyLevel clusterConsistency) {
-        this(hosts, null, clusterConsistency, QueryOptions.DEFAULT_SERIAL_CONSISTENCY_LEVEL, ProtocolOptions.Compression.NONE);
-    }
-
-    public CqlClientFactory(String hosts, String clusterName, ConsistencyLevel clusterConsistency,
-                            ConsistencyLevel conditionalUpdateConsistency, ProtocolOptions.Compression compression) {
-        this.hosts = hosts.split(",");
-        this.clusterConsistencyLevel = clusterConsistency;
-        if (conditionalUpdateConsistency != null){
-            this.serialConsistencyLevel = conditionalUpdateConsistency;
-        }
-        if (clusterName != null) {
-            this.clusterName = clusterName;
-        }
-        this.compression = compression;
-    }
-
-    public synchronized Session getSession(String keyspace) {
-        Session session = sessions.get(keyspace);
-        if (session == null) {
-            LOG.debug("Constructing session for keyspace [" + keyspace + "]");
-            session = getCluster().connect(keyspace);
-            sessions.put(keyspace, session);
-        }
-        return session;
-    }
-
-    public synchronized Session getSession() {
-        if (defaultSession == null)
-            defaultSession = getCluster().connect();
-        return defaultSession;
-    }
-    
-    public Cluster getCluster() {
-        if (cluster == null || cluster.isClosed()) {
-            if (cluster != null && cluster.isClosed()){
-                LOG.warn("Cluster closed, reconstructing cluster for [{}]", cluster.getClusterName());
-            }
-            try {
-                List<InetSocketAddress> sockets = new ArrayList<InetSocketAddress>();
-                for (String host : hosts) {
-                    if(StringUtils.contains(host, ":")) {
-                        String hostParts [] = StringUtils.split(host, ":");
-                        sockets.add(new InetSocketAddress(hostParts[0], Integer.valueOf(hostParts[1])));
-                        LOG.debug("Connecting to [" + host + "] with port [" + hostParts[1] + "]");
-                    } else {
-                        sockets.add(new InetSocketAddress(host, ProtocolOptions.DEFAULT_PORT));
-                        LOG.debug("Connecting to [" + host + "] with port [" + ProtocolOptions.DEFAULT_PORT + "]");
-                    }
-                }
-
-                Cluster.Builder builder = Cluster.builder().addContactPointsWithPorts(sockets).withCompression(compression);
-                QueryOptions queryOptions = new QueryOptions();
-                queryOptions.setConsistencyLevel(clusterConsistencyLevel);
-                queryOptions.setSerialConsistencyLevel(serialConsistencyLevel);
-                builder = builder.withQueryOptions(queryOptions);
-
-                if (StringUtils.isNotEmpty(clusterName)) {
-                    builder = builder.withClusterName(clusterName);
-                }
-
-                cluster = builder.build();
-                if (cluster == null) {
-                    throw new RuntimeException("Critical error: cluster is null after "
-                            + "attempting to build with contact points (hosts) " + hosts);
-                }
-            } catch (NoHostAvailableException e) {
-                throw new RuntimeException(e);
-            }
-        }
-        return cluster;
-    }
-}
+  package    com . hmsonline . trident . cql ;   import   java . io . Serializable ;  import   java . net . InetSocketAddress ;  import   java . util . ArrayList ;  import   java . util . HashMap ;  import   java . util . List ;  import   java . util . Map ;  import     org . apache . commons . lang . StringUtils ;  import   org . slf4j . Logger ;  import   org . slf4j . LoggerFactory ;  import     com . datastax . driver . core . Cluster ;  import     com . datastax . driver . core . ConsistencyLevel ;  import     com . datastax . driver . core . ProtocolOptions ;  import     com . datastax . driver . core . QueryOptions ;  import     com . datastax . driver . core . Session ;  import      com . datastax . driver . core . exceptions . NoHostAvailableException ;   public class CqlClientFactory  implements  Serializable  {   private static final  long  serialVersionUID = 1L ;   private static final Logger  LOG =  LoggerFactory . getLogger  (  CqlClientFactory . class ) ;   private  Map  < String , Session >  sessions =  new  HashMap  < String , Session >  ( ) ;   private Session  defaultSession = null ;   private  String  [ ]  hosts ;   private String  clusterName = null ;   private ConsistencyLevel  clusterConsistencyLevel = null ;   private ConsistencyLevel  serialConsistencyLevel = null ;   protected static Cluster  cluster ;   public CqlClientFactory  (  String hosts )  {  this  ( hosts , null ,  ConsistencyLevel . QUORUM ,  
+<<<<<<<
+QueryOptions
+=======
+ ProtocolOptions . Compression
+>>>>>>>
+ . 
+<<<<<<<
+DEFAULT_SERIAL_CONSISTENCY_LEVEL
+=======
+NONE
+>>>>>>>
+ ) ; }   public CqlClientFactory  (  String hosts ,  ConsistencyLevel clusterConsistency )  {  this  ( hosts , null , clusterConsistency ,  
+<<<<<<<
+QueryOptions
+=======
+ ProtocolOptions . Compression
+>>>>>>>
+ . 
+<<<<<<<
+DEFAULT_SERIAL_CONSISTENCY_LEVEL
+=======
+NONE
+>>>>>>>
+ ) ; } 
+<<<<<<<
+  public CqlClientFactory  (  String hosts ,  String clusterName ,  ConsistencyLevel clusterConsistency ,  ConsistencyLevel conditionalUpdateConsistency )  {    this . hosts =  hosts . split  ( "," ) ;    this . clusterConsistencyLevel = clusterConsistency ;  if  (  conditionalUpdateConsistency != null )  {    this . serialConsistencyLevel = conditionalUpdateConsistency ; }  if  (  clusterName != null )  {    this . clusterName = clusterName ; } }
+=======
+>>>>>>>
+   public synchronized Session getSession  (  String keyspace )  {  Session  session =  sessions . get  ( keyspace ) ;  if  (  session == null )  {   LOG . debug  (   "Constructing session for keyspace [" + keyspace + "]" ) ;   session =   getCluster  ( ) . connect  ( keyspace ) ;   sessions . put  ( keyspace , session ) ; }  return session ; }   public synchronized Session getSession  ( )  {  if  (  defaultSession == null )   defaultSession =   getCluster  ( ) . connect  ( ) ;  return defaultSession ; }   public Cluster getCluster  ( )  {  if  (   cluster == null ||  cluster . isClosed  ( ) )  {  if  (   cluster != null &&  cluster . isClosed  ( ) )  {   LOG . warn  ( "Cluster closed, reconstructing cluster for [{}]" ,  cluster . getClusterName  ( ) ) ; }  try  {   List  < InetSocketAddress >  sockets =  new  ArrayList  < InetSocketAddress >  ( ) ;  for ( String host : hosts )  {  if  (  StringUtils . contains  ( host , ":" ) )  {  String  hostParts  [ ] =  StringUtils . split  ( host , ":" ) ;   sockets . add  (  new InetSocketAddress  (  hostParts [ 0 ] ,  Integer . valueOf  (  hostParts [ 1 ] ) ) ) ;   LOG . debug  (     "Connecting to [" + host + "] with port [" +  hostParts [ 1 ] + "]" ) ; } else  {   sockets . add  (  new InetSocketAddress  ( host ,  ProtocolOptions . DEFAULT_PORT ) ) ;   LOG . debug  (     "Connecting to [" + host + "] with port [" +  ProtocolOptions . DEFAULT_PORT + "]" ) ; } }   Cluster . Builder  builder =    Cluster . builder  ( ) . addContactPointsWithPorts  ( sockets ) . withCompression  ( compression ) ;  QueryOptions  queryOptions =  new QueryOptions  ( ) ;   queryOptions . setConsistencyLevel  ( clusterConsistencyLevel ) ;   queryOptions . setSerialConsistencyLevel  ( serialConsistencyLevel ) ;   builder =  builder . withQueryOptions  ( queryOptions ) ;  if  (  StringUtils . isNotEmpty  ( clusterName ) )  {   builder =  builder . withClusterName  ( clusterName ) ; }   cluster =  builder . build  ( ) ;  if  (  cluster == null )  {  throw  new RuntimeException  (   "Critical error: cluster is null after " + "attempting to build with contact points (hosts) " + hosts ) ; } }  catch (   NoHostAvailableException e )  {  throw  new RuntimeException  ( e ) ; } }  return cluster ; }   private final  ProtocolOptions . Compression  compression ;   public CqlClientFactory  (  String hosts ,  String clusterName ,  ConsistencyLevel clusterConsistency ,  ConsistencyLevel conditionalUpdateConsistency ,   ProtocolOptions . Compression compression )  {    this . hosts =  hosts . split  ( "," ) ;    this . consistencyLevel = clusterConsistency ;  if  (  conditionalUpdateConsistency != null )  {    this . serialConsistencyLevel = conditionalUpdateConsistency ; }  if  (  clusterName != null )  {    this . clusterName = clusterName ; }    this . compression = compression ; } }
