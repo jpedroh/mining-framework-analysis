4e57fb0bb871e9e598c308504078d97e819fa868
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 05:09:36 +0000

    Mining Framework Analysis

--- src/main/java/de/is24/deadcode4j/plugin/packaginghandler/DefaultPackagingHandler.java
+++ src/main/java/de/is24/deadcode4j/plugin/packaginghandler/DefaultPackagingHandler.java
@@ -1,41 +1,6 @@
-package de.is24.deadcode4j.plugin.packaginghandler;
-
-import de.is24.deadcode4j.Repository;
-import org.apache.maven.plugin.logging.Log;
-import org.apache.maven.project.MavenProject;
-
-import javax.annotation.Nonnull;
-import java.io.File;
-import java.util.Collection;
-import java.util.concurrent.Callable;
-
-import static de.is24.deadcode4j.Utils.getKeyFor;
-import static java.util.Collections.emptyList;
-import static java.util.Collections.singleton;
-
-/**
- * The <code>DefaultPackagingHandler</code> returns the default output directory if it exists.
- *
- * @since 1.2.0
- */
-public class DefaultPackagingHandler extends PackagingHandler {
-
-    public DefaultPackagingHandler(Callable<Log> logAccessor) {
-        super(logAccessor);
-    }
-
-    @Override
-    @Nonnull
-    public Collection<Repository> getRepositoriesFor(@Nonnull MavenProject project) {
-        File outputDirectory = new File(project.getBuild().getOutputDirectory());
-        if (!outputDirectory.exists()) {
-            getLog().warn("The output directory of " + getKeyFor(project) +
-                    " does not exist - assuming the project simply has nothing to provide!");
-            return emptyList();
-        }
-        if (getLog().isDebugEnabled()) {
-            getLog().debug("Going to analyze output directory [" + outputDirectory + "].");
-        }
-        return singleton(new Repository(outputDirectory));
-    }
-}
+  package     de . is24 . deadcode4j . plugin . packaginghandler ;   import      org . apache . maven . plugin . logging . Log ;  import     org . apache . maven . project . MavenProject ;  import   javax . annotation . Nonnull ;  import   java . io . File ;  import   java . util . Collection ;  import   java . util . Collections ;  import    java . util . concurrent . Callable ;  import static     de . is24 . deadcode4j . Utils . getKeyFor ;  import static    java . util . Collections . emptyList ;  import static    java . util . Collections . singleton ;  import    de . is24 . deadcode4j . Repository ;   public class DefaultPackagingHandler  extends PackagingHandler  {   public DefaultPackagingHandler  (   Callable  < Log > logAccessor )  {  super  ( logAccessor ) ; } 
+<<<<<<<
+   @ Override  @ Nonnull public  Collection  < CodeRepository > getCodeRepositoriesFor  (    @ Nonnull MavenProject project )  {  File  outputDirectory =  new File  (   project . getBuild  ( ) . getOutputDirectory  ( ) ) ;  if  (  !  outputDirectory . exists  ( ) )  {    getLog  ( ) . warn  (   "The output directory of " +  getKeyFor  ( project ) + " does not exist - assuming the project simply has nothing to provide!" ) ;  return  emptyList  ( ) ; }  if  (   getLog  ( ) . isDebugEnabled  ( ) )  {    getLog  ( ) . debug  (   "Going to analyze output directory [" + outputDirectory + "]." ) ; }  return  singleton  (  new CodeRepository  (  Collections .  < File > emptyList  ( ) , outputDirectory ) ) ; }
+=======
+>>>>>>>
+    @ Override  @ Nonnull public  Collection  < Repository > getRepositoriesFor  (    @ Nonnull MavenProject project )  {  File  outputDirectory =  new File  (   project . getBuild  ( ) . getOutputDirectory  ( ) ) ;  if  (  !  outputDirectory . exists  ( ) )  {    getLog  ( ) . warn  (   "The output directory of " +  getKeyFor  ( project ) + " does not exist - assuming the project simply has nothing to provide!" ) ;  return  emptyList  ( ) ; }  if  (   getLog  ( ) . isDebugEnabled  ( ) )  {    getLog  ( ) . debug  (   "Going to analyze output directory [" + outputDirectory + "]." ) ; }  return  singleton  (  new Repository  ( outputDirectory ) ) ; } }
