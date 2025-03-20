57cd66009901b8f6e83136acc63d0c1ba42d1f76
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 12:05:57 +0000

    Mining Framework Analysis

--- chat/src/main/java/net/md_5/bungee/api/chat/HoverEvent.java
+++ chat/src/main/java/net/md_5/bungee/api/chat/HoverEvent.java
@@ -1,26 +1,18 @@
 package net.md_5.bungee.api.chat;
-
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
-@Getter
-@ToString
-@EqualsAndHashCode
-@RequiredArgsConstructor
-final public class HoverEvent
-{
+@Getter @ToString @EqualsAndHashCode @RequiredArgsConstructor final public class HoverEvent {
+  private final Action action;
 
-    private final Action action;
-    private final BaseComponent[] value;
+  private final BaseComponent[] value;
 
-    public enum Action
-    {
-
-        SHOW_TEXT,
-        SHOW_ACHIEVEMENT,
-        SHOW_ITEM,
-        SHOW_ENTITY
-    }
+  public enum Action {
+    SHOW_TEXT,
+    SHOW_ACHIEVEMENT,
+    SHOW_ITEM,
+    SHOW_ENTITY
+  }
 }
