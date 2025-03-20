540853a680730da20db102b32de9c41fcd42b9dc
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 02:23:10 +0000

    Mining Framework Analysis

--- amazon-kinesis-client/src/main/java/software/amazon/kinesis/retrieval/RetrievalConfig.java
+++ amazon-kinesis-client/src/main/java/software/amazon/kinesis/retrieval/RetrievalConfig.java
@@ -1,20 +1,4 @@
-/*
- * Copyright 2019 Amazon.com, Inc. or its affiliates.
- * Licensed under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package software.amazon.kinesis.retrieval;
-
 import com.amazonaws.services.schemaregistry.deserializers.GlueSchemaRegistryDeserializer;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -38,67 +22,60 @@ import software.amazon.kinesis.retrieval.polling.PollingConfig;
 /**
  * Used by the KCL to configure the retrieval of records from Kinesis.
  */
-@Getter
-@Setter
-@ToString
-@EqualsAndHashCode
-@Accessors(fluent = true)
-public class RetrievalConfig {
-    /**
+@Getter @Setter @ToString @EqualsAndHashCode @Accessors(fluent = true) public class RetrievalConfig {
+  /**
      * User agent set when Amazon Kinesis Client Library makes AWS requests.
      */
-    public static final String KINESIS_CLIENT_LIB_USER_AGENT = "amazon-kinesis-client-library-java";
+  public static final String KINESIS_CLIENT_LIB_USER_AGENT = "amazon-kinesis-client-library-java";
 
-    public static final String KINESIS_CLIENT_LIB_USER_AGENT_VERSION = "2.5.0-SNAPSHOT";
+  public static final String KINESIS_CLIENT_LIB_USER_AGENT_VERSION = "2.5.0-SNAPSHOT";
 
-    /**
+  /**
      * Client used to make calls to Kinesis for records retrieval
      */
-    @NonNull
-    private final KinesisAsyncClient kinesisClient;
+  @NonNull private final KinesisAsyncClient kinesisClient;
 
-    @NonNull
-    private final String applicationName;
+  @NonNull private final String applicationName;
 
-    /**
+  /**
      * Glue Schema Registry Deserializer instance.
      * If this instance is set, KCL will try to decode messages that might be
      * potentially encoded with Glue Schema Registry Serializer.
      */
-    private GlueSchemaRegistryDeserializer glueSchemaRegistryDeserializer = null;
+  private GlueSchemaRegistryDeserializer glueSchemaRegistryDeserializer = null;
 
-    /**
+  /**
      * AppStreamTracker either for multi stream tracking or single stream
      *
      * @deprecated Both single- and multi-stream support is now provided by {@link StreamTracker}.
      * @see #streamTracker
      */
-    private Either<MultiStreamTracker, StreamConfig> appStreamTracker;
+  private Either<MultiStreamTracker, StreamConfig> appStreamTracker;
 
-    /**
+  /**
      * Stream(s) to be consumed by this KCL application.
      */
-    private StreamTracker streamTracker;
+  private StreamTracker streamTracker;
 
-    /**
+  /**
      * Backoff time between consecutive ListShards calls.
      *
      * <p>
      * Default value: 1500L
      * </p>
      */
-    private long listShardsBackoffTimeInMillis = 1500L;
+  private long listShardsBackoffTimeInMillis = 1500L;
 
-    /**
+  /**
      * Max number of retries for ListShards when throttled/exception is thrown.
      *
      * <p>
      * Default value: 50
      * </p>
      */
-    private int maxListShardsRetryAttempts = 50;
+  private int maxListShardsRetryAttempts = 50;
 
-    /**
+  /**
      * The location in the shard from which the KinesisClientLibrary will start fetching records from
      * when the application starts for the first time and there is no checkpoint for the shard.
      *
@@ -110,33 +87,24 @@ public class RetrievalConfig {
      * @see StreamTracker#orphanedStreamInitialPositionInStream()
      * @see StreamTracker#createStreamConfig(StreamIdentifier)
      */
-    @Deprecated
-    private InitialPositionInStreamExtended initialPositionInStreamExtended = InitialPositionInStreamExtended
-            .newInitialPosition(InitialPositionInStream.LATEST);
+  @Deprecated private InitialPositionInStreamExtended initialPositionInStreamExtended = InitialPositionInStreamExtended.newInitialPosition(InitialPositionInStream.LATEST);
 
-    private RetrievalSpecificConfig retrievalSpecificConfig;
+  private RetrievalSpecificConfig retrievalSpecificConfig;
 
-    private RetrievalFactory retrievalFactory;
+  private RetrievalFactory retrievalFactory;
 
-    public RetrievalConfig(@NonNull KinesisAsyncClient kinesisAsyncClient, @NonNull String streamName,
-                           @NonNull String applicationName) {
-        this(kinesisAsyncClient,
-                new SingleStreamTracker(streamName, kinesisAsyncClient.serviceClientConfiguration().region()),
-                applicationName);
-    }
+  public RetrievalConfig(@NonNull KinesisAsyncClient kinesisAsyncClient, @NonNull String streamName, @NonNull String applicationName) {
+    this(kinesisAsyncClient, new SingleStreamTracker(streamName, kinesisAsyncClient.serviceClientConfiguration().region()), applicationName);
+  }
 
-    public RetrievalConfig(@NonNull KinesisAsyncClient kinesisAsyncClient, @NonNull StreamTracker streamTracker,
-                           @NonNull String applicationName) {
-        this.kinesisClient = kinesisAsyncClient;
-        this.streamTracker = streamTracker;
-        this.applicationName = applicationName;
-        this.appStreamTracker = DeprecationUtils.convert(streamTracker,
-                singleStreamTracker -> singleStreamTracker.streamConfigList().get(0));
-    }
+  public RetrievalConfig(@NonNull KinesisAsyncClient kinesisAsyncClient, @NonNull StreamTracker streamTracker, @NonNull String applicationName) {
+    this.kinesisClient = kinesisAsyncClient;
+    this.streamTracker = streamTracker;
+    this.applicationName = applicationName;
+    this.appStreamTracker = DeprecationUtils.convert(streamTracker, (singleStreamTracker) -> singleStreamTracker.streamConfigList().get(0));
+  }
 
-    /**
-     * Convenience method to reconfigure the embedded {@link StreamTracker},
-     * but only when <b>not</b> in multi-stream mode.
+  /**
      *
      * @param initialPositionInStreamExtended
      *
@@ -144,48 +112,44 @@ public class RetrievalConfig {
      * @see StreamTracker#orphanedStreamInitialPositionInStream()
      * @see StreamTracker#createStreamConfig(StreamIdentifier)
      */
-    @Deprecated
-    public RetrievalConfig initialPositionInStreamExtended(InitialPositionInStreamExtended initialPositionInStreamExtended) {
-        if (streamTracker().isMultiStream()) {
-            throw new IllegalArgumentException(
-                    "Cannot set initialPositionInStreamExtended when multiStreamTracker is set");
-        };
-
-        final StreamIdentifier streamIdentifier = getSingleStreamIdentifier();
-        final StreamConfig updatedConfig = new StreamConfig(streamIdentifier, initialPositionInStreamExtended);
-        streamTracker = new SingleStreamTracker(streamIdentifier, updatedConfig);
-        appStreamTracker = Either.right(updatedConfig);
-        return this;
+  @Deprecated public RetrievalConfig initialPositionInStreamExtended(InitialPositionInStreamExtended initialPositionInStreamExtended) {
+    if (streamTracker().isMultiStream()) {
+      throw new IllegalArgumentException("Cannot set initialPositionInStreamExtended when multiStreamTracker is set");
     }
+    ;
+    final StreamIdentifier streamIdentifier = getSingleStreamIdentifier();
+    final StreamConfig updatedConfig = new StreamConfig(streamIdentifier, initialPositionInStreamExtended);
+    streamTracker = new SingleStreamTracker(streamIdentifier, updatedConfig);
+    appStreamTracker = Either.right(updatedConfig);
+    return this;
+  }
 
-    public RetrievalConfig retrievalSpecificConfig(RetrievalSpecificConfig retrievalSpecificConfig) {
-        retrievalSpecificConfig.validateState(streamTracker.isMultiStream());
-        this.retrievalSpecificConfig = retrievalSpecificConfig;
-        return this;
-    }
+  public RetrievalConfig retrievalSpecificConfig(RetrievalSpecificConfig retrievalSpecificConfig) {
+    retrievalSpecificConfig.validateState(streamTracker.isMultiStream());
+    this.retrievalSpecificConfig = retrievalSpecificConfig;
+    return this;
+  }
 
-    public RetrievalFactory retrievalFactory() {
-        if (retrievalFactory == null) {
-            if (retrievalSpecificConfig == null) {
-                final FanOutConfig fanOutConfig = new FanOutConfig(kinesisClient())
-                        .applicationName(applicationName());
-                if (!streamTracker.isMultiStream()) {
-                    final String streamName = getSingleStreamIdentifier().streamName();
-                    fanOutConfig.streamName(streamName);
-                }
-                retrievalSpecificConfig(fanOutConfig);
-            }
-            retrievalFactory = retrievalSpecificConfig.retrievalFactory();
+  public RetrievalFactory retrievalFactory() {
+    if (retrievalFactory == null) {
+      if (retrievalSpecificConfig == null) {
+        final FanOutConfig fanOutConfig = new FanOutConfig(kinesisClient()).applicationName(applicationName());
+        if (!streamTracker.isMultiStream()) {
+          final String streamName = getSingleStreamIdentifier().streamName();
+          fanOutConfig.streamName(streamName);
         }
-        return retrievalFactory;
+        retrievalSpecificConfig(fanOutConfig);
+      }
+      retrievalFactory = retrievalSpecificConfig.retrievalFactory();
     }
+    return retrievalFactory;
+  }
 
-    /**
+  /**
      * Convenience method to return the {@link StreamIdentifier} from a
      * single-stream tracker.
      */
-    private StreamIdentifier getSingleStreamIdentifier() {
-        return streamTracker.streamConfigList().get(0).streamIdentifier();
-    }
-
+  private StreamIdentifier getSingleStreamIdentifier() {
+    return streamTracker.streamConfigList().get(0).streamIdentifier();
+  }
 }
--- amazon-kinesis-client/src/test/java/software/amazon/kinesis/common/ConfigsBuilderTest.java
+++ amazon-kinesis-client/src/test/java/software/amazon/kinesis/common/ConfigsBuilderTest.java
@@ -1,27 +1,9 @@
-/*
- * Copyright 2023 Amazon.com, Inc. or its affiliates.
- * Licensed under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package software.amazon.kinesis.common;
-
 import java.util.Arrays;
 import java.util.Optional;
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.mockito.Mockito.mock;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -35,52 +17,51 @@ import software.amazon.kinesis.processor.SingleStreamTracker;
 import software.amazon.kinesis.processor.StreamTracker;
 import software.amazon.kinesis.utils.MockObjectHelper;
 
-@RunWith(MockitoJUnitRunner.class)
-public class ConfigsBuilderTest {
+@RunWith(value = MockitoJUnitRunner.class) public class ConfigsBuilderTest {
+  private KinesisAsyncClient mockKinesisClient;
 
-    private final KinesisAsyncClient mockKinesisClient = MockObjectHelper.createKinesisClient();
+  @Mock private DynamoDbAsyncClient mockDynamoClient;
 
-    @Mock
-    private DynamoDbAsyncClient mockDynamoClient;
+  @Mock private CloudWatchAsyncClient mockCloudWatchClient;
 
-    @Mock
-    private CloudWatchAsyncClient mockCloudWatchClient;
+  @Mock private ShardRecordProcessorFactory mockShardProcessorFactory;
 
-    @Mock
-    private ShardRecordProcessorFactory mockShardProcessorFactory;
+  private static final String APPLICATION_NAME = ConfigsBuilderTest.class.getSimpleName();
 
-    private static final String APPLICATION_NAME = ConfigsBuilderTest.class.getSimpleName();
-    private static final String WORKER_IDENTIFIER = "worker-id";
+  private static final String WORKER_IDENTIFIER = "worker-id";
 
-    @Test
-    public void testTrackerConstruction() {
-        final String streamName = "single-stream";
-        final ConfigsBuilder configByName = createConfig(streamName);
-        final ConfigsBuilder configBySingleTracker = createConfig(new SingleStreamTracker(streamName));
 
-        for (final ConfigsBuilder cb : Arrays.asList(configByName, configBySingleTracker)) {
-            assertEquals(Optional.empty(), cb.appStreamTracker().left());
-            assertEquals(streamName, cb.appStreamTracker().right().get());
-            assertEquals(streamName, cb.streamTracker().streamConfigList().get(0).streamIdentifier().streamName());
-            assertFalse(cb.streamTracker().isMultiStream());
-        }
+<<<<<<< /usr/src/app/output/awslabs/amazon-kinesis-client/de77f36bc9ba3b63308721fa5ec6368672def561/amazon-kinesis-client/src/test/java/software/amazon/kinesis/common/ConfigsBuilderTest.java/left.java
+  @Before public void setUp() {
+    MockitoAnnotations.initMocks(this);
+    mockKinesisClient = MockObjectHelper.createKinesisClient();
+  }
+=======
+>>>>>>> Unknown file: This is a bug in JDime.
 
-        final StreamTracker mockMultiStreamTracker = mock(MultiStreamTracker.class);
-        final ConfigsBuilder configByMultiTracker = createConfig(mockMultiStreamTracker);
-        assertEquals(Optional.empty(), configByMultiTracker.appStreamTracker().right());
-        assertEquals(mockMultiStreamTracker, configByMultiTracker.appStreamTracker().left().get());
-        assertEquals(mockMultiStreamTracker, configByMultiTracker.streamTracker());
+
+  @Test public void testTrackerConstruction() {
+    final String streamName = "single-stream";
+    final ConfigsBuilder configByName = createConfig(streamName);
+    final ConfigsBuilder configBySingleTracker = createConfig(new SingleStreamTracker(streamName));
+    for (final ConfigsBuilder cb : Arrays.asList(configByName, configBySingleTracker)) {
+      assertEquals(Optional.empty(), cb.appStreamTracker().left());
+      assertEquals(streamName, cb.appStreamTracker().right().get());
+      assertEquals(streamName, cb.streamTracker().streamConfigList().get(0).streamIdentifier().streamName());
+      assertFalse(cb.streamTracker().isMultiStream());
     }
+    final StreamTracker mockMultiStreamTracker = mock(MultiStreamTracker.class);
+    final ConfigsBuilder configByMultiTracker = createConfig(mockMultiStreamTracker);
+    assertEquals(Optional.empty(), configByMultiTracker.appStreamTracker().right());
+    assertEquals(mockMultiStreamTracker, configByMultiTracker.appStreamTracker().left().get());
+    assertEquals(mockMultiStreamTracker, configByMultiTracker.streamTracker());
+  }
 
-    private ConfigsBuilder createConfig(String streamName) {
-        // intentional invocation of constructor where streamName is a String
-        return new ConfigsBuilder(streamName, APPLICATION_NAME, mockKinesisClient, mockDynamoClient,
-                mockCloudWatchClient, WORKER_IDENTIFIER, mockShardProcessorFactory);
-    }
+  private ConfigsBuilder createConfig(String streamName) {
+    return new ConfigsBuilder(streamName, APPLICATION_NAME, mockKinesisClient, mockDynamoClient, mockCloudWatchClient, WORKER_IDENTIFIER, mockShardProcessorFactory);
+  }
 
-    private ConfigsBuilder createConfig(StreamTracker streamTracker) {
-        return new ConfigsBuilder(streamTracker, APPLICATION_NAME, mockKinesisClient, mockDynamoClient,
-                mockCloudWatchClient, WORKER_IDENTIFIER, mockShardProcessorFactory);
-    }
-
+  private ConfigsBuilder createConfig(StreamTracker streamTracker) {
+    return new ConfigsBuilder(streamTracker, APPLICATION_NAME, mockKinesisClient, mockDynamoClient, mockCloudWatchClient, WORKER_IDENTIFIER, mockShardProcessorFactory);
+  }
 }
