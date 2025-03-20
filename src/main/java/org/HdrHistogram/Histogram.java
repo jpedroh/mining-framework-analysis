1f38c14ec34be67cffebdb3bc569b5d692e79444
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 01:20:33 +0000

    Mining Framework Analysis

--- src/main/java/org/HdrHistogram/Histogram.java
+++ src/main/java/org/HdrHistogram/Histogram.java
@@ -1,12 +1,4 @@
-/**
- * Written by Gil Tene of Azul Systems, and released to the public domain,
- * as explained at http://creativecommons.org/publicdomain/zero/1.0/
- *
- * @author Gil Tene
- */
-
 package org.HdrHistogram;
-
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.nio.ByteBuffer;
@@ -27,7 +19,7 @@ import java.util.zip.DataFormatException;
  * within the range will thus be no larger than 1/1,000th (or 0.1%) of any value. This example Histogram could
  * be used to track and analyze the counts of observed response times ranging between 1 microsecond and 1 hour
  * in magnitude, while maintaining a value resolution of 1 microsecond up to 1 millisecond, a resolution of
- * 1 millisecond (or better) up to one second, and a resolution of 1 second (or better) up to 1,000 seconds. At its
+ * 1 millisecond (or better) up to one second, and a resolution of 1 second (or better) up to 1,000 seconds. At it's
  * maximum tracked value (1 hour), it would still maintain a resolution of 3.6 seconds (or better).
  * <p>
  * Histogram tracks value counts in <b><code>long</code></b> fields. Smaller field types are available in the
@@ -41,121 +33,99 @@ import java.util.zip.DataFormatException;
  * <p>
  * See package description for {@link org.HdrHistogram} for details.
  */
-
 public class Histogram extends AbstractHistogram {
-    long totalCount;
-    long[] counts;
-    int normalizingIndexOffset;
+  long totalCount;
 
-    @Override
-    long getCountAtIndex(final int index) {
-        return counts[normalizeIndex(index, normalizingIndexOffset)];
+  long[] counts;
+
+  int normalizingIndexOffset;
+
+  @Override long getCountAtIndex(final int index) {
+    return counts[normalizeIndex(index, normalizingIndexOffset)];
+  }
+
+  @Override long getCountAtNormalizedIndex(final int index) {
+    return counts[index];
+  }
+
+  @Override void incrementCountAtIndex(final int index) {
+    counts[normalizeIndex(index, normalizingIndexOffset)]++;
+  }
+
+  @Override void addToCountAtIndex(final int index, final long value) {
+    counts[normalizeIndex(index, normalizingIndexOffset)] += value;
+  }
+
+  @Override void setCountAtIndex(int index, long value) {
+    counts[normalizeIndex(index, normalizingIndexOffset)] = value;
+  }
+
+  @Override void setCountAtNormalizedIndex(int index, long value) {
+    counts[index] = value;
+  }
+
+  @Override int getNormalizingIndexOffset() {
+    return normalizingIndexOffset;
+  }
+
+  @Override void setNormalizingIndexOffset(int normalizingIndexOffset) {
+    this.normalizingIndexOffset = normalizingIndexOffset;
+  }
+
+  @Override void shiftNormalizingIndexByOffset(int offsetToAdd, boolean lowestHalfBucketPopulated) {
+    nonConcurrentNormalizingIndexShift(offsetToAdd, lowestHalfBucketPopulated);
+  }
+
+  @Override void clearCounts() {
+    java.util.Arrays.fill(counts, 0);
+    totalCount = 0;
+  }
+
+  @Override public Histogram copy() {
+    Histogram copy = new Histogram(this);
+    copy.add(this);
+    return copy;
+  }
+
+  @Override public Histogram copyCorrectedForCoordinatedOmission(final long expectedIntervalBetweenValueSamples) {
+    Histogram copy = new Histogram(this);
+    copy.addWhileCorrectingForCoordinatedOmission(this, expectedIntervalBetweenValueSamples);
+    return copy;
+  }
+
+  @Override public long getTotalCount() {
+    return totalCount;
+  }
+
+  @Override void setTotalCount(final long totalCount) {
+    this.totalCount = totalCount;
+  }
+
+  @Override void incrementTotalCount() {
+    totalCount++;
+  }
+
+  @Override void addToTotalCount(final long value) {
+    totalCount += value;
+  }
+
+  @Override int _getEstimatedFootprintInBytes() {
+    return (512 + (8 * counts.length));
+  }
+
+  @Override void resize(long newHighestTrackableValue) {
+    int oldNormalizedZeroIndex = normalizeIndex(0, normalizingIndexOffset);
+    establishSize(newHighestTrackableValue);
+    int countsDelta = countsArrayLength - counts.length;
+    counts = Arrays.copyOf(counts, countsArrayLength);
+    if (oldNormalizedZeroIndex != 0) {
+      int newNormalizedZeroIndex = oldNormalizedZeroIndex + countsDelta;
+      int lengthToCopy = (countsArrayLength - countsDelta) - oldNormalizedZeroIndex;
+      System.arraycopy(counts, oldNormalizedZeroIndex, counts, newNormalizedZeroIndex, lengthToCopy);
     }
+  }
 
-    @Override
-    long getCountAtNormalizedIndex(final int index) {
-        return counts[index];
-    }
-
-    @Override
-    void incrementCountAtIndex(final int index) {
-        counts[normalizeIndex(index, normalizingIndexOffset)]++;
-    }
-
-    @Override
-    void addToCountAtIndex(final int index, final long value) {
-        counts[normalizeIndex(index, normalizingIndexOffset)] += value;
-    }
-
-    @Override
-    void setCountAtIndex(int index, long value) {
-        counts[normalizeIndex(index, normalizingIndexOffset)] = value;
-    }
-
-    @Override
-    void setCountAtNormalizedIndex(int index, long value) {
-        counts[index] = value;
-    }
-
-    @Override
-    int getNormalizingIndexOffset() {
-        return normalizingIndexOffset;
-    }
-
-    @Override
-    void setNormalizingIndexOffset(int normalizingIndexOffset) {
-        this.normalizingIndexOffset = normalizingIndexOffset;
-    }
-
-    @Override
-    void shiftNormalizingIndexByOffset(int offsetToAdd, boolean lowestHalfBucketPopulated) {
-        nonConcurrentNormalizingIndexShift(offsetToAdd, lowestHalfBucketPopulated);
-    }
-
-    @Override
-    void clearCounts() {
-        java.util.Arrays.fill(counts, 0);
-        totalCount = 0;
-    }
-
-    @Override
-    public Histogram copy() {
-        Histogram copy = new Histogram(this);
-        copy.add(this);
-        return copy;
-    }
-
-    @Override
-    public Histogram copyCorrectedForCoordinatedOmission(final long expectedIntervalBetweenValueSamples) {
-        Histogram copy = new Histogram(this);
-        copy.addWhileCorrectingForCoordinatedOmission(this, expectedIntervalBetweenValueSamples);
-        return copy;
-    }
-
-    @Override
-    public long getTotalCount() {
-        return totalCount;
-    }
-
-    @Override
-    void setTotalCount(final long totalCount) {
-        this.totalCount = totalCount;
-    }
-
-    @Override
-    void incrementTotalCount() {
-        totalCount++;
-    }
-
-    @Override
-    void addToTotalCount(final long value) {
-        totalCount += value;
-    }
-
-    @Override
-    int _getEstimatedFootprintInBytes() {
-        return (512 + (8 * counts.length));
-    }
-
-    @Override
-    void resize(long newHighestTrackableValue) {
-        int oldNormalizedZeroIndex = normalizeIndex(0, normalizingIndexOffset);
-
-        establishSize(newHighestTrackableValue);
-
-        int countsDelta = countsArrayLength - counts.length;
-
-        counts = Arrays.copyOf(counts, countsArrayLength);
-
-        if (oldNormalizedZeroIndex != 0) {
-            // We need to shift the stuff from the zero index and up to the end of the array:
-            int newNormalizedZeroIndex = oldNormalizedZeroIndex + countsDelta;
-            int lengthToCopy = (countsArrayLength - countsDelta) - oldNormalizedZeroIndex;
-            System.arraycopy(counts, oldNormalizedZeroIndex, counts, newNormalizedZeroIndex, lengthToCopy);
-        }
-    }
-
-    /**
+  /**
      * Construct an auto-resizing histogram with a lowest discernible value of 1 and an auto-adjusting
      * highestTrackableValue. Can auto-reize up to track values up to (Long.MAX_VALUE / 2).
      *
@@ -163,12 +133,12 @@ public class Histogram extends AbstractHistogram {
      *                                       decimal digits to which the histogram will maintain value resolution
      *                                       and separation. Must be a non-negative integer between 0 and 5.
      */
-    public Histogram(final int numberOfSignificantValueDigits) {
-        this(1, 2, numberOfSignificantValueDigits);
-        setAutoResize(true);
-    }
+  public Histogram(final int numberOfSignificantValueDigits) {
+    this(1, 2, numberOfSignificantValueDigits);
+    setAutoResize(true);
+  }
 
-    /**
+  /**
      * Construct a Histogram given the Highest value to be tracked and a number of significant decimal digits. The
      * histogram will be constructed to implicitly track (distinguish from 0) values as low as 1.
      *
@@ -178,11 +148,11 @@ public class Histogram extends AbstractHistogram {
      *                                       decimal digits to which the histogram will maintain value resolution
      *                                       and separation. Must be a non-negative integer between 0 and 5.
      */
-    public Histogram(final long highestTrackableValue, final int numberOfSignificantValueDigits) {
-        this(1, highestTrackableValue, numberOfSignificantValueDigits);
-    }
+  public Histogram(final long highestTrackableValue, final int numberOfSignificantValueDigits) {
+    this(1, highestTrackableValue, numberOfSignificantValueDigits);
+  }
 
-    /**
+  /**
      * Construct a Histogram given the Lowest and Highest values to be tracked and a number of significant
      * decimal digits. Providing a lowestDiscernibleValue is useful is situations where the units used
      * for the histogram's values are much smaller that the minimal accuracy required. E.g. when tracking
@@ -198,74 +168,65 @@ public class Histogram extends AbstractHistogram {
      *                                       decimal digits to which the histogram will maintain value resolution
      *                                       and separation. Must be a non-negative integer between 0 and 5.
      */
-    public Histogram(final long lowestDiscernibleValue, final long highestTrackableValue,
-                     final int numberOfSignificantValueDigits) {
-        super(lowestDiscernibleValue, highestTrackableValue, numberOfSignificantValueDigits);
-        counts = new long[countsArrayLength];
-        wordSizeInBytes = 8;
-    }
+  public Histogram(final long lowestDiscernibleValue, final long highestTrackableValue, final int numberOfSignificantValueDigits) {
+    super(lowestDiscernibleValue, highestTrackableValue, numberOfSignificantValueDigits);
+    counts = new long[countsArrayLength];
+    wordSizeInBytes = 8;
+  }
 
-    /**
+  /**
      * Construct a histogram with the same range settings as a given source histogram,
-     * duplicating the source's start/end timestamps (but NOT its contents)
+     * duplicating the source's start/end timestamps (but NOT it's contents)
      * @param source The source histogram to duplicate
      */
-    public Histogram(final AbstractHistogram source) {
-        super(source);
-        counts = new long[countsArrayLength];
-        wordSizeInBytes = 8;
-    }
+  public Histogram(final AbstractHistogram source) {
+    super(source);
+    counts = new long[countsArrayLength];
+    wordSizeInBytes = 8;
+  }
 
-    /**
+  /**
      * Construct a new histogram by decoding it from a ByteBuffer.
      * @param buffer The buffer to decode from
      * @param minBarForHighestTrackableValue Force highestTrackableValue to be set at least this high
      * @return The newly constructed histogram
      */
-    public static Histogram decodeFromByteBuffer(final ByteBuffer buffer,
-                                                 final long minBarForHighestTrackableValue) {
-        return (Histogram) decodeFromByteBuffer(buffer, Histogram.class, minBarForHighestTrackableValue);
-    }
+  public static Histogram decodeFromByteBuffer(final ByteBuffer buffer, final long minBarForHighestTrackableValue) {
+    return (Histogram) decodeFromByteBuffer(buffer, Histogram.class, minBarForHighestTrackableValue);
+  }
 
-    /**
+  /**
      * Construct a new histogram by decoding it from a compressed form in a ByteBuffer.
      * @param buffer The buffer to decode from
      * @param minBarForHighestTrackableValue Force highestTrackableValue to be set at least this high
      * @return The newly constructed histogram
      * @throws DataFormatException on error parsing/decompressing the buffer
      */
-    public static Histogram decodeFromCompressedByteBuffer(final ByteBuffer buffer,
-                                                           final long minBarForHighestTrackableValue) throws DataFormatException {
-        return (Histogram) decodeFromCompressedByteBuffer(buffer, Histogram.class,
-                minBarForHighestTrackableValue);
-    }
+  public static Histogram decodeFromCompressedByteBuffer(final ByteBuffer buffer, final long minBarForHighestTrackableValue) throws DataFormatException {
+    return (Histogram) decodeFromCompressedByteBuffer(buffer, Histogram.class, minBarForHighestTrackableValue);
+  }
 
-    private void readObject(final ObjectInputStream o)
-            throws IOException, ClassNotFoundException {
-        o.defaultReadObject();
-    }
+  private void readObject(final ObjectInputStream o) throws IOException, ClassNotFoundException {
+    o.defaultReadObject();
+  }
 
-    @Override
-    synchronized void fillCountsArrayFromBuffer(final ByteBuffer buffer, final int length) {
-        buffer.asLongBuffer().get(counts, 0, length);
-    }
+  @Override synchronized void fillCountsArrayFromBuffer(final ByteBuffer buffer, final int length) {
+    buffer.asLongBuffer().get(counts, 0, length);
+  }
 
-    // We try to cache the LongBuffer used in output cases, as repeated
-    // output form the same histogram using the same buffer is likely:
-    private LongBuffer cachedDstLongBuffer = null;
-    private ByteBuffer cachedDstByteBuffer = null;
-    private int cachedDstByteBufferPosition = 0;
+  private LongBuffer cachedDstLongBuffer = null;
 
-    @Override
-    synchronized void fillBufferFromCountsArray(final ByteBuffer buffer, final int length) {
-        if ((cachedDstLongBuffer == null) ||
-                (buffer != cachedDstByteBuffer) ||
-                (buffer.position() != cachedDstByteBufferPosition)) {
-            cachedDstByteBuffer = buffer;
-            cachedDstByteBufferPosition = buffer.position();
-            cachedDstLongBuffer = buffer.asLongBuffer();
-        }
-        cachedDstLongBuffer.rewind();
-        cachedDstLongBuffer.put(counts, 0, length);
+  private ByteBuffer cachedDstByteBuffer = null;
+
+  private int cachedDstByteBufferPosition = 0;
+
+  @Override synchronized void fillBufferFromCountsArray(final ByteBuffer buffer, final int length) {
+    if ((cachedDstLongBuffer == null) || (buffer != cachedDstByteBuffer) || (buffer.position() != cachedDstByteBufferPosition)) {
+      cachedDstByteBuffer = buffer;
+      cachedDstByteBufferPosition = buffer.position();
+      cachedDstLongBuffer = buffer.asLongBuffer();
     }
+    cachedDstLongBuffer.rewind();
+    cachedDstLongBuffer.put(counts, 0, length);
+  }
 }
