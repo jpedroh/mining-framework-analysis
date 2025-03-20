99861e4c84a78a1d007ee32c0b6e1df8557f2dd4
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 13:23:12 +0000

    Mining Framework Analysis

--- javaparser-core/src/main/java/com/github/javaparser/ast/stmt/BlockStmt.java
+++ javaparser-core/src/main/java/com/github/javaparser/ast/stmt/BlockStmt.java
@@ -18,7 +18,7 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  */
-
+ 
 package com.github.javaparser.ast.stmt;
 
 import static com.github.javaparser.utils.Utils.ensureNotNull;
@@ -27,6 +27,7 @@ import java.util.List;
 
 import com.github.javaparser.Range;
 import com.github.javaparser.ast.nodeTypes.NodeWithStatements;
+import com.github.javaparser.ast.expr.Expression;
 import com.github.javaparser.ast.visitor.GenericVisitor;
 import com.github.javaparser.ast.visitor.VoidVisitor;
 
@@ -35,41 +36,46 @@ import com.github.javaparser.ast.visitor.VoidVisitor;
  */
 public final class BlockStmt extends Statement implements NodeWithStatements<BlockStmt> {
 
-    private List<Statement> stmts;
+	private List<Statement> stmts;
 
-    public BlockStmt() {
-    }
+	public BlockStmt() {
+	}
 
-    public BlockStmt(final List<Statement> stmts) {
-        setStmts(stmts);
-    }
+	public BlockStmt(final List<Statement> stmts) {
+		setStmts(stmts);
+	}
 
-    public BlockStmt(final Range range, final List<Statement> stmts) {
-        super(range);
-        setStmts(stmts);
-    }
+	public BlockStmt(final Range range, final List<Statement> stmts) {
+		super(range);
+		setStmts(stmts);
+	}
+	
+	@Override
+	public <R, A> R accept(final GenericVisitor<R, A> v, final A arg) {
+		return v.visit(this, arg);
+	}
 
-    @Override
-    public <R, A> R accept(final GenericVisitor<R, A> v, final A arg) {
-        return v.visit(this, arg);
-    }
+	@Override
+	public <A> void accept(final VoidVisitor<A> v, final A arg) {
+		v.visit(this, arg);
+	}
 
-    @Override
-    public <A> void accept(final VoidVisitor<A> v, final A arg) {
-        v.visit(this, arg);
-    }
-
-    @Override
+	@Override
     public List<Statement> getStmts() {
         stmts = ensureNotNull(stmts);
         return stmts;
-    }
+	}
 
     @Override
     public BlockStmt setStmts(final List<Statement> stmts) {
-        this.stmts = stmts;
-        setAsParentNodeOf(this.stmts);
+		this.stmts = stmts;
+		setAsParentNodeOf(this.stmts);
         return this;
-    }
+	}
+
+	public BlockStmt addStatement(Expression expr) {
+		return addStatement(new ExpressionStmt(expr));
+	}
+
 
 }
