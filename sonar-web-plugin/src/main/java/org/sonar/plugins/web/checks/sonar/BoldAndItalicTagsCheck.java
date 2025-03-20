f5cbe1302b3dfe672341f8c3f844f8ef9986e0ac
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 12:32:25 +0000

    Mining Framework Analysis

--- sonar-web-plugin/src/main/java/org/sonar/plugins/web/checks/sonar/BoldAndItalicTagsCheck.java
+++ sonar-web-plugin/src/main/java/org/sonar/plugins/web/checks/sonar/BoldAndItalicTagsCheck.java
@@ -1,35 +1,16 @@
-/*
- * SonarWeb :: SonarQube Plugin
- * Copyright (c) 2010-2018 SonarSource SA and Matthijs Galesloot
- * sonarqube@googlegroups.com
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.sonar.plugins.web.checks.sonar;
-
 import org.sonar.check.Rule;
 import org.sonar.plugins.web.checks.AbstractPageCheck;
 import org.sonar.plugins.web.node.TagNode;
 
-@Rule(key = "BoldAndItalicTagsCheck")
-public class BoldAndItalicTagsCheck extends AbstractPageCheck {
-
-  @Override
-  public void startElement(TagNode node) {
+@Rule(key = "BoldAndItalicTagsCheck") public class BoldAndItalicTagsCheck extends AbstractPageCheck {
+  @Override public void startElement(TagNode node) {
     if (isBold(node)) {
       createViolation(node.getStartLinePosition(), "Replace this <" + node.getNodeName() + "> tag by <strong>.");
-    } else if (isItalicAndNotAriaHidden(node)) {
-      createViolation(node.getStartLinePosition(), "Replace this <" + node.getNodeName() + "> tag by <em>.");
+    } else {
+      if (isItalicAndNotAriaHidden(node)) {
+        createViolation(node.getStartLinePosition(), "Replace this <" + node.getNodeName() + "> tag by <em>.");
+      }
     }
   }
 
@@ -46,5 +27,4 @@ public class BoldAndItalicTagsCheck extends AbstractPageCheck {
   private static boolean isItalicAndNotAriaHidden(TagNode node) {
     return "I".equalsIgnoreCase(node.getNodeName()) && !"true".equalsIgnoreCase(node.getAttribute("aria-hidden"));
   }
-
 }
