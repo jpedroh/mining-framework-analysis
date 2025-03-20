c584c21751c82c4b152f73cd46027fbcf39f0a78
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 03:56:57 +0000

    Mining Framework Analysis

--- src/main/java/com/jcabi/github/Contents.java
+++ src/main/java/com/jcabi/github/Contents.java
@@ -1,34 +1,4 @@
-/**
- * Copyright (c) 2012-2013, JCabi.com
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met: 1) Redistributions of source code must retain the above
- * copyright notice, this list of conditions and the following
- * disclaimer. 2) Redistributions in binary form must reproduce the above
- * copyright notice, this list of conditions and the following
- * disclaimer in the documentation and/or other materials provided
- * with the distribution. 3) Neither the name of the jcabi.com nor
- * the names of its contributors may be used to endorse or promote
- * products derived from this software without specific prior written
- * permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
- * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
- * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
- * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- * OF THE POSSIBILITY OF SUCH DAMAGE.
- */
 package com.jcabi.github;
-
 import com.jcabi.aspects.Immutable;
 import java.io.IOException;
 import javax.validation.constraints.NotNull;
@@ -41,26 +11,22 @@ import javax.validation.constraints.NotNull;
  * @since 0.8
  * @see <a href="http://developer.github.com/v3/repos/contents/">Contents API</a>
  */
-@Immutable
-public interface Contents {
-
-    /**
+@Immutable public interface Contents {
+  /**
      * Owner of them.
      * @return Repo
      */
-    @NotNull(message = "repository is never NULL")
-    Repo repo();
+  @NotNull(message = "repository is never NULL") Repo repo();
 
-    /**
+  /**
      * Get the Readme file of the default branch (usually master).
      *
      * @return The Content of the readme file.
      * @see <a href="http://http://developer.github.com/v3/repos/contents/#get-the-readme">Get the README</a>
      */
-    @NotNull(message = "Content is never NULL")
-    Content readme();
+  @NotNull(message = "Content is never NULL") Content readme();
 
-    /**
+  /**
      * Create new file.
      * @param path The content path
      * @param message The commit message
@@ -69,14 +35,9 @@ public interface Contents {
      * @throws IOException If there is any I/O problem
      * @see <a href="http://developer.github.com/v3/repos/contents/#create-a-file">Create a file</a>
      */
-    @NotNull(message = "Content is never NULL")
-    Content create(
-        @NotNull(message = "path is never NULL") String path,
-        @NotNull(message = "message is never NULL") String message,
-        @NotNull(message = "content is never NULL") String content)
-        throws IOException;
+  @NotNull(message = "Content is never NULL") Content create(@NotNull(message = "path is never NULL") String path, @NotNull(message = "message is never NULL") String message, @NotNull(message = "content is never NULL") String content) throws IOException;
 
-    /**
+  /**
      * Removes a file.
      * @param path The content path
      * @param message The commit message
@@ -85,11 +46,5 @@ public interface Contents {
      * @throws IOException If there is any I/O problem
      * @see <a href="http://developer.github.com/v3/repos/contents/#delete-a-file">Delete a file</a>
      */
-    @NotNull(message = "Content is never NULL")
-    Commit remove(
-        @NotNull(message = "path is never NULL") String path,
-        @NotNull(message = "message is never NULL") String message,
-        @NotNull(message = "sha is never NULL") String sha)
-        throws IOException;
-
+  @NotNull(message = "Content is never NULL") Commit remove(@NotNull(message = "path is never NULL") String path, @NotNull(message = "message is never NULL") String message, @NotNull(message = "sha is never NULL") String sha) throws IOException;
 }
--- src/test/java/com/jcabi/github/mock/MkContentsTest.java
+++ src/test/java/com/jcabi/github/mock/MkContentsTest.java
@@ -1,34 +1,4 @@
-/**
- * Copyright (c) 2012-2013, JCabi.com
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met: 1) Redistributions of source code must retain the above
- * copyright notice, this list of conditions and the following
- * disclaimer. 2) Redistributions in binary form must reproduce the above
- * copyright notice, this list of conditions and the following
- * disclaimer in the documentation and/or other materials provided
- * with the distribution. 3) Neither the name of the jcabi.com nor
- * the names of its contributors may be used to endorse or promote
- * products derived from this software without specific prior written
- * permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
- * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
- * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
- * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- * OF THE POSSIBILITY OF SUCH DAMAGE.
- */
 package com.jcabi.github.mock;
-
 import com.jcabi.github.Contents;
 import com.jcabi.github.Repo;
 import javax.json.Json;
@@ -44,20 +14,16 @@ import org.junit.Test;
  * @since 0.8
  */
 public final class MkContentsTest {
-    /**
+  /**
      * MkContents can fetch the default branch readme file.
      * @throws Exception if some problem inside
      */
-    @Test
-    public void canFetchReadmeFile() throws Exception {
-        final Contents contents = MkContentsTest.repo().contents();
-        MatcherAssert.assertThat(
-            contents.readme(),
-            Matchers.notNullValue()
-        );
-    }
+  @Test public void canFetchReadmeFile() throws Exception {
+    final Contents contents = MkContentsTest.repo().contents();
+    MatcherAssert.assertThat(contents.readme(), Matchers.notNullValue());
+  }
 
-    /**
+  /**
      * MkContents should be able to create new files.
      *
      * @throws Exception if some problem inside
@@ -66,13 +32,10 @@ public final class MkContentsTest {
      *  forget to implement a unit test for it here and remove the Ignore
      *  annotation.
      */
-    @Test
-    @Ignore
-    public void canCreateFile() throws Exception {
-        //To be implemented.
-    }
+  @Test @Ignore public void canCreateFile() throws Exception {
+  }
 
-    /**
+  /**
      * MkContents should be able to create new files.
      *
      * @throws Exception if some problem inside
@@ -81,20 +44,15 @@ public final class MkContentsTest {
      *  forget to implement a unit test for it here and remove the Ignore
      *  annotation.
      */
-    @Test
-    @Ignore
-    public void canRemoveFile() throws Exception {
-        //To be implemented.
-    }
+  @Test @Ignore public void canRemoveFile() throws Exception {
+  }
 
-    /**
+  /**
      * Create a repo to work with.
      * @return Repo
      * @throws Exception If some problem inside
      */
-    private static Repo repo() throws Exception {
-        return new MkGithub().repos().create(
-            Json.createObjectBuilder().add("name", "test").build()
-        );
-    }
+  private static Repo repo() throws Exception {
+    return new MkGithub().repos().create(Json.createObjectBuilder().add("name", "test").build());
+  }
 }
