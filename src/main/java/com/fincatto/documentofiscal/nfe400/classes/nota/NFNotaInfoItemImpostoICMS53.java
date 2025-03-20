49998c380bbfe2ced5b6b0205729933cf804ca76
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 10:37:34 +0000

    Mining Framework Analysis

--- src/main/java/com/fincatto/documentofiscal/nfe400/classes/nota/NFNotaInfoItemImpostoICMS53.java
+++ src/main/java/com/fincatto/documentofiscal/nfe400/classes/nota/NFNotaInfoItemImpostoICMS53.java
@@ -1,105 +1,112 @@
 package com.fincatto.documentofiscal.nfe400.classes.nota;
-
 import com.fincatto.documentofiscal.DFBase;
 import com.fincatto.documentofiscal.nfe400.classes.NFNotaInfoImpostoTributacaoICMS;
 import com.fincatto.documentofiscal.nfe400.classes.NFOrigem;
 import com.fincatto.documentofiscal.validadores.DFBigDecimalValidador;
 import org.simpleframework.xml.Element;
-
 import java.math.BigDecimal;
 
 public class NFNotaInfoItemImpostoICMS53 extends DFBase {
-    private static final long serialVersionUID = 6825237127788382375L;
+  private static final long serialVersionUID = 6825237127788382375L;
 
-    @Element(name = "orig")
-    private NFOrigem origem;
+  @Element(name = "orig") private NFOrigem origem;
 
-    @Element(name = "CST")
-    private NFNotaInfoImpostoTributacaoICMS situacaoTributaria;
+  @Element(name = "CST") private NFNotaInfoImpostoTributacaoICMS situacaoTributaria;
 
-    @Element(name = "qBCMono", required = false)
-    private String quantidadeBaseCalculo;
+  @Element(name = "qBCMono", required = false) private String quantidadeBaseCalculo;
 
-    @Element(name = "adRemICMS", required = false)
-    private String percentualAliquota;
+  @Element(name = "adRemICMS", required = false) private String percentualAliquota;
 
-    @Element(name = "vIcmsMonoOp", required = false)
-    private String valorOperacao;
+  @Element(name = "vIcmsMonoOp", required = false) private String valorOperacao;
 
-    @Element(name = "pDif", required = false)
-    private String percentualDiferimento;
+  @Element(name = "pDif", required = false) private String percentualDiferimento;
 
-    @Element(name = "vICMSMonoDif", required = false)
-    private String valorTributoDiferido;
+  @Element(name = "vICMSMonoDif", required = false) private String valorTributoDiferido;
 
-    @Element(name = "vICMSMono", required = false)
-    private String valorTributo;
+  @Element(name = "vICMSMono", required = false) private String valorTributo;
 
-    public void setOrigem(final NFOrigem origem) {
-        this.origem = origem;
+  public void setOrigem(final NFOrigem origem) {
+    this.origem = origem;
+  }
+
+  public void setSituacaoTributaria(final NFNotaInfoImpostoTributacaoICMS situacaoTributaria) {
+    if (!situacaoTributaria.equals(NFNotaInfoImpostoTributacaoICMS.TRIBUTACAO_MONOFASICA_SOBRE_COMBUSTIVEIS_COM_RECOLHIMENTO_DIFERIDO)) {
+      throw new IllegalStateException("Situacao tributaria invalida no item ICMS53 ");
     }
+    this.situacaoTributaria = situacaoTributaria;
+  }
 
-    public void setSituacaoTributaria(final NFNotaInfoImpostoTributacaoICMS situacaoTributaria) {
-        if (!situacaoTributaria.equals(NFNotaInfoImpostoTributacaoICMS.TRIBUTACAO_MONOFASICA_SOBRE_COMBUSTIVEIS_COM_RECOLHIMENTO_DIFERIDO)) {
-            throw new IllegalStateException("Situacao tributaria invalida no item ICMS53 ");
-        }
-        this.situacaoTributaria = situacaoTributaria;
-    }
+  public void setQuantidadeBaseCalculo(BigDecimal quantidadeBaseCalculo) {
+    this.quantidadeBaseCalculo = DFBigDecimalValidador.tamanho15Com2CasasDecimais(quantidadeBaseCalculo, "Quantidade tributada diferida");
+  }
 
-    public void setQuantidadeBaseCalculo(BigDecimal quantidadeBaseCalculo) {
-        this.quantidadeBaseCalculo = DFBigDecimalValidador.tamanho15Com2CasasDecimais(quantidadeBaseCalculo, "Quantidade tributada diferida");
-    }
+  public void setPercentualAliquota(BigDecimal percentualAliquota) {
+    this.percentualAliquota = DFBigDecimalValidador.tamanho7ComAte4CasasDecimais(percentualAliquota, "Al\u00edquota ad rem do imposto diferido");
+  }
 
-    public void setPercentualAliquota(BigDecimal percentualAliquota) {
-        this.percentualAliquota = DFBigDecimalValidador.tamanho7ComAte4CasasDecimais(percentualAliquota, "Alíquota ad rem do imposto diferido");
-    }
+  public void setValorOperacao(BigDecimal valorOperacao) {
+    this.valorOperacao = DFBigDecimalValidador.tamanho15Com2CasasDecimais(valorOperacao, 
+<<<<<<< /usr/src/app/output/fincatto/nfe/4c7a76dd73a0d77147d11304ddf788c0f1b55abe/src/main/java/com/fincatto/documentofiscal/nfe400/classes/nota/NFNotaInfoItemImpostoICMS53.java/left.java
+    "ICMS da opera\u00e7\u00e3o"
+=======
+    "Valor do ICMS da opera\u00e7\u00e3o"
+>>>>>>> /usr/src/app/output/fincatto/nfe/4c7a76dd73a0d77147d11304ddf788c0f1b55abe/src/main/java/com/fincatto/documentofiscal/nfe400/classes/nota/NFNotaInfoItemImpostoICMS53.java/right.java
+    );
+  }
 
-    public void setValorOperacao(BigDecimal valorOperacao) {
-        this.valorOperacao = DFBigDecimalValidador.tamanho15Com2CasasDecimais(valorOperacao, "ICMS da operação");
-    }
+  public void setPercentualDiferimento(BigDecimal percentualDiferimento) {
+    this.percentualDiferimento = DFBigDecimalValidador.tamanho7ComAte4CasasDecimais(percentualDiferimento, "Percentual do diferiment");
+  }
 
-    public void setPercentualDiferimento(BigDecimal percentualDiferimento) {
-        this.percentualDiferimento = DFBigDecimalValidador.tamanho7ComAte4CasasDecimais(percentualDiferimento, "Percentual do diferiment");
-    }
+  public void setValorTributoDiferido(BigDecimal valorTributoDiferido) {
+    this.valorTributoDiferido = DFBigDecimalValidador.tamanho15Com2CasasDecimais(valorTributoDiferido, 
+<<<<<<< /usr/src/app/output/fincatto/nfe/4c7a76dd73a0d77147d11304ddf788c0f1b55abe/src/main/java/com/fincatto/documentofiscal/nfe400/classes/nota/NFNotaInfoItemImpostoICMS53.java/left.java
+    "ICMS diferido"
+=======
+    "Valor do ICMS diferido"
+>>>>>>> /usr/src/app/output/fincatto/nfe/4c7a76dd73a0d77147d11304ddf788c0f1b55abe/src/main/java/com/fincatto/documentofiscal/nfe400/classes/nota/NFNotaInfoItemImpostoICMS53.java/right.java
+    );
+  }
 
-    public void setValorTributoDiferido (BigDecimal valorTributoDiferido) {
-        this.valorTributoDiferido = DFBigDecimalValidador.tamanho15Com2CasasDecimais(valorTributoDiferido, "ICMS diferido");
-    }
+  public void setValorTributo(BigDecimal valorTributo) {
+    this.valorTributo = DFBigDecimalValidador.tamanho15Com2CasasDecimais(valorTributo, 
+<<<<<<< /usr/src/app/output/fincatto/nfe/4c7a76dd73a0d77147d11304ddf788c0f1b55abe/src/main/java/com/fincatto/documentofiscal/nfe400/classes/nota/NFNotaInfoItemImpostoICMS53.java/left.java
+    "ICMS pr\u00f3prio devido"
+=======
+    "Valor do ICMS pr\u00f3prio devido"
+>>>>>>> /usr/src/app/output/fincatto/nfe/4c7a76dd73a0d77147d11304ddf788c0f1b55abe/src/main/java/com/fincatto/documentofiscal/nfe400/classes/nota/NFNotaInfoItemImpostoICMS53.java/right.java
+    );
+  }
 
-    public void setValorTributo(BigDecimal valorTributo) {
-        this.valorTributo = DFBigDecimalValidador.tamanho15Com2CasasDecimais(valorTributo, "ICMS próprio devido");
-    }
+  public NFOrigem getOrigem() {
+    return origem;
+  }
 
-    public NFOrigem getOrigem() {
-        return origem;
-    }
+  public NFNotaInfoImpostoTributacaoICMS getSituacaoTributaria() {
+    return situacaoTributaria;
+  }
 
-    public NFNotaInfoImpostoTributacaoICMS getSituacaoTributaria() {
-        return situacaoTributaria;
-    }
+  public String getQuantidadeBaseCalculo() {
+    return quantidadeBaseCalculo;
+  }
 
-    public String getQuantidadeBaseCalculo() {
-        return quantidadeBaseCalculo;
-    }
+  public String getPercentualAliquota() {
+    return percentualAliquota;
+  }
 
-    public String getPercentualAliquota() {
-        return percentualAliquota;
-    }
+  public String getValorOperacao() {
+    return valorOperacao;
+  }
 
-    public String getValorOperacao() {
-        return valorOperacao;
-    }
+  public String getPercentualDiferimento() {
+    return percentualDiferimento;
+  }
 
-    public String getPercentualDiferimento() {
-        return percentualDiferimento;
-    }
+  public String getValorTributoDiferido() {
+    return valorTributoDiferido;
+  }
 
-    public String getValorTributoDiferido() {
-        return valorTributoDiferido;
-    }
-
-    public String getValorTributo() {
-        return valorTributo;
-    }
-
+  public String getValorTributo() {
+    return valorTributo;
+  }
 }
--- src/main/java/com/fincatto/documentofiscal/nfe400/classes/nota/NFNotaInfoItemImpostoICMS61.java
+++ src/main/java/com/fincatto/documentofiscal/nfe400/classes/nota/NFNotaInfoItemImpostoICMS61.java
@@ -1,71 +1,64 @@
 package com.fincatto.documentofiscal.nfe400.classes.nota;
-
 import com.fincatto.documentofiscal.DFBase;
 import com.fincatto.documentofiscal.nfe400.classes.NFNotaInfoImpostoTributacaoICMS;
 import com.fincatto.documentofiscal.nfe400.classes.NFOrigem;
 import com.fincatto.documentofiscal.validadores.DFBigDecimalValidador;
 import org.simpleframework.xml.Element;
-
 import java.math.BigDecimal;
 
 public class NFNotaInfoItemImpostoICMS61 extends DFBase {
-    private static final long serialVersionUID = -8806839168760474001L;
+  private static final long serialVersionUID = -8806839168760474001L;
 
-    @Element(name = "orig")
-    private NFOrigem origem;
-    
-    @Element(name = "CST")
-    private NFNotaInfoImpostoTributacaoICMS situacaoTributaria;
+  @Element(name = "orig") private NFOrigem origem;
 
-    @Element(name = "qBCMonoRet", required = false)
-    private String quantidadeBaseCalculo;
+  @Element(name = "CST") private NFNotaInfoImpostoTributacaoICMS situacaoTributaria;
 
-    @Element(name = "adRemICMSRet")
-    private String percentualAliquota;
+  @Element(name = "qBCMonoRet", required = false) private String quantidadeBaseCalculo;
 
-    @Element(name = "vICMSMonoRet")
-    private String valorTributo;
+  @Element(name = "adRemICMSRet") private String percentualAliquota;
 
-    public void setOrigem(final NFOrigem origem) {
-        this.origem = origem;
+  @Element(name = "vICMSMonoRet") private String valorTributo;
+
+  public void setOrigem(final NFOrigem origem) {
+    this.origem = origem;
+  }
+
+  public void setSituacaoTributaria(final NFNotaInfoImpostoTributacaoICMS situacaoTributaria) {
+    if (!situacaoTributaria.equals(NFNotaInfoImpostoTributacaoICMS.TRIBUTACAO_MONOFASICA_SOBRE_COMBUSTIVEIS_COBRADA_ANTERIORMENTE)) {
+      throw new IllegalStateException("Situacao tributaria invalida no item ICMS61 ");
     }
+    this.situacaoTributaria = situacaoTributaria;
+  }
 
-    public void setSituacaoTributaria(final NFNotaInfoImpostoTributacaoICMS situacaoTributaria) {
-        if (!situacaoTributaria.equals(NFNotaInfoImpostoTributacaoICMS.TRIBUTACAO_MONOFASICA_SOBRE_COMBUSTIVEIS_COBRADA_ANTERIORMENTE)) {
-            throw new IllegalStateException("Situacao tributaria invalida no item ICMS61 ");
-        }
-        this.situacaoTributaria = situacaoTributaria;
-    }
+  public void setQuantidadeBaseCalculo(final BigDecimal quantidadeBaseCalculo) {
+    this.quantidadeBaseCalculo = DFBigDecimalValidador.tamanho15Com2CasasDecimais(quantidadeBaseCalculo, "Quantidade tributada retida anteriormente");
+  }
 
-    public void setQuantidadeBaseCalculo(final BigDecimal quantidadeBaseCalculo) {
-        this.quantidadeBaseCalculo = DFBigDecimalValidador.tamanho15Com2CasasDecimais(quantidadeBaseCalculo, "Quantidade tributada retida anteriormente");
-    }
+  public void setPercentualAliquota(final BigDecimal percentualAliquota) {
+    this.percentualAliquota = DFBigDecimalValidador.tamanho7ComAte4CasasDecimais(percentualAliquota, "Al\u00edquota ad rem do imposto retido anteriormente");
+  }
 
-    public void setPercentualAliquota(final BigDecimal percentualAliquota) {
-        this.percentualAliquota = DFBigDecimalValidador.tamanho7ComAte4CasasDecimais(percentualAliquota, "Alíquota ad rem do imposto retido anteriormente");
-    }
+  public void setValorTributo(final BigDecimal valorTributo) {
+    this.valorTributo = DFBigDecimalValidador.tamanho15Com2CasasDecimais(valorTributo, "ICMS retido anteriormente");
+  }
 
-    public void setValorTributo(final BigDecimal valorTributo) {
-        this.valorTributo = DFBigDecimalValidador.tamanho15Com2CasasDecimais(valorTributo, "ICMS retido anteriormente");
-    }
+  public NFOrigem getOrigem() {
+    return origem;
+  }
 
-    public NFOrigem getOrigem() {
-        return origem;
-    }
+  public NFNotaInfoImpostoTributacaoICMS getSituacaoTributaria() {
+    return situacaoTributaria;
+  }
 
-    public NFNotaInfoImpostoTributacaoICMS getSituacaoTributaria() {
-        return situacaoTributaria;
-    }
+  public String getQuantidadeBaseCalculo() {
+    return quantidadeBaseCalculo;
+  }
 
-    public String getQuantidadeBaseCalculo() {
-        return quantidadeBaseCalculo;
-    }
+  public String getPercentualAliquota() {
+    return percentualAliquota;
+  }
 
-    public String getPercentualAliquota() {
-        return percentualAliquota;
-    }
-
-    public String getValorTributo() {
-        return valorTributo;
-    }
+  public String getValorTributo() {
+    return valorTributo;
+  }
 }
