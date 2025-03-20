15dc99a5c08dafa198d80d8443fdb30ee136c2b7
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 10:09:34 +0000

    Mining Framework Analysis

--- src/main/java/org/geojson/GeoJsonObject.java
+++ src/main/java/org/geojson/GeoJsonObject.java
@@ -1,93 +1,81 @@
 package org.geojson;
-
 import java.io.Serializable;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-
 import com.fasterxml.jackson.annotation.JsonInclude;
+import java.util.Arrays;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import java.util.HashMap;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
+import java.util.Map;
 import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+@JsonTypeInfo(property = "type", use = Id.NAME) @JsonSubTypes(value = { @Type(value = Feature.class), @Type(value = Polygon.class), @Type(value = MultiPolygon.class), @Type(value = FeatureCollection.class), @Type(value = Point.class), @Type(value = MultiPoint.class), @Type(value = MultiLineString.class), @Type(value = LineString.class) }) @JsonInclude(value = Include.NON_NULL) public abstract class GeoJsonObject implements Serializable {
+  private Crs crs;
 
-@JsonTypeInfo(property = "type", use = Id.NAME)
-@JsonSubTypes({ @Type(Feature.class), @Type(Polygon.class), @Type(MultiPolygon.class), @Type(FeatureCollection.class),
-		@Type(Point.class), @Type(MultiPoint.class), @Type(MultiLineString.class), @Type(LineString.class) })
-@JsonInclude(Include.NON_NULL)
-public abstract class GeoJsonObject implements Serializable{
+  private double[] bbox;
 
-	private Crs crs;
-	private double[] bbox;
-	@JsonInclude(Include.ALWAYS)
-	private Map<String, Object> properties = new HashMap<String, Object>();
+  @JsonInclude(value = Include.ALWAYS) private Map<String, Object> properties = new HashMap<String, Object>();
 
-	public Crs getCrs() {
-		return crs;
-	}
+  public Crs getCrs() {
+    return crs;
+  }
 
-	public void setCrs(Crs crs) {
-		this.crs = crs;
-	}
+  public void setCrs(Crs crs) {
+    this.crs = crs;
+  }
 
-	public double[] getBbox() {
-		return bbox;
-	}
+  public double[] getBbox() {
+    return bbox;
+  }
 
-	public void setBbox(double[] bbox) {
-		this.bbox = bbox;
-	}
+  public void setBbox(double[] bbox) {
+    this.bbox = bbox;
+  }
 
-	public void setProperty(String key, Object value) {
-		properties.put(key, value);
-	}
+  public void setProperty(String key, Object value) {
+    properties.put(key, value);
+  }
 
-	@SuppressWarnings("unchecked")
-	public <T> T getProperty(String key) {
-		return (T)properties.get(key);
-	}
+  @SuppressWarnings(value = { "unchecked" }) public <T extends java.lang.Object> T getProperty(String key) {
+    return (T) properties.get(key);
+  }
 
-	public Map<String, Object> getProperties() {
-		return properties;
-	}
+  public Map<String, Object> getProperties() {
+    return properties;
+  }
 
-	public void setProperties(Map<String, Object> properties) {
-		this.properties = properties;
-	}
+  public void setProperties(Map<String, Object> properties) {
+    this.properties = properties;
+  }
 
-	public abstract <T> T accept(GeoJsonObjectVisitor<T> geoJsonObjectVisitor);
+  public abstract <T extends java.lang.Object> T accept(GeoJsonObjectVisitor<T> geoJsonObjectVisitor);
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o)
-			return true;
-		if (!(o instanceof GeoJsonObject))
-			return false;
-		GeoJsonObject that = (GeoJsonObject)o;
-		if (!Arrays.equals(bbox, that.bbox)) {
-			return false;
-		}
-		if (crs != null ? !crs.equals(that.crs) : that.crs != null) {
-			return false;
-		}
-		return !(properties != null ? !properties.equals(that.properties) : that.properties != null);
-	}
+  @Override public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof GeoJsonObject)) {
+      return false;
+    }
+    GeoJsonObject that = (GeoJsonObject) o;
+    if (!Arrays.equals(bbox, that.bbox)) {
+      return false;
+    }
+    if (crs != null ? !crs.equals(that.crs) : that.crs != null) {
+      return false;
+    }
+    return !(properties != null ? !properties.equals(that.properties) : that.properties != null);
+  }
 
-	@Override
-	public int hashCode() {
-		int result = crs != null ? crs.hashCode() : 0;
-		result = 31 * result + (bbox != null ? Arrays.hashCode(bbox) : 0);
-		result = 31 * result + (properties != null ? properties.hashCode() : 0);
-		return result;
-	}
+  @Override public int hashCode() {
+    int result = crs != null ? crs.hashCode() : 0;
+    result = 31 * result + (bbox != null ? Arrays.hashCode(bbox) : 0);
+    result = 31 * result + (properties != null ? properties.hashCode() : 0);
+    return result;
+  }
 
-	@Override
-	public String toString() {
-		return "GeoJsonObject{" + "properties=" + properties + "}";
-	}
+  @Override public String toString() {
+    return "GeoJsonObject{" + "properties=" + properties + "}";
+  }
 }
