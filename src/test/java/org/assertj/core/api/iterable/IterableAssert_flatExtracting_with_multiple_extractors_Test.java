fb3c3e60f992d16a3b71890430d08ef3561ce338
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 18:23:06 +0000

    Mining Framework Analysis

--- src/test/java/org/assertj/core/api/iterable/IterableAssert_flatExtracting_with_multiple_extractors_Test.java
+++ src/test/java/org/assertj/core/api/iterable/IterableAssert_flatExtracting_with_multiple_extractors_Test.java
@@ -1,27 +1,12 @@
-/**
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License.
- *
- * Copyright 2012-2017 the original author or authors.
- */
 package org.assertj.core.api.iterable;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.data.TolkienCharacter.Race.DWARF;
 import static org.assertj.core.data.TolkienCharacter.Race.ELF;
 import static org.assertj.core.data.TolkienCharacter.Race.HOBBIT;
 import static org.assertj.core.data.TolkienCharacter.Race.MAIA;
 import static org.assertj.core.data.TolkienCharacter.Race.MAN;
-
 import java.util.ArrayList;
 import java.util.List;
-
 import org.assertj.core.data.TolkienCharacter;
 import org.assertj.core.test.ExpectedException;
 import org.junit.Before;
@@ -29,9 +14,7 @@ import org.junit.Rule;
 import org.junit.Test;
 
 public class IterableAssert_flatExtracting_with_multiple_extractors_Test {
-
-  @Rule
-  public ExpectedException thrown = ExpectedException.none();
+  @Rule public ExpectedException thrown = ExpectedException.none();
 
   private final List<TolkienCharacter> fellowshipOfTheRing = new ArrayList<>();
 
@@ -39,8 +22,7 @@ public class IterableAssert_flatExtracting_with_multiple_extractors_Test {
 
   private static final Extractor<TolkienCharacter, Integer> age = TolkienCharacter::getAge;
 
-  @Before
-  public void setUp() {
+  @Before public void setUp() {
     fellowshipOfTheRing.add(TolkienCharacter.of("Frodo", 33, HOBBIT));
     fellowshipOfTheRing.add(TolkienCharacter.of("Sam", 38, HOBBIT));
     fellowshipOfTheRing.add(TolkienCharacter.of("Gandalf", 2020, MAIA));
@@ -51,39 +33,29 @@ public class IterableAssert_flatExtracting_with_multiple_extractors_Test {
     fellowshipOfTheRing.add(TolkienCharacter.of("Boromir", 37, MAN));
   }
 
-  @Test
-  public void should_allow_assertions_on_multiple_extracted_values_flattened_in_a_single_list() {
-    assertThat(fellowshipOfTheRing).flatExtracting("age", "name")
-                                   .as("extract ages and names")
-                                   .containsSequence(33, "Frodo", 38, "Sam");
-
-    assertThat(fellowshipOfTheRing).flatExtracting(age, name)
-                                   .as("extract ages and names")
-                                   .contains(33, "Frodo", 38, "Sam");
+  @Test public void should_allow_assertions_on_multiple_extracted_values_flattened_in_a_single_list() {
+    assertThat(fellowshipOfTheRing).flatExtracting("age", "name").as("extract ages and names").containsSequence(33, "Frodo", 38, "Sam");
+    assertThat(fellowshipOfTheRing).flatExtracting(age, name).as("extract ages and names").contains(33, "Frodo", 38, "Sam");
   }
 
-  @Test
-  public void should_throw_IllegalArgumentException_when_no_fields_or_properties_are_specified() {
+  @Test public void should_throw_IllegalArgumentException_when_no_fields_or_properties_are_specified() {
     thrown.expectIllegalArgumentException();
     assertThat(fellowshipOfTheRing).flatExtracting(new String[0]);
   }
 
-  @Test
-  public void should_throw_IllegalArgumentException_when_null_fields_or_properties_vararg() {
+  @Test public void should_throw_IllegalArgumentException_when_null_fields_or_properties_vararg() {
     thrown.expectIllegalArgumentException();
     String[] fields = null;
     assertThat(fellowshipOfTheRing).flatExtracting(fields);
   }
 
-  @Test
-  public void should_throw_IllegalArgumentException_when_extracting_from_null() {
+  @Test public void should_throw_IllegalArgumentException_when_extracting_from_null() {
     thrown.expectIllegalArgumentException();
     fellowshipOfTheRing.add(null);
     assertThat(fellowshipOfTheRing).flatExtracting("age", "name");
   }
 
-  @Test
-  public void should_throw_IllegalArgumentException_when_extracting_from_null_extractors() {
+  @Test public void should_throw_IllegalArgumentException_when_extracting_from_null_extractors() {
     thrown.expect(NullPointerException.class);
     fellowshipOfTheRing.add(null);
     assertThat(fellowshipOfTheRing).flatExtracting(age, name);
