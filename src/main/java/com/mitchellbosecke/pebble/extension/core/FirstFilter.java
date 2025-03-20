4257197d61a088ad9efda6d8c64d12d87b3b669c
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 06:47:41 +0000

    Mining Framework Analysis

--- src/main/java/com/mitchellbosecke/pebble/extension/core/FirstFilter.java
+++ src/main/java/com/mitchellbosecke/pebble/extension/core/FirstFilter.java
@@ -8,6 +8,7 @@
  ******************************************************************************/
 package com.mitchellbosecke.pebble.extension.core;
 
+import com.mitchellbosecke.pebble.error.PebbleException;
 import com.mitchellbosecke.pebble.extension.Filter;
 import com.mitchellbosecke.pebble.template.EvaluationContext;
 import com.mitchellbosecke.pebble.template.PebbleTemplate;
@@ -22,36 +23,52 @@ import java.util.Map;
  * Returns the first element of a collection
  *
  * @author mbosecke
+ *
  */
 public class FirstFilter implements Filter {
 
-  @Override
-  public List<String> getArgumentNames() {
-    return null;
-  }
-
-  @Override
-  public Object apply(Object input, Map<String, Object> args, PebbleTemplate self, EvaluationContext context, int lineNumber) {
-    if (input == null) {
-      return null;
+    @Override
+    public List<String> getArgumentNames() {
+        return null;
     }
 
-    if (input instanceof String) {
-      String inputString = (String) input;
-      return inputString.charAt(0);
-    }
+    @Override
+    public Object apply(Object input, Map<String, Object> args, PebbleTemplate self, EvaluationContext context, int lineNumber)
+            throws PebbleException {
+        if (input == null) {
+            return null;
+        }
 
-    if (input.getClass().isArray()) {
-      int length = Array.getLength(input);
-      return length > 0 ? Array.get(input, 0) : null;
-    }
+        if (input instanceof String) {
+            String inputString = (String) input;
+            return inputString.charAt(0);
+        }
 
-    Collection<?> inputCollection = (Collection<?>) input;
+        if (input.getClass().isArray()) {
+            int length = Array.getLength(input);
+            return length > 0 ? Array.get(input, 0) : null;
+        } else if (input instanceof Collection) {
+            Collection<?> inputCollection = (Collection<?>) input;
+            return inputCollection.iterator().next();
+        } else {
+            throw new PebbleException(null,
+                    "The 'first' filter expects that the input is either a collection, an array or a string.",
+                    lineNumber, self.getName());
+        }
+<<<<<<< /usr/src/app/output/mbosecke/pebble/387fcf6257ff93738ac79beb3eae28506ce77f35/src/main/java/com/mitchellbosecke/pebble/extension/core/FirstFilter.java/left.java
+||||||| /usr/src/app/output/mbosecke/pebble/387fcf6257ff93738ac79beb3eae28506ce77f35/src/main/java/com/mitchellbosecke/pebble/extension/core/FirstFilter.java/base.java
+        
+        Collection<?> inputCollection = (Collection<?>) input;
+        return inputCollection.iterator().next();
+=======
+        
+        Collection<?> inputCollection = (Collection<?>) input;
 
-    Iterator<?> iterator = inputCollection.iterator();
-    if (iterator.hasNext()) {
-      return iterator.next();
+        Iterator<?> iterator = inputCollection.iterator();
+        if (iterator.hasNext()) {
+            return iterator.next();
+        }
+        return null;
+>>>>>>> /usr/src/app/output/mbosecke/pebble/387fcf6257ff93738ac79beb3eae28506ce77f35/src/main/java/com/mitchellbosecke/pebble/extension/core/FirstFilter.java/right.java
     }
-    return null;
-  }
 }
