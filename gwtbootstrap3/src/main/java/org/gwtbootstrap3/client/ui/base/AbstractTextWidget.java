a19b2cbf74b5eb15c4ac4773119f5416bb1136c1
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 02:11:28 +0000

    Mining Framework Analysis

--- gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/base/AbstractTextWidget.java
+++ gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/base/AbstractTextWidget.java
@@ -1,25 +1,4 @@
 package org.gwtbootstrap3.client.ui.base;
-
-/*
- * #%L
- * GwtBootstrap3
- * %%
- * Copyright (C) 2013 GwtBootstrap3
- * %%
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * #L%
- */
-
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.dom.client.Style;
 import com.google.gwt.editor.client.IsEditor;
@@ -38,100 +17,82 @@ import org.gwtbootstrap3.client.ui.constants.DeviceSize;
  * @author Joshua Godi
  */
 public abstract class AbstractTextWidget extends Widget implements HasId, HasHTML, HasResponsiveness, HasInlineStyle, IsEditor<LeafValueEditor<String>> {
+  private final IdMixin<AbstractTextWidget> idMixin = new IdMixin<AbstractTextWidget>(this);
 
-    private final IdMixin<AbstractTextWidget> idMixin = new IdMixin<AbstractTextWidget>(this);
-    private LeafValueEditor<String> editor;
+  private LeafValueEditor<String> editor;
 
-    protected AbstractTextWidget(final Element element) {
-        setElement(element);
-    }
+  protected AbstractTextWidget(final Element element) {
+    setElement(element);
+  }
 
-    @Override
-    public void setId(final String id) {
-        idMixin.setId(id);
-    }
+  @Override public void setId(final String id) {
+    idMixin.setId(id);
+  }
 
-    @Override
-    public String getId() {
-        return idMixin.getId();
-    }
+  @Override public String getId() {
+    return idMixin.getId();
+  }
 
-    @Override
-    public void setText(final String text) {
-        getElement().setInnerText(text);
-    }
+  @Override public void setText(final String text) {
+    getElement().setInnerText(text);
+  }
 
-    @Override
-    public String getText() {
-        return getElement().getInnerText();
-    }
+  @Override public String getText() {
+    return getElement().getInnerText();
+  }
 
-    @Override
-    public String getHTML() {
-        return getElement().getInnerHTML();
-    }
+  @Override public String getHTML() {
+    return getElement().getInnerHTML();
+  }
 
-    @Override
-    public void setHTML(final String html) {
-        getElement().setInnerHTML(html);
-    }
+  @Override public void setHTML(final String html) {
+    getElement().setInnerHTML(html);
+  }
 
-    @Override
-    public void setVisibleOn(final DeviceSize deviceSize) {
-        StyleHelper.setVisibleOn(this, deviceSize);
-    }
+  @Override public void setVisibleOn(final DeviceSize deviceSize) {
+    StyleHelper.setVisibleOn(this, deviceSize);
+  }
 
-    @Override
-    public void setHiddenOn(final DeviceSize deviceSize) {
-        StyleHelper.setHiddenOn(this, deviceSize);
-    }
+  @Override public void setHiddenOn(final DeviceSize deviceSize) {
+    StyleHelper.setHiddenOn(this, deviceSize);
+  }
 
-    @Override
-    public void setMarginTop(final double margin) {
-        getElement().getStyle().setMarginTop(margin, Style.Unit.PX);
-    }
+  @Override public void setMarginTop(final double margin) {
+    getElement().getStyle().setMarginTop(margin, Style.Unit.PX);
+  }
 
-    @Override
-    public void setMarginLeft(final double margin) {
-        getElement().getStyle().setMarginLeft(margin, Style.Unit.PX);
-    }
+  @Override public void setMarginLeft(final double margin) {
+    getElement().getStyle().setMarginLeft(margin, Style.Unit.PX);
+  }
 
-    @Override
-    public void setMarginRight(final double margin) {
-        getElement().getStyle().setMarginRight(margin, Style.Unit.PX);
-    }
+  @Override public void setMarginRight(final double margin) {
+    getElement().getStyle().setMarginRight(margin, Style.Unit.PX);
+  }
 
-    @Override
-    public void setMarginBottom(final double margin) {
-        getElement().getStyle().setMarginBottom(margin, Style.Unit.PX);
-    }
+  @Override public void setMarginBottom(final double margin) {
+    getElement().getStyle().setMarginBottom(margin, Style.Unit.PX);
+  }
 
-    @Override
-    public void setPaddingTop(final double padding) {
-        getElement().getStyle().setPaddingTop(padding, Style.Unit.PX);
-    }
+  @Override public void setPaddingTop(final double padding) {
+    getElement().getStyle().setPaddingTop(padding, Style.Unit.PX);
+  }
 
-    @Override
-    public void setPaddingLeft(final double padding) {
-        getElement().getStyle().setPaddingLeft(padding, Style.Unit.PX);
-    }
+  @Override public void setPaddingLeft(final double padding) {
+    getElement().getStyle().setPaddingLeft(padding, Style.Unit.PX);
+  }
 
-    @Override
-    public void setPaddingRight(final double padding) {
-        getElement().getStyle().setPaddingRight(padding, Style.Unit.PX);
-    }
+  @Override public void setPaddingRight(final double padding) {
+    getElement().getStyle().setPaddingRight(padding, Style.Unit.PX);
+  }
 
-    @Override
-    public void setPaddingBottom(final double padding) {
-        getElement().getStyle().setPaddingBottom(padding, Style.Unit.PX);
+  @Override public void setPaddingBottom(final double padding) {
+    getElement().getStyle().setPaddingBottom(padding, Style.Unit.PX);
+  }
+
+  @Override public LeafValueEditor<String> asEditor() {
+    if (editor == null) {
+      editor = HasTextEditor.of(this);
     }
-    
-    @Override
-    public LeafValueEditor<String> asEditor() {
-        if (editor == null) {
-          editor = HasTextEditor.of(this);
-        }
-        return editor;
-    }
-    
+    return editor;
+  }
 }
