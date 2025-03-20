f618a5a6a279a6684dc2a672e3101a7650b900c8
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 01:57:48 +0000

    Mining Framework Analysis

--- gdx-kitten-rescue/src/main/java/de/hochschuletrier/gdw/ss14/ecs/systems/CatMovementSystem.java
+++ gdx-kitten-rescue/src/main/java/de/hochschuletrier/gdw/ss14/ecs/systems/CatMovementSystem.java
@@ -1,5 +1,4 @@
 package de.hochschuletrier.gdw.ss14.ecs.systems;
-
 import com.badlogic.gdx.utils.*;
 import de.hochschuletrier.gdw.ss14.ecs.*;
 import de.hochschuletrier.gdw.ss14.ecs.components.*;
@@ -8,149 +7,111 @@ import de.hochschuletrier.gdw.ss14.states.*;
 /**
  * Created by Daniel Dreher on 03.10.2014.
  */
-public class CatMovementSystem extends ECSystem
-{
-    public CatMovementSystem(EntityManager entityManager)
-    {
-        super(entityManager, 1);
+public class CatMovementSystem extends ECSystem {
+  public CatMovementSystem(EntityManager entityManager) {
+    super(entityManager, 1);
+  }
+
+  @Override public void update(float delta) {
+    Array<Integer> entities = entityManager.getAllEntitiesWithComponents(PlayerComponent.class, MovementComponent.class, PhysicsComponent.class, InputComponent.class, CatPropertyComponent.class, JumpDataComponent.class);
+    Array<Integer> laser = entityManager.getAllEntitiesWithComponents(LaserPointerComponent.class);
+    LaserPointerComponent laserPointerComponent = null;
+    if (laser.size > 0) {
+      laserPointerComponent = entityManager.getComponent(laser.first(), LaserPointerComponent.class);
     }
-
-    @Override
-    public void update(float delta)
-    {
-        Array<Integer> entities = entityManager.getAllEntitiesWithComponents(PlayerComponent.class, MovementComponent.class, PhysicsComponent.class, InputComponent.class, CatPropertyComponent.class, JumpDataComponent.class);
-        Array<Integer> laser = entityManager.getAllEntitiesWithComponents(LaserPointerComponent.class);
-
-        LaserPointerComponent laserPointerComponent = null;
-
-        if (laser.size > 0)
-        {
-            laserPointerComponent = entityManager.getComponent(laser.first(), LaserPointerComponent.class);
+    for (Integer entity : entities) {
+      MovementComponent movementComponent = entityManager.getComponent(entity, MovementComponent.class);
+      PhysicsComponent physicsComponent = entityManager.getComponent(entity, PhysicsComponent.class);
+      InputComponent inputComponent = entityManager.getComponent(entity, InputComponent.class);
+      CatPropertyComponent catPropertyComponent = entityManager.getComponent(entity, CatPropertyComponent.class);
+      JumpDataComponent jumpDataComponent = entityManager.getComponent(entity, JumpDataComponent.class);
+      if (catPropertyComponent.state == CatStateEnum.IDLE || catPropertyComponent.state == CatStateEnum.WALK || catPropertyComponent.state == CatStateEnum.RUN) {
+        movementComponent.directionVec.x = inputComponent.whereToGo.x - physicsComponent.getPosition().x;
+        movementComponent.directionVec.y = inputComponent.whereToGo.y - physicsComponent.getPosition().y;
+        float distance = movementComponent.directionVec.len();
+        if (movementComponent.oldPositionVec == null) {
+          movementComponent.oldPositionVec = movementComponent.directionVec;
+        }
+        if (movementComponent.positionVec == null) {
+          movementComponent.positionVec = movementComponent.directionVec;
+        }
+        movementComponent.positionVec.x = movementComponent.directionVec.x * movementComponent.percentOfNewCatVelocity + movementComponent.oldPositionVec.x * (1 - movementComponent.percentOfNewCatVelocity);
+        movementComponent.positionVec.y = movementComponent.directionVec.y * movementComponent.percentOfNewCatVelocity + movementComponent.oldPositionVec.y * (1 - movementComponent.percentOfNewCatVelocity);
+        movementComponent.oldPositionVec = movementComponent.positionVec;
+        movementComponent.positionVec.nor();
+        if (distance >= 200) {
+          movementComponent.velocity += movementComponent.acceleration * delta;
+          if (movementComponent.velocity >= movementComponent.maxVelocity) {
+            movementComponent.velocity = movementComponent.maxVelocity;
+          }
+        } else {
+          if (distance >= 100) {
+            if (movementComponent.velocity >= movementComponent.middleVelocity) {
+              movementComponent.velocity += movementComponent.damping * delta;
+              if (movementComponent.velocity <= movementComponent.middleVelocity) {
+                movementComponent.velocity = movementComponent.middleVelocity;
+              }
+            } else {
+              if (movementComponent.velocity < movementComponent.middleVelocity) {
+                movementComponent.velocity += movementComponent.acceleration * delta;
+                if (movementComponent.velocity >= movementComponent.middleVelocity) {
+                  movementComponent.velocity = movementComponent.middleVelocity;
+                }
+              }
+            }
+          } else {
+            movementComponent.velocity += movementComponent.damping * 1.5f * delta;
+            if (movementComponent.velocity <= movementComponent.minVelocity) {
+              movementComponent.velocity = 0;
+            }
+          }
+        }
+        if (distance <= 70) {
+          if (catPropertyComponent.state == CatStateEnum.IDLE) {
+            catPropertyComponent.timeTillJumpTimer = catPropertyComponent.timeTillJumpTimer + delta;
+            if (catPropertyComponent.timeTillJumpTimer >= catPropertyComponent.TIME_TILL_JUMP) {
+              catPropertyComponent.state = CatStateEnum.JUMP;
+              jumpDataComponent.jumpDirection = movementComponent.directionVec.nor();
+            }
+          }
+        } else {
+          catPropertyComponent.timeTillJumpTimer = 0.0f;
+        }
+        movementComponent.directionVec = movementComponent.directionVec.nor();
+        float angle = (float) Math.atan2(-movementComponent.directionVec.x, movementComponent.directionVec.y);
+        if (laserPointerComponent != null) {
+          if (!laserPointerComponent.isVisible) {
+            movementComponent.velocity = 0.0f;
+          }
+          if (!catPropertyComponent.isHidden && laserPointerComponent.isVisible) {
+            physicsComponent.setRotation(angle);
+          }
         }
 
-        for (Integer entity : entities)
-        {
-            MovementComponent movementComponent = entityManager.getComponent(entity, MovementComponent.class);
-            PhysicsComponent physicsComponent = entityManager.getComponent(entity, PhysicsComponent.class);
-            InputComponent inputComponent = entityManager.getComponent(entity, InputComponent.class);
-            CatPropertyComponent catPropertyComponent = entityManager.getComponent(entity, CatPropertyComponent.class);
-            JumpDataComponent jumpDataComponent = entityManager.getComponent(entity, JumpDataComponent.class);
+<<<<<<< /usr/src/app/output/lusito/gamedevweek/5baaebe74aa4c0ae73b4ed034d97db6eb5c46d65/gdx-kitten-rescue/src/main/java/de/hochschuletrier/gdw/ss14/ecs/systems/CatMovementSystem.java/left.java
+        physicsComponent.setVelocityX(movementComponent.positionVec.x * movementComponent.velocity);
+=======
+>>>>>>> Unknown file: This is a bug in JDime.
 
 
-            if (catPropertyComponent.state == CatStateEnum.IDLE || catPropertyComponent.state == CatStateEnum.WALK || catPropertyComponent.state == CatStateEnum.RUN)
-            {
-                movementComponent.directionVec.x = inputComponent.whereToGo.x - physicsComponent.getPosition().x;
-                movementComponent.directionVec.y = inputComponent.whereToGo.y - physicsComponent.getPosition().y;
-                float distance = movementComponent.directionVec.len();
-
-                //needed not normalized directions for sliding (more natural curves)
-                if(movementComponent.oldPositionVec==null){
-                    movementComponent.oldPositionVec=movementComponent.directionVec;
-                }
-                if(movementComponent.positionVec==null){
-                    movementComponent.positionVec=movementComponent.directionVec;
-                }
-                movementComponent.positionVec.x = movementComponent.directionVec.x*movementComponent.percentOfNewCatVelocity
-                        + movementComponent.oldPositionVec.x*(1-movementComponent.percentOfNewCatVelocity);
-                movementComponent.positionVec.y = movementComponent.directionVec.y*movementComponent.percentOfNewCatVelocity
-                        + movementComponent.oldPositionVec.y*(1-movementComponent.percentOfNewCatVelocity);
-                movementComponent.oldPositionVec = movementComponent.positionVec;
-                movementComponent.positionVec.nor();//normalize position for PhysicsComponent
-
-                if (distance >= 200)
-                {
-                    movementComponent.velocity += movementComponent.acceleration * delta;
-
-                    if (movementComponent.velocity >= movementComponent.maxVelocity)
-                    {
-                        movementComponent.velocity = movementComponent.maxVelocity;
-                    }
-                }
-                else if (distance >= 100)
-                {
-                    /**
-                     * Falls wir von unserem Stand aus losgehen soll unsere Katze beschleunigen, bis sie "geht"
-                     */
-                    if (movementComponent.velocity >= movementComponent.middleVelocity)
-                    {
-                        movementComponent.velocity += movementComponent.damping * delta;
-                        if (movementComponent.velocity <= movementComponent.middleVelocity)
-                        {
-                            movementComponent.velocity = movementComponent.middleVelocity;
-                        }
-                    }
-                    /**
-                     * Falls unsere Katze aus dem "Rennen" aus zu nah an unseren Laserpointer kommt, soll
-                     * sie stetig langsamer werden
-                     */
-                    else if (movementComponent.velocity < movementComponent.middleVelocity)
-                    {
-                        movementComponent.velocity += movementComponent.acceleration * delta;
-                        if (movementComponent.velocity >= movementComponent.middleVelocity)
-                        {
-                            movementComponent.velocity = movementComponent.middleVelocity;
-                        }
-                    }
-                }
-                else
-                {
-                    movementComponent.velocity += movementComponent.damping * 1.5f * delta;
-                    if (movementComponent.velocity <= movementComponent.minVelocity)
-                    {
-                        movementComponent.velocity = 0;
-                    }
-                }
-
-                if (distance <= 70)
-                {
-                    if (catPropertyComponent.state == CatStateEnum.IDLE)
-                    {
-                        catPropertyComponent.timeTillJumpTimer = catPropertyComponent.timeTillJumpTimer + delta;
-                        if (catPropertyComponent.timeTillJumpTimer >= catPropertyComponent.TIME_TILL_JUMP)
-                        {
-                            catPropertyComponent.state = CatStateEnum.JUMP;
-                            jumpDataComponent.jumpDirection = movementComponent.directionVec.nor();
-                        }
-                    }
-                }
-                else
-                {
-                    catPropertyComponent.timeTillJumpTimer = 0.0f;
-                }
-
-                movementComponent.directionVec = movementComponent.directionVec.nor();
-
-                float angle = (float) Math.atan2(-movementComponent.directionVec.x, movementComponent.directionVec.y);
-
-                if (laserPointerComponent != null)
-                {
-                    if (!laserPointerComponent.isVisible)
-                    {
-                        movementComponent.velocity = 0.0f;
-                    }
-
-                    if (!catPropertyComponent.isHidden && laserPointerComponent.isVisible)
-                    {
-                        physicsComponent.setRotation(angle);
-                    }
-                }
-            } // end if (state check)
-            else if (catPropertyComponent.state == CatStateEnum.JUMP)
-            {
-                movementComponent.velocity = jumpDataComponent.jumpVelocity;
-            }
-            else if(catPropertyComponent.state == CatStateEnum.FALL)
-            {
-                movementComponent.velocity = 0.0f;
-            }
-            //positionVec not directionVec because sliding
-            physicsComponent.setVelocityX(movementComponent.positionVec.x * movementComponent.velocity);
-            physicsComponent.setVelocityY(movementComponent.positionVec.y * movementComponent.velocity);
+<<<<<<< /usr/src/app/output/lusito/gamedevweek/5baaebe74aa4c0ae73b4ed034d97db6eb5c46d65/gdx-kitten-rescue/src/main/java/de/hochschuletrier/gdw/ss14/ecs/systems/CatMovementSystem.java/left.java
+        physicsComponent.setVelocityY(movementComponent.positionVec.y * movementComponent.velocity);
+=======
+>>>>>>> Unknown file: This is a bug in JDime.
+      } else {
+        if (catPropertyComponent.state == CatStateEnum.JUMP) {
+          movementComponent.velocity = jumpDataComponent.jumpVelocity;
+        } else {
+          if (catPropertyComponent.state == CatStateEnum.FALL) {
+            movementComponent.velocity = 0.0f;
+          }
         }
+      }
+      physicsComponent.setVelocityX(movementComponent.directionVec.x * movementComponent.velocity);
+      physicsComponent.setVelocityY(movementComponent.directionVec.y * movementComponent.velocity);
     }
+  }
 
-    @Override
-    public void render(){
-
-    }
+  @Override public void render() {
+  }
 }
