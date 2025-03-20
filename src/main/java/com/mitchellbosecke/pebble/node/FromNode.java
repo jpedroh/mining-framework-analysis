ff072afdaff85f69cfc95cca49894a926d3e75bb
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 06:44:50 +0000

    Mining Framework Analysis

--- src/main/java/com/mitchellbosecke/pebble/node/FromNode.java
+++ src/main/java/com/mitchellbosecke/pebble/node/FromNode.java
@@ -1,16 +1,14 @@
 package com.mitchellbosecke.pebble.node;
-
 import com.mitchellbosecke.pebble.extension.NodeVisitor;
+import java.io.Writer;
 import com.mitchellbosecke.pebble.node.expression.Expression;
+import java.util.List;
 import com.mitchellbosecke.pebble.template.EvaluationContextImpl;
 import com.mitchellbosecke.pebble.template.PebbleTemplateImpl;
 import com.mitchellbosecke.pebble.utils.Pair;
 
-import java.io.Writer;
-import java.util.List;
-
 /**
- * From Node for
+ * From Node for <br/>
  * 
  * <p>{% from "templateName" import macroName as alias %}<p>
  * 
@@ -18,25 +16,22 @@ import java.util.List;
  *
  */
 public class FromNode extends AbstractRenderableNode {
+  private final Expression<?> fromExpression;
 
-    private final Expression<?> fromExpression;
-    private final List<Pair<String, String>> namedMacros;
+  private final List<Pair<String, String>> namedMacros;
 
-    public FromNode(int lineNumber, Expression<?> fromExpression, List<Pair<String, String>> namedMacros) {
-        super(lineNumber);
-        this.fromExpression = fromExpression;
-        this.namedMacros = namedMacros;
-    }
+  public FromNode(int lineNumber, Expression<?> fromExpression, List<Pair<String, String>> namedMacros) {
+    super(lineNumber);
+    this.fromExpression = fromExpression;
+    this.namedMacros = namedMacros;
+  }
 
-    @Override
-    public void render(PebbleTemplateImpl self, Writer writer, EvaluationContextImpl context) {
-        String templateName = (String) fromExpression.evaluate(self, context);
-        self.importNamedMacrosFromTemplate(context, templateName, namedMacros);
-    }
+  @Override public void render(PebbleTemplateImpl self, Writer writer, EvaluationContextImpl context) {
+    String templateName = (String) fromExpression.evaluate(self, context);
+    self.importNamedMacrosFromTemplate(context, templateName, namedMacros);
+  }
 
-    @Override
-    public void accept(NodeVisitor visitor) {
-        visitor.visit(this);
-    }
-
+  @Override public void accept(NodeVisitor visitor) {
+    visitor.visit(this);
+  }
 }
