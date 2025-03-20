44fadd910a1b43f4e4229d223f4d441ec25e197b
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 16:39:35 +0000

    Mining Framework Analysis

--- src/main/java/org/cts/CRSFactory.java
+++ src/main/java/org/cts/CRSFactory.java
@@ -1,42 +1,9 @@
-/*
- * Coordinate Transformations Suite (abridged CTS)  is a library developped to 
- * perform Coordinate Transformations using well known geodetic algorithms 
- * and parameter sets. 
- * Its main focus are simplicity, flexibility, interoperability, in this order.
- *
- * This library has been originally developed by Michaël Michaud under the JGeod
- * name. It has been renamed CTS in 2009 and shared to the community from 
- * the Atelier SIG code repository.
- * 
- * Since them, CTS is supported by the Atelier SIG team in collaboration with Michaël 
- * Michaud.
- * The new CTS has been funded  by the French Agence Nationale de la Recherche 
- * (ANR) under contract ANR-08-VILL-0005-01 and the regional council 
- * "Région Pays de La Loire" under the projet SOGVILLE (Système d'Orbservation 
- * Géographique de la Ville).
- *
- * CTS is free software: you can redistribute it and/or modify it under the
- * terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any later
- * version.
- *
- * CTS is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with
- * CTS. If not, see <http://www.gnu.org/licenses/>.
- *
- * For more information, please consult: <https://github.com/irstv/cts/>
- */
 package org.cts;
-
 import org.cts.crs.CRSException;
 import org.cts.crs.CoordinateReferenceSystem;
 import org.cts.parser.prj.PrjParser;
 import org.cts.registry.Registry;
 import org.cts.registry.RegistryManager;
-
 import java.io.*;
 import java.nio.charset.Charset;
 import java.util.LinkedHashMap;
@@ -61,17 +28,17 @@ import org.cts.registry.RegistryException;
  * @author Erwan Bocher
  */
 public class CRSFactory {
+  private RegistryManager registryManager = new RegistryManager();
 
-    private RegistryManager registryManager = new RegistryManager();
-    protected final CRSCache<String, CoordinateReferenceSystem> CRSPOOL = new CRSCache<String, CoordinateReferenceSystem>(10);
+  protected final CRSCache<String, CoordinateReferenceSystem> CRSPOOL = new CRSCache<String, CoordinateReferenceSystem>(10);
 
-    /**
+  /**
      * Creates a new factory.
      */
-    public CRSFactory() {
-    }
+  public CRSFactory() {
+  }
 
-    /**
+  /**
      * Return a
      *
      * @CoordinateReferenceSystem according an authority and a srid ie :
@@ -81,29 +48,29 @@ public class CRSFactory {
      * @return
      * @throws CRSException
      */
-    public CoordinateReferenceSystem getCRS(String authorityAndSrid) throws CRSException {
-        CoordinateReferenceSystem crs = CRSPOOL.get(authorityAndSrid);
-        if (crs == null) {
-            try {
-                String[] registryNameWithCode = splitRegistryNameAndCode(authorityAndSrid);
-                if (isRegistrySupported(registryNameWithCode[0])) {
-                    Registry registry = getRegistryManager().getRegistry(registryNameWithCode[0]);
-                    Map<String, String> crsParameters = registry.getParameters(registryNameWithCode[1]);
-                    if (crsParameters != null) {
-                        crs = CRSHelper.createCoordinateReferenceSystem(new Identifier(registryNameWithCode[0], registryNameWithCode[1], crsParameters.get("title")), crsParameters);
-                    }
-                    if (crs != null) {
-                        CRSPOOL.put(authorityAndSrid, crs);
-                    }
-                }
-            } catch (RegistryException ex) {
-                throw new CRSException("Cannot create the CRS", ex);
-            }
+  public CoordinateReferenceSystem getCRS(String authorityAndSrid) throws CRSException {
+    CoordinateReferenceSystem crs = CRSPOOL.get(authorityAndSrid);
+    if (crs == null) {
+      try {
+        String[] registryNameWithCode = splitRegistryNameAndCode(authorityAndSrid);
+        if (isRegistrySupported(registryNameWithCode[0])) {
+          Registry registry = getRegistryManager().getRegistry(registryNameWithCode[0]);
+          Map<String, String> crsParameters = registry.getParameters(registryNameWithCode[1]);
+          if (crsParameters != null) {
+            crs = CRSHelper.createCoordinateReferenceSystem(new Identifier(registryNameWithCode[0], registryNameWithCode[1], crsParameters.get("title")), crsParameters);
+          }
+          if (crs != null) {
+            CRSPOOL.put(authorityAndSrid, crs);
+          }
         }
-        return crs;
+      } catch (RegistryException ex) {
+        throw new CRSException("Cannot create the CRS", ex);
+      }
     }
+    return crs;
+  }
 
-    /**
+  /**
      * Return the registry name and the code base on the pattern name:code ed :
      * epsg:4326 returns epsg;4326
      *
@@ -111,68 +78,67 @@ public class CRSFactory {
      * @return
      * @throws RegistryException
      */
-    public String[] splitRegistryNameAndCode(String authorityAndSrid) throws RegistryException {
-        String[] registryAndCode = authorityAndSrid.split(":");
-        if (registryAndCode.length == 2) {
-            return registryAndCode;
-        } else {
-            throw new RegistryException("This registry pattern " + authorityAndSrid + " is not supported");
-        }
-
+  public String[] splitRegistryNameAndCode(String authorityAndSrid) throws RegistryException {
+    String[] registryAndCode = authorityAndSrid.split(":");
+    if (registryAndCode.length == 2) {
+      return registryAndCode;
+    } else {
+      throw new RegistryException("This registry pattern " + authorityAndSrid + " is not supported");
     }
+  }
 
-    /**
+  /**
      * Return the registry manager
      *
      * @return
      */
-    public RegistryManager getRegistryManager() {
-        return registryManager;
-    }
+  public RegistryManager getRegistryManager() {
+    return registryManager;
+  }
 
-    /**
+  /**
      * Check if the registry name is supported.
      *
      * @param registryName
      * @return
      */
-    public boolean isRegistrySupported(String registryName) throws RegistryException {
-        if (getRegistryManager().contains(registryName.toLowerCase())) {
-            return true;
-        } else {
-            throw new RegistryException("This registry " + registryName + " is not supported");
-        }
+  public boolean isRegistrySupported(String registryName) throws RegistryException {
+    if (getRegistryManager().contains(registryName.toLowerCase())) {
+      return true;
+    } else {
+      throw new RegistryException("This registry " + registryName + " is not supported");
     }
+  }
 
-    /**
+  /**
      * Creates a {@link CoordinateReferenceSystem} defined by an OGC WKT String
      * (PRJ).
      *
      * @param prjString the PRJ String
      * @return a {@link CoordinateReferenceSystem}
      */
-    public CoordinateReferenceSystem createFromPrj(String prjString) throws CRSException {
-        PrjParser p = new PrjParser();
-        Map<String, String> prjParameters = p.getParameters(prjString);
-        String name = prjParameters.remove("name");
-        String refname = prjParameters.remove("refname");
-        CoordinateReferenceSystem crs;
-        if (refname != null) {
-            crs = CRSPOOL.get(refname);
-            if (crs == null) {
-                String[] authorityNameWithKey = refname.split(":");
-                crs = CRSHelper.createCoordinateReferenceSystem(new Identifier(authorityNameWithKey[0], authorityNameWithKey[1], name), prjParameters);
-            }
-            if (crs != null) {
-                CRSPOOL.put(refname, crs);
-            }
-        } else {
-            crs = CRSHelper.createCoordinateReferenceSystem(new Identifier(name, name, name), prjParameters);
-        }
-        return crs;
+  public CoordinateReferenceSystem createFromPrj(String prjString) throws CRSException {
+    PrjParser p = new PrjParser();
+    Map<String, String> prjParameters = p.getParameters(prjString);
+    String name = prjParameters.remove("name");
+    String refname = prjParameters.remove("refname");
+    CoordinateReferenceSystem crs;
+    if (refname != null) {
+      crs = CRSPOOL.get(refname);
+      if (crs == null) {
+        String[] authorityNameWithKey = refname.split(":");
+        crs = CRSHelper.createCoordinateReferenceSystem(new Identifier(authorityNameWithKey[0], authorityNameWithKey[1], name), prjParameters);
+      }
+      if (crs != null) {
+        CRSPOOL.put(refname, crs);
+      }
+    } else {
+      crs = CRSHelper.createCoordinateReferenceSystem(new Identifier(name, name, name), prjParameters);
     }
+    return crs;
+  }
 
-    /**
+  /**
      * Creates a {@link CoordinateReferenceSystem} defined by an OGC WKT String
      * (PRJ).
      *
@@ -181,16 +147,16 @@ public class CRSFactory {
      * @return a {@link CoordinateReferenceSystem}
      * @throws IOException
      */
-    public CoordinateReferenceSystem createFromPrj(InputStream stream, Charset encoding) throws IOException, CRSException {
-        BufferedReader r = new BufferedReader(new InputStreamReader(stream, encoding));
-        StringBuilder b = new StringBuilder();
-        while (r.ready()) {
-            b.append(r.readLine());
-        }
-        return createFromPrj(b.toString());
+  public CoordinateReferenceSystem createFromPrj(InputStream stream, Charset encoding) throws IOException, CRSException {
+    BufferedReader r = new BufferedReader(new InputStreamReader(stream, encoding));
+    StringBuilder b = new StringBuilder();
+    while (r.ready()) {
+      b.append(r.readLine());
     }
+    return createFromPrj(b.toString());
+  }
 
-    /**
+  /**
      * Creates a {@link CoordinateReferenceSystem} defined by an OGC WKT String
      * (PRJ).
      *
@@ -198,11 +164,11 @@ public class CRSFactory {
      * @return
      * @throws IOException
      */
-    public CoordinateReferenceSystem createFromPrj(InputStream stream) throws IOException, CRSException {
-        return createFromPrj(stream, Charset.defaultCharset());
-    }
+  public CoordinateReferenceSystem createFromPrj(InputStream stream) throws IOException, CRSException {
+    return createFromPrj(stream, Charset.defaultCharset());
+  }
 
-    /**
+  /**
      * Creates a {@link CoordinateReferenceSystem} defined by an OGC WKT String
      * (PRJ).
      *
@@ -210,45 +176,40 @@ public class CRSFactory {
      * @return a {@link CoordinateReferenceSystem}
      * @throws IOException if there is a problem reading the file
      */
-    public CoordinateReferenceSystem createFromPrj(File file) throws IOException, CRSException {
-        InputStream i = null;
-        CoordinateReferenceSystem crs;
-        try {
-            i = new FileInputStream(file);
-            crs = createFromPrj(i);
-        } finally {
-            if (i != null) {
-                i.close();
-            }
-        }
-        return crs;
+  public CoordinateReferenceSystem createFromPrj(File file) throws IOException, CRSException {
+    InputStream i = null;
+    CoordinateReferenceSystem crs;
+    try {
+      i = new FileInputStream(file);
+      crs = createFromPrj(i);
+    }  finally {
+      if (i != null) {
+        i.close();
+      }
     }
+    return crs;
+  }
 
-    /**
+  /**
      * Return a list of supported codes according an registryName
      *
      * @param registryName
      * @return
      */
-    public Set<String> getSupportedCodes(String registryName) throws RegistryException {
-        return getRegistryManager().getRegistry(registryName).getSupportedCodes();
+  public Set<String> getSupportedCodes(String registryName) throws RegistryException {
+    return getRegistryManager().getRegistry(registryName).getSupportedCodes();
+  }
+
+  public class CRSCache<K extends java.lang.Object, V extends java.lang.Object> extends LinkedHashMap<K, V> {
+    private final int limit;
+
+    public CRSCache(int limit) {
+      super(16, 0.75f, true);
+      this.limit = limit;
     }
 
-    /**
-     * A simple cache to manage {@link CoordinateReferenceSystem}
-     */
-    public class CRSCache<K, V> extends LinkedHashMap<K, V> {
-
-        private final int limit;
-
-        public CRSCache(int limit) {
-            super(16, 0.75f, true);
-            this.limit = limit;
-        }
-
-        @Override
-        protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
-            return size() > limit;
-        }
+    @Override protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
+      return size() > limit;
     }
+  }
 }
--- src/main/java/org/cts/parser/prj/PrjParser.java
+++ src/main/java/org/cts/parser/prj/PrjParser.java
@@ -1,36 +1,4 @@
-/*
- * Coordinate Transformations Suite (abridged CTS)  is a library developped to 
- * perform Coordinate Transformations using well known geodetic algorithms 
- * and parameter sets. 
- * Its main focus are simplicity, flexibility, interoperability, in this order.
- *
- * This library has been originally developed by Michaël Michaud under the JGeod
- * name. It has been renamed CTS in 2009 and shared to the community from 
- * the Atelier SIG code repository.
- * 
- * Since them, CTS is supported by the Atelier SIG team in collaboration with Michaël 
- * Michaud.
- * The new CTS has been funded  by the French Agence Nationale de la Recherche 
- * (ANR) under contract ANR-08-VILL-0005-01 and the regional council 
- * "Région Pays de La Loire" under the projet SOGVILLE (Système d'Orbservation 
- * Géographique de la Ville).
- *
- * CTS is free software: you can redistribute it and/or modify it under the
- * terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any later
- * version.
- *
- * CTS is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with
- * CTS. If not, see <http://www.gnu.org/licenses/>.
- *
- * For more information, please consult: <https://github.com/irstv/cts/>
- */
 package org.cts.parser.prj;
-
 import java.nio.BufferUnderflowException;
 import java.nio.CharBuffer;
 import java.util.ArrayList;
@@ -50,15 +18,14 @@ import java.util.Map;
  * @author Antoine Gourlay, Erwan Bocher, Jules Party
  */
 public class PrjParser {
-
-    /**
+  /**
      * Creates a new parser.
      *
      */
-    public PrjParser() {
-    }
+  public PrjParser() {
+  }
 
-    /**
+  /**
      * Parses a WKT PRJ String into a set of parameters.
      *
      * This is the main entry point of the parser.
@@ -68,122 +35,114 @@ public class PrjParser {
      * @throws PrjParserException if the PRJ cannot be parsed into a CRS for any
      * reason
      */
-    public Map<String, String> getParameters(String prjString) {
-        CharBuffer s = CharBuffer.wrap(prjString);
-        PrjElement e;
-        try {
-            e = parseNode(s);
-        } catch (BufferUnderflowException ex) {
-            throw new PrjParserException("Failed to read PRJ.", ex);
-        }
-        Map<String, String> prjParameters = PrjMatcher.match(e);
-        return prjParameters;
+  public Map<String, String> getParameters(String prjString) {
+    CharBuffer s = CharBuffer.wrap(prjString);
+    PrjElement e;
+    try {
+      e = parseNode(s);
+    } catch (BufferUnderflowException ex) {
+      throw new PrjParserException("Failed to read PRJ.", ex);
     }
+    Map<String, String> prjParameters = PrjMatcher.match(e);
+    return prjParameters;
+  }
 
-    public PrjElement parseNode(CharBuffer s) {
-        // parse Name
-        boolean complexNode = false;
-        int start = s.position();
-        int ll = 0;
-        while (s.hasRemaining()) {
-            char c = s.get();
-            if (c == '[') {
-                complexNode = true;
-                break;
-            } else if (c == ']' || c == ',') {
-                break;
-            } else {
-                ll++;
-            }
-        }
-        s.position(start);
-        String name = s.subSequence(0, ll).toString();
-        s.position(start + ll + 1);
-
-        if (complexNode) {
-            // parse children and return
-            return new PrjNodeElement(name, parseNodeChildren(s));
+  public PrjElement parseNode(CharBuffer s) {
+    boolean complexNode = false;
+    int start = s.position();
+    int ll = 0;
+    while (s.hasRemaining()) {
+      char c = s.get();
+      if (c == '[') {
+        complexNode = true;
+        break;
+      } else {
+        if (c == ']' || c == ',') {
+          break;
         } else {
-            s.position(s.position() - 1);
-            return new PrjStringElement(name);
+          ll++;
         }
+      }
     }
-
-    private char next(CharBuffer s) {
-        char next;
-        do {
-            next = s.get();
-        } while (Character.isWhitespace(next));
-        return next;
+    s.position(start);
+    String name = s.subSequence(0, ll).toString();
+    s.position(start + ll + 1);
+    if (complexNode) {
+      return new PrjNodeElement(name, parseNodeChildren(s));
+    } else {
+      s.position(s.position() - 1);
+      return new PrjStringElement(name);
     }
+  }
 
-    private List<PrjElement> parseNodeChildren(CharBuffer s) {
-        List<PrjElement> elms = new ArrayList<PrjElement>();
+  private char next(CharBuffer s) {
+    char next;
+    do {
+      next = s.get();
+    } while(Character.isWhitespace(next));
+    return next;
+  }
 
-        boolean finished = false;
-
-        do {
-            char next = next(s);
-            if (next == '"') {
-                elms.add(parseString(s));
-            } else {
-                s.position(s.position() - 1);
-                if (Character.isDigit(next) || next == '-') {
-                    elms.add(parseNumber(s));
-                } else {
-                    elms.add(parseNode(s));
-                }
-            }
-
-
-            next = next(s);
-            switch (next) {
-                case ',':
-                    break;
-                case ']':
-                    finished = true;
-                    break;
-                default:
-                    throw new PrjParserException("weird character: " + next);
-            }
-        } while (!finished);
-
-        return elms;
-    }
-
-    private PrjStringElement parseString(CharBuffer s) {
-        int start = s.position();
-        int ll = 0;
-        while (s.hasRemaining()) {
-            char c = s.get();
-            if (c == '"') {
-                break;
-            } else {
-                ll++;
-            }
+  private List<PrjElement> parseNodeChildren(CharBuffer s) {
+    List<PrjElement> elms = new ArrayList<PrjElement>();
+    boolean finished = false;
+    do {
+      char next = next(s);
+      if (next == '\"') {
+        elms.add(parseString(s));
+      } else {
+        s.position(s.position() - 1);
+        if (Character.isDigit(next) || next == '-') {
+          elms.add(parseNumber(s));
+        } else {
+          elms.add(parseNode(s));
         }
-        s.position(start);
-        String str = s.subSequence(0, ll).toString();
-        s.position(start + ll + 1);
+      }
+      next = next(s);
+      switch (next) {
+        case ',':
+        break;
+        case ']':
+        finished = true;
+        break;
+        default:
+        throw new PrjParserException("weird character: " + next);
+      }
+    } while(!finished);
+    return elms;
+  }
 
-        return new PrjStringElement(str);
+  private PrjStringElement parseString(CharBuffer s) {
+    int start = s.position();
+    int ll = 0;
+    while (s.hasRemaining()) {
+      char c = s.get();
+      if (c == '\"') {
+        break;
+      } else {
+        ll++;
+      }
     }
+    s.position(start);
+    String str = s.subSequence(0, ll).toString();
+    s.position(start + ll + 1);
+    return new PrjStringElement(str);
+  }
 
-    private PrjNumberElement parseNumber(CharBuffer s) {
-        int start = s.position();
-        int ll = 0;
-        while (s.hasRemaining()) {
-            char c = s.get();
-            if (c == ',' || c == ']' || Character.isWhitespace(c)) {
-                break;
-            } else {
-                ll++;
-            }
-        }
-        s.position(start);
-        String str = s.subSequence(0, ll).toString();
-        s.position(start + ll);
-
-        return new PrjNumberElement(Double.parseDouble(str));
+  private PrjNumberElement parseNumber(CharBuffer s) {
+    int start = s.position();
+    int ll = 0;
+    while (s.hasRemaining()) {
+      char c = s.get();
+      if (c == ',' || c == ']' || Character.isWhitespace(c)) {
+        break;
+      } else {
+        ll++;
+      }
     }
+    s.position(start);
+    String str = s.subSequence(0, ll).toString();
+    s.position(start + ll);
+    return new PrjNumberElement(Double.parseDouble(str));
+  }
 }
