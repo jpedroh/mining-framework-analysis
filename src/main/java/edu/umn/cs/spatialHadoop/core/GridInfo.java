9dba5e8723bb794e69027cd7eab26d2329dbfe2a
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 09:44:54 +0000

    Mining Framework Analysis

--- src/main/java/edu/umn/cs/spatialHadoop/core/GridInfo.java
+++ src/main/java/edu/umn/cs/spatialHadoop/core/GridInfo.java
@@ -1,19 +1,8 @@
-/***********************************************************************
-* Copyright (c) 2015 by Regents of the University of Minnesota.
-* All rights reserved. This program and the accompanying materials
-* are made available under the terms of the Apache License, Version 2.0 which 
-* accompanies this distribution and is available at
-* http://www.opensource.org/licenses/apache2.0.php.
-*
-*************************************************************************/
 package edu.umn.cs.spatialHadoop.core;
-
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
-
 import org.apache.hadoop.io.Text;
-
 import edu.umn.cs.spatialHadoop.io.TextSerializerHelper;
 
 /**
@@ -28,34 +17,29 @@ public class GridInfo extends Rectangle {
 
   public GridInfo() {
   }
-  
+
   public GridInfo(double x1, double y1, double x2, double y2) {
     super(x1, y1, x2, y2);
     this.columns = 0;
     this.rows = 0;
   }
 
-  @Override
-  public void write(DataOutput out) throws IOException {
+  @Override public void write(DataOutput out) throws IOException {
     super.write(out);
     out.writeInt(columns);
     out.writeInt(rows);
   }
 
-  @Override
-  public void readFields(DataInput in) throws IOException {
+  @Override public void readFields(DataInput in) throws IOException {
     super.readFields(in);
     columns = in.readInt();
     rows = in.readInt();
   }
 
-  @Override
-  public String toString() {
-    return "grid: "+x1+","+y1+","+x2+","+y2+", " +
-    "cell: "+getAverageCellWidth()+","+getAverageCellHeight()+
-    "("+columns+"x"+rows+")";
+  @Override public String toString() {
+    return "grid: " + x1 + "," + y1 + "," + x2 + "," + y2 + ", " + "cell: " + getAverageCellWidth() + "," + getAverageCellHeight() + "(" + columns + "x" + rows + ")";
   }
-  
+
   public double getAverageCellHeight() {
     return (y2 - y1) / Math.max(rows, 1);
   }
@@ -64,34 +48,29 @@ public class GridInfo extends Rectangle {
     return (x2 - x1) / Math.max(columns, 1);
   }
 
-  @Override
-  public boolean equals(Object obj) {
-    if (obj == null)
+  @Override public boolean equals(Object obj) {
+    if (obj == null) {
       return false;
+    }
     GridInfo gi = (GridInfo) obj;
-    return super.equals(obj)
-        && this.columns == gi.columns && this.rows == gi.rows;
+    return super.equals(obj) && this.columns == gi.columns && this.rows == gi.rows;
   }
 
-  @Override
-  public int hashCode() {
+  @Override public int hashCode() {
     int result = this.columns;
     result = 31 * result + this.rows;
     return result;
   }
 
   public void calculateCellDimensions(long totalFileSize, long blockSize) {
-    // An empirical number for the expected overhead in grid file due to
-    // replication
-    int numBlocks = (int) Math.ceil((double)totalFileSize / blockSize);
+    int numBlocks = (int) Math.ceil((double) totalFileSize / blockSize);
     calculateCellDimensions(numBlocks);
   }
-  
+
   public void calculateCellDimensions(int numCells) {
     int gridCols = 1;
     int gridRows = 1;
     while (gridRows * gridCols < numCells) {
-      // (  cellWidth          >    cellHeight        )
       if ((x2 - x1) / gridCols > (y2 - y1) / gridRows) {
         gridCols++;
       } else {
@@ -102,24 +81,21 @@ public class GridInfo extends Rectangle {
     rows = gridRows;
   }
 
-  @Override
-  public Text toText(Text text) {
+  @Override public Text toText(Text text) {
     final byte[] Comma = ",".getBytes();
     super.toText(text);
     text.append(Comma, 0, Comma.length);
     TextSerializerHelper.serializeLong(columns, text, ',');
-    TextSerializerHelper.serializeLong(rows, text, '\0');
+    TextSerializerHelper.serializeLong(rows, text, '\u0000');
     return text;
   }
 
-  @Override
-  public void fromText(Text text) {
+  @Override public void fromText(Text text) {
     super.fromText(text);
     if (text.getLength() > 0) {
-      // Remove the first comma
       text.set(text.getBytes(), 1, text.getLength() - 1);
       columns = (int) TextSerializerHelper.consumeInt(text, ',');
-      rows = (int) TextSerializerHelper.consumeInt(text, '\0');
+      rows = (int) TextSerializerHelper.consumeInt(text, '\u0000');
     }
   }
 
@@ -128,13 +104,11 @@ public class GridInfo extends Rectangle {
     CellInfo[] cells = new CellInfo[columns * rows];
     double ystart = y1;
     for (int row = 0; row < rows; row++) {
-      double yend = row == rows-1 ? y2 : (y1 + (y2 - y1) * (row+1) / rows);
+      double yend = row == rows - 1 ? y2 : (y1 + (y2 - y1) * (row + 1) / rows);
       double xstart = x1;
       for (int col = 0; col < columns; col++) {
-        double xend = col == columns - 1? x2 : (x1 + (x2 - x1) * (col+1) / columns);
-
+        double xend = col == columns - 1 ? x2 : (x1 + (x2 - x1) * (col + 1) / columns);
         cells[cellIndex] = new CellInfo(++cellIndex, xstart, ystart, xend, yend);
-
         xstart = xend;
       }
       ystart = yend;
@@ -149,25 +123,34 @@ public class GridInfo extends Rectangle {
    */
   public java.awt.Rectangle getOverlappingCells(Rectangle rect) {
     int col1, col2, row1, row2;
-    col1 = (int)Math.floor((rect.x1 - this.x1) / this.getWidth() * columns);
-    if (col1 < 0) col1 = 0;
-    col2 = (int)Math.ceil((rect.x2 - this.x1) / this.getWidth() * columns);
-    if (col2 > columns) col2 = columns;
-    row1 = (int)Math.floor((rect.y1 - this.y1) / this.getHeight() * rows);
-    if (row1 < 0) row1 = 0;
-    row2 = (int)Math.ceil((rect.y2 - this.y1) / this.getHeight() * rows);
-    if (row2 > rows) row2 = rows;
+    col1 = (int) Math.floor((rect.x1 - this.x1) / this.getWidth() * columns);
+    if (col1 < 0) {
+      col1 = 0;
+    }
+    col2 = (int) Math.ceil((rect.x2 - this.x1) / this.getWidth() * columns);
+    if (col2 > columns) {
+      col2 = columns;
+    }
+    row1 = (int) Math.floor((rect.y1 - this.y1) / this.getHeight() * rows);
+    if (row1 < 0) {
+      row1 = 0;
+    }
+    row2 = (int) Math.ceil((rect.y2 - this.y1) / this.getHeight() * rows);
+    if (row2 > rows) {
+      row2 = rows;
+    }
     return new java.awt.Rectangle(col1, row1, col2 - col1, row2 - row1);
   }
-  
+
   public int getOverlappingCell(double x, double y) {
-    if (!contains(x, y))
+    if (!contains(x, y)) {
       return -1;
-    int column = (int)Math.floor((x - this.x1) / this.getWidth() * columns);
-    int row = (int)Math.floor((y - this.y1) / this.getHeight() * rows);
+    }
+    int column = (int) Math.floor((x - this.x1) / this.getWidth() * columns);
+    int row = (int) Math.floor((y - this.y1) / this.getHeight() * rows);
     return getCellId(column, row);
   }
-  
+
   /**
    * Returns the information of a grid cell given it ID
    * @param cellId
@@ -177,12 +160,12 @@ public class GridInfo extends Rectangle {
     int col = (cellId - 1) % columns;
     int row = (cellId - 1) / columns;
     double xstart = x1 + (x2 - x1) * col / columns;
-    double xend = col == columns - 1? x2 : (x1 + (x2 - x1) * (col + 1) / columns);
+    double xend = col == columns - 1 ? x2 : (x1 + (x2 - x1) * (col + 1) / columns);
     double ystart = y1 + (y2 - y1) * row / rows;
-    double yend = (row == rows - 1)? y2 : (y1 + (y2 - y1) * (row + 1) / rows);
+    double yend = (row == rows - 1) ? y2 : (y1 + (y2 - y1) * (row + 1) / rows);
     return new CellInfo(cellId, xstart, ystart, xend, yend);
   }
-  
+
   public int getCellId(int column, int row) {
     return (row * columns + column) + 1;
   }
