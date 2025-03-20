5d298e1f49cd13b38eed4e8fc3869361b9e824b2
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 02:20:44 +0000

    Mining Framework Analysis

--- gdx-kitten-rescue/src/main/java/de/hochschuletrier/gdw/ss14/ecs/components/CatPropertyComponent.java
+++ gdx-kitten-rescue/src/main/java/de/hochschuletrier/gdw/ss14/ecs/components/CatPropertyComponent.java
@@ -1,20 +1,31 @@
 package de.hochschuletrier.gdw.ss14.ecs.components;
-
 import de.hochschuletrier.gdw.ss14.states.*;
 
-public class CatPropertyComponent implements Component
-{
-    public int amountLives;
-    public boolean canSeeLaserPointer;
-    public boolean isAlive;
-    public CatStateEnum state;
-    public float jumpBuffer = 0;
+public class CatPropertyComponent implements Component {
+  public int amountLives;
 
-    public CatPropertyComponent()
-    {
-        canSeeLaserPointer = true;
-        amountLives = 9;
-        isAlive = true;
-        state = CatStateEnum.IDLE;
-    }
+  public boolean isAlive;
+
+  public 
+<<<<<<< /usr/src/app/output/lusito/gamedevweek/5798d449c99505d4ff784cb64692af7c89120741/gdx-kitten-rescue/src/main/java/de/hochschuletrier/gdw/ss14/ecs/components/CatPropertyComponent.java/left.java
+  float
+=======
+  boolean
+>>>>>>> /usr/src/app/output/lusito/gamedevweek/5798d449c99505d4ff784cb64692af7c89120741/gdx-kitten-rescue/src/main/java/de/hochschuletrier/gdw/ss14/ecs/components/CatPropertyComponent.java/right.java
+   
+<<<<<<< /usr/src/app/output/lusito/gamedevweek/5798d449c99505d4ff784cb64692af7c89120741/gdx-kitten-rescue/src/main/java/de/hochschuletrier/gdw/ss14/ecs/components/CatPropertyComponent.java/left.java
+  jumpBuffer = 0
+=======
+  canSeeLaserPointer
+>>>>>>> /usr/src/app/output/lusito/gamedevweek/5798d449c99505d4ff784cb64692af7c89120741/gdx-kitten-rescue/src/main/java/de/hochschuletrier/gdw/ss14/ecs/components/CatPropertyComponent.java/right.java
+  ;
+
+  public CatStateEnum state;
+
+  public CatPropertyComponent() {
+    canSeeLaserPointer = true;
+    amountLives = 9;
+    isAlive = true;
+    state = CatStateEnum.IDLE;
+  }
 }
