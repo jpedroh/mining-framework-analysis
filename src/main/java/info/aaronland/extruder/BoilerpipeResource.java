e1e7da9d5ef6b1074f128a14e86b378f0c026de6
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 06:26:22 +0000

    Mining Framework Analysis

--- src/main/java/info/aaronland/extruder/BoilerpipeResource.java
+++ src/main/java/info/aaronland/extruder/BoilerpipeResource.java
@@ -1,110 +1,73 @@
 package info.aaronland.extruder;
-
 import info.aaronland.extruder.Upload;
 import info.aaronland.extruder.Document;
 import info.aaronland.extruder.DocumentView;
-
 import java.io.InputStream;
 import java.io.File;
-
 import com.sun.jersey.core.header.FormDataContentDisposition;
 import com.sun.jersey.multipart.FormDataParam;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.Consumes;
-
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
 import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.core.MediaType;
-
 import java.net.URL;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import de.l3s.boilerpipe.extractors.DefaultExtractor;
 import de.l3s.boilerpipe.extractors.ArticleExtractor;
 
-@Path(value = "/boilerpipe")
-@Produces({MediaType.TEXT_HTML, MediaType.APPLICATION_JSON})
-public class BoilerpipeResource {
+@Path(value = "/boilerpipe") @Produces(value = { MediaType.TEXT_HTML, MediaType.APPLICATION_JSON }) public class BoilerpipeResource {
+  private static final Logger LOGGER = LoggerFactory.getLogger(BoilerpipeResource.class);
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(BoilerpipeResource.class);
-
-    @GET
-    public Response extrudeThisURL(@QueryParam("url") String url){
-
-	Document doc;
-	DocumentView view;
-
-	try {
-	    doc = extrudeThis(url);
-	    view = new DocumentView(doc);
-	}
-
-	// TODO: trap MalformedURLExceptions and return NOT_ACCEPTABLE here (20130901/straup)
-
-	catch (Exception e){
-            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.toString()).build();
-	}
-
-	return Response.status(Response.Status.OK).entity(view).build();
+  @GET public Response extrudeThisURL(@QueryParam(value = "url") String url) {
+    Document doc;
+    DocumentView view;
+    try {
+      doc = extrudeThis(url);
+      view = new DocumentView(doc);
+    } catch (Exception e) {
+      return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.toString()).build();
     }
+    return Response.status(Response.Status.OK).entity(view).build();
+  }
 
-    @POST
-    @Consumes(MediaType.MULTIPART_FORM_DATA)
-    public Response extrudeThisFile(@FormDataParam("file") InputStream input){
-
-	Upload upload = new Upload();
-	File tmpfile = upload.writeTmpFile(input);
-
-	String uri = "file://" + tmpfile.getAbsolutePath();
-
-	Document doc;
-	DocumentView view;
-
-	try {
-	    doc = extrudeThis(uri);
-	    view = new DocumentView(doc);
-	}
-
-	catch (Exception e){
-	    tmpfile.delete();
-            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.toString()).build();
-	}
-
-	tmpfile.delete();
-
-	return Response.status(Response.Status.OK).entity(view).build();
+  @POST @Consumes(value = MediaType.MULTIPART_FORM_DATA) public Response extrudeThisFile(@FormDataParam(value = "file") InputStream input) {
+    Upload upload = new Upload();
+    File tmpfile = upload.writeTmpFile(input);
+    String uri = "file://" + tmpfile.getAbsolutePath();
+    Document doc;
+    DocumentView view;
+    try {
+      doc = extrudeThis(uri);
+      view = new DocumentView(doc);
+    } catch (Exception e) {
+      tmpfile.delete();
+      return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.toString()).build();
     }
+    tmpfile.delete();
+    return Response.status(Response.Status.OK).entity(view).build();
+  }
 
-    private Document extrudeThis(String uri){
-
-	URL url;
-	String text;
-
-	try {
-	    url = new URL(uri);
-	}
-
-	catch (Exception e){
-	    throw new RuntimeException(e);
-	}
-
-	try {
-	    text = ArticleExtractor.INSTANCE.getText(url);
-	}
-
-	catch (Exception e){
-	    throw new RuntimeException(e);
-	}
-
-	return new Document(text);
+  private Document extrudeThis(String uri) {
+    URL url;
+    String text;
+    try {
+      url = new URL(uri);
+    } catch (Exception e) {
+      throw new RuntimeException(e);
     }
-
+    try {
+      text = ArticleExtractor.INSTANCE.getText(url);
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+    Document doc = new Document(text);
+    return doc;
+  }
 }
--- src/main/java/info/aaronland/extruder/JavaReadabilityResource.java
+++ src/main/java/info/aaronland/extruder/JavaReadabilityResource.java
@@ -1,132 +1,102 @@
 package info.aaronland.extruder;
-
 import info.aaronland.extruder.Upload;
 import info.aaronland.extruder.Document;
 import info.aaronland.extruder.DocumentView;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.Consumes;
-
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.core.MediaType;
-
 import com.sun.jersey.core.header.FormDataContentDisposition;
 import com.sun.jersey.multipart.FormDataParam;
-
 import java.io.InputStream;
 import java.io.File;
-
 import java.net.URL;
-
 import com.basistech.readability.Readability;
-
-// See below inre: Readers (20130901/straup)
 import com.basistech.readability.HttpPageReader;
 import com.basistech.readability.FilePageReader;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@Path(value = "/java-readability")
-@Produces({MediaType.TEXT_HTML, MediaType.APPLICATION_JSON})
-public class JavaReadabilityResource {
+@Path(value = "/java-readability") @Produces(value = { MediaType.TEXT_HTML, MediaType.APPLICATION_JSON }) public class JavaReadabilityResource {
+  private static final Logger LOGGER = LoggerFactory.getLogger(JavaReadabilityResource.class);
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(JavaReadabilityResource.class);
+  private static final TextUtils utils = new TextUtils();
 
-    @GET
-    public Response extrudeThisURL(@QueryParam("url") String url){
+  @GET public Response extrudeThisURL(@QueryParam(value = "url") String url) {
+    Document doc;
+    DocumentView view;
+    try {
+      doc = extrudeThis(url);
 
-	Document doc;
-	DocumentView view;
-
-	try {
-	    doc = extrudeThis(url);
-	    view = new DocumentView(doc);
-	}
-
-	// TODO: trap MalformedURLExceptions and return NOT_ACCEPTABLE here (20130901/straup)
-
-	catch (Exception e){
-            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.toString()).build();
-	}
-
-	return Response.status(Response.Status.OK).entity(view).build();
+<<<<<<< /usr/src/app/output/straup/dogeared-extruder/0176ca61e4bac5035afc89e56f3063a206c3a660/src/main/java/info/aaronland/extruder/JavaReadabilityResource.java/left.java
+      text = massageText(text)
+=======
+      view = new DocumentView(doc)
+>>>>>>> /usr/src/app/output/straup/dogeared-extruder/0176ca61e4bac5035afc89e56f3063a206c3a660/src/main/java/info/aaronland/extruder/JavaReadabilityResource.java/right.java
+      ;
+    } catch (Exception e) {
+      return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.toString()).build();
     }
+    return Response.status(Response.Status.OK).entity(view).build();
+  }
 
-    @POST
-    @Consumes(MediaType.MULTIPART_FORM_DATA)
-    public Response extrudeThisFile(@FormDataParam("file") InputStream input){
+  @POST @Consumes(value = MediaType.MULTIPART_FORM_DATA) public Response extrudeThisFile(@FormDataParam(value = "file") InputStream input) {
+    Upload upload = new Upload();
+    File tmpfile = upload.writeTmpFile(input);
+    String uri = "file://" + tmpfile.getAbsolutePath();
+    Document doc;
+    DocumentView view;
+    try {
+      doc = extrudeThis(uri);
 
-	Upload upload = new Upload();
-	File tmpfile = upload.writeTmpFile(input);
-
-	String uri = "file://" + tmpfile.getAbsolutePath();
-
-	Document doc;
-	DocumentView view;
-
-	try {
-	    doc = extrudeThis(uri);
-	    view = new DocumentView(doc);
-	}
-
-	catch (Exception e){
-	    tmpfile.delete();
-            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.toString()).build();
-	}
-
-	tmpfile.delete();
-
-	return Response.status(Response.Status.OK).entity(view).build();
+<<<<<<< /usr/src/app/output/straup/dogeared-extruder/0176ca61e4bac5035afc89e56f3063a206c3a660/src/main/java/info/aaronland/extruder/JavaReadabilityResource.java/left.java
+      text = massageText(text)
+=======
+      view = new DocumentView(doc)
+>>>>>>> /usr/src/app/output/straup/dogeared-extruder/0176ca61e4bac5035afc89e56f3063a206c3a660/src/main/java/info/aaronland/extruder/JavaReadabilityResource.java/right.java
+      ;
+    } catch (Exception e) {
+      tmpfile.delete();
+      return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.toString()).build();
     }
+    tmpfile.delete();
+    return Response.status(Response.Status.OK).entity(view).build();
+  }
 
-    private Document extrudeThis(String uri){
-
-	URL url = null;
-	String text = "";
-
-	try {
-	    url = new URL(uri);
-	}
-	
-	catch (Exception e){
-	    throw new RuntimeException(e);
-	}
-
-	try {
-	    
-	    Readability parser = new Readability();
-	    String path = url.toString();
-
-	    // Basically I need to write a URIPageReader class to hide
-	    // all this nonsense because the HttpPageReader uses the Http
-	    // classes rather than java.net.URL (20130901/straup)
-
-	    if (path.startsWith("file:")){
-		path = path.replace("file:", "");
-		FilePageReader reader = new FilePageReader();
-		parser.setPageReader(reader);
-	    }
-
-	    else {
-		HttpPageReader reader = new HttpPageReader();
-		parser.setPageReader(reader);
-	    }
-
-	    parser.processDocument(path);
-	    text = parser.getArticleText();
-	}
-
-	catch (Exception e){
-	    throw new RuntimeException(e);
-	}
-	
-	return new Document(text);
+  private Document extrudeThis(String uri) {
+    URL url = null;
+    String text = "";
+    try {
+      url = new URL(uri);
+    } catch (Exception e) {
+      throw new RuntimeException(e);
     }
+    try {
+      Readability parser = new Readability();
+      String path = url.toString();
+      if (path.startsWith("file:")) {
+        path = path.replace("file:", "");
+        FilePageReader reader = new FilePageReader();
+        parser.setPageReader(reader);
+      } else {
+        HttpPageReader reader = new HttpPageReader();
+        parser.setPageReader(reader);
+      }
+      parser.processDocument(path);
+      text = parser.getArticleText();
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+    return new Document(text);
+  }
 
+  private String massageText(String text) {
+    text = utils.text2html(text);
+    return text;
+  }
 }
