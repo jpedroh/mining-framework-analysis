69dd63c46d1cc6d7e8db1a7cb7b956aa616ec994
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 14:52:02 +0000

    Mining Framework Analysis

--- src/main/java/ee/ignorance/transformiceapi/processors/MouseDeathProcessor.java
+++ src/main/java/ee/ignorance/transformiceapi/processors/MouseDeathProcessor.java
@@ -1,20 +1,13 @@
 package ee.ignorance.transformiceapi.processors;
-
 import ee.ignorance.transformiceapi.PlayerImpl;
 import ee.ignorance.transformiceapi.event.MouseDeathEvent;
 import ee.ignorance.transformiceapi.protocol.server.AbstractResponse;
 import ee.ignorance.transformiceapi.protocol.server.MouseDeathResponse;
 
-
-public class MouseDeathProcessor extends CommandProcessor{
-
-    @Override
-    public void process(AbstractResponse command, PlayerImpl player) {
-    	MouseDeathResponse response = (MouseDeathResponse) command;
-        player.getMouseByID(response.getMouseID()).setDead(true);
-    	player.notifyListeners(new MouseDeathEvent(response.getMouseID(),
-                player.getMouseById(response.getMouseID()).getName()));
-    }
-
-
+public class MouseDeathProcessor extends CommandProcessor {
+  @Override public void process(AbstractResponse command, PlayerImpl player) {
+    MouseDeathResponse response = (MouseDeathResponse) command;
+    player.getMouseByID(response.getMouseID()).setDead(true);
+    player.notifyListeners(new MouseDeathEvent(response.getMouseID(), player.getMouseById(response.getMouseID()).getName()));
+  }
 }
