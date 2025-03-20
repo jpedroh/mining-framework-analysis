2a1770987ddee7f829b827ecd7e6c015d15cf902
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 02:49:38 +0000

    Mining Framework Analysis

--- gdx-pneumatic-man/src/main/java/de/hochschuletrier/gdw/ws1415/game/ComponentMappers.java
+++ gdx-pneumatic-man/src/main/java/de/hochschuletrier/gdw/ws1415/game/ComponentMappers.java
@@ -14,5 +14,4 @@ public class ComponentMappers {
     public static final ComponentMapper<MovementComponent> movement = ComponentMapper.getFor(MovementComponent.class);
     public static final ComponentMapper<BouncingComponent> bouncing = ComponentMapper.getFor(BouncingComponent.class);
     public static final ComponentMapper<KillsPlayerOnContactComponent> enemy = ComponentMapper.getFor(KillsPlayerOnContactComponent.class);
-
 }
