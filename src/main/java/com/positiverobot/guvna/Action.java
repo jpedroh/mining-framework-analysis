1abb15413b77cb973829789cd86750cd3a735f4c
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 23:57:15 +0000

    Mining Framework Analysis

--- src/main/java/com/positiverobot/guvna/Action.java
+++ src/main/java/com/positiverobot/guvna/Action.java
@@ -1,5 +1,11 @@
 package com.positiverobot.guvna;
 
-public interface Action<S, E> {
-    public void apply(StateMachine<S, E> stateMachine, E event, S futureState);
+public interface Action<S extends java.lang.Object, E extends java.lang.Object> {
+  public void apply(
+<<<<<<< /usr/src/app/output/olibye/guvna/34cd63519389f9e2fc0f1a8ec79f770a764e5e69/src/main/java/com/positiverobot/guvna/Action.java/left.java
+  StateMachine<S, E> stateMachine
+=======
+  StateMachine<S, E> target
+>>>>>>> /usr/src/app/output/olibye/guvna/34cd63519389f9e2fc0f1a8ec79f770a764e5e69/src/main/java/com/positiverobot/guvna/Action.java/right.java
+  , E event, S futureState);
 }
