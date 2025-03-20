a6c39e9e8ea257bfcea69e5d29d8ea6fd6a3587a
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 19:44:50 +0000

    Mining Framework Analysis

--- src/main/java/org/assertj/core/error/ShouldBeCloseTo.java
+++ src/main/java/org/assertj/core/error/ShouldBeCloseTo.java
@@ -1,21 +1,6 @@
-/**
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License.
- *
- * Copyright 2012-2016 the original author or authors.
- */
 package org.assertj.core.error;
-
 import static java.lang.String.format;
-
 import static org.assertj.core.util.DateUtil.formatAsDatetimeWithMs;
-
 import java.time.temporal.Temporal;
 import java.util.Date;
 
@@ -26,7 +11,6 @@ import java.util.Date;
  * @author Joel Costigliola
  */
 public class ShouldBeCloseTo extends BasicErrorMessageFactory {
-
   /**
    * Creates a new </code>{@link ShouldBeCloseTo}</code>.
    * @param actual the actual value in the failed assertion.
@@ -52,12 +36,7 @@ public class ShouldBeCloseTo extends BasicErrorMessageFactory {
   }
 
   private ShouldBeCloseTo(Date actual, Date other, long deltaInMilliseconds, long difference) {
-    // format Date up to the given ms, because default format is the second, thus dates with a difference less than 1s
-    // seems equal in the error message.
-    // Use standard formatting to avoid calling ToString.toStringOf for long that adds a 'L' (like 100L) to
-    // differentiate integer from long (here there is no ambiguity).
-    super(format("%nExpecting:%n <%s>%nto be close to:%n <%s>%nby less than %sms but difference was %sms",
-        formatAsDatetimeWithMs(actual), formatAsDatetimeWithMs(other), deltaInMilliseconds, difference));
+    super(format("%nExpecting:%n <%s>%nto be close to:%n <%s>%nby less than %sms but difference was %sms", formatAsDatetimeWithMs(actual), formatAsDatetimeWithMs(other), deltaInMilliseconds, difference));
   }
 
   private ShouldBeCloseTo(Temporal actual, Temporal other, String differenceDescription) {
