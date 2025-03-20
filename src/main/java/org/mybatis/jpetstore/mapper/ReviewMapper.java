6aeafb4eb766cff75fc5518b7e99a386561c7e74
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 21:56:05 +0000

    Mining Framework Analysis

--- src/main/java/org/mybatis/jpetstore/mapper/ReviewMapper.java
+++ src/main/java/org/mybatis/jpetstore/mapper/ReviewMapper.java
@@ -10,9 +10,13 @@ public interface ReviewMapper {
     List<Review> getReviews();
     Review getReviewById(String id);
     void deleteReviewById(String id);
+<<<<<<< /usr/src/app/output/mybatis/jpetstore-6/b26b0c87017c0f2ec431ee0a1425910b9a9fff00/src/main/java/org/mybatis/jpetstore/mapper/ReviewMapper.java/left.java
     Item getItemById(String id);
     Product getProductById(String id);
     void insertReview(Review review);
     void insertReviewRating(ReviewRating rr);
+||||||| /usr/src/app/output/mybatis/jpetstore-6/b26b0c87017c0f2ec431ee0a1425910b9a9fff00/src/main/java/org/mybatis/jpetstore/mapper/ReviewMapper.java/base.java
+=======
     List<Review> getReivewListByProductId(String productId);
+>>>>>>> /usr/src/app/output/mybatis/jpetstore-6/b26b0c87017c0f2ec431ee0a1425910b9a9fff00/src/main/java/org/mybatis/jpetstore/mapper/ReviewMapper.java/right.java
 }
