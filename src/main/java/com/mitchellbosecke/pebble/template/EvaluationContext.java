aaa8e3e3231df885b0e6907e326a7f247e4cc95c
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 08:12:46 +0000

    Mining Framework Analysis

--- src/main/java/com/mitchellbosecke/pebble/template/EvaluationContext.java
+++ src/main/java/com/mitchellbosecke/pebble/template/EvaluationContext.java
@@ -1,13 +1,12 @@
 package com.mitchellbosecke.pebble.template;
-
+import com.github.benmanes.caffeine.cache.Cache;
 import java.util.Locale;
 
 /**
  * Created by mitchell on 2016-11-13.
  */
 public interface EvaluationContext {
+  boolean isStrictVariables();
 
-    boolean isStrictVariables();
-
-    Locale getLocale();
+  Locale getLocale();
 }
