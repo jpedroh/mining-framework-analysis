94dec3a6eaced456a214a9187d92aff5429db2b3
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 13:55:28 +0000

    Mining Framework Analysis

--- server/src/main/java/xdi2/messaging/target/interceptor/impl/authentication/signature/DiscoverySignatureAuthenticator.java
+++ server/src/main/java/xdi2/messaging/target/interceptor/impl/authentication/signature/DiscoverySignatureAuthenticator.java
@@ -1,10 +1,7 @@
 package xdi2.messaging.target.interceptor.impl.authentication.signature;
-
 import java.security.PublicKey;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import xdi2.client.exceptions.Xdi2ClientException;
 import xdi2.core.xri3.XDI3Segment;
 import xdi2.discovery.XDIDiscoveryClient;
@@ -16,59 +13,46 @@ import xdi2.messaging.Message;
  * public keys using XDI discovery.
  */
 public class DiscoverySignatureAuthenticator extends PublicKeySignatureAuthenticator {
+  private static Logger log = LoggerFactory.getLogger(DiscoverySignatureAuthenticator.class.getName());
 
-	private static Logger log = LoggerFactory.getLogger(DiscoverySignatureAuthenticator.class.getName());
+  public static final XDIDiscoveryClient DEFAULT_DISCOVERY_CLIENT = new XDIDiscoveryClient();
 
-	public static final XDIDiscoveryClient DEFAULT_DISCOVERY_CLIENT = new XDIDiscoveryClient();
+  private XDIDiscoveryClient xdiDiscoveryClient;
 
-	private XDIDiscoveryClient xdiDiscoveryClient;
+  public DiscoverySignatureAuthenticator(XDIDiscoveryClient xdiDiscoveryClient) {
+    super();
+    this.xdiDiscoveryClient = xdiDiscoveryClient;
+  }
 
-	public DiscoverySignatureAuthenticator(XDIDiscoveryClient xdiDiscoveryClient) {
+  public DiscoverySignatureAuthenticator() {
+    this(DEFAULT_DISCOVERY_CLIENT);
+  }
 
-		super();
+  @Override public PublicKey getPublicKey(Message message) {
+    XDI3Segment senderXri = message.getSenderXri();
+    if (senderXri == null) {
+      return null;
+    }
+    PublicKey publicKey = null;
+    try {
+      XDIDiscoveryResult xdiDiscoveryResult = this.getXdiDiscoveryClient().discover(senderXri);
+      if (xdiDiscoveryResult != null) {
+        publicKey = xdiDiscoveryResult.getSignaturePublicKey();
+      }
+    } catch (Xdi2ClientException ex) {
+      if (log.isWarnEnabled()) {
+        log.warn("Cannot discover public key for " + senderXri + ": " + ex.getMessage(), ex);
+      }
+      return null;
+    }
+    return publicKey;
+  }
 
-		this.xdiDiscoveryClient = xdiDiscoveryClient;
-	}
+  public XDIDiscoveryClient getXdiDiscoveryClient() {
+    return this.xdiDiscoveryClient;
+  }
 
-	public DiscoverySignatureAuthenticator() {
-
-		this(DEFAULT_DISCOVERY_CLIENT);
-	}
-
-	@Override
-	public PublicKey getPublicKey(Message message) {
-
-		XDI3Segment senderXri = message.getSenderXri();
-		if (senderXri == null) return null;
-
-		// perform discovery
-
-		PublicKey publicKey = null;
-
-		try {
-
-			XDIDiscoveryResult xdiDiscoveryResult = this.getXdiDiscoveryClient().discover(senderXri);
-
-			if (xdiDiscoveryResult != null) publicKey = xdiDiscoveryResult.getSignaturePublicKey();
-		} catch (Xdi2ClientException ex) {
-
-			if (log.isWarnEnabled()) log.warn("Cannot discover public key for " + senderXri + ": " + ex.getMessage(), ex);
-
-			return null;
-		}
-
-		// done
-
-		return publicKey;
-	}
-
-	public XDIDiscoveryClient getXdiDiscoveryClient() {
-
-		return this.xdiDiscoveryClient;
-	}
-
-	public void setXdiDiscoveryClient(XDIDiscoveryClient xdiDiscoveryClient) {
-
-		this.xdiDiscoveryClient = xdiDiscoveryClient;
-	}
+  public void setXdiDiscoveryClient(XDIDiscoveryClient xdiDiscoveryClient) {
+    this.xdiDiscoveryClient = xdiDiscoveryClient;
+  }
 }
