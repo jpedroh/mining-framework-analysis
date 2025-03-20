51d3629a30d289015243b0b15341b4948a8e6789
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 05:36:58 +0000

    Mining Framework Analysis

--- src/main/java/com/jcabi/jdbc/PrepareArgs.java
+++ src/main/java/com/jcabi/jdbc/PrepareArgs.java
@@ -1,34 +1,4 @@
-/**
- * Copyright (c) 2012-2015, jcabi.com
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met: 1) Redistributions of source code must retain the above
- * copyright notice, this list of conditions and the following
- * disclaimer. 2) Redistributions in binary form must reproduce the above
- * copyright notice, this list of conditions and the following
- * disclaimer in the documentation and/or other materials provided
- * with the distribution. 3) Neither the name of the jcabi.com nor
- * the names of its contributors may be used to endorse or promote
- * products derived from this software without specific prior written
- * permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
- * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
- * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
- * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- * OF THE POSSIBILITY OF SUCH DAMAGE.
- */
 package com.jcabi.jdbc;
-
 import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -43,42 +13,58 @@ import java.util.Collections;
  * @since 0.13
  */
 final class PrepareArgs implements Preparation {
-
-    /**
+  /**
      * Arguments.
      */
-    private final transient Collection<Object> args;
+  private final transient Collection<Object> args;
 
-    /**
+  /**
      * Ctor.
      * @param arguments Arguments
      */
-    PrepareArgs(final Collection<Object> arguments) {
-        this.args = Collections.unmodifiableCollection(arguments);
-    }
+  PrepareArgs(final Collection<Object> arguments) {
+    this.args = Collections.unmodifiableCollection(arguments);
+  }
 
-    @Override
-    public void prepare(final PreparedStatement stmt) throws SQLException {
-        int pos = 1;
-        for (final Object arg : this.args) {
-            if (arg == null) {
-                stmt.setString(pos, null);
-            } else if (arg instanceof Long) {
-                stmt.setLong(pos, Long.class.cast(arg));
-            } else if (arg instanceof Boolean) {
-                stmt.setBoolean(pos, Boolean.class.cast(arg));
-            } else if (arg instanceof Date) {
-                stmt.setDate(pos, Date.class.cast(arg));
-            } else if (arg instanceof Integer) {
-                stmt.setInt(pos, Integer.class.cast(arg));
-            } else if (arg instanceof Utc) {
-                Utc.class.cast(arg).setTimestamp(stmt, pos);
-            } else if (arg instanceof byte[]) {
-                stmt.setBytes(pos, byte[].class.cast(arg));
+  @Override public void prepare(final PreparedStatement stmt) throws SQLException {
+    int pos = 1;
+    for (final Object arg : this.args) {
+      if (arg == null) {
+        stmt.setString(pos, null);
+      } else {
+        if (arg instanceof Long) {
+          stmt.setLong(pos, Long.class.cast(arg));
+        } else {
+          if (arg instanceof Boolean) {
+            stmt.setBoolean(pos, Boolean.class.cast(arg));
+          } else {
+            if (arg instanceof Date) {
+              stmt.setDate(pos, Date.class.cast(arg));
             } else {
-                stmt.setString(pos, arg.toString());
+              if (arg instanceof Integer) {
+                stmt.setInt(pos, Integer.class.cast(arg));
+              } else {
+                if (arg instanceof Utc) {
+                  Utc.class.cast(arg).setTimestamp(stmt, pos);
+                } else {
+                  if (arg instanceof byte[]) {
+                    stmt.setBytes(pos, 
+<<<<<<< /usr/src/app/output/jcabi/jcabi-jdbc/40e95d792465ff63e698273a475a8b256cf11a5b/src/main/java/com/jcabi/jdbc/PrepareArgs.java/left.java
+                    byte[].class.cast(arg)
+=======
+                    (byte[]) arg
+>>>>>>> /usr/src/app/output/jcabi/jcabi-jdbc/40e95d792465ff63e698273a475a8b256cf11a5b/src/main/java/com/jcabi/jdbc/PrepareArgs.java/right.java
+                    );
+                  } else {
+                    stmt.setString(pos, arg.toString());
+                  }
+                }
+              }
             }
-            ++pos;
+          }
         }
+      }
+      ++pos;
     }
+  }
 }
--- src/main/java/com/jcabi/jdbc/SingleOutcome.java
+++ src/main/java/com/jcabi/jdbc/SingleOutcome.java
@@ -1,34 +1,4 @@
-/**
- * Copyright (c) 2012-2015, jcabi.com
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met: 1) Redistributions of source code must retain the above
- * copyright notice, this list of conditions and the following
- * disclaimer. 2) Redistributions in binary form must reproduce the above
- * copyright notice, this list of conditions and the following
- * disclaimer in the documentation and/or other materials provided
- * with the distribution. 3) Neither the name of the jcabi.com nor
- * the names of its contributors may be used to endorse or promote
- * products derived from this software without specific prior written
- * permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
- * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
- * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
- * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- * OF THE POSSIBILITY OF SUCH DAMAGE.
- */
 package com.jcabi.jdbc;
-
 import com.jcabi.aspects.Immutable;
 import com.jcabi.aspects.Loggable;
 import java.sql.ResultSet;
@@ -69,103 +39,100 @@ import lombok.ToString;
  * @version $Id$
  * @since 0.1.8
  */
-@Immutable
-@ToString
-@EqualsAndHashCode(of = { "type", "silently" })
-public final class SingleOutcome<T> implements Outcome<T> {
-
-    /**
+@Immutable @ToString @EqualsAndHashCode(of = { "type", "silently" }) public final class SingleOutcome<T extends java.lang.Object> implements Outcome<T> {
+  /**
      * The type name.
      */
-    private final transient String type;
+  private final transient String type;
 
-    /**
+  /**
      * Silently return NULL if no row found.
      */
-    private final transient boolean silently;
+  private final transient boolean silently;
 
-    /**
+  /**
      * Public ctor.
      * @param tpe The type to convert to
      */
-    public SingleOutcome(
-        @NotNull(message = "type of result can't be NULL") final Class<T> tpe) {
-        this(tpe, false);
-    }
+  public SingleOutcome(@NotNull(message = "type of result can\'t be NULL") final Class<T> tpe) {
+    this(tpe, false);
+  }
 
-    /**
+  /**
      * Public ctor.
      * @param tpe The type to convert to
      * @param slnt Silently return NULL if there is no row
      */
-    public SingleOutcome(
-        @NotNull(message = "type can't be NULL") final Class<T> tpe,
-        final boolean slnt) {
-        //@checkstyle BooleanExpressionComplexity (3 lines)
-        if (tpe.equals(String.class) || tpe.equals(Long.class)
-            || tpe.equals(Boolean.class) || tpe.equals(Byte.class)
-            || tpe.equals(Date.class) || tpe.equals(Utc.class)
-            || tpe.equals(byte[].class)
-        ) {
-            this.type = tpe.getName();
-        } else {
-            throw new IllegalArgumentException(
-                String.format("type %s is not supported", tpe.getName())
-            );
-        }
-        this.silently = slnt;
+  public SingleOutcome(@NotNull(message = "type can\'t be NULL") final Class<T> tpe, final boolean slnt) {
+    if (tpe.equals(String.class) || tpe.equals(Long.class) || tpe.equals(Boolean.class) || tpe.equals(Byte.class) || tpe.equals(Date.class) || tpe.equals(Utc.class) || tpe.equals(byte[].class)) {
+      this.type = tpe.getName();
+    } else {
+      throw new IllegalArgumentException(String.format("type %s is not supported", tpe.getName()));
     }
+    this.silently = slnt;
+  }
 
-    @Override
-    @Loggable(Loggable.DEBUG)
-    public T handle(final ResultSet rset, final Statement stmt)
-        throws SQLException {
-        T result = null;
-        if (rset.next()) {
-            result = this.fetch(rset);
-        } else if (!this.silently) {
-            throw new SQLException("no records found");
-        }
-        return result;
+  @Override @Loggable(value = Loggable.DEBUG) public T handle(final ResultSet rset, final Statement stmt) throws SQLException {
+    T result = null;
+    if (rset.next()) {
+      result = this.fetch(rset);
+    } else {
+      if (!this.silently) {
+        throw new SQLException("no records found");
+      }
     }
+    return result;
+  }
 
-    /**
+  /**
      * Fetch the value from result set.
      * @param rset Result set
      * @return The result
      * @throws SQLException If some error inside
      */
-    @SuppressWarnings("unchecked")
-    private T fetch(final ResultSet rset) throws SQLException {
-        final Object result;
-        Class<T> tpe;
-        try {
-            tpe = (Class<T>) Class.forName(this.type);
-            if (tpe.equals(String.class)) {
-                result = rset.getString(1);
-            } else if (tpe.equals(Long.class)) {
-                result = rset.getLong(1);
-            } else if (tpe.equals(Boolean.class)) {
-                result = rset.getBoolean(1);
-            } else if (tpe.equals(Byte.class)) {
-                result = rset.getByte(1);
-            } else if (tpe.equals(Date.class)) {
-                result = rset.getDate(1);
-            } else if (tpe.equals(Utc.class)) {
-                result = new Utc(Utc.getTimestamp(rset, 1));
-            } else if (byte[].class.equals(tpe)) {
-                result = rset.getBytes(1);
+  @SuppressWarnings(value = { "unchecked" }) private T fetch(final ResultSet rset) throws SQLException {
+    final Object result;
+    Class<T> tpe;
+    try {
+      tpe = (Class<T>) Class.forName(this.type);
+      if (tpe.equals(String.class)) {
+        result = rset.getString(1);
+      } else {
+        if (tpe.equals(Long.class)) {
+          result = rset.getLong(1);
+        } else {
+          if (tpe.equals(Boolean.class)) {
+            result = rset.getBoolean(1);
+          } else {
+            if (tpe.equals(Byte.class)) {
+              result = rset.getByte(1);
             } else {
-                throw new IllegalStateException(
-                    String.format("type %s is not allowed", tpe.getName())
-                );
+              if (tpe.equals(Date.class)) {
+                result = rset.getDate(1);
+              } else {
+                if (tpe.equals(Utc.class)) {
+                  result = new Utc(Utc.getTimestamp(rset, 1));
+                } else {
+                  if (
+<<<<<<< /usr/src/app/output/jcabi/jcabi-jdbc/40e95d792465ff63e698273a475a8b256cf11a5b/src/main/java/com/jcabi/jdbc/SingleOutcome.java/left.java
+                  byte[].class.equals(tpe)
+=======
+                  tpe.equals(byte[].class)
+>>>>>>> /usr/src/app/output/jcabi/jcabi-jdbc/40e95d792465ff63e698273a475a8b256cf11a5b/src/main/java/com/jcabi/jdbc/SingleOutcome.java/right.java
+                  ) {
+                    result = rset.getBytes(1);
+                  } else {
+                    throw new IllegalStateException(String.format("type %s is not allowed", tpe.getName()));
+                  }
+                }
+              }
             }
-        } catch (final ClassNotFoundException ex) {
-            throw new IllegalArgumentException(
-                String.format("Unknown type: %s", this.type), ex
-            );
+          }
         }
-        return tpe.cast(result);
+      }
+    } catch (final ClassNotFoundException ex) {
+      throw new IllegalArgumentException(String.format("Unknown type: %s", this.type), ex);
     }
-
+    return tpe.cast(result);
+  }
 }
