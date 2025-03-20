1ce08c53f9eb98ee46ed603da971d0f33102f69e
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 02:49:34 +0000

    Mining Framework Analysis

--- gdx-pneumatic-man/src/main/java/de/hochschuletrier/gdw/ws1415/game/ComponentMappers.java
+++ gdx-pneumatic-man/src/main/java/de/hochschuletrier/gdw/ws1415/game/ComponentMappers.java
@@ -1,18 +1,28 @@
 package de.hochschuletrier.gdw.ws1415.game;
-
 import com.badlogic.ashley.core.ComponentMapper;
 import de.hochschuletrier.gdw.commons.gdx.physix.components.*;
 import de.hochschuletrier.gdw.ws1415.game.components.*;
 
 public class ComponentMappers {
-    public static final ComponentMapper<PositionComponent> position = ComponentMapper.getFor(PositionComponent.class);
-    public static final ComponentMapper<TriggerComponent> trigger = ComponentMapper.getFor(TriggerComponent.class);
-    public static final ComponentMapper<PhysixBodyComponent> physixBody = ComponentMapper.getFor(PhysixBodyComponent.class);
-    public static final ComponentMapper<PhysixModifierComponent> physixModifier = ComponentMapper.getFor(PhysixModifierComponent.class);
-    public static final ComponentMapper<ImpactSoundComponent> impactSound = ComponentMapper.getFor(ImpactSoundComponent.class);
-    public static final ComponentMapper<AnimationComponent> animation = ComponentMapper.getFor(AnimationComponent.class);
-    public static final ComponentMapper<MovementComponent> movement = ComponentMapper.getFor(MovementComponent.class);
-    public static final ComponentMapper<BouncingComponent> bouncing = ComponentMapper.getFor(BouncingComponent.class);
-    public static final ComponentMapper<KillsPlayerOnContactComponent> enemy = ComponentMapper.getFor(KillsPlayerOnContactComponent.class);
+  public static final ComponentMapper<PositionComponent> position = ComponentMapper.getFor(PositionComponent.class);
 
+  public static final ComponentMapper<TriggerComponent> trigger = ComponentMapper.getFor(TriggerComponent.class);
+
+  public static final ComponentMapper<PhysixBodyComponent> physixBody = ComponentMapper.getFor(PhysixBodyComponent.class);
+
+  public static final ComponentMapper<PhysixModifierComponent> physixModifier = ComponentMapper.getFor(PhysixModifierComponent.class);
+
+  public static final ComponentMapper<ImpactSoundComponent> impactSound = ComponentMapper.getFor(ImpactSoundComponent.class);
+
+  public static final ComponentMapper<AnimationComponent> animation = ComponentMapper.getFor(AnimationComponent.class);
+
+  public static final ComponentMapper<MovementComponent, KillsPlayerOnContactComponent> 
+<<<<<<< /usr/src/app/output/lusito/gamedevweek/98e1d9fc53d925c97b2c00438dd36aa657c39f70/gdx-pneumatic-man/src/main/java/de/hochschuletrier/gdw/ws1415/game/ComponentMappers.java/left.java
+  movement = ComponentMapper.getFor(MovementComponent.class)
+=======
+  enemy = ComponentMapper.getFor(KillsPlayerOnContactComponent.class)
+>>>>>>> /usr/src/app/output/lusito/gamedevweek/98e1d9fc53d925c97b2c00438dd36aa657c39f70/gdx-pneumatic-man/src/main/java/de/hochschuletrier/gdw/ws1415/game/ComponentMappers.java/right.java
+  ;
+
+  public static final ComponentMapper<BouncingComponent> bouncing = ComponentMapper.getFor(BouncingComponent.class);
 }
--- gdx-pneumatic-man/src/main/java/de/hochschuletrier/gdw/ws1415/game/Game.java
+++ gdx-pneumatic-man/src/main/java/de/hochschuletrier/gdw/ws1415/game/Game.java
@@ -1,5 +1,4 @@
 package de.hochschuletrier.gdw.ws1415.game;
-
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.PooledEngine;
 import com.badlogic.gdx.Input;
@@ -8,7 +7,6 @@ import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
-
 import de.hochschuletrier.gdw.commons.devcon.cvar.CVarBool;
 import de.hochschuletrier.gdw.commons.gdx.assets.AnimationExtended;
 import de.hochschuletrier.gdw.commons.gdx.assets.AssetManagerX;
@@ -30,139 +28,125 @@ import de.hochschuletrier.gdw.ws1415.game.systems.AnimationRenderSystem;
 import de.hochschuletrier.gdw.ws1415.game.systems.MovementSystem;
 import de.hochschuletrier.gdw.ws1415.game.systems.UpdatePositionSystem;
 import de.hochschuletrier.gdw.ws1415.game.utils.PhysixUtil;
-
 import java.util.function.Consumer;
 
 public class Game extends InputAdapter {
+  private final CVarBool physixDebug = new CVarBool("physix_debug", true, 0, "Draw physix debug");
 
-    private final CVarBool physixDebug = new CVarBool("physix_debug", true, 0, "Draw physix debug");
-    private final Hotkey togglePhysixDebug = new Hotkey(() -> physixDebug.toggle(false), Input.Keys.F1, HotkeyModifier.CTRL);
+  private final Hotkey togglePhysixDebug = new Hotkey(() -> physixDebug.toggle(false), Input.Keys.F1, HotkeyModifier.CTRL);
 
-    private final PooledEngine engine = new PooledEngine(
-            GameConstants.ENTITY_POOL_INITIAL_SIZE, GameConstants.ENTITY_POOL_MAX_SIZE,
-            GameConstants.COMPONENT_POOL_INITIAL_SIZE, GameConstants.COMPONENT_POOL_MAX_SIZE
-    );
+  private final PooledEngine engine = new PooledEngine(GameConstants.ENTITY_POOL_INITIAL_SIZE, GameConstants.ENTITY_POOL_MAX_SIZE, GameConstants.COMPONENT_POOL_INITIAL_SIZE, GameConstants.COMPONENT_POOL_MAX_SIZE);
 
-    private final PhysixSystem physixSystem = new PhysixSystem(GameConstants.BOX2D_SCALE,
-            GameConstants.VELOCITY_ITERATIONS, GameConstants.POSITION_ITERATIONS, GameConstants.PRIORITY_PHYSIX
-    );
-    private final PhysixDebugRenderSystem physixDebugRenderSystem = new PhysixDebugRenderSystem(GameConstants.PRIORITY_DEBUG_WORLD);
-    private final AnimationRenderSystem animationRenderSystem = new AnimationRenderSystem(GameConstants.PRIORITY_ANIMATIONS);
-    private final UpdatePositionSystem updatePositionSystem = new UpdatePositionSystem(GameConstants.PRIORITY_PHYSIX + 1);
-    private final MovementSystem movementSystem = new MovementSystem(GameConstants.PRIORITY_PHYSIX+2);
-    
-    private Sound impactSound;
-    private AnimationExtended ballAnimation;
+  private final PhysixSystem physixSystem = new PhysixSystem(GameConstants.BOX2D_SCALE, GameConstants.VELOCITY_ITERATIONS, GameConstants.POSITION_ITERATIONS, GameConstants.PRIORITY_PHYSIX);
 
-    public Game() {
-        // If this is a build jar file, disable hotkeys
-        if (!Main.IS_RELEASE) {
-            togglePhysixDebug.register();
-        }
+  private final PhysixDebugRenderSystem physixDebugRenderSystem = new PhysixDebugRenderSystem(GameConstants.PRIORITY_DEBUG_WORLD);
+
+  private final AnimationRenderSystem animationRenderSystem = new AnimationRenderSystem(GameConstants.PRIORITY_ANIMATIONS);
+
+  private final UpdatePositionSystem updatePositionSystem = new UpdatePositionSystem(GameConstants.PRIORITY_PHYSIX + 1);
+
+  private final MovementSystem movementSystem = new MovementSystem(GameConstants.PRIORITY_PHYSIX + 2);
+
+  private Sound impactSound;
+
+  private AnimationExtended ballAnimation;
+
+  public Game() {
+    if (!Main.IS_RELEASE) {
+      togglePhysixDebug.register();
     }
+  }
 
-    public void dispose() {
-        togglePhysixDebug.unregister();
-    }
+  public void dispose() {
+    togglePhysixDebug.unregister();
+  }
 
-    public void init(AssetManagerX assetManager) {
-        Main.getInstance().console.register(physixDebug);
-        physixDebug.addListener((CVar) -> physixDebugRenderSystem.setProcessing(physixDebug.get()));
+  public void init(AssetManagerX assetManager) {
+    Main.getInstance().console.register(physixDebug);
+    physixDebug.addListener((CVar) -> physixDebugRenderSystem.setProcessing(physixDebug.get()));
+    impactSound = assetManager.getSound("click");
+    ballAnimation = assetManager.getAnimation("ball");
+    addSystems();
+    addContactListeners();
+    setupPhysixWorld();
+    Main.inputMultiplexer.addProcessor(this);
+  }
 
-        impactSound = assetManager.getSound("click");
-        ballAnimation = assetManager.getAnimation("ball");
+  private void addSystems() {
+    engine.addSystem(physixSystem);
+    engine.addSystem(physixDebugRenderSystem);
+    engine.addSystem(animationRenderSystem);
+    engine.addSystem(updatePositionSystem);
+    engine.addSystem(movementSystem);
+  }
 
-        addSystems();
-        addContactListeners();
-        setupPhysixWorld();
+  private void addContactListeners() {
+    PhysixComponentAwareContactListener contactListener = new PhysixComponentAwareContactListener();
+    physixSystem.getWorld().setContactListener(contactListener);
+    contactListener.addListener(ImpactSoundComponent.class, new ImpactSoundListener());
+    contactListener.addListener(TriggerComponent.class, new TriggerListener());
+    contactListener.addListener(PlayerComponent.class, new PlayerContactListener());
+  }
 
-        Main.inputMultiplexer.addProcessor(this);
-    }
+  private void setupPhysixWorld() {
+    physixSystem.setGravity(0, 24);
+    PhysixBodyDef bodyDef = new PhysixBodyDef(BodyDef.BodyType.StaticBody, physixSystem).position(410, 500).fixedRotation(false);
+    Body body = physixSystem.getWorld().createBody(bodyDef);
+    body.createFixture(new PhysixFixtureDef(physixSystem).density(1).friction(0.5f).shapeBox(800, 20));
+    PhysixUtil.createHollowCircle(physixSystem, 180, 180, 150, 30, 6);
+    createTrigger(410, 600, 3200, 40, (Entity entity) -> {
+      engine.removeEntity(entity);
+    });
+  }
 
-    private void addSystems() {
-        engine.addSystem(physixSystem);
-        engine.addSystem(physixDebugRenderSystem);
-        engine.addSystem(animationRenderSystem);
-        engine.addSystem(updatePositionSystem);
-        engine.addSystem(movementSystem);
-    }
+  public void update(float delta) {
+    Main.getInstance().screenCamera.bind();
+    engine.update(delta);
+  }
 
-    private void addContactListeners() {
-        PhysixComponentAwareContactListener contactListener = new PhysixComponentAwareContactListener();
-        physixSystem.getWorld().setContactListener(contactListener);
-        contactListener.addListener(ImpactSoundComponent.class, new ImpactSoundListener());
-        contactListener.addListener(TriggerComponent.class, new TriggerListener());
-        contactListener.addListener(PlayerComponent.class, new PlayerContactListener());
-    }
+  public void createTrigger(float x, float y, float width, float height, Consumer<Entity> consumer) {
+    Entity entity = engine.createEntity();
+    PhysixModifierComponent modifyComponent = engine.createComponent(PhysixModifierComponent.class);
+    entity.add(modifyComponent);
+    TriggerComponent triggerComponent = engine.createComponent(TriggerComponent.class);
+    triggerComponent.consumer = consumer;
+    entity.add(triggerComponent);
+    modifyComponent.schedule(() -> {
+      PhysixBodyComponent bodyComponent = engine.createComponent(PhysixBodyComponent.class);
+      PhysixBodyDef bodyDef = new PhysixBodyDef(BodyType.StaticBody, physixSystem).position(x, y);
+      bodyComponent.init(bodyDef, physixSystem, entity);
+      PhysixFixtureDef fixtureDef = new PhysixFixtureDef(physixSystem).sensor(true).shapeBox(width, height);
+      bodyComponent.createFixture(fixtureDef);
+      entity.add(bodyComponent);
+    });
+    engine.addEntity(entity);
+  }
 
-    private void setupPhysixWorld() {
-        physixSystem.setGravity(0, 24);
-        PhysixBodyDef bodyDef = new PhysixBodyDef(BodyDef.BodyType.StaticBody, physixSystem).position(410, 500).fixedRotation(false);
-        Body body = physixSystem.getWorld().createBody(bodyDef);
-        body.createFixture(new PhysixFixtureDef(physixSystem).density(1).friction(0.5f).shapeBox(800, 20));
-        PhysixUtil.createHollowCircle(physixSystem, 180, 180, 150, 30, 6);
+  public void createBall(float x, float y, float radius) {
+    Entity entity = engine.createEntity();
+    entity.add(engine.createComponent(PositionComponent.class));
+    PhysixModifierComponent modifyComponent = engine.createComponent(PhysixModifierComponent.class);
+    entity.add(modifyComponent);
+    ImpactSoundComponent soundComponent = engine.createComponent(ImpactSoundComponent.class);
+    soundComponent.init(impactSound, 20, 20, 100);
+    entity.add(soundComponent);
+    AnimationComponent animComponent = engine.createComponent(AnimationComponent.class);
+    animComponent.animation = ballAnimation;
+    entity.add(animComponent);
+    modifyComponent.schedule(() -> {
+      PhysixBodyComponent bodyComponent = engine.createComponent(PhysixBodyComponent.class);
+      PhysixBodyDef bodyDef = new PhysixBodyDef(BodyType.DynamicBody, physixSystem).position(x, y).fixedRotation(false);
+      bodyComponent.init(bodyDef, physixSystem, entity);
+      PhysixFixtureDef fixtureDef = new PhysixFixtureDef(physixSystem).density(5).friction(0.2f).restitution(0.4f).shapeCircle(radius);
+      bodyComponent.createFixture(fixtureDef);
+      entity.add(bodyComponent);
+      bodyComponent.applyImpulse(0, 50000);
+    });
+    engine.addEntity(entity);
+  }
 
-        createTrigger(410, 600, 3200, 40, (Entity entity) -> {
-            engine.removeEntity(entity);
-        });
-    }
-
-    public void update(float delta) {
-        Main.getInstance().screenCamera.bind();
-        engine.update(delta);
-    }
-
-    public void createTrigger(float x, float y, float width, float height, Consumer<Entity> consumer) {
-        Entity entity = engine.createEntity();
-        PhysixModifierComponent modifyComponent = engine.createComponent(PhysixModifierComponent.class);
-        entity.add(modifyComponent);
-
-        TriggerComponent triggerComponent = engine.createComponent(TriggerComponent.class);
-        triggerComponent.consumer = consumer;
-        entity.add(triggerComponent);
-
-        modifyComponent.schedule(() -> {
-            PhysixBodyComponent bodyComponent = engine.createComponent(PhysixBodyComponent.class);
-            PhysixBodyDef bodyDef = new PhysixBodyDef(BodyType.StaticBody, physixSystem).position(x, y);
-            bodyComponent.init(bodyDef, physixSystem, entity);
-            PhysixFixtureDef fixtureDef = new PhysixFixtureDef(physixSystem).sensor(true).shapeBox(width, height);
-            bodyComponent.createFixture(fixtureDef);
-            entity.add(bodyComponent);
-        });
-        engine.addEntity(entity);
-    }
-
-    public void createBall(float x, float y, float radius) {
-        Entity entity = engine.createEntity();
-        entity.add(engine.createComponent(PositionComponent.class));
-        PhysixModifierComponent modifyComponent = engine.createComponent(PhysixModifierComponent.class);
-        entity.add(modifyComponent);
-
-        ImpactSoundComponent soundComponent = engine.createComponent(ImpactSoundComponent.class);
-        soundComponent.init(impactSound, 20, 20, 100);
-        entity.add(soundComponent);
-
-        AnimationComponent animComponent = engine.createComponent(AnimationComponent.class);
-        animComponent.animation = ballAnimation;
-        entity.add(animComponent);
-
-        modifyComponent.schedule(() -> {
-            PhysixBodyComponent bodyComponent = engine.createComponent(PhysixBodyComponent.class);
-            PhysixBodyDef bodyDef = new PhysixBodyDef(BodyType.DynamicBody, physixSystem)
-                    .position(x, y).fixedRotation(false);
-            bodyComponent.init(bodyDef, physixSystem, entity);
-            PhysixFixtureDef fixtureDef = new PhysixFixtureDef(physixSystem)
-                    .density(5).friction(0.2f).restitution(0.4f).shapeCircle(radius);
-            bodyComponent.createFixture(fixtureDef);
-            entity.add(bodyComponent);
-            bodyComponent.applyImpulse(0, 50000);
-        });
-        engine.addEntity(entity);
-    }
-
-    @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        createBall(screenX, screenY, 30);
-        return true;
-    }
+  @Override public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+    createBall(screenX, screenY, 30);
+    return true;
+  }
 }
