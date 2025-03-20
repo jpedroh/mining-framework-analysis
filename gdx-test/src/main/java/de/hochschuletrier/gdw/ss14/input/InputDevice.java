fda95670e53a2fac2d07d795ddc77391919f3842
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 00:13:49 +0000

    Mining Framework Analysis

--- gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/input/InputDevice.java
+++ gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/input/InputDevice.java
@@ -1,77 +1,90 @@
 package de.hochschuletrier.gdw.ss14.input;
-
+import java.util.HashMap;
 import java.util.LinkedList;
 
 public abstract class InputDevice {
+  public static enum DeviceType {
+    MOUSE("mouse"),
+    KEYBOARD("keyboard"),
+    GAMEPAD("gamepad")
+    ;
 
-    public static enum DeviceType {
-        MOUSE("mouse"),
-        KEYBOARD("keyboard"),
-        GAMEPAD("gamepad");
-        
-        private String deviceName;
+    private String deviceName;
 
-        private DeviceType(String deviceName) {
-            this.deviceName = deviceName;
-        }
-        
-        @Override
-        public String toString() {
-            return deviceName; 
-        }
-    };
-    
-	protected LinkedList<GameInputAdapter> listener = new LinkedList<>(); 
-	
-    /**
+    private DeviceType(String deviceName) {
+      this.deviceName = deviceName;
+    }
+
+    @Override public String toString() {
+      return deviceName;
+    }
+  }
+
+
+
+  private protected 
+<<<<<<< /usr/src/app/output/lusito/gamedevweek/d60d1e1f9f4b6bbd93fac52529eb2210b68b8634/gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/input/InputDevice.java/left.java
+  HashMap
+=======
+  LinkedList
+>>>>>>> /usr/src/app/output/lusito/gamedevweek/d60d1e1f9f4b6bbd93fac52529eb2210b68b8634/gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/input/InputDevice.java/right.java
+  <String, GameInputAdapter, InputAction> 
+<<<<<<< /usr/src/app/output/lusito/gamedevweek/d60d1e1f9f4b6bbd93fac52529eb2210b68b8634/gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/input/InputDevice.java/left.java
+  buttonMap = new HashMap<>()
+=======
+  listener = new LinkedList<>()
+>>>>>>> /usr/src/app/output/lusito/gamedevweek/d60d1e1f9f4b6bbd93fac52529eb2210b68b8634/gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/input/InputDevice.java/right.java
+  ;
+
+  /**
      * 
      * @param gia @ GameInputAdapter to the Listener
      */
-    public void addGameInputAdapter(GameInputAdapter gia) {
-    	listener.add(gia);
-    }
-    
-    /**
+  public void addGameInputAdapter(GameInputAdapter gia) {
+    listener.add(gia);
+  }
+
+  /**
      * 
      * @param gia remove GameInputAdapter gia from the listener
      */
-    public void removeGameInputAdapter(GameInputAdapter gia) {
-    	listener.remove(gia);
-	}
-    
-    /**
+  public void removeGameInputAdapter(GameInputAdapter gia) {
+    listener.remove(gia);
+  }
+
+  /**
 	 *  Laser on / off
 	 */
-	protected void fireLaserButtonPressed() {	
-		for(GameInputAdapter inp: listener) {
-			inp.laserButtonPressed();
-		}
-	}
-	
-	/**
+  protected void fireLaserButtonPressed() {
+    for (GameInputAdapter inp : listener) {
+      inp.laserButtonPressed();
+    }
+  }
+
+  /**
 	 * water pistol on
 	 */
-	protected void fireWaterPistolButtonDown() {
-		for(GameInputAdapter inp: listener) {
-			inp.waterPistolButtonDown();
-		}
-	}
-	
-	/**
+  protected void fireWaterPistolButtonDown() {
+    for (GameInputAdapter inp : listener) {
+      inp.waterPistolButtonDown();
+    }
+  }
+
+  /**
 	 * water pistol off
 	 */
-	protected void fireWaterPistolButtonUp() {
-		for(GameInputAdapter inp: listener) {
-			inp.waterPistolButtonUp();
-		}
-	}
-	
-	/**
+  protected void fireWaterPistolButtonUp() {
+    for (GameInputAdapter inp : listener) {
+      inp.waterPistolButtonUp();
+    }
+  }
+
+  /**
 	 * go to the menu or back
 	 */
-	protected void fireMenuButtonPressed() {
-		for(GameInputAdapter inp: listener) {
-			inp.menueButtonPressed();
-		}
-	}
+  protected void fireMenuButtonPressed() {
+    for (GameInputAdapter inp : listener) {
+      inp.menueButtonPressed();
+    }
+  }
 }
--- gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/input/InputManager.java
+++ gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/input/InputManager.java
@@ -1,53 +1,76 @@
 package de.hochschuletrier.gdw.ss14.input;
 
+<<<<<<< /usr/src/app/output/lusito/gamedevweek/d60d1e1f9f4b6bbd93fac52529eb2210b68b8634/gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/input/InputManager.java/left.java
 import org.slf4j.Logger;
+
+=======
+>>>>>>> Unknown file: This is a bug in JDime.
+
+<<<<<<< /usr/src/app/output/lusito/gamedevweek/d60d1e1f9f4b6bbd93fac52529eb2210b68b8634/gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/input/InputManager.java/left.java
 import org.slf4j.LoggerFactory;
 
+=======
+>>>>>>> Unknown file: This is a bug in JDime.
+
+<<<<<<< /usr/src/app/output/lusito/gamedevweek/d60d1e1f9f4b6bbd93fac52529eb2210b68b8634/gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/input/InputManager.java/left.java
+import de.hochschuletrier.gdw.ss14.game.GameSettings;
+
+=======
+>>>>>>> Unknown file: This is a bug in JDime.
+
+<<<<<<< /usr/src/app/output/lusito/gamedevweek/d60d1e1f9f4b6bbd93fac52529eb2210b68b8634/gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/input/InputManager.java/left.java
+import de.hochschuletrier.gdw.ss14.sandbox.inputTest.GameInputAdapter;
+
+=======
+>>>>>>> Unknown file: This is a bug in JDime.
+
+<<<<<<< /usr/src/app/output/lusito/gamedevweek/d60d1e1f9f4b6bbd93fac52529eb2210b68b8634/gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/input/InputManager.java/left.java
+import de.hochschuletrier.gdw.ss14.sandbox.inputTest.GeneralInputAdapter;
+
+=======
+>>>>>>> Unknown file: This is a bug in JDime.
+
 public class InputManager {
-    private static final Logger logger = LoggerFactory.getLogger(InputManager.class);
-    private static InputManager instance;
-    
-    public static InputManager getInstance () {
-        if (instance == null) {
-            logger.error("InputManager not initialized!");
-        }
-        return instance;
+  private static final Logger logger = LoggerFactory.getLogger(InputManager.class);
+
+  private static InputManager instance;
+
+  public static InputManager getInstance() {
+    if (instance == null) {
+      logger.error("InputManager not initialized!");
     }
-    
-    private InputManager(){
-        
-        /*
-        switch (GameSettings.getInstance().getInputDevice()) {
-        case MOUSE:
-            break;
-        case KEYBOARD:
-            break;
-        case GAMEPAD:
-            break;
-        }
-        */
+    return instance;
+  }
+
+  private InputManager() {
+    switch (GameSettings.getInstance().getInputDevice()) {
+      case MOUSE:
+      break;
+      case KEYBOARD:
+      break;
+      case GAMEPAD:
+      break;
     }
-    
-    private InputDevice inputDevice = new InputMouse();
-    
-    public void addGameInputAdapter(GameInputAdapter gia) {
-        inputDevice.addGameInputAdapter(gia);
+  }
+
+  private InputDevice inputDevice = new InputMouse();
+
+  public void addGameInputAdapter(GameInputAdapter gia) {
+    inputDevice.addGameInputAdapter(gia);
+  }
+
+  public void removeGameInputAdapter(GameInputAdapter gia) {
+    inputDevice.addGameInputAdapter(gia);
+  }
+
+  public void update() {
+  }
+
+  public static void init() {
+    if (instance != null) {
+      logger.info("InputManager already initialized!");
+      return;
     }
-    
-    public void removeGameInputAdapter(GameInputAdapter gia) {
-        inputDevice.addGameInputAdapter(gia);
-    }
-    
-    public void update() {
-        
-    }
-    
-    public static void init() {
-        if (instance != null) {
-            logger.info("InputManager already initialized!");
-            return;
-        }
-        instance = new InputManager();
-    }
-    
+    instance = new InputManager();
+  }
 }
