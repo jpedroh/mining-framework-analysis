82cb0b0f8ad27584f958f98d361a03a10ec4e0f7
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 08:50:37 +0000

    Mining Framework Analysis

--- source/de/tudresden/inf/ggp/basicplayer/MyRandomStrategy.java
+++ source/de/tudresden/inf/ggp/basicplayer/MyRandomStrategy.java
@@ -1,30 +1,20 @@
 package de.tudresden.inf.ggp.basicplayer;
-
 import java.util.List;
-
 import org.eclipse.palamedes.gdl.core.model.IGameNode;
 import org.eclipse.palamedes.gdl.core.model.IMove;
 import org.eclipse.palamedes.gdl.core.model.utils.GameNodeTree;
 import org.eclipse.palamedes.gdl.core.simulation.strategies.AbstractStrategy;
 
-
 public class MyRandomStrategy extends AbstractStrategy {
+  public GameNodeTree tree = new GameNodeTree();
 
-    public GameNodeTree tree = new GameNodeTree();
-
-    @Override
-    public IMove getMove(IGameNode currentNode) {
-    	/** XXX: All strategy relevant code goes in here. */
-    // TODO: MAKE RECURSIVE
-        try {
-            List<IMove[]> moves = match.getGame().getCombinedMoves(currentNode);
-            return moves.get( random.nextInt( moves.size() ) )[playerNumber];
-        }
-        catch (InterruptedException e) {
-            System.out.println("findNextMove() stopped by time.");
-        }
-        
-        return null;
+  @Override public IMove getMove(IGameNode currentNode) {
+    try {
+      List<IMove[]> moves = match.getGame().getCombinedMoves(currentNode);
+      return moves.get(random.nextInt(moves.size()))[playerNumber];
+    } catch (InterruptedException e) {
+      System.out.println("findNextMove() stopped by time.");
     }
-    
+    return null;
+  }
 }
