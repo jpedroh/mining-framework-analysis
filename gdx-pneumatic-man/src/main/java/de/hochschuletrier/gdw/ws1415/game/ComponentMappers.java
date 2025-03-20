55677bf2ac8e00a76293442fd00a59bdfdac6ed7
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 02:43:44 +0000

    Mining Framework Analysis

--- gdx-pneumatic-man/src/main/java/de/hochschuletrier/gdw/ws1415/game/ComponentMappers.java
+++ gdx-pneumatic-man/src/main/java/de/hochschuletrier/gdw/ws1415/game/ComponentMappers.java
@@ -1,19 +1,32 @@
 package de.hochschuletrier.gdw.ws1415.game;
-
 import com.badlogic.ashley.core.ComponentMapper;
 import de.hochschuletrier.gdw.commons.gdx.physix.components.*;
 import de.hochschuletrier.gdw.ws1415.game.components.*;
 
 public class ComponentMappers {
-    public static final ComponentMapper<PositionComponent> position = ComponentMapper.getFor(PositionComponent.class);
-    public static final ComponentMapper<TriggerComponent> trigger = ComponentMapper.getFor(TriggerComponent.class);
-    public static final ComponentMapper<PhysixBodyComponent> physixBody = ComponentMapper.getFor(PhysixBodyComponent.class);
-    public static final ComponentMapper<PhysixModifierComponent> physixModifier = ComponentMapper.getFor(PhysixModifierComponent.class);
-    public static final ComponentMapper<ImpactSoundComponent> impactSound = ComponentMapper.getFor(ImpactSoundComponent.class);
-    public static final ComponentMapper<AnimationComponent> animation = ComponentMapper.getFor(AnimationComponent.class);
-    public static final ComponentMapper<KillsPlayerOnContactComponent> enemy = ComponentMapper.getFor(KillsPlayerOnContactComponent.class);
-    public static final ComponentMapper<AIComponent> AI = ComponentMapper.getFor(AIComponent.class);
-    public static final ComponentMapper<HealthComponent> health = ComponentMapper.getFor(HealthComponent.class);
-    public static final ComponentMapper<BlockComponent> block = ComponentMapper.getFor(BlockComponent.class);
-    public static final ComponentMapper<LayerComponent> layer = ComponentMapper.getFor(LayerComponent.class);
+  public static final ComponentMapper<PositionComponent> position = ComponentMapper.getFor(PositionComponent.class);
+
+  public static final ComponentMapper<TriggerComponent> trigger = ComponentMapper.getFor(TriggerComponent.class);
+
+  public static final ComponentMapper<PhysixBodyComponent> physixBody = ComponentMapper.getFor(PhysixBodyComponent.class);
+
+  public static final ComponentMapper<PhysixModifierComponent> physixModifier = ComponentMapper.getFor(PhysixModifierComponent.class);
+
+  public static final ComponentMapper<ImpactSoundComponent> impactSound = ComponentMapper.getFor(ImpactSoundComponent.class);
+
+  public static final ComponentMapper<AnimationComponent> animation = ComponentMapper.getFor(AnimationComponent.class);
+
+  public static final ComponentMapper<KillsPlayerOnContactComponent> enemy = ComponentMapper.getFor(KillsPlayerOnContactComponent.class);
+
+  public static final ComponentMapper<AIComponent, HealthComponent> 
+<<<<<<< /usr/src/app/output/lusito/gamedevweek/d6b031e2569face0946671177f28d977474ef5c2/gdx-pneumatic-man/src/main/java/de/hochschuletrier/gdw/ws1415/game/ComponentMappers.java/left.java
+  AI = ComponentMapper.getFor(AIComponent.class)
+=======
+  health = ComponentMapper.getFor(HealthComponent.class)
+>>>>>>> /usr/src/app/output/lusito/gamedevweek/d6b031e2569face0946671177f28d977474ef5c2/gdx-pneumatic-man/src/main/java/de/hochschuletrier/gdw/ws1415/game/ComponentMappers.java/right.java
+  ;
+
+  public static final ComponentMapper<BlockComponent> block = ComponentMapper.getFor(BlockComponent.class);
+
+  public static final ComponentMapper<LayerComponent> layer = ComponentMapper.getFor(LayerComponent.class);
 }
