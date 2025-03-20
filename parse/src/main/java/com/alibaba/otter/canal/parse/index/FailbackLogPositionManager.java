bcb49ee32921f3708b39c975dbba7e0755071ee5
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 18:49:41 +0000

    Mining Framework Analysis

--- parse/src/main/java/com/alibaba/otter/canal/parse/index/FailbackLogPositionManager.java
+++ parse/src/main/java/com/alibaba/otter/canal/parse/index/FailbackLogPositionManager.java
@@ -1,9 +1,7 @@
 package com.alibaba.otter.canal.parse.index;
-
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.alibaba.otter.canal.parse.exception.CanalParseException;
+import org.slf4j.LoggerFactory;
 import com.alibaba.otter.canal.protocol.position.LogPosition;
 
 /**
@@ -15,69 +13,57 @@ import com.alibaba.otter.canal.protocol.position.LogPosition;
  * </pre>
  */
 public class FailbackLogPositionManager extends AbstractLogPositionManager {
+  private final static Logger logger = LoggerFactory.getLogger(FailbackLogPositionManager.class);
 
-    private final static Logger           logger = LoggerFactory.getLogger(FailbackLogPositionManager.class);
+  private final CanalLogPositionManager primary;
 
-    private final CanalLogPositionManager primary;
-    private final CanalLogPositionManager secondary;
+  private final CanalLogPositionManager secondary;
 
-    public FailbackLogPositionManager(CanalLogPositionManager primary, CanalLogPositionManager secondary){
-        if (primary == null) {
-            throw new NullPointerException("nul primary LogPositionManager");
-        }
-        if (secondary == null) {
-            throw new NullPointerException("nul secondary LogPositionManager");
-        }
-
-        this.primary = primary;
-        this.secondary = secondary;
+  public FailbackLogPositionManager(CanalLogPositionManager primary, CanalLogPositionManager secondary) {
+    if (primary == null) {
+      throw new NullPointerException("nul primary LogPositionManager");
     }
-
-    @Override
-    public void start() {
-        super.start();
-
-        if (!primary.isStart()) {
-            primary.start();
-        }
-
-        if (!secondary.isStart()) {
-            secondary.start();
-        }
+    if (secondary == null) {
+      throw new NullPointerException("nul secondary LogPositionManager");
     }
+    this.primary = primary;
+    this.secondary = secondary;
+  }
 
-    @Override
-    public void stop() {
-        super.stop();
-
-        if (secondary.isStart()) {
-            secondary.stop();
-        }
-
-        if (primary.isStart()) {
-            primary.stop();
-        }
+  @Override public void start() {
+    super.start();
+    if (!primary.isStart()) {
+      primary.start();
     }
-
-    @Override
-    public LogPosition getLatestIndexBy(String destination) {
-        LogPosition logPosition = primary.getLatestIndexBy(destination);
-        if (logPosition != null) {
-            return logPosition;
-        }
-        return secondary.getLatestIndexBy(destination);
+    if (!secondary.isStart()) {
+      secondary.start();
     }
+  }
 
-    @Override
-    public void persistLogPosition(String destination, LogPosition logPosition) throws CanalParseException {
-        try {
-            primary.persistLogPosition(destination, logPosition);
-        } catch (CanalParseException e) {
-            logger.warn("persistLogPosition use primary log position manager exception. destination: {}, logPosition: {}",
-                destination,
-                logPosition,
-                e);
-            secondary.persistLogPosition(destination, logPosition);
-        }
+  @Override public void stop() {
+    super.stop();
+    if (secondary.isStart()) {
+      secondary.stop();
     }
+    if (primary.isStart()) {
+      primary.stop();
+    }
+  }
+
+  @Override public LogPosition getLatestIndexBy(String destination) {
+    LogPosition logPosition = primary.getLatestIndexBy(destination);
+    if (logPosition != null) {
+      return logPosition;
+    }
+    return secondary.getLatestIndexBy(destination);
+  }
+
+  @Override public void persistLogPosition(String destination, LogPosition logPosition) throws CanalParseException {
+    try {
+      primary.persistLogPosition(destination, logPosition);
+    } catch (CanalParseException e) {
+      logger.warn("persistLogPosition use primary log position manager exception. destination: {}, logPosition: {}", destination, logPosition, e);
+      secondary.persistLogPosition(destination, logPosition);
+    }
+  }
 }
