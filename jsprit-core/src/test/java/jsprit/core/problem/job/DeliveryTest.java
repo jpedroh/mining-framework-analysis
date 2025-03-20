8d5a080f15ba0ce9668f668a45c9a7e6c5916b3a
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 13:46:03 +0000

    Mining Framework Analysis

--- jsprit-core/src/test/java/jsprit/core/problem/job/DeliveryTest.java
+++ jsprit-core/src/test/java/jsprit/core/problem/job/DeliveryTest.java
@@ -1,87 +1,48 @@
-/*******************************************************************************
- * Copyright (c) 2014 Stefan Schroeder.
- * 
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either 
- * version 3.0 of the License, or (at your option) any later version.
- *  
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public 
- * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
- * 
- * Contributors:
- *     Stefan Schroeder - initial API and implementation
- ******************************************************************************/
 package jsprit.core.problem.job;
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
 
 public class DeliveryTest {
-	
-	@Test(expected=IllegalStateException.class)
-	public void whenNeitherLocationIdNorCoordIsSet_itThrowsException(){
-		Delivery.Builder.newInstance("p").build();
-	}
-	
-	@Test
-	public void whenAddingTwoCapDimension_nuOfDimsShouldBeTwo(){
-		Delivery one = (Delivery)Delivery.Builder.newInstance("s").setLocationId("foofoo")
-				.addSizeDimension(0,2)
-				.addSizeDimension(1,4)
-				.build();
-		assertEquals(2,one.getSize().getNuOfDimensions());
-		assertEquals(2,one.getSize().get(0));
-		assertEquals(4,one.getSize().get(1));
-		
-	}
-	
-	@Test
-	public void whenPickupIsBuiltWithoutSpecifyingCapacity_itShouldHvCapWithOneDimAndDimValOfZero(){
-		Delivery one = (Delivery)Delivery.Builder.newInstance("s").setLocationId("foofoo")
-				.build();
-		assertEquals(1,one.getSize().getNuOfDimensions());
-		assertEquals(0,one.getSize().get(0));
-	}
-	
-	@Test
-	public void whenPickupIsBuiltWithConstructorWhereSizeIsSpecified_capacityShouldBeSetCorrectly(){
-		Delivery one = (Delivery)Delivery.Builder.newInstance("s").addSizeDimension(0, 1).setLocationId("foofoo")
-				.build();
-		assertEquals(1,one.getSize().getNuOfDimensions());
-		assertEquals(1,one.getSize().get(0));
-	}
-	
-	@Test
-	public void whenAddingSkills_theyShouldBeAddedCorrectly(){
-		Delivery s = (Delivery) Delivery.Builder.newInstance("s").setLocationId("loc")
-				.addSkill("drill").addSkill("screwdriver").build();
-		assertTrue(s.getRequiredSkills().containsSkill("drill"));
-		assertTrue(s.getRequiredSkills().containsSkill("ScrewDriver"));
-	}
-	
-	@Test
-	public void whenAddingSkillsCaseSens_theyShouldBeAddedCorrectly(){
-		Delivery s = (Delivery) Delivery.Builder.newInstance("s").setLocationId("loc")
-				.addSkill("DriLl").addSkill("screwDriver").build();
-		assertTrue(s.getRequiredSkills().containsSkill("drill"));
-		assertTrue(s.getRequiredSkills().containsSkill("drilL"));
-	}
+  @Test(expected = IllegalStateException.class) public void whenNeitherLocationIdNorCoordIsSet_itThrowsException() {
+    Delivery.Builder.newInstance("p").build();
+  }
 
-    @Test
-    public void whenAddingSkillsCaseSensV2_theyShouldBeAddedCorrectly(){
-        Delivery s = (Delivery) Delivery.Builder.newInstance("s").setLocationId("loc")
-                .addSkill("screwDriver").build();
-        assertFalse(s.getRequiredSkills().containsSkill("drill"));
-        assertFalse(s.getRequiredSkills().containsSkill("drilL"));
-    }
+  @Test public void whenAddingTwoCapDimension_nuOfDimsShouldBeTwo() {
+    Delivery one = (Delivery) Delivery.Builder.newInstance("s").setLocationId("foofoo").addSizeDimension(0, 2).addSizeDimension(1, 4).build();
+    assertEquals(2, one.getSize().getNuOfDimensions());
+    assertEquals(2, one.getSize().get(0));
+    assertEquals(4, one.getSize().get(1));
+  }
 
+  @Test public void whenPickupIsBuiltWithoutSpecifyingCapacity_itShouldHvCapWithOneDimAndDimValOfZero() {
+    Delivery one = (Delivery) Delivery.Builder.newInstance("s").setLocationId("foofoo").build();
+    assertEquals(1, one.getSize().getNuOfDimensions());
+    assertEquals(0, one.getSize().get(0));
+  }
 
+  @Test public void whenPickupIsBuiltWithConstructorWhereSizeIsSpecified_capacityShouldBeSetCorrectly() {
+    Delivery one = (Delivery) Delivery.Builder.newInstance("s").addSizeDimension(0, 1).setLocationId("foofoo").build();
+    assertEquals(1, one.getSize().getNuOfDimensions());
+    assertEquals(1, one.getSize().get(0));
+  }
+
+  @Test public void whenAddingSkills_theyShouldBeAddedCorrectly() {
+    Delivery s = (Delivery) Delivery.Builder.newInstance("s").setLocationId("loc").addSkill("drill").addSkill("screwdriver").build();
+    assertTrue(s.getRequiredSkills().containsSkill("drill"));
+    assertTrue(s.getRequiredSkills().containsSkill("ScrewDriver"));
+  }
+
+  @Test public void whenAddingSkillsCaseSens_theyShouldBeAddedCorrectly() {
+    Delivery s = (Delivery) Delivery.Builder.newInstance("s").setLocationId("loc").addSkill("DriLl").addSkill("screwDriver").build();
+    assertTrue(s.getRequiredSkills().containsSkill("drill"));
+    assertTrue(s.getRequiredSkills().containsSkill("drilL"));
+  }
+
+  @Test public void whenAddingSkillsCaseSensV2_theyShouldBeAddedCorrectly() {
+    Delivery s = (Delivery) Delivery.Builder.newInstance("s").setLocationId("loc").addSkill("screwDriver").build();
+    assertFalse(s.getRequiredSkills().containsSkill("drill"));
+    assertFalse(s.getRequiredSkills().containsSkill("drilL"));
+  }
 }
