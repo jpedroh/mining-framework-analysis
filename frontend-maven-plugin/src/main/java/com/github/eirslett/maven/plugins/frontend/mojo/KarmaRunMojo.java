e1ea79e96b4aaebaf4b8f4049beca745981d7337
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 05:13:37 +0000

    Mining Framework Analysis

--- frontend-maven-plugin/src/main/java/com/github/eirslett/maven/plugins/frontend/mojo/KarmaRunMojo.java
+++ frontend-maven-plugin/src/main/java/com/github/eirslett/maven/plugins/frontend/mojo/KarmaRunMojo.java
@@ -1,34 +1,26 @@
 package com.github.eirslett.maven.plugins.frontend.mojo;
-
 import com.github.eirslett.maven.plugins.frontend.lib.FrontendPluginFactory;
 import com.github.eirslett.maven.plugins.frontend.lib.TaskRunnerException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
-
-@Mojo(name="karma",  defaultPhase = LifecyclePhase.TEST)
-public final class KarmaRunMojo extends AbstractFrontendMojo {
-
-    /**
+@Mojo(name = "karma", defaultPhase = LifecyclePhase.TEST) public final class KarmaRunMojo extends AbstractFrontendMojo {
+  /**
      * Path to your karma configuration file, relative to the working directory (default is "karma.conf.js")
      */
-    @Parameter(defaultValue = "karma.conf.js", property = "karmaConfPath")
-    private String karmaConfPath;
+  @Parameter(defaultValue = "karma.conf.js", property = "karmaConfPath") private String karmaConfPath;
 
-    /**
+  /**
      * Skips execution of this mojo.
      */
-    @Parameter(property = "skip.karma", defaultValue = "false")
-    private Boolean skip;
+  @Parameter(property = "skip.karma", defaultValue = "false") private Boolean skip;
 
-    @Override
-    protected boolean skipExecution() {
-        return this.skip;
-    }
+  @Override protected boolean skipExecution() {
+    return this.skip;
+  }
 
-    @Override
-    public void execute(FrontendPluginFactory factory) throws TaskRunnerException {
-    	factory.getKarmaRunner().execute("start " + karmaConfPath, environmentVariables);
-    }
+  @Override public void execute(FrontendPluginFactory factory) throws TaskRunnerException {
+    factory.getKarmaRunner().execute("start " + karmaConfPath, environmentVariables);
+  }
 }
