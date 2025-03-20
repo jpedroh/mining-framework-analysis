f58d5771c8a2ae4c0802b4c0b90e8a76c7227766
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 06:31:35 +0000

    Mining Framework Analysis

--- src/main/java/com/github/steveice10/mc/protocol/data/game/entity/type/MobType.java
+++ src/main/java/com/github/steveice10/mc/protocol/data/game/entity/type/MobType.java
@@ -6,7 +6,12 @@ public enum MobType {
     ARROW,
     BAT,
     BLAZE,
+<<<<<<< /usr/src/app/output/steveice10/mcprotocollib/36e5eafd286fc1d6a554487a0a2e2f18032ece1a/src/main/java/com/github/steveice10/mc/protocol/data/game/entity/type/MobType.java/left.java
     CAT,
+||||||| /usr/src/app/output/steveice10/mcprotocollib/36e5eafd286fc1d6a554487a0a2e2f18032ece1a/src/main/java/com/github/steveice10/mc/protocol/data/game/entity/type/MobType.java/base.java
+=======
+    BOAT,
+>>>>>>> /usr/src/app/output/steveice10/mcprotocollib/36e5eafd286fc1d6a554487a0a2e2f18032ece1a/src/main/java/com/github/steveice10/mc/protocol/data/game/entity/type/MobType.java/right.java
     CAVE_SPIDER,
     CHICKEN,
     COD,
@@ -21,14 +26,34 @@ public enum MobType {
     ENDER_DRAGON,
     ENDERMAN,
     ENDERMITE,
+<<<<<<< /usr/src/app/output/steveice10/mcprotocollib/36e5eafd286fc1d6a554487a0a2e2f18032ece1a/src/main/java/com/github/steveice10/mc/protocol/data/game/entity/type/MobType.java/left.java
     EVOKER,
     FOX,
+||||||| /usr/src/app/output/steveice10/mcprotocollib/36e5eafd286fc1d6a554487a0a2e2f18032ece1a/src/main/java/com/github/steveice10/mc/protocol/data/game/entity/type/MobType.java/base.java
+    EVOCATION_ILLAGER,
+=======
+    EVOCATION_ILLAGER,
+    XP_ORB,
+    EYE_OF_ENDER_SIGNAL,
+    FALLING_SAND,
+    FIREWORKS_ROCKET_ENTITY,
+>>>>>>> /usr/src/app/output/steveice10/mcprotocollib/36e5eafd286fc1d6a554487a0a2e2f18032ece1a/src/main/java/com/github/steveice10/mc/protocol/data/game/entity/type/MobType.java/right.java
     GHAST,
     GIANT,
     GUARDIAN,
     HORSE,
     HUSK,
+<<<<<<< /usr/src/app/output/steveice10/mcprotocollib/36e5eafd286fc1d6a554487a0a2e2f18032ece1a/src/main/java/com/github/steveice10/mc/protocol/data/game/entity/type/MobType.java/left.java
     ILLUSIONER,
+||||||| /usr/src/app/output/steveice10/mcprotocollib/36e5eafd286fc1d6a554487a0a2e2f18032ece1a/src/main/java/com/github/steveice10/mc/protocol/data/game/entity/type/MobType.java/base.java
+    ILLUSION_ILLAGER,
+=======
+    ILLUSION_ILLAGER,
+    ITEM,
+    ITEM_FRAME,
+    GHAST_FIREBALL,
+    LEASH_KNOT,
+>>>>>>> /usr/src/app/output/steveice10/mcprotocollib/36e5eafd286fc1d6a554487a0a2e2f18032ece1a/src/main/java/com/github/steveice10/mc/protocol/data/game/entity/type/MobType.java/right.java
     LLAMA,
     LLAMA_SPIT,
     MAGMA_CUBE,
@@ -42,7 +67,12 @@ public enum MobType {
     MULE,
     MOOSHROOM,
     OCELOT,
+<<<<<<< /usr/src/app/output/steveice10/mcprotocollib/36e5eafd286fc1d6a554487a0a2e2f18032ece1a/src/main/java/com/github/steveice10/mc/protocol/data/game/entity/type/MobType.java/left.java
     PANDA,
+||||||| /usr/src/app/output/steveice10/mcprotocollib/36e5eafd286fc1d6a554487a0a2e2f18032ece1a/src/main/java/com/github/steveice10/mc/protocol/data/game/entity/type/MobType.java/base.java
+=======
+    PAINTING,
+>>>>>>> /usr/src/app/output/steveice10/mcprotocollib/36e5eafd286fc1d6a554487a0a2e2f18032ece1a/src/main/java/com/github/steveice10/mc/protocol/data/game/entity/type/MobType.java/right.java
     PARROT,
     PIG,
     PUFFERFISH,
@@ -58,7 +88,16 @@ public enum MobType {
     SKELETON,
     SKELETON_HORSE,
     SLIME,
+<<<<<<< /usr/src/app/output/steveice10/mcprotocollib/36e5eafd286fc1d6a554487a0a2e2f18032ece1a/src/main/java/com/github/steveice10/mc/protocol/data/game/entity/type/MobType.java/left.java
     SNOW_GOLEM,
+||||||| /usr/src/app/output/steveice10/mcprotocollib/36e5eafd286fc1d6a554487a0a2e2f18032ece1a/src/main/java/com/github/steveice10/mc/protocol/data/game/entity/type/MobType.java/base.java
+    SNOWMAN,
+=======
+    BLAZE_FIREBALL,
+    SNOWMAN,
+    SNOWBALL,
+    SPECTRAL_ARROW,
+>>>>>>> /usr/src/app/output/steveice10/mcprotocollib/36e5eafd286fc1d6a554487a0a2e2f18032ece1a/src/main/java/com/github/steveice10/mc/protocol/data/game/entity/type/MobType.java/right.java
     SPIDER,
     SQUID,
     STRAY,
@@ -83,7 +122,17 @@ public enum MobType {
     ZOMBIE,
     ZOMBIE_HORSE,
     ZOMBIE_VILLAGER,
+<<<<<<< /usr/src/app/output/steveice10/mcprotocollib/36e5eafd286fc1d6a554487a0a2e2f18032ece1a/src/main/java/com/github/steveice10/mc/protocol/data/game/entity/type/MobType.java/left.java
     PHANTOM,
     RAVAGER,
     PLAYER;
+||||||| /usr/src/app/output/steveice10/mcprotocollib/36e5eafd286fc1d6a554487a0a2e2f18032ece1a/src/main/java/com/github/steveice10/mc/protocol/data/game/entity/type/MobType.java/base.java
+    PHANTOM;
+=======
+    PHANTOM,
+    LIGHTNING_BOLT,
+    PLAYER,
+    FISHING_BOBBER,
+    TRIDENT;
+>>>>>>> /usr/src/app/output/steveice10/mcprotocollib/36e5eafd286fc1d6a554487a0a2e2f18032ece1a/src/main/java/com/github/steveice10/mc/protocol/data/game/entity/type/MobType.java/right.java
 }
