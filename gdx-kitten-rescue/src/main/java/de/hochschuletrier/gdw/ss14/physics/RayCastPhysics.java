24b48ce2b2d6dbf649b289c1d2ce7f4d3b69d7ca
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 00:55:05 +0000

    Mining Framework Analysis

--- gdx-kitten-rescue/src/main/java/de/hochschuletrier/gdw/ss14/physics/RayCastPhysics.java
+++ gdx-kitten-rescue/src/main/java/de/hochschuletrier/gdw/ss14/physics/RayCastPhysics.java
@@ -1,23 +1,25 @@
 package de.hochschuletrier.gdw.ss14.physics;
-
 import java.util.ArrayList;
-
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.RayCastCallback;
 import com.badlogic.gdx.physics.box2d.World;
 
-public class RayCastPhysics implements RayCastCallback{
-    
-    public Fixture m_fixture;
-    public boolean m_hit;
-    public Vector2 m_point;
-    public Vector2 m_normal;
-    public float m_fraction;
-    public ArrayList<RayCastPhysics> collisions = new ArrayList<>();
-    
-    /**
+public class RayCastPhysics implements RayCastCallback {
+  public Fixture m_fixture;
+
+  public boolean m_hit;
+
+  public Vector2 m_point;
+
+  public Vector2 m_normal;
+
+  public float m_fraction;
+
+  public ArrayList<RayCastPhysics> collisions = new ArrayList<>();
+
+  /**
      * Creating a RayCastCallback
      *
      * @param m_fixture  → the fixture of the collision
@@ -29,39 +31,33 @@ public class RayCastPhysics implements RayCastCallback{
      *    stored in RayCastPhysics Objects including the collisions data
      * 
      */
-    
-    public RayCastPhysics()
-    {
-        reset();
-    }
-    
-    public void reset(){
-        m_hit = false;
-        m_point = null;
-        m_normal = null;
-        m_fraction = 0;
-        collisions.clear();
-    }
+  public RayCastPhysics() {
+    reset();
+  }
 
-    public float reportRayFixture(Fixture fixture,  Vector2 point,
-             Vector2 normal, float fraction)
-    {
-            Body body = fixture.getBody();
-            Object userData = body.getUserData();
-            if (userData != null)
-            {
-                    int index = (Integer)userData.hashCode();
-                    if (index == 0){
-                            return -1f;
-                    }
-            }
-            
-            m_hit = true;
-            m_point = point;
-            m_normal = normal;
-            m_fraction = fraction;
-            m_fixture = fixture;
-            collisions.add(this);
-            return 1f;
+  public void reset() {
+    m_hit = false;
+    m_point = null;
+    m_normal = null;
+    m_fraction = 0;
+    collisions.clear();
+  }
+
+  public float reportRayFixture(Fixture fixture, Vector2 point, Vector2 normal, float fraction) {
+    Body body = fixture.getBody();
+    Object userData = body.getUserData();
+    if (userData != null) {
+      int index = (Integer) userData.hashCode();
+      if (index == 0) {
+        return -1f;
+      }
     }
+    m_hit = true;
+    m_point = point;
+    m_normal = normal;
+    m_fraction = fraction;
+    m_fixture = fixture;
+    collisions.add(this);
+    return 1f;
+  }
 }
