e63bb3a83b4f8d1bf9640787eaafd8c6d1718c49
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 23:19:06 +0000

    Mining Framework Analysis

--- src/main/java/org/openpnp/machine/reference/feeder/ReferenceLoosePartFeeder.java
+++ src/main/java/org/openpnp/machine/reference/feeder/ReferenceLoosePartFeeder.java
@@ -1,28 +1,6 @@
-/*
- * Copyright (C) 2011 Jason von Nieda <jason@vonnieda.org>
- * 
- * This file is part of OpenPnP.
- * 
- * OpenPnP is free software: you can redistribute it and/or modify it under the terms of the GNU
- * General Public License as published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- * 
- * OpenPnP is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
- * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
- * Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License along with OpenPnP. If not, see
- * <http://www.gnu.org/licenses/>.
- * 
- * For more information about OpenPnP visit http://openpnp.org
- */
-
 package org.openpnp.machine.reference.feeder;
-
 import java.util.List;
-
 import javax.swing.Action;
-
 import org.apache.commons.io.IOUtils;
 import org.opencv.core.RotatedRect;
 import org.openpnp.gui.MainFrame;
@@ -42,122 +20,90 @@ import org.simpleframework.xml.Element;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/*
- * TODO: We need to be able to determine "normal" angle for the part, which probably means width and
- * height, which is going to be tough when the body is square. Might need to make it more related to
- * bottom vision so that it can determine the correct angle for what it locates.
- * 
- * TODO: Remember the idea of using contours or whatever to find the part and then using template
- * matching to get it's orientation. That would solve the above, and make it work for polarized
- * parts too.
- * 
- * And related to that, this might be a good time to play with something like SIFT.
- */
 public class ReferenceLoosePartFeeder extends ReferenceFeeder {
-    private final static Logger logger = LoggerFactory.getLogger(ReferenceLoosePartFeeder.class);
+  private final static Logger logger = LoggerFactory.getLogger(ReferenceLoosePartFeeder.class);
 
-    @Element(required = false)
-    private CvPipeline pipeline = createDefaultPipeline();
+  @Element(required = false) private CvPipeline pipeline = createDefaultPipeline();
 
-    private Location pickLocation;
-	Nozzle nozzle;
+  private Location pickLocation;
 
-    @Override
-    public Location getPickLocation() throws Exception {
-        return pickLocation == null ? location : pickLocation;
+  Nozzle nozzle;
+
+  @Override public Location getPickLocation() throws Exception {
+    return pickLocation == null ? location : pickLocation;
+  }
+
+  @Override public void feed(Nozzle nozzle) throws Exception {
+    Camera camera = nozzle.getHead().getDefaultCamera();
+    this.nozzle = nozzle;
+    MovableUtils.moveToLocationAtSafeZ(camera, location);
+    for (int i = 0; i < 3; i++) {
+      pickLocation = getPickLocation(camera);
+      camera.moveTo(pickLocation);
     }
+  }
 
-    @Override
-    public void feed(Nozzle nozzle) throws Exception {
-        Camera camera = nozzle.getHead().getDefaultCamera();
-		this.nozzle=nozzle;
-        // Move to the feeder pick location
-        MovableUtils.moveToLocationAtSafeZ(camera, location);
-        for (int i = 0; i < 3; i++) {
-            pickLocation = getPickLocation(camera);
-            camera.moveTo(pickLocation);
-        }
+  private Location getPickLocation(Camera camera) throws Exception {
+    pipeline.setCamera(camera);
+    pipeline.
+<<<<<<< /usr/src/app/output/openpnp/openpnp/3bce6e43e27344d6140fe166b5322cfa77844280/src/main/java/org/openpnp/machine/reference/feeder/ReferenceLoosePartFeeder.java/left.java
+    setNozzle(nozzle)
+=======
+    setFeeder(this)
+>>>>>>> /usr/src/app/output/openpnp/openpnp/3bce6e43e27344d6140fe166b5322cfa77844280/src/main/java/org/openpnp/machine/reference/feeder/ReferenceLoosePartFeeder.java/right.java
+    ;
+    pipeline.process();
+    List<RotatedRect> results = (List<RotatedRect>) pipeline.getResult("results").model;
+    if (results.isEmpty()) {
+      throw new Exception("Feeder " + getName() + ": No parts found.");
     }
+    results.sort((a, b) -> {
+      Double da = VisionUtils.getPixelLocation(camera, a.center.x, a.center.y).getLinearDistanceTo(camera.getLocation());
+      Double db = VisionUtils.getPixelLocation(camera, b.center.x, b.center.y).getLinearDistanceTo(camera.getLocation());
+      return da.compareTo(db);
+    });
+    RotatedRect result = results.get(0);
+    Location location = VisionUtils.getPixelLocation(camera, result.center.x, result.center.y);
+    location = location.derive(null, null, null, result.angle);
+    location = location.derive(null, null, this.location.convertToUnits(location.getUnits()).getZ() + part.getHeight().convertToUnits(location.getUnits()).getValue(), null);
+    MainFrame.get().getCameraViews().getCameraView(camera).showFilteredImage(OpenCvUtils.toBufferedImage(pipeline.getWorkingImage()), 250);
+    return location;
+  }
 
-    private Location getPickLocation(Camera camera) throws Exception {
-        // Process the pipeline to extract RotatedRect results
-        pipeline.setCamera(camera);
-        pipeline.setNozzle(nozzle);
-        pipeline.setFeeder(this);
-        pipeline.process();
-        // Grab the results
-        List<RotatedRect> results = (List<RotatedRect>) pipeline.getResult("results").model;
-        if (results.isEmpty()) {
-            throw new Exception("Feeder " + getName() + ": No parts found.");
-        }
-        // Find the closest result
-        results.sort((a, b) -> {
-            Double da = VisionUtils.getPixelLocation(camera, a.center.x, a.center.y)
-                    .getLinearDistanceTo(camera.getLocation());
-            Double db = VisionUtils.getPixelLocation(camera, b.center.x, b.center.y)
-                    .getLinearDistanceTo(camera.getLocation());
-            return da.compareTo(db);
-        });
-        RotatedRect result = results.get(0);
-        Location location = VisionUtils.getPixelLocation(camera, result.center.x, result.center.y);
-        // Get the result's Location
-        // Update the location with the result's rotation
-        location = location.derive(null, null, null, result.angle);
-        // Update the location with the correct Z, which is the configured Location's Z
-        // plus the part height.
-        location =
-                location.derive(null, null,
-                        this.location.convertToUnits(location.getUnits()).getZ()
-                                + part.getHeight().convertToUnits(location.getUnits()).getValue(),
-                        null);
-        MainFrame.get().getCameraViews().getCameraView(camera)
-                .showFilteredImage(OpenCvUtils.toBufferedImage(pipeline.getWorkingImage()), 250);
-        return location;
-    }
+  public CvPipeline getPipeline() {
+    return pipeline;
+  }
 
-    public CvPipeline getPipeline() {
-        return pipeline;
-    }
+  public void resetPipeline() {
+    pipeline = createDefaultPipeline();
+  }
 
-    public void resetPipeline() {
-        pipeline = createDefaultPipeline();
-    }
+  @Override public Wizard getConfigurationWizard() {
+    return new ReferenceLoosePartFeederConfigurationWizard(this);
+  }
 
-    @Override
-    public Wizard getConfigurationWizard() {
-        return new ReferenceLoosePartFeederConfigurationWizard(this);
-    }
+  @Override public String getPropertySheetHolderTitle() {
+    return getClass().getSimpleName() + " " + getName();
+  }
 
-    @Override
-    public String getPropertySheetHolderTitle() {
-        return getClass().getSimpleName() + " " + getName();
-    }
+  @Override public PropertySheetHolder[] getChildPropertySheetHolders() {
+    return null;
+  }
 
-    @Override
-    public PropertySheetHolder[] getChildPropertySheetHolders() {
-        // TODO Auto-generated method stub
-        return null;
-    }
+  @Override public PropertySheet[] getPropertySheets() {
+    return new PropertySheet[] { new PropertySheetWizardAdapter(getConfigurationWizard()) };
+  }
 
-    @Override
-    public PropertySheet[] getPropertySheets() {
-        return new PropertySheet[] {new PropertySheetWizardAdapter(getConfigurationWizard())};
-    }
+  @Override public Action[] getPropertySheetHolderActions() {
+    return null;
+  }
 
-    @Override
-    public Action[] getPropertySheetHolderActions() {
-        // TODO Auto-generated method stub
-        return null;
+  public static CvPipeline createDefaultPipeline() {
+    try {
+      String xml = IOUtils.toString(ReferenceLoosePartFeeder.class.getResource("ReferenceLoosePartFeeder-DefaultPipeline.xml"));
+      return new CvPipeline(xml);
+    } catch (Exception e) {
+      throw new Error(e);
     }
-
-    public static CvPipeline createDefaultPipeline() {
-        try {
-            String xml = IOUtils.toString(ReferenceLoosePartFeeder.class
-                    .getResource("ReferenceLoosePartFeeder-DefaultPipeline.xml"));
-            return new CvPipeline(xml);
-        }
-        catch (Exception e) {
-            throw new Error(e);
-        }
-    }
+  }
 }
--- src/main/java/org/openpnp/vision/pipeline/CvPipeline.java
+++ src/main/java/org/openpnp/vision/pipeline/CvPipeline.java
@@ -1,5 +1,4 @@
 package org.openpnp.vision.pipeline;
-
 import java.io.StringReader;
 import java.io.StringWriter;
 import java.util.ArrayList;
@@ -7,7 +6,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
@@ -41,287 +39,280 @@ import org.simpleframework.xml.Serializer;
  * 
  * TODO: Add info showing pixel coordinates when mouse is in image window.
  */
-@Root
-public class CvPipeline {
-    static {
-        nu.pattern.OpenCV.loadShared();
-        System.loadLibrary(org.opencv.core.Core.NATIVE_LIBRARY_NAME);
+@Root public class CvPipeline {
+  static {
+    nu.pattern.OpenCV.loadShared();
+    System.loadLibrary(org.opencv.core.Core.NATIVE_LIBRARY_NAME);
+  }
+
+  @ElementList private ArrayList<CvStage> stages = new ArrayList<>();
+
+  private Map<CvStage, Result> results = new HashMap<CvStage, Result>();
+
+  private Mat workingImage;
+
+  private Camera camera;
+
+  private 
+<<<<<<< /usr/src/app/output/openpnp/openpnp/3bce6e43e27344d6140fe166b5322cfa77844280/src/main/java/org/openpnp/vision/pipeline/CvPipeline.java/left.java
+  Nozzle
+=======
+  Feeder
+>>>>>>> /usr/src/app/output/openpnp/openpnp/3bce6e43e27344d6140fe166b5322cfa77844280/src/main/java/org/openpnp/vision/pipeline/CvPipeline.java/right.java
+   
+<<<<<<< /usr/src/app/output/openpnp/openpnp/3bce6e43e27344d6140fe166b5322cfa77844280/src/main/java/org/openpnp/vision/pipeline/CvPipeline.java/left.java
+  nozzle
+=======
+  feeder
+>>>>>>> /usr/src/app/output/openpnp/openpnp/3bce6e43e27344d6140fe166b5322cfa77844280/src/main/java/org/openpnp/vision/pipeline/CvPipeline.java/right.java
+  ;
+
+  private long totalProcessingTimeNs;
+
+  public CvPipeline() {
+  }
+
+  public CvPipeline(String xmlPipeline) {
+    try {
+      fromXmlString(xmlPipeline);
+    } catch (Exception e) {
+      throw new Error(e);
     }
+  }
 
-    @ElementList
-    private ArrayList<CvStage> stages = new ArrayList<>();
-
-    private Map<CvStage, Result> results = new HashMap<CvStage, Result>();
-
-    private Mat workingImage;
-
-    private Camera camera;
-    private Nozzle nozzle;
-    private Feeder feeder;
-    
-    private long totalProcessingTimeNs;
-    
-    public CvPipeline() {
-        
-    }
-    
-    public CvPipeline(String xmlPipeline) {
-        try {
-            fromXmlString(xmlPipeline);
-        }
-        catch (Exception e) {
-            throw new Error(e);
-        }
-    }
-
-    /**
+  /**
      * Add the given CvStage to the end of the pipeline using the given name. If name is null a
      * unique one will be generated and set on the stage.
      * 
      * @param name
      * @param stage
      */
-    public void add(String name, CvStage stage) {
-        if (name == null) {
-            name = generateUniqueName();
-        }
-        stage.setName(name);
-        stages.add(stage);
+  public void add(String name, CvStage stage) {
+    if (name == null) {
+      name = generateUniqueName();
     }
+    stage.setName(name);
+    stages.add(stage);
+  }
 
-    /**
+  /**
      * Add the given CvStage to the end of the pipeline. If the stage does not have a name a unique
      * one will be generated and set on the stage.
      * 
      * @param stage
      */
-    public void add(CvStage stage) {
-        add(stage.getName(), stage);
-    }
+  public void add(CvStage stage) {
+    add(stage.getName(), stage);
+  }
 
-    public void insert(String name, CvStage stage, int index) {
-        if (name == null) {
-            name = generateUniqueName();
-        }
-        stage.setName(name);
-        stages.add(index, stage);
+  public void insert(String name, CvStage stage, int index) {
+    if (name == null) {
+      name = generateUniqueName();
     }
+    stage.setName(name);
+    stages.add(index, stage);
+  }
 
-    public void insert(CvStage stage, int index) {
-        insert(stage.getName(), stage, index);
+  public void insert(CvStage stage, int index) {
+    insert(stage.getName(), stage, index);
+  }
+
+  public void remove(String name) {
+    remove(getStage(name));
+  }
+
+  public void remove(CvStage stage) {
+    stages.remove(stage);
+  }
+
+  public List<CvStage> getStages() {
+    return Collections.unmodifiableList(stages);
+  }
+
+  public CvStage getStage(String name) {
+    if (name == null) {
+      return null;
     }
-
-    public void remove(String name) {
-        remove(getStage(name));
+    for (CvStage stage : stages) {
+      if (stage.getName().equals(name)) {
+        return stage;
+      }
     }
+    return null;
+  }
 
-    public void remove(CvStage stage) {
-        stages.remove(stage);
-    }
-
-    public List<CvStage> getStages() {
-        return Collections.unmodifiableList(stages);
-    }
-
-    public CvStage getStage(String name) {
-        if (name == null) {
-            return null;
-        }
-        for (CvStage stage : stages) {
-            if (stage.getName().equals(name)) {
-                return stage;
-            }
-        }
-        return null;
-    }
-
-    /**
+  /**
      * Get the Result returned by the CvStage with the given name. May return null if the stage did
      * not return a result.
      * 
      * @param name
      * @return
      */
-    public Result getResult(String name) {
-        if (name == null) {
-            return null;
-        }
-        return getResult(getStage(name));
+  public Result getResult(String name) {
+    if (name == null) {
+      return null;
     }
+    return getResult(getStage(name));
+  }
 
-    /**
+  /**
      * Get the Result returned by give CvStage. May return null if the stage did not return a
      * result.
      * 
      * @param stage
      * @return
      */
-    public Result getResult(CvStage stage) {
-        if (stage == null) {
-            return null;
-        }
-        return results.get(stage);
+  public Result getResult(CvStage stage) {
+    if (stage == null) {
+      return null;
     }
+    return results.get(stage);
+  }
 
-    /**
+  /**
      * Get the current working image. Primarily intended to be called from CvStage implementations.
      * 
      * @return
      */
-    public Mat getWorkingImage() {
-        if (workingImage == null || (workingImage.cols() == 0 && workingImage.rows() == 0)) {
-            workingImage = new Mat(480, 640, CvType.CV_8UC3, new Scalar(0, 0, 0));
-            Core.line(workingImage, new Point(0, 0), new Point(640, 480), new Scalar(0, 0, 255));
-            Core.line(workingImage, new Point(640, 0), new Point(0, 480), new Scalar(0, 0, 255));
+  public Mat getWorkingImage() {
+    if (workingImage == null || (workingImage.cols() == 0 && workingImage.rows() == 0)) {
+      workingImage = new Mat(480, 640, CvType.CV_8UC3, new Scalar(0, 0, 0));
+      Core.line(workingImage, new Point(0, 0), new Point(640, 480), new Scalar(0, 0, 255));
+      Core.line(workingImage, new Point(640, 0), new Point(0, 480), new Scalar(0, 0, 255));
+    }
+    return workingImage;
+  }
+
+  public void setCamera(Camera camera) {
+    this.camera = camera;
+  }
+
+  public Camera getCamera() {
+    return camera;
+  }
+
+  public void setNozzle(Nozzle nozzle) {
+    this.nozzle = nozzle;
+  }
+
+  public void setFeeder(Feeder feeder) {
+    this.feeder = feeder;
+  }
+
+  public Nozzle getNozzle() {
+    return nozzle;
+  }
+
+  public Feeder getFeeder() {
+    return feeder;
+  }
+
+  public long getTotalProcessingTimeNs() {
+    return totalProcessingTimeNs;
+  }
+
+  public void setTotalProcessingTimeNs(long totalProcessingTimeNs) {
+    this.totalProcessingTimeNs = totalProcessingTimeNs;
+  }
+
+  public void process() {
+    totalProcessingTimeNs = 0;
+    release();
+    for (CvStage stage : stages) {
+      long processingTimeNs = System.nanoTime();
+      Result result = null;
+      try {
+        if (!stage.isEnabled()) {
+          throw new Exception("Stage not enabled.");
         }
-        return workingImage;
-    }
-
-    public void setCamera(Camera camera) {
-        this.camera = camera;
-    }
-
-    public Camera getCamera() {
-        return camera;
-    }
-
-    public void setNozzle(Nozzle nozzle) {
-        this.nozzle = nozzle;
-    }
-
-    public Nozzle getNozzle() {
-        return nozzle;
-    }
-  
-    public void setFeeder(Feeder feeder) {
-        this.feeder = feeder;
-    }
-
-    public Feeder getFeeder() {
-        return feeder;
-    }
-
-    public long getTotalProcessingTimeNs() {
-      return totalProcessingTimeNs;
-    }
-
-    public void setTotalProcessingTimeNs(long totalProcessingTimeNs) {
-      this.totalProcessingTimeNs = totalProcessingTimeNs;
-    }
-
-    public void process() {
-
-        totalProcessingTimeNs = 0;
-        release();
-        for (CvStage stage : stages) {
-            // Process and time the stage and get the result.
-            long processingTimeNs = System.nanoTime();
-            Result result = null;
-            try {
-                if (!stage.isEnabled()) {
-                    throw new Exception("Stage not enabled.");
-                }
-                result = stage.process(this);
-            }
-            catch (Exception e) {
-                result = new Result(null, e);
-            }
-            processingTimeNs = System.nanoTime() - processingTimeNs;
-            totalProcessingTimeNs += processingTimeNs;
-
-            Mat image = null;
-            Object model = null;
-            if (result != null) {
-                image = result.image;
-                model = result.model;
-            }
-
-            // If the result image is null and there is a working image, replace the result image
-            // replace the result image with a clone of the working image.
-            if (image == null) {
-                if (workingImage != null) {
-                    image = workingImage.clone();
-                }
-            }
-            // If the result image is not null:
-            // Release the working image if the result image is different.
-            // Replace the working image with the result image.
-            // Clone the result image for storage.
-            else {
-                if (workingImage != null && workingImage != image) {
-                    workingImage.release();
-                }
-                workingImage = image;
-                image = image.clone();
-            }
-
-            results.put(stage, new Result(image, model, processingTimeNs));
+        result = stage.process(this);
+      } catch (Exception e) {
+        result = new Result(null, e);
+      }
+      processingTimeNs = System.nanoTime() - processingTimeNs;
+      totalProcessingTimeNs += processingTimeNs;
+      Mat image = null;
+      Object model = null;
+      if (result != null) {
+        image = result.image;
+        model = result.model;
+      }
+      if (image == null) {
+        if (workingImage != null) {
+          image = workingImage.clone();
         }
+      } else {
+        if (workingImage != null && workingImage != image) {
+          workingImage.release();
+        }
+        workingImage = image;
+        image = image.clone();
+      }
+      results.put(stage, new Result(image, model, processingTimeNs));
     }
+  }
 
-    /**
+  /**
      * Release any temporary resources associated with the processing of the pipeline. Should be
      * called when the pipeline is no longer needed. This is primarily to release retained native
      * resources from OpenCV.
      */
-    public void release() {
-        if (workingImage != null) {
-            workingImage.release();
-        }
-        for (Result result : results.values()) {
-            if (result.image != null) {
-                result.image.release();
-            }
-        }
-        results.clear();
+  public void release() {
+    if (workingImage != null) {
+      workingImage.release();
     }
+    for (Result result : results.values()) {
+      if (result.image != null) {
+        result.image.release();
+      }
+    }
+    results.clear();
+  }
 
-    /**
+  /**
      * Convert the pipeline to an XML string that can be read back in with #fromXmlString.
      * 
      * @return
      * @throws Exception
      */
-    public String toXmlString() throws Exception {
-        Serializer ser = Configuration.createSerializer();
-        StringWriter sw = new StringWriter();
-        ser.write(this, sw);
-        return sw.toString();
-    }
+  public String toXmlString() throws Exception {
+    Serializer ser = Configuration.createSerializer();
+    StringWriter sw = new StringWriter();
+    ser.write(this, sw);
+    return sw.toString();
+  }
 
-    /**
+  /**
      * Parse the pipeline in the given String and replace the current pipeline with the results.
      * 
      * @param s
      * @throws Exception
      */
-    public void fromXmlString(String s) throws Exception {
-        release();
-        Serializer ser = Configuration.createSerializer();
-        StringReader sr = new StringReader(s);
-        CvPipeline pipeline = ser.read(CvPipeline.class, sr);
-        stages.clear();
-        for (CvStage stage : pipeline.getStages()) {
-            add(stage);
-        }
+  public void fromXmlString(String s) throws Exception {
+    release();
+    Serializer ser = Configuration.createSerializer();
+    StringReader sr = new StringReader(s);
+    CvPipeline pipeline = ser.read(CvPipeline.class, sr);
+    stages.clear();
+    for (CvStage stage : pipeline.getStages()) {
+      add(stage);
     }
+  }
 
-    private String generateUniqueName() {
-        for (int i = 0;; i++) {
-            String name = "" + i;
-            if (getStage(name) == null) {
-                return name;
-            }
-        }
+  private String generateUniqueName() {
+    for (int i = 0; ; i++) {
+      String name = "" + i;
+      if (getStage(name) == null) {
+        return name;
+      }
     }
+  }
 
-    @Override
-    public CvPipeline clone() throws CloneNotSupportedException {
-        try {
-            return new CvPipeline(toXmlString());
-        }
-        catch (Exception e) {
-            throw new CloneNotSupportedException(e.getMessage());
-        }
+  @Override public CvPipeline clone() throws CloneNotSupportedException {
+    try {
+      return new CvPipeline(toXmlString());
+    } catch (Exception e) {
+      throw new CloneNotSupportedException(e.getMessage());
     }
+  }
 }
