8cca3849277c5684816b39984fa0299b7192b497
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 05:10:46 +0000

    Mining Framework Analysis

--- frontend-maven-plugin/src/main/java/com/github/eirslett/maven/plugins/frontend/mojo/NpmMojo.java
+++ frontend-maven-plugin/src/main/java/com/github/eirslett/maven/plugins/frontend/mojo/NpmMojo.java
@@ -1,11 +1,8 @@
 package com.github.eirslett.maven.plugins.frontend.mojo;
-
 import java.io.File;
-
 import com.github.eirslett.maven.plugins.frontend.lib.FrontendPluginFactory;
 import com.github.eirslett.maven.plugins.frontend.lib.ProxyConfig;
 import com.github.eirslett.maven.plugins.frontend.lib.TaskRunnerException;
-
 import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
@@ -15,55 +12,44 @@ import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.sonatype.plexus.build.incremental.BuildContext;
-
 import static com.github.eirslett.maven.plugins.frontend.mojo.MojoUtils.setSLF4jLogger;
 
-@Mojo(name="npm",  defaultPhase = LifecyclePhase.GENERATE_RESOURCES)
-public final class NpmMojo extends AbstractMojo {
-
-    /**
+@Mojo(name = "npm", defaultPhase = LifecyclePhase.GENERATE_RESOURCES) public final class NpmMojo extends AbstractMojo {
+  /**
      * The base directory for running all Node commands. (Usually the directory that contains package.json)
      */
-    @Parameter(defaultValue = "${basedir}", property = "workingDirectory", required = false)
-    private File workingDirectory;
+  @Parameter(defaultValue = "${basedir}", property = "workingDirectory", required = false) private File workingDirectory;
 
-    /**
+  /**
      * npm arguments. Default is "install".
      */
-    @Parameter(defaultValue = "install", property = "frontend.npm.arguments", required = false)
-    private String arguments;
+  @Parameter(defaultValue = "install", property = "frontend.npm.arguments", required = false) private String arguments;
 
-    @Parameter(property = "session", defaultValue = "${session}", readonly = true)
-    private MavenSession session;
-    
-    @Component
-    private BuildContext buildContext;
+  @Parameter(property = "session", defaultValue = "${session}", readonly = true) private MavenSession session;
 
-    /**
+  @Component private BuildContext buildContext;
+
+  /**
      * Skips the npm install
      */
-    @Parameter(property = "skip.npm", defaultValue = "false", required = false)
-    private Boolean skipNpm;
+  @Parameter(property = "skip.npm", defaultValue = "false", required = false) private Boolean skipNpm;
 
-    @Override
-    public void execute() throws MojoExecutionException, MojoFailureException {
-        if (skipNpm) {
-            getLog().info("Skipping npm install");
+  @Override public void execute() throws MojoExecutionException, MojoFailureException {
+    if (skipNpm) {
+      getLog().info("Skipping npm install");
+    } else {
+      File packageJson = new File(workingDirectory, "package.json");
+      if (buildContext == null || buildContext.hasDelta(packageJson) || !buildContext.isIncremental()) {
+        try {
+          setSLF4jLogger(getLog());
+          ProxyConfig proxyConfig = MojoUtils.getProxyConfig(session);
+          new FrontendPluginFactory(workingDirectory, proxyConfig).getNpmRunner().execute(arguments);
+        } catch (TaskRunnerException e) {
+          throw new MojoFailureException("Failed to run task", e);
         }
-        else {
-            File packageJson = new File(workingDirectory, "package.json");
-            if (buildContext == null || buildContext.hasDelta(packageJson) || !buildContext.isIncremental()) {
-                try {
-                    setSLF4jLogger(getLog());
-    
-                    ProxyConfig proxyConfig = MojoUtils.getProxyConfig(session);
-                    new FrontendPluginFactory(workingDirectory, proxyConfig).getNpmRunner().execute(arguments);
-                } catch (TaskRunnerException e) {
-                    throw new MojoFailureException("Failed to run task", e);
-                }
-            } else {
-                getLog().info("Skipping npm install as package.json unchanged");
-            }
-        }
+      } else {
+        getLog().info("Skipping npm install as package.json unchanged");
+      }
     }
+  }
 }
