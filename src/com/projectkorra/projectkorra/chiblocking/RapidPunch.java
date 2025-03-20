90f6eefc2a3df591d5bd0c16013b6872de39acc4
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 12:34:04 +0000

    Mining Framework Analysis

--- src/com/projectkorra/projectkorra/chiblocking/RapidPunch.java
+++ src/com/projectkorra/projectkorra/chiblocking/RapidPunch.java
@@ -1,10 +1,8 @@
 package com.projectkorra.projectkorra.chiblocking;
-
 import org.bukkit.Location;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
-
 import com.projectkorra.projectkorra.ability.ChiAbility;
 import com.projectkorra.projectkorra.airbending.Suffocate;
 import com.projectkorra.projectkorra.attribute.Attribute;
@@ -12,116 +10,116 @@ import com.projectkorra.projectkorra.chiblocking.passive.ChiPassive;
 import com.projectkorra.projectkorra.util.DamageHandler;
 
 public class RapidPunch extends ChiAbility {
+  @Attribute(value = Attribute.DAMAGE) private double damage;
 
-	@Attribute(Attribute.DAMAGE)
-	private double damage;
-	@Attribute("Hits")
-	private int punches;
-	@Attribute(Attribute.COOLDOWN)
-	private long cooldown;
-	private int numPunches;
-	private long interval, last = 0;
-	private Entity target;
+  @Attribute(value = "Hits") private int punches;
 
-	public RapidPunch(Player sourceplayer, Entity targetentity) {
-		super(sourceplayer);
-		if (!bPlayer.canBend(this)) {
-			return;
-		}
-		
-		this.damage = getConfig().getDouble("Abilities.Chi.RapidPunch.Damage");
-		this.punches = getConfig().getInt("Abilities.Chi.RapidPunch.Punches");
-		this.cooldown = getConfig().getLong("Abilities.Chi.RapidPunch.Cooldown");
-		this.interval = getConfig().getLong("Abilities.Chi.RapidPunch.Interval");
-		this.target = targetentity;
-		bPlayer.addCooldown(this);
-		start();
-	}
+  @Attribute(value = Attribute.COOLDOWN) private long cooldown;
 
-	@Override
-	public void progress() {
-		if (numPunches >= punches || target == null || !(target instanceof LivingEntity)) {
-			remove();
-			return;
-		}
+  private int numPunches;
 
-		if (System.currentTimeMillis() >= last + interval) {
-			LivingEntity lt = (LivingEntity) target;
-			DamageHandler.damageEntity(target, damage, this);
-	
-			if (target instanceof Player) {
-				if (ChiPassive.willChiBlock(player, (Player) target)) {
-					ChiPassive.blockChi((Player) target);
-				}
-				if (Suffocate.isChannelingSphere((Player) target)) {
-					Suffocate.remove((Player) target);
-				}
-			}
-	
-			lt.setNoDamageTicks(0);
-			numPunches++;
-		}
-	}
 
-	@Override
-	public String getName() {
-		return "RapidPunch";
-	}
+<<<<<<< Unknown file: This is a bug in JDime.
+=======
+  private long interval, last = 0;
+>>>>>>> /usr/src/app/output/projectkorra/projectkorra/b1b7a8cbc8604b820af0cf21013908f0f5053301/src/com/projectkorra/projectkorra/chiblocking/RapidPunch.java/right.java
 
-	@Override
-	public Location getLocation() {
-		return target != null ? target.getLocation() : null;
-	}
 
-	@Override
-	public long getCooldown() {
-		return cooldown;
-	}
+  private Entity target;
 
-	@Override
-	public boolean isSneakAbility() {
-		return false;
-	}
+  public RapidPunch(Player sourceplayer, Entity targetentity) {
+    super(sourceplayer);
+    if (!bPlayer.canBend(this)) {
+      return;
+    }
+    this.damage = getConfig().getDouble("Abilities.Chi.RapidPunch.Damage");
+    this.punches = getConfig().getInt("Abilities.Chi.RapidPunch.Punches");
+    this.cooldown = getConfig().getLong("Abilities.Chi.RapidPunch.Cooldown");
 
-	@Override
-	public boolean isHarmlessAbility() {
-		return false;
-	}
+<<<<<<< Unknown file: This is a bug in JDime.
+=======
+    this.interval = getConfig().getLong("Abilities.Chi.RapidPunch.Interval");
+>>>>>>> /usr/src/app/output/projectkorra/projectkorra/b1b7a8cbc8604b820af0cf21013908f0f5053301/src/com/projectkorra/projectkorra/chiblocking/RapidPunch.java/right.java
 
-	public double getDamage() {
-		return damage;
-	}
+    this.target = targetentity;
+    bPlayer.addCooldown(this);
+    start();
+  }
 
-	public void setDamage(double damage) {
-		this.damage = damage;
-	}
+  @Override public void progress() {
+    if (numPunches >= punches || target == null || !(target instanceof LivingEntity)) {
+      remove();
+      return;
+    }
+    if (System.currentTimeMillis() >= last + interval) {
+      LivingEntity lt = (LivingEntity) target;
+      DamageHandler.damageEntity(target, damage, this);
+      if (target instanceof Player) {
+        if (ChiPassive.willChiBlock(player, (Player) target)) {
+          ChiPassive.blockChi((Player) target);
+        }
+        if (Suffocate.isChannelingSphere((Player) target)) {
+          Suffocate.remove((Player) target);
+        }
+      }
+      lt.setNoDamageTicks(0);
+      numPunches++;
+    }
+  }
 
-	public int getPunches() {
-		return punches;
-	}
+  @Override public String getName() {
+    return "RapidPunch";
+  }
 
-	public void setPunches(int punches) {
-		this.punches = punches;
-	}
+  @Override public Location getLocation() {
+    return target != null ? target.getLocation() : null;
+  }
 
-	public int getNumPunches() {
-		return numPunches;
-	}
+  @Override public long getCooldown() {
+    return cooldown;
+  }
 
-	public void setNumPunches(int numPunches) {
-		this.numPunches = numPunches;
-	}
+  @Override public boolean isSneakAbility() {
+    return false;
+  }
 
-	public Entity getTarget() {
-		return target;
-	}
+  @Override public boolean isHarmlessAbility() {
+    return false;
+  }
 
-	public void setTarget(Entity target) {
-		this.target = target;
-	}
+  public double getDamage() {
+    return damage;
+  }
 
-	public void setCooldown(long cooldown) {
-		this.cooldown = cooldown;
-	}
+  public void setDamage(double damage) {
+    this.damage = damage;
+  }
 
+  public int getPunches() {
+    return punches;
+  }
+
+  public void setPunches(int punches) {
+    this.punches = punches;
+  }
+
+  public int getNumPunches() {
+    return numPunches;
+  }
+
+  public void setNumPunches(int numPunches) {
+    this.numPunches = numPunches;
+  }
+
+  public Entity getTarget() {
+    return target;
+  }
+
+  public void setTarget(Entity target) {
+    this.target = target;
+  }
+
+  public void setCooldown(long cooldown) {
+    this.cooldown = cooldown;
+  }
 }
--- src/com/projectkorra/projectkorra/chiblocking/passive/ChiPassive.java
+++ src/com/projectkorra/projectkorra/chiblocking/passive/ChiPassive.java
@@ -1,11 +1,9 @@
 package com.projectkorra.projectkorra.chiblocking.passive;
-
 import org.bukkit.ChatColor;
 import org.bukkit.Sound;
+import com.projectkorra.projectkorra.BendingPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
-
-import com.projectkorra.projectkorra.BendingPlayer;
 import com.projectkorra.projectkorra.ProjectKorra;
 import com.projectkorra.projectkorra.ability.ChiAbility;
 import com.projectkorra.projectkorra.ability.CoreAbility;
@@ -17,87 +15,82 @@ import com.projectkorra.projectkorra.configuration.ConfigManager;
 import com.projectkorra.projectkorra.util.ActionBar;
 
 public class ChiPassive {
+  public static boolean willChiBlock(Player attacker, Player player) {
+    BendingPlayer bPlayer = BendingPlayer.getBendingPlayer(player);
+    if (bPlayer == null) {
+      return false;
+    }
+    ChiAbility stance = bPlayer.getStance();
+    QuickStrike quickStrike = CoreAbility.getAbility(player, QuickStrike.class);
+    SwiftKick swiftKick = CoreAbility.getAbility(player, SwiftKick.class);
+    double newChance = getChance();
+    if (stance != null && stance instanceof AcrobatStance) {
+      newChance += ((AcrobatStance) stance).getChiBlockBoost();
+    }
+    if (quickStrike != null) {
+      newChance += quickStrike.getBlockChance();
+    } else {
+      if (swiftKick != null) {
+        newChance += swiftKick.getBlockChance();
+      }
+    }
+    if (Math.random() > newChance / 100.0) {
+      return false;
+    } else {
+      if (bPlayer.isChiBlocked()) {
+        return false;
+      }
+    }
+    return true;
+  }
 
-	public static boolean willChiBlock(Player attacker, Player player) {
-		BendingPlayer bPlayer = BendingPlayer.getBendingPlayer(player);
-		if (bPlayer == null) {
-			return false;
-		}
+  public static void blockChi(final Player player) {
+    if (Suffocate.isChannelingSphere(player)) {
+      Suffocate.remove(player);
+    }
+    final BendingPlayer bPlayer = BendingPlayer.getBendingPlayer(player);
+    if (bPlayer == null) {
+      return;
+    }
+    bPlayer.blockChi();
+    player.getWorld().playSound(player.getLocation(), Sound.ENTITY_ENDERDRAGON_HURT, 2, 0);
+    long start = System.currentTimeMillis();
+    new BukkitRunnable() {
+      @Override public void run() {
+        ActionBar.sendActionBar(ChatColor.GOLD + "* Chiblocked *", player);
+        if (System.currentTimeMillis() >= start + getDuration()) {
+          bPlayer.unblockChi();
+          cancel();
+        }
+      }
+    }.runTaskTimer(ProjectKorra.plugin, 0, 1);
+  }
 
-		ChiAbility stance = bPlayer.getStance();
-		QuickStrike quickStrike = CoreAbility.getAbility(player, QuickStrike.class);
-		SwiftKick swiftKick = CoreAbility.getAbility(player, SwiftKick.class);
-		double newChance = getChance();
+  public static double getExhaustionFactor() {
+    return ConfigManager.getConfig().getDouble("Abilities.Chi.Passive.ChiSaturation.ExhaustionFactor");
+  }
 
-		if (stance != null && stance instanceof AcrobatStance) {
-			newChance += ((AcrobatStance) stance).getChiBlockBoost();
-		}
+  public static double getFallReductionFactor() {
+    return ConfigManager.getConfig().getDouble("Abilities.Chi.Passive.Acrobatics.FallReductionFactor");
+  }
 
-		if (quickStrike != null) {
-			newChance += quickStrike.getBlockChance();
-		} else if (swiftKick != null) {
-			newChance += swiftKick.getBlockChance();
-		}
+  public static int getJumpPower() {
+    return ConfigManager.getConfig().getInt("Abilities.Chi.Passive.ChiAgility.JumpPower");
+  }
 
-		if (Math.random() > newChance / 100.0) {
-			return false;
-		} else if (bPlayer.isChiBlocked()) {
-			return false;
-		}
-		return true;
-	}
+  public static int getSpeedPower() {
+    return ConfigManager.getConfig().getInt("Abilities.Chi.Passive.ChiAgility.SpeedPower");
+  }
 
-	public static void blockChi(final Player player) {
-		if (Suffocate.isChannelingSphere(player)) {
-			Suffocate.remove(player);
-		}
+  public static double getChance() {
+    return ConfigManager.getConfig().getDouble("Abilities.Chi.Passive.BlockChi.Chance");
+  }
 
-		final BendingPlayer bPlayer = BendingPlayer.getBendingPlayer(player);
-		if (bPlayer == null) {
-			return;
-		}
-		bPlayer.blockChi();
-		player.getWorld().playSound(player.getLocation(), Sound.ENTITY_ENDERDRAGON_HURT, 2, 0);
-		
-		long start = System.currentTimeMillis();
-		new BukkitRunnable() {
-			@Override
-			public void run() {
-				ActionBar.sendActionBar(ChatColor.GOLD + "* Chiblocked *", player);
-				if (System.currentTimeMillis() >= start + getDuration()) {
-					bPlayer.unblockChi();
-					cancel();
-				}
-			}
-		}.runTaskTimer(ProjectKorra.plugin, 0, 1);
-	}
+  public static int getDuration() {
+    return ConfigManager.getConfig().getInt("Abilities.Chi.Passive.BlockChi.Duration");
+  }
 
-	public static double getExhaustionFactor() {
-		return ConfigManager.getConfig().getDouble("Abilities.Chi.Passive.ChiSaturation.ExhaustionFactor");
-	}
-
-	public static double getFallReductionFactor() {
-		return ConfigManager.getConfig().getDouble("Abilities.Chi.Passive.Acrobatics.FallReductionFactor");
-	}
-
-	public static int getJumpPower() {
-		return ConfigManager.getConfig().getInt("Abilities.Chi.Passive.ChiAgility.JumpPower");
-	}
-
-	public static int getSpeedPower() {
-		return ConfigManager.getConfig().getInt("Abilities.Chi.Passive.ChiAgility.SpeedPower");
-	}
-
-	public static double getChance() {
-		return ConfigManager.getConfig().getDouble("Abilities.Chi.Passive.BlockChi.Chance");
-	}
-
-	public static int getDuration() {
-		return ConfigManager.getConfig().getInt("Abilities.Chi.Passive.BlockChi.Duration");
-	}
-
-	public static long getTicks() {
-		return (getDuration() / 1000) * 20;
-	}
-
+  public static long getTicks() {
+    return (getDuration() / 1000) * 20;
+  }
 }
