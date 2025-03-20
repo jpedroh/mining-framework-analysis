f26d90524e9980719a26519d11e8d55d37d93240
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 02:49:29 +0000

    Mining Framework Analysis

--- gdx-pneumatic-man/src/main/java/de/hochschuletrier/gdw/ws1415/game/ComponentMappers.java
+++ gdx-pneumatic-man/src/main/java/de/hochschuletrier/gdw/ws1415/game/ComponentMappers.java
@@ -1,18 +1,1 @@
-package de.hochschuletrier.gdw.ws1415.game;
-
-import com.badlogic.ashley.core.ComponentMapper;
-import de.hochschuletrier.gdw.commons.gdx.physix.components.*;
-import de.hochschuletrier.gdw.ws1415.game.components.*;
-
-public class ComponentMappers {
-    public static final ComponentMapper<PositionComponent> position = ComponentMapper.getFor(PositionComponent.class);
-    public static final ComponentMapper<TriggerComponent> trigger = ComponentMapper.getFor(TriggerComponent.class);
-    public static final ComponentMapper<PhysixBodyComponent> physixBody = ComponentMapper.getFor(PhysixBodyComponent.class);
-    public static final ComponentMapper<PhysixModifierComponent> physixModifier = ComponentMapper.getFor(PhysixModifierComponent.class);
-    public static final ComponentMapper<ImpactSoundComponent> impactSound = ComponentMapper.getFor(ImpactSoundComponent.class);
-    public static final ComponentMapper<AnimationComponent> animation = ComponentMapper.getFor(AnimationComponent.class);
-    public static final ComponentMapper<MovementComponent> movement = ComponentMapper.getFor(MovementComponent.class);
-    public static final ComponentMapper<BouncingComponent> bouncing = ComponentMapper.getFor(BouncingComponent.class);
-    public static final ComponentMapper<KillsPlayerOnContactComponent> enemy = ComponentMapper.getFor(KillsPlayerOnContactComponent.class);
-
-}
+  package     de . hochschuletrier . gdw . ws1415 . game ;   import     com . badlogic . ashley . core . ComponentMapper ;  import       de . hochschuletrier . gdw . commons . gdx . physix . components .  * ;  import      de . hochschuletrier . gdw . ws1415 . game . components .  * ;   public class ComponentMappers  {   public static final  ComponentMapper  < PositionComponent >  position =  ComponentMapper . getFor  (  PositionComponent . class ) ;   public static final  ComponentMapper  < TriggerComponent >  trigger =  ComponentMapper . getFor  (  TriggerComponent . class ) ;   public static final  ComponentMapper  < PhysixBodyComponent >  physixBody =  ComponentMapper . getFor  (  PhysixBodyComponent . class ) ;   public static final  ComponentMapper  < PhysixModifierComponent >  physixModifier =  ComponentMapper . getFor  (  PhysixModifierComponent . class ) ;   public static final  ComponentMapper  < ImpactSoundComponent >  impactSound =  ComponentMapper . getFor  (  ImpactSoundComponent . class ) ;   public static final  ComponentMapper  < AnimationComponent >  animation =  ComponentMapper . getFor  (  AnimationComponent . class ) ;   public static final  ComponentMapper  < MovementComponent >  movement =  ComponentMapper . getFor  (  MovementComponent . class ) ;   public static final  ComponentMapper  < BouncingComponent >  bouncing =  ComponentMapper . getFor  (  BouncingComponent . class ) ;   public static final  ComponentMapper  < KillsPlayerOnContactComponent >  enemy =  ComponentMapper . getFor  (  KillsPlayerOnContactComponent . class ) ; }
