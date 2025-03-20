3553e62e05981e9327084a38a4482640fb42cac9
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 19:20:14 +0000

    Mining Framework Analysis

--- src/main/com/marginallyclever/makelangelo/plotter/Plotter.java
+++ src/main/com/marginallyclever/makelangelo/plotter/Plotter.java
@@ -1,7 +1,5 @@
 package com.marginallyclever.makelangelo.plotter;
-
 import java.util.ArrayList;
-
 import com.jogamp.opengl.GL2;
 import com.marginallyclever.convenience.Point2D;
 import com.marginallyclever.makelangelo.plotter.settings.PlotterSettings;
@@ -14,198 +12,185 @@ import com.marginallyclever.makelangelo.turtle.TurtleMove;
  * @author Dan
  * @since 7.2.10
  */
-public class Plotter implements PreviewListener, Cloneable {	
-	private PlotterSettings settings = new PlotterSettings();
+public class Plotter implements PreviewListener, Cloneable {
+  private PlotterSettings settings = new PlotterSettings();
 
-	// are motors actively engaged?  when disengaged pen can drift and re-homing is required.
-	private boolean areMotorsEngaged = true;
-	// did the robot find home?  if it has not then the pen position is undefined.
-	private boolean didFindHome = false;
-	// if pen is "up" then it is not drawing.
-	private boolean penIsUp = false;
-	// current pen position
-	private Point2D pos = new Point2D(0,0);
-	
-	@Override
-	public Object clone() throws CloneNotSupportedException {
-		Plotter b = (Plotter)super.clone();
-		b.listeners = new ArrayList<PlotterEventListener>();
-		b.pos = new Point2D();
-		b.pos.set(this.pos);
-		return b;
-	}
-	
-	// OBSERVER PATTERN
-	
-	private ArrayList<PlotterEventListener> listeners = new ArrayList<PlotterEventListener>();
+  private boolean areMotorsEngaged = true;
 
-	public void addListener(PlotterEventListener listener) {
-		listeners.add(listener);
-	}
+  private boolean didFindHome = false;
 
-	public void removeListener(PlotterEventListener listener) {
-		listeners.remove(listener);
-	}
-	
-	private void notifyListeners(PlotterEvent e) {
-		for (PlotterEventListener listener : listeners) listener.makelangeloRobotEvent(e);
-	}
+  private boolean penIsUp = false;
 
-	// OBSERVER PATTERN ENDS
-	
-	public void raisePen() {
-		penIsUp=true;
-		notifyListeners(new PlotterEvent(PlotterEvent.PEN_UPDOWN,this,true));
-	}
-	
-	public void lowerPen() {
-		penIsUp = false;
-		notifyListeners(new PlotterEvent(PlotterEvent.PEN_UPDOWN,this,false));
-	}
-	
-	private void requestUserChangeTool(int toolNumber) {
-		notifyListeners(new PlotterEvent(PlotterEvent.TOOL_CHANGE, this, toolNumber));
-	}
+  private Point2D pos = new Point2D(0, 0);
 
-	public void goHome() {
-		moveTo(settings.getHomeX(),settings.getHomeY());
-		pos.set(settings.getHomeX(),settings.getHomeY());
-		didFindHome = true;
-		notifyListeners(new PlotterEvent(PlotterEvent.HOME_FOUND,this));
-	}
-	
-	public void findHome() {
-		raisePen();
-		pos.set(settings.getHomeX(),settings.getHomeY());
-		didFindHome = true;
-		notifyListeners(new PlotterEvent(PlotterEvent.HOME_FOUND,this));
-	}
-	
-	public boolean getDidFindHome() {
-		return didFindHome;
-	}
+  @Override public Object clone() throws CloneNotSupportedException {
+    Plotter b = (Plotter) super.clone();
+    b.listeners = new ArrayList<PlotterEventListener>();
+    b.pos = new Point2D();
+    b.pos.set(this.pos);
+    return b;
+  }
 
-	// in mm
-	public void setPos(double x,double y) {
-		pos.set(x,y);
-		notifyListeners(new PlotterEvent(PlotterEvent.POSITION,this));
-	}
-	
-	// in mm
-	public void setPos(Point2D p) {
-		pos.set(p);
-		notifyListeners(new PlotterEvent(PlotterEvent.POSITION,this));
-	}
+  private ArrayList<PlotterEventListener> listeners = new ArrayList<PlotterEventListener>();
 
-	// in mm
-	public void moveTo(double x, double y) {
-		setPos(x,y);
-	}
+  public void addListener(PlotterEventListener listener) {
+    listeners.add(listener);
+  }
 
-	// in mm
-	public void moveTo(Point2D p) {
-		setPos(p);
-	}
+  public void removeListener(PlotterEventListener listener) {
+    listeners.remove(listener);
+  }
 
-	// in mm
-	public Point2D getPos() {
-		return pos;
-	}
-	
-	public boolean getAreMotorsEngaged() {
-		return areMotorsEngaged;
-	}
-		
-	public void disengageMotors() {
-		areMotorsEngaged = false;
-		notifyListeners(new PlotterEvent(PlotterEvent.MOTORS_ENGAGED,this,false));
-	}
+  private void notifyListeners(PlotterEvent e) {
+    for (PlotterEventListener listener : listeners) {
+      listener.makelangeloRobotEvent(e);
+    }
+  }
 
-	public void engageMotors() {
-		areMotorsEngaged = true;
-		notifyListeners(new PlotterEvent(PlotterEvent.MOTORS_ENGAGED,this,true));
-	}
-	
-	public PlotterSettings getSettings() {
-		return settings;
-	}
-	
-	public void setSettings(PlotterSettings s) {
-		settings=s;
-	}
-	
-	@Override
-	public void render(GL2 gl2) {
-		float[] lineWidthBuf = new float[1];
-		gl2.glGetFloatv(GL2.GL_LINE_WIDTH, lineWidthBuf, 0);
-		
-		drawPhysicalLimits(gl2);
-		
-		// hardware features
-		settings.getHardwareProperties().render(gl2, this);
+  public void raisePen() {
+    penIsUp = true;
+    notifyListeners(new PlotterEvent(PlotterEvent.PEN_UPDOWN, this, true));
+  }
 
-		gl2.glLineWidth(lineWidthBuf[0]);
-	}	
-	
-	private void drawPhysicalLimits(GL2 gl2) {
-		gl2.glLineWidth(2);
+  public void lowerPen() {
+    penIsUp = false;
+    notifyListeners(new PlotterEvent(PlotterEvent.PEN_UPDOWN, this, false));
+  }
 
-		gl2.glColor3f(0.7f, 0.7f, 0.7f); // #color Machine Hardware Drawing Limits
-		
-		gl2.glBegin(GL2.GL_LINE_LOOP);
-		gl2.glVertex2d(settings.getLimitLeft(), settings.getLimitTop());
-		gl2.glVertex2d(settings.getLimitRight(), settings.getLimitTop());
-		gl2.glVertex2d(settings.getLimitRight(), settings.getLimitBottom());
-		gl2.glVertex2d(settings.getLimitLeft(), settings.getLimitBottom());
-		gl2.glEnd();
-	}
+  private void requestUserChangeTool(int toolNumber) {
+    notifyListeners(new PlotterEvent(PlotterEvent.TOOL_CHANGE, this, toolNumber));
+  }
 
-	public boolean getPenIsUp() {
-		return penIsUp;
-	}
+  public void goHome() {
+    moveTo(settings.getHomeX(), settings.getHomeY());
+    pos.set(settings.getHomeX(), settings.getHomeY());
+    didFindHome = true;
+    notifyListeners(new PlotterEvent(PlotterEvent.HOME_FOUND, this));
+  }
 
-	public void turtleMove(TurtleMove move) {
-		switch(move.type) {
-		case TurtleMove.TRAVEL:
-			if(!penIsUp) raisePen();
-			moveTo(move.x,move.y);
-			break;
-		case TurtleMove.DRAW:
-			if(penIsUp) lowerPen();
-			moveTo(move.x,move.y);
-			break;
-		case TurtleMove.TOOL_CHANGE:
-			requestUserChangeTool((int)move.x);
-			break;
-		}
-	}
+  public void findHome() {
+    raisePen();
+    pos.set(settings.getHomeX(), settings.getHomeY());
+    didFindHome = true;
+    notifyListeners(new PlotterEvent(PlotterEvent.HOME_FOUND, this));
+  }
 
-	public double getPenUpAngle() {
-		return settings.getPenUpAngle();
-	}
+  public boolean getDidFindHome() {
+    return didFindHome;
+  }
 
-	public double getPenDownAngle() {
-		return settings.getPenDownAngle();
-	}
+  public void setPos(double x, double y) {
+    pos.set(x, y);
+    notifyListeners(new PlotterEvent(PlotterEvent.POSITION, this));
+  }
 
-	public double getPenLiftTime() {
-		return settings.getPenLiftTime();
-	}
+  public void setPos(Point2D p) {
+    pos.set(p);
+    notifyListeners(new PlotterEvent(PlotterEvent.POSITION, this));
+  }
 
-	public double getLimitBottom() {
-		return settings.getLimitBottom();
-	}
+  public void moveTo(double x, double y) {
+    setPos(x, y);
+  }
 
-	public double getLimitLeft() {
-		return settings.getLimitLeft();
-	}
+  public void moveTo(Point2D p) {
+    setPos(p);
+  }
 
-	public double getLimitRight() {
-		return settings.getLimitRight();
-	}
+  public Point2D getPos() {
+    return pos;
+  }
 
-	public double getLimitTop() {
-		return settings.getLimitTop();
-	}
-	
+  public boolean getAreMotorsEngaged() {
+    return areMotorsEngaged;
+  }
+
+  public void disengageMotors() {
+    areMotorsEngaged = false;
+    notifyListeners(new PlotterEvent(PlotterEvent.MOTORS_ENGAGED, this, false));
+  }
+
+  public void engageMotors() {
+    areMotorsEngaged = true;
+    notifyListeners(new PlotterEvent(PlotterEvent.MOTORS_ENGAGED, this, true));
+  }
+
+  public PlotterSettings getSettings() {
+    return settings;
+  }
+
+  public void setSettings(PlotterSettings s) {
+    settings = s;
+  }
+
+  @Override public void render(GL2 gl2) {
+    float[] lineWidthBuf = new float[1];
+    gl2.glGetFloatv(GL2.GL_LINE_WIDTH, lineWidthBuf, 0);
+    drawPhysicalLimits(gl2);
+    settings.getHardwareProperties().render(gl2, this);
+    gl2.glLineWidth(lineWidthBuf[0]);
+  }
+
+  private void drawPhysicalLimits(GL2 gl2) {
+    gl2.glLineWidth(2);
+    gl2.glColor3f(0.7f, 0.7f, 0.7f);
+    gl2.glBegin(GL2.GL_LINE_LOOP);
+    gl2.glVertex2d(settings.getLimitLeft(), settings.getLimitTop());
+    gl2.glVertex2d(settings.getLimitRight(), settings.getLimitTop());
+    gl2.glVertex2d(settings.getLimitRight(), settings.getLimitBottom());
+    gl2.glVertex2d(settings.getLimitLeft(), settings.getLimitBottom());
+    gl2.glEnd();
+  }
+
+  public boolean getPenIsUp() {
+    return penIsUp;
+  }
+
+  public void turtleMove(TurtleMove move) {
+    switch (move.type) {
+      case TurtleMove.TRAVEL:
+      if (!penIsUp) {
+        raisePen();
+      }
+      moveTo(move.x, move.y);
+      break;
+      case TurtleMove.DRAW:
+      if (penIsUp) {
+        lowerPen();
+      }
+      moveTo(move.x, move.y);
+      break;
+      case TurtleMove.TOOL_CHANGE:
+      requestUserChangeTool((int) move.x);
+      break;
+    }
+  }
+
+  public double getPenUpAngle() {
+    return settings.getPenUpAngle();
+  }
+
+  public double getPenDownAngle() {
+    return settings.getPenDownAngle();
+  }
+
+  public double getPenLiftTime() {
+    return settings.getPenLiftTime();
+  }
+
+  public double getLimitBottom() {
+    return settings.getLimitBottom();
+  }
+
+  public double getLimitLeft() {
+    return settings.getLimitLeft();
+  }
+
+  public double getLimitRight() {
+    return settings.getLimitRight();
+  }
+
+  public double getLimitTop() {
+    return settings.getLimitTop();
+  }
 }
