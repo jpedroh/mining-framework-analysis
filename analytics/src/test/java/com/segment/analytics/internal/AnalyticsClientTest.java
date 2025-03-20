b705346277ae29315841f1632382251701dde3b1
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 20:01:16 +0000

    Mining Framework Analysis

--- analytics/src/test/java/com/segment/analytics/internal/AnalyticsClientTest.java
+++ analytics/src/test/java/com/segment/analytics/internal/AnalyticsClientTest.java
@@ -3,12 +3,12 @@ package com.segment.analytics.internal;
 import static com.segment.analytics.internal.FlushMessage.POISON;
 import static com.segment.analytics.internal.StopMessage.STOP;
 import static org.assertj.core.api.Assertions.assertThat;
-
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.argThat;
-import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.*;
 import static org.mockito.MockitoAnnotations.openMocks;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verifyNoInteractions;
 
 import com.segment.analytics.Callback;
 import com.segment.analytics.Log;
