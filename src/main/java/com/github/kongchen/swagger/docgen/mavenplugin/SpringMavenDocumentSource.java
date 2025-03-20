e690083377b6ade60a0d116a27737e4ce54c0f2c
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 17:48:30 +0000

    Mining Framework Analysis

--- src/main/java/com/github/kongchen/swagger/docgen/mavenplugin/SpringMavenDocumentSource.java
+++ src/main/java/com/github/kongchen/swagger/docgen/mavenplugin/SpringMavenDocumentSource.java
@@ -1,14 +1,11 @@
 package com.github.kongchen.swagger.docgen.mavenplugin;
-
 import java.util.Set;
-
-import org.apache.maven.plugin.MojoFailureException;
-import org.apache.maven.plugin.logging.Log;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.github.kongchen.swagger.docgen.AbstractDocumentSource;
+import org.apache.maven.plugin.MojoFailureException;
 import com.github.kongchen.swagger.docgen.GenerateException;
+import org.apache.maven.plugin.logging.Log;
 import com.github.kongchen.swagger.docgen.reader.ClassSwaggerReader;
+import org.springframework.web.bind.annotation.RestController;
 import com.github.kongchen.swagger.docgen.reader.SpringMvcApiReader;
 import com.google.common.collect.Sets;
 
@@ -19,30 +16,25 @@ import com.google.common.collect.Sets;
  * 05/13/2013
  */
 public class SpringMavenDocumentSource extends AbstractDocumentSource {
-
-    public SpringMavenDocumentSource(ApiSource apiSource, Log log, String encoding) throws MojoFailureException {
-        super(log, apiSource);
-        if(encoding !=null) {
-            this.encoding = encoding;
-        }
+  public SpringMavenDocumentSource(ApiSource apiSource, Log log, String encoding) throws MojoFailureException {
+    super(log, apiSource);
+    if (encoding != null) {
+      this.encoding = encoding;
     }
+  }
 
-    @Override
-    protected Set<Class<?>> getValidClasses() {
-        return Sets.union(
-                super.getValidClasses(),
-                apiSource.getValidClasses(RestController.class));
-    }
+  @Override protected Set<Class<?>> getValidClasses() {
+    return Sets.union(super.getValidClasses(), apiSource.getValidClasses(RestController.class));
+  }
 
-    @Override
-    protected ClassSwaggerReader resolveApiReader() throws GenerateException {
-        String customReaderClassName = apiSource.getSwaggerApiReader();
-        if (customReaderClassName == null) {
-            SpringMvcApiReader reader = new SpringMvcApiReader(swagger, LOG);
-            reader.setTypesToSkip(this.typesToSkip);
-            return reader;
-        } else {
-            return getCustomApiReader(customReaderClassName);
-        }
+  @Override protected ClassSwaggerReader resolveApiReader() throws GenerateException {
+    String customReaderClassName = apiSource.getSwaggerApiReader();
+    if (customReaderClassName == null) {
+      SpringMvcApiReader reader = new SpringMvcApiReader(swagger, LOG);
+      reader.setTypesToSkip(this.typesToSkip);
+      return reader;
+    } else {
+      return getCustomApiReader(customReaderClassName);
     }
+  }
 }
