3adb2e1950e58bea0740d6d569630ebd193f1e91
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 22:10:05 +0000

    Mining Framework Analysis

--- src/main/java/org/openpnp/spi/base/AbstractNozzle.java
+++ src/main/java/org/openpnp/spi/base/AbstractNozzle.java
@@ -1,13 +1,10 @@
 package org.openpnp.spi.base;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-
 import javax.swing.Icon;
-
 import org.openpnp.gui.support.Icons;
 import org.openpnp.model.AbstractModelObject;
 import org.openpnp.model.Configuration;
@@ -21,103 +18,87 @@ import org.simpleframework.xml.Attribute;
 import org.simpleframework.xml.ElementList;
 
 public abstract class AbstractNozzle extends AbstractModelObject implements Nozzle {
-    @Attribute
-    protected String id;
+  @Attribute protected String id;
 
-    @Attribute(required = false)
-    protected String name;
-    
-    @ElementList(required = false)
-    protected List<String> compatibleNozzleTipIds = new ArrayList<>();
+  @ElementList(required = false) protected List<String> compatibleNozzleTipIds = new ArrayList<>();
 
-    protected Set<NozzleTip> compatibleNozzleTips; 
+  @Attribute(required = false) protected String name;
 
-    protected Head head;
-    
-    protected Part part;
-    
-    public AbstractNozzle() {
-        this.id = Configuration.createId("NOZ");
-        this.name = getClass().getSimpleName();
-    }
-    
-    @Override
-    public String getId() {
-        return id;
-    }
+  protected Set<NozzleTip> compatibleNozzleTips;
 
-    @Override
-    public Head getHead() {
-        return head;
-    }
+  protected Head head;
 
-    @Override
-    public void setHead(Head head) {
-        this.head = head;
-    }
+  protected Part part;
 
-    @Override
-    public Location getCameraToolCalibratedOffset(Camera camera) {
-        return new Location(camera.getUnitsPerPixel().getUnits());
-    }
+  public AbstractNozzle() {
+    this.id = Configuration.createId("NOZ");
+    this.name = getClass().getSimpleName();
+  }
 
-    @Override
-    public String getName() {
-        return name;
-    }
+  @Override public String getId() {
+    return id;
+  }
 
-    @Override
-    public void setName(String name) {
-        this.name = name;
-        firePropertyChange("name", null, name);
-    }
-    
-    @Override
-    public Part getPart() {
-        return part;
-    }
+  @Override public Head getHead() {
+    return head;
+  }
 
-    @Override
-    public Icon getPropertySheetHolderIcon() {
-        return Icons.captureTool;
+  @Override public void setHead(Head head) {
+    this.head = head;
+  }
+
+  @Override public Location getCameraToolCalibratedOffset(Camera camera) {
+    return new Location(camera.getUnitsPerPixel().getUnits());
+  }
+
+  @Override public String getName() {
+    return name;
+  }
+
+  @Override public void setName(String name) {
+    this.name = name;
+    firePropertyChange("name", null, name);
+  }
+
+  @Override public Part getPart() {
+    return part;
+  }
+
+  @Override public Icon getPropertySheetHolderIcon() {
+    return Icons.captureTool;
+  }
+
+  protected void syncCompatibleNozzleTipIds() {
+    compatibleNozzleTipIds.clear();
+    for (NozzleTip nt : compatibleNozzleTips) {
+      compatibleNozzleTipIds.add(nt.getId());
     }
-    
-    protected void syncCompatibleNozzleTipIds() {
-        compatibleNozzleTipIds.clear();
-        for (NozzleTip nt : compatibleNozzleTips) {
-            compatibleNozzleTipIds.add(nt.getId());
+  }
+
+  @Override public Set<NozzleTip> getCompatibleNozzleTips() {
+    if (compatibleNozzleTips == null) {
+      compatibleNozzleTips = new HashSet<>();
+      for (String nozzleTipId : compatibleNozzleTipIds) {
+        NozzleTip nt = Configuration.get().getMachine().getNozzleTip(nozzleTipId);
+        if (nt != null) {
+          compatibleNozzleTips.add(nt);
         }
+      }
     }
+    return Collections.unmodifiableSet(compatibleNozzleTips);
+  }
 
-    @Override
-    public Set<NozzleTip> getCompatibleNozzleTips() {
-        if (compatibleNozzleTips == null) {
-            compatibleNozzleTips = new HashSet<>();
-            for (String nozzleTipId : compatibleNozzleTipIds) {
-                NozzleTip nt = Configuration.get().getMachine().getNozzleTip(nozzleTipId);
-                if (nt != null) {
-                    compatibleNozzleTips.add(nt);
-                }
-            }
-        }
-        return Collections.unmodifiableSet(compatibleNozzleTips);
-    }
+  @Override public void addCompatibleNozzleTip(NozzleTip nt) {
+    getCompatibleNozzleTips();
+    compatibleNozzleTips.add(nt);
+    syncCompatibleNozzleTipIds();
+    firePropertyChange("compatibleNozzleTips", null, getCompatibleNozzleTips());
+  }
 
-    @Override
-    public void addCompatibleNozzleTip(NozzleTip nt) {
-        // Makes sure the structure has been initialized.
-        getCompatibleNozzleTips();
-        compatibleNozzleTips.add(nt);
-        syncCompatibleNozzleTipIds();
-        firePropertyChange("compatibleNozzleTips", null, getCompatibleNozzleTips());
-    }
-
-    @Override
-    public void removeCompatibleNozzleTip(NozzleTip nt) {
-        // Makes sure the structure has been initialized.
-        getCompatibleNozzleTips();
-        compatibleNozzleTips.remove(nt);
-        syncCompatibleNozzleTipIds();
-        firePropertyChange("compatibleNozzleTips", null, getCompatibleNozzleTips());
-    }
+  @Override public void removeCompatibleNozzleTip(NozzleTip nt) {
+    getCompatibleNozzleTips();
+    compatibleNozzleTips.remove(nt);
+    syncCompatibleNozzleTipIds();
+    firePropertyChange("compatibleNozzleTips", null, getCompatibleNozzleTips());
+  }
 }
