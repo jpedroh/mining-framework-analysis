44f8cdaec86ead9b49e0726f5fa1ea923317026d
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 16:49:40 +0000

    Mining Framework Analysis

--- src/main/java/org/cts/CRSFactory.java
+++ src/main/java/org/cts/CRSFactory.java
@@ -1,43 +1,10 @@
-/*
- * Coordinate Transformations Suite (abridged CTS)  is a library developped to 
- * perform Coordinate Transformations using well known geodetic algorithms 
- * and parameter sets. 
- * Its main focus are simplicity, flexibility, interoperability, in this order.
- *
- * This library has been originally developed by Michaël Michaud under the JGeod
- * name. It has been renamed CTS in 2009 and shared to the community from 
- * the Atelier SIG code repository.
- * 
- * Since them, CTS is supported by the Atelier SIG team in collaboration with Michaël 
- * Michaud.
- * The new CTS has been funded  by the French Agence Nationale de la Recherche 
- * (ANR) under contract ANR-08-VILL-0005-01 and the regional council 
- * "Région Pays de La Loire" under the projet SOGVILLE (Système d'Orbservation 
- * Géographique de la Ville).
- *
- * CTS is free software: you can redistribute it and/or modify it under the
- * terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any later
- * version.
- *
- * CTS is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with
- * CTS. If not, see <http://www.gnu.org/licenses/>.
- *
- * For more information, please consult: <https://github.com/irstv/cts/>
- */
 package org.cts;
-
 import org.cts.crs.CRSException;
 import org.cts.crs.CRSHelper;
 import org.cts.crs.CoordinateReferenceSystem;
 import org.cts.parser.prj.PrjParser;
 import org.cts.registry.Registry;
 import org.cts.registry.RegistryManager;
-
 import java.io.*;
 import java.nio.charset.Charset;
 import java.util.LinkedHashMap;
@@ -62,17 +29,17 @@ import org.cts.registry.RegistryException;
  * @author Erwan Bocher
  */
 public class CRSFactory {
+  private RegistryManager registryManager = new RegistryManager();
 
-    private RegistryManager registryManager = new RegistryManager();
-    protected final CRSCache<String, CoordinateReferenceSystem> CRSPOOL = new CRSCache<String, CoordinateReferenceSystem>(10);
+  protected final CRSCache<String, CoordinateReferenceSystem> CRSPOOL = new CRSCache<String, CoordinateReferenceSystem>(10);
 
-    /**
+  /**
      * Creates a new factory.
      */
-    public CRSFactory() {
-    }
+  public CRSFactory() {
+  }
 
-    /**
+  /**
      * Return a @CoordinateReferenceSystem according an authority and a srid 
      * ie :
      * EPSG:4326 or IGNF:LAMBE
@@ -81,77 +48,76 @@ public class CRSFactory {
      * @return
      * @throws CRSException
      */
-    public CoordinateReferenceSystem getCRS(String authorityAndSrid) throws CRSException, RegistryException {
-        CoordinateReferenceSystem crs = CRSPOOL.get(authorityAndSrid);
-        if (crs == null) {
-            String[] registryNameWithCode = splitRegistryNameAndCode(authorityAndSrid);
-            if (isRegistrySupported(registryNameWithCode[0])) {
-                Registry registry = getRegistryManager().getRegistry(registryNameWithCode[0]);
-                Map<String, String> crsParameters = registry.getParameters(registryNameWithCode[1]);
-                if (crsParameters != null) {
-                    crs = CRSHelper.createCoordinateReferenceSystem(new Identifier(registryNameWithCode[0], registryNameWithCode[1], crsParameters.get("title")), crsParameters);
-                }
-                if (crs != null) {
-                    CRSPOOL.put(authorityAndSrid, crs);
-                }
-            }
+  public CoordinateReferenceSystem getCRS(String authorityAndSrid) throws CRSException, RegistryException {
+    CoordinateReferenceSystem crs = CRSPOOL.get(authorityAndSrid);
+    if (crs == null) {
+      String[] registryNameWithCode = splitRegistryNameAndCode(authorityAndSrid);
+      if (isRegistrySupported(registryNameWithCode[0])) {
+        Registry registry = getRegistryManager().getRegistry(registryNameWithCode[0]);
+        Map<String, String> crsParameters = registry.getParameters(registryNameWithCode[1]);
+        if (crsParameters != null) {
+          crs = CRSHelper.createCoordinateReferenceSystem(new Identifier(registryNameWithCode[0], registryNameWithCode[1], crsParameters.get("title")), crsParameters);
         }
-        return crs;
+        if (crs != null) {
+          CRSPOOL.put(authorityAndSrid, crs);
+        }
+      }
     }
+    return crs;
+  }
 
-    /**
+  /**
      * Return the registry name and the code base on the pattern name:code
      * ed : epsg:4326 returns epsg;4326
      * @param authorityAndSrid
      * @return
      * @throws RegistryException 
      */
-    public String[] splitRegistryNameAndCode(String authorityAndSrid) throws RegistryException {
-        String[] registryAndCode = authorityAndSrid.split(":");
-        if (registryAndCode.length == 2) {
-            return registryAndCode;
-        } else {
-            throw new RegistryException("This registry pattern " + authorityAndSrid + " is not supported");
-        }
-
+  public String[] splitRegistryNameAndCode(String authorityAndSrid) throws RegistryException {
+    String[] registryAndCode = authorityAndSrid.split(":");
+    if (registryAndCode.length == 2) {
+      return registryAndCode;
+    } else {
+      throw new RegistryException("This registry pattern " + authorityAndSrid + " is not supported");
     }
+  }
 
-    /**
+  /**
      * Return the registry manager
      *
      * @return
      */
-    public RegistryManager getRegistryManager() {
-        return registryManager;
-    }
+  public RegistryManager getRegistryManager() {
+    return registryManager;
+  }
 
-    /**
+  /**
      * Check if the registry name is supported.
      *
      * @param registryName
      * @return
      */
-    public boolean isRegistrySupported(String registryName) throws RegistryException {
-        if (getRegistryManager().contains(registryName.toLowerCase())) {
-            return true;
-        } else {
-            throw new RegistryException("This registry " + registryName + " is not supported");
-        }
+  public boolean isRegistrySupported(String registryName) throws RegistryException {
+    if (getRegistryManager().contains(registryName.toLowerCase())) {
+      return true;
+    } else {
+      throw new RegistryException("This registry " + registryName + " is not supported");
     }
+  }
 
-    /**
+  /**
      * Creates a {@link CoordinateReferenceSystem} defined by an OGC WKT String
      * (PRJ).
      *
      * @param prjString the PRJ String
      * @return a {@link CoordinateReferenceSystem}
      */
-    public CoordinateReferenceSystem createFromPrj(String prjString) {
-        PrjParser p = new PrjParser();
-        return p.parse(prjString);
-    }
+  public CoordinateReferenceSystem createFromPrj(String prjString) {
+    PrjParser p = new PrjParser();
+    return p.parse(prjString);
+  }
 
-    /**
+  /**
      * Creates a {@link CoordinateReferenceSystem} defined by an OGC WKT String
      * (PRJ).
      *
@@ -160,16 +126,16 @@ public class CRSFactory {
      * @return a {@link CoordinateReferenceSystem}
      * @throws IOException
      */
-    public CoordinateReferenceSystem createFromPrj(InputStream stream, Charset encoding) throws IOException {
-        BufferedReader r = new BufferedReader(new InputStreamReader(stream, encoding));
-        StringBuilder b = new StringBuilder();
-        while (r.ready()) {
-            b.append(r.readLine());
-        }
-        return createFromPrj(b.toString());
+  public CoordinateReferenceSystem createFromPrj(InputStream stream, Charset encoding) throws IOException {
+    BufferedReader r = new BufferedReader(new InputStreamReader(stream, encoding));
+    StringBuilder b = new StringBuilder();
+    while (r.ready()) {
+      b.append(r.readLine());
     }
+    return createFromPrj(b.toString());
+  }
 
-    /**
+  /**
      * Creates a {@link CoordinateReferenceSystem} defined by an OGC WKT String
      * (PRJ).
      *
@@ -177,11 +143,11 @@ public class CRSFactory {
      * @return
      * @throws IOException
      */
-    public CoordinateReferenceSystem createFromPrj(InputStream stream) throws IOException {
-        return createFromPrj(stream, Charset.defaultCharset());
-    }
+  public CoordinateReferenceSystem createFromPrj(InputStream stream) throws IOException {
+    return createFromPrj(stream, Charset.defaultCharset());
+  }
 
-    /**
+  /**
      * Creates a {@link CoordinateReferenceSystem} defined by an OGC WKT String
      * (PRJ).
      *
@@ -189,44 +155,40 @@ public class CRSFactory {
      * @return a {@link CoordinateReferenceSystem}
      * @throws IOException if there is a problem reading the file
      */
-    public CoordinateReferenceSystem createFromPrj(File file) throws IOException {
-        InputStream i = null;
-        CoordinateReferenceSystem crs;
-        try {
-            i = new FileInputStream(file);
-            crs = createFromPrj(i);
-        } finally {
-            if (i != null) {
-                i.close();
-            }
-        }
-        return crs;
+  public CoordinateReferenceSystem createFromPrj(File file) throws IOException {
+    InputStream i = null;
+    CoordinateReferenceSystem crs;
+    try {
+      i = new FileInputStream(file);
+      crs = createFromPrj(i);
+    }  finally {
+      if (i != null) {
+        i.close();
+      }
     }
+    return crs;
+  }
 
-    /**
+  /**
      * Return a list of supported codes according an registryName
-     * @param registryName
-     * @return 
+     *
+     * @param registeryName
+     * @return
      */
-    public Set<String> getSupportedCodes(String registryName) throws RegistryException {
-        return getRegistryManager().getRegistry(registryName).getSupportedCodes();
+  public Set<String> getSupportedCodes(String registryName) throws RegistryException {
+    return getRegistryManager().getRegistry(registryName).getSupportedCodes();
+  }
+
+  public class CRSCache<K extends java.lang.Object, V extends java.lang.Object> extends LinkedHashMap<K, V> {
+    private final int limit;
+
+    public CRSCache(int limit) {
+      super(16, 0.75f, true);
+      this.limit = limit;
     }
 
-    /**
-     * A simple cache to manage {@link CoordinateReferenceSystem}
-     */
-    public class CRSCache<K, V> extends LinkedHashMap<K, V> {
-
-        private final int limit;
-
-        public CRSCache(int limit) {
-            super(16, 0.75f, true);
-            this.limit = limit;
-        }
-
-        @Override
-        protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
-            return size() > limit;
-        }
+    @Override protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
+      return size() > limit;
     }
+  }
 }
