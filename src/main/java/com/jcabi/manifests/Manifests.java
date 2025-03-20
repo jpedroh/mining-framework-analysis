0c82f898050b50ceda72729362ff80d1d4e65844
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 05:39:25 +0000

    Mining Framework Analysis

--- src/main/java/com/jcabi/manifests/Manifests.java
+++ src/main/java/com/jcabi/manifests/Manifests.java
@@ -1,34 +1,4 @@
-/*
- * Copyright (c) 2012-2022, jcabi.com
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met: 1) Redistributions of source code must retain the above
- * copyright notice, this list of conditions and the following
- * disclaimer. 2) Redistributions in binary form must reproduce the above
- * copyright notice, this list of conditions and the following
- * disclaimer in the documentation and/or other materials provided
- * with the distribution. 3) Neither the name of the jcabi.com nor
- * the names of its contributors may be used to endorse or promote
- * products derived from this software without specific prior written
- * permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
- * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
- * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
- * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- * OF THE POSSIBILITY OF SUCH DAMAGE.
- */
 package com.jcabi.manifests;
-
 import com.jcabi.log.Logger;
 import java.io.File;
 import java.io.IOException;
@@ -114,124 +84,101 @@ import javax.servlet.ServletContext;
  *  &lt;artifactId>jcabi-manifests&lt;/artifactId>
  * &lt;/dependency></pre>
  *
+ * @author Yegor Bugayenko (yegor@tpc2.com)
+ * @version $Id$
  * @since 0.7
- * @link <a href="http://download.oracle.com/javase/1,5.0/docs/guide/jar/jar.html#JAR%20Manifest">JAR Manifest</a>
- * @link <a href="http://maven.apache.org/shared/maven-archiver/index.html">Maven Archiver</a>
- * @link <a href="http://manifests.jcabi.com/index.html">manifests.jcabi.com</a>
- * @link <a href="http://www.yegor256.com/2014/07/03/how-to-read-manifest-mf.html">How to Read MANIFEST.MF Files</a>
+ * @see <a href="http://download.oracle.com/javase/1,5.0/docs/guide/jar/jar.html#JAR%20Manifest">JAR Manifest</a>
+ * @see <a href="http://maven.apache.org/shared/maven-archiver/index.html">Maven Archiver</a>
+ * @see <a href="http://manifests.jcabi.com/index.html">manifests.jcabi.com</a>
+ * @see <a href="http://www.yegor256.com/2014/07/03/how-to-read-manifest-mf.html">How to Read MANIFEST.MF Files</a>
  */
-@SuppressWarnings({"PMD.TooManyMethods", "PMD.ProhibitPublicStaticMethods"})
-public final class Manifests implements MfMap {
-
-    /**
+@SuppressWarnings(value = { "PMD.TooManyMethods", "PMD.ProhibitPublicStaticMethods" }) public final class Manifests implements MfMap {
+  /**
      * Default singleton.
      */
-    private static final AtomicReference<MfMap> DEFAULT =
-        new AtomicReference<>(new Manifests());
+  private static final AtomicReference<MfMap> DEFAULT = new AtomicReference<>(new Manifests());
 
-    /**
+  /**
      * Attributes retrieved.
      */
-    private final transient Map<String, String> attributes;
+  private final transient Map<String, String> attributes;
 
-    static {
-        try {
-            final MfMap instance = Manifests.DEFAULT.get();
-            Manifests.DEFAULT.compareAndSet(
-                instance,
-                instance.append(new ClasspathMfs())
-            );
-        } catch (final IOException ex) {
-            Manifests.logLoadFailedError(ex);
-        }
+  static {
+    try {
+      final MfMap instance = Manifests.DEFAULT.get();
+      Manifests.DEFAULT.compareAndSet(instance, instance.append(new ClasspathMfs()));
+    } catch (final IOException ex) {
+      Manifests.logLoadFailedError(ex);
     }
+  }
 
-    /**
+  /**
      * Public ctor.
      * @since 1.0
      */
-    public Manifests() {
-        this(new HashMap<>(0));
-    }
+  public Manifests() {
+    this(new HashMap<>(0));
+  }
 
-    /**
+  /**
      * Public ctor.
      * @param attrs Attributes to encapsulate
      * @since 1.0
      */
-    public Manifests(final Map<String, String> attrs) {
-        super();
-        this.attributes = new ConcurrentHashMap<>(attrs);
-    }
+  public Manifests(final Map<String, String> attrs) {
+    super();
+    this.attributes = new ConcurrentHashMap<>(attrs);
+  }
 
-    @Override
-    public int size() {
-        return this.attributes.size();
-    }
+  @Override public int size() {
+    return this.attributes.size();
+  }
 
-    @Override
-    public boolean isEmpty() {
-        return this.attributes.isEmpty();
-    }
+  @Override public boolean isEmpty() {
+    return this.attributes.isEmpty();
+  }
 
-    @Override
-    public boolean containsKey(final String key) {
-        return this.attributes.containsKey(key);
-    }
+  @Override public boolean containsKey(final String key) {
+    return this.attributes.containsKey(key);
+  }
 
-    @Override
-    public boolean containsValue(final String value) {
-        return this.attributes.containsValue(value);
-    }
+  @Override public boolean containsValue(final String value) {
+    return this.attributes.containsValue(value);
+  }
 
-    @Override
-    public String get(final String key) {
-        return this.attributes.get(key);
-    }
+  @Override public String get(final String key) {
+    return this.attributes.get(key);
+  }
 
-    @Override
-    public Map<String, String> getAsMap() {
-        return new HashMap<>(this.attributes);
-    }
+  @Override public Map<String, String> getAsMap() {
+    return new HashMap<>(this.attributes);
+  }
 
-    @Override
-    public Set<String> keySet() {
-        return new HashSet<>(this.attributes.keySet());
-    }
+  @Override public Set<String> keySet() {
+    return new HashSet<>(this.attributes.keySet());
+  }
 
-    @Override
-    @SuppressWarnings("PMD.GuardLogStatement")
-    public MfMap append(final Mfs streams) throws IOException {
-        final ConcurrentHashMap<String, String> joined =
-            new ConcurrentHashMap<>(this.attributes);
-        final long start = System.currentTimeMillis();
-        final Collection<InputStream> list = streams.fetch();
-        int saved = 0;
-        int ignored = 0;
-        for (final InputStream stream : list) {
-            for (final Map.Entry<String, String> attr
-                : Manifests.load(stream).entrySet()) {
-                if (joined.containsKey(attr.getKey())) {
-                    ++ignored;
-                } else {
-                    joined.put(attr.getKey(), attr.getValue());
-                    ++saved;
-                }
-            }
+  @Override @SuppressWarnings(value = { "PMD.GuardLogStatement" }) public MfMap append(final Mfs streams) throws IOException {
+    final ConcurrentHashMap<String, String> joined = new ConcurrentHashMap<>(this.attributes);
+    final long start = System.currentTimeMillis();
+    final Collection<InputStream> list = streams.fetch();
+    int saved = 0;
+    int ignored = 0;
+    for (final InputStream stream : list) {
+      for (final Map.Entry<String, String> attr : Manifests.load(stream).entrySet()) {
+        if (joined.containsKey(attr.getKey())) {
+          ++ignored;
+        } else {
+          joined.put(attr.getKey(), attr.getValue());
+          ++saved;
         }
-        Logger.info(
-            this,
-            // @checkstyle LineLength (1 line)
-            "%d attributes loaded from %d stream(s) in %[ms]s, %d saved, %d ignored: %[list]s",
-            this.attributes.size(), list.size(),
-            System.currentTimeMillis() - start,
-            saved, ignored,
-            new TreeSet<>(this.attributes.keySet())
-        );
-        return new Manifests(joined);
+      }
     }
+    Logger.info(this, "%d attributes loaded from %d stream(s) in %[ms]s, %d saved, %d ignored: %[list]s", this.attributes.size(), list.size(), System.currentTimeMillis() - start, saved, ignored, new TreeSet<>(this.attributes.keySet()));
+    return new Manifests(joined);
+  }
 
-    /**
+  /**
      * Read one attribute available in one of {@code MANIFEST.MF} files.
      *
      * <p>If such a attribute doesn't exist {@link IllegalArgumentException}
@@ -243,24 +190,16 @@ public final class Manifests implements MfMap {
      * @param name Name of the attribute
      * @return The value of the attribute retrieved
      */
-    public static String read(final String name) {
-        Assertions.notNull(name);
-        Assertions.notEmpty(name);
-        if (!Manifests.exists(name)) {
-            throw new IllegalArgumentException(
-                Logger.format(
-                    // @checkstyle LineLength (1 line)
-                    "Attribute '%s' not found in MANIFEST.MF file(s) among %d other attribute(s): %[list]s",
-                    name,
-                    Manifests.DEFAULT.get().size(),
-                    new TreeSet<>(Manifests.DEFAULT.get().keySet())
-                )
-            );
-        }
-        return Manifests.DEFAULT.get().get(name);
+  public static String read(final String name) {
+    Assertions.notNull(name);
+    Assertions.notEmpty(name);
+    if (!Manifests.exists(name)) {
+      throw new IllegalArgumentException(Logger.format("Attribute \'%s\' not found in MANIFEST.MF file(s) among %d other attribute(s): %[list]s", name, Manifests.DEFAULT.get().size(), new TreeSet<>(Manifests.DEFAULT.get().keySet())));
     }
+    return Manifests.DEFAULT.get().get(name);
+  }
 
-    /**
+  /**
      * Read one attribute available in one of {@code MANIFEST.MF} files.
      *
      * <p>If such a attribute doesn't exist, return default value.
@@ -271,19 +210,19 @@ public final class Manifests implements MfMap {
      * @param def Default value of the attribute
      * @return The value of the attribute retrieved or default value
      */
-    public static String read(final String name, final String def) {
-        Assertions.notNull(name);
-        Assertions.notEmpty(name);
-        String value;
-        if (Manifests.exists(name)) {
-            value = Manifests.DEFAULT.get().get(name);
-        } else {
-            value = def;
-        }
-        return value;
+  public static String read(final String name, final String def) {
+    Assertions.notNull(name);
+    Assertions.notEmpty(name);
+    String value;
+    if (Manifests.exists(name)) {
+      value = Manifests.DEFAULT.get().get(name);
+    } else {
+      value = def;
     }
+    return value;
+  }
 
-    /**
+  /**
      * Check whether attribute exists in any of {@code MANIFEST.MF} files.
      *
      * <p>Use this method before {@link #read(String)} to check whether an
@@ -294,17 +233,17 @@ public final class Manifests implements MfMap {
      * @param name Name of the attribute to check
      * @return Returns {@code TRUE} if it exists, {@code FALSE} otherwise
      */
-    public static boolean exists(final String name) {
-        if (name == null) {
-            throw new IllegalArgumentException("attribute name can't be NULL");
-        }
-        if (name.isEmpty()) {
-            throw new IllegalArgumentException("attribute name can't be empty");
-        }
-        return Manifests.DEFAULT.get().containsKey(name);
+  public static boolean exists(final String name) {
+    if (name == null) {
+      throw new IllegalArgumentException("attribute name can\'t be NULL");
     }
+    if (name.isEmpty()) {
+      throw new IllegalArgumentException("attribute name can\'t be empty");
+    }
+    return Manifests.DEFAULT.get().containsKey(name);
+  }
 
-    /**
+  /**
      * Append attributes from the web application {@code MANIFEST.MF}.
      *
      * <p>The method is deprecated. Instead, use this code:
@@ -316,16 +255,12 @@ public final class Manifests implements MfMap {
      * @throws IOException If some I/O problem inside
      * @deprecated Use {@link #append(Mfs)} and {@link ServletMfs} instead
      */
-    @Deprecated
-    public static void append(final ServletContext ctx) throws IOException {
-        final MfMap current = Manifests.DEFAULT.get();
-        Manifests.DEFAULT.compareAndSet(
-            current,
-            current.append(new ServletMfs(ctx))
-        );
-    }
+  @Deprecated public static void append(final ServletContext ctx) throws IOException {
+    final MfMap current = Manifests.DEFAULT.get();
+    Manifests.DEFAULT.compareAndSet(current, current.append(new ServletMfs(ctx)));
+  }
 
-    /**
+  /**
      * Append attributes from the file.
      *
      * <p>The method is deprecated. Instead, use this code:
@@ -336,19 +271,15 @@ public final class Manifests implements MfMap {
      * @throws IOException If some I/O problem inside
      * @deprecated Use {@link #append(Mfs)} and {@link FilesMfs} instead
      */
-    @Deprecated
-    public static void append(final File file) throws IOException {
-        if (file == null) {
-            throw new IllegalArgumentException("file can't be NULL");
-        }
-        final MfMap current = Manifests.DEFAULT.get();
-        Manifests.DEFAULT.compareAndSet(
-            current,
-            current.append(new FilesMfs(file))
-        );
+  @Deprecated public static void append(final File file) throws IOException {
+    if (file == null) {
+      throw new IllegalArgumentException("file can\'t be NULL");
     }
+    final MfMap current = Manifests.DEFAULT.get();
+    Manifests.DEFAULT.compareAndSet(current, current.append(new FilesMfs(file)));
+  }
 
-    /**
+  /**
      * Append attributes from input stream.
      *
      * <p>The method is deprecated. Instead, use this code:
@@ -360,19 +291,15 @@ public final class Manifests implements MfMap {
      * @since 0.8
      * @deprecated Use {@link #append(Mfs)} and {@link StreamsMfs} instead
      */
-    @Deprecated
-    public static void append(final InputStream stream) throws IOException {
-        if (stream == null) {
-            throw new IllegalArgumentException("input stream can't be NULL");
-        }
-        final MfMap current = Manifests.DEFAULT.get();
-        Manifests.DEFAULT.compareAndSet(
-            current,
-            current.append(new StreamsMfs(stream))
-        );
+  @Deprecated public static void append(final InputStream stream) throws IOException {
+    if (stream == null) {
+      throw new IllegalArgumentException("input stream can\'t be NULL");
     }
+    final MfMap current = Manifests.DEFAULT.get();
+    Manifests.DEFAULT.compareAndSet(current, current.append(new StreamsMfs(stream)));
+  }
 
-    /**
+  /**
      * Load attributes from input stream.
      *
      * <p>Inside the method we catch {@code RuntimeException} (which may look
@@ -387,40 +314,30 @@ public final class Manifests implements MfMap {
      * @throws IOException If some problem happens
      * @since 0.8
      */
-    @SuppressWarnings("PMD.AvoidCatchingGenericException")
-    private static Map<String, String> load(final InputStream stream)
-        throws IOException {
-        final ConcurrentMap<String, String> props =
-            new ConcurrentHashMap<>(0);
-        try {
-            final Manifest manifest = new Manifest(stream);
-            final Attributes attrs = manifest.getMainAttributes();
-            for (final Object key : attrs.keySet()) {
-                final String value = attrs.getValue(
-                    Attributes.Name.class.cast(key)
-                );
-                props.put(key.toString(), value);
-            }
-            Logger.debug(
-                Manifests.class,
-                "%d attribute(s) loaded %[list]s",
-                props.size(), new TreeSet<>(props.keySet())
-            );
-        // @checkstyle IllegalCatch (1 line)
-        } catch (final RuntimeException ex) {
-            Manifests.logLoadFailedError(ex);
-        } finally {
-            stream.close();
-        }
-        return props;
+  @SuppressWarnings(value = { "PMD.AvoidCatchingGenericException" }) private static Map<String, String> load(final InputStream stream) throws IOException {
+    final ConcurrentMap<String, String> props = new ConcurrentHashMap<>(0);
+    try {
+      final Manifest manifest = new Manifest(stream);
+      final Attributes attrs = manifest.getMainAttributes();
+      for (final Object key : attrs.keySet()) {
+        final String value = attrs.getValue(Attributes.Name.class.cast(key));
+        props.put(key.toString(), value);
+      }
+      Logger.debug(Manifests.class, "%d attribute(s) loaded %[list]s", props.size(), new TreeSet<>(props.keySet()));
+    } catch (final RuntimeException ex) {
+      Manifests.logLoadFailedError(ex);
+    } finally {
+      stream.close();
     }
+    return props;
+  }
 
-    /**
+  /**
      * Report the exception that was just thrown.
      *
      * @param exn The exception to report
      */
-    private static void logLoadFailedError(final Exception exn) {
-        Logger.error(Manifests.class, "#load(): failed %[exception]s", exn);
-    }
+  private static void logLoadFailedError(final Exception exn) {
+    Logger.error(Manifests.class, "#load(): failed %[exception]s", exn);
+  }
 }
--- src/test/java/com/jcabi/manifests/ManifestsTest.java
+++ src/test/java/com/jcabi/manifests/ManifestsTest.java
@@ -1,34 +1,4 @@
-/*
- * Copyright (c) 2012-2022, jcabi.com
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met: 1) Redistributions of source code must retain the above
- * copyright notice, this list of conditions and the following
- * disclaimer. 2) Redistributions in binary form must reproduce the above
- * copyright notice, this list of conditions and the following
- * disclaimer in the documentation and/or other materials provided
- * with the distribution. 3) Neither the name of the jcabi.com nor
- * the names of its contributors may be used to endorse or promote
- * products derived from this software without specific prior written
- * permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
- * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
- * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
- * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- * OF THE POSSIBILITY OF SUCH DAMAGE.
- */
 package com.jcabi.manifests;
-
 import com.jcabi.log.Logger;
 import java.io.File;
 import java.io.IOException;
@@ -41,97 +11,76 @@ import org.junit.jupiter.api.Test;
 
 /**
  * Test case for {@link Manifests}.
- *
+ * @author Yegor Bugayenko (yegor@tpc2.com)
+ * @version $Id$
  * @since 0.7
  */
 public final class ManifestsTest {
+  /**
+     * Manifests can read a single attribute, which always exist in MANIFEST.MF.
+     * @throws Exception If something goes wrong
+     */
+  @Test public void readsSingleExistingAttribute() {
+    MatcherAssert.assertThat(Manifests.read("JCabi-Version"), Matchers.notNullValue());
+  }
 
-    @Test
-    public void readsSingleExistingAttribute() {
-        MatcherAssert.assertThat(
-            Manifests.read("JCabi-Version"),
-            Matchers.notNullValue()
-        );
-    }
-
-    /**
+  /**
      * Manifests can read a default value.
      * @throws Exception If something goes wrong
      */
-    @Test
-    public void readsSingleNotExistingAttribute() throws Exception {
-        final String value = "default";
-        MatcherAssert.assertThat(
-            Manifests.read("invalid-key", value),
-            Matchers.equalTo(value)
-        );
-    }
+  @Test public void readsSingleNotExistingAttribute() throws Exception {
+    final String value = "default";
+    MatcherAssert.assertThat(Manifests.read("invalid-key", value), Matchers.equalTo(value));
+  }
 
-    /**
+  /**
      * Manifests can throw an exception if an attribute is empty.
      * @throws Exception If something goes wrong
      */
-    @Test(expected = IllegalArgumentException.class)
-    public void throwsExceptionWhenAttributeIsEmpty() throws Exception {
-        Manifests.read("Jcabi-Test-Empty-Attribute");
-    }
+  @Test public void throwsExceptionWhenAttributeIsEmpty() {
+    Assertions.assertThrows(IllegalArgumentException.class, () -> Manifests.read("Jcabi-Test-Empty-Attribute"));
+  }
 
-    @Test
-    public void throwsExceptionWhenAttributeIsEmpty() {
-        Assertions.assertThrows(
-            IllegalArgumentException.class,
-            () -> Manifests.read("Jcabi-Test-Empty-Attribute")
-        );
-    }
+  /**
+     * Manifests can throw an exception when attribute is absent.
+     * @throws Exception If something goes wrong
+     */
+  @Test public void throwsExceptionIfAttributeIsMissed() {
+    Assertions.assertThrows(IllegalArgumentException.class, () -> Manifests.read("absent-property"));
+  }
 
-    @Test
-    public void throwsExceptionIfAttributeIsMissed() {
-        Assertions.assertThrows(
-            IllegalArgumentException.class,
-            () -> Manifests.read("absent-property")
-        );
-    }
+  /**
+     * Manifests can throw an exception loading file with empty attribute.
+     * @throws Exception If something goes wrong
+     */
+  @Test public void throwsExceptionWhenNoAttributes() {
+    Assertions.assertThrows(IOException.class, () -> {
+      final File file = new File(Thread.currentThread().getContextClassLoader().getResource("META-INF/MANIFEST_INVALID.MF").getFile());
+      final Manifests mfs = new Manifests();
+      mfs.append(new FilesMfs(file));
+    });
+  }
 
-    @Test
-    public void throwsExceptionWhenNoAttributes() {
-        Assertions.assertThrows(
-            IOException.class,
-            () -> {
-                final File file = new File(
-                    Thread.currentThread().getContextClassLoader()
-                        .getResource("META-INF/MANIFEST_INVALID.MF").getFile()
-                );
-                final Manifests mfs = new Manifests();
-                mfs.append(new FilesMfs(file));
-            }
-        );
-    }
+  /**
+     * Manifests can append attributes from file.
+     * @throws Exception If something goes wrong
+     */
+  @Test public void appendsAttributesFromFile() throws Exception {
+    final String name = "Test-Attribute-From-File";
+    final String value = "some text value of attribute";
+    final File file = File.createTempFile("test-", ".MF");
+    Files.write(file.toPath(), Logger.format("%s: %s\n", name, value).getBytes(StandardCharsets.UTF_8));
+    final MfMap manifests = new Manifests().append(new FilesMfs(file));
+    MatcherAssert.assertThat("loaded from file", manifests.containsKey(name) && manifests.get(name).equals(value));
+  }
 
-    @Test
-    public void appendsAttributesFromFile() throws Exception {
-        final String name = "Test-Attribute-From-File";
-        final String value = "some text value of attribute";
-        final File file = File.createTempFile("test-", ".MF");
-        Files.write(
-            file.toPath(),
-            Logger.format("%s: %s\n", name, value).getBytes(StandardCharsets.UTF_8)
-        );
-        final MfMap manifests = new Manifests().append(new FilesMfs(file));
-        MatcherAssert.assertThat(
-            "loaded from file",
-            manifests.containsKey(name) && manifests.get(name).equals(value)
-        );
-    }
-
-    @Test
-    public void appendsAttributesFromInputStream() throws Exception {
-        final MfMap manifests = new Manifests().append(
-            new StreamsMfs(this.getClass().getResourceAsStream("test.mf"))
-        );
-        MatcherAssert.assertThat(
-            manifests.get("From-File"),
-            Matchers.equalTo("some test attribute")
-        );
-    }
-
+  /**
+     * Manifests can append input stream.
+     * @throws Exception If something goes wrong
+     * @since 0.8
+     */
+  @Test public void appendsAttributesFromInputStream() throws Exception {
+    final MfMap manifests = new Manifests().append(new StreamsMfs(this.getClass().getResourceAsStream("test.mf")));
+    MatcherAssert.assertThat(manifests.get("From-File"), Matchers.equalTo("some test attribute"));
+  }
 }
