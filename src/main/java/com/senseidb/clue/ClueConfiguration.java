dc63f4ed17d1088873a0cc752ba06c5677f82919
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 23:57:25 +0000

    Mining Framework Analysis

--- src/main/java/com/senseidb/clue/ClueConfiguration.java
+++ src/main/java/com/senseidb/clue/ClueConfiguration.java
@@ -1,15 +1,12 @@
 package com.senseidb.clue;
-
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.URI;
 import java.util.Properties;
-
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.util.Version;
-
 import com.senseidb.clue.api.DefaultDirectoryBuilder;
 import com.senseidb.clue.api.DefaultIndexReaderFactory;
 import com.senseidb.clue.api.DefaultQueryBuilder;
@@ -18,19 +15,25 @@ import com.senseidb.clue.api.IndexReaderFactory;
 import com.senseidb.clue.api.QueryBuilder;
 
 public class ClueConfiguration {
-  
   private static final String CLUE_CONF_FILE = "clue.conf";
+
   private static final String DIRECTORY_BUILDER_PARAM = "directory.builder";
+
   private static final String ANALYZER_QUERY_PARAM = "analyzer.query";
+
   private static final String INDEX_READER_FACTORY_PARAM = "indexreader.factory";
+
   private static final String QUERY_BUILDER_PARAM = "query.builder";
-  
+
   private final Analyzer analyzerQuery;
+
   private final DirectoryBuilder dirBuilder;
+
   private final QueryBuilder queryBuilder;
+
   private final IndexReaderFactory indexReaderFactory;
-  
-  private static <T> T getInstance(String className, T defaultInstance) {
+
+  private static <T extends java.lang.Object> T getInstance(String className, T defaultInstance) {
     try {
       if (className == null) {
         return defaultInstance;
@@ -38,9 +41,9 @@ public class ClueConfiguration {
       T obj = (T) (Class.forName(className).newInstance());
       return obj;
     } catch (Exception e) {
-      try { 
+      try {
         T obj2 = (T) (Class.forName(className).getConstructor(Version.class).newInstance(Version.LUCENE_45));
-        return obj2; 
+        return obj2;
       } catch (Exception e2) {
         System.out.println("unable to obtain instance of class: " + className);
         return defaultInstance;
@@ -48,16 +51,11 @@ public class ClueConfiguration {
     }
   }
 
-  private ClueConfiguration(Properties config) {    
-    analyzerQuery = getInstance(config.getProperty(ANALYZER_QUERY_PARAM), 
-        new StandardAnalyzer(Version.LUCENE_46));    
-    dirBuilder = getInstance(config.getProperty(DIRECTORY_BUILDER_PARAM),
-        new DefaultDirectoryBuilder());
-    queryBuilder = getInstance(config.getProperty(QUERY_BUILDER_PARAM),
-        new DefaultQueryBuilder());
-    indexReaderFactory = getInstance(config.getProperty(INDEX_READER_FACTORY_PARAM),
-        new DefaultIndexReaderFactory());
-    
+  private ClueConfiguration(Properties config) {
+    analyzerQuery = getInstance(config.getProperty(ANALYZER_QUERY_PARAM), new StandardAnalyzer(Version.LUCENE_46));
+    dirBuilder = getInstance(config.getProperty(DIRECTORY_BUILDER_PARAM), new DefaultDirectoryBuilder());
+    queryBuilder = getInstance(config.getProperty(QUERY_BUILDER_PARAM), new DefaultQueryBuilder());
+    indexReaderFactory = getInstance(config.getProperty(INDEX_READER_FACTORY_PARAM), new DefaultIndexReaderFactory());
     System.out.println("Analyzer: \t\t" + analyzerQuery.getClass());
     System.out.println("Query Builder: \t\t" + queryBuilder.getClass());
     System.out.println("Directory Builder: \t" + dirBuilder.getClass());
@@ -82,24 +80,23 @@ public class ClueConfiguration {
 
   public static ClueConfiguration load() throws IOException {
     String confDirPath = System.getProperty("config.dir");
-    if (confDirPath == null) {      
+    if (confDirPath == null) {
       confDirPath = "config";
     }
     File confFile = new File(confDirPath, CLUE_CONF_FILE);
     if (confFile.exists() && confFile.isFile()) {
-       System.out.println("using configuration file found at: " + confFile.getAbsolutePath());
-       Properties props = new Properties();
-       FileReader freader = new FileReader(confFile);
-       props.load(freader);
-       freader.close();
-       return new ClueConfiguration(props);
+      System.out.println("using configuration file found at: " + confFile.getAbsolutePath());
+      Properties props = new Properties();
+      FileReader freader = new FileReader(confFile);
+      props.load(freader);
+      freader.close();
+      return new ClueConfiguration(props);
     } else {
-      // use default
       System.out.println("no configuration found, using default configuration");
       return new ClueConfiguration(new Properties());
     }
   }
-  
+
   public static void main(String[] args) throws Exception {
     String dir = "hdfs:///Users/jwang";
     System.out.println(new URI(dir).getScheme());
