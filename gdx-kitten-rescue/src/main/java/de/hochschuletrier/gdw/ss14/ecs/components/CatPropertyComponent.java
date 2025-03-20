bdd8bbd7f681df5a20ea0d91acca7cb5487cbc7a
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 02:08:08 +0000

    Mining Framework Analysis

--- gdx-kitten-rescue/src/main/java/de/hochschuletrier/gdw/ss14/ecs/components/CatPropertyComponent.java
+++ gdx-kitten-rescue/src/main/java/de/hochschuletrier/gdw/ss14/ecs/components/CatPropertyComponent.java
@@ -1,30 +1,33 @@
 package de.hochschuletrier.gdw.ss14.ecs.components;
-
 import com.badlogic.gdx.math.Vector2;
-
 import de.hochschuletrier.gdw.ss14.states.CatStateEnum;
 
-public class CatPropertyComponent implements Component
-{
-    public static final int MAX_LIVES = 9;
-    
-    public Vector2 lastCheckPoint;
-    public int amountLives;
-    public boolean isHidden;
-    public boolean canSeeLaserPointer;
-    public boolean isAlive, atePositiveFood;
-    public CatStateEnum state;
-    public float timeTillJump = 0;
-    public float timeWhileJump = 0;
+public class CatPropertyComponent implements Component {
+  public static final int MAX_LIVES = 9;
 
-    public CatPropertyComponent()
-    {
-        lastCheckPoint = new Vector2();
-        canSeeLaserPointer = true;
-        amountLives = MAX_LIVES;
-        isAlive = true;
-        atePositiveFood = false;
-        state = CatStateEnum.IDLE;
-        isHidden = false;
-    }
+  public Vector2 lastCheckPoint;
+
+  public int amountLives;
+
+  public boolean isHidden;
+
+  public boolean canSeeLaserPointer;
+
+  public boolean isAlive, atePositiveFood;
+
+  public CatStateEnum state;
+
+  public float timeTillJump = 0;
+
+  public float timeWhileJump = 0;
+
+  public CatPropertyComponent() {
+    lastCheckPoint = new Vector2();
+    canSeeLaserPointer = true;
+    amountLives = MAX_LIVES;
+    isAlive = true;
+    atePositiveFood = false;
+    state = CatStateEnum.IDLE;
+    isHidden = false;
+  }
 }
