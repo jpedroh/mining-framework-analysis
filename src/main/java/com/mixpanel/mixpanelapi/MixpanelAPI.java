4060ed47993bbdeee1f1fafafbf81ce4852ff359
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 20:47:47 +0000

    Mining Framework Analysis

--- src/main/java/com/mixpanel/mixpanelapi/MixpanelAPI.java
+++ src/main/java/com/mixpanel/mixpanelapi/MixpanelAPI.java
@@ -200,6 +200,7 @@ public class MixpanelAPI {
 
     private static final int BUFFER_SIZE = 256; // Small, we expect small responses.
     private static final int READ_TIMEOUT_MILLIS = 30000; // Thirty seconds should be more than enough for a response.
-    private static final int CONNECT_TIMEOUT_MILLIS = 30000; // 30 seconds should be more than enough for a response.
+    private static final int CONNECT_TIMEOUT_MILLIS = 30000;
+// 30 seconds should be more than enough for a response.
 
 }
