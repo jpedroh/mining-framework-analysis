b479937a08e3d927387c433b4a586d734ceeec23
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 16:47:46 +0000

    Mining Framework Analysis

--- src/main/java/org/cts/CRSFactory.java
+++ src/main/java/org/cts/CRSFactory.java
@@ -1,42 +1,9 @@
-/*
- * Coordinate Transformations Suite (abridged CTS)  is a library developped to 
- * perform Coordinate Transformations using well known geodetic algorithms 
- * and parameter sets. 
- * Its main focus are simplicity, flexibility, interoperability, in this order.
- *
- * This library has been originally developed by Michaël Michaud under the JGeod
- * name. It has been renamed CTS in 2009 and shared to the community from 
- * the Atelier SIG code repository.
- * 
- * Since them, CTS is supported by the Atelier SIG team in collaboration with Michaël 
- * Michaud.
- * The new CTS has been funded  by the French Agence Nationale de la Recherche 
- * (ANR) under contract ANR-08-VILL-0005-01 and the regional council 
- * "Région Pays de La Loire" under the projet SOGVILLE (Système d'Orbservation 
- * Géographique de la Ville).
- *
- * CTS is free software: you can redistribute it and/or modify it under the
- * terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any later
- * version.
- *
- * CTS is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with
- * CTS. If not, see <http://www.gnu.org/licenses/>.
- *
- * For more information, please consult: <https://github.com/irstv/cts/>
- */
 package org.cts;
-
 import org.cts.crs.CRSException;
 import org.cts.crs.CoordinateReferenceSystem;
 import org.cts.parser.prj.PrjParser;
 import org.cts.registry.Registry;
 import org.cts.registry.RegistryManager;
-
 import java.io.*;
 import java.nio.charset.Charset;
 import java.util.LinkedHashMap;
@@ -60,17 +27,17 @@ import java.util.Set;
  * @author Erwan Bocher
  */
 public class CRSFactory {
+  private RegistryManager registryManager = new RegistryManager();
 
-    private RegistryManager registryManager = new RegistryManager();
-    protected final CRSCache<String, CoordinateReferenceSystem> CRSPOOL = new CRSCache<String, CoordinateReferenceSystem>(10);
+  protected final CRSCache<String, CoordinateReferenceSystem> CRSPOOL = new CRSCache<String, CoordinateReferenceSystem>(10);
 
-    /**
+  /**
      * Creates a new factory.
      */
-    public CRSFactory() {
-    }
+  public CRSFactory() {
+  }
 
-    /**
+  /**
      * Return a
      *
      * @CoordinateReferenceSystem according an authority and a srid ie :
@@ -80,79 +47,79 @@ public class CRSFactory {
      * @return
      * @throws CRSException
      */
-    public CoordinateReferenceSystem getCRS(String authorityAndSrid) throws CRSException {
-        CoordinateReferenceSystem crs = CRSPOOL.get(authorityAndSrid);
-        if (crs == null) {
-            if (isRegistrySupported(authorityAndSrid)) {
-                String[] registryNameWithCode = authorityAndSrid.split(":");
-                Registry registry = getRegistryManager().getRegistry(registryNameWithCode[0]);
-                Map<String, String> crsParameters = registry.getParameters(registryNameWithCode[1]);
-                if (crsParameters != null) {
-                    crs = CRSHelper.createCoordinateReferenceSystem(new Identifier(registryNameWithCode[0], registryNameWithCode[1], crsParameters.get("title")), crsParameters);
-                }
-                if (crs != null) {
-                    CRSPOOL.put(authorityAndSrid, crs);
-                }
-            }
+  public CoordinateReferenceSystem getCRS(String authorityAndSrid) throws CRSException {
+    CoordinateReferenceSystem crs = CRSPOOL.get(authorityAndSrid);
+    if (crs == null) {
+      if (isRegistrySupported(authorityAndSrid)) {
+        String[] registryNameWithCode = authorityAndSrid.split(":");
+        Registry registry = getRegistryManager().getRegistry(registryNameWithCode[0]);
+        Map<String, String> crsParameters = registry.getParameters(registryNameWithCode[1]);
+        if (crsParameters != null) {
+          crs = CRSHelper.createCoordinateReferenceSystem(new Identifier(registryNameWithCode[0], registryNameWithCode[1], crsParameters.get("title")), crsParameters);
         }
-        return crs;
+        if (crs != null) {
+          CRSPOOL.put(authorityAndSrid, crs);
+        }
+      }
     }
+    return crs;
+  }
 
-    /**
+  /**
      * Return the registry manager
      *
      * @return
      */
-    public RegistryManager getRegistryManager() {
-        return registryManager;
-    }
+  public RegistryManager getRegistryManager() {
+    return registryManager;
+  }
 
-    /**
+  /**
      * Check if the registry name of the crsCode is supported.
      *
      * @param crsCode
      * @return
      */
-    public boolean isRegistrySupported(String crsCode) {
-        int p = crsCode.indexOf(':');
-        if (p >= 0) {
-            String auth = crsCode.substring(0, p);
-            if (getRegistryManager().contains(auth.toLowerCase())) {
-                return true;
-            }
-        }
-        throw new RuntimeException("This registry is not supported");
+  public boolean isRegistrySupported(String crsCode) {
+    int p = crsCode.indexOf(':');
+    if (p >= 0) {
+      String auth = crsCode.substring(0, p);
+      if (getRegistryManager().contains(auth.toLowerCase())) {
+        return true;
+      }
     }
+    throw new RuntimeException("This registry is not supported");
+  }
 
-    /**
+  /**
      * Creates a {@link CoordinateReferenceSystem} defined by an OGC WKT String
      * (PRJ).
      *
      * @param prjString the PRJ String
      * @return a {@link CoordinateReferenceSystem}
      */
-    public CoordinateReferenceSystem createFromPrj(String prjString) {
-        PrjParser p = new PrjParser();
-        Map<String, String> prjParameters = p.getParameters(prjString);
-        String name = prjParameters.remove("name");
-        String refname = prjParameters.remove("refname");
-        CoordinateReferenceSystem crs;
-        if (refname != null) {
-            crs = CRSPOOL.get(refname);
-            if (crs == null) {
-                String[] authorityNameWithKey = refname.split(":");
-                crs = CRSHelper.createCoordinateReferenceSystem(new Identifier(authorityNameWithKey[0], authorityNameWithKey[1], name), prjParameters);
-            }
-            if (crs != null) {
-                CRSPOOL.put(refname, crs);
-            }
-        } else {
-            crs = CRSHelper.createCoordinateReferenceSystem(new Identifier(name, name, name), prjParameters);
-        }
-        return crs;
+  public CoordinateReferenceSystem createFromPrj(String prjString) {
+    PrjParser p = new PrjParser();
+    Map<String, String> prjParameters = p.getParameters(prjString);
+    String name = prjParameters.remove("name");
+    String refname = prjParameters.remove("refname");
+    CoordinateReferenceSystem crs;
+    if (refname != null) {
+      crs = CRSPOOL.get(refname);
+      if (crs == null) {
+        String[] authorityNameWithKey = refname.split(":");
+        crs = CRSHelper.createCoordinateReferenceSystem(new Identifier(authorityNameWithKey[0], authorityNameWithKey[1], name), prjParameters);
+      }
+      if (crs != null) {
+        CRSPOOL.put(refname, crs);
+      }
+    } else {
+      crs = CRSHelper.createCoordinateReferenceSystem(new Identifier(name, name, name), prjParameters);
     }
+    return crs;
+  }
 
-    /**
+  /**
      * Creates a {@link CoordinateReferenceSystem} defined by an OGC WKT String
      * (PRJ).
      *
@@ -161,17 +128,16 @@ public class CRSFactory {
      * @return a {@link CoordinateReferenceSystem}
      * @throws IOException
      */
-    public CoordinateReferenceSystem createFromPrj(InputStream stream, Charset encoding) throws IOException {
-        BufferedReader r = new BufferedReader(new InputStreamReader(stream, encoding));
-        StringBuilder b = new StringBuilder();
-        while (r.ready()) {
-            b.append(r.readLine());
-        }
-
-        return createFromPrj(b.toString());
+  public CoordinateReferenceSystem createFromPrj(InputStream stream, Charset encoding) throws IOException {
+    BufferedReader r = new BufferedReader(new InputStreamReader(stream, encoding));
+    StringBuilder b = new StringBuilder();
+    while (r.ready()) {
+      b.append(r.readLine());
     }
+    return createFromPrj(b.toString());
+  }
 
-    /**
+  /**
      * Creates a {@link CoordinateReferenceSystem} defined by an OGC WKT String
      * (PRJ).
      *
@@ -179,11 +145,11 @@ public class CRSFactory {
      * @return
      * @throws IOException
      */
-    public CoordinateReferenceSystem createFromPrj(InputStream stream) throws IOException {
-        return createFromPrj(stream, Charset.defaultCharset());
-    }
+  public CoordinateReferenceSystem createFromPrj(InputStream stream) throws IOException {
+    return createFromPrj(stream, Charset.defaultCharset());
+  }
 
-    /**
+  /**
      * Creates a {@link CoordinateReferenceSystem} defined by an OGC WKT String
      * (PRJ).
      *
@@ -191,44 +157,39 @@ public class CRSFactory {
      * @return a {@link CoordinateReferenceSystem}
      * @throws IOException if there is a problem reading the file
      */
-    public CoordinateReferenceSystem createFromPrj(File file) throws IOException {
-        InputStream i = null;
-        CoordinateReferenceSystem crs;
-        try {
-            i = new FileInputStream(file);
-            crs = createFromPrj(i);
-        } finally {
-            if (i != null) {
-                i.close();
-            }
-        }
-        return crs;
+  public CoordinateReferenceSystem createFromPrj(File file) throws IOException {
+    InputStream i = null;
+    CoordinateReferenceSystem crs;
+    try {
+      i = new FileInputStream(file);
+      crs = createFromPrj(i);
+    }  finally {
+      if (i != null) {
+        i.close();
+      }
     }
+    return crs;
+  }
 
-    /**
+  /**
      * Return a list of supported codes according an registryName
-     * @param registryName
+     * @param registeryName
      * @return 
      */
-    public Set<String> getSupportedCodes(String registryName) {        
-        return getRegistryManager().getRegistry(registryName).getSupportedCodes();
+  public Set<String> getSupportedCodes(String registryName) {
+    return getRegistryManager().getRegistry(registryName).getSupportedCodes();
+  }
+
+  public class CRSCache<K extends java.lang.Object, V extends java.lang.Object> extends LinkedHashMap<K, V> {
+    private final int limit;
+
+    public CRSCache(int limit) {
+      super(16, 0.75f, true);
+      this.limit = limit;
     }
 
-    /**
-     * A simple cache to manage {@link CoordinateReferenceSystem}
-     */
-    public class CRSCache<K, V> extends LinkedHashMap<K, V> {
-
-        private final int limit;
-
-        public CRSCache(int limit) {
-            super(16, 0.75f, true);
-            this.limit = limit;
-        }
-
-        @Override
-        protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
-            return size() > limit;
-        }
+    @Override protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
+      return size() > limit;
     }
+  }
 }
--- src/main/java/org/cts/crs/CompoundCRS.java
+++ src/main/java/org/cts/crs/CompoundCRS.java
@@ -1,36 +1,4 @@
-/*
- * Coordinate Transformations Suite (abridged CTS)  is a library developped to 
- * perform Coordinate Transformations using well known geodetic algorithms 
- * and parameter sets. 
- * Its main focus are simplicity, flexibility, interoperability, in this order.
- *
- * This library has been originally developed by Michaël Michaud under the JGeod
- * name. It has been renamed CTS in 2009 and shared to the community from 
- * the Atelier SIG code repository.
- * 
- * Since them, CTS is supported by the Atelier SIG team in collaboration with Michaël 
- * Michaud.
- * The new CTS has been funded  by the French Agence Nationale de la Recherche 
- * (ANR) under contract ANR-08-VILL-0005-01 and the regional council 
- * "Région Pays de La Loire" under the projet SOGVILLE (Système d'Orbservation 
- * Géographique de la Ville).
- *
- * CTS is free software: you can redistribute it and/or modify it under the
- * terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any later
- * version.
- *
- * CTS is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with
- * CTS. If not, see <http://www.gnu.org/licenses/>.
- *
- * For more information, please consult: <https://github.com/irstv/cts/>
- */
 package org.cts.crs;
-
 import org.cts.op.CoordinateOperation;
 import org.cts.Identifier;
 import org.cts.op.NonInvertibleOperationException;
@@ -40,122 +8,77 @@ import org.cts.cs.CoordinateSystem;
 
 /**
  * A compound CoordinateReferenceSystem is a {@link org.cts.crs.CoordinateReferenceSystem}
- * composed of two distinct CoordinateReferenceSystem : a {@link org.cts.crs.GeodeticCRS}
+ * composed by two distinct CoordinateReferenceSystem : a {@link org.cts.crs.GeodeticCRS}
  * for 2D horizontal coordinates and a {@link org.cts.crs.VerticalCRS} for the z
  * coordinate.
  *
  * @author Michaël Michaud
  */
 public abstract class CompoundCRS extends GeodeticCRS {
+  private GeodeticCRS horizontalCRS;
 
-    private GeodeticCRS horizontalCRS;
-    private VerticalCRS verticalCRS;
+  private VerticalCRS verticalCRS;
 
-    /**
+  /**
      * Create a new GeodeticCRS.
      */
-    protected CompoundCRS(Identifier identifier, GeodeticCRS horizontalCRS,
-            VerticalCRS verticalCRS) {
-        super(identifier, horizontalCRS.getDatum(), new CoordinateSystem(
-                new Axis[]{horizontalCRS.getCoordinateSystem().getAxis(0),
-                        horizontalCRS.getCoordinateSystem().getAxis(1),
-                    verticalCRS.getCoordinateSystem().getAxis(0)},
-                new Unit[]{horizontalCRS.getCoordinateSystem().getUnit(0),
-                        horizontalCRS.getCoordinateSystem().getUnit(1),
-                    verticalCRS.getCoordinateSystem().getUnit(0)}));
-        this.verticalCRS = verticalCRS;
-    }
+  protected CompoundCRS(Identifier identifier, GeodeticCRS horizontalCRS, VerticalCRS verticalCRS) {
+    super(identifier, horizontalCRS.getDatum(), new CoordinateSystem(new Axis[] { horizontalCRS.getCoordinateSystem().getAxis(0), horizontalCRS.getCoordinateSystem().getAxis(1), verticalCRS.getCoordinateSystem().getAxis(0) }, new Unit[] { horizontalCRS.getCoordinateSystem().getUnit(0), horizontalCRS.getCoordinateSystem().getUnit(1), verticalCRS.getCoordinateSystem().getUnit(0) }));
+    this.verticalCRS = verticalCRS;
+  }
 
-    /**
+  /**
      * Return this CoordinateReferenceSystem Type
      */
-    @Override
-    public Type getType() {
-        return CoordinateReferenceSystem.Type.COMPOUND;
-    }
+  @Override public Type getType() {
+    return CoordinateReferenceSystem.Type.COMPOUND;
+  }
 
-    /**
+  /**
      * @return the horizonal part of this CoordinateReferenceSystem
      */
-    public GeodeticCRS getHorizontalCRS() {
-        return horizontalCRS;
-    }
+  public GeodeticCRS getHorizontalCRS() {
+    return horizontalCRS;
+  }
 
-    /**
+  /**
      * @return the vertical part of this CoordinateReferenceSystem
      */
-    public VerticalCRS getVerticalCRS() {
-        return verticalCRS;
-    }
+  public VerticalCRS getVerticalCRS() {
+    return verticalCRS;
+  }
 
-    /**
-     * Returns the coordinate system of this CoordinateReferenceSystem.
-     */
-    /*
-     * public CoordinateSystem getCoordinateSystem() { return coordinateSystem;
-     * }
-     */
-
-    /**
+  /**
      * Returns the number of dimensions of the coordinate system.
      */
-    @Override
-    public int getDimension() {
-        return 3;
-    }
+  @Override public int getDimension() {
+    return 3;
+  }
 
-    /**
-     * Return the {@link fr.michaelm.geo.geodesy.datum.GeodeticDatum}.
-     */
-    /*
-     * public GeodeticDatum getDatum() { return geodeticDatum; }
-     */
-
-    /**
-     * Return whether this coord is a valid coord in this
-     * CoordinateReferenceSystem.
-     *
-     * @param coord standard coordinate for this CoordinateReferenceSystem
-     * datums (ex. decimal degrees for geographic datums and meters for vertical
-     * datums).
-     */
-    /*
-     * public boolean isValid(double[] coord) { return
-     * geodeticDatum.getExtent().isInside(coord); }
-     */
-
-    /**
+  /**
      * Creates a CoordinateOperation object to convert coordinates from this
      * CoordinateReferenceSystem to a GeographicReferenceSystem based on the
      * same horizonal datum and vertical datum, and using normal SI units in the
      * following order : latitude (rad), longitude (rad) height/altitude (m).
      */
-    @Override
-    public CoordinateOperation toGeographicCoordinateConverter()
-            throws NonInvertibleOperationException {
-        // To BE DONE
-        return null;
-    }
+  @Override public CoordinateOperation toGeographicCoordinateConverter() throws NonInvertibleOperationException {
+    return null;
+  }
 
-    /**
+  /**
      * Creates a CoordinateOperation object to convert coordinates from a
      * GeographicReferenceSystem based on the same horizonal datum and vertical
      * datum, and using normal SI units in the following order : latitude (rad),
      * longitude (rad) height/altitude (m) to this CoordinateReferenceSystem.
      */
-    @Override
-    public CoordinateOperation fromGeographicCoordinateConverter()
-            throws NonInvertibleOperationException {
-        // To BE DONE
-        return null;
-    }
+  @Override public CoordinateOperation fromGeographicCoordinateConverter() throws NonInvertibleOperationException {
+    return null;
+  }
 
-    /**
+  /**
      * Return a String representation of this Datum.
      */
-    @Override
-    public String toString() {
-        return "[" + getAuthorityName() + ":" + getAuthorityKey() + "] " + getName() + " ("
-                + getShortName() + ")";
-    }
+  @Override public String toString() {
+    return "[" + getAuthorityName() + ":" + getAuthorityKey() + "] " + getName() + " (" + getShortName() + ")";
+  }
 }
