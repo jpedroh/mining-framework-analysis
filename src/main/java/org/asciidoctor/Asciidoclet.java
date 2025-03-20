a1e616b196b8db1bfd857e876513025afe219951
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 14:06:05 +0000

    Mining Framework Analysis

--- src/main/java/org/asciidoctor/Asciidoclet.java
+++ src/main/java/org/asciidoctor/Asciidoclet.java
@@ -1,5 +1,4 @@
 package org.asciidoctor;
-
 import com.google.common.io.Files;
 import com.google.common.io.Resources;
 import com.sun.javadoc.DocErrorReporter;
@@ -7,7 +6,6 @@ import com.sun.javadoc.Doclet;
 import com.sun.javadoc.LanguageVersion;
 import com.sun.javadoc.RootDoc;
 import org.asciidoctor.asciidoclet.*;
-
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
@@ -17,17 +15,19 @@ import java.net.URL;
  *
  * https://github.com/asciidoctor/asciidoclet[Asciidoclet] is a Javadoc Doclet
  * that uses http://asciidoctor.org[Asciidoctor] (via the
- * https://github.com/asciidoctor/asciidoctorj[Asciidoctor Java integration])
- * to interpet http://asciidoc.org[AsciiDoc] markup within Javadoc comments.
+ * https://github.com/asciidoctor/asciidoctor-java-integration[Asciidoctor Java integration])
+ * to render http://asciidoc.org[AsciiDoc] markup within Javadoc comments.
  *
  * include::README.asciidoc[tags=usage]
  *
  * == Examples
  *
  * Custom attributes::
- * `+{project_name}+`;; {project_name}
- * `+{project_desc}+`;; {project_desc}
- * `+{project_version}+`;; {project_version}
+ * +{project_name}+ = {project_name}
+ * +
+ * +{project_desc}+ = {project_desc}
+ * +
+ * +{project_version}+ = {project_version}
  *
  * Code block (with syntax highlighting added by CodeRay)::
  * +
@@ -54,23 +54,19 @@ import java.net.URL;
  * <1> Creates an instance of the Asciidoctor Java integration
  * <2> Runs Javadoc comment strings through Asciidoctor
  *
- * Inline code:: `code()`
+ * Inline code:: `code()` or +code()+
  *
  * Headings::
  * +
  * --
  * [float]
  * = Heading 1
- *
  * [float]
  * == Heading 2
- *
  * [float]
  * === Heading 3
- *
  * [float]
  * ==== Heading 4
- *
  * [float]
  * ===== Heading 5
  * --
@@ -125,8 +121,11 @@ import java.net.URL;
  * Tables::
  * +
  * .An example table
+ * [cols="3", options="header"]
  * |===
- * |Column 1 |Column 2 |Column 3
+ * |Column 1
+ * |Column 2
+ * |Column 3
  * 
  * |1
  * |Item 1
@@ -145,6 +144,8 @@ import java.net.URL;
  * +
  * .Optional Title
  * ****
+ * *Sidebar* Block
+ *
  * Usage: Notes in a sidebar, naturally.
  * ****
  *
@@ -159,28 +160,29 @@ import java.net.URL;
  * @serial (or @serialField or @serialData)
  */
 public class Asciidoclet extends Doclet {
+  private final RootDoc rootDoc;
 
-    private final RootDoc rootDoc;
-    private final DocletOptions docletOptions;
-    private final DocletIterator iterator;
-    private final Stylesheets stylesheets;
+  private final DocletOptions docletOptions;
 
-    public Asciidoclet(RootDoc rootDoc) {
-        this.rootDoc = rootDoc;
-        this.docletOptions = new DocletOptions(rootDoc);
-        this.iterator = new DocletIterator(docletOptions);
-        this.stylesheets = new Stylesheets(docletOptions, rootDoc);
-    }
+  private final DocletIterator iterator;
 
-    // test use
-    Asciidoclet(RootDoc rootDoc, DocletIterator iterator, Stylesheets stylesheets) {
-        this.rootDoc = rootDoc;
-        this.docletOptions = new DocletOptions(rootDoc);
-        this.iterator = iterator;
-        this.stylesheets = stylesheets;
-    }
+  private final Stylesheets stylesheets;
 
-    /**
+  public Asciidoclet(RootDoc rootDoc) {
+    this.rootDoc = rootDoc;
+    this.docletOptions = new DocletOptions(rootDoc);
+    this.iterator = new DocletIterator(docletOptions);
+    this.stylesheets = new Stylesheets(docletOptions, rootDoc);
+  }
+
+  Asciidoclet(RootDoc rootDoc, DocletIterator iterator, Stylesheets stylesheets) {
+    this.rootDoc = rootDoc;
+    this.docletOptions = new DocletOptions(rootDoc);
+    this.iterator = iterator;
+    this.stylesheets = stylesheets;
+  }
+
+  /**
      * .Example usage
      * [source,java]
      * exampleDeprecated("do not use");
@@ -191,23 +193,21 @@ public class Asciidoclet extends Doclet {
      * @serialData something else
      * @link Asciidoclet
      */
-    public static void exampleDeprecated(String field) throws Exception{
-        //noop
-    }
+  public static void exampleDeprecated(String field) throws Exception {
+  }
 
-    /**
+  /**
      * Sets the language version to Java 5.
      *
      * _Javadoc spec requirement._
      *
      * @return language version number
      */
-    @SuppressWarnings("UnusedDeclaration")
-    public static LanguageVersion languageVersion() {
-        return LanguageVersion.JAVA_1_5;
-    }
+  @SuppressWarnings(value = { "UnusedDeclaration" }) public static LanguageVersion languageVersion() {
+    return LanguageVersion.JAVA_1_5;
+  }
 
-    /**
+  /**
      * Sets the option length to the standard Javadoc option length.
      *
      * _Javadoc spec requirement._
@@ -215,12 +215,11 @@ public class Asciidoclet extends Doclet {
      * @param option input option
      * @return length of required parameters
      */
-    @SuppressWarnings("UnusedDeclaration")
-    public static int optionLength(String option) {
-        return optionLength(option, new StandardAdapter());
-    }
+  @SuppressWarnings(value = { "UnusedDeclaration" }) public static int optionLength(String option) {
+    return optionLength(option, new StandardAdapter());
+  }
 
-    /**
+  /**
      * The starting point of Javadoc render.
      *
      * _Javadoc spec requirement._
@@ -228,12 +227,11 @@ public class Asciidoclet extends Doclet {
      * @param rootDoc input class documents
      * @return success
      */
-    @SuppressWarnings("UnusedDeclaration")
-    public static boolean start(RootDoc rootDoc) {
-        return new Asciidoclet(rootDoc).start(new StandardAdapter());
-    }
+  @SuppressWarnings(value = { "UnusedDeclaration" }) public static boolean start(RootDoc rootDoc) {
+    return new Asciidoclet(rootDoc).start(new StandardAdapter());
+  }
 
-    /**
+  /**
      * Processes the input options by delegating to the standard handler.
      *
      * _Javadoc spec requirement._
@@ -242,36 +240,35 @@ public class Asciidoclet extends Doclet {
      * @param errorReporter error handling
      * @return success
      */
-    @SuppressWarnings("UnusedDeclaration")
-    public static boolean validOptions(String[][] options, DocErrorReporter errorReporter) {
-        return validOptions(options, errorReporter, new StandardAdapter());
-    }
+  @SuppressWarnings(value = { "UnusedDeclaration" }) public static boolean validOptions(String[][] options, DocErrorReporter errorReporter) {
+    return validOptions(options, errorReporter, new StandardAdapter());
+  }
 
-    static int optionLength(String option, StandardAdapter standardDoclet) {
-        return DocletOptions.optionLength(option, standardDoclet);
-    }
+  static int optionLength(String option, StandardAdapter standardDoclet) {
+    return DocletOptions.optionLength(option, standardDoclet);
+  }
 
-    static boolean validOptions(String[][] options, DocErrorReporter errorReporter, StandardAdapter standardDoclet) {
-        return DocletOptions.validOptions(options, errorReporter, standardDoclet);
-    }
+  static boolean validOptions(String[][] options, DocErrorReporter errorReporter, StandardAdapter standardDoclet) {
+    return DocletOptions.validOptions(options, errorReporter, standardDoclet);
+  }
 
-    boolean start(StandardAdapter standardDoclet) {
-        return run(standardDoclet)
-                && postProcess();
-    }
+  boolean start(StandardAdapter standardDoclet) {
+    return run(standardDoclet) && postProcess();
+  }
 
-    private boolean run(StandardAdapter standardDoclet) {
-        AsciidoctorRenderer renderer = new AsciidoctorRenderer(docletOptions, rootDoc);
-        try {
-            return iterator.render(rootDoc, renderer) &&
-                    standardDoclet.start(rootDoc);
-        } finally {
-            renderer.cleanup();
-        }
+  private boolean run(StandardAdapter standardDoclet) {
+    AsciidoctorRenderer renderer = new AsciidoctorRenderer(docletOptions, rootDoc);
+    try {
+      return iterator.render(rootDoc, renderer) && standardDoclet.start(rootDoc);
+    }  finally {
+      renderer.cleanup();
     }
+  }
 
-    private boolean postProcess() {
-        if (docletOptions.stylesheetFile().isPresent()) return true;
-        return stylesheets.copy();
+  private boolean postProcess() {
+    if (docletOptions.stylesheetFile().isPresent()) {
+      return true;
     }
+    return stylesheets.copy();
+  }
 }
