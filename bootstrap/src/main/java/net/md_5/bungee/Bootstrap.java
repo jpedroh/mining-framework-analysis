13aa0c8ccb6796f9dd7c3387298913879e1009bc
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 16:44:14 +0000

    Mining Framework Analysis

--- bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
+++ bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
@@ -1,17 +1,7 @@
-package net.md_5.bungee;
-
-public class Bootstrap
-{
-
-    public static void main(String[] args) throws Exception
-    {
-        if ( Float.parseFloat( System.getProperty( "java.class.version" ) ) < 52.0 ) //BotFilter
-        {
-            System.err.println( "*** ОШИБОЧКА *** БотФильтеру нужна Java 8. Установите её, что бы запустить сервер!" );//BotFilter
-            System.out.println( "Проверить версию: java -version" );//BotFilter
-            return;
-        }
-
-        BungeeCordLauncher.main( args );
-    }
-}
+  package   net . md_5 . bungee ;   public class Bootstrap  {   public static void main  (   String  [ ] args )  throws Exception  {  if  (   Float . parseFloat  (  System . getProperty  ( "java.class.version" ) ) < 52.0 )  {    System . err . println  ( 
+<<<<<<<
+"*** ОШИБОЧКА *** БотФильтеру нужна Java 8. Установите её, что бы запустить сервер!"
+=======
+"*** ERROR *** BungeeCord requires Java 8 or above to function! Please download and install it!"
+>>>>>>>
+ ) ;    System . out . println  ( "Проверить версию: java -version" ) ;  return ; }   BungeeCordLauncher . main  ( args ) ; } }
