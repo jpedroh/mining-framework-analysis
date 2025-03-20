892c60484dcbe05a66981c800096e5114e29cae7
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 11:30:08 +0000

    Mining Framework Analysis

--- protocol/src/main/java/net/md_5/bungee/protocol/MinecraftDecoder.java
+++ protocol/src/main/java/net/md_5/bungee/protocol/MinecraftDecoder.java
@@ -1,5 +1,4 @@
 package net.md_5.bungee.protocol;
-
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToMessageDecoder;
@@ -7,72 +6,59 @@ import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-public class MinecraftDecoder extends MessageToMessageDecoder<ByteBuf>
-{
+@Getter public class MinecraftDecoder extends MessageToMessageDecoder<ByteBuf> {
+  @Setter private Protocol protocol;
 
-    @Setter
-    private Protocol protocol;
-    private final boolean server;
-    @Setter
-    private int protocolVersion;
-    @Setter
-    private boolean[] copiedBuffers;
-    @Setter
-    private boolean[] handledPackets;
+  private final boolean server;
 
-    public MinecraftDecoder(Protocol protocol, boolean server, int protocolVersion)
-    {
-        this.protocol = protocol;
-        this.server = server;
-        this.protocolVersion = protocolVersion;
-    }
+  @Setter private int protocolVersion;
 
-    @Override
-    protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception
-    {
-        Protocol.DirectionData prot = ( server ) ? protocol.TO_SERVER : protocol.TO_CLIENT;
-        ByteBuf buf = null;
+  @Setter private boolean[] copiedBuffers;
 
-        try
-        {
-            int markIndex = in.readerIndex();
-            int markReadable = in.readableBytes();
-            int packetId = DefinedPacket.readVarInt( in );
-            if ( copiedBuffers != null && packetId >= 0 && packetId < copiedBuffers.length && copiedBuffers[packetId] )
-            {
-                buf =  in.copy( markIndex, markReadable ); // Can't slice this one due to EntityMap varint :(
-            } else
-            {
-                buf =  in.slice( markIndex, markReadable ).retain();
-            }
+  @Setter private boolean[] handledPackets;
 
-            DefinedPacket packet = null;
-            if ( handledPackets == null || ( packetId >= 0 && packetId < handledPackets.length && handledPackets[packetId] ) )
-            {
-                packet = prot.createPacket( packetId, protocolVersion );
-                if ( packet != null )
-                {
-                    packet.read( in, prot.getDirection(), protocolVersion );
+  public MinecraftDecoder(Protocol protocol, boolean server, int protocolVersion) {
+    this.protocol = protocol;
+    this.server = server;
+    this.protocolVersion = protocolVersion;
+  }
 
-                    if ( in.isReadable() )
-                    {
-                        throw new BadPacketException( "Did not read all bytes from packet " + packet.getClass() + " " + packetId + " Protocol " + protocol + " Direction " + prot.getDirection() );
-                    }
-                } else
-                {
-                    in.skipBytes( in.readableBytes() );
-                }
-            }
+  @Override protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
+    Protocol.DirectionData prot = (server) ? protocol.TO_SERVER : protocol.TO_CLIENT;
+    ByteBuf buf = null;
+    try {
+      int markIndex = in.readerIndex();
+      int markReadable = in.readableBytes();
+      int packetId = DefinedPacket.readVarInt(in);
+      if (copiedBuffers != null && packetId >= 0 && packetId < copiedBuffers.length && copiedBuffers[packetId]) {
+        buf = in.copy(markIndex, markReadable);
+      } else {
+        buf = in.slice(markIndex, markReadable).retain();
+      }
+      DefinedPacket packet = null;
+      if (handledPackets == null || (packetId >= 0 && packetId < handledPackets.length && handledPackets[packetId])) {
+        packet = prot.createPacket(packetId, protocolVersion);
+        if (packet != null) {
 
-            out.add( new PacketWrapper( packet, buf ) );
-            buf = null;
-        } finally
-        {
-            if ( buf != null )
-            {
-                buf.release();
-            }
+<<<<<<< /usr/src/app/output/spigotmc/bungeecord/7838c02a6bb70dc404e03eec1526e030fce52d56/protocol/src/main/java/net/md_5/bungee/protocol/MinecraftDecoder.java/left.java
+          packet.read(in, prot.getDirection(), protocolVersion);
+=======
+          throw new BadPacketException("Did not read all bytes from packet " + packet.getClass() + " " + packetId + " Protocol " + protocol + " Direction " + prot.getDirection());
+>>>>>>> /usr/src/app/output/spigotmc/bungeecord/7838c02a6bb70dc404e03eec1526e030fce52d56/protocol/src/main/java/net/md_5/bungee/protocol/MinecraftDecoder.java/right.java
+
+          if (in.isReadable()) {
+            throw new BadPacketException("Did not read all bytes from packet " + packet.getClass() + " " + packetId + " Protocol " + protocol + " Direction " + prot);
+          }
+        } else {
+          in.skipBytes(in.readableBytes());
         }
+      }
+      out.add(new PacketWrapper(packet, buf));
+      buf = null;
+    }  finally {
+      if (buf != null) {
+        buf.release();
+      }
     }
+  }
 }
