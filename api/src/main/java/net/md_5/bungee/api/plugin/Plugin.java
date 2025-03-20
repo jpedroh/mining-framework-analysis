840ae8002bd087517b1d84b93434d65c06d45a79
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 11:52:39 +0000

    Mining Framework Analysis

--- api/src/main/java/net/md_5/bungee/api/plugin/Plugin.java
+++ api/src/main/java/net/md_5/bungee/api/plugin/Plugin.java
@@ -1,5 +1,4 @@
 package net.md_5.bungee.api.plugin;
-
 import com.google.common.base.Preconditions;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import java.io.File;
@@ -19,69 +18,57 @@ import net.md_5.bungee.api.scheduler.GroupedThreadFactory;
  * Represents any Plugin that may be loaded at runtime to enhance existing
  * functionality.
  */
-public class Plugin
-{
+public class Plugin {
+  @Getter private PluginDescription description;
 
-    @Getter
-    private PluginDescription description;
-    @Getter
-    private ProxyServer proxy;
-    @Getter
-    private File file;
-    @Getter
-    private Logger logger;
+  @Getter private ProxyServer proxy;
 
-    public Plugin()
-    {
-        ClassLoader classLoader = getClass().getClassLoader();
-        Preconditions.checkState( classLoader instanceof PluginClassloader, "Plugin requires " + PluginClassloader.class.getName() );
+  @Getter private File file;
 
-        ( (PluginClassloader) classLoader ).init( this );
-    }
+  @Getter private Logger logger;
 
-    protected Plugin(ProxyServer proxy, PluginDescription description)
-    {
-        ClassLoader classLoader = getClass().getClassLoader();
-        Preconditions.checkState( !( classLoader instanceof PluginClassloader ), "Cannot use initialization constructor at runtime" );
+  public Plugin() {
+    ClassLoader classLoader = getClass().getClassLoader();
+    Preconditions.checkState(classLoader instanceof PluginClassloader, "Plugin requires " + PluginClassloader.class.getName());
+    ((PluginClassloader) classLoader).init(this);
+  }
 
-        // init( proxy, description );
-    }
+  protected Plugin(ProxyServer proxy, PluginDescription description) {
+    ClassLoader classLoader = getClass().getClassLoader();
+    Preconditions.checkState(!(classLoader instanceof PluginClassloader), "Cannot use initialization constructor at runtime");
+  }
 
-    /**
+  /**
      * Called when the plugin has just been loaded. Most of the proxy will not
      * be initialized, so only use it for registering
      * {@link ConfigurationAdapter}'s and other predefined behavior.
      */
-    public void onLoad()
-    {
-    }
+  public void onLoad() {
+  }
 
-    /**
+  /**
      * Called when this plugin is enabled.
      */
-    public void onEnable()
-    {
-    }
+  public void onEnable() {
+  }
 
-    /**
+  /**
      * Called when this plugin is disabled.
      */
-    public void onDisable()
-    {
-    }
+  public void onDisable() {
+  }
 
-    /**
+  /**
      * Gets the data folder where this plugin may store arbitrary data. It will
      * be a child of {@link ProxyServer#getPluginsFolder()}.
      *
      * @return the data folder of this plugin
      */
-    public final File getDataFolder()
-    {
-        return new File( getProxy().getPluginsFolder(), getDescription().getName() );
-    }
+  public final File getDataFolder() {
+    return new File(getProxy().getPluginsFolder(), getDescription().getName());
+  }
 
-    /**
+  /**
      * Get a resource from within this plugins jar or container. Care must be
      * taken to close the returned stream.
      *
@@ -89,47 +76,34 @@ public class Plugin
      * @return the stream for getting this resource, or null if it does not
      * exist
      */
-    public final InputStream getResourceAsStream(String name)
-    {
-        return getClass().getClassLoader().getResourceAsStream( name );
-    }
+  public final InputStream getResourceAsStream(String name) {
+    return getClass().getClassLoader().getResourceAsStream(name);
+  }
 
-    /**
+  /**
      * Called by the loader to initialize the fields in this plugin.
      *
      * @param proxy current proxy instance
      * @param description the description that describes this plugin
      */
-    final void init(ProxyServer proxy, PluginDescription description)
-    {
-        this.proxy = proxy;
-        this.description = description;
-        this.file = description.getFile();
-        this.logger = new PluginLogger( this );
+  final void init(ProxyServer proxy, PluginDescription description) {
+    this.proxy = proxy;
+    this.description = description;
+    this.file = description.getFile();
+    this.logger = new PluginLogger(this);
+  }
+
+  private ExecutorService service;
+
+  @Deprecated public ExecutorService getExecutorService() {
+    if (service == null) {
+      String name = (getDescription() == null) ? "unknown" : getDescription().getName();
+      service = newCachedThreadPool(new ThreadFactoryBuilder().setNameFormat(name + " Pool Thread #%1$d").setThreadFactory(new GroupedThreadFactory(this, name)).build());
     }
+    return service;
+  }
 
-    //
-    private ExecutorService service;
-
-    @Deprecated
-    public ExecutorService getExecutorService()
-    {
-        if ( service == null )
-        {
-            String name = ( getDescription() == null ) ? "unknown" : getDescription().getName();
-            service = newCachedThreadPool( new ThreadFactoryBuilder().setNameFormat( name + " Pool Thread #%1$d" )
-                    .setThreadFactory( new GroupedThreadFactory( this, name ) ).build() );
-        }
-        return service;
-    }
-
-    public static ExecutorService newCachedThreadPool(ThreadFactory threadFactory)
-    {
-        return new ThreadPoolExecutor( 2, Integer.MAX_VALUE,
-                                        120L, TimeUnit.SECONDS,
-                                        new SynchronousQueue<Runnable>(),
-                                        threadFactory );
-    }
-
-    //
+  public static ExecutorService newCachedThreadPool(ThreadFactory threadFactory) {
+    return new ThreadPoolExecutor(2, Integer.MAX_VALUE, 120L, TimeUnit.SECONDS, new SynchronousQueue<Runnable>(), threadFactory);
+  }
 }
