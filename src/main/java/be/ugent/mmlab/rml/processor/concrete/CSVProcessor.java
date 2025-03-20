30c5e42582ebdd76b3c11ac7b34fed6ba2caf866
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 06:32:33 +0000

    Mining Framework Analysis

--- src/main/java/be/ugent/mmlab/rml/processor/concrete/CSVProcessor.java
+++ src/main/java/be/ugent/mmlab/rml/processor/concrete/CSVProcessor.java
@@ -1,5 +1,4 @@
 package be.ugent.mmlab.rml.processor.concrete;
-
 import be.ugent.mmlab.rml.core.RMLEngine;
 import be.ugent.mmlab.rml.core.RMLMappingFactory;
 import be.ugent.mmlab.rml.core.RMLPerformer;
@@ -23,62 +22,47 @@ import org.apache.commons.logging.LogFactory;
  * @author mielvandersande, andimou
  */
 public class CSVProcessor extends AbstractRMLProcessor {
+  private static Log log = LogFactory.getLog(RMLMappingFactory.class);
 
-    private static Log log = LogFactory.getLog(RMLMappingFactory.class);
-    
-    private char getDelimiter(LogicalSource ls) {
-        String d = RMLEngine.getFileMap().getProperty(ls.getIdentifier() + ".delimiter");
-        if (d == null) {
-            return ',';
+  private char getDelimiter(LogicalSource ls) {
+    String d = RMLEngine.getFileMap().getProperty(ls.getIdentifier() + ".delimiter");
+    if (d == null) {
+      return ',';
+    }
+    return d.charAt(0);
+  }
+
+  @Override public void execute(SesameDataSet dataset, TriplesMap map, RMLPerformer performer, String fileName) {
+    try {
+      char delimiter = getDelimiter(map.getLogicalSource());
+      log.info("[CSV Processor] filename " + fileName);
+      CsvReader reader = new CsvReader(new FileInputStream(fileName), Charset.defaultCharset());
+      reader.setDelimiter(delimiter);
+      reader.readHeaders();
+      while (reader.readRecord()) {
+        HashMap<String, String> row = new HashMap<>();
+        for (String header : reader.getHeaders()) {
+          row.put(header, reader.get(header));
         }
-        return d.charAt(0);
+        performer.perform(row, dataset, map);
+      }
+    } catch (FileNotFoundException ex) {
+      log.error(ex);
+    } catch (IOException ex) {
+      log.error(ex);
     }
+  }
 
-    @Override
-    public void execute(SesameDataSet dataset, TriplesMap map, RMLPerformer performer, String fileName) {
-        //InputStream fis = null;
-        try {
-            char delimiter = getDelimiter(map.getLogicalSource());
-
-            //TODO: add character guessing
-            //CsvReader reader = new CsvReader(fis, Charset.defaultCharset());
-            log.info("[CSV Processor] filename " + fileName);
-            CsvReader reader = new CsvReader(new FileInputStream(fileName), Charset.defaultCharset());
-            reader.setDelimiter(delimiter);
-            
-            reader.readHeaders();
-            //Iterate the rows
-            while (reader.readRecord()) {
-                HashMap<String, String> row = new HashMap<>();
-
-                for (String header : reader.getHeaders()) {
-                    row.put(header, reader.get(header));
-                }
-                //let the performer handle the rows
-                performer.perform(row, dataset, map);
-            }
-
-        } catch (FileNotFoundException ex) {
-            log.error(ex);
-        } catch (IOException ex) {
-            log.error(ex);
-        }
+  @Override public List<String> extractValueFromNode(Object node, String expression) {
+    HashMap<String, String> row = (HashMap<String, String>) node;
+    List<String> list = new ArrayList();
+    if (row.containsKey(expression)) {
+      list.add(row.get(expression));
     }
+    return list;
+  }
 
-    @Override
-    public List<String> extractValueFromNode(Object node, String expression) {
-        HashMap<String, String> row = (HashMap<String, String>) node;
-        //call the right header in the row
-        List<String> list = new ArrayList();
-        if (row.containsKey(expression)){
-            list.add(row.get(expression));
-        }
-        
-        return list;
-    }
-
-    @Override
-    public void execute_node(SesameDataSet dataset, String expression, TriplesMap parentTriplesMap, RMLPerformer performer, Object node) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+  @Override public void execute_node(SesameDataSet dataset, String expression, TriplesMap parentTriplesMap, RMLPerformer performer, Object node) {
+    throw new UnsupportedOperationException("Not supported yet.");
+  }
 }
