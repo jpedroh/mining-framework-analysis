c5b82572f9528f4407cde5af1a1e8e8afade131e
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 17:14:11 +0000

    Mining Framework Analysis

--- src/main/java/alma/fr/modules/WeissModule.java
+++ src/main/java/alma/fr/modules/WeissModule.java
@@ -1,46 +1,7 @@
-package alma.fr.modules;
-
-import java.math.BigInteger;
-
-import alma.fr.basecomponents.BaseSimple;
-import alma.fr.basecomponents.Basebase;
-import alma.fr.basecomponents.IBase;
-import alma.fr.strategiescomponents.BeginningBoundaryIdProvider;
-import alma.fr.strategiescomponents.EndingBoundaryIdProvider;
-import alma.fr.strategiescomponents.IIdProviderStrategy;
-import alma.fr.strategiescomponents.boundary.BoundaryValue;
-import alma.fr.strategiescomponents.boundary.ConstantBoundary;
-import alma.fr.strategiescomponents.boundary.IBoundary;
-import alma.fr.strategychoicecomponents.IStrategyChoice;
-import alma.fr.strategychoicecomponents.SingleStrategyChoice;
-
-import com.google.inject.Binder;
-import com.google.inject.Module;
-
-/**
- * Simple Weiss module with a boundary+ ; base 1024 ; boundary 10;
- */
-public class WeissModule implements Module {
-
-	public void configure(Binder binder) {
-		Integer baseBase = new Integer(64);
-		BigInteger boundary = new BigInteger("1000000");
-
-		/* BASE */
-		binder.bind(Integer.class).annotatedWith(Basebase.class)
-				.toInstance(baseBase);
-		binder.bind(IBase.class).to(BaseSimple.class);
-
-		/* STRATEGY */
-		binder.bind(IBoundary.class).to(ConstantBoundary.class);
-		binder.bind(BigInteger.class).annotatedWith(BoundaryValue.class)
-				.toInstance(boundary);
-
-		/* STRATEGY CHOICE */
-		binder.bind(IIdProviderStrategy.class).to(
-				EndingBoundaryIdProvider.class);
-		binder.bind(IStrategyChoice.class).to(SingleStrategyChoice.class);
-
-	}
-
-}
+  package   alma . fr . modules ;   import   java . math . BigInteger ;  import    alma . fr . basecomponents . BaseSimple ;  import    alma . fr . basecomponents . Basebase ;  import    alma . fr . basecomponents . IBase ;  import    alma . fr . strategiescomponents . BeginningBoundaryIdProvider ;  import    alma . fr . strategiescomponents . IIdProviderStrategy ;  import     alma . fr . strategiescomponents . boundary . BoundaryValue ;  import     alma . fr . strategiescomponents . boundary . ConstantBoundary ;  import     alma . fr . strategiescomponents . boundary . IBoundary ;  import    alma . fr . strategychoicecomponents . IStrategyChoice ;  import    alma . fr . strategychoicecomponents . SingleStrategyChoice ;  import    com . google . inject . Binder ;  import    com . google . inject . Module ;   public class WeissModule  implements  Module  {   public void configure  (  Binder binder )  {  BigInteger  baseBase = 
+<<<<<<<
+ new Integer  ( 64 )
+=======
+  new BigInteger  ( "2" ) . pow  ( 64 )
+>>>>>>>
+ ;  BigInteger  boundary =  new BigInteger  ( "1000000" ) ;     binder . bind  (  BigInteger . class ) . annotatedWith  (  Basebase . class ) . toInstance  ( baseBase ) ;    binder . bind  (  IBase . class ) . to  (  BaseSimple . class ) ;    binder . bind  (  IBoundary . class ) . to  (  ConstantBoundary . class ) ;     binder . bind  (  BigInteger . class ) . annotatedWith  (  BoundaryValue . class ) . toInstance  ( boundary ) ;    binder . bind  (  IIdProviderStrategy . class ) . to  (  BeginningBoundaryIdProvider . class ) ;    binder . bind  (  IStrategyChoice . class ) . to  (  SingleStrategyChoice . class ) ; } }
