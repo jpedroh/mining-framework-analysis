88650dcd363948abca2bb98e2c1b8aab2f5bfdfa
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 23:18:25 +0000

    Mining Framework Analysis

--- src/main/java/org/apache/ibatis/mapping/BoundSql.java
+++ src/main/java/org/apache/ibatis/mapping/BoundSql.java
@@ -1,24 +1,7 @@
-/*
- *    Copyright 2009-2022 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       https://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- */
 package org.apache.ibatis.mapping;
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import org.apache.ibatis.reflection.MetaObject;
 import org.apache.ibatis.reflection.property.PropertyTokenizer;
 import org.apache.ibatis.session.Configuration;
@@ -34,11 +17,14 @@ import org.apache.ibatis.session.Configuration;
  * @author Clinton Begin
  */
 public class BoundSql {
-
   private final String sql;
+
   private final List<ParameterMapping> parameterMappings;
+
   private final Object parameterObject;
+
   private final Map<String, Object> additionalParameters;
+
   private final MetaObject metaParameters;
 
   public BoundSql(Configuration configuration, String sql, List<ParameterMapping> parameterMappings, Object parameterObject) {
