0b33bdd88eeb2cb01f143962adb20651d09aa00f
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 15:20:11 +0000

    Mining Framework Analysis

--- src/main/java/com/github/linsolas/casperjsrunner/CasperJSRunnerMojo.java
+++ src/main/java/com/github/linsolas/casperjsrunner/CasperJSRunnerMojo.java
@@ -30,6 +30,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeSet;
 
 /**
  * Runs JavaScript and/or CoffeScript test files on CasperJS instance
