931c4aa86c5ed486c2968e50598b1193842302f4
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 14:49:14 +0000

    Mining Framework Analysis

--- protocol/src/main/java/net/md_5/bungee/protocol/Varint21FrameDecoder.java
+++ protocol/src/main/java/net/md_5/bungee/protocol/Varint21FrameDecoder.java
@@ -1,68 +1,19 @@
-package net.md_5.bungee.protocol;
-
-import io.netty.buffer.ByteBuf;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.handler.codec.ByteToMessageDecoder;
-import java.util.List;
-import ru.leymooo.botfilter.utils.FastCorruptedFrameException;
-
-public class Varint21FrameDecoder extends ByteToMessageDecoder
-{
-    //BotFilter start
-    private boolean fromBackend;
-
-    public void setFromBackend(boolean fromBackend)
-    {
-        this.fromBackend = fromBackend;
-    }
-    //BotFilter end
-
-    @Override
-    protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception
-    {
-        //BotFilter start - rewrite Varint21Decoder
-        if ( !ctx.channel().isActive() )
-        {
-            super.setSingleDecode( true );
-            return;
-        }
-
-        int origReaderIndex = in.readerIndex();
-
-        int i = 3;
-        while ( i-- > 0 )
-        {
-            if ( !in.isReadable() )
-            {
-                in.readerIndex( origReaderIndex );
-                return;
-            }
-
-            byte read = in.readByte();
-            if ( read >= 0 )
-            {
-                // Make sure reader index of length buffer is returned to the beginning
-                in.readerIndex( origReaderIndex );
-                int packetLength = DefinedPacket.readVarInt( in );
-
-                if ( packetLength <= 0 && !fromBackend )
-                {
-                    super.setSingleDecode( true );
-                    throw new FastCorruptedFrameException( "Empty Packet!" );
-                }
-
-                if ( in.readableBytes() < packetLength )
-                {
-                    in.readerIndex( origReaderIndex );
-                    return;
-                }
-                out.add( in.readRetainedSlice( packetLength ) );
-                return;
-            }
-        }
-
-        super.setSingleDecode( true );
-        throw new FastCorruptedFrameException( "length wider than 21-bit" );
-    }
-    //BotFilter end
-}
+  package    net . md_5 . bungee . protocol ;   import    io . netty . buffer . ByteBuf ;  import    io . netty . channel . ChannelHandlerContext ;  import     io . netty . handler . codec . ByteToMessageDecoder ;  import   java . util . List ;  import     ru . leymooo . botfilter . utils . FastCorruptedFrameException ;   public class Varint21FrameDecoder  extends ByteToMessageDecoder  {   private boolean  fromBackend ;   public void setFromBackend  (  boolean fromBackend )  {    this . fromBackend = fromBackend ; }    @ Override protected void decode  (  ChannelHandlerContext ctx ,  ByteBuf in ,   List  < Object > out )  throws Exception  {  if  (  !   ctx . channel  ( ) . isActive  ( ) )  {   
+<<<<<<<
+super
+=======
+in
+>>>>>>>
+ . 
+<<<<<<<
+setSingleDecode
+=======
+skipBytes
+>>>>>>>
+  ( 
+<<<<<<<
+true
+=======
+ in . readableBytes  ( )
+>>>>>>>
+ ) ;  return ; }   int  origReaderIndex =  in . readerIndex  ( ) ;   int  i = 3 ;  while  (   i -- > 0 )  {  if  (  !  in . isReadable  ( ) )  {   in . readerIndex  ( origReaderIndex ) ;  return ; }   byte  read =  in . readByte  ( ) ;  if  (  read >= 0 )  {   in . readerIndex  ( origReaderIndex ) ;   int  packetLength =  DefinedPacket . readVarInt  ( in ) ;  if  (   packetLength <= 0 &&  ! fromBackend )  {   super . setSingleDecode  ( true ) ;  throw  new FastCorruptedFrameException  ( "Empty Packet!" ) ; }  if  (   in . readableBytes  ( ) < packetLength )  {   in . readerIndex  ( origReaderIndex ) ;  return ; }   out . add  (  in . readRetainedSlice  ( packetLength ) ) ;  return ; } }   super . setSingleDecode  ( true ) ;  throw  new FastCorruptedFrameException  ( "length wider than 21-bit" ) ; } }
