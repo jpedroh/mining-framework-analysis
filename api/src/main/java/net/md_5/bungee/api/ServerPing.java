6a4e52d33f0ef588ed28358ed0212001087862a1
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 17:21:30 +0000

    Mining Framework Analysis

--- api/src/main/java/net/md_5/bungee/api/ServerPing.java
+++ api/src/main/java/net/md_5/bungee/api/ServerPing.java
@@ -1,5 +1,4 @@
 package net.md_5.bungee.api;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -15,146 +14,105 @@ import net.md_5.bungee.api.chat.TextComponent;
  * Represents the standard list data returned by opening a server in the
  * Minecraft client server list, or hitting it with a packet 0xFE.
  */
-@Data
-@ToString(exclude = "favicon")
-@NoArgsConstructor
-@AllArgsConstructor
-public class ServerPing
-{
+@Data @ToString(exclude = "favicon") @NoArgsConstructor @AllArgsConstructor public class ServerPing {
+  private Protocol version;
 
-    private Protocol version;
+  @Data @AllArgsConstructor public static class Protocol {
+    private String name;
 
-    @Data
-    @AllArgsConstructor
-    public static class Protocol
-    {
+    private int protocol;
+  }
 
-        private String name;
-        private int protocol;
-    }
-    private Players players;
+  private Players players;
 
-    @Data
-    @AllArgsConstructor
-    public static class Players
-    {
+  @Data @AllArgsConstructor public static class Players {
+    private int max;
 
-        private int max;
-        private int online;
-        private PlayerInfo[] sample;
-    }
+    private int online;
 
-    @Data
-    @AllArgsConstructor
-    public static class PlayerInfo
-    {
+    private PlayerInfo[] sample;
+  }
 
-        private String name;
-        private UUID uniqueId;
+  @Data @AllArgsConstructor public static class PlayerInfo {
+    private String name;
 
-        private static final UUID md5UUID = Util.getUUID( "af74a02d19cb445bb07f6866a861f783" );
+    private UUID uniqueId;
 
-        public PlayerInfo(String name, String id)
-        {
-            setName( name );
-            setId( id );
-        }
+    private static final UUID md5UUID = Util.getUUID("af74a02d19cb445bb07f6866a861f783");
 
-        public void setId(String id)
-        {
-            try
-            {
-                uniqueId = Util.getUUID( id );
-            } catch ( Exception e )
-            {
-                // Fallback on a valid uuid otherwise Minecraft complains
-                uniqueId = md5UUID;
-            }
-        }
-
-        public String getId()
-        {
-            return uniqueId.toString().replace( "-", "" );
-        }
+    public PlayerInfo(String name, String id) {
+      setName(name);
+      setId(id);
     }
 
-    private BaseComponent description;
-    private Favicon favicon;
-
-    @Data
-    public static class ModInfo
-    {
-
-        private String type = "FML";
-        private List<ModItem> modList = new ArrayList<>();
+    public void setId(String id) {
+      try {
+        uniqueId = Util.getUUID(id);
+      } catch (Exception e) {
+        uniqueId = md5UUID;
+      }
     }
 
-    @Data
-    @AllArgsConstructor
-    public static class ModItem
-    {
-
-        private String modid;
-        private String version;
+    public String getId() {
+      return uniqueId.toString().replace("-", "");
     }
+  }
 
-    // Right now, we don't get the mods from the user, so we just use a stock ModInfo object to
-    // create the server ping. Vanilla clients will ignore this.
-    private final ModInfo modinfo = new ModInfo();
+  private BaseComponent description;
 
-    @Deprecated
-    public ServerPing(Protocol version, Players players, String description, String favicon)
-    {
-        this( version, players, new TextComponent( TextComponent.fromLegacyText( description ) ), favicon == null ? null : Favicon.create( favicon ) );
-    }
+  private Favicon favicon;
 
-    @Deprecated
-    public ServerPing(Protocol version, Players players, String description, Favicon favicon)
-    {
-        this( version, players, new TextComponent( TextComponent.fromLegacyText( description ) ), favicon );
-    }
+  @Data public static class ModInfo {
+    private String type = "FML";
 
-    @Deprecated
-    public String getFavicon()
-    {
-        return getFaviconObject() == null ? null : getFaviconObject().getEncoded();
-    }
+    private List<ModItem> modList = new ArrayList<>();
+  }
 
-    public Favicon getFaviconObject()
-    {
-        return this.favicon;
-    }
+  @Data @AllArgsConstructor public static class ModItem {
+    private String modid;
 
-    @Deprecated
-    public void setFavicon(String favicon)
-    {
-        setFavicon( favicon == null ? null : Favicon.create( favicon ) );
-    }
+    private String version;
+  }
 
-    public void setFavicon(Favicon favicon)
-    {
-        this.favicon = favicon;
-    }
+  private final ModInfo modinfo = new ModInfo();
 
-    @Deprecated
-    public void setDescription(String description)
-    {
-        this.description = new TextComponent( TextComponent.fromLegacyText( description ) );
-    }
+  @Deprecated public ServerPing(Protocol version, Players players, String description, String favicon) {
+    this(version, players, new TextComponent(TextComponent.fromLegacyText(description)), favicon == null ? null : Favicon.create(favicon));
+  }
 
-    @Deprecated
-    public String getDescription()
-    {
-        return BaseComponent.toLegacyText( description );
-    }
+  @Deprecated public ServerPing(Protocol version, Players players, String description, Favicon favicon) {
+    this(version, players, new TextComponent(TextComponent.fromLegacyText(description)), favicon);
+  }
 
-    public void setDescriptionComponent(BaseComponent description)
-    {
-        this.description = description;
-    }
+  @Deprecated public String getFavicon() {
+    return getFaviconObject() == null ? null : getFaviconObject().getEncoded();
+  }
 
-    public BaseComponent getDescriptionComponent()
-    {
-        return description;
-    }
+  public Favicon getFaviconObject() {
+    return this.favicon;
+  }
+
+  @Deprecated public void setFavicon(String favicon) {
+    setFavicon(favicon == null ? null : Favicon.create(favicon));
+  }
+
+  public void setFavicon(Favicon favicon) {
+    this.favicon = favicon;
+  }
+
+  @Deprecated public void setDescription(String description) {
+    this.description = new TextComponent(TextComponent.fromLegacyText(description));
+  }
+
+  @Deprecated public String getDescription() {
+    return BaseComponent.toLegacyText(description);
+  }
+
+  public void setDescriptionComponent(BaseComponent description) {
+    this.description = description;
+  }
+
+  public BaseComponent getDescriptionComponent() {
+    return description;
+  }
 }
--- protocol/src/main/java/net/md_5/bungee/protocol/KickStringWriter.java
+++ protocol/src/main/java/net/md_5/bungee/protocol/KickStringWriter.java
@@ -1,22 +1,22 @@
 package net.md_5.bungee.protocol;
-
 import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandler.Sharable;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToByteEncoder;
 
-@ChannelHandler.Sharable
-public class KickStringWriter extends MessageToByteEncoder<String>
-{
-
-    @Override
-    protected void encode(ChannelHandlerContext ctx, String msg, ByteBuf out) throws Exception
-    {
-        out.writeByte( 0xFF );
-        out.writeShort( msg.length() );
-        for ( char c : msg.toCharArray() )
-        {
-            out.writeChar( c );
-        }
+@
+<<<<<<< /usr/src/app/output/spigotmc/bungeecord/3c8bdedc27c6e7e1e4a8df2747f49bbc0a379dc1/protocol/src/main/java/net/md_5/bungee/protocol/KickStringWriter.java/left.java
+Sharable
+=======
+ChannelHandler.Sharable
+>>>>>>> /usr/src/app/output/spigotmc/bungeecord/3c8bdedc27c6e7e1e4a8df2747f49bbc0a379dc1/protocol/src/main/java/net/md_5/bungee/protocol/KickStringWriter.java/right.java
+ public class KickStringWriter extends MessageToByteEncoder<String> {
+  @Override protected void encode(ChannelHandlerContext ctx, String msg, ByteBuf out) throws Exception {
+    out.writeByte(0xFF);
+    out.writeShort(msg.length());
+    for (char c : msg.toCharArray()) {
+      out.writeChar(c);
     }
+  }
 }
