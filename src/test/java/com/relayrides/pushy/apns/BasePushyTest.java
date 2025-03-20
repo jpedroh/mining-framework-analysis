4060ac35390a45db488e421fbb83a45e09c93dd0
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 21:26:13 +0000

    Mining Framework Analysis

--- src/test/java/com/relayrides/pushy/apns/BasePushyTest.java
+++ src/test/java/com/relayrides/pushy/apns/BasePushyTest.java
@@ -1,29 +1,6 @@
-/* Copyright (c) 2013 RelayRides
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
 package com.relayrides.pushy.apns;
-
 import static org.junit.Assert.fail;
 import io.netty.channel.nio.NioEventLoopGroup;
-
 import java.io.IOException;
 import java.security.KeyManagementException;
 import java.security.KeyStoreException;
@@ -33,93 +10,81 @@ import java.security.cert.CertificateException;
 import java.util.Random;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
-
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.rules.Timeout;
-
 import com.relayrides.pushy.apns.util.SimpleApnsPushNotification;
 
 public abstract class BasePushyTest {
-	public static final ApnsEnvironment TEST_ENVIRONMENT =
-			new ApnsEnvironment("127.0.0.1", 2195, "127.0.0.1", 2196);
+  public static final ApnsEnvironment TEST_ENVIRONMENT = new ApnsEnvironment("127.0.0.1", 2195, "127.0.0.1", 2196);
 
-	private static final long LATCH_TIMEOUT_VALUE = 2;
-	private static final TimeUnit LATCH_TIMEOUT_UNIT = TimeUnit.SECONDS;
+  private static final long LATCH_TIMEOUT_VALUE = 2;
 
-	private static NioEventLoopGroup eventLoopGroup;
+  private static final TimeUnit LATCH_TIMEOUT_UNIT = TimeUnit.SECONDS;
 
-	private PushManager<SimpleApnsPushNotification> pushManager;
-	private MockApnsServer apnsServer;
-	private MockFeedbackServer feedbackServer;
+  private static NioEventLoopGroup eventLoopGroup;
 
-	@Rule
-	public Timeout globalTimeout = new Timeout(10000);
+  private PushManager<SimpleApnsPushNotification> pushManager;
 
-	@BeforeClass
-	public static void setUpBeforeClass() {
-		BasePushyTest.eventLoopGroup = new NioEventLoopGroup();
-	}
+  private MockApnsServer apnsServer;
 
-	@Before
-	public void setUp() throws InterruptedException, UnrecoverableKeyException, KeyManagementException, KeyStoreException, NoSuchAlgorithmException, CertificateException, IOException {
+  private MockFeedbackServer feedbackServer;
 
-		this.apnsServer = new MockApnsServer(TEST_ENVIRONMENT.getApnsGatewayPort(), BasePushyTest.eventLoopGroup);
-		this.apnsServer.start();
+  @BeforeClass public static void setUpBeforeClass() {
+    BasePushyTest.eventLoopGroup = new NioEventLoopGroup();
+  }
 
-		this.feedbackServer = new MockFeedbackServer(TEST_ENVIRONMENT.getFeedbackPort(), BasePushyTest.eventLoopGroup);
-		this.feedbackServer.start();
+  @Rule public Timeout globalTimeout = new Timeout(10000);
 
-		final PushManagerFactory<SimpleApnsPushNotification> pushManagerFactory =
-				new PushManagerFactory<SimpleApnsPushNotification>(TEST_ENVIRONMENT, SSLTestUtil.createSSLContextForTestClient());
+  @Before public void setUp() throws InterruptedException, UnrecoverableKeyException, KeyManagementException, KeyStoreException, NoSuchAlgorithmException, CertificateException, IOException {
+    this.apnsServer = new MockApnsServer(TEST_ENVIRONMENT.getApnsGatewayPort(), BasePushyTest.eventLoopGroup);
+    this.apnsServer.start();
+    this.feedbackServer = new MockFeedbackServer(TEST_ENVIRONMENT.getFeedbackPort(), BasePushyTest.eventLoopGroup);
+    this.feedbackServer.start();
+    final PushManagerFactory<SimpleApnsPushNotification> pushManagerFactory = new PushManagerFactory<SimpleApnsPushNotification>(TEST_ENVIRONMENT, SSLTestUtil.createSSLContextForTestClient());
+    pushManagerFactory.setEventLoopGroup(BasePushyTest.eventLoopGroup);
+    this.pushManager = pushManagerFactory.buildPushManager();
+  }
 
-		pushManagerFactory.setEventLoopGroup(BasePushyTest.eventLoopGroup);
+  @After public void tearDown() throws InterruptedException {
+    this.apnsServer.shutdown();
+    this.feedbackServer.shutdown();
+  }
 
-		this.pushManager = pushManagerFactory.buildPushManager();
-	}
+  @AfterClass public static void tearDownAfterClass() throws InterruptedException {
+    BasePushyTest.eventLoopGroup.shutdownGracefully().await();
+  }
 
-	@After
-	public void tearDown() throws InterruptedException {
-		this.apnsServer.shutdown();
-		this.feedbackServer.shutdown();
-	}
+  public NioEventLoopGroup getEventLoopGroup() {
+    return BasePushyTest.eventLoopGroup;
+  }
 
-	@AfterClass
-	public static void tearDownAfterClass() throws InterruptedException {
-		BasePushyTest.eventLoopGroup.shutdownGracefully().await();
-	}
+  public PushManager<SimpleApnsPushNotification> getPushManager() {
+    return this.pushManager;
+  }
 
-	public NioEventLoopGroup getEventLoopGroup() {
-		return BasePushyTest.eventLoopGroup;
-	}
+  public MockApnsServer getApnsServer() {
+    return this.apnsServer;
+  }
 
-	public PushManager<SimpleApnsPushNotification> getPushManager() {
-		return this.pushManager;
-	}
+  public MockFeedbackServer getFeedbackServer() {
+    return this.feedbackServer;
+  }
 
-	public MockApnsServer getApnsServer() {
-		return this.apnsServer;
-	}
+  public SimpleApnsPushNotification createTestNotification() {
+    final byte[] token = new byte[MockApnsServer.EXPECTED_TOKEN_SIZE];
+    new Random().nextBytes(token);
+    return new SimpleApnsPushNotification(token, "{\"aps\":{\"alert\":\"Hello\"}}");
+  }
 
-	public MockFeedbackServer getFeedbackServer() {
-		return this.feedbackServer;
-	}
-
-	public SimpleApnsPushNotification createTestNotification() {
-		final byte[] token = new byte[MockApnsServer.EXPECTED_TOKEN_SIZE];
-		new Random().nextBytes(token);
-
-		return new SimpleApnsPushNotification(token, "{\"aps\":{\"alert\":\"Hello\"}}");
-	}
-
-	public void waitForLatch(final CountDownLatch latch) throws InterruptedException {
-		while (latch.getCount() > 0) {
-			if (!latch.await(LATCH_TIMEOUT_VALUE, LATCH_TIMEOUT_UNIT)) {
-				fail(String.format("Timed out waiting for latch. Remaining count: %d", latch.getCount()));
-			}
-		}
-	}
+  public void waitForLatch(final CountDownLatch latch) throws InterruptedException {
+    while (latch.getCount() > 0) {
+      if (!latch.await(LATCH_TIMEOUT_VALUE, LATCH_TIMEOUT_UNIT)) {
+        fail(String.format("Timed out waiting for latch. Remaining count: %d", latch.getCount()));
+      }
+    }
+  }
 }
