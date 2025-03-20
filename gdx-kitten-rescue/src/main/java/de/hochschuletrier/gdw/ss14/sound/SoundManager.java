f6a0ac0c233f8823a62e731b74af03ba295d95ef
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 01:52:32 +0000

    Mining Framework Analysis

--- gdx-kitten-rescue/src/main/java/de/hochschuletrier/gdw/ss14/sound/SoundManager.java
+++ gdx-kitten-rescue/src/main/java/de/hochschuletrier/gdw/ss14/sound/SoundManager.java
@@ -1,9 +1,7 @@
 package de.hochschuletrier.gdw.ss14.sound;
-
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.utils.Array;
-
 import de.hochschuletrier.gdw.commons.gdx.assets.AssetManagerX;
 import de.hochschuletrier.gdw.commons.gdx.state.*;
 import de.hochschuletrier.gdw.ss14.Main;
@@ -14,82 +12,85 @@ import de.hochschuletrier.gdw.ss14.states.GameStates;
 import de.hochschuletrier.gdw.ss14.ui.UIActions;
 
 public class SoundManager {
-	private static Sound sound;
-	private static Sound loop;
-	private static boolean isLooping;
-	private static AssetManagerX assetManager;
-	private static float SystemVolume = 0.9f;
-	
-	public static void performAction(Enum action) {
-		GameStates actualGamestate = null;
-		String actionString = action.name();
+  private static Sound sound;
 
-		for (GameStates state : GameStates.values()) {
-			if (state.isActive()) {
-				actualGamestate = state;
-				break;
-			}
-		}
-		
-		switch(actualGamestate) {
-			case MAINMENU:
-			case OPTIONSMENU:
-			case PAUSEGAME:
-				switch(actionString) {
-					case "BELLOVER":
-						SoundManager.playSound("ui_bell"+SoundManager.random(1, 5));
-						break;
-					case "BELLCLICKED":
-						SoundManager.playSound("ui_menu");
-				}
-				break;
-			case GAMEPLAY:
-				switch (actionString) {
-					case "CATWALK":
-						System.out.println("WALKING CAT!!!");
-						Array<Integer> entities = new Array<Integer>();
-						entities = (EntityManager.getInstance().getAllEntitiesWithComponents(CatPropertyComponent.class));
-						int playerEntityID = entities.first();
-						CatPropertyComponent playerProperties = EntityManager.getInstance().getComponent(playerEntityID, CatPropertyComponent.class);
-						if (playerProperties.state != CatStateEnum.WALK) {
-							SoundManager.loop.stop();
-							SoundManager.isLooping = false;
-						}
-						else 
-							SoundManager.loopSound("gp_cat_victory");
-						break;
-				} 
-				break;
-			default:
-				break;
-				
-		}
-	}
+  private static Sound loop;
 
-	private static void playSound(String sound) {
-		SoundManager.sound = SoundManager.assetManager.getSound(sound);
-		SoundManager.sound.play(SoundManager.SystemVolume);
-	}
-	
-	private static void loopSound(String sound) {
-		if (SoundManager.isLooping) {
-			//SoundManager.loop.stop();
-			
-			return;
-		}
-		else {
-		SoundManager.isLooping = true;
-		
-		SoundManager.loop = SoundManager.assetManager.getSound(sound);
-		SoundManager.loop.loop(SoundManager.SystemVolume);
-		}
-	}
-	
-	public static void setAssetManager(AssetManagerX assetManager) {
-		SoundManager.assetManager = assetManager;
-	}
+  private static boolean isLooping;
 
-	private static int random (int low, int high) {
-		return (int) Math.round(Math.random() * (high - low) + low);
-	}
+  private static AssetManagerX assetManager;
+
+  private static float SystemVolume = 0.9f;
+
+  public static void performAction(Enum action) {
+    GameStates actualGamestate = null;
+    String actionString = action.name();
+    for (GameStates state : GameStates.values()) {
+      if (state.isActive()) {
+        actualGamestate = state;
+        break;
+      }
+    }
+    switch (actualGamestate) {
+      case MAINMENU:
+      case OPTIONSMENU:
+      case PAUSEGAME:
+      switch (actionString) {
+        case "BELLOVER":
+        SoundManager.playSound("ui_bell" + SoundManager.random(1, 5));
+        break;
+        case "BELLCLICKED":
+        SoundManager.playSound("ui_menu");
+      }
+      break;
+      case GAMEPLAY:
+      switch (actionString) {
+        case "CATWALK":
+        System.out.println("WALKING CAT!!!");
+        Array<Integer> entities = new Array<Integer>();
+        entities = (EntityManager.getInstance().getAllEntitiesWithComponents(CatPropertyComponent.class));
+        int playerEntityID = entities.first();
+        CatPropertyComponent playerProperties = EntityManager.getInstance().getComponent(playerEntityID, CatPropertyComponent.class);
+        if (
+<<<<<<< /usr/src/app/output/lusito/gamedevweek/b32b56644c29a6ed63303f639c483e935734c742/gdx-kitten-rescue/src/main/java/de/hochschuletrier/gdw/ss14/sound/SoundManager.java/left.java
+        playerProperties.state
+=======
+        cp.getState()
+>>>>>>> /usr/src/app/output/lusito/gamedevweek/b32b56644c29a6ed63303f639c483e935734c742/gdx-kitten-rescue/src/main/java/de/hochschuletrier/gdw/ss14/sound/SoundManager.java/right.java
+         != CatStateEnum.WALK) {
+          SoundManager.loop.stop();
+          SoundManager.isLooping = false;
+        } else {
+          SoundManager.loopSound("gp_cat_victory");
+        }
+        break;
+      }
+      break;
+      default:
+      break;
+    }
+  }
+
+  private static void playSound(String sound) {
+    SoundManager.sound = SoundManager.assetManager.getSound(sound);
+    SoundManager.sound.play(SoundManager.SystemVolume);
+  }
+
+  private static void loopSound(String sound) {
+    if (SoundManager.isLooping) {
+      return;
+    } else {
+      SoundManager.isLooping = true;
+      SoundManager.loop = SoundManager.assetManager.getSound(sound);
+      SoundManager.loop.loop(SoundManager.SystemVolume);
+    }
+  }
+
+  public static void setAssetManager(AssetManagerX assetManager) {
+    SoundManager.assetManager = assetManager;
+  }
+
+  private static int random(int low, int high) {
+    return (int) Math.round(Math.random() * (high - low) + low);
+  }
 }
