27c8ce846388f152988a03066f7cdd4adadd8e66
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 07:43:51 +0000

    Mining Framework Analysis

--- src/main/java/com/mitchellbosecke/pebble/extension/core/JoinFilter.java
+++ src/main/java/com/mitchellbosecke/pebble/extension/core/JoinFilter.java
@@ -1,21 +1,10 @@
-/*******************************************************************************
- * This file is part of Pebble.
- * 
- * Copyright (c) 2014 by Mitchell Bösecke
- * 
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- ******************************************************************************/
 package com.mitchellbosecke.pebble.extension.core;
-
 import com.mitchellbosecke.pebble.error.PebbleException;
 import com.mitchellbosecke.pebble.extension.Filter;
-import com.mitchellbosecke.pebble.template.EvaluationContext;
-import com.mitchellbosecke.pebble.template.PebbleTemplate;
-
 import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Collection;
+import com.mitchellbosecke.pebble.template.EvaluationContext;
+import java.util.Collections;
+import com.mitchellbosecke.pebble.template.PebbleTemplate;
 import java.util.List;
 import java.util.Map;
 
@@ -27,61 +16,79 @@ import java.util.Map;
  *
  */
 public class JoinFilter implements Filter {
+  private static final List<String> argumentNames = Collections.singletonList("separator");
 
-    private final List<String> argumentNames = new ArrayList<>();
+  public JoinFilter() {
+  }
 
-    public JoinFilter() {
-        argumentNames.add("separator");
+  @Override public List<String> getArgumentNames() {
+    return argumentNames;
+  }
+
+  @Override public Object apply(Object input, Map<String, Object> args, PebbleTemplate self, EvaluationContext context, int lineNumber) throws PebbleException {
+    if (input == null) {
+      return null;
     }
+    String separator = args.containsKey("separator") ? (String) args.get("separator") : null;
 
-    @Override
-    public List<String> getArgumentNames() {
-        return argumentNames;
+<<<<<<< /usr/src/app/output/mbosecke/pebble/8dbd27621c8d8c5db8d229da6ebf18a851c146c6/src/main/java/com/mitchellbosecke/pebble/extension/core/JoinFilter.java/left.java
+    if (input.getClass().isArray()) {
+      List<Object> items = new ArrayList<>();
+      int length = Array.getLength(input);
+      for (int i = 0; i < length; i++) {
+        items.add(Array.get(input, i));
+      }
+      return join(items, glue);
+    } else {
+      if (input instanceof Collection) {
+        return join((Collection<?>) input, glue);
+      } else {
+        throw new PebbleException(null, "The \'join\' filter expects that the input is either a collection or an array.", lineNumber, self.getName());
+      }
     }
-
-    @Override
-    public Object apply(Object input, Map<String, Object> args, PebbleTemplate self, EvaluationContext context, int lineNumber)
-            throws PebbleException {
-        if (input == null) {
-            return null;
+=======
+    if (input instanceof Iterable<?>) {
+      boolean isFirst = true;
+      for (Object data : ((Iterable<?>) input)) {
+        append(builder, data, isFirst ? null : separator);
+        isFirst = false;
+      }
+    } else {
+      if (input instanceof Object[]) {
+        Object[] array = (Object[]) input;
+        for (int i = 0; i < array.length; i++) {
+          append(builder, array[i], i >= 1 ? separator : null);
         }
-
-        String glue = null;
-        if (args.containsKey("separator")) {
-            glue = (String) args.get("separator");
-        }
-
+      } else {
         if (input.getClass().isArray()) {
-            List<Object> items = new ArrayList<>();
-            int length = Array.getLength(input);
-            for (int i = 0; i < length; i++) {
-                items.add(Array.get(input, i));
-            }
-            return join(items, glue);
-        }
-
-        else if (input instanceof Collection) {
-            return join((Collection<?>) input, glue);
+          for (int i = 0; i < Array.getLength(input); i++) {
+            append(builder, Array.get(input, i), i >= 1 ? separator : null);
+          }
         } else {
-            throw new PebbleException(null,
-                    "The 'join' filter expects that the input is either a collection or an array.", lineNumber,
-                    self.getName());
+          throw new IllegalArgumentException("input is not an array or collection");
         }
+      }
     }
+>>>>>>> /usr/src/app/output/mbosecke/pebble/8dbd27621c8d8c5db8d229da6ebf18a851c146c6/src/main/java/com/mitchellbosecke/pebble/extension/core/JoinFilter.java/right.java
+  }
 
-    private String join(Collection<?> inputCollection, String glue) {
-        StringBuilder builder = new StringBuilder();
-
-        boolean isFirst = true;
-        for (Object entry : inputCollection) {
-
-            if (!isFirst && glue != null) {
-                builder.append(glue);
-            }
-            builder.append(entry);
-
-            isFirst = false;
-        }
-        return builder.toString();
+  private String join(Collection<?> inputCollection, String glue) {
+    StringBuilder builder = new StringBuilder();
+    boolean isFirst = true;
+    for (Object entry : inputCollection) {
+      if (!isFirst && glue != null) {
+        builder.append(glue);
+      }
+      builder.append(entry);
+      isFirst = false;
     }
+    return builder.toString();
+  }
+
+  private void append(StringBuilder builder, Object data, String separator) {
+    if (separator != null) {
+      builder.append(separator);
+    }
+    builder.append(data);
+  }
 }
