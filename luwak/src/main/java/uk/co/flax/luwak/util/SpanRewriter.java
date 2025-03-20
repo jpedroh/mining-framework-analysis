845b774b493cc92218c69ba498e6727bf1dc46c1
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 06:40:08 +0000

    Mining Framework Analysis

--- luwak/src/main/java/uk/co/flax/luwak/util/SpanRewriter.java
+++ luwak/src/main/java/uk/co/flax/luwak/util/SpanRewriter.java
@@ -25,7 +25,11 @@ import org.apache.lucene.index.PrefixCodedTerms;
 import org.apache.lucene.index.Term;
 import org.apache.lucene.queries.TermsQuery;
 import org.apache.lucene.search.*;
-import org.apache.lucene.search.spans.*;
+import org.apache.lucene.search.spans.SpanMultiTermQueryWrapper;
+import org.apache.lucene.search.spans.SpanNearQuery;
+import org.apache.lucene.search.spans.SpanOrQuery;
+import org.apache.lucene.search.spans.SpanQuery;
+import org.apache.lucene.search.spans.SpanTermQuery;
 import org.apache.lucene.util.BytesRef;
 
 public class SpanRewriter {
