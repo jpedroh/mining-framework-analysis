59b96b73175a7d760458f4d30fbc614e2b504fe5
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 02:10:06 +0000

    Mining Framework Analysis

--- gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/SimpleCheckBox.java
+++ gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/SimpleCheckBox.java
@@ -1,25 +1,4 @@
 package org.gwtbootstrap3.client.ui;
-
-/*
- * #%L
- * GwtBootstrap3
- * %%
- * Copyright (C) 2013 - 2014 GwtBootstrap3
- * %%
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * #L%
- */
-
 import org.gwtbootstrap3.client.ui.base.HasFormValue;
 import org.gwtbootstrap3.client.ui.base.HasId;
 import org.gwtbootstrap3.client.ui.base.HasPull;
@@ -31,7 +10,6 @@ import org.gwtbootstrap3.client.ui.base.mixin.PullMixin;
 import org.gwtbootstrap3.client.ui.constants.DeviceSize;
 import org.gwtbootstrap3.client.ui.constants.Pull;
 import org.gwtbootstrap3.client.ui.impl.SimpleCheckBoxImpl;
-
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Document;
 import com.google.gwt.dom.client.Element;
@@ -45,12 +23,10 @@ import com.google.gwt.user.client.ui.RootPanel;
 /**
  * A simple checkbox widget, with no label.
  */
-public class SimpleCheckBox extends com.google.gwt.user.client.ui.SimpleCheckBox implements HasResponsiveness, HasId,
-        HasPull, HasFormValue, HasChangeHandlers {
+public class SimpleCheckBox extends com.google.gwt.user.client.ui.SimpleCheckBox implements HasResponsiveness, HasId, HasPull, HasFormValue, HasChangeHandlers {
+  private static final SimpleCheckBoxImpl impl = GWT.create(SimpleCheckBoxImpl.class);
 
-    private static final SimpleCheckBoxImpl impl = GWT.create(SimpleCheckBoxImpl.class);
-
-    /**
+  /**
      * Creates a SimpleCheckBox widget that wraps an existing &lt;input
      * type='checkbox'&gt; element.
      * 
@@ -61,31 +37,28 @@ public class SimpleCheckBox extends com.google.gwt.user.client.ui.SimpleCheckBox
      * @param element
      *            the element to be wrapped
      */
-    public static SimpleCheckBox wrap(Element element) {
-        // Assert that the element is attached.
-        assert Document.get().getBody().isOrHasChild(element);
+  public static SimpleCheckBox wrap(Element element) {
+    assert Document.get().getBody().isOrHasChild(element);
+    SimpleCheckBox checkBox = new SimpleCheckBox(InputElement.as(element));
+    checkBox.onAttach();
+    RootPanel.detachOnWindowClose(checkBox);
+    return checkBox;
+  }
 
-        SimpleCheckBox checkBox = new SimpleCheckBox(InputElement.as(element));
+  private final IdMixin<SimpleCheckBox> idMixin = new IdMixin<SimpleCheckBox>(this);
 
-        // Mark it attached and remember it for cleanup.
-        checkBox.onAttach();
-        RootPanel.detachOnWindowClose(checkBox);
+  private final PullMixin<SimpleCheckBox> pullMixin = new PullMixin<SimpleCheckBox>(this);
 
-        return checkBox;
-    }
+  private final EnabledMixin<SimpleCheckBox> enabledMixin = new EnabledMixin<SimpleCheckBox>(this);
 
-    private final IdMixin<SimpleCheckBox> idMixin = new IdMixin<SimpleCheckBox>(this);
-    private final PullMixin<SimpleCheckBox> pullMixin = new PullMixin<SimpleCheckBox>(this);
-    private final EnabledMixin<SimpleCheckBox> enabledMixin = new EnabledMixin<SimpleCheckBox>(this);
-
-    /**
+  /**
      * Creates a new simple checkbox.
      */
-    public SimpleCheckBox() {
-        super(Document.get().createCheckInputElement());
-    }
+  public SimpleCheckBox() {
+    super(Document.get().createCheckInputElement());
+  }
 
-    /**
+  /**
      * This constructor may be used by subclasses to explicitly use an existing
      * element. This element must be an &lt;input&gt; element whose type is
      * 'checkbox'.
@@ -93,76 +66,65 @@ public class SimpleCheckBox extends com.google.gwt.user.client.ui.SimpleCheckBox
      * @param element
      *            the element to be used
      */
-    protected SimpleCheckBox(InputElement element) {
-        super(element);
-    }
+  protected SimpleCheckBox(InputElement element) {
+    super(element);
+  }
 
-    @Override
-    public HandlerRegistration addChangeHandler(ChangeHandler handler) {
-        return addDomHandler(handler, ChangeEvent.getType());
-    }
+  @Override public HandlerRegistration addChangeHandler(ChangeHandler handler) {
+    return addDomHandler(handler, ChangeEvent.getType());
+  }
 
-    @Override
-    public void setEnabled(boolean enabled) {
-        enabledMixin.setEnabled(enabled);
-    }
+  @Override public void setEnabled(boolean enabled) {
+    enabledMixin.setEnabled(enabled);
+  }
 
-    @Override
-    public boolean isEnabled() {
-        return enabledMixin.isEnabled();
-    }
+  @Override public boolean isEnabled() {
+    return enabledMixin.isEnabled();
+  }
 
-    /**
+  /**
      * {@inheritDoc}
      */
-    @Override
-    public void setId(final String id) {
-        idMixin.setId(id);
-    }
+  @Override public void setId(final String id) {
+    idMixin.setId(id);
+  }
 
-    /**
+  /**
      * {@inheritDoc}
      */
-    @Override
-    public String getId() {
-        return idMixin.getId();
-    }
+  @Override public String getId() {
+    return idMixin.getId();
+  }
 
-    /**
+  /**
      * {@inheritDoc}
      */
-    @Override
-    public void setVisibleOn(final DeviceSize deviceSize) {
-        StyleHelper.setVisibleOn(this, deviceSize);
-    }
+  @Override public void setVisibleOn(final DeviceSize deviceSize) {
+    StyleHelper.setVisibleOn(this, deviceSize);
+  }
 
-    /**
+  /**
      * {@inheritDoc}
      */
-    @Override
-    public void setHiddenOn(final DeviceSize deviceSize) {
-        StyleHelper.setHiddenOn(this, deviceSize);
-    }
+  @Override public void setHiddenOn(final DeviceSize deviceSize) {
+    StyleHelper.setHiddenOn(this, deviceSize);
+  }
 
-    /**
+  /**
      * {@inheritDoc}
      */
-    @Override
-    public void setPull(final Pull pull) {
-        pullMixin.setPull(pull);
-    }
+  @Override public void setPull(final Pull pull) {
+    pullMixin.setPull(pull);
+  }
 
-    /**
+  /**
      * {@inheritDoc}
      */
-    @Override
-    public Pull getPull() {
-        return pullMixin.getPull();
-    }
+  @Override public Pull getPull() {
+    return pullMixin.getPull();
+  }
 
-    @Override
-    protected void ensureDomEventHandlers() {
-        impl.ensureDomEventHandlers(this);
-    }
-
+  @Override protected void ensureDomEventHandlers() {
+    impl.ensureDomEventHandlers(this);
+  }
 }
--- gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/SimpleRadioButton.java
+++ gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/SimpleRadioButton.java
@@ -1,25 +1,4 @@
 package org.gwtbootstrap3.client.ui;
-
-/*
- * #%L
- * GwtBootstrap3
- * %%
- * Copyright (C) 2013 - 2014 GwtBootstrap3
- * %%
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * #L%
- */
-
 import org.gwtbootstrap3.client.ui.base.HasFormValue;
 import org.gwtbootstrap3.client.ui.base.HasId;
 import org.gwtbootstrap3.client.ui.base.HasPull;
@@ -31,7 +10,6 @@ import org.gwtbootstrap3.client.ui.base.mixin.PullMixin;
 import org.gwtbootstrap3.client.ui.constants.DeviceSize;
 import org.gwtbootstrap3.client.ui.constants.Pull;
 import org.gwtbootstrap3.client.ui.impl.SimpleRadioButtonImpl;
-
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Document;
 import com.google.gwt.dom.client.Element;
@@ -46,12 +24,10 @@ import com.google.gwt.user.client.ui.RootPanel;
 /**
  * A simple radio button widget, with no label.
  */
-public class SimpleRadioButton extends com.google.gwt.user.client.ui.SimpleRadioButton implements HasResponsiveness,
-        HasId, HasPull, HasFormValue, HasChangeHandlers {
+public class SimpleRadioButton extends com.google.gwt.user.client.ui.SimpleRadioButton implements HasResponsiveness, HasId, HasPull, HasFormValue, HasChangeHandlers {
+  private static final SimpleRadioButtonImpl impl = GWT.create(SimpleRadioButtonImpl.class);
 
-    private static final SimpleRadioButtonImpl impl = GWT.create(SimpleRadioButtonImpl.class);
-
-    /**
+  /**
      * Creates a SimpleRadioButton widget that wraps an existing &lt;input
      * type='radio'&gt; element.
      * 
@@ -62,24 +38,21 @@ public class SimpleRadioButton extends com.google.gwt.user.client.ui.SimpleRadio
      * @param element
      *            the element to be wrapped
      */
-    public static SimpleRadioButton wrap(Element element) {
-        // Assert that the element is attached.
-        assert Document.get().getBody().isOrHasChild(element);
+  public static SimpleRadioButton wrap(Element element) {
+    assert Document.get().getBody().isOrHasChild(element);
+    SimpleRadioButton radioButton = new SimpleRadioButton(InputElement.as(element));
+    radioButton.onAttach();
+    RootPanel.detachOnWindowClose(radioButton);
+    return radioButton;
+  }
 
-        SimpleRadioButton radioButton = new SimpleRadioButton(InputElement.as(element));
+  private final IdMixin<SimpleRadioButton> idMixin = new IdMixin<SimpleRadioButton>(this);
 
-        // Mark it attached and remember it for cleanup.
-        radioButton.onAttach();
-        RootPanel.detachOnWindowClose(radioButton);
+  private final PullMixin<SimpleRadioButton> pullMixin = new PullMixin<SimpleRadioButton>(this);
 
-        return radioButton;
-    }
+  private final EnabledMixin<SimpleRadioButton> enabledMixin = new EnabledMixin<SimpleRadioButton>(this);
 
-    private final IdMixin<SimpleRadioButton> idMixin = new IdMixin<SimpleRadioButton>(this);
-    private final PullMixin<SimpleRadioButton> pullMixin = new PullMixin<SimpleRadioButton>(this);
-    private final EnabledMixin<SimpleRadioButton> enabledMixin = new EnabledMixin<SimpleRadioButton>(this);
-
-    /**
+  /**
      * Creates a new radio associated with a particular group name. All radio
      * buttons associated with the same group name belong to a
      * mutually-exclusive set.
@@ -90,12 +63,11 @@ public class SimpleRadioButton extends com.google.gwt.user.client.ui.SimpleRadio
      * @param name
      *            the group name with which to associate the radio button
      */
-    @UiConstructor
-    public SimpleRadioButton(String name) {
-        this(Document.get().createRadioInputElement(name));
-    }
+  @UiConstructor public SimpleRadioButton(String name) {
+    this(Document.get().createRadioInputElement(name));
+  }
 
-    /**
+  /**
      * This constructor may be used by subclasses to explicitly use an existing
      * element. This element must be an &lt;input&gt; element whose type is
      * 'radio'.
@@ -103,76 +75,69 @@ public class SimpleRadioButton extends com.google.gwt.user.client.ui.SimpleRadio
      * @param element
      *            the element to be used
      */
-    protected SimpleRadioButton(InputElement element) {
-        super(element);
-    }
+  protected SimpleRadioButton(InputElement element) {
+    super(element);
+  }
 
-    @Override
-    public HandlerRegistration addChangeHandler(ChangeHandler handler) {
-        return addDomHandler(handler, ChangeEvent.getType());
-    }
+  @Override public HandlerRegistration addChangeHandler(ChangeHandler handler) {
+    return addDomHandler(handler, ChangeEvent.getType());
+  }
 
-    @Override
-    public void setEnabled(boolean enabled) {
-        enabledMixin.setEnabled(enabled);
-    }
+  @Override public void setEnabled(boolean enabled) {
+    enabledMixin.setEnabled(enabled);
+  }
 
-    @Override
-    public boolean isEnabled() {
-        return enabledMixin.isEnabled();
-    }
+  @Override public boolean isEnabled() {
+    return enabledMixin.isEnabled();
+  }
 
-    /**
+  /**
      * {@inheritDoc}
      */
-    @Override
-    public void setId(final String id) {
-        idMixin.setId(id);
-    }
+  @Override public void setId(final String id) {
+    idMixin.setId(id);
+  }
 
-    /**
+  /**
      * {@inheritDoc}
      */
-    @Override
-    public String getId() {
-        return idMixin.getId();
-    }
+  @Override public String getId() {
+    return idMixin.getId();
+  }
 
-    /**
+  /**
      * {@inheritDoc}
      */
-    @Override
-    public void setVisibleOn(final DeviceSize deviceSize) {
-        StyleHelper.setVisibleOn(this, deviceSize);
-    }
+  @Override public void setVisibleOn(final DeviceSize deviceSize) {
+    StyleHelper.setVisibleOn(this, deviceSize);
+  }
 
-    /**
+  /**
      * {@inheritDoc}
      */
-    @Override
-    public void setHiddenOn(final DeviceSize deviceSize) {
-        StyleHelper.setHiddenOn(this, deviceSize);
-    }
+  @Override public void setHiddenOn(final DeviceSize deviceSize) {
+    StyleHelper.setHiddenOn(this, deviceSize);
+  }
 
-    /**
+  /**
      * {@inheritDoc}
      */
-    @Override
-    public void setPull(final Pull pull) {
-        pullMixin.setPull(pull);
-    }
+  @Override public void setPull(final Pull pull) {
+    pullMixin.setPull(pull);
+  }
 
-    /**
+  /**
      * {@inheritDoc}
      */
-    @Override
-    public Pull getPull() {
-        return pullMixin.getPull();
-    }
+  @Override public Pull getPull() {
+    return pullMixin.getPull();
+  }
 
-    @Override
-    protected void ensureDomEventHandlers() {
-        impl.ensureDomEventHandlers(this);
-    }
-
+  /**
+     * No-op. CheckBox's click handler is no good for radio button, so don't use
+     * it. Our event handling is all done in {@link #onBrowserEvent}
+     */
+  @Override protected void ensureDomEventHandlers() {
+    impl.ensureDomEventHandlers(this);
+  }
 }
