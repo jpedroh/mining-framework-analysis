99ca2b580becad6bcd5d25edf0fb0ac3bef01bed
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 06:33:23 +0000

    Mining Framework Analysis

--- sonar-pmd-plugin/src/main/java/org/sonar/plugins/pmd/PmdTemplate.java
+++ sonar-pmd-plugin/src/main/java/org/sonar/plugins/pmd/PmdTemplate.java
@@ -1,31 +1,11 @@
-/*
- * SonarQube PMD Plugin
- * Copyright (C) 2012-2019 SonarSource SA
- * mailto:info AT sonarsource DOT com
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3 of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- */
 package org.sonar.plugins.pmd;
-
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
-
 import net.sourceforge.pmd.PMDConfiguration;
 import net.sourceforge.pmd.PMDException;
 import net.sourceforge.pmd.RuleContext;
@@ -38,70 +18,75 @@ import org.sonar.api.utils.log.Logger;
 import org.sonar.api.utils.log.Loggers;
 
 public class PmdTemplate {
+  private static final Logger LOG = Loggers.get(PmdTemplate.class);
 
-    private static final Logger LOG = Loggers.get(PmdTemplate.class);
-    private static final Map<String, String> JAVA_VERSIONS = prepareVersions();
+  private static final Map<String, String> JAVA_VERSIONS = prepareVersions();
 
-    private static Map<String, String> prepareVersions() {
-        final Map<String, String> versions = new HashMap<>();
-        versions.put("1.1", "1.3");
-        versions.put("1.2", "1.3");
-        versions.put("5", "1.5");
-        versions.put("6", "1.6");
-        versions.put("7", "1.7");
-        versions.put("8", "1.8");
-        versions.put("9", "9");
-        versions.put("1.9", "9");
-        versions.put("10", "10");
-        versions.put("1.10", "10");
-        versions.put("11", "11");
-        versions.put("1.11", "11");
+  private static Map<String, String> prepareVersions() {
+    final Map<String, String> versions = new HashMap<>();
+    versions.put("1.1", "1.3");
+    versions.put("1.2", "1.3");
+    versions.put("5", "1.5");
+    versions.put("6", "1.6");
+    versions.put("7", "1.7");
+    versions.put("8", "1.8");
+    versions.put("9", "9");
+    versions.put("1.9", "9");
+    versions.put("10", "10");
+    versions.put("1.10", "10");
+    versions.put("11", "11");
+    versions.put("1.11", "11");
+    return versions;
+  }
 
-        return versions;
+  private SourceCodeProcessor processor;
+
+  private PMDConfiguration configuration;
+
+  PmdTemplate(PMDConfiguration configuration, SourceCodeProcessor processor) {
+    this.configuration = configuration;
+    this.processor = processor;
+  }
+
+  public static PmdTemplate create(String javaVersion, ClassLoader classloader, Charset charset) {
+    PMDConfiguration configuration = new PMDConfiguration();
+    configuration.setDefaultLanguageVersion(languageVersion(javaVersion));
+    configuration.setClassLoader(classloader);
+    configuration.setSourceEncoding(charset.name());
+    SourceCodeProcessor processor = new SourceCodeProcessor(configuration);
+    return new PmdTemplate(configuration, processor);
+  }
+
+  static LanguageVersion languageVersion(String javaVersion) {
+    String version = normalize(javaVersion);
+    LanguageVersion languageVersion = new JavaLanguageModule().getVersion(version);
+    if (languageVersion == null) {
+      throw new IllegalArgumentException("Unsupported Java version for PMD: " + version);
     }
+    LOG.info("Java version: " + version);
+    return languageVersion;
+  }
 
-    private SourceCodeProcessor processor;
-    private PMDConfiguration configuration;
+  private static String normalize(String version) {
+    return JAVA_VERSIONS.getOrDefault(version, version);
+  }
 
-    PmdTemplate(PMDConfiguration configuration, SourceCodeProcessor processor) {
-        this.configuration = configuration;
-        this.processor = processor;
+  PMDConfiguration configuration() {
+    return configuration;
+  }
+
+  public void process(InputFile file, RuleSets rulesets, RuleContext ruleContext) {
+    ruleContext.setSourceCodeFile(
+<<<<<<< /usr/src/app/output/sonarcommunity/sonar-pmd/2301c0627b5ab5276acb103f933e68d2ab6ca3be/sonar-pmd-plugin/src/main/java/org/sonar/plugins/pmd/PmdTemplate.java/left.java
+    new File(file.uri().toString())
+=======
+    Paths.get(file.uri()).toFile()
+>>>>>>> /usr/src/app/output/sonarcommunity/sonar-pmd/2301c0627b5ab5276acb103f933e68d2ab6ca3be/sonar-pmd-plugin/src/main/java/org/sonar/plugins/pmd/PmdTemplate.java/right.java
+    );
+    try (InputStream inputStream = file.inputStream()) {
+      processor.processSourceCode(inputStream, rulesets, ruleContext);
+    } catch (RuntimeException | IOException | PMDException e) {
+      LOG.error("Fail to execute PMD. Following file is ignored: " + file, e);
     }
-
-    public static PmdTemplate create(String javaVersion, ClassLoader classloader, Charset charset) {
-        PMDConfiguration configuration = new PMDConfiguration();
-        configuration.setDefaultLanguageVersion(languageVersion(javaVersion));
-        configuration.setClassLoader(classloader);
-        configuration.setSourceEncoding(charset.name());
-        SourceCodeProcessor processor = new SourceCodeProcessor(configuration);
-        return new PmdTemplate(configuration, processor);
-    }
-
-    static LanguageVersion languageVersion(String javaVersion) {
-        String version = normalize(javaVersion);
-        LanguageVersion languageVersion = new JavaLanguageModule().getVersion(version);
-        if (languageVersion == null) {
-            throw new IllegalArgumentException("Unsupported Java version for PMD: " + version);
-        }
-        LOG.info("Java version: " + version);
-        return languageVersion;
-    }
-
-    private static String normalize(String version) {
-        return JAVA_VERSIONS.getOrDefault(version, version);
-    }
-
-    PMDConfiguration configuration() {
-        return configuration;
-    }
-
-    public void process(InputFile file, RuleSets rulesets, RuleContext ruleContext) {
-        ruleContext.setSourceCodeFile(Paths.get(file.uri()).toFile());
-
-        try (InputStream inputStream = file.inputStream()) {
-            processor.processSourceCode(inputStream, rulesets, ruleContext);
-        } catch (RuntimeException | IOException | PMDException e) {
-            LOG.error("Fail to execute PMD. Following file is ignored: " + file, e);
-        }
-    }
+  }
 }
--- sonar-pmd-plugin/src/test/java/org/sonar/plugins/pmd/PmdTemplateTest.java
+++ sonar-pmd-plugin/src/test/java/org/sonar/plugins/pmd/PmdTemplateTest.java
@@ -1,24 +1,4 @@
-/*
- * SonarQube PMD Plugin
- * Copyright (C) 2012-2019 SonarSource SA
- * mailto:info AT sonarsource DOT com
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3 of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- */
 package org.sonar.plugins.pmd;
-
 import net.sourceforge.pmd.*;
 import net.sourceforge.pmd.lang.LanguageVersionHandler;
 import org.junit.jupiter.api.Test;
@@ -27,15 +7,14 @@ import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.stubbing.Answer;
 import org.sonar.api.batch.fs.InputFile;
 import org.sonar.api.batch.fs.internal.TestInputFileBuilder;
-
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.catchThrowable;
 import static org.mockito.ArgumentMatchers.any;
@@ -43,79 +22,64 @@ import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
 
 class PmdTemplateTest {
+  private final RuleSets rulesets = mock(RuleSets.class);
 
-    private final RuleSets rulesets = mock(RuleSets.class);
-    private final RuleContext ruleContext = mock(RuleContext.class);
-    private final PMDConfiguration configuration = mock(PMDConfiguration.class);
-    private final SourceCodeProcessor processor = mock(SourceCodeProcessor.class);
-    private final InputFile inputFile = TestInputFileBuilder.create(
-            "src",
-            "test/resources/org/sonar/plugins/pmd/source.txt"
-    ).build();
+  private final RuleContext ruleContext = mock(RuleContext.class);
 
-    @Test
-    void should_process_input_file() throws Exception {
-        doAnswer((Answer<Void>) invocation -> {
-            final InputStream inputStreamArg = (InputStream) invocation.getArguments()[0];
-            final List<String> inputStreamLines =
-                    new BufferedReader(new InputStreamReader(inputStreamArg))
-                            .lines()
-                            .collect(Collectors.toList());
-            assertThat(inputStreamLines).containsExactly("Example source");
-            return null;
-        }).when(processor).processSourceCode(any(InputStream.class), eq(rulesets), eq(ruleContext));
+  private final PMDConfiguration configuration = mock(PMDConfiguration.class);
 
-        new PmdTemplate(configuration, processor).process(inputFile, rulesets, ruleContext);
+  private final SourceCodeProcessor processor = mock(SourceCodeProcessor.class);
 
-        verify(ruleContext).setSourceCodeFile(Paths.get(inputFile.uri()).toFile());
-        verify(processor).processSourceCode(any(InputStream.class), eq(rulesets), eq(ruleContext));
-    }
+  private final InputFile inputFile = TestInputFileBuilder.create("src", "test/resources/org/sonar/plugins/pmd/source.txt").build();
 
-    @Test
-    void should_ignore_PMD_error() throws PMDException {
-        doThrow(new PMDException("BUG"))
-                .when(processor).processSourceCode(any(InputStream.class), any(RuleSets.class), any(RuleContext.class));
+  @Test void should_process_input_file() throws Exception {
+    doAnswer((Answer<Void>) (invocation) -> {
+      final InputStream inputStreamArg = (InputStream) invocation.getArguments()[0];
+      final List<String> inputStreamLines = new BufferedReader(new InputStreamReader(inputStreamArg)).lines().collect(Collectors.toList());
+      assertThat(inputStreamLines).containsExactly("Example source");
+      return null;
+    }).when(processor).processSourceCode(any(InputStream.class), eq(rulesets), eq(ruleContext));
+    new PmdTemplate(configuration, processor).process(inputFile, rulesets, ruleContext);
+    verify(ruleContext).setSourceCodeFile(
+<<<<<<< /usr/src/app/output/sonarcommunity/sonar-pmd/2301c0627b5ab5276acb103f933e68d2ab6ca3be/sonar-pmd-plugin/src/test/java/org/sonar/plugins/pmd/PmdTemplateTest.java/left.java
+    new File(inputFile.uri().toString())
+=======
+    Paths.get(inputFile.uri()).toFile()
+>>>>>>> /usr/src/app/output/sonarcommunity/sonar-pmd/2301c0627b5ab5276acb103f933e68d2ab6ca3be/sonar-pmd-plugin/src/test/java/org/sonar/plugins/pmd/PmdTemplateTest.java/right.java
+    );
+    verify(processor).processSourceCode(any(InputStream.class), eq(rulesets), eq(ruleContext));
+  }
 
-        new PmdTemplate(configuration, processor).process(inputFile, rulesets, ruleContext);
-    }
+  @Test void should_ignore_PMD_error() throws PMDException {
+    doThrow(new PMDException("BUG")).when(processor).processSourceCode(any(InputStream.class), any(RuleSets.class), any(RuleContext.class));
+    new PmdTemplate(configuration, processor).process(inputFile, rulesets, ruleContext);
+  }
 
-    @ParameterizedTest
-    @ValueSource(strings = {
-            "1.2", "5", "6", "7", "8", "9", "1.9", "10", "1.10", "11", "1.11", "12", "13", "14", "15"
-    })
-    void verifyCanHandleJavaLanguageVersion(String javaVersion) {
-        final LanguageVersionHandler languageVersionHandler = PmdTemplate
-                .languageVersion(javaVersion)
-                .getLanguageVersionHandler();
+  @ParameterizedTest @ValueSource(strings = { "1.2", "5", "6", "7", "8", "9", "1.9", "10", "1.10", "11", "1.11", "12", "13", "14", "15" }) void verifyCanHandleJavaLanguageVersion(String javaVersion) {
+    final LanguageVersionHandler languageVersionHandler = PmdTemplate.languageVersion(javaVersion).getLanguageVersionHandler();
+    assertThat(languageVersionHandler).isNotNull();
+  }
 
-        assertThat(languageVersionHandler).isNotNull();
-    }
+  @Test void should_fail_on_invalid_java_version() {
+    final Throwable thrown = catchThrowable(() -> PmdTemplate.create("12.2", mock(ClassLoader.class), StandardCharsets.UTF_8));
+    assertThat(thrown).isInstanceOf(IllegalArgumentException.class);
+  }
 
-    @Test
-    void should_fail_on_invalid_java_version() {
-        final Throwable thrown = catchThrowable(() -> PmdTemplate.create("12.2", mock(ClassLoader.class), StandardCharsets.UTF_8));
-        assertThat(thrown).isInstanceOf(IllegalArgumentException.class);
-    }
+  @Test void shouldnt_fail_on_valid_java_version() {
+    PmdTemplate.create("6", mock(ClassLoader.class), StandardCharsets.UTF_8);
+  }
 
-    @Test
-    void shouldnt_fail_on_valid_java_version() {
-        PmdTemplate.create("6", mock(ClassLoader.class), StandardCharsets.UTF_8);
-    }
-
-    /**
+  /**
      * SONARPLUGINS-3318
      */
-    @Test
-    void should_set_classloader() {
-        ClassLoader classloader = mock(ClassLoader.class);
-        PmdTemplate pmdTemplate = PmdTemplate.create("6", classloader, StandardCharsets.UTF_8);
-        assertThat(pmdTemplate.configuration().getClassLoader()).isEqualTo(classloader);
-    }
+  @Test void should_set_classloader() {
+    ClassLoader classloader = mock(ClassLoader.class);
+    PmdTemplate pmdTemplate = PmdTemplate.create("6", classloader, StandardCharsets.UTF_8);
+    assertThat(pmdTemplate.configuration().getClassLoader()).isEqualTo(classloader);
+  }
 
-    @Test
-    void should_set_encoding() {
-        PmdTemplate pmdTemplate = PmdTemplate.create("6", mock(ClassLoader.class), StandardCharsets.UTF_16BE);
-        assertThat(pmdTemplate.configuration().getSourceEncoding()).isEqualTo(StandardCharsets.UTF_16BE);
-    }
-
+  @Test void should_set_encoding() {
+    PmdTemplate pmdTemplate = PmdTemplate.create("6", mock(ClassLoader.class), StandardCharsets.UTF_16BE);
+    assertThat(pmdTemplate.configuration().getSourceEncoding()).isEqualTo(StandardCharsets.UTF_16BE);
+  }
 }
