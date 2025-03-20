5d70ed0711bfc5e08412258fc32612ad8be76352
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 03:37:16 +0000

    Mining Framework Analysis

--- amazon-kinesis-client/src/main/java/software/amazon/kinesis/retrieval/RetrievalConfig.java
+++ amazon-kinesis-client/src/main/java/software/amazon/kinesis/retrieval/RetrievalConfig.java
@@ -1,20 +1,4 @@
-/*
- * Copyright 2019 Amazon.com, Inc. or its affiliates.
- * Licensed under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package software.amazon.kinesis.retrieval;
-
 import lombok.Data;
 import lombok.NonNull;
 import lombok.experimental.Accessors;
@@ -26,50 +10,51 @@ import software.amazon.kinesis.retrieval.fanout.FanOutConfig;
 /**
  * Used by the KCL to configure the retrieval of records from Kinesis.
  */
-@Data
-@Accessors(fluent = true)
-public class RetrievalConfig {
-    /**
+@Data @Accessors(fluent = true) public class RetrievalConfig {
+  /**
      * User agent set when Amazon Kinesis Client Library makes AWS requests.
      */
-    public static final String KINESIS_CLIENT_LIB_USER_AGENT = "amazon-kinesis-client-library-java";
+  public static final String KINESIS_CLIENT_LIB_USER_AGENT = "amazon-kinesis-client-library-java";
 
-    public static final String KINESIS_CLIENT_LIB_USER_AGENT_VERSION = "2.2.0";
+  public static final String KINESIS_CLIENT_LIB_USER_AGENT_VERSION = 
+<<<<<<< /usr/src/app/output/awslabs/amazon-kinesis-client/84d135d65e268226831de22f40f1c041b11ac0f7/amazon-kinesis-client/src/main/java/software/amazon/kinesis/retrieval/RetrievalConfig.java/left.java
+  "2.1.4"
+=======
+  "2.2.0"
+>>>>>>> /usr/src/app/output/awslabs/amazon-kinesis-client/84d135d65e268226831de22f40f1c041b11ac0f7/amazon-kinesis-client/src/main/java/software/amazon/kinesis/retrieval/RetrievalConfig.java/right.java
+  ;
 
-    /**
+  /**
      * Client used to make calls to Kinesis for records retrieval
      */
-    @NonNull
-    private final KinesisAsyncClient kinesisClient;
+  @NonNull private final KinesisAsyncClient kinesisClient;
 
-    /**
+  /**
      * The name of the stream to process records from.
      */
-    @NonNull
-    private final String streamName;
+  @NonNull private final String streamName;
 
-    @NonNull
-    private final String applicationName;
+  @NonNull private final String applicationName;
 
-    /**
+  /**
      * Backoff time between consecutive ListShards calls.
      *
      * <p>
      * Default value: 1500L
      * </p>
      */
-    private long listShardsBackoffTimeInMillis = 1500L;
+  private long listShardsBackoffTimeInMillis = 1500L;
 
-    /**
+  /**
      * Max number of retries for ListShards when throttled/exception is thrown.
      *
      * <p>
      * Default value: 50
      * </p>
      */
-    private int maxListShardsRetryAttempts = 50;
+  private int maxListShardsRetryAttempts = 50;
 
-    /**
+  /**
      * The location in the shard from which the KinesisClientLibrary will start fetching records from
      * when the application starts for the first time and there is no checkpoint for the shard.
      *
@@ -77,22 +62,19 @@ public class RetrievalConfig {
      * Default value: {@link InitialPositionInStream#LATEST}
      * </p>
      */
-    private InitialPositionInStreamExtended initialPositionInStreamExtended = InitialPositionInStreamExtended
-            .newInitialPosition(InitialPositionInStream.LATEST);
+  private InitialPositionInStreamExtended initialPositionInStreamExtended = InitialPositionInStreamExtended.newInitialPosition(InitialPositionInStream.LATEST);
 
-    private RetrievalSpecificConfig retrievalSpecificConfig;
+  private RetrievalSpecificConfig retrievalSpecificConfig;
 
-    private RetrievalFactory retrievalFactory;
+  private RetrievalFactory retrievalFactory;
 
-    public RetrievalFactory retrievalFactory() {
-
-        if (retrievalFactory == null) {
-            if (retrievalSpecificConfig == null) {
-                retrievalSpecificConfig = new FanOutConfig(kinesisClient()).streamName(streamName())
-                        .applicationName(applicationName());
-            }
-            retrievalFactory = retrievalSpecificConfig.retrievalFactory();
-        }
-        return retrievalFactory;
+  public RetrievalFactory retrievalFactory() {
+    if (retrievalFactory == null) {
+      if (retrievalSpecificConfig == null) {
+        retrievalSpecificConfig = new FanOutConfig(kinesisClient()).streamName(streamName()).applicationName(applicationName());
+      }
+      retrievalFactory = retrievalSpecificConfig.retrievalFactory();
     }
+    return retrievalFactory;
+  }
 }
