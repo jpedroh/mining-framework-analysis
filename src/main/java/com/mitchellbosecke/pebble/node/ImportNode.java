27b5eba8ebb97cb4dc874bdd38f676d6d9fdc579
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 06:50:30 +0000

    Mining Framework Analysis

--- src/main/java/com/mitchellbosecke/pebble/node/ImportNode.java
+++ src/main/java/com/mitchellbosecke/pebble/node/ImportNode.java
@@ -1,54 +1,6 @@
-/*******************************************************************************
- * This file is part of Pebble.
- *
- * Copyright (c) 2014 by Mitchell Bösecke
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- ******************************************************************************/
-package com.mitchellbosecke.pebble.node;
-
-import com.mitchellbosecke.pebble.extension.NodeVisitor;
-import com.mitchellbosecke.pebble.node.expression.Expression;
-import com.mitchellbosecke.pebble.template.EvaluationContextImpl;
-import com.mitchellbosecke.pebble.template.MacroAttributeProvider;
-import com.mitchellbosecke.pebble.template.PebbleTemplateImpl;
-
-import java.io.Writer;
-
-public class ImportNode extends AbstractRenderableNode {
-
-    private final Expression<?> importExpression;
-    private final String alias;
-
-    public ImportNode(int lineNumber, Expression<?> importExpression, String alias) {
-        super(lineNumber);
-        this.importExpression = importExpression;
-        this.alias = alias;
-    }
-
-    @Override
-    public void render(PebbleTemplateImpl self, Writer writer, EvaluationContextImpl context) {
-        String templateName = (String) importExpression.evaluate(self, context);
-        if (alias != null) {
-            self.importNamedTemplate(context, templateName, alias);
-
-            // put the imported template into scope
-            PebbleTemplateImpl template = self.getNamedImportedTemplate(context, alias);
-            context.getScopeChain().put(alias, new MacroAttributeProvider(template));
-
-        } else {
-            self.importTemplate(context, templateName);
-        }
-    }
-
-    @Override
-    public void accept(NodeVisitor visitor) {
-        visitor.visit(this);
-    }
-
-    public Expression<?> getImportExpression() {
-        return importExpression;
-    }
-
-}
+  package    com . mitchellbosecke . pebble . node ;   import     com . mitchellbosecke . pebble . extension . NodeVisitor ;  import      com . mitchellbosecke . pebble . node . expression . Expression ;  import     com . mitchellbosecke . pebble . template . EvaluationContextImpl ;  import     com . mitchellbosecke . pebble . template . PebbleTemplateImpl ;  import   java . io . Writer ;  import     com . mitchellbosecke . pebble . template . MacroAttributeProvider ;   public class ImportNode  extends AbstractRenderableNode  {   private final  Expression  <  ? >  importExpression ;    @ Override public void render  (  PebbleTemplateImpl self ,  Writer writer ,  EvaluationContextImpl context )  {   self . importTemplate  ( context ,  ( String )  importExpression . evaluate  ( self , context ) ) ; }    @ Override public void accept  (  NodeVisitor visitor )  {   visitor . visit  ( this ) ; }   public  Expression  <  ? > getImportExpression  ( )  {  return importExpression ; }   private final String  alias ;   public ImportNode  (   int lineNumber ,   Expression  <  ? > importExpression ,  String alias )  {  super  ( lineNumber ) ;    this . importExpression = importExpression ;    this . alias = alias ; } 
+<<<<<<<
+=======
+   @ Override public void render  (  PebbleTemplateImpl self ,  Writer writer ,  EvaluationContext context )  {  String  templateName =  ( String )  importExpression . evaluate  ( self , context ) ;  if  (  alias != null )  {   self . importNamedTemplate  ( context , templateName , alias ) ;  PebbleTemplateImpl  template =  self . getNamedImportedTemplate  ( context , alias ) ;    context . getScopeChain  ( ) . put  ( alias ,  new MacroAttributeProvider  ( template ) ) ; } else  {   self . importTemplate  ( context , templateName ) ; } }
+>>>>>>>
+ }
--- src/main/java/com/mitchellbosecke/pebble/template/EvaluationContext.java
+++ src/main/java/com/mitchellbosecke/pebble/template/EvaluationContext.java
@@ -1,15 +1,6 @@
-package com.mitchellbosecke.pebble.template;
-
-import java.util.Locale;
-
-/**
- * Created by mitchell on 2016-11-13.
- */
-public interface EvaluationContext {
-
-    boolean isStrictVariables();
-
-    Locale getLocale();
-
-    Object getVariable(String key);
-}
+  package    com . mitchellbosecke . pebble . template ;   import   java . util . Locale ;  import     com . mitchellbosecke . pebble . error . PebbleException ;  import   java . util . HashMap ;  import   java . util . Map ; 
+<<<<<<<
+  public interface EvaluationContext  {  boolean isStrictVariables  ( ) ;  Locale getLocale  ( ) ;  Object getVariable  (  String key ) ; }
+=======
+  public class EvaluationContext  {   private final boolean  strictVariables ;   private final Hierarchy  hierarchy ;   private final ScopeChain  scopeChain ;   private final Locale  locale ;   private final ExtensionRegistry  extensionRegistry ;   private final  Cache  < CacheKey , Object >  tagCache ;   private final ExecutorService  executorService ;   private final  List  < PebbleTemplateImpl >  importedTemplates ;   private final  Map  < String , PebbleTemplateImpl >  namedImportedTemplates ;   private final boolean  allowGetClass ;   public EvaluationContext  (  PebbleTemplateImpl self ,  boolean strictVariables ,  Locale locale ,  ExtensionRegistry extensionRegistry ,   Cache  < CacheKey , Object > tagCache ,  ExecutorService executorService ,   List  < PebbleTemplateImpl > importedTemplates ,   Map  < String , PebbleTemplateImpl > namedImportedTemplates ,  ScopeChain scopeChain ,  Hierarchy hierarchy ,  boolean allowGetClass )  {  if  (  hierarchy == null )  {   hierarchy =  new Hierarchy  ( self ) ; }    this . strictVariables = strictVariables ;    this . locale = locale ;    this . extensionRegistry = extensionRegistry ;    this . tagCache = tagCache ;    this . executorService = executorService ;    this . importedTemplates = importedTemplates ;    this . namedImportedTemplates = namedImportedTemplates ;    this . scopeChain = scopeChain ;    this . hierarchy = hierarchy ;    this . allowGetClass = allowGetClass ; }   public EvaluationContext shallowCopyWithoutInheritanceChain  (  PebbleTemplateImpl self )  {  EvaluationContext  result =  new EvaluationContext  ( self , strictVariables , locale , extensionRegistry , tagCache , executorService , importedTemplates , namedImportedTemplates , scopeChain , null , allowGetClass ) ;  return result ; }   public EvaluationContext threadSafeCopy  (  PebbleTemplateImpl self )  {  EvaluationContext  result =  new EvaluationContext  ( self , strictVariables , locale , extensionRegistry , tagCache , executorService ,  new  ArrayList  < >  ( importedTemplates ) ,  new  HashMap  < >  ( namedImportedTemplates ) ,  scopeChain . deepCopy  ( ) , hierarchy , allowGetClass ) ;  return result ; }   public boolean isStrictVariables  ( )  {  return strictVariables ; }   public Locale getLocale  ( )  {  return locale ; }   public ExtensionRegistry getExtensionRegistry  ( )  {  return extensionRegistry ; }   public ExecutorService getExecutorService  ( )  {  return executorService ; }   public  List  < PebbleTemplateImpl > getImportedTemplates  ( )  {  return  this . importedTemplates ; }   public PebbleTemplateImpl getNamedImportedTemplate  (  String alias )  {  return   this . namedImportedTemplates . get  ( alias ) ; }   public void addNamedImportedTemplates  (  String alias ,  PebbleTemplateImpl template )  {  if  (  namedImportedTemplates . containsKey  ( alias ) )  {  throw  new PebbleException  ( null ,  "More than one named template can not share the same name: " + alias ) ; }    this . namedImportedTemplates . put  ( alias , template ) ; }   public  Cache  < CacheKey , Object > getTagCache  ( )  {  return tagCache ; }   public ScopeChain getScopeChain  ( )  {  return scopeChain ; }   public Hierarchy getHierarchy  ( )  {  return hierarchy ; }   public boolean isAllowGetClass  ( )  {  return  this . allowGetClass ; } }
+>>>>>>>
