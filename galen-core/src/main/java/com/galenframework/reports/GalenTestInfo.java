7be4239530dc637794d2d7860c5cd65e586ba9c4
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 02:32:40 +0000

    Mining Framework Analysis

--- galen-core/src/main/java/com/galenframework/reports/GalenTestInfo.java
+++ galen-core/src/main/java/com/galenframework/reports/GalenTestInfo.java
@@ -1,142 +1,125 @@
-/*******************************************************************************
-* Copyright 2018 Ivan Shubin http://galenframework.com
-* 
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-* 
-*   http://www.apache.org/licenses/LICENSE-2.0
-* 
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-******************************************************************************/
 package com.galenframework.reports;
-
 import java.lang.reflect.Method;
 import java.util.Date;
 import java.util.List;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import com.galenframework.tests.GalenEmptyTest;
 import com.galenframework.tests.GalenTest;
 import org.apache.commons.lang3.StringEscapeUtils;
 
 public class GalenTestInfo {
+  private String name;
 
-    private String name;
+  @JsonIgnore private TestReport report = new TestReport();
 
-    @JsonIgnore
-    private TestReport report = new TestReport();
+  @JsonIgnore private Throwable exception;
 
-    @JsonIgnore
-    private Throwable exception;
-    private Date startedAt = new Date();
-    private Date endedAt = new Date();
+  private Date startedAt = new Date();
 
-    @JsonIgnore
-    private GalenTest test;
-    
-    
-    public GalenTestInfo(String name, GalenTest test) {
-        setName(name);
-        setTest(test);
-    }    
-    
-    public boolean isFailed() {
-        return exception != null || report.fetchStatistic().getErrors() > 0;
-    }
+  private Date endedAt = new Date();
 
-    public String getName() {
-        return name;
-    }
+  @JsonIgnore private GalenTest test;
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  public GalenTestInfo(String name, GalenTest test) {
+    setName(name);
+    setTest(test);
+  }
 
-    public TestReport getReport() {
-        return report;
-    }
+  public boolean isFailed() {
+    return exception != null || report.fetchStatistic().getErrors() > 0;
+  }
 
-    public void setReport(TestReport report) {
-        this.report = report;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public void setException(Throwable ex) {
-        this.exception = ex;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    public Throwable getException() {
-        return this.exception;
-    }
+  public TestReport getReport() {
+    return report;
+  }
 
-    public Date getStartedAt() {
-        return startedAt;
-    }
+  public void setReport(TestReport report) {
+    this.report = report;
+  }
 
-    public void setStartedAt(Date startedAt) {
-        this.startedAt = startedAt;
-    }
+  public void setException(Throwable ex) {
+    this.exception = ex;
+  }
 
-    public Date getEndedAt() {
-        return endedAt;
-    }
+  public Throwable getException() {
+    return this.exception;
+  }
 
-    public void setEndedAt(Date endedAt) {
-        this.endedAt = endedAt;
-    }
+  public Date getStartedAt() {
+    return startedAt;
+  }
 
-    public GalenTest getTest() {
-        return test;
-    }
+  public void setStartedAt(Date startedAt) {
+    this.startedAt = startedAt;
+  }
 
-    public void setTest(GalenTest test) {
-        this.test = test;
-    }
+  public Date getEndedAt() {
+    return endedAt;
+  }
 
-    public static GalenTestInfo fromString(final String name) {
-        return new GalenTestInfo(name, new GalenEmptyTest(name, null));
-    }
+  public void setEndedAt(Date endedAt) {
+    this.endedAt = endedAt;
+  }
 
-    public static GalenTestInfo fromString(final String name, final List<String> groups) {
-        return new GalenTestInfo(name, new GalenEmptyTest(name, groups));
-    }
+  public GalenTest getTest() {
+    return test;
+  }
 
-    public static GalenTestInfo fromMethod(Method method) {
-        String name = method.getDeclaringClass().getSimpleName() + "#" + method.getName();
-        return GalenTestInfo.fromString(name);
-    }
+  public void setTest(GalenTest test) {
+    this.test = test;
+  }
 
-    public static GalenTestInfo fromMethod(Method method, Object[] arguments) {
-        StringBuilder builder = new StringBuilder(method.getDeclaringClass().getSimpleName() + "#" + method.getName());
-        if (arguments != null && arguments.length > 0) {
-            builder.append(" (");
-            boolean shouldUseComma = false;
-            for (Object argument : arguments) {
-                if (shouldUseComma) {
-                    builder.append(", ");
-                }
-                builder.append(convertArgumentToString(argument));
-                shouldUseComma = true;
-            }
-            builder.append(")");
+  public static GalenTestInfo fromString(final String name) {
+    return new GalenTestInfo(name, new GalenEmptyTest(name, null));
+  }
+
+  public static GalenTestInfo fromString(final String name, final List<String> groups) {
+    return new GalenTestInfo(name, new GalenEmptyTest(name, groups));
+  }
+
+  public static GalenTestInfo fromMethod(Method method) {
+    String name = method.getDeclaringClass().getSimpleName() + "#" + method.getName();
+    return GalenTestInfo.fromString(name);
+  }
+
+  public static GalenTestInfo fromMethod(Method method, Object[] arguments) {
+    StringBuilder builder = new StringBuilder(method.getDeclaringClass().getSimpleName() + "#" + method.getName());
+    if (arguments != null && arguments.length > 0) {
+      builder.append(" (");
+      boolean shouldUseComma = false;
+      for (Object argument : arguments) {
+        if (shouldUseComma) {
+          builder.append(", ");
         }
-        return GalenTestInfo.fromString(builder.toString());
+        builder.append(convertArgumentToString(argument));
+        shouldUseComma = true;
+      }
+      builder.append(")");
     }
+    return GalenTestInfo.fromString(builder.toString());
+  }
 
-    private static String convertArgumentToString(Object argument) {
-        if (argument == null) {
-            return "null";
-        } else if (argument instanceof String) {
-            return "\"" + StringEscapeUtils.escapeJava(argument.toString()) + "\"";
-        } else if (argument instanceof Boolean) {
-            return Boolean.toString((Boolean)argument);
+  private static String convertArgumentToString(Object argument) {
+    if (argument == null) {
+      return "null";
+    } else {
+      if (argument instanceof String) {
+        return "\"" + StringEscapeUtils.escapeJava(argument.toString()) + "\"";
+      } else {
+        if (argument instanceof Boolean) {
+          return Boolean.toString((Boolean) argument);
         } else {
-            return argument.toString();
+          return argument.toString();
         }
+      }
     }
+  }
 }
--- galen-core/src/test/java/com/galenframework/components/mocks/driver/MockedPageItem.java
+++ galen-core/src/test/java/com/galenframework/components/mocks/driver/MockedPageItem.java
@@ -1,132 +1,122 @@
-/*******************************************************************************
-* Copyright 2018 Ivan Shubin http://galenframework.com
-* 
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-* 
-*   http://www.apache.org/licenses/LICENSE-2.0
-* 
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-******************************************************************************/
 package com.galenframework.components.mocks.driver;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-
 import java.lang.reflect.Field;
 import java.util.LinkedList;
 import java.util.List;
 
 public class MockedPageItem {
-    private String locator;
-    private String text;
-    private Integer[] area;
-    private List<MockedPageItem> subItems;
+  private String locator;
 
-    private String locatorType;
-    private String locatorValue;
-    private boolean visible = true;
-    private List<String> mockedEvents = new LinkedList<>();
+  private String text;
 
-    private String getLocatorType() {
-        return locatorType;
+  private Integer[] area;
+
+  private List<MockedPageItem> subItems;
+
+  private String locatorType;
+
+  private String locatorValue;
+
+  private boolean visible = true;
+
+  private List<String> mockedEvents = new LinkedList<>();
+
+  private String getLocatorType() {
+    return locatorType;
+  }
+
+  private String getLocatorValue() {
+    return locatorValue;
+  }
+
+  public String getLocator() {
+    return locator;
+  }
+
+  public void setLocator(String locator) {
+    this.locator = locator;
+    int id = locator.indexOf(":");
+    if (id > 0) {
+      this.locatorType = locator.substring(0, id).trim();
+      this.locatorValue = locator.substring(id + 1).trim();
+    } else {
+      throw new RuntimeException("Incorrect locator: " + locator);
     }
+  }
 
-    private String getLocatorValue() {
-        return locatorValue;
-    }
+  public String getText() {
+    return text;
+  }
 
-    public String getLocator() {
-        return locator;
-    }
+  public void setText(String text) {
+    this.text = text;
+  }
 
-    public void setLocator(String locator) {
-        this.locator = locator;
+  public Integer[] getArea() {
+    return area;
+  }
 
-        int id = locator.indexOf(":");
-        if (id > 0) {
-            this.locatorType = locator.substring(0, id).trim();
-            this.locatorValue = locator.substring(id + 1).trim();
+  public void setArea(Integer[] area) {
+    this.area = area;
+  }
+
+  public boolean matches(By by) {
+    if (by instanceof By.ByCssSelector) {
+      String selector = (String) takeFieldValueViaReflection(by, "cssSelector");
+      if ("css".equals(getLocatorType()) && selector.equals(getLocatorValue())) {
+        return true;
+      }
+    } else {
+      if (by instanceof By.ById) {
+        String selector = (String) takeFieldValueViaReflection(by, "id");
+        if ("id".equals(getLocatorType()) && selector.equals(getLocatorValue())) {
+          return true;
         }
-        else throw new RuntimeException("Incorrect locator: " + locator);
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    public Integer[] getArea() {
-        return area;
-    }
-
-    public void setArea(Integer[] area) {
-        this.area = area;
-    }
-
-    public boolean matches(By by) {
-        if (by instanceof By.ByCssSelector) {
-            String selector = (String)takeFieldValueViaReflection(by, "cssSelector");
-            if("css".equals(getLocatorType()) && selector.equals(getLocatorValue()) ) {
-                return true;
-            }
+      } else {
+        if (by instanceof By.ByXPath) {
+          String selector = (String) takeFieldValueViaReflection(by, "xpathExpression");
+          if ("xpath".equals(getLocatorType()) && selector.equals(getLocatorValue())) {
+            return true;
+          }
         }
-        else if (by instanceof By.ById) {
-            String selector = (String)takeFieldValueViaReflection(by, "id");
-            if("id".equals(getLocatorType()) && selector.equals(getLocatorValue()) ) {
-                return true;
-            }
-        }
-        else if (by instanceof By.ByXPath) {
-            String selector = (String)takeFieldValueViaReflection(by, "xpathExpression");
-            if("xpath".equals(getLocatorType()) && selector.equals(getLocatorValue()) ) {
-                return true;
-            }
-        }
-
-        return false;
+      }
     }
+    return false;
+  }
 
-    private Object takeFieldValueViaReflection(Object object, String fieldName) {
-        try {
-            Class<?> clazz = object.getClass();
-            Field field = clazz.getDeclaredField(fieldName);
-            field.setAccessible(true);
-            return field.get(object);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+  private Object takeFieldValueViaReflection(Object object, String fieldName) {
+    try {
+      Class<?> clazz = object.getClass();
+      Field field = clazz.getDeclaredField(fieldName);
+      field.setAccessible(true);
+      return field.get(object);
+    } catch (Exception e) {
+      throw new RuntimeException(e);
     }
+  }
 
-    public WebElement asWebElement() {
-        return new MockedDriverElement(this);
-    }
+  public WebElement asWebElement() {
+    return new MockedDriverElement(this);
+  }
 
-    public boolean isVisible() {
-        return visible;
-    }
+  public boolean isVisible() {
+    return visible;
+  }
 
-    public void setVisible(boolean visible) {
-        this.visible = visible;
-    }
+  public void setVisible(boolean visible) {
+    this.visible = visible;
+  }
 
-    public List<String> getMockedEvents() {
-        return this.mockedEvents;
-    }
+  public List<String> getMockedEvents() {
+    return this.mockedEvents;
+  }
 
-    public List<MockedPageItem> getSubItems() {
-        return subItems;
-    }
+  public List<MockedPageItem> getSubItems() {
+    return subItems;
+  }
 
-    public void setSubItems(List<MockedPageItem> subItems) {
-        this.subItems = subItems;
-    }
+  public void setSubItems(List<MockedPageItem> subItems) {
+    this.subItems = subItems;
+  }
 }
