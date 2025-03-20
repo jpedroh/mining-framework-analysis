a96572944f23ce124f8effa72d4ae4993b8266c1
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 11:57:30 +0000

    Mining Framework Analysis

--- src/main/java/org/crazycake/shiro/RedisCacheManager.java
+++ src/main/java/org/crazycake/shiro/RedisCacheManager.java
@@ -1,72 +1,67 @@
 package org.crazycake.shiro;
-
 import org.apache.shiro.cache.Cache;
 import org.apache.shiro.cache.CacheException;
 import org.apache.shiro.cache.CacheManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
 public class RedisCacheManager implements CacheManager {
+  private static final Logger logger = LoggerFactory.getLogger(RedisCacheManager.class);
 
-	private static final Logger logger = LoggerFactory.getLogger(RedisCacheManager.class);
+  private final ConcurrentMap<String, Cache> caches = new ConcurrentHashMap<String, Cache>();
 
-	// fast lookup by name map
-	private final ConcurrentMap<String, Cache> caches = new ConcurrentHashMap<String, Cache>();
-	private RedisSerializer keySerializer = new StringSerializer();
-	private RedisSerializer valueSerializer = new ObjectSerializer();
+  private RedisSerializer keySerializer = new StringSerializer();
 
-	private IRedisManager redisManager;
+  private RedisSerializer valueSerializer = new ObjectSerializer();
 
-	/**
+  private IRedisManager redisManager;
+
+  /**
 	 * The Redis key prefix for caches 
 	 */
-	private String keyPrefix = "shiro:cache:";
+  private String keyPrefix = "shiro:cache:";
 
-	@Override
-	public <K, V> Cache<K, V> getCache(String name) throws CacheException {
-		logger.debug("get cache, name=" + name);
-		
-		Cache cache = caches.get(name);
-		
-		if (cache == null) {
-			cache = new RedisCache<K, V>(redisManager, keySerializer, valueSerializer, keyPrefix + name + ":");
-			caches.put(name, cache);
-		}
-		return cache;
-	}
+  @Override public <K extends java.lang.Object, V extends java.lang.Object> Cache<K, V> getCache(String name) throws CacheException {
+    logger.debug("get cache, name=" + name);
+    Cache cache = caches.get(name);
+    if (cache == null) {
+      cache = new RedisCache<K, V>(redisManager, keySerializer, valueSerializer, keyPrefix + name + ":");
+      caches.put(name, cache);
+    }
+    return cache;
+  }
 
-	public IRedisManager getRedisManager() {
-		return redisManager;
-	}
+  public IRedisManager getRedisManager() {
+    return redisManager;
+  }
 
-	public void setRedisManager(IRedisManager redisManager) {
-		this.redisManager = redisManager;
-	}
+  public void setRedisManager(IRedisManager redisManager) {
+    this.redisManager = redisManager;
+  }
 
-	public String getKeyPrefix() {
-		return keyPrefix;
-	}
+  public String getKeyPrefix() {
+    return keyPrefix;
+  }
 
-	public void setKeyPrefix(String keyPrefix) {
-		this.keyPrefix = keyPrefix;
-	}
+  public void setKeyPrefix(String keyPrefix) {
+    this.keyPrefix = keyPrefix;
+  }
 
-	public RedisSerializer getKeySerializer() {
-		return keySerializer;
-	}
+  public RedisSerializer getKeySerializer() {
+    return keySerializer;
+  }
 
-	public void setKeySerializer(RedisSerializer keySerializer) {
-		this.keySerializer = keySerializer;
-	}
+  public void setKeySerializer(RedisSerializer keySerializer) {
+    this.keySerializer = keySerializer;
+  }
 
-	public RedisSerializer getValueSerializer() {
-		return valueSerializer;
-	}
+  public RedisSerializer getValueSerializer() {
+    return valueSerializer;
+  }
 
-	public void setValueSerializer(RedisSerializer valueSerializer) {
-		this.valueSerializer = valueSerializer;
-	}
+  public void setValueSerializer(RedisSerializer valueSerializer) {
+    this.valueSerializer = valueSerializer;
+  }
 }
