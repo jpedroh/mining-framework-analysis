f0d15c99c48eb8179afa418ab1488c809616b864
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 04:08:37 +0000

    Mining Framework Analysis

--- src/main/java/com/jcabi/github/RtReleases.java
+++ src/main/java/com/jcabi/github/RtReleases.java
@@ -1,45 +1,14 @@
-/**
- * Copyright (c) 2012-2013, JCabi.com
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met: 1) Redistributions of source code must retain the above
- * copyright notice, this list of conditions and the following
- * disclaimer. 2) Redistributions in binary form must reproduce the above
- * copyright notice, this list of conditions and the following
- * disclaimer in the documentation and/or other materials provided
- * with the distribution. 3) Neither the name of the jcabi.com nor
- * the names of its contributors may be used to endorse or promote
- * products derived from this software without specific prior written
- * permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
- * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
- * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
- * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- * OF THE POSSIBILITY OF SUCH DAMAGE.
- */
 package com.jcabi.github;
-
 import com.jcabi.aspects.Immutable;
 import com.jcabi.aspects.Loggable;
 import com.rexsl.test.Request;
+import javax.json.JsonObject;
 import com.rexsl.test.response.JsonResponse;
 import com.rexsl.test.response.RestResponse;
 import java.io.IOException;
 import java.net.HttpURLConnection;
-import java.util.Collections;
 import javax.json.Json;
 import javax.json.JsonStructure;
-import javax.json.JsonObject;
 import lombok.EqualsAndHashCode;
 
 /**
@@ -49,82 +18,64 @@ import lombok.EqualsAndHashCode;
  * @version $Id$
  * @since 0.8
  */
-@Immutable
-@Loggable(Loggable.DEBUG)
-@EqualsAndHashCode(of = { "entry", "owner" })
-public final class RtReleases implements Releases {
-
-    /**
+@Immutable @Loggable(value = Loggable.DEBUG) @EqualsAndHashCode(of = { "entry", "owner" }) public final class RtReleases implements Releases {
+  /**
      * RESTful API entry point.
      */
-    private final transient Request entry;
+  private final transient Request entry;
 
-    /**
+  /**
      * RESTful API releases request.
      */
-    private final transient Request request;
+  private final transient Request request;
 
-    /**
+  /**
      * Repository.
      */
-    private final transient Repo owner;
+  private final transient Repo owner;
 
-    /**
+  /**
      * Public ctor.
      * @param req RESTful API entry point
      * @param repo Repository
      */
-    public RtReleases(final Request req, final Repo repo) {
-        this.entry = req;
-        this.owner = repo;
-        this.request = this.entry.uri()
-            .path("/repos")
-            .path(repo.coordinates().user())
-            .path(repo.coordinates().repo())
-            .path("/releases")
-            .back();
-    }
+  public RtReleases(final Request req, final Repo repo) {
+    this.entry = req;
+    this.owner = repo;
+    final Coordinates coords = repo.coordinates();
+    this.request = this.entry.uri().path("/repos").path(
+<<<<<<< /usr/src/app/output/jcabi/jcabi-github/b753098546c4fce159d88e863277216c3ec2e845/src/main/java/com/jcabi/github/RtReleases.java/left.java
+    repo.coordinates().user()
+=======
+    coords.user()
+>>>>>>> /usr/src/app/output/jcabi/jcabi-github/b753098546c4fce159d88e863277216c3ec2e845/src/main/java/com/jcabi/github/RtReleases.java/right.java
+    ).path(
+<<<<<<< /usr/src/app/output/jcabi/jcabi-github/b753098546c4fce159d88e863277216c3ec2e845/src/main/java/com/jcabi/github/RtReleases.java/left.java
+    repo.coordinates().repo()
+=======
+    coords.repo()
+>>>>>>> /usr/src/app/output/jcabi/jcabi-github/b753098546c4fce159d88e863277216c3ec2e845/src/main/java/com/jcabi/github/RtReleases.java/right.java
+    ).path("/releases").back();
+  }
 
-    @Override
-    public Repo repo() {
-        return this.owner;
-    }
+  @Override public Repo repo() {
+    return this.owner;
+  }
 
-    @Override
-    public Iterable<Release> iterate() {
-        return new RtPagination<Release>(
-            this.request,
-            new RtPagination.Mapping<Release>() {
-                @Override
-                public Release map(final JsonObject object) {
-                    return new RtRelease(
-                        RtReleases.this.entry,
-                        RtReleases.this.owner.coordinates(),
-                        object.getInt("id")
-                    );
-                }
-            }
-        );
-    }
+  @Override public Iterable<Release> iterate() {
+    return new RtPagination<Release>(this.request, new RtPagination.Mapping<Release>() {
+      @Override public Release map(final JsonObject object) {
+        return new RtRelease(RtReleases.this.entry, RtReleases.this.owner.coordinates(), object.getInt("id"));
+      }
+    });
+  }
 
-    @Override
-    public Release get(final int number) {
-        return new RtRelease(this.entry, this.owner.coordinates(), number);
-    }
+  @Override public Release get(final int number) {
+    return new RtRelease(this.entry, this.owner.coordinates(), number);
+  }
 
-    @Override
-    public Release create(final String tag) throws IOException {
-        final JsonStructure json = Json.createObjectBuilder()
-            .add("tag_name", tag)
-            .build();
-        return this.get(
-            this.request.method(Request.POST)
-                .body().set(json).back()
-                .fetch().as(RestResponse.class)
-                .assertStatus(HttpURLConnection.HTTP_CREATED)
-                .as(JsonResponse.class)
-                .json().readObject().getInt("id")
-        );
-    }
-
+  @Override public Release create(final String tag) throws IOException {
+    final JsonStructure json = Json.createObjectBuilder().add("tag_name", tag).build();
+    return this.get(this.request.method(Request.POST).body().set(json).back().fetch().as(RestResponse.class).assertStatus(HttpURLConnection.HTTP_CREATED).as(JsonResponse.class).json().readObject().getInt("id"));
+  }
 }
