6c038b3806c6117a66bacc1263a5bf01dcee1000
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 05:29:37 +0000

    Mining Framework Analysis

--- src/burlap/oomdp/core/values/DoubleArrayValue.java
+++ src/burlap/oomdp/core/values/DoubleArrayValue.java
@@ -1,9 +1,6 @@
 package burlap.oomdp.core.values;
-
 import java.util.Arrays;
-
 import burlap.oomdp.core.Attribute;
-
 
 /**
  * This class implements an attribute value that is defined with a double array. In general, it is reccomended that a series of {@link RealValue} attributes
@@ -13,144 +10,124 @@ import burlap.oomdp.core.Attribute;
  * @author James MacGlashan
  *
  */
-public class DoubleArrayValue extends OOMDPValue{
+public class DoubleArrayValue extends OOMDPValue {
+  protected final double[] doubleArray;
 
-	protected final double [] doubleArray;
-	
-	public DoubleArrayValue(Attribute attribute) {
-		super(attribute);
-		this.doubleArray = null;
-	}
-	
-	public DoubleArrayValue(DoubleArrayValue v){
-		super(v);
-		DoubleArrayValue daValue = v;
-		if(daValue.doubleArray != null) {
-			this.doubleArray = daValue.doubleArray.clone();
-		} else {
-			this.doubleArray = null;
-		}
-	}
-	
-	public DoubleArrayValue(Attribute attribute, double[] doubleArray) {
-		super(attribute);
-		this.doubleArray = doubleArray;
-	}
+  public DoubleArrayValue(Attribute attribute) {
+    super(attribute);
+    this.doubleArray = null;
+  }
 
-	@Override
-	public Value copy() {
-		return new DoubleArrayValue(this);
-	}
-
-	@Override
-	public boolean valueHasBeenSet() {
-		return this.doubleArray != null;
-	}
-
-	@Override
-	public Value setValue(String v) {
-		if(v.startsWith("\"") && v.endsWith("\"")){
-			v = v.substring(1, v.length());
-		}
-		String [] comps = v.split(",");
-		double[] doubleArray = new double[comps.length];
-		for(int i = 0; i < comps.length; i++){
-			doubleArray[i] = Double.parseDouble(comps[i]);
-		}
-		return new DoubleArrayValue(this.attribute, doubleArray);
-	}
-
-	@Override
-	public StringBuilder buildStringVal(StringBuilder builder) {
-		for(int i = 0; i < this.doubleArray.length; i++){
-			if(i > 0){
-				builder.append(",");
-			}
-			builder.append(this.doubleArray[i]);
-		}
-		return builder;
-	}
-
-	@Override
-	public double getNumericRepresentation() {
-		double sum = 0;
-		for(double v : this.doubleArray){
-			sum *= 31;
-			sum += v;
-		}
-		return sum;
-	}
-
-	@Override
-	public Value setValue(int[] intArray) {
-		
-		double[] doubleArray = new double[intArray.length];
-		for(int i = 0; i < intArray.length; i++){
-			doubleArray[i] = intArray[i];
-		}
-		return new DoubleArrayValue(this.attribute, doubleArray);
-	}
-
-	@Override
-	public Value setValue(double[] doubleArray) {
-		return new DoubleArrayValue(this.attribute, doubleArray);
-	}
-
-	@Override
-	public int[] getIntArray() {
-		if(this.doubleArray == null){
-			throw new RuntimeException("Error, double array value is unset, cannot return a value for it.");
-		}
-		int [] intArray = new int[this.doubleArray.length];
-		for(int i = 0; i < this.doubleArray.length; i++){
-			intArray[i] = (int)this.doubleArray[i];
-		}
-		return intArray;
-	}
-
-	@Override
-	public double[] getDoubleArray() {
-		if(this.doubleArray == null){
-			throw new RuntimeException("Error, double array value is unset, cannot return a value for it.");
-		}
-		return this.doubleArray;
-	}
-	
-	
-	@Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + Arrays.hashCode(doubleArray);
-        return result;
+  public DoubleArrayValue(DoubleArrayValue v) {
+    super(v);
+    DoubleArrayValue daValue = v;
+    if (daValue.doubleArray != null) {
+      this.doubleArray = daValue.doubleArray.clone();
+    } else {
+      this.doubleArray = null;
     }
+  }
 
-	@Override
-    public boolean equals(Object obj){
-        if (this == obj) {
-            return true;
-        }
-        if(!(obj instanceof DoubleArrayValue)){
-            return false;
-        }
-        
-        DoubleArrayValue o = (DoubleArrayValue)obj;
-        
-        if(!o.attribute.equals(this.attribute)){
-            return false;
-        }
-        
-        if(this.doubleArray.length != o.doubleArray.length){
-            return false;
-        }
-        
-        for(int i = 0; i < this.doubleArray.length; i++){
-            if(this.doubleArray[i] != o.doubleArray[i]){
-                return false;
-            }
-        }
-        
-        return true;
-        
+  public DoubleArrayValue(Attribute attribute, double[] doubleArray) {
+    super(attribute);
+    this.doubleArray = doubleArray;
+  }
+
+  @Override public Value copy() {
+    return new DoubleArrayValue(this);
+  }
+
+  @Override public boolean valueHasBeenSet() {
+    return this.doubleArray != null;
+  }
+
+  @Override public Value setValue(String v) {
+    if (v.startsWith("\"") && v.endsWith("\"")) {
+      v = v.substring(1, v.length());
     }
+    String[] comps = v.split(",");
+    double[] doubleArray = new double[comps.length];
+    for (int i = 0; i < comps.length; i++) {
+      doubleArray[i] = Double.parseDouble(comps[i]);
+    }
+    return new DoubleArrayValue(this.attribute, doubleArray);
+  }
+
+  @Override public StringBuilder buildStringVal(StringBuilder builder) {
+    for (int i = 0; i < this.doubleArray.length; i++) {
+      if (i > 0) {
+        builder.append(",");
+      }
+      builder.append(this.doubleArray[i]);
+    }
+    return builder;
+  }
+
+  @Override public double getNumericRepresentation() {
+    double sum = 0;
+    for (double v : this.doubleArray) {
+      sum *= 31;
+      sum += v;
+    }
+    return sum;
+  }
+
+  @Override public Value setValue(int[] intArray) {
+    double[] doubleArray = new double[intArray.length];
+    for (int i = 0; i < intArray.length; i++) {
+      doubleArray[i] = intArray[i];
+    }
+    return new DoubleArrayValue(this.attribute, doubleArray);
+  }
+
+  @Override public Value setValue(double[] doubleArray) {
+    return new DoubleArrayValue(this.attribute, doubleArray);
+  }
+
+  @Override public int[] getIntArray() {
+    if (this.doubleArray == null) {
+      throw new RuntimeException("Error, double array value is unset, cannot return a value for it.");
+    }
+    int[] intArray = new int[this.doubleArray.length];
+    for (int i = 0; i < this.doubleArray.length; i++) {
+      intArray[i] = (int) this.doubleArray[i];
+    }
+    return intArray;
+  }
+
+  @Override public double[] getDoubleArray() {
+    if (this.doubleArray == null) {
+      throw new RuntimeException("Error, double array value is unset, cannot return a value for it.");
+    }
+    return this.doubleArray;
+  }
+
+  @Override public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + Arrays.hashCode(doubleArray);
+    return result;
+  }
+
+  @Override public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (!(obj instanceof DoubleArrayValue)) {
+      return false;
+    }
+    DoubleArrayValue o = (DoubleArrayValue) obj;
+    if (!o.attribute.equals(this.attribute)) {
+      return false;
+    }
+    if (this.doubleArray.length != o.doubleArray.length) {
+      return false;
+    }
+    for (int i = 0; i < this.doubleArray.length; i++) {
+      if (this.doubleArray[i] != o.doubleArray[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
 }
--- src/burlap/oomdp/core/values/RelationalValue.java
+++ src/burlap/oomdp/core/values/RelationalValue.java
@@ -1,11 +1,8 @@
 package burlap.oomdp.core.values;
-
 import java.util.Collection;
 import java.util.Set;
 import java.util.TreeSet;
-
 import burlap.oomdp.core.Attribute;
-
 
 /**
  * A relational valued value subclass in which values are stored as a single String object for the name of the object instance to which it is linked.
@@ -13,114 +10,98 @@ import burlap.oomdp.core.Attribute;
  * @author James MacGlashan
  *
  */
-public class RelationalValue  extends OOMDPValue implements Value {
-	private static final String UNSET = "";
-	/**
+public class RelationalValue extends OOMDPValue implements Value {
+  private static final String UNSET = "";
+
+  /**
 	 * A string representing the object target of this value. Targets are specified by the object name identifier.
 	 * If the relational target is unset, then this value will be set to the empty string "", which is the default value.
 	 */
-	protected final String		target;
-	
-	
-	/**
+  protected final String target;
+
+  /**
 	 * Initializes this value to be an assignment for Attribute attribute.
 	 * @param attribute
 	 */
-	public RelationalValue(Attribute attribute){
-		super(attribute);
-		this.target = UNSET;
-	}
-	
-	
-	/**
+  public RelationalValue(Attribute attribute) {
+    super(attribute);
+    this.target = UNSET;
+  }
+
+  /**
 	 * Initializes this value as a copy from the source Value object v.
 	 * @param v the source Value to make this object a copy of.
 	 */
-	public RelationalValue(RelationalValue v){
-		super(v);
-		RelationalValue rv = v;
-		this.target = rv.target;
-	}
-	
-	public RelationalValue(Attribute attribute, String target) {
-		super(attribute);
-		this.target = target;
-	}
-	
-	@Override
-	public Value copy() {
-		return new RelationalValue(this);
-	}
+  public RelationalValue(RelationalValue v) {
+    super(v);
+    RelationalValue rv = v;
+    this.target = rv.target;
+  }
 
-	@Override
-	public boolean valueHasBeenSet() {
-		return true;
-	}
-	@Override
-	public Value setValue(String v) {
-		return new RelationalValue(this.attribute, v);
-	}
-	
-	@Override
-	public Value addRelationalTarget(String t) {
-		return new RelationalValue(this.attribute, t);
-	}
-	@Override
-	public Value clearRelationTargets() {
-		return new RelationalValue(this.attribute);
-	}
-	
-	@Override
-	public Value removeRelationalTarget(String target) {
-		if(this.target.equals(target)){
-			return new RelationalValue(this.attribute);
-		}
-		return this;
-	}
-	
-	@Override
-	public Set<String> getAllRelationalTargets() {
-		Set <String> res = new TreeSet<String>();
-		res.add(this.target);
-		return res;
-	}
+  public RelationalValue(Attribute attribute, String target) {
+    super(attribute);
+    this.target = target;
+  }
 
-	@Override
-	public StringBuilder buildStringVal(StringBuilder builder) {
-		return builder.append(this.target);
-	}
+  @Override public Value copy() {
+    return new RelationalValue(this);
+  }
 
-	@Override
-	public double getNumericRepresentation() {
-		return 0;
-	}
-	
-	
-	@Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((target == null) ? 0 : target.hashCode());
-        return result;
+  @Override public boolean valueHasBeenSet() {
+    return true;
+  }
+
+  @Override public Value setValue(String v) {
+    return new RelationalValue(this.attribute, v);
+  }
+
+  @Override public Value addRelationalTarget(String t) {
+    return new RelationalValue(this.attribute, t);
+  }
+
+  @Override public Value clearRelationTargets() {
+    return new RelationalValue(this.attribute);
+  }
+
+  @Override public Value removeRelationalTarget(String target) {
+    if (this.target.equals(target)) {
+      return new RelationalValue(this.attribute);
     }
+    return this;
+  }
 
+  @Override public Set<String> getAllRelationalTargets() {
+    Set<String> res = new TreeSet<String>();
+    res.add(this.target);
+    return res;
+  }
 
-	@Override
-    public boolean equals(Object obj){
-        if (this == obj) {
-            return true;
-        }
-        if(!(obj instanceof RelationalValue)){
-            return false;
-        }
-        
-        RelationalValue op = (RelationalValue)obj;
-        if(!op.attribute.equals(attribute)){
-            return false;
-        }
-        
-        return this.target.equals(op.target);
-        
+  @Override public StringBuilder buildStringVal(StringBuilder builder) {
+    return builder.append(this.target);
+  }
+
+  @Override public double getNumericRepresentation() {
+    return 0;
+  }
+
+  @Override public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((target == null) ? 0 : target.hashCode());
+    return result;
+  }
+
+  @Override public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
     }
-
+    if (!(obj instanceof RelationalValue)) {
+      return false;
+    }
+    RelationalValue op = (RelationalValue) obj;
+    if (!op.attribute.equals(attribute)) {
+      return false;
+    }
+    return this.target.equals(op.target);
+  }
 }
