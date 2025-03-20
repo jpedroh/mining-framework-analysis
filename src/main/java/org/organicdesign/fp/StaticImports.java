160f09f36521c5d4c04684354be6705dd206d8c6
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 00:25:33 +0000

    Mining Framework Analysis

--- src/main/java/org/organicdesign/fp/StaticImports.java
+++ src/main/java/org/organicdesign/fp/StaticImports.java
@@ -1,21 +1,9 @@
-// Copyright 2014-09-22 PlanBase Inc. & Glen Peterson
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package org.organicdesign.fp;
-
+import java.util.Arrays;
 import org.organicdesign.fp.collections.ImList;
+import java.util.Comparator;
 import org.organicdesign.fp.collections.ImMap;
+import java.util.Map;
 import org.organicdesign.fp.collections.ImSet;
 import org.organicdesign.fp.collections.ImSortedMap;
 import org.organicdesign.fp.collections.ImSortedSet;
@@ -31,10 +19,6 @@ import org.organicdesign.fp.collections.UnmodIterable;
 import org.organicdesign.fp.tuple.Tuple2;
 import org.organicdesign.fp.tuple.Tuple3;
 import org.organicdesign.fp.xform.Xform;
-
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.Map;
 
 /**
  <p>A mini data definition language composed of vec(), tup(), map(), set(), plus xform() which makes
@@ -54,9 +38,9 @@ import java.util.Map;
  // Create a map with a few key value pairs
  map(tup("a", 1), tup("b", 2), tup("c", 3);</code></pre>
 
- <p>There are only a few methods in this project to take varargs and they are all in this file.
- Writing out versions that took multiple type-safe arguments caused IntelliJ to present all of them
- for auto-completion which was overwhelming, so I reverted to varargs.  Also,
+ <p>vec(), map(), and set() are the only three methods in this project to take varargs.  I tried
+ writing out versions that took multiple type-safe arguments, but IntelliJ presented you with a
+ menu of all of them for auto-completion which was overwhelming, so I reverted to varargs.  Also,
  varargs relax some type safety rules (variance) for data definition in a generally helpful (rarely
  dangerous) way.</p>
 
@@ -71,23 +55,15 @@ import java.util.Map;
  implementations, which shouldn't break anything.  Let me know if you find that the danger outweighs
  convenience or have advice on what to do about it.</p>
  */
-@SuppressWarnings("UnusedDeclaration")
-public final class StaticImports {
-    // Prevent instantiation
-    private StaticImports() { throw new UnsupportedOperationException("No instantiation"); }
+@SuppressWarnings(value = { "UnusedDeclaration" }) public final class StaticImports {
+  private StaticImports() {
+    throw new UnsupportedOperationException("No instantiation");
+  }
 
-//    /**
-//     This turned out to be a bad idea due to the complexity and slowness of serializing
-//     a class extended from an immutable tuple.  I made tuples serializable and was able to back out
-//     other breaking changes.
-//     */
-//    @Deprecated
-//    public static <K,V> Tuple2<K,V> kv(K t, V u) { return Tuple2.of(t, u); }
-
-    /**
+  /**
      Returns a new PersistentHashMap of the given keys and their paired values.  Use the
      {@link StaticImports#tup(Object, Object)} method to define those key/value pairs briefly and
-     easily.  This data definition method is one of the few methods in this project that support
+     easily.  This data definition method is one of the three methods in this project that support
      varargs.
 
      @param kvPairs Key/value pairs (to go into the map).  In the case of a duplicate key, later
@@ -96,13 +72,14 @@ public final class StaticImports {
 
      @return a new PersistentHashMap of the given key/value pairs
      */
-    @SafeVarargs
-    public static <K,V> ImMap<K,V> map(Map.Entry<K,V>... kvPairs) {
-        if ( (kvPairs == null) || (kvPairs.length < 1) ) { return PersistentHashMap.empty(); }
-        return PersistentHashMap.of(Arrays.asList(kvPairs));
+  @SafeVarargs public static <K extends java.lang.Object, V extends java.lang.Object> ImMap<K, V> map(Map.Entry<K, V>... kvPairs) {
+    if ((kvPairs == null) || (kvPairs.length < 1)) {
+      return PersistentHashMap.empty();
     }
+    return PersistentHashMap.of(Arrays.asList(kvPairs));
+  }
 
-    /**
+  /**
      Returns a new MutableUnsortedMap of the given keys and their paired values.  Use the
      {@link StaticImports#tup(Object, Object)} method to define those key/value pairs briefly and
      easily.  This data definition method is one of the few methods in this project that support
@@ -114,57 +91,61 @@ public final class StaticImports {
 
      @return a new MutableUnsortedMap of the given key/value pairs
      */
-    @SafeVarargs
-    public static <K,V> MutableUnsortedMap<K,V> mutableMap(Map.Entry<K,V>... kvPairs) {
-        MutableUnsortedMap<K,V> ret = PersistentHashMap.emptyMutable();
-        if (kvPairs == null) { return ret; }
-        for (Map.Entry<K,V> me : kvPairs) {
-            ret.assoc(me);
-        }
-        return ret;
+  @SafeVarargs public static <K extends java.lang.Object, V extends java.lang.Object> MutableUnsortedMap<K, V> mutableMap(Map.Entry<K, V>... kvPairs) {
+    MutableUnsortedMap<K, V> ret = PersistentHashMap.emptyMutable();
+    if (kvPairs == null) {
+      return ret;
     }
+    for (Map.Entry<K, V> me : kvPairs) {
+      ret.assoc(me);
+    }
+    return ret;
+  }
 
-    /**
+  /**
      Returns a new MutableUnsortedSet of the values.  This data definition method is one of the few
      methods in this project that support varargs.  If the input contains duplicate elements, later
      values overwrite earlier ones.
      */
-    @SafeVarargs
-    public static <T> MutableUnsortedSet<T> mutableSet(T... items) {
-        MutableUnsortedSet<T> ret = PersistentHashSet.emptyMutable();
-        if (items == null) { return ret; }
-        for (T t : items) {
-            ret.put(t);
-        }
-        return ret;
+  @SafeVarargs public static <T extends java.lang.Object> MutableUnsortedSet<T> mutableSet(T... items) {
+    MutableUnsortedSet<T> ret = PersistentHashSet.emptyMutable();
+    if (items == null) {
+      return ret;
     }
+    for (T t : items) {
+      ret.put(t);
+    }
+    return ret;
+  }
 
-    /**
+  /**
      Returns a MutableVector of the given items.  This data definition method is one of the
      few methods in this project that support varargs.
      */
-    @SafeVarargs
-    public static <T> MutableList<T> mutableVec(T... items) {
-        MutableList<T> ret = PersistentVector.emptyMutable();
-        if (items == null) { return ret; }
-        for (T t : items) {
-            ret.append(t);
-        }
-        return ret;
+  @SafeVarargs static public <T extends java.lang.Object> MutableList<T> mutableVec(T... items) {
+    MutableList<T> ret = PersistentVector.emptyMutable();
+    if (items == null) {
+      return ret;
     }
+    for (T t : items) {
+      ret.append(t);
+    }
+    return ret;
+  }
 
-    /**
-     Returns a new PersistentHashSet of the values.  This data definition method is one of the few
+  /**
+     Returns a new PersistentHashSet of the values.  This data definition method is one of the three
      methods in this project that support varargs.  If the input contains duplicate elements, later
      values overwrite earlier ones.
      */
-    @SafeVarargs
-    public static <T> ImSet<T> set(T... items) {
-        if ( (items == null) || (items.length < 1) ) { return PersistentHashSet.empty(); }
-        return PersistentHashSet.of(Arrays.asList(items));
+  @SafeVarargs public static <T extends java.lang.Object> ImSet<T> set(T... items) {
+    if ((items == null) || (items.length < 1)) {
+      return PersistentHashSet.empty();
     }
+    return PersistentHashSet.of(Arrays.asList(items));
+  }
 
-    /**
+  /**
      Returns a new PersistentTreeMap of the specified comparator and the given key/value pairs.  Use
      the tup() method to define those key/value pairs briefly and easily.  The keys are sorted
      according to the comparator you provide.
@@ -180,12 +161,11 @@ public final class StaticImports {
 
      @return a new PersistentTreeMap of the specified comparator and the given key/value pairs
      */
-    public static <K,V> ImSortedMap<K,V>
-    sortedMap(Comparator<? super K> comp, Iterable<Map.Entry<K,V>> kvPairs) {
-        return PersistentTreeMap.ofComp(comp, kvPairs);
-    }
+  public static <K extends java.lang.Object, V extends java.lang.Object> ImSortedMap<K, V> sortedMap(Comparator<? super K> comp, Iterable<Map.Entry<K, V>> kvPairs) {
+    return PersistentTreeMap.ofComp(comp, kvPairs);
+  }
 
-    /**
+  /**
      Returns a new PersistentTreeMap of the given comparable keys and their paired values, sorted in
      the default ordering of the keys.  Use the tup() method to define those key/value pairs briefly
      and easily.
@@ -195,10 +175,11 @@ public final class StaticImports {
      @return a new PersistentTreeMap of the specified comparator and the given key/value pairs which
      uses the default comparator defined on the element type.
      */
-    public static <K extends Comparable<K>,V> ImSortedMap<K,V>
-    sortedMap(Iterable<Map.Entry<K,V>> kvPairs) { return PersistentTreeMap.of(kvPairs); }
+  public static <K extends Comparable<K>, V extends java.lang.Object> ImSortedMap<K, V> sortedMap(Iterable<Map.Entry<K, V>> kvPairs) {
+    return PersistentTreeMap.of(kvPairs);
+  }
 
-    /**
+  /**
      Returns a new PersistentTreeSet of the given comparator and items.
 
      @param comp A comparator that defines the sort order of elements in the new set.  This
@@ -207,69 +188,46 @@ public final class StaticImports {
      earlier ones.
      @return a new PersistentTreeSet of the specified comparator and the given elements
      */
-    public static <T> ImSortedSet<T> sortedSet(Comparator<? super T> comp, Iterable<T> elements) {
-        return Xform.of(elements).toImSortedSet(comp);
-    }
+  public static <T extends java.lang.Object> ImSortedSet<T> sortedSet(Comparator<? super T> comp, Iterable<T> elements) {
+    return Xform.of(elements).toImSortedSet(comp);
+  }
 
-    /** Returns a new PersistentTreeSet of the given comparable items. */
-    public static <T extends Comparable<T>> ImSortedSet<T> sortedSet(Iterable<T> items) {
-        return PersistentTreeSet.of(items);
-    }
+  /** Returns a new PersistentTreeSet of the given comparable items. */
+  public static <T extends Comparable<T>> ImSortedSet<T> sortedSet(Iterable<T> items) {
+    return PersistentTreeSet.of(items);
+  }
 
-    /** Returns a new Tuple2 of the given items. */
-    public static <T,U> Tuple2<T,U> tup(T t, U u) { return Tuple2.of(t, u); }
+  /** Returns a new Tuple2 of the given items. */
+  public static <T extends java.lang.Object, U extends java.lang.Object> Tuple2<T, U> tup(T t, U u) {
+    return Tuple2.of(t, u);
+  }
 
-    /** Returns a new Tuple3 of the given items. */
-    public static <T,U,V> Tuple3<T,U,V> tup(T t, U u, V v) { return Tuple3.of(t, u, v); }
+  /** Returns a new Tuple3 of the given items. */
+  public static <T extends java.lang.Object, U extends java.lang.Object, V extends java.lang.Object> Tuple3<T, U, V> tup(T t, U u, V v) {
+    return Tuple3.of(t, u, v);
+  }
 
-    /**
+  /**
      Returns a new PersistentVector of the given items.  This data definition method is one of the
-     few methods in this project that support varargs.
+     three methods in this project that support varargs.
      */
-    @SafeVarargs
-    static public <T> ImList<T> vec(T... items) {
-        if ( (items == null) || (items.length < 1) ) { return PersistentVector.empty(); }
-        return PersistentVector.ofIter(Arrays.asList(items));
+  @SafeVarargs static public <T extends java.lang.Object> ImList<T> vec(T... items) {
+    if ((items == null) || (items.length < 1)) {
+      return PersistentVector.empty();
     }
+    return PersistentVector.ofIter(Arrays.asList(items));
+  }
 
-    /**
-     If you need to wrap a regular Java collection or other iterable outside this project to perform
-     a transformation on it, this method is the most convenient, efficient way to do so.
-     */
-    public static <T> UnmodIterable<T> xform(Iterable<T> iterable) { return Xform.of(iterable); }
+  /** Wrap a regular Java collection or other iterable outside this project to perform a transformation on it. */
+  public static <T extends java.lang.Object> UnmodIterable<T> xform(Iterable<T> iterable) {
+    return Xform.of(iterable);
+  }
 
-    /**
+  /**
      If you need to wrap a regular Java array outside this project to perform
      a transformation on it, this method is the most convenient, efficient way to do so.
      */
-    @SafeVarargs
-    public static <T> UnmodIterable<T> xformArray(T... items) {
-        return Xform.of(Arrays.asList(items));
-    }
-
-    // TODO: Enable this to make Maps, Strings, and StringBuilders work like other collections.
-//    /** Wrap a Java.util.Map to perform a transformation on it. */
-//    public static <K,V> UnmodIterable<Map.Entry<K,V>> xform(Map<K,V> map) {
-//        return Xform.of(map.entrySet());
-//    }
-//
-//    /** Wrap a String to perform a transformation on it. */
-//    public static Transformable<Character> xform(CharSequence seq) {
-//        //noinspection Convert2Lambda
-//        return Xform.of(new Iterable<Character>() {
-//            @Override public Iterator<Character> iterator() {
-//                return new Iterator<Character>() {
-//                    private int idx = 0;
-//                    @Override public boolean hasNext() { return idx < seq.length(); }
-//
-//                    @Override public Character next() {
-//                        int nextIdx = idx + 1;
-//                        Character c = seq.charAt(idx);
-//                        idx = nextIdx;
-//                        return c;
-//                    }
-//                };
-//            }
-//        });
-//    }
+  @SafeVarargs public static <T extends java.lang.Object> UnmodIterable<T> xformArray(T... items) {
+    return Xform.of(Arrays.asList(items));
+  }
 }
