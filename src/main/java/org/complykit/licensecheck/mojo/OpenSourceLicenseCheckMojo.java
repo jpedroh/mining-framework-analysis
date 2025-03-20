b964c68a1bd40077e2486173a33c85c3bcbc5fdc
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 21:40:10 +0000

    Mining Framework Analysis

--- src/main/java/org/complykit/licensecheck/mojo/OpenSourceLicenseCheckMojo.java
+++ src/main/java/org/complykit/licensecheck/mojo/OpenSourceLicenseCheckMojo.java
@@ -23,23 +23,6 @@ THE SOFTWARE.
  */
 package org.complykit.licensecheck.mojo;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
-import java.util.Set;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.regex.PatternSyntaxException;
-
 import org.apache.maven.RepositoryUtils;
 import org.apache.maven.artifact.Artifact;
 import org.apache.maven.plugin.AbstractMojo;
