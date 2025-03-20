bc08ab0d936893fe3c51314d396562cba0624df4
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 05:09:21 +0000

    Mining Framework Analysis

--- frontend-maven-plugin/src/main/java/com/github/eirslett/maven/plugins/frontend/mojo/MojoUtils.java
+++ frontend-maven-plugin/src/main/java/com/github/eirslett/maven/plugins/frontend/mojo/MojoUtils.java
@@ -1,31 +1,6 @@
-package com.github.eirslett.maven.plugins.frontend.mojo;
-
-import com.github.eirslett.maven.plugins.frontend.lib.ProxyConfig;
-import org.apache.maven.execution.MavenSession;
-import org.apache.maven.plugin.MojoFailureException;
-import org.apache.maven.settings.Proxy;
-import org.apache.maven.settings.crypto.DefaultSettingsDecryptionRequest;
-import org.apache.maven.settings.crypto.SettingsDecrypter;
-import org.apache.maven.settings.crypto.SettingsDecryptionResult;
-
-class MojoUtils {
-    static <E extends Throwable> MojoFailureException toMojoFailureException(E e) {
-        return new MojoFailureException(e.getMessage() + ": " + e.getCause().getMessage(), e);
-    }
-
-    static ProxyConfig getProxyConfig(MavenSession mavenSession, SettingsDecrypter decrypter) {
-        if (mavenSession == null ||
-                mavenSession.getSettings() == null ||
-                mavenSession.getSettings().getActiveProxy() == null ||
-                !mavenSession.getSettings().getActiveProxy().isActive()) {
-            return null;
-        } else {
-            Proxy mavenProxy = mavenSession.getSettings().getActiveProxy();
-            final DefaultSettingsDecryptionRequest decryptionRequest = new DefaultSettingsDecryptionRequest(mavenProxy);
-            SettingsDecryptionResult decryptedResult = decrypter.decrypt(decryptionRequest);
-            mavenProxy = decryptedResult.getProxy();
-            return new ProxyConfig(mavenProxy.getId(), mavenProxy.getProtocol(), mavenProxy.getHost(),
-                    mavenProxy.getPort(), mavenProxy.getUsername(), mavenProxy.getPassword());
-        }
-    }
-}
+  package       com . github . eirslett . maven . plugins . frontend . mojo ;   import        com . github . eirslett . maven . plugins . frontend . lib . ProxyConfig ;  import     org . apache . maven . execution . MavenSession ;  import     org . apache . maven . plugin . MojoFailureException ;  import     org . apache . maven . settings . Proxy ;  import      org . apache . maven . settings . crypto . DefaultSettingsDecryptionRequest ;  import      org . apache . maven . settings . crypto . SettingsDecrypter ;  import      org . apache . maven . settings . crypto . SettingsDecryptionResult ;  class MojoUtils  {   static  <  E  extends Throwable > MojoFailureException toMojoFailureException  (  E e )  {  return  new MojoFailureException  (    e . getMessage  ( ) + ": " +   e . getCause  ( ) . getMessage  ( ) , e ) ; } 
+<<<<<<<
+  static ProxyConfig getProxyConfig  (  MavenSession mavenSession )  {  if  (     mavenSession == null ||   mavenSession . getSettings  ( ) == null ||    mavenSession . getSettings  ( ) . getActiveProxy  ( ) == null ||  !    mavenSession . getSettings  ( ) . getActiveProxy  ( ) . isActive  ( ) )  {  return null ; } else  {  Proxy  mavenProxy =   mavenSession . getSettings  ( ) . getActiveProxy  ( ) ;  return  new ProxyConfig  (  mavenProxy . getId  ( ) ,  mavenProxy . getProtocol  ( ) ,  mavenProxy . getHost  ( ) ,  mavenProxy . getPort  ( ) ,  mavenProxy . getUsername  ( ) ,  mavenProxy . getPassword  ( ) ) ; } }
+=======
+>>>>>>>
+   static ProxyConfig getProxyConfig  (  MavenSession mavenSession ,  SettingsDecrypter decrypter )  {  if  (     mavenSession == null ||   mavenSession . getSettings  ( ) == null ||    mavenSession . getSettings  ( ) . getActiveProxy  ( ) == null ||  !    mavenSession . getSettings  ( ) . getActiveProxy  ( ) . isActive  ( ) )  {  return null ; } else  {  Proxy  mavenProxy =   mavenSession . getSettings  ( ) . getActiveProxy  ( ) ;   final DefaultSettingsDecryptionRequest  decryptionRequest =  new DefaultSettingsDecryptionRequest  ( mavenProxy ) ;  SettingsDecryptionResult  decryptedResult =  decrypter . decrypt  ( decryptionRequest ) ;   mavenProxy =  decryptedResult . getProxy  ( ) ;  return  new ProxyConfig  (  mavenProxy . getProtocol  ( ) ,  mavenProxy . getHost  ( ) ,  mavenProxy . getPort  ( ) ,  mavenProxy . getUsername  ( ) ,  mavenProxy . getPassword  ( ) ) ; } } }
