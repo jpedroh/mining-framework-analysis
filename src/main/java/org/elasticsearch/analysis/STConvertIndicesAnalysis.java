68f4a44e066e2d59fefa94c8a4d4b4a198c8d08a
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 19:06:54 +0000

    Mining Framework Analysis

--- src/main/java/org/elasticsearch/analysis/STConvertIndicesAnalysis.java
+++ src/main/java/org/elasticsearch/analysis/STConvertIndicesAnalysis.java
@@ -1,6 +1,4 @@
 package org.elasticsearch.analysis;
-
-
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.Tokenizer;
 import org.elasticsearch.common.component.AbstractComponent;
@@ -9,7 +7,6 @@ import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.env.Environment;
 import org.elasticsearch.index.analysis.*;
 import org.elasticsearch.indices.analysis.IndicesAnalysisService;
-
 import java.io.Reader;
 
 /**
@@ -17,181 +14,113 @@ import java.io.Reader;
  * will be shared among all indices.
  */
 public class STConvertIndicesAnalysis extends AbstractComponent {
-    
-    private static final String STCONVERT = "stconvert";
-    private static final String CONVERT_TYPE = "convert_type";
-    private static final String KEEP_BOTH = "keep_both";
-    private static final String TSCONVERT = "tsconvert";
-    private static final String STCONVERT_KEEP_BOTH = "stconvert_keep_both";
-    private static final String TSCONVERT_KEEP_BOTH = "tsconvert_keep_both";
+  private static final String STCONVERT = "stconvert";
 
-    @Inject
-    public STConvertIndicesAnalysis(final Settings settings,
-                                 IndicesAnalysisService indicesAnalysisService, Environment env) {
-        super(settings);
+  private static final String CONVERT_TYPE = "convert_type";
 
-        //analyzers
-        indicesAnalysisService.analyzerProviderFactories().put(STCONVERT,
-                new PreBuiltAnalyzerProviderFactory(STCONVERT, AnalyzerScope.GLOBAL,
-                        new STConvertAnalyzer(Settings.builder()
-                                .put(CONVERT_TYPE, "s2t")
-                                .put(KEEP_BOTH, false)
-                                .build())));
+  private static final String KEEP_BOTH = "keep_both";
 
-        indicesAnalysisService.analyzerProviderFactories().put(TSCONVERT,
-                new PreBuiltAnalyzerProviderFactory(TSCONVERT, AnalyzerScope.GLOBAL,
-                        new STConvertAnalyzer(Settings.builder()
-                                .put(CONVERT_TYPE, "t2s")
-                                .put(KEEP_BOTH, false)
-                                .build())));
+  private static final String TSCONVERT = "tsconvert";
 
-        indicesAnalysisService.analyzerProviderFactories().put(STCONVERT_KEEP_BOTH,
-                new PreBuiltAnalyzerProviderFactory(STCONVERT_KEEP_BOTH, AnalyzerScope.GLOBAL,
-                        new STConvertAnalyzer(Settings.builder()
-                                .put(CONVERT_TYPE, "s2t")
-                                .put(KEEP_BOTH,true)
-                                .build())));
+  private static final String STCONVERT_KEEP_BOTH = "stconvert_keep_both";
 
-        indicesAnalysisService.analyzerProviderFactories().put(TSCONVERT_KEEP_BOTH,
-                new PreBuiltAnalyzerProviderFactory(TSCONVERT_KEEP_BOTH, AnalyzerScope.GLOBAL,
-                        new STConvertAnalyzer(Settings.builder()
-                                .put(CONVERT_TYPE, "t2s")
-                                .put(KEEP_BOTH, true)
-                                .build())));
+  private static final String TSCONVERT_KEEP_BOTH = "tsconvert_keep_both";
 
+  @Inject public STConvertIndicesAnalysis(final Settings settings, IndicesAnalysisService indicesAnalysisService, Environment env) {
+    super(settings);
+    indicesAnalysisService.analyzerProviderFactories().put(STCONVERT, new PreBuiltAnalyzerProviderFactory(STCONVERT, AnalyzerScope.GLOBAL, new STConvertAnalyzer(Settings.builder().put(CONVERT_TYPE, "s2t").put(KEEP_BOTH, false).build())));
+    indicesAnalysisService.analyzerProviderFactories().put(TSCONVERT, new PreBuiltAnalyzerProviderFactory(TSCONVERT, AnalyzerScope.GLOBAL, new STConvertAnalyzer(Settings.builder().put(CONVERT_TYPE, "t2s").put(KEEP_BOTH, false).build())));
+    indicesAnalysisService.analyzerProviderFactories().put(STCONVERT_KEEP_BOTH, new PreBuiltAnalyzerProviderFactory(STCONVERT_KEEP_BOTH, AnalyzerScope.GLOBAL, new STConvertAnalyzer(Settings.builder().put(CONVERT_TYPE, "s2t").put(KEEP_BOTH, true).build())));
+    indicesAnalysisService.analyzerProviderFactories().put(TSCONVERT_KEEP_BOTH, new PreBuiltAnalyzerProviderFactory(TSCONVERT_KEEP_BOTH, AnalyzerScope.GLOBAL, new STConvertAnalyzer(Settings.builder().put(CONVERT_TYPE, "t2s").put(KEEP_BOTH, true).build())));
+    indicesAnalysisService.tokenizerFactories().put(STCONVERT, new PreBuiltTokenizerFactoryFactory(new TokenizerFactory() {
+      @Override public String name() {
+        return STCONVERT;
+      }
 
-        //tokenizers
+      @Override public Tokenizer create() {
+        return new STConvertTokenizer(STConvertType.SIMPLE_2_TRADITIONAL, ",", false);
+      }
+    }));
+    indicesAnalysisService.tokenizerFactories().put(TSCONVERT, new PreBuiltTokenizerFactoryFactory(new TokenizerFactory() {
+      @Override public String name() {
+        return TSCONVERT;
+      }
 
-        indicesAnalysisService.tokenizerFactories().put(STCONVERT,
-                new PreBuiltTokenizerFactoryFactory(new TokenizerFactory() {
-                    @Override
-                    public String name() {
-                        return STCONVERT;
-                    }
+      @Override public Tokenizer create() {
+        return new STConvertTokenizer(STConvertType.TRADITIONAL_2_SIMPLE, ",", false);
+      }
+    }));
+    indicesAnalysisService.tokenizerFactories().put(STCONVERT_KEEP_BOTH, new PreBuiltTokenizerFactoryFactory(new TokenizerFactory() {
+      @Override public String name() {
+        return STCONVERT_KEEP_BOTH;
+      }
 
-                    @Override
-                    public Tokenizer create() {
-                        return new STConvertTokenizer(STConvertType.SIMPLE_2_TRADITIONAL,",",false);
-                    }
-                }));
+      @Override public Tokenizer create() {
+        return new STConvertTokenizer(STConvertType.SIMPLE_2_TRADITIONAL, ",", true);
+      }
+    }));
+    indicesAnalysisService.tokenizerFactories().put(TSCONVERT_KEEP_BOTH, new PreBuiltTokenizerFactoryFactory(new TokenizerFactory() {
+      @Override public String name() {
+        return TSCONVERT_KEEP_BOTH;
+      }
 
-        indicesAnalysisService.tokenizerFactories().put(TSCONVERT,
-                new PreBuiltTokenizerFactoryFactory(new TokenizerFactory() {
-                    @Override
-                    public String name() {
-                        return TSCONVERT;
-                    }
+      @Override public Tokenizer create() {
+        return new STConvertTokenizer(STConvertType.TRADITIONAL_2_SIMPLE, ",", true);
+      }
+    }));
+    indicesAnalysisService.tokenFilterFactories().put(STCONVERT, new PreBuiltTokenFilterFactoryFactory(new TokenFilterFactory() {
+      @Override public String name() {
+        return STCONVERT;
+      }
 
-                    @Override
-                    public Tokenizer create() {
-                        return new STConvertTokenizer(STConvertType.TRADITIONAL_2_SIMPLE,",",false);
-                    }
-                }));
-        indicesAnalysisService.tokenizerFactories().put(STCONVERT_KEEP_BOTH,
-                new PreBuiltTokenizerFactoryFactory(new TokenizerFactory() {
-                    @Override
-                    public String name() {
-                        return STCONVERT_KEEP_BOTH;
-                    }
+      @Override public TokenStream create(TokenStream tokenStream) {
+        return new STConvertTokenFilter(tokenStream, STConvertType.SIMPLE_2_TRADITIONAL, ",", false);
+      }
+    }));
+    indicesAnalysisService.tokenFilterFactories().put(TSCONVERT, new PreBuiltTokenFilterFactoryFactory(new TokenFilterFactory() {
+      @Override public String name() {
+        return TSCONVERT;
+      }
 
-                    @Override
-                    public Tokenizer create() {
-                        return new STConvertTokenizer(STConvertType.SIMPLE_2_TRADITIONAL,",",true);
-                    }
-                }));
-        indicesAnalysisService.tokenizerFactories().put(TSCONVERT_KEEP_BOTH,
-                new PreBuiltTokenizerFactoryFactory(new TokenizerFactory() {
-                    @Override
-                    public String name() {
-                        return TSCONVERT_KEEP_BOTH;
-                    }
+      @Override public TokenStream create(TokenStream tokenStream) {
+        return new STConvertTokenFilter(tokenStream, STConvertType.TRADITIONAL_2_SIMPLE, ",", false);
+      }
+    }));
+    indicesAnalysisService.tokenFilterFactories().put(STCONVERT_KEEP_BOTH, new PreBuiltTokenFilterFactoryFactory(new TokenFilterFactory() {
+      @Override public String name() {
+        return STCONVERT_KEEP_BOTH;
+      }
 
-                    @Override
-                    public Tokenizer create() {
-                        return new STConvertTokenizer(STConvertType.TRADITIONAL_2_SIMPLE,",",true);
-                    }
-                }));
+      @Override public TokenStream create(TokenStream tokenStream) {
+        return new STConvertTokenFilter(tokenStream, STConvertType.SIMPLE_2_TRADITIONAL, ",", true);
+      }
+    }));
+    indicesAnalysisService.tokenFilterFactories().put(TSCONVERT_KEEP_BOTH, new PreBuiltTokenFilterFactoryFactory(new TokenFilterFactory() {
+      @Override public String name() {
+        return TSCONVERT_KEEP_BOTH;
+      }
 
+      @Override public TokenStream create(TokenStream tokenStream) {
+        return new STConvertTokenFilter(tokenStream, STConvertType.TRADITIONAL_2_SIMPLE, ",", true);
+      }
+    }));
+    indicesAnalysisService.charFilterFactories().put(STCONVERT, new PreBuiltCharFilterFactoryFactory(new CharFilterFactory() {
+      @Override public String name() {
+        return STCONVERT;
+      }
 
+      @Override public Reader create(Reader tokenStream) {
+        return new STConvertCharFilter(tokenStream, STConvertType.SIMPLE_2_TRADITIONAL);
+      }
+    }));
+    indicesAnalysisService.charFilterFactories().put(TSCONVERT, new PreBuiltCharFilterFactoryFactory(new CharFilterFactory() {
+      @Override public String name() {
+        return TSCONVERT;
+      }
 
-        //tokenFilters
-        indicesAnalysisService.tokenFilterFactories().put(STCONVERT,
-                new PreBuiltTokenFilterFactoryFactory(new TokenFilterFactory() {
-                    @Override
-                    public String name() {
-                        return STCONVERT;
-                    }
-
-                    @Override
-                    public TokenStream create(TokenStream tokenStream) {
-                        return new STConvertTokenFilter(tokenStream, STConvertType.SIMPLE_2_TRADITIONAL,",",false);
-                    }
-                }));
-
-        indicesAnalysisService.tokenFilterFactories().put(TSCONVERT,
-                new PreBuiltTokenFilterFactoryFactory(new TokenFilterFactory() {
-                    @Override
-                    public String name() {
-                        return TSCONVERT;
-                    }
-
-                    @Override
-                    public TokenStream create(TokenStream tokenStream) {
-                        return new STConvertTokenFilter(tokenStream, STConvertType.TRADITIONAL_2_SIMPLE,",",false);
-                    }
-                }));
-
-        indicesAnalysisService.tokenFilterFactories().put(STCONVERT_KEEP_BOTH,
-                new PreBuiltTokenFilterFactoryFactory(new TokenFilterFactory() {
-                    @Override
-                    public String name() {
-                        return STCONVERT_KEEP_BOTH;
-                    }
-
-                    @Override
-                    public TokenStream create(TokenStream tokenStream) {
-                        return new STConvertTokenFilter(tokenStream, STConvertType.SIMPLE_2_TRADITIONAL,",",true);
-                    }
-                }));
-
-        indicesAnalysisService.tokenFilterFactories().put(TSCONVERT_KEEP_BOTH,
-                new PreBuiltTokenFilterFactoryFactory(new TokenFilterFactory() {
-                    @Override
-                    public String name() {
-                        return TSCONVERT_KEEP_BOTH;
-                    }
-
-                    @Override
-                    public TokenStream create(TokenStream tokenStream) {
-                        return new STConvertTokenFilter(tokenStream, STConvertType.TRADITIONAL_2_SIMPLE,",",true);
-                    }
-                }));
-
-        //char filter
-        indicesAnalysisService.charFilterFactories().put(STCONVERT,
-                new PreBuiltCharFilterFactoryFactory(new CharFilterFactory() {
-                    @Override
-                    public String name() {
-                        return STCONVERT;
-                    }
-
-                    @Override
-                    public Reader create(Reader tokenStream) {
-                        return new STConvertCharFilter(tokenStream,STConvertType.SIMPLE_2_TRADITIONAL);
-                    }
-                }));
-        indicesAnalysisService.charFilterFactories().put(TSCONVERT,
-                new PreBuiltCharFilterFactoryFactory(new CharFilterFactory() {
-                    @Override
-                    public String name() {
-                        return TSCONVERT;
-                    }
-
-                    @Override
-                    public Reader create(Reader tokenStream) {
-                        return new STConvertCharFilter(tokenStream,STConvertType.TRADITIONAL_2_SIMPLE);
-                    }
-                }));
-
-    }
+      @Override public Reader create(Reader tokenStream) {
+        return new STConvertCharFilter(tokenStream, STConvertType.TRADITIONAL_2_SIMPLE);
+      }
+    }));
+  }
 }
