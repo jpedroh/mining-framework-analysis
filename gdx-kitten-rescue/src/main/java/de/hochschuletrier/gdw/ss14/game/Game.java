0b068d999c3f86726e9eb77010e2b93ca1b0cbed
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 02:21:26 +0000

    Mining Framework Analysis

--- gdx-kitten-rescue/src/main/java/de/hochschuletrier/gdw/ss14/game/Game.java
+++ gdx-kitten-rescue/src/main/java/de/hochschuletrier/gdw/ss14/game/Game.java
@@ -1,5 +1,4 @@
 package de.hochschuletrier.gdw.ss14.game;
-
 import com.badlogic.gdx.math.Vector2;
 import de.hochschuletrier.gdw.commons.gdx.assets.AssetManagerX;
 import de.hochschuletrier.gdw.commons.gdx.physix.PhysixManager;
@@ -13,82 +12,74 @@ import de.hochschuletrier.gdw.ss14.input.InputManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Game{
-    private static final Logger logger = LoggerFactory.getLogger(Game.class);
+public class Game {
+  private static final Logger logger = LoggerFactory.getLogger(Game.class);
 
-    private Engine engine;
+  private Engine engine;
 
-    private MapManager mapManager;
-    private EntityManager entityManager;
-    private PhysixManager physixManager;
+  private MapManager mapManager;
 
-    private Vector2 mapCenter = new Vector2();
+  private EntityManager entityManager;
 
-    public Game(AssetManagerX am){
-        engine = new Engine();
-        entityManager = new EntityManager();
-        physixManager = new PhysixManager(3.0f, 0.0f, 0.0f);
-        mapManager = new MapManager(entityManager, physixManager, am);
+  private PhysixManager physixManager;
 
-        EntityFactory.phyManager = physixManager;
-        EntityFactory.manager = entityManager;
-        EntityFactory.assetManager = am;
+  private Vector2 mapCenter = new Vector2();
+
+  public Game(AssetManagerX am) {
+    engine = new Engine();
+    entityManager = new EntityManager();
+    physixManager = new PhysixManager(3.0f, 0.0f, 0.0f);
+    mapManager = new MapManager(entityManager, physixManager, am);
+    EntityFactory.phyManager = physixManager;
+    EntityFactory.manager = entityManager;
+    EntityFactory.assetManager = am;
+  }
+
+  public void init(AssetManagerX assetManager) {
+    initializeSystems();
+    initializeTestComponents();
+    mapManager.loadMap("Katzenklappentest");
+    mapManager.setFloor(0);
+    InputManager.init();
+  }
+
+  private void initializeSystems() {
+    engine.addSystem(new InputSystem(entityManager));
+    engine.addSystem(new DogInputSystem(entityManager));
+    engine.addSystem(new PlayerMovementSystem(entityManager));
+    engine.addSystem(new DogMovementSystem(entityManager));
+    engine.addSystem(new HitAnimationSystem(entityManager));
+    engine.addSystem(new CameraSystem(entityManager, 1024));
+    engine.addSystem(new PhysixDebugRenderSystem(entityManager, physixManager));
+    engine.addSystem(new PhysixUpdateSystem(entityManager, physixManager));
+    engine.addSystem(new TileMapRenderingSystem(entityManager, 0));
+    engine.addSystem(new AnimationSystem(entityManager, 1));
+    engine.addSystem(new RenderSystem(entityManager, 1200));
+  }
+
+  private void initializeTestComponents() {
+    int dogEntity = EntityFactory.constructDog(new Vector2(0, 0), 60.0f, 40.0f, 0, 100f);
+    int dogEntity2 = EntityFactory.constructDog(new Vector2(500, 350), 60.0f, 40.0f, 0, 100f);
+  }
+
+  public TiledMap loadMap(String filename) {
+    try {
+      return new TiledMap(filename, LayerObject.PolyMode.ABSOLUTE);
+    } catch (Exception ex) {
+      throw new IllegalArgumentException("Map konnte nicht geladen werden: " + filename);
     }
+  }
 
-    public void init(AssetManagerX assetManager){
-        initializeSystems();
-        initializeTestComponents();
+  public TiledMap getMap() {
+    return null;
+  }
 
-        mapManager.loadMap("Katzenklappentest");
-        mapManager.setFloor(0);
-        InputManager.init();
-    }
+  public void update(float delta) {
+    InputManager.getInstance().update();
+    engine.update(delta);
+  }
 
-    private void initializeSystems(){
-        // Game logic related systems
-        engine.addSystem(new InputSystem(entityManager));
-        engine.addSystem(new DogInputSystem(entityManager));
-        engine.addSystem(new PlayerMovementSystem(entityManager));
-        engine.addSystem(new DogMovementSystem(entityManager));
-        engine.addSystem(new HitAnimationSystem(entityManager));
-
-        engine.addSystem(new CameraSystem(entityManager, 1024));
-
-        // physic systems
-        engine.addSystem(new PhysixDebugRenderSystem(entityManager, physixManager));
-        engine.addSystem(new PhysixUpdateSystem(entityManager, physixManager));
-
-        // Rendering related systems
-        engine.addSystem(new TileMapRenderingSystem(entityManager, 0));
-        engine.addSystem(new AnimationSystem(entityManager, 1));
-        engine.addSystem(new RenderSystem(entityManager, 1200));
-    }
-
-    private void initializeTestComponents(){
-        int dogEntity = EntityFactory.constructDog(new Vector2(0, 0), 60.0f, 40.0f, 0, 100f);
-        int dogEntity2 = EntityFactory.constructDog(new Vector2(500, 350), 60.0f, 40.0f, 0, 100f);
-    }
-
-    public TiledMap loadMap(String filename){
-        try{
-            return new TiledMap(filename, LayerObject.PolyMode.ABSOLUTE);
-        }
-        catch(Exception ex){
-            throw new IllegalArgumentException("Map konnte nicht geladen werden: "+filename);
-        }
-    }
-
-    public TiledMap getMap(){
-
-        return null;
-    }
-
-    public void update(float delta){
-        InputManager.getInstance().update();
-        engine.update(delta);
-    }
-
-    public void render(){
-        engine.render();
-    }
+  public void render() {
+    engine.render();
+  }
 }
