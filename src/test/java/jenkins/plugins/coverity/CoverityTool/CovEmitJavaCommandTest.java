e7ae476cb212cd4963b0eca26f98a6e4db00f788
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 22:21:54 +0000

    Mining Framework Analysis

--- src/test/java/jenkins/plugins/coverity/CoverityTool/CovEmitJavaCommandTest.java
+++ src/test/java/jenkins/plugins/coverity/CoverityTool/CovEmitJavaCommandTest.java
@@ -1,59 +1,6 @@
-/*******************************************************************************
- * Copyright (c) 2017 Synopsys, Inc
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *    Synopsys, Inc - initial implementation and documentation
- *******************************************************************************/
-package jenkins.plugins.coverity.CoverityTool;
-
-import jenkins.plugins.coverity.CoverityPublisher;
-import jenkins.plugins.coverity.InvocationAssistance;
-import org.apache.commons.lang.StringUtils;
-import org.junit.Test;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-public class CovEmitJavaCommandTest extends CommandTestBase {
-
-    @Test
-    public void addJavaWarFilesTest() throws IOException, InterruptedException {
-        List<String> javaWarFiles = new ArrayList<>();
-        javaWarFiles.add("webapp1.war");
-        javaWarFiles.add("webapp2.war");
-
-        InvocationAssistance invocationAssistance = new InvocationAssistance(
-                false, StringUtils.EMPTY, false, StringUtils.EMPTY, false, false,
-                StringUtils.EMPTY, StringUtils.EMPTY, StringUtils.EMPTY, StringUtils.EMPTY,
-                false, StringUtils.EMPTY, StringUtils.EMPTY, javaWarFiles, false, false,
-                StringUtils.EMPTY, StringUtils.EMPTY, null, false
-        );
-        CoverityPublisher publisher = new CoverityPublisher(
-                null, invocationAssistance, false, false, false, false, false,
-                null, null
-        );
-
-        ICommand covEmitJavaCommand = new CovEmitJavaCommand(build, launcher, listener, publisher, StringUtils.EMPTY, envVars, false);
-        setExpectedArguments(new String[] {"cov-emit-java", "--dir", "TestDir", "--webapp-archive", "webapp1.war", "--webapp-archive", "webapp2.war"});
-        covEmitJavaCommand.runCommand();
-        consoleLogger.verifyLastMessage("[Coverity] cov-emit-java command line arguments: " + actualArguments.toString());
-    }
-
-    @Test
-    public void cannotExecuteTest() throws IOException, InterruptedException {
-        CoverityPublisher publisher = new CoverityPublisher(
-                null, null, false, false, false, false, false,
-                StringUtils.EMPTY, StringUtils.EMPTY, StringUtils.EMPTY,
-                null, null, null
-        );
-
-        ICommand covEmitJavaCommand = new CovEmitJavaCommand(build, launcher, listener, publisher, StringUtils.EMPTY, envVars, false);
-        covEmitJavaCommand.runCommand();
-        consoleLogger.verifyLastMessage("[Coverity] Skipping command because it can't be executed");
-    }
-}
+  package    jenkins . plugins . coverity . CoverityTool ;   import    jenkins . plugins . coverity . CoverityPublisher ;  import    jenkins . plugins . coverity . InvocationAssistance ;  import     org . apache . commons . lang . StringUtils ;  import   org . junit . Test ;  import   java . io . IOException ;  import   java . util . ArrayList ;  import   java . util . List ;   public class CovEmitJavaCommandTest  extends CommandTestBase  {    @ Test public void addJavaWarFilesTest  ( )  throws IOException , InterruptedException  {   List  < String >  javaWarFiles =  new  ArrayList  < >  ( ) ;   javaWarFiles . add  ( "webapp1.war" ) ;   javaWarFiles . add  ( "webapp2.war" ) ;  InvocationAssistance  invocationAssistance =  new InvocationAssistance  ( false ,  StringUtils . EMPTY , false ,  StringUtils . EMPTY , false , false ,  StringUtils . EMPTY ,  StringUtils . EMPTY ,  StringUtils . EMPTY ,  StringUtils . EMPTY , false ,  StringUtils . EMPTY ,  StringUtils . EMPTY , javaWarFiles , false , false ,  StringUtils . EMPTY ,  StringUtils . EMPTY , null , false ) ;  CoverityPublisher  publisher =  new CoverityPublisher  ( null , invocationAssistance , false , false , false , false , false ,  StringUtils . EMPTY ,  StringUtils . EMPTY ,  StringUtils . EMPTY , null , null , null ) ;  ICommand  covEmitJavaCommand =  new CovEmitJavaCommand  ( build , launcher , listener , publisher ,  StringUtils . EMPTY , envVars , false ) ;   setExpectedArguments  (  new String  [ ]  { "cov-emit-java" , "--dir" , "TestDir" , "--webapp-archive" , "webapp1.war" , "--webapp-archive" , "webapp2.war" } ) ;   covEmitJavaCommand . runCommand  ( ) ;   consoleLogger . verifyLastMessage  (  "[Coverity] cov-emit-java command line arguments: " +  actualArguments . toString  ( ) ) ; }    @ Test public void cannotExecuteTest  ( )  throws IOException , InterruptedException  {  CoverityPublisher  publisher =  new CoverityPublisher  ( null , null , false , false , false , false , false ,  StringUtils . EMPTY ,  StringUtils . EMPTY ,  StringUtils . EMPTY , null , null , null ) ;  ICommand  covEmitJavaCommand =  new CovEmitJavaCommand  ( build , launcher , listener , publisher ,  StringUtils . EMPTY , envVars , false ) ;   covEmitJavaCommand . runCommand  ( ) ;   consoleLogger . verifyLastMessage  ( "[Coverity] Skipping command because it can't be executed" ) ; } 
+<<<<<<<
+=======
+   @ Test public void CovEmitJavaCommand_AddJavaWarFilesTest  ( )  {   mocker . replay  ( ) ;   List  < String >  javaWarFiles =  new  ArrayList  < >  ( ) ;   javaWarFiles . add  ( "webapp1.war" ) ;   javaWarFiles . add  ( "webapp2.war" ) ;  InvocationAssistance  invocationAssistance =  new InvocationAssistance  ( false ,  StringUtils . EMPTY , false ,  StringUtils . EMPTY , false , false ,  StringUtils . EMPTY ,  StringUtils . EMPTY ,  StringUtils . EMPTY ,  StringUtils . EMPTY , false ,  StringUtils . EMPTY ,  StringUtils . EMPTY , javaWarFiles , false , false ,  StringUtils . EMPTY ,  StringUtils . EMPTY , null , false ) ;  CoverityPublisher  publisher =  new CoverityPublisher  ( null , invocationAssistance , false , false , false , false , false , null , null ) ;  CovCommand  covEmitJavaCommand =  new CovEmitJavaCommand  ( build , launcher , listener , publisher ,  StringUtils . EMPTY , envVars , false ) ;   List  < String >  covEmitJavaArguments =  covEmitJavaCommand . getCommandLines  ( ) ;   assertEquals  ( 7 ,  covEmitJavaArguments . size  ( ) ) ;   checkCommandLineArg  ( covEmitJavaArguments , "cov-emit-java" ) ;   checkCommandLineArg  ( covEmitJavaArguments , "--dir" ) ;   checkCommandLineArg  ( covEmitJavaArguments , "TestDir" ) ;   checkCommandLineArg  ( covEmitJavaArguments , "--webapp-archive" ) ;   checkCommandLineArg  ( covEmitJavaArguments , "webapp1.war" ) ;   checkCommandLineArg  ( covEmitJavaArguments , "--webapp-archive" ) ;   checkCommandLineArg  ( covEmitJavaArguments , "webapp2.war" ) ;   assertEquals  ( 0 ,  covEmitJavaArguments . size  ( ) ) ; }
+>>>>>>>
+ }
--- src/test/java/jenkins/plugins/coverity/CoverityTool/CovImportMsvscaCommandTest.java
+++ src/test/java/jenkins/plugins/coverity/CoverityTool/CovImportMsvscaCommandTest.java
@@ -1,62 +1,6 @@
-/*******************************************************************************
- * Copyright (c) 2017 Synopsys, Inc
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *    Synopsys, Inc - initial implementation and documentation
- *******************************************************************************/
-package jenkins.plugins.coverity.CoverityTool;
-
-import jenkins.plugins.coverity.CoverityPublisher;
-import jenkins.plugins.coverity.InvocationAssistance;
-import jnr.ffi.annotations.In;
-import org.apache.commons.lang.StringUtils;
-import org.junit.Test;
-
-import java.io.File;
-import java.io.IOException;
-
-public class CovImportMsvscaCommandTest extends CommandTestBase {
-
-    @Test
-    public void addMsvscaOutputFilesTest() throws IOException, InterruptedException {
-
-        File analysisLog1 = new File("CodeAnalysisLog1.xml");
-        File analysisLog2 = new File("CodeAnalysisLog2.xml");
-        File[] outputFiles = new File[] {analysisLog1, analysisLog2};
-
-        InvocationAssistance invocationAssistance = new InvocationAssistance(
-                false, StringUtils.EMPTY, false, StringUtils.EMPTY, false, false,
-                StringUtils.EMPTY, StringUtils.EMPTY, StringUtils.EMPTY, StringUtils.EMPTY,
-                false, StringUtils.EMPTY, StringUtils.EMPTY, null, false, true,
-                StringUtils.EMPTY, StringUtils.EMPTY, null, false
-        );
-
-        CoverityPublisher publisher = new CoverityPublisher(
-                null, invocationAssistance, false, false, false, false, false,
-                null, null
-        );
-
-        ICommand covImportMsvscaCommand = new CovImportMsvscaCommand(build, launcher, listener, publisher, StringUtils.EMPTY, envVars, outputFiles);
-        setExpectedArguments(new String[] {
-                "cov-import-msvsca", "--dir", "TestDir", "--append", analysisLog1.getAbsolutePath(), analysisLog2.getAbsolutePath()
-        });
-        covImportMsvscaCommand.runCommand();
-        consoleLogger.verifyLastMessage("[Coverity] cov-import-msvsca command line arguments: " + actualArguments.toString());
-    }
-
-    @Test
-    public void cannotExecuteTest() throws IOException, InterruptedException {
-        CoverityPublisher publisher = new CoverityPublisher(
-                null, null, false, false, false, false, false,
-                null, null
-        );
-
-        ICommand covImportMsvscaCommand = new CovImportMsvscaCommand(build, launcher, listener, publisher, StringUtils.EMPTY, envVars, null);
-        covImportMsvscaCommand.runCommand();
-        consoleLogger.verifyLastMessage("[Coverity] Skipping command because it can't be executed");
-    }
-}
+  package    jenkins . plugins . coverity . CoverityTool ;   import    jenkins . plugins . coverity . CoverityPublisher ;  import    jenkins . plugins . coverity . InvocationAssistance ;  import    jnr . ffi . annotations . In ;  import     org . apache . commons . lang . StringUtils ;  import   org . junit . Test ;  import   java . io . File ;  import   java . io . IOException ;   public class CovImportMsvscaCommandTest  extends CommandTestBase  {    @ Test public void addMsvscaOutputFilesTest  ( )  throws IOException , InterruptedException  {  File  analysisLog1 =  new File  ( "CodeAnalysisLog1.xml" ) ;  File  analysisLog2 =  new File  ( "CodeAnalysisLog2.xml" ) ;   File  [ ]  outputFiles =  new File  [ ]  { analysisLog1 , analysisLog2 } ;  InvocationAssistance  invocationAssistance =  new InvocationAssistance  ( false ,  StringUtils . EMPTY , false ,  StringUtils . EMPTY , false , false ,  StringUtils . EMPTY ,  StringUtils . EMPTY ,  StringUtils . EMPTY ,  StringUtils . EMPTY , false ,  StringUtils . EMPTY ,  StringUtils . EMPTY , null , false , true ,  StringUtils . EMPTY ,  StringUtils . EMPTY , null , false ) ;  CoverityPublisher  publisher =  new CoverityPublisher  ( null , invocationAssistance , false , false , false , false , false ,  StringUtils . EMPTY ,  StringUtils . EMPTY ,  StringUtils . EMPTY , null , null , null ) ;  ICommand  covImportMsvscaCommand =  new CovImportMsvscaCommand  ( build , launcher , listener , publisher ,  StringUtils . EMPTY , envVars , outputFiles ) ;   setExpectedArguments  (  new String  [ ]  { "cov-import-msvsca" , "--dir" , "TestDir" , "--append" ,  analysisLog1 . getAbsolutePath  ( ) ,  analysisLog2 . getAbsolutePath  ( ) } ) ;   covImportMsvscaCommand . runCommand  ( ) ;   consoleLogger . verifyLastMessage  (  "[Coverity] cov-import-msvsca command line arguments: " +  actualArguments . toString  ( ) ) ; }    @ Test public void cannotExecuteTest  ( )  throws IOException , InterruptedException  {  CoverityPublisher  publisher =  new CoverityPublisher  ( null , null , false , false , false , false , false ,  StringUtils . EMPTY ,  StringUtils . EMPTY ,  StringUtils . EMPTY , null , null , null ) ;  ICommand  covImportMsvscaCommand =  new CovImportMsvscaCommand  ( build , launcher , listener , publisher ,  StringUtils . EMPTY , envVars , null ) ;   covImportMsvscaCommand . runCommand  ( ) ;   consoleLogger . verifyLastMessage  ( "[Coverity] Skipping command because it can't be executed" ) ; } 
+<<<<<<<
+=======
+   @ Test public void CovImportMsvscaCommand_AddMsvscaOutputFilesTest  ( )  {   mocker . replay  ( ) ;  File  analysisLog1 =  new File  ( "CodeAnalysisLog1.xml" ) ;  File  analysisLog2 =  new File  ( "CodeAnalysisLog2.xml" ) ;   File  [ ]  outputFiles =  new File  [ ]  { analysisLog1 , analysisLog2 } ;  CoverityPublisher  publisher =  new CoverityPublisher  ( null , null , false , false , false , false , false , null , null ) ;  CovCommand  covImportMsvscaCommand =  new CovImportMsvscaCommand  ( build , launcher , listener , publisher ,  StringUtils . EMPTY , envVars , outputFiles ) ;   List  < String >  covImportMsvscaArguments =  covImportMsvscaCommand . getCommandLines  ( ) ;   assertEquals  ( 6 ,  covImportMsvscaArguments . size  ( ) ) ;   checkCommandLineArg  ( covImportMsvscaArguments , "cov-import-msvsca" ) ;   checkCommandLineArg  ( covImportMsvscaArguments , "--dir" ) ;   checkCommandLineArg  ( covImportMsvscaArguments , "TestDir" ) ;   checkCommandLineArg  ( covImportMsvscaArguments , "--append" ) ;   checkCommandLineArg  ( covImportMsvscaArguments ,  analysisLog1 . getAbsolutePath  ( ) ) ;   checkCommandLineArg  ( covImportMsvscaArguments ,  analysisLog2 . getAbsolutePath  ( ) ) ;   assertEquals  ( 0 ,  covImportMsvscaArguments . size  ( ) ) ; }
+>>>>>>>
+ }
