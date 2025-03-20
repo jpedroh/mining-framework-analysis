95ae32b555e2b8a3a4d7d192330c1443a25e2cb1
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 17:06:57 +0000

    Mining Framework Analysis

--- src/main/java/com/github/steveice10/mc/protocol/data/game/entity/player/GameMode.java
+++ src/main/java/com/github/steveice10/mc/protocol/data/game/entity/player/GameMode.java
@@ -1,11 +1,1 @@
-package com.github.steveice10.mc.protocol.data.game.entity.player;
-
-import com.github.steveice10.mc.protocol.data.game.level.notify.ClientNotificationValue;
-
-public enum GameMode implements ClientNotificationValue {
-    SURVIVAL,
-    CREATIVE,
-    ADVENTURE,
-    SPECTATOR,
-    UNKNOWN
-}
+  package         com . github . steveice10 . mc . protocol . data . game . entity . player ;   import          com . github . steveice10 . mc . protocol . data . game . level . notify . ClientNotificationValue ;   public enum GameMode  implements  ClientNotificationValue  {  SURVIVAL ,  CREATIVE ,  ADVENTURE ,  SPECTATOR ,  UNKNOWN }
