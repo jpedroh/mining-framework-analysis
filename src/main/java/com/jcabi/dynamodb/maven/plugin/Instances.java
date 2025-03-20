b684d803df4efaa3248dce0d8adfb7af4533e834
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 05:12:27 +0000

    Mining Framework Analysis

--- src/main/java/com/jcabi/dynamodb/maven/plugin/Instances.java
+++ src/main/java/com/jcabi/dynamodb/maven/plugin/Instances.java
@@ -1,34 +1,4 @@
-/**
- * Copyright (c) 2012-2015, jcabi.com
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met: 1) Redistributions of source code must retain the above
- * copyright notice, this list of conditions and the following
- * disclaimer. 2) Redistributions in binary form must reproduce the above
- * copyright notice, this list of conditions and the following
- * disclaimer in the documentation and/or other materials provided
- * with the distribution. 3) Neither the name of the jcabi.com nor
- * the names of its contributors may be used to endorse or promote
- * products derived from this software without specific prior written
- * permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
- * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
- * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
- * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- * OF THE POSSIBILITY OF SUCH DAMAGE.
- */
 package com.jcabi.dynamodb.maven.plugin;
-
 import com.jcabi.aspects.Loggable;
 import com.jcabi.log.VerboseProcess;
 import com.jcabi.log.VerboseRunnable;
@@ -52,35 +22,24 @@ import lombok.ToString;
  * @checkstyle ClassDataAbstractionCoupling (500 lines)
  * @see <a href="http://docs.aws.amazon.com/amazondynamodb/latest/developerguide/Tools.html">DynamoDB Local</a>
  */
-@ToString
-@EqualsAndHashCode(of = "processes")
-@Loggable(Loggable.INFO)
-@SuppressWarnings("PMD.DoNotUseThreads")
-final class Instances {
-
-    /**
+@ToString @EqualsAndHashCode(of = "processes") @Loggable(value = Loggable.INFO) @SuppressWarnings(value = { "PMD.DoNotUseThreads" }) final class Instances {
+  /**
      * Running processes.
      */
-    private final transient ConcurrentMap<Integer, Process> processes =
-        new ConcurrentHashMap<Integer, Process>(0);
+  private final transient ConcurrentMap<Integer, Process> processes = new ConcurrentHashMap<Integer, Process>(0);
 
-    /**
+  /**
      * Public ctor.
      */
-    Instances() {
-        Runtime.getRuntime().addShutdownHook(
-            new Thread(
-                new Runnable() {
-                    @Override
-                    public void run() {
-                        Instances.this.shutdown();
-                    }
-                }
-            )
-        );
-    }
+  Instances() {
+    Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+      @Override public void run() {
+        Instances.this.shutdown();
+      }
+    }));
+  }
 
-    /**
+  /**
      * Start a new one at this port.
      * @param dist Path to DynamoDBLocal distribution
      * @param port The port to start at
@@ -89,19 +48,15 @@ final class Instances {
      * @throws IOException If fails to start
      * @checkstyle ParameterNumber (5 lines)
      */
-    public void start(@NotNull final File dist, final int port, final File home,
-        @NotNull final List<String> args)
-        throws IOException {
-        final Process process = Instances.process(dist, port, home, args);
-        final Thread thread = new Thread(
-            new VerboseRunnable(new InstanceProcess(process))
-        );
-        thread.setDaemon(true);
-        thread.start();
-        this.processes.put(port, process);
-    }
+  public void start(@NotNull final File dist, final int port, final File home, @NotNull final List<String> args) throws IOException {
+    final Process process = Instances.process(dist, port, home, args);
+    final Thread thread = new Thread(new VerboseRunnable(new InstanceProcess(process)));
+    thread.setDaemon(true);
+    thread.start();
+    this.processes.put(port, process);
+  }
 
-    /**
+  /**
      * Launch a new one at this port. This goal will block maven.
      * @param dist Path to DynamoDBLocal distribution
      * @param port The port to start at
@@ -110,42 +65,37 @@ final class Instances {
      * @throws IOException If fails to start
      * @checkstyle ParameterNumber (5 lines)
      */
-    public void run(@NotNull final File dist, final int port, final File home,
-                    @NotNull final List<String> args) throws IOException {
-        final Process process = Instances.process(dist, port, home, args);
-        this.processes.put(port, process);
-        new VerboseRunnable(new InstanceProcess(process)).run();
-    }
+  public void run(@NotNull final File dist, final int port, final File home, @NotNull final List<String> args) throws IOException {
+    final Process process = Instances.process(dist, port, home, args);
+    this.processes.put(port, process);
+    new VerboseRunnable(new InstanceProcess(process)).run();
+  }
 
-    /**
+  /**
      * Stop a running one at this port.
      * @param port The port to stop at
      */
-    public void stop(final int port) {
-        synchronized (this.processes) {
-            final Process process = this.processes.get(port);
-            if (process == null) {
-                throw new IllegalArgumentException(
-                    String.format(
-                        "No DynamoDB Local instances running on port %d", port
-                    )
-                );
-            }
-            process.destroy();
-            this.processes.remove(port);
-        }
+  public void stop(final int port) {
+    synchronized (this.processes) {
+      final Process process = this.processes.get(port);
+      if (process == null) {
+        throw new IllegalArgumentException(String.format("No DynamoDB Local instances running on port %d", port));
+      }
+      process.destroy();
+      this.processes.remove(port);
     }
+  }
 
-    /**
+  /**
      * Shutdown everything that is still running.
      */
-    private void shutdown() {
-        for (final int port : this.processes.keySet()) {
-            this.stop(port);
-        }
+  private void shutdown() {
+    for (final int port : this.processes.keySet()) {
+      this.stop(port);
     }
+  }
 
-    /**
+  /**
      * Create new process.
      * @param dist Path to DynamoDBLocal distribution
      * @param port The port to start at
@@ -155,45 +105,35 @@ final class Instances {
      * @throws IOException If fails to start
      * @checkstyle ParameterNumber (5 lines)
      */
-    private static Process process(final File dist, final int port,
-        final File home, final List<String> args) throws IOException {
-        final List<String> command = new ArrayList<String>(args.size());
-        command.add(new File(home, "bin/java").getAbsolutePath());
-        command.add(
-            new StringBuilder("-Djava.library.path=")
-                .append(dist)
-                .append(System.getProperty("path.separator"))
-                .append(new File(dist, "DynamoDBLocal_lib"))
-                .toString()
-        );
-        command.add("-jar");
-        command.add("DynamoDBLocal.jar");
-        command.add("--port");
-        command.add(Integer.toString(port));
-        command.addAll(args);
-        return new ProcessBuilder().command(command)
-            .directory(dist).redirectErrorStream(true).start();
-    }
+  private static Process process(final File dist, final int port, final File home, final List<String> args) throws IOException {
+    final List<String> command = new ArrayList<String>(args.size());
+    command.add(new File(home, "bin/java").getAbsolutePath());
+    command.add(new StringBuilder("-Djava.library.path=").append(dist).append(System.getProperty("path.separator")).append(new File(dist, "DynamoDBLocal_lib")).toString());
+    command.add("-jar");
+    command.add("DynamoDBLocal.jar");
+    command.add("--port");
+    command.add(Integer.toString(port));
+    command.addAll(args);
+    return new ProcessBuilder().command(command).directory(dist).redirectErrorStream(true).start();
+  }
 
+  private static final class InstanceProcess implements Callable<Void> {
     /**
-     * Instance process of each local DynamoDB.
-     */
-    private static final class InstanceProcess implements Callable<Void> {
-        /**
          * Process.
          */
-        private final transient Process prc;
-        /**
+    private final transient Process prc;
+
+    /**
          * Constructor.
          * @param process The process to work with.
          */
-        InstanceProcess(final Process process) {
-            this.prc = process;
-        }
-        @Override
-        public Void call() throws Exception {
-            new VerboseProcess(this.prc).stdoutQuietly();
-            return null;
-        }
+    InstanceProcess(final Process process) {
+      this.prc = process;
     }
+
+    @Override public Void call() throws Exception {
+      new VerboseProcess(this.prc).stdoutQuietly();
+      return null;
+    }
+  }
 }
