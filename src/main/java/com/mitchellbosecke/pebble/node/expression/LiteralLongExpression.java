50266d374655984b38c89419c85114171cfaa1b4
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 07:57:29 +0000

    Mining Framework Analysis

--- src/main/java/com/mitchellbosecke/pebble/node/expression/LiteralLongExpression.java
+++ src/main/java/com/mitchellbosecke/pebble/node/expression/LiteralLongExpression.java
@@ -1,46 +1,32 @@
-/*******************************************************************************
- * This file is part of Pebble.
- *
- * Copyright (c) 2014 by Mitchell Bösecke
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- ******************************************************************************/
 package com.mitchellbosecke.pebble.node.expression;
-
 import com.mitchellbosecke.pebble.error.PebbleException;
 import com.mitchellbosecke.pebble.extension.NodeVisitor;
 import com.mitchellbosecke.pebble.template.EvaluationContextImpl;
 import com.mitchellbosecke.pebble.template.PebbleTemplateImpl;
 
 public class LiteralLongExpression implements Expression<Long> {
+  private final Long value;
 
-    private final Long value;
-    private final int lineNumber;
+  private final int lineNumber;
 
-    public LiteralLongExpression(Long value, int lineNumber) {
-        this.value = value;
-        this.lineNumber = lineNumber;
-    }
+  public LiteralLongExpression(Long value, int lineNumber) {
+    this.value = value;
+    this.lineNumber = lineNumber;
+  }
 
-    @Override
-    public void accept(NodeVisitor visitor) {
-        visitor.visit(this);
-    }
+  @Override public void accept(NodeVisitor visitor) {
+    visitor.visit(this);
+  }
 
-    @Override
-    public Long evaluate(PebbleTemplateImpl self, EvaluationContextImpl context) throws PebbleException {
-        return value;
-    }
+  @Override public Long evaluate(PebbleTemplateImpl self, EvaluationContextImpl context) throws PebbleException {
+    return value;
+  }
 
-    @Override
-    public int getLineNumber() {
-        return this.lineNumber;
-    }
+  @Override public int getLineNumber() {
+    return this.lineNumber;
+  }
 
-    @Override
-    public String toString() {
-        return value.toString();
-    }
-
+  @Override public String toString() {
+    return value.toString();
+  }
 }
