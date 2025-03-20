2f6d96658145fd040c0cb2cbc531ddcc2a9778f7
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 02:30:11 +0000

    Mining Framework Analysis

--- gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/base/mixin/IconTextMixin.java
+++ gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/base/mixin/IconTextMixin.java
@@ -1,25 +1,4 @@
 package org.gwtbootstrap3.client.ui.base.mixin;
-
-/*
- * #%L
- * GwtBootstrap3
- * %%
- * Copyright (C) 2013 GwtBootstrap3
- * %%
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * #L%
- */
-
 import com.google.gwt.user.client.ui.HasText;
 import org.gwtbootstrap3.client.ui.HasIcon;
 import org.gwtbootstrap3.client.ui.HasIconPosition;
@@ -33,167 +12,151 @@ import org.gwtbootstrap3.client.ui.constants.*;
  *
  * @author Sven Jacobs
  */
-public class IconTextMixin<T extends ComplexWidget & HasText & HasIcon & HasIconPosition> implements
-        HasText, HasIcon, HasIconPosition {
+public class IconTextMixin<T extends ComplexWidget & HasText & HasIcon & HasIconPosition> implements HasText, HasIcon, HasIconPosition {
+  private final T widget;
 
-    private final T widget;
-    private final Text text = new Text();
-    private final Text separator = new Text(" ");
-    private Icon icon;
-    private IconPosition iconPosition = IconPosition.LEFT;
-    private IconSize iconSize = IconSize.NONE;
-    private IconFlip iconFlip = IconFlip.NONE;
-    private IconRotate iconRotate = IconRotate.NONE;
-    private boolean iconMuted = false;
-    private boolean iconSpin = false;
-    private boolean iconBordered = false;
-    private boolean iconLight = false;
+  private final Text text = new Text();
 
-    public IconTextMixin(final T widget) {
-        this.widget = widget;
+  private final Text separator = new Text(" ");
+
+  private Icon icon;
+
+  private IconPosition iconPosition = IconPosition.LEFT;
+
+  private IconSize iconSize = IconSize.NONE;
+
+  private IconFlip iconFlip = IconFlip.NONE;
+
+  private IconRotate iconRotate = IconRotate.NONE;
+
+  private boolean iconMuted = false;
+
+  private boolean iconSpin = false;
+
+  private boolean iconBordered = false;
+
+  private boolean iconLight = false;
+
+  public IconTextMixin(final T widget) {
+    this.widget = widget;
+  }
+
+  public void addTextWidgetToParent() {
+    widget.add(text);
+  }
+
+  @Override public void setText(final String text) {
+    this.text.setText(text);
+  }
+
+  @Override public String getText() {
+    return text.getText();
+  }
+
+  @Override public void setIcon(final IconType iconType) {
+    render(new Icon(iconType));
+  }
+
+  @Override public IconType getIcon() {
+    return icon == null ? null : icon.getType();
+  }
+
+  @Override public void setIconPosition(final IconPosition iconPosition) {
+    this.iconPosition = iconPosition;
+    render(icon);
+  }
+
+  @Override public IconPosition getIconPosition() {
+    return iconPosition;
+  }
+
+  @Override public void setIconSize(final IconSize iconSize) {
+    this.iconSize = iconSize;
+    render(icon);
+  }
+
+  @Override public IconSize getIconSize() {
+    return iconSize;
+  }
+
+  @Override public void setIconFlip(IconFlip iconFlip) {
+    this.iconFlip = iconFlip;
+    render(icon);
+  }
+
+  @Override public IconFlip getIconFlip() {
+    return iconFlip;
+  }
+
+  @Override public void setIconRotate(IconRotate iconRotate) {
+    this.iconRotate = iconRotate;
+    render(icon);
+  }
+
+  @Override public IconRotate getIconRotate() {
+    return iconRotate;
+  }
+
+  @Override public void setIconBordered(boolean iconBordered) {
+    this.iconBordered = iconBordered;
+    render(icon);
+  }
+
+  @Override public boolean isIconBordered() {
+    return iconBordered;
+  }
+
+  @Override public void setIconMuted(boolean iconMuted) {
+    this.iconMuted = iconMuted;
+    render(icon);
+  }
+
+  @Override public boolean isIconMuted() {
+    return iconMuted;
+  }
+
+  @Override public void setIconLight(boolean iconLight) {
+    this.iconLight = iconLight;
+    render(icon);
+  }
+
+  @Override public boolean isIconLight() {
+    return iconLight;
+  }
+
+  @Override public void setIconSpin(boolean iconSpin) {
+    this.iconSpin = iconSpin;
+    render(icon);
+  }
+
+  @Override public boolean isIconSpin() {
+    return iconSpin;
+  }
+
+  private void render(final Icon newIcon) {
+    text.removeFromParent();
+    separator.removeFromParent();
+    if (icon != null) {
+      icon.removeFromParent();
     }
-
-    public void addTextWidgetToParent() {
-        widget.add(text);
+    icon = newIcon;
+    if (icon != null) {
+      icon.setSize(iconSize);
+      icon.setFlip(iconFlip);
+      icon.setRotate(iconRotate);
+      icon.setMuted(iconMuted);
+      icon.setSpin(iconSpin);
+      icon.setBorder(iconBordered);
+      icon.setLight(iconLight);
     }
-
-    @Override
-    public void setText(final String text) {
-        this.text.setText(text);
+    if (icon != null && iconPosition == IconPosition.LEFT) {
+      widget.add(icon);
+      widget.add(separator);
     }
-
-    @Override
-    public String getText() {
-        return text.getText();
+    widget.add(text);
+    if (icon != null && iconPosition == IconPosition.RIGHT) {
+      widget.add(separator);
+      widget.add(icon);
     }
-
-    @Override
-    public void setIcon(final IconType iconType) {
-        render(new Icon(iconType));
-    }
-
-    @Override
-    public IconType getIcon() {
-        return icon == null ? null : icon.getType();
-    }
-
-    @Override
-    public void setIconPosition(final IconPosition iconPosition) {
-        this.iconPosition = iconPosition;
-        render(icon);
-    }
-
-    @Override
-    public IconPosition getIconPosition() {
-        return iconPosition;
-    }
-
-    @Override
-    public void setIconSize(final IconSize iconSize) {
-        this.iconSize = iconSize;
-        render(icon);
-    }
-
-    @Override
-    public IconSize getIconSize() {
-        return iconSize;
-    }
-
-    @Override
-    public void setIconFlip(IconFlip iconFlip) {
-        this.iconFlip = iconFlip;
-        render(icon);
-    }
-
-    @Override
-    public IconFlip getIconFlip() {
-        return iconFlip;
-    }
-
-    @Override
-    public void setIconRotate(IconRotate iconRotate) {
-        this.iconRotate = iconRotate;
-        render(icon);
-    }
-
-    @Override
-    public IconRotate getIconRotate() {
-        return iconRotate;
-    }
-
-    @Override
-    public void setIconBordered(boolean iconBordered) {
-        this.iconBordered = iconBordered;
-        render(icon);
-    }
-
-    @Override
-    public boolean isIconBordered() {
-        return iconBordered;
-    }
-
-    @Override
-    public void setIconMuted(boolean iconMuted) {
-        this.iconMuted = iconMuted;
-        render(icon);
-    }
-
-    @Override
-    public boolean isIconMuted() {
-        return iconMuted;
-    }
-
-    @Override
-    public void setIconLight(boolean iconLight) {
-        this.iconLight = iconLight;
-        render(icon);
-    }
-
-    @Override
-    public boolean isIconLight() {
-        return iconLight;
-    }
-
-    @Override
-    public void setIconSpin(boolean iconSpin) {
-        this.iconSpin = iconSpin;
-        render(icon);
-    }
-
-    @Override
-    public boolean isIconSpin() {
-        return iconSpin;
-    }
-
-    private void render(final Icon newIcon) {
-        text.removeFromParent();
-        separator.removeFromParent();
-
-        if (icon != null) {
-            icon.removeFromParent();
-        }
-
-        icon = newIcon;
-        if (icon != null) {
-            icon.setSize(iconSize);
-            icon.setFlip(iconFlip);
-            icon.setRotate(iconRotate);
-            icon.setMuted(iconMuted);
-            icon.setSpin(iconSpin);
-            icon.setBorder(iconBordered);
-            icon.setLight(iconLight);
-        }
-
-        if (icon != null && iconPosition == IconPosition.LEFT) {
-            widget.add(icon);
-            widget.add(separator);
-        }
-
-        widget.add(text);
-
-        if (icon != null && iconPosition == IconPosition.RIGHT) {
-            widget.add(separator);
-            widget.add(icon);
-        }
-    }
+  }
 }
