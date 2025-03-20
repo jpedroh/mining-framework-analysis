fb02d9a71e3214b33de6b0117c32c08a2f75175c
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 04:18:12 +0000

    Mining Framework Analysis

--- src/test/java/com/jcabi/github/mock/MkRepoTest.java
+++ src/test/java/com/jcabi/github/mock/MkRepoTest.java
@@ -1,34 +1,4 @@
-/**
- * Copyright (c) 2012-2013, JCabi.com
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met: 1) Redistributions of source code must retain the above
- * copyright notice, this list of conditions and the following
- * disclaimer. 2) Redistributions in binary form must reproduce the above
- * copyright notice, this list of conditions and the following
- * disclaimer in the documentation and/or other materials provided
- * with the distribution. 3) Neither the name of the jcabi.com nor
- * the names of its contributors may be used to endorse or promote
- * products derived from this software without specific prior written
- * permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
- * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
- * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
- * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- * OF THE POSSIBILITY OF SUCH DAMAGE.
- */
 package com.jcabi.github.mock;
-
 import com.jcabi.github.Milestones;
 import com.jcabi.github.Coordinates;
 import com.jcabi.github.Repo;
@@ -46,53 +16,35 @@ import org.junit.Test;
  * @checkstyle MultipleStringLiterals (500 lines)
  */
 public final class MkRepoTest {
-
-    /**
+  /**
      * Repo can work.
      * @throws Exception If some problem inside
      */
-    @Test
-    public void works() throws Exception {
-        final Repos repos = new MkRepos(new MkStorage.InFile(), "jeff");
-        final Repo repo = repos.create(
-            Json.createObjectBuilder().add("name", "test").build()
-        );
-        MatcherAssert.assertThat(
-            repo.coordinates(),
-            Matchers.hasToString("jeff/test")
-        );
-    }
+  @Test public void works() throws Exception {
+    final Repos repos = new MkRepos(new MkStorage.InFile(), "jeff");
+    final Repo repo = repos.create(Json.createObjectBuilder().add("name", "test").build());
+    MatcherAssert.assertThat(repo.coordinates(), Matchers.hasToString("jeff/test"));
+  }
 
-    /**
+  /**
      * This tests that the milestones() method in MkRepo is working fine.
      * @throws Exception - if anything goes wrong.
      */
-    @Test
-    public void returnsMkMilestones() throws Exception {
-        final Repos repos = new MkRepos(new MkStorage.InFile(), "jeff");
-        final Repo repo = repos.create(
-            Json.createObjectBuilder().add("name", "test1").build()
-        );
-        final Milestones milestones = repo.milestones();
-        MatcherAssert.assertThat(
-            milestones,
-            Matchers.notNullValue()
-        );
-    }
+  @Test public void returnsMkMilestones() throws Exception {
+    final Repos repos = new MkRepos(new MkStorage.InFile(), "jeff");
+    final Repo repo = repos.create(Json.createObjectBuilder().add("name", "test1").build());
+    final Milestones milestones = repo.milestones();
+    MatcherAssert.assertThat(milestones, Matchers.notNullValue());
+  }
 
-    /**    
+  /**
      * Repo can fetch its commits.
      *
      * @throws IOException if some problem inside
      */
-    @Test
-    public void fetchCommits() throws IOException {
-        final String user = "testuser";
-        final Repo repo = new MkRepo(
-            new MkStorage.InFile(),
-            user,
-            new Coordinates.Simple(user, "testrepo")
-        );
-        MatcherAssert.assertThat(repo.commits(), Matchers.notNullValue());
-    }
+  @Test public void fetchCommits() throws IOException {
+    final String user = "testuser";
+    final Repo repo = new MkRepo(new MkStorage.InFile(), user, new Coordinates.Simple(user, "testrepo"));
+    MatcherAssert.assertThat(repo.commits(), Matchers.notNullValue());
+  }
 }
