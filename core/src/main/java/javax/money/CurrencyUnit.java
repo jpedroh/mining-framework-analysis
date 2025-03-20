e181fc7e0c4d70b7c4a0f916f911bb61b551cef0
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 05:09:49 +0000

    Mining Framework Analysis

--- core/src/main/java/javax/money/CurrencyUnit.java
+++ core/src/main/java/javax/money/CurrencyUnit.java
@@ -1,4 +1,37 @@
 /*
+<<<<<<< /usr/src/app/output/javamoney/jsr354-api/ca9413613c8a0fa9d883c20d60ce03de38dd0344/core/src/main/java/javax/money/CurrencyUnit.java/left.java
+ * Copyright (c) 2012, Credit Suisse (Anatole Tresch), Werner Keil
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ *  * Neither the name of JSR-310 nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+||||||| /usr/src/app/output/javamoney/jsr354-api/ca9413613c8a0fa9d883c20d60ce03de38dd0344/core/src/main/java/javax/money/CurrencyUnit.java/base.java
+ * JSR 354: Money and Currency API
+=======
  * Copyright (c) 2012-2013, Credit Suisse
  *
  * All rights reserved.
@@ -28,12 +61,14 @@
  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+>>>>>>> /usr/src/app/output/javamoney/jsr354-api/ca9413613c8a0fa9d883c20d60ce03de38dd0344/core/src/main/java/javax/money/CurrencyUnit.java/right.java
  */
 package javax.money;
 
 import java.util.Currency;
 import java.util.ServiceLoader;
 
+import javax.money.spi.CurrencyUnitProvider;
 
 /**
  * * A unit of currency.
--- core/src/main/java/javax/money/UnknownCurrencyException.java
+++ core/src/main/java/javax/money/UnknownCurrencyException.java
@@ -1,10 +1,13 @@
 /*
-<<<<<<< HEAD
+<<<<<<< /usr/src/app/output/javamoney/jsr354-api/ca9413613c8a0fa9d883c20d60ce03de38dd0344/core/src/main/java/javax/money/UnknownCurrencyException.java/left.java
  * Copyright (c) 2012, Credit Suisse (Anatole Tresch), Werner Keil
+||||||| /usr/src/app/output/javamoney/jsr354-api/ca9413613c8a0fa9d883c20d60ce03de38dd0344/core/src/main/java/javax/money/UnknownCurrencyException.java/base.java
+ *  Copyright 2009-2012 Werner Keil
 =======
  * Copyright (c) 2012-2013, Credit Suisse
->>>>>>> a63812e1178535e10625662ed6299a6669e1d3fa
+>>>>>>> /usr/src/app/output/javamoney/jsr354-api/ca9413613c8a0fa9d883c20d60ce03de38dd0344/core/src/main/java/javax/money/UnknownCurrencyException.java/right.java
  *
+<<<<<<< /usr/src/app/output/javamoney/jsr354-api/ca9413613c8a0fa9d883c20d60ce03de38dd0344/core/src/main/java/javax/money/UnknownCurrencyException.java/left.java
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -17,11 +20,7 @@
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
  *
-<<<<<<< HEAD
  *  * Neither the name of JSR-310 nor the names of its contributors
-=======
- *  * Neither the name of JSR-354 nor the names of its contributors
->>>>>>> a63812e1178535e10625662ed6299a6669e1d3fa
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
  *
@@ -36,6 +35,36 @@
  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+||||||| /usr/src/app/output/javamoney/jsr354-api/ca9413613c8a0fa9d883c20d60ce03de38dd0344/core/src/main/java/javax/money/UnknownCurrencyException.java/base.java
+=======
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ *  * Neither the name of JSR-354 nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+>>>>>>> /usr/src/app/output/javamoney/jsr354-api/ca9413613c8a0fa9d883c20d60ce03de38dd0344/core/src/main/java/javax/money/UnknownCurrencyException.java/right.java
  */
 package javax.money;
 
