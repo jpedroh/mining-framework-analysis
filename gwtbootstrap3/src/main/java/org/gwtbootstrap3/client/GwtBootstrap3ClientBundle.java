0bedd260ecac5b4a32563e3b7c119cc80f2ec91a
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 02:08:57 +0000

    Mining Framework Analysis

--- gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/GwtBootstrap3ClientBundle.java
+++ gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/GwtBootstrap3ClientBundle.java
@@ -32,7 +32,13 @@ public interface GwtBootstrap3ClientBundle extends ClientBundle {
 
     static final GwtBootstrap3ClientBundle INSTANCE = GWT.create(GwtBootstrap3ClientBundle.class);
 
+<<<<<<< /usr/src/app/output/gwtbootstrap3/gwtbootstrap3/3f6b71884670971b10f90b5ce25daedc6dad12ea/gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/GwtBootstrap3ClientBundle.java/left.java
     @Source("resource/js/jquery-1.11.2.min.js")
+||||||| /usr/src/app/output/gwtbootstrap3/gwtbootstrap3/3f6b71884670971b10f90b5ce25daedc6dad12ea/gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/GwtBootstrap3ClientBundle.java/base.java
+    @Source("resource/js/jquery-1.11.0.min.js")
+=======
+    @Source("resource/js/jquery-1.11.0.min.cache.js")
+>>>>>>> /usr/src/app/output/gwtbootstrap3/gwtbootstrap3/3f6b71884670971b10f90b5ce25daedc6dad12ea/gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/GwtBootstrap3ClientBundle.java/right.java
     TextResource jQuery();
 
     @Source("resource/js/bootstrap-3.3.1.min.cache.js")
