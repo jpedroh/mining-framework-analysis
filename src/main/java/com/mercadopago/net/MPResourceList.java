a5b2c9848434abf2ed3873e14e70dbc4b4c2c5b4
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 23:54:24 +0000

    Mining Framework Analysis

--- src/main/java/com/mercadopago/net/MPResourceList.java
+++ src/main/java/com/mercadopago/net/MPResourceList.java
@@ -1,17 +1,1 @@
-package com.mercadopago.net;
-
-import java.util.List;
-import lombok.Getter;
-import lombok.Setter;
-
-/**
- * List of resources returned by an API.
- *
- * @param <T> Type of resource being returned
- */
-@Getter
-@Setter
-public class MPResourceList<T> extends MPResource {
-  /** List of results. */
-  private List<T> results;
-}
+  package   com . mercadopago . net ;   import   java . util . List ;  import  lombok . Getter ;  import  lombok . Setter ;    @ Getter  @ Setter public class MPResourceList  <  T >  extends MPResource  {   private  List  < T >  results ; }
