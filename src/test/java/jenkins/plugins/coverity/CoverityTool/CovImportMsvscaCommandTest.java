98a59e9d2ebe90d614ba29a870811fcd4b945252
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 22:21:57 +0000

    Mining Framework Analysis

--- src/test/java/jenkins/plugins/coverity/CoverityTool/CovImportMsvscaCommandTest.java
+++ src/test/java/jenkins/plugins/coverity/CoverityTool/CovImportMsvscaCommandTest.java
@@ -1,62 +1,52 @@
-/*******************************************************************************
- * Copyright (c) 2017 Synopsys, Inc
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *    Synopsys, Inc - initial implementation and documentation
- *******************************************************************************/
 package jenkins.plugins.coverity.CoverityTool;
-
 import jenkins.plugins.coverity.CoverityPublisher;
 import jenkins.plugins.coverity.InvocationAssistance;
 import jnr.ffi.annotations.In;
 import org.apache.commons.lang.StringUtils;
 import org.junit.Test;
-
 import java.io.File;
 import java.io.IOException;
 
 public class CovImportMsvscaCommandTest extends CommandTestBase {
+  @Test public void addMsvscaOutputFilesTest() throws IOException, InterruptedException {
+    File analysisLog1 = new File("CodeAnalysisLog1.xml");
+    File analysisLog2 = new File("CodeAnalysisLog2.xml");
+    File[] outputFiles = new File[] { analysisLog1, analysisLog2 };
+    InvocationAssistance invocationAssistance = new InvocationAssistance(false, StringUtils.EMPTY, false, StringUtils.EMPTY, false, false, StringUtils.EMPTY, StringUtils.EMPTY, StringUtils.EMPTY, StringUtils.EMPTY, false, StringUtils.EMPTY, StringUtils.EMPTY, null, false, true, StringUtils.EMPTY, StringUtils.EMPTY, null, false);
+    CoverityPublisher publisher = new CoverityPublisher(null, invocationAssistance, false, false, false, false, false, StringUtils.EMPTY, StringUtils.EMPTY, StringUtils.EMPTY, null, null, null);
+    ICommand covImportMsvscaCommand = new CovImportMsvscaCommand(build, launcher, listener, publisher, StringUtils.EMPTY, envVars, outputFiles);
+    setExpectedArguments(new String[] { "cov-import-msvsca", "--dir", "TestDir", "--append", analysisLog1.getAbsolutePath(), analysisLog2.getAbsolutePath() });
+    covImportMsvscaCommand.runCommand();
+    consoleLogger.verifyLastMessage("[Coverity] cov-import-msvsca command line arguments: " + actualArguments.toString());
+  }
 
-    @Test
-    public void addMsvscaOutputFilesTest() throws IOException, InterruptedException {
 
-        File analysisLog1 = new File("CodeAnalysisLog1.xml");
-        File analysisLog2 = new File("CodeAnalysisLog2.xml");
-        File[] outputFiles = new File[] {analysisLog1, analysisLog2};
+<<<<<<< Unknown file: This is a bug in JDime.
+=======
+  @Test public void CovImportMsvscaCommand_AddMsvscaOutputFilesTest() {
+    mocker.replay();
+    File analysisLog1 = new File("CodeAnalysisLog1.xml");
+    File analysisLog2 = new File("CodeAnalysisLog2.xml");
+    File[] outputFiles = new File[] { analysisLog1, analysisLog2 };
+    CoverityPublisher publisher = new CoverityPublisher(null, null, false, false, false, false, false, null, null);
+    CovCommand covImportMsvscaCommand = new CovImportMsvscaCommand(build, launcher, listener, publisher, StringUtils.EMPTY, envVars, outputFiles);
+    List<String> covImportMsvscaArguments = covImportMsvscaCommand.getCommandLines();
+    assertEquals(6, covImportMsvscaArguments.size());
+    checkCommandLineArg(covImportMsvscaArguments, "cov-import-msvsca");
+    checkCommandLineArg(covImportMsvscaArguments, "--dir");
+    checkCommandLineArg(covImportMsvscaArguments, "TestDir");
+    checkCommandLineArg(covImportMsvscaArguments, "--append");
+    checkCommandLineArg(covImportMsvscaArguments, analysisLog1.getAbsolutePath());
+    checkCommandLineArg(covImportMsvscaArguments, analysisLog2.getAbsolutePath());
+    assertEquals(0, covImportMsvscaArguments.size());
+  }
+>>>>>>> /usr/src/app/output/jenkinsci/coverity-plugin/5846a9f06a151725dff014e3a580a9349a20e8ec/src/test/java/jenkins/plugins/coverity/CoverityTool/CovImportMsvscaCommandTest.java/right.java
 
-        InvocationAssistance invocationAssistance = new InvocationAssistance(
-                false, StringUtils.EMPTY, false, StringUtils.EMPTY, false, false,
-                StringUtils.EMPTY, StringUtils.EMPTY, StringUtils.EMPTY, StringUtils.EMPTY,
-                false, StringUtils.EMPTY, StringUtils.EMPTY, null, false, true,
-                StringUtils.EMPTY, StringUtils.EMPTY, null, false
-        );
 
-        CoverityPublisher publisher = new CoverityPublisher(
-                null, invocationAssistance, false, false, false, false, false,
-                null, null
-        );
-
-        ICommand covImportMsvscaCommand = new CovImportMsvscaCommand(build, launcher, listener, publisher, StringUtils.EMPTY, envVars, outputFiles);
-        setExpectedArguments(new String[] {
-                "cov-import-msvsca", "--dir", "TestDir", "--append", analysisLog1.getAbsolutePath(), analysisLog2.getAbsolutePath()
-        });
-        covImportMsvscaCommand.runCommand();
-        consoleLogger.verifyLastMessage("[Coverity] cov-import-msvsca command line arguments: " + actualArguments.toString());
-    }
-
-    @Test
-    public void cannotExecuteTest() throws IOException, InterruptedException {
-        CoverityPublisher publisher = new CoverityPublisher(
-                null, null, false, false, false, false, false,
-                null, null
-        );
-
-        ICommand covImportMsvscaCommand = new CovImportMsvscaCommand(build, launcher, listener, publisher, StringUtils.EMPTY, envVars, null);
-        covImportMsvscaCommand.runCommand();
-        consoleLogger.verifyLastMessage("[Coverity] Skipping command because it can't be executed");
-    }
+  @Test public void cannotExecuteTest() throws IOException, InterruptedException {
+    CoverityPublisher publisher = new CoverityPublisher(null, null, false, false, false, false, false, StringUtils.EMPTY, StringUtils.EMPTY, StringUtils.EMPTY, null, null, null);
+    ICommand covImportMsvscaCommand = new CovImportMsvscaCommand(build, launcher, listener, publisher, StringUtils.EMPTY, envVars, null);
+    covImportMsvscaCommand.runCommand();
+    consoleLogger.verifyLastMessage("[Coverity] Skipping command because it can\'t be executed");
+  }
 }
