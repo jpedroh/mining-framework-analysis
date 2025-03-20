91ad1af988b2ec2d2695dcc2a96d286c02554d7b
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 15:18:54 +0000

    Mining Framework Analysis

--- src/main/java/com/fasterxml/jackson/datatype/jsr310/JSR310Module.java
+++ src/main/java/com/fasterxml/jackson/datatype/jsr310/JSR310Module.java
@@ -1,21 +1,4 @@
-/*
- * Copyright 2013 FasterXML.com
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may
- * not use this file except in compliance with the License. You may obtain
- * a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the license for the specific language governing permissions and
- * limitations under the license.
- */
-
 package com.fasterxml.jackson.datatype.jsr310;
-
 import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDate;
@@ -30,7 +13,6 @@ import java.time.YearMonth;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
-
 import com.fasterxml.jackson.core.Version;
 import com.fasterxml.jackson.databind.BeanDescription;
 import com.fasterxml.jackson.databind.DeserializationConfig;
@@ -124,127 +106,97 @@ import com.fasterxml.jackson.datatype.jsr310.ser.key.ZonedDateTimeKeySerializer;
  * @since 2.2.0
  * @see com.fasterxml.jackson.datatype.jsr310.ser.key.Jsr310NullKeySerializer
  */
-public final class JSR310Module extends SimpleModule
-{
-    private static final long serialVersionUID = 1L;
+public final class JSR310Module extends SimpleModule {
+  private static final long serialVersionUID = 1L;
 
-    public JSR310Module()
-    {
-        super(Version.unknownVersion()); // !!! TEST
-//        super(PackageVersion.VERSION);
+  public JSR310Module() {
+    super(Version.unknownVersion());
+    addDeserializer(Duration.class, DurationDeserializer.INSTANCE);
+    addDeserializer(Instant.class, InstantDeserializer.INSTANT);
+    addDeserializer(LocalDateTime.class, LocalDateTimeDeserializer.INSTANCE);
+    addDeserializer(LocalDate.class, LocalDateDeserializer.INSTANCE);
+    addDeserializer(LocalTime.class, LocalTimeDeserializer.INSTANCE);
+    addDeserializer(MonthDay.class, JSR310StringParsableDeserializer.MONTH_DAY);
+    addDeserializer(OffsetDateTime.class, InstantDeserializer.OFFSET_DATE_TIME);
+    addDeserializer(OffsetTime.class, OffsetTimeDeserializer.INSTANCE);
+    addDeserializer(Period.class, JSR310StringParsableDeserializer.PERIOD);
+    addDeserializer(Year.class, YearDeserializer.INSTANCE);
+    addDeserializer(YearMonth.class, YearMonthDeserializer.INSTANCE);
+    addDeserializer(ZonedDateTime.class, InstantDeserializer.ZONED_DATE_TIME);
+    addDeserializer(ZoneId.class, JSR310StringParsableDeserializer.ZONE_ID);
+    addDeserializer(ZoneOffset.class, JSR310StringParsableDeserializer.ZONE_OFFSET);
+    addSerializer(Duration.class, DurationSerializer.INSTANCE);
+    addSerializer(Instant.class, InstantSerializer.INSTANCE);
+    addSerializer(LocalDateTime.class, LocalDateTimeSerializer.INSTANCE);
+    addSerializer(LocalDate.class, LocalDateSerializer.INSTANCE);
+    addSerializer(LocalTime.class, LocalTimeSerializer.INSTANCE);
+    addSerializer(MonthDay.class, new ToStringSerializer(MonthDay.class));
+    addSerializer(OffsetDateTime.class, OffsetDateTimeSerializer.INSTANCE);
+    addSerializer(OffsetTime.class, OffsetTimeSerializer.INSTANCE);
+    addSerializer(Period.class, new ToStringSerializer(Period.class));
+    addSerializer(Year.class, YearSerializer.INSTANCE);
+    addSerializer(YearMonth.class, YearMonthSerializer.INSTANCE);
+    addSerializer(ZonedDateTime.class, ZonedDateTimeSerializer.INSTANCE);
+    addSerializer(ZoneId.class, new ToStringSerializer(ZoneId.class));
+    addSerializer(ZoneOffset.class, new ToStringSerializer(ZoneOffset.class));
+    addKeySerializer(ZonedDateTime.class, ZonedDateTimeKeySerializer.INSTANCE);
+    addKeyDeserializer(Duration.class, DurationKeyDeserializer.INSTANCE);
+    addKeyDeserializer(Instant.class, InstantKeyDeserializer.INSTANCE);
+    addKeyDeserializer(LocalDateTime.class, LocalDateTimeKeyDeserializer.INSTANCE);
+    addKeyDeserializer(LocalDate.class, LocalDateKeyDeserializer.INSTANCE);
+    addKeyDeserializer(LocalTime.class, LocalTimeKeyDeserializer.INSTANCE);
+    addKeyDeserializer(MonthDay.class, MonthDayKeyDeserializer.INSTANCE);
+    addKeyDeserializer(OffsetDateTime.class, OffsetDateTimeKeyDeserializer.INSTANCE);
+    addKeyDeserializer(OffsetTime.class, OffsetTimeKeyDeserializer.INSTANCE);
+    addKeyDeserializer(Period.class, PeriodKeyDeserializer.INSTANCE);
+    addKeyDeserializer(Year.class, YearKeyDeserializer.INSTANCE);
+    addKeyDeserializer(YearMonth.class, YearMothKeyDeserializer.INSTANCE);
+    addKeyDeserializer(ZonedDateTime.class, ZonedDateTimeKeyDeserializer.INSTANCE);
+    addKeyDeserializer(ZoneId.class, ZoneIdKeyDeserializer.INSTANCE);
+    addKeyDeserializer(ZoneOffset.class, ZoneOffsetKeyDeserializer.INSTANCE);
+  }
 
-        // first deserializers
-        addDeserializer(Duration.class, DurationDeserializer.INSTANCE);
-        addDeserializer(Instant.class, InstantDeserializer.INSTANT);
-        addDeserializer(LocalDateTime.class, LocalDateTimeDeserializer.INSTANCE);
-        addDeserializer(LocalDate.class, LocalDateDeserializer.INSTANCE);
-        addDeserializer(LocalTime.class, LocalTimeDeserializer.INSTANCE);
-        addDeserializer(MonthDay.class, JSR310StringParsableDeserializer.MONTH_DAY);
-        addDeserializer(OffsetDateTime.class, InstantDeserializer.OFFSET_DATE_TIME);
-        addDeserializer(OffsetTime.class, OffsetTimeDeserializer.INSTANCE);
-        addDeserializer(Period.class, JSR310StringParsableDeserializer.PERIOD);
-        addDeserializer(Year.class, YearDeserializer.INSTANCE);
-        addDeserializer(YearMonth.class, YearMonthDeserializer.INSTANCE);
-        addDeserializer(ZonedDateTime.class, InstantDeserializer.ZONED_DATE_TIME);
-        addDeserializer(ZoneId.class, JSR310StringParsableDeserializer.ZONE_ID);
-        addDeserializer(ZoneOffset.class, JSR310StringParsableDeserializer.ZONE_OFFSET);
-
-        // then serializers:
-        addSerializer(Duration.class, DurationSerializer.INSTANCE);
-        addSerializer(Instant.class, InstantSerializer.INSTANCE);
-        addSerializer(LocalDateTime.class, LocalDateTimeSerializer.INSTANCE);
-        addSerializer(LocalDate.class, LocalDateSerializer.INSTANCE);
-        addSerializer(LocalTime.class, LocalTimeSerializer.INSTANCE);
-        addSerializer(MonthDay.class, new ToStringSerializer(MonthDay.class));
-        addSerializer(OffsetDateTime.class, OffsetDateTimeSerializer.INSTANCE);
-        addSerializer(OffsetTime.class, OffsetTimeSerializer.INSTANCE);
-        addSerializer(Period.class, new ToStringSerializer(Period.class));
-        addSerializer(Year.class, YearSerializer.INSTANCE);
-        addSerializer(YearMonth.class, YearMonthSerializer.INSTANCE);
-        addSerializer(ZonedDateTime.class, ZonedDateTimeSerializer.INSTANCE);
-        // note: actual concrete type is `ZoneRegion`, but that's not visible:
-        addSerializer(ZoneId.class, new ToStringSerializer(ZoneId.class));
-        
-        addSerializer(ZoneOffset.class, new ToStringSerializer(ZoneOffset.class));
-
-        // key serializers
-        addKeySerializer(ZonedDateTime.class, ZonedDateTimeKeySerializer.INSTANCE);
-
-        // key deserializers
-        addKeyDeserializer(Duration.class, DurationKeyDeserializer.INSTANCE);
-        addKeyDeserializer(Instant.class, InstantKeyDeserializer.INSTANCE);
-        addKeyDeserializer(LocalDateTime.class, LocalDateTimeKeyDeserializer.INSTANCE);
-        addKeyDeserializer(LocalDate.class, LocalDateKeyDeserializer.INSTANCE);
-        addKeyDeserializer(LocalTime.class, LocalTimeKeyDeserializer.INSTANCE);
-        addKeyDeserializer(MonthDay.class, MonthDayKeyDeserializer.INSTANCE);
-        addKeyDeserializer(OffsetDateTime.class, OffsetDateTimeKeyDeserializer.INSTANCE);
-        addKeyDeserializer(OffsetTime.class, OffsetTimeKeyDeserializer.INSTANCE);
-        addKeyDeserializer(Period.class, PeriodKeyDeserializer.INSTANCE);
-        addKeyDeserializer(Year.class, YearKeyDeserializer.INSTANCE);
-        addKeyDeserializer(YearMonth.class, YearMothKeyDeserializer.INSTANCE);
-        addKeyDeserializer(ZonedDateTime.class, ZonedDateTimeKeyDeserializer.INSTANCE);
-        addKeyDeserializer(ZoneId.class, ZoneIdKeyDeserializer.INSTANCE);
-        addKeyDeserializer(ZoneOffset.class, ZoneOffsetKeyDeserializer.INSTANCE);
-    }
-
-    @Override
-    public void setupModule(SetupContext context) {
-        super.setupModule(context);
-        context.addValueInstantiators(new ValueInstantiators.Base() {
-            @Override
-            public ValueInstantiator findValueInstantiator(DeserializationConfig config,
-                    BeanDescription beanDesc, ValueInstantiator defaultInstantiator)
-            {
-                Class<?> raw = beanDesc.getBeanClass();
-                // 15-May-2015, tatu: In theory not safe, but in practice we do need to do "fuzzy" matching
-                //    because we will (for now) be getting a subtype, but in future may want to downgrade
-                //    to the common base type. Even more, serializer may purposefully force use of base type.
-                //    So... in practice it really should always work, in the end. :)
-                if (ZoneId.class.isAssignableFrom(raw)) {
-                    // let's assume we should be getting "empty" StdValueInstantiator here:
-                    if (defaultInstantiator instanceof StdValueInstantiator) {
-                        StdValueInstantiator inst = (StdValueInstantiator) defaultInstantiator;
-                        // one further complication: we need ZoneId info, not sub-class
-                        AnnotatedClass ac;
-                        if (raw == ZoneId.class) {
-                            ac = beanDesc.getClassInfo();
-                        } else {
-                            // we don't need Annotations, so constructing directly is fine here
-                            // even if it's not generally recommended
-                            ac = AnnotatedClass.construct(ZoneId.class, null, null);
-                        }
-                        if (!inst.canCreateFromString()) {
-                            AnnotatedMethod factory = _findFactory(ac, "of", String.class);
-                            if (factory != null) {
-                                inst.configureFromStringCreator(factory);
-                            }
-                            // otherwise... should we indicate an error?
-                        }
-                        //return ZoneIdInstantiator.construct(config, beanDesc, defaultInstantiator);
-                    }
-                }
-                return defaultInstantiator;
+  @Override public void setupModule(SetupContext context) {
+    super.setupModule(context);
+    context.addValueInstantiators(new ValueInstantiators.Base() {
+      @Override public ValueInstantiator findValueInstantiator(DeserializationConfig config, BeanDescription beanDesc, ValueInstantiator defaultInstantiator) {
+        Class<?> raw = beanDesc.getBeanClass();
+        if (ZoneId.class.isAssignableFrom(raw)) {
+          if (defaultInstantiator instanceof StdValueInstantiator) {
+            StdValueInstantiator inst = (StdValueInstantiator) defaultInstantiator;
+            AnnotatedClass ac;
+            if (raw == ZoneId.class) {
+              ac = beanDesc.getClassInfo();
+            } else {
+              ac = AnnotatedClass.construct(ZoneId.class, null, null);
             }
-        });
-    }
-
-    // For 
-    protected AnnotatedMethod _findFactory(AnnotatedClass cls, String name, Class<?>... argTypes)
-    {
-        final int argCount = argTypes.length;
-        for (AnnotatedMethod method : cls.getStaticMethods()) {
-            if (!name.equals(method.getName())
-                || (method.getParameterCount() != argCount)) {
-                continue;
+            if (!inst.canCreateFromString()) {
+              AnnotatedMethod factory = _findFactory(ac, "of", String.class);
+              if (factory != null) {
+                inst.configureFromStringCreator(factory);
+              }
             }
-            for (int i = 0; i < argCount; ++i) {
-                Class<?> argType = method.getParameter(i).getRawType();
-                if (!argType.isAssignableFrom(argTypes[i])) {
-                    continue;
-                }
-            }
-            return method;
+          }
         }
-        return null;
+        return defaultInstantiator;
+      }
+    });
+  }
+
+  protected AnnotatedMethod _findFactory(AnnotatedClass cls, String name, Class<?>... argTypes) {
+    final int argCount = argTypes.length;
+    for (AnnotatedMethod method : cls.getStaticMethods()) {
+      if (!name.equals(method.getName()) || (method.getParameterCount() != argCount)) {
+        continue;
+      }
+      for (int i = 0; i < argCount; ++i) {
+        Class<?> argType = method.getParameter(i).getRawType();
+        if (!argType.isAssignableFrom(argTypes[i])) {
+          continue;
+        }
+      }
+      return method;
     }
+    return null;
+  }
 }
