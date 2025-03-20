86aa82daa0a5f976a7ed30b39d01bec1c433ccf6
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 00:10:37 +0000

    Mining Framework Analysis

--- restygwt/src/main/java/org/fusesource/restygwt/rebind/util/AnnotationCopyUtil.java
+++ restygwt/src/main/java/org/fusesource/restygwt/rebind/util/AnnotationCopyUtil.java
@@ -1,22 +1,4 @@
-/**
- * Copyright (C) 2009-2011 the original author or authors.
- * See the notice.md file distributed with this work for additional
- * information regarding copyright ownership.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.fusesource.restygwt.rebind.util;
-
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Array;
 import java.lang.reflect.Method;
@@ -27,98 +9,96 @@ import java.lang.reflect.Method;
  * @author <a href="mailto:bogdan.mustiata@gmail.com">Bogdan Mustiata</a>
  */
 public class AnnotationCopyUtil {
-    public static String getAnnotationAsString(Annotation annotation) {
-        StringBuilder result = encodeAnnotationName(annotation);
-
-        if (hasAnnotationAttributes(annotation)) {
-            encodeAnnotationAttributes(annotation, result);
-        }
-
-        return result.toString();
+  public static String getAnnotationAsString(Annotation annotation) {
+    StringBuilder result = encodeAnnotationName(annotation);
+    if (hasAnnotationAttributes(annotation)) {
+      encodeAnnotationAttributes(annotation, result);
     }
+    return result.toString();
+  }
 
-    private static StringBuilder encodeAnnotationName(Annotation annotation) {
-        return new StringBuilder( "@" )
-                .append(annotation.annotationType().getCanonicalName());
+  private static StringBuilder encodeAnnotationName(Annotation annotation) {
+    return new StringBuilder("@").append(annotation.annotationType().getCanonicalName());
+  }
+
+  private static boolean hasAnnotationAttributes(Annotation annotation) {
+    return annotation.annotationType().getDeclaredMethods().length != 0;
+  }
+
+  private static void encodeAnnotationAttributes(Annotation annotation, StringBuilder result) {
+    result.append("(");
+    OnceFirstIterator<String> comma = new OnceFirstIterator<String>("", ", ");
+    for (Method method : annotation.annotationType().getDeclaredMethods()) {
+      Object value = readAnnotationAttribute(annotation, method);
+      result.append(comma.next()).append(method.getName()).append(" = ").append(encodeAnnotationValue(value));
     }
+    result.append(")");
+  }
 
-    private static boolean hasAnnotationAttributes(Annotation annotation) {
-        return annotation.annotationType().getDeclaredMethods().length != 0;
+  private static Object readAnnotationAttribute(Annotation annotation, Method annotationAttribute) {
+    try {
+      return annotationAttribute.invoke(annotation);
+    } catch (Exception e) {
+      throw new IllegalArgumentException("Unable to read attribute " + annotationAttribute + " from " + annotation, e);
     }
+  }
 
-    private static void encodeAnnotationAttributes(Annotation annotation, StringBuilder result) {
-        result.append("(");
-
-        OnceFirstIterator<String> comma = new OnceFirstIterator<String>("", ", ");
-        for (Method method : annotation.annotationType().getDeclaredMethods()) {
-            Object value = readAnnotationAttribute(annotation, method);
-
-            result.append( comma.next() )
-                  .append(method.getName())
-                  .append( " = " )
-                  .append( encodeAnnotationValue(value) );
-        }
-
-        result.append(")");
-    }
-
-    private static Object readAnnotationAttribute(Annotation annotation, Method annotationAttribute) {
-        try {
-            return annotationAttribute.invoke(annotation);
-        } catch (Exception e) {
-            throw new IllegalArgumentException("Unable to read attribute " + annotationAttribute + " from " + annotation, e);
-        }
-    }
-
-    private static String encodeAnnotationValue(Object value) {
-        if (value instanceof String) {
-            return readStringValue(value);
-        } else if (value instanceof Number) {
-            return readNumberValue(value);
-        } else if (value == null) {
-            return "null";
-        } else if (value.getClass().isArray()) {
+  private static String encodeAnnotationValue(Object value) {
+    if (value instanceof String) {
+      return readStringValue(value);
+    } else {
+      if (value instanceof Number) {
+        return readNumberValue(value);
+      } else {
+        if (value == null) {
+          return "null";
+        } else {
+          if (value.getClass().isArray()) {
             return readArrayValue(value);
-        } else if (value instanceof Annotation) {
-            return getAnnotationAsString((Annotation) value);
-        } else if (value instanceof Boolean) {
-            return readBooleanValue((Boolean) value);
-        } else if (value instanceof Class) {
-            return readClassValue((Class) value);
+          } else {
+            if (value instanceof Annotation) {
+              return getAnnotationAsString((Annotation) value);
+            } else {
+              if (value instanceof Boolean) {
+                return readBooleanValue((Boolean) value);
+              } else {
+                if (value instanceof Class) {
+                  return readClassValue((Class) value);
+                }
+              }
+            }
+          }
         }
-
-        throw new RuntimeException("Unsupported value for encodeAnnotationValue: " + value);
+      }
     }
+    throw new RuntimeException("Unsupported value for encodeAnnotationValue: " + value);
+  }
 
-    private static String readBooleanValue(Boolean value) {
-        return Boolean.toString(value);
+  private static String readBooleanValue(Boolean value) {
+    return Boolean.toString(value);
+  }
+
+  private static String readArrayValue(Object value) {
+    StringBuilder result = new StringBuilder();
+    OnceFirstIterator<String> comma = new OnceFirstIterator<String>("", ", ");
+    result.append("{");
+    for (int i = 0; i < Array.getLength(value); i++) {
+      Object arrayValue = Array.get(value, i);
+      result.append(comma.next()).append(encodeAnnotationValue(arrayValue));
     }
+    result.append("}");
+    return result.toString();
+  }
 
-    private static String readArrayValue(Object value) {
-        StringBuilder result = new StringBuilder();
-        OnceFirstIterator<String> comma = new OnceFirstIterator<String>("", ", ");
+  private static String readNumberValue(Object value) {
+    return value.toString();
+  }
 
-        result.append("{");
-        for (int i = 0; i < Array.getLength(value); i++) {
-            Object arrayValue = Array.get(value, i);
+  private static String readStringValue(Object value) {
+    return "\"" + value.toString().replace("\"", "\\\"").replace("\n", "\\n") + "\"";
+  }
 
-            result.append(comma.next())
-                  .append(encodeAnnotationValue(arrayValue));
-        }
-        result.append("}");
-
-        return result.toString();
-    }
-
-    private static String readNumberValue(Object value) {
-        return value.toString();
-    }
-
-    private static String readStringValue(Object value) {
-        return "\"" + value.toString().replace("\"", "\\\"").replace("\n", "\\n") + "\"";
-    }
-
-    private static String readClassValue(Class value) {
-        return value.getCanonicalName() + ".class";
-    }
+  private static String readClassValue(Class value) {
+    return value.getCanonicalName() + ".class";
+  }
 }
