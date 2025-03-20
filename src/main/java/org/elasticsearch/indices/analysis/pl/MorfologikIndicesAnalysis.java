024e285f90e3383cdce320c5a8d6efeea4719f0a
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 01:37:15 +0000

    Mining Framework Analysis

--- src/main/java/org/elasticsearch/indices/analysis/pl/MorfologikIndicesAnalysis.java
+++ src/main/java/org/elasticsearch/indices/analysis/pl/MorfologikIndicesAnalysis.java
@@ -1,24 +1,4 @@
-/*
- * Licensed to ElasticSearch and Shay Banon under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership. ElasticSearch licenses this
- * file to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
 package org.elasticsearch.indices.analysis.pl;
-
 import org.elasticsearch.common.component.AbstractComponent;
 import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.lucene.Lucene;
@@ -28,11 +8,9 @@ import org.elasticsearch.index.analysis.PreBuiltAnalyzerProviderFactory;
 import org.elasticsearch.index.analysis.PreBuiltTokenFilterFactoryFactory;
 import org.elasticsearch.indices.analysis.IndicesAnalysisService;
 import org.elasticsearch.index.analysis.TokenFilterFactory;
-
 import morfologik.stemming.*;
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.morfologik.*;
-
 import java.io.IOException;
 
 /**
@@ -40,28 +18,17 @@ import java.io.IOException;
  * among all indices.
  */
 public class MorfologikIndicesAnalysis extends AbstractComponent {
+  @Inject public MorfologikIndicesAnalysis(Settings settings, IndicesAnalysisService indicesAnalysisService) {
+    super(settings);
+    indicesAnalysisService.analyzerProviderFactories().put("morfologik", new PreBuiltAnalyzerProviderFactory("morfologik", AnalyzerScope.INDICES, new MorfologikAnalyzer(Lucene.ANALYZER_VERSION)));
+    indicesAnalysisService.tokenFilterFactories().put("morfologik_stem", new PreBuiltTokenFilterFactoryFactory(new TokenFilterFactory() {
+      @Override public String name() {
+        return "morfologik_stem";
+      }
 
-    @Inject
-    public MorfologikIndicesAnalysis(Settings settings, IndicesAnalysisService indicesAnalysisService) {
-        super(settings);
-
-        indicesAnalysisService.analyzerProviderFactories().put(
-            "morfologik",
-            new PreBuiltAnalyzerProviderFactory(
-                "morfologik",
-                AnalyzerScope.INDICES,
-                new MorfologikAnalyzer(Lucene.ANALYZER_VERSION)
-            )
-        );
-
-        indicesAnalysisService.tokenFilterFactories().put("morfologik_stem", new PreBuiltTokenFilterFactoryFactory(new TokenFilterFactory() {
-            @Override public String name() {
-                return "morfologik_stem";
-            }
-
-            @Override public TokenStream create(TokenStream tokenStream) {
-                return new MorfologikFilter(tokenStream, Lucene.ANALYZER_VERSION);
-            }
-        }));
-    }
+      @Override public TokenStream create(TokenStream tokenStream) {
+        return new MorfologikFilter(tokenStream, Lucene.ANALYZER_VERSION);
+      }
+    }));
+  }
 }
