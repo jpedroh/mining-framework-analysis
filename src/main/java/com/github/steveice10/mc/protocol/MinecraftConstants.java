369d1a0e5570d53b4eebaa8af95f8b156c343005
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 02:54:23 +0000

    Mining Framework Analysis

--- src/main/java/com/github/steveice10/mc/protocol/MinecraftConstants.java
+++ src/main/java/com/github/steveice10/mc/protocol/MinecraftConstants.java
@@ -1,80 +1,32 @@
 package com.github.steveice10.mc.protocol;
-
 import com.github.steveice10.mc.auth.data.GameProfile;
 import com.github.steveice10.mc.auth.service.SessionService;
 
-/**
- * Class containing various constants for Minecraft sessions.
- */
 public final class MinecraftConstants {
-    // General Constants
+  public static final String GAME_VERSION = "1.16.3";
 
-    /**
-     * Current supported game version.
-     */
-    public static final String GAME_VERSION = "1.16.3";
+  public static final int PROTOCOL_VERSION = 753;
 
-    /**
-     * Current supported protocol version.
-     */
-    public static final int PROTOCOL_VERSION = 753;
+  public static final String PROFILE_KEY = "profile";
 
-    // General Key Constants
+  public static final String SESSION_SERVICE_KEY = "session-service";
 
-    /**
-     * Session flag where the user {@link GameProfile} is stored.
-     */
-    public static final String PROFILE_KEY = "profile";
+  public static final String ACCESS_TOKEN_KEY = "access-token";
 
-    /**
-     * Session flag for providing a custom {@link SessionService} instance.
-     */
-    public static final String SESSION_SERVICE_KEY = "session-service";
+  public static final String SERVER_INFO_HANDLER_KEY = "server-info-handler";
 
-    // Client Key Constants
+  public static final String SERVER_PING_TIME_HANDLER_KEY = "server-ping-time-handler";
 
-    /**
-     * Session flag where the user's access token is stored. Client only.
-     */
-    public static final String ACCESS_TOKEN_KEY = "access-token";
+  public static final String VERIFY_USERS_KEY = "verify-users";
 
-    /**
-     * Session flag for providing a custom server info response handler. Client only.
-     */
-    public static final String SERVER_INFO_HANDLER_KEY = "server-info-handler";
+  public static final String SERVER_INFO_BUILDER_KEY = "info-builder";
 
-    /**
-     * Session flag for providing a custom ping time response handler. Client only.
-     */
-    public static final String SERVER_PING_TIME_HANDLER_KEY = "server-ping-time-handler";
+  public static final String SERVER_LOGIN_HANDLER_KEY = "login-handler";
 
-    // Server Key Constants
+  public static final String PING_KEY = "ping";
 
-    /**
-     * Session flag for determining whether to verify users. Server only.
-     */
-    public static final String VERIFY_USERS_KEY = "verify-users";
+  public static final String SERVER_COMPRESSION_THRESHOLD = "compression-threshold";
 
-    /**
-     * Session flag for providing a custom server info response builder. Server only.
-     */
-    public static final String SERVER_INFO_BUILDER_KEY = "info-builder";
-
-    /**
-     * Session flag for providing a custom server login handler. Server only.
-     */
-    public static final String SERVER_LOGIN_HANDLER_KEY = "login-handler";
-
-    /**
-     * Session flag for storing the current ping time. Server only.
-     */
-    public static final String PING_KEY = "ping";
-
-    /**
-     * Session flag for determining the packet compression threshold. Server only.
-     */
-    public static final String SERVER_COMPRESSION_THRESHOLD = "compression-threshold";
-
-    private MinecraftConstants() {
-    }
+  private MinecraftConstants() {
+  }
 }
