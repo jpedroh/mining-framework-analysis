b0ac7ee3b4d34bc9d3e1bc841f1bb733acd3c9d6
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 01:56:05 +0000

    Mining Framework Analysis

--- agent/plugins/mail-plugin/src/test/java/org/glowroot/agent/plugin/mail/MailPluginIT.java
+++ agent/plugins/mail-plugin/src/test/java/org/glowroot/agent/plugin/mail/MailPluginIT.java
@@ -1,24 +1,7 @@
-/**
- * Copyright 2019 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.glowroot.agent.plugin.mail;
-
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
-
 import com.google.common.collect.Lists;
 import com.icegreen.greenmail.util.GreenMail;
 import com.icegreen.greenmail.util.GreenMailUtil;
@@ -27,90 +10,71 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
 import org.glowroot.agent.it.harness.AppUnderTest;
 import org.glowroot.agent.it.harness.Container;
 import org.glowroot.agent.it.harness.Containers;
 import org.glowroot.agent.it.harness.TransactionMarker;
 import org.glowroot.wire.api.model.TraceOuterClass.Trace;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MailPluginIT {
+  private static Container container;
 
-    private static Container container;
+  @BeforeAll public static void setUp() throws Exception {
+    container = Containers.create();
+  }
 
-    @BeforeAll
-    public static void setUp() throws Exception {
-        container = Containers.create();
+  @AfterAll public static void tearDown() throws Exception {
+    container.close();
+  }
+
+  @AfterEach public void afterEachTest() throws Exception {
+    container.checkAndReset();
+  }
+
+  @Test public void shouldSendMessage() throws Exception {
+    Trace trace = container.execute(ExecuteSend.class);
+    checkTimers(trace);
+    Iterator<Trace.Entry> i = trace.getEntryList().iterator();
+    Trace.Entry entry = i.next();
+    assertThat(entry.getDepth()).isEqualTo(0);
+    assertThat(entry.getMessage()).startsWith("mail connect smtp://");
+    assertThat(i.hasNext()).isTrue();
+    entry = i.next();
+    assertThat(entry.getDepth()).isEqualTo(0);
+    assertThat(entry.getMessage()).isEqualTo("mail send message");
+    assertThat(i.hasNext()).isFalse();
+  }
+
+  private static void checkTimers(Trace trace) {
+    Trace.Timer rootTimer = trace.getHeader().getMainThreadRootTimer();
+    List<String> timerNames = Lists.newArrayList();
+    for (Trace.Timer timer : rootTimer.getChildTimerList()) {
+      timerNames.add(timer.getName());
     }
-
-    @AfterAll
-    public static void tearDown() throws Exception {
-        container.close();
+    Collections.sort(timerNames);
+    assertThat(timerNames).containsExactly("mail");
+    for (Trace.Timer timer : rootTimer.getChildTimerList()) {
+      assertThat(timer.getChildTimerList()).isEmpty();
     }
+    assertThat(trace.getHeader().getAsyncTimerCount()).isZero();
+  }
 
-    @AfterEach
-    public void afterEachTest() throws Exception {
-        container.checkAndReset();
+  public static class ExecuteSend extends DoMail {
+    @Override public void transactionMarker() {
+      GreenMailUtil.sendTextEmailTest("to@localhost.com", "from@localhost.com", "some subject", "some body");
     }
+  }
 
-    @Test
-    public void shouldSendMessage() throws Exception {
-        // when
-        Trace trace = container.execute(ExecuteSend.class);
-
-        // then
-        checkTimers(trace);
-
-        Iterator<Trace.Entry> i = trace.getEntryList().iterator();
-
-        Trace.Entry entry = i.next();
-        assertThat(entry.getDepth()).isEqualTo(0);
-        assertThat(entry.getMessage()).startsWith("mail connect smtp://");
-
-        assertThat(i.hasNext()).isTrue();
-
-        entry = i.next();
-        assertThat(entry.getDepth()).isEqualTo(0);
-        assertThat(entry.getMessage()).isEqualTo("mail send message");
-
-        assertThat(i.hasNext()).isFalse();
-
+  private abstract static class DoMail implements AppUnderTest, TransactionMarker {
+    @Override public void executeApp() throws Exception {
+      GreenMail greenMail = new GreenMail(ServerSetupTest.SMTP);
+      try {
+        greenMail.start();
+        transactionMarker();
+      }  finally {
+        greenMail.stop();
+      }
     }
-
-    private static void checkTimers(Trace trace) {
-        Trace.Timer rootTimer = trace.getHeader().getMainThreadRootTimer();
-        List<String> timerNames = Lists.newArrayList();
-        for (Trace.Timer timer : rootTimer.getChildTimerList()) {
-            timerNames.add(timer.getName());
-        }
-        Collections.sort(timerNames);
-        assertThat(timerNames).containsExactly("mail");
-        for (Trace.Timer timer : rootTimer.getChildTimerList()) {
-            assertThat(timer.getChildTimerList()).isEmpty();
-        }
-        assertThat(trace.getHeader().getAsyncTimerCount()).isZero();
-    }
-
-    public static class ExecuteSend extends DoMail {
-        @Override
-        public void transactionMarker() {
-            GreenMailUtil.sendTextEmailTest("to@localhost.com", "from@localhost.com",
-                    "some subject", "some body");
-        }
-    }
-
-    private abstract static class DoMail implements AppUnderTest, TransactionMarker {
-        @Override
-        public void executeApp() throws Exception {
-            GreenMail greenMail = new GreenMail(ServerSetupTest.SMTP);
-            try {
-                greenMail.start();
-                transactionMarker();
-            } finally {
-                greenMail.stop();
-            }
-        }
-    }
+  }
 }
