7190b2fb2fc243cf85c29fb9e3dc5f262042e0fb
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 18:03:47 +0000

    Mining Framework Analysis

--- src/main/java/com/sdl/selenium/web/table/TableCell.java
+++ src/main/java/com/sdl/selenium/web/table/TableCell.java
@@ -1,48 +1,46 @@
 package com.sdl.selenium.web.table;
-
 import com.sdl.selenium.web.SearchType;
 import com.sdl.selenium.web.WebLocator;
 import org.apache.log4j.Logger;
 
 public class TableCell extends Cell {
-    private static final Logger logger = Logger.getLogger(TableCell.class);
+  private static final Logger logger = Logger.getLogger(TableCell.class);
 
-    public TableCell() {
-        setRenderMillis(200);
-        setClassName("TableCell");
-        setTag("td");
-        defaultSearchTextType.add(SearchType.DEEP_CHILD_NODE_OR_SELF);
-    }
+  public TableCell() {
+    setRenderMillis(200);
+    setClassName("TableCell");
+    setTag("td");
+    defaultSearchTextType.add(SearchType.DEEP_CHILD_NODE_OR_SELF);
+  }
 
-    public TableCell(WebLocator container) {
-        this();
-        setContainer(container);
-    }
+  public TableCell(WebLocator container) {
+    this();
+    setContainer(container);
+  }
 
-    public TableCell(WebLocator container, int columnIndex) {
-        this(container);
-        setPosition(columnIndex);
-    }
+  public TableCell(WebLocator container, int columnIndex) {
+    this(container);
+    setPosition(columnIndex);
+  }
 
-    public TableCell(int columnIndex, String columnText, SearchType ...searchType) {
-        this();
-        setPosition(columnIndex);
-        setText(columnText);
-        setSearchTextType(searchType);
-    }
+  public TableCell(int columnIndex, String columnText, SearchType... searchType) {
+    this();
+    setPosition(columnIndex);
+    setText(columnText);
+    setSearchTextType(searchType);
+  }
 
-    public TableCell(WebLocator container, int columnIndex, String columnText, SearchType ...searchType) {
-        this(container, columnIndex);
-        setText(columnText);
-        setSearchTextType(searchType);
-    }
+  public TableCell(WebLocator container, int columnIndex, String columnText, SearchType... searchType) {
+    this(container, columnIndex);
+    setText(columnText);
+    setSearchTextType(searchType);
+  }
 
-    @Override
-    protected String addPositionToPath(String itemPath) {
-        if (hasPosition()) {
-            String itemPathText = getItemPathText();
-            itemPath = "//" + getTag() + "[" + getPosition() + "]" + ("".equals(itemPathText) ? "" : (itemPathText == null ? "" : "[" + itemPathText + "]"));
-        }
-        return itemPath;
+  @Override protected String addPositionToPath(String itemPath) {
+    if (hasPosition()) {
+      String itemPathText = getItemPathText();
+      itemPath = "//" + getTag() + "[" + getPosition() + "]" + ("".equals(itemPathText) ? "" : (itemPathText == null ? "" : "[" + itemPathText + "]"));
     }
+    return itemPath;
+  }
 }
