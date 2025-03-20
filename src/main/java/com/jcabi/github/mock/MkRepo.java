6e6cb40d3bb9c92878754a1762f309f50154bda1
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 04:00:50 +0000

    Mining Framework Analysis

--- src/main/java/com/jcabi/github/mock/MkRepo.java
+++ src/main/java/com/jcabi/github/mock/MkRepo.java
@@ -1,34 +1,4 @@
-/**
- * Copyright (c) 2012-2013, JCabi.com
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met: 1) Redistributions of source code must retain the above
- * copyright notice, this list of conditions and the following
- * disclaimer. 2) Redistributions in binary form must reproduce the above
- * copyright notice, this list of conditions and the following
- * disclaimer in the documentation and/or other materials provided
- * with the distribution. 3) Neither the name of the jcabi.com nor
- * the names of its contributors may be used to endorse or promote
- * products derived from this software without specific prior written
- * permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
- * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
- * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
- * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- * OF THE POSSIBILITY OF SUCH DAMAGE.
- */
 package com.jcabi.github.mock;
-
 import com.jcabi.aspects.Immutable;
 import com.jcabi.aspects.Loggable;
 import com.jcabi.github.Assignees;
@@ -58,175 +28,144 @@ import lombok.ToString;
  * @version $Id$
  * @since 0.5
  * @checkstyle ClassDataAbstractionCouplingCheck (500 lines)
- * @checkstyle ClassFanOutComplexity (500 lines)
  */
-@Immutable
-@Loggable(Loggable.DEBUG)
-@ToString
-@EqualsAndHashCode(of = {"storage", "self", "coords" })
-@SuppressWarnings("PMD.TooManyMethods")
-final class MkRepo implements Repo {
-
-    /**
+@Immutable @Loggable(value = Loggable.DEBUG) @ToString @EqualsAndHashCode(of = { "storage", "self", "coords" }) @SuppressWarnings(value = { "PMD.TooManyMethods" }) final class MkRepo implements Repo {
+  /**
      * Storage.
      */
-    private final transient MkStorage storage;
+  private final transient MkStorage storage;
 
-    /**
+  /**
      * Login of the user logged in.
      */
-    private final transient String self;
+  private final transient String self;
 
-    /**
+  /**
      * Repo coordinates.
      */
-    private final transient Coordinates coords;
+  private final transient Coordinates coords;
 
-    /**
+  /**
      * Public ctor.
      * @param stg Storage
      * @param login User to login
      * @param repo Repo name
      */
-    MkRepo(final MkStorage stg, final String login,
-        final Coordinates repo) {
-        this.storage = stg;
-        this.self = login;
-        this.coords = repo;
-    }
+  MkRepo(final MkStorage stg, final String login, final Coordinates repo) {
+    this.storage = stg;
+    this.self = login;
+    this.coords = repo;
+  }
 
-    @Override
-    public Github github() {
-        return new MkGithub(this.storage, this.self);
-    }
+  @Override public Github github() {
+    return new MkGithub(this.storage, this.self);
+  }
 
-    @Override
-    public Coordinates coordinates() {
-        return this.coords;
-    }
+  @Override public Coordinates coordinates() {
+    return this.coords;
+  }
 
-    @Override
-    public Issues issues() {
-        try {
-            return new MkIssues(this.storage, this.self, this.coords);
-        } catch (final IOException ex) {
-            throw new IllegalStateException(ex);
-        }
+  @Override public Issues issues() {
+    try {
+      return new MkIssues(this.storage, this.self, this.coords);
+    } catch (final IOException ex) {
+      throw new IllegalStateException(ex);
     }
+  }
 
-    @Override
-    public Milestones milestones() {
-        try {
-            return new MkMilestones(this.storage, this.self, this.coords);
-        } catch (final IOException ex) {
-            throw new IllegalStateException(ex);
-        }
+  @Override public Milestones milestones() {
+    try {
+      return new MkMilestones(this.storage, this.self, this.coords);
+    } catch (final IOException ex) {
+      throw new IllegalStateException(ex);
     }
+  }
 
-    @Override
-    public Pulls pulls() {
-        try {
-            return new MkPulls(this.storage, this.self, this.coords);
-        } catch (final IOException ex) {
-            throw new IllegalStateException(ex);
-        }
+  @Override public Pulls pulls() {
+    try {
+      return new MkPulls(this.storage, this.self, this.coords);
+    } catch (final IOException ex) {
+      throw new IllegalStateException(ex);
     }
+  }
 
-    @Override
-    public Hooks hooks() {
-        try {
-            return new MkHooks(this.storage, this.self, this.coords);
-        } catch (final IOException ex) {
-            throw new IllegalStateException(ex);
-        }
+  @Override public Hooks hooks() {
+    try {
+      return new MkHooks(this.storage, this.self, this.coords);
+    } catch (final IOException ex) {
+      throw new IllegalStateException(ex);
     }
+  }
 
-    @Override
-    public Iterable<Event> events() {
-        return null;
+  @Override public Iterable<Event> events() {
+    return null;
+  }
+
+  @Override public Labels labels() {
+    try {
+      return new MkLabels(this.storage, this.self, this.coords);
+    } catch (final IOException ex) {
+      throw new IllegalStateException(ex);
     }
+  }
 
-    @Override
-    public Labels labels() {
-        try {
-            return new MkLabels(this.storage, this.self, this.coords);
-        } catch (final IOException ex) {
-            throw new IllegalStateException(ex);
-        }
+  @Override public Assignees assignees() {
+    try {
+      return new MkAssignees(this.storage, this.self, this.coords);
+    } catch (final IOException ex) {
+      throw new IllegalStateException(ex);
     }
+  }
 
-    @Override
-    public Assignees assignees() {
-        try {
-            return new MkAssignees(this.storage, this.self, this.coords);
-        } catch (final IOException ex) {
-            throw new IllegalStateException(ex);
-        }
+  @Override public Releases releases() {
+    try {
+      return new MkReleases(this.storage, this.self, this.coords);
+    } catch (final IOException ex) {
+      throw new IllegalStateException(ex);
     }
+  }
 
-    @Override
-    public Releases releases() {
-        try {
-            return new MkReleases(this.storage, this.self, this.coords);
-        } catch (final IOException ex) {
-            throw new IllegalStateException(ex);
-        }
+  @Override public Forks forks() {
+    try {
+      return new MkForks(this.storage, this.self, this.coords);
+    } catch (final IOException ex) {
+      throw new IllegalStateException(ex);
     }
+  }
 
-    @Override
-    public Forks forks() {
-        try {
-            return new MkForks(this.storage, this.self, this.coords);
-        } catch (final IOException ex) {
-            throw new IllegalStateException(ex);
-        }
+  @Override public Collaborators collaborators() {
+    throw new UnsupportedOperationException("Not implemented");
+  }
+
+  @Override public DeployKeys keys() {
+    return new MkDeployKeys(this.storage, this.self, this.coords);
+  }
+
+  @Override public Contents contents() {
+    try {
+      return new MkContents(this.storage, this.self, this.coords);
+    } catch (final IOException ex) {
+      throw new IllegalStateException(ex);
     }
+  }
 
-    @Override
-    public Collaborators collaborators() {
-        throw new UnsupportedOperationException("Not implemented");
-    }
+  @Override public void patch(final JsonObject json) throws IOException {
+    new JsonPatch(this.storage).patch(this.xpath(), json);
+  }
 
-    @Override
-    public DeployKeys keys() {
-        return new MkDeployKeys(this.storage, this.self, this.coords);
-    }
+  @Override public RepoCommits commits() {
+    return new MkRepoCommits(this.storage, this.coordinates());
+  }
 
-    @Override
-    public Contents contents() {
-        try {
-            return new MkContents(this.storage, this.self, this.coords);
-        } catch (final IOException ex) {
-            throw new IllegalStateException(ex);
-        }
-    }
+  @Override public JsonObject json() throws IOException {
+    return new JsonNode(this.storage.xml().nodes(this.xpath()).get(0)).json();
+  }
 
-    @Override
-    public void patch(final JsonObject json) throws IOException {
-        new JsonPatch(this.storage).patch(this.xpath(), json);
-    }
-
-    @Override
-    public RepoCommits commits() {
-        return new MkRepoCommits(this.storage, this.coordinates());
-    }
-
-    @Override
-    public JsonObject json() throws IOException {
-        return new JsonNode(
-            this.storage.xml().nodes(this.xpath()).get(0)
-        ).json();
-    }
-
-    /**
+  /**
      * XPath of this element in XML tree.
      * @return XPath
      */
-    private String xpath() {
-        return String.format(
-            "/github/repos/repo[@coords='%s']",
-            this.coords
-        );
-    }
-
+  private String xpath() {
+    return String.format("/github/repos/repo[@coords=\'%s\']", this.coords);
+  }
 }
