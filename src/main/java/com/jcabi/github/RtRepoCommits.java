2efbc7678cf2825877a7bdb3b7e12713c1303df2
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 04:24:43 +0000

    Mining Framework Analysis

--- src/main/java/com/jcabi/github/RtRepoCommits.java
+++ src/main/java/com/jcabi/github/RtRepoCommits.java
@@ -1,34 +1,4 @@
-/**
- * Copyright (c) 2012-2013, JCabi.com
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met: 1) Redistributions of source code must retain the above
- * copyright notice, this list of conditions and the following
- * disclaimer. 2) Redistributions in binary form must reproduce the above
- * copyright notice, this list of conditions and the following
- * disclaimer in the documentation and/or other materials provided
- * with the distribution. 3) Neither the name of the jcabi.com nor
- * the names of its contributors may be used to endorse or promote
- * products derived from this software without specific prior written
- * permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
- * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
- * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
- * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- * OF THE POSSIBILITY OF SUCH DAMAGE.
- */
 package com.jcabi.github;
-
 import com.jcabi.aspects.Immutable;
 import com.jcabi.aspects.Loggable;
 import com.jcabi.http.Request;
@@ -40,77 +10,70 @@ import lombok.EqualsAndHashCode;
  * Commits of a Github repository.
  * @author Alexander Sinyagin (sinyagin.alexander@gmail.com)
  * @version $Id$
- * @todo #117 RtRepoCommits should be able to fetch commits. Let's
- *  implement this method. When done, remove this puzzle and
- *  Ignore annotation from a test for the method.
  */
-@Immutable
-@Loggable(Loggable.DEBUG)
-@EqualsAndHashCode(of = "request")
-final class RtRepoCommits implements RepoCommits {
-
-    /**
-     * RESTful API entry point.
-     */
-    private final transient Request entry;
-
-    /**
+@Immutable @Loggable(value = Loggable.DEBUG) @EqualsAndHashCode(of = "request") final class RtRepoCommits implements RepoCommits {
+  /**
      * RESTful request for the commits.
      */
-    private final transient Request request;
+  private final transient Request request;
 
-    /**
-     * Parent repository.
+  /**
+     * RESTful API entry point.
      */
-    private final transient Repo owner;
+  private final transient Request entry;
 
-    /**
+  /**
      * Github.
      */
-    private final transient Github github;
+  private final transient Github github;
 
-    /**
+  /**
+     * Repository.
+     */
+  private final transient Repo 
+<<<<<<< /usr/src/app/output/jcabi/jcabi-github/3af65704843d882e9e391f5b19f9e7f8f404a3e5/src/main/java/com/jcabi/github/RtRepoCommits.java/left.java
+  repo
+=======
+  owner
+>>>>>>> /usr/src/app/output/jcabi/jcabi-github/3af65704843d882e9e391f5b19f9e7f8f404a3e5/src/main/java/com/jcabi/github/RtRepoCommits.java/right.java
+  ;
+
+  /**
      * Public ctor.
      * @param req Entry point of API
-     * @param repo Repository
+     * @param repo Repository coordinates
      */
-    RtRepoCommits(final Request req, final Repo repo) {
-        this.entry = req;
-        this.owner = repo;
-        this.request = req.uri()
-            .path("/repos")
-            .path(repo.coordinates().user())
-            .path(repo.coordinates().repo())
-            .path("/commits")
-            .back();
-        this.github = new RtGithub(this.req);
-    }
+  RtRepoCommits(final Request req, final Repo repo) {
+    this.entry = req;
+    this.owner = repo;
+    this.request = req.uri().path("/repos").path(repo.coordinates().user()).path(repo.coordinates().repo()).path("/commits").back();
+    this.github = new RtGithub(this.request);
+    this.repo = new RtRepo(this.github, this.request, repo);
+  }
 
-    @Override
-    public Iterable<Commit> iterate() {
-        return new RtPagination<Commit>(
-            this.request,
-            new RtPagination.Mapping<Commit>() {
-                @Override
-                public Commit map(final JsonObject object) {
-                    return get(object.getString("sha"));
-                }
-            }
-        );
-    }
+  @Override public Iterable<Commit> iterate() {
+    return new RtPagination<Commit>(this.request, new RtPagination.Mapping<Commit>() {
+      @Override public Commit map(final JsonObject object) {
+        return get(object.getString("sha"));
+      }
+    });
+  }
 
-    @Override
-    public Commit get(final String sha) {
-        return new RtCommit(this.entry, this.owner, sha);
-    }
+  @Override public Commit get(final String sha) {
+    return new RtCommit(this.entry, this.
+<<<<<<< /usr/src/app/output/jcabi/jcabi-github/3af65704843d882e9e391f5b19f9e7f8f404a3e5/src/main/java/com/jcabi/github/RtRepoCommits.java/left.java
+    repo
+=======
+    owner
+>>>>>>> /usr/src/app/output/jcabi/jcabi-github/3af65704843d882e9e391f5b19f9e7f8f404a3e5/src/main/java/com/jcabi/github/RtRepoCommits.java/right.java
+    , sha);
+  }
 
-    @Override
-    public String toString() {
-        return this.request.uri().get().toString();
-    }
+  @Override public String toString() {
+    return this.request.uri().get().toString();
+  }
 
-    @Override
-    public JsonObject json() throws IOException {
-        return new RtJson(this.request).fetch();
-    }
+  @Override public JsonObject json() throws IOException {
+    return new RtJson(this.request).fetch();
+  }
 }
--- src/test/java/com/jcabi/github/RtRepoCommitsTest.java
+++ src/test/java/com/jcabi/github/RtRepoCommitsTest.java
@@ -1,34 +1,4 @@
-/**
- * Copyright (c) 2012-2013, JCabi.com
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met: 1) Redistributions of source code must retain the above
- * copyright notice, this list of conditions and the following
- * disclaimer. 2) Redistributions in binary form must reproduce the above
- * copyright notice, this list of conditions and the following
- * disclaimer in the documentation and/or other materials provided
- * with the distribution. 3) Neither the name of the jcabi.com nor
- * the names of its contributors may be used to endorse or promote
- * products derived from this software without specific prior written
- * permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
- * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
- * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
- * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- * OF THE POSSIBILITY OF SUCH DAMAGE.
- */
 package com.jcabi.github;
-
 import com.jcabi.http.request.FakeRequest;
 import javax.json.Json;
 import org.hamcrest.MatcherAssert;
@@ -42,53 +12,29 @@ import org.junit.Test;
  * @version $Id$
  */
 public final class RtRepoCommitsTest {
-
-    /**
+  /**
      * RtRepoCommits can return commits' iterator.
      */
-    @Test
-    public void returnIterator() {
-        final String sha = "6dcb09b5b57875f334f61aebed695e2e4193db51";
-        final RepoCommits commits = new RtRepoCommits(
-            new FakeRequest().withBody(
-                Json.createArrayBuilder().add(
-                    // @checkstyle MultipleStringLiterals (1 line)
-                    Json.createObjectBuilder().add("sha", sha)
-                ).build().toString()
-            ),
-            RtRepoCommitsTest.repo()
-        );
-        MatcherAssert.assertThat(
-            commits.iterate().iterator().next().sha(),
-            Matchers.equalTo(sha)
-        );
-    }
+  @Test public void returnIterator() {
+    final String sha = "6dcb09b5b57875f334f61aebed695e2e4193db51";
+    final RepoCommits commits = new RtRepoCommits(new FakeRequest().withBody(Json.createArrayBuilder().add(Json.createObjectBuilder().add("sha", sha)).build().toString()), RtRepoCommitsTest.repo());
+    MatcherAssert.assertThat(commits.iterate().iterator().next().sha(), Matchers.equalTo(sha));
+  }
 
-    /**
+  /**
      * RtRepoCommits can get commit.
      */
-    @Test
-    public void getCommit() {
-        final String sha = "6dcb09b5b57875f334f61aebed695e2e4193db52";
-        final RepoCommits commits = new RtRepoCommits(
-            new FakeRequest().withBody(
-                Json.createObjectBuilder()
-                    .add("sha", sha)
-                    .build()
-                    .toString()
-            ),
-            RtRepoCommitsTest.repo()
-        );
-        MatcherAssert.assertThat(commits.get(sha).sha(), Matchers.equalTo(sha));
-    }
+  @Test public void getCommit() {
+    final String sha = "6dcb09b5b57875f334f61aebed695e2e4193db52";
+    final RepoCommits commits = new RtRepoCommits(new FakeRequest().withBody(Json.createObjectBuilder().add("sha", sha).build().toString()), RtRepoCommitsTest.repo());
+    MatcherAssert.assertThat(commits.get(sha).sha(), Matchers.equalTo(sha));
+  }
 
-    /**
+  /**
      * Create repository for tests.
      * @return Repository
      */
-    private static Repo repo() {
-        return new RtGithub().repos()
-            .get(new Coordinates.Simple("user", "repo"));
-    }
-
+  private static Repo repo() {
+    return new RtGithub().repos().get(new Coordinates.Simple("user", "repo"));
+  }
 }
