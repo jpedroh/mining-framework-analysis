636c6bec90ccfe521695ba5a78e3c25e207a339a
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 13:56:37 +0000

    Mining Framework Analysis

--- jsprit-core/src/main/java/jsprit/core/problem/vehicle/VehicleType.java
+++ jsprit-core/src/main/java/jsprit/core/problem/vehicle/VehicleType.java
@@ -1,67 +1,15 @@
-/*******************************************************************************
- * Copyright (C) 2013  Stefan Schroeder
- * 
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either 
- * version 3.0 of the License, or (at your option) any later version.
- * 
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public 
- * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
- ******************************************************************************/
 package jsprit.core.problem.vehicle;
-
 import jsprit.core.problem.Capacity;
 import jsprit.core.problem.vehicle.VehicleTypeImpl.VehicleCostParams;
 
-/**
- * Basic interface for vehicle-type-data.
- * 
- * @author schroeder
- *
- */
 public interface VehicleType {
+  public String getTypeId();
 
-	/**
-	 * Returns typeId.
-	 * 
-	 * @return typeId
-	 */
-	public String getTypeId();
+  public int getCapacity();
 
-	/**
-	 * Returns capacity.
-	 * 
-	 * <p>In future versions there will be a capacity-object with an arbitrary number of capacity dimensions. (stefan,11.01.14)
-	 * 
-	 * @return cap
-	 */
-	public int getCapacity();
-	
-	/**
-	 * Returns capacity dimensions.
-	 * 
-	 * @return {@link Capacity}
-	 */
-	public Capacity getCapacityDimensions();
-	
-	/**
-	 * Returns maximum velocity of this vehicle-type.
-	 * 
-	 * @return max velocity
-	 */
-	public double getMaxVelocity();
+  public Capacity getCapacityDimensions();
 
-	/**
-	 * Return the cost-parameter of this vehicle-type.
-	 * 
-	 * @return parameter
-	 */
-	public VehicleCostParams getVehicleCostParams();
+  public double getMaxVelocity();
 
+  public VehicleCostParams getVehicleCostParams();
 }
