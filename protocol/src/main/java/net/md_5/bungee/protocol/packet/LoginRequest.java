18048a176b687799b9f4ecd7f8fc1f67fd2ae5b9
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 14:26:27 +0000

    Mining Framework Analysis

--- protocol/src/main/java/net/md_5/bungee/protocol/packet/LoginRequest.java
+++ protocol/src/main/java/net/md_5/bungee/protocol/packet/LoginRequest.java
@@ -1,5 +1,4 @@
 package net.md_5.bungee.protocol.packet;
-
 import io.netty.buffer.ByteBuf;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -9,33 +8,21 @@ import net.md_5.bungee.protocol.AbstractPacketHandler;
 import net.md_5.bungee.protocol.DefinedPacket;
 import net.md_5.bungee.protocol.ProtocolConstants.Direction;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@EqualsAndHashCode(callSuper = false)
-public class LoginRequest extends DefinedPacket
-{
+@Data @NoArgsConstructor @AllArgsConstructor @EqualsAndHashCode(callSuper = false) public class LoginRequest extends DefinedPacket {
+  public static final int EXPECTED_MAX_LENGTH = 1 + (32 * 4);
 
-    public static final int EXPECTED_MAX_LENGTH = 1 + ( 32 * 4 ); //BotFilter
+  private String data;
 
-    private String data;
+  @Override public void read(ByteBuf buf, Direction direction, int protocolVersion) {
+    DefinedPacket.doLengthSanityChecks(buf, this, direction, protocolVersion, 0, EXPECTED_MAX_LENGTH);
+    data = readString(buf, 32);
+  }
 
-    @Override
-    public void read(ByteBuf buf, Direction direction, int protocolVersion)
-    {
-        DefinedPacket.doLengthSanityChecks( buf, this, direction, protocolVersion, 0, EXPECTED_MAX_LENGTH ); //BotFilter
-        data = readString( buf, 32 ); //BotFilter read 32 characters instead of 15
-    }
+  @Override public void write(ByteBuf buf) {
+    writeString(data, buf);
+  }
 
-    @Override
-    public void write(ByteBuf buf)
-    {
-        writeString( data, buf );
-    }
-
-    @Override
-    public void handle(AbstractPacketHandler handler) throws Exception
-    {
-        handler.handle( this );
-    }
+  @Override public void handle(AbstractPacketHandler handler) throws Exception {
+    handler.handle(this);
+  }
 }
--- proxy/src/main/java/net/md_5/bungee/BungeeCordLauncher.java
+++ proxy/src/main/java/net/md_5/bungee/BungeeCordLauncher.java
@@ -1,5 +1,4 @@
 package net.md_5.bungee;
-
 import java.security.Security;
 import java.util.Arrays;
 import java.util.logging.Level;
@@ -10,53 +9,38 @@ import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.command.ConsoleCommandSender;
 
-public class BungeeCordLauncher
-{
-
-    public static void main(String[] args) throws Exception
-    {
-        Security.setProperty( "networkaddress.cache.ttl", "30" );
-        Security.setProperty( "networkaddress.cache.negative.ttl", "10" );
-        // For JDK9+ we force-enable multi-release jar file support #3087
-        if ( System.getProperty( "jdk.util.jar.enableMultiRelease" ) == null )
-        {
-            System.setProperty( "jdk.util.jar.enableMultiRelease", "force" );
-        }
-
-        OptionParser parser = new OptionParser();
-        parser.allowsUnrecognizedOptions();
-        parser.acceptsAll( Arrays.asList( "help" ), "Show the help" );
-        parser.acceptsAll( Arrays.asList( "v", "version" ), "Print version and exit" );
-        parser.acceptsAll( Arrays.asList( "noconsole" ), "Disable console input" );
-
-        OptionSet options = parser.parse( args );
-
-        if ( options.has( "help" ) )
-        {
-            parser.printHelpOn( System.out );
-            return;
-        }
-        if ( options.has( "version" ) )
-        {
-            System.out.println( BungeeCord.class.getPackage().getImplementationVersion() );
-            return;
-        }
-
-        BungeeCord bungee = new BungeeCord();
-        ProxyServer.setInstance( bungee );
-        bungee.getLogger().log( Level.WARNING, "Включаю BungeCord BotFilter {0} от vk.com/Leymooo_s (http://rubukkit.org/threads/137038)", bungee.getGameVersion() ); //BotFilter
-        bungee.start();
-
-        if ( !options.has( "noconsole" ) )
-        {
-            String line;
-            while ( bungee.isRunning && ( line = bungee.getConsoleReader().readLine( ">" ) ) != null )
-            {
-                if ( !bungee.getPluginManager().dispatchCommand( ConsoleCommandSender.getInstance(), line ) )
-                {
-                    bungee.getConsole().sendMessage( new ComponentBuilder( "Команда не найдена :(" ).color( ChatColor.RED ).create() ); //BotFilter
-                }
-            }
-        }
+public class BungeeCordLauncher {
+  public static void main(String[] args) throws Exception {
+    Security.setProperty("networkaddress.cache.ttl", "30");
+    Security.setProperty("networkaddress.cache.negative.ttl", "10");
+    if (System.getProperty("jdk.util.jar.enableMultiRelease") == null) {
+      System.setProperty("jdk.util.jar.enableMultiRelease", "force");
     }
+    OptionParser parser = new OptionParser();
+    parser.allowsUnrecognizedOptions();
+    parser.acceptsAll(Arrays.asList("help"), "Show the help");
+    parser.acceptsAll(Arrays.asList("v", "version"), "Print version and exit");
+    parser.acceptsAll(Arrays.asList("noconsole"), "Disable console input");
+    OptionSet options = parser.parse(args);
+    if (options.has("help")) {
+      parser.printHelpOn(System.out);
+      return;
+    }
+    if (options.has("version")) {
+      System.out.println(BungeeCord.class.getPackage().getImplementationVersion());
+      return;
+    }
+    BungeeCord bungee = new BungeeCord();
+    ProxyServer.setInstance(bungee);
+    bungee.getLogger().log(Level.WARNING, "\u0412\u043a\u043b\u044e\u0447\u0430\u044e BungeCord BotFilter {0} \u043e\u0442 vk.com/Leymooo_s (http://rubukkit.org/threads/137038)", bungee.getGameVersion());
+    bungee.start();
+    if (!options.has("noconsole")) {
+      String line;
+      while (bungee.isRunning && (line = bungee.getConsoleReader().readLine(">")) != null) {
+        if (!bungee.getPluginManager().dispatchCommand(ConsoleCommandSender.getInstance(), line)) {
+          bungee.getConsole().sendMessage(new ComponentBuilder("\u041a\u043e\u043c\u0430\u043d\u0434\u0430 \u043d\u0435 \u043d\u0430\u0439\u0434\u0435\u043d\u0430 :(").color(ChatColor.RED).create());
+        }
+      }
+    }
+  }
 }
