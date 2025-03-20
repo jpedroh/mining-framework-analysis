71ed5dcc8d16b8b613bf6d6066de2ae8bb958628
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 02:45:32 +0000

    Mining Framework Analysis

--- rival-evaluate/src/main/java/net/recommenders/rival/evaluation/metric/error/RMSE.java
+++ rival-evaluate/src/main/java/net/recommenders/rival/evaluation/metric/error/RMSE.java
@@ -1,8 +1,6 @@
 package net.recommenders.rival.evaluation.metric.error;
-
 import java.util.List;
 import net.recommenders.rival.core.DataModel;
-
 import java.util.Map;
 import net.recommenders.rival.evaluation.metric.EvaluationMetric;
 
@@ -13,41 +11,45 @@ import net.recommenders.rival.evaluation.metric.EvaluationMetric;
  * @author <a href="http://github.com/alansaid">Alan</a>.
  */
 public class RMSE extends AbstractErrorMetric implements EvaluationMetric<Long> {
+  /**
+     * @inheritDoc
+     */
+  public RMSE(DataModel<Long, Long> predictions, DataModel<Long, Long> test) {
+    super(predictions, test);
+  }
 
+  /**
+     *
+     * @inheritDoc
+     */
+  public RMSE(DataModel<Long, Long> predictions, DataModel<Long, Long> test, ErrorStrategy errorStrategy) {
+    super(predictions, test, errorStrategy);
+  }
 
-    public RMSE(DataModel<Long, Long> predictions, DataModel<Long, Long> test) {
-        super(predictions, test);
-    }
-
-    public RMSE(DataModel<Long, Long> predictions, DataModel<Long, Long> test, ErrorStrategy errorStrategy) {
-        super(predictions, test, errorStrategy);
-    }
-
-    /**
+  /**
      * Instantiates and computes the RMSE value. Prior to running this, there is
      * no valid value.
      *
+     * @return The global RMSE
      */
-    public void compute() {
-        Map<Long, List<Double>> data = processDataAsPredictedDifferencesToTest();
-        value = 0.0;
-        int testItems = 0;
-        for (long testUser : test.getUsers()) {
-            int userItems = 0;
-            double umse = 0.0;
-
-            if (data.containsKey(testUser)) {
-                for (double difference : data.get(testUser)) {
-                    umse += difference * difference;
-                    userItems++;
-                }
-            }
-
-            testItems += userItems;
-            value += umse;
-            umse = (userItems == 0) ? Double.NaN : Math.sqrt(umse / userItems);
-            metricPerUser.put(testUser, umse);
+  public void compute() {
+    Map<Long, List<Double>> data = processDataAsPredictedDifferencesToTest();
+    value = 0.0;
+    int testItems = 0;
+    for (long testUser : test.getUsers()) {
+      int userItems = 0;
+      double umse = 0.0;
+      if (data.containsKey(testUser)) {
+        for (double difference : data.get(testUser)) {
+          umse += difference * difference;
+          userItems++;
         }
-        value = (testItems == 0) ? Double.NaN : Math.sqrt(value / testItems);
+      }
+      testItems += userItems;
+      value += umse;
+      umse = (userItems == 0) ? Double.NaN : Math.sqrt(umse / userItems);
+      metricPerUser.put(testUser, umse);
     }
+    value = (testItems == 0) ? Double.NaN : Math.sqrt(value / testItems);
+  }
 }
