c90c47ca67cb2c009a0da56f8d14a659a850e3c9
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 09:29:33 +0000

    Mining Framework Analysis

--- src/main/java/com/fincatto/documentofiscal/nfe400/classes/nota/NFMeioPagamento.java
+++ src/main/java/com/fincatto/documentofiscal/nfe400/classes/nota/NFMeioPagamento.java
@@ -10,8 +10,15 @@ public enum NFMeioPagamento {
     VALE_ALIMENTACAO("10", "Vale alimenta\u00e7\u00e3o"),
     VALE_REFEICAO("11", "Vale refei\u00e7\u00e3o"),
     VALE_PRESENTE("12", "Vale presente"),
+<<<<<<< /usr/src/app/output/fincatto/nfe/7219068602d38887a94aaa6f8d725f045c7c33e8/src/main/java/com/fincatto/documentofiscal/nfe400/classes/nota/NFMeioPagamento.java/left.java
     VALE_COMBUSTIVEL("13", "Vale combust\u00edvel"),
-    DUPLICATA_MERCANTIL("14", "Duplicata Mercantil"),
+    DUPLICATA_MERCANTIL("14", "Duplicata mercantil"), //nao deve mais ser utilizado, mas mantido para leitura de notas antigas
+||||||| /usr/src/app/output/fincatto/nfe/7219068602d38887a94aaa6f8d725f045c7c33e8/src/main/java/com/fincatto/documentofiscal/nfe400/classes/nota/NFMeioPagamento.java/base.java
+    VALE_COMBUSTIVEL("13", "Vale combust\u00edvel"), //nao deve mais ser utilizado, mas mantido para leitura de notas antigas
+=======
+    VALE_COMBUSTIVEL("13", "Vale combust\u00edvel"),
+    DUPLICATA_MERCANTIL("14", "Duplicata Mercantil"), //nao deve mais ser utilizado, mas mantido para leitura de notas antigas
+>>>>>>> /usr/src/app/output/fincatto/nfe/7219068602d38887a94aaa6f8d725f045c7c33e8/src/main/java/com/fincatto/documentofiscal/nfe400/classes/nota/NFMeioPagamento.java/right.java
     BOLETO_BANCARIO("15", "Boleto Bancario"),
     SEM_PAGAMENTO("90", "Sem pagamento"),
     OUTRO("99", "Outro");
