36b191e9dc3304ede6f582a25f18f84b0342a823
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 13:50:00 +0000

    Mining Framework Analysis

--- core/src/test/java/xdi2/tests/core/impl/keyvalue/BDBKeyValueGraphTest.java
+++ core/src/test/java/xdi2/tests/core/impl/keyvalue/BDBKeyValueGraphTest.java
@@ -1,46 +1,33 @@
 package xdi2.tests.core.impl.keyvalue;
-
 import xdi2.core.GraphFactory;
 import xdi2.core.impl.keyvalue.bdb.BDBKeyValueGraphFactory;
 import xdi2.core.impl.keyvalue.bdb.BDBKeyValueStore;
 import xdi2.tests.core.impl.AbstractGraphTest;
 
 public class BDBKeyValueGraphTest extends AbstractGraphTest {
+  private static BDBKeyValueGraphFactory graphFactory = new BDBKeyValueGraphFactory();
 
-	private static BDBKeyValueGraphFactory graphFactory = new BDBKeyValueGraphFactory();
+  public static final String DATABASE_PATH = "./xdi2-bdb-keyvalue/";
 
-	public static final String DATABASE_PATH = "./xdi2-bdb-keyvalue/";
+  static {
+    graphFactory.setDatabasePath(DATABASE_PATH);
+  }
 
-	static {
-		
-		graphFactory.setDatabasePath(DATABASE_PATH);
-	}
+  @Override protected void setUp() throws Exception {
+    super.setUp();
+    BDBKeyValueStore.cleanup(DATABASE_PATH);
+  }
 
-	@Override
-	protected void setUp() throws Exception {
+  @Override protected void tearDown() throws Exception {
+    super.tearDown();
+    BDBKeyValueStore.cleanup(DATABASE_PATH);
+  }
 
-		super.setUp();
+  @Override protected GraphFactory getGraphFactory() {
+    return graphFactory;
+  }
 
-		BDBKeyValueStore.cleanup(DATABASE_PATH);
-	}
-
-	@Override
-	protected void tearDown() throws Exception {
-
-		super.tearDown();
-
-		BDBKeyValueStore.cleanup(DATABASE_PATH);
-	}
-
-	@Override
-	protected GraphFactory getGraphFactory() {
-
-		return graphFactory;
-	}
-
-	@Override
-	protected boolean supportsPersistence() {
-
-		return true;
-	}
+  @Override protected boolean supportsPersistence() {
+    return true;
+  }
 }
