b8cdfc5078d21ea025cd2c064e5cc6e380f5697e
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 19:43:02 +0000

    Mining Framework Analysis

--- src/main/java/org/selophane/elements/factory/internal/ElementDecorator.java
+++ src/main/java/org/selophane/elements/factory/internal/ElementDecorator.java
@@ -1,5 +1,4 @@
 package org.selophane.elements.factory.internal;
-
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.internal.Locatable;
 import org.openqa.selenium.internal.WrapsElement;
@@ -13,7 +12,6 @@ import org.openqa.selenium.support.pagefactory.internal.LocatingElementListHandl
 import org.selophane.elements.base.Element;
 import org.selophane.elements.base.ElementImpl;
 import org.selophane.elements.base.ImplementedBy;
-
 import java.lang.reflect.*;
 import java.util.List;
 
@@ -23,83 +21,73 @@ import java.util.List;
  * It is designed to support and return concrete implementations of wrappers for a variety of common html elements.
  */
 public class ElementDecorator implements FieldDecorator {
-    /**
+  /**
      * factory to use when generating ElementLocator.
      */
-    private ElementLocatorFactory factory;
+  private ElementLocatorFactory factory;
 
-    /**
+  /**
      * Constructor for an ElementLocatorFactory. This class is designed to replace DefaultFieldDecorator.
      *
      * @param factory for locating elements.
      */
-    public ElementDecorator(ElementLocatorFactory factory) {
-        this.factory = factory;
+  public ElementDecorator(ElementLocatorFactory factory) {
+    this.factory = factory;
+  }
+
+  @Override public Object decorate(ClassLoader loader, Field field) {
+    if (!(WebElement.class.isAssignableFrom(field.getType()) || isDecoratableList(field))) {
+      return null;
     }
-
-    @Override
-    public Object decorate(ClassLoader loader, Field field) {
-        if (!(WebElement.class.isAssignableFrom(field.getType()) || isDecoratableList(field))) {
-            return null;
-        }
-
-        if (field.getDeclaringClass() == ElementImpl.class) {
-            return null;
-        }
-        
-        ElementLocator locator = factory.createLocator(field);
-        if (locator == null) {
-            return null;
-        }
-
-        Class<?> fieldType = field.getType();
-        if (WebElement.class.equals(fieldType)) {
-            fieldType = Element.class;
-        }
-
-        if (WebElement.class.isAssignableFrom(fieldType)) {
-            return proxyForLocator(loader, fieldType, locator);
-        } else if (List.class.isAssignableFrom(fieldType)) {
-            Class<?> erasureClass = getErasureClass(field);
-            return proxyForListLocator(loader, erasureClass, locator);
-        } else {
-            return null;
-        }
+    if (field.getDeclaringClass() == ElementImpl.class) {
+      return null;
     }
-
-    private Class<?> getErasureClass(Field field) {
-        // Type erasure in Java isn't complete. Attempt to discover the generic
-        // interfaceType of the list.
-        Type genericType = field.getGenericType();
-        if (!(genericType instanceof ParameterizedType)) {
-            return null;
-        }
-        return (Class<?>) ((ParameterizedType) genericType).getActualTypeArguments()[0];
+    ElementLocator locator = factory.createLocator(field);
+    if (locator == null) {
+      return null;
     }
-
-    private boolean isDecoratableList(Field field) {
-        if (!List.class.isAssignableFrom(field.getType())) {
-            return false;
-        }
-
+    Class<?> fieldType = field.getType();
+    if (WebElement.class.equals(fieldType)) {
+      fieldType = Element.class;
+    }
+    if (WebElement.class.isAssignableFrom(fieldType)) {
+      return proxyForLocator(loader, fieldType, locator);
+    } else {
+      if (List.class.isAssignableFrom(fieldType)) {
         Class<?> erasureClass = getErasureClass(field);
-        if (erasureClass == null) {
-            return false;
-        }
-
-        if (!WebElement.class.isAssignableFrom(erasureClass)) {
-            return false;
-        }
-
-        if (field.getAnnotation(FindBy.class) == null && field.getAnnotation(FindBys.class) == null &&
-                field.getAnnotation(FindAll.class) == null) {
-            return false;
-        }
-
-        return true;
+        return proxyForListLocator(loader, erasureClass, locator);
+      } else {
+        return null;
+      }
     }
+  }
 
-    /**
+  private Class<?> getErasureClass(Field field) {
+    Type genericType = field.getGenericType();
+    if (!(genericType instanceof ParameterizedType)) {
+      return null;
+    }
+    return (Class<?>) ((ParameterizedType) genericType).getActualTypeArguments()[0];
+  }
+
+  private boolean isDecoratableList(Field field) {
+    if (!List.class.isAssignableFrom(field.getType())) {
+      return false;
+    }
+    Class<?> erasureClass = getErasureClass(field);
+    if (erasureClass == null) {
+      return false;
+    }
+    if (!WebElement.class.isAssignableFrom(erasureClass)) {
+      return false;
+    }
+    if (field.getAnnotation(FindBy.class) == null && field.getAnnotation(FindBys.class) == null && field.getAnnotation(FindAll.class) == null) {
+      return false;
+    }
+    return true;
+  }
+
+  /**
      * Generate a type-parameterized locator proxy for the element in question. We use our customized InvocationHandler
      * here to wrap classes.
      *
@@ -109,16 +97,14 @@ public class ElementDecorator implements FieldDecorator {
      * @param <T>           The interface of the proxy.
      * @return a proxy representing the class we need to wrap.
      */
-    protected <T> T proxyForLocator(ClassLoader loader, Class<T> interfaceType, ElementLocator locator) {
-        InvocationHandler handler = new ElementHandler(interfaceType, locator);
+  protected <T extends java.lang.Object> T proxyForLocator(ClassLoader loader, Class<T> interfaceType, ElementLocator locator) {
+    InvocationHandler handler = new ElementHandler(interfaceType, locator);
+    T proxy;
+    proxy = interfaceType.cast(Proxy.newProxyInstance(loader, new Class[] { interfaceType, WebElement.class, WrapsElement.class, Locatable.class }, handler));
+    return proxy;
+  }
 
-        T proxy;
-        proxy = interfaceType.cast(Proxy.newProxyInstance(
-                loader, new Class[]{interfaceType, WebElement.class, WrapsElement.class, Locatable.class}, handler));
-        return proxy;
-    }
-
-    /**
+  /**
      * generates a proxy for a list of elements to be wrapped.
      *
      * @param loader        classloader for the class we're presently wrapping with proxies
@@ -127,17 +113,15 @@ public class ElementDecorator implements FieldDecorator {
      * @param <T>           class of the interface.
      * @return proxy with the same type as we started with.
      */
-    @SuppressWarnings("unchecked")
-    protected <T> List<T> proxyForListLocator(ClassLoader loader, Class<T> interfaceType, ElementLocator locator) {
-        InvocationHandler handler;
-        if (interfaceType.getAnnotation(ImplementedBy.class) != null) {
-            handler = new ElementListHandler(interfaceType, locator);
-        } else {
-            handler = new LocatingElementListHandler(locator);
-        }
-        List<T> proxy;
-        proxy = (List<T>) Proxy.newProxyInstance(
-                loader, new Class[]{List.class}, handler);
-        return proxy;
+  @SuppressWarnings(value = { "unchecked" }) protected <T extends java.lang.Object> List<T> proxyForListLocator(ClassLoader loader, Class<T> interfaceType, ElementLocator locator) {
+    InvocationHandler handler;
+    if (interfaceType.getAnnotation(ImplementedBy.class) != null) {
+      handler = new ElementListHandler(interfaceType, locator);
+    } else {
+      handler = new LocatingElementListHandler(locator);
     }
+    List<T> proxy;
+    proxy = (List<T>) Proxy.newProxyInstance(loader, new Class[] { List.class }, handler);
+    return proxy;
+  }
 }
