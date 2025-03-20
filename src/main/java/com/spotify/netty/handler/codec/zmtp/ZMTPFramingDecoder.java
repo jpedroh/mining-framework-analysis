7ea6fc8f06fd4ac5c74221430b232d30c6608d3d
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 14:37:26 +0000

    Mining Framework Analysis

--- src/main/java/com/spotify/netty/handler/codec/zmtp/ZMTPFramingDecoder.java
+++ src/main/java/com/spotify/netty/handler/codec/zmtp/ZMTPFramingDecoder.java
@@ -1,55 +1,11 @@
-/*
- * Copyright (c) 2012-2013 Spotify AB
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- */
-
-package com.spotify.netty.handler.codec.zmtp;
-
-import io.netty.buffer.ByteBuf;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.handler.codec.ByteToMessageDecoder;
-
-import java.util.List;
-
-/**
- * Netty FrameDecoder for zmtp protocol
- * <p/>
- * Decodes ZMTP frames into a ZMTPMessage - will return a ZMTPMessage as a message event
- */
-class ZMTPFramingDecoder extends ByteToMessageDecoder {
-
-    private final ZMTPMessageParser parser;
-    private final ZMTPSession session;
-
-    /**
-     * Creates a new decoder
-     */
-    public ZMTPFramingDecoder(final ZMTPSession session) {
-        this.parser = new ZMTPMessageParser(session.isEnveloped(), session.getSizeLimit(),
-                session.getActualVersion());
-        this.session = session;
-    }
-
-    /**
-     * Responsible for decoding incoming data to zmtp frames
-     */
-    @Override
-    protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
-        // Parse incoming frames
-        final ZMTPParsedMessage msg = parser.parse(in);
-        if (msg != null) {
-            out.add(new ZMTPIncomingMessage(session, msg.getMessage(), msg.isTruncated(), msg.getByteSize()));
-        }
-    }
-}
+  package      com . spotify . netty . handler . codec . zmtp ;   import    io . netty . buffer . ByteBuf ;  import    io . netty . buffer . Unpooled ;  import    io . netty . channel . Channel ;  import    io . netty . channel . ChannelFuture ;  import    io . netty . channel . ChannelFutureListener ;  import    io . netty . channel . ChannelHandlerContext ;  import     io . netty . handler . codec . ByteToMessageDecoder ;  import   java . util . List ;  class ZMTPFramingDecoder  extends ByteToMessageDecoder  {   private final ZMTPMessageParser  parser ;   private final ZMTPSession  session ;   public ZMTPFramingDecoder  (   final ZMTPSession session )  {    this . parser =  new ZMTPMessageParser  (  session . isEnveloped  ( ) ,  session . getSizeLimit  ( ) ,  session . getActualVersion  ( ) ) ;    this . session = session ; } 
+<<<<<<<
+  private ChannelFuture sendIdentity  (   final Channel channel )  {   final ByteBuf  msg ;  if  (  session . useLocalIdentity  ( ) )  {   msg =  Unpooled . buffer  (  2 +   session . getLocalIdentity  ( ) . length ) ;   ZMTPUtils . encodeLength  (  1 +   session . getLocalIdentity  ( ) . length , msg ) ;   msg . writeByte  ( FINAL_FLAG ) ;   msg . writeBytes  (  session . getLocalIdentity  ( ) ) ; } else  {   msg =  Unpooled . buffer  ( 2 ) ;   msg . writeByte  ( 1 ) ;   msg . writeByte  ( FINAL_FLAG ) ; }  return  channel . writeAndFlush  ( msg ) ; }
+=======
+>>>>>>>
+   private boolean handleRemoteIdentity  (   final ByteBuf buffer )  throws ZMTPException  {   buffer . markReaderIndex  ( ) ;   final  long  len =  ZMTPUtils . decodeLength  ( buffer ) ;  if  (  len > 256 )  {  throw  new ZMTPException  ( "Remote identity longer than the allowed 255 octets" ) ; }  if  (   len ==  - 1 ||   buffer . readableBytes  ( ) < len )  {   buffer . resetReaderIndex  ( ) ;  return false ; }   buffer . readByte  ( ) ;  if  (  len == 1 )  {   session . setRemoteIdentity  ( null ) ; } else  {   final   byte  [ ]  identity =  new  byte  [   (  int ) len - 1 ] ;   buffer . readBytes  ( identity ) ;   session . setRemoteIdentity  ( identity ) ; }  return true ; }    @ Override public void channelActive  (   final ChannelHandlerContext ctx )  throws Exception  {    this . session . setChannel  (  ctx . channel  ( ) ) ;    sendIdentity  (  ctx . channel  ( ) ) . addListener  (  new ChannelFutureListener  ( )  {    @ Override public void operationComplete  (   final ChannelFuture future )  throws Exception  {  if  (  future . isSuccess  ( ) )  {   ctx . fireChannelActive  ( ) ; } else  {  throw  new ZMTPException  ( "handshake failed" ,  future . cause  ( ) ) ; } } } ) ; }    @ Override protected void decode  (  ChannelHandlerContext ctx ,  ByteBuf in ,   List  < Object > out )  throws Exception  {  if  (   in . readableBytes  ( ) < 2 )  {  return ; }  if  (   session . getRemoteIdentity  ( ) == null )  {  if  (  !  handleRemoteIdentity  ( in ) )  {  return ; } }  ZMTPParsedMessage  msg =  parser . parse  ( in ) ;  if  (  msg == null )  {  return ; }   out . add  (  new ZMTPIncomingMessage  ( session ,  msg . getMessage  ( ) ,  msg . isTruncated  ( ) ,  msg . getByteSize  ( ) ) ) ; } 
+<<<<<<<
+=======
+   @ Override protected Object decode  (  ChannelHandlerContext ctx ,  Channel channel ,  ChannelBuffer buffer )  throws Exception  {   final ZMTPParsedMessage  msg =  parser . parse  ( buffer ) ;  if  (  msg == null )  {  return null ; }  return  new ZMTPIncomingMessage  ( session ,  msg . getMessage  ( ) ,  msg . isTruncated  ( ) ,  msg . getByteSize  ( ) ) ; }
+>>>>>>>
+ }
--- src/main/java/com/spotify/netty/handler/codec/zmtp/ZMTPFramingEncoder.java
+++ src/main/java/com/spotify/netty/handler/codec/zmtp/ZMTPFramingEncoder.java
@@ -1,49 +1,6 @@
-/*
- * Copyright (c) 2012-2013 Spotify AB
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- */
-
-package com.spotify.netty.handler.codec.zmtp;
-
-import io.netty.buffer.ByteBuf;
-import io.netty.buffer.Unpooled;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.handler.codec.MessageToMessageEncoder;
-
-import java.util.List;
-
-/**
- * Netty encoder for ZMTP messages.
- */
-class ZMTPFramingEncoder extends MessageToMessageEncoder<ZMTPMessage> {
-
-    private final ZMTPSession session;
-
-    public ZMTPFramingEncoder(final ZMTPSession session) {
-        this.session = session;
-    }
-
-    @Override
-    protected void encode(ChannelHandlerContext ctx, ZMTPMessage msg, List<Object> out) throws Exception {
-        // TODO (dano): integrate with write batching to avoid buffer creation and reduce garbage
-
-        final int size = ZMTPUtils.messageSize(
-                msg, session.isEnveloped(), session.getActualVersion());
-        final ByteBuf buffer = Unpooled.buffer(size);
-
-        ZMTPUtils.writeMessage(msg, buffer, session.isEnveloped(), session.getActualVersion());
-
-        out.add(buffer);
-    }
-}
+  package      com . spotify . netty . handler . codec . zmtp ;   import    io . netty . buffer . ByteBuf ;  import    io . netty . channel . ChannelHandlerContext ;  import     io . netty . handler . codec . MessageToByteEncoder ;  class ZMTPFramingEncoder  extends  MessageToByteEncoder  < ZMTPMessage >  {   private final ZMTPSession  session ;   public ZMTPFramingEncoder  (   final ZMTPSession session )  {    this . session = session ; }    @ Override protected void encode  (  ChannelHandlerContext ctx ,  ZMTPMessage msg ,  ByteBuf out )  throws Exception  {   ZMTPUtils . writeMessage  ( msg , out ,  session . isEnveloped  ( ) ) ; } 
+<<<<<<<
+=======
+   @ Override protected Object encode  (   final ChannelHandlerContext channelHandlerContext ,   final Channel channel ,   final Object o )  throws Exception  {  if  (  !  (  o instanceof ZMTPMessage ) )  {  return o ; }   final ZMTPMessage  message =  ( ZMTPMessage ) o ;   final  int  size =  ZMTPUtils . messageSize  ( message ,  session . isEnveloped  ( ) ,  session . getActualVersion  ( ) ) ;   final ChannelBuffer  buffer =  ChannelBuffers . buffer  ( size ) ;   ZMTPUtils . writeMessage  ( message , buffer ,  session . isEnveloped  ( ) ,  session . getActualVersion  ( ) ) ;  return buffer ; }
+>>>>>>>
+ }
