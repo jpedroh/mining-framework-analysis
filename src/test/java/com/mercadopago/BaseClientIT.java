19e87e62e25234cc75dad112f443cab5e4ee976c
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 23:38:04 +0000

    Mining Framework Analysis

--- src/test/java/com/mercadopago/BaseClientIT.java
+++ src/test/java/com/mercadopago/BaseClientIT.java
@@ -1,27 +1,20 @@
 package com.mercadopago;
-
 import com.mercadopago.core.MPRequestOptions;
 import org.junit.jupiter.api.BeforeAll;
 
 /** BaseClientIT class. */
 public abstract class BaseClientIT {
-
   protected static final int DEFAULT_TIMEOUT = 2000;
 
   protected static final String accessToken = System.getenv("ACCESS_TOKEN");
 
-  @BeforeAll
-  static void setup() {
+  @BeforeAll static void setup() {
     MercadoPagoConfig.setAccessToken(accessToken);
     MercadoPagoConfig.setHttpClient(null);
   }
 
   protected static MPRequestOptions buildRequestOptions() {
-    return MPRequestOptions.builder()
-        .connectionTimeout(DEFAULT_TIMEOUT)
-        .connectionRequestTimeout(DEFAULT_TIMEOUT)
-        .socketTimeout(DEFAULT_TIMEOUT)
-        .build();
+    return MPRequestOptions.builder().connectionTimeout(DEFAULT_TIMEOUT).connectionRequestTimeout(DEFAULT_TIMEOUT).socketTimeout(DEFAULT_TIMEOUT).build();
   }
 
   protected static String generateTestEmail() {
