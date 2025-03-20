042e134f112f73b3cbee29a06803beee4b1a5c2c
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 03:04:33 +0000

    Mining Framework Analysis

--- src/main/java/com/jeroenreijn/examples/configuration/WebMvcConfig.java
+++ src/main/java/com/jeroenreijn/examples/configuration/WebMvcConfig.java
@@ -1,9 +1,7 @@
 package com.jeroenreijn.examples.configuration;
-
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
-
 import com.jeroenreijn.examples.view.*;
 import org.fusesource.scalate.spring.view.ScalateViewResolver;
 import org.rythmengine.spring.web.RythmConfigurer;
@@ -32,298 +30,241 @@ import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 import org.thymeleaf.templatemode.TemplateMode;
-
 import com.github.enpassant.ickenham.springmvc.IckenhamViewResolver;
 import com.github.jknack.handlebars.springmvc.HandlebarsViewResolver;
 import com.jeroenreijn.examples.repository.InMemoryPresentationsRepository;
 import com.jeroenreijn.examples.repository.PresentationsRepository;
 import com.x5.template.spring.ChunkTemplateView;
-
 import de.neuland.jade4j.JadeConfiguration;
 import de.neuland.jade4j.spring.template.SpringTemplateLoader;
 import de.neuland.jade4j.spring.view.JadeViewResolver;
 import httl.web.springmvc.HttlViewResolver;
 
-@Configuration
-@EnableWebMvc
-@ComponentScan(basePackages = { "com.jeroenreijn.examples.controller", "com.jeroenreijn.examples.factory" })
-public class WebMvcConfig implements ApplicationContextAware, WebMvcConfigurer {
-	private ApplicationContext applicationContext;
+@Configuration @EnableWebMvc @ComponentScan(basePackages = { "com.jeroenreijn.examples.controller", "com.jeroenreijn.examples.factory" }) public class WebMvcConfig implements ApplicationContextAware, WebMvcConfigurer {
+  private ApplicationContext applicationContext;
 
-	@Override
-	public void setApplicationContext(final ApplicationContext applicationContext) throws BeansException {
-		this.applicationContext = applicationContext;
-	}
+  @Override public void setApplicationContext(final ApplicationContext applicationContext) throws BeansException {
+    this.applicationContext = applicationContext;
+  }
 
-	@Override
-	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-		configurer.enable();
-	}
+  @Override public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+    configurer.enable();
+  }
 
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/robots.txt").addResourceLocations("/robots.txt");
-		registry.addResourceHandler("/webjars/**").addResourceLocations("/webjars/");
-		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
-	}
+  @Override public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry.addResourceHandler("/robots.txt").addResourceLocations("/robots.txt");
+    registry.addResourceHandler("/webjars/**").addResourceLocations("/webjars/");
+    registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+  }
 
-	@Bean
-	public MessageSource messageSource() {
-		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
-		messageSource.setBasename("classpath:/messages");
-		messageSource.setDefaultEncoding("UTF-8");
+  @Bean public MessageSource messageSource() {
+    ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+    messageSource.setBasename("classpath:/messages");
+    messageSource.setDefaultEncoding("UTF-8");
+    return messageSource;
+  }
 
-		return messageSource;
-	}
+  @Bean public LocaleResolver localeResolver() {
+    SessionLocaleResolver slr = new SessionLocaleResolver();
+    slr.setDefaultLocale(Locale.US);
+    return slr;
+  }
 
-	@Bean
-	public LocaleResolver localeResolver() {
-		SessionLocaleResolver slr = new SessionLocaleResolver();
-		slr.setDefaultLocale(Locale.US);
+  @Bean public PresentationsRepository presentationsRepository() {
+    PresentationsRepository inMemory = new InMemoryPresentationsRepository();
+    return inMemory;
+  }
 
-		return slr;
-	}
+  @Bean public SpringResourceTemplateResolver thymeleafTemplateResolver() {
+    SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+    templateResolver.setApplicationContext(this.applicationContext);
+    templateResolver.setPrefix("/WEB-INF/thymeleaf/");
+    templateResolver.setSuffix(".html");
+    templateResolver.setTemplateMode(TemplateMode.HTML);
+    templateResolver.setCacheable(false);
+    templateResolver.setCharacterEncoding("UTF-8");
+    return templateResolver;
+  }
 
-	@Bean
-	public PresentationsRepository presentationsRepository() {
-		PresentationsRepository inMemory = new InMemoryPresentationsRepository();
+  @Bean public SpringTemplateEngine thymeleafTemplateEngine() {
+    SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+    templateEngine.setTemplateResolver(thymeleafTemplateResolver());
+    return templateEngine;
+  }
 
-		return inMemory;
-	}
+  @Bean public ViewResolver thymeleafViewResolver() {
+    ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+    viewResolver.setViewNames(new String[] { "*-thymeleaf" });
+    viewResolver.setTemplateEngine(thymeleafTemplateEngine());
+    viewResolver.setCharacterEncoding("UTF-8");
+    viewResolver.setCache(false);
+    return viewResolver;
+  }
 
-	@Bean
-	public SpringResourceTemplateResolver thymeleafTemplateResolver() {
-		SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
-		templateResolver.setApplicationContext(this.applicationContext);
-		templateResolver.setPrefix("/WEB-INF/thymeleaf/");
-		templateResolver.setSuffix(".html");
-		templateResolver.setTemplateMode(TemplateMode.HTML);
-		templateResolver.setCacheable(false);
-		templateResolver.setCharacterEncoding("UTF-8");
+  @Bean public ViewResolver handlebarsViewResolver() {
+    HandlebarsViewResolver viewResolver = new HandlebarsViewResolver();
+    viewResolver.setViewNames("*-handlebars");
+    viewResolver.setPrefix("/WEB-INF/handlebars/");
+    viewResolver.setSuffix(".hbs");
+    viewResolver.setCache(false);
+    return viewResolver;
+  }
 
-		return templateResolver;
-	}
+  @Bean(name = "chunkTemplatesConfig") @Scope(value = "prototype") public Map<String, String> chunkTemplatesConfig() {
+    Map<String, String> config = new HashMap<>();
+    config.put("default_extension", "chtml");
+    config.put("cache_minutes", "0");
+    config.put("layers", "");
+    config.put("theme_path", "");
+    config.put("hide_errors", "FALSE");
+    config.put("error_log", "");
+    config.put("encoding", "UTF-8");
+    config.put("locale", "");
+    config.put("filters", "");
+    return config;
+  }
 
-	@Bean
-	public SpringTemplateEngine thymeleafTemplateEngine() {
-		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
-		templateEngine.setTemplateResolver(thymeleafTemplateResolver());
+  @Bean public ViewResolver chunkViewResolver() {
+    InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+    viewResolver.setViewClass(ChunkTemplateView.class);
+    viewResolver.setPrefix("/WEB-INF/chunk/");
+    viewResolver.setViewNames("*-chunk");
+    viewResolver.setSuffix(".chtml");
+    viewResolver.setCache(false);
+    viewResolver.setRequestContextAttribute("rc");
+    viewResolver.setContentType("text/html;charset=UTF-8");
+    return viewResolver;
+  }
 
-		return templateEngine;
-	}
+  @Bean public ViewResolver jspViewResolver() {
+    InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+    viewResolver.setViewClass(JstlView.class);
+    viewResolver.setPrefix("/WEB-INF/jsp/");
+    viewResolver.setViewNames("*-jsp");
+    viewResolver.setSuffix(".jsp");
+    viewResolver.setCache(false);
+    viewResolver.setRequestContextAttribute("rc");
+    return viewResolver;
+  }
 
-	@Bean
-	public ViewResolver thymeleafViewResolver() {
-		ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
-		viewResolver.setViewNames(new String[] { "*-thymeleaf" });
-		viewResolver.setTemplateEngine(thymeleafTemplateEngine());
-		viewResolver.setCharacterEncoding("UTF-8");
-		viewResolver.setCache(false);
+  @Bean public ViewResolver httlViewResolver() {
+    HttlViewResolver viewResolver = new HttlViewResolver();
+    viewResolver.setContentType("text/html;charset=UTF-8");
+    viewResolver.setViewNames("*-httl");
+    viewResolver.setCache(false);
+    return viewResolver;
+  }
 
-		return viewResolver;
-	}
+  @Bean public SpringTemplateLoader jade4jTemplateLoader() {
+    SpringTemplateLoader templateLoader = new SpringTemplateLoader();
+    templateLoader.setEncoding("UTF-8");
+    return templateLoader;
+  }
 
-	@Bean
-	public ViewResolver handlebarsViewResolver() {
-		HandlebarsViewResolver viewResolver = new HandlebarsViewResolver();
-		viewResolver.setViewNames("*-handlebars");
-		viewResolver.setPrefix("/WEB-INF/handlebars/");
-		viewResolver.setSuffix(".hbs");
-		viewResolver.setCache(false);
+  @Bean public JadeConfiguration jadeConfiguration() {
+    JadeConfiguration config = new JadeConfiguration();
+    config.setPrettyPrint(true);
+    config.setCaching(false);
+    config.setTemplateLoader(applicationContext.getBean(SpringTemplateLoader.class));
+    return config;
+  }
 
-		return viewResolver;
-	}
+  @Bean public ViewResolver jadeViewResolver() {
+    JadeViewResolver viewResolver = new JadeViewResolver();
+    viewResolver.setPrefix("/WEB-INF/jade/");
+    viewResolver.setSuffix(".jade");
+    viewResolver.setViewNames("*-jade");
+    viewResolver.setRenderExceptions(true);
+    viewResolver.setCache(false);
+    viewResolver.setConfiguration(applicationContext.getBean(JadeConfiguration.class));
+    return viewResolver;
+  }
 
-	@Bean(name = "chunkTemplatesConfig")
-	@Scope("prototype")
-	public Map<String, String> chunkTemplatesConfig() {
-		Map<String, String> config = new HashMap<>();
-		config.put("default_extension", "chtml");
-		config.put("cache_minutes", "0");
-		config.put("layers", "");
-		config.put("theme_path", "");
-		config.put("hide_errors", "FALSE");
-		config.put("error_log", "");
-		config.put("encoding", "UTF-8");
-		config.put("locale", "");
-		config.put("filters", "");
+  @Bean public ViewResolver scalateViewResolver() {
+    ScalateViewResolver viewResolver = new ScalateViewResolver();
+    viewResolver.setPrefix("/WEB-INF/scalate/");
+    viewResolver.setSuffix(".scaml");
+    viewResolver.setViewNames("*-scalate");
+    viewResolver.setRequestContextAttribute("rc");
+    viewResolver.setCache(false);
+    viewResolver.setContentType("text/html;charset=UTF-8");
+    return viewResolver;
+  }
 
-		return config;
-	}
+  @Bean public ViewResolver htmlFlowViewResolver() {
+    HtmlFlowViewResolver viewResolver = new HtmlFlowViewResolver();
+    viewResolver.setViewNames("*-htmlFlow");
+    viewResolver.setCache(false);
+    return viewResolver;
+  }
 
-	@Bean
-	public ViewResolver chunkViewResolver() {
-		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-		viewResolver.setViewClass(ChunkTemplateView.class);
-		viewResolver.setPrefix("/WEB-INF/chunk/");
-		viewResolver.setViewNames("*-chunk");
-		viewResolver.setSuffix(".chtml");
-		viewResolver.setCache(false);
-		viewResolver.setRequestContextAttribute("rc");
-		viewResolver.setContentType("text/html;charset=UTF-8");
+  @Bean public ViewResolver trimouViewResolver() {
+    MessageSource messageSource = applicationContext.getBean(MessageSource.class);
+    TrimouViewResolver viewResolver = new TrimouViewResolver(messageSource);
+    viewResolver.setPrefix("classpath:/templates/trimou/");
+    viewResolver.setSuffix(".trimou");
+    viewResolver.setViewNames("*-trimou");
+    viewResolver.setCache(false);
+    return viewResolver;
+  }
 
-		return viewResolver;
-	}
+  @Bean public ViewResolver rockerViewResolver() {
+    RockerViewResolver viewResolver = new RockerViewResolver();
+    viewResolver.setViewNames("*-rocker");
+    viewResolver.setCache(false);
+    return viewResolver;
+  }
 
-	@Bean
-	public ViewResolver jspViewResolver() {
-		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-		viewResolver.setViewClass(JstlView.class);
-		viewResolver.setPrefix("/WEB-INF/jsp/");
-		viewResolver.setViewNames("*-jsp");
-		viewResolver.setSuffix(".jsp");
-		viewResolver.setCache(false);
-		viewResolver.setRequestContextAttribute("rc");
+  @Bean public ViewResolver ickenhamViewResolver() {
+    IckenhamViewResolver viewResolver = new IckenhamViewResolver();
+    viewResolver.setPrefix("/WEB-INF/ickenham/");
+    viewResolver.setSuffix(".hbs");
+    viewResolver.setViewNames("*-ickenham");
+    viewResolver.setRequestContextAttribute("rc");
+    viewResolver.setCache(false);
+    viewResolver.setContentType("text/html;charset=UTF-8");
+    return viewResolver;
+  }
 
-		return viewResolver;
-	}
+  @Bean public RythmConfigurer rythmConfigurer() {
+    RythmConfigurer conf = new RythmConfigurer();
+    conf.setDevMode(true);
+    conf.setResourceLoaderPath("/WEB-INF/rythm/");
+    conf.setAutoImports("com.jeroenreijn.examples.model.*");
+    return conf;
+  }
 
-	@Bean
-	public ViewResolver httlViewResolver() {
-		HttlViewResolver viewResolver = new HttlViewResolver();
-		viewResolver.setContentType("text/html;charset=UTF-8");
-		viewResolver.setViewNames("*-httl");
-		viewResolver.setCache(false);
+  @Bean public ViewResolver rythmViewResolver() {
+    RythmViewResolver viewResolver = new RythmViewResolver();
+    viewResolver.setPrefix("/WEB-INF/rythm/");
+    viewResolver.setSuffix(".html");
+    viewResolver.setViewNames("*-rythm");
+    viewResolver.setCache(false);
+    viewResolver.setContentType("text/html;charset=UTF-8");
+    return viewResolver;
+  }
 
-		return viewResolver;
-	}
+  @Bean public LiqpViewResolver liqpViewResolver() {
+    LiqpViewResolver viewResolver = new LiqpViewResolver(applicationContext.getBean(MessageSource.class));
+    viewResolver.setViewClass(LiqpView.class);
+    viewResolver.setPrefix("classpath:./templates/liqp/");
+    viewResolver.setSuffix(".liqp");
+    viewResolver.setViewNames("*-liqp");
+    viewResolver.setCache(false);
+    viewResolver.setContentType("text/html;charset=UTF-8");
+    return viewResolver;
+  }
 
-	@Bean
-	public SpringTemplateLoader jade4jTemplateLoader() {
-		SpringTemplateLoader templateLoader = new SpringTemplateLoader();
-		templateLoader.setEncoding("UTF-8");
+  @Bean public ViewResolver kotlinxHtmlViewResolver() {
+    KotlinxHtmlViewResolver viewResolver = new KotlinxHtmlViewResolver();
+    viewResolver.setViewNames("*-kotlinx");
+    viewResolver.setCache(false);
+    return viewResolver;
+  }
 
-		return templateLoader;
-	}
-
-	@Bean
-	public JadeConfiguration jadeConfiguration() {
-		JadeConfiguration config = new JadeConfiguration();
-		config.setPrettyPrint(true);
-		config.setCaching(false);
-		config.setTemplateLoader(applicationContext.getBean(SpringTemplateLoader.class));
-
-		return config;
-	}
-
-	@Bean
-	public ViewResolver jadeViewResolver() {
-		JadeViewResolver viewResolver = new JadeViewResolver();
-		viewResolver.setPrefix("/WEB-INF/jade/");
-		viewResolver.setSuffix(".jade");
-		viewResolver.setViewNames("*-jade");
-		viewResolver.setRenderExceptions(true);
-		viewResolver.setCache(false);
-		viewResolver.setConfiguration(applicationContext.getBean(JadeConfiguration.class));
-
-		return viewResolver;
-	}
-
-	@Bean
-	public ViewResolver scalateViewResolver() {
-		ScalateViewResolver viewResolver = new ScalateViewResolver();
-		viewResolver.setPrefix("/WEB-INF/scalate/");
-		viewResolver.setSuffix(".scaml");
-		viewResolver.setViewNames("*-scalate");
-		viewResolver.setRequestContextAttribute("rc");
-		viewResolver.setCache(false);
-		viewResolver.setContentType("text/html;charset=UTF-8");
-
-		return viewResolver;
-	}
-
-	@Bean
-	public ViewResolver htmlFlowViewResolver() {
-		HtmlFlowViewResolver viewResolver = new HtmlFlowViewResolver();
-		viewResolver.setViewNames("*-htmlFlow");
-		viewResolver.setCache(false);
-
-		return viewResolver;
-	}
-
-	@Bean
-	public ViewResolver trimouViewResolver() {
-		MessageSource messageSource = applicationContext.getBean(MessageSource.class);
-		TrimouViewResolver viewResolver = new TrimouViewResolver(messageSource);
-		viewResolver.setPrefix("classpath:/templates/trimou/");
-		viewResolver.setSuffix(".trimou");
-		viewResolver.setViewNames("*-trimou");
-		viewResolver.setCache(false);
-
-		return viewResolver;
-	}
-
-	@Bean
-	public ViewResolver rockerViewResolver() {
-		RockerViewResolver viewResolver = new RockerViewResolver();
-		viewResolver.setViewNames("*-rocker");
-		viewResolver.setCache(false);
-
-		return viewResolver;
-	}
-
-	@Bean
-	public ViewResolver ickenhamViewResolver() {
-		IckenhamViewResolver viewResolver = new IckenhamViewResolver();
-		viewResolver.setPrefix("/WEB-INF/ickenham/");
-		viewResolver.setSuffix(".hbs");
-		viewResolver.setViewNames("*-ickenham");
-		viewResolver.setRequestContextAttribute("rc");
-		viewResolver.setCache(false);
-		viewResolver.setContentType("text/html;charset=UTF-8");
-
-		return viewResolver;
-	}
-
-	@Bean
-	public RythmConfigurer rythmConfigurer() {
-		RythmConfigurer conf = new RythmConfigurer();
-		conf.setDevMode(true);
-		conf.setResourceLoaderPath("/WEB-INF/rythm/");
-		conf.setAutoImports("com.jeroenreijn.examples.model.*");
-
-		return conf;
-	}
-
-	@Bean
-	public ViewResolver rythmViewResolver() {
-		RythmViewResolver viewResolver = new RythmViewResolver();
-		viewResolver.setPrefix("/WEB-INF/rythm/");
-		viewResolver.setSuffix(".html");
-		viewResolver.setViewNames("*-rythm");
-		viewResolver.setCache(false);
-		viewResolver.setContentType("text/html;charset=UTF-8");
-
-		return viewResolver;
-	}
-
-	@Bean
-	public LiqpViewResolver liqpViewResolver() {
-		LiqpViewResolver viewResolver = new LiqpViewResolver(applicationContext.getBean(MessageSource.class));
-		viewResolver.setViewClass(LiqpView.class);
-		viewResolver.setPrefix("classpath:./templates/liqp/");
-		viewResolver.setSuffix(".liqp");
-		viewResolver.setViewNames("*-liqp");
-		viewResolver.setCache(false);
-		viewResolver.setContentType("text/html;charset=UTF-8");
-
-		return viewResolver;
-	}
-
-	@Bean
-	public ViewResolver kotlinxHtmlViewResolver() {
-		KotlinxHtmlViewResolver viewResolver = new KotlinxHtmlViewResolver();
-		viewResolver.setViewNames("*-kotlinx");
-		viewResolver.setCache(false);
-
-		return viewResolver;
-	}
-
-	@Controller
-	static class FaviconController {
-		@RequestMapping("favicon.ico")
-		String favicon() {
-			return "forward:/resources/images/favicon.ico";
-		}
-	}
+  @Controller static class FaviconController {
+    @RequestMapping(value = "favicon.ico") String favicon() {
+      return "forward:/resources/images/favicon.ico";
+    }
+  }
 }
