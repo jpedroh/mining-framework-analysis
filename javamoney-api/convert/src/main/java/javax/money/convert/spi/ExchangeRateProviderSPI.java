0a516e18826f825119d322f110063b4cf046664a
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 05:08:56 +0000

    Mining Framework Analysis

--- javamoney-api/convert/src/main/java/javax/money/convert/spi/ExchangeRateProviderSPI.java
+++ javamoney-api/convert/src/main/java/javax/money/convert/spi/ExchangeRateProviderSPI.java
@@ -1,36 +1,4 @@
-/*
- * Copyright (c) 2012-2013, Credit Suisse
- *
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- *  * Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- *
- *  * Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- *
- *  * Neither the name of JSR-354 nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
 package javax.money.convert.spi;
-
 import javax.money.CurrencyUnit;
 import javax.money.convert.ExchangeRate;
 import javax.money.convert.ExchangeRateType;
@@ -41,9 +9,8 @@ import javax.money.convert.ExchangeRateType;
  * @version 0.1.1
  */
 public interface ExchangeRateProviderSPI {
-
-	/**
-	 * Get an exchange rate for the given parameters.
+  /**
+	 * Get exchange rates for the given parameters.
 	 * 
 	 * @param source
 	 *            the source currency.
@@ -56,11 +23,10 @@ public interface ExchangeRateProviderSPI {
 	 *            If the quote should be deferred.
 	 * @return the according exchange rate, or null.
 	 */
-	public ExchangeRate getExchangeRate(CurrencyUnit source,
-			CurrencyUnit target, ExchangeRateType type, boolean deferred);
+  public ExchangeRate getExchangeRate(CurrencyUnit source, CurrencyUnit target, ExchangeRateType type, boolean deferred);
 
-	/**
-	 * Get an exchange rate for the given parameters.
+  /**
+	 * Get exchange rates for the given parameters.
 	 * 
 	 * @param source
 	 *            the source currency.
@@ -76,7 +42,11 @@ public interface ExchangeRateProviderSPI {
 	 *            how long the quote should be considered valid.
 	 * @return the according exchange rate, or null.
 	 */
-	public ExchangeRate getExchangeRate(CurrencyUnit source,
-			CurrencyUnit target, ExchangeRateType type, Long timestamp);
-
+  public ExchangeRate getExchangeRate(CurrencyUnit source, CurrencyUnit target, ExchangeRateType type, 
+<<<<<<< /usr/src/app/output/javamoney/jsr354-api/75bfd2e554316270cfbd84c551e34e6f07c2b4d5/javamoney-api/convert/src/main/java/javax/money/convert/spi/ExchangeRateProviderSPI.java/left.java
+  Long timestamp
+=======
+  long timestamp
+>>>>>>> /usr/src/app/output/javamoney/jsr354-api/75bfd2e554316270cfbd84c551e34e6f07c2b4d5/javamoney-api/convert/src/main/java/javax/money/convert/spi/ExchangeRateProviderSPI.java/right.java
+  );
 }
