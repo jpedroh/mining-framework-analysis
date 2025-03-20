0f0235e6d098f9df29167fc04e84204c5749c983
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 07:07:31 +0000

    Mining Framework Analysis

--- src/benchmark/java/de/ruedigermoeller/abstraktor/sample/ForkJoinRecursiveDeep.java
+++ src/benchmark/java/de/ruedigermoeller/abstraktor/sample/ForkJoinRecursiveDeep.java
@@ -1,5 +1,4 @@
 package de.ruedigermoeller.abstraktor.sample;
-
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.ForkJoinTask;
 import java.util.concurrent.RecursiveTask;
@@ -7,83 +6,83 @@ import java.util.concurrent.RecursiveTask;
 /**
  * originally created by Aleksey Shipilev
  * 
- * added some minor correction, adapted to blog benchmark 'style'
+ * added some minor correction to actually compute Pi, adapted to blog benchmark 'style'
  * 
  */
 public class ForkJoinRecursiveDeep {
-
-    public static double calculatePi(int sliceNr) {
-        double acc = 0.0;
-        for (int i = sliceNr * ITERS; i <= ((sliceNr + 1) * ITERS - 1); i++) {
-            acc += 4.0 * (1 - (i % 2) * 2) / (2 * i + 1);
-        }
-        return acc;
+  public static double calculatePi(int sliceNr) {
+    double acc = 0.0;
+    for (int i = sliceNr * ITERS; i <= ((sliceNr + 1) * ITERS - 1); i++) {
+      acc += 4.0 * (1 - (i % 2) * 2) / (2 * i + 1);
     }
-    
-    /*
-      The fork-join task below deeply recurses, up until the leaf
-      contains a single slice.
-     */
+    return acc;
+  }
 
-    static class PiForkJoinTask extends RecursiveTask<Double> {
-        private final int slices;
-        private final int slicesOffset;
+  static class PiForkJoinTask extends RecursiveTask<Double> {
+    private final int slices;
 
-        public PiForkJoinTask(int slices, int offset) {
-            this.slices = slices;
-            this.slicesOffset = offset;
-        }
+    private final int slicesOffset;
 
-        @Override
-        protected Double compute() {
-            if ( slices == 0 )
-                return 0.0;
-            if (slices == 1) {
-                return calculatePi(slicesOffset);
-            }
-
-            int lslices = slices / 2;
-            int rslices = slices - lslices;
-            PiForkJoinTask t1 = new PiForkJoinTask(lslices,slicesOffset);
-            PiForkJoinTask t2 = new PiForkJoinTask(rslices,lslices+slicesOffset);
-
-            ForkJoinTask.invokeAll(t1, t2);
-
-            return t1.join() + t2.join();
-        }
+    public PiForkJoinTask(int slices, int offset) {
+      this.slices = slices;
+      this.slicesOffset = offset;
     }
 
-    public double run() throws InterruptedException {
-        return pool.invoke(new PiForkJoinTask(SLICES,0));
+    @Override protected Double compute() {
+      if (slices == 0) {
+        return 0.0;
+      }
+      if (slices == 1) {
+        return calculatePi(slicesOffset);
+      }
+      int lslices = slices / 2;
+      int rslices = slices - lslices;
+      PiForkJoinTask t1 = new PiForkJoinTask(lslices, slicesOffset);
+      PiForkJoinTask t2 = new PiForkJoinTask(rslices, lslices + slicesOffset);
+      ForkJoinTask.invokeAll(t1, t2);
+      return t1.join() + t2.join();
     }
+  }
 
-    public static final int ITERS = 100;
-    public static final int SLICES = 1000*1000;
-    private static int NUM_CORE = 16;
+  public double run() throws InterruptedException {
+    return pool.invoke(new PiForkJoinTask(SLICES, 0));
+  }
 
-    static ForkJoinPool pool;
-    public static void main(String arg[] ) throws InterruptedException {
+  public static final int ITERS = 100;
 
-        String res[] = new String[NUM_CORE];
-        for ( int i = 1; i <= NUM_CORE; i++ ) {
-            long sum = 0;
-            System.out.println("--------------------------");
-            pool = new ForkJoinPool(i);
-            for ( int ii = 0; ii < 20; ii++ ) {
-                long tim = System.currentTimeMillis();
-                final ForkJoinRecursiveDeep dis = new ForkJoinRecursiveDeep();
-                System.out.println(dis.run());
-                long t = System.currentTimeMillis()-tim;
-                if ( ii >= 10 )
-                    sum += t;
-            }
-            res[i-1] = i+": "+(sum/10);
-            pool.shutdown();
+  public static final int SLICES = 1000 * 1000;
+
+  private static int NUM_CORE = 16;
+
+  static ForkJoinPool pool;
+
+  public static void main(String[] arg) throws InterruptedException {
+
+<<<<<<< Unknown file: This is a bug in JDime.
+=======
+    int NUM_CORE = 16;
+>>>>>>> /usr/src/app/output/ruedigermoeller/kontraktor/54fa3de6899427de183bc5434da2d17bbee13023/src/benchmark/java/de/ruedigermoeller/abstraktor/sample/ForkJoinRecursiveDeep.java/right.java
+
+    String res[] = new String[NUM_CORE];
+    for (int i = 1; i <= NUM_CORE; i++) {
+      long sum = 0;
+      System.out.println("--------------------------");
+      pool = new ForkJoinPool(i);
+      for (int ii = 0; ii < 20; ii++) {
+        long tim = System.currentTimeMillis();
+        final ForkJoinRecursiveDeep dis = new ForkJoinRecursiveDeep();
+        System.out.println(dis.run());
+        long t = System.currentTimeMillis() - tim;
+        if (ii >= 10) {
+          sum += t;
         }
-        for (int i = 0; i < res.length; i++) {
-            String re = res[i];
-            System.out.println(re);
-        }
+      }
+      res[i - 1] = i + ": " + (sum / 10);
+      pool.shutdown();
     }
-
+    for (int i = 0; i < res.length; i++) {
+      String re = res[i];
+      System.out.println(re);
+    }
+  }
 }
