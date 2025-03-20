0d50a92854977495fc750d11bdcafa9c4a4454cb
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 04:55:06 +0000

    Mining Framework Analysis

--- src/main/java/oripa/file/InitDataFileReader.java
+++ src/main/java/oripa/file/InitDataFileReader.java
@@ -1,23 +1,4 @@
-/**
- * ORIPA - Origami Pattern Editor
- * Copyright (C) 2013-     ORIPA OSS Project  https://github.com/oripa/oripa
- * Copyright (C) 2005-2009 Jun Mitani         http://mitani.cs.tsukuba.ac.jp/
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 package oripa.file;
-
 import java.beans.XMLDecoder;
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
@@ -29,21 +10,18 @@ import java.io.FileInputStream;
  *
  */
 public class InitDataFileReader {
-	/**
+  /**
 	 *
 	 * @param filePath
 	 *            init file location
 	 * @return init data in filePath or default init data otherwise
 	 */
-	public InitData read(final String filePath) {
-		InitData initData = new InitData();
-		try (var fis = new FileInputStream(filePath);
-				var bis = new BufferedInputStream(fis);
-				var dec = new XMLDecoder(bis);) {
-			initData = (InitData) dec.readObject();
-		} catch (Exception e) {
-		}
-
-		return initData;
-	}
+  public InitData read(final String filePath) {
+    InitData initData = new InitData();
+    try (var fis = new FileInputStream(filePath); var bis = new BufferedInputStream(fis); var dec = new XMLDecoder(bis)) {
+      initData = (InitData) dec.readObject();
+    } catch (Exception e) {
+    }
+    return initData;
+  }
 }
