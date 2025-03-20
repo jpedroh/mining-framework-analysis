64201e1608921c8f5904e701e7b05c549a7ca831
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 05:05:34 +0000

    Mining Framework Analysis

--- frontend-plugin-core/src/main/java/com/github/eirslett/maven/plugins/frontend/lib/FrontendPluginFactory.java
+++ frontend-plugin-core/src/main/java/com/github/eirslett/maven/plugins/frontend/lib/FrontendPluginFactory.java
@@ -1,74 +1,72 @@
 package com.github.eirslett.maven.plugins.frontend.lib;
-
 import java.io.File;
 
 public final class FrontendPluginFactory {
-    
-    private static final Platform defaultPlatform = Platform.guess();
-    private static final String DEFAULT_CACHE_PATH = "cache";
+  private static final Platform defaultPlatform = Platform.guess();
 
-    private final File workingDirectory;
-    private final File installDirectory;
-    private final CacheResolver cacheResolver;
+  private static final String DEFAULT_CACHE_PATH = "cache";
 
-    public FrontendPluginFactory(File workingDirectory, File installDirectory){
-        this(workingDirectory, installDirectory, getDefaultCacheResolver(installDirectory));
-    }
+  private final File workingDirectory;
 
-    public FrontendPluginFactory(File workingDirectory, File installDirectory, CacheResolver cacheResolver){
-        this.workingDirectory = workingDirectory;
-        this.installDirectory = installDirectory;
-        this.cacheResolver = cacheResolver;
-    }
+  private final File installDirectory;
 
-    public NodeAndNPMInstaller getNodeAndNPMInstaller(ProxyConfig proxy){
-        return new DefaultNodeAndNPMInstaller(
-                getInstallConfig(),
-                new DefaultArchiveExtractor(),
-                new DefaultFileDownloader(proxy));
-    }
-    
-    public BowerRunner getBowerRunner(ProxyConfig proxy) {
-        return new DefaultBowerRunner(getExecutorConfig(), proxy);
-    }    
+  private final CacheResolver cacheResolver;
 
-    public JspmRunner getJspmRunner() {
-        return new DefaultJspmRunner(getExecutorConfig());
-    }
+  public FrontendPluginFactory(File workingDirectory, File installDirectory) {
+    this(workingDirectory, installDirectory, getDefaultCacheResolver(installDirectory));
+  }
 
-    public NpmRunner getNpmRunner(ProxyConfig proxy, String npmRegistryURL) {
-        return new DefaultNpmRunner(getExecutorConfig(), proxy, npmRegistryURL);
-    }
+  public FrontendPluginFactory(File workingDirectory, File installDirectory, CacheResolver cacheResolver) {
+    this.workingDirectory = workingDirectory;
+    this.installDirectory = installDirectory;
+    this.cacheResolver = cacheResolver;
+  }
 
-    public GruntRunner getGruntRunner(){
-        return new DefaultGruntRunner(getExecutorConfig());
-    }
+  public NodeAndNPMInstaller getNodeAndNPMInstaller(ProxyConfig proxy) {
+    return new DefaultNodeAndNPMInstaller(getInstallConfig(), new DefaultArchiveExtractor(), new DefaultFileDownloader(proxy));
+  }
 
-    public EmberRunner getEmberRunner() {
-        return new DefaultEmberRunner(getExecutorConfig());
-    }
+  public BowerRunner getBowerRunner(ProxyConfig proxy) {
+    return new DefaultBowerRunner(getExecutorConfig(), proxy);
+  }
 
-    public KarmaRunner getKarmaRunner(){
-        return new DefaultKarmaRunner(getExecutorConfig());
-    }
+  public JspmRunner getJspmRunner() {
+    return new DefaultJspmRunner(getExecutorConfig());
+  }
 
-    public GulpRunner getGulpRunner(){
-        return new DefaultGulpRunner(getExecutorConfig());
-    }
+  public NpmRunner getNpmRunner(ProxyConfig proxy, String npmRegistryURL) {
+    return new DefaultNpmRunner(getExecutorConfig(), proxy, npmRegistryURL);
+  }
 
-    public WebpackRunner getWebpackRunner(){
-        return new DefaultWebpackRunner(getExecutorConfig());
-    }
+  public GruntRunner getGruntRunner() {
+    return new DefaultGruntRunner(getExecutorConfig());
+  }
 
-    private NodeExecutorConfig getExecutorConfig() {
-        return new InstallNodeExecutorConfig(getInstallConfig());
-    }
+  public EmberRunner getEmberRunner() {
+    return new DefaultEmberRunner(getExecutorConfig());
+  }
 
-    private InstallConfig getInstallConfig() {
-        return new DefaultInstallConfig(installDirectory, workingDirectory, cacheResolver, defaultPlatform);
-    }
+  public KarmaRunner getKarmaRunner() {
+    return new DefaultKarmaRunner(getExecutorConfig());
+  }
 
-    private static final CacheResolver getDefaultCacheResolver(File root) {
-        return new DirectoryCacheResolver(new File(root, DEFAULT_CACHE_PATH));
-    }
+  public GulpRunner getGulpRunner() {
+    return new DefaultGulpRunner(getExecutorConfig());
+  }
+
+  public WebpackRunner getWebpackRunner() {
+    return new DefaultWebpackRunner(getExecutorConfig());
+  }
+
+  private NodeExecutorConfig getExecutorConfig() {
+    return new InstallNodeExecutorConfig(getInstallConfig());
+  }
+
+  private InstallConfig getInstallConfig() {
+    return new DefaultInstallConfig(installDirectory, workingDirectory, cacheResolver, defaultPlatform);
+  }
+
+  private static final CacheResolver getDefaultCacheResolver(File root) {
+    return new DirectoryCacheResolver(new File(root, DEFAULT_CACHE_PATH));
+  }
 }
