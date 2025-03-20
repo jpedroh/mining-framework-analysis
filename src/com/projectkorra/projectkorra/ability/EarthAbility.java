9a79a01805c684e4804ffcf00a93821f5643d0f2
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-20 09:10:18 +0000

    Mining Framework Analysis

--- src/com/projectkorra/projectkorra/ability/EarthAbility.java
+++ src/com/projectkorra/projectkorra/ability/EarthAbility.java
@@ -558,7 +558,14 @@ public abstract class EarthAbility extends ElementalAbility {
 			}
 		}
 		}
-
+	public static void playLavaBendingSound(Location loc) {
+		if (getConfig().getBoolean("Properties.Earth.PlaySound")) {
+			float volume = (float) getConfig().getDouble("Properties.Earth.LavaSound.Volume");
+			float pitch = (float) getConfig().getDouble("Properties.Earth.LavaSound.Pitch");
+			
+			loc.getWorld().playSound(loc, Sound.valueOf(getConfig().getString("Properties.Earth.LavaSound.Sound")), volume, pitch);
+		}
+	}
 
 	public static void removeAllEarthbendedBlocks() {
 		for (Block block : MOVED_EARTH.keySet()) {
