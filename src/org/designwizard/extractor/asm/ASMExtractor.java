1c43277bcaf1ed5761ee20bbd8d278a9a27ad11d
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 15:58:01 +0000

    Mining Framework Analysis

--- src/org/designwizard/extractor/asm/ASMExtractor.java
+++ src/org/designwizard/extractor/asm/ASMExtractor.java
@@ -1,5 +1,4 @@
 package org.designwizard.extractor.asm;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -12,7 +11,6 @@ import java.util.Enumeration;
 import java.util.List;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
-
 import org.designwizard.api.util.FileUtil;
 import org.designwizard.extractor.Extractor;
 import org.designwizard.extractor.asm.event.ExtractionListener;
@@ -21,75 +19,50 @@ import org.designwizard.extractor.asm.visitor.FactsExtractionClassVisitor;
 import org.objectweb.asm.ClassReader;
 
 public class ASMExtractor implements Extractor {
+  private List<ExtractionListener> managers = new ArrayList<ExtractionListener>();
 
-	private List<ExtractionListener> managers = new ArrayList<ExtractionListener>();
+  public void processJar(String fileName) throws IOException {
+    String entryName = null;
+    JarEntry jarEntry = null;
+    JarFile jarFile = new JarFile(fileName);
+    Enumeration<JarEntry> e = jarFile.entries();
+    URL[] urls = { new File(fileName).toURI().toURL() };
+    URLClassLoader classLoader = new URLClassLoader(urls, null);
+    while (e.hasMoreElements()) {
+      jarEntry = e.nextElement();
+      entryName = jarEntry.toString();
+      if (entryName.endsWith(".class")) {
+        this.processClass(entryName, classLoader);
+      }
+    }
+    jarFile.close();
+  }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.objectweb.asm.Extractor#processJar(java.lang.String)
-	 */
-	public void processJar(String fileName) throws IOException {
+  public void processDir(String fileName) throws IOException {
+    File file = new File(fileName);
+    URLClassLoader classLoader = new URLClassLoader(new URL[] { new File(fileName).toURI().toURL() });
+    if (file.isFile() && file.getName().endsWith(".class")) {
+      processClass(fileName, classLoader);
+    } else {
+      if (file.isDirectory()) {
+        File[] files = file.listFiles();
+        for (File classFile : files) {
+          processDir(fileName + File.separator + classFile.getName());
+        }
+      }
+    }
+    classLoader.close();
+  }
 
-		String entryName = null;
-		JarEntry jarEntry = null;
-		JarFile jarFile = new JarFile(fileName);
-		Enumeration<JarEntry> e = jarFile.entries();
+  private String removeDirectoryName(String fileName) {
+    int pos = fileName.indexOf(File.separator);
+    if (pos > 0) {
+      return fileName.substring(pos + 1);
+    }
+    return "";
+  }
 
-		URL[] urls = { new File(fileName).toURI().toURL() };
-		URLClassLoader classLoader = new URLClassLoader(urls, null);
-
-		while (e.hasMoreElements()) {
-			jarEntry = e.nextElement();
-			entryName = jarEntry.toString();
-			if (entryName.endsWith(".class")) {
-				this.processClass(entryName, classLoader);
-			}
-		}
-		jarFile.close();
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.objectweb.asm.Extractor#processClassesDir(java.lang.String)
-	 */
-	public void processDir(String fileName) throws IOException {
-
-		File file = new File(fileName);
-		URLClassLoader classLoader = new URLClassLoader(new URL[] { new File(fileName).toURI().toURL() });
-
-		if (file.isFile() && file.getName().endsWith(".class")) {
-
-			processClass(fileName, classLoader);
-
-		} else if (file.isDirectory()) {
-
-			File[] files = file.listFiles();
-
-			for (File classFile : files) {
-
-				processDir(fileName + File.separator + classFile.getName());
-
-			}
-
-		}
-		classLoader.close();
-	}
-
-	private String removeDirectoryName(String fileName) {
-		int pos = fileName.indexOf(File.separator);
-		if (pos > 0) {
-
-			return fileName.substring(pos + 1);
-
-		}
-
-		return "";
-
-	}
-
-	/**
+  /**
 	 * Process the class with the specified className.
 	 * 
 	 * @param className
@@ -99,70 +72,53 @@ public class ASMExtractor implements Extractor {
 	 * @throws IOException
 	 *             If an I/O error occurs.
 	 */
-	private void processClass(String className, URLClassLoader loader) throws IOException {
-		
-		String toPut = className.replace(".class", "");
+  private void processClass(String className, URLClassLoader loader) throws IOException {
+    String toPut = className.replace(".class", "");
+    InputStream in = loader.getResourceAsStream(toPut.replace('.', File.separatorChar) + ".class");
+    if (in == null) {
+      in = new FileInputStream(new File(className).getAbsoluteFile());
+    }
+    ClassReader cr = new ClassReader(in);
+    FactsEventSource cv = new FactsExtractionClassVisitor(className.replace(".class", ""));
+    cv.addListener(this.managers);
+    cr.accept(cv, 0);
+    in.close();
+  }
 
-		InputStream in = loader.getResourceAsStream(toPut.replace('.',File.separatorChar) + ".class");
+  public void addListener(ExtractionListener manager) {
+    this.managers.add(manager);
+  }
 
-		if (in == null)	in = new FileInputStream(new File(className).getAbsoluteFile());
-		ClassReader cr = new ClassReader(in);
-		
+  public void processEar(String pathOfEarFile, String... internalPath) throws IOException {
+    File directory = null;
+    try {
+      directory = FileUtil.extractFilesFromZipFile(pathOfEarFile, internalPath[0]);
+      File resultFile = new File(directory.getAbsolutePath() + File.separator + internalPath[0]);
+      if (resultFile.isDirectory()) {
+        if (internalPath.length > 1) {
+          throw new IOException("Incomplete Path");
+        }
+        processDir(resultFile);
+      } else {
+        if (resultFile.getAbsolutePath().endsWith(".war") || resultFile.getAbsolutePath().endsWith(".jar")) {
+          if (internalPath.length == 1) {
+            processEar(resultFile.getAbsolutePath(), "");
+          } else {
+            processEar(resultFile.getAbsolutePath(), Arrays.copyOfRange(internalPath, 1, internalPath.length));
+          }
+        } else {
+          throw new IOException("Incorrect Path");
+        }
+      }
+    }  finally {
+      boolean deleted = FileUtil.deleteDir(directory);
+      if (!deleted) {
+        FileUtil.deleteDir(directory);
+      }
+    }
+  }
 
-		FactsEventSource cv = new FactsExtractionClassVisitor(className.replace(".class", ""));
-		cv.addListener(this.managers);
-		cr.accept(cv, 0);
-		in.close();
-
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.objectweb.asm.Extractor#addListener(org.objectweb.asm.ExtractionListener
-	 * )
-	 */
-	public void addListener(ExtractionListener manager) {
-		this.managers.add(manager);
-	}
-
-	
-	
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.objectweb.asm.Extractor#processEar(java.lang.String)
-	 */
-	public void processEar(String pathOfEarFile, String... internalPath) throws IOException {
-		File directory = null;
-		try {
-			
-			directory = FileUtil.extractFilesFromZipFile(pathOfEarFile, internalPath[0]);
-			File resultFile = new File(directory.getAbsolutePath() + File.separator + internalPath[0]);
-			if (resultFile.isDirectory()) {
-				if (internalPath.length > 1) throw new IOException("Incomplete Path");
-				processDir(resultFile);
-			} else if (resultFile.getAbsolutePath().endsWith(".war") || resultFile.getAbsolutePath().endsWith(".jar")) {
-				if (internalPath.length == 1) processEar(resultFile.getAbsolutePath(), "");
-				else {
-					processEar(resultFile.getAbsolutePath(), Arrays
-							.copyOfRange(internalPath, 1, internalPath.length));
-				}
-//			} else if (resultFile.getAbsolutePath().endsWith(".jar")) {
-//				if (internalPath.length > 1) throw new IOException("Incomplete Path");
-//				processEar(resultFile.getAbsolutePath(), "");
-			} else {
-				throw new IOException("Incorrect Path");
-			}
-				
-		} finally {
-			boolean deleted = FileUtil.deleteDir(directory);
-			if (!deleted) FileUtil.deleteDir(directory);
-		}
-	}
-
-	/**
+  /**
 	 * Searches recursively for jar files and processes them.
 	 * 
 	 * @param directory
@@ -170,37 +126,36 @@ public class ASMExtractor implements Extractor {
 	 * @throws IOException
 	 *             If an I/O error occurs.
 	 */
-	private void processDir(File directory) throws IOException {
-		File[] files = directory.listFiles();
-		for (File file : files) {
-			if (file.isDirectory()) {
-				processDir(file);
-			} else if (file.getAbsolutePath().endsWith(".jar")) {
-				processJar(file.getAbsolutePath());
-			} else if (file.getAbsolutePath().endsWith(".class")) {
+  private void processDir(File directory) throws IOException {
+    File[] files = directory.listFiles();
+    for (File file : files) {
+      if (file.isDirectory()) {
+        processDir(file);
+      } else {
+        if (file.getAbsolutePath().endsWith(".jar")) {
+          processJar(file.getAbsolutePath());
+        } else {
+          if (file.getAbsolutePath().endsWith(".class")) {
+            String className = file.getAbsolutePath();
+            FileInputStream in = new FileInputStream(className);
+            ClassReader cr = new ClassReader(in);
+            FactsEventSource cv = new FactsExtractionClassVisitor(className.replace(".class", ""));
+            cv.addListener(this.managers);
+            cr.accept(cv, 0);
+            in.close();
+          } else {
+            if (file.getAbsolutePath().endsWith(".ear") || file.getAbsolutePath().endsWith(".war")) {
+              processEar(file.getAbsolutePath(), "");
+            }
+          }
+        }
+      }
+    }
+  }
 
-				String className = file.getAbsolutePath();
-				FileInputStream in = new FileInputStream(className);
-				ClassReader cr = new ClassReader(in);
-				
-				FactsEventSource cv = new FactsExtractionClassVisitor(className.replace(".class",
-						""));
-				cv.addListener(this.managers);
-				cr.accept(cv, 0);
-				in.close();
-
-			} else if (file.getAbsolutePath().endsWith(".ear") || file.getAbsolutePath().endsWith(".war")) {
-				processEar(file.getAbsolutePath(), "");
-			}
-		}
-	}
-
-
-	@Override
-	public void processClass(String classFilePath) throws IOException {
-		URLClassLoader classLoader = new URLClassLoader(
-				new URL[] { new File(classFilePath).toURI().toURL() });
-		classFilePath = removeDirectoryName(classFilePath);
-		processClass(classFilePath, classLoader);
-	}
+  @Override public void processClass(String classFilePath) throws IOException {
+    URLClassLoader classLoader = new URLClassLoader(new URL[] { new File(classFilePath).toURI().toURL() });
+    classFilePath = removeDirectoryName(classFilePath);
+    processClass(classFilePath, classLoader);
+  }
 }
