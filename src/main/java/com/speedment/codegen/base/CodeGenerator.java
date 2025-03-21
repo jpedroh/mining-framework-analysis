e9e9eed91a2bf3591ab2581a3b230444f4e41b32
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 16:03:58 +0000

    Mining Framework Analysis

--- src/main/java/com/speedment/codegen/base/CodeGenerator.java
+++ src/main/java/com/speedment/codegen/base/CodeGenerator.java
@@ -1,21 +1,4 @@
-/**
- *
- * Copyright (c) 2006-2015, Speedment, Inc. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); You may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at:
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- */
 package com.speedment.codegen.base;
-
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
@@ -26,13 +9,12 @@ import java.util.stream.Stream;
  * @author Emil Forslund
  */
 public interface CodeGenerator {
-
-    /**
+  /**
      * @return the dependency manager.
      */
-    DependencyManager getDependencyMgr();
+  DependencyManager getDependencyMgr();
 
-    /**
+  /**
      * Returns the current rendering stack. The top element will be the one most
      * recent rendered and the bottom one will be the element that was first
      * passed to the generator. Elements are removed from the stack once they
@@ -51,9 +33,9 @@ public interface CodeGenerator {
      *
      * @return the current rendering stack.
      */
-    List<Object> getRenderStack();
+  List<Object> getRenderStack();
 
-    /**
+  /**
      * Renders the specified model into a stream of code models. This is used
      * internally to provide the other interface methods.
      *
@@ -61,9 +43,9 @@ public interface CodeGenerator {
      * @param model The model to generate.
      * @return A stream of code objects.
      */
-    <M> Stream<Code<M>> codeOn(M model);
+  <M extends java.lang.Object> Stream<Code<M>> codeOn(M model);
 
-    /**
+  /**
      * Renders all the specified models into a stream of code models. This is
      * used internally to provide the other interface methods. ¨
      *
@@ -71,11 +53,11 @@ public interface CodeGenerator {
      * @param models The models to generate.
      * @return A stream of code objects.
      */
-    default <M> Stream<Code<M>> codeOn(Collection<M> models) {
-        return models.stream().map(model -> codeOn(model)).flatMap(m -> m);
-    }
+  default <M extends java.lang.Object> Stream<Code<M>> codeOn(Collection<M> models) {
+    return models.stream().map((model) -> codeOn(model)).flatMap((m) -> m);
+  }
 
-    /**
+  /**
      * Locates the <code>CodeView</code> that corresponds to the specified model
      * and uses it to generate a String. If no view is associated with the model
      * type, an empty optional will be returned.
@@ -83,23 +65,39 @@ public interface CodeGenerator {
      * @param model The model.
      * @return The viewed text if any.
      */
-    default Optional<String> on(Object model) {
-        if (model instanceof Optional<?>) {
-            return ((Optional<?>) model)
-                .flatMap(m -> codeOn(m).findAny().map(c -> c.getText()));
-        } else {
-            return codeOn(model).findAny().map(c -> c.getText());
-        }
-    }
+  default Optional<String> on(Object model) {
+    if (
+<<<<<<< /usr/src/app/output/pyknic/codegen/b718264121f2990b0cd2df982f93714ce9b2fe14/src/main/java/com/speedment/codegen/base/CodeGenerator.java/left.java
+    model instanceof Optional<?>
+=======
+    model instanceof Optional
+>>>>>>> /usr/src/app/output/pyknic/codegen/b718264121f2990b0cd2df982f93714ce9b2fe14/src/main/java/com/speedment/codegen/base/CodeGenerator.java/right.java
+    ) {
 
-    /**
+<<<<<<< /usr/src/app/output/pyknic/codegen/b718264121f2990b0cd2df982f93714ce9b2fe14/src/main/java/com/speedment/codegen/base/CodeGenerator.java/left.java
+      return ((Optional<?>) model).flatMap((m) -> codeOn(m).findAny().map((c) -> c.getText()));
+=======
+      final Optional result = (Optional<?>) model;
+>>>>>>> /usr/src/app/output/pyknic/codegen/b718264121f2990b0cd2df982f93714ce9b2fe14/src/main/java/com/speedment/codegen/base/CodeGenerator.java/right.java
+
+      if (result.isPresent()) {
+        model = result.get();
+      } else {
+        return Optional.empty();
+      }
+    } else {
+      return codeOn(model).findAny().map((c) -> c.getText());
+    }
+  }
+
+  /**
      * Renders all the specified models into a stream of strings.
      *
      * @param <M>
      * @param models The models to generate.
      * @return A stream of code objects.
      */
-    default <M> Stream<String> onEach(Collection<M> models) {
-        return codeOn(models).map(c -> c.getText());
-    }
+  default <M extends java.lang.Object> Stream<String> onEach(Collection<M> models) {
+    return codeOn(models).map((c) -> c.getText());
+  }
 }
--- src/main/java/com/speedment/codegen/java/views/interfaces/DocumentableView.java
+++ src/main/java/com/speedment/codegen/java/views/interfaces/DocumentableView.java
@@ -1,21 +1,4 @@
-/**
- *
- * Copyright (c) 2006-2015, Speedment, Inc. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); You may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at:
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- */
 package com.speedment.codegen.java.views.interfaces;
-
 import static com.speedment.codegen.Formatting.EMPTY;
 import static com.speedment.codegen.Formatting.nl;
 import com.speedment.codegen.base.CodeGenerator;
@@ -28,7 +11,19 @@ import com.speedment.codegen.lang.interfaces.Documentable;
  * @param <M>
  */
 public interface DocumentableView<M extends Documentable<M>> extends CodeView<M> {
-    default String renderJavadoc(CodeGenerator cg, M model) {
-        return cg.on(model.getJavadoc()).map(jd -> jd + nl()).orElse(EMPTY);
-    }
+  default String renderJavadoc(CodeGenerator cg, M model) {
+    return cg.on(model.getJavadoc()).map((
+<<<<<<< /usr/src/app/output/pyknic/codegen/b718264121f2990b0cd2df982f93714ce9b2fe14/src/main/java/com/speedment/codegen/java/views/interfaces/DocumentableView.java/left.java
+    jd
+=======
+    j
+>>>>>>> /usr/src/app/output/pyknic/codegen/b718264121f2990b0cd2df982f93714ce9b2fe14/src/main/java/com/speedment/codegen/java/views/interfaces/DocumentableView.java/right.java
+    ) -> 
+<<<<<<< /usr/src/app/output/pyknic/codegen/b718264121f2990b0cd2df982f93714ce9b2fe14/src/main/java/com/speedment/codegen/java/views/interfaces/DocumentableView.java/left.java
+    jd
+=======
+    j
+>>>>>>> /usr/src/app/output/pyknic/codegen/b718264121f2990b0cd2df982f93714ce9b2fe14/src/main/java/com/speedment/codegen/java/views/interfaces/DocumentableView.java/right.java
+     + nl()).orElse(EMPTY);
+  }
 }
