403806a477ae182f6b96bc9cf18785897d234642
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 11:26:28 +0000

    Mining Framework Analysis

--- php-frontend/src/test/java/org/sonar/php/parser/statement/EnumCaseTest.java
+++ php-frontend/src/test/java/org/sonar/php/parser/statement/EnumCaseTest.java
@@ -1,41 +1,31 @@
-/*
- * SonarQube PHP Plugin
- * Copyright (C) 2010-2021 SonarSource SA
- * mailto:info AT sonarsource DOT com
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3 of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- */
-package org.sonar.php.parser.statement;
-
-import org.junit.Test;
-import org.sonar.php.parser.PHPLexicalGrammar;
-
-import static org.sonar.php.utils.Assertions.assertThat;
-
-public class EnumCaseTest {
-
-  @Test
-  public void test() {
-    assertThat(PHPLexicalGrammar.ENUM_CASE)
-      .matches("case A;")
-      .matches("#[A1(1)] case A;")
-      .matches("case Enum;")
-      .matches("case A = 'A';")
-      .matches("case A = 'A' . 'B';")
-      .matches("case A = MyClass::CONSTANT;")
-      .notMatches("case A")
-    ;
-  }
-}
+  package     org . sonar . php . parser . statement ;   import   org . junit . Test ;  import     org . sonar . php . parser . PHPLexicalGrammar ;  import static      org . sonar . php . utils . Assertions . assertThat ;   public class EnumCaseTest  {    @ Test public void test  ( )  {       
+<<<<<<<
+assertThat
+=======
+  assertThat  (  PHPLexicalGrammar . ENUM_CASE ) . matches  ( "case A;" )
+>>>>>>>
+ . matches  ( 
+<<<<<<<
+ PHPLexicalGrammar . ENUM_CASE
+=======
+"#[A1(1)] case A;"
+>>>>>>>
+ ) . matches  ( 
+<<<<<<<
+"case A;"
+=======
+"case A = 'A';"
+>>>>>>>
+ ) . matches  ( 
+<<<<<<<
+"#[A1(1)] case A;"
+=======
+"case A = 'A' . 'B';"
+>>>>>>>
+ ) . matches  ( 
+<<<<<<<
+"case Enum;"
+=======
+"case A = MyClass::CONSTANT;"
+>>>>>>>
+ ) . notMatches  ( "case A" ) ; } }
