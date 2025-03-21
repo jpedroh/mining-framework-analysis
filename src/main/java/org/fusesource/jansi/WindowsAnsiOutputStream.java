9126af0773ae36829a5d9fb2a2be40c589dbd46c
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 09:03:39 +0000

    Mining Framework Analysis

--- src/main/java/org/fusesource/jansi/WindowsAnsiOutputStream.java
+++ src/main/java/org/fusesource/jansi/WindowsAnsiOutputStream.java
@@ -1,21 +1,4 @@
-/**
- * Copyright (C) 2009, Progress Software Corporation and/or its 
- * subsidiaries or affiliates.  All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.fusesource.jansi;
-
 import static org.fusesource.jansi.internal.Kernel32.BACKGROUND_BLUE;
 import static org.fusesource.jansi.internal.Kernel32.BACKGROUND_GREEN;
 import static org.fusesource.jansi.internal.Kernel32.BACKGROUND_RED;
@@ -23,15 +6,12 @@ import static org.fusesource.jansi.internal.Kernel32.FOREGROUND_BLUE;
 import static org.fusesource.jansi.internal.Kernel32.FOREGROUND_GREEN;
 import static org.fusesource.jansi.internal.Kernel32.FOREGROUND_RED;
 import static org.fusesource.jansi.internal.Kernel32.KERNEL32;
-
 import java.io.IOException;
 import java.io.OutputStream;
-
 import org.fusesource.jansi.internal.Kernel32;
 import org.fusesource.jansi.internal.WindowsSupport;
 import org.fusesource.jansi.internal.Kernel32.CONSOLE_SCREEN_BUFFER_INFO;
 import org.fusesource.jansi.internal.Kernel32.COORD;
-
 import com.sun.jna.Pointer;
 import com.sun.jna.ptr.IntByReference;
 
@@ -39,201 +19,175 @@ import com.sun.jna.ptr.IntByReference;
  * @since 1.0
  */
 public final class WindowsAnsiOutputStream extends AnsiOutputStream {
-	
-	private static final Pointer console = KERNEL32.GetStdHandle(Kernel32.STD_OUTPUT_HANDLE);
+  private static final Pointer console = KERNEL32.GetStdHandle(Kernel32.STD_OUTPUT_HANDLE);
 
-    private static final short FOREGROUND_BLACK   = 0;
-    private static final short FOREGROUND_YELLOW  = FOREGROUND_RED|FOREGROUND_GREEN;    
-    private static final short FOREGROUND_MAGENTA = FOREGROUND_BLUE|FOREGROUND_RED;    
-    private static final short FOREGROUND_CYAN    = FOREGROUND_BLUE|FOREGROUND_GREEN;
-    private static final short FOREGROUND_WHITE   = FOREGROUND_RED|FOREGROUND_GREEN|FOREGROUND_BLUE;
+  private static final short FOREGROUND_BLACK = 0;
 
-    private static final short BACKGROUND_BLACK   = 0;
-    private static final short BACKGROUND_YELLOW  = BACKGROUND_RED|BACKGROUND_GREEN;    
-    private static final short BACKGROUND_MAGENTA = BACKGROUND_BLUE|BACKGROUND_RED;    
-    private static final short BACKGROUND_CYAN    = BACKGROUND_BLUE|BACKGROUND_GREEN;
-    private static final short BACKGROUND_WHITE   = BACKGROUND_RED|BACKGROUND_GREEN|BACKGROUND_BLUE;
-    
-    private static final short ANSI_FOREGROUND_COLOR_MAP[] = {
-    	FOREGROUND_BLACK,
-    	FOREGROUND_RED,
-    	FOREGROUND_GREEN,
-    	FOREGROUND_YELLOW,
-    	FOREGROUND_BLUE,
-    	FOREGROUND_MAGENTA,
-    	FOREGROUND_CYAN,
-    	FOREGROUND_WHITE,
-    };
-        
-    private static final short ANSI_BACKGROUND_COLOR_MAP[] = {
-    	BACKGROUND_BLACK,
-    	BACKGROUND_RED,
-    	BACKGROUND_GREEN,
-    	BACKGROUND_YELLOW,
-    	BACKGROUND_BLUE,
-    	BACKGROUND_MAGENTA,
-    	BACKGROUND_CYAN,
-    	BACKGROUND_WHITE,
-    };
-	
-	private final CONSOLE_SCREEN_BUFFER_INFO.ByReference info = new CONSOLE_SCREEN_BUFFER_INFO.ByReference();
-    private final short originalColors;
-    
-	private boolean negative;
-	
-	public WindowsAnsiOutputStream(OutputStream os) throws IOException {
-		super(os);
-		getConsoleInfo();
-		originalColors = info.attributes;
-	}
+  private static final short FOREGROUND_YELLOW = FOREGROUND_RED | FOREGROUND_GREEN;
 
-	private void getConsoleInfo() throws IOException {
-		out.flush();
-		if( KERNEL32.GetConsoleScreenBufferInfo(console, info) == 0 ) {
-			throw new IOException("Could not get the screen info: "+WindowsSupport.getLastErrorMessage());
-		}
-		if( negative ) {
-			info.attributes = invertAttributeColors(info.attributes); 
-		}
-	}	
-	
- 	@Override
- 	public void close() throws IOException {
- 	    out.flush();
- 		info.attributes = originalColors;
- 		this.negative = false;
- 		applyAttribute();
- 		super.close();
- 	}
-	
-	private void applyAttribute() throws IOException {
-		out.flush();
-		short attributes = info.attributes;
-		if( negative ) {
-			attributes = invertAttributeColors(attributes); 
-		}
-		if( KERNEL32.SetConsoleTextAttribute(console, attributes) == 0 ) {
-			throw new IOException(WindowsSupport.getLastErrorMessage());
-		}
-	}
+  private static final short FOREGROUND_MAGENTA = FOREGROUND_BLUE | FOREGROUND_RED;
 
-	private short invertAttributeColors(short attibutes) {
-		// Swap the the Foreground and Background bits.
-		int fg = 0x000F & attibutes;
-		fg <<= 8;
-		int bg = 0X00F0 * attibutes;
-		bg >>=8;
-		attibutes = (short) ((attibutes & 0xFF00) | fg | bg);
-		return attibutes;
-	}
+  private static final short FOREGROUND_CYAN = FOREGROUND_BLUE | FOREGROUND_GREEN;
 
-	private void applyCursorPosition() throws IOException {
-		if( KERNEL32.SetConsoleCursorPosition(console, info.cursorPosition.copy()) == 0 ) {
-			throw new IOException(WindowsSupport.getLastErrorMessage());
-		}
-	}
-	
-	@Override
-	protected void processEraseScreen(int eraseOption) throws IOException {
-		getConsoleInfo();
-		switch(eraseOption) {
-		case ERASE_SCREEN:
-			COORD.ByValue coord = new COORD.ByValue();
-			coord.x = 0;
-			coord.y = info.window.top;
-			int length = info.window.height() * info.size.x;
-			IntByReference written = new IntByReference();
-			KERNEL32.FillConsoleOutputCharacterW(console, ' ', length, coord, written);
-		case ERASE_SCREEN_TO_BEGINING:
-		case ERASE_SCREEN_TO_END:
-		}		
-	}
-	
-	@Override
-	protected void processCursorLeft(int count) throws IOException {
-		getConsoleInfo();
-		info.cursorPosition.x = (short) Math.max(0, info.cursorPosition.x-count);
-		applyCursorPosition();		
-	}
+  private static final short FOREGROUND_WHITE = FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE;
 
-	@Override
-	protected void processCursorRight(int count) throws IOException {
-		getConsoleInfo();
-		info.cursorPosition.x = (short)Math.min(info.window.width(), info.cursorPosition.x+count);
-		applyCursorPosition();		
-	}
-	
-	@Override
-	protected void processCursorDown(int count) throws IOException {
-		getConsoleInfo();
-		info.cursorPosition.y = (short) Math.min(info.size.y, info.cursorPosition.y+count);
-		applyCursorPosition();		
-	}
-	
-	@Override
-	protected void processCursorUp(int count) throws IOException {
-		getConsoleInfo();
-		info.cursorPosition.y = (short) Math.max(info.window.top, info.cursorPosition.y-count);
-		applyCursorPosition();		
-	}
-	
-	@Override
-	protected void processCursorTo(int x, int y) throws IOException {
-		getConsoleInfo();
-		info.cursorPosition.y = (short) Math.max(info.window.top, Math.min(info.size.y, info.window.top+y-1));
-		info.cursorPosition.x = (short) Math.max(0, Math.min(info.window.width(), x-1));
-		applyCursorPosition();		
-	}
+  private static final short BACKGROUND_BLACK = 0;
 
-	@Override
-	protected void processSetForegroundColor(int color) throws IOException {
-		info.attributes = (short)((info.attributes & ~0x0007 ) | ANSI_FOREGROUND_COLOR_MAP[color]);
-		applyAttribute();
-	}
+  private static final short BACKGROUND_YELLOW = BACKGROUND_RED | BACKGROUND_GREEN;
 
-	@Override
-	protected void processSetBackgroundColor(int color) throws IOException {
-		info.attributes = (short)((info.attributes & ~0x0070 ) | ANSI_BACKGROUND_COLOR_MAP[color]);
-		applyAttribute();
-	}
+  private static final short BACKGROUND_MAGENTA = BACKGROUND_BLUE | BACKGROUND_RED;
 
-	@Override
-	protected void processAttributeRest() throws IOException {
-		info.attributes = (short)((info.attributes & ~0x00FF ) | originalColors);
-		applyAttribute();
-	}
-	
-	@Override
-	protected void processSetAttribute(int attribute) throws IOException {
-		switch(attribute) {
-			case ATTRIBUTE_INTENSITY_BOLD:
-				info.attributes = (short)(info.attributes | Kernel32.FOREGROUND_INTENSITY );
-				applyAttribute();
-				break;
-			case ATTRIBUTE_INTENSITY_NORMAL:
-				info.attributes = (short)(info.attributes & ~Kernel32.FOREGROUND_INTENSITY );
-				applyAttribute();
-				break;
-			
-			// Yeah, setting the background intensity is not underlining.. but it's best we can do 
-			// using the Windows console API 
-			case ATTRIBUTE_UNDERLINE:
-				info.attributes = (short)(info.attributes | Kernel32.BACKGROUND_INTENSITY );
-				applyAttribute();
-				break;
-			case ATTRIBUTE_UNDERLINE_OFF:
-				info.attributes = (short)(info.attributes & ~Kernel32.BACKGROUND_INTENSITY );
-				applyAttribute();
-				break;
-				
-			case ATTRIBUTE_NEGATIVE_ON:
-				negative = true;
-				applyAttribute();
-				break;
-			case ATTRIBUTE_NEGATIVE_Off:
-				negative = false;
-				applyAttribute();
-				break;
-		}
-	}
+  private static final short BACKGROUND_CYAN = BACKGROUND_BLUE | BACKGROUND_GREEN;
+
+  private static final short BACKGROUND_WHITE = BACKGROUND_RED | BACKGROUND_GREEN | BACKGROUND_BLUE;
+
+  private static final short ANSI_FOREGROUND_COLOR_MAP[] = { FOREGROUND_BLACK, FOREGROUND_RED, FOREGROUND_GREEN, FOREGROUND_YELLOW, FOREGROUND_BLUE, FOREGROUND_MAGENTA, FOREGROUND_CYAN, FOREGROUND_WHITE };
+
+  private static final short ANSI_BACKGROUND_COLOR_MAP[] = { BACKGROUND_BLACK, BACKGROUND_RED, BACKGROUND_GREEN, BACKGROUND_YELLOW, BACKGROUND_BLUE, BACKGROUND_MAGENTA, BACKGROUND_CYAN, BACKGROUND_WHITE };
+
+  private final CONSOLE_SCREEN_BUFFER_INFO.ByReference info = new CONSOLE_SCREEN_BUFFER_INFO.ByReference();
+
+  private final short originalColors;
+
+  private boolean negative;
+
+  public WindowsAnsiOutputStream(OutputStream os) throws IOException {
+    super(os);
+    getConsoleInfo();
+    originalColors = info.attributes;
+  }
+
+  private void getConsoleInfo() throws IOException {
+    out.flush();
+    if (KERNEL32.GetConsoleScreenBufferInfo(console, info) == 0) {
+      throw new IOException("Could not get the screen info: " + WindowsSupport.getLastErrorMessage());
+    }
+    if (negative) {
+      info.attributes = invertAttributeColors(info.attributes);
+    }
+  }
+
+  @Override public void close() throws IOException {
+    out.flush();
+    info.attributes = originalColors;
+    this.negative = false;
+    applyAttribute();
+    super.close();
+  }
+
+  private void applyAttribute() throws IOException {
+    out.flush();
+    short attributes = info.attributes;
+    if (negative) {
+      attributes = invertAttributeColors(attributes);
+    }
+    if (KERNEL32.SetConsoleTextAttribute(console, attributes) == 0) {
+      throw new IOException(WindowsSupport.getLastErrorMessage());
+    }
+  }
+
+  private short invertAttributeColors(short attibutes) {
+    int fg = 0x000F & attibutes;
+    fg <<= 8;
+    int bg = 0X00F0 * attibutes;
+    bg >>= 8;
+    attibutes = (short) ((attibutes & 0xFF00) | fg | bg);
+    return attibutes;
+  }
+
+  private void applyCursorPosition() throws IOException {
+    if (KERNEL32.SetConsoleCursorPosition(console, info.cursorPosition.copy()) == 0) {
+      throw new IOException(WindowsSupport.getLastErrorMessage());
+    }
+  }
+
+  @Override protected void processEraseScreen(int eraseOption) throws IOException {
+    getConsoleInfo();
+    switch (eraseOption) {
+      case ERASE_SCREEN:
+      COORD.ByValue coord = new COORD.ByValue();
+      coord.x = 0;
+      coord.y = info.window.top;
+      int length = info.window.height() * info.size.x;
+      IntByReference written = new IntByReference();
+      KERNEL32.FillConsoleOutputCharacterW(console, ' ', length, coord, written);
+      case ERASE_SCREEN_TO_BEGINING:
+      case ERASE_SCREEN_TO_END:
+    }
+  }
+
+  @Override protected void processCursorLeft(int count) throws IOException {
+    getConsoleInfo();
+    info.cursorPosition.x = (short) Math.max(0, info.cursorPosition.x - count);
+    applyCursorPosition();
+  }
+
+  @Override protected void processCursorRight(int count) throws IOException {
+    getConsoleInfo();
+    info.cursorPosition.x = (short) Math.min(info.window.width(), info.cursorPosition.x + count);
+    applyCursorPosition();
+  }
+
+  @Override protected void processCursorDown(int count) throws IOException {
+    getConsoleInfo();
+    info.cursorPosition.y = (short) Math.min(info.size.y, info.cursorPosition.y + count);
+    applyCursorPosition();
+  }
+
+  @Override protected void processCursorUp(int count) throws IOException {
+    getConsoleInfo();
+    info.cursorPosition.y = (short) Math.max(info.window.top, info.cursorPosition.y - count);
+    applyCursorPosition();
+  }
+
+  @Override protected void processCursorTo(int x, int y) throws IOException {
+    getConsoleInfo();
+    info.cursorPosition.y = (short) Math.max(info.window.top, Math.min(info.size.y, info.window.top + y - 1));
+    info.cursorPosition.x = (short) Math.max(0, Math.min(info.window.width(), x - 1));
+    applyCursorPosition();
+  }
+
+  @Override protected void processSetForegroundColor(int color) throws IOException {
+    info.attributes = (short) ((info.attributes & ~0x0007) | ANSI_FOREGROUND_COLOR_MAP[color]);
+    applyAttribute();
+  }
+
+  @Override protected void processSetBackgroundColor(int color) throws IOException {
+    info.attributes = (short) ((info.attributes & ~0x0070) | ANSI_BACKGROUND_COLOR_MAP[color]);
+    applyAttribute();
+  }
+
+  @Override protected void processAttributeRest() throws IOException {
+    info.attributes = (short) ((info.attributes & ~0x00FF) | originalColors);
+    applyAttribute();
+  }
+
+  @Override protected void processSetAttribute(int attribute) throws IOException {
+    switch (attribute) {
+      case ATTRIBUTE_INTENSITY_BOLD:
+      info.attributes = (short) (info.attributes | Kernel32.FOREGROUND_INTENSITY);
+      applyAttribute();
+      break;
+      case ATTRIBUTE_INTENSITY_NORMAL:
+      info.attributes = (short) (info.attributes & ~Kernel32.FOREGROUND_INTENSITY);
+      applyAttribute();
+      break;
+      case ATTRIBUTE_UNDERLINE:
+      info.attributes = (short) (info.attributes | Kernel32.BACKGROUND_INTENSITY);
+      applyAttribute();
+      break;
+      case ATTRIBUTE_UNDERLINE_OFF:
+      info.attributes = (short) (info.attributes & ~Kernel32.BACKGROUND_INTENSITY);
+      applyAttribute();
+      break;
+      case ATTRIBUTE_NEGATIVE_ON:
+      negative = true;
+      applyAttribute();
+      break;
+      case ATTRIBUTE_NEGATIVE_Off:
+      negative = false;
+      applyAttribute();
+      break;
+    }
+  }
 }
