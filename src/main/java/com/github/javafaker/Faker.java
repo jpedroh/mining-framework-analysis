3234f007372ab8d61a29b5d36942e10f5e227de4
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 07:31:35 +0000

    Mining Framework Analysis

--- src/main/java/com/github/javafaker/Faker.java
+++ src/main/java/com/github/javafaker/Faker.java
@@ -1,8 +1,6 @@
 package com.github.javafaker;
-
 import com.github.javafaker.service.FakeValuesService;
 import com.github.javafaker.service.RandomService;
-
 import java.util.Locale;
 import java.util.Random;
 
@@ -13,103 +11,139 @@ import java.util.Random;
  * @author ren
  */
 public class Faker {
-    private final RandomService randomService;
-    private final FakeValuesService fakeValuesService;
+  private final RandomService randomService;
 
-    private final Ancient ancient;
-    private final App app;
-    private final Lorem lorem;
-    private final Music music;
-    private final Name name;
-    private final Number number;
-    private final Internet internet;
-    private final PhoneNumber phoneNumber;
-    private final Pokemon pokemon;
-    private final Address address;
-    private final Business business;
-    private final Book book;
-    private final ChuckNorris chuckNorris;
-    private final Color color;
-    private final Commerce commerce;
-    private final Company company;
-    private final Crypto crypto;
-    private final IdNumber idNumber;
-    private final Hacker hacker;
-    private final Options options;
-    private final Code code;
-    private final Finance finance;
-    private final Food food;
-    private final GameOfThrones gameOfThrones;
-    private final DateAndTime dateAndTime;
-    private final Educator educator;
-    private final Shakespeare shakespeare;
-    private final SlackEmoji slackEmoji;
-    private final Space space;
-    private final Superhero superhero;
-    private final Bool bool;
-    private final Team team;
-    private final Beer beer;
-    private final University university;
-    private final Cat cat;
-    private final File file;
-    private final Stock stock;
+  private final FakeValuesService fakeValuesService;
 
-    public Faker() {
-        this(Locale.ENGLISH);
-    }
+  private final Ancient ancient;
 
-    public Faker(Locale locale) {
-        this(locale, null);
-    }
+  private final App app;
 
-    public Faker(Random random) {
-        this(Locale.ENGLISH, random);
-    }
+  private final Lorem lorem;
 
-    public Faker(Locale locale, Random random) {
-        this.randomService = new RandomService(random);
-        this.fakeValuesService = new FakeValuesService(locale, randomService);
+  private final Music music;
 
-        this.ancient = new Ancient(this);
-        this.app = new App(this);
-        this.lorem = new Lorem(this);
-        this.music = new Music(this);
-        this.name = new Name(this);
-        this.number = new Number(this);
-        this.internet = new Internet(this);
-        this.phoneNumber = new PhoneNumber(this);
-        this.pokemon = new Pokemon(this);
-        this.address = new Address(this);
-        this.book = new Book(this);
-        this.business = new Business(this);
-        this.chuckNorris = new ChuckNorris(this);
-        this.color = new Color(this);
-        this.idNumber = new IdNumber(this);
-        this.hacker = new Hacker(this);
-        this.company = new Company(this);
-        this.crypto = new Crypto(this);
-        this.commerce = new Commerce(this);
-        this.options = new Options(this);
-        this.code = new Code(this);
-        this.file = new File(this);
-        this.finance = new Finance(this);
-        this.food = new Food(this);
-        this.gameOfThrones = new GameOfThrones(this);
-        this.dateAndTime = new DateAndTime(this);
-        this.educator = new Educator(this);
-        this.shakespeare = new Shakespeare(this);
-        this.slackEmoji = new SlackEmoji(this);
-        this.space = new Space(this);
-        this.superhero = new Superhero(this);
-        this.team = new Team(this);
-        this.bool = new Bool(this);
-        this.beer = new Beer(this);
-        this.university = new University(this);
-        this.cat = new Cat(this);
-        this.stock = new Stock(this);
-    }
+  private final Name name;
 
-    /**
+  private final Number number;
+
+  private final Internet internet;
+
+  private final PhoneNumber phoneNumber;
+
+  private final Pokemon pokemon;
+
+  private final Address address;
+
+  private final Business business;
+
+  private final Book book;
+
+  private final ChuckNorris chuckNorris;
+
+  private final Color color;
+
+  private final Commerce commerce;
+
+  private final Company company;
+
+  private final Crypto crypto;
+
+  private final IdNumber idNumber;
+
+  private final Hacker hacker;
+
+  private final Options options;
+
+  private final Code code;
+
+  private final Finance finance;
+
+  private final Food food;
+
+  private final GameOfThrones gameOfThrones;
+
+  private final DateAndTime dateAndTime;
+
+  private final Educator educator;
+
+  private final Shakespeare shakespeare;
+
+  private final SlackEmoji slackEmoji;
+
+  private final Space space;
+
+  private final Superhero superhero;
+
+  private final Bool bool;
+
+  private final Team team;
+
+  private final Beer beer;
+
+  private final University university;
+
+  private final Cat cat;
+
+  private final File file;
+
+  private final Stock stock;
+
+  public Faker() {
+    this(Locale.ENGLISH);
+  }
+
+  public Faker(Locale locale) {
+    this(locale, null);
+  }
+
+  public Faker(Random random) {
+    this(Locale.ENGLISH, random);
+  }
+
+  public Faker(Locale locale, Random random) {
+    this.randomService = new RandomService(random);
+    this.fakeValuesService = new FakeValuesService(locale, randomService);
+    this.ancient = new Ancient(this);
+    this.app = new App(this);
+    this.lorem = new Lorem(this);
+    this.music = new Music(this);
+    this.name = new Name(this);
+    this.number = new Number(this);
+    this.internet = new Internet(this);
+    this.phoneNumber = new PhoneNumber(this);
+    this.pokemon = new Pokemon(this);
+    this.address = new Address(this);
+    this.book = new Book(this);
+    this.business = new Business(this);
+    this.chuckNorris = new ChuckNorris(this);
+    this.color = new Color(this);
+    this.idNumber = new IdNumber(this);
+    this.hacker = new Hacker(this);
+    this.company = new Company(this);
+    this.crypto = new Crypto(this);
+    this.commerce = new Commerce(this);
+    this.options = new Options(this);
+    this.code = new Code(this);
+    this.file = new File(this);
+    this.finance = new Finance(this);
+    this.food = new Food(this);
+    this.gameOfThrones = new GameOfThrones(this);
+    this.dateAndTime = new DateAndTime(this);
+    this.educator = new Educator(this);
+    this.shakespeare = new Shakespeare(this);
+    this.slackEmoji = new SlackEmoji(this);
+    this.space = new Space(this);
+    this.superhero = new Superhero(this);
+    this.team = new Team(this);
+    this.bool = new Bool(this);
+    this.beer = new Beer(this);
+    this.university = new University(this);
+    this.cat = new Cat(this);
+    this.stock = new Stock(this);
+  }
+
+  /**
      * Returns a string with the '#' characters in the parameter replaced with random digits between 0-9 inclusive.
      * <p>
      * For example, the string "ABC##EFG" could be replaced with a string like "ABC99EFG".
@@ -117,11 +151,11 @@ public class Faker {
      * @param numberString
      * @return
      */
-    public String numerify(String numberString) {
-        return fakeValuesService.numerify(numberString);
-    }
+  public String numerify(String numberString) {
+    return fakeValuesService.numerify(numberString);
+  }
 
-    /**
+  /**
      * Returns a string with the '?' characters in the parameter replaced with random alphabetic
      * characters.
      * <p>
@@ -130,11 +164,11 @@ public class Faker {
      * @param letterString
      * @return
      */
-    public String letterify(String letterString) {
-        return fakeValuesService.letterify(letterString);
-    }
+  public String letterify(String letterString) {
+    return fakeValuesService.letterify(letterString);
+  }
 
-    /**
+  /**
      * Returns a string with the '?' characters in the parameter replaced with random alphabetic
      * characters.
      * <p>
@@ -144,22 +178,22 @@ public class Faker {
      * @param isUpper
      * @return
      */
-    public String letterify(String letterString, boolean isUpper) {
-        return fakeValuesService.letterify(letterString, isUpper);
-    }
+  public String letterify(String letterString, boolean isUpper) {
+    return fakeValuesService.letterify(letterString, isUpper);
+  }
 
-    /**
+  /**
      * Applies both a {@link #numerify(String)} and a {@link #letterify(String)}
      * over the incoming string.
      *
      * @param string
      * @return
      */
-    public String bothify(String string) {
-        return fakeValuesService.bothify(string);
-    }
+  public String bothify(String string) {
+    return fakeValuesService.bothify(string);
+  }
 
-    /**
+  /**
      * Applies both a {@link #numerify(String)} and a {@link #letterify(String)}
      * over the incoming string.
      *
@@ -167,181 +201,181 @@ public class Faker {
      * @param isUpper
      * @return
      */
-    public String bothify(String string, boolean isUpper) {
-        return fakeValuesService.bothify(string, isUpper);
-    }
+  public String bothify(String string, boolean isUpper) {
+    return fakeValuesService.bothify(string, isUpper);
+  }
 
-    /**
+  /**
      * Generates a String that matches the given regular expression.
      */
-    public String regexify(String regex) {
-        return fakeValuesService.regexify(regex);
-    }
+  public String regexify(String regex) {
+    return fakeValuesService.regexify(regex);
+  }
 
-    RandomService random() {
-        return this.randomService;
-    }
+  RandomService random() {
+    return this.randomService;
+  }
 
-    FakeValuesService fakeValuesService() {
-        return this.fakeValuesService;
-    }
+  FakeValuesService fakeValuesService() {
+    return this.fakeValuesService;
+  }
 
-    public Ancient ancient() {
-        return ancient;
-    }
+  public Ancient ancient() {
+    return ancient;
+  }
 
-    public App app() {
-        return app;
-    }
+  public App app() {
+    return app;
+  }
 
-    public Music music() {
-        return music;
-    }
+  public Music music() {
+    return music;
+  }
 
-    public Name name() {
-        return name;
-    }
+  public Name name() {
+    return name;
+  }
 
-    public Number number() {
-        return number;
-    }
+  public Number number() {
+    return number;
+  }
 
-    public Internet internet() {
-        return internet;
-    }
+  public Internet internet() {
+    return internet;
+  }
 
-    public PhoneNumber phoneNumber() {
-        return phoneNumber;
-    }
+  public PhoneNumber phoneNumber() {
+    return phoneNumber;
+  }
 
-    public Pokemon pokemon() {
-        return pokemon;
-    }
+  public Pokemon pokemon() {
+    return pokemon;
+  }
 
-    public Lorem lorem() {
-        return lorem;
-    }
+  public Lorem lorem() {
+    return lorem;
+  }
 
-    public Address address() {
-        return address;
-    }
+  public Address address() {
+    return address;
+  }
 
-    public Book book() {
-        return book;
-    }
+  public Book book() {
+    return book;
+  }
 
-    public Business business() {
-        return business;
-    }
+  public Business business() {
+    return business;
+  }
 
-    public ChuckNorris chuckNorris() {
-        return chuckNorris;
-    }
+  public ChuckNorris chuckNorris() {
+    return chuckNorris;
+  }
 
-    public Color color() {
-        return color;
-    }
+  public Color color() {
+    return color;
+  }
 
-    public Commerce commerce() {
-        return commerce;
-    }
+  public Commerce commerce() {
+    return commerce;
+  }
 
-    public Company company() {
-        return company;
-    }
+  public Company company() {
+    return company;
+  }
 
-    public Crypto crypto() {
-        return crypto;
-    }
+  public Crypto crypto() {
+    return crypto;
+  }
 
-    public Hacker hacker() {
-        return hacker;
-    }
+  public Hacker hacker() {
+    return hacker;
+  }
 
-    public IdNumber idNumber() {
-        return idNumber;
-    }
+  public IdNumber idNumber() {
+    return idNumber;
+  }
 
-    public Options options() {
-        return options;
-    }
+  public Options options() {
+    return options;
+  }
 
-    public Code code() {
-        return code;
-    }
+  public Code code() {
+    return code;
+  }
 
-    public File file() {
-        return file;
-    }
+  public File file() {
+    return file;
+  }
 
-    public Finance finance() {
-        return finance;
-    }
+  public Finance finance() {
+    return finance;
+  }
 
-    public Food food() {
-        return food;
-    }
+  public Food food() {
+    return food;
+  }
 
-    public GameOfThrones gameOfThrones() {
-        return gameOfThrones;
-    }
+  public GameOfThrones gameOfThrones() {
+    return gameOfThrones;
+  }
 
-    public DateAndTime date() {
-        return dateAndTime;
-    }
+  public DateAndTime date() {
+    return dateAndTime;
+  }
 
-    public Educator educator() {
-        return educator;
-    }
+  public Educator educator() {
+    return educator;
+  }
 
-    public SlackEmoji slackEmoji() {
-        return slackEmoji;
-    }
+  public SlackEmoji slackEmoji() {
+    return slackEmoji;
+  }
 
-    public Shakespeare shakespeare() {
-        return shakespeare;
-    }
+  public Shakespeare shakespeare() {
+    return shakespeare;
+  }
 
-    public Space space() {
-        return space;
-    }
+  public Space space() {
+    return space;
+  }
 
-    public Superhero superhero() {
-        return superhero;
-    }
+  public Superhero superhero() {
+    return superhero;
+  }
 
-    public Bool bool() {
-        return bool;
-    }
+  public Bool bool() {
+    return bool;
+  }
 
-    public Team team() {
-        return team;
-    }
+  public Team team() {
+    return team;
+  }
 
-    public Beer beer() {
-        return beer;
-    }
+  public Beer beer() {
+    return beer;
+  }
 
-    public University university() {
-        return university;
-    }
+  public University university() {
+    return university;
+  }
 
-    public Cat cat() {
-        return cat;
-    }
+  public Cat cat() {
+    return cat;
+  }
 
-    public Stock stock() {
-        return stock;
-    }
+  public Stock stock() {
+    return stock;
+  }
 
-    public String resolve(String key) {
-        return this.fakeValuesService.resolve(key, this, this);
-    }
+  public String resolve(String key) {
+    return this.fakeValuesService.resolve(key, this, this);
+  }
 
-    /**
+  /**
      * Allows the evaluation of native YML expressions to allow you to build your own.
      *
-     * The following are valid expressions:
+     * The following are valid expressions: 
      * <ul>
      *     <li>#{regexify '(a|b){2,3}'}</li>
      *     <li>#{regexify '\\.\\*\\?\\+'}</li>
@@ -353,7 +387,7 @@ public class Faker {
      * @return the evaluated string expression
      * @throws RuntimeException if unable to evaluate the expression
      */
-    public String expression(String expression) {
-        return this.fakeValuesService.expression(expression, this);
-    }
+  public String expression(String expression) {
+    return this.fakeValuesService.expression(expression, this);
+  }
 }
