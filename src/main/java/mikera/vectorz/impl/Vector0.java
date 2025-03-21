6ec2aa5c1c857bfb71be111ec239efe03f4cc293
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 01:31:05 +0000

    Mining Framework Analysis

--- src/main/java/mikera/vectorz/impl/Vector0.java
+++ src/main/java/mikera/vectorz/impl/Vector0.java
@@ -1,7 +1,5 @@
 package mikera.vectorz.impl;
-
 import java.io.ObjectStreamException;
-
 import mikera.arrayz.ISparse;
 import mikera.arrayz.impl.IDense;
 import mikera.vectorz.AVector;
@@ -18,165 +16,134 @@ import mikera.vectorz.util.IntArrays;
  * @author Mike
  */
 public final class Vector0 extends APrimitiveVector implements IDense, ISparse {
-	private static final long serialVersionUID = -8153360223054646075L;
+  private static final long serialVersionUID = -8153360223054646075L;
 
-	private Vector0() {
-	}
+  private Vector0() {
+  }
 
-	public static Vector0 of() {
-		return INSTANCE;
-	}
+  public static Vector0 of() {
+    return INSTANCE;
+  }
 
-	public static Vector0 of(double... values) {
-		if (values.length != 0)
-			throw new IllegalArgumentException(
-					"Vector0 cannot have components!");
-		return INSTANCE;
-	}
+  public static Vector0 of(double... values) {
+    if (values.length != 0) {
+      throw new IllegalArgumentException("Vector0 cannot have components!");
+    }
+    return INSTANCE;
+  }
 
-	public static final Vector0 INSTANCE = new Vector0();
+  public static final Vector0 INSTANCE = new Vector0();
 
-	@Override
-	public int length() {
-		return 0;
-	}
+  @Override public int length() {
+    return 0;
+  }
 
-	@Override
-	public double elementSum() {
-		return 0.0;
-	}
-	
-	@Override
-	public double elementProduct() {
-		return 1.0;
-	}
+  @Override public double elementSum() {
+    return 0.0;
+  }
 
-	@Override
-	public long nonZeroCount() {
-		return 0;
-	}
-	
-	@Override
-	public int[] nonZeroIndices() {
-		return IntArrays.EMPTY_INT_ARRAY;
-	}
+  @Override public double elementProduct() {
+    return 1.0;
+  }
 
-	@Override
-	public double get(int i) {
-		throw new IndexOutOfBoundsException(
-				"Attempt to get on zero length vector!");
-	}
+  @Override public long nonZeroCount() {
+    return 0;
+  }
 
-	@Override
-	public void set(int i, double value) {
-		throw new IndexOutOfBoundsException(
-				"Attempt to set on zero length vector!");
-	}
-	
-	@Override
-	public void addToArray(int offset, double[] array, int arrayOffset, int length) {
-		// nothing to do
-	}
+  @Override public int[] nonZeroIndices() {
+    return IntArrays.EMPTY_INT_ARRAY;
+  }
 
-	@Override
-	public Vector0 clone() {
-		return this;
-	}
+  @Override public double get(int i) {
+    throw new IndexOutOfBoundsException("Attempt to get on zero length vector!");
+  }
 
-	@Override
-	public boolean isMutable() {
-		// i.e is immutable
-		return false;
-	}
-	
-	@Override
-	public boolean isFullyMutable() {
-		// i.e there are no immutable elements!
-		return true;
-	}
+  @Override public void set(int i, double value) {
+    throw new IndexOutOfBoundsException("Attempt to set on zero length vector!");
+  }
 
-	@Override
-	public int hashCode() {
-		// 1 is hashcode for zero-length double array
-		return 1;
-	}
+  @Override public void addToArray(int offset, double[] array, int arrayOffset, int length) {
+  }
 
-	@Override
-	public boolean isZero() {
-		return true;
-	}
-	
-	@Override
-	public boolean isRangeZero(int start, int length) {
-		return true;
-	}
+  @Override public Vector0 clone() {
+    return this;
+  }
 
-	@Override
-	public double elementSquaredSum() {
-		return 0.0;
-	}
+  @Override public boolean isMutable() {
+    return false;
+  }
 
-	@Override
-	public double magnitude() {
-		return 0.0;
-	}
+  @Override public boolean isFullyMutable() {
+    return true;
+  }
 
-	@Override
-	public AVector join(AVector v) {
-		return v;
-	}
+  @Override public int hashCode() {
+    return 1;
+  }
 
-	@Override
-	public Vector0 immutable() {
-		return this;
-	}
-	
-	@Override
-	public Vector dense() {
-		return Vector.EMPTY;
-	}
-	
-	@Override
-	public double[] toDoubleArray() {
-		return DoubleArrays.EMPTY;
-	}
-	
-	@Override
-	public boolean equals(AVector v) {
-		return v.length()==0;
-	}
-	
-	@Override
-	public boolean equalsArray(double[] data, int offset) {
-		return true;
-	}
-	
-	@Override
-	public Vector0 subVector(int start, int length) {
-		if ((start==0)&&(length==0)) return this;
-		throw new IndexOutOfBoundsException(ErrorMessages.invalidRange(this, start, length));
-	}
+  @Override public boolean isZero() {
+    return true;
+  }
 
-	/**
+  @Override public boolean isRangeZero(int start, int length) {
+    return true;
+  }
+
+  @Override public double elementSquaredSum() {
+    return 0.0;
+  }
+
+  @Override public double magnitude() {
+    return 0.0;
+  }
+
+  @Override public AVector join(AVector v) {
+    return v;
+  }
+
+  @Override public Vector0 immutable() {
+    return this;
+  }
+
+  @Override public Vector dense() {
+    return Vector.EMPTY;
+  }
+
+  @Override public double[] toDoubleArray() {
+    return DoubleArrays.EMPTY;
+  }
+
+  @Override public boolean equals(AVector v) {
+    return v.length() == 0;
+  }
+
+  @Override public boolean equalsArray(double[] data, int offset) {
+    return true;
+  }
+
+  @Override public Vector0 subVector(int start, int length) {
+    if ((start == 0) && (length == 0)) {
+      return this;
+    }
+    throw new IndexOutOfBoundsException(ErrorMessages.invalidRange(this, start, length));
+  }
+
+  /**
 	 * readResolve method to ensure we always use the singleton
 	 */
-	private Object readResolve() throws ObjectStreamException {
-		return INSTANCE;
-	}
+  private Object readResolve() throws ObjectStreamException {
+    return INSTANCE;
+  }
 
-	@Override
-	public Vector0 exactClone() {
-		// immutable, so return self
-		return this;
-	}
+  @Override public Vector0 exactClone() {
+    return this;
+  }
 
-	@Override
-	public double dotProduct(double[] data, int offset) {
-		return 0.0;
-	}
+  @Override public double dotProduct(double[] data, int offset) {
+    return 0.0;
+  }
 
-	@Override
-	public double dotProduct(double[] data, int offset, int stride) {
-		return 0.0;
-	}
+  @Override public double dotProduct(double[] data, int offset, int stride) {
+    return 0.0;
+  }
 }
