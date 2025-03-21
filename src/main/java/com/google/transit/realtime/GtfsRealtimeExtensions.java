b0c897fd1babc6b1d4395c5ea19c4ea01bd504fa
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 00:55:37 +0000

    Mining Framework Analysis

--- src/main/java/com/google/transit/realtime/GtfsRealtimeExtensions.java
+++ src/main/java/com/google/transit/realtime/GtfsRealtimeExtensions.java
@@ -1,37 +1,4 @@
-/**
- * Portions of this module are modifications based on work created and shared by Google:
- *
- *   http://code.google.com/policies.html
- *
- * and used according to terms described in the Creative Commons 3.0 Attribution License:
- *
- *   http://creativecommons.org/licenses/by/3.0/
- *
- * Specifically, the gtfs-realtime.proto:
- *
- *   http://code.google.com/transit/realtime/docs/gtfs-realtime_proto.html
- *
- * was used to generate the classes contained within.
- *
- * Portions of this module are modifications based on work created by the New York
- * City Metropolitan Transportation Authority.
- *
- * All code is Copyright (C) 2011 Google, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *          http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- */
 package com.google.transit.realtime;
-
 import com.google.protobuf.ExtensionRegistry;
 
 /**
@@ -40,7 +7,6 @@ import com.google.protobuf.ExtensionRegistry;
  * @author bdferris
  */
 public class GtfsRealtimeExtensions {
-
   /**
    * Adds all known GTFS-realtime extension messages to the specified extension
    * registry, except LIRR, unless includeLIRR=true. If includeLIRR=true, the
@@ -52,12 +18,29 @@ public class GtfsRealtimeExtensions {
    * default to MNR.
    * 
    * @param registry registry to add the extensions to
+   * @param includeLIRR if true, include LIRR extension; if false, include MNR
+   *                    extension.
    */
   public static void registerExtensions(ExtensionRegistry registry) {
-    GtfsRealtimeNYCT.registerAllExtensions(registry);
+    registry.add(GtfsRealtimeMTARR.mtaRailroadStopTimeUpdate);
+
+<<<<<<< /usr/src/app/output/onebusaway/onebusaway-gtfs-realtime-api/96a36bebafe864cf710f1c7d4fb59d6b0f6be102/src/main/java/com/google/transit/realtime/GtfsRealtimeExtensions.java/left.java
+    GtfsRealtimeNYCT.registerAllExtensions(registry)
+=======
+    registry.add(GtfsRealtimeMTARR.carriageDescriptor)
+>>>>>>> /usr/src/app/output/onebusaway/onebusaway-gtfs-realtime-api/96a36bebafe864cf710f1c7d4fb59d6b0f6be102/src/main/java/com/google/transit/realtime/GtfsRealtimeExtensions.java/right.java
+    ;
     GtfsRealtimeOneBusAway.registerAllExtensions(registry);
-    GtfsRealtimeMTARR.registerAllExtensions(registry)
+
+<<<<<<< /usr/src/app/output/onebusaway/onebusaway-gtfs-realtime-api/96a36bebafe864cf710f1c7d4fb59d6b0f6be102/src/main/java/com/google/transit/realtime/GtfsRealtimeExtensions.java/left.java
+    if (includeLIRR) {
+      GtfsRealtimeLIRR.registerAllExtensions(registry);
+    } else {
+      GtfsRealtimeMNR.registerAllExtensions(registry);
+    }
+=======
+>>>>>>> Unknown file: This is a bug in JDime.
+
     GtfsRealtimeServiceStatus.registerAllExtensions(registry);
   }
-
 }
