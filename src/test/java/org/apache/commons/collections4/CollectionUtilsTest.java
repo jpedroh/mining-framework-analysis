20e2359c36f9863d85a4bc8136e59152c227ab7b
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 04:16:20 +0000

    Mining Framework Analysis

--- src/test/java/org/apache/commons/collections4/CollectionUtilsTest.java
+++ src/test/java/org/apache/commons/collections4/CollectionUtilsTest.java
@@ -25,7 +25,6 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
 
 import java.util.ArrayList;
 import java.util.Arrays;
