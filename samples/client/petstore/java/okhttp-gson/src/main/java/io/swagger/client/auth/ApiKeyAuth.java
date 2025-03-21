79bbf46697824c7f85950ff2011a680a9dace930
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 22:48:14 +0000

    Mining Framework Analysis

--- samples/client/petstore/java/okhttp-gson/src/main/java/io/swagger/client/auth/ApiKeyAuth.java
+++ samples/client/petstore/java/okhttp-gson/src/main/java/io/swagger/client/auth/ApiKeyAuth.java
@@ -1,16 +1,21 @@
 package io.swagger.client.auth;
-
 import io.swagger.client.Pair;
-
 import java.util.Map;
 import java.util.List;
 
-@javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaClientCodegen", date = "2015-11-25T15:16:28.364+08:00")
-public class ApiKeyAuth implements Authentication {
+@javax.annotation.Generated(value = { "class io.swagger.codegen.languages.JavaClientCodegen" }, date = 
+<<<<<<< /usr/src/app/output/wordnik/swagger-codegen/55bab1f6b6dd20f44c7d16e1a5f74d1ceef0134c/samples/client/petstore/java/okhttp-gson/src/main/java/io/swagger/client/auth/ApiKeyAuth.java/left.java
+"2015-11-25T15:16:28.364+08:00"
+=======
+"2015-11-20T17:28:54.086+08:00"
+>>>>>>> /usr/src/app/output/wordnik/swagger-codegen/55bab1f6b6dd20f44c7d16e1a5f74d1ceef0134c/samples/client/petstore/java/okhttp-gson/src/main/java/io/swagger/client/auth/ApiKeyAuth.java/right.java
+) public class ApiKeyAuth implements Authentication {
   private final String location;
+
   private final String paramName;
 
   private String apiKey;
+
   private String apiKeyPrefix;
 
   public ApiKeyAuth(String location, String paramName) {
@@ -42,8 +47,7 @@ public class ApiKeyAuth implements Authentication {
     this.apiKeyPrefix = apiKeyPrefix;
   }
 
-  @Override
-  public void applyToParams(List<Pair> queryParams, Map<String, String> headerParams) {
+  @Override public void applyToParams(List<Pair> queryParams, Map<String, String> headerParams) {
     if (apiKey == null) {
       return;
     }
@@ -55,8 +59,10 @@ public class ApiKeyAuth implements Authentication {
     }
     if (location == "query") {
       queryParams.add(new Pair(paramName, value));
-    } else if (location == "header") {
-      headerParams.put(paramName, value);
+    } else {
+      if (location == "header") {
+        headerParams.put(paramName, value);
+      }
     }
   }
 }
