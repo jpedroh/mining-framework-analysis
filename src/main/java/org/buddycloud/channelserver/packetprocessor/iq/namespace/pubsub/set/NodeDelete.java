0100b5a21f9207e9c2bc12ca24ebffdb00f9c9a9
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 08:18:06 +0000

    Mining Framework Analysis

--- src/main/java/org/buddycloud/channelserver/packetprocessor/iq/namespace/pubsub/set/NodeDelete.java
+++ src/main/java/org/buddycloud/channelserver/packetprocessor/iq/namespace/pubsub/set/NodeDelete.java
@@ -1,9 +1,7 @@
 package org.buddycloud.channelserver.packetprocessor.iq.namespace.pubsub.set;
-
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.concurrent.BlockingQueue;
-
 import org.buddycloud.channelserver.Configuration;
 import org.buddycloud.channelserver.channel.ChannelManager;
 import org.buddycloud.channelserver.db.exception.NodeStoreException;
@@ -24,166 +22,155 @@ import org.xmpp.resultsetmanagement.ResultSet;
 import org.xmpp.resultsetmanagement.ResultSetImpl;
 
 public class NodeDelete extends PubSubElementProcessorAbstract {
+  private static final String NODE_REG_EX = "^/user/[^@]+@[^/]+/[^/]+$";
 
-    private static final String NODE_REG_EX = "^/user/[^@]+@[^/]+/[^/]+$";
-	private ResultSet<NodeSubscription> subscriptions;
+  private ResultSet<NodeSubscription> subscriptions;
 
-    public NodeDelete(BlockingQueue<Packet> outQueue, ChannelManager channelManager) {
-        setChannelManager(channelManager);
-        setOutQueue(outQueue);
+  public NodeDelete(BlockingQueue<Packet> outQueue, ChannelManager channelManager) {
+    setChannelManager(channelManager);
+    setOutQueue(outQueue);
+  }
+
+  public void process(Element elm, JID actorJID, IQ reqIQ, Element rsm) throws Exception {
+    this.element = elm;
+    this.response = IQ.createResultIQ(reqIQ);
+    this.request = reqIQ;
+    this.actor = actorJID;
+    this.node = element.attributeValue("node");
+    if (actorJID == null) {
+      actor = request.getFrom();
     }
+    if (!nodePresent() || !nodeValid()) {
+      outQueue.put(response);
+      return;
+    }
+    if (!Configuration.getInstance().isLocalNode(node)) {
+      makeRemoteRequest();
+      return;
+    }
+    if (!nodeExists() || !actorIsRegistered() || !nodeHandledByThisServer() || !actorAllowedToDelete()) {
+      outQueue.put(response);
+      return;
+    }
+    getNodeListeners();
+    deleteNode();
+    sendNotifications();
+  }
 
-    public void process(Element elm, JID actorJID, IQ reqIQ, Element rsm) throws Exception {
+  private void getNodeListeners() {
+    try {
+      subscriptions = channelManager.getNodeSubscriptionListeners(node);
+    } catch (NodeStoreException e) {
+      subscriptions = new ResultSetImpl<NodeSubscription>(new ArrayList<NodeSubscription>());
+    }
+  }
 
-        this.element = elm;
-        this.response = IQ.createResultIQ(reqIQ);
-        this.request = reqIQ;
-        this.actor = actorJID;
-        this.node = element.attributeValue("node");
-
-        if (actorJID == null) {
-            actor = request.getFrom();
+  private void sendNotifications() throws NodeStoreException {
+    try {
+      Message notification = createNotificationMessage();
+      if (subscriptions != null) {
+        for (NodeSubscription subscription : subscriptions) {
+          notification.setTo(subscription.getListener().toString());
+          outQueue.put(notification.createCopy());
         }
-        if (!nodePresent() || !nodeValid()) {
-            outQueue.put(response);
-            return;
-        }
-        if (!Configuration.getInstance().isLocalNode(node)) {
-            makeRemoteRequest();
-            return;
-        }
-        if (!nodeExists() || !actorIsRegistered() || !nodeHandledByThisServer()
-                || !actorAllowedToDelete()) {
-            outQueue.put(response);
-            return;
-        }
-        getNodeListeners();
-        deleteNode();
-        sendNotifications();
+      }
+      Collection<JID> admins = getAdminUsers();
+      for (JID admin : admins) {
+        notification.setTo(admin);
+        outQueue.put(notification.createCopy());
+      }
+    } catch (Exception e) {
+      logger.error(e);
     }
+  }
 
-    private void getNodeListeners() {
-    	try {
-			subscriptions = channelManager.getNodeSubscriptionListeners(node);
-		} catch (NodeStoreException e) {
-			subscriptions = new ResultSetImpl<NodeSubscription>(new ArrayList<NodeSubscription>());
-		}
-	}
+  private Message createNotificationMessage() {
+    Message notification = new Message();
+    notification.setType(Message.Type.headline);
+    notification.getElement().addAttribute("remote-server-discover", "false");
+    Element eventEl = notification.addChildElement("event", JabberPubsub.NS_PUBSUB_EVENT);
+    Element deleteEl = eventEl.addElement("delete");
+    deleteEl.addAttribute("node", node);
+    return notification;
+  }
 
-	private void sendNotifications() throws NodeStoreException {
-        try {
-            Message notification = createNotificationMessage();
-            if (subscriptions != null) {
-                for (NodeSubscription subscription : subscriptions) {
-                    notification.setTo(subscription.getListener().toString());
-                    outQueue.put(notification.createCopy());
-                }
-            }
-            Collection<JID> admins = getAdminUsers();
-            for (JID admin : admins) {
-                notification.setTo(admin);
-                outQueue.put(notification.createCopy());
-            }
-        } catch (Exception e) {
-            logger.error(e);
-        }
+  private void deleteNode() throws InterruptedException {
+    try {
+      channelManager.deleteNode(node);
+    } catch (NodeStoreException e) {
+      logger.error(e);
+      setErrorCondition(PacketError.Type.wait, PacketError.Condition.internal_server_error);
+      outQueue.put(response);
+      return;
     }
+    response.setType(IQ.Type.result);
+    outQueue.put(response);
+  }
 
-    private Message createNotificationMessage() {
-        Message notification = new Message();
-        notification.setType(Message.Type.headline);
-        notification.getElement().addAttribute("remote-server-discover", "false");
-        Element eventEl = notification.addChildElement("event", JabberPubsub.NS_PUBSUB_EVENT);
-        Element deleteEl = eventEl.addElement("delete");
-        deleteEl.addAttribute("node", node);
-        return notification;
+  public boolean accept(Element elm) {
+    return elm.getName().equals("delete");
+  }
+
+  private boolean nodePresent() {
+    if (node != null && !node.trim().equals("")) {
+      return true;
     }
+    response.setType(IQ.Type.error);
+    Element nodeIdRequired = new DOMElement("nodeid-required", new Namespace("", JabberPubsub.NS_PUBSUB_ERROR));
+    Element badRequest = new DOMElement(PacketError.Condition.bad_request.toXMPP(), new Namespace("", JabberPubsub.NS_XMPP_STANZAS));
+    Element error = new DOMElement("error");
+    error.addAttribute("type", "modify");
+    error.add(badRequest);
+    error.add(nodeIdRequired);
+    response.setChildElement(error);
+    return false;
+  }
 
-    private void deleteNode() throws InterruptedException {
-        try {
-            channelManager.deleteNode(node);
-        } catch (NodeStoreException e) {
-            logger.error(e);
-            setErrorCondition(PacketError.Type.wait, PacketError.Condition.internal_server_error);
-            outQueue.put(response);
-            return;
-        }
-        response.setType(IQ.Type.result);
-        outQueue.put(response);
+  private boolean nodeExists() throws NodeStoreException {
+    if (channelManager.nodeExists(node)) {
+      return true;
     }
+    setErrorCondition(PacketError.Type.cancel, PacketError.Condition.item_not_found);
+    return false;
+  }
 
-    public boolean accept(Element elm) {
-        return elm.getName().equals("delete");
+  private boolean actorIsRegistered() {
+    if (actor.getDomain().equals(getServerDomain())) {
+      return true;
     }
+    setErrorCondition(PacketError.Type.auth, PacketError.Condition.forbidden);
+    return false;
+  }
 
-    private boolean nodePresent() {
-        if (node != null && !node.trim().equals("")) {
-            return true;
-        }
-        response.setType(IQ.Type.error);
-        Element nodeIdRequired = new DOMElement("nodeid-required",
-                new Namespace("", JabberPubsub.NS_PUBSUB_ERROR));
-        Element badRequest = new DOMElement(
-                PacketError.Condition.bad_request.toXMPP(), new Namespace("",
-                        JabberPubsub.NS_XMPP_STANZAS));
-        Element error = new DOMElement("error");
-        error.addAttribute("type", "modify");
-        error.add(badRequest);
-        error.add(nodeIdRequired);
-        response.setChildElement(error);
-        return false;
+  private boolean actorAllowedToDelete() throws NodeStoreException {
+    boolean isOwner = channelManager.getNodeMembership(node, actor).getAffiliation().equals(Affiliations.owner);
+    if (isOwner) {
+      return true;
     }
+    setErrorCondition(PacketError.Type.auth, PacketError.Condition.not_authorized);
+    return false;
+  }
 
-    private boolean nodeExists() throws NodeStoreException {
-        if (channelManager.nodeExists(node)) {
-            return true;
-        }
-        setErrorCondition(PacketError.Type.cancel, PacketError.Condition.item_not_found);
-        return false;
+  private boolean nodeHandledByThisServer() {
+    if (!node.contains("@" + getServerDomain()) && !node.contains("@" + getTopicsDomain())) {
+      setErrorCondition(PacketError.Type.modify, PacketError.Condition.not_acceptable);
+      return false;
     }
+    return true;
+  }
 
-    private boolean actorIsRegistered() {
-        if (actor.getDomain().equals(getServerDomain())) {
-            return true;
-        }
-        setErrorCondition(PacketError.Type.auth, PacketError.Condition.forbidden);
-        return false;
+  private boolean nodeValid() {
+    if (!node.matches(NODE_REG_EX)) {
+      setErrorCondition(PacketError.Type.modify, PacketError.Condition.bad_request);
+      return false;
     }
+    return true;
+  }
 
-    private boolean actorAllowedToDelete() throws NodeStoreException {
-        boolean isOwner = channelManager.getNodeMembership(node, actor).getAffiliation().equals(Affiliations.owner);
-
-        if (isOwner) {
-            return true;
-        }
-        setErrorCondition(PacketError.Type.auth, PacketError.Condition.not_authorized);
-        return false;
-    }
-
-    private boolean nodeHandledByThisServer() {
-        if (!node.contains("@" + getServerDomain())
-                && !node.contains("@" + getTopicsDomain())) {
-            setErrorCondition(PacketError.Type.modify,
-                    PacketError.Condition.not_acceptable);
-            return false;
-        }
-        return true;
-    }
-
-    private boolean nodeValid() {
-        if (!node.matches(NODE_REG_EX)) {
-            setErrorCondition(PacketError.Type.modify,
-                    PacketError.Condition.bad_request);
-            return false;
-        }
-        return true;
-    }
-
-    private void makeRemoteRequest() throws InterruptedException {
-        request.setTo(new JID(node.split("/")[2]).getDomain());
-        Element actor = request.getElement().element("pubsub")
-                .addElement("actor", Buddycloud.NS);
-        actor.addText(request.getFrom().toBareJID());
-        outQueue.put(request);
-    }
+  private void makeRemoteRequest() throws InterruptedException {
+    request.setTo(new JID(node.split("/")[2]).getDomain());
+    Element actor = request.getElement().element("pubsub").addElement("actor", Buddycloud.NS);
+    actor.addText(request.getFrom().toBareJID());
+    outQueue.put(request);
+  }
 }
