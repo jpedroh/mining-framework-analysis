14d16c007f97aba9dd7e188b77884b96f65444cb
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 04:03:56 +0000

    Mining Framework Analysis

--- src/main/java/net/masterthought/cucumber/util/Util.java
+++ src/main/java/net/masterthought/cucumber/util/Util.java
@@ -1,12 +1,10 @@
 package net.masterthought.cucumber.util;
-
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.util.HashMap;
 import java.util.Locale;
-
-import org.apache.commons.lang3.StringEscapeUtils;
 import net.masterthought.cucumber.json.Element;
+import org.apache.commons.lang3.StringEscapeUtils;
 import net.masterthought.cucumber.json.Step;
 import net.masterthought.cucumber.json.support.Status;
 import org.joda.time.Period;
@@ -14,67 +12,49 @@ import org.joda.time.format.PeriodFormatter;
 import org.joda.time.format.PeriodFormatterBuilder;
 
 public final class Util {
+  public static final NumberFormat PERCENT_FORMATTER = NumberFormat.getPercentInstance(Locale.US);
 
-    // provide Locale so tests can validate . (instead of ,) separator
-    public static final NumberFormat PERCENT_FORMATTER = NumberFormat.getPercentInstance(Locale.US);
-    static {
-        PERCENT_FORMATTER.setMinimumFractionDigits(2);
-        PERCENT_FORMATTER.setMaximumFractionDigits(2);
-    }
+  static {
+    PERCENT_FORMATTER.setMinimumFractionDigits(2);
+    PERCENT_FORMATTER.setMaximumFractionDigits(2);
+  }
 
-    private static final NumberFormat DECIMAL_FORMATTER = DecimalFormat.getInstance(Locale.US);
+  private static final NumberFormat DECIMAL_FORMATTER = DecimalFormat.getInstance(Locale.US);
 
-    static {
-        DECIMAL_FORMATTER.setMinimumFractionDigits(2);
-        DECIMAL_FORMATTER.setMaximumFractionDigits(2);
-    }
+  static {
+    DECIMAL_FORMATTER.setMinimumFractionDigits(2);
+    DECIMAL_FORMATTER.setMaximumFractionDigits(2);
+  }
 
-    public static final Util INSTANCE = new Util();
+  public static final Util INSTANCE = new Util();
 
-    private static final PeriodFormatter TIME_FORMATTER = new PeriodFormatterBuilder()
-            .appendDays()
-            .appendSuffix("d")
-            .appendSeparator(" ")
-            .appendHours()
-            .appendSuffix("h")
-            .appendSeparator(" ")
-            .appendMinutes()
-            .appendSuffix("m")
-            .appendSeparator(" ")
-            .appendSeconds()
-            .appendSuffix("s")
-            .appendSeparator(" ")
-            .minimumPrintedDigits(3)
-            .appendMillis()
-            .appendSuffix("ms")
-            .toFormatter();
+  private static final PeriodFormatter TIME_FORMATTER = new PeriodFormatterBuilder().appendDays().appendSuffix("d").appendSeparator(" ").appendHours().appendSuffix("h").appendSeparator(" ").appendMinutes().appendSuffix("m").appendSeparator(" ").appendSeconds().appendSuffix("s").appendSeparator(" ").minimumPrintedDigits(3).appendMillis().appendSuffix("ms").toFormatter();
 
-    private Util() {
-    }
+  private Util() {
+  }
 
-    public static String formatDuration(long duration) {
-        return TIME_FORMATTER.print(new Period(0, duration / 1000000));
-    }
+  public static String formatDuration(long duration) {
+    return TIME_FORMATTER.print(new Period(0, duration / 1000000));
+  }
 
-    /**
+  /**
      * Returns value converted to percentage format.
      *
      * @param value value to convert
      * @param total sum of all values
      * @return converted values including '%' character
      */
-    public static String formatAsPercentage(int value, int total) {
-        // value '1F' is to force floating conversion instead of loosing decimal part
-        float average = total == 0 ? 0 : 1F * value / total;
-        return PERCENT_FORMATTER.format(average);
-    }
+  public static String formatAsPercentage(int value, int total) {
+    float average = total == 0 ? 0 : 1F * value / total;
+    return PERCENT_FORMATTER.format(average);
+  }
 
-    public static String formatAsDecimal(int value, int total) {
-        float average = total == 0 ? 0 : 100F * value / total;
-        return DECIMAL_FORMATTER.format(average);
-    }
+  public static String formatAsDecimal(int value, int total) {
+    float average = total == 0 ? 0 : 100F * value / total;
+    return DECIMAL_FORMATTER.format(average);
+  }
 
-    /**
+  /**
      * Converts characters of passed string by replacing to dash (-) each character that might not be accepted as file
      * name such as / ? or &gt;.
      * 
@@ -82,34 +62,30 @@ public final class Util {
      *            sequence that should be converted
      * @return converted string
      */
-    public static String toValidFileName(String value) {
-        return StringEscapeUtils.escapeJava(value).replaceAll("[^\\d\\w]", "-");
-    }
+  public static String toValidFileName(String value) {
+    return StringEscapeUtils.escapeJava(value).replaceAll("[^\\d\\w]", "-");
+  }
 
-    /**
+  /**
      *
      * @param elements the array of elements containing the scenario and step data
      * @return {@code failedMap} object containing the failure information
      */
-    public static HashMap<String, String[]> getFailedCauseMap(Element[] elements) {
-        HashMap<String, String[]> failedMap = new HashMap<>();
-        int failedCount = 0;
-        for(Element element : elements) {
-            if(element.isScenario() && (!element.getStatus().isPassed())) {
-                Element scenario = element;
-                for(Step step : scenario.getSteps()) {
-                    if(step.getResult().getStatus().equals(Status.FAILED)) {
-                        String errorMessage = step.getResult().getErrorMessage();
-                        String[] info = {
-                                scenario.getName(),
-                                step.getName(),
-                                errorMessage == null ? "Error message not found." : errorMessage
-                        };
-                        failedMap.put(String.valueOf(failedCount++), info);
-                    }
-                }
-            }
+  public static HashMap<String, String[]> getFailedCauseMap(Element[] elements) {
+    HashMap<String, String[]> failedMap = new HashMap<>();
+    int failedCount = 0;
+    for (Element element : elements) {
+      if (element.isScenario() && (!element.getStatus().isPassed())) {
+        Element scenario = element;
+        for (Step step : scenario.getSteps()) {
+          if (step.getResult().getStatus().equals(Status.FAILED)) {
+            String errorMessage = step.getResult().getErrorMessage();
+            String[] info = { scenario.getName(), step.getName(), errorMessage == null ? "Error message not found." : errorMessage };
+            failedMap.put(String.valueOf(failedCount++), info);
+          }
         }
-        return failedMap;
+      }
     }
+    return failedMap;
+  }
 }
--- src/test/java/net/masterthought/cucumber/generators/integrations/FeaturesOverviewPageIntegrationTest.java
+++ src/test/java/net/masterthought/cucumber/generators/integrations/FeaturesOverviewPageIntegrationTest.java
@@ -1,9 +1,6 @@
 package net.masterthought.cucumber.generators.integrations;
-
 import static org.assertj.core.api.Assertions.assertThat;
-
 import org.junit.Test;
-
 import net.masterthought.cucumber.generators.FeaturesOverviewPage;
 import net.masterthought.cucumber.generators.integrations.helpers.DocumentAssertion;
 import net.masterthought.cucumber.generators.integrations.helpers.LeadAssertion;
@@ -14,157 +11,94 @@ import net.masterthought.cucumber.generators.integrations.helpers.WebAssertion;
  * @author Damian Szczepanik (damianszczepanik@github)
  */
 public class FeaturesOverviewPageIntegrationTest extends PageTest {
+  @Test public void generatePage_generatesTitle() {
+    setUpWithJson(SAMPLE_JSON);
+    configuration.setRunWithJenkins(true);
+    configuration.setBuildNumber("1");
+    page = new FeaturesOverviewPage(reportResult, configuration);
+    final String titleValue = String.format("Cucumber Reports (no %s) - Features Overview", configuration.getBuildNumber());
+    page.generatePage();
+    DocumentAssertion document = documentFrom(page.getWebPage());
+    String title = document.getHead().getTitle();
+    assertThat(title).isEqualTo(titleValue);
+  }
 
-    @Test
-    public void generatePage_generatesTitle() {
+  @Test public void generatePage_generatesLead() {
+    setUpWithJson(SAMPLE_JSON);
+    page = new FeaturesOverviewPage(reportResult, configuration);
+    page.generatePage();
+    DocumentAssertion document = documentFrom(page.getWebPage());
+    LeadAssertion lead = document.getLead();
+    assertThat(lead.getHeader()).isEqualTo("Features Statistics");
+    assertThat(lead.getDescription()).isEqualTo("The following graphs show passing and failing statistics for features");
+  }
 
-        // given
-        setUpWithJson(SAMPLE_JSON);
-        configuration.setRunWithJenkins(true);
-        configuration.setBuildNumber("1");
-        page = new FeaturesOverviewPage(reportResult, configuration);
-        final String titleValue = String.format("Cucumber Reports (no %s) - Features Overview",
-                configuration.getBuildNumber());
-
-        // when
-        page.generatePage();
-
-        // then
-        DocumentAssertion document = documentFrom(page.getWebPage());
-        String title = document.getHead().getTitle();
-
-        assertThat(title).isEqualTo(titleValue);
+  @Test public void generatePage_generatesClassifications() {
+    final String[] names = { "Platform", "Browser", "Branch", "Repository" };
+    final String[] values = { "Win", "Opera", "master", "<a href=\"example.com\" rel=\"nofollow noopener noreferrer\">Example Repository</a>" };
+    setUpWithJson(SAMPLE_JSON);
+    for (int i = 0; i < names.length; i++) {
+      configuration.addClassifications(names[i], values[i]);
     }
-
-    @Test
-    public void generatePage_generatesLead() {
-
-        // given
-        setUpWithJson(SAMPLE_JSON);
-        page = new FeaturesOverviewPage(reportResult, configuration);
-
-        // when
-        page.generatePage();
-
-        // then
-        DocumentAssertion document = documentFrom(page.getWebPage());
-        LeadAssertion lead = document.getLead();
-
-        assertThat(lead.getHeader()).isEqualTo("Features Statistics");
-        assertThat(lead.getDescription()).isEqualTo("The following graphs show passing and failing statistics for features");
+    page = new FeaturesOverviewPage(reportResult, configuration);
+    page.generatePage();
+    DocumentAssertion document = documentFrom(page.getWebPage());
+    TableRowAssertion[] classifications = document.getClassifications();
+    assertThat(classifications).hasSize(names.length);
+    for (int i = 0; i < names.length; i++) {
+      String[] cells = classifications[i].getCellsHtml();
+      assertThat(cells).containsExactly(names[i], values[i]);
     }
+  }
 
-    @Test
-    public void generatePage_generatesClassifications() {
+  @Test public void generatePage_generatesCharts() {
+    setUpWithJson(SAMPLE_JSON);
+    page = new FeaturesOverviewPage(reportResult, configuration);
+    page.generatePage();
+    DocumentAssertion document = documentFrom(page.getWebPage());
+    assertThat(document.byId("charts", WebAssertion.class)).isNotNull();
+  }
 
-        // given
-        final String[] names = {"Platform", "Browser", "Branch", "Repository"};
-        final String[] values = {"Win", "Opera", "master", "<a href=\"example.com\" rel=\"nofollow noopener noreferrer\">Example Repository</a>"};
-        setUpWithJson(SAMPLE_JSON);
-        for (int i = 0; i < names.length; i++) {
-            configuration.addClassifications(names[i], values[i]);
-        }
-        page = new FeaturesOverviewPage(reportResult, configuration);
+  @Test public void generatePage_generatesStatsTableHeader() {
+    setUpWithJson(SAMPLE_JSON);
+    page = new FeaturesOverviewPage(reportResult, configuration);
+    page.generatePage();
+    DocumentAssertion document = documentFrom(page.getWebPage());
+    TableRowAssertion[] headerRows = document.getReport().getTableStats().getHeaderRows();
+    assertThat(headerRows).hasSize(2);
+    TableRowAssertion firstRow = headerRows[0];
+    firstRow.hasExactValues("", "Steps", "Scenarios", "Features");
+    TableRowAssertion secondRow = headerRows[1];
+    secondRow.hasExactValues("Feature", "Passed", "Failed", "Skipped", "Pending", "Undefined", "Total", "Passed", "Failed", "Total", "Duration", "Status");
+  }
 
-        // when
-        page.generatePage();
+  @Test public void generatePage_generatesStatsTableBody() {
+    setUpWithJson(SAMPLE_JSON);
+    page = new FeaturesOverviewPage(reportResult, configuration);
+    page.generatePage();
+    DocumentAssertion document = documentFrom(page.getWebPage());
+    TableRowAssertion[] bodyRows = document.getReport().getTableStats().getBodyRows();
+    assertThat(bodyRows).hasSize(2);
+    TableRowAssertion firstRow = bodyRows[0];
+    firstRow.hasExactValues("1st feature", "10", "0", "0", "0", "0", "10", "1", "0", "1", "1m 39s 263ms", "Passed");
+    firstRow.hasExactCSSClasses("tagname", "passed", "", "", "", "", "total", "passed", "", "total", "duration", "passed");
+    firstRow.hasExactDataValues("", "", "", "", "", "", "", "", "", "", "99263122889", "");
+    firstRow.getReportLink().hasLabelAndAddress("1st feature", "report-feature_net-masterthought-example-s--ATM--u6771-u4EAC-feature.html");
+    TableRowAssertion secondRow = bodyRows[1];
+    secondRow.hasExactValues("Second feature", "5", "1", "2", "1", "2", "11", "1", "1", "2", "092ms", "Failed Account may not have sufficient funds the card is valid Error message not found.");
+    secondRow.hasExactCSSClasses("tagname", "passed", "failed", "skipped", "pending", "undefined", "total", "passed", "failed", "total", "duration", "failed");
+    secondRow.hasExactDataValues("", "", "", "", "", "", "", "", "", "", "92610000", "");
+    secondRow.getReportLink().hasLabelAndAddress("Second feature", "report-feature_net-masterthought-example-ATMK-feature.html");
+  }
 
-        // then
-        DocumentAssertion document = documentFrom(page.getWebPage());
-        TableRowAssertion[] classifications = document.getClassifications();
-
-        assertThat(classifications).hasSize(names.length);
-        for (int i = 0; i < names.length; i++) {
-            String[] cells = classifications[i].getCellsHtml();
-            assertThat(cells).containsExactly(names[i], values[i]);
-        }
-    }
-
-    @Test
-    public void generatePage_generatesCharts() {
-
-        // given
-        setUpWithJson(SAMPLE_JSON);
-        page = new FeaturesOverviewPage(reportResult, configuration);
-
-        // when
-        page.generatePage();
-
-        // then
-        DocumentAssertion document = documentFrom(page.getWebPage());
-
-        assertThat(document.byId("charts", WebAssertion.class)).isNotNull();
-    }
-
-    @Test
-    public void generatePage_generatesStatsTableHeader() {
-
-        // given
-        setUpWithJson(SAMPLE_JSON);
-        page = new FeaturesOverviewPage(reportResult, configuration);
-
-        // when
-        page.generatePage();
-
-        // then
-        DocumentAssertion document = documentFrom(page.getWebPage());
-        TableRowAssertion[] headerRows = document.getReport().getTableStats().getHeaderRows();
-
-        assertThat(headerRows).hasSize(2);
-
-        TableRowAssertion firstRow = headerRows[0];
-        firstRow.hasExactValues("", "Steps", "Scenarios", "Features");
-
-        TableRowAssertion secondRow = headerRows[1];
-        secondRow.hasExactValues("Feature", "Passed", "Failed", "Skipped", "Pending", "Undefined", "Total",
-                "Passed", "Failed", "Total", "Duration", "Status");
-    }
-
-    @Test
-    public void generatePage_generatesStatsTableBody() {
-
-        // given
-        setUpWithJson(SAMPLE_JSON);
-        page = new FeaturesOverviewPage(reportResult, configuration);
-
-        // when
-        page.generatePage();
-
-        // then
-        DocumentAssertion document = documentFrom(page.getWebPage());
-        TableRowAssertion[] bodyRows = document.getReport().getTableStats().getBodyRows();
-
-        assertThat(bodyRows).hasSize(2);
-
-        TableRowAssertion firstRow = bodyRows[0];
-        firstRow.hasExactValues("1st feature", "10", "0", "0", "0", "0", "10", "1", "0", "1", "1m 39s 263ms", "Passed");
-        firstRow.hasExactCSSClasses("tagname", "passed", "", "", "", "", "total", "passed", "", "total", "duration", "passed");
-        firstRow.hasExactDataValues("", "", "", "", "", "", "", "", "", "", "99263122889", "");
-        firstRow.getReportLink().hasLabelAndAddress("1st feature", "report-feature_net-masterthought-example-s--ATM--u6771-u4EAC-feature.html");
-
-        TableRowAssertion secondRow = bodyRows[1];
-        secondRow.hasExactValues("Second feature", "5", "1", "2", "1", "2", "11", "1", "1", "2", "092ms", "Failed Account may not have sufficient funds the card is valid Error message not found.");
-        secondRow.hasExactCSSClasses("tagname", "passed", "failed", "skipped", "pending", "undefined", "total", "passed", "failed", "total", "duration", "failed");
-        secondRow.hasExactDataValues("", "", "", "", "", "", "", "", "", "", "92610000", "");
-        secondRow.getReportLink().hasLabelAndAddress("Second feature", "report-feature_net-masterthought-example-ATMK-feature.html");
-    }
-
-    @Test
-    public void generatePage_generatesStatsTableFooter() {
-
-        // given
-        setUpWithJson(SAMPLE_JSON);
-        page = new FeaturesOverviewPage(reportResult, configuration);
-
-        // when
-        page.generatePage();
-
-        // then
-        DocumentAssertion document = documentFrom(page.getWebPage());
-        TableRowAssertion[] footerRows = document.getReport().getTableStats().getAllFooterRows();
-
-        assertThat(footerRows).hasSize(2);
-        footerRows[0].hasExactValues("2", "15", "1", "2", "1", "2", "21", "2", "1", "3", "1m 39s 355ms", "");
-        footerRows[1].hasExactValues("", "71.43%", "4.76%", "9.52%", "4.76%", "9.52%", "", "66.67%", "33.33%", "", "", "50.00%");
-    }
+  @Test public void generatePage_generatesStatsTableFooter() {
+    setUpWithJson(SAMPLE_JSON);
+    page = new FeaturesOverviewPage(reportResult, configuration);
+    page.generatePage();
+    DocumentAssertion document = documentFrom(page.getWebPage());
+    TableRowAssertion[] footerRows = document.getReport().getTableStats().getAllFooterRows();
+    assertThat(footerRows).hasSize(2);
+    footerRows[0].hasExactValues("2", "15", "1", "2", "1", "2", "21", "2", "1", "3", "1m 39s 355ms", "");
+    footerRows[1].hasExactValues("", "71.43%", "4.76%", "9.52%", "4.76%", "9.52%", "", "66.67%", "33.33%", "", "", "50.00%");
+  }
 }
