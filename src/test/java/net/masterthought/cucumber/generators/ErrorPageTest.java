856cd958500932086be384d7f92dd9882e1810ca
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 04:16:24 +0000

    Mining Framework Analysis

--- src/test/java/net/masterthought/cucumber/generators/ErrorPageTest.java
+++ src/test/java/net/masterthought/cucumber/generators/ErrorPageTest.java
@@ -1,41 +1,7 @@
-package net.masterthought.cucumber.generators;
-
-import java.util.List;
-
-import net.masterthought.cucumber.generators.integrations.PageTest;
-import org.apache.commons.lang3.exception.ExceptionUtils;
-import org.apache.velocity.VelocityContext;
-import org.junit.Before;
-import org.junit.Test;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
-/**
- * @author Damian Szczepanik (damianszczepanik@github)
- */
-public class ErrorPageTest extends PageTest {
-
-    @Before
-    public void setUp() {
-        setUpWithJson(SAMPLE_JSON);
-    }
-
-    @Test
-    public void prepareReport_AddsCustomProperties() {
-
-        // give
-        Exception exception = new Exception();
-        page = new ErrorPage(null, configuration, exception, jsonReports);
-
-        // when
-        page.prepareReport();
-
-        // then
-        VelocityContext context = page.context;
-        assertThat(context.getKeys()).hasSize(14);
-        assertThat(context.get("classifications")).isInstanceOf(List.class);
-        assertThat(context.get("output_message")).isEqualTo(ExceptionUtils.getStackTrace(exception));
-        assertThat(context.get("json_files")).isEqualTo(jsonReports);
-        assertThat(context.get("directory_qualifier")).isEqualTo(configuration.getDirectoryQualifier());
-    }
-}
+  package    net . masterthought . cucumber . generators ;   import static      org . assertj . core . api . Assertions . assertThat ;  import   java . util . List ;  import      org . apache . commons . lang3 . exception . ExceptionUtils ;  import    org . apache . velocity . VelocityContext ;  import   org . junit . Before ;  import   org . junit . Test ;  import      net . masterthought . cucumber . generators . integrations . PageTest ;   public class ErrorPageTest  extends PageTest  {    @ Before public void setUp  ( )  {   setUpWithJson  ( SAMPLE_JSON ) ; }    @ Test public void prepareReport_AddsCustomProperties  ( )  {  Exception  exception =  new Exception  ( ) ;   page =  new ErrorPage  ( null , configuration , exception , jsonReports ) ;   page . prepareReport  ( ) ;  VelocityContext  context =  page . context ;    assertThat  (  context . getKeys  ( ) ) . hasSize  ( 
+<<<<<<<
+15
+=======
+13
+>>>>>>>
+ ) ;    assertThat  (  context . get  ( "classifications" ) ) . isInstanceOf  (  List . class ) ;    assertThat  (  context . get  ( "output_message" ) ) . isEqualTo  (  ExceptionUtils . getStackTrace  ( exception ) ) ;    assertThat  (  context . get  ( "json_files" ) ) . isEqualTo  ( jsonReports ) ;    assertThat  (  context . get  ( "directory_qualifier" ) ) . isEqualTo  (  configuration . getDirectoryQualifier  ( ) ) ; } }
