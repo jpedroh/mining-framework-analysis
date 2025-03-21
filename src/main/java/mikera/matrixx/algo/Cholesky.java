ee0c6aa1eae938fa1b52e81472b171dc50848891
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 01:52:42 +0000

    Mining Framework Analysis

--- src/main/java/mikera/matrixx/algo/Cholesky.java
+++ src/main/java/mikera/matrixx/algo/Cholesky.java
@@ -1,12 +1,11 @@
 package mikera.matrixx.algo;
-
 import mikera.matrixx.AMatrix;
 import mikera.matrixx.Matrix;
 import mikera.matrixx.algo.decompose.chol.ICholeskyResult;
 import mikera.matrixx.algo.decompose.chol.impl.SimpleCholesky;
 
 /**
- * Class implementing Cholesky decomposition
+ * Class implementing a standard Cholesky decomposition
  * 
  *    A = L.L*
  *    
@@ -20,25 +19,17 @@ import mikera.matrixx.algo.decompose.chol.impl.SimpleCholesky;
  *
  */
 public class Cholesky {
-	// TODO: refactor to use best available Cholesky decomposition algorithm for different matrix types and sizes
-	
-	/**
+  /**
 	 * Decompose a matrix according the the Cholesky decomposition A = L.L*
 	 * 
 	 * @param a Any symmetric, positive definite matrix
-	 * @return The decomposition result, or null if not possible
+	 * @return The decomposition result
 	 */
-	public static final ICholeskyResult decompose(AMatrix a) {
-		return SimpleCholesky.decompose(a.toMatrix());
-	}
-	
-	/**
-	 * Decompose a Matrix according the the Cholesky decomposition A = L.L*
-	 * 
-	 * @param a Any symmetric, positive definite matrix
-	 * @return The decomposition result, or null if not possible
-	 */
-	public static final ICholeskyResult decompose(Matrix a) {
-		return SimpleCholesky.decompose(a);
-	}
+  public static final ICholeskyResult decompose(AMatrix a) {
+    return SimpleCholesky.decompose(a.toMatrix());
+  }
+
+  public static final ICholeskyResult decompose(Matrix a) {
+    return SimpleCholesky.decompose(a);
+  }
 }
