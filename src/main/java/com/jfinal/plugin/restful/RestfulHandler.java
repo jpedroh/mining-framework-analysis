d3969f77e17623c83cf1db5ab227f58c228c46a6
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 10:42:01 +0000

    Mining Framework Analysis

--- src/main/java/com/jfinal/plugin/restful/RestfulHandler.java
+++ src/main/java/com/jfinal/plugin/restful/RestfulHandler.java
@@ -1,9 +1,7 @@
 package com.jfinal.plugin.restful;
-
 import com.jfinal.core.Action;
 import com.jfinal.core.JFinal;
 import com.jfinal.handler.Handler;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
@@ -15,59 +13,80 @@ import java.util.TreeMap;
  * 需特别按照多参数Restful Url处理时，使用@ActionKey("@GET/v1/company/:companyId/staff/:staffId")形式注解
  */
 public class RestfulHandler extends Handler {
-    private TreeMap<String, Action> actionTreeMap;
-    private boolean initialized;
+  private TreeMap<String, Action> actionTreeMap;
 
-    public void init() {
-        actionTreeMap = new TreeMap<String, Action>(new RestfulKeyComparator());
-        JFinal jf = JFinal.me();
-        List<String> actionKeys = jf.getAllActionKeys();
-        String[] empty = new String[]{null};
-        for (String k : actionKeys) {
-            if (!k.startsWith("@")) {
-                continue;
-            }
-            actionTreeMap.put(k, jf.getAction(k, empty));
-        }
-        initialized = true;
+  private boolean 
+<<<<<<< /usr/src/app/output/jfinal/jfinal/77040eaf33ff2f0c2d3c7ea6d7b3b6d0755999d7/src/main/java/com/jfinal/plugin/restful/RestfulHandler.java/left.java
+  initialized
+=======
+  init
+>>>>>>> /usr/src/app/output/jfinal/jfinal/77040eaf33ff2f0c2d3c7ea6d7b3b6d0755999d7/src/main/java/com/jfinal/plugin/restful/RestfulHandler.java/right.java
+  ;
+
+  public void init() {
+    actionTreeMap = new TreeMap<String, Action>(new RestfulKeyComparator());
+    JFinal jf = JFinal.me();
+    List<String> actionKeys = jf.getAllActionKeys();
+    String[] empty = new String[] { null };
+    for (String k : actionKeys) {
+      if (!k.startsWith("@")) {
+        continue;
+      }
+      actionTreeMap.put(k, jf.getAction(k, empty));
     }
+    initialized = true;
+  }
 
-    @Override
-    public void handle(String target, HttpServletRequest request, HttpServletResponse response, boolean[] isHandled) {
-        if (!initialized) {
-            init();
+  @Override public void handle(String target, HttpServletRequest request, HttpServletResponse response, boolean[] isHandled) {
+    if (!
+<<<<<<< /usr/src/app/output/jfinal/jfinal/77040eaf33ff2f0c2d3c7ea6d7b3b6d0755999d7/src/main/java/com/jfinal/plugin/restful/RestfulHandler.java/left.java
+    initialized
+=======
+    init
+>>>>>>> /usr/src/app/output/jfinal/jfinal/77040eaf33ff2f0c2d3c7ea6d7b3b6d0755999d7/src/main/java/com/jfinal/plugin/restful/RestfulHandler.java/right.java
+    ) {
+      actionTreeMap = new TreeMap<String, Action>(new RestfulKeyComparator());
+      JFinal jf = JFinal.me();
+      List<String> actionKeys = jf.getAllActionKeys();
+      String[] empty = new String[] { null };
+      for (String k : actionKeys) {
+        if (!k.startsWith("@")) {
+          continue;
         }
+        actionTreeMap.put(k, jf.getAction(k, empty));
+      }
 
-        String actionKey = matchActionKey(target, request.getMethod());
-
-        if (actionKey != null) {
-            parseUrlPara(target, actionKey, request);
-            target = actionKey;
-        }
-
-        next.handle(target, request, response, isHandled);
+<<<<<<< /usr/src/app/output/jfinal/jfinal/77040eaf33ff2f0c2d3c7ea6d7b3b6d0755999d7/src/main/java/com/jfinal/plugin/restful/RestfulHandler.java/left.java
+      init();
+=======
+      init = true;
+>>>>>>> /usr/src/app/output/jfinal/jfinal/77040eaf33ff2f0c2d3c7ea6d7b3b6d0755999d7/src/main/java/com/jfinal/plugin/restful/RestfulHandler.java/right.java
     }
-
-    private String matchActionKey(String url, String method) {
-        Action action = actionTreeMap.get("@" + method + url);// @GET/v1/company/:companyId/staff/:staffId
-        if (action == null) {
-            return null;
-        }
-        return action.getActionKey();
+    String actionKey = matchActionKey(target, request.getMethod());
+    if (actionKey != null) {
+      parseUrlPara(target, actionKey, request);
+      target = actionKey;
     }
+    next.handle(target, request, response, isHandled);
+  }
 
-    private void parseUrlPara(String target, String actionKey, HttpServletRequest request) {
-        String[] targetSegments = target.split("/");
-        String[] actionKeySegments = actionKey.split("/");
-
-        for (int i = 0, j = targetSegments.length; i < j; i++) {
-            if (!actionKeySegments[i].startsWith(":")) {
-                continue;
-            }
-
-            String name = actionKeySegments[i].substring(1);
-
-            request.setAttribute(name, targetSegments[i]);
-        }
+  private String matchActionKey(String url, String method) {
+    Action action = actionTreeMap.get("@" + method + url);
+    if (action == null) {
+      return null;
     }
+    return action.getActionKey();
+  }
+
+  private void parseUrlPara(String target, String actionKey, HttpServletRequest request) {
+    String[] targetSegments = target.split("/");
+    String[] actionKeySegments = actionKey.split("/");
+    for (int i = 0, j = targetSegments.length; i < j; i++) {
+      if (!actionKeySegments[i].startsWith(":")) {
+        continue;
+      }
+      String name = actionKeySegments[i].substring(1);
+      request.setAttribute(name, targetSegments[i]);
+    }
+  }
 }
