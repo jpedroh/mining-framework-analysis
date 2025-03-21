34840b38179860c78d68fc88f83398e1e7aed806
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 11:25:47 +0000

    Mining Framework Analysis

--- src/main/java/thymeleafexamples/springmail/config/SpringWebInitializer.java
+++ src/main/java/thymeleafexamples/springmail/config/SpringWebInitializer.java
@@ -1,14 +1,12 @@
 package thymeleafexamples.springmail.config;
-
 import java.util.EnumSet;
 import javax.servlet.DispatcherType;
 import javax.servlet.FilterRegistration;
 import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.servlet.DispatcherServlet;
-
 import javax.servlet.ServletContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import javax.servlet.ServletException;
+import org.springframework.web.servlet.DispatcherServlet;
 import javax.servlet.ServletRegistration.Dynamic;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
@@ -17,31 +15,28 @@ import org.springframework.web.filter.CharacterEncodingFilter;
  *  Spring configuration and Spring MVC bootstrapping.
  */
 public class SpringWebInitializer implements WebApplicationInitializer {
+  public static final String ENCODING = "UTF-8";
 
-    public static final String ENCODING = "UTF-8";
+  public void onStartup(ServletContext servletContext) throws ServletException {
+    AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+    context.register(ThymeleafWebConfig.class);
+    context.register(MailConfig.class);
+    context.setServletContext(servletContext);
+    configureSpringMvcFrontController(servletContext, context);
+    configureEncodingFilter(servletContext);
+  }
 
-	@Override
-	public void onStartup(ServletContext servletContext) throws ServletException {
-		AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
-		context.register(ThymeleafWebConfig.class);
-		context.register(MailConfig.class);
-		context.setServletContext(servletContext);
-        configureSpringMvcFrontController(servletContext, context);
-        configureEncodingFilter(servletContext);
-	}
-    
-    private void configureSpringMvcFrontController(ServletContext servletContext, WebApplicationContext context) {
-		Dynamic servlet = servletContext.addServlet("dispatcher", new DispatcherServlet(context));
-		servlet.addMapping("/");
-		servlet.setLoadOnStartup(1);
-    }
+  private void configureSpringMvcFrontController(ServletContext servletContext, WebApplicationContext context) {
+    Dynamic servlet = servletContext.addServlet("dispatcher", new DispatcherServlet(context));
+    servlet.addMapping("/");
+    servlet.setLoadOnStartup(1);
+  }
 
-    private void configureEncodingFilter(ServletContext servletContext) {
-        CharacterEncodingFilter encodingFilter = new CharacterEncodingFilter();
-        encodingFilter.setEncoding(ENCODING);
-        encodingFilter.setForceEncoding(true);
-        FilterRegistration.Dynamic filterRegistration = servletContext.addFilter("encodingFilter", encodingFilter);
-        filterRegistration.addMappingForUrlPatterns(EnumSet.of(DispatcherType.REQUEST), true, "/*");
-   }
-
+  private void configureEncodingFilter(ServletContext servletContext) {
+    CharacterEncodingFilter encodingFilter = new CharacterEncodingFilter();
+    encodingFilter.setEncoding(ENCODING);
+    encodingFilter.setForceEncoding(true);
+    FilterRegistration.Dynamic filterRegistration = servletContext.addFilter("encodingFilter", encodingFilter);
+    filterRegistration.addMappingForUrlPatterns(EnumSet.of(DispatcherType.REQUEST), true, "/*");
+  }
 }
