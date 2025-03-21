b9dfec919ee7d4dc043ae03dc35c464043263c51
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 06:23:16 +0000

    Mining Framework Analysis

--- src/main/java/com/ning/billing/recurly/RecurlyClient.java
+++ src/main/java/com/ning/billing/recurly/RecurlyClient.java
@@ -1,32 +1,12 @@
-/*
- * Copyright 2010-2012 Ning, Inc.
- *
- * Ning licenses this file to you under the Apache License, version 2.0
- * (the "License"); you may not use this file except in compliance with the
- * License.  You may obtain a copy of the License at:
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
- * License for the specific language governing permissions and limitations
- * under the License.
- */
-
 package com.ning.billing.recurly;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.concurrent.ExecutionException;
 import java.util.List;
-
 import javax.annotation.Nullable;
 import javax.xml.bind.DatatypeConverter;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import com.ning.billing.recurly.model.Accounts;
 import com.ning.billing.recurly.model.Plan;
 import com.ning.billing.recurly.model.Plans;
@@ -37,7 +17,6 @@ import com.ning.http.client.AsyncCompletionHandler;
 import com.ning.http.client.AsyncHttpClient;
 import com.ning.http.client.AsyncHttpClientConfig;
 import com.ning.http.client.Response;
-
 import com.fasterxml.jackson.databind.AnnotationIntrospector;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.introspect.JacksonAnnotationIntrospector;
@@ -46,48 +25,48 @@ import com.fasterxml.jackson.datatype.joda.JodaModule;
 import com.fasterxml.jackson.module.jaxb.JaxbAnnotationIntrospector;
 
 public class RecurlyClient {
+  private static final Logger log = LoggerFactory.getLogger(RecurlyClient.class);
 
-    private static final Logger log = LoggerFactory.getLogger(RecurlyClient.class);
+  private final XmlMapper xmlMapper = new XmlMapper();
 
-    private final XmlMapper xmlMapper = new XmlMapper();
+  private final String key;
 
-    private final String key;
-    private final String baseUrl;
-    private AsyncHttpClient client;
+  private final String baseUrl;
 
-    public RecurlyClient(final String apiKey) {
-        this(apiKey, "api.recurly.com", 443, "v2");
-    }
+  private AsyncHttpClient client;
 
-    public RecurlyClient(final String apiKey, final String host, final int port, final String version) {
-        this.key = DatatypeConverter.printBase64Binary(apiKey.getBytes());
-        this.baseUrl = String.format("https://%s:%d/%s", host, port, version);
+  public RecurlyClient(final String apiKey) {
+    this(apiKey, "api.recurly.com", 443, "v2");
+  }
 
-        final AnnotationIntrospector primary = new JacksonAnnotationIntrospector();
-        final AnnotationIntrospector secondary = new JaxbAnnotationIntrospector();
-        final AnnotationIntrospector pair = new AnnotationIntrospector.Pair(primary, secondary);
-        xmlMapper.setAnnotationIntrospector(pair);
-        xmlMapper.registerModule(new JodaModule());
-        xmlMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-    }
+  public RecurlyClient(final String apiKey, final String host, final int port, final String version) {
+    this.key = DatatypeConverter.printBase64Binary(apiKey.getBytes());
+    this.baseUrl = String.format("https://%s:%d/%s", host, port, version);
+    final AnnotationIntrospector primary = new JacksonAnnotationIntrospector();
+    final AnnotationIntrospector secondary = new JaxbAnnotationIntrospector();
+    final AnnotationIntrospector pair = new AnnotationIntrospector.Pair(primary, secondary);
+    xmlMapper.setAnnotationIntrospector(pair);
+    xmlMapper.registerModule(new JodaModule());
+    xmlMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+  }
 
-    /**
+  /**
      * Open the underlying http client
      */
-    public synchronized void open() {
-        client = createHttpClient();
-    }
+  public synchronized void open() {
+    client = createHttpClient();
+  }
 
-    /**
+  /**
      * Close the underlying http client
      */
-    public synchronized void close() {
-        if (client != null) {
-            client.close();
-        }
+  public synchronized void close() {
+    if (client != null) {
+      client.close();
     }
+  }
 
-    /**
+  /**
      * Create Account
      * <p/>
      * Creates a new account. You may optionally include billing information.
@@ -95,22 +74,22 @@ public class RecurlyClient {
      * @param account account object
      * @return the newly created account object on success, null otherwise
      */
-    public Account createAccount(final Account account) {
-        return doPOST(Account.ACCOUNT_RESOURCE, account, Account.class);
-    }
+  public Account createAccount(final Account account) {
+    return doPOST(Account.ACCOUNT_RESOURCE, account, Account.class);
+  }
 
-    /**
+  /**
      * Get Accounts
      * <p/>
      * Returns information about all accounts.
      *
      * @return account object on success, null otherwise
      */
-    public Accounts getAccounts() {
-        return doGET(Accounts.ACCOUNTS_RESOURCE, Accounts.class);
-    }
+  public Accounts getAccounts() {
+    return doGET(Accounts.ACCOUNTS_RESOURCE, Accounts.class);
+  }
 
-    /**
+  /**
      * Get Account
      * <p/>
      * Returns information about a single account.
@@ -118,11 +97,11 @@ public class RecurlyClient {
      * @param accountCode recurly account id
      * @return account object on success, null otherwise
      */
-    public Account getAccount(final String accountCode) {
-        return doGET(Account.ACCOUNT_RESOURCE + "/" + accountCode, Account.class);
-    }
+  public Account getAccount(final String accountCode) {
+    return doGET(Account.ACCOUNT_RESOURCE + "/" + accountCode, Account.class);
+  }
 
-    /**
+  /**
      * Update Account
      * <p/>
      * Updates an existing account.
@@ -131,11 +110,11 @@ public class RecurlyClient {
      * @param account     account object
      * @return the updated account object on success, null otherwise
      */
-    public Account updateAccount(final String accountCode, final Account account) {
-        return doPUT(Account.ACCOUNT_RESOURCE + "/" + accountCode, account, Account.class);
-    }
+  public Account updateAccount(final String accountCode, final Account account) {
+    return doPUT(Account.ACCOUNT_RESOURCE + "/" + accountCode, account, Account.class);
+  }
 
-    /**
+  /**
      * Close Account
      * <p/>
      * Marks an account as closed and cancels any active subscriptions. Any saved billing information will also be
@@ -143,11 +122,11 @@ public class RecurlyClient {
      *
      * @param accountCode recurly account id
      */
-    public void closeAccount(final String accountCode) {
-        doDELETE(Account.ACCOUNT_RESOURCE + "/" + accountCode);
-    }
+  public void closeAccount(final String accountCode) {
+    doDELETE(Account.ACCOUNT_RESOURCE + "/" + accountCode);
+  }
 
-    /**
+  /**
      * Update an account's billing info
      * <p/>
      * When new or updated credit card information is updated, the billing information is only saved if the credit card
@@ -162,14 +141,13 @@ public class RecurlyClient {
      * @param billingInfo billing info object to create or update
      * @return the newly created or update billing info object on success, null otherwise 
     */
-    public BillingInfo createOrUpdateBillingInfo(final BillingInfo billingInfo) {
-        final String accountCode = billingInfo.getAccount().getAccountCode();
-        // Unset it to avoid confusing Recurly
-        billingInfo.setAccount(null);
-        return doPUT(Account.ACCOUNT_RESOURCE + "/" + accountCode + BillingInfo.BILLING_INFO_RESOURCE, billingInfo, BillingInfo.class);
-    }
+  public BillingInfo createOrUpdateBillingInfo(final BillingInfo billingInfo) {
+    final String accountCode = billingInfo.getAccount().getAccountCode();
+    billingInfo.setAccount(null);
+    return doPUT(Account.ACCOUNT_RESOURCE + "/" + accountCode + BillingInfo.BILLING_INFO_RESOURCE, billingInfo, BillingInfo.class);
+  }
 
-    /**
+  /**
      * Lookup an account's billing info
      * <p/>
      * Returns only the account's current billing information.
@@ -177,11 +155,11 @@ public class RecurlyClient {
      * @param accountCode recurly account id
      * @return the current billing info object associated with this account on success, null otherwise
      */
-    public BillingInfo getBillingInfo(final String accountCode) {
-        return doGET(Account.ACCOUNT_RESOURCE + "/" + accountCode + BillingInfo.BILLING_INFO_RESOURCE, BillingInfo.class);
-    }
+  public BillingInfo getBillingInfo(final String accountCode) {
+    return doGET(Account.ACCOUNT_RESOURCE + "/" + accountCode + BillingInfo.BILLING_INFO_RESOURCE, BillingInfo.class);
+  }
 
-    /**
+  /**
      * Clear an account's billing info
      * <p/>
      * You may remove any stored billing information for an account. If the account has a subscription, the renewal will
@@ -189,148 +167,132 @@ public class RecurlyClient {
      *
      * @param accountCode recurly account id
      */
-    public void clearBillingInfo(final String accountCode) {
-        doDELETE(Account.ACCOUNT_RESOURCE + "/" + accountCode + BillingInfo.BILLING_INFO_RESOURCE);
-    }
+  public void clearBillingInfo(final String accountCode) {
+    doDELETE(Account.ACCOUNT_RESOURCE + "/" + accountCode + BillingInfo.BILLING_INFO_RESOURCE);
+  }
 
-    ///////////////////////////////////////////////////////////////////////////
-
-    /**
+  /**
      * Create a Plan's info
      * <p/>
      * 
      * @param plan The plan to create on recurly
      * @return the plan object as identified by the passed in ID
      */
-    public Plan createPlan(final Plan plan) {
-        return doPOST(Plan.PLANS_RESOURCE, plan, Plan.class);
-    }
+  public Plan createPlan(final Plan plan) {
+    return doPOST(Plan.PLANS_RESOURCE, plan, Plan.class);
+  }
 
-    /**
+  /**
      * Get a Plan's details
      * <p/>
      * 
      * @param planCode recurly id of plan
      * @return the plan object as identified by the passed in ID
      */
-    public Plan getPlan(final String planCode) {
-        return doGET(Plan.PLANS_RESOURCE + "/" + planCode, Plan.class);
-    }
+  public Plan getPlan(final String planCode) {
+    return doGET(Plan.PLANS_RESOURCE + "/" + planCode, Plan.class);
+  }
 
-    /**
+  /**
      * Return all the plans
      * <p/>
      * 
      * @return the plan object as identified by the passed in ID
      */
-    public Plans getPlans() {
-        return doGET(Plans.PLANS_RESOURCE, Plans.class);
+  public Plans getPlans() {
+    return doGET(Plans.PLANS_RESOURCE, Plans.class);
+  }
+
+  private <T extends java.lang.Object> T doGET(final String resource, final Class<T> clazz) {
+    return callRecurlySafe(client.prepareGet(baseUrl + resource), clazz);
+  }
+
+  private <T extends java.lang.Object> T doPOST(final String resource, final RecurlyObject payload, final Class<T> clazz) {
+    final String xmlPayload;
+    try {
+      xmlPayload = xmlMapper.writeValueAsString(payload);
+    } catch (IOException e) {
+      log.warn("Unable to serialize {} object as XML: {}", clazz.getName(), payload.toString());
+      return null;
     }
+    return callRecurlySafe(client.preparePost(baseUrl + resource).setBody(xmlPayload), clazz);
+  }
 
-
-    ///////////////////////////////////////////////////////////////////////////
-
-
-    private <T> T doGET(final String resource, final Class<T> clazz) {
-        return callRecurlySafe(client.prepareGet(baseUrl + resource), clazz);
+  private <T extends java.lang.Object> T doPUT(final String resource, final RecurlyObject payload, final Class<T> clazz) {
+    final String xmlPayload;
+    try {
+      xmlPayload = xmlMapper.writeValueAsString(payload);
+    } catch (IOException e) {
+      log.warn("Unable to serialize {} object as XML: {}", clazz.getName(), payload.toString());
+      return null;
     }
+    return callRecurlySafe(client.preparePut(baseUrl + resource).setBody(xmlPayload), clazz);
+  }
 
-    private <T> T doPOST(final String resource, final RecurlyObject payload, final Class<T> clazz) {
-        final String xmlPayload;
+  private void doDELETE(final String resource) {
+    callRecurlySafe(client.prepareDelete(baseUrl + resource), null);
+  }
+
+  private <T extends java.lang.Object> T callRecurlySafe(final AsyncHttpClient.BoundRequestBuilder builder, @Nullable final Class<T> clazz) {
+    try {
+      return callRecurly(builder, clazz);
+    } catch (IOException e) {
+      log.warn("Error while calling Recurly", e);
+      return null;
+    } catch (ExecutionException e) {
+      log.error("Execution error", e);
+      return null;
+    } catch (InterruptedException e) {
+      log.error("Interrupted while calling Recurly", e);
+      return null;
+    }
+  }
+
+  private <T extends java.lang.Object> T callRecurly(final AsyncHttpClient.BoundRequestBuilder builder, @Nullable final Class<T> clazz) throws IOException, ExecutionException, InterruptedException {
+    return builder.addHeader("Authorization", "Basic " + key).addHeader("Accept", "application/xml").addHeader("Content-Type", "application/xml; charset=utf-8").execute(new AsyncCompletionHandler<T>() {
+      @Override public T onCompleted(final Response response) throws Exception {
+        if (response.getStatusCode() >= 300) {
+          log.warn("Recurly error: {}", response.getResponseBody());
+          return null;
+        }
+        if (clazz == null) {
+          return null;
+        }
+        final InputStream in = response.getResponseBodyAsStream();
         try {
-            xmlPayload = xmlMapper.writeValueAsString(payload);
-        } catch (IOException e) {
-            log.warn("Unable to serialize {} object as XML: {}", clazz.getName(), payload.toString());
-            return null;
+          String payload = convertStreamToString(in);
+          System.out.println("**** MESSAGE ****");
+          System.out.println(payload);
+          T obj = xmlMapper.readValue(payload, clazz);
+          return obj;
+        }  finally {
+          closeStream(in);
         }
+      }
+    }).get();
+  }
 
-        return callRecurlySafe(client.preparePost(baseUrl + resource).setBody(xmlPayload), clazz);
+  private String convertStreamToString(java.io.InputStream is) {
+    try {
+      return new java.util.Scanner(is).useDelimiter("\\A").next();
+    } catch (java.util.NoSuchElementException e) {
+      return "";
     }
+  }
 
-    private <T> T doPUT(final String resource, final RecurlyObject payload, final Class<T> clazz) {
-        final String xmlPayload;
-        try {
-            xmlPayload = xmlMapper.writeValueAsString(payload);
-        } catch (IOException e) {
-            log.warn("Unable to serialize {} object as XML: {}", clazz.getName(), payload.toString());
-            return null;
-        }
-
-        return callRecurlySafe(client.preparePut(baseUrl + resource).setBody(xmlPayload), clazz);
+  private void closeStream(final InputStream in) {
+    if (in != null) {
+      try {
+        in.close();
+      } catch (IOException e) {
+        log.warn("Failed to close http-client - provided InputStream: {}", e.getLocalizedMessage());
+      }
     }
+  }
 
-    private void doDELETE(final String resource) {
-        callRecurlySafe(client.prepareDelete(baseUrl + resource), null);
-    }
-
-    private <T> T callRecurlySafe(final AsyncHttpClient.BoundRequestBuilder builder, @Nullable final Class<T> clazz) {
-        try {
-            return callRecurly(builder, clazz);
-        } catch (IOException e) {
-            log.warn("Error while calling Recurly", e);
-            return null;
-        } catch (ExecutionException e) {
-            log.error("Execution error", e);
-            return null;
-        } catch (InterruptedException e) {
-            log.error("Interrupted while calling Recurly", e);
-            return null;
-        }
-    }
-
-    private <T> T callRecurly(final AsyncHttpClient.BoundRequestBuilder builder, @Nullable final Class<T> clazz) throws IOException, ExecutionException, InterruptedException {
-        return builder.addHeader("Authorization", "Basic " + key)
-                      .addHeader("Accept", "application/xml")
-                      .addHeader("Content-Type", "application/xml; charset=utf-8")
-                      .execute(new AsyncCompletionHandler<T>() {
-                          @Override
-                          public T onCompleted(final Response response) throws Exception {
-                              if (response.getStatusCode() >= 300) {
-                                  log.warn("Recurly error: {}", response.getResponseBody());
-                                  return null;
-                              }
-
-                              if (clazz == null) {
-                                  return null;
-                              }
-
-                              final InputStream in = response.getResponseBodyAsStream();
-                              try {
-                                  String payload = convertStreamToString(in);
-                                  System.out.println("**** MESSAGE ****");
-                                  System.out.println(payload);
-                                  T obj = xmlMapper.readValue(payload, clazz);
-                                  return obj;
-                              } finally {
-                                  closeStream(in);
-                              }
-                          }
-                      }).get();
-    }
-
-    private String convertStreamToString(java.io.InputStream is) {
-        try {
-            return new java.util.Scanner(is).useDelimiter("\\A").next();
-        } catch (java.util.NoSuchElementException e) {
-            return "";
-        }
-    }
-
-    private void closeStream(final InputStream in) {
-        if (in != null) {
-            try {
-                in.close();
-            } catch (IOException e) {
-                log.warn("Failed to close http-client - provided InputStream: {}", e.getLocalizedMessage());
-            }
-        }
-    }
-
-    private AsyncHttpClient createHttpClient() {
-        // Don't limit the number of connections per host
-        // See https://github.com/ning/async-http-client/issues/issue/28
-        final AsyncHttpClientConfig.Builder builder = new AsyncHttpClientConfig.Builder();
-        builder.setMaximumConnectionsPerHost(-1);
-        return new AsyncHttpClient(builder.build());
-    }
+  private AsyncHttpClient createHttpClient() {
+    final AsyncHttpClientConfig.Builder builder = new AsyncHttpClientConfig.Builder();
+    builder.setMaximumConnectionsPerHost(-1);
+    return new AsyncHttpClient(builder.build());
+  }
 }
--- src/main/java/com/ning/billing/recurly/model/Plans.java
+++ src/main/java/com/ning/billing/recurly/model/Plans.java
@@ -1,27 +1,15 @@
-/*
- * Copyright 2010-2012 Ning, Inc.
- *
- * Ning licenses this file to you under the Apache License, version 2.0
- * (the "License"); you may not use this file except in compliance with the
- * License.  You may obtain a copy of the License at:
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
- * License for the specific language governing permissions and limitations
- * under the License.
- */
-
 package com.ning.billing.recurly.model;
-
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
 
-@XmlRootElement(name = "plans")
-public class Plans extends RecurlyObjects<Plan> {
+@XmlRootElement(name = "plans") @XmlAccessorType(value = XmlAccessType.FIELD) public class Plans extends RecurlyObjects<Plan> {
+  @XmlTransient public static final String PLANS_RESOURCE = "/plans";
 
-    @XmlTransient
-    public static final String PLANS_RESOURCE = "/plans";
+
+<<<<<<< /usr/src/app/output/killbilling/recurly-java-library/08f428502f99854cbe37544d682bdc674b5ebc95/src/main/java/com/ning/billing/recurly/model/Plans.java/left.java
+  @XmlElement(name = "plan") private List<Plan> plans;
+=======
+>>>>>>> Unknown file: This is a bug in JDime.
 }
