58ae8414520cf932e172280e7d555b2d4aa8091e
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 16:15:00 +0000

    Mining Framework Analysis

--- ninja-core/src/main/java/ninja/Bootstrap.java
+++ ninja-core/src/main/java/ninja/Bootstrap.java
@@ -1,44 +1,26 @@
-/**
- * Copyright (C) 2012-2016 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package ninja;
-
 import java.util.ArrayList;
 import java.util.List;
-
 import org.slf4j.Logger;
+import ninja.application.ApplicationRoutes;
 import org.slf4j.LoggerFactory;
-
+import ninja.lifecycle.LifecycleSupport;
 import com.google.common.base.Optional;
+import ninja.logging.LogbackConfigurator;
 import com.google.common.base.Preconditions;
+import ninja.scheduler.SchedulerSupport;
 import com.google.inject.AbstractModule;
+import ninja.utils.NinjaConstant;
 import com.google.inject.Guice;
+import ninja.utils.NinjaProperties;
 import com.google.inject.Injector;
+import ninja.utils.NinjaPropertiesImpl;
 import com.google.inject.Module;
 import com.google.inject.Singleton;
 import com.google.inject.Stage;
 import com.google.inject.multibindings.Multibinder;
-
-import ninja.application.ApplicationRoutes;
-import ninja.logging.LogbackConfigurator;
-import ninja.params.ParamParser;
-import ninja.utils.NinjaConstant;
-import ninja.utils.NinjaProperties;
-import ninja.utils.NinjaPropertiesImpl;
 import ninja.conf.FrameworkModule;
+import ninja.params.ParamParser;
 import ninja.conf.NinjaBaseModule;
 import ninja.conf.NinjaClassicModule;
 
@@ -52,230 +34,158 @@ import ninja.conf.NinjaClassicModule;
  * and NinjaServletContext for an example of a subclass.
  */
 public class Bootstrap {
-    static private final Logger logger = LoggerFactory.getLogger(Bootstrap.class);
+  static private final Logger logger = LoggerFactory.getLogger(Bootstrap.class);
 
-    static public final String APPLICATION_GUICE_MODULE_CONVENTION_LOCATION = "conf.Module";
-    static public final String APPLICATION_GUICE_SERVLET_MODULE_CONVENTION_LOCATION = "conf.ServletModule";
-    static public final String ROUTES_CONVENTION_LOCATION = "conf.Routes";
-    static public final String NINJA_CONVENTION_LOCATION = "conf.Ninja";
+  static public final String APPLICATION_GUICE_MODULE_CONVENTION_LOCATION = "conf.Module";
 
-    private final NinjaPropertiesImpl ninjaProperties;
-    private final List<Module> modulesToLoad;
-    private final Optional<String> applicationModulesBasePackage;
-    private Injector injector = null;
+  static public final String APPLICATION_GUICE_SERVLET_MODULE_CONVENTION_LOCATION = "conf.ServletModule";
 
-    public Bootstrap(NinjaPropertiesImpl ninjaProperties) {
-        Preconditions.checkNotNull(ninjaProperties);
-        this.ninjaProperties = ninjaProperties;
-        this.modulesToLoad =  new ArrayList<>();
-        
-        // custom base package for application modules (e.g. com.example.conf.Routes)
-        this.applicationModulesBasePackage
-            = Optional.fromNullable(ninjaProperties.get(
-                    NinjaConstant.APPLICATION_MODULES_BASE_PACKAGE));
-        
+  static public final String ROUTES_CONVENTION_LOCATION = "conf.Routes";
+
+  static public final String NINJA_CONVENTION_LOCATION = "conf.Ninja";
+
+  private final NinjaPropertiesImpl ninjaProperties;
+
+  private final List<Module> modulesToLoad;
+
+  private final Optional<String> applicationModulesBasePackage;
+
+  private Injector injector = null;
+
+  public Bootstrap(NinjaPropertiesImpl ninjaProperties) {
+    Preconditions.checkNotNull(ninjaProperties);
+    this.ninjaProperties = ninjaProperties;
+    this.modulesToLoad = new ArrayList<>();
+    this.applicationModulesBasePackage = Optional.fromNullable(ninjaProperties.get(NinjaConstant.APPLICATION_MODULES_BASE_PACKAGE));
+  }
+
+  public synchronized void boot() {
+    initLogback();
+    if (this.injector != null) {
+      throw new RuntimeException("Bootstrap already booted");
     }
-
-    public synchronized void boot() {
-        // 1. initialize logging
-        initLogback();
-
-        if (this.injector != null) {
-            throw new RuntimeException("Bootstrap already booted");
-        }
-
-        // 2. configure all modules for injector
-        try {
-            configure();
-        } catch (Exception e) {
-            throw new RuntimeException("Unable to configure Ninja", e);
-        }
-        
-        // 3. create injector
-        long startTime = System.currentTimeMillis();
-
-        try {
-            initInjector();
-        } catch (Exception e) {
-            throw new RuntimeException("Ninja injector cannot be generated. Please check log for further errors.", e);
-        }
-        
-        long injectorStartupTime = System.currentTimeMillis() - startTime;
-        logger.info("Ninja injector started in " + injectorStartupTime + " ms.");
-        
-        // 4. initialize routes
-        try {
-            initRoutes();
-        } catch (Exception e) {
-            throw new RuntimeException("Unable to initialize Ninja routes", e);
-        }
-        
-        // 5. framework started!
-        Ninja ninja = injector.getInstance(Ninja.class);
-        ninja.onFrameworkStart();
+    try {
+      configure();
+    } catch (Exception e) {
+      throw new RuntimeException("Unable to configure Ninja", e);
     }
-
-    public synchronized void shutdown() {
-        if (this.injector != null) {
-            Ninja ninja = injector.getInstance(Ninja.class);
-            ninja.onFrameworkShutdown();
-            injector = null;
-        } else {
-            logger.error("Shutdown of Ninja not clean => injector already null.");
-        }
+    long startTime = System.currentTimeMillis();
+    try {
+      initInjector();
+    } catch (Exception e) {
+      throw new RuntimeException("Ninja injector cannot be generated. Please check log for further errors.", e);
     }
-    
-    public Injector getInjector() {
-        return this.injector;
+    long injectorStartupTime = System.currentTimeMillis() - startTime;
+    logger.info("Ninja injector started in " + injectorStartupTime + " ms.");
+    try {
+      initRoutes();
+    } catch (Exception e) {
+      throw new RuntimeException("Unable to initialize Ninja routes", e);
     }
-    
-    public void addModule(Module module) {
-        this.modulesToLoad.add(module);
+    Ninja ninja = injector.getInstance(Ninja.class);
+    ninja.onFrameworkStart();
+  }
+
+  public synchronized void shutdown() {
+    if (this.injector != null) {
+      Ninja ninja = injector.getInstance(Ninja.class);
+      ninja.onFrameworkShutdown();
+      injector = null;
+    } else {
+      logger.error("Shutdown of Ninja not clean => injector already null.");
     }
-    
-    protected void configure() throws Exception {
+  }
 
-        // Base configuration of Ninja
-        addModule(new NinjaBaseModule(ninjaProperties));
-        
-        // Main application module (conf.Module or com.example.conf.Module)
-        String applicationModuleClassName
-                = resolveApplicationClassName(APPLICATION_GUICE_MODULE_CONVENTION_LOCATION);
+  public Injector getInjector() {
+    return this.injector;
+  }
 
-        AbstractModule applicationModule = null;
-        
-        if (doesClassExist(applicationModuleClassName)) {
-            Class<?> applicationModuleClass = Class
-                    .forName(applicationModuleClassName);
+  public void addModule(Module module) {
+    this.modulesToLoad.add(module);
+  }
 
-            // Tries to instantiate module by giving the NinjaProperties as constructor arg
-            try {
-                applicationModule = (AbstractModule) applicationModuleClass
-                        .getConstructor(NinjaProperties.class).newInstance(ninjaProperties);
-            } catch (NoSuchMethodException e) {
-                applicationModule = (AbstractModule) applicationModuleClass
-                        .getConstructor().newInstance();
-            }
-        }
-        
-        // Slipstream in the "classic" ninja configuration?
-        if (applicationModule == null || !(applicationModule instanceof FrameworkModule)) {
-            // Classic configuration of Ninja
-            logger.info("Enabling Ninja classic configuration");
-            addModule(new NinjaClassicModule(ninjaProperties));
-        }
-        
-        if (applicationModule != null) {
-            addModule(applicationModule);
-        }
-        
-        // Ninja module
-        String applicationNinjaClassName
-                = resolveApplicationClassName(NINJA_CONVENTION_LOCATION);
-
-        final Class<? extends Ninja> ninjaClass;
-
-        if (doesClassExist(applicationNinjaClassName)) {
-
-            final Class<?> clazzPotentially = Class.forName(applicationNinjaClassName);
-
-            if (Ninja.class.isAssignableFrom(clazzPotentially)) {
-
-                ninjaClass = (Class<? extends Ninja>) clazzPotentially;
-
-            } else {
-
-                final String ERROR_MESSAGE = String.format(
-                        "Found a class %s in your application's conf directory."
-                        + " This class does not implement Ninja interface %s. "
-                        + " Please implement the interface or remove the class.",
-                        applicationNinjaClassName,
-                        Ninja.class.getName());
-
-                logger.error(ERROR_MESSAGE);
-
-                throw new IllegalStateException(ERROR_MESSAGE);
-
-            }
-
-        } else {
-
-           ninjaClass = NinjaDefault.class;
-
-        }
-
-        addModule(new AbstractModule() {
-            @Override
-            protected void configure() {
-                bind(Ninja.class).to(ninjaClass).in(Singleton.class);
-                Multibinder.newSetBinder(binder(), ParamParser.class);
-            }
-        });
+  protected void configure() throws Exception {
+    addModule(new NinjaBaseModule(ninjaProperties));
+    String applicationModuleClassName = resolveApplicationClassName(APPLICATION_GUICE_MODULE_CONVENTION_LOCATION);
+    AbstractModule applicationModule = null;
+    if (doesClassExist(applicationModuleClassName)) {
+      Class<?> applicationModuleClass = Class.forName(applicationModuleClassName);
+      try {
+        applicationModule = (AbstractModule) applicationModuleClass.getConstructor(NinjaProperties.class).newInstance(ninjaProperties);
+      } catch (NoSuchMethodException e) {
+        applicationModule = (AbstractModule) applicationModuleClass.getConstructor().newInstance();
+      }
     }
-    
-    private void initInjector() throws Exception {
-        // Let the injector generate all instances and stuff
-        this.injector = Guice.createInjector(Stage.PRODUCTION, modulesToLoad);
+    if (applicationModule == null || !(applicationModule instanceof FrameworkModule)) {
+      logger.info("Enabling Ninja classic configuration");
+      addModule(new NinjaClassicModule(ninjaProperties));
     }
-    
-    public void initRoutes() throws Exception {
-        String applicationRoutesClassName
-                = resolveApplicationClassName(ROUTES_CONVENTION_LOCATION);
-
-        if (doesClassExist(applicationRoutesClassName)) {
-
-            Class<?> clazz = Class.forName(applicationRoutesClassName);
-            
-            ApplicationRoutes applicationRoutes = (ApplicationRoutes) injector
-                    .getInstance(clazz);
-
-            Router router = this.injector.getInstance(Router.class);
-
-            applicationRoutes.init(router);
-            
-            router.compileRoutes();
-        }
+    if (applicationModule != null) {
+      addModule(applicationModule);
     }
-    
-    private void initLogback() {
-        // init logging at the very very top
-        try {
-            Class.forName("ch.qos.logback.classic.joran.JoranConfigurator");
-            LogbackConfigurator.initConfiguration(ninjaProperties);
-            logger.info("Successfully configured Logback.");
-             // It is available
-        } catch (ClassNotFoundException exception) {
-            logger.info(
-                    "Logback is not on classpath (you are probably using slf4j-jdk14). I did not configure anything. It's up to you now...", exception);
-        }
+    String applicationNinjaClassName = resolveApplicationClassName(NINJA_CONVENTION_LOCATION);
+    final Class<? extends Ninja> ninjaClass;
+    if (doesClassExist(applicationNinjaClassName)) {
+      final Class<?> clazzPotentially = Class.forName(applicationNinjaClassName);
+      if (Ninja.class.isAssignableFrom(clazzPotentially)) {
+        ninjaClass = (Class<? extends Ninja>) clazzPotentially;
+      } else {
+        final String ERROR_MESSAGE = String.format("Found a class %s in your application\'s conf directory." + " This class does not implement Ninja interface %s. " + " Please implement the interface or remove the class.", applicationNinjaClassName, Ninja.class.getName());
+        logger.error(ERROR_MESSAGE);
+        throw new IllegalStateException(ERROR_MESSAGE);
+      }
+    } else {
+      ninjaClass = NinjaDefault.class;
     }
-    
-    protected boolean doesClassExist(String nameWithPackage) {
+    addModule(new AbstractModule() {
+      @Override protected void configure() {
+        bind(Ninja.class).to(ninjaClass).in(Singleton.class);
+        Multibinder.newSetBinder(binder(), ParamParser.class);
+      }
+    });
+  }
 
-        boolean exists = false;
+  private void initInjector() throws Exception {
+    this.injector = Guice.createInjector(Stage.PRODUCTION, modulesToLoad);
+  }
 
-        try {
-            Class.forName(nameWithPackage, false, this.getClass()
-                    .getClassLoader());
-            exists = true;
-        } catch (ClassNotFoundException e) {
-            exists = false;
-        }
-
-        return exists;
-
+  public void initRoutes() throws Exception {
+    String applicationRoutesClassName = resolveApplicationClassName(ROUTES_CONVENTION_LOCATION);
+    if (doesClassExist(applicationRoutesClassName)) {
+      Class<?> clazz = Class.forName(applicationRoutesClassName);
+      ApplicationRoutes applicationRoutes = (ApplicationRoutes) injector.getInstance(clazz);
+      Router router = this.injector.getInstance(Router.class);
+      applicationRoutes.init(router);
+      router.compileRoutes();
     }
+  }
 
-    protected String resolveApplicationClassName(String classLocationAsDefinedByNinja) {
-        if (applicationModulesBasePackage.isPresent()) {
-            return new StringBuilder()
-                .append(applicationModulesBasePackage.get())
-                .append('.')
-                .append(classLocationAsDefinedByNinja)
-                .toString();
-        } else {
-            return classLocationAsDefinedByNinja;
-        }
+  private void initLogback() {
+    try {
+      Class.forName("ch.qos.logback.classic.joran.JoranConfigurator");
+      LogbackConfigurator.initConfiguration(ninjaProperties);
+      logger.info("Successfully configured Logback.");
+    } catch (ClassNotFoundException exception) {
+      logger.info("Logback is not on classpath (you are probably using slf4j-jdk14). I did not configure anything. It\'s up to you now...", exception);
     }
+  }
 
+  protected boolean doesClassExist(String nameWithPackage) {
+    boolean exists = false;
+    try {
+      Class.forName(nameWithPackage, false, this.getClass().getClassLoader());
+      exists = true;
+    } catch (ClassNotFoundException e) {
+      exists = false;
+    }
+    return exists;
+  }
+
+  protected String resolveApplicationClassName(String classLocationAsDefinedByNinja) {
+    if (applicationModulesBasePackage.isPresent()) {
+      return new StringBuilder().append(applicationModulesBasePackage.get()).append('.').append(classLocationAsDefinedByNinja).toString();
+    } else {
+      return classLocationAsDefinedByNinja;
+    }
+  }
 }
