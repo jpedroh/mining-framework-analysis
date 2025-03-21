6f602df1130e134fb1df0fc94245bb738d263249
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 07:33:46 +0000

    Mining Framework Analysis

--- src/test/java/com/github/javafaker/integration/FakerIT.java
+++ src/test/java/com/github/javafaker/integration/FakerIT.java
@@ -1,12 +1,10 @@
 package com.github.javafaker.integration;
-
 import com.github.javafaker.Faker;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
@@ -15,7 +13,6 @@ import java.util.Collection;
 import java.util.Locale;
 import java.util.Random;
 import java.util.Set;
-
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.isEmptyString;
@@ -31,95 +28,77 @@ import static org.reflections.ReflectionUtils.withReturnType;
  * and that methods return values. The unit tests should ensure what the values returned
  * are correct. These tests just ensure that the methods can be invoked.
  */
-@RunWith(value = Parameterized.class)
-public class FakerIT {
+@RunWith(value = Parameterized.class) public class FakerIT {
+  private static final Locale FINNISH_LOCALE = new Locale("fi", "FI");
 
-    private static final Locale FINNISH_LOCALE = new Locale("fi", "FI");
-    private static final Logger logger = LoggerFactory.getLogger(FakerIT.class);
-    private Faker faker;
+  private static final Logger logger = LoggerFactory.getLogger(FakerIT.class);
 
-    public FakerIT(Locale locale, Random random) {
-        if (locale != null && random != null) {
-            faker = new Faker(locale, random);
-        } else if (locale != null) {
-            faker = new Faker(locale);
-        } else if (random != null) {
-            faker = new Faker(random);
+  private Faker faker;
+
+  public FakerIT(Locale locale, Random random) {
+    if (locale != null && random != null) {
+      faker = new Faker(locale, random);
+    } else {
+      if (locale != null) {
+        faker = new Faker(locale);
+      } else {
+        if (random != null) {
+          faker = new Faker(random);
         } else {
-            faker = new Faker();
+          faker = new Faker();
         }
+      }
     }
+  }
 
-    @Parameterized.Parameters(name = "testing locale {0} and random {1}")
-    public static Collection<Object[]> data() {
-        Object[][] data = new Object[][]{
-                {Locale.ENGLISH, null},
-                {Locale.FRENCH, null},
-                {Locale.CANADA_FRENCH, null},
-                {Locale.TRADITIONAL_CHINESE, null},
-                {new Locale("pt"), null},
-                {FINNISH_LOCALE, null},
-                {Locale.ENGLISH, new Random()},
-                {null, new Random()},
-                {null, null}};
-        return Arrays.asList(data);
+  @Parameterized.Parameters(name = "testing locale {0} and random {1}") public static Collection<Object[]> data() {
+    Object[][] data = new Object[][] { { Locale.ENGLISH, null }, { Locale.FRENCH, null }, { Locale.CANADA_FRENCH, null }, { Locale.TRADITIONAL_CHINESE, null }, { new Locale("pt"), null }, { FINNISH_LOCALE, null }, { Locale.ENGLISH, new Random() }, { null, new Random() }, { null, null } };
+    return Arrays.asList(data);
+  }
+
+  @Test public void testAllFakerMethodsThatReturnStrings() throws Exception {
+    testAllMethodsThatReturnStringsActuallyReturnStrings(faker);
+    testAllMethodsThatReturnStringsActuallyReturnStrings(faker.address());
+    testAllMethodsThatReturnStringsActuallyReturnStrings(faker.app());
+    testAllMethodsThatReturnStringsActuallyReturnStrings(faker.business());
+    testAllMethodsThatReturnStringsActuallyReturnStrings(faker.book());
+    testAllMethodsThatReturnStringsActuallyReturnStrings(faker.chuckNorris());
+    testAllMethodsThatReturnStringsActuallyReturnStrings(faker.color());
+    testAllMethodsThatReturnStringsActuallyReturnStrings(faker.commerce());
+    testAllMethodsThatReturnStringsActuallyReturnStrings(faker.company());
+    testAllMethodsThatReturnStringsActuallyReturnStrings(faker.crypto());
+    testAllMethodsThatReturnStringsActuallyReturnStrings(faker.educator());
+    testAllMethodsThatReturnStringsActuallyReturnStrings(faker.internet());
+    testAllMethodsThatReturnStringsActuallyReturnStrings(faker.lorem());
+    testAllMethodsThatReturnStringsActuallyReturnStrings(faker.phoneNumber());
+    testAllMethodsThatReturnStringsActuallyReturnStrings(faker.name());
+    testAllMethodsThatReturnStringsActuallyReturnStrings(faker.finance());
+    testAllMethodsThatReturnStringsActuallyReturnStrings(faker.shakespeare());
+    testAllMethodsThatReturnStringsActuallyReturnStrings(faker.superhero());
+    testAllMethodsThatReturnStringsActuallyReturnStrings(faker.team());
+    testAllMethodsThatReturnStringsActuallyReturnStrings(faker.beer());
+  }
+
+  private void testAllMethodsThatReturnStringsActuallyReturnStrings(Object object) throws IllegalAccessException, InvocationTargetException {
+    @SuppressWarnings(value = { "unchecked" }) Set<Method> methodsThatReturnStrings = getAllMethods(object.getClass(), withModifier(Modifier.PUBLIC), withReturnType(String.class), withParametersCount(0));
+    for (Method method : methodsThatReturnStrings) {
+      final Object returnValue = method.invoke(object);
+      logger.info(String.format("Invoked %s.%s = %s", object.getClass().getSimpleName().toLowerCase(), method.getName(), returnValue));
+      assertThat(returnValue, is(notNullValue()));
+      assertThat((String) returnValue, not(isEmptyString()));
     }
+  }
 
-    @Test
-    public void testAllFakerMethodsThatReturnStrings() throws Exception {
-        testAllMethodsThatReturnStringsActuallyReturnStrings(faker);
-        testAllMethodsThatReturnStringsActuallyReturnStrings(faker.address());
-        testAllMethodsThatReturnStringsActuallyReturnStrings(faker.app());
-        testAllMethodsThatReturnStringsActuallyReturnStrings(faker.business());
-        testAllMethodsThatReturnStringsActuallyReturnStrings(faker.book());
-        testAllMethodsThatReturnStringsActuallyReturnStrings(faker.chuckNorris());
-        testAllMethodsThatReturnStringsActuallyReturnStrings(faker.color());
-        testAllMethodsThatReturnStringsActuallyReturnStrings(faker.commerce());
-        testAllMethodsThatReturnStringsActuallyReturnStrings(faker.company());
-        testAllMethodsThatReturnStringsActuallyReturnStrings(faker.crypto());
-        testAllMethodsThatReturnStringsActuallyReturnStrings(faker.educator());
-        testAllMethodsThatReturnStringsActuallyReturnStrings(faker.internet());
-        testAllMethodsThatReturnStringsActuallyReturnStrings(faker.lorem());
-        testAllMethodsThatReturnStringsActuallyReturnStrings(faker.phoneNumber());
-        testAllMethodsThatReturnStringsActuallyReturnStrings(faker.name());
-        testAllMethodsThatReturnStringsActuallyReturnStrings(faker.finance());
-        testAllMethodsThatReturnStringsActuallyReturnStrings(faker.shakespeare());
-        testAllMethodsThatReturnStringsActuallyReturnStrings(faker.superhero());
-        testAllMethodsThatReturnStringsActuallyReturnStrings(faker.team());
-        testAllMethodsThatReturnStringsActuallyReturnStrings(faker.beer());
-    }
-
-    private void testAllMethodsThatReturnStringsActuallyReturnStrings(Object object) throws IllegalAccessException, InvocationTargetException {
-        @SuppressWarnings("unchecked")
-        Set<Method> methodsThatReturnStrings = getAllMethods(object.getClass(),
-                withModifier(Modifier.PUBLIC),
-                withReturnType(String.class),
-                withParametersCount(0));
-
-        for (Method method : methodsThatReturnStrings) {
-            final Object returnValue = method.invoke(object);
-            logger.info(String.format("Invoked %s.%s = %s", object.getClass().getSimpleName().toLowerCase(), method.getName(), returnValue));
-            assertThat(returnValue, is(notNullValue()));
-            assertThat((String) returnValue, not(isEmptyString()));
-
-        }
-    }
-
-    @Test
-    public void testExceptionsNotCoveredInAboveTest() throws Exception {
-        assertThat(faker.bothify("####???"), is(notNullValue()));
-        assertThat(faker.letterify("????"), is(notNullValue()));
-        assertThat(faker.numerify("####"), is(notNullValue()));
-
-        assertThat(faker.lorem().paragraph(1), is(notNullValue()));
-        assertThat(faker.lorem().paragraphs(1), is(notNullValue()));
-
-        assertThat(faker.lorem().sentence(1), is(notNullValue()));
-        assertThat(faker.lorem().sentences(1), is(notNullValue()));
-
-        assertThat(faker.address().streetAddress(), is(notNullValue()));
-
-        assertThat(faker.lorem().words(), is(notNullValue()));
-        assertThat(faker.lorem().words(1), is(notNullValue()));
-    }
+  @Test public void testExceptionsNotCoveredInAboveTest() throws Exception {
+    assertThat(faker.bothify("####???"), is(notNullValue()));
+    assertThat(faker.letterify("????"), is(notNullValue()));
+    assertThat(faker.numerify("####"), is(notNullValue()));
+    assertThat(faker.lorem().paragraph(1), is(notNullValue()));
+    assertThat(faker.lorem().paragraphs(1), is(notNullValue()));
+    assertThat(faker.lorem().sentence(1), is(notNullValue()));
+    assertThat(faker.lorem().sentences(1), is(notNullValue()));
+    assertThat(faker.address().streetAddress(), is(notNullValue()));
+    assertThat(faker.lorem().words(), is(notNullValue()));
+    assertThat(faker.lorem().words(1), is(notNullValue()));
+  }
 }
