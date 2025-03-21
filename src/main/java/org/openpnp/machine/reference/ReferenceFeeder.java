a4c6077c13bbade996df81a71ed5af0387c53709
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 01:59:23 +0000

    Mining Framework Analysis

--- src/main/java/org/openpnp/machine/reference/ReferenceFeeder.java
+++ src/main/java/org/openpnp/machine/reference/ReferenceFeeder.java
@@ -1,7 +1,5 @@
 package org.openpnp.machine.reference;
-
 import java.util.List;
-
 import org.openpnp.model.Configuration;
 import org.openpnp.model.LengthUnit;
 import org.openpnp.model.Location;
@@ -14,138 +12,118 @@ import org.simpleframework.xml.Element;
 import org.simpleframework.xml.core.Commit;
 
 public abstract class ReferenceFeeder extends AbstractFeeder {
-    public static Location ROOT_FEEDER_LOCATION = new Location(LengthUnit.Millimeters,0,0,0,0);
-    
-    // This now holds the location and rotation of the feeder relative to its parent's location  
-    @Element
-    private Location location = new Location(LengthUnit.Millimeters);
+  public static Location ROOT_FEEDER_LOCATION = new Location(LengthUnit.Millimeters, 0, 0, 0, 0);
 
-    // This now holds the rotation of the part within the feeder
-    @Attribute(required=false)
-    protected Double rotationInFeeder;
-    
-    @Commit
-    public void commit() {
-        if (rotationInFeeder == null) {
-            /*
-             * Originally the rotation of the part was stored in the feeder's location field
-             * and there was really no concept of the feeder's rotation.  Now, with the introduction
-             * of feeder groups, the rotation of the part in the feeder is independent of the
-             * rotation of the feeder.  The feeder's location field now contains the location and
-             * rotation of the feeder relative to its parent and the rotationInFeeder field contains
-             * the rotation of the part relative to the feeder.
-             */
-            Logger.trace(name + ": Old feeder format found, updating to new format..." );
-            rotationInFeeder = 0.0;
-        }
-    }
-    
-    public void setRotationInFeeder(Double rotationInFeeder) {
-        this.rotationInFeeder = rotationInFeeder;
-    }
-    
-    public Double getRotationInFeeder() {
-        if (rotationInFeeder == null) {
-            rotationInFeeder = 0.0;
-        }
-        return rotationInFeeder;
-    }
-        
-    @Override
-    public void setParentId(String parentId) {
-        Location globalLocation = getLocation();
-        super.setParentId(parentId);
-        setLocation(globalLocation);
-    }
+  @Element private Location location = new Location(LengthUnit.Millimeters);
 
-    private Location getLocalLocation() {
-        return location;
-    }
+  @Attribute(required = false) protected Double rotationInFeeder;
 
-    private void setLocalLocation(Location localLocation) {
-        Object oldValue = this.location;
-        this.location = localLocation;
-        firePropertyChange("location", oldValue, localLocation);
+  @Commit public void commit() {
+    if (rotationInFeeder == null) {
+      Logger.trace(name + ": Old feeder format found, updating to new format...");
+      rotationInFeeder = 0.0;
     }
-    
-    public Location getLocation() {
-        Location parentLocation;
-        if (parentId.equals(ROOT_FEEDER_ID)) {
-            parentLocation = ROOT_FEEDER_LOCATION;
-        } else {
-            parentLocation = ((ReferenceFeeder) Configuration.get().getMachine().getFeeder(parentId)).getLocation();
-        }
-        return convertToGlobalLocation(parentLocation, getLocalLocation());
-    }
+  }
 
-    public void setLocation(Location globalLocation) {
-        Location parentLocation;
-        if (parentId.equals(ROOT_FEEDER_ID)) {
-            parentLocation = ROOT_FEEDER_LOCATION;
-        } else {
-            parentLocation = ((ReferenceFeeder) Configuration.get().getMachine().getFeeder(parentId)).getLocation();
-        }
-        setLocalLocation(convertToLocalLocation(parentLocation, globalLocation));
-    }
+  public void setRotationInFeeder(Double rotationInFeeder) {
+    this.rotationInFeeder = rotationInFeeder;
+  }
 
-    @Override
-    public Location getJobPreparationLocation()  {
-        // the default RefrenceFeeder has no prep. location
-        return null;
+  public Double getRotationInFeeder() {
+    if (rotationInFeeder == null) {
+      rotationInFeeder = 0.0;
     }
-    
-    @Override
-    public void prepareForJob(boolean visit) throws Exception {
-        // the default RefrenceFeeder needs no prep.
-    }
-    
-    public Location convertToGlobalLocation(Location localLocation) {
-        Location originLocation = getLocation();
-        return convertToGlobalLocation(originLocation, localLocation);
-    }
-    
-    public Location convertToLocalLocation(Location globalLocation) {
-        Location originLocation = getLocation();
-        return convertToLocalLocation(originLocation, globalLocation);
-    }
+    return rotationInFeeder;
+  }
 
-    public Location convertToGlobalDeltaLocation(Location localDeltaLocation) {
-        Location originLocation = getLocation();
-        return convertToGlobalDeltaLocation(originLocation, localDeltaLocation);
-    }
-    
-    public Location convertToLocalDeltaLocation(Location globalDeltaLocation) {
-        Location originLocation = getLocation();
-        return convertToLocalDeltaLocation(originLocation, globalDeltaLocation);
-    }
+  @Override public void setParentId(String parentId) {
+    Location globalLocation = getLocation();
+    super.setParentId(parentId);
+    setLocation(globalLocation);
+  }
 
-    static public Location convertToGlobalLocation(Location origin, Location localLocation) {
-        if (localLocation == null) {
-            return null;
-        }
-        return localLocation.vectorAdd(origin);
-    }
-    
-    static public Location convertToLocalLocation(Location origin, Location globalLocation) {
-        if (globalLocation == null) {
-            return null;
-        }
-        return globalLocation.vectorSubtract(origin);
-    }
-    
-    static public Location convertToGlobalDeltaLocation(Location origin, Location localDeltaLocation) {
-        if (localDeltaLocation == null) {
-            return null;
-        }
-        return convertToGlobalLocation(origin, localDeltaLocation).subtract(origin);
-    }
+  private Location getLocalLocation() {
+    return location;
+  }
 
-    static public Location convertToLocalDeltaLocation(Location origin, Location globalDeltaLocation) {
-        if (globalDeltaLocation == null) {
-            return null;
-        }
-        return convertToLocalLocation(origin, globalDeltaLocation.add(origin));
-    }
+  private void setLocalLocation(Location localLocation) {
+    Object oldValue = this.location;
+    this.location = localLocation;
+    firePropertyChange("location", oldValue, localLocation);
+  }
 
+  public Location getLocation() {
+    Location parentLocation;
+    if (parentId.equals(ROOT_FEEDER_ID)) {
+      parentLocation = ROOT_FEEDER_LOCATION;
+    } else {
+      parentLocation = ((ReferenceFeeder) Configuration.get().getMachine().getFeeder(parentId)).getLocation();
+    }
+    return convertToGlobalLocation(parentLocation, getLocalLocation());
+  }
+
+  public void setLocation(Location globalLocation) {
+    Location parentLocation;
+    if (parentId.equals(ROOT_FEEDER_ID)) {
+      parentLocation = ROOT_FEEDER_LOCATION;
+    } else {
+      parentLocation = ((ReferenceFeeder) Configuration.get().getMachine().getFeeder(parentId)).getLocation();
+    }
+    setLocalLocation(convertToLocalLocation(parentLocation, globalLocation));
+  }
+
+  @Override public Location getJobPreparationLocation() {
+    return null;
+  }
+
+  @Override public void prepareForJob(boolean visit) throws Exception {
+  }
+
+  public Location convertToGlobalLocation(Location localLocation) {
+    Location originLocation = getLocation();
+    return convertToGlobalLocation(originLocation, localLocation);
+  }
+
+  public Location convertToLocalLocation(Location globalLocation) {
+    Location originLocation = getLocation();
+    return convertToLocalLocation(originLocation, globalLocation);
+  }
+
+  public Location convertToGlobalDeltaLocation(Location localDeltaLocation) {
+    Location originLocation = getLocation();
+    return convertToGlobalDeltaLocation(originLocation, localDeltaLocation);
+  }
+
+  public Location convertToLocalDeltaLocation(Location globalDeltaLocation) {
+    Location originLocation = getLocation();
+    return convertToLocalDeltaLocation(originLocation, globalDeltaLocation);
+  }
+
+  static public Location convertToGlobalLocation(Location origin, Location localLocation) {
+    if (localLocation == null) {
+      return null;
+    }
+    return localLocation.vectorAdd(origin);
+  }
+
+  static public Location convertToLocalLocation(Location origin, Location globalLocation) {
+    if (globalLocation == null) {
+      return null;
+    }
+    return globalLocation.vectorSubtract(origin);
+  }
+
+  static public Location convertToGlobalDeltaLocation(Location origin, Location localDeltaLocation) {
+    if (localDeltaLocation == null) {
+      return null;
+    }
+    return convertToGlobalLocation(origin, localDeltaLocation).subtract(origin);
+  }
+
+  static public Location convertToLocalDeltaLocation(Location origin, Location globalDeltaLocation) {
+    if (globalDeltaLocation == null) {
+      return null;
+    }
+    return convertToLocalLocation(origin, globalDeltaLocation.add(origin));
+  }
 }
-
--- src/main/java/org/openpnp/spi/base/AbstractFeeder.java
+++ src/main/java/org/openpnp/spi/base/AbstractFeeder.java
@@ -1,7 +1,5 @@
 package org.openpnp.spi.base;
-
 import javax.swing.Icon;
-
 import org.openpnp.ConfigurationListener;
 import org.openpnp.gui.support.Icons;
 import org.openpnp.gui.support.PropertySheetWizardAdapter;
@@ -15,174 +13,141 @@ import org.simpleframework.xml.Attribute;
 import org.simpleframework.xml.core.Commit;
 
 public abstract class AbstractFeeder extends AbstractModelObject implements Feeder {
-    @Attribute
-    protected String id;
+  @Attribute protected String id;
 
-    @Attribute(required = false)
-    protected String parentId;
+  @Attribute(required = false) protected String parentId;
 
-    @Attribute(required = false)
-    protected String name;
+  @Attribute(required = false) protected String name;
 
-    @Attribute
-    protected boolean enabled;
+  @Attribute protected boolean enabled;
 
-    @Attribute
-    protected String partId;
-    
-    /**
+  @Attribute protected String partId;
+
+  /**
      * Note: This is feedRetryCount in reality. It was left as retryCount for backwards
      * compatibility when pickRetryCount was added. 
      */
-    @Attribute(required=false)
-    protected int retryCount = 3;
-    
-    @Attribute(required = false)
-    protected int pickRetryCount = 3;
+  @Attribute(required = false) protected int retryCount = 3;
 
-    protected Part part;
-    
-    //protected WizardContainer wizardContainer;
-    
-    public AbstractFeeder() {
-        this.id = Configuration.createId("FDR");
-        this.parentId = ROOT_FEEDER_ID;
-        this.name = getClass().getSimpleName();
-        Configuration.get().addListener(new ConfigurationListener.Adapter() {
-            @Override
-            public void configurationLoaded(Configuration configuration) throws Exception {
-                part = configuration.getPart(partId);
-            }
-        });
-    }
+  @Attribute(required = false) protected int pickRetryCount = 3;
 
-    @Override
-    public String getId() {
-        return id;
-    }
+  protected Part part;
 
-    @Override
-    public boolean isEnabled() {
-        if (parentId.equals(ROOT_FEEDER_ID)) {
-            return enabled;
-        } else {
-            return enabled && Configuration.get().getMachine().getFeeder(parentId).isEnabled();
-        }
-    }
+  public AbstractFeeder() {
+    this.id = Configuration.createId("FDR");
+    this.parentId = ROOT_FEEDER_ID;
+    this.name = getClass().getSimpleName();
+    Configuration.get().addListener(new ConfigurationListener.Adapter() {
+      @Override public void configurationLoaded(Configuration configuration) throws Exception {
+        part = configuration.getPart(partId);
+      }
+    });
+  }
 
-    @Override
-    public void setEnabled(boolean enabled) {
-        Object oldValue = this.enabled;
-        this.enabled = enabled;
-        firePropertyChange("enabled", oldValue, enabled);
-    }
-    
-    public boolean isLocallyEnabled() {
-        return enabled;
-    }
+  @Override public String getId() {
+    return id;
+  }
 
-    @Override
-    public String getParentId() {
-        return parentId;
+  @Override public boolean isEnabled() {
+    if (parentId.equals(ROOT_FEEDER_ID)) {
+      return enabled;
+    } else {
+      return enabled && Configuration.get().getMachine().getFeeder(parentId).isEnabled();
     }
+  }
 
-    /*
-     * Any class extending AbstractFeeder that intends to support feeder nesting, needs to override
-     * setParentId, addChild, removeChild, removeAllChildern, and isPotentialParentOf to provide the
-     * required functionality.  Any class, that desires to only have the machine as its parent, should
-     * override isParentIdChangable to return false.
-     */
-    @Override
-    public void setParentId(String parentId) {
-        if (isParentIdChangable()) {
-            if (!this.parentId.equals(ROOT_FEEDER_ID)) {
-                Configuration.get().getMachine().getFeeder(this.parentId).removeChild(getId());
-            }
-            this.parentId = parentId;
-            if (!parentId.equals(ROOT_FEEDER_ID)) {
-                Configuration.get().getMachine().getFeeder(parentId).addChild(getId());
-            }
-        }
-    }
+  @Override public void setEnabled(boolean enabled) {
+    Object oldValue = this.enabled;
+    this.enabled = enabled;
+    firePropertyChange("enabled", oldValue, enabled);
+  }
 
-    @Override
-    public boolean isParentIdChangable() {
-        return true;
-    }
-    
-    @Override
-    public void addChild(String childId) {
-    }
-    
-    @Override
-    public void removeChild(String childId) {
-    }
-    
-    @Override
-    public void removeAllChildren() {
-    }
-    
-    @Override
-    public boolean isPotentialParentOf(Feeder feeder) {
-        return false;
-    }
+  public boolean isLocallyEnabled() {
+    return enabled;
+  }
 
-    @Override
-    public void preDeleteCleanUp() {
-        if (!parentId.equals(ROOT_FEEDER_ID)) {
-            Configuration.get().getMachine().getFeeder(parentId).removeChild(getId());
-        }
-    }
-    
-    
-    @Override
-    public void setPart(Part part) {
-        Object oldValue = this.part;
-        this.part = part;
-        firePropertyChange("part", oldValue, part);
-        this.partId = part.getId();
-    }
+  @Override public String getParentId() {
+    return parentId;
+  }
 
-    @Override
-    public Part getPart() {
-        return part;
+  @Override public void setParentId(String parentId) {
+    if (isParentIdChangable()) {
+      if (!this.parentId.equals(ROOT_FEEDER_ID)) {
+        Configuration.get().getMachine().getFeeder(this.parentId).removeChild(getId());
+      }
+      this.parentId = parentId;
+      if (!parentId.equals(ROOT_FEEDER_ID)) {
+        Configuration.get().getMachine().getFeeder(parentId).addChild(getId());
+      }
     }
+  }
 
-    @Override
-    public String getName() {
-        return name;
-    }
+  @Override public boolean isParentIdChangable() {
+    return true;
+  }
 
-    @Override
-    public void setName(String name) {
-        this.name = name;
-    }
+  @Override public void addChild(String childId) {
+  }
 
-    @Override
-    public Icon getPropertySheetHolderIcon() {
-        return Icons.feeder;
-    }
+  @Override public void removeChild(String childId) {
+  }
 
-    public int getFeedRetryCount() {
-        return retryCount;
-    }
+  @Override public void removeAllChildren() {
+  }
 
-    public void setFeedRetryCount(int retryCount) {
-        this.retryCount = retryCount;
-    }
-    
-    public int getPickRetryCount() {
-        return pickRetryCount;
-    }
+  @Override public boolean isPotentialParentOf(Feeder feeder) {
+    return false;
+  }
 
-    public void setPickRetryCount(int pickRetryCount) {
-        this.pickRetryCount = pickRetryCount;
+  @Override public void preDeleteCleanUp() {
+    if (!parentId.equals(ROOT_FEEDER_ID)) {
+      Configuration.get().getMachine().getFeeder(parentId).removeChild(getId());
     }
+  }
 
-    @Override
-    public PropertySheet[] getPropertySheets() {
-        return new PropertySheet[] {new PropertySheetWizardAdapter(getConfigurationWizard(), "Configuration")};
-    }
-    
-    public void postPick(Nozzle nozzle) throws Exception { }
+  @Override public void setPart(Part part) {
+    Object oldValue = this.part;
+    this.part = part;
+    firePropertyChange("part", oldValue, part);
+    this.partId = part.getId();
+  }
+
+  @Override public Part getPart() {
+    return part;
+  }
+
+  @Override public String getName() {
+    return name;
+  }
+
+  @Override public void setName(String name) {
+    this.name = name;
+  }
+
+  @Override public Icon getPropertySheetHolderIcon() {
+    return Icons.feeder;
+  }
+
+  public int getFeedRetryCount() {
+    return retryCount;
+  }
+
+  public void setFeedRetryCount(int retryCount) {
+    this.retryCount = retryCount;
+  }
+
+  public int getPickRetryCount() {
+    return pickRetryCount;
+  }
+
+  public void setPickRetryCount(int pickRetryCount) {
+    this.pickRetryCount = pickRetryCount;
+  }
+
+  @Override public PropertySheet[] getPropertySheets() {
+    return new PropertySheet[] { new PropertySheetWizardAdapter(getConfigurationWizard(), "Configuration") };
+  }
+
+  public void postPick(Nozzle nozzle) throws Exception {
+  }
 }
