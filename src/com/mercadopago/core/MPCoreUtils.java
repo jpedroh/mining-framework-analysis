8f8c8bb10e837282442c68ea107ecac9c1278119
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 00:18:53 +0000

    Mining Framework Analysis

--- src/com/mercadopago/core/MPCoreUtils.java
+++ src/com/mercadopago/core/MPCoreUtils.java
@@ -1,11 +1,9 @@
 package com.mercadopago.core;
-
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
 import com.mercadopago.exceptions.MPException;
 import org.apache.commons.io.output.ByteArrayOutputStream;
-
 import java.io.InputStream;
 
 /**
@@ -15,43 +13,39 @@ import java.io.InputStream;
  * Created by Eduardo Paoletta on 11/17/16.
  */
 public class MPCoreUtils {
+  public static final String FORMAT_ISO8601 = "yyyy-MM-dd\'T\'HH:mm:ssZ";
 
-    public static final String FORMAT_ISO8601 = "yyyy-MM-dd'T'HH:mm:ssZ";
-
-    /**
+  /**
      * Static method that transforms all attributes members of the instance in a JSON Element.
      * @return                  a JSON Object with the attributes members of the instance
      */
-    public static JsonObject getJson(Object object) {
-        Gson gson = new GsonBuilder().setDateFormat(FORMAT_ISO8601).create();
-        return (JsonObject) gson.toJsonTree(object);
-    }
+  public static JsonObject getJson(Object object) {
+    Gson gson = new GsonBuilder().setDateFormat(FORMAT_ISO8601).create();
+    return (JsonObject) gson.toJsonTree(object);
+  }
 
-    /**
+  /**
      * Static method that transform an Input Stream to a String object, returns an empty string if InputStream is null.
      *
      * @param is                    Input Stream to process
      * @return                      a String with the stream content
      * @throws MPException
      */
-    public static String inputStreamToString(InputStream is) throws MPException {
-        String value = "";
-        if (is != null) {
-            try {
-                ByteArrayOutputStream result = new ByteArrayOutputStream();
-                byte[] buffer = new byte[1024];
-                int length;
-                while ((length = is.read(buffer)) != -1) {
-                    result.write(buffer, 0, length);
-                }
-                value = result.toString("UTF-8");
-
-            } catch (Exception ex) {
-                throw new MPException(ex);
-            }
+  public static String inputStreamToString(InputStream is) throws MPException {
+    String value = "";
+    if (is != null) {
+      try {
+        ByteArrayOutputStream result = new ByteArrayOutputStream();
+        byte[] buffer = new byte[1024];
+        int length;
+        while ((length = is.read(buffer)) != -1) {
+          result.write(buffer, 0, length);
         }
-        return value;
-
+        value = result.toString("UTF-8");
+      } catch (Exception ex) {
+        throw new MPException(ex);
+      }
     }
-
+    return value;
+  }
 }
--- src/test/mercadopago/core/MPBaseTest.java
+++ src/test/mercadopago/core/MPBaseTest.java
@@ -1,14 +1,11 @@
 package test.mercadopago.core;
-
 import com.mercadopago.core.restannotations.GET;
 import com.mercadopago.core.restannotations.POST;
 import com.mercadopago.core.restannotations.PUT;
 import com.mercadopago.core.MPBase;
 import com.mercadopago.exceptions.MPException;
 import org.junit.Test;
-
 import java.util.Date;
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertSame;
 
@@ -19,142 +16,118 @@ import static org.junit.Assert.assertSame;
  * Created by Eduardo Paoletta on 11/4/16.
  */
 public class MPBaseTest extends MPBase {
+  private String id = null;
 
-    private String id = null;
-    private String testString = "Test String";
-    private Integer testInteger = 666;
+  private String testString = "Test String";
 
-    @GET(path="/getpath/slug/:id")
-    public String load(String id) throws MPException {
-        return super.processMethod("load", id);
-    }
+  private Integer testInteger = 666;
 
-    @GET(path="/getpath/slug/")
-    @PUT(path="/putpath/slug/")
-    public String loadAll() throws MPException {
-        return super.processMethod("loadAll");
-    }
+  @GET(path = "/getpath/slug/:id") public String load(String id) throws MPException {
+    return super.processMethod("load", id);
+  }
 
-    //Save method will be used to test non annotated method
-    //@POST(path="/postpath/slug")
-    public String save() throws MPException {
-        return super.processMethod("save");
-    }
+  @GET(path = "/getpath/slug/") @PUT(path = "/putpath/slug/") public String loadAll() throws MPException {
+    return super.processMethod("loadAll");
+  }
 
-    @POST(path="/postpath/slug")
-    public String create() throws MPException {
-        return super.processMethod("create");
-    }
+  public String save() throws MPException {
+    return super.processMethod("save");
+  }
 
-    @PUT(path="/putpath/slug/:id")
-    public String update(String id) throws MPException {
-        return super.processMethod("update", id);
-    }
+  @POST(path = "/postpath/slug") public String create() throws MPException {
+    return super.processMethod("create");
+  }
 
-    //Delete method will be used to test non existing method
-    /*
-    @DELETE(path="/deletepath/slug/:id")
-    public String delete(String id) throws MPException {
-        return super.processMethod("delete", id);
-    }
-    */
+  @PUT(path = "/putpath/slug/:id") public String update(String id) throws MPException {
+    return super.processMethod("update", id);
+  }
 
-    /**
+  /**
      * Test MPBase using a not allowed method to call processMethod.
      * It should return an MPException
      */
-    @Test
-    public void nonAllowedMethodTest() throws Exception {
-        Exception auxException = null;
-        try {
-            super.processMethod("nonallowedmethod");
-        } catch (MPException mpException) {
-            assertEquals("Exception must have \"Method \"nonallowedmethod\" not allowed\" message", mpException.getMessage(), "Method \"nonallowedmethod\" not allowed");
-            auxException = mpException;
-        }
-        assertSame("Exception type must be \"MPException\"", MPException.class, auxException.getClass());
+  @Test public void nonAllowedMethodTest() throws Exception {
+    Exception auxException = null;
+    try {
+      super.processMethod("nonallowedmethod");
+    } catch (MPException mpException) {
+      assertEquals("Exception must have \"Method \"nonallowedmethod\" not allowed\" message", mpException.getMessage(), "Method \"nonallowedmethod\" not allowed");
+      auxException = mpException;
     }
+    assertSame("Exception type must be \"MPException\"", MPException.class, auxException.getClass());
+  }
 
-    /**
+  /**
      * Test MPBase using a non existent method to call processMethod.
      * It should return an MPException
      */
-    @Test
-    public void notExistentMethodTest() throws Exception {
-        Exception auxException = null;
-        try {
-            super.processMethod("delete", "test_id");
-        } catch (MPException mpException) {
-            assertEquals("Exception must have \"No method found\" message", mpException.getMessage(), "No method found");
-            auxException = mpException;
-        }
-        assertSame("Exception type must be \"MPException\"", MPException.class, auxException.getClass());
+  @Test public void notExistentMethodTest() throws Exception {
+    Exception auxException = null;
+    try {
+      super.processMethod("delete", "test_id");
+    } catch (MPException mpException) {
+      assertEquals("Exception must have \"No method found\" message", mpException.getMessage(), "No method found");
+      auxException = mpException;
     }
+    assertSame("Exception type must be \"MPException\"", MPException.class, auxException.getClass());
+  }
 
-    /**
+  /**
      * Test MPBase using a not annotated method to call proccessMethod.
      * It should return an MPException.
      */
-    @Test
-    public void notAnnotatedMethodTest() throws Exception {
-        Exception auxException = null;
-        try {
-            save();
-        } catch (MPException mpException) {
-            assertEquals("Exception must have \"No rest method found\" message", mpException.getMessage(), "No rest method found");
-            auxException = mpException;
-        }
-        assertSame("Exception type must be \"MPException\"", MPException.class, auxException.getClass());
+  @Test public void notAnnotatedMethodTest() throws Exception {
+    Exception auxException = null;
+    try {
+      save();
+    } catch (MPException mpException) {
+      assertEquals("Exception must have \"No rest method found\" message", mpException.getMessage(), "No rest method found");
+      auxException = mpException;
     }
+    assertSame("Exception type must be \"MPException\"", MPException.class, auxException.getClass());
+  }
 
-    /**
+  /**
      * Test MPBase using a method decorated with multiple rest information annotations.
      * It should return an MPException
      */
-    @Test
-    public void multipleAnnotationMethodTest() throws Exception {
-        Exception auxException = null;
-        try {
-            loadAll();
-        } catch (MPException mpException) {
-            assertEquals("Exception must have \"Multiple rest methods found\" message", mpException.getMessage(), "Multiple rest methods found");
-            auxException = mpException;
-        }
-        assertSame("Exception type must be \"MPException\"", MPException.class, auxException.getClass());
+  @Test public void multipleAnnotationMethodTest() throws Exception {
+    Exception auxException = null;
+    try {
+      loadAll();
+    } catch (MPException mpException) {
+      assertEquals("Exception must have \"Multiple rest methods found\" message", mpException.getMessage(), "Multiple rest methods found");
+      auxException = mpException;
     }
+    assertSame("Exception type must be \"MPException\"", MPException.class, auxException.getClass());
+  }
 
-    /**
+  /**
      * Test MPBase using a method without an argument, but decorated with a rest information annotation that especified :param
      * It should return an MPException
      */
-    @Test
-    public void methodWithNoArgumentTest() throws  Exception {
-        Exception auxException = null;
-        try {
-            load(null);
-        } catch (MPException mpException) {
-            assertEquals("Exception must have \"No argument supplied for method\" message", mpException.getMessage(), "No argument supplied/found for method path");
-            auxException = mpException;
-        }
-        assertSame("Exception type must be \"MPException\"", MPException.class, auxException.getClass());
+  @Test public void methodWithNoArgumentTest() throws Exception {
+    Exception auxException = null;
+    try {
+      load(null);
+    } catch (MPException mpException) {
+      assertEquals("Exception must have \"No argument supplied for method\" message", mpException.getMessage(), "No argument supplied/found for method path");
+      auxException = mpException;
     }
+    assertSame("Exception type must be \"MPException\"", MPException.class, auxException.getClass());
+  }
 
-    //TODO: This test will change with future features implementations on MPBase
-    /**
+  /**
      * Test methods for all different standarized methods and possible uses.
      */
-    @Test
-    public void methodPathTest() throws Exception {
-        assertEquals("{\"method\":\"GET\",\"path\":\"https://api.mercadopago.com/getpath/slug/test_id\"}", load("test_id"));
-        assertEquals("{\"method\":\"POST\",\"path\":\"https://api.mercadopago.com/postpath/slug\",\"payload\":{\"testString\":\"Test String\",\"testInteger\":666}}", create());
-        assertEquals("{\"method\":\"PUT\",\"path\":\"https://api.mercadopago.com/putpath/slug/test_id\",\"payload\":{}}", update("test_id"));
-
-        MPBaseTest resource = new MPBaseTest();
-        resource.id = "5";
-        resource.load(null);
-        resource.testString = "TestUpdate";
-        assertEquals("{\"method\":\"PUT\",\"path\":\"https://api.mercadopago.com/putpath/slug/5\",\"payload\":{\"testString\":\"TestUpdate\"}}", resource.update(null));
-
-    }
-
+  @Test public void methodPathTest() throws Exception {
+    assertEquals("{\"method\":\"GET\",\"path\":\"https://api.mercadopago.com/getpath/slug/test_id\"}", load("test_id"));
+    assertEquals("{\"method\":\"POST\",\"path\":\"https://api.mercadopago.com/postpath/slug\",\"payload\":{\"testString\":\"Test String\",\"testInteger\":666}}", create());
+    assertEquals("{\"method\":\"PUT\",\"path\":\"https://api.mercadopago.com/putpath/slug/test_id\",\"payload\":{}}", update("test_id"));
+    MPBaseTest resource = new MPBaseTest();
+    resource.id = "5";
+    resource.load(null);
+    resource.testString = "TestUpdate";
+    assertEquals("{\"method\":\"PUT\",\"path\":\"https://api.mercadopago.com/putpath/slug/5\",\"payload\":{\"testString\":\"TestUpdate\"}}", resource.update(null));
+  }
 }
