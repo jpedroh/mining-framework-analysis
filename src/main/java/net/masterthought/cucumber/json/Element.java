d07a9600fe931efa08a3b5dd058de58db693a9f5
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 03:54:15 +0000

    Mining Framework Analysis

--- src/main/java/net/masterthought/cucumber/json/Element.java
+++ src/main/java/net/masterthought/cucumber/json/Element.java
@@ -1,152 +1,161 @@
 package net.masterthought.cucumber.json;
-
 import net.masterthought.cucumber.json.support.Resultsable;
 import org.apache.commons.lang.StringUtils;
-
 import net.masterthought.cucumber.json.support.Durationable;
 import net.masterthought.cucumber.json.support.Status;
 import net.masterthought.cucumber.json.support.StatusCounter;
 import net.masterthought.cucumber.util.Util;
 
 public class Element implements Durationable {
+  private String index = null;
 
-    private String index = null;
-    // Start: attributes from JSON file report
-    private final String name = null;
-    private final String type = null;
-    private final String description = null;
-    private final String keyword = null;
-    private final Step[] steps = new Step[0];
-    private final Hook[] before = new Hook[0];
-    private final Hook[] after = new Hook[0];
-    private final Tag[] tags = new Tag[0];
-    // End: attributes from JSON file report
+  private final String name = null;
 
-    private static final String SCENARIO_TYPE = "scenario";
+  private final String type = null;
 
-    private Status elementStatus;
-    private Status beforeStatus;
-    private Status afterStatus;
-    private Status stepsStatus;
+  private final String description = null;
 
-    private Feature feature;
-    private long duration;
+  private final String keyword = null;
 
-    public Step[] getSteps() {
-        return steps;
-    }
+  private final Step[] steps = new Step[0];
 
-    public Hook[] getBefore() {
-        return before;
-    }
+  private final Hook[] before = new Hook[0];
 
-    public Hook[] getAfter() {
-        return after;
-    }
+  private final Hook[] after = new Hook[0];
 
-    public Tag[] getTags() {
-        return tags;
-    }
+  private final Tag[] tags = new Tag[0];
 
-    public Status getStatus() {
-        return elementStatus;
-    }
+  private static final String SCENARIO_TYPE = "scenario";
 
-    public Status getBeforeStatus() {
-        return beforeStatus;
-    }
+  private Status elementStatus;
 
-    public Status getAfterStatus() {
-        return afterStatus;
-    }
+  private Status beforeStatus;
 
-    public Status getStepsStatus() {
-        return stepsStatus;
-    }
+  private Status afterStatus;
 
-    public String getName() {
-        return name;
-    }
+  private Status stepsStatus;
 
-    public String getKeyword() {
-        return keyword;
-    }
+  private Feature feature;
 
-    public String getType() {
-        return type;
-    }
+  private long duration;
 
-    public String getDescription() {
-        return StringUtils.defaultString(description);
-    }
+  public Step[] getSteps() {
+    return steps;
+  }
 
-    public boolean isScenario() {
-        return SCENARIO_TYPE.equalsIgnoreCase(type);
-    }
+  public Hook[] getBefore() {
+    return before;
+  }
 
-    public Feature getFeature() {
-        return feature;
-    }
+  public Hook[] getAfter() {
+    return after;
+  }
 
-    @Override
-    public long getDuration() {
-        return duration;
-    }
+  public Tag[] getTags() {
+    return tags;
+  }
 
-    @Override
-    public String getFormattedDuration() {
-        return Util.formatDuration(duration);
-    }
+  public Status getStatus() {
+    return elementStatus;
+  }
 
-    /**
+  public Status getBeforeStatus() {
+    return beforeStatus;
+  }
+
+  public Status getAfterStatus() {
+    return afterStatus;
+  }
+
+  public Status getStepsStatus() {
+    return stepsStatus;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getKeyword() {
+    return keyword;
+  }
+
+  public String getType() {
+    return type;
+  }
+
+  public String getDescription() {
+    return StringUtils.defaultString(description);
+  }
+
+  public boolean isScenario() {
+    return SCENARIO_TYPE.equalsIgnoreCase(type);
+  }
+
+  public Feature getFeature() {
+    return feature;
+  }
+
+  @Override public long getDuration() {
+    return duration;
+  }
+
+  @Override public String getFormattedDuration() {
+    return Util.formatDuration(duration);
+  }
+
+  /**
      * Retrieves the index value for which this current instance maps to
      * in the Elements array contained within the 'feature' class-member.
      * @return the index value
      */
-    public String getIndex() { return this.index; }
+  public String getIndex() {
+    return this.index;
+  }
 
-    /**
+  /**
      * Sets the index value for which this current instance maps to
      * in the Elements array contained within the 'feature' class-member.
      * @param index the index value
      */
-    public void setIndex(Integer index) { this.index = index.toString(); }
+  public void setIndex(Integer index) {
+    this.index = index.toString();
+  }
 
-    public void setMetaData(Feature feature) {
-        this.feature = feature;
-
-        beforeStatus = calculateHookStatus(before);
-        afterStatus = calculateHookStatus(after);
-        stepsStatus = calculateStepsStatus();
-        elementStatus = calculateElementStatus();
-        Resultsable[] resultsables = Util.joinResultables(before, steps, after);
-        for(Resultsable resultsable : resultsables) { resultsable.setId(resultsable.generateId(this)); }
+  public void setMetaData(Feature feature) {
+    this.feature = feature;
+    beforeStatus = calculateHookStatus(before);
+    afterStatus = calculateHookStatus(after);
+    stepsStatus = calculateStepsStatus();
+    elementStatus = calculateElementStatus();
+    Resultsable[] resultsables = Util.joinResultables(before, steps, after);
+    for (Resultsable resultsable : resultsables) {
+      resultsable.setId(resultsable.generateId(this));
     }
+  }
 
-    private Status calculateHookStatus(Hook[] hooks) {
-        StatusCounter statusCounter = new StatusCounter();
-        for (Hook hook : hooks) {
-            statusCounter.incrementFor(hook.getResult().getStatus());
-        }
-
-        return statusCounter.getFinalStatus();
+  private Status calculateHookStatus(Hook[] hooks) {
+    StatusCounter statusCounter = new StatusCounter();
+    for (Hook hook : hooks) {
+      statusCounter.incrementFor(hook.getResult().getStatus());
     }
+    return statusCounter.getFinalStatus();
+  }
 
-    private Status calculateElementStatus() {
-        StatusCounter statusCounter = new StatusCounter();
-        statusCounter.incrementFor(stepsStatus);
-        statusCounter.incrementFor(beforeStatus);
-        statusCounter.incrementFor(afterStatus);
-        return statusCounter.getFinalStatus();
-    }
+  private Status calculateElementStatus() {
+    StatusCounter statusCounter = new StatusCounter();
+    statusCounter.incrementFor(stepsStatus);
+    statusCounter.incrementFor(beforeStatus);
+    statusCounter.incrementFor(afterStatus);
+    return statusCounter.getFinalStatus();
+  }
 
-    private Status calculateStepsStatus() {
-        StatusCounter statusCounter = new StatusCounter();
-        for (Step step : steps) {
-            Result result = step.getResult();
-            statusCounter.incrementFor(result.getStatus());
-            duration += result.getDuration();
-        }
-        return statusCounter.getFinalStatus();
+  private Status calculateStepsStatus() {
+    StatusCounter statusCounter = new StatusCounter();
+    for (Step step : steps) {
+      Result result = step.getResult();
+      statusCounter.incrementFor(result.getStatus());
+      duration += result.getDuration();
     }
+    return statusCounter.getFinalStatus();
+  }
 }
--- src/test/java/net/masterthought/cucumber/generators/integrations/TagsOverviewPageIntegrationTest.java
+++ src/test/java/net/masterthought/cucumber/generators/integrations/TagsOverviewPageIntegrationTest.java
@@ -1,195 +1,116 @@
 package net.masterthought.cucumber.generators.integrations;
-
 import static org.assertj.core.api.Assertions.assertThat;
-
 import net.masterthought.cucumber.generators.integrations.helpers.*;
 import org.junit.Test;
-
 import net.masterthought.cucumber.generators.TagsOverviewPage;
 
 /**
  * @author Damian Szczepanik (damianszczepanik@github)
  */
 public class TagsOverviewPageIntegrationTest extends PageTest {
+  @Test public void generatePage_generatesTitle() {
+    setUpWithJson(SAMPLE_JSON);
+    page = new TagsOverviewPage(reportResult, configuration);
+    final String titleValue = String.format("Cucumber Reports  - Tags Overview", configuration.getBuildNumber());
+    page.generatePage();
+    DocumentAssertion document = documentFrom(page.getWebPage());
+    String title = document.getHead().getTitle();
+    assertThat(title).isEqualTo(titleValue);
+  }
 
-    @Test
-    public void generatePage_generatesTitle() {
+  @Test public void generatePage_generatesLead() {
+    setUpWithJson(SAMPLE_JSON);
+    page = new TagsOverviewPage(reportResult, configuration);
+    page.generatePage();
+    DocumentAssertion document = documentFrom(page.getWebPage());
+    LeadAssertion lead = document.getLead();
+    assertThat(lead.getHeader()).isEqualTo("Tags Statistics");
+    assertThat(lead.getDescription()).isEqualTo("The following graph shows passing and failing statistics for tags");
+  }
 
-        // given
-        setUpWithJson(SAMPLE_JSON);
-        page = new TagsOverviewPage(reportResult, configuration);
-        final String titleValue = String.format("Cucumber Reports  - Tags Overview",
-                configuration.getBuildNumber());
+  @Test public void generatePage_generatesCharts() {
+    setUpWithJson(SAMPLE_JSON);
+    page = new TagsOverviewPage(reportResult, configuration);
+    page.generatePage();
+    DocumentAssertion document = documentFrom(page.getWebPage());
+    assertThat(document.byId("charts", WebAssertion.class)).isNotNull();
+  }
 
-        // when
-        page.generatePage();
+  @Test public void generatePage_insertsChartData() {
+    setUpWithJson(SAMPLE_JSON);
+    page = new TagsOverviewPage(reportResult, configuration);
+    page.generatePage();
+    DocumentAssertion document = documentFrom(page.getWebPage());
+    assertThat(document.html()).contains("labels:  [ \"@checkout\",  \"@fast\",  \"@featureTag\", ]");
+    assertThat(document.html()).contains("data:  [ 62.50,  100.00,  100.00, ]");
+    assertThat(document.html()).contains("data:  [ 6.25,  0.00,  0.00, ]");
+    assertThat(document.html()).contains("data:  [ 12.50,  0.00,  0.00, ]");
+    assertThat(document.html()).contains("data:  [ 6.25,  0.00,  0.00, ]");
+    assertThat(document.html()).contains("data:  [ 12.50,  0.00,  0.00, ]");
+  }
 
-        // then
-        DocumentAssertion document = documentFrom(page.getWebPage());
-        String title = document.getHead().getTitle();
+  @Test public void generatePage_generatesStatsTableHeader() {
+    setUpWithJson(SAMPLE_JSON);
+    page = new TagsOverviewPage(reportResult, configuration);
+    page.generatePage();
+    DocumentAssertion document = documentFrom(page.getWebPage());
+    TableRowAssertion[] headerRows = document.getReport().getTableStats().getHeaderRows();
+    assertThat(headerRows).hasSize(2);
+    TableRowAssertion firstRow = headerRows[0];
+    firstRow.hasExactValues("", "Steps", "Scenarios", "Features");
+    TableRowAssertion secondRow = headerRows[1];
+    secondRow.hasExactValues("Tag", "Passed", "Failed", "Skipped", "Pending", "Undefined", "Total", "Passed", "Failed", "Total", "Duration", "Status");
+  }
 
-        assertThat(title).isEqualTo(titleValue);
-    }
+  @Test public void generatePage_generatesStatsTableBody() {
+    setUpWithJson(SAMPLE_JSON);
+    page = new TagsOverviewPage(reportResult, configuration);
+    page.generatePage();
+    DocumentAssertion document = documentFrom(page.getWebPage());
+    TableRowAssertion[] bodyRows = document.getReport().getTableStats().getBodyRows();
+    assertThat(bodyRows).hasSize(3);
+    TableRowAssertion firstRow = bodyRows[0];
+    firstRow.hasExactValues("@checkout", "10", "1", "2", "1", "2", "16", "1", "1", "2", "231ms", "Failed");
+    firstRow.hasExactCSSClasses("tagname", "passed", "failed", "skipped", "pending", "undefined", "total", "passed", "failed", "total", "duration", "failed");
+    firstRow.hasExactDataValues("", "", "", "", "", "", "", "", "", "", "231054778", "");
+    firstRow.getReportLink().hasLabelAndAddress("@checkout", "report-tag_checkout.html");
+    CalloutAssertion[] secondRowCallouts = firstRow.getDropup().getCallouts();
+    assertThat(secondRowCallouts).hasSize(2);
+    secondRowCallouts[0].hasExactScenarioValue("Account may not have sufficient funds");
+    secondRowCallouts[0].getScenarioStepLink().hasLabelAndAddress("MachineFactory.wait()", "report-tag_checkout.html#0-hook-1500995314");
+    secondRowCallouts[0].hasExactErrorValue("Error message not found.");
+    secondRowCallouts[1].hasExactScenarioValue("Account may not have sufficient funds");
+    secondRowCallouts[1].getScenarioStepLink().hasLabelAndAddress("the card is valid", "report-tag_checkout.html#0-step-15");
+    secondRowCallouts[1].hasExactErrorValue("Error message not found.");
+    TableRowAssertion secondRow = bodyRows[1];
+    secondRow.hasExactValues("@fast", "6", "0", "0", "0", "0", "6", "1", "0", "1", "139ms", "Passed");
+    secondRow.hasExactCSSClasses("tagname", "passed", "", "", "", "", "total", "passed", "", "total", "duration", "passed");
+    secondRow.hasExactDataValues("", "", "", "", "", "", "", "", "", "", "139004778", "");
+    secondRow.getReportLink().hasLabelAndAddress("@fast", "report-tag_fast.html");
+    TableRowAssertion lastRow = bodyRows[2];
+    lastRow.hasExactValues("@featureTag", "6", "0", "0", "0", "0", "6", "1", "0", "1", "139ms", "Passed");
+    lastRow.hasExactCSSClasses("tagname", "passed", "", "", "", "", "total", "passed", "", "total", "duration", "passed");
+    lastRow.hasExactDataValues("", "", "", "", "", "", "", "", "", "", "139004778", "");
+    lastRow.getReportLink().hasLabelAndAddress("@featureTag", "report-tag_featureTag.html");
+  }
 
-    @Test
-    public void generatePage_generatesLead() {
+  @Test public void generatePage_generatesStatsTableFooter() {
+    setUpWithJson(SAMPLE_JSON);
+    page = new TagsOverviewPage(reportResult, configuration);
+    page.generatePage();
+    DocumentAssertion document = documentFrom(page.getWebPage());
+    TableRowAssertion[] footerRows = document.getReport().getTableStats().getAllFooterRows();
+    assertThat(footerRows).hasSize(2);
+    footerRows[0].hasExactValues("", "22", "1", "2", "1", "2", "28", "3", "1", "4", "509ms", "3");
+    footerRows[1].hasExactValues("", "78.57%", "3.57%", "7.14%", "3.57%", "7.14%", "", "75.00%", "25.00%", "", "", "50.00%");
+  }
 
-        // given
-        setUpWithJson(SAMPLE_JSON);
-        page = new TagsOverviewPage(reportResult, configuration);
-
-        // when
-        page.generatePage();
-
-        // then
-        DocumentAssertion document = documentFrom(page.getWebPage());
-        LeadAssertion lead = document.getLead();
-
-        assertThat(lead.getHeader()).isEqualTo("Tags Statistics");
-        assertThat(lead.getDescription()).isEqualTo("The following graph shows passing and failing statistics for tags");
-    }
-
-    @Test
-    public void generatePage_generatesCharts() {
-
-        // given
-        setUpWithJson(SAMPLE_JSON);
-        page = new TagsOverviewPage(reportResult, configuration);
-
-        // when
-        page.generatePage();
-
-        // then
-        DocumentAssertion document = documentFrom(page.getWebPage());
-
-        assertThat(document.byId("charts", WebAssertion.class)).isNotNull();
-    }
-
-    @Test
-    public void generatePage_insertsChartData() {
-
-        // given
-        setUpWithJson(SAMPLE_JSON);
-        page = new TagsOverviewPage(reportResult, configuration);
-
-        // when
-        page.generatePage();
-
-        // then
-        DocumentAssertion document = documentFrom(page.getWebPage());
-
-        // check that data used by the charts is correctly inserted into the script section
-
-        assertThat(document.html()).contains("labels:  [ \"@checkout\",  \"@fast\",  \"@featureTag\", ]");
-        assertThat(document.html()).contains("data:  [ 62.50,  100.00,  100.00, ]");
-        assertThat(document.html()).contains("data:  [ 6.25,  0.00,  0.00, ]");
-        assertThat(document.html()).contains("data:  [ 12.50,  0.00,  0.00, ]");
-        assertThat(document.html()).contains("data:  [ 6.25,  0.00,  0.00, ]");
-        assertThat(document.html()).contains("data:  [ 12.50,  0.00,  0.00, ]");
-
-    }
-
-    @Test
-    public void generatePage_generatesStatsTableHeader() {
-
-        // given
-        setUpWithJson(SAMPLE_JSON);
-        page = new TagsOverviewPage(reportResult, configuration);
-
-        // when
-        page.generatePage();
-
-        // then
-        DocumentAssertion document = documentFrom(page.getWebPage());
-        TableRowAssertion[] headerRows = document.getReport().getTableStats().getHeaderRows();
-
-        assertThat(headerRows).hasSize(2);
-
-        TableRowAssertion firstRow = headerRows[0];
-        firstRow.hasExactValues("", "Steps", "Scenarios", "Features");
-
-        TableRowAssertion secondRow = headerRows[1];
-        secondRow.hasExactValues("Tag", "Passed", "Failed", "Skipped", "Pending", "Undefined", "Total",
-                "Passed", "Failed", "Total", "Duration", "Status");
-    }
-
-    @Test
-    public void generatePage_generatesStatsTableBody() {
-
-        // given
-        setUpWithJson(SAMPLE_JSON);
-        page = new TagsOverviewPage(reportResult, configuration);
-
-        // when
-        page.generatePage();
-
-        // then
-        DocumentAssertion document = documentFrom(page.getWebPage());
-        TableRowAssertion[] bodyRows = document.getReport().getTableStats().getBodyRows();
-
-        assertThat(bodyRows).hasSize(3);
-
-        TableRowAssertion firstRow = bodyRows[0];
-        firstRow.hasExactValues("@checkout", "10", "1", "2", "1", "2", "16", "1", "1", "2", "231ms", "Failed");
-        firstRow.hasExactCSSClasses("tagname", "passed", "failed", "skipped", "pending", "undefined", "total", "passed",
-                "failed", "total", "duration", "failed");
-        firstRow.hasExactDataValues("", "", "", "", "", "", "", "", "", "", "231054778", "");
-        firstRow.getReportLink().hasLabelAndAddress("@checkout", "report-tag_checkout.html");
-
-        CalloutAssertion[] secondRowCallouts = firstRow.getDropup().getCallouts();
-        assertThat(secondRowCallouts).hasSize(2);
-        secondRowCallouts[0].hasExactScenarioValue("Account may not have sufficient funds");
-        secondRowCallouts[0].getScenarioStepLink().hasLabelAndAddress("MachineFactory.wait()", "report-tag_checkout.html#0-hook-1500995314");
-        secondRowCallouts[0].hasExactErrorValue("Error message not found.");
-        secondRowCallouts[1].hasExactScenarioValue("Account may not have sufficient funds");
-        secondRowCallouts[1].getScenarioStepLink().hasLabelAndAddress("the card is valid", "report-tag_checkout.html#0-step-15");
-        secondRowCallouts[1].hasExactErrorValue("Error message not found.");
-
-        TableRowAssertion secondRow = bodyRows[1];
-        secondRow.hasExactValues("@fast", "6", "0", "0", "0", "0", "6", "1", "0", "1", "139ms", "Passed");
-        secondRow.hasExactCSSClasses("tagname", "passed", "", "", "", "", "total", "passed", "", "total", "duration", "passed");
-        secondRow.hasExactDataValues("", "", "", "", "", "", "", "", "", "", "139004778", "");
-        secondRow.getReportLink().hasLabelAndAddress("@fast", "report-tag_fast.html");
-
-        TableRowAssertion lastRow = bodyRows[2];
-        lastRow.hasExactValues("@featureTag", "6", "0", "0", "0", "0", "6", "1", "0", "1", "139ms", "Passed");
-        lastRow.hasExactCSSClasses("tagname", "passed", "", "", "", "", "total", "passed", "", "total", "duration", "passed");
-        lastRow.hasExactDataValues("", "", "", "", "", "", "", "", "", "", "139004778", "");
-        lastRow.getReportLink().hasLabelAndAddress("@featureTag", "report-tag_featureTag.html");
-    }
-
-    @Test
-    public void generatePage_generatesStatsTableFooter() {
-
-        // given
-        setUpWithJson(SAMPLE_JSON);
-        page = new TagsOverviewPage(reportResult, configuration);
-
-        // when
-        page.generatePage();
-
-        // then
-        DocumentAssertion document = documentFrom(page.getWebPage());
-        TableRowAssertion[] footerRows = document.getReport().getTableStats().getAllFooterRows();
-
-        assertThat(footerRows).hasSize(2);
-        footerRows[0].hasExactValues("", "22", "1", "2", "1", "2", "28", "3", "1", "4", "509ms", "3");
-        footerRows[1].hasExactValues("", "78.57%", "3.57%", "7.14%", "3.57%", "7.14%", "", "75.00%", "25.00%", "", "", "50.00%");
-    }
-
-    @Test
-    public void generatePage_onJsonWithoutTags_generatesProperMessage() {
-
-        // given
-        setUpWithJson(SIMPLE_JSON);
-        page = new TagsOverviewPage(reportResult, configuration);
-
-        // when
-        page.generatePage();
-
-        // then
-        DocumentAssertion document = documentFrom(page.getWebPage());
-        SummaryAssertion summary = document.getReport();
-        assertThat(summary.getEmptyReportMessage()).isEqualTo("You have no tags in your cucumber report");
-    }
+  @Test public void generatePage_onJsonWithoutTags_generatesProperMessage() {
+    setUpWithJson(SIMPLE_JSON);
+    page = new TagsOverviewPage(reportResult, configuration);
+    page.generatePage();
+    DocumentAssertion document = documentFrom(page.getWebPage());
+    SummaryAssertion summary = document.getReport();
+    assertThat(summary.getEmptyReportMessage()).isEqualTo("You have no tags in your cucumber report");
+  }
 }
