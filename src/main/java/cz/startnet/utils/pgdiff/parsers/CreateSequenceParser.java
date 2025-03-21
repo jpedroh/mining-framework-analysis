1f6ccae857df978f91577f63b6fd684dcf743afb
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 08:19:54 +0000

    Mining Framework Analysis

--- src/main/java/cz/startnet/utils/pgdiff/parsers/CreateSequenceParser.java
+++ src/main/java/cz/startnet/utils/pgdiff/parsers/CreateSequenceParser.java
@@ -1,10 +1,4 @@
-/**
- * Copyright 2006 StartNet s.r.o.
- *
- * Distributed under MIT license
- */
 package cz.startnet.utils.pgdiff.parsers;
-
 import cz.startnet.utils.pgdiff.Resources;
 import cz.startnet.utils.pgdiff.schema.PgDatabase;
 import cz.startnet.utils.pgdiff.schema.PgSchema;
@@ -17,76 +11,91 @@ import java.text.MessageFormat;
  * @author fordfrog
  */
 public class CreateSequenceParser {
-
-    /**
+  /**
      * Parses CREATE SEQUENCE statement.
      *
      * @param database  database
      * @param statement CREATE SEQUENCE statement
      */
-    public static void parse(final PgDatabase database,
-            final String statement) {
-        final Parser parser = new Parser(statement);
-        parser.expect("CREATE", "SEQUENCE");
-
-        final String sequenceName = parser.parseIdentifier();
-        final PgSequence sequence =
-                new PgSequence(ParserUtils.getObjectName(sequenceName));
-        final String schemaName =
-                ParserUtils.getSchemaName(sequenceName, database);
-        final PgSchema schema = database.getSchema(schemaName);
-
-        if (schema == null) {
-            throw new RuntimeException(MessageFormat.format(
-                    Resources.getString("CannotFindSchema"), schemaName,
-                    statement));
-        }
-
-        schema.addSequence(sequence);
-
-        while (!parser.expectOptional(";")) {
-            if (parser.expectOptional("AS")) {
-                sequence.setDataType(parser.parseString());
-            } else if (parser.expectOptional("INCREMENT")) {
-                parser.expectOptional("BY");
-                sequence.setIncrement(parser.parseString());
-            } else if (parser.expectOptional("MINVALUE")) {
-                sequence.setMinValue(parser.parseString());
-            } else if (parser.expectOptional("MAXVALUE")) {
-                sequence.setMaxValue(parser.parseString());
-            } else if (parser.expectOptional("START")) {
+  public static void parse(final PgDatabase database, final String statement) {
+    final Parser parser = new Parser(statement);
+    parser.expect("CREATE", "SEQUENCE");
+    final String sequenceName = parser.parseIdentifier();
+    final PgSequence sequence = new PgSequence(ParserUtils.getObjectName(sequenceName));
+    final String schemaName = ParserUtils.getSchemaName(sequenceName, database);
+    final PgSchema schema = database.getSchema(schemaName);
+    if (schema == null) {
+      throw new RuntimeException(MessageFormat.format(Resources.getString("CannotFindSchema"), schemaName, statement));
+    }
+    schema.addSequence(sequence);
+    while (!parser.expectOptional(";")) {
+      if (parser.expectOptional("AS")) {
+        sequence.
+<<<<<<< /usr/src/app/output/fordfrog/apgdiff/e87c0a43d2a289df3bcc87b2530487bfc9953a26/src/main/java/cz/startnet/utils/pgdiff/parsers/CreateSequenceParser.java/left.java
+        setAs(parser.parseString())
+=======
+        setDataType(parser.parseString())
+>>>>>>> /usr/src/app/output/fordfrog/apgdiff/e87c0a43d2a289df3bcc87b2530487bfc9953a26/src/main/java/cz/startnet/utils/pgdiff/parsers/CreateSequenceParser.java/right.java
+        ;
+      } else {
+        if (parser.expectOptional("INCREMENT")) {
+          parser.expectOptional("BY");
+          sequence.setIncrement(parser.parseString());
+        } else {
+          if (parser.expectOptional("MINVALUE")) {
+            sequence.setMinValue(parser.parseString());
+          } else {
+            if (parser.expectOptional("MAXVALUE")) {
+              sequence.setMaxValue(parser.parseString());
+            } else {
+              if (parser.expectOptional("START")) {
                 parser.expectOptional("WITH");
                 sequence.setStartWith(parser.parseString());
-            } else if (parser.expectOptional("CACHE")) {
-                sequence.setCache(parser.parseString());
-            } else if (parser.expectOptional("CYCLE")) {
-                sequence.setCycle(true);
-            } else if (parser.expectOptional("OWNED", "BY")) {
-                if (parser.expectOptional("NONE")) {
-                    sequence.setOwnedBy(null);
+              } else {
+                if (parser.expectOptional("CACHE")) {
+                  sequence.setCache(parser.parseString());
                 } else {
-                    sequence.setOwnedBy(ParserUtils.getObjectName(
-                            parser.parseIdentifier()));
+                  if (parser.expectOptional("CYCLE")) {
+                    sequence.setCycle(true);
+                  } else {
+                    if (parser.expectOptional("OWNED", "BY")) {
+                      if (parser.expectOptional("NONE")) {
+                        sequence.setOwnedBy(null);
+                      } else {
+                        sequence.setOwnedBy(ParserUtils.getObjectName(parser.parseIdentifier()));
+                      }
+                    } else {
+                      if (parser.expectOptional("NO")) {
+                        if (parser.expectOptional("MINVALUE")) {
+                          sequence.setMinValue(null);
+                        } else {
+                          if (parser.expectOptional("MAXVALUE")) {
+                            sequence.setMaxValue(null);
+                          } else {
+                            if (parser.expectOptional("CYCLE")) {
+                              sequence.setCycle(false);
+                            } else {
+                              parser.throwUnsupportedCommand();
+                            }
+                          }
+                        }
+                      } else {
+                        parser.throwUnsupportedCommand();
+                      }
+                    }
+                  }
                 }
-            } else if (parser.expectOptional("NO")) {
-                if (parser.expectOptional("MINVALUE")) {
-                    sequence.setMinValue(null);
-                } else if (parser.expectOptional("MAXVALUE")) {
-                    sequence.setMaxValue(null);
-                } else if (parser.expectOptional("CYCLE")) {
-                    sequence.setCycle(false);
-                } else {
-                    parser.throwUnsupportedCommand();
-                }
-            } else {
-                parser.throwUnsupportedCommand();
+              }
             }
+          }
         }
+      }
     }
+  }
 
-    /**
+  /**
      * Creates a new instance of CreateSequenceParser.
      */
-    private CreateSequenceParser() {
-    }
+  private CreateSequenceParser() {
+  }
 }
--- src/main/java/cz/startnet/utils/pgdiff/schema/PgSequence.java
+++ src/main/java/cz/startnet/utils/pgdiff/schema/PgSequence.java
@@ -1,14 +1,7 @@
-/**
- * Copyright 2006 StartNet s.r.o.
- *
- * Distributed under MIT license
- */
 package cz.startnet.utils.pgdiff.schema;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
 import cz.startnet.utils.pgdiff.PgDiffUtils;
 
 /**
@@ -17,393 +10,390 @@ import cz.startnet.utils.pgdiff.PgDiffUtils;
  * @author fordfrog
  */
 public class PgSequence {
-
-    /**
+  /**
      * Value for AS or null if no value is specified.
      */
-    private String as;
-    /**
+  private String 
+<<<<<<< /usr/src/app/output/fordfrog/apgdiff/e87c0a43d2a289df3bcc87b2530487bfc9953a26/src/main/java/cz/startnet/utils/pgdiff/schema/PgSequence.java/left.java
+  as
+=======
+  dataType
+>>>>>>> /usr/src/app/output/fordfrog/apgdiff/e87c0a43d2a289df3bcc87b2530487bfc9953a26/src/main/java/cz/startnet/utils/pgdiff/schema/PgSequence.java/right.java
+  ;
+
+  /**
      * Value for CACHE or null if no value is specified.
      */
-    private String cache;
-    /**
+  private String cache;
+
+  /**
      * Value for INCREMENT BY or null if no value is specified.
      */
-    private String increment;
-    /**
+  private String increment;
+
+  /**
      * Value for MAXVALUE or null if no value is specified.
      */
-    private String maxValue;
-    /**
+  private String maxValue;
+
+  /**
      * Value for MINVALUE or null if no value is specified.
      */
-    private String minValue;
-    /**
+  private String minValue;
+
+  /**
      * Name of the sequence.
      */
-    private String name;
-    /**
+  private String name;
+
+  /**
      * Value for START WITH or null if no value is specified.
      */
-    private String startWith;
-    /**
+  private String startWith;
+
+  /**
      * True if CYCLE, false if NO CYCLE.
      */
-    private boolean cycle;
-    /**
+  private boolean cycle;
+
+  /**
      * Column the sequence is owned by.
      */
-    private String ownedBy;
-    /**
+  private String ownedBy;
+
+  /**
      * Comment.
      */
-    private String comment;
-    /**
+  private String comment;
+
+  /**
      * List of privileges defined on the sequence.
      */
-    @SuppressWarnings("CollectionWithoutInitialCapacity")
-    private final List<PgSequencePrivilege> privileges = new ArrayList<PgSequencePrivilege>();
-    /**
-     *  Value for AS or null if no value is specified.
-     */
-    private String dataType;
+  @SuppressWarnings(value = { "CollectionWithoutInitialCapacity" }) private final List<PgSequencePrivilege> privileges = new ArrayList<PgSequencePrivilege>();
 
-    /**
+  /**
      * Creates a new PgSequence object.
      *
      * @param name name of the sequence
      */
-    public PgSequence(final String name) {
-        this.name = name;
-    }
+  public PgSequence(final String name) {
+    this.name = name;
+  }
 
-    /**
+  /**
      * Setter for {@link #as}.
      *
      * @param as {@link #as}
      */
-    public void setAs(final String as) {
-        this.as = as;
-    }
+  public void setAs(final String as) {
+    this.as = as;
+  }
 
-    /**
+  /**
      * Getter for {@link #as}.
      *
      * @return {@link #as}
      */
-    public String getAs() {
-        return as;
-    }
+  public String getAs() {
+    return as;
+  }
 
-    /**
+  /**
      * Setter for {@link #cache}.
      *
      * @param cache {@link #cache}
      */
-    public void setCache(final String cache) {
-        this.cache = cache;
-    }
+  public void setCache(final String cache) {
+    this.cache = cache;
+  }
 
-    /**
+  /**
      * Getter for {@link #cache}.
      *
      * @return {@link #cache}
      */
-    public String getCache() {
-        return cache;
-    }
+  public String getCache() {
+    return cache;
+  }
 
-    /**
+  /**
      * Getter for {@link #comment}.
      *
      * @return {@link #comment}
      */
-    public String getComment() {
-        return comment;
-    }
+  public String getComment() {
+    return comment;
+  }
 
-    /**
+  /**
      * Setter for {@link #comment}.
      *
      * @param comment {@link #comment}
      */
-    public void setComment(final String comment) {
-        this.comment = comment;
-    }
+  public void setComment(final String comment) {
+    this.comment = comment;
+  }
 
-    /**
+  /**
      * Creates and returns SQL statement for creation of the sequence.
      *
      * @return created SQL statement
      */
-    public String getCreationSQL() {
-        final StringBuilder sbSQL = new StringBuilder(100);
-        sbSQL.append("CREATE SEQUENCE ");
-        
-        sbSQL.append(PgDiffUtils.getCreateIfNotExists());
-        
-        sbSQL.append(PgDiffUtils.getQuotedName(name));
-        
-        if(dataType!=null){
-            sbSQL.append(System.getProperty("line.separator"));
-            sbSQL.append("\tAS ");
-            sbSQL.append(dataType); 
-
-        }
-
-        if (as != null) {
-            sbSQL.append(System.getProperty("line.separator"));
-            sbSQL.append("\tAS ");
-            sbSQL.append(as);
-        }
-
-        if (startWith != null) {
-            sbSQL.append(System.getProperty("line.separator"));
-            sbSQL.append("\tSTART WITH ");
-            sbSQL.append(startWith);
-        }
-
-        if (increment != null) {
-            sbSQL.append(System.getProperty("line.separator"));
-            sbSQL.append("\tINCREMENT BY ");
-            sbSQL.append(increment);
-        }
-
-        sbSQL.append(System.getProperty("line.separator"));
-        sbSQL.append("\t");
-
-        if (maxValue == null) {
-            sbSQL.append("NO MAXVALUE");
-        } else {
-            sbSQL.append("MAXVALUE ");
-            sbSQL.append(maxValue);
-        }
-
-        sbSQL.append(System.getProperty("line.separator"));
-        sbSQL.append("\t");
-
-        if (minValue == null) {
-            sbSQL.append("NO MINVALUE");
-        } else {
-            sbSQL.append("MINVALUE ");
-            sbSQL.append(minValue);
-        }
-
-        if (cache != null) {
-            sbSQL.append(System.getProperty("line.separator"));
-            sbSQL.append("\tCACHE ");
-            sbSQL.append(cache);
-        }
-
-        if (cycle) {
-            sbSQL.append(System.getProperty("line.separator"));
-            sbSQL.append("\tCYCLE");
-        }
-
-        sbSQL.append(';');
-
-        if (comment != null && !comment.isEmpty()) {
-            sbSQL.append(System.getProperty("line.separator"));
-            sbSQL.append(System.getProperty("line.separator"));
-            sbSQL.append("COMMENT ON SEQUENCE ");
-            sbSQL.append(PgDiffUtils.getQuotedName(name));
-            sbSQL.append(" IS ");
-            sbSQL.append(comment);
-            sbSQL.append(';');
-        }
-
-        return sbSQL.toString();
+  public String getCreationSQL() {
+    final StringBuilder sbSQL = new StringBuilder(100);
+    sbSQL.append("CREATE SEQUENCE ");
+    sbSQL.append(PgDiffUtils.getCreateIfNotExists());
+    sbSQL.append(PgDiffUtils.getQuotedName(name));
+    if (
+<<<<<<< /usr/src/app/output/fordfrog/apgdiff/e87c0a43d2a289df3bcc87b2530487bfc9953a26/src/main/java/cz/startnet/utils/pgdiff/schema/PgSequence.java/left.java
+    as
+=======
+    dataType
+>>>>>>> /usr/src/app/output/fordfrog/apgdiff/e87c0a43d2a289df3bcc87b2530487bfc9953a26/src/main/java/cz/startnet/utils/pgdiff/schema/PgSequence.java/right.java
+     != null) {
+      sbSQL.append(System.getProperty("line.separator"));
+      sbSQL.append("\tAS ");
+      sbSQL.append(
+<<<<<<< /usr/src/app/output/fordfrog/apgdiff/e87c0a43d2a289df3bcc87b2530487bfc9953a26/src/main/java/cz/startnet/utils/pgdiff/schema/PgSequence.java/left.java
+      as
+=======
+      dataType
+>>>>>>> /usr/src/app/output/fordfrog/apgdiff/e87c0a43d2a289df3bcc87b2530487bfc9953a26/src/main/java/cz/startnet/utils/pgdiff/schema/PgSequence.java/right.java
+      );
     }
+    if (startWith != null) {
+      sbSQL.append(System.getProperty("line.separator"));
+      sbSQL.append("\tSTART WITH ");
+      sbSQL.append(startWith);
+    }
+    if (increment != null) {
+      sbSQL.append(System.getProperty("line.separator"));
+      sbSQL.append("\tINCREMENT BY ");
+      sbSQL.append(increment);
+    }
+    sbSQL.append(System.getProperty("line.separator"));
+    sbSQL.append("\t");
+    if (maxValue == null) {
+      sbSQL.append("NO MAXVALUE");
+    } else {
+      sbSQL.append("MAXVALUE ");
+      sbSQL.append(maxValue);
+    }
+    sbSQL.append(System.getProperty("line.separator"));
+    sbSQL.append("\t");
+    if (minValue == null) {
+      sbSQL.append("NO MINVALUE");
+    } else {
+      sbSQL.append("MINVALUE ");
+      sbSQL.append(minValue);
+    }
+    if (cache != null) {
+      sbSQL.append(System.getProperty("line.separator"));
+      sbSQL.append("\tCACHE ");
+      sbSQL.append(cache);
+    }
+    if (cycle) {
+      sbSQL.append(System.getProperty("line.separator"));
+      sbSQL.append("\tCYCLE");
+    }
+    sbSQL.append(';');
+    if (comment != null && !comment.isEmpty()) {
+      sbSQL.append(System.getProperty("line.separator"));
+      sbSQL.append(System.getProperty("line.separator"));
+      sbSQL.append("COMMENT ON SEQUENCE ");
+      sbSQL.append(PgDiffUtils.getQuotedName(name));
+      sbSQL.append(" IS ");
+      sbSQL.append(comment);
+      sbSQL.append(';');
+    }
+    return sbSQL.toString();
+  }
 
-    /**
+  /**
      * Creates and returns SQL statement for modification "OWNED BY" parameter.
      *
      * @return created SQL statement
      */
-    public String getOwnedBySQL() {
-        final StringBuilder sbSQL = new StringBuilder(100);
-
-        sbSQL.append("ALTER SEQUENCE ");
-        sbSQL.append(PgDiffUtils.getQuotedName(name));
-
-        if (ownedBy != null && !ownedBy.isEmpty()) {
-            sbSQL.append(System.getProperty("line.separator"));
-            sbSQL.append("\tOWNED BY ");
-            sbSQL.append(ownedBy);
-        }
-
-        sbSQL.append(';');
-
-        return sbSQL.toString();
+  public String getOwnedBySQL() {
+    final StringBuilder sbSQL = new StringBuilder(100);
+    sbSQL.append("ALTER SEQUENCE ");
+    sbSQL.append(PgDiffUtils.getQuotedName(name));
+    if (ownedBy != null && !ownedBy.isEmpty()) {
+      sbSQL.append(System.getProperty("line.separator"));
+      sbSQL.append("\tOWNED BY ");
+      sbSQL.append(ownedBy);
     }
+    sbSQL.append(';');
+    return sbSQL.toString();
+  }
 
-    /**
+  /**
      * Setter for {@link #cycle}.
      *
      * @param cycle {@link #cycle}
      */
-    public void setCycle(final boolean cycle) {
-        this.cycle = cycle;
-    }
+  public void setCycle(final boolean cycle) {
+    this.cycle = cycle;
+  }
 
-    /**
+  /**
      * Getter for {@link #cycle}.
      *
      * @return {@link #cycle}
      */
-    public boolean isCycle() {
-        return cycle;
-    }
+  public boolean isCycle() {
+    return cycle;
+  }
 
-    /**
+  /**
      * Creates and returns SQL statement for dropping the sequence.
      *
      * @return created SQL
      */
-    public String getDropSQL() {
-        return "DROP SEQUENCE " + PgDiffUtils.getDropIfExists() + PgDiffUtils.getQuotedName(getName()) + ";";
-    }
+  public String getDropSQL() {
+    return "DROP SEQUENCE " + PgDiffUtils.getDropIfExists() + PgDiffUtils.getQuotedName(getName()) + ";";
+  }
 
-    /**
+  /**
      * Setter for {@link #increment}.
      *
      * @param increment {@link #increment}
      */
-    public void setIncrement(final String increment) {
-        this.increment = increment;
-    }
+  public void setIncrement(final String increment) {
+    this.increment = increment;
+  }
 
-    /**
+  /**
      * Getter for {@link #increment}.
      *
      * @return {@link #increment}
      */
-    public String getIncrement() {
-        return increment;
-    }
+  public String getIncrement() {
+    return increment;
+  }
 
-    /**
+  /**
      * Setter for {@link #maxValue}.
      *
      * @param maxValue {@link #maxValue}
      */
-    public void setMaxValue(final String maxValue) {
-        this.maxValue = maxValue;
-    }
+  public void setMaxValue(final String maxValue) {
+    this.maxValue = maxValue;
+  }
 
-    /**
+  /**
      * Getter for {@link #maxValue}.
      *
      * @return {@link #maxValue}
      */
-    public String getMaxValue() {
-        return maxValue;
-    }
+  public String getMaxValue() {
+    return maxValue;
+  }
 
-    /**
+  /**
      * Setter for {@link #minValue}.
      *
      * @param minValue {@link #minValue}
      */
-    public void setMinValue(final String minValue) {
-        this.minValue = minValue;
-    }
+  public void setMinValue(final String minValue) {
+    this.minValue = minValue;
+  }
 
-    /**
+  /**
      * Getter for {@link #minValue}.
      *
      * @return {@link #minValue}
      */
-    public String getMinValue() {
-        return minValue;
-    }
+  public String getMinValue() {
+    return minValue;
+  }
 
-    /**
+  /**
      * Setter for {@link #name}.
      *
      * @param name {@link #name}
      */
-    public void setName(final String name) {
-        this.name = name;
-    }
+  public void setName(final String name) {
+    this.name = name;
+  }
 
-    /**
+  /**
      * Getter for {@link #name}.
      *
      * @return {@link #name}
      */
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    /**
+  /**
      * Setter for {@link #startWith}.
      *
      * @param startWith {@link #startWith}
      */
-    public void setStartWith(final String startWith) {
-        this.startWith = startWith;
-    }
+  public void setStartWith(final String startWith) {
+    this.startWith = startWith;
+  }
 
-    /**
+  /**
      * Getter for {@link #startWith}.
      *
      * @return {@link #startWith}
      */
-    public String getStartWith() {
-        return startWith;
-    }
+  public String getStartWith() {
+    return startWith;
+  }
 
-    /**
+  /**
      * Getter for {@link #ownedBy}.
      *
      * @return {@link #ownedBy}
      */
-    public String getOwnedBy() {
-        return ownedBy;
-    }
+  public String getOwnedBy() {
+    return ownedBy;
+  }
 
-    /**
+  /**
      * Setter for {@link #ownedBy}.
      *
      * @param ownedBy {@link #ownedBy}
      */
-    public void setOwnedBy(final String ownedBy) {
-        this.ownedBy = ownedBy;
-    }
+  public void setOwnedBy(final String ownedBy) {
+    this.ownedBy = ownedBy;
+  }
 
-    public List<PgSequencePrivilege> getPrivileges() {
-        return Collections.unmodifiableList(privileges);
-    }
+  public List<PgSequencePrivilege> getPrivileges() {
+    return Collections.unmodifiableList(privileges);
+  }
 
-    public PgSequencePrivilege getPrivilege(final String roleName) {
-        for (PgSequencePrivilege privilege : privileges) {
-            if (privilege.getRoleName().equals(roleName)) {
-                return privilege;
-            }
-        }
-        return null;
+  public PgSequencePrivilege getPrivilege(final String roleName) {
+    for (PgSequencePrivilege privilege : privileges) {
+      if (privilege.getRoleName().equals(roleName)) {
+        return privilege;
+      }
     }
+    return null;
+  }
 
-    public void addPrivilege(final PgSequencePrivilege privilege) {
-        privileges.add(privilege);
-    }
-    
-     /**
+  public void addPrivilege(final PgSequencePrivilege privilege) {
+    privileges.add(privilege);
+  }
+
+  /**
      * Getter for {@link #dataType}.
      *
      * @return {@link #dataType}
      */
-    public String getDataType() {
-        return dataType;
-    }
+  public String getDataType() {
+    return dataType;
+  }
 
-     /**
+  /**
      * Setter for {@link #ownedBy}.
      *
      * @param dataType {@link #dataType}
      */
-    public void setDataType(final String dataType) {
-        this.dataType = dataType;
-    }
+  public void setDataType(final String dataType) {
+    this.dataType = dataType;
+  }
 }
