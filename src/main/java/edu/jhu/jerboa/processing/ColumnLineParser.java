f582856b8a74f4105c3ccea164dc9dd31b02077b
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 23:52:23 +0000

    Mining Framework Analysis

--- src/main/java/edu/jhu/jerboa/processing/ColumnLineParser.java
+++ src/main/java/edu/jhu/jerboa/processing/ColumnLineParser.java
@@ -1,16 +1,8 @@
-// Copyright 2010-2012 Benjamin Van Durme. All rights reserved.
-// This software is released under the 2-clause BSD license.
-// See jerboa/LICENSE, or http://cs.jhu.edu/~vandurme/jerboa/LICENSE
-
-// Benjamin Van Durme, vandurme@cs.jhu.edu, 28 Oct 2010
-
 package edu.jhu.jerboa.processing;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.Hashtable;
 import java.util.Vector;
-
 import edu.jhu.jerboa.util.JerboaProperties;
 
 /**
@@ -34,86 +26,91 @@ import edu.jhu.jerboa.util.JerboaProperties;
 */
 public class ColumnLineParser implements ILineParser {
   private static String[] stringArr = new String[0];
+
   private String separator;
+
   private int[] fields;
+
   private String conjoinFields;
+
   private boolean lowercase;
+
   TokenizationKind tokenization;
 
-  public ColumnLineParser () throws Exception {
-    separator = JerboaProperties.getProperty("ColumnLineParser.separator",null);
-    lowercase = JerboaProperties.getBoolean("ColumnLineParser.lowercase",false);
-    String[] fieldStrings = JerboaProperties.getStrings("ColumnLineParser.fields",null);
+  public ColumnLineParser() throws Exception {
+    separator = JerboaProperties.getProperty("ColumnLineParser.separator", null);
+    lowercase = JerboaProperties.getBoolean("ColumnLineParser.lowercase", false);
+    String[] fieldStrings = JerboaProperties.getStrings("ColumnLineParser.fields", null);
     if (fieldStrings != null) {
-	    fields = new int[fieldStrings.length];
-	    for (int i = 0; i < fieldStrings.length; i++)
+      fields = new int[fieldStrings.length];
+      for (int i = 0; i < fieldStrings.length; i++) {
         fields[i] = Integer.parseInt(fieldStrings[i]);
+      }
     } else {
-	    fields = null;
+      fields = null;
     }
-    conjoinFields = JerboaProperties.getProperty("ColumnLineParser.conjoinFields",null);
+    conjoinFields = JerboaProperties.getProperty("ColumnLineParser.conjoinFields", null);
     String type = JerboaProperties.getProperty("ColumnLineParser.tokenizationKind", null);
-    if (type != null)
-	    tokenization = TokenizationKind.valueOf(type);
-    else
-	    tokenization = null;
+    if (type != null) {
+      tokenization = TokenizationKind.valueOf(type);
+    } else {
+      tokenization = null;
+    }
   }
 
   /**
      Result contains:
      "content" : String[] of tokens
   */
-  public Hashtable<String,Object> parseLine(BufferedReader reader) throws IOException {
+  public Hashtable<String, Object> parseLine(BufferedReader reader) throws IOException {
     Vector<String> content = new Vector<String>();
     String line;
     String[] tokens = null;
     String[] fieldTokens = null;
-
     if ((line = reader.readLine()) != null) {
       if (lowercase) {
         line = line.toLowerCase();
       }
-
-	    if (separator != null) {
+      if (separator != null) {
         fieldTokens = line.split(separator);
-	    } else {
+      } else {
         fieldTokens = new String[1];
         fieldTokens[0] = line;
-	    }
-
-	    if (fields != null) {
+      }
+      if (fields != null) {
         for (int i = 0; i < fields.length; i++) {
           if (tokenization != null) {
-            tokens = Tokenizer.tokenize(fieldTokens[fields[i]],
-                                        tokenization);
-            for (int j = 0; j < tokens.length; j++)
+            tokens = Tokenizer.tokenize(fieldTokens[fields[i]], tokenization);
+            for (int j = 0; j < tokens.length; j++) {
               content.addElement(tokens[j]);
-          } else
+            }
+          } else {
             content.addElement(fieldTokens[fields[i]]);
+          }
         }
-	    } else {
-        for (int i = 0; i < fieldTokens.length; i++)
+      } else {
+        for (int i = 0; i < fieldTokens.length; i++) {
           if (tokenization != null) {
-            tokens = Tokenizer.tokenize(fieldTokens[fields[i]],
-                                        tokenization);
-            for (int j = 0; j < tokens.length; j++)
+            tokens = Tokenizer.tokenize(fieldTokens[fields[i]], tokenization);
+            for (int j = 0; j < tokens.length; j++) {
               content.addElement(tokens[j]);
+            }
           } else {
             content.addElement(fieldTokens[i]);
           }
-	    }
-						
-	    if (conjoinFields != null) {
+        }
+      }
+      if (conjoinFields != null) {
         StringBuilder sb = new StringBuilder();
-	    sb.append(content.elementAt(0));
-        for (int i = 1; i < content.size(); i++)
+        sb.append(content.elementAt(0));
+        for (int i = 1; i < content.size(); i++) {
           sb.append(conjoinFields + content.elementAt(i));
+        }
         content.clear();
         content.addElement(sb.toString());
-	    }
+      }
     }
-		
-    Hashtable<String,Object> h = new Hashtable<String, Object>();
+    Hashtable<String, Object> h = new Hashtable<String, Object>();
     h.put("content", (String[]) content.toArray(stringArr));
     return h;
   }
