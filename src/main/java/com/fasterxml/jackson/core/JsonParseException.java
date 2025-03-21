538dcf459b08b86f984ed968333cc98f8255078f
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 17:05:07 +0000

    Mining Framework Analysis

--- src/main/java/com/fasterxml/jackson/core/JsonParseException.java
+++ src/main/java/com/fasterxml/jackson/core/JsonParseException.java
@@ -1,10 +1,4 @@
-/* Jackson JSON-processor.
- *
- * Copyright (c) 2007- Tatu Saloranta, tatu.saloranta@iki.fi
- */
-
 package com.fasterxml.jackson.core;
-
 import com.fasterxml.jackson.core.util.RequestPayload;
 
 /**
@@ -13,63 +7,57 @@ import com.fasterxml.jackson.core.util.RequestPayload;
  * is encountered.
  */
 public class JsonParseException extends JsonProcessingException {
-    private static final long serialVersionUID = 2L; // 2.7
+  private static final long serialVersionUID = 2L;
 
-    // transient since 2.7.4
-    protected transient JsonParser _processor;
+  protected transient JsonParser _processor;
 
-    /**
-     * @since 2.8
-     */
-    protected RequestPayload _requestPayload;
+  protected RequestPayload _requestPayload;
 
-    @Deprecated // since 2.7
-    public JsonParseException(String msg, JsonLocation loc) {
-        super(msg, loc);
-    }
+  @Deprecated public JsonParseException(String msg, JsonLocation loc) {
+    super(msg, loc);
+  }
 
-    @Deprecated // since 2.7
-    public JsonParseException(String msg, JsonLocation loc, Throwable root) {
-        super(msg, loc, root);
-    }
+  @Deprecated public JsonParseException(String msg, JsonLocation loc, Throwable root) {
+    super(msg, loc, root);
+  }
 
-    /**
+  /**
      * Constructor that uses current parsing location as location, and
      * sets processor (accessible via {@link #getProcessor()}) to
      * specified parser.
      *
      * @since 2.7
      */
-    public JsonParseException(JsonParser p, String msg) {
-        super(msg, (p == null) ? null : p.getCurrentLocation());
-        _processor = p;
-    }
+  public JsonParseException(JsonParser p, String msg) {
+    super(msg, (p == null) ? null : p.getCurrentLocation());
+    _processor = p;
+  }
 
-    /**
+  /**
      * @since 2.7
      */
-    public JsonParseException(JsonParser p, String msg, Throwable root) {
-        super(msg, (p == null) ? null : p.getCurrentLocation(), root);
-        _processor = p;
-    }
-    
-    /**
+  public JsonParseException(JsonParser p, String msg, Throwable root) {
+    super(msg, (p == null) ? null : p.getCurrentLocation(), root);
+    _processor = p;
+  }
+
+  /**
      * @since 2.7
      */
-    public JsonParseException(JsonParser p, String msg, JsonLocation loc) {
-        super(msg, loc);
-        _processor = p;
-    }
+  public JsonParseException(JsonParser p, String msg, JsonLocation loc) {
+    super(msg, loc);
+    _processor = p;
+  }
 
-    /**
+  /**
      * @since 2.7
      */
-    public JsonParseException(JsonParser p, String msg, JsonLocation loc, Throwable root) {
-        super(msg, loc, root);
-        _processor = p;
-    }
+  public JsonParseException(JsonParser p, String msg, JsonLocation loc, Throwable root) {
+    super(msg, loc, root);
+    _processor = p;
+  }
 
-    /**
+  /**
      * Fluent method that may be used to assign originating {@link JsonParser},
      * to be accessed using {@link #getProcessor()}.
      *<p>
@@ -77,12 +65,12 @@ public class JsonParseException extends JsonProcessingException {
      *
      * @since 2.7
      */
-    public JsonParseException withParser(JsonParser p) {
-        _processor = p;
-        return this;
-    }
+  public JsonParseException withParser(JsonParser p) {
+    _processor = p;
+    return this;
+  }
 
-    /**
+  /**
      * Fluent method that may be used to assign payload to this exception,
      * to let recipient access it for diagnostics purposes.
      *<p>
@@ -90,17 +78,16 @@ public class JsonParseException extends JsonProcessingException {
      *
      * @since 2.8
      */
-    public JsonParseException withRequestPayload(RequestPayload p) {
-        _requestPayload = p;
-        return this;
-    }
-    
-    @Override
-    public JsonParser getProcessor() {
-        return _processor;
-    }
+  public JsonParseException withRequestPayload(RequestPayload p) {
+    _requestPayload = p;
+    return this;
+  }
 
-    /**
+  @Override public JsonParser getProcessor() {
+    return _processor;
+  }
+
+  /**
      * Method that may be called to find payload that was being parsed, if
      * one was specified for parser that threw this Exception.
      *
@@ -108,11 +95,11 @@ public class JsonParseException extends JsonProcessingException {
      *
      * @since 2.8
      */
-    public RequestPayload getRequestPayload() {
-        return _requestPayload;
-    }
+  public RequestPayload getRequestPayload() {
+    return _requestPayload;
+  }
 
-    /**
+  /**
      * The method returns the String representation of the request payload if
      * one was specified for parser that threw this Exception.
      * 
@@ -120,19 +107,18 @@ public class JsonParseException extends JsonProcessingException {
      * 
      * @since 2.8
      */
-    public String getRequestPayloadAsString() {
-        return (_requestPayload != null) ? _requestPayload.toString() : null;
-    }
-    
-    /**
+  public String getRequestPayloadAsString() {
+    return (_requestPayload != null) ? _requestPayload.toString() : null;
+  }
+
+  /**
      * Overriding the getMessage() to include the request body
      */
-    @Override 
-    public String getMessage() {
-        String msg = super.getMessage();
-        if (_requestPayload != null) {
-            msg +=  "\nRequest payload : " + _requestPayload.toString();
-        }
-        return msg;
+  @Override public String getMessage() {
+    String msg = super.getMessage();
+    if (_requestPayload != null) {
+      msg += "\nRequest payload : " + _requestPayload.toString();
     }
+    return msg;
+  }
 }
