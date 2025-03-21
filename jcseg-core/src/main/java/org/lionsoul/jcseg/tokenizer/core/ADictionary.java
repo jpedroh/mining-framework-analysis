f8eea18e2cd029a20b76dda7171662b6bff2cdac
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 14:54:25 +0000

    Mining Framework Analysis

--- jcseg-core/src/main/java/org/lionsoul/jcseg/tokenizer/core/ADictionary.java
+++ jcseg-core/src/main/java/org/lionsoul/jcseg/tokenizer/core/ADictionary.java
@@ -1,16 +1,6 @@
 package org.lionsoul.jcseg.tokenizer.core;
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.FilenameFilter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.Serializable;
+import java.io.*;
 import java.security.CodeSource;
 import java.util.ArrayList;
 import java.util.Collections;
