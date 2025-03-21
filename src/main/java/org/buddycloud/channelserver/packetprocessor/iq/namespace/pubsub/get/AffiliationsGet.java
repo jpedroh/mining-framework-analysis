78abb11dd918c1221d1f32176a20779351b03d13
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 08:16:15 +0000

    Mining Framework Analysis

--- src/main/java/org/buddycloud/channelserver/packetprocessor/iq/namespace/pubsub/get/AffiliationsGet.java
+++ src/main/java/org/buddycloud/channelserver/packetprocessor/iq/namespace/pubsub/get/AffiliationsGet.java
@@ -1,152 +1,25 @@
-package org.buddycloud.channelserver.packetprocessor.iq.namespace.pubsub.get;
-
-import java.util.concurrent.BlockingQueue;
-
-import org.apache.log4j.Logger;
-import org.buddycloud.channelserver.Configuration;
-import org.buddycloud.channelserver.channel.ChannelManager;
-import org.buddycloud.channelserver.db.exception.NodeStoreException;
-import org.buddycloud.channelserver.packetprocessor.iq.namespace.pubsub.JabberPubsub;
-import org.buddycloud.channelserver.packetprocessor.iq.namespace.pubsub.PubSubElementProcessorAbstract;
-import org.buddycloud.channelserver.pubsub.model.NodeMembership;
-import org.buddycloud.channelserver.utils.XMLConstants;
-import org.buddycloud.channelserver.utils.node.item.payload.Buddycloud;
-import org.dom4j.Element;
-import org.xmpp.packet.IQ;
-import org.xmpp.packet.JID;
-import org.xmpp.packet.Packet;
-import org.xmpp.resultsetmanagement.ResultSet;
-
-public class AffiliationsGet extends PubSubElementProcessorAbstract {
-
-    private final BlockingQueue<Packet> outQueue;
-    private final ChannelManager channelManager;
-
-    private IQ requestIq;
-    private String node;
-    private JID actorJid;
-    private IQ result;
-    private String firstItem;
-
-    private static final Logger LOGGER = Logger.getLogger(AffiliationsGet.class);
-
-
-    public AffiliationsGet(BlockingQueue<Packet> outQueue, ChannelManager channelManager) {
-        this.outQueue = outQueue;
-        this.channelManager = channelManager;
-    }
-
-    @Override
-    public void process(Element elm, JID actorJID, IQ reqIQ, Element rsm) throws Exception {
-        result = IQ.createResultIQ(reqIQ);
-        requestIq = reqIQ;
-        actorJid = actorJID;
-
-        node = elm.attributeValue(XMLConstants.NODE_ATTR);
-        if (!Configuration.getInstance().isLocalJID(requestIq.getFrom())) {
-            result.getElement().addAttribute(XMLConstants.REMOTE_SERVER_DISCOVER_ATTR, Boolean.FALSE.toString());
-
-        }
-        String namespace = JabberPubsub.NS_PUBSUB_OWNER;
-        if (node == null) {
-            namespace = JabberPubsub.NAMESPACE_URI;
-        }
-
-
-        Element pubsub = result.setChildElement(XMLConstants.PUBSUB_ELEM, namespace);
-        Element affiliations = pubsub.addElement(XMLConstants.AFFILIATION_ELEM);
-
-        if (actorJid == null) {
-            actorJid = requestIq.getFrom();
-        }
-
-        boolean isProcessedLocally = true;
-        if (node == null) {
-            isProcessedLocally = getUserMemberships(affiliations);
-        } else {
-            isProcessedLocally = getNodeAffiliations(affiliations);
-        }
-        if (!isProcessedLocally) {
-            return;
-        }
-
-        outQueue.put(result);
-    }
-
-    private boolean getNodeAffiliations(Element affiliations) throws NodeStoreException, InterruptedException {
-        if (!Configuration.getInstance().isLocalNode(node) && (!channelManager.isCachedNode(node))) {
-
-            makeRemoteRequest(node.split("/")[2]);
-            return false;
-        }
-        ResultSet<NodeMembership> nodeMemberships;
-        nodeMemberships = channelManager.getNodeMemberships(node);
-
-        if ((!nodeMemberships.isEmpty()) && (!Configuration.getInstance().isLocalNode(node))) {
-            makeRemoteRequest(node.split("/")[2]);
-            return false;
-        }
-
-
-        for (NodeMembership nodeMembership : nodeMemberships) {
-
-            if (actorJid.toBareJID().equals(nodeMembership.getUser().toBareJID())) {
-                if (null == firstItem) {
-                    firstItem = nodeMembership.getUser().toString();
-                }
-
-                affiliations.addElement(XMLConstants.AFFILIATION_ELEM).addAttribute(XMLConstants.NODE_ATTR, nodeMembership.getNodeId())
-                        .addAttribute(XMLConstants.AFFILIATION_ELEM, nodeMembership.getAffiliation().toString())
-                        .addAttribute(XMLConstants.JID_ATTR, nodeMembership.getUser().toString());
-            }
-        }
-
-        return true;
-    }
-
-    private boolean isOwnerModerator() throws NodeStoreException {
-        return channelManager.getNodeMembership(node, actorJid).getAffiliation().canAuthorize();
-    }
-
-    private boolean getUserMemberships(Element affiliations) throws NodeStoreException, InterruptedException {
-
-        if (!Configuration.getInstance().isLocalJID(actorJid) && (!channelManager.isCachedJID(requestIq.getFrom()))) {
-            makeRemoteRequest(actorJid.getDomain());
-            return false;
-        }
-
-        ResultSet<NodeMembership> memberships = channelManager.getUserMemberships(actorJid);
-
-        for (NodeMembership membership : memberships) {
-
-            if (actorJid.toBareJID().equals(membership.getUser().toBareJID())) {
-                LOGGER.trace("Adding affiliation for " + membership.getUser() + " affiliation " + membership.getAffiliation() + " (no node provided)");
-
-                if (null == firstItem) {
-                    firstItem = membership.getNodeId();
-                }
-
-                affiliations.addElement(XMLConstants.AFFILIATION_ELEM).addAttribute(XMLConstants.NODE_ATTR, membership.getNodeId())
-                        .addAttribute(XMLConstants.AFFILIATION_ELEM, membership.getAffiliation().toString())
-                        .addAttribute(XMLConstants.JID_ATTR, membership.getUser().toBareJID());
-            }
-
-        }
-        return true;
-    }
-
-    private void makeRemoteRequest(String node) throws InterruptedException {
-        LOGGER.info("Going federated for <affiliations />");
-        requestIq.setTo(new JID(node).getDomain());
-        if (null == requestIq.getElement().element("pubsub").element("actor")) {
-            Element actor = requestIq.getElement().element("pubsub").addElement("actor", Buddycloud.NS);
-            actor.addText(requestIq.getFrom().toBareJID());
-        }
-        outQueue.put(requestIq);
-    }
-
-    @Override
-    public boolean accept(Element elm) {
-        return XMLConstants.AFFILIATION_ELEM.equals(elm.getName());
-    }
-}
+  package        org . buddycloud . channelserver . packetprocessor . iq . namespace . pubsub . get ;   import    java . util . concurrent . BlockingQueue ;  import    org . apache . log4j . Logger ;  import     org . buddycloud . channelserver . channel . ChannelManager ;  import      org . buddycloud . channelserver . db . exception . NodeStoreException ;  import        org . buddycloud . channelserver . packetprocessor . iq . namespace . pubsub . JabberPubsub ;  import        org . buddycloud . channelserver . packetprocessor . iq . namespace . pubsub . PubSubElementProcessorAbstract ;  import      org . buddycloud . channelserver . pubsub . model . NodeMembership ;  import     org . buddycloud . channelserver . utils . XMLConstants ;  import        org . buddycloud . channelserver . utils . node . item . payload . Buddycloud ;  import   org . dom4j . Element ;  import    org . xmpp . packet . IQ ;  import    org . xmpp . packet . JID ;  import    org . xmpp . packet . Packet ;  import    org . xmpp . resultsetmanagement . ResultSet ;  import    org . buddycloud . channelserver . Configuration ;   public class AffiliationsGet  extends PubSubElementProcessorAbstract  {   private final  BlockingQueue  < Packet >  outQueue ;   private final ChannelManager  channelManager ;   private IQ  requestIq ;   private String  node ;   private JID  actorJid ;   private IQ  result ;   private String  firstItem ;   private static final Logger  LOGGER =  Logger . getLogger  (  AffiliationsGet . class ) ;   public AffiliationsGet  (   BlockingQueue  < Packet > outQueue ,  ChannelManager channelManager )  {    this . outQueue = outQueue ;    this . channelManager = channelManager ; }    @ Override public void process  (  Element elm ,  JID actorJID ,  IQ reqIQ ,  Element rsm )  throws Exception  {   result =  IQ . createResultIQ  ( reqIQ ) ;   requestIq = reqIQ ;   actorJid = actorJID ;   node =  elm . attributeValue  (  XMLConstants . NODE_ATTR ) ;  if  ( 
+<<<<<<<
+ !  channelManager . isLocalJID  (  requestIq . getFrom  ( ) )
+=======
+ false ==   Configuration . getInstance  ( ) . isLocalJID  (  requestIq . getFrom  ( ) )
+>>>>>>>
+ )  {    result . getElement  ( ) . addAttribute  (  XMLConstants . REMOTE_SERVER_DISCOVER_ATTR ,   Boolean . FALSE . toString  ( ) ) ; }  String  namespace =  JabberPubsub . NS_PUBSUB_OWNER ;  if  (  node == null )  {   namespace =  JabberPubsub . NAMESPACE_URI ; }  Element  pubsub =  result . setChildElement  (  XMLConstants . PUBSUB_ELEM , namespace ) ;  Element  affiliations =  pubsub . addElement  (  XMLConstants . AFFILIATION_ELEM ) ;  if  (  actorJid == null )  {   actorJid =  requestIq . getFrom  ( ) ; }  boolean  isProcessedLocally = true ;  if  (  node == null )  {   isProcessedLocally =  getUserMemberships  ( affiliations ) ; } else  {   isProcessedLocally =  getNodeAffiliations  ( affiliations ) ; }  if  (  ! isProcessedLocally )  {  return ; }   outQueue . put  ( result ) ; }   private boolean getNodeAffiliations  (  Element affiliations )  throws NodeStoreException , InterruptedException  {  if  (  
+<<<<<<<
+ !  channelManager . isLocalNode  ( node )
+=======
+ false ==   Configuration . getInstance  ( ) . isLocalNode  ( node )
+>>>>>>>
+ &&  (  !  channelManager . isCachedNode  ( node ) ) )  {   makeRemoteRequest  (   node . split  ( "/" ) [ 2 ] ) ;  return false ; }   ResultSet  < NodeMembership >  nodeMemberships ;   nodeMemberships =  channelManager . getNodeMemberships  ( node ) ;  if  (   (  !  nodeMemberships . isEmpty  ( ) ) &&  ( 
+<<<<<<<
+ !  channelManager . isLocalNode  ( node )
+=======
+ false ==   Configuration . getInstance  ( ) . isLocalNode  ( node )
+>>>>>>>
+ ) )  {   makeRemoteRequest  (   node . split  ( "/" ) [ 2 ] ) ;  return false ; }  for ( NodeMembership nodeMembership : nodeMemberships )  {  if  (   actorJid . toBareJID  ( ) . equals  (   nodeMembership . getUser  ( ) . toBareJID  ( ) ) )  {  if  (  null == firstItem )  {   firstItem =   nodeMembership . getUser  ( ) . toString  ( ) ; }      affiliations . addElement  (  XMLConstants . AFFILIATION_ELEM ) . addAttribute  (  XMLConstants . NODE_ATTR ,  nodeMembership . getNodeId  ( ) ) . addAttribute  (  XMLConstants . AFFILIATION_ELEM ,   nodeMembership . getAffiliation  ( ) . toString  ( ) ) . addAttribute  (  XMLConstants . JID_ATTR ,   nodeMembership . getUser  ( ) . toString  ( ) ) ; } }  return true ; }   private boolean isOwnerModerator  ( )  throws NodeStoreException  {  return    channelManager . getNodeMembership  ( node , actorJid ) . getAffiliation  ( ) . canAuthorize  ( ) ; }   private boolean getUserMemberships  (  Element affiliations )  throws NodeStoreException , InterruptedException  {  if  (  
+<<<<<<<
+ !  channelManager . isLocalJID  ( actorJid )
+=======
+ false ==   Configuration . getInstance  ( ) . isLocalJID  ( actorJid )
+>>>>>>>
+ &&  (  !  channelManager . isCachedJID  (  requestIq . getFrom  ( ) ) ) )  {   makeRemoteRequest  (  actorJid . getDomain  ( ) ) ;  return false ; }   ResultSet  < NodeMembership >  memberships =  channelManager . getUserMemberships  ( actorJid ) ;  for ( NodeMembership membership : memberships )  {  if  (   actorJid . toBareJID  ( ) . equals  (   membership . getUser  ( ) . toBareJID  ( ) ) )  {   LOGGER . trace  (     "Adding affiliation for " +  membership . getUser  ( ) + " affiliation " +  membership . getAffiliation  ( ) + " (no node provided)" ) ;  if  (  null == firstItem )  {   firstItem =  membership . getNodeId  ( ) ; }      affiliations . addElement  (  XMLConstants . AFFILIATION_ELEM ) . addAttribute  (  XMLConstants . NODE_ATTR ,  membership . getNodeId  ( ) ) . addAttribute  (  XMLConstants . AFFILIATION_ELEM ,   membership . getAffiliation  ( ) . toString  ( ) ) . addAttribute  (  XMLConstants . JID_ATTR ,   membership . getUser  ( ) . toBareJID  ( ) ) ; } }  return true ; }   private void makeRemoteRequest  (  String node )  throws InterruptedException  {   LOGGER . info  ( "Going federated for <affiliations />" ) ;   requestIq . setTo  (   new JID  ( node ) . getDomain  ( ) ) ;  if  (  null ==    requestIq . getElement  ( ) . element  ( "pubsub" ) . element  ( "actor" ) )  {  Element  actor =    requestIq . getElement  ( ) . element  ( "pubsub" ) . addElement  ( "actor" ,  Buddycloud . NS ) ;   actor . addText  (   requestIq . getFrom  ( ) . toBareJID  ( ) ) ; }   outQueue . put  ( requestIq ) ; }    @ Override public boolean accept  (  Element elm )  {  return   XMLConstants . AFFILIATION_ELEM . equals  (  elm . getName  ( ) ) ; } }
--- src/main/java/org/buddycloud/channelserver/packetprocessor/iq/namespace/pubsub/get/SubscriptionsGet.java
+++ src/main/java/org/buddycloud/channelserver/packetprocessor/iq/namespace/pubsub/get/SubscriptionsGet.java
@@ -1,177 +1,24 @@
-package org.buddycloud.channelserver.packetprocessor.iq.namespace.pubsub.get;
-
-import java.util.concurrent.BlockingQueue;
-
-import org.buddycloud.channelserver.Configuration;
-import org.buddycloud.channelserver.channel.ChannelManager;
-import org.buddycloud.channelserver.db.exception.NodeStoreException;
-import org.buddycloud.channelserver.packetprocessor.iq.namespace.pubsub.JabberPubsub;
-import org.buddycloud.channelserver.packetprocessor.iq.namespace.pubsub.PubSubElementProcessorAbstract;
-import org.buddycloud.channelserver.pubsub.model.NodeMembership;
-import org.buddycloud.channelserver.pubsub.subscription.Subscriptions;
-import org.buddycloud.channelserver.utils.XMLConstants;
-import org.buddycloud.channelserver.utils.node.item.payload.Buddycloud;
-import org.dom4j.Element;
-import org.xmpp.packet.IQ;
-import org.xmpp.packet.JID;
-import org.xmpp.packet.Packet;
-import org.xmpp.resultsetmanagement.ResultSet;
-
-public class SubscriptionsGet extends PubSubElementProcessorAbstract {
-
-    private final BlockingQueue<Packet> outQueue;
-    private ChannelManager channelManager;
-
-    private IQ result;
-    private String node;
-    private JID actorJid;
-    private IQ requestIq;
-
-    public SubscriptionsGet(BlockingQueue<Packet> outQueue, ChannelManager channelManager) {
-        this.outQueue = outQueue;
-        this.channelManager = channelManager;
-    }
-
-    public void setChannelManager(ChannelManager dataStore) {
-        channelManager = dataStore;
-    }
-
-
-    @Override
-    public void process(Element elm, JID actorJID, IQ reqIQ, Element rsm) throws Exception {
-        result = IQ.createResultIQ(reqIQ);
-        actorJid = actorJID;
-        requestIq = reqIQ;
-
-        Element pubsub = result.setChildElement(XMLConstants.PUBSUB_ELEM, JabberPubsub.NAMESPACE_URI);
-        Element subscriptions = pubsub.addElement(XMLConstants.SUBSCRIPTIONS_ELEM);
-
-
-        node = reqIQ.getChildElement().element(XMLConstants.SUBSCRIPTIONS_ELEM).attributeValue(XMLConstants.NODE_ATTR);
-
-        if (null == actorJid) {
-            actorJid = reqIQ.getFrom();
-        }
-
-        boolean isProcessedLocally = true;
-
-        if (node == null) {
-            isProcessedLocally = getUserMemberships(subscriptions);
-        } else {
-            if (!Configuration.getInstance().isLocalNode(node)) {
-                result.getElement().addAttribute(XMLConstants.REMOTE_SERVER_DISCOVER_ATTR, Boolean.FALSE.toString());
-
-            }
-            isProcessedLocally = getNodeMemberships(subscriptions);
-        }
-        if (!isProcessedLocally) {
-            return;
-        }
-
-        outQueue.put(result);
-    }
-
-    /**
-     * Don't include any subscriptions other than Subscriptions.subscriptions, unless the user is
-     * one of owner, moderator or the user.
-     * 
-     * @param subscriptions
-     * @return
-     * @throws NodeStoreException
-     * @throws InterruptedException
-     */
-    private boolean getNodeMemberships(Element subscriptions) throws NodeStoreException, InterruptedException {
-
-        ResultSet<NodeMembership> cur = channelManager.getNodeMemberships(node);
-
-        if (Configuration.getInstance().isLocalNode(node)) {
-            subscriptions.addAttribute(XMLConstants.NODE_ATTR, node);
-
-            for (NodeMembership ns : cur) {
-                if (!isUserPriviledged(ns.getUser()) && !ns.getSubscription().equals(Subscriptions.subscribed)) {
-                    continue;
-                }
-
-                Element subscription = subscriptions.addElement(XMLConstants.SUBSCRIPTION_ELEM);
-                subscription.addAttribute(XMLConstants.NODE_ATTR, ns.getNodeId())
-                        .addAttribute(XMLConstants.SUBSCRIPTION_ELEM, ns.getSubscription().toString())
-                        .addAttribute(XMLConstants.JID_ATTR, ns.getUser().toBareJID());
-
-                // Only add the invited by attribute if the user has the necessary priviledges
-                if (null != ns.getInvitedBy() && isUserPriviledged(ns.getUser())) {
-                    subscription.addAttribute(XMLConstants.INVITED_BY_ELEM, ns.getInvitedBy().toBareJID());
-                }
-
-            }
-        } else {
-            // TODO(garethf) this looks like makeRemoteRequest
-            if (!channelManager.isCachedNode(node) || (null != requestIq.getElement().element(XMLConstants.PUBSUB_ELEM).element(XMLConstants.SET_ELEM))
-                    && !cur.isEmpty()) {
-                makeRemoteRequest(new JID(node.split("/")[2]).getDomain());
-
-            }
-            return false;
-        }
-
-        return true;
-    }
-
-    /**
-     * Don't include any subscriptions other than Subscriptions.subscriptions, unless the user is
-     * one of owner, moderator or the user.
-     * 
-     * @param ns
-     * 
-     * @return
-     * @throws NodeStoreException
-     */
-    private boolean isUserPriviledged(JID jid) throws NodeStoreException {
-        boolean isUser = actorJid.toBareJID().equals(jid.toBareJID());
-
-        return (isUser || isOwnerModerator()) ? true : false;
-    }
-
-    private boolean getUserMemberships(Element subscriptions) throws NodeStoreException, InterruptedException {
-        // let's get all subscriptions.
-        ResultSet<NodeMembership> cur;
-        cur = channelManager.getUserMemberships(actorJid);
-
-        if ((null != requestIq.getElement().element(XMLConstants.PUBSUB_ELEM).element("set")) && (!cur.isEmpty())
-                && (!Configuration.getInstance().isLocalJID(actorJid))) {
-
-            makeRemoteRequest(actorJid.getDomain());
-            return false;
-        }
-        for (NodeMembership ns : cur) {
-            Element subscription = subscriptions.addElement(XMLConstants.SUBSCRIPTION_ELEM);
-            subscription.addAttribute(XMLConstants.NODE_ATTR, ns.getNodeId())
-                    .addAttribute(XMLConstants.SUBSCRIPTION_ELEM, ns.getSubscription().toString())
-                    .addAttribute(XMLConstants.JID_ATTR, ns.getUser().toBareJID());
-            if (null != ns.getInvitedBy() && isOwnerModerator()) {
-                subscription.addAttribute(XMLConstants.INVITED_BY_ELEM, ns.getInvitedBy().toBareJID());
-
-            }
-        }
-        return true;
-    }
-
-    private boolean isOwnerModerator() throws NodeStoreException {
-        return channelManager.getNodeMembership(node, actorJid).getAffiliation().canAuthorize();
-    }
-
-    private void makeRemoteRequest(String to) throws InterruptedException {
-        IQ forwarder = requestIq.createCopy();
-        forwarder.setTo(to);
-        if (null == forwarder.getElement().element(XMLConstants.PUBSUB_ELEM).element(XMLConstants.ACTOR_ELEM)) {
-            Element actor = forwarder.getElement().element(XMLConstants.PUBSUB_ELEM).addElement(XMLConstants.ACTOR_ELEM, Buddycloud.NS);
-
-            actor.addText(requestIq.getFrom().toBareJID());
-        }
-        outQueue.put(forwarder);
-    }
-
-    @Override
-    public boolean accept(Element elm) {
-        return XMLConstants.SUBSCRIPTIONS_ELEM.equals(elm.getName());
-    }
-}
+  package        org . buddycloud . channelserver . packetprocessor . iq . namespace . pubsub . get ;   import    java . util . concurrent . BlockingQueue ;  import     org . buddycloud . channelserver . channel . ChannelManager ;  import      org . buddycloud . channelserver . db . exception . NodeStoreException ;  import        org . buddycloud . channelserver . packetprocessor . iq . namespace . pubsub . JabberPubsub ;  import        org . buddycloud . channelserver . packetprocessor . iq . namespace . pubsub . PubSubElementProcessorAbstract ;  import      org . buddycloud . channelserver . pubsub . model . NodeMembership ;  import      org . buddycloud . channelserver . pubsub . subscription . Subscriptions ;  import     org . buddycloud . channelserver . utils . XMLConstants ;  import        org . buddycloud . channelserver . utils . node . item . payload . Buddycloud ;  import   org . dom4j . Element ;  import    org . xmpp . packet . IQ ;  import    org . xmpp . packet . JID ;  import    org . xmpp . packet . Packet ;  import    org . xmpp . resultsetmanagement . ResultSet ;  import    org . buddycloud . channelserver . Configuration ;   public class SubscriptionsGet  extends PubSubElementProcessorAbstract  {   private final  BlockingQueue  < Packet >  outQueue ;   private ChannelManager  channelManager ;   private IQ  result ;   private String  node ;   private JID  actorJid ;   private IQ  requestIq ;   public SubscriptionsGet  (   BlockingQueue  < Packet > outQueue ,  ChannelManager channelManager )  {    this . outQueue = outQueue ;    this . channelManager = channelManager ; }   public void setChannelManager  (  ChannelManager dataStore )  {   channelManager = dataStore ; }    @ Override public void process  (  Element elm ,  JID actorJID ,  IQ reqIQ ,  Element rsm )  throws Exception  {   result =  IQ . createResultIQ  ( reqIQ ) ;   actorJid = actorJID ;   requestIq = reqIQ ;  Element  pubsub =  result . setChildElement  (  XMLConstants . PUBSUB_ELEM ,  JabberPubsub . NAMESPACE_URI ) ;  Element  subscriptions =  pubsub . addElement  (  XMLConstants . SUBSCRIPTIONS_ELEM ) ;   node =    reqIQ . getChildElement  ( ) . element  (  XMLConstants . SUBSCRIPTIONS_ELEM ) . attributeValue  (  XMLConstants . NODE_ATTR ) ;  if  (  null == actorJid )  {   actorJid =  reqIQ . getFrom  ( ) ; }  boolean  isProcessedLocally = true ;  if  (  node == null )  {   isProcessedLocally =  getUserMemberships  ( subscriptions ) ; } else  {  if  ( 
+<<<<<<<
+ !  channelManager . isLocalNode  ( node )
+=======
+ false ==   Configuration . getInstance  ( ) . isLocalNode  ( node )
+>>>>>>>
+ )  {    result . getElement  ( ) . addAttribute  (  XMLConstants . REMOTE_SERVER_DISCOVER_ATTR ,   Boolean . FALSE . toString  ( ) ) ; }   isProcessedLocally =  getNodeMemberships  ( subscriptions ) ; }  if  (  ! isProcessedLocally )  {  return ; }   outQueue . put  ( result ) ; }   private boolean getNodeMemberships  (  Element subscriptions )  throws NodeStoreException , InterruptedException  { 
+<<<<<<<
+=======
+ if  (   false ==   Configuration . getInstance  ( ) . isLocalNode  ( node ) &&  (  false ==  channelManager . isCachedNode  ( node ) ) )  {   makeRemoteRequest  (   new JID  (   node . split  ( "/" ) [ 2 ] ) . getDomain  ( ) ) ;  return false ; }
+>>>>>>>
+   ResultSet  < NodeMembership >  cur =  channelManager . getNodeMemberships  ( node ) ;  if  ( 
+<<<<<<<
+ channelManager . isLocalNode  ( node )
+=======
+   (  null !=    requestIq . getElement  ( ) . element  ( "pubsub" ) . element  ( "set" ) ) &&  (  0 ==  cur . size  ( ) ) &&  (  false ==   Configuration . getInstance  ( ) . isLocalNode  ( node ) )
+>>>>>>>
+ )  {   subscriptions . addAttribute  (  XMLConstants . NODE_ATTR , node ) ;  for ( NodeMembership ns : cur )  {  if  (   !  isUserPriviledged  (  ns . getUser  ( ) ) &&  !   ns . getSubscription  ( ) . equals  (  Subscriptions . subscribed ) )  {  continue ; }  Element  subscription =  subscriptions . addElement  (  XMLConstants . SUBSCRIPTION_ELEM ) ;     subscription . addAttribute  (  XMLConstants . NODE_ATTR ,  ns . getNodeId  ( ) ) . addAttribute  (  XMLConstants . SUBSCRIPTION_ELEM ,   ns . getSubscription  ( ) . toString  ( ) ) . addAttribute  (  XMLConstants . JID_ATTR ,   ns . getUser  ( ) . toBareJID  ( ) ) ;  if  (   null !=  ns . getInvitedBy  ( ) &&  isUserPriviledged  (  ns . getUser  ( ) ) )  {   subscription . addAttribute  (  XMLConstants . INVITED_BY_ELEM ,   ns . getInvitedBy  ( ) . toBareJID  ( ) ) ; } } } else  {  if  (   !  channelManager . isCachedNode  ( node ) ||   (  null !=    requestIq . getElement  ( ) . element  (  XMLConstants . PUBSUB_ELEM ) . element  (  XMLConstants . SET_ELEM ) ) &&  !  cur . isEmpty  ( ) )  {   makeRemoteRequest  (   new JID  (   node . split  ( "/" ) [ 2 ] ) . getDomain  ( ) ) ; }  return false ; }  return true ; }   private boolean isUserPriviledged  (  JID jid )  throws NodeStoreException  {  boolean  isUser =   actorJid . toBareJID  ( ) . equals  (  jid . toBareJID  ( ) ) ;  return   (  isUser ||  isOwnerModerator  ( ) ) ? true : false ; }   private boolean getUserMemberships  (  Element subscriptions )  throws NodeStoreException , InterruptedException  {   ResultSet  < NodeMembership >  cur ;   cur =  channelManager . getUserMemberships  ( actorJid ) ;  if  (    (  null !=    requestIq . getElement  ( ) . element  (  XMLConstants . PUBSUB_ELEM ) . element  ( "set" ) ) &&  (  !  cur . isEmpty  ( ) ) &&  ( 
+<<<<<<<
+ !  channelManager . isLocalJID  ( actorJid )
+=======
+ false ==   Configuration . getInstance  ( ) . isLocalJID  ( actorJid )
+>>>>>>>
+ ) )  {   makeRemoteRequest  (  actorJid . getDomain  ( ) ) ;  return false ; }  for ( NodeMembership ns : cur )  {  Element  subscription =  subscriptions . addElement  (  XMLConstants . SUBSCRIPTION_ELEM ) ;     subscription . addAttribute  (  XMLConstants . NODE_ATTR ,  ns . getNodeId  ( ) ) . addAttribute  (  XMLConstants . SUBSCRIPTION_ELEM ,   ns . getSubscription  ( ) . toString  ( ) ) . addAttribute  (  XMLConstants . JID_ATTR ,   ns . getUser  ( ) . toBareJID  ( ) ) ;  if  (   null !=  ns . getInvitedBy  ( ) &&  isOwnerModerator  ( ) )  {   subscription . addAttribute  (  XMLConstants . INVITED_BY_ELEM ,   ns . getInvitedBy  ( ) . toBareJID  ( ) ) ; } }  return true ; }   private boolean isOwnerModerator  ( )  throws NodeStoreException  {  return    channelManager . getNodeMembership  ( node , actorJid ) . getAffiliation  ( ) . canAuthorize  ( ) ; }   private void makeRemoteRequest  (  String to )  throws InterruptedException  {  IQ  forwarder =  requestIq . createCopy  ( ) ;   forwarder . setTo  ( to ) ;  if  (  null ==    forwarder . getElement  ( ) . element  (  XMLConstants . PUBSUB_ELEM ) . element  (  XMLConstants . ACTOR_ELEM ) )  {  Element  actor =    forwarder . getElement  ( ) . element  (  XMLConstants . PUBSUB_ELEM ) . addElement  (  XMLConstants . ACTOR_ELEM ,  Buddycloud . NS ) ;   actor . addText  (   requestIq . getFrom  ( ) . toBareJID  ( ) ) ; }   outQueue . put  ( forwarder ) ; }    @ Override public boolean accept  (  Element elm )  {  return   XMLConstants . SUBSCRIPTIONS_ELEM . equals  (  elm . getName  ( ) ) ; } }
