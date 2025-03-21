a9e6d3fd03cff43fdc611f10a7794616996d6790
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 23:23:45 +0000

    Mining Framework Analysis

--- cloudfoundry-client-reactor/src/main/java/org/cloudfoundry/reactor/_InfoPayloadRootProvider.java
+++ cloudfoundry-client-reactor/src/main/java/org/cloudfoundry/reactor/_InfoPayloadRootProvider.java
@@ -1,21 +1,4 @@
-/*
- * Copyright 2013-2017 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.cloudfoundry.reactor;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.cloudfoundry.reactor.util.JsonCodec;
 import org.cloudfoundry.reactor.util.NetworkLogging;
@@ -25,48 +8,37 @@ import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Mono;
 import reactor.ipc.netty.http.client.HttpClientRequest;
-
 import java.util.Map;
 
 /**
  * A {@link RootProvider} that returns endpoints extracted from the `/v2/info` API for the configured endpoint.
  */
-@Value.Immutable
-abstract class _InfoPayloadRootProvider extends AbstractRootProvider {
+@Value.Immutable abstract class _InfoPayloadRootProvider extends AbstractRootProvider {
 
-    protected Mono<UriComponents> doGetRoot(ConnectionContext connectionContext) {
-        return Mono.just(getRoot());
-    }
+<<<<<<< /usr/src/app/output/cloudfoundry/cf-java-client/9f65cf5bd50bead523ac98e52e9938333f3fc08d/cloudfoundry-client-reactor/src/main/java/org/cloudfoundry/reactor/_InfoPayloadRootProvider.java/left.java
+  @Override @SuppressWarnings(value = { "unchecked" }) protected Mono<Map<String, String>> doGetPayload(ConnectionContext connectionContext) {
+    return getRoot(connectionContext).map((uri) -> UriComponentsBuilder.fromUriString(uri).pathSegment("v2", "info").build().encode().toUriString()).flatMap((uri) -> connectionContext.getHttpClient().get(uri, (request) -> Mono.just(request).map(UserAgent::addUserAgent).map(JsonCodec::addDecodeHeaders).flatMapMany(HttpClientRequest::send)).doOnSubscribe(NetworkLogging.get(uri)).transform(NetworkLogging.response(uri))).transform(JsonCodec.decode(getObjectMapper(), Map.class)).switchIfEmpty(Mono.error(new IllegalArgumentException("Info endpoint does not contain a payload"))).map((m) -> (Map<String, String>) m).checkpoint();
+  }
+=======
+>>>>>>> Unknown file: This is a bug in JDime.
 
-    protected Mono<UriComponents> doGetRoot(String key, ConnectionContext connectionContext) {
-        return getInfo(connectionContext)
-            .map(info -> {
-                if (!info.containsKey(key)) {
-                    throw new IllegalArgumentException(String.format("Info payload does not contain key '%s'", key));
-                }
 
-                return normalize(UriComponentsBuilder.fromUriString(info.get(key)));
-            });
-    }
+  protected Mono<UriComponents> doGetRoot(ConnectionContext connectionContext) {
+    return Mono.just(getRoot());
+  }
 
-    abstract ObjectMapper getObjectMapper();
+  protected Mono<UriComponents> doGetRoot(String key, ConnectionContext connectionContext) {
+    return getInfo(connectionContext).map((info) -> {
+      if (!info.containsKey(key)) {
+        throw new IllegalArgumentException(String.format("Info payload does not contain key \'%s\'", key));
+      }
+      return normalize(UriComponentsBuilder.fromUriString(info.get(key)));
+    });
+  }
 
-    @SuppressWarnings("unchecked")
-    @Value.Derived
-    private Mono<Map<String, String>> getInfo(ConnectionContext connectionContext) {
-        return getRoot(connectionContext)
-            .map(uri -> UriComponentsBuilder.fromUriString(uri).pathSegment("v2", "info").build().encode().toUriString())
-            .flatMap(uri -> connectionContext.getHttpClient()
-                .get(uri, request -> Mono.just(request)
-                    .map(UserAgent::addUserAgent)
-                    .map(JsonCodec::addDecodeHeaders)
-                    .flatMapMany(HttpClientRequest::send))
-                .doOnSubscribe(NetworkLogging.get(uri))
-                .transform(NetworkLogging.response(uri)))
-            .transform(JsonCodec.decode(getObjectMapper(), Map.class))
-            .switchIfEmpty(Mono.error(new IllegalArgumentException("Info endpoint does not contain a payload")))
-            .map(m -> (Map<String, String>) m)
-            .checkpoint();
-    }
+  abstract ObjectMapper getObjectMapper();
 
+  @SuppressWarnings(value = { "unchecked" }) @Value.Derived private Mono<Map<String, String>> getInfo(ConnectionContext connectionContext) {
+    return getRoot(connectionContext).map((uri) -> UriComponentsBuilder.fromUriString(uri).pathSegment("v2", "info").build().encode().toUriString()).then((uri) -> connectionContext.getHttpClient().get(uri, (request) -> Mono.just(request).map(UserAgent::addUserAgent).map(JsonCodec::addDecodeHeaders).flatMapMany(HttpClientRequest::send)).doOnSubscribe(NetworkLogging.get(uri)).transform(NetworkLogging.response(uri))).transform(JsonCodec.decode(getObjectMapper(), Map.class)).switchIfEmpty(Mono.error(new IllegalArgumentException("Info endpoint does not contain a payload"))).map((m) -> (Map<String, String>) m).checkpoint();
+  }
 }
--- cloudfoundry-client-reactor/src/main/java/org/cloudfoundry/reactor/_RootPayloadRootProvider.java
+++ cloudfoundry-client-reactor/src/main/java/org/cloudfoundry/reactor/_RootPayloadRootProvider.java
@@ -1,21 +1,4 @@
-/*
- * Copyright 2013-2017 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.cloudfoundry.reactor;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.cloudfoundry.reactor.util.JsonCodec;
 import org.cloudfoundry.reactor.util.NetworkLogging;
@@ -25,55 +8,42 @@ import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Mono;
 import reactor.ipc.netty.http.client.HttpClientRequest;
-
 import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
  * A {@link RootProvider} that returns endpoints extracted from the `/` API for the configured endpoint.
  */
-@Value.Immutable
-abstract class _RootPayloadRootProvider extends AbstractRootProvider {
+@Value.Immutable abstract class _RootPayloadRootProvider extends AbstractRootProvider {
 
-    @Override
-    protected Mono<UriComponents> doGetRoot(ConnectionContext connectionContext) {
-        return Mono.just(getRoot());
-    }
+<<<<<<< /usr/src/app/output/cloudfoundry/cf-java-client/9f65cf5bd50bead523ac98e52e9938333f3fc08d/cloudfoundry-client-reactor/src/main/java/org/cloudfoundry/reactor/_RootPayloadRootProvider.java/left.java
+  @Override @SuppressWarnings(value = { "unchecked" }) protected Mono<Map<String, String>> doGetPayload(ConnectionContext connectionContext) {
+    return getRoot(connectionContext).flatMap((uri) -> connectionContext.getHttpClient().get(uri, (request) -> Mono.just(request).map(UserAgent::addUserAgent).map(JsonCodec::addDecodeHeaders).flatMapMany(HttpClientRequest::send)).doOnSubscribe(NetworkLogging.get(uri)).transform(NetworkLogging.response(uri))).transform(JsonCodec.decode(getObjectMapper(), Map.class)).switchIfEmpty(Mono.error(new IllegalArgumentException("Root endpoint does not contain a payload"))).map(this::parsePayload).checkpoint();
+  }
+=======
+>>>>>>> Unknown file: This is a bug in JDime.
 
-    @Override
-    protected Mono<UriComponents> doGetRoot(String key, ConnectionContext connectionContext) {
-        return getPayload(connectionContext)
-            .map(payload -> {
-                if (!payload.containsKey(key)) {
-                    throw new IllegalArgumentException(String.format("Root payload does not contain key '%s'", key));
-                }
 
-                return normalize(UriComponentsBuilder.fromUriString(payload.get(key)));
-            });
-    }
+  @Override protected Mono<UriComponents> doGetRoot(ConnectionContext connectionContext) {
+    return Mono.just(getRoot());
+  }
 
-    abstract ObjectMapper getObjectMapper();
+  @Override protected Mono<UriComponents> doGetRoot(String key, ConnectionContext connectionContext) {
+    return getPayload(connectionContext).map((payload) -> {
+      if (!payload.containsKey(key)) {
+        throw new IllegalArgumentException(String.format("Root payload does not contain key \'%s\'", key));
+      }
+      return normalize(UriComponentsBuilder.fromUriString(payload.get(key)));
+    });
+  }
 
-    @SuppressWarnings("unchecked")
-    @Value.Derived
-    private Mono<Map<String, String>> getPayload(ConnectionContext connectionContext) {
-        return getRoot(connectionContext)
-            .flatMap(uri -> connectionContext.getHttpClient()
-                .get(uri, request -> Mono.just(request)
-                    .map(UserAgent::addUserAgent)
-                    .map(JsonCodec::addDecodeHeaders)
-                    .flatMapMany(HttpClientRequest::send))
-                .doOnSubscribe(NetworkLogging.get(uri))
-                .transform(NetworkLogging.response(uri)))
-            .transform(JsonCodec.decode(getObjectMapper(), Map.class))
-            .switchIfEmpty(Mono.error(new IllegalArgumentException("Root endpoint does not contain a payload")))
-            .map(this::parsePayload)
-            .checkpoint();
-    }
+  abstract ObjectMapper getObjectMapper();
 
-    private Map<String, String> parsePayload(Map<String, Map<String, Map<String, String>>> payload) {
-        return payload.get("links").entrySet().stream()
-            .collect(Collectors.toMap(Map.Entry::getKey, entry -> entry.getValue().get("href")));
-    }
+  @SuppressWarnings(value = { "unchecked" }) @Value.Derived private Mono<Map<String, String>> getPayload(ConnectionContext connectionContext) {
+    return getRoot(connectionContext).then((uri) -> connectionContext.getHttpClient().get(uri, (request) -> Mono.just(request).map(UserAgent::addUserAgent).map(JsonCodec::addDecodeHeaders).flatMapMany(HttpClientRequest::send)).doOnSubscribe(NetworkLogging.get(uri)).transform(NetworkLogging.response(uri))).transform(JsonCodec.decode(getObjectMapper(), Map.class)).switchIfEmpty(Mono.error(new IllegalArgumentException("Root endpoint does not contain a payload"))).map(this::parsePayload).checkpoint();
+  }
 
+  private Map<String, String> parsePayload(Map<String, Map<String, Map<String, String>>> payload) {
+    return payload.get("links").entrySet().stream().collect(Collectors.toMap(Map.Entry::getKey, (entry) -> entry.getValue().get("href")));
+  }
 }
