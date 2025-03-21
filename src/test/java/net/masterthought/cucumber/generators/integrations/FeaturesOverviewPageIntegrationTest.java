62577772ccd0fec4357acc3191f17e52b889d21d
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 03:51:58 +0000

    Mining Framework Analysis

--- src/test/java/net/masterthought/cucumber/generators/integrations/FeaturesOverviewPageIntegrationTest.java
+++ src/test/java/net/masterthought/cucumber/generators/integrations/FeaturesOverviewPageIntegrationTest.java
@@ -166,5 +166,6 @@ public class FeaturesOverviewPageIntegrationTest extends PageTest {
         assertThat(footerRows).hasSize(2);
         footerRows[0].hasExactValues("2", "15", "1", "2", "1", "2", "21", "2", "1", "0", "0", "3", "1m 39s 355ms", "");
         footerRows[1].hasExactValues("", "71.43%", "4.76%", "9.52%", "4.76%", "9.52%", "", "66.67%", "33.33%", "0.00%", "0.00%", "", "", "50.00%");
+
     }
 }
