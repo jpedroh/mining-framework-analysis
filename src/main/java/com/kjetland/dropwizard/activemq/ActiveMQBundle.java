c0c69d34ef58ca560b9269ab5520fee0686e9b93
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 14:52:21 +0000

    Mining Framework Analysis

--- src/main/java/com/kjetland/dropwizard/activemq/ActiveMQBundle.java
+++ src/main/java/com/kjetland/dropwizard/activemq/ActiveMQBundle.java
@@ -1,5 +1,4 @@
 package com.kjetland.dropwizard.activemq;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.dropwizard.ConfiguredBundle;
 import io.dropwizard.lifecycle.Managed;
@@ -9,159 +8,114 @@ import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.activemq.jms.pool.PooledConnectionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import javax.jms.JMSException;
 import java.util.Optional;
 
 public class ActiveMQBundle implements ConfiguredBundle<ActiveMQConfigHolder>, Managed, ActiveMQSenderFactory {
+  private final Logger log = LoggerFactory.getLogger(getClass());
 
-    private final Logger log = LoggerFactory.getLogger(getClass());
-    private ActiveMQConnectionFactory realConnectionFactory;
-    private PooledConnectionFactory connectionFactory = null;
-    private ObjectMapper objectMapper;
-    private Environment environment;
-    private long shutdownWaitInSeconds;
-    private Optional<Integer> defaultTimeToLiveInSeconds;
+  private ActiveMQConnectionFactory realConnectionFactory;
 
-    public ActiveMQBundle() {
+  private PooledConnectionFactory connectionFactory = null;
 
+  private ObjectMapper objectMapper;
+
+  private Environment environment;
+
+  private long shutdownWaitInSeconds;
+
+  private Optional<Integer> defaultTimeToLiveInSeconds;
+
+  public ActiveMQBundle() {
+  }
+
+  @Override public void run(ActiveMQConfigHolder configuration, Environment environment) throws Exception {
+    this.environment = environment;
+    final String brokerUrl = configuration.getActiveMQ().brokerUrl;
+    final int configuredTTL = configuration.getActiveMQ().timeToLiveInSeconds;
+    defaultTimeToLiveInSeconds = Optional.ofNullable(configuredTTL > 0 ? configuredTTL : null);
+    log.info("Setting up activeMq with brokerUrl {}", brokerUrl);
+    log.debug("All activeMQ config: " + configuration.getActiveMQ());
+    realConnectionFactory = new ActiveMQConnectionFactory(brokerUrl);
+    connectionFactory = new PooledConnectionFactory();
+    connectionFactory.setConnectionFactory(realConnectionFactory);
+    configurePool(configuration.getActiveMQ().pool);
+    objectMapper = environment.getObjectMapper();
+    environment.lifecycle().manage(this);
+    environment.healthChecks().register("ActiveMQ", new ActiveMQHealthCheck(realConnectionFactory, configuration.getActiveMQ().healthCheckMillisecondsToWait));
+    this.shutdownWaitInSeconds = configuration.getActiveMQ().shutdownWaitInSeconds;
+  }
+
+  private void configurePool(ActiveMQPoolConfig poolConfig) {
+    if (poolConfig == null) {
+      return;
     }
-
-    @Override
-    public void run(ActiveMQConfigHolder configuration, Environment environment) throws Exception {
-        this.environment = environment;
-        final String brokerUrl = configuration.getActiveMQ().brokerUrl;
-        final int configuredTTL = configuration.getActiveMQ().timeToLiveInSeconds;
-        defaultTimeToLiveInSeconds = Optional.ofNullable(configuredTTL > 0 ? configuredTTL : null);
-
-        log.info("Setting up activeMq with brokerUrl {}", brokerUrl);
-
-        log.debug("All activeMQ config: " + configuration.getActiveMQ());
-
-        realConnectionFactory = new ActiveMQConnectionFactory(brokerUrl);
-        connectionFactory = new PooledConnectionFactory();
-        connectionFactory.setConnectionFactory(realConnectionFactory);
-
-        configurePool(configuration.getActiveMQ().pool);
-
-        objectMapper = environment.getObjectMapper();
-
-        environment.lifecycle().manage(this);
-
-        // Must use realConnectionFactory instead of (pooled) connectionFactory for the healthCheck
-        // Is needs its own connection since it is both sending and receiving.
-        // If using pool, then it might block since no one is available..
-        environment.healthChecks().register("ActiveMQ",
-                new ActiveMQHealthCheck(
-                        realConnectionFactory,
-                        configuration.getActiveMQ().healthCheckMillisecondsToWait)
-        );
-        this.shutdownWaitInSeconds = configuration.getActiveMQ().shutdownWaitInSeconds;
+    if (poolConfig.maxConnections != null) {
+      connectionFactory.setMaxConnections(poolConfig.maxConnections);
     }
-
-    private void configurePool(ActiveMQPoolConfig poolConfig) {
-        if (poolConfig == null) {
-            return;
-        }
-
-        if (poolConfig.maxConnections != null) {
-            connectionFactory.setMaxConnections(poolConfig.maxConnections);
-        }
-
-        if (poolConfig.maximumActiveSessionPerConnection != null) {
-            connectionFactory.setMaximumActiveSessionPerConnection(poolConfig.maximumActiveSessionPerConnection);
-        }
-
-        if (poolConfig.blockIfSessionPoolIsFull != null) {
-            connectionFactory.setBlockIfSessionPoolIsFull(poolConfig.blockIfSessionPoolIsFull);
-        }
-
-        if (poolConfig.idleTimeoutMills != null) {
-            connectionFactory.setIdleTimeout(poolConfig.idleTimeoutMills);
-        }
-
-        if (poolConfig.expiryTimeoutMills != null) {
-            connectionFactory.setExpiryTimeout(poolConfig.expiryTimeoutMills);
-        }
-
-        if (poolConfig.createConnectionOnStartup != null) {
-            connectionFactory.setCreateConnectionOnStartup(poolConfig.createConnectionOnStartup);
-        }
-
-        if (poolConfig.timeBetweenExpirationCheckMillis != null) {
-            connectionFactory.setTimeBetweenExpirationCheckMillis(poolConfig.timeBetweenExpirationCheckMillis);
-        }
-
+    if (poolConfig.maximumActiveSessionPerConnection != null) {
+      connectionFactory.setMaximumActiveSessionPerConnection(poolConfig.maximumActiveSessionPerConnection);
     }
-
-    @Override
-    public void initialize(Bootstrap<?> bootstrap) {
-
+    if (poolConfig.blockIfSessionPoolIsFull != null) {
+      connectionFactory.setBlockIfSessionPoolIsFull(poolConfig.blockIfSessionPoolIsFull);
     }
-
-    @Override
-    public void start() throws Exception {
-        log.info("Starting activeMQ client");
-        connectionFactory.start();
+    if (poolConfig.idleTimeoutMills != null) {
+      connectionFactory.setIdleTimeout(poolConfig.idleTimeoutMills);
     }
-
-    @Override
-    public void stop() throws Exception {
-        log.info("Stopping activeMQ client");
-        connectionFactory.stop();
+    if (poolConfig.expiryTimeoutMills != null) {
+      connectionFactory.setExpiryTimeout(poolConfig.expiryTimeoutMills);
     }
-
-    public ActiveMQSender createSender(String destination, boolean persistent) {
-        return createSender(destination, persistent, defaultTimeToLiveInSeconds);
+    if (poolConfig.createConnectionOnStartup != null) {
+      connectionFactory.setCreateConnectionOnStartup(poolConfig.createConnectionOnStartup);
     }
-
-    public ActiveMQSender createSender(String destination, boolean persistent, Optional<Integer> timeToLiveInSeconds) {
-        return new ActiveMQSenderImpl(connectionFactory, objectMapper, destination, timeToLiveInSeconds, persistent);
+    if (poolConfig.timeBetweenExpirationCheckMillis != null) {
+      connectionFactory.setTimeBetweenExpirationCheckMillis(poolConfig.timeBetweenExpirationCheckMillis);
     }
+  }
 
-    // This must be used during run-phase
-    public <T> void registerReceiver(String destination, ActiveMQReceiver<T> receiver, Class<? extends T> clazz,
-                                     final boolean ackMessageOnException) {
+  @Override public void initialize(Bootstrap<?> bootstrap) {
+  }
 
-        ActiveMQReceiverHandler<T> handler = new ActiveMQReceiverHandler<>(
-                destination,
-                connectionFactory,
-                receiver,
-                clazz,
-                objectMapper,
-                (message, exception) -> {
-                    if (ackMessageOnException) {
-                        log.error("Error processing received message - acknowledging it anyway", exception);
-                        return true;
-                    } else {
-                        log.error("Error processing received message - NOT acknowledging it", exception);
-                        return false;
-                    }
-                },
-                shutdownWaitInSeconds
-        );
+  @Override public void start() throws Exception {
+    log.info("Starting activeMQ client");
+    connectionFactory.start();
+  }
 
-        internalRegisterReceiver(destination, handler);
-    }
+  @Override public void stop() throws Exception {
+    log.info("Stopping activeMQ client");
+    connectionFactory.stop();
+  }
 
-    private <T> void internalRegisterReceiver(String destination, ActiveMQReceiverHandler<T> handler) {
-        environment.lifecycle().manage(handler);
-        environment.healthChecks().register("ActiveMQ receiver for " + destination, handler.getHealthCheck());
-    }
+  public ActiveMQSender createSender(String destination, boolean persistent) {
+    return createSender(destination, persistent, defaultTimeToLiveInSeconds);
+  }
 
-    // This must be used during run-phase
-    public <T> void registerReceiver(String destination, ActiveMQReceiver<T> receiver, Class<? extends T> clazz,
-                                     ActiveMQExceptionHandler exceptionHandler) {
+  public ActiveMQSender createSender(String destination, boolean persistent, Optional<Integer> timeToLiveInSeconds) {
+    return new ActiveMQSenderImpl(connectionFactory, objectMapper, destination, timeToLiveInSeconds, persistent);
+  }
 
-        ActiveMQReceiverHandler<T> handler = new ActiveMQReceiverHandler<>(
-                destination,
-                connectionFactory,
-                receiver,
-                clazz,
-                objectMapper,
-                exceptionHandler,
-                shutdownWaitInSeconds);
+  public <T extends java.lang.Object> void registerReceiver(String destination, ActiveMQReceiver<T> receiver, Class<? extends T> clazz, final boolean ackMessageOnException) {
+    ActiveMQReceiverHandler<T> handler = null;
+    handler = new ActiveMQReceiverHandler<T>(destination, connectionFactory, receiver, clazz, objectMapper, (message, exception) -> {
+      if (ackMessageOnException) {
+        log.error("Error processing received message - acknowledging it anyway", exception);
+        return true;
+      } else {
+        log.error("Error processing received message - NOT acknowledging it", exception);
+        return false;
+      }
+    }, shutdownWaitInSeconds);
+    internalRegisterReceiver(destination, handler);
+  }
 
-        internalRegisterReceiver(destination, handler);
-    }
+  private <T extends java.lang.Object> void internalRegisterReceiver(String destination, ActiveMQReceiverHandler<T> handler) {
+    environment.lifecycle().manage(handler);
+    environment.healthChecks().register("ActiveMQ receiver for " + destination, handler.getHealthCheck());
+  }
 
+  public <T extends java.lang.Object> void registerReceiver(String destination, ActiveMQReceiver<T> receiver, Class<? extends T> clazz, ActiveMQExceptionHandler exceptionHandler) {
+    ActiveMQReceiverHandler<T> handler = null;
+    handler = new ActiveMQReceiverHandler<T>(destination, connectionFactory, receiver, clazz, objectMapper, exceptionHandler, shutdownWaitInSeconds);
+    internalRegisterReceiver(destination, handler);
+  }
 }
--- src/main/java/com/kjetland/dropwizard/activemq/ActiveMQSenderImpl.java
+++ src/main/java/com/kjetland/dropwizard/activemq/ActiveMQSenderImpl.java
@@ -1,9 +1,7 @@
 package com.kjetland.dropwizard.activemq;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
 import javax.jms.DeliveryMode;
@@ -17,124 +15,98 @@ import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 
 public class ActiveMQSenderImpl implements ActiveMQSender {
+  private final Logger log = LoggerFactory.getLogger(getClass());
 
-    private final Logger log = LoggerFactory.getLogger(getClass());
-    private final ConnectionFactory connectionFactory;
-    private final ObjectMapper objectMapper;
-    private final String destination;
-    private final Optional<Integer> timeToLiveInSeconds;
-    private final boolean persistent;
-    protected final DestinationCreator destinationCreator = new DestinationCreatorImpl();
+  private final ConnectionFactory connectionFactory;
 
+  private final ObjectMapper objectMapper;
 
-    public ActiveMQSenderImpl(ConnectionFactory connectionFactory, ObjectMapper objectMapper, String destination,
-                              Optional<Integer> timeToLiveInSeconds, boolean persistent) {
-        this.connectionFactory = connectionFactory;
-        this.objectMapper = objectMapper;
-        this.destination = destination;
-        this.timeToLiveInSeconds = timeToLiveInSeconds;
-        this.persistent = persistent;
+  private final String destination;
+
+  private final Optional<Integer> timeToLiveInSeconds;
+
+  private final boolean persistent;
+
+  protected final DestinationCreator destinationCreator = new DestinationCreatorImpl();
+
+  public ActiveMQSenderImpl(ConnectionFactory connectionFactory, ObjectMapper objectMapper, String destination, Optional<Integer> timeToLiveInSeconds, boolean persistent) {
+    this.connectionFactory = connectionFactory;
+    this.objectMapper = objectMapper;
+    this.destination = destination;
+    this.timeToLiveInSeconds = timeToLiveInSeconds;
+    this.persistent = persistent;
+  }
+
+  @Override public void send(Object object) {
+    try {
+      final String json = objectMapper.writeValueAsString(object);
+      internalSend(json);
+    } catch (Exception e) {
+      throw new RuntimeException("Error sending to jms", e);
     }
+  }
 
-    @Override
-    public void send(Object object) {
+  @Override public void sendJson(String json) {
+    try {
+      internalSend(json);
+    } catch (Exception e) {
+      throw new RuntimeException("Error sending to jms", e);
+    }
+  }
+
+  private void internalSend(String json) throws JMSException {
+    log.info("Sending to {}: {}", destination, json);
+    final Connection connection = connectionFactory.createConnection();
+    try {
+      final Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+      try {
+        final Destination d = destinationCreator.create(session, destination);
+        final MessageProducer messageProducer = session.createProducer(d);
         try {
-
-            final String json = objectMapper.writeValueAsString(object);
-            internalSend(json);
-
-        } catch (Exception e) {
-            throw new RuntimeException("Error sending to jms", e);
+          messageProducer.setDeliveryMode(persistent ? DeliveryMode.PERSISTENT : DeliveryMode.NON_PERSISTENT);
+          if (timeToLiveInSeconds.isPresent()) {
+            messageProducer.setTimeToLive(TimeUnit.SECONDS.toMillis(timeToLiveInSeconds.get()));
+          }
+          final TextMessage textMessage = session.createTextMessage(json);
+          textMessage.setText(json);
+          messageProducer.send(textMessage);
+        }  finally {
+          ActiveMQUtils.silent(() -> messageProducer.close());
         }
-
+      }  finally {
+        ActiveMQUtils.silent(() -> session.close());
+      }
+    }  finally {
+      ActiveMQUtils.silent(() -> connection.close());
     }
+  }
 
-    @Override
-    public void sendJson(String json) {
+  @Override public void send(JMSFunction<Session, Message> messageCreator) {
+    try {
+      final Connection connection = connectionFactory.createConnection();
+      try {
+        final Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
         try {
-
-            internalSend(json);
-
-        } catch (Exception e) {
-            throw new RuntimeException("Error sending to jms", e);
-        }
-
-    }
-
-    private void internalSend(String json) throws JMSException {
-        log.info("Sending to {}: {}", destination, json);
-
-        // Since we're using the pooled connectionFactory,
-        // we can create connection, session and producer on the fly here.
-        // as long as we do the cleanup / return to pool
-
-        final Connection connection = connectionFactory.createConnection();
-        try {
-
-            final Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-            try {
-
-                final Destination d = destinationCreator.create(session, destination);
-                final MessageProducer messageProducer = session.createProducer(d);
-                try {
-                    messageProducer.setDeliveryMode(persistent ? DeliveryMode.PERSISTENT : DeliveryMode.NON_PERSISTENT);
-                    if (timeToLiveInSeconds.isPresent()) {
-                        messageProducer.setTimeToLive(TimeUnit.SECONDS.toMillis(timeToLiveInSeconds.get()));
-                    }
-
-                    final TextMessage textMessage = session.createTextMessage(json);
-                    textMessage.setText(json);
-                    messageProducer.send(textMessage);
-
-                } finally {
-                    ActiveMQUtils.silent(() -> messageProducer.close());
-                }
-            } finally {
-                ActiveMQUtils.silent(() -> session.close());
+          final Destination d = destinationCreator.create(session, destination);
+          final MessageProducer messageProducer = session.createProducer(d);
+          try {
+            messageProducer.setDeliveryMode(persistent ? DeliveryMode.PERSISTENT : DeliveryMode.NON_PERSISTENT);
+            if (timeToLiveInSeconds.isPresent()) {
+              messageProducer.setTimeToLive(TimeUnit.SECONDS.toMillis(timeToLiveInSeconds.get()));
             }
-
-        } finally {
-            ActiveMQUtils.silent(() -> connection.close());
+            final Message message = messageCreator.apply(session);
+            messageProducer.send(message);
+          }  finally {
+            ActiveMQUtils.silent(() -> messageProducer.close());
+          }
+        }  finally {
+          ActiveMQUtils.silent(() -> session.close());
         }
-
-
+      }  finally {
+        ActiveMQUtils.silent(() -> connection.close());
+      }
+    } catch (JMSException jmsException) {
+      throw new RuntimeException("Error sending to jms", jmsException);
     }
-
-    @Override
-    public void send(JMSFunction<Session, Message> messageCreator) {
-        // Since we're using the pooled connectionFactory,
-        // we can create connection, session and producer on the fly here.
-        // as long as we do the cleanup / return to pool
-
-        try {
-            final Connection connection = connectionFactory.createConnection();
-            try {
-
-                final Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-                try {
-
-                    final Destination d = destinationCreator.create(session, destination);
-                    final MessageProducer messageProducer = session.createProducer(d);
-                    try {
-                        messageProducer.setDeliveryMode(persistent ? DeliveryMode.PERSISTENT : DeliveryMode.NON_PERSISTENT);
-                        if (timeToLiveInSeconds.isPresent()) {
-                            messageProducer.setTimeToLive(TimeUnit.SECONDS.toMillis(timeToLiveInSeconds.get()));
-                        }
-                        final Message message = messageCreator.apply(session);
-                        messageProducer.send(message);
-
-                    } finally {
-                        ActiveMQUtils.silent(() -> messageProducer.close());
-                    }
-                } finally {
-                    ActiveMQUtils.silent(() -> session.close());
-                }
-
-            } finally {
-                ActiveMQUtils.silent(() -> connection.close());
-            }
-        } catch ( JMSException jmsException) {
-            throw new RuntimeException("Error sending to jms", jmsException);
-        }
-    }
+  }
 }
