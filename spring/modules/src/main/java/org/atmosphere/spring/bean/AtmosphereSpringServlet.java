0dab99ab653bac55da793cfab92948d15e547b4c
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 00:36:08 +0000

    Mining Framework Analysis

--- spring/modules/src/main/java/org/atmosphere/spring/bean/AtmosphereSpringServlet.java
+++ spring/modules/src/main/java/org/atmosphere/spring/bean/AtmosphereSpringServlet.java
@@ -1,26 +1,9 @@
-/*
- * Copyright 2015 Async-IO.org
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- */
 package org.atmosphere.spring.bean;
-
 import org.atmosphere.cpr.AtmosphereFramework;
 import org.atmosphere.cpr.AtmosphereRequestImpl;
 import org.atmosphere.cpr.AtmosphereResponseImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.context.support.WebApplicationContextUtils;
-
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -34,64 +17,44 @@ import java.io.IOException;
  * @author Evgeny Konovalov
  */
 public class AtmosphereSpringServlet extends HttpServlet {
+  private static final long serialVersionUID = 6755906261738522768L;
 
-    private static final long serialVersionUID = 6755906261738522768L;
+  @Autowired private AtmosphereFramework framework;
 
-    @Autowired
-    private AtmosphereFramework framework;
-    
-    @Autowired
-	private AtmosphereSpringContext atmosphereSpringContext;
+  @Autowired private AtmosphereSpringContext atmosphereSpringContext;
 
-    @Override
-    public void init(ServletConfig config) throws ServletException {
-        super.init(config);
-        
-     	WebApplicationContextUtils.
-                getRequiredWebApplicationContext(getServletContext()).
-                getAutowireCapableBeanFactory().
-                autowireBean(this);
-     	
-        // set the real servlet context
-        atmosphereSpringContext.setServletContext(config.getServletContext());
-     	framework.init(atmosphereSpringContext, false);
+  @Override public void init(ServletConfig config) throws ServletException {
+    super.init(config);
+    WebApplicationContextUtils.getRequiredWebApplicationContext(getServletContext()).getAutowireCapableBeanFactory().autowireBean(this);
+    atmosphereSpringContext.setServletContext(config.getServletContext());
+    framework.init(atmosphereSpringContext, false);
+  }
 
-    }
+  @Override public void doHead(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+    doPost(req, res);
+  }
 
-    @Override
-    public void doHead(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
-        doPost(req, res);
-    }
+  @Override public void doOptions(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+    doPost(req, res);
+  }
 
-    @Override
-    public void doOptions(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
-        doPost(req, res);
-    }
+  @Override public void doTrace(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+    doPost(req, res);
+  }
 
-    @Override
-    public void doTrace(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
-        doPost(req, res);
-    }
+  @Override public void doDelete(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+    doPost(req, res);
+  }
 
-    @Override
-    public void doDelete(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
-        doPost(req, res);
-    }
+  @Override public void doPut(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+    doPost(req, res);
+  }
 
-    @Override
-    public void doPut(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
-        doPost(req, res);
-    }
+  @Override public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+    doPost(req, res);
+  }
 
-    @Override
-    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
-        doPost(req, res);
-    }
-
-    @Override
-    public void doPost(HttpServletRequest req, HttpServletResponse res)
-            throws IOException, ServletException {
-        framework.doCometSupport(AtmosphereRequestImpl.wrap(req), AtmosphereResponseImpl.wrap(res));
-    }
-
+  @Override public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+    framework.doCometSupport(AtmosphereRequestImpl.wrap(req), AtmosphereResponseImpl.wrap(res));
+  }
 }
