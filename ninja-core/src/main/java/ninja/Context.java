f8bee39d88eb912dc497a767ce381fe24b1ee9c9
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 16:09:02 +0000

    Mining Framework Analysis

--- ninja-core/src/main/java/ninja/Context.java
+++ ninja-core/src/main/java/ninja/Context.java
@@ -1,22 +1,21 @@
 package ninja;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import ninja.bodyparser.BodyParserEngineJson;
 import ninja.bodyparser.BodyParserEngineManager;
 import ninja.session.FlashCookie;
 import ninja.session.SessionCookie;
-
 import java.util.Map;
 
 public interface Context {
+  enum HTTP_STATUS {
+    notFound404,
+    ok200,
+    forbidden403,
+    teapot418
+  }
 
-	enum HTTP_STATUS {
-		notFound404, ok200, forbidden403, teapot418
-	}
-
-	/**
+  /**
 	 * Returns the uri as seen by the server.
 	 * 
 	 * http://example.com/index would return
@@ -24,9 +23,9 @@ public interface Context {
 	 * 
 	 * @return the uri as seen by the server
 	 */
-    String getRequestUri();
+  String getRequestUri();
 
-    /**
+  /**
      * Returns the flash cookie. Flash cookies only live for one request.
      * Good uses are error messages to display. Almost everything else
      * is bad use of Flash Cookies.
@@ -35,9 +34,9 @@ public interface Context {
      * 
      * @return the flash cookie of that request.
      */
-    FlashCookie getFlashCookie();
-	
-    /**
+  FlashCookie getFlashCookie();
+
+  /**
      * Returns the client side session. It is a cookie. Therefore you
      * cannot store a lot of information inside the cookie. This is by intention.
      * 
@@ -46,16 +45,16 @@ public interface Context {
      * 
      * @return the Session of that request / response cycle.
      */
-	SessionCookie getSessionCookie();
+  SessionCookie getSessionCookie();
 
-	/**
+  /**
 	 * Simply redirect to another url.
 	 * 
 	 * @param url The url to redirect to
 	 */
-	void redirect(String url);
+  void redirect(String url);
 
-	/**
+  /**
 	 * Set the response content type.
 	 * 
 	 * The content type is usually also important because the rendering engine
@@ -66,9 +65,9 @@ public interface Context {
 	 * 
 	 * @param contentType examples are "application/json" or "text/html".
 	 */
-	void setContentType(String contentType);
+  void setContentType(String contentType);
 
-	/**
+  /**
 	 * Th explicitTemplateName is a fully qualified name of a template
 	 * from the root of the package. It includes the suffix of
 	 * a template (eg ftl.html).
@@ -81,122 +80,113 @@ public interface Context {
 	 * @param explicitTemplateName is something like "/views/ApplicationController/index.ftl.html"
 	 * @return the very same Context for chaining.
 	 */
-	Context template(String explicitTemplateName);
+  Context template(String explicitTemplateName);
 
-    /**
+  /**
      * Get the underlying HTTP servlet request
      *
      * @return The HTTP servlet request
      */
-	HttpServletRequest getHttpServletRequest();
+  HttpServletRequest getHttpServletRequest();
 
-    /**
+  /**
      * Get the underlying HTTP servlet response
      *
      * @return The HTTP servlet response
      */
-	HttpServletResponse getHttpServletResponse();
+  HttpServletResponse getHttpServletResponse();
 
-    /**
+  /**
      * Set the status code for the response
      *
      * @param httpStatus The status code
      * @return This context
      */
-	Context status(HTTP_STATUS httpStatus);
+  Context status(HTTP_STATUS httpStatus);
 
-    /**
+  /**
      * Get the path parameter for the given key
      *
      * @param key The key of the path parameter
      * @return The path parameter, or null if no such path parameter is defined
      */
-	String getPathParameter(String key);
+  String getPathParameter(String key);
 
-    /**
+  /**
      * Get the parameter with the given key from the request.  The parameter may either be a query parameter, or in the
      * case of form submissions, may be a form parameter
      *
      * @param key The key of the parameter
      * @return The value, or null if no parameter was found
      */
-    String getParameter(String key);
+  String getParameter(String key);
 
-    /**
+  /**
      * Get all the parameters from the request
      *
      * @return The parameters
      */
-    Map<String, String[]> getParameters();
+  Map<String, String[]> getParameters();
 
-    /**
+  /**
      * Get the request header with the given name
      *
      * @return The header value
      */
-    String getHeader(String name);
+  String getHeader(String name);
 
-    /**
+  /**
      * Get all the headers from the request
      *
      * @return The headers
      */
-    Map<String, String> getHeaders();
+  Map<String, String> getHeaders();
 
-    /**
+  /**
      * Get the name of the template that will be rendered for this request
      *
      * @return The name of the template to be rendered
      */
-	String getTemplateName();
+  String getTemplateName();
 
-    /**
+  /**
      * Render the result stored in this context
      */
-	void render();
+  void render();
 
-    /**
+  /**
      * Render the given object, using the appropriate template engine for the content type
      *
      * @param object The object to render
      * @throws IllegalArgumentException If no template engine could be found for the content type
      */
-    void render(Object object);
+  void render(Object object);
 
-    ///////////////////////////////////////////////////////////////////////////
-	// Convenience Methods to render a specific type. Html, Json and maybe Xml
-	// Uses no content negotation whatsoever
-	///////////////////////////////////////////////////////////////////////////
-	/**
+  /**
 	 * Renders something as text/html. Either uses a predefined template or
 	 * the template determined by auto configuration.
 	 */
-	void renderHtml();
+  void renderHtml();
 
-	/**
+  /**
 	 * Renders this object. Most likely the render engine will take a Map.
 	 * The default rendering engine is Freemarker. And Fremarker
 	 * will take a map of objects.
 	 * 
 	 * @param object The object to render as Html
 	 */
-	void renderHtml(Object object);
+  void renderHtml(Object object);
 
-	/**
+  /**
 	 * Render the object as json. Uses the underlying Json implementation.
 	 * By default we are using Gson. And Gson is accepting any Object and will
 	 * transform it into a Json response.
 	 * 
 	 * @param object The object to render as Json
 	 */
-	void renderJson(Object object);
-	
-	///////////////////////////////////////////////////////////////////////////
-	// Allows to get the nicely parsed content of the request.
-	// For instance if the content is a json you could simply get the json
-	// as Java object.
-	///////////////////////////////////////////////////////////////////////////
-	/**
+  void renderJson(Object object);
+
+  /**
 	 * This will give you the request body nicely parsed. You can register your
 	 * own parsers depending on the request type.
 	 * 
@@ -206,16 +196,15 @@ public interface Context {
 	 * @param classOfT The class of the result.
 	 * @return The parsed request or null if something went wrong.
 	 */
-	<T> T parseBody(Class<T> classOfT);
+  <T extends java.lang.Object> T parseBody(Class<T> classOfT);
 
-    /**
+  /**
      * Indicate that this request will be handled asynchronously
      */
-    void handleAsync();
+  void handleAsync();
 
-    /**
+  /**
      * Indicate that request processing of an async request is complete
      */
-    void requestComplete();
-
+  void requestComplete();
 }
--- ninja-core/src/main/java/ninja/ContextImpl.java
+++ ninja-core/src/main/java/ninja/ContextImpl.java
@@ -1,13 +1,11 @@
 package ninja;
-
 import java.io.IOException;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import com.google.common.collect.Iterables;
 import ninja.async.AsyncStrategy;
 import ninja.async.AsyncStrategyFactoryHolder;
 import ninja.bodyparser.BodyParserEngine;
@@ -16,282 +14,216 @@ import ninja.session.FlashCookie;
 import ninja.session.SessionCookie;
 import ninja.template.TemplateEngine;
 import ninja.template.TemplateEngineManager;
-
 import com.google.inject.Inject;
 
 public class ContextImpl implements Context {
+  private HttpServletRequest httpServletRequest;
 
-	private HttpServletRequest httpServletRequest;
+  private HttpServletResponse httpServletResponse;
 
-	private HttpServletResponse httpServletResponse;
+  private Router router;
 
-	private Router router;
+  private String templateName = null;
 
-	// * if set this template is used. otherwise the default mapping **/
-	private String templateName = null;
+  private HTTP_STATUS httpStatus;
 
-	private HTTP_STATUS httpStatus;
+  public String contentType;
 
-	public String contentType;
+  private AsyncStrategy asyncStrategy;
 
-    private AsyncStrategy asyncStrategy;
-    private final Object asyncLock = new Object();
+  private final Object asyncLock = new Object();
 
-	private final TemplateEngineManager templateEngineManager;
+  private final TemplateEngineManager templateEngineManager;
 
-	private final BodyParserEngineManager bodyParserEngineManager;
+  private final BodyParserEngineManager bodyParserEngineManager;
 
-	private final FlashCookie flashCookie;
+  private final FlashCookie flashCookie;
 
-	private final SessionCookie sessionCookie;
+  private final SessionCookie sessionCookie;
 
-	@Inject
-	public ContextImpl(BodyParserEngineManager bodyParserEngineManager,
-			FlashCookie flashCookie, Router router,
-			SessionCookie sessionCookie,
-			TemplateEngineManager templateEngineManager) {
+  @Inject public ContextImpl(BodyParserEngineManager bodyParserEngineManager, FlashCookie flashCookie, Router router, SessionCookie sessionCookie, TemplateEngineManager templateEngineManager) {
+    this.bodyParserEngineManager = bodyParserEngineManager;
+    this.flashCookie = flashCookie;
+    this.router = router;
+    this.sessionCookie = sessionCookie;
+    this.templateEngineManager = templateEngineManager;
+    this.httpStatus = HTTP_STATUS.ok200;
+  }
 
-		this.bodyParserEngineManager = bodyParserEngineManager;
-		this.flashCookie = flashCookie;
-		this.router = router;
-		this.sessionCookie = sessionCookie;
-		this.templateEngineManager = templateEngineManager;
+  public void init(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
+    this.httpServletRequest = httpServletRequest;
+    this.httpServletResponse = httpServletResponse;
+    flashCookie.init(this);
+    sessionCookie.init(this);
+  }
 
-		this.httpStatus = HTTP_STATUS.ok200;
-	}
+  public HttpServletRequest getHttpServletRequest() {
+    return httpServletRequest;
+  }
 
-	public void init(HttpServletRequest httpServletRequest,
-			HttpServletResponse httpServletResponse) {
-		this.httpServletRequest = httpServletRequest;
-		this.httpServletResponse = httpServletResponse;
+  public HttpServletResponse getHttpServletResponse() {
+    return this.httpServletResponse;
+  }
 
-		// init flash scope:
-		flashCookie.init(this);
+  @Override public Context template(String explicitTemplateName) {
+    this.templateName = explicitTemplateName;
+    return this;
+  }
 
-		// init session scope:
-		sessionCookie.init(this);
+  @Override public Context status(HTTP_STATUS httpStatus) {
+    this.httpStatus = httpStatus;
+    return this;
+  }
 
-	}
+  @Override public String getPathParameter(String key) {
+    Route route = router.getRouteFor(httpServletRequest.getMethod(), httpServletRequest.getServletPath());
+    return route.getParameters(httpServletRequest.getServletPath()).get(key);
+  }
 
-	public HttpServletRequest getHttpServletRequest() {
-		return httpServletRequest;
-	}
+  @Override public String getParameter(String key) {
+    return httpServletRequest.getParameter(key);
+  }
 
-	public HttpServletResponse getHttpServletResponse() {
-		return this.httpServletResponse;
-	}
+  @Override public Map<String, String[]> getParameters() {
+    return httpServletRequest.getParameterMap();
+  }
 
-	@Override
-	public Context template(String explicitTemplateName) {
-		this.templateName = explicitTemplateName;
-		return this;
-	}
+  @Override public String getHeader(String name) {
+    return httpServletRequest.getHeader(name);
+  }
 
-	@Override
-	public Context status(HTTP_STATUS httpStatus) {
-		this.httpStatus = httpStatus;
-		return this;
-	}
-
-	@Override
-	public String getPathParameter(String key) {
-
-		// FIXME: not really efficient...
-		Route route = router.getRouteFor(httpServletRequest.getMethod(),
-				httpServletRequest.getServletPath());
-
-		return route.getParameters(httpServletRequest.getServletPath())
-				.get(key);
-
-	}
-
-    @Override
-    public String getParameter(String key) {
-        return httpServletRequest.getParameter(key);
+  @Override public Map<String, String> getHeaders() {
+    Map<String, String> headers = new HashMap<String, String>();
+    Enumeration<String> enumeration = httpServletRequest.getHeaderNames();
+    while (enumeration.hasMoreElements()) {
+      String name = enumeration.nextElement();
+      headers.put(name, httpServletRequest.getHeader(name));
     }
+    return headers;
+  }
 
-    @Override
-    public Map<String, String[]> getParameters() {
-        return httpServletRequest.getParameterMap();
+  @Override public void redirect(String url) {
+    try {
+      httpServletResponse.sendRedirect(url);
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+  }
 
-    @Override
-    public String getHeader(String name) {
-        return httpServletRequest.getHeader(name);
+  @Override public void render() {
+    render(null);
+  }
+
+  @Override public void render(Object object) {
+    finalizeResponseHeaders(contentType);
+    TemplateEngine templateEngine = templateEngineManager.getTemplateEngineForContentType(contentType);
+    if (templateEngine == null) {
+      throw new IllegalArgumentException("No template engine found for content type " + contentType);
     }
+    templateEngine.invoke(this, object);
+  }
 
-    @Override
-    public Map<String, String> getHeaders() {
-        Map<String, String> headers = new HashMap<String, String>();
-        Enumeration<String> enumeration = httpServletRequest.getHeaderNames();
-        while (enumeration.hasMoreElements()) {
-            String name = enumeration.nextElement();
-            headers.put(name, httpServletRequest.getHeader(name));
-        }
-        return headers;
-    }
+  @Override public void renderHtml() {
+    renderHtml(new HashMap<String, String>());
+  }
 
-    @Override
-	public void redirect(String url) {
+  @Override public void renderHtml(Object object) {
+    finalizeResponseHeaders(ContentTypes.TEXT_HTML);
+    TemplateEngine templateEngine = templateEngineManager.getTemplateEngineForContentType(ContentTypes.TEXT_HTML);
+    templateEngine.invoke(this, object);
+  }
 
-		try {
-			httpServletResponse.sendRedirect(url);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+  @Override public void renderJson(Object object) {
+    finalizeResponseHeaders(ContentTypes.APPLICATION_JSON);
+    TemplateEngine templateEngine = templateEngineManager.getTemplateEngineForContentType(ContentTypes.APPLICATION_JSON);
+    templateEngine.invoke(this, object);
+  }
 
-	}
+  private void finalizeResponseHeaders(String contentType) {
+    setContentType(contentType);
+    setStatusOnResponse(httpStatus);
+    flashCookie.save(this);
+    sessionCookie.save(this);
+  }
 
-	@Override
-	public void render() {
-
-		render(null);
-
-	}
-
-	@Override
-	public void render(Object object) {
-
-		finalizeResponseHeaders(contentType);
-
-		TemplateEngine templateEngine = templateEngineManager
-				.getTemplateEngineForContentType(contentType);
-
-        if (templateEngine == null) {
-            throw new IllegalArgumentException("No template engine found for content type " + contentType);
-        }
-
-		templateEngine.invoke(this, object);
-
-	}
-
-	@Override
-	public void renderHtml() {
-		renderHtml(new HashMap<String, String>());
-	}
-
-	@Override
-	public void renderHtml(Object object) {
-		finalizeResponseHeaders(ContentTypes.TEXT_HTML);
-
-		TemplateEngine templateEngine = templateEngineManager
-				.getTemplateEngineForContentType(ContentTypes.TEXT_HTML);
-
-		templateEngine.invoke(this, object);
-
-	}
-
-	@Override
-	public void renderJson(Object object) {
-
-		finalizeResponseHeaders(ContentTypes.APPLICATION_JSON);
-
-		TemplateEngine templateEngine = templateEngineManager
-				.getTemplateEngineForContentType(ContentTypes.APPLICATION_JSON);
-
-		templateEngine.invoke(this, object);
-
-
-	}
-
-	private void finalizeResponseHeaders(String contentType) {
-		setContentType(contentType);
-		setStatusOnResponse(httpStatus);
-
-		flashCookie.save(this);
-		sessionCookie.save(this);
-
-	}
-
-	/**
+  /**
 	 * set status on response finally...
 	 * 
 	 * @param httpStatus
 	 */
-	private void setStatusOnResponse(HTTP_STATUS httpStatus) {
-
-		if (httpStatus.equals(HTTP_STATUS.ok200)) {
-			httpServletResponse.setStatus(200);
-		} else if (httpStatus.equals(HTTP_STATUS.notFound404)) {
-			httpServletResponse.setStatus(404);
-		} else if (httpStatus.equals(HTTP_STATUS.forbidden403)) {
-			httpServletResponse.setStatus(403);
-		} else if (httpStatus.equals(HTTP_STATUS.teapot418)) {
-			httpServletResponse.setStatus(418);
-		}
-
-	}
-
-	@Override
-	public void setContentType(String contentType) {
-	    this.contentType = contentType;
-		httpServletResponse.setContentType(contentType);
-
-	}
-
-	@Override
-	public String getTemplateName() {
-		return templateName;
-	}
-
-	@Override
-	public <T> T parseBody(Class<T> classOfT) {
-
-		BodyParserEngine bodyParserEngine = bodyParserEngineManager
-				.getBodyParserEngineForContentType(ContentTypes.APPLICATION_JSON);
-
-		if (bodyParserEngine == null) {
-			return null;
-		}
-
-		return bodyParserEngine.invoke(this, classOfT);
-
-	}
-
-	@Override
-	public FlashCookie getFlashCookie() {
-		return flashCookie;
-	}
-
-	@Override
-	public SessionCookie getSessionCookie() {
-		return sessionCookie;
-	}
-
-    @Override
-    public String getRequestUri() {
-        return getHttpServletRequest().getRequestURI();
-    }
-
-    @Override
-    public void handleAsync() {
-        synchronized (asyncLock) {
-            if (asyncStrategy == null) {
-                asyncStrategy = AsyncStrategyFactoryHolder.INSTANCE.createStrategy(httpServletRequest);
-                asyncStrategy.handleAsync();
-            }
+  private void setStatusOnResponse(HTTP_STATUS httpStatus) {
+    if (httpStatus.equals(HTTP_STATUS.ok200)) {
+      httpServletResponse.setStatus(200);
+    } else {
+      if (httpStatus.equals(HTTP_STATUS.notFound404)) {
+        httpServletResponse.setStatus(404);
+      } else {
+        if (httpStatus.equals(HTTP_STATUS.forbidden403)) {
+          httpServletResponse.setStatus(403);
+        } else {
+          if (httpStatus.equals(HTTP_STATUS.teapot418)) {
+            httpServletResponse.setStatus(418);
+          }
         }
+      }
     }
+  }
 
-    @Override
-    public void requestComplete() {
-        synchronized (asyncLock) {
-            if (asyncStrategy == null) {
-                throw new IllegalStateException("Request complete called on non async request");
-            }
-            asyncStrategy.requestComplete();
-        }
+  @Override public void setContentType(String contentType) {
+    this.contentType = contentType;
+    httpServletResponse.setContentType(contentType);
+  }
+
+  @Override public String getTemplateName() {
+    return templateName;
+  }
+
+  @Override public <T extends java.lang.Object> T parseBody(Class<T> classOfT) {
+    BodyParserEngine bodyParserEngine = bodyParserEngineManager.getBodyParserEngineForContentType(ContentTypes.APPLICATION_JSON);
+    if (bodyParserEngine == null) {
+      return null;
     }
+    return bodyParserEngine.invoke(this, classOfT);
+  }
 
-    /**
+  @Override public FlashCookie getFlashCookie() {
+    return flashCookie;
+  }
+
+  @Override public SessionCookie getSessionCookie() {
+    return sessionCookie;
+  }
+
+  @Override public String getRequestUri() {
+    return getHttpServletRequest().getRequestURI();
+  }
+
+  @Override public void handleAsync() {
+    synchronized (asyncLock) {
+      if (asyncStrategy == null) {
+        asyncStrategy = AsyncStrategyFactoryHolder.INSTANCE.createStrategy(httpServletRequest);
+        asyncStrategy.handleAsync();
+      }
+    }
+  }
+
+  @Override public void requestComplete() {
+    synchronized (asyncLock) {
+      if (asyncStrategy == null) {
+        throw new IllegalStateException("Request complete called on non async request");
+      }
+      asyncStrategy.requestComplete();
+    }
+  }
+
+  /**
      * Used to indicate that the controller has finished executing
      */
-    public void controllerReturned() {
-        synchronized (asyncLock) {
-            if (asyncStrategy != null) {
-                asyncStrategy.controllerReturned();
-            }
-        }
+  public void controllerReturned() {
+    synchronized (asyncLock) {
+      if (asyncStrategy != null) {
+        asyncStrategy.controllerReturned();
+      }
     }
+  }
 }
