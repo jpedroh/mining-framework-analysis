ef481598d986bad4a3303b9578f1b67f0a2071e2
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 01:50:45 +0000

    Mining Framework Analysis

--- src/main/java/nl/sijpesteijn/testing/fitnesse/plugins/FitNesseStarterMojo.java
+++ src/main/java/nl/sijpesteijn/testing/fitnesse/plugins/FitNesseStarterMojo.java
@@ -1,5 +1,4 @@
 package nl.sijpesteijn.testing.fitnesse.plugins;
-
 import nl.sijpesteijn.testing.fitnesse.plugins.runner.FitNesseCommander;
 import nl.sijpesteijn.testing.fitnesse.plugins.utils.MafiaException;
 import org.apache.maven.plugin.MojoExecutionException;
@@ -9,40 +8,30 @@ import org.apache.maven.plugins.annotations.Mojo;
 /**
  * Goal to start a FitNesse instance.
  */
-@Mojo(name = "start")
-public class FitNesseStarterMojo extends AbstractStartFitNesseMojo {
-
-    /**
+@Mojo(name = "start") public class FitNesseStarterMojo extends AbstractStartFitNesseMojo {
+  /**
      * {@inheritDoc}
      */
-    @Override
-    public final void execute() throws MojoExecutionException, MojoFailureException {
-        getLog().debug(toString());
-        final FitNesseCommander commander = new FitNesseCommander(getCommanderConfig(getJvmDependencies(),
-                getJvmArguments(),
-                getRetainDays(), getFitNessePort(), getFitNesseAuthenticate()));
-        try {
-            commander.start();
-        } catch (MafiaException me) {
-            throw new MojoExecutionException(me.getMessage(), me);
-        }
-        if (commander.hasError()) {
-            logErrorMessages(commander.getOutput(), commander.getErrorOutput());
-            throw new MojoExecutionException("Could not start FitNesse: (error output: " + commander.getErrorOutput()
-                    + ", output: " + commander.getOutput() + ")");
-        } else {
-            getLog().info("FitNesse start on: http://localhost:" + getFitNessePort());
-        }
+  @Override public final void execute() throws MojoExecutionException, MojoFailureException {
+    getLog().debug(toString());
+    final FitNesseCommander commander = new FitNesseCommander(getCommanderConfig(getJvmDependencies(), getJvmArguments(), getRetainDays(), getFitNessePort(), getFitNesseAuthenticate()));
+    try {
+      commander.start();
+    } catch (MafiaException me) {
+      throw new MojoExecutionException(me.getMessage(), me);
     }
+    if (commander.hasError()) {
+      logErrorMessages(commander.getOutput(), commander.getErrorOutput());
+      throw new MojoExecutionException("Could not start FitNesse: (error output: " + commander.getErrorOutput() + ", output: " + commander.getOutput() + ")");
+    } else {
+      getLog().info("FitNesse start on: http://localhost:" + getFitNessePort());
+    }
+  }
 
-    /**
+  /**
      * {@inheritDoc}
      */
-    @Override
-    public final String toString() {
-        return super.toString()
-                + ", Retain days: " + getRetainDays() + ", JVM arguments: " + getJvmArguments()
-                + ", JVM dependencies: " + getJvmDependencies();
-    }
-
+  @Override public final String toString() {
+    return super.toString() + ", Retain days: " + getRetainDays() + ", JVM arguments: " + getJvmArguments() + ", JVM dependencies: " + getJvmDependencies();
+  }
 }
--- src/main/java/nl/sijpesteijn/testing/fitnesse/plugins/runner/FitNesseCommander.java
+++ src/main/java/nl/sijpesteijn/testing/fitnesse/plugins/runner/FitNesseCommander.java
@@ -1,8 +1,6 @@
 package nl.sijpesteijn.testing.fitnesse.plugins.runner;
-
 import nl.sijpesteijn.testing.fitnesse.plugins.utils.MafiaException;
 import org.apache.commons.lang.StringUtils;
-
 import java.io.File;
 import java.util.List;
 
@@ -10,198 +8,168 @@ import java.util.List;
  * FitNesseCommander interface.
  */
 public class FitNesseCommander {
-
-    /**
+  /**
      * Fitnesse commander configuration.
      */
-    private final FitNesseCommanderConfig commanderConfig;
+  private final FitNesseCommanderConfig commanderConfig;
 
-    /**
+  /**
      * Error stream monitor.
      */
-    private StreamToBufferMonitor errorMonitor;
+  private StreamToBufferMonitor errorMonitor;
 
-    /**
+  /**
      * Input stream monitor.
      */
-    private StreamToBufferMonitor inputMonitor;
+  private StreamToBufferMonitor inputMonitor;
 
-    /**
+  /**
      * Process.
      */
-    private Process process;
+  private Process process;
 
-    /**
+  /**
      * Constructor.
      *
      * @param commanderConfig - The command configuration.
      */
-    public FitNesseCommander(final FitNesseCommanderConfig commanderConfig) {
-        this.commanderConfig = commanderConfig;
-    }
+  public FitNesseCommander(final FitNesseCommanderConfig commanderConfig) {
+    this.commanderConfig = commanderConfig;
+  }
 
-    /**
+  /**
      * Start FitNesse.
      *
      * @throws MafiaException thrown in case of an error.
      */
-    public final void start() throws MafiaException {
-        String logArgument = "";
-        if (commanderConfig.getFitNesseLogDirectory() != null) {
-            logArgument = " -l " + commanderConfig.getFitNesseLogDirectory();
-        }
-        String authArgument = "";
-        if (commanderConfig.getFitNesseAuthenticate() != null) {
-        	authArgument = " -a " + commanderConfig.getFitNesseAuthenticate();
-        }
-        
-        String updatePreventsArgument = "";
-        if (commanderConfig.getFitNesseUpdatePrevents() != null && commanderConfig.getFitNesseUpdatePrevents()) {
-        	updatePreventsArgument = " -o";
-        }
-        
-        final String command = "java" + getJVMArguments(commanderConfig.getJvmArguments())
-                + " -cp " + commanderConfig.getClasspathString()
-                + " fitnesseMain.FitNesseMain -p " + commanderConfig.getFitNessePort()
-                + " -d " + commanderConfig.getWikiRoot()
-                + " -r " + commanderConfig.getNameRootPage()
-                + " -e " + commanderConfig.getRetainDays()
-                + logArgument
-                + authArgument
-                + updatePreventsArgument;
-        
-        commanderConfig.getLog().info("Starting FitNesse. This could take some more seconds when first used....");
-       
-
-        this.commanderConfig.getLog().debug("Starting FitNesse with Command:" + command);
-        
-        run(command);
+  public final void start() throws MafiaException {
+    String logArgument = "";
+    if (commanderConfig.getFitNesseLogDirectory() != null) {
+      logArgument = " -l " + commanderConfig.getFitNesseLogDirectory();
     }
+    String authArgument = "";
+    if (commanderConfig.getFitNesseAuthenticate() != null) {
+      authArgument = " -a " + commanderConfig.getFitNesseAuthenticate();
+    }
+    String updatePreventsArgument = "";
+    if (commanderConfig.getFitNesseUpdatePrevents() != null && commanderConfig.getFitNesseUpdatePrevents()) {
+      updatePreventsArgument = " -o";
+    }
+    final String command = "java" + getJVMArguments(commanderConfig.getJvmArguments()) + " -cp " + commanderConfig.getClasspathString() + " fitnesseMain.FitNesseMain -p " + commanderConfig.getFitNessePort() + " -d " + commanderConfig.getWikiRoot() + " -r " + commanderConfig.getNameRootPage() + " -e " + commanderConfig.getRetainDays() + logArgument + authArgument + updatePreventsArgument;
+    commanderConfig.getLog().info("Starting FitNesse. This could take some more seconds when first used....");
+    this.commanderConfig.getLog().debug("Starting FitNesse with Command:" + command);
+    run(command);
+  }
 
-    /**
+  /**
      * Stop FitNesse.
      *
      * @throws MafiaException thrown in case of an error.
      */
-    public final void stop() throws MafiaException {
-    	
-        String authArgument = "";
-        if (commanderConfig.getFitNesseAuthenticate() != null 
-          // in Shutdown fitnesse does not support user/paassword reading from file
-          // so we detect a valid user:password with the ":" separator
-          && commanderConfig.getFitNesseAuthenticate().contains(":")) {
-        	String userPasswordArgument = commanderConfig.getFitNesseAuthenticate().replaceFirst(":", " ");
-        	authArgument = " -c " + userPasswordArgument;
-        }
-    	
-
-        final String command = "java -cp " + commanderConfig.getClasspathString() + " fitnesse.Shutdown -p "
-                + commanderConfig.getFitNessePort()
-                + authArgument;
-
-        this.commanderConfig.getLog().debug("Stopping FitNesse with Command:" + command);
-        run(command);
+  public final void stop() throws MafiaException {
+    String authArgument = "";
+    if (commanderConfig.getFitNesseAuthenticate() != null && commanderConfig.getFitNesseAuthenticate().contains(":")) {
+      String userPasswordArgument = commanderConfig.getFitNesseAuthenticate().replaceFirst(":", " ");
+      authArgument = " -c " + userPasswordArgument;
     }
+    final String command = "java -cp " + commanderConfig.getClasspathString() + " fitnesse.Shutdown -p " + commanderConfig.getFitNessePort() + authArgument;
+    this.commanderConfig.getLog().debug("Stopping FitNesse with Command:" + command);
+    run(command);
+  }
 
-    /**
+  /**
      * Run the command.
      *
      * @param command - command.
      * @throws MafiaException - unable to execute command.
      */
-    private void run(final String command) throws MafiaException {
-        try {
-            commanderConfig.getLog().debug("Running command: " + command);
-            process = Runtime.getRuntime().exec(command, null, new File(commanderConfig.getWikiRoot()));
-            errorMonitor = new StreamToBufferMonitor(process.getErrorStream());
-            new Thread(errorMonitor).start();
-            inputMonitor = new StreamToBufferMonitor(process.getInputStream());
-            new Thread(inputMonitor).start();
-            waitForProcess();
-        } catch (Exception e) {
-            throw new MafiaException("Could not run command.", e);
-        }
+  private void run(final String command) throws MafiaException {
+    try {
+      commanderConfig.getLog().debug("Running command: " + command);
+      process = Runtime.getRuntime().exec(command, null, new File(commanderConfig.getWikiRoot()));
+      errorMonitor = new StreamToBufferMonitor(process.getErrorStream());
+      new Thread(errorMonitor).start();
+      inputMonitor = new StreamToBufferMonitor(process.getInputStream());
+      new Thread(inputMonitor).start();
+      waitForProcess();
+    } catch (Exception e) {
+      throw new MafiaException("Could not run command.", e);
     }
+  }
 
-    /**
+  /**
      * Wait for process.
      *
      * @throws InterruptedException - unable to check process status.
      */
-    @SuppressWarnings("PMD")
-    private void waitForProcess() throws InterruptedException {
-        while (true) {
-            try {
-                process.exitValue();
-                return;
-            } catch (IllegalThreadStateException itse) {
-                // process has not finished yet.
-            }
-            if (inputMonitor.isFinished()) {
-                if (errorMonitor.getBuffer().toString().trim()
-                        .endsWith("Unpacking new version of FitNesse resources. Please be patient.")) {
-                    Thread.sleep(commanderConfig.getUnpackWaitTime());
-                }
-                return;
-            }
-            Thread.sleep(commanderConfig.getUnpackWaitTime());
+  @SuppressWarnings(value = { "PMD" }) private void waitForProcess() throws InterruptedException {
+    while (true) {
+      try {
+        process.exitValue();
+        return;
+      } catch (IllegalThreadStateException itse) {
+      }
+      if (inputMonitor.isFinished()) {
+        if (errorMonitor.getBuffer().toString().trim().endsWith("Unpacking new version of FitNesse resources. Please be patient.")) {
+          Thread.sleep(commanderConfig.getUnpackWaitTime());
         }
+        return;
+      }
+      Thread.sleep(commanderConfig.getUnpackWaitTime());
     }
+  }
 
-    /**
+  /**
      * Get jvm arguments.
      *
      * @param arguments - jvm arguments.
      * @return - jvm argument string.
      */
-    private String getJVMArguments(final List<String> arguments) {
-        if (arguments == null) {
-            return "";
-        }
-        StringBuffer list = new StringBuffer(" ");
-        for (final String argument : arguments) {
-            if (argument.startsWith("-")) {
-                list.append(argument);
-                list.append(" ");
-            } else {
-                list.append(" -D");
-                list.append(argument);
-                list.append(" ");
-            }
-        }
-        return list.toString();
+  private String getJVMArguments(final List<String> arguments) {
+    if (arguments == null) {
+      return "";
     }
+    StringBuffer list = new StringBuffer(" ");
+    for (final String argument : arguments) {
+      if (argument.startsWith("-")) {
+        list.append(argument);
+        list.append(" ");
+      } else {
+        list.append(" -D");
+        list.append(argument);
+        list.append(" ");
+      }
+    }
+    return list.toString();
+  }
 
-    /**
+  /**
      * Return the error output.
      *
      * @return {@link java.lang.String}
      */
-    public final String getErrorOutput() {
-        return errorMonitor.getBuffer().toString();
-    }
+  public final String getErrorOutput() {
+    return errorMonitor.getBuffer().toString();
+  }
 
-    /**
+  /**
      * Return the output.
      *
      * @return {@link java.lang.String}
      */
-    public final String getOutput() {
-        return inputMonitor.getBuffer().toString();
-    }
+  public final String getOutput() {
+    return inputMonitor.getBuffer().toString();
+  }
 
-    /**
+  /**
      * Indication if an error occurred.
      *
      * @return boolean
      */
-    public final boolean hasError() {
-        if (inputMonitor.getBuffer().toString().trim().contains("Started...")
-                || errorMonitor.getBuffer().toString().trim().contains("Please be patient.") || StringUtils
-                .isEmpty(errorMonitor.getBuffer().toString())
-                || inputMonitor.getBuffer().toString().contains("Bootstrapping FitNesse, the fully integrated standalone wiki and acceptance testing framework.")) {
-            return false;
-        }
-        return true;
+  public final boolean hasError() {
+    if (inputMonitor.getBuffer().toString().trim().contains("Started...") || errorMonitor.getBuffer().toString().trim().contains("Please be patient.") || StringUtils.isEmpty(errorMonitor.getBuffer().toString()) || inputMonitor.getBuffer().toString().contains("Bootstrapping FitNesse, the fully integrated standalone wiki and acceptance testing framework.")) {
+      return false;
     }
+    return true;
+  }
 }
