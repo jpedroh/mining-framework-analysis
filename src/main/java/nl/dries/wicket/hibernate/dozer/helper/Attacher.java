65783e634f9e80a0ce8a7c299359d9ffeeb37241
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 07:32:19 +0000

    Mining Framework Analysis

--- src/main/java/nl/dries/wicket/hibernate/dozer/helper/Attacher.java
+++ src/main/java/nl/dries/wicket/hibernate/dozer/helper/Attacher.java
@@ -1,12 +1,9 @@
 package nl.dries.wicket.hibernate.dozer.helper;
-
 import java.io.Serializable;
-
 import nl.dries.wicket.hibernate.dozer.SessionFinder;
 import nl.dries.wicket.hibernate.dozer.properties.AbstractPropertyDefinition;
 import nl.dries.wicket.hibernate.dozer.properties.CollectionPropertyDefinition;
 import nl.dries.wicket.hibernate.dozer.properties.SimplePropertyDefinition;
-
 import org.hibernate.EntityMode;
 import org.hibernate.LockMode;
 import org.hibernate.collection.PersistentCollection;
@@ -26,183 +23,125 @@ import org.hibernate.type.TypeHelper;
  * 
  * @author dries
  */
-public class Attacher
-{
-	/** The Hibernate session */
-	private final SessionFinder sessionFinder;
+public class Attacher {
+  /** The Hibernate session */
+  private final SessionFinder sessionFinder;
 
-	/** The property to attach */
-	private final AbstractPropertyDefinition propertyDefinition;
+  /** The property to attach */
+  private final AbstractPropertyDefinition propertyDefinition;
 
-	/** The proxy, that will be replaced by attaching */
-	private final Object proxy;
+  /** The proxy, that will be replaced by attaching */
+  private final Object proxy;
 
-	/**
+  /**
 	 * Construct
 	 * 
 	 * @param def
 	 *            the {@link AbstractPropertyDefinition}
-	 * @param proxy
-	 *            the current, proxied, value
 	 */
-	public Attacher(AbstractPropertyDefinition def, Object proxy)
-	{
-		this.propertyDefinition = def;
-		this.sessionFinder = def.getModelCallback().getSessionFinder();
-		this.proxy = proxy;
-	}
+  public Attacher(AbstractPropertyDefinition def, Object proxy) {
+    this.propertyDefinition = def;
+    this.sessionFinder = def.getModelCallback().getSessionFinder();
+    this.proxy = proxy;
+  }
 
-	/**
+  /**
 	 * Attach a property
 	 * 
 	 * @param def
 	 *            the {@link SimplePropertyDefinition}
 	 * @return the value of the property
 	 */
-	protected Object attach(SimplePropertyDefinition def)
-	{
-		SessionImplementor sessionImpl = (SessionImplementor) sessionFinder.getHibernateSession(def
-			.getHibernateProperty().getEntityClass());
+  protected Object attach(SimplePropertyDefinition def) {
+    SessionImplementor sessionImpl = (SessionImplementor) sessionFinder.getHibernateSession(def.getHibernateProperty().getEntityClass());
+    EntityPersister persister = getPersister(def.getHibernateProperty(), sessionImpl);
+    PersistenceContext persistenceContext = sessionImpl.getPersistenceContext();
+    EntityKey key = new EntityKey(def.getHibernateProperty().getId(), persister, EntityMode.POJO);
+    Object instance = persistenceContext.getEntity(key);
+    if (instance == null) {
+      Object existing = persistenceContext.getProxy(key);
+      if (existing == null) {
+        instance = persister.createProxy(def.getHibernateProperty().getId(), sessionImpl);
+        persistenceContext.getBatchFetchQueue().addBatchLoadableEntityKey(key);
+        persistenceContext.addProxy(key, instance);
+      } else {
+        instance = existing;
+      }
+    }
+    return instance;
+  }
 
-		EntityPersister persister = getPersister(def.getHibernateProperty(), sessionImpl);
-		PersistenceContext persistenceContext = sessionImpl.getPersistenceContext();
-
-		EntityKey key = new EntityKey(def.getHibernateProperty().getId(), persister, EntityMode.POJO);
-
-		// Check existing instance
-		Object instance = persistenceContext.getEntity(key);
-
-		// No instance found
-		if (instance == null)
-		{
-			// Also no proxy found, generate new one
-			Object existing = persistenceContext.getProxy(key);
-			if (existing == null)
-			{
-				instance = persister.createProxy(def.getHibernateProperty().getId(), sessionImpl);
-				persistenceContext.getBatchFetchQueue().addBatchLoadableEntityKey(key);
-				persistenceContext.addProxy(key, instance);
-			}
-			else
-			{
-				instance = existing;
-			}
-		}
-
-		// Set the property to te real value, or a proxy
-		return instance;
-	}
-
-	/**
+  /**
 	 * Attach a collection
 	 * 
 	 * @param def
 	 *            the {@link CollectionPropertyDefinition}
 	 */
-	protected Object attach(CollectionPropertyDefinition def)
-	{
-		SessionImplementor sessionImpl = (SessionImplementor) sessionFinder.getHibernateSession(def.getOwner()
-			.getClass());
+  protected Object attach(CollectionPropertyDefinition def) {
+    SessionImplementor sessionImpl = (SessionImplementor) sessionFinder.getHibernateSession(def.getOwner().getClass());
+    CollectionPersister persister = getCollectionPersister(def, sessionImpl);
+    PersistenceContext persistenceContext = sessionImpl.getPersistenceContext();
+    ClassMetadata metadata = sessionImpl.getFactory().getClassMetadata(def.getOwner().getClass());
+    Serializable identifier = metadata.getIdentifier(def.getOwner(), sessionImpl);
+    PersistentCollection collection = def.getCollectionType().createCollection(sessionImpl);
+    collection.setOwner(def.getOwner());
+    collection.setSnapshot(identifier, def.getRole(), null);
+    persistenceContext.addUninitializedCollection(persister, collection, identifier);
+    EntityKey key = new EntityKey(identifier, getOwnPersister(def.getOwner(), sessionImpl), EntityMode.POJO);
+    if (!persistenceContext.containsEntity(key)) {
+      EntityPersister ownPersister = getOwnPersister(def.getOwner(), sessionImpl);
+      Object[] values = ownPersister.getPropertyValues(def.getOwner(), EntityMode.POJO);
+      TypeHelper.deepCopy(values, ownPersister.getPropertyTypes(), ownPersister.getPropertyUpdateability(), values, sessionImpl);
+      Object version = Versioning.getVersion(values, ownPersister);
+      persistenceContext.addEntity(def.getOwner(), (ownPersister.isMutable() ? Status.MANAGED : Status.READ_ONLY), values, key, version, LockMode.NONE, true, ownPersister, false, true);
+    }
+    persistenceContext.getCollectionEntries().remove(proxy);
+    return collection;
+  }
 
-		CollectionPersister persister = getCollectionPersister(def, sessionImpl);
-		PersistenceContext persistenceContext = sessionImpl.getPersistenceContext();
-
-		ClassMetadata metadata = sessionImpl.getFactory().getClassMetadata(def.getOwner().getClass());
-		Serializable identifier = metadata.getIdentifier(def.getOwner(), sessionImpl);
-
-		PersistentCollection collection = def.getCollectionType().createCollection(sessionImpl);
-		collection.setOwner(def.getOwner());
-		collection.setSnapshot(identifier, def.getRole(), null); // Sort of 'fake' state...
-
-		persistenceContext.addUninitializedCollection(persister, collection, identifier);
-
-		// Possibly re-attach owner
-		EntityKey key = new EntityKey(identifier, getOwnPersister(def.getOwner(), sessionImpl), EntityMode.POJO);
-		if (!persistenceContext.containsEntity(key))
-		{
-			EntityPersister ownPersister = getOwnPersister(def.getOwner(), sessionImpl);
-
-			Object[] values = ownPersister.getPropertyValues(def.getOwner(), EntityMode.POJO);
-			TypeHelper.deepCopy(
-				values,
-				ownPersister.getPropertyTypes(),
-				ownPersister.getPropertyUpdateability(),
-				values,
-				sessionImpl
-				);
-			Object version = Versioning.getVersion(values, ownPersister);
-
-			persistenceContext.addEntity(
-				def.getOwner(),
-				(ownPersister.isMutable() ? Status.MANAGED : Status.READ_ONLY),
-				values,
-				key,
-				version,
-				LockMode.NONE,
-				true,
-				ownPersister,
-				false,
-				true // will be ignored, using the existing Entry instead
-				);
-		}
-
-		// Remove proxy from the Hibernate context
-		persistenceContext.getCollectionEntries().remove(proxy);
-
-		// Return value
-		return collection;
-	}
-
-	/**
+  /**
 	 * Returns a {@link EntityPersister} for the given entity class
 	 * 
 	 * @param val
 	 *            the {@link HibernateProperty}
 	 * @return {@link EntityPersister}
 	 */
-	protected EntityPersister getPersister(HibernateProperty val, SessionImplementor sessionImpl)
-	{
-		SessionFactoryImplementor factory = sessionImpl.getFactory();
-		return factory.getEntityPersister(val.getEntityClass().getName());
-	}
+  protected EntityPersister getPersister(HibernateProperty val, SessionImplementor sessionImpl) {
+    SessionFactoryImplementor factory = sessionImpl.getFactory();
+    return factory.getEntityPersister(val.getEntityClass().getName());
+  }
 
-	/**
+  /**
 	 * Returns a {@link CollectionPersister} for the given entity class
 	 * 
 	 * @param def
 	 *            the {@link AbstractPropertyDefinition}
 	 * @return a {@link CollectionPersister}
 	 */
-	protected CollectionPersister getCollectionPersister(CollectionPropertyDefinition def,
-		SessionImplementor sessionImpl)
-	{
-		SessionFactoryImplementor factory = sessionImpl.getFactory();
-		return factory.getCollectionPersister(def.getRole());
-	}
+  protected CollectionPersister getCollectionPersister(CollectionPropertyDefinition def, SessionImplementor sessionImpl) {
+    SessionFactoryImplementor factory = sessionImpl.getFactory();
+    return factory.getCollectionPersister(def.getRole());
+  }
 
-	/**
+  /**
 	 * @param owner
 	 *            the owner
 	 * @return the {@link EntityPersister} for the object to attach
 	 */
-	protected EntityPersister getOwnPersister(Object owner, SessionImplementor sessionImpl)
-	{
-		SessionFactoryImplementor factory = sessionImpl.getFactory();
-		return factory.getEntityPersister(owner.getClass().getName());
-	}
+  protected EntityPersister getOwnPersister(Object owner, SessionImplementor sessionImpl) {
+    SessionFactoryImplementor factory = sessionImpl.getFactory();
+    return factory.getEntityPersister(owner.getClass().getName());
+  }
 
-	/**
+  /**
 	 * Attach driver
 	 * 
 	 * @return the attached object
 	 */
-	public Object attach()
-	{
-		if (propertyDefinition instanceof SimplePropertyDefinition)
-		{
-			return attach((SimplePropertyDefinition) propertyDefinition);
-		}
-
-		return attach((CollectionPropertyDefinition) propertyDefinition);
-	}
+  public Object attach() {
+    if (propertyDefinition instanceof SimplePropertyDefinition) {
+      return attach((SimplePropertyDefinition) propertyDefinition);
+    }
+    return attach((CollectionPropertyDefinition) propertyDefinition);
+  }
 }
--- src/main/java/nl/dries/wicket/hibernate/dozer/helper/HibernateCollectionType.java
+++ src/main/java/nl/dries/wicket/hibernate/dozer/helper/HibernateCollectionType.java
@@ -1,5 +1,4 @@
 package nl.dries.wicket.hibernate.dozer.helper;
-
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
@@ -9,7 +8,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
-
 import org.hibernate.collection.PersistentBag;
 import org.hibernate.collection.PersistentCollection;
 import org.hibernate.collection.PersistentMap;
@@ -23,143 +21,109 @@ import org.slf4j.LoggerFactory;
  * Hibernate collection types
  * 
  * @author dries
- */
-public enum HibernateCollectionType
-{
-	/** */
-	LIST(PersistentBag.class, ArrayList.class, List.class),
-	/** */
-	SET(PersistentSet.class, HashSet.class, Set.class),
-	/** */
-	SORTED_SET(PersistentSortedSet.class, TreeSet.class, Set.class),
-	/** */
-	MAP(PersistentMap.class, HashMap.class, Map.class);
+ */public enum HibernateCollectionType {
+  LIST(PersistentBag.class, ArrayList.class, List.class),
+  SET(PersistentSet.class, HashSet.class, Set.class),
+  SORTED_SET(PersistentSortedSet.class, TreeSet.class, Set.class),
+  MAP(PersistentMap.class, HashMap.class, Map.class)
+  ;
 
-	/** Logger */
-	private static final Logger LOG = LoggerFactory.getLogger(HibernateCollectionType.class);
+  /** Logger */
+  private static final Logger LOG = LoggerFactory.getLogger(HibernateCollectionType.class);
 
-	/** Specific Hibernate collection class */
-	private Class<? extends PersistentCollection> hibernateCollectionClass;
+  /** Specific Hibernate collection class */
+  private Class<? extends PersistentCollection> hibernateCollectionClass;
 
-	/** Plain 'Java collections' type */
-	private Class<?> plainTypeClass;
+  /** Plain 'Java collections' type */
+  private Class<?> plainTypeClass;
 
-	/** Plain interface collection type */
-	private Class<?> plainInterface;
+  /** Plain interface collection type */
+  private Class<?> plainInterface;
 
-	/**
+  /**
 	 * Construct
 	 * 
 	 * @param hibernateCollectionClass
 	 * @param plainTypeClass
 	 */
-	private HibernateCollectionType(Class<? extends PersistentCollection> hibernateCollectionClass,
-		Class<?> plainTypeClass, Class<?> plainInterface)
-	{
-		this.hibernateCollectionClass = hibernateCollectionClass;
-		this.plainTypeClass = plainTypeClass;
-		this.plainInterface = plainInterface;
-	}
+  private HibernateCollectionType(Class<? extends PersistentCollection> hibernateCollectionClass, Class<?> plainTypeClass, Class<?> plainInterface) {
+    this.hibernateCollectionClass = hibernateCollectionClass;
+    this.plainTypeClass = plainTypeClass;
+    this.plainInterface = plainInterface;
+  }
 
-	/**
+  /**
 	 * @return the plainInterface
 	 */
-	public Class<?> getPlainInterface()
-	{
-		return plainInterface;
-	}
+  public Class<?> getPlainInterface() {
+    return plainInterface;
+  }
 
-	/**
+  /**
 	 * Creates a instance of the {@link PersistentCollection} defined by this type
 	 * 
 	 * @param sessionImpl
 	 *            {@link SessionImplementor}
 	 * @return {@link PersistentCollection} instance
 	 */
-	public PersistentCollection createCollection(SessionImplementor sessionImpl)
-	{
-		PersistentCollection collection = null;
+  public PersistentCollection createCollection(SessionImplementor sessionImpl) {
+    PersistentCollection collection = null;
+    try {
+      Constructor<? extends PersistentCollection> constructor = hibernateCollectionClass.getConstructor(SessionImplementor.class);
+      collection = constructor.newInstance(sessionImpl);
+    } catch (NoSuchMethodException | SecurityException e) {
+      LOG.error("Persistent collection type {} has no SessionImplementor constructor", hibernateCollectionClass);
+    } catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+      LOG.error("Cannot create collection instance of type " + hibernateCollectionClass, e);
+    }
+    return collection;
+  }
 
-		try
-		{
-			Constructor<? extends PersistentCollection> constructor = hibernateCollectionClass
-				.getConstructor(SessionImplementor.class);
-			collection = constructor.newInstance(sessionImpl);
-		}
-		catch (NoSuchMethodException | SecurityException e)
-		{
-			LOG.error("Persistent collection type {} has no SessionImplementor constructor", hibernateCollectionClass);
-		}
-		catch (InstantiationException | IllegalAccessException | IllegalArgumentException
-			| InvocationTargetException e)
-		{
-			LOG.error("Cannot create collection instance of type " + hibernateCollectionClass, e);
-		}
-
-		return collection;
-	}
-
-	/**
+  /**
 	 * Creates a plain Java collection instance based on a {@link PersistentCollection} one
 	 * 
 	 * @param persistentCollection
 	 *            the {@link PersistentCollection}
 	 * @return plain collection
 	 */
-	public Object createPlainCollection(PersistentCollection persistentCollection)
-	{
-		Object collection = null;
-		try
-		{
-			if (Map.class.isAssignableFrom(plainTypeClass))
-			{
-				collection = plainTypeClass.newInstance();
-			}
-			else
-			{
-				collection = plainTypeClass.newInstance();
-			}
-		}
-		catch (SecurityException e)
-		{
-			LOG.error("Colection type {} has no empty constructor", plainTypeClass);
-		}
-		catch (InstantiationException | IllegalAccessException | IllegalArgumentException e)
-		{
-			LOG.error("Cannot create collection instance of type " + plainTypeClass, e);
-		}
+  public Object createPlainCollection(PersistentCollection persistentCollection) {
+    Object collection = null;
+    try {
+      if (Map.class.isAssignableFrom(plainTypeClass)) {
+        collection = plainTypeClass.newInstance();
+      } else {
+        collection = plainTypeClass.newInstance();
+      }
+    } catch (SecurityException e) {
+      LOG.error("Colection type {} has no empty constructor", plainTypeClass);
+    } catch (InstantiationException | IllegalAccessException | IllegalArgumentException e) {
+      LOG.error("Cannot create collection instance of type " + plainTypeClass, e);
+    }
+    return collection;
+  }
 
-		return collection;
-	}
-
-	/**
+  /**
 	 * Determine collection type based on input value
 	 * 
 	 * @param sourceFieldValue
 	 *            the input {@link PersistentCollection}
 	 * @return {@link HibernateCollectionType}
 	 */
-	public static HibernateCollectionType determineType(PersistentCollection sourceFieldValue)
-	{
-		final HibernateCollectionType type;
-
-		if (sourceFieldValue.getValue() instanceof PersistentSortedSet)
-		{
-			type = HibernateCollectionType.SORTED_SET;
-		}
-		else if (sourceFieldValue.getValue() instanceof PersistentSet)
-		{
-			type = HibernateCollectionType.SET;
-		}
-		else if (sourceFieldValue.getValue() instanceof PersistentBag)
-		{
-			type = HibernateCollectionType.LIST;
-		}
-		else
-		{
-			type = HibernateCollectionType.MAP;
-		}
-
-		return type;
-	}
+  public static HibernateCollectionType determineType(PersistentCollection sourceFieldValue) {
+    final HibernateCollectionType type;
+    if (sourceFieldValue.getValue() instanceof PersistentSortedSet) {
+      type = HibernateCollectionType.SORTED_SET;
+    } else {
+      if (sourceFieldValue.getValue() instanceof PersistentSet) {
+        type = HibernateCollectionType.SET;
+      } else {
+        if (sourceFieldValue.getValue() instanceof PersistentBag) {
+          type = HibernateCollectionType.LIST;
+        } else {
+          type = HibernateCollectionType.MAP;
+        }
+      }
+    }
+    return type;
+  }
 }
