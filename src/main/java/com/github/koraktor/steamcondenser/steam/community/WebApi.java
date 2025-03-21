67f97d1d15bc497c6592c992c96746e24d3e3dc9
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 10:20:19 +0000

    Mining Framework Analysis

--- src/main/java/com/github/koraktor/steamcondenser/steam/community/WebApi.java
+++ src/main/java/com/github/koraktor/steamcondenser/steam/community/WebApi.java
@@ -1,28 +1,17 @@
-/**
- * This code is free software; you can redistribute it and/or modify it under
- * the terms of the new BSD License.
- *
- * Copyright (c) 2010-2013, Sebastian Staudt
- */
-
 package com.github.koraktor.steamcondenser.steam.community;
-
 import java.util.HashMap;
 import java.util.Map;
-
+import org.slf4j.Logger;
+import java.util.logging.Level;
+import org.slf4j.LoggerFactory;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.params.ClientPNames;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.github.koraktor.steamcondenser.exceptions.WebApiException;
 
 /**
@@ -34,23 +23,22 @@ import com.github.koraktor.steamcondenser.exceptions.WebApiException;
  * @author Sebastian Staudt
  */
 abstract public class WebApi {
+  protected static final Logger LOG = LoggerFactory.getLogger(WebApi.class);
 
-    protected static final Logger LOG = LoggerFactory.getLogger(WebApi.class);
+  protected static String apiKey;
 
-    protected static String apiKey;
+  protected static boolean secure = true;
 
-    protected static boolean secure = true;
-
-    /**
+  /**
      * Returns the Steam Web API key currently used by Steam Condenser
      *
      * @return The currently active Steam Web API key
      */
-    public static String getApiKey() {
-        return apiKey;
-    }
+  public static String getApiKey() {
+    return apiKey;
+  }
 
-    /**
+  /**
      * Returns a raw list of interfaces and their methods that are available in
      * Steam's Web API
      *
@@ -59,15 +47,12 @@ abstract public class WebApi {
      *
      * @return array The list of interfaces and methods
      */
-    public static JSONArray getInterfaces()
-            throws JSONException, WebApiException {
-        String data = WebApi.getJSON("ISteamWebAPIUtil", "GetSupportedAPIList");
-        return new JSONObject(data).
-                getJSONObject("apilist").
-                getJSONArray("interfaces");
-    }
+  public static JSONArray getInterfaces() throws JSONException, WebApiException {
+    String data = WebApi.getJSON("ISteamWebAPIUtil", "GetSupportedAPIList");
+    return new JSONObject(data).getJSONObject("apilist").getJSONArray("interfaces");
+  }
 
-    /**
+  /**
      * Sets the Steam Web API key
      *
      * @param apiKey The 128bit API key as a hexadecimal string that has to be
@@ -75,24 +60,23 @@ abstract public class WebApi {
      * @throws WebApiException if the given API key is not a valid 128bit
      *        hexadecimal string
      */
-    public static void setApiKey(String apiKey) throws WebApiException {
-        if(apiKey != null && !apiKey.matches("^[0-9A-F]{32}$")) {
-            throw new WebApiException(WebApiException.Cause.INVALID_KEY);
-        }
-
-        WebApi.apiKey = apiKey;
+  public static void setApiKey(String apiKey) throws WebApiException {
+    if (apiKey != null && !apiKey.matches("^[0-9A-F]{32}$")) {
+      throw new WebApiException(WebApiException.Cause.INVALID_KEY);
     }
+    WebApi.apiKey = apiKey;
+  }
 
-    /**
+  /**
      * Sets whether HTTPS should be used for the communication with the Web API
      *
      * @param secure Whether to use HTTPS
      */
-    public static void setSecure(boolean secure) {
-        WebApi.secure = secure;
-    }
+  public static void setSecure(boolean secure) {
+    WebApi.secure = secure;
+  }
 
-    /**
+  /**
      * Fetches JSON data from Steam Web API using the specified interface,
      * method and version. Additional parameters are supplied via HTTP GET.
      * Data is returned as a JSON-encoded string.
@@ -104,12 +88,11 @@ abstract public class WebApi {
      * @return Data is returned as a JSON-encoded string.
      * @throws WebApiException In case of any request failure
      */
-    public static String getJSON(String apiInterface, String method)
-            throws WebApiException {
-        return load("json", apiInterface, method, 1, null);
-    }
+  public static String getJSON(String apiInterface, String method) throws WebApiException {
+    return load("json", apiInterface, method, 1, null);
+  }
 
-    /**
+  /**
      * Fetches JSON data from Steam Web API using the specified interface,
      * method and version. Additional parameters are supplied via HTTP GET.
      * Data is returned as a JSON-encoded string.
@@ -122,12 +105,11 @@ abstract public class WebApi {
      * @return Data is returned as a JSON-encoded string.
      * @throws WebApiException In case of any request failure
      */
-    public static String getJSON(String apiInterface, String method, int version)
-            throws WebApiException {
-        return load("json", apiInterface, method, version, null);
-    }
+  public static String getJSON(String apiInterface, String method, int version) throws WebApiException {
+    return load("json", apiInterface, method, version, null);
+  }
 
-    /**
+  /**
      * Fetches JSON data from Steam Web API using the specified interface,
      * method and version. Additional parameters are supplied via HTTP GET.
      *
@@ -140,12 +122,11 @@ abstract public class WebApi {
      * @return Data is returned as a JSON-encoded string.
      * @throws WebApiException In case of any request failure
      */
-    public static String getJSON(String apiInterface, String method, int version, Map<String, Object> params)
-            throws WebApiException {
-        return load("json", apiInterface, method, version, params);
-    }
+  public static String getJSON(String apiInterface, String method, int version, Map<String, Object> params) throws WebApiException {
+    return load("json", apiInterface, method, version, params);
+  }
 
-    /**
+  /**
      * Fetches JSON data from Steam Web API using the specified interface,
      * method and version. Additional parameters are supplied via HTTP GET.
      * Data is returned as a Hash containing the JSON data.
@@ -158,12 +139,11 @@ abstract public class WebApi {
      * @throws JSONException In case of misformatted JSON data
      * @throws WebApiException In case of any request failure
      */
-    public static JSONObject getJSONData(String apiInterface, String method)
-            throws JSONException, WebApiException {
-        return getJSONData(apiInterface, method, 1, null);
-    }
+  public static JSONObject getJSONData(String apiInterface, String method) throws JSONException, WebApiException {
+    return getJSONData(apiInterface, method, 1, null);
+  }
 
-    /**
+  /**
      * Fetches JSON data from Steam Web API using the specified interface,
      * method and version. Additional parameters are supplied via HTTP GET.
      * Data is returned as a Hash containing the JSON data.
@@ -177,12 +157,11 @@ abstract public class WebApi {
      * @throws JSONException In case of misformatted JSON data
      * @throws WebApiException In case of any request failure
      */
-    public static JSONObject getJSONData(String apiInterface, String method, int version)
-            throws JSONException, WebApiException {
-        return getJSONData(apiInterface, method, version, null);
-    }
+  public static JSONObject getJSONData(String apiInterface, String method, int version) throws JSONException, WebApiException {
+    return getJSONData(apiInterface, method, version, null);
+  }
 
-    /**
+  /**
      * Fetches JSON data from Steam Web API using the specified interface,
      * method and version. Additional parameters are supplied via HTTP GET.
      *
@@ -196,19 +175,16 @@ abstract public class WebApi {
      * @throws JSONException In case of misformatted JSON data
      * @throws WebApiException In case of any request failure
      */
-    public static JSONObject getJSONData(String apiInterface, String method, int version, Map<String, Object> params)
-            throws JSONException, WebApiException {
-        String data = getJSON(apiInterface, method, version, params);
-        JSONObject result = new JSONObject(data).getJSONObject("result");
-
-        if(result.getInt("status") != 1) {
-            throw new WebApiException(WebApiException.Cause.STATUS_BAD, result.getInt("status"), result.getString("statusDetail"));
-        }
-
-        return result;
+  public static JSONObject getJSONData(String apiInterface, String method, int version, Map<String, Object> params) throws JSONException, WebApiException {
+    String data = getJSON(apiInterface, method, version, params);
+    JSONObject result = new JSONObject(data).getJSONObject("result");
+    if (result.getInt("status") != 1) {
+      throw new WebApiException(WebApiException.Cause.STATUS_BAD, result.getInt("status"), result.getString("statusDetail"));
     }
+    return result;
+  }
 
-    /**
+  /**
      * Fetches data from Steam Web API using the specified interface, method
      * and version. Additional parameters are supplied via HTTP GET. Data is
      * returned as a String in the given format.
@@ -222,12 +198,11 @@ abstract public class WebApi {
      *        "json", "vdf" or "xml").
      * @throws WebApiException In case of any request failure
      */
-    public static String load(String format, String apiInterface, String method)
-            throws WebApiException {
-        return load(format, apiInterface, method, 1, null);
-    }
+  public static String load(String format, String apiInterface, String method) throws WebApiException {
+    return load(format, apiInterface, method, 1, null);
+  }
 
-    /**
+  /**
      * Fetches data from Steam Web API using the specified interface, method
      * and version. Additional parameters are supplied via HTTP GET. Data is
      * returned as a String in the given format.
@@ -242,12 +217,11 @@ abstract public class WebApi {
      *        "json", "vdf" or "xml").
      * @throws WebApiException In case of any request failure
      */
-    public static String load(String format, String apiInterface, String method, int version)
-            throws WebApiException {
-        return load(format, apiInterface, method, version, null);
-    }
+  public static String load(String format, String apiInterface, String method, int version) throws WebApiException {
+    return load(format, apiInterface, method, version, null);
+  }
 
-    /**
+  /**
      * Fetches data from Steam Web API using the specified interface, method
      * and version. Additional parameters are supplied via HTTP GET. Data is
      * returned as a String in the given format.
@@ -263,60 +237,48 @@ abstract public class WebApi {
      *        "json", "vdf", or "xml").
      * @throws WebApiException In case of any request failure
      */
-    public static String load(String format, String apiInterface, String method, int version, Map<String, Object> params)
-            throws WebApiException {
-        String protocol = secure ? "https" : "http";
-        String url = String.format("%s://api.steampowered.com/%s/%s/v%04d/?", protocol, apiInterface, method, version);
-
-        if(params == null) {
-            params = new HashMap<String, Object>();
-        }
-        params.put("format", format);
-        if (apiKey != null) {
-            params.put("key", apiKey);
-        }
-
-        boolean first = true;
-        for(Map.Entry<String, Object> param : params.entrySet()) {
-            if(first) {
-                first = false;
-            } else {
-                url += '&';
-            }
-
-            url += String.format("%s=%s", param.getKey(), param.getValue());
-        }
-
-        if (LOG.isInfoEnabled()) {
-            String debugUrl = (apiKey == null) ?
-                url : url.replace(apiKey, "SECRET");
-            LOG.info("Querying Steam Web API: " + debugUrl);
-        }
-
-        String data;
-        try {
-            DefaultHttpClient httpClient = new DefaultHttpClient();
-            httpClient.getParams().setBooleanParameter(ClientPNames.HANDLE_AUTHENTICATION, false);
-            HttpGet request = new HttpGet(url);
-            HttpResponse response = httpClient.execute(request);
-
-            Integer statusCode = response.getStatusLine().getStatusCode();
-            if(!statusCode.toString().startsWith("20")) {
-                if(statusCode == 401) {
-                    throw new WebApiException(WebApiException.Cause.UNAUTHORIZED);
-                }
-
-                throw new WebApiException(WebApiException.Cause.HTTP_ERROR, statusCode, response.getStatusLine().getReasonPhrase());
-            }
-
-            data = EntityUtils.toString(response.getEntity());
-        } catch (WebApiException e) {
-            throw e;
-        } catch(Exception e) {
-            throw new WebApiException("Could not communicate with the Web API.", e);
-        }
-
-        return data;
+  public static String load(String format, String apiInterface, String method, int version, Map<String, Object> params) throws WebApiException {
+    String protocol = secure ? "https" : "http";
+    String url = String.format("%s://api.steampowered.com/%s/%s/v%04d/?", protocol, apiInterface, method, version);
+    if (params == null) {
+      params = new HashMap<String, Object>();
     }
-
+    params.put("format", format);
+    if (apiKey != null) {
+      params.put("key", apiKey);
+    }
+    boolean first = true;
+    for (Map.Entry<String, Object> param : params.entrySet()) {
+      if (first) {
+        first = false;
+      } else {
+        url += '&';
+      }
+      url += String.format("%s=%s", param.getKey(), param.getValue());
+    }
+    if (LOG.isLoggable(Level.INFO)) {
+      String debugUrl = (apiKey == null) ? url : url.replace(apiKey, "SECRET");
+      LOG.info("Querying Steam Web API: " + debugUrl);
+    }
+    String data;
+    try {
+      DefaultHttpClient httpClient = new DefaultHttpClient();
+      httpClient.getParams().setBooleanParameter(ClientPNames.HANDLE_AUTHENTICATION, false);
+      HttpGet request = new HttpGet(url);
+      HttpResponse response = httpClient.execute(request);
+      Integer statusCode = response.getStatusLine().getStatusCode();
+      if (!statusCode.toString().startsWith("20")) {
+        if (statusCode == 401) {
+          throw new WebApiException(WebApiException.Cause.UNAUTHORIZED);
+        }
+        throw new WebApiException(WebApiException.Cause.HTTP_ERROR, statusCode, response.getStatusLine().getReasonPhrase());
+      }
+      data = EntityUtils.toString(response.getEntity());
+    } catch (WebApiException e) {
+      throw e;
+    } catch (Exception e) {
+      throw new WebApiException("Could not communicate with the Web API.", e);
+    }
+    return data;
+  }
 }
--- src/main/java/com/github/koraktor/steamcondenser/steam/servers/MasterServer.java
+++ src/main/java/com/github/koraktor/steamcondenser/steam/servers/MasterServer.java
@@ -1,19 +1,10 @@
-/**
- * This code is free software; you can redistribute it and/or modify it under
- * the terms of the new BSD License.
- *
- * Copyright (c) 2008-2013, Sebastian Staudt
- */
-
 package com.github.koraktor.steamcondenser.steam.servers;
-
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.util.Vector;
 import java.util.concurrent.TimeoutException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import com.github.koraktor.steamcondenser.exceptions.SteamCondenserException;
 import com.github.koraktor.steamcondenser.steam.packets.A2M_GET_SERVERS_BATCH2_Paket;
 import com.github.koraktor.steamcondenser.steam.packets.M2A_SERVER_BATCH_Paket;
@@ -30,79 +21,78 @@ import com.github.koraktor.steamcondenser.steam.sockets.MasterServerSocket;
  * @author Sebastian Staudt
  */
 public class MasterServer extends Server {
+  protected static final Logger LOG = LoggerFactory.getLogger(MasterServer.class);
 
-    protected static final Logger LOG = LoggerFactory.getLogger(MasterServer.class);
-
-    /**
+  /**
      * The master server address to query for GoldSrc game servers
      */
-    public static final String GOLDSRC_MASTER_SERVER = "hl1master.steampowered.com:27011";
+  public static final String GOLDSRC_MASTER_SERVER = "hl1master.steampowered.com:27011";
 
-    /**
+  /**
      * The master server address to query for GoldSrc game servers
      */
-    public static final String SOURCE_MASTER_SERVER = "hl2master.steampowered.com:27011";
+  public static final String SOURCE_MASTER_SERVER = "hl2master.steampowered.com:27011";
 
-    /**
+  /**
      * The region code for the US east coast
      */
-    public static final byte REGION_US_EAST_COAST = 0x00;
+  public static final byte REGION_US_EAST_COAST = 0x00;
 
-    /**
+  /**
      * The region code for the US west coast
      */
-    public static final byte REGION_US_WEST_COAST = 0x01;
+  public static final byte REGION_US_WEST_COAST = 0x01;
 
-    /**
+  /**
      * The region code for South America
      */
-    public static final byte REGION_SOUTH_AMERICA = 0x02;
+  public static final byte REGION_SOUTH_AMERICA = 0x02;
 
-    /**
+  /**
      * The region code for Europe
      */
-    public static final byte REGION_EUROPE = 0x03;
+  public static final byte REGION_EUROPE = 0x03;
 
-    /**
+  /**
      * The region code for Asia
      */
-    public static final byte REGION_ASIA = 0x04;
+  public static final byte REGION_ASIA = 0x04;
 
-    /**
+  /**
      * The region code for Australia
      */
-    public static final byte REGION_AUSTRALIA = 0x05;
+  public static final byte REGION_AUSTRALIA = 0x05;
 
-    /**
+  /**
      * The region code for the Middle East
      */
-    public static final byte REGION_MIDDLE_EAST = 0x06;
+  public static final byte REGION_MIDDLE_EAST = 0x06;
 
-    /**
+  /**
      * The region code for Africa
      */
-    public static final byte REGION_AFRICA = 0x07;
+  public static final byte REGION_AFRICA = 0x07;
 
-    /**
+  /**
      * The region code for the whole world
      */
-    public static final byte REGION_ALL = (byte)0xFF;
+  public static final byte REGION_ALL = (byte) 0xFF;
 
-    public static int retries = 3;
+  public static int retries = 3;
 
-    protected MasterServerSocket socket;
+  protected MasterServerSocket socket;
 
-    /**
+  /**
      * Sets the number of consecutive requests that may fail, before getting
      * the server list is cancelled (default: 3)
      *
      * @param newRetries The number of allowed retries
      */
-    public static void setRetries(int newRetries) {
-        retries = newRetries;
-    }
+  public static void setRetries(int newRetries) {
+    retries = newRetries;
+  }
 
-    /**
+  /**
      * Creates a new instance of a master server object
      *
      * @param address Either an IP address, a DNS name or one of them combined
@@ -110,11 +100,11 @@ public class MasterServer extends Server {
      *        'server.example.com:27016' it will override the second argument.
      * @throws SteamCondenserException if initializing the socket fails
      */
-    public MasterServer(String address) throws SteamCondenserException {
-        super(address, null);
-    }
+  public MasterServer(String address) throws SteamCondenserException {
+    super(address, null);
+  }
 
-    /**
+  /**
      * Creates a new instance of a master server object
      *
      * @param address Either an IP address, a DNS name or one of them combined
@@ -123,12 +113,11 @@ public class MasterServer extends Server {
      * @param port The port the server is listening on
      * @throws SteamCondenserException if initializing the socket fails
      */
-    public MasterServer(String address, Integer port)
-            throws SteamCondenserException {
-        super(address, port);
-    }
+  public MasterServer(String address, Integer port) throws SteamCondenserException {
+    super(address, port);
+  }
 
-    /**
+  /**
      * Creates a new instance of a GoldSrc server object
      *
      * @param address Either an IP address, a DNS name or one of them combined
@@ -136,11 +125,11 @@ public class MasterServer extends Server {
      *        'server.example.com:27016' it will override the second argument.
      * @throws SteamCondenserException if initializing the socket fails
      */
-    public MasterServer(InetAddress address) throws SteamCondenserException {
-        super(address, null);
-    }
+  public MasterServer(InetAddress address) throws SteamCondenserException {
+    super(address, null);
+  }
 
-    /**
+  /**
      * Creates a new instance of a master server object
      *
      * @param address Either an IP address, a DNS name or one of them combined
@@ -149,12 +138,11 @@ public class MasterServer extends Server {
      * @param port The port the server is listening on
      * @throws SteamCondenserException if initializing the socket fails
      */
-    public MasterServer(InetAddress address, Integer port)
-            throws SteamCondenserException {
-        super(address, port);
-    }
+  public MasterServer(InetAddress address, Integer port) throws SteamCondenserException {
+    super(address, port);
+  }
 
-    /**
+  /**
      * Returns a list of all available game servers
      * <p/>
      * <strong>Note:</strong> Receiving all servers from the master server is
@@ -167,12 +155,11 @@ public class MasterServer extends Server {
      * @throws TimeoutException if too many timeouts occur while querying the
      *         master server
      */
-    public Vector<InetSocketAddress> getServers()
-            throws SteamCondenserException, TimeoutException {
-        return this.getServers(MasterServer.REGION_ALL, "", false);
-    }
+  public Vector<InetSocketAddress> getServers() throws SteamCondenserException, TimeoutException {
+    return this.getServers(MasterServer.REGION_ALL, "", false);
+  }
 
-    /**
+  /**
      * Returns a list of game server matching the given region and filters
      * <p/>
      * Filtering:
@@ -194,12 +181,11 @@ public class MasterServer extends Server {
      * @throws TimeoutException if too many timeouts occur while querying the
      *         master server
      */
-    public Vector<InetSocketAddress> getServers(byte regionCode, String filter)
-            throws SteamCondenserException, TimeoutException {
-        return this.getServers(regionCode, filter, false);
-    }
+  public Vector<InetSocketAddress> getServers(byte regionCode, String filter) throws SteamCondenserException, TimeoutException {
+    return this.getServers(regionCode, filter, false);
+  }
 
-    /**
+  /**
      * Returns a list of game server matching the given region and filters
      * <p/>
      * Filtering:
@@ -234,63 +220,59 @@ public class MasterServer extends Server {
      * @throws TimeoutException if too many timeouts occur while querying the
      *         master server
      */
-    public Vector<InetSocketAddress> getServers(byte regionCode, String filter, boolean force)
-            throws SteamCondenserException, TimeoutException {
-        int failCount    = 0;
-        boolean finished = false;
-        int portNumber   = 0;
-        String hostName  = "0.0.0.0";
-        Vector<String> serverStringArray;
-        Vector<InetSocketAddress> serverArray = new Vector<InetSocketAddress>();
-
-        while(true) {
-            try {
-                failCount = 0;
-                do {
-                    this.socket.send(new A2M_GET_SERVERS_BATCH2_Paket(regionCode, hostName + ":" + portNumber, filter));
-                    try {
-                        serverStringArray = ((M2A_SERVER_BATCH_Paket) this.socket.getReply()).getServers();
-
-                        for(String serverString : serverStringArray) {
-                            hostName = serverString.substring(0, serverString.lastIndexOf(":"));
-                            portNumber = Integer.valueOf(serverString.substring(serverString.lastIndexOf(":") + 1));
-
-                            if(!hostName.equals("0.0.0.0") && portNumber != 0) {
-                                serverArray.add(new InetSocketAddress(hostName, portNumber));
-                            } else {
-                                finished = true;
-                            }
-                        }
-                        failCount = 0;
-                    } catch(TimeoutException e) {
-                        failCount ++;
-                        if(failCount == retries) {
-                            throw e;
-                        }
-                        LOG.info("Request to master server " + this.ipAddress + " timed out, retrying...");
-                    }
-                } while(!finished);
-                break;
-            } catch(TimeoutException e) {
-                if (force) {
-                    break;
-                } else if(this.rotateIp()) {
-                    throw e;
-                }
-                LOG.info("Request to master server failed, retrying " + this.ipAddress + "...");
+  public Vector<InetSocketAddress> getServers(byte regionCode, String filter, boolean force) throws SteamCondenserException, TimeoutException {
+    int failCount = 0;
+    boolean finished = false;
+    int portNumber = 0;
+    String hostName = "0.0.0.0";
+    Vector<String> serverStringArray;
+    Vector<InetSocketAddress> serverArray = new Vector<InetSocketAddress>();
+    while (true) {
+      try {
+        failCount = 0;
+        do {
+          this.socket.send(new A2M_GET_SERVERS_BATCH2_Paket(regionCode, hostName + ":" + portNumber, filter));
+          try {
+            serverStringArray = ((M2A_SERVER_BATCH_Paket) this.socket.getReply()).getServers();
+            for (String serverString : serverStringArray) {
+              hostName = serverString.substring(0, serverString.lastIndexOf(":"));
+              portNumber = Integer.valueOf(serverString.substring(serverString.lastIndexOf(":") + 1));
+              if (!hostName.equals("0.0.0.0") && portNumber != 0) {
+                serverArray.add(new InetSocketAddress(hostName, portNumber));
+              } else {
+                finished = true;
+              }
             }
+            failCount = 0;
+          } catch (TimeoutException e) {
+            failCount++;
+            if (failCount == retries) {
+              throw e;
+            }
+            LOG.info("Request to master server " + this.ipAddress + " timed out, retrying...");
+          }
+        } while(!finished);
+        break;
+      } catch (TimeoutException e) {
+        if (force) {
+          break;
+        } else {
+          if (this.rotateIp()) {
+            throw e;
+          }
         }
-
-        return serverArray;
+        LOG.info("Request to master server failed, retrying " + this.ipAddress + "...");
+      }
     }
+    return serverArray;
+  }
 
-    /**
+  /**
      * Initializes the socket to communicate with the master server
      *
      * @see MasterServerSocket
      */
-    public void initSocket() throws SteamCondenserException {
-        this.socket = new MasterServerSocket(this.ipAddress, this.port);
-    }
-
+  public void initSocket() throws SteamCondenserException {
+    this.socket = new MasterServerSocket(this.ipAddress, this.port);
+  }
 }
