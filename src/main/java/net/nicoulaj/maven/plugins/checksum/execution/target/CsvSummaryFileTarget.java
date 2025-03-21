8d6e6f2941ff183a4808b2c2c0975736c11cb099
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 19:13:08 +0000

    Mining Framework Analysis

--- src/main/java/net/nicoulaj/maven/plugins/checksum/execution/target/CsvSummaryFileTarget.java
+++ src/main/java/net/nicoulaj/maven/plugins/checksum/execution/target/CsvSummaryFileTarget.java
@@ -1,29 +1,12 @@
-/*
- * Copyright 2010-2012 Julien Nicoulaud <julien.nicoulaud@gmail.com>
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package net.nicoulaj.maven.plugins.checksum.execution.target;
-
+import org.codehaus.plexus.util.FileUtils;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.SortedSet;
 import java.util.TreeSet;
-
 import net.nicoulaj.maven.plugins.checksum.artifacts.ArtifactListener;
-import org.codehaus.plexus.util.FileUtils;
 
 /**
  * An {@link ExecutionTarget} that writes digests to a CSV file.
@@ -31,140 +14,109 @@ import org.codehaus.plexus.util.FileUtils;
  * @author <a href="mailto:julien.nicoulaud@gmail.com">Julien Nicoulaud</a>
  * @since 1.0
  */
-public class CsvSummaryFileTarget
-    implements ExecutionTarget
-{
-    /**
+public class CsvSummaryFileTarget implements ExecutionTarget {
+  /**
      * The line separator character.
      */
-    public static final String LINE_SEPARATOR = System.getProperty( "line.separator" );
+  public static final String LINE_SEPARATOR = System.getProperty("line.separator");
 
-    /**
+  /**
      * The CSV column separator character.
      */
-    public static final String CSV_COLUMN_SEPARATOR = ",";
+  public static final String CSV_COLUMN_SEPARATOR = ",";
 
-    /**
+  /**
      * The CSV comment marker character.
      */
-    public static final String CSV_COMMENT_MARKER = "#";
+  public static final String CSV_COMMENT_MARKER = "#";
 
-    /**
+  /**
      * Encoding to use for generated files.
      */
-    protected String encoding;
+  protected String encoding;
 
-    /**
+  /**
      * The association file => (algorithm,hashcode).
      */
-    protected Map<File, Map<String, String>> filesHashcodes;
+  protected Map<File, Map<String, String>> filesHashcodes;
 
-    /**
+  /**
      * The set of algorithms encountered.
      */
-    protected SortedSet<String> algorithms;
+  protected SortedSet<String> algorithms;
 
-    /**
+  /**
      * The target file where the summary is written.
      */
-    protected File summaryFile;
+  protected File summaryFile;
 
-    /**
+  /**
      * List of listeners which are notified every time a CSV file is created.
      *
      * @since 1.3
      */
-    protected final Iterable<? extends ArtifactListener> artifactListeners;
+  protected final Iterable<? extends ArtifactListener> artifactListeners;
 
-    /**
+  /**
      * Build a new instance of {@link CsvSummaryFileTarget}.
+     *
      * @param summaryFile the file to which the summary should be written.
      * @param encoding    the encoding to use for generated files.
-     * @param artifactListeners
      */
-    public CsvSummaryFileTarget(File summaryFile, String encoding, Iterable<? extends ArtifactListener> artifactListeners)
-    {
-        this.summaryFile = summaryFile;
-        this.encoding = encoding;
-        this.artifactListeners = artifactListeners;
-    }
+  public CsvSummaryFileTarget(File summaryFile, String encoding, Iterable<? extends ArtifactListener> artifactListeners) {
+    this.summaryFile = summaryFile;
+    this.encoding = encoding;
+    this.artifactListeners = artifactListeners;
+  }
 
-    /**
+  /**
      * {@inheritDoc}
      */
-    @Override
-    public void init()
-    {
-        filesHashcodes = new HashMap<File, Map<String, String>>();
-        algorithms = new TreeSet<String>();
-    }
+  @Override public void init() {
+    filesHashcodes = new HashMap<File, Map<String, String>>();
+    algorithms = new TreeSet<String>();
+  }
 
-    /**
+  /**
      * {@inheritDoc}
      */
-    public void write( String digest, File file, String algorithm )
-    {
-        // Initialize an entry for the file if needed.
-        if ( !filesHashcodes.containsKey( file ) )
-        {
-            filesHashcodes.put( file, new HashMap<String, String>() );
-        }
-
-        // Store the algorithm => hashcode mapping for this file.
-        Map<String, String> fileHashcodes = filesHashcodes.get( file );
-        fileHashcodes.put( algorithm, digest );
-
-        // Store the algorithm.
-        algorithms.add( algorithm );
+  public void write(String digest, File file, String algorithm) {
+    if (!filesHashcodes.containsKey(file)) {
+      filesHashcodes.put(file, new HashMap<String, String>());
     }
+    Map<String, String> fileHashcodes = filesHashcodes.get(file);
+    fileHashcodes.put(algorithm, digest);
+    algorithms.add(algorithm);
+  }
 
-    /**
+  /**
      * {@inheritDoc}
      */
-    @Override
-    public void close()
-        throws ExecutionTargetCloseException
-    {
-        StringBuilder sb = new StringBuilder();
-
-        // Write the CSV file header.
-        sb.append( CSV_COMMENT_MARKER ).append( "File" );
-        for ( String algorithm : algorithms )
-        {
-            sb.append( CSV_COLUMN_SEPARATOR ).append( algorithm );
-        }
-
-        // Write a line for each file.
-        for ( File file : filesHashcodes.keySet() )
-        {
-            sb.append( LINE_SEPARATOR ).append( file.getName() );
-            Map<String, String> fileHashcodes = filesHashcodes.get( file );
-            for ( String algorithm : algorithms )
-            {
-                sb.append( CSV_COLUMN_SEPARATOR );
-                if ( fileHashcodes.containsKey( algorithm ) )
-                {
-                    sb.append( fileHashcodes.get( algorithm ) );
-                }
-            }
-        }
-
-        sb.append( LINE_SEPARATOR );
-
-        // Make sure the parent directory exists.
-        FileUtils.mkdir( summaryFile.getParent() );
-
-        // Write the result to the summary file.
-        try
-        {
-            FileUtils.fileWrite( summaryFile.getPath(), encoding, sb.toString() );
-            for (ArtifactListener artifactListener : artifactListeners) {
-                artifactListener.artifactCreated(summaryFile, "csv");
-            }
-        }
-        catch ( IOException e )
-        {
-            throw new ExecutionTargetCloseException( e.getMessage() );
-        }
+  @Override public void close() throws ExecutionTargetCloseException {
+    StringBuilder sb = new StringBuilder();
+    sb.append(CSV_COMMENT_MARKER).append("File");
+    for (String algorithm : algorithms) {
+      sb.append(CSV_COLUMN_SEPARATOR).append(algorithm);
     }
+    for (File file : filesHashcodes.keySet()) {
+      sb.append(LINE_SEPARATOR).append(file.getName());
+      Map<String, String> fileHashcodes = filesHashcodes.get(file);
+      for (String algorithm : algorithms) {
+        sb.append(CSV_COLUMN_SEPARATOR);
+        if (fileHashcodes.containsKey(algorithm)) {
+          sb.append(fileHashcodes.get(algorithm));
+        }
+      }
+    }
+    sb.append(LINE_SEPARATOR);
+    FileUtils.mkdir(summaryFile.getParent());
+    try {
+      FileUtils.fileWrite(summaryFile.getPath(), encoding, sb.toString());
+      for (ArtifactListener artifactListener : artifactListeners) {
+        artifactListener.artifactCreated(summaryFile, "csv");
+      }
+    } catch (IOException e) {
+      throw new ExecutionTargetCloseException(e.getMessage());
+    }
+  }
 }
--- src/main/java/net/nicoulaj/maven/plugins/checksum/mojo/AbstractChecksumMojo.java
+++ src/main/java/net/nicoulaj/maven/plugins/checksum/mojo/AbstractChecksumMojo.java
@@ -1,30 +1,13 @@
-/*
- * Copyright 2010-2012 Julien Nicoulaud <julien.nicoulaud@gmail.com>
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package net.nicoulaj.maven.plugins.checksum.mojo;
-
+import net.nicoulaj.maven.plugins.checksum.Constants;
 import java.io.File;
+import net.nicoulaj.maven.plugins.checksum.execution.Execution;
 import java.util.Arrays;
 import java.util.Collections;
+import net.nicoulaj.maven.plugins.checksum.execution.ExecutionException;
 import java.util.List;
-
-import net.nicoulaj.maven.plugins.checksum.Constants;
 import net.nicoulaj.maven.plugins.checksum.artifacts.ArtifactAttacher;
 import net.nicoulaj.maven.plugins.checksum.artifacts.ArtifactListener;
-import net.nicoulaj.maven.plugins.checksum.execution.Execution;
-import net.nicoulaj.maven.plugins.checksum.execution.ExecutionException;
 import net.nicoulaj.maven.plugins.checksum.execution.FailOnErrorExecution;
 import net.nicoulaj.maven.plugins.checksum.execution.NeverFailExecution;
 import net.nicoulaj.maven.plugins.checksum.execution.target.CsvSummaryFileTarget;
@@ -48,29 +31,24 @@ import org.codehaus.plexus.util.StringUtils;
  * @author <a href="mailto:julien.nicoulaud@gmail.com">Julien Nicoulaud</a>
  * @since 1.1
  */
-abstract class AbstractChecksumMojo
-    extends AbstractMojo
-{
-
-    /**
+abstract class AbstractChecksumMojo extends AbstractMojo {
+  /**
      * The Maven project.
      *
      * @since 1.0
      */
-    @Parameter( property = "project", required = true, readonly = true )
-    protected MavenProject project;
+  @Parameter(property = "project", required = true, readonly = true) protected MavenProject project;
 
-    /**
+  /**
      * The Maven Project Helper.
      *
      * Used to attach checksums as build artifacts.
      *
      * @since 1.3
      */
-    @Component
-    protected MavenProjectHelper projectHelper;
+  @Component protected MavenProjectHelper projectHelper;
 
-    /**
+  /**
      * The list of checksum algorithms used.
      * <p/>
      * <p>Default value is MD5 and SHA-1.<br/>Allowed values are CRC32, MD2, MD4, MD5, SHA-1, SHA-224, SHA-256, SHA-384,
@@ -85,123 +63,97 @@ abstract class AbstractChecksumMojo
      *
      * @since 1.0
      */
-    @Parameter
-    protected List<String> algorithms = Arrays.asList( Constants.DEFAULT_EXECUTION_ALGORITHMS );
+  @Parameter protected List<String> algorithms = Arrays.asList(Constants.DEFAULT_EXECUTION_ALGORITHMS);
 
-    /**
+  /**
      * Indicates whether the build will fail if there are errors.
      *
      * @since 1.0
      */
-    @Parameter( defaultValue = "true" )
-    protected boolean failOnError;
+  @Parameter(defaultValue = "true") protected boolean failOnError;
 
-    /**
+  /**
      * Encoding to use for generated files.
      *
      * @since 1.0
      */
-    @Parameter( property = "encoding", defaultValue = "${project.build.sourceEncoding}" )
-    protected String encoding = Constants.DEFAULT_ENCODING;
+  @Parameter(property = "encoding", defaultValue = "${project.build.sourceEncoding}") protected String encoding = Constants.DEFAULT_ENCODING;
 
-    /**
+  /**
      * Should the checksums be attached as build artifacts.
      *
      * @since 1.3
      */
-    @Parameter( property = "attachChecksums", defaultValue = "false")
-    protected boolean attachChecksums;
+  @Parameter(property = "attachChecksums", defaultValue = "false") protected boolean attachChecksums;
 
-    /**
+  /**
      * Indicates whether the build will print checksums in the build log.
      *
      * @since 1.0
      */
-    @Parameter( defaultValue = "false" )
-    protected boolean quiet;
+  @Parameter(defaultValue = "false") protected boolean quiet;
 
-    /**
+  /**
      * {@inheritDoc}
      */
-    public void execute()
-        throws MojoExecutionException, MojoFailureException
-    {
-        // Prepare an execution.
-        Execution execution = ( failOnError ) ? new FailOnErrorExecution() : new NeverFailExecution( getLog() );
-        execution.setAlgorithms( algorithms );
-        execution.setFiles( getFilesToProcess() );
-        if ( !quiet )
-        {
-            execution.addTarget( new MavenLogTarget( getLog() ) );
-        }
-        if ( isIndividualFiles() )
-        {
-            File outputDirectory = null;
-            if ( StringUtils.isNotEmpty( getIndividualFilesOutputDirectory() ) )
-            {
-                outputDirectory = FileUtils.resolveFile( new File( project.getBuild().getDirectory() ),
-                                                         getIndividualFilesOutputDirectory() );
-            }
-            execution.addTarget( new OneHashPerFileTarget( encoding, outputDirectory, createArtifactListeners()) );
-        }
-        if ( isCsvSummary() )
-        {
-            execution.addTarget( new CsvSummaryFileTarget(
-                FileUtils.resolveFile( new File( project.getBuild().getDirectory() ), getCsvSummaryFile() ),
-                encoding, createArtifactListeners()) );
-        }
-        if ( isXmlSummary() )
-        {
-            execution.addTarget( new XmlSummaryFileTarget(
-                FileUtils.resolveFile( new File( project.getBuild().getDirectory() ), getXmlSummaryFile() ),
-                encoding, createArtifactListeners()) );
-        }
-        if ( isShasumSummary() )
-        {
-            execution.addTarget( new ShasumSummaryFileTarget(
-                FileUtils.resolveFile( new File( project.getBuild().getDirectory() ), getShasumSummaryFile() )) );
-        }
-
-        // Run the execution.
-        try
-        {
-            execution.run();
-        }
-        catch ( ExecutionException e )
-        {
-            getLog().error( e.getMessage() );
-            throw new MojoFailureException( e.getMessage() );
-        }
+  public void execute() throws MojoExecutionException, MojoFailureException {
+    Execution execution = (failOnError) ? new FailOnErrorExecution() : new NeverFailExecution(getLog());
+    execution.setAlgorithms(algorithms);
+    execution.setFiles(getFilesToProcess());
+    if (!quiet) {
+      execution.addTarget(new MavenLogTarget(getLog()));
     }
-
-    private Iterable<? extends ArtifactListener> createArtifactListeners() {
-        if (!attachChecksums) {
-            return Collections.emptyList();
-        }
-        return Collections.singletonList(new ArtifactAttacher(project, projectHelper));
+    if (isIndividualFiles()) {
+      File outputDirectory = null;
+      if (StringUtils.isNotEmpty(getIndividualFilesOutputDirectory())) {
+        outputDirectory = FileUtils.resolveFile(new File(project.getBuild().getDirectory()), getIndividualFilesOutputDirectory());
+      }
+      execution.addTarget(new OneHashPerFileTarget(encoding, outputDirectory, createArtifactListeners()));
     }
+    if (isCsvSummary()) {
+      execution.addTarget(new CsvSummaryFileTarget(FileUtils.resolveFile(new File(project.getBuild().getDirectory()), getCsvSummaryFile()), encoding, createArtifactListeners()));
+    }
+    if (isXmlSummary()) {
+      execution.addTarget(new XmlSummaryFileTarget(FileUtils.resolveFile(new File(project.getBuild().getDirectory()), getXmlSummaryFile()), encoding, createArtifactListeners()));
+    }
+    if (isShasumSummary()) {
+      execution.addTarget(new ShasumSummaryFileTarget(FileUtils.resolveFile(new File(project.getBuild().getDirectory()), getShasumSummaryFile())));
+    }
+    try {
+      execution.run();
+    } catch (ExecutionException e) {
+      getLog().error(e.getMessage());
+      throw new MojoFailureException(e.getMessage());
+    }
+  }
 
-    /**
+  private Iterable<? extends ArtifactListener> createArtifactListeners() {
+    if (!attachChecksums) {
+      return Collections.emptyList();
+    }
+    return Collections.singletonList(new ArtifactAttacher(project, projectHelper));
+  }
+
+  /**
      * Build the list of files from which digests should be generated.
      *
      * @return the list of files that should be processed.
      */
-    protected abstract List<File> getFilesToProcess();
+  protected abstract List<File> getFilesToProcess();
 
-    protected abstract boolean isIndividualFiles();
+  protected abstract boolean isIndividualFiles();
 
-    protected abstract String getIndividualFilesOutputDirectory();
+  protected abstract String getIndividualFilesOutputDirectory();
 
-    protected abstract boolean isCsvSummary();
+  protected abstract boolean isCsvSummary();
 
-    protected abstract String getCsvSummaryFile();
+  protected abstract String getCsvSummaryFile();
 
-    protected abstract boolean isXmlSummary();
+  protected abstract boolean isXmlSummary();
 
-    protected abstract String getXmlSummaryFile();
+  protected abstract String getXmlSummaryFile();
 
-    protected abstract boolean isShasumSummary();
+  protected abstract boolean isShasumSummary();
 
-    protected abstract String getShasumSummaryFile();
-
+  protected abstract String getShasumSummaryFile();
 }
