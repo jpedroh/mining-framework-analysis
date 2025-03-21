4dd3db85e920bc2daa976299a3a6334c285a8dab
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 08:39:30 +0000

    Mining Framework Analysis

--- src/main/java/uk/co/jemos/podam/common/PodamConstructor.java
+++ src/main/java/uk/co/jemos/podam/common/PodamConstructor.java
@@ -1,32 +1,10 @@
 package uk.co.jemos.podam.common;
-
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-/**
- * It identifies a constructor to use in order to create an instance of a class.
- * <p>
- * This annotation should be used in POJOs with attributes but without setters.
- * A typical example are immutable classes, where attributes are final and
- * declared in the constructor and they have getters but not setters.
- * </p>
- * 
- * @author mtedone
- * 
- */
-@Documented
-@PodamAnnotation
-@Target({ElementType.CONSTRUCTOR, ElementType.METHOD})
-@Retention(RetentionPolicy.RUNTIME)
-@PodamAnnotation
-public @interface PodamConstructor {
-
-	/** Gives users the possibility to leave comments
-	 *
-	 * @return comment value
-	 */
-	String comment() default "";
+@Documented @Target(value = { ElementType.CONSTRUCTOR, ElementType.METHOD }) @PodamAnnotation @Retention(value = RetentionPolicy.RUNTIME) public @interface PodamConstructor {
+  String comment() default "";
 }
--- src/main/java/uk/co/jemos/podam/common/PodamExclude.java
+++ src/main/java/uk/co/jemos/podam/common/PodamExclude.java
@@ -1,35 +1,10 @@
-/**
- * 
- */
 package uk.co.jemos.podam.common;
-
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-/**
- * Annotation which will have PODAM to skip the annotated attribute from
- * consideration.
- * <p>
- * PODAM will skip POJO attributes annotated with this annotation, thus their
- * value will not be set.
- * </p>
- * 
- * @author mtedone
- * 
- */
-@Documented
-@PodamAnnotation
-@Target(value = ElementType.FIELD)
-@Retention(RetentionPolicy.RUNTIME)
-@PodamAnnotation
-public @interface PodamExclude {
-
-	/** Gives users the possibility to leave comments
-	 *
-	 * @return comment value
-	 */
-	String comment() default "";
+@Documented @Target(value = { ElementType.FIELD }) @PodamAnnotation @Retention(value = RetentionPolicy.RUNTIME) public @interface PodamExclude {
+  String comment() default "";
 }
