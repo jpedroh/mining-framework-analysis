f7e6fddb96a2373be8abfacff2ec9d36eb68a666
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 20:38:40 +0000

    Mining Framework Analysis

--- src/main/java/com/fasterxml/jackson/core/base/GeneratorBase.java
+++ src/main/java/com/fasterxml/jackson/core/base/GeneratorBase.java
@@ -30,16 +30,13 @@ public abstract class GeneratorBase extends JsonGenerator
             StreamWriteFeature.WRITE_NUMBERS_AS_STRINGS.getMask()
             | StreamWriteFeature.STRICT_DUPLICATE_DETECTION.getMask()
             ;
-
     // // // Constants for validation messages (since 2.6)
-
     protected final static String WRITE_BINARY = "write a binary value";
     protected final static String WRITE_BOOLEAN = "write a boolean value";
     protected final static String WRITE_NULL = "write a null";
     protected final static String WRITE_NUMBER = "write a number";
     protected final static String WRITE_RAW = "write a raw (unencoded) value";
     protected final static String WRITE_STRING = "write a string";
-
     /**
      * This value is the limit of scale allowed for serializing {@link BigDecimal}
      * in "plain" (non-engineering) notation; intent is to prevent asymmetric
@@ -47,13 +44,11 @@ public abstract class GeneratorBase extends JsonGenerator
      * huge "plain" serialization. See [core#315] for details.
      */
     protected final static int MAX_BIG_DECIMAL_SCALE = 9999;
-    
     /*
     /**********************************************************
     /* Configuration
     /**********************************************************
      */
-
     /**
      * Context object used both to pass some initial settings and to allow
      * triggering of Object serialization through generator.
@@ -61,46 +56,39 @@ public abstract class GeneratorBase extends JsonGenerator
      * @since 3.0
      */
     protected final ObjectWriteContext _objectWriteContext;
-
     /**
      * Bit flag composed of bits that indicate which
      * {@link com.fasterxml.jackson.core.StreamWriteFeature}s
      * are enabled.
      */
     protected int _streamWriteFeatures;
-
     /**
      * Flag set to indicate that implicit conversion from number
      * to JSON String is needed (as per
      * {@link com.fasterxml.jackson.core.StreamWriteFeature#WRITE_NUMBERS_AS_STRINGS}).
      */
     protected boolean _cfgNumbersAsStrings;
-
     /*
     /**********************************************************
     /* State
     /**********************************************************
      */
-
     /**
      * Object that keeps track of the current contextual state
      * of the generator.
      */
     protected JsonWriteContext _outputContext;
-
     /**
      * Flag that indicates whether generator is closed or not. Gets
      * set when it is closed by an explicit call
      * ({@link #close}).
      */
     protected boolean _closed;
-
     /*
     /**********************************************************
     /* Life-cycle
     /**********************************************************
      */
-
     protected GeneratorBase(ObjectWriteContext writeCtxt, int features) {
         super();
         _objectWriteContext = writeCtxt;
@@ -110,7 +98,6 @@ public abstract class GeneratorBase extends JsonGenerator
         _outputContext = JsonWriteContext.createRootContext(dups);
         _cfgNumbersAsStrings = StreamWriteFeature.WRITE_NUMBERS_AS_STRINGS.enabledIn(features);
     }
-
     protected GeneratorBase(ObjectWriteContext writeCtxt, int features, JsonWriteContext ctxt) {
         super();
         _objectWriteContext = writeCtxt;
@@ -118,40 +105,32 @@ public abstract class GeneratorBase extends JsonGenerator
         _outputContext = ctxt;
         _cfgNumbersAsStrings = StreamWriteFeature.WRITE_NUMBERS_AS_STRINGS.enabledIn(features);
     }
-
     /**
      * Implemented with standard version number detection algorithm, typically using
      * a simple generated class, with information extracted from Maven project file
      * during build.
      */
     @Override public Version version() { return PackageVersion.VERSION; }
-
     @Override
     public Object getCurrentValue() {
         return _outputContext.getCurrentValue();
     }
-
     @Override
     public void setCurrentValue(Object v) {
         if (_outputContext != null) {
             _outputContext.setCurrentValue(v);
         }
     }
-
     /*
     /**********************************************************
     /* Configuration
     /**********************************************************
      */
-
     @Override public final boolean isEnabled(StreamWriteFeature f) { return (_streamWriteFeatures & f.getMask()) != 0; }
     @Override public int streamWriteFeatures() { return _streamWriteFeatures; }
-
     @Override
     public int formatWriteFeatures() { return 0; }
-
     //public JsonGenerator configure(Feature f, boolean state) { }
-
     @Override
     public JsonGenerator enable(StreamWriteFeature f) {
         final int mask = f.getMask();
@@ -164,7 +143,6 @@ public abstract class GeneratorBase extends JsonGenerator
         }
         return this;
     }
-
     @Override
     public JsonGenerator disable(StreamWriteFeature f) {
         final int mask = f.getMask();
@@ -176,27 +154,22 @@ public abstract class GeneratorBase extends JsonGenerator
         }
         return this;
     }
-
     /*
     /**********************************************************
     /* Public API, accessors
     /**********************************************************
      */
-
     @Override public TokenStreamContext getOutputContext() { return _outputContext; }
     @Override public ObjectWriteContext getObjectWriteContext() { return _objectWriteContext; }
-
     /*
     /**********************************************************
     /* Public API, write methods, structural
     /**********************************************************
      */
-
     //public void writeStartArray() throws IOException
     //public void writeEndArray() throws IOException
     //public void writeStartObject() throws IOException
     //public void writeEndObject() throws IOException
-
     @Override
     public void writeStartArray(Object forValue, int size) throws IOException {
         writeStartArray(size);
@@ -204,76 +177,59 @@ public abstract class GeneratorBase extends JsonGenerator
             _outputContext.setCurrentValue(forValue);
         }
     }
-
     @Override
     public void writeStartObject(Object forValue) throws IOException
     {
         writeStartObject();
-        if (forValue != null) {
-            setCurrentValue(forValue);
+        if ((_outputContext != null) && (forValue != null)) {
+            _outputContext.setCurrentValue(forValue);
         }
     }
-
     /*
-    /**********************************************************
-    /* Public API, write methods, textual
-    /**********************************************************
-     */
-
+        /**********************************************************
+        /* Public API, write methods, textual
+        /**********************************************************
+         */
     @Override public void writeFieldName(SerializableString name) throws IOException {
         writeFieldName(name.getValue());
     }
-
     //public abstract void writeString(String text) throws IOException;
-
     //public abstract void writeString(char[] text, int offset, int len) throws IOException;
-
     //public abstract void writeString(Reader reader, int len) throws IOException;
-
     //public abstract void writeRaw(String text) throws IOException,;
-
     //public abstract void writeRaw(char[] text, int offset, int len) throws IOException;
-
     @Override
     public void writeString(SerializableString text) throws IOException {
         writeString(text.getValue());
     }
-
     @Override public void writeRawValue(String text) throws IOException {
         _verifyValueWrite("write raw value");
         writeRaw(text);
     }
-
     @Override public void writeRawValue(String text, int offset, int len) throws IOException {
         _verifyValueWrite("write raw value");
         writeRaw(text, offset, len);
     }
-
     @Override public void writeRawValue(char[] text, int offset, int len) throws IOException {
         _verifyValueWrite("write raw value");
         writeRaw(text, offset, len);
     }
-
     @Override public void writeRawValue(SerializableString text) throws IOException {
         _verifyValueWrite("write raw value");
         writeRaw(text);
     }
-
     @Override
     public int writeBinary(Base64Variant b64variant, InputStream data, int dataLength) throws IOException {
         // Let's implement this as "unsupported" to make it easier to add new parser impls
         _reportUnsupportedOperation();
         return 0;
     }
-
     /*
     /**********************************************************
     /* Public API, write methods, primitive
     /**********************************************************
      */
-
     // Not implemented at this level, added as placeholders
-
      /*
     public abstract void writeNumber(int i)
     public abstract void writeNumber(long l)
@@ -283,13 +239,11 @@ public abstract class GeneratorBase extends JsonGenerator
     public abstract void writeBoolean(boolean state)
     public abstract void writeNull()
     */
-
     /*
     /**********************************************************
     /* Public API, write methods, POJOs, trees
     /**********************************************************
      */
-
     @Override
     public void writeObject(Object value) throws IOException {
         if (value == null) {
@@ -302,7 +256,6 @@ public abstract class GeneratorBase extends JsonGenerator
             _objectWriteContext.writeValue(this, value);
         }
     }
-
     @Override
     public void writeTree(TreeNode rootNode) throws IOException {
         // As with 'writeObject()', we are not to check if write would work
@@ -312,29 +265,24 @@ public abstract class GeneratorBase extends JsonGenerator
             _objectWriteContext.writeTree(this, rootNode);
         }
     }
-
     /*
     /**********************************************************
     /* Public API, low-level output handling
     /**********************************************************
      */
-
     @Override public abstract void flush() throws IOException;
     @Override public void close() throws IOException { _closed = true; }
     @Override public boolean isClosed() { return _closed; }
-
     /*
     /**********************************************************
     /* Package methods for this, sub-classes
     /**********************************************************
      */
-
     /**
      * Method called to release any buffers generator may be holding,
      * once generator is being closed.
      */
     protected abstract void _releaseBuffers();
-
     /**
      * Method called before trying to write a value (scalar or structured),
      * to verify that this is legal in current output state, as well as to
@@ -344,7 +292,6 @@ public abstract class GeneratorBase extends JsonGenerator
      *   if value output is NOT legal in current generator output state.
      */
     protected abstract void _verifyValueWrite(String typeMsg) throws IOException;
-
     /**
      * Overridable factory method called to instantiate an appropriate {@link PrettyPrinter}
      * for case of "just use the default one", when {@link #useDefaultPrettyPrinter()} is called.
@@ -352,7 +299,6 @@ public abstract class GeneratorBase extends JsonGenerator
     protected PrettyPrinter _constructDefaultPrettyPrinter() {
         return new DefaultPrettyPrinter();
     }
-
     /**
      * Helper method used to serialize a {@link java.math.BigDecimal} as a String,
      * for serialization, taking into account configuration settings
@@ -370,13 +316,39 @@ scale, MAX_BIG_DECIMAL_SCALE, MAX_BIG_DECIMAL_SCALE));
         }
         return value.toString();
     }
-
     /*
     /**********************************************************
     /* UTF-8 related helper method(s)
     /**********************************************************
      */
-
+    // // // Constants for validation messages (since 2.6)
+    /*
+    /**********************************************************
+    /* Configuration
+    /**********************************************************
+     */
+    /**
+     * @since 2.5
+     */
+    /*
+    /**********************************************************
+    /* Configuration
+    /**********************************************************
+     */
+    //public JsonGenerator configure(Feature f, boolean state) { }
+    //public void writeStartArray() throws IOException
+    //public void writeEndArray() throws IOException
+    //public void writeStartObject() throws IOException
+    //public void writeEndObject() throws IOException
+    //public abstract void writeString(String text) throws IOException;
+    //public abstract void writeString(char[] text, int offset, int len) throws IOException;
+    //public abstract void writeString(Reader reader, int len) throws IOException;
+    //public abstract void writeRaw(String text) throws IOException,;
+    //public abstract void writeRaw(char[] text, int offset, int len) throws IOException;
+    // Not implemented at this level, added as placeholders
+    /**
+     * @since 2.5
+     */
     protected final int _decodeSurrogate(int surr1, int surr2) throws IOException
     {
         // First is known to be valid, but how about the other?
