c32014e1b5eb93175a5443578cf8fc2c2e7f128f
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 07:48:14 +0000

    Mining Framework Analysis

--- src/main/java/com/github/javafaker/Faker.java
+++ src/main/java/com/github/javafaker/Faker.java
@@ -1,16 +1,13 @@
 package com.github.javafaker;
-
 import static org.apache.commons.lang.StringUtils.capitalize;
 import static org.apache.commons.lang.StringUtils.join;
 import static org.apache.commons.lang.math.RandomUtils.nextInt;
-
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
-
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.WordUtils;
 import org.apache.commons.lang.math.RandomUtils;
@@ -25,79 +22,77 @@ import org.slf4j.LoggerFactory;
  * @author ren
  *
  */
-@SuppressWarnings({ "unchecked", "rawtypes" })
-public class Faker {
-    private static final Logger logger = LoggerFactory.getLogger(Faker.class);
-    private static final char[] METHOD_NAME_DELIMITERS = { '_' };
+@SuppressWarnings(value = { "unchecked", "rawtypes" }) public class Faker {
+  private static final Logger logger = LoggerFactory.getLogger(Faker.class);
 
-    private Map<String, Object> fakeValuesMap;
+  private static final char[] METHOD_NAME_DELIMITERS = { '_' };
 
-    public Faker() {
-        this(Locale.ENGLISH);
+  private Map<String, Object> fakeValuesMap;
+
+  public Faker() {
+    this(Locale.ENGLISH);
+  }
+
+  public Faker(Locale locale) {
+    logger.info("Using default locale " + locale);
+    String languageCode = locale.getLanguage();
+    Map valuesMap = (Map) Yaml.load(findStream(languageCode + ".yml"));
+    valuesMap = (Map) valuesMap.get(languageCode);
+    fakeValuesMap = (Map<String, Object>) valuesMap.get("faker");
+  }
+
+  private InputStream findStream(String filename) {
+    InputStream streamOnClass = getClass().getResourceAsStream(filename);
+    if (streamOnClass != null) {
+      return streamOnClass;
     }
+    return getClass().getClassLoader().getResourceAsStream(filename);
+  }
 
-    public Faker(Locale locale) {
-        logger.info("Using default locale " + locale);
-        String languageCode = locale.getLanguage();
-        Map valuesMap = (Map) Yaml.load(findStream(languageCode + ".yml"));
-        valuesMap = (Map) valuesMap.get(languageCode);
-        fakeValuesMap = (Map<String, Object>) valuesMap.get("faker");
-    }
-
-    private InputStream findStream(String filename) {
-      InputStream streamOnClass = getClass().getResourceAsStream(filename);
-      if (streamOnClass != null) {
-          return streamOnClass;
+  public String numerify(String numberString) {
+    StringBuffer sb = new StringBuffer();
+    for (int i = 0; i < numberString.length(); i++) {
+      if (numberString.charAt(i) == '#') {
+        sb.append(RandomUtils.nextInt(10));
+      } else {
+        sb.append(numberString.charAt(i));
       }
-      return getClass().getClassLoader().getResourceAsStream(filename);
     }
+    return sb.toString();
+  }
 
-    public String numerify(String numberString) {
-        StringBuffer sb = new StringBuffer();
-        for (int i = 0; i < numberString.length(); i++) {
-            if (numberString.charAt(i) == '#') {
-                sb.append(RandomUtils.nextInt(10));
-            } else {
-                sb.append(numberString.charAt(i));
-            }
-        }
-
-        return sb.toString();
+  public String letterify(String letterString) {
+    StringBuffer sb = new StringBuffer();
+    for (int i = 0; i < letterString.length(); i++) {
+      if (letterString.charAt(i) == '?') {
+        sb.append((char) (97 + RandomUtils.nextInt(26)));
+      } else {
+        sb.append(letterString.charAt(i));
+      }
     }
+    return sb.toString();
+  }
 
-    public String letterify(String letterString) {
-        StringBuffer sb = new StringBuffer();
-        for (int i = 0; i < letterString.length(); i++) {
-            if (letterString.charAt(i) == '?') {
-                sb.append((char) (97 + RandomUtils.nextInt(26))); // a-z
-            } else {
-                sb.append(letterString.charAt(i));
-            }
-        }
+  public String bothify(String string) {
+    return letterify(numerify(string));
+  }
 
-        return sb.toString();
-    }
-
-    public String bothify(String string) {
-        return letterify(numerify(string));
-    }
-
-    /**
+  /**
      * Fetch a random value from an array item specified by the key
      *
      * @param key
      * @return
      */
-    public Object fetch(String key) {
-        List valuesArray = (List) fetchObject(key);
-        return valuesArray.get(RandomUtils.nextInt(valuesArray.size()));
-    }
+  public Object fetch(String key) {
+    List valuesArray = (List) fetchObject(key);
+    return valuesArray.get(RandomUtils.nextInt(valuesArray.size()));
+  }
 
-    public String fetchString(String key) {
-        return (String) fetch(key);
-    }
+  public String fetchString(String key) {
+    return (String) fetch(key);
+  }
 
-    /**
+  /**
      * Return the object selected by the key from yaml file.
      *
      * @param key
@@ -105,159 +100,147 @@ public class Faker {
      *            dot. E.g. name.first_name
      * @return
      */
-    public Object fetchObject(String key) {
-        String[] path = key.split("\\.");
-        Object currentValue = fakeValuesMap;
-        for (String pathSection : path) {
-            currentValue = ((Map<String, Object>) currentValue).get(pathSection);
-        }
-        return currentValue;
+  public Object fetchObject(String key) {
+    String[] path = key.split("\\.");
+    Object currentValue = fakeValuesMap;
+    for (String pathSection : path) {
+      currentValue = ((Map<String, Object>) currentValue).get(pathSection);
     }
+    return currentValue;
+  }
 
-    public String composite(String formatKey, String joiner) {
-        List<String> format = (List<String>) fetch(formatKey);
-
-        String[] parts = new String[format.size()];
-        for (int i = 0; i < parts.length; i++) {
-            // remove leading colon
-            String methodName = format.get(i).substring(1);
-            // convert to camel case
-            methodName = WordUtils.capitalizeFully(methodName, METHOD_NAME_DELIMITERS).replaceAll("_", "");
-            methodName = methodName.substring(0, 1).toLowerCase() + methodName.substring(1);
-
-            try {
-                parts[i] = (String) Faker.class.getDeclaredMethod(methodName, (Class[]) null).invoke(this);
-            } catch (Exception e) {
-                e.printStackTrace();
-                throw new RuntimeException(e);
-            }
-        }
-
-        String result = StringUtils.join(parts, joiner);
-        return result;
+  public String composite(String formatKey, String joiner) {
+    List<String> format = (List<String>) fetch(formatKey);
+    String[] parts = new String[format.size()];
+    for (int i = 0; i < parts.length; i++) {
+      String methodName = format.get(i).substring(1);
+      methodName = WordUtils.capitalizeFully(methodName, METHOD_NAME_DELIMITERS).replaceAll("_", "");
+      methodName = methodName.substring(0, 1).toLowerCase() + methodName.substring(1);
+      try {
+        parts[i] = (String) Faker.class.getDeclaredMethod(methodName, (Class[]) null).invoke(this);
+      } catch (Exception e) {
+        e.printStackTrace();
+        throw new RuntimeException(e);
+      }
     }
-    
-    public String name() {
-      return composite("name.formats", " ");
-    }
+    String result = StringUtils.join(parts, joiner);
+    return result;
+  }
 
-    public String firstName() {
-        return fetchString("name.first_name");
-    }
+  public String name() {
+    return composite("name.formats", " ");
+  }
 
-    public String lastName() {
-        return fetchString("name.last_name");
-    }
+  public String firstName() {
+    return fetchString("name.first_name");
+  }
 
-    public String prefix() {
-        return fetchString("name.prefix");
-    }
+  public String lastName() {
+    return fetchString("name.last_name");
+  }
 
-    public String suffix() {
-        return fetchString("name.suffix");
-    }
+  public String prefix() {
+    return fetchString("name.prefix");
+  }
 
-    public String emailAddress() { return join(new Object[] {
-            firstName().toLowerCase(),
-            ".",
-            lastName().toLowerCase(),
-            "@",
-            fetchString("internet.free_email")
-    }); }
+  public String suffix() {
+    return fetchString("name.suffix");
+  }
 
-    public String phoneNumber() {
-        return numerify(fetchString("phone_number.formats"));
-    }
+  public String emailAddress() {
+    return join(new Object[] { firstName().toLowerCase(), ".", lastName().toLowerCase(), "@", fetchString("internet.free_email") });
+  }
 
-    // lorem
-    public List<String> words(int num) {
-        List<String> words = (List<String>) fetchObject("lorem.words");
-        List<String> returnList = new ArrayList();
-        for (int i = 0; i < num; i++) {
-            returnList.add(words.get(nextInt(words.size())));
-        }
-        return returnList;
-    }
+  public String phoneNumber() {
+    return numerify(fetchString("phone_number.formats"));
+  }
 
-    public List<String> words() {
-        return words(3);
+  public List<String> words(int num) {
+    List<String> words = (List<String>) fetchObject("lorem.words");
+    List<String> returnList = new ArrayList();
+    for (int i = 0; i < num; i++) {
+      returnList.add(words.get(nextInt(words.size())));
     }
+    return returnList;
+  }
 
-    public String sentence(int wordCount) {
-        return capitalize(join(words(wordCount + RandomUtils.nextInt(6)), " ") + ".");
-    }
+  public List<String> words() {
+    return words(3);
+  }
 
-    public String sentence() {
-        return sentence(3);
-    }
+  public String sentence(int wordCount) {
+    return capitalize(join(words(wordCount + RandomUtils.nextInt(6)), " ") + ".");
+  }
 
-    public List<String> sentences(int sentenceCount) {
-        List<String> sentences = new ArrayList<String>(sentenceCount);
-        for (int i = 0; i < sentenceCount; i++) {
-            sentences.add(sentence());
-        }
-        return sentences;
-    }
+  public String sentence() {
+    return sentence(3);
+  }
 
-    public String paragraph(int sentenceCount) {
-        return join(sentences(sentenceCount + nextInt(3)), " ");
+  public List<String> sentences(int sentenceCount) {
+    List<String> sentences = new ArrayList<String>(sentenceCount);
+    for (int i = 0; i < sentenceCount; i++) {
+      sentences.add(sentence());
     }
+    return sentences;
+  }
 
-    public String paragraph() {
-        return paragraph(3);
-    }
+  public String paragraph(int sentenceCount) {
+    return join(sentences(sentenceCount + nextInt(3)), " ");
+  }
 
-    public List<String> paragraphs(int paragraphCount) {
-        List<String> paragraphs = new ArrayList<String>(paragraphCount);
-        for (int i = 0; i < paragraphCount; i++) {
-            paragraphs.add(paragraph());
-        }
-        return paragraphs;
-    }
+  public String paragraph() {
+    return paragraph(3);
+  }
 
-    // address
+  public List<String> paragraphs(int paragraphCount) {
+    List<String> paragraphs = new ArrayList<String>(paragraphCount);
+    for (int i = 0; i < paragraphCount; i++) {
+      paragraphs.add(paragraph());
+    }
+    return paragraphs;
+  }
 
-    public String streetName() {
-        return composite("address.street_name_formats", (String)fetchObject("address.street_name_joiner"));
-    }
-    
-    public String streetAddressNumber() {
-      return fetchString("address.street_address");
-    }
+  public String streetName() {
+    return composite("address.street_name_formats", (String) fetchObject("address.street_name_joiner"));
+  }
 
-    public String streetAddress(boolean includeSecondary) {
-        String streetAddress = composite("address.street_formats", " ");
-        if (includeSecondary) {
-            streetAddress = streetAddress + " " + secondaryAddress();
-        }
-        return numerify(streetAddress);
-    }
+  public String streetAddressNumber() {
+    return fetchString("address.street_address");
+  }
 
-    public String secondaryAddress() {
-        return numerify(fetchString("address.secondary_address"));
+  public String streetAddress(boolean includeSecondary) {
+    String streetAddress = composite("address.street_formats", " ");
+    if (includeSecondary) {
+      streetAddress = streetAddress + " " + secondaryAddress();
     }
+    return numerify(streetAddress);
+  }
 
-    public String zipCode() {
-        return bothify(fetchString("address.postcode"));
-    }
+  public String secondaryAddress() {
+    return numerify(fetchString("address.secondary_address"));
+  }
 
-    public String streetSuffix() {
-        return fetchString("address.street_suffix");
-    }
+  public String zipCode() {
+    return bothify(fetchString("address.postcode"));
+  }
 
-    public String citySuffix() {
-        return fetchString("address.city_suffix");
-    }
+  public String streetSuffix() {
+    return fetchString("address.street_suffix");
+  }
 
-    public String cityPrefix() {
-        return fetchString("address.city_prefix");
-    }
+  public String citySuffix() {
+    return fetchString("address.city_suffix");
+  }
 
-    public String stateAbbr() {
-        return fetchString("address.state_abbr");
-    }
+  public String cityPrefix() {
+    return fetchString("address.city_prefix");
+  }
 
-    public String country() {
-        return fetchString("address.country");
-    }
+  public String stateAbbr() {
+    return fetchString("address.state_abbr");
+  }
+
+  public String country() {
+    return fetchString("address.country");
+  }
 }
--- src/test/java/com/github/javafaker/FakerTest.java
+++ src/test/java/com/github/javafaker/FakerTest.java
@@ -1,175 +1,148 @@
 package com.github.javafaker;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.Locale;
-
 import static org.junit.Assert.*;
 import static org.junit.runners.Parameterized.*;
 
-@RunWith(value = Parameterized.class)
-public class FakerTest {
-    private static final Logger logger = LoggerFactory.getLogger(FakerTest.class);
-    private Faker faker;
+@RunWith(value = Parameterized.class) public class FakerTest {
+  private static final Logger logger = LoggerFactory.getLogger(FakerTest.class);
 
-    public FakerTest(Locale locale) {
-        faker = new Faker(locale);
-    }
+  private Faker faker;
 
-    @Parameters(name = "testing locale {0}")
-    public static Collection<Object[]> data() {
-        Object[][] data = new Object[][]{{Locale.ENGLISH}, {Locale.FRENCH}, {new Locale("fi", "FI")}};
-        return Arrays.asList(data);
-    }
+  public FakerTest(Locale locale) {
+    faker = new Faker(locale);
+  }
 
-    @Test
-    public void testNames() {
-        String firstName = faker.firstName();
-        logger.info("Test first name: " + firstName);
-        assertNotNull(firstName);
+  @Parameters(name = "testing locale {0}") public static Collection<Object[]> data() {
+    Object[][] data = new Object[][] { { Locale.ENGLISH }, { Locale.FRENCH }, { new Locale("fi", "FI") } };
+    return Arrays.asList(data);
+  }
 
-        String lastName = faker.lastName();
-        logger.info("Test last name: " + lastName);
-        assertNotNull(lastName);
+  @Test public void testNames() {
+    String firstName = faker.firstName();
+    logger.info("Test first name: " + firstName);
+    assertNotNull(firstName);
+    String lastName = faker.lastName();
+    logger.info("Test last name: " + lastName);
+    assertNotNull(lastName);
+    String prefix = faker.prefix();
+    logger.info("Test prefix: " + prefix);
+    assertNotNull(prefix);
+    String suffix = faker.suffix();
+    logger.info("Test suffix: " + suffix);
+    assertNotNull(suffix);
+    String name = faker.name();
+    logger.info("Test name: " + name);
+    assertNotNull(name);
+  }
 
-        String prefix = faker.prefix();
-        logger.info("Test prefix: " + prefix);
-        assertNotNull(prefix);
+  @Test public void testEmailAddress() {
+    String emailAddress = faker.emailAddress();
+    logger.info("Test email address: " + emailAddress);
+    assertNotNull(emailAddress);
+  }
 
-        String suffix = faker.suffix();
-        logger.info("Test suffix: " + suffix);
-        assertNotNull(suffix);
+  @Test public void testPhoneNumber() {
+    String phoneNumber = faker.phoneNumber();
+    logger.info("Phone number: " + phoneNumber);
+    assertNotNull(phoneNumber);
+  }
 
-        String name = faker.name();
-        logger.info("Test name: " + name);
-        assertNotNull(name);
-    }
+  @Test public void testSpecifiedNumOfWords() {
+    List<String> words = faker.words(4);
+    logger.info("Test words: " + words.toString());
+    assertEquals(4, words.size());
+  }
 
-    @Test
-    public void testEmailAddress() {
-        String emailAddress = faker.emailAddress();
-        logger.info("Test email address: " + emailAddress);
-        assertNotNull(emailAddress);
-    }
+  @Test public void testSentence() {
+    String sentence = faker.sentence(7);
+    logger.info("Test sentence: " + sentence);
+    assertNotNull(sentence);
+  }
 
-    @Test
-    public void testPhoneNumber() {
-        String phoneNumber = faker.phoneNumber();
-        logger.info("Phone number: " + phoneNumber);
-        assertNotNull(phoneNumber);
-    }
+  @Test public void testSentences() {
+    List<String> sentences = faker.sentences(3);
+    logger.info("Test sentences: " + sentences);
+    assertEquals(3, sentences.size());
+  }
 
+  @Test public void testSpecifiedNumOfParagraphs() {
+    String paragraph = faker.paragraph(5);
+    logger.info("Test paragraph: " + paragraph);
+    assertNotNull(paragraph);
+  }
 
-    @Test
-    public void testSpecifiedNumOfWords() {
-        List<String> words = faker.words(4);
-        logger.info("Test words: " + words.toString());
-        assertEquals(4, words.size());
-    }
+  @Test public void testStreetName() {
+    String streetName = faker.streetName();
+    logger.info("Street name: " + streetName);
+    assertNotNull(streetName);
+  }
 
-    @Test
-    public void testSentence() {
-        String sentence = faker.sentence(7);
-        logger.info("Test sentence: " + sentence);
-        assertNotNull(sentence);
-    }
+  @Test public void testStreetAddress() {
+    String streetAddress = faker.streetAddress(true);
+    logger.info("Street address: " + streetAddress);
+    assertNotNull(streetAddress);
+  }
 
-    @Test
-    public void testSentences() {
-        List<String> sentences = faker.sentences(3);
-        logger.info("Test sentences: " + sentences);
-        assertEquals(3, sentences.size());
-    }
+  @Test public void testSecondaryAddress() {
+    String secondaryAddress = faker.secondaryAddress();
+    logger.info("Secondary address: " + secondaryAddress);
+    assertNotNull(secondaryAddress);
+  }
 
-    @Test
-    public void testSpecifiedNumOfParagraphs() {
-        String paragraph = faker.paragraph(5);
-        logger.info("Test paragraph: " + paragraph);
-        assertNotNull(paragraph);
-    }
+  @Test public void testZipCode() {
+    String zip = faker.zipCode();
+    logger.info("Address zip code: " + zip);
+    assertNotNull(zip);
+  }
 
-    @Test
-    public void testStreetName() {
-        String streetName = faker.streetName();
-        logger.info("Street name: " + streetName);
-        assertNotNull(streetName);
-    }
+  @Test public void testStreetSuffix() {
+    String streetSuffix = faker.streetSuffix();
+    logger.info("Street suffix: " + streetSuffix);
+    assertNotNull(streetSuffix);
+  }
 
-    @Test
-    public void testStreetAddress() {
-        String streetAddress = faker.streetAddress(true);
-        logger.info("Street address: " + streetAddress);
-        assertNotNull(streetAddress);
-    }
+  @Test public void testCitySuffix() {
+    String citySuffix = faker.citySuffix();
+    logger.info("City suffix: " + citySuffix);
+    assertNotNull(citySuffix);
+  }
 
-    @Test
-    public void testSecondaryAddress() {
-        String secondaryAddress = faker.secondaryAddress();
-        logger.info("Secondary address: " + secondaryAddress);
-        assertNotNull(secondaryAddress);
-    }
+  @Test public void testCityPrefix() {
+    String cityPrefix = faker.cityPrefix();
+    logger.info("City prefix: " + cityPrefix);
+    assertNotNull(cityPrefix);
+  }
 
-    @Test
-    public void testZipCode() {
-        String zip = faker.zipCode();
-        logger.info("Address zip code: " + zip);
-        assertNotNull(zip);
-    }
+  @Test public void testStateAbbr() {
+    String stateAbbr = faker.stateAbbr();
+    logger.info("State abbr: " + stateAbbr);
+    assertNotNull(stateAbbr);
+  }
 
-    @Test
-    public void testStreetSuffix() {
-        String streetSuffix = faker.streetSuffix();
-        logger.info("Street suffix: " + streetSuffix);
-        assertNotNull(streetSuffix);
-    }
+  @Test public void testCountry() {
+    String country = faker.country();
+    logger.info("Country: " + country);
+    assertNotNull(country);
+  }
 
-    @Test
-    public void testCitySuffix() {
-        String citySuffix = faker.citySuffix();
-        logger.info("City suffix: " + citySuffix);
-        assertNotNull(citySuffix);
-    }
+  @Test public void testParagraph() {
+    String paragraph = faker.paragraph();
+    logger.info("Paragraph: " + paragraph);
+    assertNotNull(paragraph);
+  }
 
-    @Test
-    public void testCityPrefix() {
-        String cityPrefix = faker.cityPrefix();
-        logger.info("City prefix: " + cityPrefix);
-        assertNotNull(cityPrefix);
-    }
-
-    @Test
-    public void testStateAbbr() {
-        String stateAbbr = faker.stateAbbr();
-        logger.info("State abbr: " + stateAbbr);
-        assertNotNull(stateAbbr);
-    }
-
-    @Test
-    public void testCountry() {
-        String country = faker.country();
-        logger.info("Country: " + country);
-        assertNotNull(country);
-    }
-
-    @Test
-    public void testParagraph() {
-        String paragraph = faker.paragraph();
-        logger.info("Paragraph: " + paragraph);
-        assertNotNull(paragraph);
-    }
-
-    @Test
-    public void testParagraphs() {
-        final int paragraphCount = 10;
-        List<String> paragraphs = faker.paragraphs(paragraphCount);
-        logger.info("Paragraphs: " + paragraphs);
-        assertEquals(paragraphCount, paragraphs.size());
-    }
+  @Test public void testParagraphs() {
+    final int paragraphCount = 10;
+    List<String> paragraphs = faker.paragraphs(paragraphCount);
+    logger.info("Paragraphs: " + paragraphs);
+    assertEquals(paragraphCount, paragraphs.size());
+  }
 }
