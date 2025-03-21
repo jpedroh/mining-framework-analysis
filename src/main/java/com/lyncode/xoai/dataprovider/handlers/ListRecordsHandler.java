7aa296aab4a3a45caffd54dc48e4124c1d110551
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 02:45:55 +0000

    Mining Framework Analysis

--- src/main/java/com/lyncode/xoai/dataprovider/handlers/ListRecordsHandler.java
+++ src/main/java/com/lyncode/xoai/dataprovider/handlers/ListRecordsHandler.java
@@ -1,5 +1,4 @@
 package com.lyncode.xoai.dataprovider.handlers;
-
 import com.lyncode.xoai.dataprovider.core.*;
 import com.lyncode.xoai.dataprovider.data.About;
 import com.lyncode.xoai.dataprovider.data.Item;
@@ -14,173 +13,149 @@ import com.lyncode.xoai.dataprovider.services.api.ResumptionTokenFormatter;
 import com.lyncode.xoai.dataprovider.xml.oaipmh.*;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
-
 import javax.xml.stream.XMLStreamException;
 import javax.xml.transform.TransformerException;
 import java.io.IOException;
 import java.util.List;
 
-
 public class ListRecordsHandler extends VerbHandler<ListRecordsType> {
-    private static Logger log = LogManager.getLogger(ListRecordsHandler.class);
-    private final int maxListSize;
-    private SetRepositoryHelper setRepository;
-    private ItemRepositoryHelper itemRepositoryHelper;
-    private RepositoryConfiguration identify;
-    private XOAIContext context;
-    private ResumptionTokenFormatter resumptionFormat;
+  private static Logger log = LogManager.getLogger(ListRecordsHandler.class);
 
+  private final int maxListSize;
 
-    public ListRecordsHandler(DateProvider formatter, int maxListSize,
-                              SetRepositoryHelper setRepository,
-                              ItemRepositoryHelper itemRepositoryHelper,
-                              RepositoryConfiguration identify,
-                              XOAIContext context, ResumptionTokenFormatter format) {
+  private SetRepositoryHelper setRepository;
 
-        super(formatter);
-        this.maxListSize = maxListSize;
-        this.setRepository = setRepository;
-        this.itemRepositoryHelper = itemRepositoryHelper;
-        this.identify = identify;
-        this.context = context;
-        this.resumptionFormat = format;
+  private ItemRepositoryHelper itemRepositoryHelper;
+
+  private RepositoryConfiguration identify;
+
+  private XOAIContext context;
+
+  private ResumptionTokenFormatter resumptionFormat;
+
+  public ListRecordsHandler(DateProvider formatter, int maxListSize, SetRepositoryHelper setRepository, ItemRepositoryHelper itemRepositoryHelper, RepositoryConfiguration identify, XOAIContext context, ResumptionTokenFormatter format) {
+    super(formatter);
+    this.maxListSize = maxListSize;
+    this.setRepository = setRepository;
+    this.itemRepositoryHelper = itemRepositoryHelper;
+    this.identify = identify;
+    this.context = context;
+    this.resumptionFormat = format;
+  }
+
+  @Override public ListRecordsType handle(OAIParameters parameters) throws OAIException, HandlerException {
+    ListRecordsType res = new ListRecordsType();
+    ResumptionToken token = parameters.getResumptionToken();
+    int length = maxListSize;
+    if (parameters.hasSet() && !setRepository.supportSets()) {
+      throw new DoesNotSupportSetsException();
     }
-
-
-    @Override
-    public ListRecordsType handle(OAIParameters parameters) throws OAIException, HandlerException {
-        ListRecordsType res = new ListRecordsType();
-        ResumptionToken token = parameters.getResumptionToken();
-        int length = maxListSize;
-
-        if (parameters.hasSet() && !setRepository.supportSets())
-            throw new DoesNotSupportSetsException();
-
-        log.debug("Getting items from data source");
-        ListItemsResults result;
-        if (!parameters.hasSet()) {
-            if (parameters.hasFrom() && !parameters.hasUntil())
-                result = itemRepositoryHelper.getItems(context, token.getOffset(),
-                        length, parameters.getMetadataPrefix(),
-                        parameters.getFrom());
-            else if (!parameters.hasFrom() && parameters.hasUntil())
-                result = itemRepositoryHelper.getItemsUntil(context, token.getOffset(),
-                        length, parameters.getMetadataPrefix(),
-                        parameters.getUntil());
-            else if (parameters.hasFrom() && parameters.hasUntil())
-                result = itemRepositoryHelper.getItems(context, token.getOffset(),
-                        length, parameters.getMetadataPrefix(),
-                        parameters.getFrom(), parameters.getUntil());
-            else
-                result = itemRepositoryHelper.getItems(context, token.getOffset(),
-                        length, parameters.getMetadataPrefix());
+    log.debug("Getting items from data source");
+    ListItemsResults result;
+    if (!parameters.hasSet()) {
+      if (parameters.hasFrom() && !parameters.hasUntil()) {
+        result = itemRepositoryHelper.getItems(context, token.getOffset(), length, parameters.getMetadataPrefix(), parameters.getFrom());
+      } else {
+        if (!parameters.hasFrom() && parameters.hasUntil()) {
+          result = itemRepositoryHelper.getItemsUntil(context, token.getOffset(), length, parameters.getMetadataPrefix(), parameters.getUntil());
         } else {
-            if (!setRepository.exists(context, parameters.getSet()))
-                throw new NoMatchesException();
-            if (parameters.hasFrom() && !parameters.hasUntil())
-                result = itemRepositoryHelper.getItems(context, token.getOffset(),
-                        length, parameters.getMetadataPrefix(),
-                        parameters.getSet(), parameters.getFrom());
-            else if (!parameters.hasFrom() && parameters.hasUntil())
-                result = itemRepositoryHelper.getItemsUntil(context, token.getOffset(),
-                        length, parameters.getMetadataPrefix(),
-                        parameters.getSet(), parameters.getUntil());
-            else if (parameters.hasFrom() && parameters.hasUntil())
-                result = itemRepositoryHelper.getItems(context, token.getOffset(),
-                        length, parameters.getMetadataPrefix(),
-                        parameters.getSet(), parameters.getFrom(),
-                        parameters.getUntil());
-            else
-                result = itemRepositoryHelper.getItems(context, token.getOffset(),
-                        length, parameters.getMetadataPrefix(),
-                        parameters.getSet());
+          if (parameters.hasFrom() && parameters.hasUntil()) {
+            result = itemRepositoryHelper.getItems(context, token.getOffset(), length, parameters.getMetadataPrefix(), parameters.getFrom(), parameters.getUntil());
+          } else {
+            result = itemRepositoryHelper.getItems(context, token.getOffset(), length, parameters.getMetadataPrefix());
+          }
         }
-        log.debug("Items retrived from data source");
-
-        List<Item> results = result.getResults();
-        if (results.isEmpty())
-            throw new NoMatchesException();
-
-        ResumptionToken newToken;
-        if (result.hasMore()) {
-            newToken = new ResumptionToken(token.getOffset() + length,
-                    parameters);
+      }
+    } else {
+      if (!setRepository.exists(context, parameters.getSet())) {
+        throw new NoMatchesException();
+      }
+      if (parameters.hasFrom() && !parameters.hasUntil()) {
+        result = itemRepositoryHelper.getItems(context, token.getOffset(), length, parameters.getMetadataPrefix(), parameters.getSet(), parameters.getFrom());
+      } else {
+        if (!parameters.hasFrom() && parameters.hasUntil()) {
+          result = itemRepositoryHelper.getItemsUntil(context, token.getOffset(), length, parameters.getMetadataPrefix(), parameters.getSet(), parameters.getUntil());
         } else {
-            newToken = new ResumptionToken();
+          if (parameters.hasFrom() && parameters.hasUntil()) {
+            result = itemRepositoryHelper.getItems(context, token.getOffset(), length, parameters.getMetadataPrefix(), parameters.getSet(), parameters.getFrom(), parameters.getUntil());
+          } else {
+            result = itemRepositoryHelper.getItems(context, token.getOffset(), length, parameters.getMetadataPrefix(), parameters.getSet());
+          }
         }
-
-        if (parameters.hasResumptionToken() || !newToken.isEmpty()) {
-            ResumptionTokenType resToken = new ResumptionTokenType();
-            if (!newToken.isEmpty())
-                resToken.setValue(resumptionFormat.format(newToken));
-            resToken.setCursor(token.getOffset() / maxListSize);
-            if (result.hasTotalResults())
-                resToken.setCompleteListSize(result.getTotal());
-            res.setResumptionToken(resToken);
-        }
-
-        log.debug("Now adding records to the OAI-PMH Output");
-        for (Item i : results)
-            res.getRecord().add(this.createRecord(parameters, i));
-
-        return res;
+      }
     }
-
-
-    private RecordType createRecord(OAIParameters parameters, Item item)
-            throws BadArgumentException, CannotDisseminateRecordException,
-            OAIException, NoMetadataFormatsException, CannotDisseminateFormatException {
-        log.debug("Metadata format: " + parameters.getMetadataPrefix());
-        MetadataFormat format = context.getFormatByPrefix(parameters
-                .getMetadataPrefix());
-        RecordType record = new RecordType();
-        HeaderType header = new HeaderType();
-        log.debug("ItemHelper: " + item.getIdentifier());
-        header.setIdentifier(item.getIdentifier());
-
-        ItemHelper itemHelperWrap = new ItemHelper(item);
-
-        header.setDatestamp(getFormatter().format(item.getDatestamp(), identify.getGranularity()));
-        for (ReferenceSet s : itemHelperWrap.getSets(context))
-            header.getSetSpec().add(s.getSetSpec());
-        if (item.isDeleted())
-            header.setStatus(StatusType.DELETED);
-        record.setHeader(header);
-
-        if (!item.isDeleted()) {
-            MetadataType metadata = null;
-            try {
-                if (context.getTransformer().hasXslTemplates()) {
-                    metadata = new MetadataType(itemHelperWrap.toPipeline(true)
-                            .apply(context.getTransformer().getXslTemplates().getValue())
-                            .apply(format.getXsltTemplates())
-                            .getTransformed());
-                } else {
-                    metadata = new MetadataType(itemHelperWrap.toPipeline(true)
-                            .apply(format.getXsltTemplates())
-                            .getTransformed());
-                }
-            } catch (WritingXmlException e) {
-                throw new OAIException(e);
-            } catch (XMLStreamException e) {
-                throw new OAIException(e);
-            } catch (TransformerException e) {
-                throw new OAIException(e);
-            } catch (IOException e) {
-                throw new OAIException(e);
-            }
-
-            record.setMetadata(metadata);
-
-            log.debug("Outputting About");
-            if (item.getAbout() != null) {
-                for (About abj : item.getAbout()) {
-                    AboutType about = new AboutType();
-                    about.setAny(abj.getXML());
-                    record.getAbout().add(about);
-                }
-            }
-        }
-        return record;
+    log.debug("Items retrived from data source");
+    List<Item> results = result.getResults();
+    if (results.isEmpty()) {
+      throw new NoMatchesException();
     }
+    ResumptionToken newToken;
+    if (result.hasMore()) {
+      newToken = new ResumptionToken(token.getOffset() + length, parameters);
+    } else {
+      newToken = new ResumptionToken();
+    }
+    if (parameters.hasResumptionToken() || !newToken.isEmpty()) {
+      ResumptionTokenType resToken = new ResumptionTokenType();
+      if (!newToken.isEmpty()) {
+        resToken.setValue(resumptionFormat.format(newToken));
+      }
+      resToken.setCursor(token.getOffset() / maxListSize);
+      if (result.hasTotalResults()) {
+        resToken.setCompleteListSize(result.getTotal());
+      }
+      res.setResumptionToken(resToken);
+    }
+    log.debug("Now adding records to the OAI-PMH Output");
+    for (Item i : results) {
+      res.getRecord().add(this.createRecord(parameters, i));
+    }
+    return res;
+  }
+
+  private RecordType createRecord(OAIParameters parameters, Item item) throws BadArgumentException, CannotDisseminateRecordException, OAIException, NoMetadataFormatsException, CannotDisseminateFormatException {
+    log.debug("Metadata format: " + parameters.getMetadataPrefix());
+    MetadataFormat format = context.getFormatByPrefix(parameters.getMetadataPrefix());
+    RecordType record = new RecordType();
+    HeaderType header = new HeaderType();
+    log.debug("ItemHelper: " + item.getIdentifier());
+    header.setIdentifier(item.getIdentifier());
+    ItemHelper itemHelperWrap = new ItemHelper(item);
+    header.setDatestamp(getFormatter().format(item.getDatestamp(), identify.getGranularity()));
+    for (ReferenceSet s : itemHelperWrap.getSets(context)) {
+      header.getSetSpec().add(s.getSetSpec());
+    }
+    if (item.isDeleted()) {
+      header.setStatus(StatusType.DELETED);
+    }
+    record.setHeader(header);
+    if (!item.isDeleted()) {
+      MetadataType metadata = null;
+      try {
+        if (context.getTransformer().hasXslTemplates()) {
+          metadata = new MetadataType(itemHelperWrap.toPipeline(true).apply(context.getTransformer().getXslTemplates().getValue()).apply(format.getXsltTemplates()).getTransformed());
+        } else {
+          metadata = new MetadataType(itemHelperWrap.toPipeline(true).apply(format.getXsltTemplates()).getTransformed());
+        }
+      } catch (WritingXmlException e) {
+        throw new OAIException(e);
+      } catch (XMLStreamException e) {
+        throw new OAIException(e);
+      } catch (TransformerException e) {
+        throw new OAIException(e);
+      } catch (IOException e) {
+        throw new OAIException(e);
+      }
+      record.setMetadata(metadata);
+      log.debug("Outputting About");
+      if (item.getAbout() != null) {
+        for (About abj : item.getAbout()) {
+          AboutType about = new AboutType();
+          about.setAny(abj.getXML());
+          record.getAbout().add(about);
+        }
+      }
+    }
+    return record;
+  }
 }
