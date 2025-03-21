d6a15afd68416a1c22757381cf08d29015ca1e7b
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 15:54:54 +0000

    Mining Framework Analysis

--- ninja-core/src/main/java/ninja/Context.java
+++ ninja-core/src/main/java/ninja/Context.java
@@ -1,43 +1,45 @@
 package ninja;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Map;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import ninja.bodyparser.BodyParserEngineJson;
 import ninja.bodyparser.BodyParserEngineManager;
 import ninja.session.FlashCookie;
 import ninja.session.SessionCookie;
 import ninja.utils.ResponseStreams;
-
 import org.apache.commons.fileupload.FileItemIterator;
 
 public interface Context {
+  enum HTTP_STATUS {
+    notFound404(404),
+    ok200(200),
+    forbidden403(403),
+    teapot418(418),
+    badRequest400(400),
+    noContent204(204),
+    created201(201)
+    ;
 
-	enum HTTP_STATUS {
-        notFound404(404), ok200(200), forbidden403(403), teapot418(418), badRequest400(400),
-        noContent204(204), created201(201);
-        public final int code;
+    public final int code;
 
-        private HTTP_STATUS(int code) {
-            this.code = code;
-        }
+    private HTTP_STATUS(int code) {
+      this.code = code;
     }
-	
-	/**
+  }
+
+  /**
 	 * Content type of the request we got.
 	 * Important for content negotiation...
 	 * 
 	 * @return the content type of the incoming request.
 	 * 
 	 */
-	String getRequestContentType();
+  String getRequestContentType();
 
-	/**
+  /**
 	 * Returns the uri as seen by the server.
 	 * 
 	 * http://example.com/index would return
@@ -45,9 +47,9 @@ public interface Context {
 	 * 
 	 * @return the uri as seen by the server
 	 */
-    String getRequestUri();
+  String getRequestUri();
 
-    /**
+  /**
      * Returns the flash cookie. Flash cookies only live for one request.
      * Good uses are error messages to display. Almost everything else
      * is bad use of Flash Cookies.
@@ -56,9 +58,9 @@ public interface Context {
      * 
      * @return the flash cookie of that request.
      */
-    FlashCookie getFlashCookie();
-	
-    /**
+  FlashCookie getFlashCookie();
+
+  /**
      * Returns the client side session. It is a cookie. Therefore you
      * cannot store a lot of information inside the cookie. This is by intention.
      * 
@@ -67,17 +69,17 @@ public interface Context {
      * 
      * @return the Session of that request / response cycle.
      */
-	SessionCookie getSessionCookie();
+  SessionCookie getSessionCookie();
 
-    /**
+  /**
      * Add the given cookie to the response
      *
      * @param cookie The cookie to add
      * @return This context
      */
-    public Context addCookie(Cookie cookie);
+  public Context addCookie(Cookie cookie);
 
-    /**
+  /**
      * Get the underlying HTTP servlet request
      * 
      * @Deprecated because it directly refers to the servlet api.
@@ -88,10 +90,9 @@ public interface Context {
      *
      * @return The HTTP servlet request
      */
-	@Deprecated
-	HttpServletRequest getHttpServletRequest();
+  @Deprecated HttpServletRequest getHttpServletRequest();
 
-    /**
+  /**
      * Get the underlying HTTP servlet response
      * 
      * @Deprecated because it directly refers to the servlet api.
@@ -103,19 +104,18 @@ public interface Context {
      * @return The HTTP servlet response
      *
      */
-	@Deprecated
-	HttpServletResponse getHttpServletResponse();
+  @Deprecated HttpServletResponse getHttpServletResponse();
 
-    /**
+  /**
      * Get the parameter with the given key from the request.  The parameter may either be a query parameter, or in the
      * case of form submissions, may be a form parameter
      *
      * @param key The key of the parameter
      * @return The value, or null if no parameter was found
      */
-    String getParameter(String key);
+  String getParameter(String key);
 
-	/**
+  /**
 	 * Same like {@link #getParameter(String)}, but returns given defaultValue
 	 * instead of null in case parameter cannot be found
 	 * 
@@ -123,9 +123,9 @@ public interface Context {
 	 * @param defaultValue
 	 * @return
 	 */
-	String getParameter(String key, String defaultValue);
-	
-	/**
+  String getParameter(String key, String defaultValue);
+
+  /**
 	 * Same like {@link #getParameter(String)}, but converts the
 	 * parameter to Integer if found.
 	 * 
@@ -133,9 +133,9 @@ public interface Context {
 	 * @param defaultValue
 	 * @return
 	 */
-	Integer getParameterAsInteger(String key);
-	
-	/**
+  Integer getParameterAsInteger(String key);
+
+  /**
 	 * Same like {@link #getParameter(String, String)}, but converts the
 	 * parameter to Integer if found.
 	 * 
@@ -143,17 +143,17 @@ public interface Context {
 	 * @param defaultValue
 	 * @return
 	 */
-	Integer getParameterAsInteger(String key, Integer defaultValue);
-	
-    /**
+  Integer getParameterAsInteger(String key, Integer defaultValue);
+
+  /**
      * Get the path parameter for the given key
      *
      * @param key The key of the path parameter
      * @return The path parameter, or null if no such path parameter is defined
      */
-	String getPathParameter(String key);
+  String getPathParameter(String key);
 
-	/**
+  /**
 	 * Get the path parameter for the given key and convert it to Integer.
 	 * 
 	 * @param key
@@ -161,43 +161,38 @@ public interface Context {
 	 * @return the numeric path parameter, or null of no such path parameter is
 	 *         defined, or if it cannot be parsed to int
 	 */
-	Integer getPathParameterAsInteger(String key);
+  Integer getPathParameterAsInteger(String key);
 
-    /**
+  /**
      * Get all the parameters from the request
      *
      * @return The parameters
      */
-    Map<String, String[]> getParameters();
+  Map<String, String[]> getParameters();
 
-    /**
+  /**
      * Get the request header with the given name
      *
      * @return The header value
      */
-    String getHeader(String name);
+  String getHeader(String name);
 
-    /**
+  /**
      * Get all the headers from the request
      *
      * @return The headers
      */
-    Map<String, String> getHeaders();
+  Map<String, String> getHeaders();
 
-    /**
+  /**
      * Get the cookie value from the request, if defined
      *
      * @param name The name of the cookie
      * @return The cookie value, or null if the cookie was not found
      */
-    String getCookieValue(String name);
+  String getCookieValue(String name);
 
-	///////////////////////////////////////////////////////////////////////////
-	// Allows to get the nicely parsed content of the request.
-	// For instance if the content is a json you could simply get the json
-	// as Java object.
-	///////////////////////////////////////////////////////////////////////////
-	/**
+  /**
 	 * This will give you the request body nicely parsed. You can register your
 	 * own parsers depending on the request type.
 	 * 
@@ -207,26 +202,26 @@ public interface Context {
 	 * @param classOfT The class of the result.
 	 * @return The parsed request or null if something went wrong.
 	 */
-	<T> T parseBody(Class<T> classOfT);
+  <T extends java.lang.Object> T parseBody(Class<T> classOfT);
 
-    /**
+  /**
      * Indicate that this request is going to be handled asynchronously
      */
-    void handleAsync();
+  void handleAsync();
 
-    /**
+  /**
      * Indicate that request processing of an async request is complete.
      */
-    void returnResultAsync(Result result);
+  void returnResultAsync(Result result);
 
-    /**
+  /**
      * Indicate that processing this request is complete.
      */
-    void asyncRequestComplete();
+  void asyncRequestComplete();
 
-    Result controllerReturned();
-    
-    /**
+  Result controllerReturned();
+
+  /**
      * Finalizing the headers copies all stuff into the headers. 
      * 
      * After finalizing the headers you can access the responseStreams.
@@ -234,34 +229,34 @@ public interface Context {
      * @param result
      * @return
      */
-    ResponseStreams finalizeHeaders(Result result);
+  ResponseStreams finalizeHeaders(Result result);
 
-    /**
+  /**
      * Get the input stream to read the request.
      *
      * Must not be used if getReader has been called.
      *
      * @return The input stream
      */
-    InputStream getInputStream() throws IOException;
+  InputStream getInputStream() throws IOException;
 
-    /**
+  /**
      * Get the reader to read the request.
      *
      * Must not be used if getInputStream has been called.
      *
      * @return The reader
      */
-    BufferedReader getReader() throws IOException;
+  BufferedReader getReader() throws IOException;
 
-    /**
+  /**
      * Get the route for this context
      *
      * @return The route
      */
-    Route getRoute();
-    
-    /**
+  Route getRoute();
+
+  /**
      * Check if request is of type multipart.
      * Important when you want to process uploads for instance.
      * 
@@ -269,9 +264,9 @@ public interface Context {
      * 
      * @return true if request is of type multipart.
      */
-    boolean isMultipart();
-    
-    /**
+  boolean isMultipart();
+
+  /**
      * Gets the FileItemIterator of the input.
      * 
      * Can be used to process uploads in a streaming fashion.
@@ -279,7 +274,5 @@ public interface Context {
      * 
      * @return the FileItemIterator of the request or null if there was an error.
      */
-    FileItemIterator getFileItemIterator();
-    
-    
+  FileItemIterator getFileItemIterator();
 }
