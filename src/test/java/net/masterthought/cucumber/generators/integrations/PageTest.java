bf48460dec93ae1db1b57cc687b4148fdc7b5d23
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 03:18:49 +0000

    Mining Framework Analysis

--- src/test/java/net/masterthought/cucumber/generators/integrations/PageTest.java
+++ src/test/java/net/masterthought/cucumber/generators/integrations/PageTest.java
@@ -1,17 +1,14 @@
 package net.masterthought.cucumber.generators.integrations;
-
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import org.apache.commons.io.FileUtils;
 import java.util.Arrays;
 import java.util.List;
-
-import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang.StringUtils;
 import org.jsoup.Jsoup;
 import org.junit.After;
-
 import net.masterthought.cucumber.ReportBuilder;
 import net.masterthought.cucumber.ReportGenerator;
 import net.masterthought.cucumber.ValidationException;
@@ -23,31 +20,27 @@ import net.masterthought.cucumber.json.Output;
  * @author Damian Szczepanik (damianszczepanik@github)
  */
 public abstract class PageTest extends ReportGenerator {
+  protected AbstractPage page;
 
-    protected AbstractPage page;
+  @After public void cleanUp() {
+    File report = new File(configuration.getReportDirectory(), ReportBuilder.BASE_DIRECTORY);
+    FileUtils.deleteQuietly(report);
+  }
 
-    @After
-    public void cleanUp() {
-        // delete report file if was already created by any of test
-        File report = new File(configuration.getReportDirectory(), ReportBuilder.BASE_DIRECTORY);
-        FileUtils.deleteQuietly(report);
+  protected DocumentAssertion documentFrom(String pageName) {
+    File input = new File(configuration.getReportDirectory(), ReportBuilder.BASE_DIRECTORY + File.separatorChar + pageName);
+    try {
+      return new DocumentAssertion(Jsoup.parse(input, StandardCharsets.UTF_8.name(), StringUtils.EMPTY));
+    } catch (IOException e) {
+      throw new ValidationException(e);
     }
+  }
 
-    protected DocumentAssertion documentFrom(String pageName) {
-        File input = new File(configuration.getReportDirectory(),
-                ReportBuilder.BASE_DIRECTORY + File.separatorChar + pageName);
-        try {
-            return new DocumentAssertion(Jsoup.parse(input, StandardCharsets.UTF_8.name(), StringUtils.EMPTY));
-        } catch (IOException e) {
-            throw new ValidationException(e);
-        }
+  protected String[] getMessages(Output[] outputs) {
+    List<String> messages = new ArrayList<>();
+    for (Output outputElement : outputs) {
+      messages.addAll(Arrays.asList(outputElement.getMessages()));
     }
-
-    protected String[] getMessages(Output[] outputs) {
-        List<String> messages = new ArrayList<>();
-        for (Output outputElement : outputs) {
-            messages.addAll(Arrays.asList(outputElement.getMessages()));
-        }
-        return messages.toArray(new String[messages.size()]);
-    }
+    return messages.toArray(new String[messages.size()]);
+  }
 }
