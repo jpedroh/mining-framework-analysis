5ade2d5a56348127af07b425dccc90a200c4ea5f
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 07:34:00 +0000

    Mining Framework Analysis

--- src/main/java/nl/dries/wicket/hibernate/dozer/visitor/ObjectVisitor.java
+++ src/main/java/nl/dries/wicket/hibernate/dozer/visitor/ObjectVisitor.java
@@ -1,15 +1,12 @@
 package nl.dries.wicket.hibernate.dozer.visitor;
-
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
-
 import nl.dries.wicket.hibernate.dozer.SessionFinder;
 import nl.dries.wicket.hibernate.dozer.helper.ModelCallback;
-import nl.dries.wicket.hibernate.dozer.helper.Seen;
-
 import org.hibernate.engine.SessionFactoryImplementor;
+import nl.dries.wicket.hibernate.dozer.helper.Seen;
 import org.hibernate.engine.SessionImplementor;
 import org.hibernate.proxy.HibernateProxyHelper;
 
@@ -18,91 +15,77 @@ import org.hibernate.proxy.HibernateProxyHelper;
  * 
  * @author schulten
  */
-public class ObjectVisitor<T>
-{
-	/** Root */
-	private final T root;
+public class ObjectVisitor<T extends java.lang.Object> {
+  /** Root */
+  private final T root;
 
-	/** */
-	private final SessionFinder sessionFinder;
+  /** */
+  private final SessionFinder sessionFinder;
 
-	/*** */
-	private final ModelCallback callback;
+  /*** */
+  private final ModelCallback callback;
 
-	/** Seen objects, to prevent never ending recursion etc */
-	private final Seen seen;
+  /** Seen objects, to prevent never ending recursion etc */
+  private final Seen seen;
 
-	/**
+  /**
 	 * @param root
 	 * @param sessionFinder
 	 * @param callback
 	 */
-	public ObjectVisitor(T root, SessionFinder sessionFinder, ModelCallback callback)
-	{
-		this.root = root;
-		this.sessionFinder = sessionFinder;
-		this.callback = callback;
-		this.seen = new Seen();
-	}
+  public ObjectVisitor(T root, SessionFinder sessionFinder, ModelCallback callback) {
+    this.root = root;
+    this.sessionFinder = sessionFinder;
+    this.callback = callback;
+    this.seen = new Seen();
+  }
 
-	/**
+  /**
 	 * Walk the object tree, handeling registering un-initialized proxies
 	 * 
 	 * @return the root object
 	 */
-	public T walk()
-	{
-		walk(root);
-		return root;
-	}
+  public T walk() {
+    walk(root);
+    return root;
+  }
 
-	/**
+  /**
 	 * Recursive walker
 	 * 
 	 * @param current
 	 *            current object
+	 * @param previousHibernateObject
+	 *            the previous Hibernate object (for detecting parent <-> child mappings)
+	 * @param chain
+	 *            the current visited object chain, includes current object
 	 */
-	private void walk(Object current)
-	{
-		Class<?> objectClass = HibernateProxyHelper.getClassWithoutInitializingProxy(current);
-
-		SessionImplementor sessionImpl = (SessionImplementor) sessionFinder.getHibernateSession(objectClass);
-		SessionFactoryImplementor factory = sessionImpl.getFactory();
-
-		final VisitorStrategy strategy;
-		if (factory.getClassMetadata(objectClass) != null)
-		{
-			strategy = new HibernateObjectVisitor(sessionImpl, callback, factory.getClassMetadata(objectClass));
-		}
-		else if (current instanceof Collection<?>)
-		{
-			strategy = new CollectionVisitor();
-		}
-		else if (current instanceof Map<?, ?>)
-		{
-			strategy = new MapVisitor();
-		}
-		else
-		{
-			strategy = new BasicObjectVisitor(sessionFinder, callback);
-		}
-
-		seen.add(current);
-
-		Set<Object> toWalk = strategy.visit(current);
-
-		Iterator<Object> iter = toWalk.iterator();
-		while (iter.hasNext())
-		{
-			Object next = iter.next();
-
-			// Check if we have already seen the exact object tree before vistiting it, preventing never ending
-			// recursion
-			if (!seen.contains(next))
-			{
-				walk(next);
-			}
-		}
-	}
-
+  private void walk(Object current) {
+    Class<?> objectClass = HibernateProxyHelper.getClassWithoutInitializingProxy(current);
+    SessionImplementor sessionImpl = (SessionImplementor) sessionFinder.getHibernateSession(objectClass);
+    SessionFactoryImplementor factory = sessionImpl.getFactory();
+    final VisitorStrategy strategy;
+    if (factory.getClassMetadata(objectClass) != null) {
+      strategy = new HibernateObjectVisitor(sessionImpl, callback, factory.getClassMetadata(objectClass));
+    } else {
+      if (current instanceof Collection<?>) {
+        strategy = new CollectionVisitor();
+      } else {
+        if (current instanceof Map<?, ?>) {
+          strategy = new MapVisitor();
+        } else {
+          strategy = new BasicObjectVisitor(sessionFinder, callback);
+        }
+      }
+    }
+    seen.add(current);
+    Set<Object> toWalk = strategy.visit(current);
+    Iterator<Object> iter = toWalk.iterator();
+    while (iter.hasNext()) {
+      Object next = iter.next();
+      if (!seen.contains(next)) {
+        walk(next);
+      }
+    }
+  }
 }
