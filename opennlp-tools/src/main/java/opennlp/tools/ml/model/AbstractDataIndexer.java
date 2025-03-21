dc217f97dd55da3de951f4cd9d227a402848b300
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 22:45:26 +0000

    Mining Framework Analysis

--- opennlp-tools/src/main/java/opennlp/tools/ml/model/AbstractDataIndexer.java
+++ opennlp-tools/src/main/java/opennlp/tools/ml/model/AbstractDataIndexer.java
@@ -1,63 +1,48 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
 package opennlp.tools.ml.model;
-
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-
 import opennlp.tools.ml.AbstractTrainer;
 import opennlp.tools.ml.PluggableParameters;
 import opennlp.tools.util.InsufficientTrainingDataException;
-
 
 /**
  * Abstract class for collecting event and context counts used in training.
  *
  */
 public abstract class AbstractDataIndexer implements DataIndexer {
-
   public static final String CUTOFF_PARAM = AbstractTrainer.CUTOFF_PARAM;
+
   public static final int CUTOFF_DEFAULT = AbstractTrainer.CUTOFF_DEFAULT;
 
   public static final String SORT_PARAM = "sort";
+
   public static final boolean SORT_DEFAULT = true;
 
   PluggableParameters parameters;
 
-  public void init(Map<String,String> indexingParameters,Map<String, String> reportMap){
-    parameters=new PluggableParameters(indexingParameters, reportMap);
+  public void init(Map<String, String> indexingParameters, Map<String, String> reportMap) {
+    parameters = new PluggableParameters(indexingParameters, reportMap);
   }
 
   private int numEvents;
+
   /** The integer contexts associated with each unique event. */
   protected int[][] contexts;
+
   /** The integer outcome associated with each unique event. */
   protected int[] outcomeList;
+
   /** The number of times an event occured in the training data. */
   protected int[] numTimesEventsSeen;
+
   /** The predicate/context names. */
   protected String[] predLabels;
+
   /** The names of the outcomes. */
   protected String[] outcomeLabels;
+
   /** The number of times each predicate occured. */
   protected int[] predCounts;
 
@@ -99,42 +84,34 @@ public abstract class AbstractDataIndexer implements DataIndexer {
     int numUniqueEvents = 1;
     numEvents = eventsToCompare.size();
     if (sort && eventsToCompare.size() > 0) {
-
       Collections.sort(eventsToCompare);
-
       ComparableEvent ce = eventsToCompare.get(0);
       for (int i = 1; i < numEvents; i++) {
         ComparableEvent ce2 = eventsToCompare.get(i);
-
         if (ce.compareTo(ce2) == 0) {
-          ce.seen++; // increment the seen count
-          eventsToCompare.set(i, null); // kill the duplicate
-        }
-        else {
-          ce = ce2; // a new champion emerges...
-          numUniqueEvents++; // increment the # of unique events
+          ce.seen++;
+          eventsToCompare.set(i, null);
+        } else {
+          ce = ce2;
+          numUniqueEvents++;
         }
       }
-
-    }
-    else {
+    } else {
       numUniqueEvents = eventsToCompare.size();
     }
-
-    if(numUniqueEvents == 0) {
+    if (numUniqueEvents == 0) {
       throw new InsufficientTrainingDataException("Insufficient training data to create model.");
     }
-
-    if (sort) System.out.println("done. Reduced " + numEvents + " events to " + numUniqueEvents + ".");
-
+    if (sort) {
+      System.out.println("done. Reduced " + numEvents + " events to " + numUniqueEvents + ".");
+    }
     contexts = new int[numUniqueEvents][];
     outcomeList = new int[numUniqueEvents];
     numTimesEventsSeen = new int[numUniqueEvents];
-
     for (int i = 0, j = 0; i < numEvents; i++) {
       ComparableEvent evt = eventsToCompare.get(i);
       if (null == evt) {
-        continue; // this was a dupe, skip over it.
+        continue;
       }
       numTimesEventsSeen[j] = evt.seen;
       outcomeList[j] = evt.outcome;
@@ -143,7 +120,6 @@ public abstract class AbstractDataIndexer implements DataIndexer {
     }
     return numUniqueEvents;
   }
-
 
   public int getNumEvents() {
     return numEvents;
@@ -156,13 +132,12 @@ public abstract class AbstractDataIndexer implements DataIndexer {
    * @param counter The predicate counters.
    * @param cutoff The cutoff which determines whether a predicate is included.
    */
-  protected static void update(String[] ec, Set<String> predicateSet, Map<String,Integer> counter, int cutoff) {
+  protected static void update(String[] ec, Set<String> predicateSet, Map<String, Integer> counter, int cutoff) {
     for (String s : ec) {
       Integer i = counter.get(s);
       if (i == null) {
         counter.put(s, 1);
-      }
-      else {
+      } else {
         counter.put(s, i + 1);
       }
       if (!predicateSet.contains(s) && counter.get(s) >= cutoff) {
@@ -181,7 +156,7 @@ public abstract class AbstractDataIndexer implements DataIndexer {
    * @return a <code>String[]</code> value
    * @since maxent 1.2.6
    */
-  protected static String[] toIndexedStringArray(Map<String,Integer> labelToIndexMap) {
+  protected static String[] toIndexedStringArray(Map<String, Integer> labelToIndexMap) {
     final String[] array = new String[labelToIndexMap.size()];
     for (String label : labelToIndexMap.keySet()) {
       array[labelToIndexMap.get(label)] = label;
