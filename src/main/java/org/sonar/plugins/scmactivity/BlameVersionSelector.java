2245f6535de15d2a35d589dc543fb7b64da1f922
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 10:41:16 +0000

    Mining Framework Analysis

--- src/main/java/org/sonar/plugins/scmactivity/BlameVersionSelector.java
+++ src/main/java/org/sonar/plugins/scmactivity/BlameVersionSelector.java
@@ -1,63 +1,6 @@
-/*
- * SonarQube SCM Activity Plugin
- * Copyright (C) 2010 SonarSource
- * dev@sonar.codehaus.org
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3 of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
- */
-package org.sonar.plugins.scmactivity;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.sonar.api.BatchExtension;
-import org.sonar.api.batch.SensorContext;
-import org.sonar.api.batch.fs.InputFile;
-import org.sonar.api.resources.Resource;
-
-import java.io.File;
-
-public class BlameVersionSelector implements BatchExtension {
-  private static final Logger LOG = LoggerFactory.getLogger(BlameVersionSelector.class);
-
-  private final ScmConfiguration configuration;
-  private final Blame blame;
-
-  public BlameVersionSelector(ScmConfiguration configuration, Blame blame) {
-    this.configuration = configuration;
-    this.blame = blame;
-  }
-
-  public MeasureUpdate detect(Resource sonarFile, InputFile inputFile, SensorContext context, boolean hasPreviousMeasures) {
-    File file = inputFile.file();
-
-    if (inputFile.status() == InputFile.Status.SAME && hasPreviousMeasures && !configuration.isReloadBlameEnabled()) {
-      return fileNotChanged(file, sonarFile);
-    }
-
-    return fileChanged(file, sonarFile, inputFile.lines());
-  }
-
-  private MeasureUpdate fileNotChanged(File file, Resource resource) {
-    LOG.debug("File not changed since previous analysis: {}", file);
-
-    return new CopyPreviousMeasures(resource);
-  }
-
-  private MeasureUpdate fileChanged(File file, Resource resource, int lineCount) {
-    LOG.debug("File changed since previous analysis: {}", file);
-
-    return blame.save(file, resource, lineCount);
-  }
-}
+  package    org . sonar . plugins . scmactivity ;   import   org . slf4j . Logger ;  import   org . slf4j . LoggerFactory ;  import    org . sonar . api . BatchExtension ;  import     org . sonar . api . batch . SensorContext ;  import     org . sonar . api . config . Settings ;  import     org . sonar . api . resources . Resource ;  import   java . io . File ;  import      org . sonar . api . batch . fs . InputFile ;   public class BlameVersionSelector  implements  BatchExtension  {   private static final Logger  LOG =  LoggerFactory . getLogger  (  BlameVersionSelector . class ) ;   private final ScmConfiguration  configuration ;   private final Blame  blame ;   public BlameVersionSelector  (  ScmConfiguration configuration ,  Blame blame ,  Sha1Generator sha1Generator ,  FileToResource fileToResource ,  ProjectFileSystem projectFileSystem )  {    this . configuration = configuration ;    this . blame = blame ;    this . sha1Generator = sha1Generator ;    this . fileToResource = fileToResource ;    this . projectFileSystem = projectFileSystem ; } 
+<<<<<<<
+  public MeasureUpdate detect  (  InputFile inputFile ,  String previousSha1 ,  SensorContext context )  {  File  file =  inputFile . getFile  ( ) ;  try  {  Resource  resource =  fileToResource . toResource  ( inputFile , context ) ;  Charset  charset =  projectFileSystem . getSourceCharset  ( ) ;  String  fileContent =  FileUtils . readFileToString  ( file ,  charset . name  ( ) ) ;  String  currentSha1 =  sha1Generator . find  ( fileContent ) ;  if  (   currentSha1 . equals  ( previousSha1 ) &&  !  configuration . isReloadBlameEnabled  ( ) )  {  return  fileNotChanged  ( file , resource ) ; }   String  [ ]  lines =  fileContent . split  ( "(\r)?\n|\r" ,  - 1 ) ;  return  fileChanged  ( file , resource , currentSha1 ,  lines . length ) ; }  catch (   IOException e )  {   LOG . error  ( "Unable to get scm information: {}" , file , e ) ;  return  MeasureUpdate . NONE ; } }
+=======
+>>>>>>>
+   private MeasureUpdate fileNotChanged  (  File file ,  Resource resource )  {   LOG . debug  ( "File not changed since previous analysis: {}" , file ) ;  return  new CopyPreviousMeasures  ( resource ) ; }   public BlameVersionSelector  (  Blame blame )  {    this . blame = blame ; }   public MeasureUpdate detect  (  Resource sonarFile ,  InputFile inputFile ,  SensorContext context ,  boolean hasPreviousMeasures )  {  File  file =  inputFile . file  ( ) ;  if  (    inputFile . status  ( ) ==   InputFile . Status . SAME && hasPreviousMeasures )  {  return  fileNotChanged  ( file , sonarFile ) ; }  return  fileChanged  ( file , sonarFile ,  inputFile . lines  ( ) ) ; }   private MeasureUpdate fileChanged  (  File file ,  Resource resource ,   int lineCount )  {   LOG . debug  ( "File changed since previous analysis: {}" , file ) ;  return  blame . save  ( file , resource , lineCount ) ; } }
