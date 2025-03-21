0909940ac65ee6a9a106c0e38ae58e83eb9c09bc
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 16:45:11 +0000

    Mining Framework Analysis

--- jOOQ-meta/src/main/java/org/jooq/meta/tools/FilePattern.java
+++ jOOQ-meta/src/main/java/org/jooq/meta/tools/FilePattern.java
@@ -1,49 +1,10 @@
-/*
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * Other licenses:
- * -----------------------------------------------------------------------------
- * Commercial licenses for this work are available. These replace the above
- * ASL 2.0 and offer limited warranties, support, maintenance, and commercial
- * database integrations.
- *
- * For more information, please visit: http://www.jooq.org/licenses
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- */
 package org.jooq.meta.tools;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.regex.Pattern;
-
 import org.jooq.Internal;
 import org.jooq.tools.JooqLogger;
 
@@ -56,28 +17,30 @@ import org.jooq.tools.JooqLogger;
  *
  * @author Lukas Eder
  */
-@Internal
-public final class FilePattern {
+@Internal public final class FilePattern {
+  private static final JooqLogger log = JooqLogger.getLogger(FilePattern.class);
 
-    private static final JooqLogger log = JooqLogger.getLogger(FilePattern.class);
-
-    public static final Comparator<File> fileComparator(String sort) {
-        if ("alphanumeric".equals(sort))
-            return new Comparator<File>() {
-                @Override
-                public int compare(File o1, File o2) {
-                    return o1.compareTo(o2);
-                }
-            };
-        else if ("none".equals(sort))
-            return null;
-        else if ("flyway".equals(sort))
-            return FlywayFileComparator.INSTANCE;
-        else
-            return FileComparator.INSTANCE;
+  public static final Comparator<File> fileComparator(String sort) {
+    if ("alphanumeric".equals(sort)) {
+      return new Comparator<File>() {
+        @Override public int compare(File o1, File o2) {
+          return o1.compareTo(o2);
+        }
+      };
+    } else {
+      if ("none".equals(sort)) {
+        return null;
+      } else {
+        if ("flyway".equals(sort)) {
+          return FlywayFileComparator.INSTANCE;
+        } else {
+          return FileComparator.INSTANCE;
+        }
+      }
     }
+  }
 
-    /**
+  /**
      * Try loading <code>pattern</code>.
      * <p>
      * This method tries loading contents from <code>pattern</code> using the
@@ -92,100 +55,71 @@ public final class FilePattern {
      * (interpreted as an ant-style file pattern), and load them</li>
      * </ul>
      */
-    public static final void load(
-        String encoding,
-        String pattern,
-        Comparator<File> fileComparator,
-        Loader loader
-    ) throws Exception {
-        InputStream in = null;
-        boolean loaded = false;
-
-        try {
-            in = FilePattern.class.getResourceAsStream(pattern);
-
-            if (in != null) {
-                log.info("Reading from classpath: " + pattern);
-                loader.load(encoding, in);
-                loaded = true;
-            }
-            else {
-                File file = new File(pattern);
-
-                if (file.exists()) {
-                    load(encoding, file, fileComparator, null, loader);
-                    loaded = true;
-                }
-                else {
-
-                    // [#8336] Relative paths aren't necessarily relative to the
-                    //         working directory, but maybe to some subdirectory
-                    if (pattern.contains("*") || pattern.contains("?"))
-                        file = new File(pattern.replaceAll("[*?].*", "")).getCanonicalFile();
-                    else
-                        file = new File(".").getCanonicalFile();
-
-                    Pattern regex = Pattern.compile("^.*?"
-                       + pattern
-                        .replace("\\", "/")
-                        .replace(".", "\\.")
-                        .replace("?", ".")
-                        .replace("**", ".+?")
-                        .replace("*", "[^/]*")
-                       + "$"
-                    );
-
-                    load(encoding, file, fileComparator, regex, loader);
-                    loaded = true;
-                }
-            }
-
-            if (!loaded)
-                log.error("Could not find source(s) : " + pattern);
+  public static final void load(String encoding, String pattern, Comparator<File> fileComparator, Loader loader) throws Exception {
+    InputStream in = null;
+    boolean loaded = false;
+    try {
+      in = FilePattern.class.getResourceAsStream(pattern);
+      if (in != null) {
+        log.info("Reading from classpath: " + pattern);
+        loader.load(encoding, in);
+        loaded = true;
+      } else {
+        File file = new File(pattern);
+        if (file.exists()) {
+          load(encoding, file, fileComparator, null, loader);
+          loaded = true;
+        } else {
+          if (pattern.contains("*") || pattern.contains("?")) {
+            file = new File(pattern.replaceAll("[*?].*", "")).getCanonicalFile();
+          } else {
+            file = new File(".").getCanonicalFile();
+          }
+          Pattern regex = Pattern.compile("^.*?" + pattern.replace("\\", "/").replace(".", "\\.").replace("?", ".").replace("**", ".+?").replace("*", "[^/]*") + "$");
+          load(encoding, file, fileComparator, regex, loader);
+          loaded = true;
         }
-        finally {
-            try {
-                if (in != null)
-                    in.close();
-            }
-            catch (Exception ignore) {}
+      }
+      if (!loaded) {
+        log.error("Could not find source(s) : " + pattern);
+      }
+    }  finally {
+      try {
+        if (in != null) {
+          in.close();
         }
+      } catch (Exception ignore) {
+      }
     }
+  }
 
-    private static final void load(
-        String encoding,
-        File file,
-        Comparator<File> fileComparator,
-        Pattern pattern,
-        Loader loader
-    ) throws Exception {
-        if (file.isFile()) {
-            if (pattern == null || pattern.matcher(file.getCanonicalPath().replace("\\", "/")).matches()) {
-                log.info("Reading from: " + file + " [*]");
-                loader.load(encoding, new FileInputStream(file));
-            }
+  private static final void load(String encoding, File file, Comparator<File> fileComparator, Pattern pattern, Loader loader) throws Exception {
+    if (file.isFile()) {
+      if (pattern == null || pattern.matcher(file.getCanonicalPath().replace("\\", "/")).matches()) {
+        log.info("Reading from: " + file + " [*]");
+        loader.load(encoding, new FileInputStream(file));
+      }
+    } else {
+      if (file.isDirectory()) {
+        log.info("Reading from: " + file);
+        File[] files = file.listFiles();
+        if (files != null) {
+          if (fileComparator != null) {
+            Arrays.sort(files, fileComparator);
+          }
+          for (File f : files) {
+            load(encoding, f, fileComparator, pattern, loader);
+          }
         }
-        else if (file.isDirectory()) {
-            log.info("Reading from: " + file);
-
-            File[] files = file.listFiles();
-
-            if (files != null) {
-                if (fileComparator != null)
-                    Arrays.sort(files, fileComparator);
-
-                for (File f : files)
-                    load(encoding, f, fileComparator, pattern, loader);
-            }
+      } else {
+        if (!file.exists() && file.getParentFile() != null) {
+          load(encoding, file.getParentFile(), fileComparator, pattern, loader);
         }
-
-        // [#7767] Backtrack to a parent directory in case the current file pattern doesn't match yet
-        else if (!file.exists() && file.getParentFile() != null) {
-            load(encoding, file.getParentFile(), fileComparator, pattern, loader);
-        }
+      }
     }
+  }
 
-    public interface Loader {
-        void load(String encoding, InputStream in) throws Exception;
-    }
+  public interface Loader {
+    void load(String encoding, InputStream in) throws Exception;
+  }
 }
