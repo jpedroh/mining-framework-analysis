ed8b0a905ca16507dc05cd3096091e1d095663d3
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 08:57:20 +0000

    Mining Framework Analysis

--- src/test/java/hudson/tasks/MailerTest.java
+++ src/test/java/hudson/tasks/MailerTest.java
@@ -59,6 +59,7 @@ import jenkins.model.Jenkins;
 import jenkins.model.JenkinsLocationConfiguration;
 
 import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.util.Collection;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicLong;
