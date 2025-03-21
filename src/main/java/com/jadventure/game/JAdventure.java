6d9df72acfdaf35fc6e08cfac195e59135f9e401
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 01:47:08 +0000

    Mining Framework Analysis

--- src/main/java/com/jadventure/game/JAdventure.java
+++ src/main/java/com/jadventure/game/JAdventure.java
@@ -1,11 +1,8 @@
 package com.jadventure.game;
-
 import com.jadventure.game.menus.MainMenu;
-
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.io.IOException;
-
 import com.jadventure.game.queueprovider.QueueProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,78 +14,78 @@ import org.slf4j.LoggerFactory;
  * the game.
  */
 public class JAdventure {
-    private static Logger logger = LoggerFactory.getLogger(JAdventure.class);
+  private static Logger logger = LoggerFactory.getLogger(JAdventure.class);
 
-    public static void main(String[] args) {
-        logger.info("Starting JAdventure " + toString(args));
-        GameModeType mode = getGameMode(args);
-        logger.debug("Starting in mode " + mode.name());
-        String serverName = "localhost";
-        int port = 4044;
-        if (mode == GameModeType.SERVER) {
-            port = Integer.parseInt(args[1]);
-        } else if (mode == GameModeType.CLIENT) {
-            serverName = args[2];
-            port = Integer.parseInt(args[1]);
-        }
-        if (GameModeType.CLIENT == mode) {
-            new Client(serverName, port);
-        } else if (GameModeType.SERVER == mode) {
+  public static void main(String[] args) {
+    logger.info("Starting JAdventure " + toString(args));
+    GameModeType mode = getGameMode(args);
+    logger.debug("Starting in mode " + mode.name());
+    String serverName = "localhost";
+    int port = 4044;
+    if (mode == GameModeType.SERVER) {
+      port = Integer.parseInt(args[1]);
+    } else {
+      if (mode == GameModeType.CLIENT) {
+        serverName = args[2];
+        port = Integer.parseInt(args[1]);
+      }
+    }
+    if (GameModeType.CLIENT == mode) {
+      new Client(serverName, port);
+    } else {
+      if (GameModeType.SERVER == mode) {
+        while (true) {
+          ServerSocket listener = null;
+          try {
+            listener = new ServerSocket(port);
             while (true) {
-            	ServerSocket listener = null;
-                try {
-                    listener = new ServerSocket(port);
-                    while (true) {
-                        Socket server = listener.accept();
-                        Runnable r = new MainMenu(server, mode);
-                        new Thread(r).start();
-                    }
-                } catch (IOException c) {
-                    c.printStackTrace();
-                } finally {
-                	try {
-						listener.close();
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-                }
+              Socket server = listener.accept();
+              Runnable r = new MainMenu(server, mode);
+              new Thread(r).start();
             }
-        } else {
-            QueueProvider.startStandaloneMessenger();
-            new MainMenu();
-        }
-    }
-
-    private static GameModeType getGameMode(String[] args) {
-        if (args == null || args.length == 0 || "".equals(args[0].trim())) {
-            return GameModeType.STAND_ALONE;
-        }
-
-        try {
-            return GameModeType.valueOf(args[0].toUpperCase());
-        }
-        catch (IllegalArgumentException iae) {
-            logger.warn("No game mode '" + args[0].toUpperCase() + "' known." +
-                    "Terminating application.");
-            System.exit(-1);
-        }
-        return GameModeType.STAND_ALONE;
-    }
-
-    private static String toString(String[] args) {
-        if (args.length == 0) {
-            return "";
-        }
-
-        final StringBuilder bldr = new StringBuilder();
-        bldr.append("[ ");
-        for (int index = 0; index < args.length; index++) {
-            if (index > 0) {
-                bldr.append(", ");
+          } catch (IOException c) {
+            c.printStackTrace();
+          } finally {
+            try {
+              listener.close();
+            } catch (IOException e) {
+              e.printStackTrace();
             }
-            bldr.append(args[index]);
+          }
         }
-        bldr.append(" ]");
-        return bldr.toString();
+      } else {
+        QueueProvider.startStandaloneMessenger();
+        new MainMenu();
+      }
     }
+  }
+
+  private static GameModeType getGameMode(String[] args) {
+    if (args == null || args.length == 0 || "".equals(args[0].trim())) {
+      return GameModeType.STAND_ALONE;
+    }
+    try {
+      return GameModeType.valueOf(args[0].toUpperCase());
+    } catch (IllegalArgumentException iae) {
+      logger.warn("No game mode \'" + args[0].toUpperCase() + "\' known." + "Terminating application.");
+      System.exit(-1);
+    }
+    return GameModeType.STAND_ALONE;
+  }
+
+  private static String toString(String[] args) {
+    if (args.length == 0) {
+      return "";
+    }
+    final StringBuilder bldr = new StringBuilder();
+    bldr.append("[ ");
+    for (int index = 0; index < args.length; index++) {
+      if (index > 0) {
+        bldr.append(", ");
+      }
+      bldr.append(args[index]);
+    }
+    bldr.append(" ]");
+    return bldr.toString();
+  }
 }
