1e6f6dd08c74f9a9481d7b7819cd8c48b442e79e
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 10:23:33 +0000

    Mining Framework Analysis

--- admin-service/src/main/java/com/findwise/hydra/admin/rest/AppConfig.java
+++ admin-service/src/main/java/com/findwise/hydra/admin/rest/AppConfig.java
@@ -1,7 +1,5 @@
 package com.findwise.hydra.admin.rest;
-
 import java.io.File;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 import org.springframework.context.annotation.Bean;
@@ -11,77 +9,57 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
-
 import com.findwise.hydra.admin.ConfigurationService;
 import com.findwise.hydra.admin.documents.DocumentsService;
 import com.findwise.hydra.admin.stages.StagesService;
 import com.findwise.hydra.mongodb.MongoConnector;
 import com.findwise.hydra.mongodb.MongoType;
 
-@Configuration
-@ComponentScan(basePackages = "com.findwise.hydra.admin.rest")
-public class AppConfig {
+@Configuration @ComponentScan(basePackages = "com.findwise.hydra.admin.rest") public class AppConfig {
+  private static final String PROPERTIES_FILE = "admin-service.properties";
 
-	private static final String PROPERTIES_FILE = "admin-service.properties";
-	private static final String CONFIG_DIR_PROPERTY = "hydra.admin.config.dir";
+  private static final String CONFIG_DIR_PROPERTY = "hydra.admin.config.dir";
 
-	@Autowired
-	private DatabaseConfig databaseConfig;
+  @Autowired private DatabaseConfig databaseConfig;
 
-	@Bean
-	@Autowired
-	public MongoConnector connector(DatabaseConfig config) {
-		return new MongoConnector(config);
-	}
+  @Bean @Autowired public MongoConnector connector(DatabaseConfig config) {
+    return new MongoConnector(config);
+  }
 
-	@Bean
-	public DatabaseConfig databaseConfig() {
-		return new DatabaseConfig();
-	}
+  @Bean public DatabaseConfig databaseConfig() {
+    return new DatabaseConfig();
+  }
 
-	@Bean(name = "multipartResolver")
-	public CommonsMultipartResolver multipartResolver() {
-		CommonsMultipartResolver cmr = new CommonsMultipartResolver();
+  @Bean(name = "multipartResolver") public CommonsMultipartResolver multipartResolver() {
+    CommonsMultipartResolver cmr = new CommonsMultipartResolver();
+    cmr.setMaxUploadSize(1024 * 1024 * 1024);
+    return cmr;
+  }
 
-		cmr.setMaxUploadSize(1024 * 1024 * 1024); // 1 Gigabyte...
+  @Bean(name = "jsonpCallbackFilter") public static JsonpCallbackFilter jsonpCallback() {
+    return new JsonpCallbackFilter();
+  }
 
-		return cmr;
-	}
+  @Bean public PropertyPlaceholderConfigurer properties() {
+    String configurationDirectory = System.getProperty(CONFIG_DIR_PROPERTY);
+    File propertiesFile = new File(configurationDirectory, PROPERTIES_FILE);
+    final Resource[] resources = new Resource[] { new ClassPathResource(PROPERTIES_FILE), new FileSystemResource(propertiesFile) };
+    PropertyPlaceholderConfigurer ppc = new PropertyPlaceholderConfigurer();
+    ppc.setIgnoreUnresolvablePlaceholders(true);
+    ppc.setIgnoreResourceNotFound(true);
+    ppc.setLocations(resources);
+    return ppc;
+  }
 
-        @Bean(name = "jsonpCallbackFilter")
-        public static JsonpCallbackFilter jsonpCallback(){
-            return new JsonpCallbackFilter();
-        }
+  @Bean public ConfigurationService<MongoType> configurationService(MongoConnector connector) {
+    return new ConfigurationService<MongoType>(connector);
+  }
 
-	@Bean
-	public PropertyPlaceholderConfigurer properties() {
-		String configurationDirectory = System.getProperty(CONFIG_DIR_PROPERTY);
-		File propertiesFile = new File(configurationDirectory, PROPERTIES_FILE);
-		final Resource[] resources = new Resource[] {
-				new ClassPathResource(PROPERTIES_FILE),
-				new FileSystemResource(propertiesFile) };
-		PropertyPlaceholderConfigurer ppc = new PropertyPlaceholderConfigurer();
-		ppc.setIgnoreUnresolvablePlaceholders(true);
-		ppc.setIgnoreResourceNotFound(true);
-		ppc.setLocations(resources);
-		return ppc;
-	}
+  @Bean public DocumentsService<MongoType> documentsService(MongoConnector connector) {
+    return new DocumentsService<MongoType>(connector);
+  }
 
-	@Bean
-	public ConfigurationService<MongoType> configurationService(
-			MongoConnector connector) {
-		return new ConfigurationService<MongoType>(connector);
-	}
-
-	@Bean
-	public DocumentsService<MongoType> documentsService(MongoConnector connector) {
-		return new DocumentsService<MongoType>(connector);
-
-	}
-
-	@Bean
-	public StagesService<MongoType> stagesService(MongoConnector connector) {
-		return new StagesService<MongoType>(connector);
-	}
-
+  @Bean public StagesService<MongoType> stagesService(MongoConnector connector) {
+    return new StagesService<MongoType>(connector);
+  }
 }
