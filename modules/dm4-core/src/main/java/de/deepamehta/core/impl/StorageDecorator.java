960db3475a2692b764c1a6721ac9faec621ebaaf
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 13:30:12 +0000

    Mining Framework Analysis

--- modules/dm4-core/src/main/java/de/deepamehta/core/impl/StorageDecorator.java
+++ modules/dm4-core/src/main/java/de/deepamehta/core/impl/StorageDecorator.java
@@ -512,7 +512,7 @@ public class StorageDecorator {
      *          Note: its composite value is not initialized.
      */
     RelatedTopicModel fetchRelatedTopic(long objectId, String assocTypeUri, String myRoleTypeUri,
-                                                       String othersRoleTypeUri, String othersTopicTypeUri) {
+                                                 String othersRoleTypeUri, String othersTopicTypeUri) {
         ResultList<RelatedTopicModel> topics = fetchRelatedTopics(objectId, assocTypeUri, myRoleTypeUri,
             othersRoleTypeUri, othersTopicTypeUri);
         switch (topics.getSize()) {
@@ -538,7 +538,7 @@ public class StorageDecorator {
      *          Note: their composite values are not initialized.
      */
     ResultList<RelatedTopicModel> fetchRelatedTopics(long objectId, String assocTypeUri, String myRoleTypeUri,
-                                                                  String othersRoleTypeUri, String othersTopicTypeUri) {
+                                                              String othersRoleTypeUri, String othersTopicTypeUri) {
         List<RelatedTopicModel> relTopics = storage.fetchRelatedTopics(objectId, assocTypeUri, myRoleTypeUri,
             othersRoleTypeUri, othersTopicTypeUri);
         return new ResultList(relTopics.size(), relTopics);
