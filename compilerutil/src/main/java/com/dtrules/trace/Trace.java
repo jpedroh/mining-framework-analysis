8d8b613244907bc0e779ca1e14c09574f4ef51f8
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 13:05:40 +0000

    Mining Framework Analysis

--- compilerutil/src/main/java/com/dtrules/trace/Trace.java
+++ compilerutil/src/main/java/com/dtrules/trace/Trace.java
@@ -1,10 +1,8 @@
 package com.dtrules.trace;
-
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
-
 import com.dtrules.entity.IREntity;
 import com.dtrules.infrastructure.RulesException;
 import com.dtrules.session.IRSession;
@@ -13,52 +11,37 @@ import com.dtrules.xmlparser.GenericXMLParser;
 import com.dtrules.xmlparser.XMLPrinter;
 
 public class Trace {
-	
-	TraceNode root   = null;
-	
-	Map<String,IREntity> entitytable = new HashMap<String,IREntity>();
-	
-	/**
-	 * Create a TraceNode tree from the given tracefile filepath. 
-	 * @param tracefile
-	 * @return
-	 * @throws Exception
-	 */
-	public TraceNode load (String tracefile) throws Exception {
-		InputStream tracefilestream = new FileInputStream(tracefile);
-		return load(tracefilestream);
-	}
+  TraceNode root = null;
 
-	/**
-	 * Create a TraceNode tree from the given tracefilestream.
-	 * @param tracefilestream
-	 * @return
-	 * @throws Exception
-	 */
-	public TraceNode load (InputStream tracefilestream) throws Exception {
-		TraceLoader loader = new TraceLoader();
-		GenericXMLParser.load(tracefilestream, loader);
-		root = loader.tagStack.pop().children.get(0);
-		return root;
-	}
-	
-	public TraceNode root () {
-		return root;
-	}
+  Map<String, IREntity> entitytable = new HashMap<String, IREntity>();
 
-	/**
-	 * Print the TraceNode tree for this object.
-	 */
-	public void print(){
-		XMLPrinter out = new XMLPrinter(System.out);
-		out.setSpaceCnt(2);
-		if(root != null ){
-			root.print( out);
-		}else{
-			System.out.println("No tree has been loaded");
-		}
-	}
-	/**
+  public TraceNode load(String tracefile) throws Exception {
+    InputStream tracefilestream = new FileInputStream(tracefile);
+    return load(tracefilestream);
+  }
+
+  public TraceNode load(InputStream tracefilestream) throws Exception {
+    TraceLoader loader = new TraceLoader();
+    GenericXMLParser.load(tracefilestream, loader);
+    root = loader.tagStack.pop().children.get(0);
+    return root;
+  }
+
+  public TraceNode root() {
+    return root;
+  }
+
+  public void print() {
+    XMLPrinter out = new XMLPrinter(System.out);
+    out.setSpaceCnt(2);
+    if (root != null) {
+      root.print(out);
+    } else {
+      System.out.println("No tree has been loaded");
+    }
+  }
+
+  /**
 	 * Returns a session that represents a Rules Engine in the state at 
 	 * the given node.
 	 * 
@@ -66,53 +49,37 @@ public class Trace {
 	 * @param position
 	 * @return IRSession
 	 */
-	public IRSession setState(RuleSet rs, TraceNode position) {
-		try{
-			IRSession session = rs.newSession();
-			root.setState(this, session,position);
-			return session;
-		}catch(RulesException e){
-			return null;
-		}
-	}
-	/**
+  public IRSession setState(RuleSet rs, TraceNode position) {
+    try {
+      IRSession session = rs.newSession();
+      root.setState(this, session, position);
+      return session;
+    } catch (RulesException e) {
+      return null;
+    }
+  }
+
+  /**
 	 * Find the i'th node in the tree.
 	 * @param i
 	 * @return
 	 */
-	public TraceNode find(int i){
-		return root.find(i);
-	}
-	
-	/**
-	 * A simple test file.
-	 * @param args
-	 * @throws Exception
-	 */
-	static public void main(String [] args) throws Exception {
-		
-		String tracefile    = System.getProperty("user.dir")+"/testdata/job_trace.xml";
-		Trace trace 		= new Trace();
-		
-		trace.load(tracefile);
-		//trace.print();
-		
-		TraceNode t = trace.find(11074);
-		
-		RulesDirectory rd = new RulesDirectory(
-				System.getProperty("user.dir")+"/../sampleprojects/CHIP/", "DTRules.xml");
-		
-		RuleSet rs = rd.getRuleSet("CHIP");
-		
-		IRSession s = trace.setState(rs, t);
-		
-		int edepth = s.getState().edepth();
-		for(int i = 0; i < edepth; i++){
-			IREntity e = s.getState().getes(i);
-			System.out.println(e.getName()+" "+e.getID());
-		}
-		
-	}
-	
-	
+  public TraceNode find(int i) {
+    return root.find(i);
+  }
+
+  static public void main(String[] args) throws Exception {
+    String tracefile = System.getProperty("user.dir") + "/testdata/job_trace.xml";
+    Trace trace = new Trace();
+    trace.load(tracefile);
+    TraceNode t = trace.find(11074);
+    RulesDirectory rd = new RulesDirectory(System.getProperty("user.dir") + "/../sampleprojects/CHIP/", "DTRules.xml");
+    RuleSet rs = rd.getRuleSet("CHIP");
+    IRSession s = trace.setState(rs, t);
+    int edepth = s.getState().edepth();
+    for (int i = 0; i < edepth; i++) {
+      IREntity e = s.getState().getes(i);
+      System.out.println(e.getName() + " " + e.getID());
+    }
+  }
 }
