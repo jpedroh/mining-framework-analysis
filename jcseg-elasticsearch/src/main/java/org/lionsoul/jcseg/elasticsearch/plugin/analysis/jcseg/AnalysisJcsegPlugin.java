1051884ce743ea54389d045c13ea6f0fd37acbfc
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 14:56:32 +0000

    Mining Framework Analysis

--- jcseg-elasticsearch/src/main/java/org/lionsoul/jcseg/elasticsearch/plugin/analysis/jcseg/AnalysisJcsegPlugin.java
+++ jcseg-elasticsearch/src/main/java/org/lionsoul/jcseg/elasticsearch/plugin/analysis/jcseg/AnalysisJcsegPlugin.java
@@ -1,8 +1,6 @@
 package org.lionsoul.jcseg.elasticsearch.plugin.analysis.jcseg;
-
 import java.util.Collection;
 import java.util.Collections;
-
 import org.elasticsearch.common.inject.Module;
 import org.elasticsearch.index.analysis.AnalysisModule;
 import org.elasticsearch.plugins.Plugin;
@@ -15,22 +13,19 @@ import org.lionsoul.jcseg.elasticsearch.indices.analysis.jcseg.JcsegIndicesAnaly
  * @author	chenxin<chenxin619315@gmail.com>
  */
 public class AnalysisJcsegPlugin extends Plugin {
+  @Override public String name() {
+    return "analysis-jcseg";
+  }
 
-    @Override public String name() {
-        return "analysis-jcseg";
-    }
+  @Override public String description() {
+    return "jcseg analysis support";
+  }
 
+  @Override public Collection<Module> nodeModules() {
+    return Collections.<Module>singletonList(new JcsegIndicesAnalysisModule());
+  }
 
-    @Override public String description() {
-        return "jcseg analysis support";
-    }
-
-    @Override
-    public Collection<Module> nodeModules() {
-        return Collections.<Module>singletonList(new JcsegIndicesAnalysisModule());
-    }
-
-    public void onModule(AnalysisModule module) {
-        module.addProcessor(new JcsegAnalysisBinderProcessor());
-    }
+  public void onModule(AnalysisModule module) {
+    module.addProcessor(new JcsegAnalysisBinderProcessor());
+  }
 }
