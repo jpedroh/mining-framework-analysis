ffdbd633e590f853d0c6787719e7d0ca89647dd8
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 12:56:29 +0000

    Mining Framework Analysis

--- modules/dm4-core/src/main/java/de/deepamehta/core/impl/DeepaMehtaObjectIterable.java
+++ modules/dm4-core/src/main/java/de/deepamehta/core/impl/DeepaMehtaObjectIterable.java
@@ -1,128 +1,93 @@
 package de.deepamehta.core.impl;
-
 import de.deepamehta.core.Association;
 import de.deepamehta.core.DeepaMehtaObject;
 import de.deepamehta.core.Topic;
 import de.deepamehta.core.model.AssociationModel;
 import de.deepamehta.core.model.DeepaMehtaObjectModel;
 import de.deepamehta.core.model.TopicModel;
-
 import java.util.Iterator;
-
-
 
 /**
  * An iterable over all topics stored in the DB.
  */
 class TopicIterable implements Iterable<Topic> {
+  private Iterator<Topic> topics;
 
-    private Iterator<Topic> topics;
+  TopicIterable(EmbeddedService dms) {
+    this.topics = new TopicIterator(dms);
+  }
 
-    TopicIterable(EmbeddedService dms) {
-        this.topics = new TopicIterator(dms);
-    }
-
-    @Override
-    public Iterator<Topic> iterator() {
-        return topics;
-    }
+  @Override public Iterator<Topic> iterator() {
+    return topics;
+  }
 }
 
 /**
  * An iterable over all associations stored in the DB.
  */
 class AssociationIterable implements Iterable<Association> {
+  private Iterator<Association> assocs;
 
-    private Iterator<Association> assocs;
+  AssociationIterable(EmbeddedService dms) {
+    this.assocs = new AssociationIterator(dms);
+  }
 
-    AssociationIterable(EmbeddedService dms) {
-        this.assocs = new AssociationIterator(dms);
-    }
-
-    @Override
-    public Iterator<Association> iterator() {
-        return assocs;
-    }
+  @Override public Iterator<Association> iterator() {
+    return assocs;
+  }
 }
-
-
-
-// ===
-
-
 
 class TopicIterator extends ObjectIterator<Topic, TopicModel> {
+  TopicIterator(EmbeddedService dms) {
+    super(dms);
+  }
 
-    TopicIterator(EmbeddedService dms) {
-        super(dms);
-    }
+  @Override Iterator<TopicModel> fetchObjects() {
+    return dms.storageDecorator.fetchAllTopics();
+  }
 
-    @Override
-    Iterator<TopicModel> fetchObjects() {
-        return dms.storageDecorator.fetchAllTopics();
-    }
-
-    @Override
-    Topic instantiateObject(TopicModel model) {
-        return dms.instantiateTopic(model, true);           // checkAccess=true
-    }
+  @Override Topic instantiateObject(TopicModel model) {
+    return dms.instantiateTopic(model, true);
+  }
 }
-
-
 
 class AssociationIterator extends ObjectIterator<Association, AssociationModel> {
+  AssociationIterator(EmbeddedService dms) {
+    super(dms);
+  }
 
-    AssociationIterator(EmbeddedService dms) {
-        super(dms);
-    }
+  @Override Iterator<AssociationModel> fetchObjects() {
+    return dms.storageDecorator.fetchAllAssociations();
+  }
 
-    @Override
-    Iterator<AssociationModel> fetchObjects() {
-        return dms.storageDecorator.fetchAllAssociations();
-    }
-
-    @Override
-    Association instantiateObject(AssociationModel model) {
-        return dms.instantiateAssociation(model, true);     // checkAccess=true
-    }
+  @Override Association instantiateObject(AssociationModel model) {
+    return dms.instantiateAssociation(model, true);
+  }
 }
 
-
-
 abstract class ObjectIterator<O extends DeepaMehtaObject, M extends DeepaMehtaObjectModel> implements Iterator<O> {
+  protected EmbeddedService dms;
 
-    // ---------------------------------------------------------------------------------------------- Instance Variables
+  private Iterator<M> objects;
 
-    protected EmbeddedService dms;
-    private Iterator<M> objects;
+  ObjectIterator(EmbeddedService dms) {
+    this.dms = dms;
+    this.objects = fetchObjects();
+  }
 
-    // ---------------------------------------------------------------------------------------------------- Constructors
+  @Override public boolean hasNext() {
+    return objects.hasNext();
+  }
 
-    ObjectIterator(EmbeddedService dms) {
-        this.dms = dms;
-        this.objects = fetchObjects();
-    }
+  @Override public O next() {
+    return instantiateObject(objects.next());
+  }
 
-    // -------------------------------------------------------------------------------------------------- Public Methods
+  @Override public void remove() {
+    objects.remove();
+  }
 
-    @Override
-    public boolean hasNext() {
-        return objects.hasNext();
-    }
+  abstract Iterator<M> fetchObjects();
 
-    @Override
-    public O next() {
-        return instantiateObject(objects.next());
-    }
-
-    @Override
-    public void remove() {
-        objects.remove();
-    }
-
-    // ----------------------------------------------------------------------------------------- Package Private Methods
-
-    abstract Iterator<M> fetchObjects();
-
-    abstract O instantiateObject(M model);
+  abstract O instantiateObject(M model);
 }
--- modules/dm4-facets/src/main/java/de/deepamehta/plugins/facets/service/FacetsService.java
+++ modules/dm4-facets/src/main/java/de/deepamehta/plugins/facets/service/FacetsService.java
@@ -1,24 +1,16 @@
 package de.deepamehta.plugins.facets.service;
-
 import de.deepamehta.plugins.facets.model.FacetValue;
-
 import de.deepamehta.core.DeepaMehtaObject;
 import de.deepamehta.core.RelatedTopic;
 import de.deepamehta.core.Topic;
 import de.deepamehta.core.service.PluginService;
 import de.deepamehta.core.service.ResultList;
-
 import java.util.List;
 
-
-
 public interface FacetsService extends PluginService {
+  Topic getFacet(long topicId, String facetTypeUri);
 
-    // ### TODO: rename to getFacetValue
-    // ### TODO: add fetchComposite parameter
-    Topic getFacet(long topicId, String facetTypeUri);
-
-    /**
+  /**
      * Retrieves a facet value.
      * ### TODO: rename to getFacetValue
      * ### TODO: add fetchComposite parameter
@@ -28,14 +20,11 @@ public interface FacetsService extends PluginService {
      *
      * @return  The retrieved facet value (including its child topics) or <code>null</code> if no such topic extists.
      */
-    Topic getFacet(DeepaMehtaObject object, String facetTypeUri);
+  Topic getFacet(DeepaMehtaObject object, String facetTypeUri);
 
-    // ---
+  ResultList<RelatedTopic> getFacets(long topicId, String facetTypeUri);
 
-    // ### TODO: rename to getFacetValues
-    ResultList<RelatedTopic> getFacets(long topicId, String facetTypeUri);
-
-    /**
+  /**
      * Retrieves the values of a multi-facet.
      * ### TODO: rename to getFacetValues
      * ### TODO: add fetchComposite parameter
@@ -45,28 +34,22 @@ public interface FacetsService extends PluginService {
      *
      * @return  The retrieved facet values (including their child topics). The list may be empty.
      */
-    ResultList<RelatedTopic> getFacets(DeepaMehtaObject object, String facetTypeUri);
+  ResultList<RelatedTopic> getFacets(DeepaMehtaObject object, String facetTypeUri);
 
-    // ---
+  Topic getFacettedTopic(long topicId, List<String> facetTypeUris);
 
-    Topic getFacettedTopic(long topicId, List<String> facetTypeUris);
+  void addFacetTypeToTopic(long topicId, String facetTypeUri);
 
-    void addFacetTypeToTopic(long topicId, String facetTypeUri);
+  void updateFacet(long topicId, String facetTypeUri, FacetValue value);
 
-    // ---
-
-    void updateFacet(long topicId, String facetTypeUri, FacetValue value);
-
-    /**
+  /**
      * Updates a facet.
      *
      * @param   object          The facetted object: a topic, association, a type ...
      * @param   facetTypeUri    URI of the facet type.
      * @param   facetValue      The new facet value.
      */
-    void updateFacet(DeepaMehtaObject object, String facetTypeUri, FacetValue value);
+  void updateFacet(DeepaMehtaObject object, String facetTypeUri, FacetValue value);
 
-    // ---
-
-    boolean hasFacet(long topicId, String facetTypeUri, long facetTopicId);
+  boolean hasFacet(long topicId, String facetTypeUri, long facetTopicId);
 }
