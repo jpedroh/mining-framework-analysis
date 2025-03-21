bb7eefbe91e3885d626db0b1dfaf225180daed9c
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 15:49:42 +0000

    Mining Framework Analysis

--- ninja-core/src/test/java/ninja/ContextImplTest.java
+++ ninja-core/src/test/java/ninja/ContextImplTest.java
@@ -1,21 +1,16 @@
 package ninja;
-
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-
 import java.util.Map;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import ninja.bodyparser.BodyParserEngineManager;
 import ninja.session.FlashCookie;
 import ninja.session.SessionCookie;
-
 import ninja.utils.NinjaConstant;
 import ninja.utils.ResultHandler;
 import ninja.validation.Validation;
@@ -26,243 +21,120 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.Matchers;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
-
 import com.google.common.collect.Maps;
 
-@RunWith(MockitoJUnitRunner.class)
-public class ContextImplTest {
-    
-    @Mock
-    private SessionCookie sessionCookie;
-    
-    @Mock
-    private FlashCookie flashCookie;
-    
-    @Mock
-    private BodyParserEngineManager bodyParserEngineManager;    
-    
-    @Mock
-    private HttpServletRequest httpServletRequest;
-    
-    @Mock
-    private HttpServletResponse httpServletResponse;
-    
-    @Mock
-    private Route route;
+@RunWith(value = MockitoJUnitRunner.class) public class ContextImplTest {
+  @Mock private SessionCookie sessionCookie;
 
-    @Mock
-    private ResultHandler resultHandler;
+  @Mock private FlashCookie flashCookie;
 
-    @Mock
-    private Validation validation;
+  @Mock private BodyParserEngineManager bodyParserEngineManager;
 
-    private ContextImpl context;
+  @Mock private HttpServletRequest httpServletRequest;
 
-    @Before
-    public void setUp() {
-        //default setup for httpServlet request.
-        //According to servlet spec the following will be returned:
-        when(httpServletRequest.getContextPath()).thenReturn("");
-        when(httpServletRequest.getRequestURI()).thenReturn("/");
-        
-        
-        context = new ContextImpl(bodyParserEngineManager, flashCookie, sessionCookie,
-                resultHandler, validation);
-    }
+  @Mock private HttpServletResponse httpServletResponse;
 
-    @Test
-    public void testGetRequestUri() {
-        
-        //say the httpServletRequest to return a certain value:
-        when(httpServletRequest.getRequestURI()).thenReturn("/index");
-        
-        //init the context from a (mocked) servlet
-        context.init(httpServletRequest, httpServletResponse);
-        
-        //make sure this is correct
-        assertEquals("/index", context.getRequestUri());
-    }
+  @Mock private Route route;
 
-    @Test
-    public void testAddCookie() {
-        Cookie cookie = Cookie.builder("cookie", "yum").setDomain("domain").build();
-        context.init(httpServletRequest, httpServletResponse);
-        //context.addCookie(cookie);
+  @Mock private ResultHandler resultHandler;
 
-        //generate an arbitrary result:
-        Result result = Results.html();
-        result.addCookie(cookie);
-        
-        //finalize the headers => the cookies must be copied over to the servletcookies
-        context.finalizeHeaders(result);
+  @Mock private Validation validation;
 
-        //and verify the stuff:
-        ArgumentCaptor<javax.servlet.http.Cookie> cookieCaptor = ArgumentCaptor.forClass(javax.servlet.http.Cookie.class);
-        verify(httpServletResponse).addCookie(cookieCaptor.capture());
+  private ContextImpl context;
 
-        javax.servlet.http.Cookie resultCookie = cookieCaptor.getValue();
-        assertThat(resultCookie.getName(), equalTo("cookie"));
-        assertThat(resultCookie.getValue(), equalTo("yum"));
-        assertThat(resultCookie.getPath(), equalTo("/"));
-        assertThat(resultCookie.getSecure(), equalTo(false));
-        assertThat(resultCookie.getMaxAge(), equalTo(-1));
-    }
-    
-    
-    @Test
-    public void testGetPathParameter() {
-    	//init the context
-        context.init(httpServletRequest, httpServletResponse);
-        
-        //mock a parametermap:
-        Map<String, String> parameterMap = Maps.newHashMap();
-        parameterMap.put("parameter", "parameter");
-        
-        //and return the parameter map when any parameter is called...
-        when(route.getParameters(Matchers.anyString())).thenReturn(parameterMap);	
-		
-        context.setRoute(route);
-        
-        //this parameter is not there and must return null
-        assertEquals(null, context.getPathParameter("parameter_not_set")); 
-        
-        assertEquals("parameter", context.getPathParameter("parameter")); 
+  @Before public void setUp() {
+    when(httpServletRequest.getContextPath()).thenReturn("");
+    when(httpServletRequest.getRequestURI()).thenReturn("/");
+    context = new ContextImpl(bodyParserEngineManager, flashCookie, sessionCookie, resultHandler, validation);
+  }
 
-    }
-    
-    
-    @Test
-    public void testGetPathParameterAsInteger() {
-    	//init the context
-        context.init(httpServletRequest, httpServletResponse);
-        
-        //mock a parametermap:
-        Map<String, String> parameterMap = Maps.newHashMap();
-        parameterMap.put("parameter", "parameter");
-        
-        //and return the parameter map when any parameter is called...
-        when(route.getParameters(Matchers.anyString())).thenReturn(parameterMap);	
-		
-        context.setRoute(route);
-        
-        //this will not work and return null
-        assertEquals(null, context.getPathParameterAsInteger("parameter")); 
-        
-        //now set an integer into the parametermap:
-        parameterMap.put("parameter", "1");
-        
-        //this will work and return 1
-        assertEquals(new Integer(1), context.getPathParameterAsInteger("parameter")); 
-        
-    }
-    
-    @Test
-    public void testGetParameter() {
-    	
-    	//init the context
-        context.init(httpServletRequest, httpServletResponse);
-        
-        //and return the parameter map when any parameter is called...
-        when(httpServletRequest.getParameter("key")).thenReturn("value");	
+  @Test public void testGetRequestUri() {
+    when(httpServletRequest.getRequestURI()).thenReturn("/index");
+    context.init(httpServletRequest, httpServletResponse);
+    assertEquals("/index", context.getRequestUri());
+  }
 
-        //this will not work and return null
-        assertEquals(null, context.getParameter("key_not_there")); 
-        
-        //this will return the default value:
-        assertEquals("defaultValue", context.getParameter("key_not_there", "defaultValue")); 
-        
-        //this will work as the value is there...
-        assertEquals("value", context.getParameter("key")); 
+  @Test public void testAddCookie() {
+    Cookie cookie = Cookie.builder("cookie", "yum").setDomain("domain").build();
+    context.init(httpServletRequest, httpServletResponse);
+    Result result = Results.html();
+    result.addCookie(cookie);
+    context.finalizeHeaders(result);
+    ArgumentCaptor<javax.servlet.http.Cookie> cookieCaptor = ArgumentCaptor.forClass(javax.servlet.http.Cookie.class);
+    verify(httpServletResponse).addCookie(cookieCaptor.capture());
+    javax.servlet.http.Cookie resultCookie = cookieCaptor.getValue();
+    assertThat(resultCookie.getName(), equalTo("cookie"));
+    assertThat(resultCookie.getValue(), equalTo("yum"));
+    assertThat(resultCookie.getPath(), equalTo("/"));
+    assertThat(resultCookie.getSecure(), equalTo(false));
+    assertThat(resultCookie.getMaxAge(), equalTo(-1));
+  }
 
-    }
-    
-    @Test
-    public void testGetParameterAsInteger() {
-    	
-    	//init the context
-        context.init(httpServletRequest, httpServletResponse);
-        
-        //and return the parameter map when any parameter is called...
-        when(httpServletRequest.getParameter("key")).thenReturn("1");	
+  @Test public void testGetPathParameter() {
+    context.init(httpServletRequest, httpServletResponse);
+    Map<String, String> parameterMap = Maps.newHashMap();
+    parameterMap.put("parameter", "parameter");
+    when(route.getParameters(Matchers.anyString())).thenReturn(parameterMap);
+    context.setRoute(route);
+    assertEquals(null, context.getPathParameter("parameter_not_set"));
+    assertEquals("parameter", context.getPathParameter("parameter"));
+  }
 
-        //this will not work and return null
-        assertEquals(null, context.getParameterAsInteger("key_not_there")); 
-        
-        //this will return the default value:
-        assertEquals(new Integer(100), context.getParameterAsInteger("key_not_there", 100)); 
-        
-        //this will work as the value is there...
-        assertEquals(new Integer(1), context.getParameterAsInteger("key"));  
-    	
-    }
-    
-    @Test
-    public void testContentTypeGetsConvertedProperlyUponFinalize() {
-        
-        //init the context
-        context.init(httpServletRequest, httpServletResponse);
-        
-        //this must be Content-Type: application/json; encoding=utf-8
-        Result result = Results.json();
-        
-        context.finalizeHeaders(result);
-        
-        verify(httpServletResponse).setCharacterEncoding(result.getCharset());
-        verify(httpServletResponse).setContentType(result.getContentType());
-    }
-    
-    
-    @Test
-    public void testContentTypeWithNullEncodingGetsConvertedProperlyUponFinalize() {
-        
-        //init the context
-        context.init(httpServletRequest, httpServletResponse);
-        
-        //this must be Content-Type: application/json; encoding=utf-8
-        Result result = Results.json();
-        //force a characterset that is not there. Stupid but tests that its working.
-        result.charset(null);
-        
-        context.finalizeHeaders(result);
-        
-        //make sure utf-8 is used under all circumstances:
-        verify(httpServletResponse).setCharacterEncoding(NinjaConstant.UTF_8);
-    }
-    
-    
-    @Test
-    public void testGetRequestPathWorksAsExpectedWithContext() {
-        
-        // we got a context
-        when(httpServletRequest.getContextPath()).thenReturn("/my/funky/prefix");
-        
-        // we got a request uri
-        when(httpServletRequest.getRequestURI()).thenReturn("/my/funky/prefix/myapp/is/here");       
-        
-        
-        context.init(httpServletRequest, httpServletResponse);
-        
+  @Test public void testGetPathParameterAsInteger() {
+    context.init(httpServletRequest, httpServletResponse);
+    Map<String, String> parameterMap = Maps.newHashMap();
+    parameterMap.put("parameter", "parameter");
+    when(route.getParameters(Matchers.anyString())).thenReturn(parameterMap);
+    context.setRoute(route);
+    assertEquals(null, context.getPathParameterAsInteger("parameter"));
+    parameterMap.put("parameter", "1");
+    assertEquals(new Integer(1), context.getPathParameterAsInteger("parameter"));
+  }
 
-        assertEquals("/myapp/is/here", context.getRequestPath());
-        
-    }
-    
-    @Test
-    public void testGetRequestPathWorksAsExpectedWithOutContext() {
-        
-        // we got not context.
-        // according to spec it will return an empty string
-        when(httpServletRequest.getContextPath()).thenReturn("");
-        when(httpServletRequest.getRequestURI()).thenReturn("/index");
-        // we got a request uri
-        when(httpServletRequest.getRequestURI()).thenReturn("/myapp/is/here");       
-        
-        
-        context.init(httpServletRequest, httpServletResponse);
-        
+  @Test public void testGetParameter() {
+    context.init(httpServletRequest, httpServletResponse);
+    when(httpServletRequest.getParameter("key")).thenReturn("value");
+    assertEquals(null, context.getParameter("key_not_there"));
+    assertEquals("defaultValue", context.getParameter("key_not_there", "defaultValue"));
+    assertEquals("value", context.getParameter("key"));
+  }
 
-        assertEquals("/myapp/is/here", context.getRequestPath());
-        
-    }
+  @Test public void testGetParameterAsInteger() {
+    context.init(httpServletRequest, httpServletResponse);
+    when(httpServletRequest.getParameter("key")).thenReturn("1");
+    assertEquals(null, context.getParameterAsInteger("key_not_there"));
+    assertEquals(new Integer(100), context.getParameterAsInteger("key_not_there", 100));
+    assertEquals(new Integer(1), context.getParameterAsInteger("key"));
+  }
 
+  @Test public void testContentTypeGetsConvertedProperlyUponFinalize() {
+    context.init(httpServletRequest, httpServletResponse);
+    Result result = Results.json();
+    context.finalizeHeaders(result);
+    verify(httpServletResponse).setCharacterEncoding(result.getCharset());
+    verify(httpServletResponse).setContentType(result.getContentType());
+  }
+
+  @Test public void testContentTypeWithNullEncodingGetsConvertedProperlyUponFinalize() {
+    context.init(httpServletRequest, httpServletResponse);
+    Result result = Results.json();
+    result.charset(null);
+    context.finalizeHeaders(result);
+    verify(httpServletResponse).setCharacterEncoding(NinjaConstant.UTF_8);
+  }
+
+  @Test public void testGetRequestPathWorksAsExpectedWithContext() {
+    when(httpServletRequest.getContextPath()).thenReturn("/my/funky/prefix");
+    when(httpServletRequest.getRequestURI()).thenReturn("/my/funky/prefix/myapp/is/here");
+    context.init(httpServletRequest, httpServletResponse);
+    assertEquals("/myapp/is/here", context.getRequestPath());
+  }
+
+  @Test public void testGetRequestPathWorksAsExpectedWithOutContext() {
+    when(httpServletRequest.getContextPath()).thenReturn("");
+    when(httpServletRequest.getRequestURI()).thenReturn("/index");
+    when(httpServletRequest.getRequestURI()).thenReturn("/myapp/is/here");
+    context.init(httpServletRequest, httpServletResponse);
+    assertEquals("/myapp/is/here", context.getRequestPath());
+  }
 }
