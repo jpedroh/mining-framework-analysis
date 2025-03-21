97c0fb6640cdd968099906157c5e7a1504ea5e4f
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 08:54:57 +0000

    Mining Framework Analysis

--- src/main/java/net/java/otr4j/OtrEngineHost.java
+++ src/main/java/net/java/otr4j/OtrEngineHost.java
@@ -1,14 +1,6 @@
-/*
- * otr4j, the open source java otr library.
- *
- * Distributable under LGPL license.
- * See terms of license at gnu.org.
- */
 package net.java.otr4j;
-
 import java.security.KeyPair;
 import java.util.List;
-
 import net.java.otr4j.session.InstanceTag;
 import net.java.otr4j.session.SessionID;
 
@@ -21,47 +13,39 @@ import net.java.otr4j.session.SessionID;
  * 
  */
 public abstract interface OtrEngineHost {
-	public abstract void injectMessage(SessionID sessionID, String msg)
-			throws OtrException;
+  public abstract void injectMessage(SessionID sessionID, String msg) throws OtrException;
 
-	public abstract void unreadableMessageReceived(SessionID sessionID)
-			throws OtrException;
+  public abstract void unreadableMessageReceived(SessionID sessionID) throws OtrException;
 
-	public abstract void unencryptedMessageReceived(SessionID sessionID,
-			String msg) throws OtrException;
+  public abstract void unencryptedMessageReceived(SessionID sessionID, String msg) throws OtrException;
 
-	public abstract void showError(SessionID sessionID, String error)
-			throws OtrException;
+  public abstract void showError(SessionID sessionID, String error) throws OtrException;
 
-	public abstract void smpError(SessionID sessionID, int tlvType,
-			boolean cheated) throws OtrException;
+  public abstract void smpError(SessionID sessionID, int tlvType, boolean cheated) throws OtrException;
 
-	public abstract void smpAborted(SessionID sessionID) throws OtrException;
+  public abstract void smpAborted(SessionID sessionID) throws OtrException;
 
-	public abstract void finishedSessionMessage(SessionID sessionID,
-			String msgText) throws OtrException;
+  public abstract void finishedSessionMessage(SessionID sessionID, String msgText) throws OtrException;
 
-	public abstract void requireEncryptedMessage(SessionID sessionID,
-			String msgText) throws OtrException;
+  public abstract void requireEncryptedMessage(SessionID sessionID, String msgText) throws OtrException;
 
-	public abstract OtrPolicy getSessionPolicy(SessionID sessionID);
+  public abstract OtrPolicy getSessionPolicy(SessionID sessionID);
 
-	public abstract KeyPair getLocalKeyPair(SessionID sessionID)
-			throws OtrException;
+  public abstract KeyPair getLocalKeyPair(SessionID sessionID) throws OtrException;
 
-	public abstract byte[] getLocalFingerprintRaw(SessionID sessionID);
+  public abstract byte[] getLocalFingerprintRaw(SessionID sessionID);
 
-	public abstract void askForSecret(SessionID sessionID, InstanceTag receiverTag, String question);
+  public abstract void askForSecret(SessionID sessionID, InstanceTag receiverTag, String question);
 
-	public abstract void verify(SessionID sessionID, String fingerprint, boolean approved);
+  public abstract void verify(SessionID sessionID, String fingerprint, boolean approved);
 
-	public abstract void unverify(SessionID sessionID, String fingerprint);
+  public abstract void unverify(SessionID sessionID, String fingerprint);
 
-	public abstract String getReplyForUnreadableMessage(SessionID sessionID);
+  public abstract String getReplyForUnreadableMessage(SessionID sessionID);
 
-	public abstract String getFallbackMessage(SessionID sessionID);
+  public abstract String getFallbackMessage(SessionID sessionID);
 
-	public abstract void messageFromAnotherInstanceReceived(SessionID sessionID);
+  public abstract void messageFromAnotherInstanceReceived(SessionID sessionID);
 
-	public abstract void multipleInstancesDetected(SessionID sessionID);
+  public abstract void multipleInstancesDetected(SessionID sessionID);
 }
--- src/test/java/net/java/otr4j/OtrEngineImplTest.java
+++ src/test/java/net/java/otr4j/OtrEngineImplTest.java
@@ -1,11 +1,9 @@
 package net.java.otr4j;
-
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.util.List;
 import java.util.logging.Logger;
-
 import net.java.otr4j.OtrPolicy;
 import net.java.otr4j.OtrEngineImpl;
 import net.java.otr4j.OtrPolicyImpl;
@@ -16,278 +14,199 @@ import net.java.otr4j.session.SessionID;
 import net.java.otr4j.session.SessionStatus;
 
 public class OtrEngineImplTest extends junit.framework.TestCase {
+  private SessionID aliceSessionID = new SessionID("Alice@Wonderland", "Bob@Wonderland", "Scytale");
 
-	private SessionID aliceSessionID = new SessionID("Alice@Wonderland",
-			"Bob@Wonderland", "Scytale");
-	private SessionID bobSessionID = new SessionID("Bob@Wonderland",
-			"Alice@Wonderland", "Scytale");
+  private SessionID bobSessionID = new SessionID("Bob@Wonderland", "Alice@Wonderland", "Scytale");
 
-	private static Logger logger = Logger.getLogger(OtrEngineImplTest.class
-			.getName());
+  private static Logger logger = Logger.getLogger(OtrEngineImplTest.class.getName());
 
-	class DummyOtrEngineHost implements OtrEngineHost {
+  class DummyOtrEngineHost implements OtrEngineHost {
+    public DummyOtrEngineHost(OtrPolicy policy) {
+      this.policy = policy;
+    }
 
-		public DummyOtrEngineHost(OtrPolicy policy) {
-			this.policy = policy;
-		}
+    private OtrPolicy policy;
 
-		private OtrPolicy policy;
-		public String lastInjectedMessage;
+    public String lastInjectedMessage;
 
-		public OtrPolicy getSessionPolicy(SessionID ctx) {
-			return this.policy;
-		}
+    public OtrPolicy getSessionPolicy(SessionID ctx) {
+      return this.policy;
+    }
 
-		public void injectMessage(SessionID sessionID, String msg) {
+    public void injectMessage(SessionID sessionID, String msg) {
+      this.lastInjectedMessage = msg;
+      String msgDisplay = (msg.length() > 10) ? msg.substring(0, 10) + "..." : msg;
+      logger.finest("IM injects message: " + msgDisplay);
+    }
 
-			this.lastInjectedMessage = msg;
-			String msgDisplay = (msg.length() > 10) ? msg.substring(0, 10)
-					+ "..." : msg;
-			logger.finest("IM injects message: " + msgDisplay);
-		}
+    public void smpError(SessionID sessionID, int tlvType, boolean cheated) throws OtrException {
+      logger.severe("SM verification error with user: " + sessionID);
+    }
 
-		public void smpError(SessionID sessionID, int tlvType, boolean cheated)
-				throws OtrException{
-			logger.severe("SM verification error with user: " + sessionID);
-		}
+    public void smpAborted(SessionID sessionID) throws OtrException {
+      logger.severe("SM verification has been aborted by user: " + sessionID);
+    }
 
-		public void smpAborted(SessionID sessionID) throws OtrException {
-			logger.severe("SM verification has been aborted by user: "
-					+ sessionID);
-		}
+    public void finishedSessionMessage(SessionID sessionID, String msgText) throws OtrException {
+      logger.severe("SM session was finished. You shouldn\'t send messages to: " + sessionID);
+    }
 
-		public void finishedSessionMessage(SessionID sessionID, String msgText)
-				throws OtrException {
-			logger.severe("SM session was finished. You shouldn't send messages to: "
-					+ sessionID);
-		}
+    public void requireEncryptedMessage(SessionID sessionID, String msgText) throws OtrException {
+      logger.severe("Message can\'t be sent while encrypted session is not established: " + sessionID);
+    }
 
-		public void requireEncryptedMessage(SessionID sessionID, String msgText)
-				throws OtrException {
-			logger.severe("Message can't be sent while encrypted session is not established: "
-					+ sessionID);
-		}
+    public void unreadableMessageReceived(SessionID sessionID) throws OtrException {
+      logger.warning("Unreadable message received from: " + sessionID);
+    }
 
-		public void unreadableMessageReceived(SessionID sessionID)
-				throws OtrException {
-			logger.warning("Unreadable message received from: " + sessionID);
-		}
+    public void unencryptedMessageReceived(SessionID sessionID, String msg) throws OtrException {
+      logger.warning("Unencrypted message received: " + msg + " from " + sessionID);
+    }
 
-		public void unencryptedMessageReceived(SessionID sessionID, String msg)
-				throws OtrException {
-			logger.warning("Unencrypted message received: " + msg + " from "
-					+ sessionID);
-		}
+    public void showError(SessionID sessionID, String error) throws OtrException {
+      logger.severe("IM shows error to user: " + error);
+    }
 
-		public void showError(SessionID sessionID, String error)
-				throws OtrException {
-			logger.severe("IM shows error to user: " + error);
-		}
+    public String getReplyForUnreadableMessage(SessionID sessionID) {
+      return "You sent me an unreadable encrypted message.";
+    }
 
-		public String getReplyForUnreadableMessage(SessionID sessionID) {
-			return "You sent me an unreadable encrypted message.";
-		}
+    public void sessionStatusChanged(SessionID sessionID) {
+    }
 
-		public void sessionStatusChanged(SessionID sessionID) {
-			// don't care.
-		}
+    public KeyPair getLocalKeyPair(SessionID paramSessionID) {
+      KeyPairGenerator kg;
+      try {
+        kg = KeyPairGenerator.getInstance("DSA");
+      } catch (NoSuchAlgorithmException e) {
+        e.printStackTrace();
+        return null;
+      }
+      return kg.genKeyPair();
+    }
 
-		public KeyPair getLocalKeyPair(SessionID paramSessionID) {
-			KeyPairGenerator kg;
-			try {
-				kg = KeyPairGenerator.getInstance("DSA");
-			} catch (NoSuchAlgorithmException e) {
-				e.printStackTrace();
-				return null;
-			}
-			return kg.genKeyPair();
-		}
+    public void askForSecret(SessionID sessionID, InstanceTag tag, String question) {
+      logger.finest("Ask for secret from: " + sessionID + ", question: " + question);
+    }
 
-		public void askForSecret(SessionID sessionID, InstanceTag tag, String question) {
-			logger.finest("Ask for secret from: " + sessionID + ", question: "
-					+ question);
-		}
+    public void verify(SessionID sessionID, String fingerprint, boolean approved) {
+      logger.finest("Session was verified: " + sessionID);
+      if (!approved) {
+        logger.finest("Your answer for the question was verified." + "You should ask your opponent too or check shared secret.");
+      }
+    }
 
-		public void verify(SessionID sessionID, String fingerprint, boolean approved) {
-			logger.finest("Session was verified: " + sessionID);
-			if (!approved)
-				logger.finest("Your answer for the question was verified."
-						+ "You should ask your opponent too or check shared secret.");
-		}
+    public void unverify(SessionID sessionID, String fingerprint) {
+      logger.finest("Session was not verified: " + sessionID);
+    }
 
-		public void unverify(SessionID sessionID, String fingerprint) {
-			logger.finest("Session was not verified: " + sessionID);
-		}
+    public byte[] getLocalFingerprintRaw(SessionID sessionID) {
+      try {
+        return new OtrCryptoEngineImpl().getFingerprintRaw(getLocalKeyPair(sessionID).getPublic());
+      } catch (OtrCryptoException e) {
+        e.printStackTrace();
+      }
+      return null;
+    }
 
-		public byte[] getLocalFingerprintRaw(SessionID sessionID) {
-			try {
-				return new OtrCryptoEngineImpl()
-						.getFingerprintRaw(getLocalKeyPair(sessionID)
-								.getPublic());
-			} catch (OtrCryptoException e) {
-				e.printStackTrace();
-			}
-			return null;
-		}
+    public String getFallbackMessage(SessionID sessionID) {
+      return "Off-the-Record private conversation has been requested. However, you do not have a plugin to support that.";
+    }
 
-		public String getFallbackMessage(SessionID sessionID) {
-			return "Off-the-Record private conversation has been requested. However, you do not have a plugin to support that.";
-		}
+    public void messageFromAnotherInstanceReceived(SessionID sessionID) {
+      logger.warning("You received an encrypted message that was intended for another session. If you\'re logged in " + "from multiple locations, another session may have received this message");
+    }
 
-		public void messageFromAnotherInstanceReceived(SessionID sessionID) {
-			logger.warning("You received an encrypted message that was intended for another session. If you're logged in " +
-					"from multiple locations, another session may have received this message");
-		}
+    public void multipleInstancesDetected(SessionID sessionID) {
+      logger.warning("Your buddy is logged in multiple times and OTR has established multiple sessions. You can select an outgoing " + "session from the menu above");
+    }
+  }
 
-		public void multipleInstancesDetected(SessionID sessionID) {
-			logger.warning("Your buddy is logged in multiple times and OTR has established multiple sessions. You can select an outgoing " +
-					"session from the menu above");
-		}
-	}
+  public void testSession1() throws Exception {
+    this.startSession();
+    this.exchageMessages();
+    this.endSession();
+  }
 
-	public void testSession1() throws Exception {
+  public void testSession2() throws Exception {
+    this.startSessionWithQuery();
+    this.exchageMessages();
+    this.endSession();
+  }
 
-		this.startSession();
-		this.exchageMessages();
-		this.endSession();
-	}
+  private OtrEngineImpl usAlice;
 
-	public void testSession2() throws Exception {
+  private OtrEngineImpl usBob;
 
-		this.startSessionWithQuery();
-		this.exchageMessages();
-		this.endSession();
-	}
+  private DummyOtrEngineHost host;
 
-	private OtrEngineImpl usAlice;
-	private OtrEngineImpl usBob;
-	private DummyOtrEngineHost host;
+  private void startSession() throws OtrException {
+    host = new DummyOtrEngineHost(new OtrPolicyImpl(OtrPolicy.ALLOW_V2 | OtrPolicy.ALLOW_V3 | OtrPolicy.ERROR_START_AKE));
+    usAlice = new OtrEngineImpl(host);
+    usBob = new OtrEngineImpl(host);
+    usAlice.startSession(aliceSessionID);
+    usBob.transformReceiving(bobSessionID, host.lastInjectedMessage);
+    usAlice.transformReceiving(aliceSessionID, host.lastInjectedMessage);
+    usBob.transformReceiving(bobSessionID, host.lastInjectedMessage);
+    usAlice.transformReceiving(aliceSessionID, host.lastInjectedMessage);
+    usBob.transformReceiving(bobSessionID, host.lastInjectedMessage);
+    if (usBob.getSessionStatus(bobSessionID) != SessionStatus.ENCRYPTED || usAlice.getSessionStatus(aliceSessionID) != SessionStatus.ENCRYPTED) {
+      fail("Could not establish a secure session.");
+    }
+  }
 
-	private void startSession() throws OtrException {
-		host = new DummyOtrEngineHost(new OtrPolicyImpl(OtrPolicy.ALLOW_V2 | OtrPolicy.ALLOW_V3
-				| OtrPolicy.ERROR_START_AKE));
+  private void startSessionWithQuery() throws OtrException {
+    host = new DummyOtrEngineHost(new OtrPolicyImpl(OtrPolicy.ALLOW_V2 | OtrPolicy.ALLOW_V3 | OtrPolicy.ERROR_START_AKE));
+    usAlice = new OtrEngineImpl(host);
+    usBob = new OtrEngineImpl(host);
+    usAlice.transformReceiving(aliceSessionID, "<p>?OTRv23?\n" + "<span style=\"font-weight: bold;\">Bob@Wonderland/</span> has requested an <a href=\"http://otr.cypherpunks.ca/\">Off-the-Record private conversation</a>.  However, you do not have a plugin to support that.\n" + "See <a href=\"http://otr.cypherpunks.ca/\">http://otr.cypherpunks.ca/</a> for more information.</p>");
+    usBob.transformReceiving(bobSessionID, host.lastInjectedMessage);
+    usAlice.transformReceiving(aliceSessionID, host.lastInjectedMessage);
+    usBob.transformReceiving(bobSessionID, host.lastInjectedMessage);
+    usAlice.transformReceiving(aliceSessionID, host.lastInjectedMessage);
+    if (usBob.getSessionStatus(bobSessionID) != SessionStatus.ENCRYPTED || usAlice.getSessionStatus(aliceSessionID) != SessionStatus.ENCRYPTED) {
+      fail("Could not establish a secure session.");
+    }
+  }
 
-		usAlice = new OtrEngineImpl(host);
-		usBob = new OtrEngineImpl(host);
+  private void exchageMessages() throws OtrException {
+    String clearTextMessage = "Hello Bob, this new IM software you installed on my PC the other day says we are talking Off-the-Record, what is that supposed to mean?";
+    String sentMessage = usAlice.transformSending(aliceSessionID, clearTextMessage);
+    String receivedMessage = usBob.transformReceiving(bobSessionID, sentMessage);
+    if (!clearTextMessage.equals(receivedMessage)) {
+      fail();
+    }
+    clearTextMessage = "Hey Alice, it means that our communication is encrypted and authenticated.";
+    sentMessage = usBob.transformSending(bobSessionID, clearTextMessage);
+    receivedMessage = usAlice.transformReceiving(aliceSessionID, sentMessage);
+    if (!clearTextMessage.equals(receivedMessage)) {
+      fail();
+    }
+    clearTextMessage = "Oh, is that all?";
+    sentMessage = usAlice.transformSending(aliceSessionID, clearTextMessage);
+    receivedMessage = usBob.transformReceiving(bobSessionID, sentMessage);
+    if (!clearTextMessage.equals(receivedMessage)) {
+      fail();
+    }
+    clearTextMessage = "Actually no, our communication has the properties of perfect forward secrecy and deniable authentication.";
+    sentMessage = usBob.transformSending(bobSessionID, clearTextMessage);
+    receivedMessage = usAlice.transformReceiving(aliceSessionID, sentMessage);
+    if (!clearTextMessage.equals(receivedMessage)) {
+      fail();
+    }
+    clearTextMessage = "Oh really?! pouvons-nous parler en fran\u00e7ais?";
+    sentMessage = usAlice.transformSending(aliceSessionID, clearTextMessage);
+    receivedMessage = usBob.transformReceiving(bobSessionID, sentMessage);
+    if (!clearTextMessage.equals(receivedMessage)) {
+      fail();
+    }
+  }
 
-		usAlice.startSession(aliceSessionID);
-
-		// Bob receives query, sends D-H commit.
-
-		usBob.transformReceiving(bobSessionID, host.lastInjectedMessage);
-
-		// Alice received D-H Commit, sends D-H key.
-		usAlice
-				.transformReceiving(aliceSessionID,
-						host.lastInjectedMessage);
-
-		// Bob receives D-H Key, sends reveal signature.
-		usBob.transformReceiving(bobSessionID, host.lastInjectedMessage);
-
-		// Alice receives Reveal Signature, sends signature and goes secure.
-		usAlice
-				.transformReceiving(aliceSessionID,
-						host.lastInjectedMessage);
-
-		// Bobs receives Signature, goes secure.
-		usBob.transformReceiving(bobSessionID, host.lastInjectedMessage);
-
-		if (usBob.getSessionStatus(bobSessionID) != SessionStatus.ENCRYPTED
-				|| usAlice.getSessionStatus(aliceSessionID) != SessionStatus.ENCRYPTED)
-			fail("Could not establish a secure session.");
-	}
-
-	private void startSessionWithQuery() throws OtrException {
-		host = new DummyOtrEngineHost(new OtrPolicyImpl(OtrPolicy.ALLOW_V2 | OtrPolicy.ALLOW_V3
-				| OtrPolicy.ERROR_START_AKE));
-
-		usAlice = new OtrEngineImpl(host);
-		usBob = new OtrEngineImpl(host);
-
-		usAlice.transformReceiving(aliceSessionID, "<p>?OTRv23?\n" +
-				"<span style=\"font-weight: bold;\">Bob@Wonderland/</span> has requested an <a href=\"http://otr.cypherpunks.ca/\">Off-the-Record private conversation</a>.  However, you do not have a plugin to support that.\n" +
-				"See <a href=\"http://otr.cypherpunks.ca/\">http://otr.cypherpunks.ca/</a> for more information.</p>");
-		
-		// Bob receives D-H commit, sends D-H key.
-		usBob.transformReceiving(bobSessionID, host.lastInjectedMessage);
-
-		// Alice received D-H Key, sends reveal signature.
-		usAlice
-				.transformReceiving(aliceSessionID,
-						host.lastInjectedMessage);
-
-		// Bob receives reveal signature, sends signature.
-		usBob.transformReceiving(bobSessionID, host.lastInjectedMessage);
-
-		// Alice receives signature, goes secure
-		usAlice
-				.transformReceiving(aliceSessionID,
-						host.lastInjectedMessage);
-
-		if (usBob.getSessionStatus(bobSessionID) != SessionStatus.ENCRYPTED
-				|| usAlice.getSessionStatus(aliceSessionID) != SessionStatus.ENCRYPTED)
-			fail("Could not establish a secure session.");
-	}
-
-	private void exchageMessages() throws OtrException {
-		// We are both secure, send encrypted message.
-		String clearTextMessage = "Hello Bob, this new IM software you installed on my PC the other day says we are talking Off-the-Record, what is that supposed to mean?";
-		String sentMessage = usAlice.transformSending(aliceSessionID, clearTextMessage);
-
-		// Receive encrypted message.
-		String receivedMessage = usBob.transformReceiving(bobSessionID,
-				sentMessage);
-
-		if (!clearTextMessage.equals(receivedMessage))
-			fail();
-
-		// Send encrypted message.
-		clearTextMessage = "Hey Alice, it means that our communication is encrypted and authenticated.";
-		sentMessage = usBob.transformSending(bobSessionID, clearTextMessage);
-
-		// Receive encrypted message.
-		receivedMessage = usAlice.transformReceiving(aliceSessionID,
-				sentMessage);
-		if (!clearTextMessage.equals(receivedMessage))
-			fail();
-
-		// Send encrypted message.
-		clearTextMessage = "Oh, is that all?";
-		sentMessage = usAlice
-				.transformSending(aliceSessionID, clearTextMessage);
-
-		// Receive encrypted message.
-		receivedMessage = usBob.transformReceiving(bobSessionID, sentMessage);
-		if (!clearTextMessage.equals(receivedMessage))
-			fail();
-
-		// Send encrypted message.
-		clearTextMessage = "Actually no, our communication has the properties of perfect forward secrecy and deniable authentication.";
-		sentMessage = usBob.transformSending(bobSessionID, clearTextMessage);
-
-		// Receive encrypted message.
-		receivedMessage = usAlice.transformReceiving(aliceSessionID,
-				sentMessage);
-		if (!clearTextMessage.equals(receivedMessage))
-			fail();
-
-		// Send encrypted message. Test UTF-8 space characters.
-		clearTextMessage = "Oh really?! pouvons-nous parler en français?";
-		sentMessage = usAlice
-				.transformSending(aliceSessionID, clearTextMessage);
-
-		// Receive encrypted message.
-		receivedMessage = usBob.transformReceiving(bobSessionID, sentMessage);
-		if (!clearTextMessage.equals(receivedMessage))
-			fail();
-	}
-
-	private void endSession() throws OtrException {
-		usBob.endSession(bobSessionID);
-		usAlice.endSession(aliceSessionID);
-
-		if (usBob.getSessionStatus(bobSessionID) != SessionStatus.PLAINTEXT
-				|| usAlice.getSessionStatus(aliceSessionID) != SessionStatus.PLAINTEXT)
-			fail("Failed to end session.");
-	}
+  private void endSession() throws OtrException {
+    usBob.endSession(bobSessionID);
+    usAlice.endSession(aliceSessionID);
+    if (usBob.getSessionStatus(bobSessionID) != SessionStatus.PLAINTEXT || usAlice.getSessionStatus(aliceSessionID) != SessionStatus.PLAINTEXT) {
+      fail("Failed to end session.");
+    }
+  }
 }
