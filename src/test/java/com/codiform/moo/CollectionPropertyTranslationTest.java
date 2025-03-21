34f509524740cb1caaee41171032794a2e7990de
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 11:28:01 +0000

    Mining Framework Analysis

--- src/test/java/com/codiform/moo/CollectionPropertyTranslationTest.java
+++ src/test/java/com/codiform/moo/CollectionPropertyTranslationTest.java
@@ -1,19 +1,14 @@
 package com.codiform.moo;
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotSame;
-
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.SortedSet;
-import java.util.TreeSet;
 import java.util.Map;
-
+import java.util.TreeSet;
 import junit.framework.Assert;
-
 import org.junit.Test;
-
 import com.codiform.moo.annotation.TranslateCollection;
 import com.codiform.moo.curry.Translate;
 
@@ -21,203 +16,193 @@ import com.codiform.moo.curry.Translate;
  * Testing how properties that contain collection classes are translated.
  */
 public class CollectionPropertyTranslationTest {
+  @Test public void testTranslateCopiesMap() {
+    StockPrices domain = new StockPrices();
+    domain.setPrice("AAPL", 246.25);
+    domain.setPrice("MSFT", 28.98);
+    domain.setPrice("ORCL", 23.91);
+    StockPricesDto dto = Translate.to(StockPricesDto.class).from(domain);
+    assertNotSame(domain.getPrices(), dto.getPrices());
+    assertEquals(domain.getPrices(), dto.getPrices());
+  }
 
-	@Test
-	public void testTranslateCopiesMap() {
-		StockPrices domain = new StockPrices( );
-		domain.setPrice( "AAPL", 246.25 );
-		domain.setPrice( "MSFT", 28.98 );
-		domain.setPrice( "ORCL", 23.91 );
-		
-		StockPricesDto dto = Translate.to(StockPricesDto.class).from(domain);
-		
-		assertNotSame( domain.getPrices(), dto.getPrices() );
-		assertEquals( domain.getPrices(), dto.getPrices() );
-	}
-	
-	/**
+  /**
 	 * Defensively copy collections.
 	 */
-	@Test
-	public void testTranslateCopiesCollections() {
-		List<String> before = Arrays.asList("a", "b", "c");
-		TestDto dto = new Moo().translate(new StringsDomain(
-				before), TestDto.class);
-		Assert.assertEquals(before, dto.getStrings());
-		assertNotSame(before, dto.getStrings());
-		assertEquals(before, dto.getStrings());
-	}
+  @Test public void testTranslateCopiesCollections() {
+    List<String> before = Arrays.asList("a", "b", "c");
+    TestDto dto = new Moo().translate(new StringsDomain(before), TestDto.class);
+    Assert.assertEquals(before, dto.getStrings());
+    assertNotSame(before, dto.getStrings());
+    assertEquals(before, dto.getStrings());
+  }
 
-	/**
+  /**
 	 * Safely copy nulls inside collections.
 	 */
-	@Test
-	public void testCopiesNullsInsideCollections() {
-		List<String> before = Arrays.asList("a", "b", null, "c");
-		TestDto dto = new Moo().translate(new StringsDomain(
-				before), TestDto.class);
-		Assert.assertEquals(before, dto.getStrings());
-	}
+  @Test public void testCopiesNullsInsideCollections() {
+    List<String> before = Arrays.asList("a", "b", null, "c");
+    TestDto dto = new Moo().translate(new StringsDomain(before), TestDto.class);
+    Assert.assertEquals(before, dto.getStrings());
+  }
 
-	@Test
-	public void testTranslatesCollectionsOfTranslations() {
-		OrdinalList ordinals = new OrdinalList(new Ordinal(1, "first"), new Ordinal(
-				2, "second"), new Ordinal(3, "third"));
-		OrdinalListDto ordinalsDto = new Moo().translate(ordinals,
-				OrdinalListDto.class);
-		Assert.assertNotNull(ordinalsDto.getOrdinals());
-		Assert.assertEquals(ordinals.getOrdinals().size(), ordinalsDto
-				.getOrdinals().size());
-		for (int index = 0; index < ordinals.getOrdinals().size(); index++) {
-			Ordinal ordinal = ordinals.getOrdinals().get(index);
-			OrdinalDto ordinalDto = ordinalsDto.getOrdinals().get(index);
-			Assert.assertEquals(ordinal.getRank(), ordinalDto.getRank());
-			Assert.assertEquals(ordinal.getName(), ordinalDto.getName());
-		}
-	}
+  @Test public void testTranslatesCollectionsOfTranslations() {
+    OrdinalList ordinals = new OrdinalList(new Ordinal(1, "first"), new Ordinal(2, "second"), new Ordinal(3, "third"));
+    OrdinalListDto ordinalsDto = new Moo().translate(ordinals, OrdinalListDto.class);
+    Assert.assertNotNull(ordinalsDto.getOrdinals());
+    Assert.assertEquals(ordinals.getOrdinals().size(), ordinalsDto.getOrdinals().size());
+    for (int index = 0; index < ordinals.getOrdinals().size(); index++) {
+      Ordinal ordinal = ordinals.getOrdinals().get(index);
+      OrdinalDto ordinalDto = ordinalsDto.getOrdinals().get(index);
+      Assert.assertEquals(ordinal.getRank(), ordinalDto.getRank());
+      Assert.assertEquals(ordinal.getName(), ordinalDto.getName());
+    }
+  }
 
-	@Test
-	public void testNaturallySortsTranslationsOfNaturallySortedCollections() {
-		OrdinalSet ordinals = new OrdinalSet(new Ordinal(2, "second"), new Ordinal(
-				1, "first"), new Ordinal(3, "third"));
-		int index = 1;
-		for( Ordinal item : ordinals.getOrdinals() ) {
-			assertEquals( item.getRank(), index++ );
-		}
-		
-		OrdinalSetDto ordinalsDto = new Moo().translate(ordinals,
-				OrdinalSetDto.class);
-		Assert.assertNotNull(ordinalsDto.getOrdinals());
-		Assert.assertEquals(ordinals.getOrdinals().size(), ordinalsDto
-				.getOrdinals().size());
-		index = 1;
-		for( OrdinalDto item : ordinalsDto.getOrdinals() ) {
-			assertEquals( item.getRank(), index++ );
-		}
-	}
+  @Test public void testNaturallySortsTranslationsOfNaturallySortedCollections() {
+    OrdinalSet ordinals = new OrdinalSet(new Ordinal(2, "second"), new Ordinal(1, "first"), new Ordinal(3, "third"));
+    int index = 1;
+    for (Ordinal item : ordinals.getOrdinals()) {
+      assertEquals(item.getRank(), index++);
+    }
+    OrdinalSetDto ordinalsDto = new Moo().translate(ordinals, OrdinalSetDto.class);
+    Assert.assertNotNull(ordinalsDto.getOrdinals());
+    Assert.assertEquals(ordinals.getOrdinals().size(), ordinalsDto.getOrdinals().size());
+    index = 1;
+    for (OrdinalDto item : ordinalsDto.getOrdinals()) {
+      assertEquals(item.getRank(), index++);
+    }
+  }
 
-	public static class OrdinalList {
-		private List<Ordinal> ordinals;
+  public static class OrdinalList {
+    private List<Ordinal> ordinals;
 
-		public OrdinalList(Ordinal... ordinals) {
-			this.ordinals = Arrays.asList(ordinals);
-		}
+    public OrdinalList(Ordinal... ordinals) {
+      this.ordinals = Arrays.asList(ordinals);
+    }
 
-		public List<Ordinal> getOrdinals() {
-			return ordinals;
-		}
-	}
+    public List<Ordinal> getOrdinals() {
+      return ordinals;
+    }
+  }
 
-	public static class OrdinalSet {
-		private SortedSet<Ordinal> ordinals;
 
-		public OrdinalSet(Ordinal... ordinals) {
-			this.ordinals = new TreeSet<Ordinal>();
-			for( Ordinal item : ordinals )
-				this.ordinals.add( item );
-		}
+<<<<<<< /usr/src/app/output/geoffreywiseman/moo/c87b8cb339acf2e0e5d38ed55775bd9b6a3c3d1f/src/test/java/com/codiform/moo/CollectionPropertyTranslationTest.java/left.java
+  public static class OrdinalSet {
+    private SortedSet<Ordinal> ordinals;
 
-		public SortedSet<Ordinal> getOrdinals() {
-			return ordinals;
-		}
-	}
+    public OrdinalSet(Ordinal... ordinals) {
+      this.ordinals = new TreeSet<Ordinal>();
+      for (Ordinal item : ordinals) {
+        this.ordinals.add(item);
+      }
+    }
 
-	public static class Ordinal implements Comparable<Ordinal> {
-		private int rank;
-		private String name;
+    public SortedSet<Ordinal> getOrdinals() {
+      return ordinals;
+    }
+  }
+=======
+  public static class StockPrices {
+    private Map<String, Double> prices = new HashMap<String, Double>();
 
-		public Ordinal(int rank, String name) {
-			this.rank = rank;
-			this.name = name;
-		}
+    public void setPrice(String symbol, double price) {
+      prices.put(symbol, price);
+    }
 
-		public int getRank() {
-			return rank;
-		}
+    public Map<String, Double> getPrices() {
+      return prices;
+    }
+  }
+>>>>>>> /usr/src/app/output/geoffreywiseman/moo/c87b8cb339acf2e0e5d38ed55775bd9b6a3c3d1f/src/test/java/com/codiform/moo/CollectionPropertyTranslationTest.java/right.java
 
-		public String getName() {
-			return name;
-		}
 
-		public int compareTo(Ordinal o) {
-			return rank - o.rank;
-		}
-	}
+  public static class Ordinal implements Comparable<Ordinal> {
+    private int rank;
 
-	public static class OrdinalListDto {
-		@TranslateCollection(OrdinalDto.class)
-		private List<OrdinalDto> ordinals;
+    private String name;
 
-		public List<OrdinalDto> getOrdinals() {
-			return ordinals;
-		}
-	}
+    public Ordinal(int rank, String name) {
+      this.rank = rank;
+      this.name = name;
+    }
 
-	public static class OrdinalSetDto {
-		@TranslateCollection(OrdinalDto.class)
-		private SortedSet<OrdinalDto> ordinals;
+    public int getRank() {
+      return rank;
+    }
 
-		public SortedSet<OrdinalDto> getOrdinals() {
-			return ordinals;
-		}
-	}
+    public String getName() {
+      return name;
+    }
 
-	public static class OrdinalDto implements Comparable<OrdinalDto> {
-		private int rank;
-		private String name;
+    public int compareTo(Ordinal o) {
+      return rank - o.rank;
+    }
+  }
 
-		public int getRank() {
-			return rank;
-		}
+  public static class OrdinalListDto {
+    @TranslateCollection(value = OrdinalDto.class) private List<OrdinalDto> ordinals;
 
-		public String getName() {
-			return name;
-		}
-		
-		public int compareTo( OrdinalDto other ) {
-			return rank - other.rank;
-		}
-	}
+    public List<OrdinalDto> getOrdinals() {
+      return ordinals;
+    }
+  }
 
-	public static class StringsDomain {
-		private List<String> strings;
 
-		public StringsDomain(List<String> strings) {
-			this.strings = strings;
-		}
+<<<<<<< /usr/src/app/output/geoffreywiseman/moo/c87b8cb339acf2e0e5d38ed55775bd9b6a3c3d1f/src/test/java/com/codiform/moo/CollectionPropertyTranslationTest.java/left.java
+  public static class OrdinalSetDto {
+    @TranslateCollection(value = OrdinalDto.class) private SortedSet<OrdinalDto> ordinals;
 
-		public List<String> getStrings() {
-			return strings;
-		}
-	}
+    public SortedSet<OrdinalDto> getOrdinals() {
+      return ordinals;
+    }
+  }
+=======
+  public static class StockPricesDto {
+    private Map<String, Double> prices = new HashMap<String, Double>();
 
-	public static class TestDto {
-		private List<String> strings;
+    public Map<String, Double> getPrices() {
+      return prices;
+    }
+  }
+>>>>>>> /usr/src/app/output/geoffreywiseman/moo/c87b8cb339acf2e0e5d38ed55775bd9b6a3c3d1f/src/test/java/com/codiform/moo/CollectionPropertyTranslationTest.java/right.java
 
-		public List<String> getStrings() {
-			return strings;
-		}
-	}
 
-	public static class StockPrices {
-		private Map<String,Double> prices = new HashMap<String,Double>();
-		
-		public void setPrice(String symbol, double price) {
-			prices.put( symbol, price );
-		}
+  public static class OrdinalDto implements Comparable<OrdinalDto> {
+    private int rank;
 
-		public Map<String,Double> getPrices() {
-			return prices;
-		}
+    private String name;
 
-	}
+    public int getRank() {
+      return rank;
+    }
 
-	public static class StockPricesDto {
-		private Map<String,Double> prices = new HashMap<String,Double>();
-		
-		public Map<String,Double> getPrices() {
-			return prices;
-		}
+    public String getName() {
+      return name;
+    }
 
-	}
+    public int compareTo(OrdinalDto other) {
+      return rank - other.rank;
+    }
+  }
+
+  public static class StringsDomain {
+    private List<String> strings;
+
+    public StringsDomain(List<String> strings) {
+      this.strings = strings;
+    }
+
+    public List<String> getStrings() {
+      return strings;
+    }
+  }
+
+  public static class TestDto {
+    private List<String> strings;
+
+    public List<String> getStrings() {
+      return strings;
+    }
+  }
 }
