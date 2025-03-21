72e9c35ab9abe89d77d9326de0526414f2f7df49
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 11:52:07 +0000

    Mining Framework Analysis

--- modules/core/src/test/java/com/illposed/osc/OSCPortTest.java
+++ modules/core/src/test/java/com/illposed/osc/OSCPortTest.java
@@ -1,96 +1,105 @@
-/*
- * Copyright (C) 2001, C. Ramakrishnan / Illposed Software.
- * All rights reserved.
- *
- * This code is licensed under the BSD 3-Clause license.
- * See file LICENSE (or LICENSE.html) for more information.
- */
-
 package com.illposed.osc;
 
+<<<<<<< /usr/src/app/output/hoijui/javaosc/f806396898fdfbf9533f2ff881b05ff5b6df5f6e/modules/core/src/test/java/com/illposed/osc/OSCPortTest.java/left.java
 import java.util.ArrayList;
+
+=======
+>>>>>>> Unknown file: This is a bug in JDime.
+
+<<<<<<< /usr/src/app/output/hoijui/javaosc/f806396898fdfbf9533f2ff881b05ff5b6df5f6e/modules/core/src/test/java/com/illposed/osc/OSCPortTest.java/left.java
 import java.util.Date;
+
+=======
+>>>>>>> Unknown file: This is a bug in JDime.
+
+<<<<<<< /usr/src/app/output/hoijui/javaosc/f806396898fdfbf9533f2ff881b05ff5b6df5f6e/modules/core/src/test/java/com/illposed/osc/OSCPortTest.java/left.java
 import java.util.List;
 
+=======
+>>>>>>> Unknown file: This is a bug in JDime.
+
 public class OSCPortTest extends junit.framework.TestCase {
+  private OSCPortOut sender;
 
-	private OSCPortOut sender;
-	private OSCPortIn  receiver;
+  private OSCPortIn receiver;
 
-	@Override
-	protected void setUp() throws Exception {
-		super.setUp();
-		sender = new OSCPortOut();
-		receiver = new OSCPortIn(OSCPort.defaultSCOSCPort());
-	}
+  /**
+	 * @see junit.framework.TestCase#setUp()
+	 */
+  @Override protected void setUp() throws Exception {
+    super.setUp();
+    sender = new OSCPortOut();
+    receiver = new OSCPortIn(OSCPort.defaultSCOSCPort());
+  }
 
-	@Override
-	protected void tearDown() throws Exception {
-		sender.close();
-		receiver.close();
-		super.tearDown();
-	}
+  /**
+	 * @see junit.framework.TestCase#tearDown()
+	 */
+  @Override protected void tearDown() throws Exception {
+    sender.close();
+    receiver.close();
+    super.tearDown();
+  }
 
-	public void testStart() throws Exception {
-		OSCMessage mesg = new OSCMessage("/sc/stop");
-		sender.send(mesg);
-	}
+  public void testStart() throws Exception {
+    OSCMessage mesg = new OSCMessage("/sc/stop");
+    sender.send(mesg);
+  }
 
-	public void testMessageWithArgs() throws Exception {
-		List<Object> args = new ArrayList<Object>(2);
-		args.add(new Integer(3));
-		args.add("hello");
-		OSCMessage mesg = new OSCMessage("/foo/bar", args);
-		sender.send(mesg);
-	}
+  public void testMessageWithArgs() throws Exception {
+    List<Object> args = new ArrayList<Object>(2);
+    args.add(new Integer(3));
+    args.add("hello");
+    OSCMessage mesg = new OSCMessage("/foo/bar", args);
+    sender.send(mesg);
+  }
 
-	public void testBundle() throws Exception {
-		List<Object> args = new ArrayList<Object>(2);
-		args.add(new Integer(3));
-		args.add("hello");
-		List<OSCPacket> msgs = new ArrayList<OSCPacket>(1);
-		msgs.add(new OSCMessage("/foo/bar", args));
-		OSCBundle bundle = new OSCBundle(msgs);
-		sender.send(bundle);
-	}
+  public void testBundle() throws Exception {
+    List<Object> args = new ArrayList<Object>(2);
+    args.add(new Integer(3));
+    args.add("hello");
+    List<OSCPacket> msgs = new ArrayList<OSCPacket>(1);
+    msgs.add(new OSCMessage("/foo/bar", args));
+    OSCBundle bundle = new OSCBundle(msgs);
+    sender.send(bundle);
+  }
 
-	public void testBundle2() throws Exception {
-		OSCMessage mesg = new OSCMessage("/foo/bar");
-		mesg.addArgument(new Integer(3));
-		mesg.addArgument("hello");
-		OSCBundle bundle = new OSCBundle();
-		bundle.addPacket(mesg);
-		sender.send(bundle);
-	}
+  public void testBundle2() throws Exception {
+    OSCMessage mesg = new OSCMessage("/foo/bar");
+    mesg.addArgument(new Integer(3));
+    mesg.addArgument("hello");
+    OSCBundle bundle = new OSCBundle();
+    bundle.addPacket(mesg);
+    sender.send(bundle);
+  }
 
-	public void testReceiving() throws Exception {
-		OSCMessage mesg = new OSCMessage("/message/receiving");
-		TestOSCListener listener = new TestOSCListener();
-		receiver.addListener("/message/receiving", listener);
-		receiver.startListening();
-		sender.send(mesg);
-		Thread.sleep(100); // wait a bit
-		receiver.stopListening();
-		if (!listener.isMessageReceived()) {
-			fail("Message was not received");
-		}
-	}
+  public void testReceiving() throws Exception {
+    OSCMessage mesg = new OSCMessage("/message/receiving");
+    TestOSCListener listener = new TestOSCListener();
+    receiver.addListener("/message/receiving", listener);
+    receiver.startListening();
+    sender.send(mesg);
+    Thread.sleep(100);
+    receiver.stopListening();
+    if (!listener.isMessageReceived()) {
+      fail("Message was not received");
+    }
+  }
 
-	public void testBundleReceiving() throws Exception {
-		OSCBundle bundle = new OSCBundle();
-		bundle.addPacket(new OSCMessage("/bundle/receiving"));
-		TestOSCListener listener = new TestOSCListener();
-		receiver.addListener("/bundle/receiving", listener);
-		receiver.startListening();
-		sender.send(bundle);
-		Thread.sleep(100); // wait a bit
-		receiver.stopListening();
-		if (!listener.isMessageReceived()) {
-			fail("Message was not received");
-		}
-		if (!listener.getReceivedTimestamp().equals(bundle.getTimestamp())) {
-			fail("Message should have timestamp " + bundle.getTimestamp()
-					+ " but has " + listener.getReceivedTimestamp());
-		}
-	}
+  public void testBundleReceiving() throws Exception {
+    OSCBundle bundle = new OSCBundle();
+    bundle.addPacket(new OSCMessage("/bundle/receiving"));
+    TestOSCListener listener = new TestOSCListener();
+    receiver.addListener("/bundle/receiving", listener);
+    receiver.startListening();
+    sender.send(bundle);
+    Thread.sleep(100);
+    receiver.stopListening();
+    if (!listener.isMessageReceived()) {
+      fail("Message was not received");
+    }
+    if (!listener.getReceivedTimestamp().equals(bundle.getTimestamp())) {
+      fail("Message should have timestamp " + bundle.getTimestamp() + " but has " + listener.getReceivedTimestamp());
+    }
+  }
 }
