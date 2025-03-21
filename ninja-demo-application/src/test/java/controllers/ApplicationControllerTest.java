20eb87235dfe85d4e049018e6f2923405e2809f1
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 15:51:56 +0000

    Mining Framework Analysis

--- ninja-demo-application/src/test/java/controllers/ApplicationControllerTest.java
+++ ninja-demo-application/src/test/java/controllers/ApplicationControllerTest.java
@@ -1,73 +1,34 @@
 package controllers;
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-
 import java.util.Map;
-
 import ninja.NinjaApiTest;
 import ninja.NinjaApiTestHelper;
-
 import org.apache.http.HttpResponse;
 import org.junit.Test;
-
 import com.google.common.collect.Maps;
 
 public class ApplicationControllerTest extends NinjaApiTest {
+  @Test public void testThatRedirectWorks() {
+    Map<String, String> headers = Maps.newHashMap();
+    String result = NinjaApiTestHelper.makeRequest(getServerAddress() + "/redirect", headers);
+    assertTrue(result.contains("And developing large web applications becomes fun again."));
+  }
 
-	@Test
-	public void testThatRedirectWorks() {
+  @Test public void testHtmlEscapingInTeamplateWorks() {
+    String expectedContent = "&lt;script&gt;alert(\'Hello\');&lt;/script&gt;";
+    Map<String, String> headers = Maps.newHashMap();
+    String result = NinjaApiTestHelper.makeRequest(getServerAddress() + "htmlEscaping", headers);
+    assertTrue(result.contains(expectedContent));
+  }
 
-		// Some empty headers for now...
-		Map<String, String> headers = Maps.newHashMap();
-
-		// /redirect will send a location: redirect in the headers
-		String result = NinjaApiTestHelper.makeRequest(getServerAddress()
-				+ "/redirect", headers);
-
-		// If the redirect has worked we must see the following text
-		// from the index screen:
-		assertTrue(result
-				.contains("And developing large web applications becomes fun again."));
-
-	}
-
-	@Test
-	public void testHtmlEscapingInTeamplateWorks() {
-
-		//IF the escaping works I expect the following string inside the page:
-		String expectedContent = "&lt;script&gt;alert('Hello');&lt;/script&gt;";
-		// Some empty headers for now...
-		Map<String, String> headers = Maps.newHashMap();
-
-		// /redirect will send a location: redirect in the headers
-
-		String result = NinjaApiTestHelper.makeRequest(getServerAddress()
-				+ "htmlEscaping", headers);
-
-		// If the redirect has worked we must see the following text
-		// from the index screen:
-		assertTrue(result.contains(expectedContent));
-
-	}
-
-	@Test
-	public void makeSureSessionsGetSentToClient() {
-
-		// Some empty headers for now...
-		Map<String, String> headers = Maps.newHashMap();
-
-		// /redirect will send a location: redirect in the headers
-		HttpResponse httpResponse = NinjaApiTestHelper
-				.makeRequestAndGetResponse(getServerAddress() + "session",
-						headers);
-
-		String setCookie = httpResponse.getHeaders("Set-Cookie")[0].getValue();
-
-		assertTrue(setCookie.contains("NINJA_SESSION"));
-		assertTrue(setCookie.contains("___TS"));
-		assertTrue(setCookie.contains("username"));
-		assertTrue(setCookie.contains("kevin"));
-
-	}
+  @Test public void makeSureSessionsGetSentToClient() {
+    Map<String, String> headers = Maps.newHashMap();
+    HttpResponse httpResponse = NinjaApiTestHelper.makeRequestAndGetResponse(getServerAddress() + "session", headers);
+    String setCookie = httpResponse.getHeaders("Set-Cookie")[0].getValue();
+    assertTrue(setCookie.contains("NINJA_SESSION"));
+    assertTrue(setCookie.contains("___TS"));
+    assertTrue(setCookie.contains("username"));
+    assertTrue(setCookie.contains("kevin"));
+  }
 }
