1955790114a896d75e5a77365962fc6af2f1adff
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 10:00:34 +0000

    Mining Framework Analysis

--- src/main/java/gov/nysenate/openleg/model/spotcheck/SpotCheckReport.java
+++ src/main/java/gov/nysenate/openleg/model/spotcheck/SpotCheckReport.java
@@ -1,9 +1,7 @@
 package gov.nysenate.openleg.model.spotcheck;
-
 import com.google.common.collect.HashBasedTable;
 import com.google.common.collect.Table;
 import gov.nysenate.openleg.service.spotcheck.base.SpotCheckReportService;
-
 import java.time.LocalDateTime;
 import java.util.*;
 import java.util.stream.Collectors;
@@ -15,238 +13,219 @@ import java.util.stream.Collectors;
  * @see SpotCheckReportService
  * @param <ContentKey>
  */
-public class SpotCheckReport<ContentKey>
-{
-    /** Auto increment id */
-    private int id;
+public class SpotCheckReport<ContentKey extends java.lang.Object> {
+  /** Auto increment id */
+  private int id;
 
-    /** Identifier for this report. */
-    protected SpotCheckReportId reportId;
+  /** Identifier for this report. */
+  protected SpotCheckReportId reportId;
 
-    /** All observations associated with this report. */
-    protected Map<ContentKey, SpotCheckObservation<ContentKey>> observations = new HashMap<>();
+  /** All observations associated with this report. */
+  protected Map<ContentKey, SpotCheckObservation<ContentKey>> observations = new HashMap<>();
 
-    /** miscellaneous notes pertaining to this report */
-    protected String notes;
+  /** miscellaneous notes pertaining to this report */
+  protected String notes;
 
-    /** --- Constructors --- */
+  /** --- Constructors --- */
+  public SpotCheckReport() {
+  }
 
-    public SpotCheckReport() {}
+  public SpotCheckReport(SpotCheckReportId reportId) {
+    this.reportId = reportId;
+  }
 
-    public SpotCheckReport(SpotCheckReportId reportId) {
-        this.reportId = reportId;
-    }
+  public SpotCheckReport(SpotCheckReportId reportId, String notes) {
+    this(reportId);
+    this.notes = notes;
+  }
 
-    public SpotCheckReport(SpotCheckReportId reportId, String notes) {
-        this(reportId);
-        this.notes = notes;
-    }
+  /** --- Methods --- */
+  public SpotCheckReportSummary getSummary() {
+    SpotCheckReportSummary summary = new SpotCheckReportSummary(reportId, notes);
+    summary.addCountsFromObservations(observations.values());
+    return summary;
+  }
 
-    /** --- Methods --- */
-
-    public SpotCheckReportSummary getSummary() {
-        SpotCheckReportSummary summary = new SpotCheckReportSummary(reportId, notes);
-        summary.addCountsFromObservations(observations.values());
-        return summary;
-    }
-
-    /**
+  /**
      * Get a count of open mismatches
      * @param ignored boolean - returns the count of ignored mismatches if true, which are not included if false
      * @return long
      */
-    public long getOpenMismatchCount(boolean ignored) {
-        return observations.values().stream()
-                .map(obs -> obs.getMismatches().values().stream()
-                        .filter(mismatch -> !mismatch.isIgnored() ^ ignored)
-                        .filter(mismatch -> mismatch.getState() == MismatchState.OPEN)
-                        .count()
-                )
-                .reduce(0L, (a, b) -> a + b);
-    }
+  public long getOpenMismatchCount(boolean ignored) {
+    return observations.values().stream().map((obs) -> obs.getMismatches().values().stream().filter((mismatch) -> !mismatch.isIgnored() ^ ignored).filter((mismatch) -> mismatch.getState() == MismatchState.OPEN).count()).reduce(0L, (a, b) -> a + b);
+  }
 
-    /**
+  /**
      * Get the number of mismatches across all observations grouped by the mismatch status.
      * @param ignored boolean - get the status count of ignored mismatches if true, which are not included if false
      * @return Map<SpotCheckMismatchStatus, Long>
      */
-    public Map<MismatchState, Long> getMismatchStatusCounts(boolean ignored) {
-        if (observations != null) {
-            Map<MismatchState, Long> counts = new HashMap<>();
-            for (MismatchState status : MismatchState.values()) {
-                counts.put(status, 0L);
-            }
-            observations.values().stream()
-                .flatMap(e -> e.getMismatchStatusCounts(ignored).entrySet().stream())
-                .forEach(e -> counts.merge(e.getKey(), e.getValue(), Long::sum));
-            return counts;
-        }
-        throw new IllegalStateException("The observations on this report have not yet been set.");
+  public Map<MismatchState, Long> getMismatchStatusCounts(boolean ignored) {
+    if (observations != null) {
+      Map<MismatchState, Long> counts = new HashMap<>();
+      for (MismatchState status : MismatchState.values()) {
+        counts.put(status, 0L);
+      }
+      observations.values().stream().flatMap((e) -> e.getMismatchStatusCounts(ignored).entrySet().stream()).forEach((e) -> counts.merge(e.getKey(), e.getValue(), Long::sum));
+      return counts;
     }
+    throw new IllegalStateException("The observations on this report have not yet been set.");
+  }
 
-    /**
+  /**
      * Gets a count of mismatch types grouped by statuses across all observations.
      * @param ignored boolean - get type/status counts of ignored mismatches if true, which are not included if false
      * @return Map<SpotCheckMismatchType, Map<SpotCheckMismatchStatus, Long>>
      */
-    public Map<SpotCheckMismatchType, Map<MismatchState, Long>> getMismatchTypeStatusCounts(boolean ignored) {
-        if (observations != null) {
-            Map<SpotCheckMismatchType, Map<MismatchState, Long>> counts = new HashMap<>();
-            observations.values().stream()
-                .flatMap(e -> e.getMismatchStatusTypes(ignored).entrySet().stream())
-                .forEach(e -> {
-                    if (!counts.containsKey(e.getKey())) {
-                        counts.put(e.getKey(), new HashMap<>());
-                    }
-                    counts.get(e.getKey()).merge(e.getValue(), 1L, (a,b) -> a + 1L);
-                });
-            return counts;
+  public Map<SpotCheckMismatchType, Map<MismatchState, Long>> getMismatchTypeStatusCounts(boolean ignored) {
+    if (observations != null) {
+      Map<SpotCheckMismatchType, Map<MismatchState, Long>> counts = new HashMap<>();
+      observations.values().stream().flatMap((e) -> e.getMismatchStatusTypes(ignored).entrySet().stream()).forEach((e) -> {
+        if (!counts.containsKey(e.getKey())) {
+          counts.put(e.getKey(), new HashMap<>());
         }
-        throw new IllegalStateException("The observations on this report have not yet been set.");
+        counts.get(e.getKey()).merge(e.getValue(), 1L, (a, b) -> a + 1L);
+      });
+      return counts;
     }
+    throw new IllegalStateException("The observations on this report have not yet been set.");
+  }
 
-    /**
+  /**
      * Gets a count of mismatch statuses grouped by type across all observations.
      * @param ignored boolean - get status/type counts of ignored mismatches if true, which are not included if false
      * @return Map<SpotCheckMismatchStatus, Map<SpotCheckMismatchType, Long>>
      */
-    public Map<MismatchState, Map<SpotCheckMismatchType, Long>> getMismatchStatusTypeCounts(boolean ignored) {
-        if (observations != null) {
-            Table<MismatchState, SpotCheckMismatchType, Long> countTable = HashBasedTable.create();
-            observations.values().stream()
-                    .flatMap(obs -> obs.getMismatchStatusTypes(ignored).entrySet().stream())
-                    .forEach(entry -> {
-                        long currentValue = Optional.ofNullable(countTable.get(entry.getValue(), entry.getKey()))
-                                .orElse(0l);
-                        countTable.put(entry.getValue(), entry.getKey(), currentValue + 1);
-                    });
-            return countTable.rowMap();
-        }
-        throw new IllegalStateException("The observations on this report have not yet been set.");
+  public Map<MismatchState, Map<SpotCheckMismatchType, Long>> getMismatchStatusTypeCounts(boolean ignored) {
+    if (observations != null) {
+      Table<MismatchState, SpotCheckMismatchType, Long> countTable = HashBasedTable.create();
+      observations.values().stream().flatMap((obs) -> obs.getMismatchStatusTypes(ignored).entrySet().stream()).forEach((entry) -> {
+        long currentValue = Optional.ofNullable(countTable.get(entry.getValue(), entry.getKey())).orElse(0l);
+        countTable.put(entry.getValue(), entry.getKey(), currentValue + 1);
+      });
+      return countTable.rowMap();
     }
+    throw new IllegalStateException("The observations on this report have not yet been set.");
+  }
 
-    /**
+  /**
      * Get the number of mismatches across all observations grouped by the mismatch type.
      * @param ignored boolean - get type counts of ignored mismatches if true, which are not included if false
      * @return Map<SpotCheckMismatchType, Long>
      */
-    public Map<SpotCheckMismatchType, Long> getMismatchTypeCounts(boolean ignored) {
-        if (observations != null) {
-            Map<SpotCheckMismatchType, Long> counts = new HashMap<>();
-            observations.values().stream()
-                .flatMap(e -> e.getMismatchTypes(ignored).stream())
-                .forEach(e -> counts.merge(e, 1L, (a,b) -> a + 1L));
-            return counts;
-        }
-        throw new IllegalStateException("The observations on this report have not yet been set.");
+  public Map<SpotCheckMismatchType, Long> getMismatchTypeCounts(boolean ignored) {
+    if (observations != null) {
+      Map<SpotCheckMismatchType, Long> counts = new HashMap<>();
+      observations.values().stream().flatMap((e) -> e.getMismatchTypes(ignored).stream()).forEach((e) -> counts.merge(e, 1L, (a, b) -> a + 1L));
+      return counts;
     }
+    throw new IllegalStateException("The observations on this report have not yet been set.");
+  }
 
-    /**
+  /**
      * Add the observation to the map.
      */
-    public void addObservation(SpotCheckObservation<ContentKey> observation) {
-        if (observation == null) {
-            throw new IllegalArgumentException("Supplied observation cannot be null");
-        }
-        observation.setReferenceId(reportId.getReferenceId());
-        observation.setObservedDateTime(LocalDateTime.now());
-        this.observations.put(observation.getKey(), observation);
+  public void addObservation(SpotCheckObservation<ContentKey> observation) {
+    if (observation == null) {
+      throw new IllegalArgumentException("Supplied observation cannot be null");
     }
+    observation.setReferenceId(reportId.getReferenceId());
+    observation.setObservedDateTime(LocalDateTime.now());
+    this.observations.put(observation.getKey(), observation);
+  }
 
-    /**
+  /**
      * Add the collection of observations to the map.
      */
-    public void addObservations(Collection<SpotCheckObservation<ContentKey>> observations) {
-        if (observations == null) {
-            throw new IllegalArgumentException("Supplied observation collection cannot be null");
-        }
-        observations.forEach(this::addObservation);
+  public void addObservations(Collection<SpotCheckObservation<ContentKey>> observations) {
+    if (observations == null) {
+      throw new IllegalArgumentException("Supplied observation collection cannot be null");
     }
+    observations.forEach(this::addObservation);
+  }
 
-    /**
+  /**
      * Get the number of content items observed
      */
-    public int getObservedCount() {
-        return Optional.ofNullable(this.observations).map(Map::size).orElse(0);
-    }
+  public int getObservedCount() {
+    return Optional.ofNullable(this.observations).map(Map::size).orElse(0);
+  }
 
-    /**
+  /**
      * Get ContentKey's that were checked by this report.
      * @return
      */
-    public Set<ContentKey> getCheckedKeys() {
-        return this.getObservations().values().stream().map(SpotCheckObservation::getKey).collect(Collectors.toSet());
-    }
+  public Set<ContentKey> getCheckedKeys() {
+    return this.getObservations().values().stream().map(SpotCheckObservation::getKey).collect(Collectors.toSet());
+  }
 
-    /**
+  /**
      * Add an observation to the report indicating that content is missing from the reference data
      * @param missingKey ContentKey - id of the missing data
      */
-    public void addRefMissingObs(ContentKey missingKey) {
-        this.addObservation(SpotCheckObservation.getRefMissingObs(reportId.getReferenceId(), missingKey));
-    }
+  public void addRefMissingObs(ContentKey missingKey) {
+    this.addObservation(SpotCheckObservation.getRefMissingObs(reportId.getReferenceId(), missingKey));
+  }
 
-    /**
+  /**
      * Add an observation to the report indicating that content is missing from Openleg data
      * @param missingKey ContentKey - id of the missing data
      */
-    public void addObservedDataMissingObs(ContentKey missingKey) {
-        this.addObservation(SpotCheckObservation.getObserveDataMissingObs(reportId.getReferenceId(), missingKey));
-    }
+  public void addObservedDataMissingObs(ContentKey missingKey) {
+    this.addObservation(SpotCheckObservation.getObserveDataMissingObs(reportId.getReferenceId(), missingKey));
+  }
 
-    /**
+  /**
      * Add an empty observation to the report, indicating that there are no errors.
      * @param contentKey ContentKey
      */
-    public void addEmptyObservation(ContentKey contentKey) {
-        this.addObservation(new SpotCheckObservation<>(reportId.getReferenceId(), contentKey));
-    }
+  public void addEmptyObservation(ContentKey contentKey) {
+    this.addObservation(new SpotCheckObservation<>(reportId.getReferenceId(), contentKey));
+  }
 
-    /** --- Delegates --- */
+  /** --- Delegates --- */
+  public LocalDateTime getReportDateTime() {
+    return reportId.getReportDateTime();
+  }
 
-    public LocalDateTime getReportDateTime() {
-        return reportId.getReportDateTime();
-    }
+  public LocalDateTime getReferenceDateTime() {
+    return reportId.getReferenceDateTime();
+  }
 
-    public LocalDateTime getReferenceDateTime() {
-        return reportId.getReferenceDateTime();
-    }
+  public SpotCheckRefType getReferenceType() {
+    return reportId.getReferenceType();
+  }
 
-    public SpotCheckRefType getReferenceType() {
-        return reportId.getReferenceType();
-    }
+  /** --- Basic Getters/Setters --- */
+  public int getId() {
+    return id;
+  }
 
-    /** --- Basic Getters/Setters --- */
+  public void setId(int id) {
+    this.id = id;
+  }
 
-    public int getId() {
-        return id;
-    }
+  public SpotCheckReportId getReportId() {
+    return reportId;
+  }
 
-    public void setId(int id) {
-        this.id = id;
-    }
+  public void setReportId(SpotCheckReportId reportId) {
+    this.reportId = reportId;
+  }
 
-    public SpotCheckReportId getReportId() {
-        return reportId;
-    }
+  public Map<ContentKey, SpotCheckObservation<ContentKey>> getObservations() {
+    return observations;
+  }
 
-    public void setReportId(SpotCheckReportId reportId) {
-        this.reportId = reportId;
-    }
+  public void setObservations(Map<ContentKey, SpotCheckObservation<ContentKey>> observations) {
+    this.observations = observations;
+  }
 
-    public Map<ContentKey, SpotCheckObservation<ContentKey>> getObservations() {
-        return observations;
-    }
+  public String getNotes() {
+    return notes;
+  }
 
-    public void setObservations(Map<ContentKey, SpotCheckObservation<ContentKey>> observations) {
-        this.observations = observations;
-    }
-
-    public String getNotes() {
-        return notes;
-    }
-
-    public void setNotes(String notes) {
-        this.notes = notes;
-    }
+  public void setNotes(String notes) {
+    this.notes = notes;
+  }
 }
