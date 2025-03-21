2d402199f11a016eb58f3fb49e8b67df1f169cf4
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 19:55:19 +0000

    Mining Framework Analysis

--- src/main/java/st/redline/compiler/BlockAnalyser.java
+++ src/main/java/st/redline/compiler/BlockAnalyser.java
@@ -1,33 +1,41 @@
-/* Redline Smalltalk, Copyright (c) James C. Ladd. All rights reserved. See LICENSE in the root of this distribution */
 package st.redline.compiler;
 
 public class BlockAnalyser extends MethodAnalyser {
+  public BlockAnalyser(String className, String packageName, int countOfArguments, boolean isClassMethod, Analyser containingAnalyser) {
+    super(className, packageName, countOfArguments, isClassMethod, containingAnalyser);
 
-	public BlockAnalyser(String className, String packageName, int countOfArguments, boolean isClassMethod, Analyser containingAnalyser) {
-		super(className, packageName, countOfArguments, isClassMethod, containingAnalyser);
-	}
+<<<<<<< /usr/src/app/output/redline-smalltalk/redline-smalltalk/8d475d7ea4bcf0001b0486803b76c29ef6ba8698/src/main/java/st/redline/compiler/BlockAnalyser.java/left.java
+    this.containingAnalyser = containingAnalyser;
+=======
+>>>>>>> Unknown file: This is a bug in JDime.
 
-	protected void initialize() {
-		classBytecodeWriter = new BlockBytecodeWriter(className, packageName, countOfArguments);
-	}
 
-	public boolean continueBlockVisit() {
-		return true;
-	}
+<<<<<<< /usr/src/app/output/redline-smalltalk/redline-smalltalk/8d475d7ea4bcf0001b0486803b76c29ef6ba8698/src/main/java/st/redline/compiler/BlockAnalyser.java/left.java
+    this.blockSequence(containingAnalyser.blockSequence() + 1);
+=======
+>>>>>>> Unknown file: This is a bug in JDime.
+  }
 
-	public void visit(Block block) {
-//		System.out.println("Block() Analysis begin " + block);
-		classBytecodeWriter.openClass();
-	}
+  protected void initialize() {
+    classBytecodeWriter = new BlockBytecodeWriter(className, packageName, countOfArguments);
+  }
 
-	public void visitEnd(Block block) {
-		if (!block.hasStatements())
-			classBytecodeWriter.stackPushNil(block.line());
-		classBytecodeWriter.closeClass();
-	}
+  public boolean continueBlockVisit() {
+    return true;
+  }
 
-	public void visit(BlockVariableName blockVariableName, String value, int line) {
-//		System.out.println("visit(BlockVariable) " + value);
-		registerMethodArgument(blockVariableName);
-	}
+  public void visit(Block block) {
+    classBytecodeWriter.openClass();
+  }
+
+  public void visitEnd(Block block) {
+    if (!block.hasStatements()) {
+      classBytecodeWriter.stackPushNil(block.line());
+    }
+    classBytecodeWriter.closeClass();
+  }
+
+  public void visit(BlockVariableName blockVariableName, String value, int line) {
+    registerMethodArgument(blockVariableName);
+  }
 }
