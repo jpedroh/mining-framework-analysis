ba5083685d3e7406cdd5c57927a8ac3ff3a14a82
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 00:55:34 +0000

    Mining Framework Analysis

--- src/main/java/com/google/transit/realtime/GtfsRealtimeExtensions.java
+++ src/main/java/com/google/transit/realtime/GtfsRealtimeExtensions.java
@@ -1,63 +1,6 @@
-/**
- * Portions of this module are modifications based on work created and shared by Google:
- *
- *   http://code.google.com/policies.html
- *
- * and used according to terms described in the Creative Commons 3.0 Attribution License:
- *
- *   http://creativecommons.org/licenses/by/3.0/
- *
- * Specifically, the gtfs-realtime.proto:
- *
- *   http://code.google.com/transit/realtime/docs/gtfs-realtime_proto.html
- *
- * was used to generate the classes contained within.
- *
- * Portions of this module are modifications based on work created by the New York
- * City Metropolitan Transportation Authority.
- *
- * All code is Copyright (C) 2011 Google, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *          http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- */
-package com.google.transit.realtime;
-
-import com.google.protobuf.ExtensionRegistry;
-
-/**
- * Support for GTFS-realtime extensions.
- * 
- * @author bdferris
- */
-public class GtfsRealtimeExtensions {
-
-  /**
-   * Adds all known GTFS-realtime extension messages to the specified extension
-   * registry, except LIRR, unless includeLIRR=true. If includeLIRR=true, the
-   * LIRR extension will be added to the registry but the MNR extension will
-   * not be.
-   *
-   * LIRR and MNR GTFS-RT extensions both use extension ID 1005. Since MNR has
-   * been assigned extension ID 1005 in the Google registry, it makes sense to
-   * default to MNR.
-   * 
-   * @param registry registry to add the extensions to
-   */
-  public static void registerExtensions(ExtensionRegistry registry) {
-    GtfsRealtimeNYCT.registerAllExtensions(registry);
-    GtfsRealtimeOneBusAway.registerAllExtensions(registry);
-    GtfsRealtimeMTARR.registerAllExtensions(registry)
-    GtfsRealtimeServiceStatus.registerAllExtensions(registry);
-  }
-
-}
+  package    com . google . transit . realtime ;   import    com . google . protobuf . ExtensionRegistry ;   public class GtfsRealtimeExtensions  { 
+<<<<<<<
+  public static void registerExtensions  (  ExtensionRegistry registry ,  boolean includeLIRR )  {   GtfsRealtimeNYCT . registerAllExtensions  ( registry ) ;   GtfsRealtimeOneBusAway . registerAllExtensions  ( registry ) ;  if  ( includeLIRR )  {   GtfsRealtimeLIRR . registerAllExtensions  ( registry ) ; } else  {   GtfsRealtimeMNR . registerAllExtensions  ( registry ) ; }   GtfsRealtimeServiceStatus . registerAllExtensions  ( registry ) ; }
+=======
+>>>>>>>
+   public static void registerExtensions  (  ExtensionRegistry registry )  {   registry . add  (  GtfsRealtimeNYCT . nyctFeedHeader ) ;   registry . add  (  GtfsRealtimeNYCT . nyctStopTimeUpdate ) ;   registry . add  (  GtfsRealtimeNYCT . nyctTripDescriptor ) ;   registry . add  (  GtfsRealtimeOneBusAway . obaFeedHeader ) ;   registry . add  (  GtfsRealtimeOneBusAway . obaFeedEntity ) ;   registry . add  (  GtfsRealtimeOneBusAway . obaTripUpdate ) ;   registry . add  (  GtfsRealtimeOneBusAway . obaEntitySelector ) ;   registry . add  (  GtfsRealtimeOneBusAway . obaStopTimeUpdate ) ;   registry . add  (  GtfsRealtimeMTARR . mtaRailroadStopTimeUpdate ) ;   registry . add  (  GtfsRealtimeMTARR . carriageDescriptor ) ;   registry . add  (  GtfsRealtimeServiceStatus . mercuryFeedHeader ) ;   registry . add  (  GtfsRealtimeServiceStatus . mercuryEntitySelector ) ;   registry . add  (  GtfsRealtimeServiceStatus . mercuryAlert ) ; } }
