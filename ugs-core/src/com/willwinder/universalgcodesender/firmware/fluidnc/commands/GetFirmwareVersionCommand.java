320d0a8b8ec19ef61b199198d3c416c08ecd48b1
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 20:43:31 +0000

    Mining Framework Analysis

--- ugs-core/src/com/willwinder/universalgcodesender/firmware/fluidnc/commands/GetFirmwareVersionCommand.java
+++ ugs-core/src/com/willwinder/universalgcodesender/firmware/fluidnc/commands/GetFirmwareVersionCommand.java
@@ -1,105 +1,85 @@
-/*
-    Copyright 2022 Will Winder
-
-    This file is part of Universal Gcode Sender (UGS).
-
-    UGS is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    UGS is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with UGS.  If not, see <http://www.gnu.org/licenses/>.
- */
 package com.willwinder.universalgcodesender.firmware.fluidnc.commands;
-
 import com.willwinder.universalgcodesender.utils.SemanticVersion;
 import org.apache.commons.lang3.StringUtils;
-
 import java.text.ParseException;
 import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class GetFirmwareVersionCommand extends SystemCommand {
-    private static final Pattern VERSION_FLUIDNC_PATTERN = Pattern.compile("\\[VER:[0-9.]+ (?<variant>[a-zA-Z0-9]+) v(?<version>(?<major>[0-9]*)(.(?<minor>[0-9]+)(.(?<patch>[0-9]+))?)?([a-zA-Z]+)?)(.*:.*)*]", Pattern.CASE_INSENSITIVE);
-    private static final Pattern VERSION_GRBL_PATTERN = Pattern.compile("\\[VER:(?<version>(?<major>[0-9]*)(.(?<minor>[0-9]+)(.(?<patch>[0-9]+))?)).*]", Pattern.CASE_INSENSITIVE);
+  private static final Pattern VERSION_FLUIDNC_PATTERN = Pattern.compile("\\[VER:[0-9.]+ (?<variant>[a-zA-Z0-9]+) v(?<version>(?<major>[0-9]*)(.(?<minor>[0-9]+)(.(?<patch>[0-9]+))?)?([a-zA-Z]+)?)(.*:.*)*]", Pattern.CASE_INSENSITIVE);
 
-    public GetFirmwareVersionCommand() {
-        super("$I");
+  private static final Pattern VERSION_GRBL_PATTERN = Pattern.compile("\\[VER:(?<version>(?<major>[0-9]*)(.(?<minor>[0-9]+)(.(?<patch>[0-9]+))?)).*]", Pattern.CASE_INSENSITIVE);
+
+
+
+  public GetFirmwareVersionCommand() {
+    super("$I");
+  }
+
+  public String getFirmware() {
+    Optional<String> firmwareOptional = parseFluidNCVariant();
+    if (!firmwareOptional.isPresent()) {
+      firmwareOptional = parseGrblVariant();
     }
+    return firmwareOptional.orElse("Unknown");
+  }
 
-    public String getFirmware() {
-        Optional<String> firmwareOptional = parseFluidNCVariant();
-        if (!firmwareOptional.isPresent()) {
-            firmwareOptional = parseGrblVariant();
+  private Optional<String> parseGrblVariant() {
+    String response = StringUtils.defaultString(getResponse());
+    for (String line : StringUtils.split(response, "\n")) {
+      Matcher versionMatcher = VERSION_GRBL_PATTERN.matcher(line);
+      if (versionMatcher.find()) {
+        return Optional.of("GRBL");
+      }
+    }
+    return Optional.empty();
+  }
+
+  private Optional<String> parseFluidNCVariant() {
+    String response = StringUtils.defaultString(getResponse());
+    for (String line : StringUtils.split(response, "\n")) {
+      Matcher versionMatcher = VERSION_FLUIDNC_PATTERN.matcher(line);
+      if (versionMatcher.find()) {
+        return Optional.of(versionMatcher.group("variant"));
+      }
+    }
+    return Optional.empty();
+  }
+
+  public SemanticVersion getVersion() {
+    Optional<SemanticVersion> versionOptional = parseFluidNCVs();
+    if (!versionOptional.isPresent()) {
+      versionOptional = parseGrblVersion();
+    }
+    return versionOptional.orElse(new SemanticVersion());
+  }
+
+  private Optional<SemanticVersion> parseGrblVersion() {
+    String response = StringUtils.defaultString(getResponse());
+    for (String line : StringUtils.split(response, "\n")) {
+      Matcher versionMatcher = VERSION_GRBL_PATTERN.matcher(line);
+      if (versionMatcher.find()) {
+        try {
+          return Optional.of(new SemanticVersion(versionMatcher.group("version")));
+        } catch (ParseException e) {
         }
-
-        return firmwareOptional.orElse("Unknown");
+      }
     }
+    return Optional.empty();
+  }
 
-    private Optional<String> parseGrblVariant() {
-        String response = StringUtils.defaultString(getResponse());
-        for (String line : StringUtils.split(response, "\n")) {
-            Matcher versionMatcher = VERSION_GRBL_PATTERN.matcher(line);
-            if (versionMatcher.find()) {
-                return Optional.of("GRBL");
-            }
+  private Optional<SemanticVersion> parseFluidNCVs() {
+    String response = StringUtils.defaultString(getResponse());
+    for (String line : StringUtils.split(response, "\n")) {
+      Matcher matcher = VERSION_FLUIDNC_PATTERN.matcher(line);
+      if (matcher.find()) {
+        try {
+          return Optional.of(new SemanticVersion(matcher.group("version")));
+        } catch (ParseException e) {
         }
-        return Optional.empty();
+      }
     }
-
-    private Optional<String> parseFluidNCVariant() {
-        String response = StringUtils.defaultString(getResponse());
-        for (String line : StringUtils.split(response, "\n")) {
-            Matcher versionMatcher = VERSION_FLUIDNC_PATTERN.matcher(line);
-            if (versionMatcher.find()) {
-                return Optional.of(versionMatcher.group("variant"));
-            }
-        }
-        return Optional.empty();
-    }
-
-    public SemanticVersion getVersion() {
-        Optional<SemanticVersion> versionOptional = parseFluidNCVs();
-        if (!versionOptional.isPresent()) {
-            versionOptional = parseGrblVersion();
-        }
-        return versionOptional.orElse(new SemanticVersion());
-    }
-
-    private Optional<SemanticVersion> parseGrblVersion() {
-        String response = StringUtils.defaultString(getResponse());
-        for (String line : StringUtils.split(response, "\n")) {
-            Matcher versionMatcher = VERSION_GRBL_PATTERN.matcher(line);
-            if (versionMatcher.find()) {
-                try {
-                    return Optional.of(new SemanticVersion(versionMatcher.group("version")));
-                } catch (ParseException e) {
-                    // Never mind
-                }
-            }
-        }
-        return Optional.empty();
-    }
-
-    private Optional<SemanticVersion> parseFluidNCVs() {
-        String response = StringUtils.defaultString(getResponse());
-        for (String line : StringUtils.split(response, "\n")) {
-            Matcher matcher = VERSION_FLUIDNC_PATTERN.matcher(line);
-            if (matcher.find()) {
-                try {
-                    return Optional.of(new SemanticVersion(matcher.group("version")));
-                } catch (ParseException e) {
-                    // Never mind
-                }
-            }
-        }
-        return Optional.empty();
-    }
+    return Optional.empty();
+  }
 }
--- ugs-core/src/com/willwinder/universalgcodesender/utils/ControllerUtils.java
+++ ugs-core/src/com/willwinder/universalgcodesender/utils/ControllerUtils.java
@@ -1,27 +1,7 @@
-/*
-    Copyright 2022 Will Winder
-
-    This file is part of Universal Gcode Sender (UGS).
-
-    UGS is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    UGS is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with UGS.  If not, see <http://www.gnu.org/licenses/>.
- */
 package com.willwinder.universalgcodesender.utils;
-
 import com.willwinder.universalgcodesender.IController;
 import com.willwinder.universalgcodesender.types.CommandListener;
 import com.willwinder.universalgcodesender.types.GcodeCommand;
-
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
@@ -30,10 +10,9 @@ import java.util.function.Supplier;
  * @author Joacim Breiler
  */
 public class ControllerUtils {
+  private static final int MAX_EXECUTION_TIME = 2000;
 
-    private static final int MAX_EXECUTION_TIME = 2000;
-
-    /**
+  /**
      * Sends a command and blocks the thread until the command is done - either with an ok or error.
      *
      * @param controller       the controller to send the command through
@@ -41,24 +20,22 @@ public class ControllerUtils {
      * @param maxExecutionTime the max number of milliseconds to wait before throwing a timeout error
      * @throws Exception if the command could not be sent or a timeout occurred
      */
-    public static <T extends GcodeCommand> T sendAndWaitForCompletion(IController controller, T command, long maxExecutionTime) throws Exception {
-        final AtomicBoolean isDone = new AtomicBoolean(false);
-        CommandListener listener = c -> isDone.set(c.isDone());
-        command.addListener(listener);
-        controller.sendCommandImmediately(command);
-
-        long startTime = System.currentTimeMillis();
-        while (!isDone.get()) {
-            if (System.currentTimeMillis() > startTime + maxExecutionTime) {
-                throw new RuntimeException("The command \"" + command.getCommandString() + "\" has timed out as it wasn't finished within " + maxExecutionTime + "ms");
-            }
-            Thread.sleep(10);
-        }
-
-        return command;
+  public static <T extends GcodeCommand> T sendAndWaitForCompletion(IController controller, T command, long maxExecutionTime) throws Exception {
+    final AtomicBoolean isDone = new AtomicBoolean(false);
+    CommandListener listener = (c) -> isDone.set(c.isDone());
+    command.addListener(listener);
+    controller.sendCommandImmediately(command);
+    long startTime = System.currentTimeMillis();
+    while (!isDone.get()) {
+      if (System.currentTimeMillis() > startTime + maxExecutionTime) {
+        throw new RuntimeException("The command \"" + command.getCommandString() + "\" has timed out as it wasn\'t finished within " + maxExecutionTime + "ms");
+      }
+      Thread.sleep(10);
     }
+    return command;
+  }
 
-    /**
+  /**
      * Sends a command and blocks the thread until the command is done - either with an ok or error.
      *
      * @param controller the controller to send the command through
@@ -67,29 +44,27 @@ public class ControllerUtils {
      * @return the executed command with the response
      * @throws Exception if the command could not be sent or a timeout occurred
      */
+  public static <T extends GcodeCommand> T sendAndWaitForCompletion(IController controller, T command) throws Exception {
+    return sendAndWaitForCompletion(controller, command, MAX_EXECUTION_TIME);
+  }
 
-    public static <T extends GcodeCommand> T sendAndWaitForCompletion(IController controller, T command) throws Exception {
-        return sendAndWaitForCompletion(controller, command, MAX_EXECUTION_TIME);
-    }
-
-    /**
+  /**
      * Waits for all commands to complete before continuing
      *
      * @param controller the controller to check for active commands
      */
-    public static void waitOnActiveCommands(IController controller) throws InterruptedException {
-        long maxExecutionTime = MAX_EXECUTION_TIME;
-        long startTime = System.currentTimeMillis();
-        while (controller.getActiveCommand().isPresent()) {
-            if (startTime + maxExecutionTime < System.currentTimeMillis()) {
-                throw new RuntimeException("The command \"" + controller.getActiveCommand().get().getCommandString() + "\" has timed out as it wasn't finished within " + maxExecutionTime + "ms");
-            }
-
-            Thread.sleep(10);
-        }
+  public static void waitOnActiveCommands(IController controller) throws InterruptedException {
+    long maxExecutionTime = MAX_EXECUTION_TIME;
+    long startTime = System.currentTimeMillis();
+    while (controller.getActiveCommand().isPresent()) {
+      if (startTime + maxExecutionTime < System.currentTimeMillis()) {
+        throw new RuntimeException("The command \"" + controller.getActiveCommand().get().getCommandString() + "\" has timed out as it wasn\'t finished within " + maxExecutionTime + "ms");
+      }
+      Thread.sleep(10);
     }
+  }
 
-    /**
+  /**
      * Sends a command and blocks the thread until the command is done and was ok. It will retry to send the command in case of a timeout or an error.
      *
      * @param commandSupplier  a supplier for creating gcode commands to send
@@ -101,36 +76,32 @@ public class ControllerUtils {
      * @return the last executed command with the response
      * @throws InterruptedException
      */
-    public static <T extends GcodeCommand> T sendAndWaitForCompletionWithRetry(Supplier<T> commandSupplier, IController controller, long maxExecutionTime, int retryCount, Consumer<Integer> onExecute) throws InterruptedException {
-        int times = 0;
-
-        T command = null;
-        while (times < retryCount && controller.isCommOpen()) {
-            long startTime = System.currentTimeMillis();
-            try {
-                onExecute.accept(times + 1);
-                command = commandSupplier.get();
-                if (command == null) {
-                    throw new IllegalArgumentException("The command generated was null");
-                }
-
-                command = sendAndWaitForCompletion(controller, command, maxExecutionTime);
-            } catch (Exception e) {
-                // Never mind
-            }
-
-            if (!command.isDone() || command.isError()) {
-                // Make sure that we sleep at least the max execution time in case the command got a wierd response.
-                if (startTime + maxExecutionTime > System.currentTimeMillis()) {
-                    long sleepTime = startTime + maxExecutionTime - System.currentTimeMillis();
-                    Thread.sleep(sleepTime);
-                }
-            } else if (command.isDone() && !command.isError()) {
-                break;
-            }
-            times++;
+  public static <T extends GcodeCommand> T sendAndWaitForCompletionWithRetry(Supplier<T> commandSupplier, IController controller, long maxExecutionTime, int retryCount, Consumer<Integer> onExecute) throws InterruptedException {
+    int times = 0;
+    T command = null;
+    while (times < retryCount && controller.isCommOpen()) {
+      long startTime = System.currentTimeMillis();
+      try {
+        onExecute.accept(times + 1);
+        command = commandSupplier.get();
+        if (command == null) {
+          throw new IllegalArgumentException("The command generated was null");
         }
-
-        return command;
+        command = sendAndWaitForCompletion(controller, command, maxExecutionTime);
+      } catch (Exception e) {
+      }
+      if (!command.isDone() || command.isError()) {
+        if (startTime + maxExecutionTime > System.currentTimeMillis()) {
+          long sleepTime = startTime + maxExecutionTime - System.currentTimeMillis();
+          Thread.sleep(sleepTime);
+        }
+      } else {
+        if (command.isDone() && !command.isError()) {
+          break;
+        }
+      }
+      times++;
     }
+    return command;
+  }
 }
