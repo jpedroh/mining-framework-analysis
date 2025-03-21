f31aace7bc92fe6349dfb8257e16d102bd695626
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 02:50:26 +0000

    Mining Framework Analysis

--- driver-core/src/main/java/com/datastax/driver/core/ConsistencyLevel.java
+++ driver-core/src/main/java/com/datastax/driver/core/ConsistencyLevel.java
@@ -18,6 +18,27 @@ package com.datastax.driver.core;
 import com.datastax.driver.core.exceptions.DriverInternalError;
 
 public enum ConsistencyLevel {
+<<<<<<< /usr/src/app/output/datastax/java-driver/3705c952425480778ddd51751be4db54cf75bc81/driver-core/src/main/java/com/datastax/driver/core/ConsistencyLevel.java/left.java
+||||||| /usr/src/app/output/datastax/java-driver/3705c952425480778ddd51751be4db54cf75bc81/driver-core/src/main/java/com/datastax/driver/core/ConsistencyLevel.java/base.java
+    ANY,
+    ONE,
+    TWO,
+    THREE,
+    QUORUM,
+    ALL,
+    LOCAL_QUORUM,
+    EACH_QUORUM;
+=======
+    ANY,
+    ONE,
+    TWO,
+    THREE,
+    QUORUM,
+    ALL,
+    LOCAL_QUORUM,
+    EACH_QUORUM,
+    LOCAL_ONE;
+>>>>>>> /usr/src/app/output/datastax/java-driver/3705c952425480778ddd51751be4db54cf75bc81/driver-core/src/main/java/com/datastax/driver/core/ConsistencyLevel.java/right.java
 
     ANY          (0),
     ONE          (1),
@@ -31,6 +52,7 @@ public enum ConsistencyLevel {
     LOCAL_SERIAL (9),
     LOCAL_ONE   (10);
 
+<<<<<<< /usr/src/app/output/datastax/java-driver/3705c952425480778ddd51751be4db54cf75bc81/driver-core/src/main/java/com/datastax/driver/core/ConsistencyLevel.java/left.java
     // Used by the native protocol
     final int code;
     private static final ConsistencyLevel[] codeIdx;
@@ -43,6 +65,30 @@ public enum ConsistencyLevel {
             if (codeIdx[cl.code] != null)
                 throw new IllegalStateException("Duplicate code");
             codeIdx[cl.code] = cl;
+||||||| /usr/src/app/output/datastax/java-driver/3705c952425480778ddd51751be4db54cf75bc81/driver-core/src/main/java/com/datastax/driver/core/ConsistencyLevel.java/base.java
+    static ConsistencyLevel from(org.apache.cassandra.db.ConsistencyLevel cl) {
+        switch (cl) {
+            case ANY: return ANY;
+            case ONE: return ONE;
+            case TWO: return TWO;
+            case THREE: return THREE;
+            case QUORUM: return QUORUM;
+            case ALL: return ALL;
+            case LOCAL_QUORUM: return LOCAL_QUORUM;
+            case EACH_QUORUM: return EACH_QUORUM;
+=======
+    static ConsistencyLevel from(org.apache.cassandra.db.ConsistencyLevel cl) {
+        switch (cl) {
+            case ANY: return ANY;
+            case ONE: return ONE;
+            case TWO: return TWO;
+            case THREE: return THREE;
+            case QUORUM: return QUORUM;
+            case ALL: return ALL;
+            case LOCAL_QUORUM: return LOCAL_QUORUM;
+            case EACH_QUORUM: return EACH_QUORUM;
+            case LOCAL_ONE: return LOCAL_ONE;
+>>>>>>> /usr/src/app/output/datastax/java-driver/3705c952425480778ddd51751be4db54cf75bc81/driver-core/src/main/java/com/datastax/driver/core/ConsistencyLevel.java/right.java
         }
     }
 
@@ -50,9 +96,36 @@ public enum ConsistencyLevel {
         this.code = code;
     }
 
+<<<<<<< /usr/src/app/output/datastax/java-driver/3705c952425480778ddd51751be4db54cf75bc81/driver-core/src/main/java/com/datastax/driver/core/ConsistencyLevel.java/left.java
     static ConsistencyLevel fromCode(int code) {
         if (code < 0 || code >= codeIdx.length)
             throw new DriverInternalError(String.format("Unknown code %d for a consistency level", code));
         return codeIdx[code];
+||||||| /usr/src/app/output/datastax/java-driver/3705c952425480778ddd51751be4db54cf75bc81/driver-core/src/main/java/com/datastax/driver/core/ConsistencyLevel.java/base.java
+        switch (cl) {
+            case ANY: return org.apache.cassandra.db.ConsistencyLevel.ANY;
+            case ONE: return org.apache.cassandra.db.ConsistencyLevel.ONE;
+            case TWO: return org.apache.cassandra.db.ConsistencyLevel.TWO;
+            case THREE: return org.apache.cassandra.db.ConsistencyLevel.THREE;
+            case QUORUM: return org.apache.cassandra.db.ConsistencyLevel.QUORUM;
+            case ALL: return org.apache.cassandra.db.ConsistencyLevel.ALL;
+            case LOCAL_QUORUM: return org.apache.cassandra.db.ConsistencyLevel.LOCAL_QUORUM;
+            case EACH_QUORUM: return org.apache.cassandra.db.ConsistencyLevel.EACH_QUORUM;
+        }
+        throw new AssertionError();
+=======
+        switch (cl) {
+            case ANY: return org.apache.cassandra.db.ConsistencyLevel.ANY;
+            case ONE: return org.apache.cassandra.db.ConsistencyLevel.ONE;
+            case TWO: return org.apache.cassandra.db.ConsistencyLevel.TWO;
+            case THREE: return org.apache.cassandra.db.ConsistencyLevel.THREE;
+            case QUORUM: return org.apache.cassandra.db.ConsistencyLevel.QUORUM;
+            case ALL: return org.apache.cassandra.db.ConsistencyLevel.ALL;
+            case LOCAL_QUORUM: return org.apache.cassandra.db.ConsistencyLevel.LOCAL_QUORUM;
+            case EACH_QUORUM: return org.apache.cassandra.db.ConsistencyLevel.EACH_QUORUM;
+            case LOCAL_ONE: return org.apache.cassandra.db.ConsistencyLevel.LOCAL_ONE;
+        }
+        throw new AssertionError();
+>>>>>>> /usr/src/app/output/datastax/java-driver/3705c952425480778ddd51751be4db54cf75bc81/driver-core/src/main/java/com/datastax/driver/core/ConsistencyLevel.java/right.java
     }
 }
