4cc1465559ff7d2d4e7070279caeadea0106d01d
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 07:36:36 +0000

    Mining Framework Analysis

--- src/main/java/com/github/javafaker/Faker.java
+++ src/main/java/com/github/javafaker/Faker.java
@@ -1,12 +1,10 @@
 package com.github.javafaker;
-
 import com.github.javafaker.service.DefaultingFakeValuesService;
 import com.github.javafaker.service.FakeValuesService;
 import com.github.javafaker.service.FakeValuesServiceInterface;
 import com.github.javafaker.service.RandomService;
 import org.apache.commons.lang.WordUtils;
 import org.apache.commons.lang.reflect.MethodUtils;
-
 import java.lang.reflect.Proxy;
 import java.util.Locale;
 import java.util.Random;
@@ -18,73 +16,106 @@ import java.util.Random;
  * @author ren
  */
 public class Faker implements Resolver {
-    private final RandomService randomService;
-    private final FakeValuesService fakeValuesService;
-    private final App app;
-    private final Lorem lorem;
-    private final Name name;
-    private final Number number;
-    private final Internet internet;
-    private final PhoneNumber phoneNumber;
-    private final Address address;
-    private final Business business;
-    private final Book book;
-    private final Color color;
-    private final Commerce commerce;
-    private final Company company;
-    private final Hacker hacker;
-    private final Options options;
-    private final Code code;
-    private final Finance finance;
-    private final DateAndTime dateAndTime;
+  private final RandomService randomService;
 
-    public Faker() {
-        this(Locale.ENGLISH);
-    }
+  private final FakeValuesService fakeValuesService;
 
-    public Faker(Locale locale) {
-        this(locale, null);
-    }
+  private final 
+<<<<<<< /usr/src/app/output/dius/java-faker/eaea01edb6684f0a0dec286d205db88293dc3581/src/main/java/com/github/javafaker/Faker.java/left.java
+  App
+=======
+  Commerce
+>>>>>>> /usr/src/app/output/dius/java-faker/eaea01edb6684f0a0dec286d205db88293dc3581/src/main/java/com/github/javafaker/Faker.java/right.java
+   
+<<<<<<< /usr/src/app/output/dius/java-faker/eaea01edb6684f0a0dec286d205db88293dc3581/src/main/java/com/github/javafaker/Faker.java/left.java
+  app
+=======
+  commerce
+>>>>>>> /usr/src/app/output/dius/java-faker/eaea01edb6684f0a0dec286d205db88293dc3581/src/main/java/com/github/javafaker/Faker.java/right.java
+  ;
 
-    public Faker(Random random) {
-        this(Locale.ENGLISH, random);
-    }
+  private final Lorem lorem;
 
-    public Faker(Locale locale, Random random) {
-        this.randomService = new RandomService(random);
-        this.fakeValuesService = new FakeValuesService(locale, randomService);
-        FakeValuesService defaultEnglishFakeValuesService = new FakeValuesService(locale.ENGLISH, randomService);
-        FakeValuesServiceInterface proxiedFakeValueService = createProxiedFakeValuesService(fakeValuesService,
-                defaultEnglishFakeValuesService);
+  private final Name name;
 
-        this.app = new App(this, proxiedFakeValueService);
-        this.lorem = new Lorem(proxiedFakeValueService, randomService);
-        this.name = new Name(this, proxiedFakeValueService);
-        this.number = new Number(randomService);
-        this.internet = new Internet(name, proxiedFakeValueService, randomService);
-        this.phoneNumber = new PhoneNumber(proxiedFakeValueService);
-        this.address = new Address(this, name, proxiedFakeValueService, randomService);
-        this.book = new Book(this, proxiedFakeValueService);
-        this.business = new Business(proxiedFakeValueService);
-        this.color = new Color(proxiedFakeValueService);
-        this.hacker = new Hacker(proxiedFakeValueService);
-        this.company = new Company(this, proxiedFakeValueService, randomService);
-        this.commerce = new Commerce(proxiedFakeValueService, randomService);
-        this.options = new Options(randomService);
-        this.code = new Code(randomService);
-        this.finance = new Finance(proxiedFakeValueService, randomService);
-        this.dateAndTime = new DateAndTime(randomService);
-    }
+  private final Number number;
 
-    private static FakeValuesServiceInterface createProxiedFakeValuesService(FakeValuesServiceInterface fakeValuesServiceInterface,
-                                                                                      FakeValuesServiceInterface defaultFakeValuesServiceInterface) {
-        return (FakeValuesServiceInterface) Proxy.newProxyInstance(Faker.class.getClassLoader(),
-                new Class[]{FakeValuesServiceInterface.class},
-                new DefaultingFakeValuesService(fakeValuesServiceInterface,
-                                                defaultFakeValuesServiceInterface));
-    }
+  private final Internet internet;
 
-    /**
+  private final PhoneNumber phoneNumber;
+
+  private final Address address;
+
+  private final Business business;
+
+  private final Book book;
+
+  private final Color color;
+
+  private final Company company;
+
+  private final Hacker hacker;
+
+  private final Options options;
+
+  private final Code code;
+
+  private final Finance finance;
+
+  private final DateAndTime dateAndTime;
+
+  public Faker() {
+    this(Locale.ENGLISH);
+  }
+
+  public Faker(Locale locale) {
+    this(locale, null);
+  }
+
+  public Faker(Random random) {
+    this(Locale.ENGLISH, random);
+  }
+
+  public Faker(Locale locale, Random random) {
+    this.randomService = new RandomService(random);
+    this.fakeValuesService = new FakeValuesService(locale, randomService);
+    FakeValuesService defaultEnglishFakeValuesService = new FakeValuesService(locale.ENGLISH, randomService);
+    FakeValuesServiceInterface proxiedFakeValueService = createProxiedFakeValuesService(fakeValuesService, defaultEnglishFakeValuesService);
+    this.app = new App(this, proxiedFakeValueService);
+    this.lorem = new Lorem(proxiedFakeValueService, randomService);
+    this.name = new Name(this, proxiedFakeValueService);
+    this.number = new Number(randomService);
+    this.internet = new Internet(name, proxiedFakeValueService, randomService);
+    this.phoneNumber = new PhoneNumber(proxiedFakeValueService);
+    this.address = new Address(this, name, proxiedFakeValueService, randomService);
+    this.book = new Book(this, proxiedFakeValueService);
+    this.business = new Business(proxiedFakeValueService);
+    this.color = new Color(proxiedFakeValueService);
+    this.
+<<<<<<< /usr/src/app/output/dius/java-faker/eaea01edb6684f0a0dec286d205db88293dc3581/src/main/java/com/github/javafaker/Faker.java/left.java
+    hacker
+=======
+    commerce
+>>>>>>> /usr/src/app/output/dius/java-faker/eaea01edb6684f0a0dec286d205db88293dc3581/src/main/java/com/github/javafaker/Faker.java/right.java
+     = new 
+<<<<<<< /usr/src/app/output/dius/java-faker/eaea01edb6684f0a0dec286d205db88293dc3581/src/main/java/com/github/javafaker/Faker.java/left.java
+    Hacker
+=======
+    Commerce
+>>>>>>> /usr/src/app/output/dius/java-faker/eaea01edb6684f0a0dec286d205db88293dc3581/src/main/java/com/github/javafaker/Faker.java/right.java
+    (proxiedFakeValueService, randomService);
+    this.company = new Company(this, proxiedFakeValueService, randomService);
+    this.options = new Options(randomService);
+    this.code = new Code(randomService);
+    this.finance = new Finance(proxiedFakeValueService, randomService);
+    this.dateAndTime = new DateAndTime(randomService);
+  }
+
+  private static FakeValuesServiceInterface createProxiedFakeValuesService(FakeValuesServiceInterface fakeValuesServiceInterface, FakeValuesServiceInterface defaultFakeValuesServiceInterface) {
+    return (FakeValuesServiceInterface) Proxy.newProxyInstance(Faker.class.getClassLoader(), new Class[] { FakeValuesServiceInterface.class }, new DefaultingFakeValuesService(fakeValuesServiceInterface, defaultFakeValuesServiceInterface));
+  }
+
+  /**
      * Returns a string with the '#' characters in the parameter replaced with random digits between 0-9 inclusive.
      * <p>
      * For example, the string "ABC##EFG" could be replaced with a string like "ABC99EFG".
@@ -92,11 +123,11 @@ public class Faker implements Resolver {
      * @param numberString
      * @return
      */
-    public String numerify(String numberString) {
-        return fakeValuesService.numerify(numberString);
-    }
+  public String numerify(String numberString) {
+    return fakeValuesService.numerify(numberString);
+  }
 
-    /**
+  /**
      * Returns a string with the '?' characters in the parameter replaced with random alphabetic
      * characters.
      * <p>
@@ -105,113 +136,114 @@ public class Faker implements Resolver {
      * @param letterString
      * @return
      */
-    public String letterify(String letterString) {
-        return fakeValuesService.letterify(letterString);
-    }
+  public String letterify(String letterString) {
+    return fakeValuesService.letterify(letterString);
+  }
 
-    /**
+  /**
      * Applies both a {@link #numerify(String)} and a {@link #letterify(String)}
      * over the incoming string.
      *
      * @param string
      * @return
      */
-    public String bothify(String string) {
-        return fakeValuesService.bothify(string);
-    }
+  public String bothify(String string) {
+    return fakeValuesService.bothify(string);
+  }
 
-    /**
+  /**
      * Generates a String that matches the given regular expression,
      */
-    public String regexify(String regex) {
-        return fakeValuesService.regexify(regex);
-    }
+  public String regexify(String regex) {
+    return fakeValuesService.regexify(regex);
+  }
 
-    public App app() {
-        return app;
-    }
+  public App app() {
+    return app;
+  }
 
-    public Name name() {
-        return name;
-    }
+  public Name name() {
+    return name;
+  }
 
-    public Number number() { return number; }
+  public Number number() {
+    return number;
+  }
 
-    public Internet internet() {
-        return internet;
-    }
+  public Internet internet() {
+    return internet;
+  }
 
-    public PhoneNumber phoneNumber() {
-        return phoneNumber;
-    }
+  public PhoneNumber phoneNumber() {
+    return phoneNumber;
+  }
 
-    public Lorem lorem() {
-        return lorem;
-    }
+  public Lorem lorem() {
+    return lorem;
+  }
 
-    public Address address() {
-        return address;
-    }
+  public Address address() {
+    return address;
+  }
 
-    public Book book() {
-        return book;
-    }
+  public Book book() {
+    return book;
+  }
 
-    public Business business() {
-        return business;
-    }
+  public Business business() {
+    return business;
+  }
 
-    public Color color() {
-        return color;
-    }
+  public Color color() {
+    return color;
+  }
 
-    public Commerce commerce() {
-        return commerce;
-    }
+  public Commerce commerce() {
+    return commerce;
+  }
 
-    public Company company() {
-        return company;
-    }
+  public Company company() {
+    return company;
+  }
 
-    public Hacker hacker() {
-        return hacker;
-    }
+  public Hacker hacker() {
+    return hacker;
+  }
 
-    public Options options() {
-        return options;
-    }
+  public Options options() {
+    return options;
+  }
 
-    public Code code() {
-        return code;
-    }
+  public Code code() {
+    return code;
+  }
 
-    public Finance finance() {
-        return finance;
-    }
+  public Finance finance() {
+    return finance;
+  }
 
-    public DateAndTime date() {
-        return dateAndTime;
-    }
+  public DateAndTime date() {
+    return dateAndTime;
+  }
 
-    /**
+  /**
      * Resolves a key in the format of class.method_name
      *
      * @param key
      * @return
      */
-    public String resolve(String key) {
-        String[] keySplit = key.split("\\.", 2);
-        String object = keySplit[0].toLowerCase();
-        String methodName = keySplit[1];
-
-        char[] METHOD_NAME_REPLACEMENT = {'_'};
-        methodName = WordUtils.capitalizeFully(methodName, METHOD_NAME_REPLACEMENT).replaceAll("_", "");
-        methodName = methodName.substring(0, 1).toLowerCase() + methodName.substring(1);
-        try {
-            Object objectWithMethodToInvoke = MethodUtils.invokeMethod(this, object, null);
-            return (String) MethodUtils.invokeMethod(objectWithMethodToInvoke, methodName, null);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+  public String resolve(String key) {
+    String[] keySplit = key.split("\\.", 2);
+    String object = keySplit[0].toLowerCase();
+    String methodName = keySplit[1];
+    char[] METHOD_NAME_REPLACEMENT = { '_' };
+    methodName = WordUtils.capitalizeFully(methodName, METHOD_NAME_REPLACEMENT).replaceAll("_", "");
+    methodName = methodName.substring(0, 1).toLowerCase() + methodName.substring(1);
+    try {
+      Object objectWithMethodToInvoke = MethodUtils.invokeMethod(this, object, null);
+      return (String) MethodUtils.invokeMethod(objectWithMethodToInvoke, methodName, null);
+    } catch (Exception e) {
+      throw new RuntimeException(e);
     }
+  }
 }
