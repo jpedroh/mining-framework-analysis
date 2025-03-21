5181286540cf7e995ec1fa24c4af7e01709e6647
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 16:04:20 +0000

    Mining Framework Analysis

--- src/main/java/io/rainfall/reporting/HtmlReporter.java
+++ src/main/java/io/rainfall/reporting/HtmlReporter.java
@@ -1,27 +1,9 @@
-/*
- * Copyright 2014 Aurélien Broszniowski
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package io.rainfall.reporting;
-
 import io.rainfall.Reporter;
 import io.rainfall.statistics.StatisticsHolder;
 import io.rainfall.statistics.StatisticsPeek;
 import io.rainfall.statistics.StatisticsPeekHolder;
 import org.HdrHistogram.Histogram;
-
 import java.io.BufferedOutputStream;
 import java.io.BufferedWriter;
 import java.io.File;
@@ -50,23 +32,30 @@ import java.util.TimeZone;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
-
 /**
  * @author Aurelien Broszniowski
  */
-
 public class HtmlReporter<E extends Enum<E>> extends Reporter<E> {
-
   private String basedir;
+
   private String averageLatencyFile = "averageLatency.csv";
+
   private String tpsFile = "tps.csv";
+
   private String percentilesFile = "total-percentiles.csv";
+
   private String gcFile = "gc.csv";
+
   private String reportFile;
+
   private final File jarFile = new File(getClass().getProtectionDomain().getCodeSource().getLocation().getPath());
+
   private final static String CRLF = System.getProperty("line.separator");
+
   private Calendar calendar = GregorianCalendar.getInstance(TimeZone.getDefault());
+
   private SimpleDateFormat sdf = new SimpleDateFormat("yy-MM-dd HH:mm:ss");
+
   private final GcStatsCollector gcStatsCollector = new GcStatsCollector();
 
   public HtmlReporter() {
@@ -77,15 +66,12 @@ public class HtmlReporter<E extends Enum<E>> extends Reporter<E> {
     try {
       this.basedir = new File(outputPath).getAbsoluteFile().getAbsolutePath();
       this.reportFile = this.basedir + File.separatorChar + "report.html";
-
       deleteDirectory(new File(this.basedir));
-
-      if (jarFile.isFile()) {  // Run with JAR file
+      if (jarFile.isFile()) {
         extractFromJar("/report", this.basedir);
       } else {
         extractFromPath(new File(HtmlReporter.class.getClass().getResource("/report").toURI()), new File(this.basedir));
       }
-
       gcStatsCollector.registerGcEventListeners();
     } catch (URISyntaxException e) {
       throw new RuntimeException("Can not read report template");
@@ -97,50 +83,39 @@ public class HtmlReporter<E extends Enum<E>> extends Reporter<E> {
   private void extractFromPath(final File src, final File dst) throws IOException {
     if (src.isDirectory()) {
       dst.mkdirs();
-
       String files[] = src.list();
-
       for (String file : files) {
         File srcFile = new File(src, file);
         File destFile = new File(dst, file);
         extractFromPath(srcFile, destFile);
       }
-
     } else {
       InputStream in = new FileInputStream(src);
       OutputStream out = new FileOutputStream(dst);
-
       byte[] buffer = new byte[1024];
-
       int length;
       while ((length = in.read(buffer)) > 0) {
         out.write(buffer, 0, length);
       }
-
       in.close();
       out.close();
     }
   }
 
-  @Override
-  public void report(final StatisticsPeekHolder<E> statisticsPeekHolder) {
+  @Override public void report(final StatisticsPeekHolder<E> statisticsPeekHolder) {
     try {
       if (!new File(reportFile).exists()) {
         copyReportTemplate(statisticsPeekHolder);
       }
-
       StatisticsPeek<E> totalStatisticsPeeks = statisticsPeekHolder.getTotalStatisticsPeeks();
-
       Set<String> keys = statisticsPeekHolder.getStatisticsPeeksNames();
-
       for (String key : keys) {
         StatisticsPeek<E> statisticsPeeks = statisticsPeekHolder.getStatisticsPeeks(key);
         logPeriodicStats(key, statisticsPeeks, statisticsPeekHolder.getResultsReported());
       }
-
-      if (totalStatisticsPeeks != null)
+      if (totalStatisticsPeeks != null) {
         logPeriodicStats("total", totalStatisticsPeeks, statisticsPeekHolder.getResultsReported());
-
+      }
     } catch (IOException e) {
       throw new RuntimeException("Can not write report data");
     } catch (URISyntaxException e) {
@@ -148,8 +123,7 @@ public class HtmlReporter<E extends Enum<E>> extends Reporter<E> {
     }
   }
 
-  @Override
-  public void summarize(final StatisticsHolder<E> statisticsHolder) {
+  @Override public void summarize(final StatisticsHolder<E> statisticsHolder) {
     gcStatsCollector.unregisterGcEventListeners();
     StringBuilder sb = new StringBuilder();
     Enum<E>[] results = statisticsHolder.getResultsReported();
@@ -159,7 +133,6 @@ public class HtmlReporter<E extends Enum<E>> extends Reporter<E> {
         try {
           histogram = histogram.copyCorrectedForCoordinatedOmission(1000000L);
         } catch (Throwable t) {
-          // eat it. Sometimes, some places, it throws an exception here.
         }
         String percentilesFilename = this.basedir + File.separatorChar + getPercentilesFilename(result.name());
         PrintStream stream = new PrintStream(new File(percentilesFilename));
@@ -169,73 +142,58 @@ public class HtmlReporter<E extends Enum<E>> extends Reporter<E> {
           e.printStackTrace();
         }
         stream.close();
-
         String mean = "NaN";
         try {
           mean = "" + histogram.getMean();
         } catch (Exception e) {
           e.printStackTrace();
         }
-
         String maxValue = "NaN";
         try {
           maxValue = "" + histogram.getMaxValue();
         } catch (Exception e) {
           e.printStackTrace();
         }
-
-        sb.append("reportPercentiles('")
-            .append(getPercentilesFilename(result.name()).substring(0, getPercentilesFilename(result.name()).length() - 4))
-            .append("', 'Reponse Time percentiles for ").append(result.name())
-            .append("', '" + mean + "', '" + maxValue)
-            .append("');").append(CRLF);
+        sb.append("reportPercentiles(\'").append(getPercentilesFilename(result.name()).substring(0, getPercentilesFilename(result.name()).length() - 4)).append("\', \'Reponse Time percentiles for ").append(result.name()).append("\', \'" + mean + "\', \'" + maxValue).append("\');").append(CRLF);
       }
       substituteInFile(new FileInputStream(new File(reportFile)), reportFile, "//!summary!", sb);
-
-
     } catch (Exception e) {
       throw new RuntimeException("Can not report to Html", e);
     }
-    //TODO :  put onglets
   }
 
   private void logPeriodicStats(String name, StatisticsPeek<E> statisticsPeek, final Enum<E>[] resultsReported) throws IOException {
     String avgFilename = this.basedir + File.separatorChar + getAverageLatencyFilename(name);
     String tpsFilename = this.basedir + File.separatorChar + getTpsFilename(name);
     String gcFilename = this.basedir + File.separatorChar + getGcFilename();
-
     Writer averageLatencyOutput;
     Writer tpsOutput;
     Writer gcOutput;
-
     averageLatencyOutput = new BufferedWriter(new FileWriter(avgFilename, true));
-    if (new File(avgFilename).length() == 0)
+    if (new File(avgFilename).length() == 0) {
       addHeader(averageLatencyOutput, resultsReported);
+    }
     tpsOutput = new BufferedWriter(new FileWriter(tpsFilename, true));
-    if (new File(tpsFilename).length() == 0)
+    if (new File(tpsFilename).length() == 0) {
       addHeader(tpsOutput, resultsReported);
+    }
     gcOutput = new BufferedWriter(new FileWriter(gcFilename, true));
-    if (new File(gcFilename).length() == 0)
+    if (new File(gcFilename).length() == 0) {
       addHeader(gcOutput, GcStatsCollector.GcStats.Header.values());
-
+    }
     String timestamp = formatTimestampInNano(statisticsPeek.getTimestamp());
-
     StringBuilder averageLatencySb = new StringBuilder(timestamp);
     StringBuilder tpsSb = new StringBuilder(timestamp);
-
     for (Enum<E> result : resultsReported) {
-      averageLatencySb.append(",")
-          .append(String.format("%.4f", (statisticsPeek.getPeriodicAverageLatencyInMs(result))));
+      averageLatencySb.append(",").append(String.format("%.4f", (statisticsPeek.getPeriodicAverageLatencyInMs(result))));
       tpsSb.append(",").append(statisticsPeek.getPeriodicTps(result));
     }
     averageLatencyOutput.append(averageLatencySb.toString()).append("\n");
     tpsOutput.append(tpsSb.toString()).append("\n");
-
     List<GcStatsCollector.GcStats> gcStatsList = gcStatsCollector.drain();
     for (GcStatsCollector.GcStats gcStats : gcStatsList) {
       gcOutput.append(toCsv(gcStats)).append("\n");
     }
-
     averageLatencyOutput.close();
     tpsOutput.close();
     gcOutput.close();
@@ -243,11 +201,7 @@ public class HtmlReporter<E extends Enum<E>> extends Reporter<E> {
 
   private String toCsv(GcStatsCollector.GcStats gcStats) {
     long jvmStartTime = ManagementFactory.getRuntimeMXBean().getStartTime();
-    return String.valueOf(formatTimestampInNano(jvmStartTime + gcStats.getStartTimestamp())) + "," +
-        gcStats.getDuration() + "," +
-        gcStats.getAction() + "," +
-        gcStats.getCause() + "," +
-        gcStats.getName();
+    return String.valueOf(formatTimestampInNano(jvmStartTime + gcStats.getStartTimestamp())) + "," + gcStats.getDuration() + "," + gcStats.getAction() + "," + gcStats.getCause() + "," + gcStats.getName();
   }
 
   /**
@@ -260,28 +214,20 @@ public class HtmlReporter<E extends Enum<E>> extends Reporter<E> {
   public void extractFromJar(String sourceDirectory, String writeDirectory) throws IOException {
     final URL dirURL = getClass().getResource(sourceDirectory);
     final String path = sourceDirectory.substring(1);
-
     if ((dirURL != null) && dirURL.getProtocol().equals("jar")) {
-      final JarURLConnection jarConnection = (JarURLConnection)dirURL.openConnection();
+      final JarURLConnection jarConnection = (JarURLConnection) dirURL.openConnection();
       System.out.println("jarConnection is " + jarConnection);
-
       final ZipFile jar = jarConnection.getJarFile();
-
-      final Enumeration<? extends ZipEntry> entries = jar.entries(); // gives ALL entries in jar
-
+      final Enumeration<? extends ZipEntry> entries = jar.entries();
       while (entries.hasMoreElements()) {
         final ZipEntry entry = entries.nextElement();
         final String name = entry.getName();
-        // System.out.println( name );
         if (!name.startsWith(path)) {
-          // entry in wrong subdir -- don't copy
           continue;
         }
         final String entryTail = name.substring(path.length());
-
         final File f = new File(writeDirectory + File.separator + entryTail);
         if (entry.isDirectory()) {
-          // if its a directory, create it -- REVIEW @yzhang and also any intermediate dirs
           final boolean bMade = f.mkdirs();
           System.out.println((bMade ? "  creating " : "  unable to create ") + f.getCanonicalPath());
         } else {
@@ -290,7 +236,6 @@ public class HtmlReporter<E extends Enum<E>> extends Reporter<E> {
           final OutputStream os = new BufferedOutputStream(new FileOutputStream(f));
           final byte buffer[] = new byte[4096];
           int readCount;
-          // write contents of 'is' to 'os'
           while ((readCount = is.read(buffer)) > 0) {
             os.write(buffer, 0, readCount);
           }
@@ -298,12 +243,12 @@ public class HtmlReporter<E extends Enum<E>> extends Reporter<E> {
           is.close();
         }
       }
-
-    } else if (dirURL == null) {
-      throw new IllegalStateException("can't find " + sourceDirectory + " on the classpath");
     } else {
-      // not a "jar" protocol URL
-      throw new IllegalStateException("don't know how to handle extracting from " + dirURL);
+      if (dirURL == null) {
+        throw new IllegalStateException("can\'t find " + sourceDirectory + " on the classpath");
+      } else {
+        throw new IllegalStateException("don\'t know how to handle extracting from " + dirURL);
+      }
     }
   }
 
@@ -323,17 +268,15 @@ public class HtmlReporter<E extends Enum<E>> extends Reporter<E> {
     return this.gcFile;
   }
 
-  private final static int[] illegalChars = { 34, 60, 62, 124, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
-      17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 58, 42, 63, 92, 47, '@', '.', '\'', '"', '!', '#', '$',
-      '%', '^', '&', '*', '(', ')', '\\' };
+  private final static int[] illegalChars = { 34, 60, 62, 124, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 58, 42, 63, 92, 47, '@', '.', '\'', '\"', '!', '#', '$', '%', '^', '&', '*', '(', ')', '\\' };
 
   public String cleanFilename(String filename) {
     Arrays.sort(illegalChars);
     StringBuilder cleanName = new StringBuilder();
     for (int i = 0; i < filename.length(); i++) {
-      int c = (int)filename.charAt(i);
+      int c = (int) filename.charAt(i);
       if (Arrays.binarySearch(illegalChars, c) < 0) {
-        cleanName.append((char)c);
+        cleanName.append((char) c);
       }
     }
     return cleanName.toString();
@@ -341,28 +284,18 @@ public class HtmlReporter<E extends Enum<E>> extends Reporter<E> {
 
   private void copyReportTemplate(final StatisticsPeekHolder<E> peek) throws IOException, URISyntaxException {
     StringBuilder sb = new StringBuilder();
-
     Set<String> names = peek.getStatisticsPeeksNames();
-    // Periodic
     for (String name : names) {
       String tpsFilename = getTpsFilename(name);
-      sb.append("reportTps('").append(tpsFilename.substring(0, tpsFilename.length() - 4))
-          .append("', 'Periodic TPS - ").append(name)
-          .append("');").append(CRLF);
+      sb.append("reportTps(\'").append(tpsFilename.substring(0, tpsFilename.length() - 4)).append("\', \'Periodic TPS - ").append(name).append("\');").append(CRLF);
     }
-    sb.append("reportTps('total-tps', 'Periodic Total TPS');").append(CRLF);
+    sb.append("reportTps(\'total-tps\', \'Periodic Total TPS\');").append(CRLF);
     for (String key : names) {
       String averageLatencyFilename = getAverageLatencyFilename(key);
-      sb.append("reportResponseTime('")
-          .append(averageLatencyFilename.substring(0, averageLatencyFilename.length() - 4))
-          .append("', 'Periodic Reponse Time - ").append(key)
-          .append("');").append(CRLF);
+      sb.append("reportResponseTime(\'").append(averageLatencyFilename.substring(0, averageLatencyFilename.length() - 4)).append("\', \'Periodic Reponse Time - ").append(key).append("\');").append(CRLF);
     }
-    sb.append("reportResponseTime('total-averageLatency', 'Periodic Average Response Time of all entities');")
-        .append(CRLF);
-    sb.append("reportGc('gc', 'GC Time');")
-        .append(CRLF);
-
+    sb.append("reportResponseTime(\'total-averageLatency\', \'Periodic Average Response Time of all entities\');").append(CRLF);
+    sb.append("reportGc(\'gc\', \'GC Time\');").append(CRLF);
     InputStream in = HtmlReporter.class.getClass().getResourceAsStream("/template/Tps-template.html");
     substituteInFile(in, reportFile, "//!report!", sb);
   }
@@ -383,17 +316,14 @@ public class HtmlReporter<E extends Enum<E>> extends Reporter<E> {
       while (scanner.hasNextLine()) {
         fileContents.append(scanner.nextLine()).append(CRLF);
       }
-    } finally {
+    }  finally {
       scanner.close();
     }
     in.close();
-
-    // create template
     byte[] replace = fileContents.toString().replace(marker, sb.toString()).getBytes();
     OutputStream out = new FileOutputStream(outputFile);
     out.write(replace, 0, replace.length);
     out.close();
-
   }
 
   private String formatTimestampInNano(final long timestamp) {
@@ -411,8 +341,9 @@ public class HtmlReporter<E extends Enum<E>> extends Reporter<E> {
   }
 
   private void deleteDirectory(File path) {
-    if (path == null)
+    if (path == null) {
       return;
+    }
     if (path.exists()) {
       for (File f : path.listFiles()) {
         if (f.isDirectory()) {
