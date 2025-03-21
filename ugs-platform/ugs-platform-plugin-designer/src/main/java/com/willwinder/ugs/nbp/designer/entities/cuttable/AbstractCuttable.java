3ecc52bf84ac3f04a5e618dd3599b13bb49514a5
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 19:33:15 +0000

    Mining Framework Analysis

--- ugs-platform/ugs-platform-plugin-designer/src/main/java/com/willwinder/ugs/nbp/designer/entities/cuttable/AbstractCuttable.java
+++ ugs-platform/ugs-platform-plugin-designer/src/main/java/com/willwinder/ugs/nbp/designer/entities/cuttable/AbstractCuttable.java
@@ -100,6 +100,27 @@ public abstract class AbstractCuttable extends AbstractEntity implements Cuttabl
             graphics.draw(getShape());
         }
     }
+    @Override
+    public void render(Graphics2D graphics) {
+        if (getCutType() != CutType.NONE && getCutDepth() == 0) {
+            graphics.setStroke(new BasicStroke(0.4f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER, 1, new float[]{0.8f, 0.8f}, 0));
+            graphics.setColor(Colors.SHAPE_HINT);
+            graphics.draw(getShape());
+        } else if (getCutType() == CutType.POCKET) {
+            graphics.setStroke(new BasicStroke(0.4f));
+            graphics.setColor(getCutColor());
+            graphics.fill(getShape());
+            graphics.draw(getShape());
+        } else if (getCutType() == CutType.INSIDE_PATH || getCutType() == CutType.ON_PATH || getCutType() == CutType.OUTSIDE_PATH) {
+            graphics.setStroke(new BasicStroke(0.4f));
+            graphics.setColor(getCutColor());
+            graphics.draw(getShape());
+        } else {
+            graphics.setStroke(new BasicStroke(0.4f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER, 1, new float[]{0.8f, 0.8f}, 0));
+            graphics.setColor(Colors.SHAPE_OUTLINE);
+            graphics.draw(getShape());
+        }
+    }
 
     private Color getCutColor() {
         int color = Math.max(0, Math.min(255, (int) Math.round(255d * getCutAlpha()) - 25));
