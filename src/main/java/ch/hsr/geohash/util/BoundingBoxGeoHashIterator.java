ed8ef19ff88a05fbfd1f42716af069c8f65a5c20
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 02:44:30 +0000

    Mining Framework Analysis

--- src/main/java/ch/hsr/geohash/util/BoundingBoxGeoHashIterator.java
+++ src/main/java/ch/hsr/geohash/util/BoundingBoxGeoHashIterator.java
@@ -1,54 +1,13 @@
-package ch.hsr.geohash.util;
-
-import ch.hsr.geohash.GeoHash;
-import ch.hsr.geohash.util.TwoGeoHashBoundingBox;
-
-import java.util.Iterator;
-import java.util.NoSuchElementException;
-
-/**
- * Iterate over all of the values within a bounding box at a particular
- * resolution
- */
-public class BoundingBoxGeoHashIterator implements Iterator<GeoHash> {
-	private TwoGeoHashBoundingBox boundingBox;
-	private GeoHash current;
-
-	public BoundingBoxGeoHashIterator(TwoGeoHashBoundingBox bbox) {
-		boundingBox = bbox;
-		current = bbox.getSouthWestCorner();
-	}
-
-	public TwoGeoHashBoundingBox getBoundingBox() {
-		return boundingBox;
-	}
-
-	@Override
-	public boolean hasNext() {
-		return current != null;
-	}
-
-	@Override
-	public GeoHash next() {
-		if (!hasNext()) {
-			throw new NoSuchElementException();
-		}
-
-		GeoHash rv = current;
-		if (rv.equals(boundingBox.getNorthEastCorner())) {
-		    current = null;
-        } else {
-			current = rv.next();
-            while (hasNext() && !boundingBox.getBoundingBox().contains(current.getOriginatingPoint())) {
-                current = current.next();
-            }
-        }
-
-		return rv;
-	}
-
-	@Override
-	public void remove() {
-		throw new UnsupportedOperationException();
-	}
-}
+  package    ch . hsr . geohash . util ;   import   java . util . Iterator ;  import   java . util . NoSuchElementException ;  import    ch . hsr . geohash . GeoHash ;   public class BoundingBoxGeoHashIterator  implements   Iterator  < GeoHash >  {   private TwoGeoHashBoundingBox  boundingBox ;   private GeoHash  current ;   public BoundingBoxGeoHashIterator  (  TwoGeoHashBoundingBox bbox )  {   boundingBox = bbox ;   current =  bbox . getSouthWestCorner  ( ) ; }   public TwoGeoHashBoundingBox getBoundingBox  ( )  {  return boundingBox ; }    @ Override public boolean hasNext  ( )  {  return  
+<<<<<<<
+ current . compareTo  (  boundingBox . getNorthEastCorner  ( ) )
+=======
+current
+>>>>>>>
+ != null ; }    @ Override public GeoHash next  ( )  {  if  (  !  hasNext  ( ) )  {  throw  new NoSuchElementException  ( ) ; }  GeoHash  rv = current ; 
+<<<<<<<
+ while  (   hasNext  ( ) &&  !   boundingBox . getBoundingBox  ( ) . contains  (  current . getOriginatingPoint  ( ) ) )  {   current =  current . next  ( ) ; }
+=======
+ if  (  rv . equals  (  boundingBox . getTopRight  ( ) ) )  {   current = null ; } else  {   current =  rv . next  ( ) ;  while  (   hasNext  ( ) &&  !   boundingBox . getBoundingBox  ( ) . contains  (  current . getPoint  ( ) ) )  {   current =  current . next  ( ) ; } }
+>>>>>>>
+  return rv ; }    @ Override public void remove  ( )  {  throw  new UnsupportedOperationException  ( ) ; } }
