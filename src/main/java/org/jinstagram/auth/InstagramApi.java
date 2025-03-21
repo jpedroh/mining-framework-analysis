3e09a51bdd30b8cd58b52e1afef9f624e66a8b8d
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 21:27:45 +0000

    Mining Framework Analysis

--- src/main/java/org/jinstagram/auth/InstagramApi.java
+++ src/main/java/org/jinstagram/auth/InstagramApi.java
@@ -1,8 +1,9 @@
 package org.jinstagram.auth;
-
+import com.google.gson.JsonObject;
 import static org.jinstagram.http.URLUtils.formURLEncode;
-
+import com.google.gson.JsonParseException;
 import org.apache.commons.lang3.StringUtils;
+import com.google.gson.JsonParser;
 import org.jinstagram.auth.exceptions.OAuthException;
 import org.jinstagram.auth.model.Constants;
 import org.jinstagram.auth.model.OAuthConfig;
@@ -11,59 +12,47 @@ import org.jinstagram.auth.oauth.InstagramService;
 import org.jinstagram.http.Verbs;
 import org.jinstagram.utils.Preconditions;
 
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParseException;
-import com.google.gson.JsonParser;
-
 public class InstagramApi {
-	public String getAccessTokenEndpoint() {
-		return Constants.ACCESS_TOKEN_ENDPOINT;
-	}
+  public String getAccessTokenEndpoint() {
+    return Constants.ACCESS_TOKEN_ENDPOINT;
+  }
 
-	public Verbs getAccessTokenVerb() {
-		return Verbs.POST;
-	}
+  public Verbs getAccessTokenVerb() {
+    return Verbs.POST;
+  }
 
-	public String getAuthorizationUrl(OAuthConfig config) {
-		Preconditions.checkValidUrl(config.getCallback(),
-				"Must provide a valid url as callback. Instagram does not support OOB");
+  public String getAuthorizationUrl(OAuthConfig config) {
+    Preconditions.checkValidUrl(config.getCallback(), "Must provide a valid url as callback. Instagram does not support OOB");
+    if (config.hasScope()) {
+      return String.format(Constants.SCOPED_AUTHORIZE_URL, config.getApiKey(), formURLEncode(config.getCallback()), config.getScope());
+    } else {
+      return String.format(Constants.AUTHORIZE_URL, config.getApiKey(), formURLEncode(config.getCallback()));
+    }
+  }
 
-		// Append scope if present
-		if (config.hasScope()) {
-			return String.format(Constants.SCOPED_AUTHORIZE_URL, config.getApiKey(),
-					formURLEncode(config.getCallback()), config.getScope());
-		} else {
-			return String.format(Constants.AUTHORIZE_URL, config.getApiKey(), formURLEncode(config.getCallback()));
-		}
-	}
+  public AccessTokenExtractor getAccessTokenExtractor() {
+    return new AccessTokenExtractor() {
+      @Override public Token extract(String response) {
+        Preconditions.checkEmptyString(response, "Cannot extract a token from a null or empty String");
+        try {
+          JsonParser parser = new JsonParser();
+          JsonObject obj = parser.parse(response).getAsJsonObject();
+          String token = null;
+          if (obj.has("access_token")) {
+            token = obj.get("access_token").getAsString();
+          }
+          if (StringUtils.isEmpty(token)) {
+            throw new OAuthException("Cannot extract an access token. Response was: " + response);
+          }
+          return new Token(token, "", response);
+        } catch (JsonParseException e) {
+          throw new OAuthException("Cannot extract an access token. Response was: " + response);
+        }
+      }
+    };
+  }
 
-	public AccessTokenExtractor getAccessTokenExtractor() {
-		return new AccessTokenExtractor() {
-			@Override
-			public Token extract(String response) {
-				Preconditions.checkEmptyString(response, "Cannot extract a token from a null or empty String");
-
-				try {
-					JsonParser parser = new JsonParser();
-					JsonObject obj = parser.parse(response).getAsJsonObject();
-					String token = null;
-
-					if(obj.has("access_token")) {
-						token = obj.get("access_token").getAsString();
-					}
-
-					if(StringUtils.isEmpty(token)) {
-						throw new OAuthException("Cannot extract an access token. Response was: " + response);
-					}
-					return new Token(token, "", response);
-				} catch(JsonParseException e) {
-					throw new OAuthException("Cannot extract an access token. Response was: " + response);
-				}
-			}
-		};
-	}
-
-	public InstagramService createService(OAuthConfig config) {
-		return new InstagramService(this, config);
-	}
+  public InstagramService createService(OAuthConfig config) {
+    return new InstagramService(this, config);
+  }
 }
