0ae06606f2354d60e79375e52df267755743e88b
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 11:29:10 +0000

    Mining Framework Analysis

--- bridge-java-core/src/main/java/com/flotype/bridge/Bridge.java
+++ bridge-java-core/src/main/java/com/flotype/bridge/Bridge.java
@@ -1,10 +1,8 @@
 package com.flotype.bridge;
-
 import java.io.IOException;
 import java.lang.reflect.Proxy;
-import java.util.concurrent.Executors;
-
 import org.slf4j.Logger;
+import java.util.concurrent.Executors;
 import org.slf4j.LoggerFactory;
 
 /**
@@ -17,14 +15,19 @@ import org.slf4j.LoggerFactory;
  * @author sridatta
  */
 public class Bridge {
-	BridgeEventHandler eventHandler = null;
-	private static Logger log = LoggerFactory.getLogger(Bridge.class);
-	protected Dispatcher dispatcher = new Dispatcher(this);
-	protected BridgeClient context = null; 
-	boolean ready = false;
-	private Connection connection;
-	
-	/**
+  BridgeEventHandler eventHandler = null;
+
+  private static Logger log = LoggerFactory.getLogger(Bridge.class);
+
+  protected Dispatcher dispatcher = new Dispatcher(this);
+
+  protected BridgeClient context = null;
+
+  boolean ready = false;
+
+  private Connection connection;
+
+  /**
 	 * Default Bridge constructor which uses all default settings. This
 	 * constructor provides all basic setup instructions to connect to the
 	 * Bridge server operated by Flotype. Instead of establishing a direct
@@ -36,17 +39,16 @@ public class Bridge {
 	 * <li>reconnect: true
 	 * </ul>
 	 */
-	public Bridge() {
-		connection = new Connection(this);
-		Executors.newSingleThreadExecutor();
-		this.setRedirector(Utils.DEFAULT_REDIRECTOR);
-		this.setEventHandler(Utils.DEFAULT_EVENT_HANDLER);
-		this.setReconnect(Utils.DEFAULT_RECONNECT);
+  public Bridge() {
+    connection = new Connection(this);
+    Executors.newSingleThreadExecutor();
+    this.setRedirector(Utils.DEFAULT_REDIRECTOR);
+    this.setEventHandler(Utils.DEFAULT_EVENT_HANDLER);
+    this.setReconnect(Utils.DEFAULT_RECONNECT);
+    dispatcher.storeObject("system", new SystemService(this, dispatcher));
+  }
 
-		dispatcher.storeObject("system", new SystemService(this, dispatcher));
-	}
-
-	/**
+  /**
 	 * Bridge constructor which allows all options to be specified manually This
 	 * constructor calls the default constructor and then calls setter methods
 	 * to change from the defaults.
@@ -64,17 +66,16 @@ public class Bridge {
 	 *            Boolean specifying whether or not to reconnect if connection
 	 *            is disrupted
 	 */
-	public Bridge(String host, Integer port, String apiKey,
-			BridgeEventHandler eventHandler, boolean reconnect) {
-		this();
-		this.setHost(host);
-		this.setPort(port);
-		this.setApiKey(apiKey);
-		this.setEventHandler(eventHandler);
-		this.setReconnect(reconnect);
-	}
+  public Bridge(String host, Integer port, String apiKey, BridgeEventHandler eventHandler, boolean reconnect) {
+    this();
+    this.setHost(host);
+    this.setPort(port);
+    this.setApiKey(apiKey);
+    this.setEventHandler(eventHandler);
+    this.setReconnect(reconnect);
+  }
 
-	/**
+  /**
 	 * Bridge constructor to be used for a redirected connection.
 	 * 
 	 * @param redirectorUrl
@@ -88,16 +89,15 @@ public class Bridge {
 	 *            Boolean specifying whether or not to reconnect if connection
 	 *            is disrupted
 	 */
-	public Bridge(String redirectorUrl, String apiKey,
-			BridgeEventHandler eventHandler, boolean reconnect) {
-		this();
-		this.setRedirector(redirectorUrl);
-		this.setApiKey(apiKey);
-		this.setEventHandler(eventHandler);
-		this.setReconnect(reconnect);
-	}
+  public Bridge(String redirectorUrl, String apiKey, BridgeEventHandler eventHandler, boolean reconnect) {
+    this();
+    this.setRedirector(redirectorUrl);
+    this.setApiKey(apiKey);
+    this.setEventHandler(eventHandler);
+    this.setReconnect(reconnect);
+  }
 
-	/**
+  /**
 	 * Establishes the connection and handshake process. If a redirected
 	 * connection is specified, the client will make a synchronous HTTP request
 	 * to the redirector server and then start a TCP socket with the host and
@@ -107,17 +107,17 @@ public class Bridge {
 	 * @return
 	 * @throws IOException
 	 */
-	public boolean connect() throws IOException {
-		this.connection.start();
-		return true;
-	}
+  public boolean connect() throws IOException {
+    this.connection.start();
+    return true;
+  }
 
-	protected void send(final Reference destination, final Object[] args) {
-		String msg = JSONCodec.createSEND(this, destination, args);
-		connection.send(msg);
-	}
+  protected void send(final Reference destination, final Object[] args) {
+    String msg = JSONCodec.createSEND(this, destination, args);
+    connection.send(msg);
+  }
 
-	/**
+  /**
 	 * Makes an instance of local or remote Bridge object available to remote
 	 * Bridge clients with the given name. If the object is a locally created
 	 * object, it will be stored in this Bridge instance's object table and this
@@ -129,12 +129,12 @@ public class Bridge {
 	 *            The name of the service to be exposed to other Bridge clients
 	 * @param bridgeObject
 	 */
-	public void publishService(String name, BridgeObjectBase bridgeObject) {
-		BridgeObject callback = null;
-		publishService(name, bridgeObject, callback);
-	}
+  public void publishService(String name, BridgeObjectBase bridgeObject) {
+    BridgeObject callback = null;
+    publishService(name, bridgeObject, callback);
+  }
 
-	/**
+  /**
 	 * Makes an instance of local or remote Bridge object available to remote
 	 * Bridge clients with the given name. Behaves identically to
 	 * {@link #publishService(String, BridgeObjectBase)} but takes a local
@@ -147,28 +147,27 @@ public class Bridge {
 	 * @param callback
 	 *            A local object that has a method named callback
 	 */
-	public void publishService(String name, BridgeObjectBase bridgeObject,
-			BridgeObjectBase callback) {
-		if (name.equals("system")) {
-			log.error("Invalid service name {}", name);
-			return;
-		}
+  public void publishService(String name, BridgeObjectBase bridgeObject, BridgeObjectBase callback) {
+    if (name.equals("system")) {
+      log.error("Invalid service name {}", name);
+      return;
+    }
+    if (bridgeObject instanceof BridgeObject) {
+      dispatcher.storeObject(name, bridgeObject);
+    }
+    Reference callbackRef = null;
+    if (callback instanceof BridgeObject) {
+      callbackRef = dispatcher.storeRandomObject(callback);
+    } else {
+      if (callback instanceof BridgeRemoteObject) {
+        callbackRef = (Reference) Proxy.getInvocationHandler(callback);
+      }
+    }
+    String msg = JSONCodec.createJWP(this, name, callbackRef);
+    this.connection.send(msg);
+  }
 
-		if (bridgeObject instanceof BridgeObject) {
-			dispatcher.storeObject(name, bridgeObject);
-		}
-
-		Reference callbackRef = null;
-		if (callback instanceof BridgeObject) {
-			callbackRef = dispatcher.storeRandomObject(callback);
-		} else if (callback instanceof BridgeRemoteObject) {
-			callbackRef = (Reference) Proxy.getInvocationHandler(callback);
-		}
-		String msg = JSONCodec.createJWP(this, name, callbackRef);
-		this.connection.send(msg);
-	}
-
-	/**
+  /**
 	 * Creates a proxy object to a service published by a remote Bridge client.
 	 * 
 	 * @param serviceName
@@ -177,13 +176,12 @@ public class Bridge {
 	 * @return A proxy object that points to `serviceName` and has methods
 	 *         defined in the interface `serviceInterface`
 	 */
-	public <T> T getService(String serviceName, Class<T> serviceInterface) {
-		Reference result = Reference.createServiceReference(this, serviceName,
-				Utils.getMethods(serviceInterface));
-		return Utils.createProxy(result, serviceInterface);
-	}
+  public <T extends java.lang.Object> T getService(String serviceName, Class<T> serviceInterface) {
+    Reference result = Reference.createServiceReference(this, serviceName, Utils.getMethods(serviceInterface));
+    return Utils.createProxy(result, serviceInterface);
+  }
 
-	/**
+  /**
 	 * Creates a proxy object to a channel of remote Bridge clients.
 	 * 
 	 * @param channelName
@@ -192,27 +190,25 @@ public class Bridge {
 	 * @return A proxy object that points to `channelName` and has methods
 	 *         defined in the interface `channelInterface`
 	 */
-	public <T> T getChannel(String channelName, Class<T> channelInterface) {
-		String msg = JSONCodec.createGETCHANNEL(this, channelName);
-		this.connection.send(msg);
+  public <T extends java.lang.Object> T getChannel(String channelName, Class<T> channelInterface) {
+    String msg = JSONCodec.createGETCHANNEL(this, channelName);
+    this.connection.send(msg);
+    Reference result = Reference.createChannelReference(this, channelName, Utils.getMethods(channelInterface));
+    return Utils.createProxy(result, channelInterface);
+  }
 
-		Reference result = Reference.createChannelReference(this, channelName,
-				Utils.getMethods(channelInterface));
-		return Utils.createProxy(result, channelInterface);
-	}
-
-	/**
+  /**
 	 * Joins this Bridge client to a channel whose messages will be handled by
 	 * the given handler.
 	 * 
 	 * @param channelName
 	 * @param handler
 	 */
-	public void joinChannel(String channelName, BridgeObject handler) {
-		joinChannel(channelName, handler, null);
-	}
+  public void joinChannel(String channelName, BridgeObject handler) {
+    joinChannel(channelName, handler, null);
+  }
 
-	/**
+  /**
 	 * Joins this Bridge client to a channel whose messages will be handled by
 	 * the given handler.
 	 * 
@@ -221,109 +217,106 @@ public class Bridge {
 	 * @param callback
 	 *            A callback that will be called upon joining the channel
 	 */
-	public void joinChannel(String channelName, BridgeObjectBase handler,
-			BridgeObjectBase callback) {
-		Reference handlerRef = null;
-		if (handler instanceof BridgeObject) {
-			handlerRef = dispatcher.storeRandomObject(handler);
-		} else if (handler instanceof BridgeRemoteObject) {
-			handlerRef = (Reference) Proxy.getInvocationHandler(handler);
-		}
-		Reference callbackRef = null;
-		if (handler instanceof BridgeObject) {
-			callbackRef = dispatcher.storeRandomObject(callback);
-		} else if (callback instanceof BridgeRemoteObject) {
-			callbackRef = (Reference) Proxy.getInvocationHandler(callback);
-		}
-		String msg = JSONCodec.createJC(this, channelName, handlerRef,
-				callbackRef);
-		this.connection.send(msg);
-	}
+  public void joinChannel(String channelName, BridgeObjectBase handler, BridgeObjectBase callback) {
+    Reference handlerRef = null;
+    if (handler instanceof BridgeObject) {
+      handlerRef = dispatcher.storeRandomObject(handler);
+    } else {
+      if (handler instanceof BridgeRemoteObject) {
+        handlerRef = (Reference) Proxy.getInvocationHandler(handler);
+      }
+    }
+    Reference callbackRef = null;
+    if (handler instanceof BridgeObject) {
+      callbackRef = dispatcher.storeRandomObject(callback);
+    } else {
+      if (callback instanceof BridgeRemoteObject) {
+        callbackRef = (Reference) Proxy.getInvocationHandler(callback);
+      }
+    }
+    String msg = JSONCodec.createJC(this, channelName, handlerRef, callbackRef);
+    this.connection.send(msg);
+  }
 
-	/**
+  /**
 	 * Leaves the channel and removes the handler
 	 * 
 	 * @param name
 	 * @param handler
 	 */
-	public void leaveChannel(String name, BridgeObject handler) {
-		leaveChannel(name, handler, null);
-	}
+  public void leaveChannel(String name, BridgeObject handler) {
+    leaveChannel(name, handler, null);
+  }
 
-	/**
+  /**
 	 * Leaves the channel and removes the handler
 	 * 
 	 * @param name
 	 * @param handler
 	 */
-	public void leaveChannel(String name, BridgeObject handler,
-			BridgeObject callback) {
-		Reference handlerRef = dispatcher.storeObject(name, handler);
-		Reference callbackRef = dispatcher.storeRandomObject(callback);
-		String msg = JSONCodec.createLEAVECHANNEL(this, name, handlerRef,
-				callbackRef);
-		this.connection.send(msg);
-	}
-	
-	public BridgeClient getContext() {
-		return this.context;
-	}
+  public void leaveChannel(String name, BridgeObject handler, BridgeObject callback) {
+    Reference handlerRef = dispatcher.storeObject(name, handler);
+    Reference callbackRef = dispatcher.storeRandomObject(callback);
+    String msg = JSONCodec.createLEAVECHANNEL(this, name, handlerRef, callbackRef);
+    this.connection.send(msg);
+  }
 
-	public Bridge setApiKey(String apiKey) {
-		this.connection.setApiKey(apiKey);
-		return this;
-	}
+  public BridgeClient getContext() {
+    return this.context;
+  }
 
-	public Bridge setRedirector(String redirectorUrl) {
-		this.connection.setRedirector(redirectorUrl);
-		return this;
-	}
-	
-	public Bridge setSecure(boolean secure) {
-		this.connection.setSecure(secure);
-		return this;
-	}
+  public Bridge setApiKey(String apiKey) {
+    this.connection.setApiKey(apiKey);
+    return this;
+  }
 
-	public Bridge setHost(String host) {
-		this.connection.setHost(host);
-		return this;
-	}
+  public Bridge setRedirector(String redirectorUrl) {
+    this.connection.setRedirector(redirectorUrl);
+    return this;
+  }
 
-	public Bridge setPort(Integer port) {
-		this.connection.setPort(port);
-		return this;
-	}
+  public Bridge setSecure(boolean secure) {
+    this.connection.setSecure(secure);
+    return this;
+  }
 
-	public Bridge setEventHandler(BridgeEventHandler eventHandler) {
-		this.eventHandler = eventHandler;
-		return this;
-	}
+  public Bridge setHost(String host) {
+    this.connection.setHost(host);
+    return this;
+  }
 
-	public Bridge setReconnect(boolean reconnect) {
-		return this;
-	}
+  public Bridge setPort(Integer port) {
+    this.connection.setPort(port);
+    return this;
+  }
 
-	protected void onDisconnect() {
-		this.ready = false;
-		//pool.shutdownNow(); // Cancel currently executing tasks
-	}
+  public Bridge setEventHandler(BridgeEventHandler eventHandler) {
+    this.eventHandler = eventHandler;
+    return this;
+  }
 
-	protected void onReady() {
-		this.ready = true;
-		this.eventHandler.onReady();
-	}
+  public Bridge setReconnect(boolean reconnect) {
+    return this;
+  }
 
-	protected void onReconnect() {
-		this.ready = true;
-		this.eventHandler.onReconnect();
-	}
+  protected void onDisconnect() {
+    this.ready = false;
+  }
 
-	protected void onConnected() {
+  protected void onReady() {
+    this.ready = true;
+    this.eventHandler.onReady();
+  }
 
-	}
+  protected void onReconnect() {
+    this.ready = true;
+    this.eventHandler.onReconnect();
+  }
 
-	protected String getClientId() {
-		return connection.clientId;
-	}
+  protected void onConnected() {
+  }
 
+  protected String getClientId() {
+    return connection.clientId;
+  }
 }
