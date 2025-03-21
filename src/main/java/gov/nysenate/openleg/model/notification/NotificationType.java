99a99eeb60a19d7afb5d1420415e15fb0b8df2c0
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 09:41:54 +0000

    Mining Framework Analysis

--- src/main/java/gov/nysenate/openleg/model/notification/NotificationType.java
+++ src/main/java/gov/nysenate/openleg/model/notification/NotificationType.java
@@ -18,6 +18,32 @@ public enum NotificationType {
     PROCESS_WARNING         (WARNING),
     SCRAPING_EXCEPTION      (WARNING),
     LRS_OUTAGE              (WARNING),
+<<<<<<< /usr/src/app/output/nysenate/openlegislation/2438250a01426fe434525552464119c5358a8384/src/main/java/gov/nysenate/openleg/model/notification/NotificationType.java/left.java
+    UNVERIFIED_MEMBER       (ALL),
+    SPOTCHECK               (ALL),
+    SPOTCHECK_MISMATCH      (SPOTCHECK),
+    SPOTCHECK_ALL_CLEAR     (SPOTCHECK),
+    DAYBREAK_SPOTCHECK      (SPOTCHECK_MISMATCH),
+    CALENDAR_SPOTCHECK      (SPOTCHECK_MISMATCH),
+    AGENDA_SPOTCHECK        (SPOTCHECK_MISMATCH),
+    BILL_TEXT_SPOTCHECK     (SPOTCHECK_MISMATCH),
+    SENSITE_BILL_SPOTCHECK  (SPOTCHECK_MISMATCH),
+    SENSITE_CALENDAR_SPOTCHECK (SPOTCHECK_MISMATCH),
+    SENSITE_AGENDA_SPOTCHECK (SPOTCHECK_MISMATCH),
+    OPENLEG_SPOTCHECK  (SPOTCHECK_MISMATCH),
+||||||| /usr/src/app/output/nysenate/openlegislation/2438250a01426fe434525552464119c5358a8384/src/main/java/gov/nysenate/openleg/model/notification/NotificationType.java/base.java
+    UNVERIFIED_MEMBER       (ALL),
+    SPOTCHECK               (ALL),
+    SPOTCHECK_MISMATCH      (SPOTCHECK),
+    SPOTCHECK_ALL_CLEAR     (SPOTCHECK),
+    DAYBREAK_SPOTCHECK      (SPOTCHECK_MISMATCH),
+    CALENDAR_SPOTCHECK      (SPOTCHECK_MISMATCH),
+    AGENDA_SPOTCHECK        (SPOTCHECK_MISMATCH),
+    BILL_TEXT_SPOTCHECK     (SPOTCHECK_MISMATCH),
+    SENSITE_BILL_SPOTCHECK  (SPOTCHECK_MISMATCH),
+    SENSITE_CALENDAR_SPOTCHECK (SPOTCHECK_MISMATCH),
+    SENSITE_AGENDA_SPOTCHECK (SPOTCHECK_MISMATCH),
+=======
     UNVERIFIED_MEMBER       (WARNING),
     SPOTCHECK               (WARNING),
     DAYBREAK_SPOTCHECK      (SPOTCHECK),
@@ -27,7 +53,7 @@ public enum NotificationType {
     SENSITE_BILL_SPOTCHECK  (SPOTCHECK),
     SENSITE_CALENDAR_SPOTCHECK (SPOTCHECK),
     SENSITE_AGENDA_SPOTCHECK (SPOTCHECK),
-    OPENLEG_SPOTCHECK  (SPOTCHECK),
+>>>>>>> /usr/src/app/output/nysenate/openlegislation/2438250a01426fe434525552464119c5358a8384/src/main/java/gov/nysenate/openleg/model/notification/NotificationType.java/right.java
     NEW_API_KEY             (ALL),
     ;
 
