88c00a2efdbad6a18f2649240cf8c5901ffa7786
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 21:38:02 +0000

    Mining Framework Analysis

--- src/main/java/com/urbanairship/datacube/WriteBuilder.java
+++ src/main/java/com/urbanairship/datacube/WriteBuilder.java
@@ -1,46 +1,37 @@
-/*
-Copyright 2012 Urban Airship and Contributors
-*/
-
 package com.urbanairship.datacube;
-
+import java.util.Map;
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Multimap;
 import com.google.common.collect.SetMultimap;
 
-import java.util.Map;
-
 public class WriteBuilder {
-    private final Map<Dimension<?>, SetMultimap<BucketType, byte[]>> buckets;
+  private final Map<Dimension<?>, SetMultimap<BucketType, byte[]>> buckets;
 
-    public WriteBuilder() {
-        buckets = Maps.newHashMap();
+  public WriteBuilder() {
+    buckets = Maps.newHashMap();
+  }
+
+  public <O extends java.lang.Object> WriteBuilder at(Dimension<O> dimension, O coord) {
+    Bucketer<O> bucketer = dimension.getBucketer();
+    SetMultimap<BucketType, CSerializable> bucketsAndCoords = bucketer.bucketForWrite(coord);
+    buckets.put(dimension, serializeCoords(bucketsAndCoords));
+    return this;
+  }
+
+  Map<Dimension<?>, SetMultimap<BucketType, byte[]>> getBuckets() {
+    return buckets;
+  }
+
+  private static SetMultimap<BucketType, byte[]> serializeCoords(Multimap<BucketType, CSerializable> inputMap) {
+    SetMultimap<BucketType, byte[]> outputMap = HashMultimap.create();
+    for (Map.Entry<BucketType, CSerializable> e : inputMap.entries()) {
+      outputMap.put(e.getKey(), e.getValue().serialize());
     }
+    return outputMap;
+  }
 
-    public <O> WriteBuilder at(Dimension<O> dimension, O coord) {
-        Bucketer<O> bucketer = dimension.getBucketer();
-
-        SetMultimap<BucketType, CSerializable> bucketsAndCoords = bucketer.bucketForWrite(coord);
-        buckets.put(dimension, serializeCoords(bucketsAndCoords));
-
-        return this;
-    }
-
-    Map<Dimension<?>, SetMultimap<BucketType, byte[]>> getBuckets() {
-        return buckets;
-    }
-
-    private static SetMultimap<BucketType, byte[]> serializeCoords(
-            Multimap<BucketType, CSerializable> inputMap) {
-        SetMultimap<BucketType, byte[]> outputMap = HashMultimap.create();
-        for (Map.Entry<BucketType, CSerializable> e : inputMap.entries()) {
-            outputMap.put(e.getKey(), e.getValue().serialize());
-        }
-        return outputMap;
-    }
-
-    public String toString() {
-        return buckets.toString();
-    }
+  public String toString() {
+    return buckets.toString();
+  }
 }
