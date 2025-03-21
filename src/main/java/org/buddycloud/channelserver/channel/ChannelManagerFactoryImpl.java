80f22c87d8a30d4df9485a200f8f56ca53dae1de
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 09:04:56 +0000

    Mining Framework Analysis

--- src/main/java/org/buddycloud/channelserver/channel/ChannelManagerFactoryImpl.java
+++ src/main/java/org/buddycloud/channelserver/channel/ChannelManagerFactoryImpl.java
@@ -1,18 +1,1 @@
-package org.buddycloud.channelserver.channel;
-
-import org.buddycloud.channelserver.db.NodeStoreFactory;
-
-public class ChannelManagerFactoryImpl implements ChannelManagerFactory {
-
-    private final NodeStoreFactory nodeStoreFactory;
-
-    public ChannelManagerFactoryImpl(final NodeStoreFactory nodeStoreFactory) {
-        this.nodeStoreFactory = nodeStoreFactory;
-    }
-
-    @Override
-    public ChannelManager create() {
-        return new ChannelManagerImpl(nodeStoreFactory.create());
-    }
-
-}
+  package    org . buddycloud . channelserver . channel ;   import     org . buddycloud . channelserver . db . NodeStoreFactory ;   public class ChannelManagerFactoryImpl  implements  ChannelManagerFactory  {   private final NodeStoreFactory  nodeStoreFactory ;   public ChannelManagerFactoryImpl  (   final NodeStoreFactory nodeStoreFactory )  {    this . nodeStoreFactory = nodeStoreFactory ; }    @ Override public ChannelManager create  ( )  {  return  new ChannelManagerImpl  (  nodeStoreFactory . create  ( ) ) ; } }
