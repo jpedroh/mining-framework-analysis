366deab2e970a37acdb621f670630e0fa10b41c0
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 12:18:51 +0000

    Mining Framework Analysis

--- src/main/java/com/chargebee/models/Transaction.java
+++ src/main/java/com/chargebee/models/Transaction.java
@@ -1,5 +1,4 @@
 package com.chargebee.models;
-
 import com.chargebee.*;
 import com.chargebee.internal.*;
 import com.chargebee.internal.HttpUtil.Method;
@@ -10,114 +9,100 @@ import java.sql.Timestamp;
 import java.util.*;
 
 public class Transaction extends Resource<Transaction> {
+  public enum Type {
+    AUTHORIZE,
+    PAYMENT,
+    REFUND,
+    _UNKNOWN
+  }
 
-    public enum Type {
-        AUTHORIZE,
-        PAYMENT,
-        REFUND,
-        _UNKNOWN; /*Indicates unexpected value for this enum. You can get this when there is a
-        java-client version incompatibility. We suggest you to upgrade to the latest version */
-    }
+  public enum Status {
+    SUCCESS,
+    VOIDED,
+    FAILURE,
+    _UNKNOWN
+  }
 
-    public enum Status {
-        SUCCESS,
-        VOIDED,
-        FAILURE,
-        _UNKNOWN; /*Indicates unexpected value for this enum. You can get this when there is a
-        java-client version incompatibility. We suggest you to upgrade to the latest version */
-    }
+  public Transaction(String jsonStr) {
+    super(jsonStr);
+  }
 
-    //Constructors
-    //============
+  public Transaction(JSONObject jsonObj) {
+    super(jsonObj);
+  }
 
-    public Transaction(String jsonStr) {
-        super(jsonStr);
-    }
+  public String id() {
+    return reqString("id");
+  }
 
-    public Transaction(JSONObject jsonObj) {
-        super(jsonObj);
-    }
+  public String subscriptionId() {
+    return reqString("subscription_id");
+  }
 
-    // Fields
-    //=======
+  public Gateway gateway() {
+    return reqEnum("gateway", Gateway.class);
+  }
 
-    public String id() {
-        return reqString("id");
-    }
+  public String invoiceId() {
+    return reqString("invoice_id");
+  }
 
-    public String subscriptionId() {
-        return reqString("subscription_id");
-    }
+  public Type type() {
+    return reqEnum("type", Type.class);
+  }
 
-    public Gateway gateway() {
-        return reqEnum("gateway", Gateway.class);
-    }
+  public Timestamp date() {
+    return optTimestamp("date");
+  }
 
-    public String invoiceId() {
-        return reqString("invoice_id");
-    }
+  public Integer amount() {
+    return optInteger("amount");
+  }
 
-    public Type type() {
-        return reqEnum("type", Type.class);
-    }
+  public String idAtGateway() {
+    return optString("id_at_gateway");
+  }
 
-    public Timestamp date() {
-        return optTimestamp("date");
-    }
+  public String maskedCardNumber() {
+    return reqString("masked_card_number");
+  }
 
-    public Integer amount() {
-        return optInteger("amount");
-    }
+  public String errorCode() {
+    return optString("error_code");
+  }
 
-    public String idAtGateway() {
-        return optString("id_at_gateway");
-    }
+  public String errorText() {
+    return optString("error_text");
+  }
 
-    public String maskedCardNumber() {
-        return reqString("masked_card_number");
-    }
+  public Long refundedTxId() {
+    return optLong("refunded_tx_id");
+  }
 
-    public String errorCode() {
-        return optString("error_code");
-    }
+  public String refundMemo() {
+    return optString("refund_memo");
+  }
 
-    public String errorText() {
-        return optString("error_text");
-    }
+  public Timestamp voidedAt() {
+    return optTimestamp("voided_at");
+  }
 
-    public Long refundedTxId() {
-        return optLong("refunded_tx_id");
-    }
+  public Status status() {
+    return reqEnum("status", Status.class);
+  }
 
-    public String refundMemo() {
-        return optString("refund_memo");
-    }
+  public static ListRequest list() throws IOException {
+    String url = url("transactions");
+    return new ListRequest(url);
+  }
 
-    public Timestamp voidedAt() {
-        return optTimestamp("voided_at");
-    }
+  public static Request retrieve(String id) throws IOException {
+    String url = url("transactions", nullCheck(id));
+    return new Request(Method.GET, url);
+  }
 
-    public Status status() {
-        return reqEnum("status", Status.class);
-    }
-
-    // Operations
-    //===========
-
-    public static ListRequest list() throws IOException {
-        String url = url("transactions");
-        return new ListRequest(url);
-    }
-
-    public static Request retrieve(String id) throws IOException {
-        String url = url("transactions", nullCheck(id));
-        return new Request(Method.GET, url);
-    }
-
-    public static ListRequest transactionsForSubscription(String id) throws IOException {
-        String url = url("subscriptions", nullCheck(id), "transactions");
-        return new ListRequest(url);
-    }
-
-
+  public static ListRequest transactionsForSubscription(String id) throws IOException {
+    String url = url("subscriptions", nullCheck(id), "transactions");
+    return new ListRequest(url);
+  }
 }
