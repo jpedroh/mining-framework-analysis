a9d5751a961e4db787201415bb452c679356335c
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 12:12:46 +0000

    Mining Framework Analysis

--- src/main/java/com/googlecode/javaewah/benchmark/ClusteredDataGenerator.java
+++ src/main/java/com/googlecode/javaewah/benchmark/ClusteredDataGenerator.java
@@ -8,10 +8,20 @@ package com.googlecode.javaewah.benchmark;
 
 
 /**
+<<<<<<< /usr/src/app/output/lemire/javaewah/6107a7a096e94d19d8a00bfcb043a62e8a237d4b/src/main/java/com/googlecode/javaewah/benchmark/ClusteredDataGenerator.java/left.java
  * This class will generate lists of random integers with a "clustered" distribution.  
  * Reference:
  * Anh VN, Moffat A. Index compression using 64-bit words. Software: Practice and Experience 2010; 40(2):131-147.  
  * 
+||||||| /usr/src/app/output/lemire/javaewah/6107a7a096e94d19d8a00bfcb043a62e8a237d4b/src/main/java/com/googlecode/javaewah/benchmark/ClusteredDataGenerator.java/base.java
+ * This class will generate uniformly distributed lists of random integers.
+ * 
+=======
+ * This class will generate lists of random integers with a "clustered" distribution.
+ * Reference:
+ * Anh VN, Moffat A. Index compression using 64-bit words. Software: Practice and Experience 2010; 40(2):131-147. 
+ *
+>>>>>>> /usr/src/app/output/lemire/javaewah/6107a7a096e94d19d8a00bfcb043a62e8a237d4b/src/main/java/com/googlecode/javaewah/benchmark/ClusteredDataGenerator.java/right.java
  * @author Daniel Lemire
  */
 public class ClusteredDataGenerator {
