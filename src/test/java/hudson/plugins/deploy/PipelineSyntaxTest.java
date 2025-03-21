7136172baf1bf8f4d58e03c267c5a22b2f0a57e6
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 11:29:48 +0000

    Mining Framework Analysis

--- src/test/java/hudson/plugins/deploy/PipelineSyntaxTest.java
+++ src/test/java/hudson/plugins/deploy/PipelineSyntaxTest.java
@@ -1,5 +1,4 @@
 package hudson.plugins.deploy;
-
 import hudson.model.Result;
 import hudson.plugins.deploy.tomcat.Tomcat8xAdapter;
 import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
@@ -10,141 +9,80 @@ import org.jenkinsci.plugins.workflow.steps.CoreStep;
 import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
-
 import java.util.Collections;
 
 /**
- * Tests pipeline compatibility. Since there is no Builder that sets the build status all of these tests
+ * Tests pipeline compatibility. Since there ia no Builder that sets the build status all of these tests
  * will ultimately result in a no-op.
  */
 public class PipelineSyntaxTest {
+  @Rule public JenkinsRule j = new JenkinsRule();
 
-    @Rule
-    public JenkinsRule j = new JenkinsRule();
+  private String getFullScript(String func) {
+    return "node {\n" + "writeFile(file: \'readme.txt\', text: \'this creates a workspace if one doesnt already exist\')\n" + func + "}";
+  }
 
-    private String getFullScript (String func) {
-        return "node {\n" +
-                    "writeFile(file: 'readme.txt', text: 'this creates a workspace if one doesnt already exist')\n" +
-                    func +
-                "}";
-    }
 
-    /**
-    @Test
-    public void testNoAdapterDeploy() throws Exception {
-        WorkflowJob p = j.getInstance().createProject(WorkflowJob.class, "DryRunTest");
-        p.setDefinition(new CpsFlowDefinition(
-                getFullScript("deploy(war: 'target/app.war', contextPath: 'app', onFailure: false)"),
-                false));
-        WorkflowRun r = p.scheduleBuild2(0).get();
-        // we expect a failed build status because there are no WAR files to deploy
-        j.assertBuildStatus(Result.FAILURE, r);
-        j.assertLogContains("No wars found. Deploy aborted.", r);
-    }
+<<<<<<< Unknown file: This is a bug in JDime.
+=======
+  @Test public void testNoAdapterDeploy() throws Exception {
+    WorkflowJob p = j.getInstance().createProject(WorkflowJob.class, "DryRunTest");
+    p.setDefinition(new CpsFlowDefinition(getFullScript("deploy(war: \'target/app.war\', contextPath: \'app\', onFailure: false)"), false));
+    WorkflowRun r = p.scheduleBuild2(0).get();
+    j.assertBuildStatus(Result.FAILURE, r);
+    j.assertLogContains("No wars found. Deploy aborted.", r);
+  }
+>>>>>>> /usr/src/app/output/jenkinsci/deploy-plugin/c58791723cc93e49d7430e51478ccd3d5fab8637/src/test/java/hudson/plugins/deploy/PipelineSyntaxTest.java/right.java
 
-    @Test
-    public void testMockAdapterDeploy() throws Exception {
-        WorkflowJob p = j.getInstance().createProject(WorkflowJob.class, "MockTest");
-        p.setDefinition(new CpsFlowDefinition(
-                getFullScript("deploy(adapters: [workflowAdapter()], war: 'target/app.war', contextPath: 'app')"),
-                false));
-        WorkflowRun r = p.scheduleBuild2(0).get();
-        // we expect a failed build status because there are no WAR files to deploy
-        j.assertBuildStatus(Result.FAILURE, r);
-        j.assertLogContains("No wars found. Deploy aborted.", r);
-    }
 
-    @Test
-    public void testMockAdaptersDeploy() throws Exception {
-        WorkflowJob p = j.getInstance().createProject(WorkflowJob.class, "MockTest");
-        p.setDefinition(new CpsFlowDefinition(
-                getFullScript("deploy(adapters: [workflowAdapter(), workflowAdapter(), workflowAdapter()], war: 'target/app.war', contextPath: 'app')"),
-                false));
-        WorkflowRun r = p.scheduleBuild2(0).get();
-        // we expect a failed build status because there are no WAR files to deploy
-        j.assertBuildStatus(Result.FAILURE, r);
-        j.assertLogContains("No wars found. Deploy aborted.", r);
-    }
 
-    @Test
-    public void testGlassFishAdapter() throws Exception {
-        WorkflowJob p = j.getInstance().createProject(WorkflowJob.class, "GlassfishTest");
-        p.setDefinition(new CpsFlowDefinition(
-                getFullScript(
-                "def gf2 = glassfish2( " +
-                    "home: 'FAKE', " +
-                    "credentialsId: 'FAKE'," +
-                    "adminPort: '1234', " +
-                    "hostname: 'localhost') \n" +
-                "def gf3 = glassfish3( " +
-                    "home: 'FAKE', " +
-                    "credentialsId: 'FAKE'," +
-                    "adminPort: '1234', " +
-                    "hostname: 'localhost') \n" +
-                "deploy(adapters: [gf2, gf3], war: 'target/app.war', contextPath: 'app')"),
-                false));
+<<<<<<< Unknown file: This is a bug in JDime.
+=======
+  @Test public void testMockAdapterDeploy() throws Exception {
+    WorkflowJob p = j.getInstance().createProject(WorkflowJob.class, "MockTest");
+    p.setDefinition(new CpsFlowDefinition(getFullScript("deploy(adapters: [workflowAdapter()], war: \'target/app.war\', contextPath: \'app\')"), false));
+    WorkflowRun r = p.scheduleBuild2(0).get();
+    j.assertBuildStatus(Result.FAILURE, r);
+    j.assertLogContains("No wars found. Deploy aborted.", r);
+  }
+>>>>>>> /usr/src/app/output/jenkinsci/deploy-plugin/c58791723cc93e49d7430e51478ccd3d5fab8637/src/test/java/hudson/plugins/deploy/PipelineSyntaxTest.java/right.java
 
-        WorkflowRun r = p.scheduleBuild2(0).get();
-        // we expect a failed build status because there are no WAR files to deploy
-        j.assertBuildStatus(Result.FAILURE, r);
-        j.assertLogContains("No wars found. Deploy aborted.", r);
-    }
 
-    @Test
-    public void testTomcatAdapter() throws Exception {
-        WorkflowJob p = j.getInstance().createProject(WorkflowJob.class, "TomcatTest");
-        p.setDefinition(new CpsFlowDefinition(
-                getFullScript(
-                "def tc7 = tomcat7( " +
-                    "url: 'FAKE', " +
-                    "credentialsId: 'FAKE') \n" +
-                "def tc8 = tomcat8( " +
-                    "home: 'FAKE', " +
-                    "credentialsId: 'FAKE') \n" +
-                "deploy(adapters: [tc7, tc8], war: 'target/app.war', contextPath: 'app')"),
-                false));
-        WorkflowRun r = p.scheduleBuild2(0).get();
-        // we expect a failed build status because there are no WAR files to deploy
-        j.assertBuildStatus(Result.FAILURE, r);
-        j.assertLogContains("No wars found. Deploy aborted.", r);
-    }
 
-    @Test
-    public void testLegacyAdapterThrows() throws Exception {
-        WorkflowJob p = j.getInstance().createProject(WorkflowJob.class, "legacyTest");
-        p.setDefinition(new CpsFlowDefinition(
-                getFullScript(
-                        "writeFile(file: 'target/app.war', text: '')\n" +
-                        "deploy(adapters: [legacyAdapter()], war: 'target/app.war', contextPath: 'app', onFailure: true)"),
-                false));
-        WorkflowRun r = p.scheduleBuild2(0).get();
-        j.assertBuildStatus(Result.FAILURE, r);
-        j.assertLogContains("Please contact the plugin maintainer", r);
-    }
+<<<<<<< Unknown file: This is a bug in JDime.
+=======
+  @Test public void testMockAdaptersDeploy() throws Exception {
+    WorkflowJob p = j.getInstance().createProject(WorkflowJob.class, "MockTest");
+    p.setDefinition(new CpsFlowDefinition(getFullScript("deploy(adapters: [workflowAdapter(), workflowAdapter(), workflowAdapter()], war: \'target/app.war\', contextPath: \'app\')"), false));
+    WorkflowRun r = p.scheduleBuild2(0).get();
+    j.assertBuildStatus(Result.FAILURE, r);
+    j.assertLogContains("No wars found. Deploy aborted.", r);
+  }
+>>>>>>> /usr/src/app/output/jenkinsci/deploy-plugin/c58791723cc93e49d7430e51478ccd3d5fab8637/src/test/java/hudson/plugins/deploy/PipelineSyntaxTest.java/right.java
 
-    @Test
-    public void testSnippetizerDefaults() throws Exception {
-        WorkflowJob p = j.getInstance().createProject(WorkflowJob.class, "SnippetTest");
-        SnippetizerTester t = new SnippetizerTester(j);
 
-        ContainerAdapter tc = new Tomcat8xAdapter("http://example.com", "test-id");
-        DeployPublisher dp = new DeployPublisher(Collections.singletonList(tc), "app.war");
 
-        t.assertRoundTrip(new CoreStep(dp), "deploy adapters: [tomcat8(credentialsId: 'test-id', url: 'http://example.com')], war: 'app.war'");
-    }
+<<<<<<< Unknown file: This is a bug in JDime.
+=======
+  @Test public void testGlassFishAdapter() throws Exception {
+    WorkflowJob p = j.getInstance().createProject(WorkflowJob.class, "GlassfishTest");
+    p.setDefinition(new CpsFlowDefinition(getFullScript("def gf2 = glassfish2( " + "home: \'FAKE\', " + "credentialsId: \'FAKE\'," + "adminPort: \'1234\', " + "hostname: \'localhost\') \n" + "def gf3 = glassfish3( " + "home: \'FAKE\', " + "credentialsId: \'FAKE\'," + "adminPort: \'1234\', " + "hostname: \'localhost\') \n" + "deploy(adapters: [gf2, gf3], war: \'target/app.war\', contextPath: \'app\')"), false));
+    WorkflowRun r = p.scheduleBuild2(0).get();
+    j.assertBuildStatus(Result.FAILURE, r);
+    j.assertLogContains("No wars found. Deploy aborted.", r);
+  }
+>>>>>>> /usr/src/app/output/jenkinsci/deploy-plugin/c58791723cc93e49d7430e51478ccd3d5fab8637/src/test/java/hudson/plugins/deploy/PipelineSyntaxTest.java/right.java
 
-    @Test
-    public void testSnippetizerNonDefault() throws Exception {
-        WorkflowJob p = j.getInstance().createProject(WorkflowJob.class, "SnippetTest");
-        SnippetizerTester t = new SnippetizerTester(j);
 
-        ContainerAdapter tc = new Tomcat8xAdapter("http://example.com", "test-id");
-        DeployPublisher dp = new DeployPublisher(Collections.singletonList(tc), "app.war");
-        dp.setOnFailure(!j.jenkins.getDescriptorByType(DeployPublisher.DescriptorImpl.class).defaultOnFailure(p));
-        dp.setContextPath("my-app");
 
-        t.assertRoundTrip(new CoreStep(dp), "deploy adapters: [tomcat8(credentialsId: 'test-id', url: 'http://example.com')], contextPath: 'my-app', onFailure: false, war: 'app.war'");
-    }
-    */
-
+<<<<<<< Unknown file: This is a bug in JDime.
+=======
+  @Test public void testTomcatAdapter() throws Exception {
+    WorkflowJob p = j.getInstance().createProject(WorkflowJob.class, "TomcatTest");
+    p.setDefinition(new CpsFlowDefinition(getFullScript("def tc7 = tomcat7( " + "url: \'FAKE\', " + "credentialsId: \'FAKE\') \n" + "def tc8 = tomcat8( " + "home: \'FAKE\', " + "credentialsId: \'FAKE\') \n" + "deploy(adapters: [tc7, tc8], war: \'target/app.war\', contextPath: \'app\')"), false));
+    WorkflowRun r = p.scheduleBuild2(0).get();
+    j.assertBuildStatus(Result.FAILURE, r);
+    j.assertLogContains("No wars found. Deploy aborted.", r);
+  }
+>>>>>>> /usr/src/app/output/jenkinsci/deploy-plugin/c58791723cc93e49d7430e51478ccd3d5fab8637/src/test/java/hudson/plugins/deploy/PipelineSyntaxTest.java/right.java
 }
