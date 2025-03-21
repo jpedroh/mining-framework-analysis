f6e4982e2ca00895a32222713e3f49931980cbda
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 15:49:17 +0000

    Mining Framework Analysis

--- openscoring-service/src/main/java/org/openscoring/service/ModelRegistry.java
+++ openscoring-service/src/main/java/org/openscoring/service/ModelRegistry.java
@@ -1,23 +1,4 @@
-/*
- * Copyright (c) 2014 Villu Ruusmann
- *
- * This file is part of Openscoring
- *
- * Openscoring is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Openscoring is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with Openscoring.  If not, see <http://www.gnu.org/licenses/>.
- */
 package org.openscoring.service;
-
 import java.security.Principal;
 import java.util.Date;
 import java.util.Map;
@@ -25,67 +6,189 @@ import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.function.Function;
+import org.jpmml.evaluator.ResultMapper;
 
 public class ModelRegistry {
+  private ConcurrentMap<Principal, ConcurrentMap<String, Model>> models = new ConcurrentHashMap<>();
 
-	private ConcurrentMap<Principal, ConcurrentMap<String, Model>> models = new ConcurrentHashMap<>();
+  private Function<Principal, ConcurrentMap<String, Model>> initializer = new Function<Principal, ConcurrentMap<String, Model>>() {
+    @Override public ConcurrentMap<String, Model> apply(Principal principal) {
+      return new ConcurrentHashMap<>();
+    }
+  };
 
-	private Function<Principal, ConcurrentMap<String, Model>> initializer = new Function<Principal, ConcurrentMap<String, Model>>(){
+  public ModelRegistry() {
 
-		@Override
-		public ConcurrentMap<String, Model> apply(Principal principal){
-			return new ConcurrentHashMap<>();
-		}
-	};
+<<<<<<< Unknown file: This is a bug in JDime.
+=======
+    Config modelRegistryConfig = config.getConfig("modelRegistry");
+>>>>>>> /usr/src/app/output/jpmml/openscoring/9aca0c7a8409f3888295b16cc03a74d8def3ed21/openscoring-service/src/main/java/org/openscoring/service/ModelRegistry.java/right.java
 
 
-	public ModelRegistry(){
-	}
+<<<<<<< Unknown file: This is a bug in JDime.
+=======
+    LoadingModelEvaluatorBuilder modelEvaluatorBuilder = new LoadingModelEvaluatorBuilder();
+>>>>>>> /usr/src/app/output/jpmml/openscoring/9aca0c7a8409f3888295b16cc03a74d8def3ed21/openscoring-service/src/main/java/org/openscoring/service/ModelRegistry.java/right.java
 
-	public Map<String, Model> getModels(Principal owner){
-		return this.models.computeIfAbsent(owner, getInitializer());
-	}
 
-	public Model get(ModelRef modelRef){
-		return get(modelRef, false);
-	}
+<<<<<<< Unknown file: This is a bug in JDime.
+=======
+    String modelEvaluatorFactoryClassName = modelRegistryConfig.getString("modelEvaluatorFactoryClass");
+>>>>>>> /usr/src/app/output/jpmml/openscoring/9aca0c7a8409f3888295b16cc03a74d8def3ed21/openscoring-service/src/main/java/org/openscoring/service/ModelRegistry.java/right.java
 
-	public Model get(ModelRef modelRef, boolean touch){
-		Map<String, Model> models = getModels(modelRef.getOwner());
 
-		Model model = models.get(modelRef.getId());
-		if(model != null && touch){
-			model.putProperty(Model.PROPERTY_ACCESSED_TIMESTAMP, new Date());
-		}
+<<<<<<< Unknown file: This is a bug in JDime.
+=======
+    if (modelEvaluatorFactoryClassName != null) {
+      Class<? extends ModelEvaluatorFactory> modelEvaluatorFactoryClazz = loadClass(ModelEvaluatorFactory.class, modelEvaluatorFactoryClassName);
+      modelEvaluatorBuilder.setModelEvaluatorFactory(newInstance(modelEvaluatorFactoryClazz));
+    }
+>>>>>>> /usr/src/app/output/jpmml/openscoring/9aca0c7a8409f3888295b16cc03a74d8def3ed21/openscoring-service/src/main/java/org/openscoring/service/ModelRegistry.java/right.java
 
-		return model;
-	}
 
-	public boolean put(ModelRef modelRef, Model model){
-		Map<String, Model> models = getModels(modelRef.getOwner());
+<<<<<<< Unknown file: This is a bug in JDime.
+=======
+    String valueFactoryFactoryClassName = modelRegistryConfig.getString("valueFactoryFactoryClass");
+>>>>>>> /usr/src/app/output/jpmml/openscoring/9aca0c7a8409f3888295b16cc03a74d8def3ed21/openscoring-service/src/main/java/org/openscoring/service/ModelRegistry.java/right.java
 
-		Model oldModel = models.putIfAbsent(modelRef.getId(), Objects.requireNonNull(model));
 
-		return (oldModel == null);
-	}
+<<<<<<< Unknown file: This is a bug in JDime.
+=======
+    if (valueFactoryFactoryClassName != null) {
+      Class<? extends ValueFactoryFactory> valueFactoryFactoryClazz = loadClass(ValueFactoryFactory.class, valueFactoryFactoryClassName);
+      modelEvaluatorBuilder.setValueFactoryFactory(newInstance(valueFactoryFactoryClazz));
+    }
+>>>>>>> /usr/src/app/output/jpmml/openscoring/9aca0c7a8409f3888295b16cc03a74d8def3ed21/openscoring-service/src/main/java/org/openscoring/service/ModelRegistry.java/right.java
 
-	public boolean replace(ModelRef modelRef, Model oldModel, Model model){
-		Map<String, Model> models = getModels(modelRef.getOwner());
 
-		return models.replace(modelRef.getId(), oldModel, Objects.requireNonNull(model));
-	}
+<<<<<<< Unknown file: This is a bug in JDime.
+=======
+    ResultMapper resultMapper = new ResultMapper() {
+      @Override public FieldName apply(FieldName name) {
+        if (name == null) {
+          return ModelResource.DEFAULT_NAME;
+        }
+        return name;
+      }
+    };
+>>>>>>> /usr/src/app/output/jpmml/openscoring/9aca0c7a8409f3888295b16cc03a74d8def3ed21/openscoring-service/src/main/java/org/openscoring/service/ModelRegistry.java/right.java
 
-	public boolean remove(ModelRef modelRef, Model model){
-		Map<String, Model> models = getModels(modelRef.getOwner());
 
-		return models.remove(modelRef.getId(), model);
-	}
+<<<<<<< Unknown file: This is a bug in JDime.
+=======
+    modelEvaluatorBuilder.setResultMapper(resultMapper);
+>>>>>>> /usr/src/app/output/jpmml/openscoring/9aca0c7a8409f3888295b16cc03a74d8def3ed21/openscoring-service/src/main/java/org/openscoring/service/ModelRegistry.java/right.java
 
-	public Function<Principal, ConcurrentMap<String, Model>> getInitializer(){
-		return this.initializer;
-	}
 
-	public void setInitializer(Function<Principal, ConcurrentMap<String, Model>> initializer){
-		this.initializer = Objects.requireNonNull(initializer);
-	}
+<<<<<<< Unknown file: This is a bug in JDime.
+=======
+    boolean validate = modelRegistryConfig.getBoolean("validate");
+>>>>>>> /usr/src/app/output/jpmml/openscoring/9aca0c7a8409f3888295b16cc03a74d8def3ed21/openscoring-service/src/main/java/org/openscoring/service/ModelRegistry.java/right.java
+
+
+<<<<<<< Unknown file: This is a bug in JDime.
+=======
+    if (validate) {
+      Schema schema;
+      try {
+        schema = JAXBUtil.getSchema();
+      } catch (SAXException | IOException e) {
+        throw new RuntimeException(e);
+      }
+      modelEvaluatorBuilder.setSchema(schema).setValidationEventHandler(new SimpleValidationEventHandler());
+    }
+>>>>>>> /usr/src/app/output/jpmml/openscoring/9aca0c7a8409f3888295b16cc03a74d8def3ed21/openscoring-service/src/main/java/org/openscoring/service/ModelRegistry.java/right.java
+
+
+<<<<<<< Unknown file: This is a bug in JDime.
+=======
+    boolean locatable = modelRegistryConfig.getBoolean("locatable");
+>>>>>>> /usr/src/app/output/jpmml/openscoring/9aca0c7a8409f3888295b16cc03a74d8def3ed21/openscoring-service/src/main/java/org/openscoring/service/ModelRegistry.java/right.java
+
+
+<<<<<<< Unknown file: This is a bug in JDime.
+=======
+    modelEvaluatorBuilder.setLocatable(locatable);
+>>>>>>> /usr/src/app/output/jpmml/openscoring/9aca0c7a8409f3888295b16cc03a74d8def3ed21/openscoring-service/src/main/java/org/openscoring/service/ModelRegistry.java/right.java
+
+
+<<<<<<< Unknown file: This is a bug in JDime.
+=======
+    VisitorBattery visitors = new VisitorBattery();
+>>>>>>> /usr/src/app/output/jpmml/openscoring/9aca0c7a8409f3888295b16cc03a74d8def3ed21/openscoring-service/src/main/java/org/openscoring/service/ModelRegistry.java/right.java
+
+
+<<<<<<< Unknown file: This is a bug in JDime.
+=======
+    List<String> visitorClassNames = modelRegistryConfig.getStringList("visitorClasses");
+>>>>>>> /usr/src/app/output/jpmml/openscoring/9aca0c7a8409f3888295b16cc03a74d8def3ed21/openscoring-service/src/main/java/org/openscoring/service/ModelRegistry.java/right.java
+
+    for (String visitorClassName : visitorClassNames) {
+      Class<?> clazz = loadClass(Object.class, visitorClassName);
+      if ((Visitor.class).isAssignableFrom(clazz)) {
+        Class<? extends Visitor> visitorClazz = clazz.asSubclass(Visitor.class);
+        visitors.add(visitorClazz);
+      } else {
+        if ((VisitorBattery.class).isAssignableFrom(clazz)) {
+          Class<? extends VisitorBattery> visitorBatteryClazz = clazz.asSubclass(VisitorBattery.class);
+          VisitorBattery visitorBattery = newInstance(visitorBatteryClazz);
+          visitors.addAll(visitorBattery);
+        } else {
+          throw new IllegalArgumentException(new ClassCastException(clazz.toString()));
+        }
+      }
+    }
+
+<<<<<<< Unknown file: This is a bug in JDime.
+=======
+    modelEvaluatorBuilder.setVisitors(visitors);
+>>>>>>> /usr/src/app/output/jpmml/openscoring/9aca0c7a8409f3888295b16cc03a74d8def3ed21/openscoring-service/src/main/java/org/openscoring/service/ModelRegistry.java/right.java
+
+
+<<<<<<< Unknown file: This is a bug in JDime.
+=======
+    this.modelEvaluatorBuilder = modelEvaluatorBuilder;
+>>>>>>> /usr/src/app/output/jpmml/openscoring/9aca0c7a8409f3888295b16cc03a74d8def3ed21/openscoring-service/src/main/java/org/openscoring/service/ModelRegistry.java/right.java
+  }
+
+  public Map<String, Model> getModels(Principal owner) {
+    return this.models.computeIfAbsent(owner, getInitializer());
+  }
+
+  public Model get(ModelRef modelRef) {
+    return get(modelRef, false);
+  }
+
+  public Model get(ModelRef modelRef, boolean touch) {
+    Map<String, Model> models = getModels(modelRef.getOwner());
+    Model model = models.get(modelRef.getId());
+    if (model != null && touch) {
+      model.putProperty(Model.PROPERTY_ACCESSED_TIMESTAMP, new Date());
+    }
+    return model;
+  }
+
+  public boolean put(ModelRef modelRef, Model model) {
+    Map<String, Model> models = getModels(modelRef.getOwner());
+    Model oldModel = models.putIfAbsent(modelRef.getId(), Objects.requireNonNull(model));
+    return (oldModel == null);
+  }
+
+  public boolean replace(ModelRef modelRef, Model oldModel, Model model) {
+    Map<String, Model> models = getModels(modelRef.getOwner());
+    return models.replace(modelRef.getId(), oldModel, Objects.requireNonNull(model));
+  }
+
+  public boolean remove(ModelRef modelRef, Model model) {
+    Map<String, Model> models = getModels(modelRef.getOwner());
+    return models.remove(modelRef.getId(), model);
+  }
+
+  public Function<Principal, ConcurrentMap<String, Model>> getInitializer() {
+    return this.initializer;
+  }
+
+  public void setInitializer(Function<Principal, ConcurrentMap<String, Model>> initializer) {
+    this.initializer = Objects.requireNonNull(initializer);
+  }
 }
