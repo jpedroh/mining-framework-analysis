ca18eca2b05bb4a346cc78619b43e52d792ba071
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 09:37:19 +0000

    Mining Framework Analysis

--- src/main/java/org/wltea/analyzer/help/Sleep.java
+++ src/main/java/org/wltea/analyzer/help/Sleep.java
@@ -1,36 +1,40 @@
 package org.wltea.analyzer.help;
-
 import org.elasticsearch.common.logging.ESLogger;
 import org.elasticsearch.common.logging.Loggers;
 
 public class Sleep {
+  public static final ESLogger logger = Loggers.getLogger("ik-analyzer");
 
-	public static ESLogger logger= Loggers.getLogger("ik-analyzer");
-
-	public enum Type{MSEC,SEC,MIN,HOUR};
-	public static void sleep(Type type,int num){
-		try {
-			switch(type){
-				case MSEC:
-					Thread.sleep(num);
-					return;
-				case SEC:
-					Thread.sleep(num*1000);
-					return;
-				case MIN:
-					Thread.sleep(num*60*1000);
-					return;
-				case HOUR:
-					Thread.sleep(num*60*60*1000);
-					return;
-				default:
-					System.err.println("输入类型错误，应为MSEC,SEC,MIN,HOUR之一");
-					return;
-			}
-		} catch (InterruptedException e) {
-			logger.error(e.getMessage(), e);
-		}
-	}
+  public enum Type {
+    MSEC,
+    SEC,
+    MIN,
+    HOUR
+  }
 
 
+
+  public static void sleep(Type type, int num) {
+    try {
+      switch (type) {
+        case MSEC:
+        Thread.sleep(num);
+        return;
+        case SEC:
+        Thread.sleep(num * 1000L);
+        return;
+        case MIN:
+        Thread.sleep(num * 60 * 1000L);
+        return;
+        case HOUR:
+        Thread.sleep(num * 60 * 60 * 1000L);
+        return;
+        default:
+        System.err.println("\u8f93\u5165\u7c7b\u578b\u9519\u8bef\uff0c\u5e94\u4e3aMSEC,SEC,MIN,HOUR\u4e4b\u4e00");
+        return;
+      }
+    } catch (InterruptedException e) {
+      logger.error(e.getMessage(), e);
+    }
+  }
 }
--- src/main/java/org/wltea/analyzer/sample/LuceneIndexAndSearchDemo.java
+++ src/main/java/org/wltea/analyzer/sample/LuceneIndexAndSearchDemo.java
@@ -1,32 +1,5 @@
-/**
- * IK 中文分词  版本 5.0
- * IK Analyzer release 5.0
- *
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * 源代码由林良益(linliangyi2005@gmail.com)提供
- * 版权声明 2012，乌龙茶工作室
- * provided by Linliangyi and copyright 2012 by Oolong studio
- *
- *
- */
 package org.wltea.analyzer.sample;
-
 import java.io.IOException;
-
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
@@ -52,99 +25,76 @@ import org.elasticsearch.common.logging.ESLogger;
 import org.elasticsearch.common.logging.Loggers;
 import org.wltea.analyzer.lucene.IKAnalyzer;
 
-
-
-
 /**
  * 使用IKAnalyzer进行Lucene索引和查询的演示
  * 2012-3-2
- *
+ * 
  * 以下是结合Lucene4.0 API的写法
  *
  */
 public class LuceneIndexAndSearchDemo {
+  public static final ESLogger logger = Loggers.getLogger("ik-analyzer");
 
-	public static ESLogger logger= Loggers.getLogger("ik-analyzer");
-
-	/**
+  /**
 	 * 模拟：
 	 * 创建一个单条记录的索引，并对其进行搜索
 	 * @param args
 	 */
-	public static void main(String[] args){
-		//Lucene Document的域名
-		String fieldName = "text";
-		//检索内容
-		String text = "IK Analyzer是一个结合词典分词和文法分词的中文分词开源工具包。它使用了全新的正向迭代最细粒度切分算法。";
-
-		//实例化IKAnalyzer分词器
-		Analyzer analyzer = new IKAnalyzer(true);
-
-		Directory directory = null;
-		IndexWriter iwriter = null;
-		IndexReader ireader = null;
-		IndexSearcher isearcher = null;
-		try {
-			//建立内存索引对象
-			directory = new RAMDirectory();
-
-			//配置IndexWriterConfig
-			IndexWriterConfig iwConfig = new IndexWriterConfig(analyzer);
-			iwConfig.setOpenMode(OpenMode.CREATE_OR_APPEND);
-			iwriter = new IndexWriter(directory , iwConfig);
-			//写入索引
-			Document doc = new Document();
-			doc.add(new StringField("ID", "10000", Field.Store.YES));
-			doc.add(new TextField(fieldName, text, Field.Store.YES));
-			iwriter.addDocument(doc);
-			iwriter.close();
-
-
-			//搜索过程**********************************
-			//实例化搜索器   
-			ireader = DirectoryReader.open(directory);
-			isearcher = new IndexSearcher(ireader);
-
-			String keyword = "中文分词工具包";
-			//使用QueryParser查询分析器构造Query对象
-			QueryParser qp = new QueryParser(fieldName,  analyzer);
-			qp.setDefaultOperator(QueryParser.AND_OPERATOR);
-			Query query = qp.parse(keyword);
-			System.out.println("Query = " + query);
-
-			//搜索相似度最高的5条记录
-			TopDocs topDocs = isearcher.search(query , 5);
-			System.out.println("命中：" + topDocs.totalHits);
-			//输出结果
-			ScoreDoc[] scoreDocs = topDocs.scoreDocs;
-			for (int i = 0; i < topDocs.totalHits; i++){
-				Document targetDoc = isearcher.doc(scoreDocs[i].doc);
-				System.out.println("内容：" + targetDoc.toString());
-			}
-
-		} catch (CorruptIndexException e) {
-			logger.error(e.getMessage(), e);
-		} catch (LockObtainFailedException e) {
-			logger.error(e.getMessage(), e);
-		} catch (IOException e) {
-			logger.error(e.getMessage(), e);
-		} catch (ParseException e) {
-			logger.error(e.getMessage(), e);
-		} finally{
-			if(ireader != null){
-				try {
-					ireader.close();
-				} catch (IOException e) {
-					logger.error(e.getMessage(), e);
-				}
-			}
-			if(directory != null){
-				try {
-					directory.close();
-				} catch (IOException e) {
-					logger.error(e.getMessage(), e);
-				}
-			}
-		}
-	}
+  public static void main(String[] args) {
+    String fieldName = "text";
+    String text = "IK Analyzer\u662f\u4e00\u4e2a\u7ed3\u5408\u8bcd\u5178\u5206\u8bcd\u548c\u6587\u6cd5\u5206\u8bcd\u7684\u4e2d\u6587\u5206\u8bcd\u5f00\u6e90\u5de5\u5177\u5305\u3002\u5b83\u4f7f\u7528\u4e86\u5168\u65b0\u7684\u6b63\u5411\u8fed\u4ee3\u6700\u7ec6\u7c92\u5ea6\u5207\u5206\u7b97\u6cd5\u3002";
+    Analyzer analyzer = new IKAnalyzer(true);
+    Directory directory = null;
+    IndexWriter iwriter = null;
+    IndexReader ireader = null;
+    IndexSearcher isearcher = null;
+    try {
+      directory = new RAMDirectory();
+      IndexWriterConfig iwConfig = new IndexWriterConfig(analyzer);
+      iwConfig.setOpenMode(OpenMode.CREATE_OR_APPEND);
+      iwriter = new IndexWriter(directory, iwConfig);
+      Document doc = new Document();
+      doc.add(new StringField("ID", "10000", Field.Store.YES));
+      doc.add(new TextField(fieldName, text, Field.Store.YES));
+      iwriter.addDocument(doc);
+      iwriter.close();
+      ireader = DirectoryReader.open(directory);
+      isearcher = new IndexSearcher(ireader);
+      String keyword = "\u4e2d\u6587\u5206\u8bcd\u5de5\u5177\u5305";
+      QueryParser qp = new QueryParser(fieldName, analyzer);
+      qp.setDefaultOperator(QueryParser.AND_OPERATOR);
+      Query query = qp.parse(keyword);
+      System.out.println("Query = " + query);
+      TopDocs topDocs = isearcher.search(query, 5);
+      System.out.println("\u547d\u4e2d\uff1a" + topDocs.totalHits);
+      ScoreDoc[] scoreDocs = topDocs.scoreDocs;
+      for (int i = 0; i < topDocs.totalHits; i++) {
+        Document targetDoc = isearcher.doc(scoreDocs[i].doc);
+        System.out.println("\u5185\u5bb9\uff1a" + targetDoc.toString());
+      }
+    } catch (CorruptIndexException e) {
+      logger.error(e.getMessage(), e);
+    } catch (LockObtainFailedException e) {
+      logger.error(e.getMessage(), e);
+    } catch (IOException e) {
+      logger.error(e.getMessage(), e);
+    } catch (ParseException e) {
+      logger.error(e.getMessage(), e);
+    } finally {
+      if (ireader != null) {
+        try {
+          ireader.close();
+        } catch (IOException e) {
+          logger.error(e.getMessage(), e);
+        }
+      }
+      if (directory != null) {
+        try {
+          directory.close();
+        } catch (IOException e) {
+          logger.error(e.getMessage(), e);
+        }
+      }
+    }
+  }
 }
