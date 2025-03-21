848b115d6a981d72a8ac02ed6f3ce023ef69e8dc
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 13:05:23 +0000

    Mining Framework Analysis

--- src/main/java/org/jvnet/hudson/update_center/Plugin.java
+++ src/main/java/org/jvnet/hudson/update_center/Plugin.java
@@ -1,28 +1,4 @@
-/*
- * The MIT License
- *
- * Copyright (c) 2004-2010, Sun Microsystems, Inc.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
 package org.jvnet.hudson.update_center;
-
 import hudson.plugins.jira.soap.RemotePage;
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
@@ -32,7 +8,6 @@ import org.dom4j.DocumentFactory;
 import org.dom4j.Element;
 import org.dom4j.Node;
 import org.dom4j.io.SAXReader;
-
 import java.io.File;
 import java.io.IOException;
 import java.rmi.RemoteException;
@@ -51,254 +26,262 @@ import java.util.regex.Pattern;
  * @author Kohsuke Kawaguchi
  */
 public class Plugin {
-    /**
+  /**
      * Plugin artifact ID.
      */
-    public final String artifactId;
-    /**
+  public final String artifactId;
+
+  /**
      * Latest version of this plugin.
      */
-    public final HPI latest;
-    /**
+  public final HPI latest;
+
+  /**
      * Latest version of this plugin.
      */
-    public final HPI previous;
-    /**
+  public final HPI previous;
+
+  /**
      * Confluence page of this plugin in Wiki.
      * Null if we couldn't find it.
      */
-    public final RemotePage page;
-    /**
+  public final RemotePage page;
+
+  /**
      * Confluence labels for the plugin wiki page.
      * Null if wiki page wasn't found.
      */
-    public final String[] labels;
-    /**
+  public final String[] labels;
+
+  /**
      * Hostname of SCM info from POM of latest release, or null.
      * To determine if source lives in github or java.net svn.
      */
-    public final String scm;
-    /**
+  public final String scm;
+
+  /**
      * Deprecated plugins should not be included in update center.
      */
-    public final boolean deprecated;
+  public final boolean deprecated;
 
-    /**
+  /**
      * POM parsed as a DOM.
      */
-    private final Document pom;
+  private final Document pom;
 
-    public Plugin(String artifactId, HPI latest, HPI previous, ConfluencePluginList cpl) throws IOException {
-        this.artifactId = artifactId;
-        this.latest = latest;
-        this.previous = previous;
-        this.pom = readPOM();
-        this.page = findPage(cpl);
-        this.labels = getLabels(cpl);
-        boolean dep = false;
-        if (labels != null)
-            for (String label : labels)
-                if ("deprecated".equals(label)) {
-                    dep = true;
-                    break;
-                }
-        this.scm = getScmHost();
-        this.deprecated = dep;
-    }
-
-    private Document readPOM() throws IOException {
-        try {
-            DocumentFactory factory = new DocumentFactory();
-            factory.setXPathNamespaceURIs(Collections.singletonMap("m","http://maven.apache.org/POM/4.0.0"));
-            File pomFile = latest.resolvePOM();
-            return new SAXReader(factory).read(pomFile);
-        } catch (DocumentException e) {
-            System.err.println("Can't parse POM for "+artifactId);
-            e.printStackTrace();
-            return null;
+  public Plugin(String artifactId, HPI latest, HPI previous, ConfluencePluginList cpl) throws IOException {
+    this.artifactId = artifactId;
+    this.latest = latest;
+    this.previous = previous;
+    this.pom = readPOM();
+    this.page = findPage(cpl);
+    this.labels = getLabels(cpl);
+    boolean dep = false;
+    if (labels != null) {
+      for (String label : labels) {
+        if ("deprecated".equals(label)) {
+          dep = true;
+          break;
         }
+      }
     }
+    this.scm = getScmHost();
+    this.deprecated = dep;
+  }
 
-    /**
+  private Document readPOM() throws IOException {
+    try {
+      DocumentFactory factory = new DocumentFactory();
+      factory.setXPathNamespaceURIs(Collections.singletonMap("m", "http://maven.apache.org/POM/4.0.0"));
+      File pomFile = latest.resolvePOM();
+      return new SAXReader(factory).read(pomFile);
+    } catch (DocumentException e) {
+      System.err.println("Can\'t parse POM for " + artifactId);
+      e.printStackTrace();
+      return null;
+    }
+  }
+
+  /**
      * Locates the page for this plugin on Wiki.
      *
      * <p>
      * First we'll try to parse POM and obtain the URL.
      * If that fails, find the nearest name from the children list.
      */
-    private RemotePage findPage(ConfluencePluginList cpl) throws IOException {
-        if (pom != null) {
-            Node url = pom.selectSingleNode("/project/url");
-            if(url==null)
-                url = pom.selectSingleNode("/m:project/m:url");
-            if(url!=null) {
-                String wikiPage = ((Element)url).getTextTrim();
-                try {
-                    return cpl.getPage(wikiPage); // found the confluence page successfully
-                } catch (RemoteException e) {
-                    System.err.println("Failed to fetch "+wikiPage);
-                    e.printStackTrace();
-                }
-            }
-        }
-
+  private RemotePage findPage(ConfluencePluginList cpl) throws IOException {
+    if (pom != null) {
+      Node url = pom.selectSingleNode("/project/url");
+      if (url == null) {
+        url = pom.selectSingleNode("/m:project/m:url");
+      }
+      if (url != null) {
+        String wikiPage = ((Element) url).getTextTrim();
         try {
-            String p = OVERRIDES.getProperty(artifactId);
-            if(p!=null)
-                return cpl.getPage(p);
+          return cpl.getPage(wikiPage);
         } catch (RemoteException e) {
-            System.err.println("Override failed for "+artifactId);
-            e.printStackTrace();
+          System.err.println("Failed to fetch " + wikiPage);
+          e.printStackTrace();
         }
-
-        // try to guess the Wiki page
-        try {
-            return cpl.findNearest(artifactId);
-        } catch (RemoteException e) {
-            System.err.println("Failed to locate nearest");
-            e.printStackTrace();
-        }
-
-        return null;
+      }
     }
+    try {
+      String p = OVERRIDES.getProperty(artifactId);
+      if (p != null) {
+        return cpl.getPage(p);
+      }
+    } catch (RemoteException e) {
+      System.err.println("Override failed for " + artifactId);
+      e.printStackTrace();
+    }
+    try {
+      return cpl.findNearest(artifactId);
+    } catch (RemoteException e) {
+      System.err.println("Failed to locate nearest");
+      e.printStackTrace();
+    }
+    return null;
+  }
 
-    private static final Pattern HOSTNAME_PATTERN = Pattern.compile("://(?:\\w*@)?([\\w.]+)[/:]");
+  private static final Pattern HOSTNAME_PATTERN = Pattern.compile("://(?:\\w*@)?([\\w.]+)[/:]");
 
-    /**
+  /**
      * Get hostname of SCM specified in POM of latest release, or null.
      */
-    private String getScmHost() {
-        if (pom != null) {
-            Node scm = pom.selectSingleNode("/project/scm/connection");
-            if (scm == null)
-                scm = pom.selectSingleNode("/m:project/m:scm/m:connection");
-            if (scm != null) {
-                String conn = ((Element)scm).getTextTrim();
-                Matcher m = HOSTNAME_PATTERN.matcher(conn);
-                if (m.find())
-                    return m.group(1);
-            }
+  private String getScmHost() {
+    if (pom != null) {
+      Node scm = pom.selectSingleNode("/project/scm/connection");
+      if (scm == null) {
+        scm = pom.selectSingleNode("/m:project/m:scm/m:connection");
+      }
+      if (scm != null) {
+        String conn = ((Element) scm).getTextTrim();
+        Matcher m = HOSTNAME_PATTERN.matcher(conn);
+        if (m.find()) {
+          return m.group(1);
         }
-        return null;
+      }
     }
+    return null;
+  }
 
-    private String[] getLabels(ConfluencePluginList cpl) {
-        if (page!=null) try {
-            return cpl.getLabels(page);
-        } catch (RemoteException e) {
-            System.err.println("Failed to fetch labels for " + page.getUrl());
-            e.printStackTrace();
-        }
-
-        return null;
+  private String[] getLabels(ConfluencePluginList cpl) {
+    if (page != null) {
+      try {
+        return cpl.getLabels(page);
+      } catch (RemoteException e) {
+        System.err.println("Failed to fetch labels for " + page.getUrl());
+        e.printStackTrace();
+      }
     }
+    return null;
+  }
 
-    /**
+  /**
      * Obtains the excerpt of this wiki page in HTML. Otherwise null.
      */
-    public String getExcerptInHTML() {
-        String content = page.getContent();
-        if(content==null)
-            return null;
-
-        Matcher m = EXCERPT_PATTERN.matcher(content);
-        if(!m.find())
-            return null;
-
-        String excerpt = m.group(1);
-        String oneLiner = NEWLINE_PATTERN.matcher(excerpt).replaceAll(" ");
-        return HYPERLINK_PATTERN.matcher(oneLiner).replaceAll("<a href='$2'>$1</a>");
+  public String getExcerptInHTML() {
+    String content = page.getContent();
+    if (content == null) {
+      return null;
     }
-
-    // Tweaking to ignore leading whitespace after the initial {excerpt}
-    private static final Pattern EXCERPT_PATTERN = Pattern.compile("\\{excerpt(?::hidden(?:=true)?)?\\}\\s*(.+)\\{excerpt\\}", Pattern.DOTALL);
-    private static final Pattern HYPERLINK_PATTERN = Pattern.compile("\\[([^|\\]]+)\\|([^|\\]]+)(|([^]])+)?\\]");
-    private static final Pattern NEWLINE_PATTERN = Pattern.compile("(?:\\r\\n|\\n)");
-
-    public String getTitle() {
-        String title = "";
-        if (page!=null) {
-            title = page.getTitle();
-        }
-        return title;
+    Matcher m = EXCERPT_PATTERN.matcher(content);
+    if (!m.find()) {
+      return null;
     }
+    String excerpt = m.group(1);
+    String oneLiner = NEWLINE_PATTERN.matcher(excerpt).replaceAll(" ");
+    return HYPERLINK_PATTERN.matcher(oneLiner).replaceAll("<a href=\'$2\'>$1</a>");
+  }
 
-    public String getWiki() {
-        String wiki = "";
-        if (page!=null) {
-            wiki = page.getUrl();
-        }
-        return wiki;
+  private static final Pattern EXCERPT_PATTERN = Pattern.compile("\\{excerpt(?::hidden(?:=true)?)?\\}\\s*(.+)\\{excerpt\\}", Pattern.DOTALL);
+
+  private static final Pattern HYPERLINK_PATTERN = Pattern.compile("\\[([^|\\]]+)\\|([^|\\]]+)(|([^]])+)?\\]");
+
+  private static final Pattern NEWLINE_PATTERN = Pattern.compile("(?:\\r\\n|\\n)");
+
+  public String getTitle() {
+    String title = "";
+    if (page != null) {
+      title = page.getTitle();
     }
-    
-    public JSONObject toJSON() throws IOException {
-        JSONObject json = latest.toJSON(artifactId);
+    return title;
+  }
 
-        SimpleDateFormat fisheyeDateFormatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'.00Z'", Locale.US);
-        fisheyeDateFormatter.setTimeZone(TimeZone.getTimeZone("UTC"));
-        json.put("releaseTimestamp", fisheyeDateFormatter.format(latest.getTimestamp()));
-        if (previous!=null) {
-            json.put("previousVersion", previous.version);
-            json.put("previousTimestamp", fisheyeDateFormatter.format(previous.getTimestamp()));
-        }
-
-        if (page!=null) {
-            json.put("wiki",page.getUrl());
-            json.put("title",page.getTitle());
-            String excerpt = getExcerptInHTML();
-            if(excerpt!=null)
-                json.put("excerpt",excerpt);
-            if(labels!=null)
-                json.put("labels",labels);
-        }
-        if (scm!=null) {
-            json.put("scm", scm);
-        }
-
-        HPI hpi = latest;
-        json.put("requiredCore", fixNull(hpi.getRequiredHudsonVersion()));
-        
-        if (hpi.getCompatibleSinceVersion() != null) {
-            json.put("compatibleSinceVersion",hpi.getCompatibleSinceVersion());
-        }
-        if (hpi.getSandboxStatus() != null) {
-            json.put("sandboxStatus",hpi.getSandboxStatus());
-        }
-
-        JSONArray deps = new JSONArray();
-        for (HPI.Dependency d : hpi.getDependencies())
-            deps.add(d.toJSON());
-        json.put("dependencies",deps);
-
-        JSONArray devs = new JSONArray();
-        List<HPI.Developer> devList = hpi.getDevelopers();
-        if (!devList.isEmpty()) {
-            for (HPI.Developer dev : devList)
-                devs.add(dev.toJSON());
-        } else {
-            devs.add(new HPI.Developer("", latest.getBuiltBy(), "").toJSON());
-        }
-        json.put("developers", devs);
-
-        return json;
+  public String getWiki() {
+    String wiki = "";
+    if (page != null) {
+      wiki = page.getUrl();
     }
+    return wiki;
+  }
 
-    /**
+  public JSONObject toJSON() throws IOException {
+    JSONObject json = latest.toJSON(artifactId);
+    SimpleDateFormat fisheyeDateFormatter = new SimpleDateFormat("yyyy-MM-dd\'T\'HH:mm:ss\'.00Z\'", Locale.US);
+    fisheyeDateFormatter.setTimeZone(TimeZone.getTimeZone("UTC"));
+    json.put("releaseTimestamp", fisheyeDateFormatter.format(latest.getTimestamp()));
+    if (previous != null) {
+      json.put("previousVersion", previous.version);
+      json.put("previousTimestamp", fisheyeDateFormatter.format(previous.getTimestamp()));
+    }
+    if (page != null) {
+      json.put("wiki", page.getUrl());
+      json.put("title", page.getTitle());
+      String excerpt = getExcerptInHTML();
+      if (excerpt != null) {
+        json.put("excerpt", excerpt);
+      }
+      if (labels != null) {
+        json.put("labels", labels);
+      }
+    }
+    if (scm != null) {
+      json.put("scm", scm);
+    }
+    HPI hpi = latest;
+    json.put("requiredCore", fixNull(hpi.getRequiredHudsonVersion()));
+    if (hpi.getCompatibleSinceVersion() != null) {
+      json.put("compatibleSinceVersion", hpi.getCompatibleSinceVersion());
+    }
+    if (hpi.getSandboxStatus() != null) {
+      json.put("sandboxStatus", hpi.getSandboxStatus());
+    }
+    JSONArray deps = new JSONArray();
+    for (HPI.Dependency d : hpi.getDependencies()) {
+      deps.add(d.toJSON());
+    }
+    json.put("dependencies", deps);
+    JSONArray devs = new JSONArray();
+    List<HPI.Developer> devList = hpi.getDevelopers();
+    if (!devList.isEmpty()) {
+      for (HPI.Developer dev : devList) {
+        devs.add(dev.toJSON());
+      }
+    } else {
+      devs.add(new HPI.Developer("", latest.getBuiltBy(), "").toJSON());
+    }
+    json.put("developers", devs);
+    return json;
+  }
+
+  /**
      * Earlier versions of the maven-hpi-plugin put "null" string literal, so we need to treat it as real null.
      */
-    private String fixNull(String v) {
-        if("null".equals(v))    return null;
-        return v;
+  private String fixNull(String v) {
+    if ("null".equals(v)) {
+      return null;
     }
+    return v;
+  }
 
+  private static final Properties OVERRIDES = new Properties();
 
-    private static final Properties OVERRIDES = new Properties();
-
-    static {
-        try {
-            OVERRIDES.load(Plugin.class.getClassLoader().getResourceAsStream("wiki-overrides.properties"));
-        } catch (IOException e) {
-            throw new Error(e);
-        }
+  static {
+    try {
+      OVERRIDES.load(Plugin.class.getClassLoader().getResourceAsStream("wiki-overrides.properties"));
+    } catch (IOException e) {
+      throw new Error(e);
     }
+  }
 }
