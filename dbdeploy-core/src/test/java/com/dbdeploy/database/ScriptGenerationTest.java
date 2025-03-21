85bc60cda3b4ca0e2bca7318110abf5352956183
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 04:41:18 +0000

    Mining Framework Analysis

--- dbdeploy-core/src/test/java/com/dbdeploy/database/ScriptGenerationTest.java
+++ dbdeploy-core/src/test/java/com/dbdeploy/database/ScriptGenerationTest.java
@@ -1,5 +1,4 @@
 package com.dbdeploy.database;
-
 import com.dbdeploy.ChangeScriptApplier;
 import com.dbdeploy.Controller;
 import com.dbdeploy.appliers.TemplateBasedApplier;
@@ -9,88 +8,91 @@ import com.dbdeploy.scripts.ChangeScript;
 import com.dbdeploy.scripts.ChangeScriptRepository;
 import com.dbdeploy.scripts.StubChangeScript;
 import org.junit.Test;
-
+import static org.junit.Assert.assertEquals;
 import java.io.*;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
-
 public class ScriptGenerationTest {
+  @Test public void generateConsolidatedChangesScriptForAllDatabasesAndCompareAgainstTemplate() throws Exception {
+    for (String syntax : Arrays.asList("hsql", "mssql", "mysql", "ora", "syb-ase", "db2", "pgsql")) {
+      try {
+        System.out.printf("Testing syntax %s\n", syntax);
+        runIntegratedTestAndConfirmOutputResults(syntax);
+      } catch (Exception e) {
+        throw new RuntimeException("Failed while testing syntax " + syntax, e);
+      }
+    }
+  }
 
-	@Test
-	public void generateConsolidatedChangesScriptForAllDatabasesAndCompareAgainstTemplate() throws Exception {
-		for (String syntax : Arrays.asList("hsql", "mssql", "mysql", "ora", "syb-ase", "db2", "pgsql")) {
-			try {
-				System.out.printf("Testing syntax %s\n", syntax);
-				runIntegratedTestAndConfirmOutputResults(syntax);
-			} catch (Exception e) {
-				throw new RuntimeException("Failed while testing syntax " + syntax, e);
-			}
-		}
-	}
+  private void runIntegratedTestAndConfirmOutputResults(String syntaxName) throws Exception {
+    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    PrintStream printStream = new PrintStream(outputStream);
+    ChangeScript changeOne = new StubChangeScript(1, "001_change.sql", "-- contents of change script 1");
+    ChangeScript changeTwo = new StubChangeScript(2, "002_change.sql", "-- contents of change script 2");
+    List<ChangeScript> changeScripts = Arrays.asList(changeOne, changeTwo);
+    ChangeScriptRepository changeScriptRepository = new ChangeScriptRepository(changeScripts);
+    final StubSchemaManager schemaManager = new StubSchemaManager();
+    ChangeScriptApplier applier = new TemplateBasedApplier(printStream, syntaxName, "changelog", null);
+    Controller controller = new Controller(changeScriptRepository, schemaManager, applier, null);
+    controller.processChangeScripts(Long.MAX_VALUE);
+    assertEquals(readExpectedFileContents(getExpectedFilename(syntaxName)), outputStream.toString());
+  }
 
-	private void runIntegratedTestAndConfirmOutputResults(String syntaxName) throws Exception {
+  private String getExpectedFilename(String dbSyntaxName) {
+    return dbSyntaxName + "_expected.sql";
+  }
 
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-		PrintStream printStream = new PrintStream(outputStream);
+  private String readExpectedFileContents(String expectedFilename) throws IOException {
+    final InputStream stream = getClass().getResourceAsStream(expectedFilename);
+    BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+    try {
+      return readEntireStreamIntoAStringWithConversionToSystemDependantLineTerminators(reader);
+    }  finally {
+      reader.close();
+    }
+  }
 
-		ChangeScript changeOne = new StubChangeScript(1, "001_change.sql", "-- contents of change script 1");
-		ChangeScript changeTwo = new StubChangeScript(2, "002_change.sql", "-- contents of change script 2");
+  private String readEntireStreamIntoAStringWithConversionToSystemDependantLineTerminators(BufferedReader reader) throws IOException {
+    StringWriter contentWithSystemDependentLineTerminators = new StringWriter();
+    PrintWriter newLineConvertingContentWriter = new PrintWriter(contentWithSystemDependentLineTerminators);
+    try {
+      String line;
+      while ((line = reader.readLine()) != null) {
+        newLineConvertingContentWriter.println(line);
+      }
+      newLineConvertingContentWriter.flush();
+      return contentWithSystemDependentLineTerminators.toString();
+    }  finally {
+      newLineConvertingContentWriter.close();
+    }
+  }
 
-		List<ChangeScript> changeScripts = Arrays.asList(changeOne, changeTwo);
-		ChangeScriptRepository changeScriptRepository = new ChangeScriptRepository(changeScripts);
+  private class StubSchemaManager extends DatabaseSchemaVersionManager {
+    public StubSchemaManager() {
+      super(null, "changelog");
+    }
 
-
-
-		final StubSchemaManager schemaManager = new StubSchemaManager();
-		ChangeScriptApplier applier = new TemplateBasedApplier(printStream, syntaxName, "changelog", null);
-		Controller controller = new Controller(changeScriptRepository, schemaManager, applier, null);
-
-		controller.processChangeScripts(Long.MAX_VALUE);
-
-		assertEquals(readExpectedFileContents(getExpectedFilename(syntaxName)), outputStream.toString());
-	}
-
-	private String getExpectedFilename(String dbSyntaxName) {
-		return dbSyntaxName + "_expected.sql";
-	}
-
-	private String readExpectedFileContents(String expectedFilename) throws IOException {
-		final InputStream stream = getClass().getResourceAsStream(expectedFilename);
-		BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
-		try {
-			return readEntireStreamIntoAStringWithConversionToSystemDependantLineTerminators(reader);
-		} finally {
-			reader.close();
-		}
-	}
-
-	private String readEntireStreamIntoAStringWithConversionToSystemDependantLineTerminators(BufferedReader reader) throws IOException {
-		StringWriter contentWithSystemDependentLineTerminators = new StringWriter();
-		PrintWriter newLineConvertingContentWriter = new PrintWriter(contentWithSystemDependentLineTerminators);
-		try {
-			String line;
-			while ((line = reader.readLine()) != null) {
-				newLineConvertingContentWriter.println(line);
-			}
-			newLineConvertingContentWriter.flush();
-			return contentWithSystemDependentLineTerminators.toString();
-		} finally {
-			newLineConvertingContentWriter.close();
-		}
-	}
+    @Override public List<Long> getAppliedChanges() throws SchemaVersionTrackingException {
+      return Collections.emptyList();
+    }
+  }
 
 
-	private class StubSchemaManager extends DatabaseSchemaVersionManager {
-		public StubSchemaManager() {
-			super(null, "changelog");
-		}
+<<<<<<< Unknown file: This is a bug in JDime.
+=======
+  private class StubChangeScript extends ChangeScript {
+    private final String changeContents;
 
-		@Override
-		public List<Long> getAppliedChanges() throws SchemaVersionTrackingException {
-			return Collections.emptyList();
-		}
-	}
+    public StubChangeScript(long changeNumber, String description, String changeContents) {
+      super(changeNumber, description);
+      this.changeContents = changeContents;
+    }
+
+    @Override public String getContent() {
+      return changeContents;
+    }
+  }
+>>>>>>> /usr/src/app/output/tackley/dbdeploy/50e6bb3003bf2e91512f9263a966f9e30fb17113/dbdeploy-core/src/test/java/com/dbdeploy/database/ScriptGenerationTest.java/right.java
 }
