4aee5bfeb7f915e3e6e58f130953ed74ca5e9dfe
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 21:43:38 +0000

    Mining Framework Analysis

--- dspace-api/src/main/java/org/dspace/app/orcid/model/OrcidEntityType.java
+++ dspace-api/src/main/java/org/dspace/app/orcid/model/OrcidEntityType.java
@@ -1,58 +1,6 @@
-/**
- * The contents of this file are subject to the license and copyright
- * detailed in the LICENSE and NOTICE files at the root of the source
- * tree and available online at
- *
- * http://www.dspace.org/license/
- */
-package org.dspace.app.orcid.model;
-
-import java.util.Arrays;
-
-/**
- * The types of activities defined on ORCID that can be synchronized.
- *
- * @author Luca Giamminonni (luca.giamminonni at 4science.it)
- *
- */
-public enum OrcidEntityType {
-
-    /**
-     * The publication/work activity.
-     */
-    PUBLICATION("Publication", "/work"),
-
-    /**
-     * The funding activity.
-     */
-    FUNDING("Project", "/funding");
-
-    private final String entityType;
-
-    private final String path;
-
-    private OrcidEntityType(String entityType, String path) {
-        this.entityType = entityType;
-        this.path = path;
-    }
-
-    public String getEntityType() {
-        return entityType;
-    }
-
-    public String getPath() {
-        return path;
-    }
-
-    public static boolean isValidEntityType(String entityType) {
-        return Arrays.stream(OrcidEntityType.values())
-            .anyMatch(orcidEntityType -> orcidEntityType.getEntityType().equalsIgnoreCase(entityType));
-    }
-
-    public static OrcidEntityType fromEntityType(String entityType) {
-        return Arrays.stream(OrcidEntityType.values())
-            .filter(orcidEntityType -> orcidEntityType.getEntityType().equalsIgnoreCase(entityType))
-            .findFirst()
-            .orElse(null);
-    }
-}
+  package     org . dspace . app . orcid . model ; 
+<<<<<<<
+  import   java . util . Arrays ;
+=======
+>>>>>>>
+   public enum OrcidEntityType  {  PUBLICATION  ( "Publication" , "/work" ) ,  FUNDING  ( "Project" , "/funding" )  ;   private final String  entityType ;   private OrcidEntityType  (  String entityType ,  String path )  {    this . entityType = entityType ;    this . path = path ; }   public String getEntityType  ( )  {  return entityType ; }   public static boolean isValidEntityType  (  String entityType )  {  return   Arrays . stream  (  OrcidEntityType . values  ( ) ) . anyMatch  (  orcidEntityType ->   orcidEntityType . getEntityType  ( ) . equalsIgnoreCase  ( entityType ) ) ; }   public static OrcidEntityType fromEntityType  (  String entityType )  {  return     Arrays . stream  (  OrcidEntityType . values  ( ) ) . filter  (  orcidEntityType ->   orcidEntityType . getEntityType  ( ) . equalsIgnoreCase  ( entityType ) ) . findFirst  ( ) . orElse  ( null ) ; } }
