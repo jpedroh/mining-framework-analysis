0ee970af6e683dee6c672534230ba2e83d0e8917
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 09:23:50 +0000

    Mining Framework Analysis

--- src/test/java/com/github/javafaker/AviationTest.java
+++ src/test/java/com/github/javafaker/AviationTest.java
@@ -1,26 +1,19 @@
 package com.github.javafaker;
-
-import static com.github.javafaker.matchers.IsStringWithContents.isStringWithContents;
 import static com.github.javafaker.matchers.MatchesRegularExpression.matchesRegularExpression;
-import static org.hamcrest.MatcherAssert.assertThat;
-
 import org.junit.Test;
+import static com.github.javafaker.matchers.IsStringWithContents.isStringWithContents;
+import static org.junit.Assert.assertThat;
 
 public class AviationTest extends AbstractFakerTest {
+  @Test public void airport() {
+    assertThat(faker.aviation().airport(), matchesRegularExpression("\\w{4}"));
+  }
 
-    @Test
-    public void airport() {
-        assertThat(faker.aviation().airport(), matchesRegularExpression("\\w{4}"));
-    }
+  @Test public void aircraft() {
+    assertThat(faker.aviation().aircraft(), isStringWithContents());
+  }
 
-    @Test
-    public void aircraft() {
-        assertThat(faker.aviation().aircraft(), isStringWithContents());
-    }
-
-    @Test
-    public void metar() {
-        assertThat(faker.aviation().METAR(), isStringWithContents());
-    }
-    
+  @Test public void metar() {
+    assertThat(faker.aviation().METAR(), isStringWithContents());
+  }
 }
--- src/test/java/com/github/javafaker/WitcherTest.java
+++ src/test/java/com/github/javafaker/WitcherTest.java
@@ -1,39 +1,30 @@
 package com.github.javafaker;
-
 import static com.github.javafaker.matchers.MatchesRegularExpression.matchesRegularExpression;
-import static org.hamcrest.MatcherAssert.assertThat;
-
 import org.junit.Test;
+import static org.junit.Assert.assertThat;
 
 public class WitcherTest extends AbstractFakerTest {
+  @Test public void testCharacter() {
+    assertThat(faker.witcher().character(), matchesRegularExpression("[A-Za-z\' -\u00e9\u00fa\u00ef]+"));
+  }
 
-    @Test
-    public void testCharacter() {
-        assertThat(faker.witcher().character(), matchesRegularExpression("[A-Za-z' -éúï]+"));
-    }
+  @Test public void testWitcher() {
+    assertThat(faker.witcher().witcher(), matchesRegularExpression("[A-Za-z -\u00eb\u00fa\u00ef]+"));
+  }
 
-    @Test
-    public void testWitcher() {
-        assertThat(faker.witcher().witcher(), matchesRegularExpression("[A-Za-z -ëúï]+"));
-    }
+  @Test public void testSchool() {
+    assertThat(faker.witcher().school(), matchesRegularExpression("[A-Za-z]+"));
+  }
 
-    @Test
-    public void testSchool() {
-        assertThat(faker.witcher().school(), matchesRegularExpression("[A-Za-z]+"));
-    }
+  @Test public void testLocation() {
+    assertThat(faker.witcher().location(), matchesRegularExpression("[A-Za-z -\u00e1\u00e2\u00e9]+"));
+  }
 
-    @Test
-    public void testLocation() {
-        assertThat(faker.witcher().location(), matchesRegularExpression("[A-Za-z -áâé]+"));
-    }
+  @Test public void testQuote() {
+    assertThat(faker.witcher().quote(), matchesRegularExpression("[-A-Za-z0-9 \u2014;\u2026\\?\\!\\.\u2019\u2018\'\u201d\u201c,\\[\\]]+"));
+  }
 
-    @Test
-    public void testQuote() {
-        assertThat(faker.witcher().quote(), matchesRegularExpression("[-A-Za-z0-9 —;…\\?\\!\\.’‘'”“,\\[\\]]+"));
-    }
-
-    @Test
-    public void testMonster() {
-        assertThat(faker.witcher().monster(), matchesRegularExpression("[A-Za-z -]+"));
-    }
+  @Test public void testMonster() {
+    assertThat(faker.witcher().monster(), matchesRegularExpression("[A-Za-z -]+"));
+  }
 }
