fd5fcab331fe140035bf3cc46d8ac601d41b2b95
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 03:16:41 +0000

    Mining Framework Analysis

--- src/test/java/org/jenkinsci/plugins/tokenmacro/PipelineTest.java
+++ src/test/java/org/jenkinsci/plugins/tokenmacro/PipelineTest.java
@@ -1,5 +1,4 @@
 package org.jenkinsci.plugins.tokenmacro;
-
 import com.google.common.collect.ListMultimap;
 import hudson.FilePath;
 import hudson.model.AbstractBuild;
@@ -14,12 +13,10 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
 import org.jvnet.hudson.test.TestExtension;
-
 import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-
 import static junit.framework.TestCase.assertEquals;
 import static org.junit.Assert.fail;
 
@@ -27,145 +24,106 @@ import static org.junit.Assert.fail;
  * Created by acearl on 6/14/2016.
  */
 public class PipelineTest {
-    private StreamTaskListener listener;
+  private StreamTaskListener listener;
 
-    @Rule
-    public final JenkinsRule j = new JenkinsRule();
+  @Rule public final JenkinsRule j = new JenkinsRule();
 
-    @Before
-    public void setup() throws Exception {
-	    j.createOnlineSlave(Label.get("agents"));
+  @Before public void setup() throws Exception {
+    j.createOnlineSlave(Label.get("agents"));
+  }
+
+  @Test public void testEnvironmentVariables() throws Exception {
+    WorkflowJob job = j.jenkins.createProject(WorkflowJob.class, "foo");
+    job.setDefinition(new CpsFlowDefinition("pipeline {\n" + "  agent any\n" + "  environment {\n" + "    VERSION = \"1.0.0\"\n" + "  }\n\n" + "  stages {\n" + "    stage(\'Cool\') {\n" + "      steps {\n" + "        echo \"VERSION=\" + tm(\'${ENV, var=\"VERSION\"}\')\n" + "      }\n" + "    }\n" + "  }\n" + "}", true));
+    Run<?, ?> run = j.assertBuildStatusSuccess(job.scheduleBuild2(0));
+    j.assertLogContains("VERSION=1.0.0", run);
+  }
+
+  @Test public void testEscapedExpandAll() throws Exception {
+    WorkflowJob job = j.jenkins.createProject(WorkflowJob.class, "foo");
+    job.setDefinition(new CpsFlowDefinition("node(\'agents\') {\n\techo \'Hello, world\'\n}", true));
+    Run<?, ?> run = j.assertBuildStatusSuccess(job.scheduleBuild2(0));
+    listener = StreamTaskListener.fromStdout();
+    assertEquals(j.jenkins.getRootUrl() + "job/foo/1/", TokenMacro.expand(run, null, listener, "${BUILD_URL}"));
+    assertEquals(j.jenkins.getRootUrl() + "job/foo/1/", TokenMacro.expand(run, null, listener, "$BUILD_URL"));
+    assertEquals("{abc=[def, ghi], jkl=[true]}", TokenMacro.expand(run, null, listener, "${TEST,abc=\"def\",abc=\"ghi\",jkl=true}"));
+  }
+
+  @Test public void testException() throws Exception {
+    WorkflowJob job = j.jenkins.createProject(WorkflowJob.class, "foo");
+    job.setDefinition(new CpsFlowDefinition("node(\'agents\') {\n\techo \'Hello, world\'\n}", true));
+    Run<?, ?> run = j.assertBuildStatusSuccess(job.scheduleBuild2(0));
+    listener = StreamTaskListener.fromStdout();
+    try {
+      TokenMacro.expand(run, null, listener, "${TEST_NESTEDX}");
+      fail();
+    } catch (MacroEvaluationException e) {
+    }
+    assertEquals(" ${TEST_NESTEDX}", TokenMacro.expand(run, null, listener, " ${TEST_NESTEDX}", false, null));
+    assertEquals("${TEST_NESTEDX,abc=\"def\",abc=\"ghi\",jkl=true}", TokenMacro.expand(run, null, listener, "${TEST_NESTEDX,abc=\"def\",abc=\"ghi\",jkl=true}", false, null));
+  }
+
+  @Test public void testUnconvertedMacro() throws Exception {
+    WorkflowJob job = j.jenkins.createProject(WorkflowJob.class, "foo");
+    job.setDefinition(new CpsFlowDefinition("node(\'agents\') {\n\techo \'Hello, world\'\n}", true));
+    Run<?, ?> run = j.assertBuildStatusSuccess(job.scheduleBuild2(0));
+    listener = StreamTaskListener.fromStdout();
+    assertEquals("TEST_2 is not supported in this context", TokenMacro.expand(run, null, listener, "${TEST_2}"));
+  }
+
+  @TestExtension public static class TestMacro extends TokenMacro {
+    private static final String MACRO_NAME = "TEST";
+
+    @Override public boolean acceptsMacroName(String macroName) {
+      return macroName.equals(MACRO_NAME);
     }
 
-    @Test
-    public void testEnvironmentVariables() throws Exception {
-        WorkflowJob job = j.jenkins.createProject(WorkflowJob.class, "foo");
-        job.setDefinition(new CpsFlowDefinition("pipeline {\n" +
-                "  agent any\n" +
-                "  environment {\n" +
-                "    VERSION = \"1.0.0\"\n" +
-                "  }\n\n" +
-                "  stages {\n" +
-                "    stage('Cool') {\n" +
-                "      steps {\n" +
-                "        echo \"VERSION=\" + tm('${ENV, var=\"VERSION\"}')\n" +
-                "      }\n" +
-                "    }\n" +
-                "  }\n" +
-                "}", true));
-        Run<?,?> run = j.assertBuildStatusSuccess(job.scheduleBuild2(0));
-        j.assertLogContains("VERSION=1.0.0", run);
+    @Override public List<String> getAcceptedMacroNames() {
+      return Collections.singletonList(MACRO_NAME);
     }
 
-    @Test
-    public void testEscapedExpandAll() throws Exception {
-        WorkflowJob job = j.jenkins.createProject(WorkflowJob.class, "foo");
-
-        job.setDefinition(new CpsFlowDefinition("node('agents') {\n\techo 'Hello, world'\n}", true));
-        Run<?,?> run = j.assertBuildStatusSuccess(job.scheduleBuild2(0));
-
-        listener = StreamTaskListener.fromStdout();
-        assertEquals(j.jenkins.getRootUrl()+"job/foo/1/",TokenMacro.expand(run,null,listener,"${BUILD_URL}"));
-        assertEquals(j.jenkins.getRootUrl()+"job/foo/1/",TokenMacro.expand(run,null,listener,"$BUILD_URL"));
-
-        assertEquals("{abc=[def, ghi], jkl=[true]}",TokenMacro.expand(run,null,listener,"${TEST,abc=\"def\",abc=\"ghi\",jkl=true}"));
+    @Override public String evaluate(AbstractBuild<?, ?> context, TaskListener listener, String macroName, Map<String, String> arguments, ListMultimap<String, String> argumentMultimap) throws MacroEvaluationException, IOException, InterruptedException {
+      return evaluate(context, null, listener, macroName, arguments, argumentMultimap);
     }
 
-    @Test
-    public void testException() throws Exception {
-        WorkflowJob job = j.jenkins.createProject(WorkflowJob.class, "foo");
-        job.setDefinition(new CpsFlowDefinition("node('agents') {\n\techo 'Hello, world'\n}", true));
-        Run<?,?> run = j.assertBuildStatusSuccess(job.scheduleBuild2(0));
+    @Override public String evaluate(Run<?, ?> run, FilePath workspace, TaskListener listener, String macroName, Map<String, String> arguments, ListMultimap<String, String> argumentMultimap) throws MacroEvaluationException, IOException, InterruptedException {
+      return argumentMultimap.toString();
+    }
+  }
 
-        listener = StreamTaskListener.fromStdout();
+  @TestExtension public static class NestedTestMacro extends TokenMacro {
+    private static final String MACRO_NAME = "TEST_NESTED";
 
-        try {
-            TokenMacro.expand(run,null,listener,"${TEST_NESTEDX}");
-            fail();
-        } catch(MacroEvaluationException e) {
-            // do nothing, just want to catch the exception when it occurs
-        }
-
-        assertEquals(" ${TEST_NESTEDX}", TokenMacro.expand(run,null,listener," ${TEST_NESTEDX}",false,null));
-        assertEquals("${TEST_NESTEDX,abc=\"def\",abc=\"ghi\",jkl=true}", TokenMacro.expand(run,null,listener,"${TEST_NESTEDX,abc=\"def\",abc=\"ghi\",jkl=true}",false,null));
+    @Override public boolean acceptsMacroName(String macroName) {
+      return macroName.equals(MACRO_NAME);
     }
 
-    @Test
-    public void testUnconvertedMacro() throws Exception {
-        WorkflowJob job = j.jenkins.createProject(WorkflowJob.class, "foo");
-        job.setDefinition(new CpsFlowDefinition("node('agents') {\n\techo 'Hello, world'\n}", true));
-        Run<?,?> run = j.assertBuildStatusSuccess(job.scheduleBuild2(0));
-
-        listener = StreamTaskListener.fromStdout();
-
-        assertEquals("TEST_2 is not supported in this context", TokenMacro.expand(run,null,listener,"${TEST_2}"));
+    @Override public String evaluate(AbstractBuild<?, ?> context, TaskListener listener, String macroName, Map<String, String> arguments, ListMultimap<String, String> argumentMultimap) throws MacroEvaluationException, IOException, InterruptedException {
+      return "${TEST,abc=\"def\",abc=\"ghi\",jkl=true}";
     }
 
-    @TestExtension
-    public static class TestMacro extends TokenMacro {
-        private static final String MACRO_NAME = "TEST";
-
-        @Override
-        public boolean acceptsMacroName(String macroName) {
-            return macroName.equals(MACRO_NAME);
-        }
-
-        @Override
-        public List<String> getAcceptedMacroNames() {
-            return Collections.singletonList(MACRO_NAME);
-        }
-
-        @Override
-        public String evaluate(AbstractBuild<?, ?> context, TaskListener listener, String macroName, Map<String, String> arguments, ListMultimap<String, String> argumentMultimap) throws MacroEvaluationException, IOException, InterruptedException {
-            return evaluate(context,null,listener,macroName,arguments,argumentMultimap);
-        }
-
-        @Override
-        public String evaluate(Run<?,?> run, FilePath workspace, TaskListener listener, String macroName, Map<String, String> arguments, ListMultimap<String, String> argumentMultimap) throws MacroEvaluationException, IOException, InterruptedException {
-            return argumentMultimap.toString();
-        }
+    @Override public List<String> getAcceptedMacroNames() {
+      return Collections.singletonList(MACRO_NAME);
     }
 
-    @TestExtension
-    public static class NestedTestMacro extends TokenMacro {
-        private static final String MACRO_NAME = "TEST_NESTED";
+    @Override public boolean hasNestedContent() {
+      return true;
+    }
+  }
 
-        @Override
-        public boolean acceptsMacroName(String macroName) { return macroName.equals(MACRO_NAME); }
+  @TestExtension public static class TestMacro2 extends TokenMacro {
+    private static final String MACRO_NAME = "TEST_2";
 
-        @Override
-        public String evaluate(AbstractBuild<?, ?> context, TaskListener listener, String macroName, Map<String, String> arguments, ListMultimap<String, String> argumentMultimap) throws MacroEvaluationException, IOException, InterruptedException {
-            return "${TEST,abc=\"def\",abc=\"ghi\",jkl=true}";
-        }
-
-        @Override
-        public List<String> getAcceptedMacroNames() {
-            return Collections.singletonList(MACRO_NAME);
-        }
-
-        @Override
-        public boolean hasNestedContent() {
-            return true;
-        }
+    @Override public boolean acceptsMacroName(String macroName) {
+      return macroName.equals(MACRO_NAME);
     }
 
-    @TestExtension
-    public static class TestMacro2 extends TokenMacro {
-        private static final String MACRO_NAME = "TEST_2";
-
-        @Override
-        public boolean acceptsMacroName(String macroName) {
-            return macroName.equals(MACRO_NAME);
-        }
-
-        @Override
-        public List<String> getAcceptedMacroNames() {
-            return Collections.singletonList(MACRO_NAME);
-        }
-
-        @Override
-        public String evaluate(AbstractBuild<?, ?> context, TaskListener listener, String macroName, Map<String, String> arguments, ListMultimap<String, String> argumentMultimap) throws MacroEvaluationException, IOException, InterruptedException {
-            return argumentMultimap.toString();
-        }
+    @Override public List<String> getAcceptedMacroNames() {
+      return Collections.singletonList(MACRO_NAME);
     }
+
+    @Override public String evaluate(AbstractBuild<?, ?> context, TaskListener listener, String macroName, Map<String, String> arguments, ListMultimap<String, String> argumentMultimap) throws MacroEvaluationException, IOException, InterruptedException {
+      return argumentMultimap.toString();
+    }
+  }
 }
