fb465bc22876aa93a404880690fc61acf8a5047c
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 20:53:53 +0000

    Mining Framework Analysis

--- src/test/java/com/fasterxml/jackson/core/VersionTest.java
+++ src/test/java/com/fasterxml/jackson/core/VersionTest.java
@@ -3,41 +3,47 @@ package com.fasterxml.jackson.core;
 /**
  * Unit tests for class {@link Version}.
  */
-public class VersionTest extends BaseTest
-{
+public class VersionTest extends BaseTest {
   public void testCompareToOne() {
-      Version version = Version.unknownVersion();
-      Version versionTwo = new Version(0, -263, -1820, "", "", "");
-
-      assertEquals(263, version.compareTo(versionTwo));
+    Version version = Version.unknownVersion();
+    Version versionTwo = new Version(0, -263, -1820, "", "", "");
+    assertEquals(263, version.compareTo(versionTwo));
   }
 
   public void testCompareToReturningZero() {
-      Version version = Version.unknownVersion();
-      Version versionTwo = new Version(0, 0, 0, "", "", "");
-
-      assertEquals(0, version.compareTo(versionTwo));
+    Version version = Version.unknownVersion();
+    Version versionTwo = new Version(0, 0, 0, "", "", "");
+    assertEquals(0, version.compareTo(versionTwo));
   }
 
   public void testCreatesVersionTaking6ArgumentsAndCallsCompareTo() {
-      Version version = new Version(0, 0, 0, null, null, "");
-      Version versionTwo = new Version(0, 0, 0, "", "", "//0.0.0");
-
-      assertTrue(version.compareTo(versionTwo) < 0);
+    Version version = new Version(0, 0, 0, null, null, "");
+    Version versionTwo = new Version(0, 0, 0, "", "", "//0.0.0");
+    assertTrue(version.compareTo(versionTwo) < 0);
   }
 
   public void testCompareToTwo() {
-      Version version = Version.unknownVersion();
-      Version versionTwo = new Version(-1, 0, 0, "SNAPSHOT", "groupId", "artifactId");
-
-      int diff = version.compareTo(versionTwo);
-      assertTrue("Diff should be negative, was: "+diff, diff < 0);
+    Version version = Version.unknownVersion();
+    Version versionTwo = new Version(-1, 0, 0, "SNAPSHOT", 
+<<<<<<< /usr/src/app/output/fasterxml/jackson-core/d708338d421df111ab3d4a36bb90b14900594ec0/src/test/java/com/fasterxml/jackson/core/VersionTest.java/left.java
+    "groupId"
+=======
+    ""
+>>>>>>> /usr/src/app/output/fasterxml/jackson-core/d708338d421df111ab3d4a36bb90b14900594ec0/src/test/java/com/fasterxml/jackson/core/VersionTest.java/right.java
+    , 
+<<<<<<< /usr/src/app/output/fasterxml/jackson-core/d708338d421df111ab3d4a36bb90b14900594ec0/src/test/java/com/fasterxml/jackson/core/VersionTest.java/left.java
+    "artifactId"
+=======
+    ""
+>>>>>>> /usr/src/app/output/fasterxml/jackson-core/d708338d421df111ab3d4a36bb90b14900594ec0/src/test/java/com/fasterxml/jackson/core/VersionTest.java/right.java
+    );
+    int diff = version.compareTo(versionTwo);
+    assertTrue("Diff should be negative, was: " + diff, diff < 0);
   }
 
   public void testCompareToAndCreatesVersionTaking6ArgumentsAndUnknownVersion() {
-      Version version = Version.unknownVersion();
-      Version versionTwo = new Version(0, 0, 0, "SNAPSHOT", "groupId", "artifactId");
-
-      assertTrue(version.compareTo(versionTwo) < 0);
+    Version version = Version.unknownVersion();
+    Version versionTwo = new Version(0, 0, 0, "SNAPSHOT", "groupId", "artifactId");
+    assertTrue(version.compareTo(versionTwo) < 0);
   }
 }
