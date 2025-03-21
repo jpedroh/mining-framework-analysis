5d106c7c7700e95670cd3548ef3b94db046b966e
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 08:48:23 +0000

    Mining Framework Analysis

--- sonar-php-plugin/src/test/java/org/sonar/plugins/php/PHPProfileTest.java
+++ sonar-php-plugin/src/test/java/org/sonar/plugins/php/PHPProfileTest.java
@@ -1,24 +1,4 @@
-/*
- * SonarQube PHP Plugin
- * Copyright (C) 2010-2016 SonarSource SA
- * mailto:contact AT sonarsource DOT com
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3 of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- */
 package org.sonar.plugins.php;
-
 import org.junit.Test;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
@@ -28,22 +8,17 @@ import org.sonar.api.rules.RuleFinder;
 import org.sonar.api.utils.ValidationMessages;
 import org.sonar.php.checks.CheckList;
 import org.sonar.plugins.php.api.Php;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class PHPProfileTest {
-
-  @Test
-  public void should_create_sonar_way_profile() {
+  @Test public void should_create_sonar_way_profile() {
     ValidationMessages validation = ValidationMessages.create();
-
     RuleFinder ruleFinder = ruleFinder();
     PHPProfile definition = new PHPProfile(ruleFinder);
     RulesProfile profile = definition.createProfile(validation);
-
     assertThat(profile.getLanguage()).isEqualTo(Php.KEY);
     assertThat(profile.getName()).isEqualTo(CheckList.SONAR_WAY_PROFILE);
     assertThat(profile.getActiveRulesByRepository(CheckList.REPOSITORY_KEY)).hasSize(64);
@@ -60,5 +35,4 @@ public class PHPProfileTest {
       }
     }).getMock();
   }
-
 }
