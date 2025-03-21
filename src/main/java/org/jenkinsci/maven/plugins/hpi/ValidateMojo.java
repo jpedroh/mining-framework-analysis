0880ddacf309edb100af6254891d0428c1eb0773
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 23:30:14 +0000

    Mining Framework Analysis

--- src/main/java/org/jenkinsci/maven/plugins/hpi/ValidateMojo.java
+++ src/main/java/org/jenkinsci/maven/plugins/hpi/ValidateMojo.java
@@ -1,5 +1,4 @@
 package org.jenkinsci.maven.plugins.hpi;
-
 import hudson.util.VersionNumber;
 import io.jenkins.lib.versionnumber.JavaSpecificationVersion;
 import org.apache.maven.plugin.MojoExecutionException;
@@ -12,29 +11,38 @@ import org.apache.maven.project.MavenProject;
  *
  * @author Kohsuke Kawaguchi
  */
-@Mojo(name = "validate", defaultPhase = LifecyclePhase.VALIDATE)
-public class ValidateMojo extends AbstractJenkinsMojo {
+@Mojo(name = "validate", defaultPhase = LifecyclePhase.VALIDATE) public class ValidateMojo extends AbstractJenkinsMojo {
+  @Override public void execute() throws MojoExecutionException {
 
-    @Override
-    public void execute() throws MojoExecutionException {
-        JavaSpecificationVersion javaVersion = getMinimumJavaVersion();
-        if (JavaSpecificationVersion.forCurrentJVM().isOlderThan(javaVersion)) {
-            throw new MojoExecutionException("Java " + javaVersion + " or later is necessary to build this plugin.");
-        }
+<<<<<<< /usr/src/app/output/jenkinsci/maven-hpi-plugin/47546f63c65605f85296c517613a8ef93b04d7f5/src/main/java/org/jenkinsci/maven/plugins/hpi/ValidateMojo.java/left.java
+    MavenProject
+=======
+    JavaSpecificationVersion
+>>>>>>> /usr/src/app/output/jenkinsci/maven-hpi-plugin/47546f63c65605f85296c517613a8ef93b04d7f5/src/main/java/org/jenkinsci/maven/plugins/hpi/ValidateMojo.java/right.java
+     
+<<<<<<< /usr/src/app/output/jenkinsci/maven-hpi-plugin/47546f63c65605f85296c517613a8ef93b04d7f5/src/main/java/org/jenkinsci/maven/plugins/hpi/ValidateMojo.java/left.java
+    parent = project.getParent()
+=======
+    javaVersion = getMinimumJavaVersion()
+>>>>>>> /usr/src/app/output/jenkinsci/maven-hpi-plugin/47546f63c65605f85296c517613a8ef93b04d7f5/src/main/java/org/jenkinsci/maven/plugins/hpi/ValidateMojo.java/right.java
+    ;
 
-        if (new VersionNumber(findJenkinsVersion()).compareTo(new VersionNumber("2.204")) < 0) {
-            throw new MojoExecutionException("This version of maven-hpi-plugin requires Jenkins 2.204 or later");
-        }
-
-        MavenProject parent = project.getParent();
-        if (parent != null
-                && parent.getGroupId().equals("org.jenkins-ci.plugins")
-                && parent.getArtifactId().equals("plugin")
-                && !parent.getProperties().containsKey("java.level")
-                && project.getProperties().containsKey("java.level")) {
-            getLog().warn("Ignoring deprecated java.level property."
-                    + " This property should be removed from your plugin's POM."
-                    + " In the future this warning will be changed to an error and will break the build.");
-        }
+<<<<<<< Unknown file: This is a bug in JDime.
+=======
+    if (JavaSpecificationVersion.forCurrentJVM().isOlderThan(javaVersion)) {
+      throw new MojoExecutionException("Java " + javaVersion + " or later is necessary to build this plugin.");
     }
+>>>>>>> /usr/src/app/output/jenkinsci/maven-hpi-plugin/47546f63c65605f85296c517613a8ef93b04d7f5/src/main/java/org/jenkinsci/maven/plugins/hpi/ValidateMojo.java/right.java
+
+
+<<<<<<< /usr/src/app/output/jenkinsci/maven-hpi-plugin/47546f63c65605f85296c517613a8ef93b04d7f5/src/main/java/org/jenkinsci/maven/plugins/hpi/ValidateMojo.java/left.java
+    if (parent != null && parent.getGroupId().equals("org.jenkins-ci.plugins") && parent.getArtifactId().equals("plugin") && !parent.getProperties().containsKey("java.level") && project.getProperties().containsKey("java.level")) {
+      getLog().warn("Ignoring deprecated java.level property." + " This property should be removed from your plugin\'s POM." + " In the future this warning will be changed to an error and will break the build.");
+    }
+=======
+    if (new VersionNumber(findJenkinsVersion()).compareTo(new VersionNumber("2.204")) < 0) {
+      throw new MojoExecutionException("This version of maven-hpi-plugin requires Jenkins 2.204 or later");
+    }
+>>>>>>> /usr/src/app/output/jenkinsci/maven-hpi-plugin/47546f63c65605f85296c517613a8ef93b04d7f5/src/main/java/org/jenkinsci/maven/plugins/hpi/ValidateMojo.java/right.java
+  }
 }
