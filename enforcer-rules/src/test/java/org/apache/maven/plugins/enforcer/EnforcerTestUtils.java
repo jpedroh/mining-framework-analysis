080e82644ca0e1546d0039ebd79977268481754a
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 11:09:25 +0000

    Mining Framework Analysis

--- enforcer-rules/src/test/java/org/apache/maven/plugins/enforcer/EnforcerTestUtils.java
+++ enforcer-rules/src/test/java/org/apache/maven/plugins/enforcer/EnforcerTestUtils.java
@@ -1,29 +1,7 @@
 package org.apache.maven.plugins.enforcer;
-
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-
 import java.util.Properties;
-
 import org.apache.maven.enforcer.rule.api.EnforcerRuleHelper;
 import org.apache.maven.execution.MavenExecutionRequest;
 import org.apache.maven.execution.MavenExecutionResult;
@@ -43,98 +21,85 @@ import org.sonatype.aether.RepositorySystemSession;
  *
  * @author <a href="mailto:brianf@apache.org">Brian Fox</a>
  */
-public final class EnforcerTestUtils
-{
-    /**
+public final class EnforcerTestUtils {
+  /**
      * Gets the maven session.
      *
      * @return the maven session
      */
-    public static MavenSession getMavenSession()
-    {
-        PlexusContainer mock = mock( PlexusContainer.class );
+  public static MavenSession getMavenSession() {
+    PlexusContainer mock = mock(PlexusContainer.class);
+    MavenExecutionRequest mer = mock(MavenExecutionRequest.class);
+    Properties systemProperties = new Properties();
+    systemProperties.put("maven.version", "3.0");
+    when(mer.getUserProperties()).thenReturn(new Properties());
+    when(mer.getSystemProperties()).thenReturn(systemProperties);
+    MavenExecutionResult meresult = mock(MavenExecutionResult.class);
+    return new MavenSession(mock, (RepositorySystemSession) null, mer, meresult);
+  }
 
-        MavenExecutionRequest mer = mock( MavenExecutionRequest.class );
-
-        Properties systemProperties = new Properties();
-        systemProperties.put( "maven.version", "3.0" );
-        when( mer.getUserProperties() ).thenReturn( new Properties() );
-        when( mer.getSystemProperties() ).thenReturn( systemProperties );
-
-        MavenExecutionResult meresult = mock( MavenExecutionResult.class );
-        return new MavenSession( mock, (RepositorySystemSession) null, mer, meresult );
-    }
-
-    /**
+  /**
      * Gets the helper.
      *
      * @return the helper
      */
-    public static EnforcerRuleHelper getHelper()
-    {
-        return getHelper( new MockProject(), false );
-    }
+  public static EnforcerRuleHelper getHelper() {
+    return getHelper(new MockProject(), false);
+  }
 
-    /**
+  /**
      * Gets the helper.
      *
      * @param mockExpression the mock expression
      * @return the helper
      */
-    public static EnforcerRuleHelper getHelper( boolean mockExpression )
-    {
-        return getHelper( new MockProject(), mockExpression );
-    }
+  public static EnforcerRuleHelper getHelper(boolean mockExpression) {
+    return getHelper(new MockProject(), mockExpression);
+  }
 
-    /**
+  /**
      * Gets the helper.
      *
      * @param project the project
      * @return the helper
      */
-    public static EnforcerRuleHelper getHelper( MavenProject project )
-    {
-        return getHelper( project, false );
-    }
+  public static EnforcerRuleHelper getHelper(MavenProject project) {
+    return getHelper(project, false);
+  }
 
-    /**
+  /**
      * Gets the helper.
      *
      * @param project the project
      * @param mockExpression the mock expression
      * @return the helper
      */
-    public static EnforcerRuleHelper getHelper( MavenProject project, boolean mockExpression )
-    {
-        MavenSession session = getMavenSession();
-        ExpressionEvaluator eval;
-        if ( mockExpression )
-        {
-            eval = new MockEnforcerExpressionEvaluator( session );
-        }
-        else
-        {
-            MojoExecution mockExecution = mock( MojoExecution.class );
-            session.setCurrentProject( project );
-            eval = new PluginParameterExpressionEvaluator( session, mockExecution );
-        }
-        return new DefaultEnforcementRuleHelper( session, eval, new SystemStreamLog(), null );
+  public static EnforcerRuleHelper getHelper(MavenProject project, boolean mockExpression) {
+    MavenSession session = getMavenSession();
+    ExpressionEvaluator eval;
+    if (mockExpression) {
+      eval = new MockEnforcerExpressionEvaluator(session);
+    } else {
+      MojoExecution mockExecution = mock(MojoExecution.class);
+      session.setCurrentProject(project);
+      eval = new PluginParameterExpressionEvaluator(session, mockExecution);
     }
+    return new DefaultEnforcementRuleHelper(session, eval, new SystemStreamLog(), null);
+  }
 
-    /**
+  /**
      * Gets the helper.
      *
      * @param project the project
      * @param eval the expression evaluator to use
      * @return the helper
      */
-    public static EnforcerRuleHelper getHelper( MavenProject project, ExpressionEvaluator eval )
-    {
-        MavenSession session = getMavenSession();
-        return new DefaultEnforcementRuleHelper( session, eval, new SystemStreamLog(), null );
-    }
+  public static EnforcerRuleHelper getHelper(MavenProject project, ExpressionEvaluator eval) {
+    MavenSession session = getMavenSession();
+    return new DefaultEnforcementRuleHelper(session, eval, new SystemStreamLog(), null);
+  }
 
-    /**
+  /**
      * New plugin.
      *
      * @param groupId the group id
@@ -142,12 +107,11 @@ public final class EnforcerTestUtils
      * @param version the version
      * @return the plugin
      */
-    public static Plugin newPlugin( String groupId, String artifactId, String version )
-    {
-        Plugin plugin = new Plugin();
-        plugin.setArtifactId( artifactId );
-        plugin.setGroupId( groupId );
-        plugin.setVersion( version );
-        return plugin;
-    }
+  public static Plugin newPlugin(String groupId, String artifactId, String version) {
+    Plugin plugin = new Plugin();
+    plugin.setArtifactId(artifactId);
+    plugin.setGroupId(groupId);
+    plugin.setVersion(version);
+    return plugin;
+  }
 }
