68ad526229de8ecc4292af1e27bab7c00711003e
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 23:09:03 +0000

    Mining Framework Analysis

--- modules/swagger-codegen/src/test/java/io/swagger/codegen/csharp/CSharpClientOptionsTest.java
+++ modules/swagger-codegen/src/test/java/io/swagger/codegen/csharp/CSharpClientOptionsTest.java
@@ -1,34 +1,39 @@
 package io.swagger.codegen.csharp;
-
 import io.swagger.codegen.AbstractOptionsTest;
 import io.swagger.codegen.CodegenConfig;
 import io.swagger.codegen.languages.CSharpClientCodegen;
 import io.swagger.codegen.options.CSharpClientOptionsProvider;
-
 import mockit.Expectations;
 import mockit.Tested;
 
 public class CSharpClientOptionsTest extends AbstractOptionsTest {
+  @Tested private CSharpClientCodegen clientCodegen;
 
-    @Tested
-    private CSharpClientCodegen clientCodegen;
+  public CSharpClientOptionsTest() {
+    super(new CSharpClientOptionsProvider());
+  }
 
-    public CSharpClientOptionsTest() {
-        super(new CSharpClientOptionsProvider());
-    }
+  @Override protected CodegenConfig getCodegenConfig() {
+    return clientCodegen;
+  }
 
-    @Override
-    protected CodegenConfig getCodegenConfig() {
-        return clientCodegen;
-    }
+  @Override protected void setExpectations() {
+    new Expectations(clientCodegen) {
+      {
+        clientCodegen.setPackageName(CSharpClientOptionsProvider.PACKAGE_NAME_VALUE);
+        times = 1;
+        clientCodegen.setPackageVersion(CSharpClientOptionsProvider.PACKAGE_VERSION_VALUE);
+        times = 1;
+      }
+    };
+  }
 
-    @Override
-    protected void setExpectations() {
-        new Expectations(clientCodegen) {{
-            clientCodegen.setPackageName(CSharpClientOptionsProvider.PACKAGE_NAME_VALUE);
-            times = 1;
-            clientCodegen.setPackageVersion(CSharpClientOptionsProvider.PACKAGE_VERSION_VALUE);
-            times = 1;
-        }};
-    }
+
+<<<<<<< /usr/src/app/output/wordnik/swagger-codegen/6b4aaf8ada78a8a55d0488d820085fb024b1a364/modules/swagger-codegen/src/test/java/io/swagger/codegen/csharp/CSharpClientOptionsTest.java/left.java
+  @Override protected Map<String, String> getAvaliableOptions() {
+    ImmutableMap.Builder<String, String> builder = new ImmutableMap.Builder<String, String>();
+    return builder.put(CodegenConstants.PACKAGE_NAME, PACKAGE_NAME_VALUE).put(CodegenConstants.PACKAGE_VERSION, PACKAGE_VERSION_VALUE).put(CodegenConstants.SORT_PARAMS_BY_REQUIRED_FLAG, "true").build();
+  }
+=======
+>>>>>>> Unknown file: This is a bug in JDime.
 }
--- modules/swagger-codegen/src/test/java/io/swagger/codegen/python/PythonClientOptionsTest.java
+++ modules/swagger-codegen/src/test/java/io/swagger/codegen/python/PythonClientOptionsTest.java
@@ -1,34 +1,39 @@
 package io.swagger.codegen.python;
-
 import io.swagger.codegen.AbstractOptionsTest;
 import io.swagger.codegen.CodegenConfig;
 import io.swagger.codegen.languages.PythonClientCodegen;
 import io.swagger.codegen.options.PythonClientOptionsProvider;
-
 import mockit.Expectations;
 import mockit.Tested;
 
 public class PythonClientOptionsTest extends AbstractOptionsTest {
+  @Tested private PythonClientCodegen clientCodegen;
 
-    @Tested
-    private PythonClientCodegen clientCodegen;
+  public PythonClientOptionsTest() {
+    super(new PythonClientOptionsProvider());
+  }
 
-    public PythonClientOptionsTest() {
-        super(new PythonClientOptionsProvider());
-    }
+  @Override protected CodegenConfig getCodegenConfig() {
+    return clientCodegen;
+  }
 
-    @Override
-    protected CodegenConfig getCodegenConfig() {
-        return clientCodegen;
-    }
+  @Override protected void setExpectations() {
+    new Expectations(clientCodegen) {
+      {
+        clientCodegen.setPackageName(PythonClientOptionsProvider.PACKAGE_NAME_VALUE);
+        times = 1;
+        clientCodegen.setPackageVersion(PythonClientOptionsProvider.PACKAGE_VERSION_VALUE);
+        times = 1;
+      }
+    };
+  }
 
-    @Override
-    protected void setExpectations() {
-        new Expectations(clientCodegen) {{
-            clientCodegen.setPackageName(PythonClientOptionsProvider.PACKAGE_NAME_VALUE);
-            times = 1;
-            clientCodegen.setPackageVersion(PythonClientOptionsProvider.PACKAGE_VERSION_VALUE);
-            times = 1;
-        }};
-    }
+
+<<<<<<< /usr/src/app/output/wordnik/swagger-codegen/6b4aaf8ada78a8a55d0488d820085fb024b1a364/modules/swagger-codegen/src/test/java/io/swagger/codegen/python/PythonClientOptionsTest.java/left.java
+  @Override protected Map<String, String> getAvaliableOptions() {
+    ImmutableMap.Builder<String, String> builder = new ImmutableMap.Builder<String, String>();
+    return builder.put(CodegenConstants.PACKAGE_NAME, PACKAGE_NAME_VALUE).put(CodegenConstants.PACKAGE_VERSION, PACKAGE_VERSION_VALUE).put(CodegenConstants.SORT_PARAMS_BY_REQUIRED_FLAG, "true").build();
+  }
+=======
+>>>>>>> Unknown file: This is a bug in JDime.
 }
