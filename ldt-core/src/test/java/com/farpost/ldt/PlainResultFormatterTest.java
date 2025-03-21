f3fb1bfdfdbd54d9fc41da1dadb4808467f2f501
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 09:12:47 +0000

    Mining Framework Analysis

--- ldt-core/src/test/java/com/farpost/ldt/PlainResultFormatterTest.java
+++ ldt-core/src/test/java/com/farpost/ldt/PlainResultFormatterTest.java
@@ -1,22 +1,25 @@
-package com.farpost.ldt;
-
-import org.testng.annotations.Test;
-
-import static com.farpost.ldt.formatter.AbstractPlainResultFormatter.formatTime;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-
-public class PlainResultFormatterTest {
-
-	@Test
-	public void formatterCanFormatSeconds() throws InterruptedException {
-		assertThat(formatTime(0), equalTo("<1mcs"));
-		assertThat(formatTime(23), equalTo("~23mcs"));
-		assertThat(formatTime(15000), equalTo("15ms"));
-		assertThat(formatTime(1000000), equalTo(String.format("%.2fs", 1.0)));
-		assertThat(formatTime(1120000), equalTo(String.format("%.2fs", 1.12)));
-		assertThat(formatTime(1020000), equalTo(String.format("%.2fs", 1.02)));
-		assertThat(formatTime(1235000), equalTo(String.format("%.2fs", 1.24)));
-		assertThat(formatTime(65235000l), equalTo("1m 5s"));
-	}
-}
+  package   com . farpost . ldt ;   import    org . testng . annotations . Test ;  import static      com . farpost . ldt . formatter . AbstractPlainResultFormatter . formatTime ;  import static    org . hamcrest . MatcherAssert . assertThat ;  import static    org . hamcrest . Matchers . equalTo ;   public class PlainResultFormatterTest  {    @ Test public void formatterCanFormatSeconds  ( )  throws InterruptedException  {   assertThat  (  formatTime  ( 0 ) ,  equalTo  ( "<1mcs" ) ) ;   assertThat  (  formatTime  ( 23 ) ,  equalTo  ( "~23mcs" ) ) ;   assertThat  (  formatTime  ( 15000 ) ,  equalTo  ( "15ms" ) ) ;   assertThat  (  formatTime  ( 1000000 ) ,  equalTo  ( 
+<<<<<<<
+"1.00s"
+=======
+ String . format  ( "%.2fs" , 1.0 )
+>>>>>>>
+ ) ) ;   assertThat  (  formatTime  ( 1120000 ) ,  equalTo  ( 
+<<<<<<<
+"1.12s"
+=======
+ String . format  ( "%.2fs" , 1.12 )
+>>>>>>>
+ ) ) ;   assertThat  (  formatTime  ( 1020000 ) ,  equalTo  ( 
+<<<<<<<
+"1.02s"
+=======
+ String . format  ( "%.2fs" , 1.02 )
+>>>>>>>
+ ) ) ;   assertThat  (  formatTime  ( 1235000 ) ,  equalTo  ( 
+<<<<<<<
+"1.24s"
+=======
+ String . format  ( "%.2fs" , 1.24 )
+>>>>>>>
+ ) ) ;   assertThat  (  formatTime  ( 65235000l ) ,  equalTo  ( "1m 5s" ) ) ; } }
