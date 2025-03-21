f376d883c584a0e6a15e52819e5c0dcd9d552e76
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 21:54:45 +0000

    Mining Framework Analysis

--- src/main/java/com/github/alexcojocaru/mojo/elasticsearch/StartElasticsearchNodeMojo.java
+++ src/main/java/com/github/alexcojocaru/mojo/elasticsearch/StartElasticsearchNodeMojo.java
@@ -1,5 +1,4 @@
 package com.github.alexcojocaru.mojo.elasticsearch;
-
 import java.io.File;
 import java.io.IOException;
 import org.apache.commons.io.FileUtils;
@@ -15,135 +14,94 @@ import org.elasticsearch.common.settings.Settings;
  * @phase pre-integration-test
  */
 public class StartElasticsearchNodeMojo extends AbstractElasticsearchNodeMojo {
-
-    /**
+  /**
      * @parameter default-value="${project.build.directory}"
      * @required
      */
-    protected File outputDirectory;
+  protected File outputDirectory;
 
-    /**
+  /**
      * @parameter default-value="false"
      */
-    protected boolean keepData;
+  protected boolean keepData;
 
-    /**
+  /**
      * @parameter default-value="elasticsearch-data"
      */
-    protected String dataDirname;
+  protected String dataDirname;
 
-    /**
+  /**
      * @parameter default-value="elasticsearch-logs"
      */
-    protected String logsDirname;
+  protected String logsDirname;
 
-    /**
+  /**
      * @parameter default-value=""
      */
-    protected String configPath;
+  protected String configPath;
 
-    /**
+  /**
      * @parameter default-value=""
      */
-    protected String pluginsPath;
+  protected String pluginsPath;
 
-    /**
+  /**
      * @parameter default-value=false
      */
-    protected boolean autoCreateIndex;
+  protected boolean autoCreateIndex;
 
-    /**
+  /**
      * @parameter
      * @required
      */
-    protected Integer tcpPort;
+  protected Integer tcpPort;
 
-    @Override
-    public void execute() throws MojoExecutionException
-    {
-        File dataDirectory = prepareDirectory(outputDirectory, dataDirname, "data directory");
-        File logsDirectory = prepareDirectory(outputDirectory, logsDirname, "logs directory");
-
-        Settings.Builder builder = Settings.settingsBuilder()
-                .put("cluster.name", clusterName)
-                .put("action.auto_create_index", autoCreateIndex)
-                .put("transport.tcp.port", tcpPort)
-                .put("http.port", httpPort)
-                // ES v2.0.0 requires this property; set it to the parent of the data/log dirs.
-                .put("path.home", outputDirectory.getAbsolutePath())
-                .put("path.data", dataDirectory.getAbsolutePath())
-                .put("path.logs", logsDirectory.getAbsolutePath());
-
-        if (configPath != null && configPath.trim().length() > 0 && new File(configPath).exists())
-        {
-            builder.put("path.conf", configPath);
-        }
-        if (pluginsPath != null && pluginsPath.trim().length() > 0 && new File(pluginsPath).exists())
-        {
-            builder.put("path.plugins", pluginsPath);
-        }
-
-        
-        Settings settings = builder.build();
-        startNode(settings);
+  @Override public void execute() throws MojoExecutionException {
+    File dataDirectory = prepareDirectory(outputDirectory, dataDirname, "data directory");
+    File logsDirectory = prepareDirectory(outputDirectory, logsDirname, "logs directory");
+    Settings.Builder builder = Settings.settingsBuilder().put("cluster.name", clusterName).put("action.auto_create_index", autoCreateIndex).put("transport.tcp.port", tcpPort).put("http.port", httpPort).put("path.home", outputDirectory.getAbsolutePath()).put("path.data", dataDirectory.getAbsolutePath()).put("path.logs", logsDirectory.getAbsolutePath());
+    if (configPath != null && configPath.trim().length() > 0 && new File(configPath).exists()) {
+      builder.put("path.conf", configPath);
     }
-
-    protected ElasticsearchNode startNode(Settings settings) throws MojoExecutionException
-    {
-        if (getNode() != null)
-        {
-            return getNode();
-        }
-        else
-        {
-            ElasticsearchNode elasticsearchNode = new ElasticsearchNode(settings);
-            super.getPluginContext().put(clusterName, elasticsearchNode);
-            return elasticsearchNode;
-        }
+    if (pluginsPath != null && pluginsPath.trim().length() > 0 && new File(pluginsPath).exists()) {
+      builder.put("path.plugins", pluginsPath);
     }
+    Settings settings = builder.build();
+    startNode(settings);
+  }
 
-   /**
+  protected ElasticsearchNode startNode(Settings settings) throws MojoExecutionException {
+    if (getNode() != null) {
+      return getNode();
+    } else {
+      ElasticsearchNode elasticsearchNode = new ElasticsearchNode(settings);
+      super.getPluginContext().put(clusterName, elasticsearchNode);
+      return elasticsearchNode;
+    }
+  }
+
+  /**
      * 
      * @param parentDir
      * @param dirname
      * @param purpose What the directory is used for (i.e. 'data directory', 'logs directory., etc).
      * @throws MojoExecutionException
      */
-    private File prepareDirectory(File parentDir, String dirname, String purpose)
-            throws MojoExecutionException
-    {
-        File dir = new File(parentDir, dirname);
-
-        // If the directory already exists, delete it.
-        if (dir.exists())
-        {
-            if (keepData)
-            {
-                return dir;
-            }
-
-            try
-            {
-                FileUtils.deleteDirectory(dir);
-            }
-            catch (IOException e)
-            {
-                throw new MojoExecutionException(
-                        "Cannot delete the existing Elasticsearch " + purpose + " "
-                        + dir.getAbsolutePath(), e);
-            }
-        }
-
-        // Create a new Elasticsearch directory.
-        if (!dir.mkdirs())
-        {
-            throw new MojoExecutionException(
-                    "Cannot create the Elasticsearch " + purpose + " "
-                    + dir.getAbsolutePath());
-        }
-        
+  private File prepareDirectory(File parentDir, String dirname, String purpose) throws MojoExecutionException {
+    File dir = new File(parentDir, dirname);
+    if (dir.exists()) {
+      if (keepData) {
         return dir;
+      }
+      try {
+        FileUtils.deleteDirectory(dir);
+      } catch (IOException e) {
+        throw new MojoExecutionException("Cannot delete the existing Elasticsearch " + purpose + " " + dir.getAbsolutePath(), e);
+      }
     }
-
-    
+    if (!dir.mkdirs()) {
+      throw new MojoExecutionException("Cannot create the Elasticsearch " + purpose + " " + dir.getAbsolutePath());
+    }
+    return dir;
+  }
 }
--- src/test/java/com/github/alexcojocaru/mojo/elasticsearch/StartElasticsearchDataMojoTest.java
+++ src/test/java/com/github/alexcojocaru/mojo/elasticsearch/StartElasticsearchDataMojoTest.java
@@ -1,5 +1,4 @@
 package com.github.alexcojocaru.mojo.elasticsearch;
-
 import org.apache.http.HttpResponse;
 import org.apache.http.StatusLine;
 import org.apache.http.client.HttpClient;
@@ -7,9 +6,7 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPut;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.maven.plugin.testing.AbstractMojoTestCase;
-
 import com.github.alexcojocaru.mojo.elasticsearch.NetUtil.ElasticsearchPort;
-
 import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
@@ -18,112 +15,77 @@ import java.util.Map;
  * @author alexcojocaru
  *
  */
-public class StartElasticsearchDataMojoTest extends AbstractMojoTestCase
-{
+public class StartElasticsearchDataMojoTest extends AbstractMojoTestCase {
+  private StartElasticsearchNodeMojo mojo;
 
-    private StartElasticsearchNodeMojo mojo;
-    private HttpClient httpClient;
+  private HttpClient httpClient;
 
-    @Override
-    protected void setUp() throws Exception
-    {
-        super.setUp();
+  @Override protected void setUp() throws Exception {
+    super.setUp();
+    File testPom = new File(getBasedir(), "src/test/resources/goals/start/pom.xml");
+    Map<ElasticsearchPort, Integer> esPorts = NetUtil.findOpenPortsForElasticsearch();
+    mojo = (StartElasticsearchNodeMojo) lookupMojo("start", testPom);
+    mojo.setPluginContext(new HashMap());
+    mojo.httpPort = esPorts.get(ElasticsearchPort.HTTP);
+    mojo.tcpPort = esPorts.get(ElasticsearchPort.TCP);
+    httpClient = HttpClientBuilder.create().build();
+  }
 
-        File testPom = new File(getBasedir(), "src/test/resources/goals/start/pom.xml");
+  @Override protected void tearDown() throws Exception {
+    super.tearDown();
+    stopNode();
+  }
 
-        Map<ElasticsearchPort, Integer> esPorts = NetUtil.findOpenPortsForElasticsearch();
-
-        mojo = (StartElasticsearchNodeMojo) lookupMojo("start", testPom);
-        mojo.setPluginContext(new HashMap());
-        
-        // I cannot find another way of setting the two required propperties at run time.
-        mojo.httpPort = esPorts.get(ElasticsearchPort.HTTP);
-        mojo.tcpPort = esPorts.get(ElasticsearchPort.TCP);
-
-        httpClient = HttpClientBuilder.create().build();
+  private void stopNode() throws Exception {
+    if (mojo != null && mojo.getNode() != null) {
+      mojo.getNode().stop();
     }
-    
-    @Override
-    protected void tearDown() throws Exception
-    {
-        super.tearDown();
+  }
 
-        stopNode();
-    }
+  public void testMojoLookup() throws Exception {
+    assertNotNull(mojo);
+  }
 
-    private void stopNode() throws Exception
-    {
-        if (mojo != null && mojo.getNode() != null)
-        {
-            mojo.getNode().stop();
-        }
-    }
+  public void testMojoExecution() throws Exception {
+    assertNotNull(mojo);
+    mojo.execute();
+    HttpGet get = new HttpGet(getUri());
+    HttpResponse response = httpClient.execute(get);
+    assertEquals(200, response.getStatusLine().getStatusCode());
+    assertEquals(true, mojo.autoCreateIndex);
+  }
 
-    
-    public void testMojoLookup() throws Exception
-    {
-        assertNotNull(mojo);
-    }
-    
-    public void testMojoExecution() throws Exception
-    {
-        assertNotNull(mojo);
-        mojo.execute();
+  private String getUri() throws Exception {
+    return "http://localhost:" + mojo.getNode().getHttpPort();
+  }
 
-        HttpGet get = new HttpGet(getUri());
-        HttpResponse response = httpClient.execute(get);
-        assertEquals(200, response.getStatusLine().getStatusCode());
-        assertEquals(true, mojo.autoCreateIndex);
-    }
+  public void testKeepData() throws Exception {
+    assertNotNull(mojo);
+    mojo.execute();
+    final String indexName = "myindex";
+    StatusLine statusLine = createIndex(indexName);
+    assertEquals(statusLine.getReasonPhrase(), 2, statusLine.getStatusCode() / 100);
+    Thread.sleep(500);
+    statusLine = createIndex(indexName);
+    assertEquals(statusLine.getReasonPhrase(), 4, statusLine.getStatusCode() / 100);
+    stopNode();
+    setUp();
+    Thread.sleep(500);
+    mojo.execute();
+    statusLine = createIndex(indexName);
+    assertEquals(statusLine.getReasonPhrase(), 2, statusLine.getStatusCode() / 100);
+    stopNode();
+    setUp();
+    Thread.sleep(500);
+    mojo.keepData = true;
+    mojo.execute();
+    statusLine = createIndex(indexName);
+    assertEquals(statusLine.getReasonPhrase(), 4, statusLine.getStatusCode() / 100);
+  }
 
-    private String getUri() throws Exception
-    {
-        return "http://localhost:" + mojo.getNode().getHttpPort();
-    }
-
-    public void testKeepData() throws Exception
-    {
-        assertNotNull(mojo);
-
-        mojo.execute();
-
-        final String indexName = "myindex";
-
-        // succeeds
-        StatusLine statusLine = createIndex(indexName);
-        assertEquals(statusLine.getReasonPhrase(), 2, statusLine.getStatusCode() / 100);
-
-        // fails because exists
-        Thread.sleep(500);
-        statusLine = createIndex(indexName);
-        assertEquals(statusLine.getReasonPhrase(), 4, statusLine.getStatusCode() / 100);
-
-        stopNode();
-        setUp();
-        Thread.sleep(500);
-        mojo.execute();
-
-        // succeeds because deleted and newly created
-        statusLine = createIndex(indexName);
-        assertEquals(statusLine.getReasonPhrase(), 2, statusLine.getStatusCode() / 100);
-
-        stopNode();
-        setUp();
-        Thread.sleep(500);
-        mojo.keepData = true;
-        mojo.execute();
-
-        // fails because existed from previous
-        statusLine = createIndex(indexName);
-        assertEquals(statusLine.getReasonPhrase(), 4, statusLine.getStatusCode() / 100);
-
-    }
-
-    private StatusLine createIndex(final String indexName) throws Exception
-    {
-        final String indexUri = getUri() + "/" + indexName;
-        final HttpResponse response = httpClient.execute(new HttpPut(indexUri));
-        return response.getStatusLine();
-    }
-
+  private StatusLine createIndex(final String indexName) throws Exception {
+    final String indexUri = getUri() + "/" + indexName;
+    final HttpResponse response = httpClient.execute(new HttpPut(indexUri));
+    return response.getStatusLine();
+  }
 }
