05a9ba66f0d38dfb80938d9a2505f081d25a459c
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 10:54:31 +0000

    Mining Framework Analysis

--- nfctools-ndef/src/main/java/org/nfctools/ndef/ext/ExternalTypeEncoder.java
+++ nfctools-ndef/src/main/java/org/nfctools/ndef/ext/ExternalTypeEncoder.java
@@ -1,68 +1,57 @@
-/**
- * Copyright 2011-2012 Adrian Stabiszewski, as@nfctools.org
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.nfctools.ndef.ext;
-
 import java.util.HashMap;
 import java.util.Map;
-
 import org.nfctools.ndef.NdefConstants;
 import org.nfctools.ndef.NdefEncoderException;
+import org.nfctools.ndef.NdefException;
 import org.nfctools.ndef.NdefMessageEncoder;
 import org.nfctools.ndef.NdefRecord;
 import org.nfctools.ndef.Record;
+import org.nfctools.ndef.wkt.WellKnownRecordConfig;
 import org.nfctools.ndef.wkt.encoder.RecordEncoder;
 
 public class ExternalTypeEncoder implements RecordEncoder {
+  private Map<Class<?>, ExternalTypeRecordConfig> externalRecordTypes = new HashMap<Class<?>, ExternalTypeRecordConfig>();
 
-	private Map<Class<?>, ExternalTypeRecordConfig> externalRecordTypes = new HashMap<Class<?>, ExternalTypeRecordConfig>();
+  @Override public boolean canEncode(Record record) {
+    return record instanceof ExternalTypeRecord;
+  }
 
-	@Override
-	public boolean canEncode(Record record) {
-		return record instanceof ExternalTypeRecord;
-	}
+  @Override public NdefRecord encodeRecord(Record record, NdefMessageEncoder messageEncoder) {
+    ExternalTypeRecord externalType = (ExternalTypeRecord) record;
 
-	@Override
-	public NdefRecord encodeRecord(Record record, NdefMessageEncoder messageEncoder) {
-		ExternalTypeRecord externalType = (ExternalTypeRecord)record;
-		
-		if(!externalType.hasNamespace()) {
-			throw new NdefEncoderException("Expected namespace", record);
-		}
-		
-		ExternalTypeRecordConfig config = externalRecordTypes.get(record.getClass());
-		
-		byte[] payload;
-		if(config != null) {
-			payload = config.getContentEncoder().encodeContent(externalType).getBytes(NdefConstants.DEFAULT_CHARSET);
-		} else if(externalType instanceof UnsupportedExternalTypeRecord){
-			UnsupportedExternalTypeRecord externalTypeUnsupportedRecord = (UnsupportedExternalTypeRecord)externalType;
-			
-			if(!externalTypeUnsupportedRecord.hasContent()) {
-				throw new NdefEncoderException("Expected content", record);
-			}
+<<<<<<< /usr/src/app/output/grundid/nfctools/15659385c0127435a9d7ed840ff8f444d3d63053/nfctools-ndef/src/main/java/org/nfctools/ndef/ext/ExternalTypeEncoder.java/left.java
+    if (!externalType.hasNamespace()) {
+      throw new NdefEncoderException("Expected namespace", record);
+    }
+=======
+    ExternalTypeRecordConfig config = externalRecordTypes.get(record.getClass());
+>>>>>>> /usr/src/app/output/grundid/nfctools/15659385c0127435a9d7ed840ff8f444d3d63053/nfctools-ndef/src/main/java/org/nfctools/ndef/ext/ExternalTypeEncoder.java/right.java
 
-			payload = externalTypeUnsupportedRecord.getContent().getBytes(NdefConstants.DEFAULT_CHARSET);
-		} else {
-			throw new IllegalArgumentException("Unable to encode external type " + externalType.getClass().getName()); // TODO change to ndef exception
-		}
-		byte[] type = externalType.getNamespace().getBytes(NdefConstants.DEFAULT_CHARSET);
-		return new NdefRecord(NdefConstants.TNF_EXTERNAL_TYPE, type, record.getId(), payload);
-	}
-	
-	public void addRecordConfig(ExternalTypeRecordConfig config) {
-		externalRecordTypes.put(config.getRecordClass(), config);
-	}
+    byte[] payload;
+
+<<<<<<< /usr/src/app/output/grundid/nfctools/15659385c0127435a9d7ed840ff8f444d3d63053/nfctools-ndef/src/main/java/org/nfctools/ndef/ext/ExternalTypeEncoder.java/left.java
+    if (!externalType.hasContent()) {
+      throw new NdefEncoderException("Expected content", record);
+    }
+=======
+    if (config != null) {
+      payload = config.getContentEncoder().encodeContent(externalType).getBytes(NdefConstants.DEFAULT_CHARSET);
+    } else {
+      if (externalType instanceof UnsupportedExternalTypeRecord) {
+        UnsupportedExternalTypeRecord externalTypeUnsupportedRecord = (UnsupportedExternalTypeRecord) externalType;
+        payload = externalTypeUnsupportedRecord.getContent().getBytes(NdefConstants.DEFAULT_CHARSET);
+      } else {
+        throw new IllegalArgumentException("Unable to encode external type " + externalType.getClass().getName());
+      }
+    }
+>>>>>>> /usr/src/app/output/grundid/nfctools/15659385c0127435a9d7ed840ff8f444d3d63053/nfctools-ndef/src/main/java/org/nfctools/ndef/ext/ExternalTypeEncoder.java/right.java
+
+    byte[] type = externalType.getNamespace().getBytes(NdefConstants.DEFAULT_CHARSET);
+    return new NdefRecord(NdefConstants.TNF_EXTERNAL_TYPE, type, record.getId(), payload);
+  }
+
+  public void addRecordConfig(ExternalTypeRecordConfig config) {
+    externalRecordTypes.put(config.getRecordClass(), config);
+  }
 }
