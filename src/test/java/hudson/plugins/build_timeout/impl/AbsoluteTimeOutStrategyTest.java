884292cfd0332ffcee38d5eeab78c44fe202c257
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 07:55:01 +0000

    Mining Framework Analysis

--- src/test/java/hudson/plugins/build_timeout/impl/AbsoluteTimeOutStrategyTest.java
+++ src/test/java/hudson/plugins/build_timeout/impl/AbsoluteTimeOutStrategyTest.java
@@ -1,31 +1,5 @@
-/*
- * The MIT License
- * 
- * Copyright (c) 2014 IKEDA Yasuyuki
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
 package hudson.plugins.build_timeout.impl;
-
 import java.util.Arrays;
-
 import hudson.model.ParametersDefinitionProperty;
 import hudson.model.Result;
 import hudson.model.Cause;
@@ -37,7 +11,6 @@ import hudson.plugins.build_timeout.BuildTimeOutOperation;
 import hudson.plugins.build_timeout.BuildTimeoutWrapper;
 import hudson.plugins.build_timeout.BuildTimeoutWrapperIntegrationTest;
 import hudson.plugins.build_timeout.operations.AbortOperation;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
@@ -50,50 +23,38 @@ import org.jvnet.hudson.test.SleepBuilder;
  * Many tests for {@link AbsoluteTimeOutStrategy} are also in {@link BuildTimeoutWrapperIntegrationTest}
  */
 public class AbsoluteTimeOutStrategyTest {
+  @Rule public JenkinsRule j = new JenkinsRule();
 
-    @Rule
-    public JenkinsRule j = new JenkinsRule();
+  private long origTimeout = 0;
 
-    private long origTimeout = 0;
-    
-    @Before
-    public void before() {
-        // this allows timeout shorter than 3 minutes.
-        origTimeout = BuildTimeoutWrapper.MINIMUM_TIMEOUT_MILLISECONDS;
-        BuildTimeoutWrapper.MINIMUM_TIMEOUT_MILLISECONDS = 1000;
-    }
-    
-    @After
-    public void after() {
-        BuildTimeoutWrapper.MINIMUM_TIMEOUT_MILLISECONDS = origTimeout;
-    }
-    
-    @Test
-    public void configurationWithParameter() throws Exception {
-        FreeStyleProject p = j.createFreeStyleProject();
-        // needed since Jenkins 2.3
-        p.addProperty(new ParametersDefinitionProperty(new StringParameterDefinition("TIMEOUT", null)));
-        p.getBuildWrappersList().add(
-                new BuildTimeoutWrapper(
-                        new AbsoluteTimeOutStrategy("${TIMEOUT}"),
-                        Arrays.<BuildTimeOutOperation>asList(new AbortOperation()),
-                        null
-                )
-        );
-        p.getBuildersList().add(new SleepBuilder(5000));
-        
-        // If called with TIMEOUT=1, the build succeeds.
-        j.assertBuildStatus(Result.SUCCESS, p.scheduleBuild2(
-                0,
-                new Cause.UserCause(),
-                new ParametersAction(new StringParameterValue("TIMEOUT", "1"))
-        ).get());
-        
-        // If called with TIMEOUT=0, the build is aborted immediately.
-        j.assertBuildStatus(Result.ABORTED, p.scheduleBuild2(
-                0,
-                new Cause.UserCause(),
-                new ParametersAction(new StringParameterValue("TIMEOUT", "0"))
-        ).get());
-    }
+  @Before public void before() {
+    origTimeout = BuildTimeoutWrapper.MINIMUM_TIMEOUT_MILLISECONDS;
+    BuildTimeoutWrapper.MINIMUM_TIMEOUT_MILLISECONDS = 1000;
+  }
+
+  @After public void after() {
+    BuildTimeoutWrapper.MINIMUM_TIMEOUT_MILLISECONDS = origTimeout;
+  }
+
+
+<<<<<<< /usr/src/app/output/jenkinsci/build-timeout-plugin/d51178be1cd90cf9173edd1705a7bf0dc75ab16d/src/test/java/hudson/plugins/build_timeout/impl/AbsoluteTimeOutStrategyTest.java/left.java
+  @Test public void testConfigurationWithParameter() throws Exception {
+    FreeStyleProject p = j.createFreeStyleProject();
+    p.addProperty(new ParametersDefinitionProperty(new StringParameterDefinition("TIMEOUT", null)));
+    p.getBuildWrappersList().add(new BuildTimeoutWrapper(new AbsoluteTimeOutStrategy("${TIMEOUT}"), Arrays.<BuildTimeOutOperation>asList(new AbortOperation()), null));
+    p.getBuildersList().add(new SleepBuilder(5000));
+    j.assertBuildStatus(Result.SUCCESS, p.scheduleBuild2(0, new Cause.UserCause(), new ParametersAction(new StringParameterValue("TIMEOUT", "1"))).get());
+    j.assertBuildStatus(Result.ABORTED, p.scheduleBuild2(0, new Cause.UserCause(), new ParametersAction(new StringParameterValue("TIMEOUT", "0"))).get());
+  }
+=======
+>>>>>>> Unknown file: This is a bug in JDime.
+
+
+  @Test public void configurationWithParameter() throws Exception {
+    FreeStyleProject p = j.createFreeStyleProject();
+    p.getBuildWrappersList().add(new BuildTimeoutWrapper(new AbsoluteTimeOutStrategy("${TIMEOUT}"), Arrays.<BuildTimeOutOperation>asList(new AbortOperation()), null));
+    p.getBuildersList().add(new SleepBuilder(5000));
+    j.assertBuildStatus(Result.SUCCESS, p.scheduleBuild2(0, new Cause.UserCause(), new ParametersAction(new StringParameterValue("TIMEOUT", "1"))).get());
+    j.assertBuildStatus(Result.ABORTED, p.scheduleBuild2(0, new Cause.UserCause(), new ParametersAction(new StringParameterValue("TIMEOUT", "0"))).get());
+  }
 }
--- src/test/java/hudson/plugins/build_timeout/operations/AbortAndRestartOperationTest.java
+++ src/test/java/hudson/plugins/build_timeout/operations/AbortAndRestartOperationTest.java
@@ -1,44 +1,16 @@
-/*
- * The MIT License
- * 
- * Copyright (c) 2015 Jochen A. Fuerbacher
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
 package hudson.plugins.build_timeout.operations;
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
-
 import java.util.Arrays;
 import java.util.LinkedList;
-
 import hudson.model.ParametersDefinitionProperty;
 import hudson.model.StringParameterDefinition;
 import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
 import org.jvnet.hudson.test.SleepBuilder;
-
 import hudson.model.Cause;
 import hudson.model.FreeStyleProject;
 import hudson.model.ParametersAction;
@@ -49,115 +21,76 @@ import hudson.plugins.build_timeout.QuickBuildTimeOutStrategy;
 import hudson.plugins.build_timeout.BuildTimeoutWrapper;
 
 public class AbortAndRestartOperationTest {
-    
-    @Rule
-    public JenkinsRule j = new JenkinsRule();
+  @Rule public JenkinsRule j = new JenkinsRule();
 
-    @Test
-    public void abortAndRestartOnce() throws Exception {
-        FreeStyleProject testproject = j.createFreeStyleProject();
+  @Test public void abortAndRestartOnce() throws Exception {
+    FreeStyleProject testproject = j.createFreeStyleProject();
+    QuickBuildTimeOutStrategy strategy = new QuickBuildTimeOutStrategy(5000);
+    AbortAndRestartOperation operation = new AbortAndRestartOperation("1");
+    LinkedList<BuildTimeOutOperation> list = new LinkedList<>();
+    list.add(operation);
+    BuildTimeoutWrapper wrapper = new BuildTimeoutWrapper(strategy, list, "");
+    testproject.getBuildWrappersList().add(wrapper);
+    testproject.getBuildersList().add(new SleepBuilder(5 * 60 * 1000));
+    testproject.scheduleBuild(new Cause.UserIdCause());
+    j.waitUntilNoActivityUpTo(25000);
+    assertNotNull(testproject.getFirstBuild());
+    assertFalse(testproject.getFirstBuild().equals(testproject.getLastBuild()));
+    assertEquals(testproject.getBuilds().size(), 2);
+    assertEquals(Result.ABORTED, testproject.getFirstBuild().getResult());
+    assertEquals(Result.ABORTED, testproject.getLastBuild().getResult());
+  }
 
-        QuickBuildTimeOutStrategy strategy = new QuickBuildTimeOutStrategy(5000);
-        AbortAndRestartOperation operation = new AbortAndRestartOperation("1"); //Number of restarts
-        LinkedList<BuildTimeOutOperation> list = new LinkedList<>();
-        list.add(operation);
-        
-        BuildTimeoutWrapper wrapper = new BuildTimeoutWrapper(strategy,list,"");
-        testproject.getBuildWrappersList().add(wrapper);
-        
-        
-        testproject.getBuildersList().add(new SleepBuilder(5*60*1000)); //5 minutes
-                       
-        testproject.scheduleBuild(new Cause.UserIdCause());
+  @Test public void abortAndRestartTwice() throws Exception {
+    FreeStyleProject testproject = j.createFreeStyleProject();
+    QuickBuildTimeOutStrategy strategy = new QuickBuildTimeOutStrategy(5000);
+    AbortAndRestartOperation operation = new AbortAndRestartOperation("2");
+    LinkedList<BuildTimeOutOperation> list = new LinkedList<>();
+    list.add(operation);
+    BuildTimeoutWrapper wrapper = new BuildTimeoutWrapper(strategy, list, "");
+    testproject.getBuildWrappersList().add(wrapper);
+    testproject.getBuildersList().add(new SleepBuilder(5 * 60 * 1000));
+    assertTrue(testproject.getBuilds().size() == 0);
+    testproject.scheduleBuild(new Cause.UserIdCause());
+    j.waitUntilNoActivityUpTo(25000);
+    assertNotNull(testproject.getFirstBuild());
+    assertFalse(testproject.getFirstBuild().equals(testproject.getLastBuild()));
+    assertEquals(testproject.getBuilds().size(), 3);
+    assertEquals(Result.ABORTED, testproject.getBuildByNumber(1).getResult());
+    assertEquals(Result.ABORTED, testproject.getBuildByNumber(2).getResult());
+    assertEquals(Result.ABORTED, testproject.getBuildByNumber(3).getResult());
+  }
 
-        j.waitUntilNoActivityUpTo(25000);
-            
-        assertNotNull(testproject.getFirstBuild());
-        assertFalse(testproject.getFirstBuild().equals(testproject.getLastBuild()));
-        assertEquals(testproject.getBuilds().size(), 2);
-        
-        assertEquals(Result.ABORTED, testproject.getFirstBuild().getResult());
-        assertEquals(Result.ABORTED, testproject.getLastBuild().getResult());
-    }
-    
-    @Test
-    public void abortAndRestartTwice() throws Exception {
-        FreeStyleProject testproject = j.createFreeStyleProject();
 
-        QuickBuildTimeOutStrategy strategy = new QuickBuildTimeOutStrategy(5000);
-        AbortAndRestartOperation operation = new AbortAndRestartOperation("2"); //Number of restarts
-        LinkedList<BuildTimeOutOperation> list = new LinkedList<>();
-        list.add(operation);
-        
-        BuildTimeoutWrapper wrapper = new BuildTimeoutWrapper(strategy,list,"");
-        testproject.getBuildWrappersList().add(wrapper);
-        
-        testproject.getBuildersList().add(new SleepBuilder(5*60*1000)); //5 minutes
-        
-        assertTrue(testproject.getBuilds().size()==0);
+<<<<<<< /usr/src/app/output/jenkinsci/build-timeout-plugin/d51178be1cd90cf9173edd1705a7bf0dc75ab16d/src/test/java/hudson/plugins/build_timeout/operations/AbortAndRestartOperationTest.java/left.java
+  @Test public void testUsingVariable() throws Exception {
+    FreeStyleProject p = j.createFreeStyleProject();
+    p.addProperty(new ParametersDefinitionProperty(new StringParameterDefinition("RESTART", null)));
+    p.getBuildWrappersList().add(new BuildTimeoutWrapper(new QuickBuildTimeOutStrategy(1000), Arrays.<BuildTimeOutOperation>asList(new AbortAndRestartOperation("${RESTART}")), ""));
+    p.getBuildersList().add(new SleepBuilder(5 * 60 * 1000));
+    j.assertBuildStatus(Result.ABORTED, p.scheduleBuild2(0, new Cause.UserIdCause(), new ParametersAction(new StringParameterValue("RESTART", "1"))).get());
+    j.waitUntilNoActivityUpTo(25000);
+    assertEquals(2, p.getBuilds().size());
+  }
+=======
+>>>>>>> Unknown file: This is a bug in JDime.
 
-        testproject.scheduleBuild(new Cause.UserIdCause());
-        
-        j.waitUntilNoActivityUpTo(25000);
-        
-        assertNotNull(testproject.getFirstBuild());
-        assertFalse(testproject.getFirstBuild().equals(testproject.getLastBuild()));
-        assertEquals(testproject.getBuilds().size(), 3);
-        
-        assertEquals(Result.ABORTED, testproject.getBuildByNumber(1).getResult());
-        assertEquals(Result.ABORTED, testproject.getBuildByNumber(2).getResult());
-        assertEquals(Result.ABORTED, testproject.getBuildByNumber(3).getResult());
-    }
-    
-    @Test
-    public void usingVariable() throws Exception {
-        FreeStyleProject p = j.createFreeStyleProject();
-        // needed since Jenkins 2.3
-        p.addProperty(new ParametersDefinitionProperty(new StringParameterDefinition("RESTART", null)));
-        p.getBuildWrappersList().add(new BuildTimeoutWrapper(
-                     new QuickBuildTimeOutStrategy(1000),
-                     Arrays.<BuildTimeOutOperation>asList(new AbortAndRestartOperation("${RESTART}")),
-                     ""
-        ));
-        
-        p.getBuildersList().add(new SleepBuilder(5*60*1000)); //5 minutes
-        
-        j.assertBuildStatus(
-                Result.ABORTED,
-                p.scheduleBuild2(
-                        0,
-                        new Cause.UserIdCause(),
-                        new ParametersAction(new StringParameterValue("RESTART", "1"))
-                ).get()
-        );
-        j.waitUntilNoActivityUpTo(25000);
-        
-        assertEquals(2, p.getBuilds().size());
-    }
-    
-    @Test
-    public void usingBadRestart() throws Exception {
-        FreeStyleProject p = j.createFreeStyleProject();
-        p.getBuildWrappersList().add(new BuildTimeoutWrapper(
-                     new QuickBuildTimeOutStrategy(1000),
-                     Arrays.<BuildTimeOutOperation>asList(new AbortAndRestartOperation("${RESTART}")),
-                     ""
-        ));
-        
-        p.getBuildersList().add(new SleepBuilder(5*60*1000)); //5 minutes
-        
-        j.assertBuildStatus(
-                Result.ABORTED,
-                p.scheduleBuild2(
-                        0,
-                        new Cause.UserIdCause(),
-                        new ParametersAction(new StringParameterValue("RESTART", "xxx"))
-                ).get()
-        );
-        j.waitUntilNoActivityUpTo(25000);
-        
-        // Build is not restarted
-        assertEquals(1, p.getBuilds().size());
-    }
+
+  @Test public void usingVariable() throws Exception {
+    FreeStyleProject p = j.createFreeStyleProject();
+    p.getBuildWrappersList().add(new BuildTimeoutWrapper(new QuickBuildTimeOutStrategy(1000), Arrays.<BuildTimeOutOperation>asList(new AbortAndRestartOperation("${RESTART}")), ""));
+    p.getBuildersList().add(new SleepBuilder(5 * 60 * 1000));
+    j.assertBuildStatus(Result.ABORTED, p.scheduleBuild2(0, new Cause.UserIdCause(), new ParametersAction(new StringParameterValue("RESTART", "1"))).get());
+    j.waitUntilNoActivityUpTo(25000);
+    assertEquals(2, p.getBuilds().size());
+  }
+
+  @Test public void usingBadRestart() throws Exception {
+    FreeStyleProject p = j.createFreeStyleProject();
+    p.getBuildWrappersList().add(new BuildTimeoutWrapper(new QuickBuildTimeOutStrategy(1000), Arrays.<BuildTimeOutOperation>asList(new AbortAndRestartOperation("${RESTART}")), ""));
+    p.getBuildersList().add(new SleepBuilder(5 * 60 * 1000));
+    j.assertBuildStatus(Result.ABORTED, p.scheduleBuild2(0, new Cause.UserIdCause(), new ParametersAction(new StringParameterValue("RESTART", "xxx"))).get());
+    j.waitUntilNoActivityUpTo(25000);
+    assertEquals(1, p.getBuilds().size());
+  }
 }
-
