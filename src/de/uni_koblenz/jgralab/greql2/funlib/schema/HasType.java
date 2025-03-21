8ec71827c319b0cb24baa1112e770264b209054c
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 22:33:59 +0000

    Mining Framework Analysis

--- src/de/uni_koblenz/jgralab/greql2/funlib/schema/HasType.java
+++ src/de/uni_koblenz/jgralab/greql2/funlib/schema/HasType.java
@@ -1,5 +1,4 @@
 package de.uni_koblenz.jgralab.greql2.funlib.schema;
-
 import de.uni_koblenz.jgralab.AttributedElement;
 import de.uni_koblenz.jgralab.greql2.exception.GreqlException;
 import de.uni_koblenz.jgralab.greql2.funlib.Function;
@@ -7,30 +6,25 @@ import de.uni_koblenz.jgralab.greql2.types.TypeCollection;
 import de.uni_koblenz.jgralab.schema.AttributedElementClass;
 
 public class HasType extends Function {
+  public HasType() {
+    super("Returns true, iff the given attributed element or attributed element class has an attribute with the given name.", Category.SCHEMA_ACCESS);
+  }
 
-	public HasType() {
-		super(
-				"Returns true, iff the given attributed element or attributed element class has an attribute with the given name.",
-				Category.SCHEMA_ACCESS);
-	}
+  public Boolean evaluate(AttributedElement el, String qn) {
+    AttributedElementClass aec = el.getSchema().getAttributedElementClass(qn);
+    if (aec == null) {
+      throw new GreqlException("hasType: Schema doesn\'t contain a type \'");
+    }
+    return evaluate(el, aec);
+  }
 
-	public Boolean evaluate(AttributedElement el, String qn) {
-		AttributedElementClass aec = el.getSchema().getAttributedElementClass(
-				qn);
-		if (aec == null) {
-			throw new GreqlException("hasType: Schema doesn't contain a type '");
-		}
-		return evaluate(el, aec);
-	}
+  private Boolean evaluate(AttributedElement el, AttributedElementClass aec) {
+    AttributedElementClass c = el.getAttributedElementClass();
+    return c.equals(aec) || c.isSubClassOf(aec);
+  }
 
-	private Boolean evaluate(AttributedElement el, AttributedElementClass aec) {
-		AttributedElementClass c = el.getAttributedElementClass();
-		return c.equals(aec) || c.isSubClassOf(aec);
-	}
-
-	public Boolean evaluate(AttributedElement el, TypeCollection tc) {
-		AttributedElementClass c = el.getAttributedElementClass();
-		return tc.acceptsType(c);
-	}
-
+  public Boolean evaluate(AttributedElement el, TypeCollection tc) {
+    AttributedElementClass c = el.getAttributedElementClass();
+    return tc.acceptsType(c);
+  }
 }
