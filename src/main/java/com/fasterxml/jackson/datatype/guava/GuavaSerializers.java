82cd2667e6232e65494b0ae8fa6d6462f979341c
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 15:44:30 +0000

    Mining Framework Analysis

--- src/main/java/com/fasterxml/jackson/datatype/guava/GuavaSerializers.java
+++ src/main/java/com/fasterxml/jackson/datatype/guava/GuavaSerializers.java
@@ -1,7 +1,5 @@
 package com.fasterxml.jackson.datatype.guava;
-
 import java.util.HashSet;
-
 import com.fasterxml.jackson.databind.ser.std.StdDelegatingSerializer;
 import com.fasterxml.jackson.databind.type.TypeFactory;
 import com.fasterxml.jackson.databind.util.ArrayBuilders;
@@ -25,69 +23,52 @@ import com.fasterxml.jackson.datatype.guava.ser.GuavaOptionalSerializer;
 import com.fasterxml.jackson.datatype.guava.ser.MultimapSerializer;
 import com.fasterxml.jackson.datatype.guava.ser.RangeSerializer;
 
-public class GuavaSerializers extends Serializers.Base
-{
-    static class FluentConverter extends StdConverter<Object,Iterable<?>> {
-        static final FluentConverter instance = new FluentConverter();
+public class GuavaSerializers extends Serializers.Base {
+  static class FluentConverter extends StdConverter<Object, Iterable<?>> {
+    static final FluentConverter instance = new FluentConverter();
 
-        @Override
-        public Iterable<?> convert(Object value) {
-            return (Iterable<?>) value;
-        }
+    @Override public Iterable<?> convert(Object value) {
+      return (Iterable<?>) value;
     }
+  }
 
-    @Override
-    public JsonSerializer<?> findSerializer(SerializationConfig config, JavaType type, BeanDescription beanDesc)
-    {
-        Class<?> raw = type.getRawClass();
-        if (Optional.class.isAssignableFrom(raw)){
-            return new GuavaOptionalSerializer(type);
-        }
-        if (Range.class.isAssignableFrom(raw)) {
-            return new RangeSerializer(type);
-        }
-        // since 2.4
-        if (HostAndPort.class.isAssignableFrom(raw)) {
-            return ToStringSerializer.instance;
-        }
-        // since 2.4.3
-        if (InternetDomainName.class.isAssignableFrom(raw)) {
-            return ToStringSerializer.instance;
-        }
-        // not sure how useful, but why not?
-        if (CacheBuilderSpec.class.isAssignableFrom(raw) || CacheBuilder.class.isAssignableFrom(raw)) {
-            return ToStringSerializer.instance;
-        }
-        if (HashCode.class.isAssignableFrom(raw)) {
-            return ToStringSerializer.instance;
-	}
-        // since 2.4.5
-        if (FluentIterable.class.isAssignableFrom(raw)) {
-            JavaType[] params = config.getTypeFactory().findTypeParameters(type, Iterable.class);
-            JavaType vt = (params == null || params.length != 1) ? TypeFactory.unknownType() : params[0];
-            // 04-Dec-2014, tatu: Not 100% sure why latter would fail... need to investigate when I have time
-            JavaType delegate = config.getTypeFactory().constructParametrizedType(Iterable.class, Iterable.class, vt);
-//            JavaType delegate = config.getTypeFactory().constructParametrizedType(FluentIterable.class, Iterable.class, vt);
-            return new StdDelegatingSerializer(FluentConverter.instance, delegate, null);
-        }
-        return super.findSerializer(config, type, beanDesc);
+  @Override public JsonSerializer<?> findSerializer(SerializationConfig config, JavaType type, BeanDescription beanDesc) {
+    Class<?> raw = type.getRawClass();
+    if (Optional.class.isAssignableFrom(raw)) {
+      return new GuavaOptionalSerializer(type);
     }
-
-    @Override
-    public JsonSerializer<?> findMapLikeSerializer(SerializationConfig config,
-            MapLikeType type, BeanDescription beanDesc, JsonSerializer<Object> keySerializer,
-            TypeSerializer elementTypeSerializer, JsonSerializer<Object> elementValueSerializer)
-    {
-        if (Multimap.class.isAssignableFrom(type.getRawClass())) {
-            final AnnotationIntrospector intr = config.getAnnotationIntrospector();
-            Object filterId = intr.findFilterId((Annotated)beanDesc.getClassInfo());
-            String[] ignored = intr.findPropertiesToIgnore(beanDesc.getClassInfo());
-            HashSet<String> ignoredEntries = (ignored == null || ignored.length == 0)
-                    ? null : ArrayBuilders.arrayToSet(ignored);
-
-            return new MultimapSerializer(type, beanDesc,
-                    keySerializer, elementTypeSerializer, elementValueSerializer, ignoredEntries, filterId);
-        }
-        return null;
+    if (Range.class.isAssignableFrom(raw)) {
+      return new RangeSerializer(type);
     }
+    if (HostAndPort.class.isAssignableFrom(raw)) {
+      return ToStringSerializer.instance;
+    }
+    if (InternetDomainName.class.isAssignableFrom(raw)) {
+      return ToStringSerializer.instance;
+    }
+    if (CacheBuilderSpec.class.isAssignableFrom(raw) || CacheBuilder.class.isAssignableFrom(raw)) {
+      return ToStringSerializer.instance;
+    }
+    if (HashCode.class.isAssignableFrom(raw)) {
+      return ToStringSerializer.instance;
+    }
+    if (FluentIterable.class.isAssignableFrom(raw)) {
+      JavaType[] params = config.getTypeFactory().findTypeParameters(type, Iterable.class);
+      JavaType vt = (params == null || params.length != 1) ? TypeFactory.unknownType() : params[0];
+      JavaType delegate = config.getTypeFactory().constructParametrizedType(Iterable.class, Iterable.class, vt);
+      return new StdDelegatingSerializer(FluentConverter.instance, delegate, null);
+    }
+    return super.findSerializer(config, type, beanDesc);
+  }
+
+  @Override public JsonSerializer<?> findMapLikeSerializer(SerializationConfig config, MapLikeType type, BeanDescription beanDesc, JsonSerializer<Object> keySerializer, TypeSerializer elementTypeSerializer, JsonSerializer<Object> elementValueSerializer) {
+    if (Multimap.class.isAssignableFrom(type.getRawClass())) {
+      final AnnotationIntrospector intr = config.getAnnotationIntrospector();
+      Object filterId = intr.findFilterId((Annotated) beanDesc.getClassInfo());
+      String[] ignored = intr.findPropertiesToIgnore(beanDesc.getClassInfo());
+      HashSet<String> ignoredEntries = (ignored == null || ignored.length == 0) ? null : ArrayBuilders.arrayToSet(ignored);
+      return new MultimapSerializer(type, beanDesc, keySerializer, elementTypeSerializer, elementValueSerializer, ignoredEntries, filterId);
+    }
+    return null;
+  }
 }
