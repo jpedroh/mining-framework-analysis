6927f555e76f785227504a8143057808bc0c971c
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 09:07:34 +0000

    Mining Framework Analysis

--- src/main/java/gestores/GestorBD.java
+++ src/main/java/gestores/GestorBD.java
@@ -95,7 +95,7 @@ public class GestorBD {
             ex.printStackTrace();
         } catch (ClassNotFoundException ex) {
             response.setMsgStatus("Error");
-            response.setMsgError("Error al establecer conexión con base de datos.");
+            response.setMsgError("Error al establecer conexiï¿½n con base de datos.");
             //error de sintaxis de sql
             ex.printStackTrace();
         }
