0e0b6e1a5dd47d8c44adef2ca02cd5ab3f8870c9
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 09:24:56 +0000

    Mining Framework Analysis

--- src/test/java/org/buddycloud/channelserver/db/jdbc/JDBCNodeStoreMembershipTest.java
+++ src/test/java/org/buddycloud/channelserver/db/jdbc/JDBCNodeStoreMembershipTest.java
@@ -1,12 +1,9 @@
 package org.buddycloud.channelserver.db.jdbc;
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.Date;
-
 import org.buddycloud.channelserver.packetHandler.iq.IQTestHandler;
 import org.buddycloud.channelserver.pubsub.affiliation.Affiliations;
 import org.buddycloud.channelserver.pubsub.model.NodeMembership;
@@ -15,73 +12,45 @@ import org.buddycloud.channelserver.pubsub.subscription.Subscriptions;
 import org.junit.Test;
 
 public class JDBCNodeStoreMembershipTest extends JDBCNodeStoreAbstract {
+  public JDBCNodeStoreMembershipTest() throws SQLException, IOException, ClassNotFoundException {
+    dbTester = new DatabaseTester();
+    IQTestHandler.readConf();
+  }
 
-	public JDBCNodeStoreMembershipTest() throws SQLException, IOException,
-			ClassNotFoundException {
-		dbTester = new DatabaseTester();
-		IQTestHandler.readConf();
-	}
+  @Test public void canGetNodeMembership() throws Exception {
+    dbTester.loadData("node_1");
+    NodeMembership result = store.getNodeMembership(TEST_SERVER1_NODE1_ID, TEST_SERVER1_USER1_JID);
+    NodeMembership expected = new NodeMembershipImpl(TEST_SERVER1_NODE1_ID, TEST_SERVER1_USER1_JID, Subscriptions.subscribed, Affiliations.owner, null);
+    assertEquals("An unexpected node membership was returned", expected, result);
+  }
 
-	@Test
-	public void canGetNodeMembership() throws Exception {
-		dbTester.loadData("node_1");
+  @Test public void canGetNodeMembershipWhereTheresOnlySubscription() throws Exception {
+    dbTester.loadData("node_1");
+    store.deleteUserAffiliations(TEST_SERVER1_USER1_JID);
+    NodeMembership result = store.getNodeMembership(TEST_SERVER1_NODE1_ID, TEST_SERVER1_USER1_JID);
+    NodeMembership expected = new NodeMembershipImpl(TEST_SERVER1_NODE1_ID, TEST_SERVER1_USER1_JID, Subscriptions.subscribed, Affiliations.none, null);
+    assertEquals("An unexpected node membership was returned", expected, result);
+  }
 
-		NodeMembership result = store.getNodeMembership(
-				TEST_SERVER1_NODE1_ID, TEST_SERVER1_USER1_JID);
+  @Test public void canGetNodeMembershipWhereTheresOnlyAffiliation() throws Exception {
+    dbTester.loadData("node_1");
+    store.deleteUserSubscriptions(TEST_SERVER1_USER1_JID);
+    NodeMembership result = store.getNodeMembership(TEST_SERVER1_NODE1_ID, TEST_SERVER1_USER1_JID);
+    NodeMembership expected = new NodeMembershipImpl(TEST_SERVER1_NODE1_ID, TEST_SERVER1_USER1_JID, Subscriptions.none, Affiliations.none, null);
+    assertEquals("An unexpected node membership was returned", expected, result);
+  }
 
-		NodeMembership expected = new NodeMembershipImpl(
-				TEST_SERVER1_NODE1_ID, TEST_SERVER1_USER1_JID,
-				Subscriptions.subscribed, Affiliations.owner, null);
+  @Test public void canGetNodeMembershipWhereTheresNoMembership() throws Exception {
+    NodeMembership result = store.getNodeMembership(TEST_SERVER1_NODE1_ID, TEST_SERVER1_USER1_JID);
+    NodeMembership expected = new NodeMembershipImpl(TEST_SERVER1_NODE1_ID, TEST_SERVER1_USER1_JID, Subscriptions.none, Affiliations.none, null);
+    assertEquals("An unexpected node membership was returned", expected, result);
+  }
 
-		assertEquals("An unexpected node membership was returned", expected,
-				result);
-	}
-	
-	@Test
-	public void canGetNodeMembershipWhereTheresOnlySubscription() throws Exception {
-		dbTester.loadData("node_1");
-
-		store.deleteUserAffiliations(TEST_SERVER1_USER1_JID);
-		
-		NodeMembership result = store.getNodeMembership(
-				TEST_SERVER1_NODE1_ID, TEST_SERVER1_USER1_JID);
-
-		NodeMembership expected = new NodeMembershipImpl(
-				TEST_SERVER1_NODE1_ID, TEST_SERVER1_USER1_JID,
-				Subscriptions.subscribed, Affiliations.none, null);
-
-		assertEquals("An unexpected node membership was returned", expected,
-				result);
-	}
-	
-	@Test
-	public void canGetNodeMembershipWhereTheresNoMembership() throws Exception {
-		
-		NodeMembership result = store.getNodeMembership(
-				TEST_SERVER1_NODE1_ID, TEST_SERVER1_USER1_JID);
-
-		NodeMembership expected = new NodeMembershipImpl(
-				TEST_SERVER1_NODE1_ID, TEST_SERVER1_USER1_JID,
-				Subscriptions.none, Affiliations.none, null);
-
-		assertEquals("An unexpected node membership was returned", expected,
-				result);
-	}
-	
-	@Test
-	public void querySelectsTheMostRecentUpdatedDate() throws Exception {
-		dbTester.loadData("node_1");
-		Date originalDate = store.getNodeMembership(
-				TEST_SERVER1_NODE1_ID, TEST_SERVER1_USER1_JID).getLastUpdated();
-        
-        store.setUserAffiliation(
-    		TEST_SERVER1_NODE1_ID,
-    		TEST_SERVER1_USER1_JID,
-    		Affiliations.owner
-        );
-		Date newDate = store.getNodeMembership(
-				TEST_SERVER1_NODE1_ID, TEST_SERVER1_USER1_JID).getLastUpdated();
-
-		assertTrue(newDate.after(originalDate));
-	}
+  @Test public void querySelectsTheMostRecentUpdatedDate() throws Exception {
+    dbTester.loadData("node_1");
+    Date originalDate = store.getNodeMembership(TEST_SERVER1_NODE1_ID, TEST_SERVER1_USER1_JID).getLastUpdated();
+    store.setUserAffiliation(TEST_SERVER1_NODE1_ID, TEST_SERVER1_USER1_JID, Affiliations.owner);
+    Date newDate = store.getNodeMembership(TEST_SERVER1_NODE1_ID, TEST_SERVER1_USER1_JID).getLastUpdated();
+    assertTrue(newDate.after(originalDate));
+  }
 }
--- src/test/java/org/buddycloud/channelserver/db/jdbc/JDBCNodeStoreUserMembershipsTest.java
+++ src/test/java/org/buddycloud/channelserver/db/jdbc/JDBCNodeStoreUserMembershipsTest.java
@@ -1,13 +1,10 @@
 package org.buddycloud.channelserver.db.jdbc;
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.Date;
 import java.util.HashSet;
-
 import org.apache.commons.collections.CollectionUtils;
 import org.buddycloud.channelserver.packetHandler.iq.IQTestHandler;
 import org.buddycloud.channelserver.pubsub.affiliation.Affiliations;
@@ -18,88 +15,65 @@ import org.junit.Test;
 import org.xmpp.resultsetmanagement.ResultSet;
 
 public class JDBCNodeStoreUserMembershipsTest extends JDBCNodeStoreAbstract {
+  public JDBCNodeStoreUserMembershipsTest() throws SQLException, IOException, ClassNotFoundException {
+    dbTester = new DatabaseTester();
+    IQTestHandler.readConf();
+  }
 
-	public JDBCNodeStoreUserMembershipsTest() throws SQLException, IOException,
-			ClassNotFoundException {
-		dbTester = new DatabaseTester();
-		IQTestHandler.readConf();
-	}
+  @Test public void getUserMemberships() throws Exception {
+    dbTester.loadData("node_1");
+    dbTester.loadData("node_2");
+    ResultSet<NodeMembership> result = store.getUserMemberships(TEST_SERVER1_USER1_JID);
+    HashSet<NodeMembership> expected = new HashSet<NodeMembership>() {
+      {
+        add(new NodeMembershipImpl(TEST_SERVER1_NODE1_ID, TEST_SERVER1_USER1_JID, Subscriptions.subscribed, Affiliations.owner, null, new Date()));
+        add(new NodeMembershipImpl(TEST_SERVER1_NODE2_ID, TEST_SERVER1_USER1_JID, Subscriptions.subscribed, Affiliations.publisher, null, new Date()));
+      }
+    };
+    assertEquals("Incorrect number of user memberships returned", expected.size(), result.size());
+    assertTrue("Incorrect user memberships returned", CollectionUtils.isEqualCollection(expected, result));
+  }
 
-	@Test
-	public void getUserMemberships() throws Exception {
-		dbTester.loadData("node_1");
-		dbTester.loadData("node_2");
+  @Test public void getUserMembershipsUsesBareJID() throws Exception {
+    dbTester.loadData("node_1");
+    dbTester.loadData("node_2");
+    ResultSet<NodeMembership> result = store.getUserMemberships(TEST_SERVER1_USER1_JID_WITH_RESOURCE);
+    HashSet<NodeMembership> expected = new HashSet<NodeMembership>() {
+      {
+        add(new NodeMembershipImpl(TEST_SERVER1_NODE1_ID, TEST_SERVER1_USER1_JID, Subscriptions.subscribed, Affiliations.owner, null, new Date()));
+        add(new NodeMembershipImpl(TEST_SERVER1_NODE2_ID, TEST_SERVER1_USER1_JID, Subscriptions.subscribed, Affiliations.publisher, null, new Date()));
+      }
+    };
+    assertEquals("Incorrect number of user memberships returned", expected.size(), result.size());
+    assertTrue("Incorrect user memberships returned", CollectionUtils.isEqualCollection(expected, result));
+  }
 
-		ResultSet<NodeMembership> result = store
-				.getUserMemberships(TEST_SERVER1_USER1_JID);
+  @Test public void canGetUserMembershipWhereTheresOnlySubscription() throws Exception {
+    dbTester.loadData("node_1");
+    store.deleteUserAffiliations(TEST_SERVER1_USER1_JID);
+    ResultSet<NodeMembership> result = store.getUserMemberships(TEST_SERVER1_USER1_JID);
+    NodeMembership expected = new NodeMembershipImpl(TEST_SERVER1_NODE1_ID, TEST_SERVER1_USER1_JID, Subscriptions.subscribed, Affiliations.none, null);
+    assertEquals("An unexpected user membership was returned", expected, result.get(0));
+  }
 
-		HashSet<NodeMembership> expected = new HashSet<NodeMembership>() {
-			{
-				add(new NodeMembershipImpl(TEST_SERVER1_NODE1_ID,
-						TEST_SERVER1_USER1_JID, Subscriptions.subscribed, Affiliations.owner, null, new Date()));
-				add(new NodeMembershipImpl(TEST_SERVER1_NODE2_ID,
-						TEST_SERVER1_USER1_JID, Subscriptions.subscribed, Affiliations.publisher, null,
-						new Date()));
-			}
-		};
 
-		assertEquals("Incorrect number of user memberships returned",
-				expected.size(), result.size());
-		assertTrue("Incorrect user memberships returned",
-				CollectionUtils.isEqualCollection(expected, result));
-	}
+<<<<<<< Unknown file: This is a bug in JDime.
+=======
+  @Test public void canGetUserMembershipWhereTheresOnlyAffiliation() throws Exception {
+    dbTester.loadData("node_1");
+    store.deleteUserSubscriptions(TEST_SERVER1_USER1_JID);
+    ResultSet<NodeMembership> result = store.getUserMemberships(TEST_SERVER1_USER1_JID);
+    NodeMembership expected = new NodeMembershipImpl(TEST_SERVER1_NODE1_ID, TEST_SERVER1_USER1_JID, Subscriptions.none, Affiliations.owner, null);
+    assertEquals("An unexpected user membership was returned", expected, result.get(0));
+  }
+>>>>>>> /usr/src/app/output/buddycloud/buddycloud-server-java/c0daab08a4d3af4ab7be092cd53a9fba7d51eaf5/src/test/java/org/buddycloud/channelserver/db/jdbc/JDBCNodeStoreUserMembershipsTest.java/right.java
 
-	@Test
-	public void getUserMembershipsUsesBareJID() throws Exception {
-		dbTester.loadData("node_1");
-		dbTester.loadData("node_2");
 
-		ResultSet<NodeMembership> result = store
-				.getUserMemberships(TEST_SERVER1_USER1_JID_WITH_RESOURCE);
-
-		HashSet<NodeMembership> expected = new HashSet<NodeMembership>() {
-			{
-				add(new NodeMembershipImpl(TEST_SERVER1_NODE1_ID,
-						TEST_SERVER1_USER1_JID, Subscriptions.subscribed, Affiliations.owner, null, new Date()));
-				add(new NodeMembershipImpl(TEST_SERVER1_NODE2_ID,
-						TEST_SERVER1_USER1_JID, Subscriptions.subscribed, Affiliations.publisher, null,
-						new Date()));
-			}
-		};
-
-		assertEquals("Incorrect number of user memberships returned",
-				expected.size(), result.size());
-		assertTrue("Incorrect user memberships returned",
-				CollectionUtils.isEqualCollection(expected, result));
-	}
-	@Test
-	public void canGetUserMembershipWhereTheresOnlySubscription() throws Exception {
-		dbTester.loadData("node_1");
-
-		store.deleteUserAffiliations(TEST_SERVER1_USER1_JID);
-		
-		ResultSet<NodeMembership> result = store.getUserMemberships(TEST_SERVER1_USER1_JID);
-
-		NodeMembership expected = new NodeMembershipImpl(TEST_SERVER1_NODE1_ID, TEST_SERVER1_USER1_JID,
-				Subscriptions.subscribed, Affiliations.none, null);
-
-		assertEquals("An unexpected user membership was returned", expected,
-				result.get(0));
-	}
-	
-	@Test
-	public void querySelectsTheMostRecentUpdatedDate() throws Exception {
-		dbTester.loadData("node_1");
-		Date originalDate = store.getUserMemberships(TEST_SERVER1_USER1_JID).get(0).getLastUpdated();
-        
-        store.setUserAffiliation(
-    		TEST_SERVER1_NODE1_ID,
-    		TEST_SERVER1_USER1_JID,
-    		Affiliations.owner
-        );
-		Date newDate = store.getNodeMembership(
-				TEST_SERVER1_NODE1_ID, TEST_SERVER1_USER1_JID).getLastUpdated();
-
-		assertTrue(newDate.after(originalDate));
-	}
+  @Test public void querySelectsTheMostRecentUpdatedDate() throws Exception {
+    dbTester.loadData("node_1");
+    Date originalDate = store.getUserMemberships(TEST_SERVER1_USER1_JID).get(0).getLastUpdated();
+    store.setUserAffiliation(TEST_SERVER1_NODE1_ID, TEST_SERVER1_USER1_JID, Affiliations.owner);
+    Date newDate = store.getNodeMembership(TEST_SERVER1_NODE1_ID, TEST_SERVER1_USER1_JID).getLastUpdated();
+    assertTrue(newDate.after(originalDate));
+  }
 }
