ae8e18a581494945fcb682d0e628400faeb50ab3
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 10:38:26 +0000

    Mining Framework Analysis

--- src/main/java/gov/nysenate/openleg/api/ui/ReactAppCtrl.java
+++ src/main/java/gov/nysenate/openleg/api/ui/ReactAppCtrl.java
@@ -1,5 +1,4 @@
 package gov.nysenate.openleg.api.ui;
-
 import gov.nysenate.openleg.api.auth.AuthedUser;
 import gov.nysenate.openleg.api.response.BaseResponse;
 import gov.nysenate.openleg.api.response.ViewObjectResponse;
@@ -19,7 +18,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
@@ -28,85 +26,54 @@ import java.util.Map;
  * Simple entry point to the front-end. Returns the main react app which will handle
  * all the heavy lifting.
  */
-@Controller
-public class ReactAppCtrl {
-    private static final Logger logger = LoggerFactory.getLogger(ReactAppCtrl.class);
+@Controller public class ReactAppCtrl {
+  private static final Logger logger = LoggerFactory.getLogger(ReactAppCtrl.class);
 
-    @Autowired
-    private OpenLegEnvironment environment;
+  @Autowired private OpenLegEnvironment environment;
 
-    @Value("${ga.tracking.id}")
-    private String gaTrackingId;
-    @Value("${api.auth.ip.whitelist}")
-    private String ipWhitelist;
+  @Value(value = "${ga.tracking.id}") private String gaTrackingId;
 
-    @RequestMapping({"/",
-            "/data/**",
-            "/bills/**",
-            "/calendars/**",
-            "/agendas/**",
-            "/transcripts/**",
-            "/members/**",
-            "/laws/**",
-            "/sources/**",
-            "/reports/**",
-            "/manage/**",
-            "/admin/**",
-            "/register/**",
-            "/subscriptions/**",
-            "/public",
-    })
-    public String home(HttpServletRequest request) {
-        setRequestAttributes(request);
-        return "index";
+  @Value(value = "${api.auth.ip.whitelist}") private String ipWhitelist;
+
+  @RequestMapping(value = { "/", "/data/**", "/bills/**", "/calendars/**", "/agendas/**", "/transcripts/**", "/members/**", "/laws/**", "/sources/**", "/reports/**", "/manage/**", "/admin/**", "/register/**", "/subscriptions/**", "/public" }) public String home(HttpServletRequest request) {
+    setRequestAttributes(request);
+    return "index";
+  }
+
+  @ResponseBody @RequestMapping(value = "/loginapikey", method = RequestMethod.POST) public BaseResponse login(@RequestBody Map<String, String> body, HttpServletRequest request) {
+    String ipAddr = request.getRemoteAddr();
+    String apiKey = body.get("apiKey");
+    try {
+      SecurityUtils.getSubject().login(new ApiKeyLoginToken(apiKey, ipAddr));
+      return new ViewObjectResponse<>(new AuthedUser(true, false));
+    } catch (AuthenticationException ex) {
+      logger.info("Invalid API Key attempt with key: {}", apiKey);
     }
+    return new ErrorResponse(ErrorCode.API_KEY_INVALID);
+  }
 
-    @ResponseBody
-    @RequestMapping(value = "/loginapikey", method = RequestMethod.POST)
-    public BaseResponse login(@RequestBody Map<String, String> body, HttpServletRequest request) {
-        String ipAddr = request.getRemoteAddr();
-        String apiKey = body.get("apiKey");
-        try {
-            SecurityUtils.getSubject().login(new ApiKeyLoginToken(apiKey, ipAddr));
-            return new ViewObjectResponse<>(new AuthedUser(true, false));
-        } catch (AuthenticationException ex) {
-            logger.info("Invalid API Key attempt with key: {}", apiKey);
-        }
-        return new ErrorResponse(ErrorCode.API_KEY_INVALID);
+  @ResponseBody @RequestMapping(value = "/admin/login", method = RequestMethod.POST) public BaseResponse loginAdmin(@RequestBody Map<String, String> body, HttpServletRequest request) {
+    String username = body.get("username");
+    String password = body.get("password");
+    String host = request.getRemoteAddr();
+    try {
+      SecurityUtils.getSubject().login(new UsernamePasswordToken(username, password, host));
+      return new ViewObjectResponse<>(new AuthedUser(true, true));
+    } catch (AuthenticationException ex) {
+      return new ErrorResponse(ErrorCode.UNAUTHORIZED);
     }
+  }
 
-    @ResponseBody
-    @RequestMapping(value = "/admin/login", method = RequestMethod.POST)
-    public BaseResponse loginAdmin(@RequestBody Map<String, String> body, HttpServletRequest request) {
-        String username = body.get("username");
-        String password = body.get("password");
-        String host = request.getRemoteAddr();
-        try {
-            SecurityUtils.getSubject().login(new UsernamePasswordToken(username, password, host));
-            return new ViewObjectResponse<>(new AuthedUser(true, true));
-        } catch (AuthenticationException ex) {
-            return new ErrorResponse(ErrorCode.UNAUTHORIZED);
-        }
-    }
+  @ResponseBody @RequestMapping(value = "/globals") public BaseResponse globals(HttpServletRequest request) {
+    String forwardedForIp = request.getHeader("x-forwarded-for");
+    String ipAddr = forwardedForIp == null ? request.getRemoteAddr() : forwardedForIp;
+    GlobalsView gv = new GlobalsView(ipWhitelist, ipAddr, environment.getSenSiteUrl(), environment.getOpenlegRefUrl());
+    return new ViewObjectResponse<>(gv);
+  }
 
-    @ResponseBody
-    @RequestMapping("/globals")
-    public BaseResponse globals(HttpServletRequest request) {
-        String forwardedForIp = request.getHeader("x-forwarded-for");
-        String ipAddr = forwardedForIp == null ? request.getRemoteAddr() : forwardedForIp;
-        GlobalsView gv = new GlobalsView(ipWhitelist, ipAddr,
-                environment.getSenSiteUrl(), environment.getOpenlegRefUrl());
-        return new ViewObjectResponse<>(gv);
-    }
-
-    /* --- Internal Methods --- */
-
-    private void setRequestAttributes(ServletRequest request) {
-        // Google Analytics
-        request.setAttribute("gaTrackingId", gaTrackingId);
-        // NYSenate.gov url
-        request.setAttribute("senSitePath", environment.getSenSiteUrl());
-        // Openleg reference URL
-        request.setAttribute("openlegRefPath", environment.getOpenlegRefUrl());
-    }
+  private void setRequestAttributes(ServletRequest request) {
+    request.setAttribute("gaTrackingId", gaTrackingId);
+    request.setAttribute("senSitePath", environment.getSenSiteUrl());
+    request.setAttribute("openlegRefPath", environment.getOpenlegRefUrl());
+  }
 }
