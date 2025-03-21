afb3e8f74d47d33e9943ef3f783e403bf628e291
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 15:18:56 +0000

    Mining Framework Analysis

--- ninja-core/src/main/java/ninja/template/directives/TemplateEngineFreemarkerAuthenticityFormDirective.java
+++ ninja-core/src/main/java/ninja/template/directives/TemplateEngineFreemarkerAuthenticityFormDirective.java
@@ -1,9 +1,7 @@
 package ninja.template.directives;
-
 import java.io.IOException;
 import java.io.Writer;
 import java.util.Map;
-
 import ninja.Context;
 import ninja.utils.NinjaConstant;
 import freemarker.core.Environment;
@@ -13,26 +11,27 @@ import freemarker.template.TemplateException;
 import freemarker.template.TemplateModel;
 import freemarker.template.TemplateModelException;
 
+@SuppressWarnings(value = { "rawtypes" }) public class TemplateEngineFreemarkerAuthenticityFormDirective implements TemplateDirectiveModel {
+  private Context context;
 
-@SuppressWarnings("rawtypes")
-public class TemplateEngineFreemarkerAuthenticityFormDirective implements TemplateDirectiveModel {
-    private Context context;
+  public TemplateEngineFreemarkerAuthenticityFormDirective(Context context) {
+    this.context = context;
+  }
 
-    public TemplateEngineFreemarkerAuthenticityFormDirective(Context context) {
-        this.context = context;
+  @Override public void execute(Environment env, Map params, TemplateModel[] loopVars, TemplateDirectiveBody body) throws TemplateException, IOException {
+    if (!params.isEmpty()) {
+      throw new TemplateModelException("This directive doesn\'t allow parameters.");
     }
-    
-    @Override
-    public void execute(Environment env, Map params, TemplateModel[] loopVars, TemplateDirectiveBody body) throws TemplateException, IOException {
-        if (!params.isEmpty()) {
-            throw new TemplateModelException("This directive doesn't allow parameters.");
-        }
-
-        if (loopVars.length != 0) {
-            throw new TemplateModelException("This directive doesn't allow loop variables.");
-        }
-
-        Writer out = env.getOut();
-        out.append("<input type=\"hidden\" value=\"" + context.getSession().getAuthenticityToken() + "\" name=\"" + NinjaConstant.AUTHENTICITY_TOKEN +  "\" />");
+    if (loopVars.length != 0) {
+      throw new TemplateModelException("This directive doesn\'t allow loop variables.");
     }
+    Writer out = env.getOut();
+    out.append("<input type=\"hidden\" value=\"" + context.
+<<<<<<< /usr/src/app/output/ninjaframework/ninja/fe1237f21fd065e4a834be262563600e8da2f8b4/ninja-core/src/main/java/ninja/template/directives/TemplateEngineFreemarkerAuthenticityFormDirective.java/left.java
+    getSession().getAuthenticityToken()
+=======
+    context.getSession().getAuthenticityToken()
+>>>>>>> /usr/src/app/output/ninjaframework/ninja/fe1237f21fd065e4a834be262563600e8da2f8b4/ninja-core/src/main/java/ninja/template/directives/TemplateEngineFreemarkerAuthenticityFormDirective.java/right.java
+     + "\" name=\"" + NinjaConstant.AUTHENTICITY_TOKEN + "\" />");
+  }
 }
