665d7b9492d85731feb75afa33414b5b34fa03c4
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 23:08:58 +0000

    Mining Framework Analysis

--- modules/swagger-codegen/src/test/java/io/swagger/codegen/csharp/CSharpClientOptionsTest.java
+++ modules/swagger-codegen/src/test/java/io/swagger/codegen/csharp/CSharpClientOptionsTest.java
@@ -1,34 +1,6 @@
-package io.swagger.codegen.csharp;
-
-import io.swagger.codegen.AbstractOptionsTest;
-import io.swagger.codegen.CodegenConfig;
-import io.swagger.codegen.languages.CSharpClientCodegen;
-import io.swagger.codegen.options.CSharpClientOptionsProvider;
-
-import mockit.Expectations;
-import mockit.Tested;
-
-public class CSharpClientOptionsTest extends AbstractOptionsTest {
-
-    @Tested
-    private CSharpClientCodegen clientCodegen;
-
-    public CSharpClientOptionsTest() {
-        super(new CSharpClientOptionsProvider());
-    }
-
-    @Override
-    protected CodegenConfig getCodegenConfig() {
-        return clientCodegen;
-    }
-
-    @Override
-    protected void setExpectations() {
-        new Expectations(clientCodegen) {{
-            clientCodegen.setPackageName(CSharpClientOptionsProvider.PACKAGE_NAME_VALUE);
-            times = 1;
-            clientCodegen.setPackageVersion(CSharpClientOptionsProvider.PACKAGE_VERSION_VALUE);
-            times = 1;
-        }};
-    }
-}
+  package    io . swagger . codegen . csharp ;   import    io . swagger . codegen . AbstractOptionsTest ;  import    io . swagger . codegen . CodegenConfig ;  import     io . swagger . codegen . languages . CSharpClientCodegen ;  import  mockit . Expectations ;  import  mockit . Tested ;  import     io . swagger . codegen . options . CSharpClientOptionsProvider ;   public class CSharpClientOptionsTest  extends AbstractOptionsTest  {    @ Tested private CSharpClientCodegen  clientCodegen ;    @ Override protected CodegenConfig getCodegenConfig  ( )  {  return clientCodegen ; }    @ Override protected void setExpectations  ( )  {   new Expectations  ( clientCodegen )  {  {   clientCodegen . setPackageName  (  CSharpClientOptionsProvider . PACKAGE_NAME_VALUE ) ;   times = 1 ;   clientCodegen . setPackageVersion  (  CSharpClientOptionsProvider . PACKAGE_VERSION_VALUE ) ;   times = 1 ; } } ; } 
+<<<<<<<
+   @ Override protected  Map  < String , String > getAvaliableOptions  ( )  {    ImmutableMap . Builder  < String , String >  builder =  new   ImmutableMap . Builder  < String , String >  ( ) ;  return     builder . put  (  CodegenConstants . PACKAGE_NAME , PACKAGE_NAME_VALUE ) . put  (  CodegenConstants . PACKAGE_VERSION , PACKAGE_VERSION_VALUE ) . put  (  CodegenConstants . SORT_PARAMS_BY_REQUIRED_FLAG , "true" ) . build  ( ) ; }
+=======
+>>>>>>>
+   public CSharpClientOptionsTest  ( )  {  super  (  new CSharpClientOptionsProvider  ( ) ) ; } }
--- modules/swagger-codegen/src/test/java/io/swagger/codegen/python/PythonClientOptionsTest.java
+++ modules/swagger-codegen/src/test/java/io/swagger/codegen/python/PythonClientOptionsTest.java
@@ -1,34 +1,6 @@
-package io.swagger.codegen.python;
-
-import io.swagger.codegen.AbstractOptionsTest;
-import io.swagger.codegen.CodegenConfig;
-import io.swagger.codegen.languages.PythonClientCodegen;
-import io.swagger.codegen.options.PythonClientOptionsProvider;
-
-import mockit.Expectations;
-import mockit.Tested;
-
-public class PythonClientOptionsTest extends AbstractOptionsTest {
-
-    @Tested
-    private PythonClientCodegen clientCodegen;
-
-    public PythonClientOptionsTest() {
-        super(new PythonClientOptionsProvider());
-    }
-
-    @Override
-    protected CodegenConfig getCodegenConfig() {
-        return clientCodegen;
-    }
-
-    @Override
-    protected void setExpectations() {
-        new Expectations(clientCodegen) {{
-            clientCodegen.setPackageName(PythonClientOptionsProvider.PACKAGE_NAME_VALUE);
-            times = 1;
-            clientCodegen.setPackageVersion(PythonClientOptionsProvider.PACKAGE_VERSION_VALUE);
-            times = 1;
-        }};
-    }
-}
+  package    io . swagger . codegen . python ;   import    io . swagger . codegen . AbstractOptionsTest ;  import    io . swagger . codegen . CodegenConfig ;  import     io . swagger . codegen . languages . PythonClientCodegen ;  import  mockit . Expectations ;  import  mockit . Tested ;  import     io . swagger . codegen . options . PythonClientOptionsProvider ;   public class PythonClientOptionsTest  extends AbstractOptionsTest  {    @ Tested private PythonClientCodegen  clientCodegen ;    @ Override protected CodegenConfig getCodegenConfig  ( )  {  return clientCodegen ; }    @ Override protected void setExpectations  ( )  {   new Expectations  ( clientCodegen )  {  {   clientCodegen . setPackageName  (  PythonClientOptionsProvider . PACKAGE_NAME_VALUE ) ;   times = 1 ;   clientCodegen . setPackageVersion  (  PythonClientOptionsProvider . PACKAGE_VERSION_VALUE ) ;   times = 1 ; } } ; } 
+<<<<<<<
+   @ Override protected  Map  < String , String > getAvaliableOptions  ( )  {    ImmutableMap . Builder  < String , String >  builder =  new   ImmutableMap . Builder  < String , String >  ( ) ;  return     builder . put  (  CodegenConstants . PACKAGE_NAME , PACKAGE_NAME_VALUE ) . put  (  CodegenConstants . PACKAGE_VERSION , PACKAGE_VERSION_VALUE ) . put  (  CodegenConstants . SORT_PARAMS_BY_REQUIRED_FLAG , "true" ) . build  ( ) ; }
+=======
+>>>>>>>
+   public PythonClientOptionsTest  ( )  {  super  (  new PythonClientOptionsProvider  ( ) ) ; } }
