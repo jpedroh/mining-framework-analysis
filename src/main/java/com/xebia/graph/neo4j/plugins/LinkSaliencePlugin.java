db4f09c3fd12a6a09396bb369e3135e7da90d2d5
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 00:33:51 +0000

    Mining Framework Analysis

--- src/main/java/com/xebia/graph/neo4j/plugins/LinkSaliencePlugin.java
+++ src/main/java/com/xebia/graph/neo4j/plugins/LinkSaliencePlugin.java
@@ -1,5 +1,4 @@
 package com.xebia.graph.neo4j.plugins;
-
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.server.plugins.Description;
 import org.neo4j.server.plugins.Name;
@@ -8,38 +7,19 @@ import org.neo4j.server.plugins.PluginTarget;
 import org.neo4j.server.plugins.ServerPlugin;
 import org.neo4j.server.plugins.Source;
 
-@Description("Computes link salience of each edge in a (sub-) graph")
-public class LinkSaliencePlugin extends ServerPlugin {
+@Description(value = "Computes link salience of each edge in a (sub-) graph") public class LinkSaliencePlugin extends ServerPlugin {
+  @Name(value = "computeLinkSalience") @PluginTarget(value = GraphDatabaseService.class) public void computeLinkSalience(@Source GraphDatabaseService graphDb, @Description(value = "Name of the relationship property representing edge weight.") @Parameter(name = "weightProperty") String weightProperty, @Description(value = "Indicates whether the graph should be treated as a directed graph. " + "False means edge direction is ignored in computing shortest paths.") @Parameter(name = "directed") Boolean treatGraphAsDirected) {
+    LinkSalienceComputer salienceComputer = new LinkSalienceComputer(graphDb);
+    salienceComputer.computeLinkSalience(weightProperty, treatGraphAsDirected);
+  }
 
-	@Name("computeLinkSalience")
-	@PluginTarget(GraphDatabaseService.class)
-	public void computeLinkSalience(
-			@Source GraphDatabaseService graphDb,
-			@Description("Name of the relationship property representing edge weight.") @Parameter(name = "weightProperty") String weightProperty,
-			@Description("Indicates whether the graph should be treated as a directed graph. "
-					+ "False means edge direction is ignored in computing shortest paths.") @Parameter(name = "directed") Boolean treatGraphAsDirected) {
-		LinkSalienceComputer salienceComputer = new LinkSalienceComputer(graphDb, weightProperty, treatGraphAsDirected);
-		salienceComputer.computeLinkSalience();
-	}
+  @Name(value = "computeLinkSalienceWithDijkstra") @PluginTarget(value = GraphDatabaseService.class) public void computeLinkSalienceWithDijkstra(@Source GraphDatabaseService graphDb, @Description(value = "Name of the relationship property representing edge weight.") @Parameter(name = "weightProperty") String weightProperty) {
+    LinkSalienceComputer salienceComputer = new LinkSalienceComputer(graphDb);
+    salienceComputer.computeLinkSalienceWithDijkstra(weightProperty);
+  }
 
-	@Name("computeLinkSalienceWithDijkstra")
-	@PluginTarget(GraphDatabaseService.class)
-	public void computeLinkSalienceWithDijkstra(
-			@Source GraphDatabaseService graphDb,
-			@Description("Name of the relationship property representing edge weight.") @Parameter(name = "weightProperty") String weightProperty) {
-		LinkSalienceComputer salienceComputer = new LinkSalienceComputer(graphDb, weightProperty);
-		salienceComputer.computeLinkSalienceWithDijkstra();
-	}
-
-	@Name("computeLinkSalienceForQueryResult")
-	@PluginTarget(GraphDatabaseService.class)
-	public void computeLinkSalienceForQueryResult(
-			@Source GraphDatabaseService graphDb,
-			@Description("Name of the cypher query to provide the subgraph.") @Parameter(name = "query") String query,
-			@Description("Name of the relationship property representing edge weight.") @Parameter(name = "weightProperty") String weightProperty,
-			@Description("Indicates whether the graph should be treated as a directed graph. "
-					+ "False means edge direction is ignored in computing shortest paths.") @Parameter(name = "directed") Boolean treatGraphAsDirected) {
-		LinkSalienceComputer salienceComputer = new LinkSalienceComputer(graphDb, weightProperty, treatGraphAsDirected);
-		salienceComputer.computeLinkSalienceForQueryResult(query);
-	}
+  @Name(value = "computeLinkSalienceForQueryResult") @PluginTarget(value = GraphDatabaseService.class) public void computeLinkSalienceForQueryResult(@Source GraphDatabaseService graphDb, @Description(value = "Name of the cypher query to provide the subgraph.") @Parameter(name = "query") String query, @Description(value = "Name of the relationship property representing edge weight.") @Parameter(name = "weightProperty") String weightProperty) {
+    LinkSalienceComputer salienceComputer = new LinkSalienceComputer(graphDb);
+    salienceComputer.computeLinkSalienceForQueryResult(query, weightProperty);
+  }
 }
