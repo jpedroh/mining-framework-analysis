93a668a5ee062479f8e2e4b320c544c267d62547
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 05:00:37 +0000

    Mining Framework Analysis

--- driver-core/src/main/java/com/datastax/driver/core/HostConnectionPool.java
+++ driver-core/src/main/java/com/datastax/driver/core/HostConnectionPool.java
@@ -1,94 +1,17 @@
-/*
- *      Copyright (C) 2012-2014 DataStax Inc.
- *
- *   Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
- */
-package com.datastax.driver.core;
-
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
-import java.util.concurrent.atomic.AtomicReference;
-
-/**
- * A set of connections to a live host.
- *
- * We use different strategies depending of the protocol version in use.
- */
-abstract class HostConnectionPool {
-
-    static HostConnectionPool newInstance(Host host, HostDistance hostDistance, SessionManager manager, ProtocolVersion version) throws ConnectionException, UnsupportedProtocolVersionException, ClusterNameMismatchException {
-        switch (version) {
-            case V1:
-            case V2:
-                return new DynamicConnectionPool(host, hostDistance, manager);
-            case V3:
-                return new SingleConnectionPool(host, hostDistance, manager);
-            default:
-                throw version.unsupported();
-        }
-    }
-
-    final Host host;
-    volatile HostDistance hostDistance;
-    protected final SessionManager manager;
-
-    protected final AtomicReference<CloseFuture> closeFuture = new AtomicReference<CloseFuture>();
-
-    protected HostConnectionPool(Host host, HostDistance hostDistance, SessionManager manager) {
-        assert hostDistance != HostDistance.IGNORED;
-        this.host = host;
-        this.hostDistance = hostDistance;
-        this.manager = manager;
-    }
-
-    abstract PooledConnection borrowConnection(long timeout, TimeUnit unit) throws ConnectionException, TimeoutException;
-
-    abstract void returnConnection(PooledConnection connection);
-
-    abstract void ensureCoreConnections();
-
-    abstract void replaceDefunctConnection(final PooledConnection connection);
-
-    abstract void trashIdleConnections(long now);
-
-    abstract int opened();
-
-    abstract int inFlightQueriesCount();
-
-    protected abstract CloseFuture makeCloseFuture();
-
-    public final boolean isClosed() {
-        return closeFuture.get() != null;
-    }
-
-    public final CloseFuture closeAsync() {
-
-        CloseFuture future = closeFuture.get();
-        if (future != null)
-            return future;
-
-        future = makeCloseFuture();
-
-        return closeFuture.compareAndSet(null, future)
-            ? future
-            : closeFuture.get(); // We raced, it's ok, return the future that was actually set
-    }
-
-    static class PoolState {
-        volatile String keyspace;
-
-        public void setKeyspace(String keyspace) {
-            this.keyspace = keyspace;
-        }
-    }
-}
+  package    com . datastax . driver . core ;   import    java . util . concurrent . TimeUnit ;  import    java . util . concurrent . TimeoutException ;  import     java . util . concurrent . atomic . AtomicReference ;   abstract class HostConnectionPool  {   static HostConnectionPool newInstance  (  Host host ,  HostDistance hostDistance ,  SessionManager manager ,  ProtocolVersion version )  throws ConnectionException , UnsupportedProtocolVersionException , ClusterNameMismatchException  {  switch  ( version )  {   case V1 :   case V2 :  return  new DynamicConnectionPool  ( host , hostDistance , manager ) ;   case V3 :  return  new SingleConnectionPool  ( host , hostDistance , manager ) ;   default :  throw  version . unsupported  ( ) ; } }   final Host  host ;   volatile HostDistance  hostDistance ;   protected final SessionManager  manager ;   protected final  AtomicReference  < CloseFuture >  closeFuture =  new  AtomicReference  < CloseFuture >  ( ) ;   protected HostConnectionPool  (  Host host ,  HostDistance hostDistance ,  SessionManager manager )  {  assert  hostDistance !=  HostDistance . IGNORED ;    this . host = host ;    this . hostDistance = hostDistance ;    this . manager = manager ; }   abstract PooledConnection borrowConnection  (   long timeout ,  TimeUnit unit )  throws ConnectionException , TimeoutException ;   abstract void returnConnection  (  PooledConnection connection ) 
+<<<<<<<
+;
+=======
+ {  if  (  isClosed  ( ) )  {   close  ( connection ) ;  return ; }  if  (  connection . isDefunct  ( ) )  {  return ; }   int  inFlight =   connection . inFlight . decrementAndGet  ( ) ;  if  (  trash . contains  ( connection ) )  {  if  (   inFlight == 0 &&  trash . remove  ( connection ) )   close  ( connection ) ; } else  {  if  (    connections . size  ( ) >   options  ( ) . getCoreConnectionsPerHost  ( hostDistance ) &&  inFlight <=   options  ( ) . getMinSimultaneousRequestsPerConnectionThreshold  ( hostDistance ) )  {   connection . setTrashTimeIn  (   options  ( ) . getIdleTimeoutSeconds  ( ) ) ; } else  if  (   connection . maxAvailableStreams  ( ) < MIN_AVAILABLE_STREAMS )  {   replaceConnection  ( connection ) ; } else  {   connection . cancelTrashTime  ( ) ;   signalAvailableConnection  ( ) ; } } }
+>>>>>>>
+   abstract void ensureCoreConnections  ( ) ;   abstract void replaceDefunctConnection  (   final PooledConnection connection ) ;   abstract  int opened  ( ) ;   abstract  int inFlightQueriesCount  ( ) ;   protected abstract CloseFuture makeCloseFuture  ( ) ;   public final boolean isClosed  ( )  {  return   closeFuture . get  ( ) != null ; }   public final CloseFuture closeAsync  ( )  {  CloseFuture  future =  closeFuture . get  ( ) ;  if  (  future != null )  return future ;   future =  makeCloseFuture  ( ) ;  return   closeFuture . compareAndSet  ( null , future ) ? future :  closeFuture . get  ( ) ; }   static class PoolState  {   volatile String  keyspace ;   public void setKeyspace  (  String keyspace )  {    this . keyspace = keyspace ; } } 
+<<<<<<<
+=======
+  private PooledConnection waitForConnection  (   long timeout ,  TimeUnit unit )  throws ConnectionException , TimeoutException  {  if  (  timeout == 0 )  throw  new TimeoutException  ( ) ;   long  start =  System . nanoTime  ( ) ;   long  remaining = timeout ;  do  {  try  {   awaitAvailableConnection  ( remaining , unit ) ; }  catch (   InterruptedException e )  {    Thread . currentThread  ( ) . interrupt  ( ) ;   timeout = 0 ; }  if  (  isClosed  ( ) )  throw  new ConnectionException  (  host . getSocketAddress  ( ) , "Pool is shutdown" ) ;   int  minInFlight =  Integer . MAX_VALUE ;  PooledConnection  leastBusy = null ;  for ( PooledConnection connection : connections )  {   int  inFlight =   connection . inFlight . get  ( ) ;  if  (  inFlight < minInFlight )  {   minInFlight = inFlight ;   leastBusy = connection ; } }  if  (  leastBusy != null )  {  while  ( true )  {   int  inFlight =   leastBusy . inFlight . get  ( ) ;  if  (  inFlight >=  leastBusy . maxAvailableStreams  ( ) )  break ;  if  (   leastBusy . inFlight . compareAndSet  ( inFlight ,  inFlight + 1 ) )  return leastBusy ; } }   remaining =  timeout -  Cluster . timeSince  ( start , unit ) ; } while  (  remaining > 0 ) ;  throw  new TimeoutException  ( ) ; }
+>>>>>>>
+  void trashIdleConnections  (   long now )  {  for ( PooledConnection connection : connections )  {  if  (   connection . getTrashTime  ( ) < now )  {   trashConnection  ( connection ) ; } } } 
+<<<<<<<
+=======
+  private boolean trashConnection  (  PooledConnection connection )  {  if  (   connection . markForTrash . compareAndSet  ( false , true ) )  {  for ( ; ; )  {   int  opened =  open . get  ( ) ;  if  (  opened <=   options  ( ) . getCoreConnectionsPerHost  ( hostDistance ) )  {    connection . markForTrash . set  ( false ) ;   connection . cancelTrashTime  ( ) ;  return false ; }  if  (  open . compareAndSet  ( opened ,  opened - 1 ) )  break ; }   doTrashConnection  ( connection ) ; }  return true ; }
+>>>>>>>
+ }
