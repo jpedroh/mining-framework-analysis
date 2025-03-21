4cce5bf9285a39f691d216abf330c0babb064732
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 08:39:55 +0000

    Mining Framework Analysis

--- php-frontend/src/main/java/org/sonar/php/parser/PHPLexicalGrammar.java
+++ php-frontend/src/main/java/org/sonar/php/parser/PHPLexicalGrammar.java
@@ -1,24 +1,4 @@
-/*
- * SonarQube PHP Plugin
- * Copyright (C) 2010 SonarSource and Akram Ben Aissi
- * sonarqube@googlegroups.com
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3 of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
- */
 package org.sonar.php.parser;
-
 import com.sonar.sslr.api.GenericTokenType;
 import org.sonar.php.api.PHPKeyword;
 import org.sonar.php.api.PHPPunctuator;
@@ -27,43 +7,28 @@ import org.sonar.sslr.grammar.LexerlessGrammarBuilder;
 import org.sonar.sslr.parser.LexerlessGrammar;
 
 public enum PHPLexicalGrammar implements GrammarRuleKey {
-
   COMPILATION_UNIT,
   SCRIPT,
-
-  /**
-   * Declaration
-   */
-
   FUNCTION_DECLARATION,
   CLASS_DECLARATION,
   INTERFACE_DECLARATION,
   TRAIT_DECLARATION,
-
   CLASS_MEMBER,
-
   METHOD_DECLARATION,
   CLASS_VARIABLE_DECLARATION,
   CLASS_CONSTANT_DECLARATION,
   TRAIT_USE_STATEMENT,
-
   NAMESPACE_NAME,
   INTERFACE_LIST,
   PARAMETER_LIST,
   PARAMETER,
-
   VARIABLE_DECLARATION,
   MEMBER_MODIFIER,
   MEMBER_CONST_DECLARATION,
-
   TRAIT_METHOD_REFERENCE_FULLY_QUALIFIED,
   TRAIT_METHOD_REFERENCE,
   TRAIT_ALIAS,
   TRAIT_PRECEDENCE,
-
-  /**
-   * Lexical
-   */
   EOF,
   HEREDOC,
   NUMERIC_LITERAL,
@@ -76,42 +41,22 @@ public enum PHPLexicalGrammar implements GrammarRuleKey {
   FILE_OPENING_TAG,
   INLINE_HTML,
   KEYWORDS,
-
-  /**
-   * SPACING
-   */
   SPACING,
-
-  /**
-   * End of statement.
-   */
   EOS,
-
-  /**
-   * Statement
-   */
   TOP_STATEMENT,
   STATEMENT,
   INNER_STATEMENT,
-
   BLOCK,
-
   EMPTY_STATEMENT,
-
   INLINE_HTML_STATEMENT,
-
   NAMESPACE_STATEMENT,
   DECLARE_STATEMENT,
-
   CONSTANT_DECLARATION,
   CONSTANT_VAR,
-
   USE_STATEMENT,
   USE_CLAUSE,
-
   EXPRESSION_STATEMENT,
   ECHO_STATEMENT,
-
   IF_STATEMENT,
   ELSEIF_CLAUSE,
   ELSE_CLAUSE,
@@ -119,21 +64,15 @@ public enum PHPLexicalGrammar implements GrammarRuleKey {
   STANDARD_IF_STATEMENT,
   ALTERNATIVE_ELSEIF_CLAUSE,
   ALTERNATIVE_ELSE_CLAUSE,
-
   WHILE_STATEMENT,
   ALTERNATIVE_WHILE_STATEMENT,
-
   DO_WHILE_STATEMENT,
-
   FOR_STATEMENT,
   FOR_EXPR,
-
   FOREACH_STATEMENT,
   FOREACH_VARIABLE,
-
   SWITCH_STATEMENT,
   SWITCH_CASE_CLAUSE,
-
   LABEL,
   BREAK_STATEMENT,
   CONTINUE_STATEMENT,
@@ -141,20 +80,14 @@ public enum PHPLexicalGrammar implements GrammarRuleKey {
   THROW_STATEMENT,
   GOTO_STATEMENT,
   YIELD_STATEMENT,
-
   TRY_STATEMENT,
   CATCH_BLOCK,
-
   GLOBAL_STATEMENT,
   GLOBAL_VAR,
   STATIC_STATEMENT,
   STATIC_VAR,
   UNSET_VARIABLE_STATEMENT,
   UNSET_VARIABLES,
-
-  /**
-   * Expression
-   */
   PRIMARY_EXPRESSION,
   MEMBER_EXPRESSION,
   VARIABLE_WITHOUT_OBJECTS,
@@ -188,7 +121,6 @@ public enum PHPLexicalGrammar implements GrammarRuleKey {
   SIMPLE_ENCAPS_VARIABLE,
   ENCAPS_VAR,
   EXIT_EXPRESSION,
-
   NULL,
   CLASS_CONSTANT,
   FILE_CONSTANT,
@@ -202,7 +134,6 @@ public enum PHPLexicalGrammar implements GrammarRuleKey {
   DOUBLE_QUOTE,
   REGULAR_STRING_LITERAL,
   VARIABLE_VARIABLE_DOLLAR,
-
   ISSET,
   EMPTY,
   WHITESPACES,
@@ -214,7 +145,6 @@ public enum PHPLexicalGrammar implements GrammarRuleKey {
   REQUIRE,
   CLONE,
   PRINT,
-
   INTEGER,
   INT,
   DOUBLE,
@@ -223,7 +153,8 @@ public enum PHPLexicalGrammar implements GrammarRuleKey {
   OBJECT,
   BOOLEAN,
   BOOL,
-  BINARY;
+  BINARY
+  ;
 
   public static LexerlessGrammar createGrammar() {
     return createGrammarBuilder().build();
@@ -231,44 +162,27 @@ public enum PHPLexicalGrammar implements GrammarRuleKey {
 
   public static LexerlessGrammarBuilder createGrammarBuilder() {
     LexerlessGrammarBuilder b = LexerlessGrammarBuilder.create();
-
     lexical(b);
     punctuators(b);
     keywords(b);
-
     return b;
   }
 
   public static void lexical(LexerlessGrammarBuilder b) {
-    b.rule(SPACING).is(
-      b.skippedTrivia(b.regexp("[" + LexicalConstant.LINE_TERMINATOR + LexicalConstant.WHITESPACE + "]*+")),
-      b.zeroOrMore(
-        b.commentTrivia(b.regexp(LexicalConstant.COMMENT)),
-        b.skippedTrivia(b.regexp("[" + LexicalConstant.LINE_TERMINATOR + LexicalConstant.WHITESPACE + "]*+")))
-      ).skip();
-
-    // Literals
+    b.rule(SPACING).is(b.skippedTrivia(b.regexp("[" + LexicalConstant.LINE_TERMINATOR + LexicalConstant.WHITESPACE + "]*+")), b.zeroOrMore(b.commentTrivia(b.regexp(LexicalConstant.COMMENT)), b.skippedTrivia(b.regexp("[" + LexicalConstant.LINE_TERMINATOR + LexicalConstant.WHITESPACE + "]*+")))).skip();
     b.rule(HEREDOC).is(SPACING, b.regexp(LexicalConstant.HEREDOC));
     b.rule(NUMERIC_LITERAL).is(SPACING, b.regexp(LexicalConstant.NUMERIC_LITERAL));
     b.rule(REGULAR_STRING_LITERAL).is(SPACING, b.regexp(LexicalConstant.STRING_LITERAL));
-
     b.rule(STRING_WITH_ENCAPS_VAR_CHARACTERS).is(b.regexp(LexicalConstant.STRING_WITH_ENCAPS_VAR_CHARACTERS));
     b.rule(DOUBLE_QUOTE).is("\"");
-    // FIXME: this recovery is introduce in order to parse ${var}, as expression cannot match keywords.
     b.rule(SEMI_COMPLEX_RECOVERY_EXPRESSION).is(b.regexp("[^}]++"));
-
-    // Identifier
     b.rule(WHITESPACES).is(b.regexp("[" + LexicalConstant.WHITESPACE + "]*+"));
     b.rule(REGULAR_VAR_IDENTIFIER).is(SPACING, VARIABLE_IDENTIFIER).skip();
     b.rule(VARIABLE_IDENTIFIER).is(b.regexp(LexicalConstant.VAR_IDENTIFIER));
     b.rule(IDENTIFIER).is(SPACING, b.nextNot(KEYWORDS), b.regexp(LexicalConstant.IDENTIFIER));
-
-    // Tags & Inline HTML
     b.rule(FILE_OPENING_TAG).is(SPACING, b.regexp(LexicalConstant.PHP_START_TAG)).skip();
     b.rule(INLINE_HTML).is(SPACING, b.regexp(LexicalConstant.PHP_END_TAG)).skip();
-
     b.rule(EOF).is(b.token(GenericTokenType.EOF, b.endOfInput())).skip();
-
     b.rule(NULL).is(word(b, "NULL")).skip();
     b.rule(CLASS_CONSTANT).is(word(b, "__CLASS__")).skip();
     b.rule(FILE_CONSTANT).is(word(b, "__FILE__")).skip();
@@ -278,12 +192,9 @@ public enum PHPLexicalGrammar implements GrammarRuleKey {
     b.rule(METHOD_CONSTANT).is(word(b, "__METHOD__")).skip();
     b.rule(NAMESPACE_CONSTANT).is(word(b, "__NAMESPACE__")).skip();
     b.rule(TRAIT_CONSTANT).is(word(b, "__TRAIT__")).skip();
-
     b.rule(BOOLEAN_LITERAL).is(b.firstOf(word(b, "TRUE"), word(b, "FALSE")));
-
     b.rule(NEXT_IS_DOLLAR).is(b.next(PHPPunctuator.DOLLAR));
     b.rule(VARIABLE_VARIABLE_DOLLAR).is(PHPPunctuator.DOLLAR, b.nextNot(b.firstOf(IDENTIFIER, KEYWORDS, PHPPunctuator.LCURLYBRACE)));
-
     b.rule(ISSET).is(word(b, "ISSET")).skip();
     b.rule(EMPTY).is(word(b, "EMPTY")).skip();
     b.rule(INCLUDE_ONCE).is(word(b, "INCLUDE_ONCE")).skip();
@@ -293,7 +204,6 @@ public enum PHPLexicalGrammar implements GrammarRuleKey {
     b.rule(REQUIRE).is(word(b, "REQUIRE")).skip();
     b.rule(CLONE).is(word(b, "CLONE")).skip();
     b.rule(PRINT).is(word(b, "PRINT")).skip();
-
     b.rule(INTEGER).is(word(b, "INTEGER")).skip();
     b.rule(INT).is(word(b, "INT")).skip();
     b.rule(DOUBLE).is(word(b, "DOUBLE")).skip();
@@ -303,29 +213,18 @@ public enum PHPLexicalGrammar implements GrammarRuleKey {
     b.rule(BOOLEAN).is(word(b, "BOOLEAN")).skip();
     b.rule(BOOL).is(word(b, "BOOL")).skip();
     b.rule(BINARY).is(word(b, "BINARY")).skip();
-
   }
 
   private static void keywords(LexerlessGrammarBuilder b) {
     Object[] rest = new Object[PHPKeyword.values().length - 2];
-
     for (int i = 0; i < PHPKeyword.values().length; i++) {
       PHPKeyword tokenType = PHPKeyword.values()[i];
-
-      // PHP keywords are case insensitive
       b.rule(tokenType).is(SPACING, b.regexp("(?i)" + tokenType.getValue()), b.nextNot(b.regexp(LexicalConstant.IDENTIFIER_PART))).skip();
       if (i > 1) {
         rest[i - 2] = b.regexp("(?i)" + tokenType.getValue());
       }
     }
-
-    b.rule(KEYWORDS).is(SPACING,
-      b.firstOf(
-        PHPKeyword.getKeywordValues()[0],
-        PHPKeyword.getKeywordValues()[1],
-        rest),
-      b.nextNot(b.regexp(LexicalConstant.IDENTIFIER_PART))
-      );
+    b.rule(KEYWORDS).is(SPACING, b.firstOf(PHPKeyword.getKeywordValues()[0], PHPKeyword.getKeywordValues()[1], rest), b.nextNot(b.regexp(LexicalConstant.IDENTIFIER_PART)));
   }
 
   private static void punctuators(LexerlessGrammarBuilder b) {
@@ -337,5 +236,4 @@ public enum PHPLexicalGrammar implements GrammarRuleKey {
   private static Object word(LexerlessGrammarBuilder b, String word) {
     return b.sequence(SPACING, b.regexp("(?i)" + word), b.nextNot(b.regexp(LexicalConstant.IDENTIFIER_PART)));
   }
-
 }
--- php-frontend/src/test/java/org/sonar/php/tree/visitors/PHPIssueTest.java
+++ php-frontend/src/test/java/org/sonar/php/tree/visitors/PHPIssueTest.java
@@ -1,78 +1,48 @@
-/*
- * SonarQube PHP Plugin
- * Copyright (C) 2010 SonarSource and Akram Ben Aissi
- * sonarqube@googlegroups.com
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3 of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
- */
 package org.sonar.php.tree.visitors;
-
 import org.fest.assertions.Assertions;
 import org.junit.Test;
 import org.sonar.php.tree.impl.expression.NameIdentifierTreeImpl;
 import org.sonar.php.tree.impl.lexical.InternalSyntaxToken;
 import org.sonar.php.utils.DummyCheck;
 import org.sonar.plugins.php.api.visitors.PHPCheck;
-
 import java.util.Collections;
 
 public class PHPIssueTest {
-
   private static final PHPCheck CHECK = new DummyCheck();
-  @Test
-  public void test_no_line() throws Exception {
-    PHPIssue issue = new PHPIssue(CHECK, "message");
 
+  @Test public void test_no_line() throws Exception {
+    PHPIssue issue = new PHPIssue(CHECK, "message");
     Assertions.assertThat(issue.check()).isEqualTo(CHECK);
     Assertions.assertThat(issue.message()).isEqualTo("message");
     Assertions.assertThat(issue.line()).isEqualTo(0);
     Assertions.assertThat(issue.cost()).isNull();
   }
 
-  @Test
-  public void test_with_line() throws Exception {
+  @Test public void test_with_line() throws Exception {
     final int line = 7;
     PHPIssue issue = new PHPIssue(CHECK, "message").line(line);
-
     Assertions.assertThat(issue.check()).isEqualTo(CHECK);
     Assertions.assertThat(issue.message()).isEqualTo("message");
     Assertions.assertThat(issue.line()).isEqualTo(line);
     Assertions.assertThat(issue.cost()).isNull();
   }
 
-  @Test
-  public void test_with_line_and_cost() throws Exception {
+  @Test public void test_with_line_and_cost() throws Exception {
     final int cost = 7;
     PHPIssue issue = new PHPIssue(CHECK, "message").cost(cost);
-
     Assertions.assertThat(issue.check()).isEqualTo(CHECK);
     Assertions.assertThat(issue.message()).isEqualTo("message");
     Assertions.assertThat(issue.line()).isEqualTo(0);
     Assertions.assertThat(issue.cost()).isEqualTo(cost);
   }
 
-  @Test
-  public void test_setting_line_from_tree() throws Exception {
+  @Test public void test_setting_line_from_tree() throws Exception {
     final int line = 3;
     NameIdentifierTreeImpl tree = new NameIdentifierTreeImpl(new InternalSyntaxToken(line, 1, "tree", Collections.EMPTY_LIST, 0, false));
     PHPIssue issue = new PHPIssue(CHECK, "message").tree(tree);
-
     Assertions.assertThat(issue.check()).isEqualTo(CHECK);
     Assertions.assertThat(issue.message()).isEqualTo("message");
     Assertions.assertThat(issue.line()).isEqualTo(line);
     Assertions.assertThat(issue.cost()).isNull();
   }
-
 }
