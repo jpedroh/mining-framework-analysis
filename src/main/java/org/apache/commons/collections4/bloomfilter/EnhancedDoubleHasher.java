c26e9606428c55f82cb267a1149b52c1e348ef5b
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 04:35:01 +0000

    Mining Framework Analysis

--- src/main/java/org/apache/commons/collections4/bloomfilter/EnhancedDoubleHasher.java
+++ src/main/java/org/apache/commons/collections4/bloomfilter/EnhancedDoubleHasher.java
@@ -129,6 +129,42 @@ public class EnhancedDoubleHasher implements Hasher {
         return increment;
     }
 
+<<<<<<< /usr/src/app/output/apache/commons-collections/57ab36b8585d1ec77c2ce84413f8599fd75b8d65/src/main/java/org/apache/commons/collections4/bloomfilter/EnhancedDoubleHasher.java/left.java
+||||||| /usr/src/app/output/apache/commons-collections/57ab36b8585d1ec77c2ce84413f8599fd75b8d65/src/main/java/org/apache/commons/collections4/bloomfilter/EnhancedDoubleHasher.java/base.java
+    /**
+     * Performs a modulus calculation on an unsigned long and an integer divisor.
+     * @param dividend a unsigned long value to calculate the modulus of.
+     * @param divisor the divisor for the modulus calculation.
+     * @return the remainder or modulus value.
+     */
+    static int mod(final long dividend, final int divisor) {
+        // See Hacker's Delight (2nd ed), section 9.3.
+        // Assume divisor is positive.
+        // Divide half the unsigned number and then double the quotient result.
+        final long quotient = (dividend >>> 1) / divisor << 1;
+        final long remainder = dividend - quotient * divisor;
+        // remainder in [0, 2 * divisor)
+        return (int) (remainder >= divisor ? remainder - divisor : remainder);
+    }
+
+=======
+    /**
+     * Performs a modulus calculation on an unsigned long and an integer divisor.
+     * @param dividend a unsigned long value to calculate the modulus of.
+     * @param divisor the divisor for the modulus calculation (must be strictly positive).
+     * @return the remainder or modulus value.
+     */
+    static int mod(final long dividend, final int divisor) {
+        // See Hacker's Delight (2nd ed), section 9.3.
+        // Assume divisor is positive.
+        // Divide half the unsigned number and then double the quotient result.
+        final long quotient = (dividend >>> 1) / divisor << 1;
+        final long remainder = dividend - quotient * divisor;
+        // remainder in [0, 2 * divisor)
+        return (int) (remainder >= divisor ? remainder - divisor : remainder);
+    }
+
+>>>>>>> /usr/src/app/output/apache/commons-collections/57ab36b8585d1ec77c2ce84413f8599fd75b8d65/src/main/java/org/apache/commons/collections4/bloomfilter/EnhancedDoubleHasher.java/right.java
     @Override
     public IndexProducer indices(final Shape shape) {
         Objects.requireNonNull(shape, "shape");
