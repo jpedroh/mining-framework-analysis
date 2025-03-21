78ced9d69c1a4b197273fc3b56ad2d6e8cbd5931
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 11:53:44 +0000

    Mining Framework Analysis

--- src/main/java/org/webbitserver/HttpRequest.java
+++ src/main/java/org/webbitserver/HttpRequest.java
@@ -1,5 +1,4 @@
 package org.webbitserver;
-
 import java.net.HttpCookie;
 import java.net.SocketAddress;
 import java.util.List;
@@ -14,19 +13,18 @@ import java.util.Set;
  * @see HttpResponse
  */
 public interface HttpRequest extends DataHolder {
+  String COOKIE_HEADER = "Cookie";
 
-    String COOKIE_HEADER = "Cookie";
+  String uri();
 
-    String uri();
-
-    /**
+  /**
      * Modify uri
      *
      * @param uri new uri
      */
-    HttpRequest uri(String uri);
+  HttpRequest uri(String uri);
 
-    /**
+  /**
      * Retrieve the value single HTTP header.
      * <p/>
      * If the header is not found, null is returned.
@@ -34,119 +32,117 @@ public interface HttpRequest extends DataHolder {
      * If there are multiple headers with the same name, it will return one of them, but it is not
      * defined which one. Instead, use {@link #headers(String)}.
      */
-    String header(String name);
+  String header(String name);
 
-    /**
+  /**
      * Retrieve all values for an HTTP header. If no values are found, an empty List is returned.
      */
-    List<String> headers(String name);
+  List<String> headers(String name);
 
-    /**
+  /**
      * Whether a specific HTTP header was present in the request.
      */
-    boolean hasHeader(String name);
+  boolean hasHeader(String name);
 
-    /**
+  /**
      * @return all inbound cookies
      */
-    List<HttpCookie> cookies();
+  List<HttpCookie> cookies();
 
-    /**
+  /**
      * Get a cookie with a specific name
      *
      * @param name cookie name
      * @return cookie with that name
      */
-    HttpCookie cookie(String name);
+  HttpCookie cookie(String name);
 
-    /**
+  /**
      * Get query parameter value.
      *
      * @param key parameter name
      * @return the value of the parameter
      * @see {@link #queryParams(String)}
      */
-    String queryParam(String key);
+  String queryParam(String key);
 
-    /**
+  /**
      * Get all query parameter values.
      *
      * @param key parameter name
      * @return the values of the parameter
      * @see {@link #queryParam(String)}
      */
-    List<String> queryParams(String key);
+  List<String> queryParams(String key);
 
-    /**
+  /**
      * List all query parameter keys.
      * @see {@link #queryParam(String)}
      */
-    Set<String> queryParamKeys();
+  Set<String> queryParamKeys();
 
-    /**
+  /**
      * Get post parameter value.
      *
      * @param key parameter name
      * @return the value of the parameter
      * @see {@link #postParams(String)}
      */
-    String postParam(String key);
+  String postParam(String key);
 
-    /**
+  /**
      * Get all post parameter values.
      *
      * @param key parameter name
      * @return the values of the parameter
      * @see {@link #postParam(String)}
      */
-    List<String> postParams(String key);
+  List<String> postParams(String key);
 
-    /**
+  /**
      * List all post parameter keys.
      * @see {@link #postParam(String)}
      */
-    Set<String> postParamKeys();
+  Set<String> postParamKeys();
 
-    /**
+  /**
      * Get the value of named cookie
      *
      * @param name cookie name
      * @return cookie value, or null if the cookie does not exist.
      */
-    String cookieValue(String name);
+  String cookieValue(String name);
 
-    /**
+  /**
      * Returns all headers sent from client.
      */
-    List<Map.Entry<String, String>> allHeaders();
+  List<Map.Entry<String, String>> allHeaders();
 
-    /**
+  /**
      * HTTP method (e.g. "GET" or "POST")
      */
-    String method();
+  String method();
 
-    /**
+  /**
      * The body
      */
-    String body();
+  String body();
 
-    @Override
-    HttpRequest data(String key, Object value); // Override DataHolder to provide more specific return type.
+  @Override HttpRequest data(String key, Object value);
 
-    /**
+  /**
      * Remote address of connection (i.e. the host of the client).
      */
-    SocketAddress remoteAddress();
+  SocketAddress remoteAddress();
 
-    /**
+  /**
      * A unique identifier for this request. This should be treated as an opaque object,
      * that can be used to track the lifecycle of a request.
      */
-    Object id();
+  Object id();
 
-    /**
+  /**
      * Timestamp (millis since epoch) of when this request was first received by the server.
      */
-    long timestamp();
-
+  long timestamp();
 }
--- src/main/java/org/webbitserver/stub/StubHttpRequest.java
+++ src/main/java/org/webbitserver/stub/StubHttpRequest.java
@@ -1,9 +1,7 @@
 package org.webbitserver.stub;
-
 import org.webbitserver.HttpRequest;
 import org.webbitserver.InboundCookieParser;
 import org.webbitserver.helpers.QueryParameters;
-
 import java.net.HttpCookie;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
@@ -15,177 +13,162 @@ import java.util.*;
  * Useful for testing.
  */
 public class StubHttpRequest extends StubDataHolder implements HttpRequest {
+  private String uri = "/";
 
-    private String uri = "/";
-    private String method = "GET";
-    private List<Map.Entry<String, String>> headers = new ArrayList<Map.Entry<String, String>>();
-    private SocketAddress remoteAddress = new InetSocketAddress("localhost", 0);
-    private Object id = "StubID";
-    private long timestamp = 0;
-    private String body;
+  private String method = "GET";
 
-    public StubHttpRequest() {
+  private List<Map.Entry<String, String>> headers = new ArrayList<Map.Entry<String, String>>();
+
+  private SocketAddress remoteAddress = new InetSocketAddress("localhost", 0);
+
+  private Object id = "StubID";
+
+  private long timestamp = 0;
+
+  private String body;
+
+  public StubHttpRequest() {
+  }
+
+  public StubHttpRequest(String uri) {
+    this.uri = uri;
+  }
+
+  @Override public String uri() {
+    return uri;
+  }
+
+  public StubHttpRequest uri(String uri) {
+    this.uri = uri;
+    return this;
+  }
+
+  @Override public String header(String name) {
+    for (Map.Entry<String, String> header : headers) {
+      if (header.getKey().equals(name)) {
+        return header.getValue();
+      }
     }
+    return null;
+  }
 
-    public StubHttpRequest(String uri) {
-        this.uri = uri;
+  @Override public boolean hasHeader(String name) {
+    for (Map.Entry<String, String> header : headers) {
+      if (header.getKey().equals(name)) {
+        return true;
+      }
     }
+    return false;
+  }
 
-    @Override
-    public String uri() {
-        return uri;
-    }
+  @Override public List<HttpCookie> cookies() {
+    return InboundCookieParser.parse(headers(COOKIE_HEADER));
+  }
 
-    public StubHttpRequest uri(String uri) {
-        this.uri = uri;
-        return this;
+  @Override public HttpCookie cookie(String name) {
+    for (HttpCookie cookie : cookies()) {
+      if (cookie.getName().equals(name)) {
+        return cookie;
+      }
     }
+    return null;
+  }
 
-    @Override
-    public String header(String name) {
-        for (Map.Entry<String, String> header : headers) {
-            if (header.getKey().equals(name)) {
-                return header.getValue();
-            }
-        }
-        return null;
-    }
+  @Override public String queryParam(String key) {
+    return new QueryParameters(URI.create(uri()).getQuery()).first(key);
+  }
 
-    @Override
-    public boolean hasHeader(String name) {
-        for (Map.Entry<String, String> header : headers) {
-            if (header.getKey().equals(name)) {
-                return true;
-            }
-        }
-        return false;
-    }
+  @Override public List<String> queryParams(String key) {
+    return new QueryParameters(URI.create(uri()).getQuery()).all(key);
+  }
 
-    @Override
-    public List<HttpCookie> cookies() {
-        return InboundCookieParser.parse(headers(COOKIE_HEADER));
-    }
+  @Override public Set<String> queryParamKeys() {
+    return new QueryParameters(URI.create(uri()).getQuery()).keys();
+  }
 
-    @Override
-    public HttpCookie cookie(String name) {
-        for (HttpCookie cookie : cookies()) {
-            if (cookie.getName().equals(name)) {
-                return cookie;
-            }
-        }
-        return null;
-    }
+  @Override public String postParam(String key) {
+    return new QueryParameters(body()).first(key);
+  }
 
-    @Override
-    public String queryParam(String key) {
-        return new QueryParameters(URI.create(uri()).getQuery()).first(key);
-    }
+  @Override public List<String> postParams(String key) {
+    return new QueryParameters(body()).all(key);
+  }
 
-    @Override
-    public List<String> queryParams(String key) {
-        return new QueryParameters(URI.create(uri()).getQuery()).all(key);
-    }
+  @Override public Set<String> postParamKeys() {
+    return new QueryParameters(body()).keys();
+  }
 
-    @Override
-    public Set<String> queryParamKeys() {
-        return new QueryParameters(URI.create(uri()).getQuery()).keys();
-    }
+  @Override public String cookieValue(String name) {
+    HttpCookie cookie = cookie(name);
+    return cookie == null ? null : cookie.getValue();
+  }
 
-    @Override
-    public String postParam(String key) {
-        return new QueryParameters(body()).first(key);
+  @Override public List<String> headers(String name) {
+    List<String> result = new ArrayList<String>();
+    for (Map.Entry<String, String> header : headers) {
+      if (header.getKey().equals(name)) {
+        result.add(header.getValue());
+      }
     }
+    return result;
+  }
 
-    @Override
-    public List<String> postParams(String key) {
-        return new QueryParameters(body()).all(key);
-    }
+  @Override public List<Map.Entry<String, String>> allHeaders() {
+    return headers;
+  }
 
-    @Override
-    public Set<String> postParamKeys() {
-        return new QueryParameters(body()).keys();
-    }
+  @Override public String method() {
+    return method;
+  }
 
-    @Override
-    public String cookieValue(String name) {
-        HttpCookie cookie = cookie(name);
-        return cookie == null ? null : cookie.getValue();
-    }
+  @Override public String body() {
+    return body;
+  }
 
-    @Override
-    public List<String> headers(String name) {
-        List<String> result = new ArrayList<String>();
-        for (Map.Entry<String, String> header : headers) {
-            if (header.getKey().equals(name)) {
-                result.add(header.getValue());
-            }
-        }
-        return result;
-    }
+  public StubHttpRequest body(String body) {
+    this.body = body;
+    return this;
+  }
 
-    @Override
-    public List<Map.Entry<String, String>> allHeaders() {
-        return headers;
-    }
+  public StubHttpRequest method(String method) {
+    this.method = method;
+    return this;
+  }
 
-    @Override
-    public String method() {
-        return method;
-    }
+  public StubHttpRequest header(String name, String value) {
+    headers.add(new AbstractMap.SimpleEntry<String, String>(name, value));
+    return this;
+  }
 
-    @Override
-    public String body() {
-        return body;
-    }
+  @Override public StubHttpRequest data(String key, Object value) {
+    super.data(key, value);
+    return this;
+  }
 
-    public StubHttpRequest body(String body) {
-        this.body = body;
-        return this;
-    }
+  @Override public SocketAddress remoteAddress() {
+    return remoteAddress;
+  }
 
-    public StubHttpRequest method(String method) {
-        this.method = method;
-        return this;
-    }
+  @Override public Object id() {
+    return id;
+  }
 
-    public StubHttpRequest header(String name, String value) {
-        headers.add(new AbstractMap.SimpleEntry<String, String>(name, value));
-        return this;
-    }
+  public StubHttpRequest id(Object id) {
+    this.id = id;
+    return this;
+  }
 
-    @Override
-    public StubHttpRequest data(String key, Object value) {
-        super.data(key, value);
-        return this;
-    }
+  @Override public long timestamp() {
+    return timestamp;
+  }
 
-    @Override
-    public SocketAddress remoteAddress() {
-        return remoteAddress;
-    }
+  public StubHttpRequest timestamp(long timestamp) {
+    this.timestamp = timestamp;
+    return this;
+  }
 
-    @Override
-    public Object id() {
-        return id;
-    }
-
-    public StubHttpRequest id(Object id) {
-        this.id = id;
-        return this;
-    }
-
-    @Override
-    public long timestamp() {
-        return timestamp;
-    }
-
-    public StubHttpRequest timestamp(long timestamp) {
-        this.timestamp = timestamp;
-        return this;
-    }
-
-    public StubHttpRequest remoteAddress(SocketAddress remoteAddress) {
-        this.remoteAddress = remoteAddress;
-        return this;
-    }
+  public StubHttpRequest remoteAddress(SocketAddress remoteAddress) {
+    this.remoteAddress = remoteAddress;
+    return this;
+  }
 }
