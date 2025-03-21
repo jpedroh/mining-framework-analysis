a83066d34b98e99c6f960e179c5da5dd14116a66
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 15:11:02 +0000

    Mining Framework Analysis

--- src/main/java/net/iponweb/disthene/config/CarbonConfiguration.java
+++ src/main/java/net/iponweb/disthene/config/CarbonConfiguration.java
@@ -1,85 +1,85 @@
-package net.iponweb.disthene.config;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-
-/**
- * @author Andrei Ivanov
- */
-public class CarbonConfiguration {
-
-    private String bind;
-    private int port;
-    private List<Rollup> rollups = new ArrayList<>();
-    private Rollup baseRollup;
-    private int aggregatorDelay;
-    private List<String> authorizedTenants = new ArrayList<>();
-    private boolean allowAll = true;
-
-    public String getBind() {
-        return bind;
-    }
-
-    public void setBind(String bind) {
-        this.bind = bind;
-    }
-
-    public int getPort() {
-        return port;
-    }
-
-    public void setPort(int port) {
-        this.port = port;
-    }
-
-    public int getAggregatorDelay() {
-        return aggregatorDelay;
-    }
-
-    public void setAggregatorDelay(int aggregatorDelay) {
-        this.aggregatorDelay = aggregatorDelay;
-    }
-
-    public List<Rollup> getRollups() {
-        return rollups;
-    }
-
-    public void setRollups(List<Rollup> rollups) {
-        baseRollup = rollups.get(0);
-        this.rollups = rollups.subList(1, rollups.size());
-    }
-
-    public Rollup getBaseRollup() {
-        return baseRollup;
-    }
-
-    public List<String> getAuthorizedTenants() {
-        return authorizedTenants;
-    }
-
-    public void setAuthorizedTenants(List<String> authorizedTenants) {
-        this.authorizedTenants = Objects.requireNonNullElseGet(authorizedTenants, ArrayList::new);
-    }
-
-    public boolean isAllowAll() {
-        return allowAll;
-    }
-
-    public void setAllowAll(boolean allowAll) {
-        this.allowAll = allowAll;
-    }
-
-    @Override
-    public String toString() {
-        return "CarbonConfiguration{" +
-                "bind='" + bind + '\'' +
-                ", port=" + port +
-                ", rollups=" + rollups +
-                ", baseRollup=" + baseRollup +
-                ", aggregatorDelay=" + aggregatorDelay +
-                ", authorizedTenants=" + authorizedTenants +
-                ", allowAll=" + allowAll +
-                '}';
-    }
-}
+  package    net . iponweb . disthene . config ;   import   java . util . ArrayList ;  import   java . util . List ;  import   java . util . Objects ;   public class CarbonConfiguration  {   private String  bind ;   private  int  port ;   private  List  < Rollup >  rollups =  new  ArrayList  < >  ( ) ;   private Rollup  baseRollup ;   private  int  aggregatorDelay ;   private boolean  aggregateBaseRollup ;   public String getBind  ( )  {  return bind ; }   public void setBind  (  String bind )  {    this . bind = bind ; }   public  int getPort  ( )  {  return port ; }   public void setPort  (   int port )  {    this . port = port ; }   public  int getAggregatorDelay  ( )  {  return aggregatorDelay ; }   public void setAggregatorDelay  (   int aggregatorDelay )  {    this . aggregatorDelay = aggregatorDelay ; }   public boolean getAggregateBaseRollup  ( )  {  return aggregateBaseRollup ; }   public void setAggregateBaseRollup  (  boolean aggregateBaseRollup )  {    this . aggregateBaseRollup = aggregateBaseRollup ; }   public  List  < Rollup > getRollups  ( )  {  return rollups ; }   public void setRollups  (   List  < Rollup > rollups )  {   baseRollup =  rollups . get  ( 0 ) ;    this . rollups =  rollups . subList  ( 1 ,  rollups . size  ( ) ) ; }   public Rollup getBaseRollup  ( )  {  return baseRollup ; }    @ Override public String toString  ( )  {  return               
+<<<<<<<
+"CarbonConfiguration{"
+=======
+  "CarbonConfiguration{" + "bind='" + bind
+>>>>>>>
+ + 
+<<<<<<<
+"bind='"
+=======
+'\''
+>>>>>>>
+ + 
+<<<<<<<
+bind
+=======
+", port="
+>>>>>>>
+ + 
+<<<<<<<
+'\''
+=======
+port
+>>>>>>>
+ + 
+<<<<<<<
+", port="
+=======
+", rollups="
+>>>>>>>
+ + 
+<<<<<<<
+port
+=======
+rollups
+>>>>>>>
+ + 
+<<<<<<<
+", rollups="
+=======
+", baseRollup="
+>>>>>>>
+ + 
+<<<<<<<
+rollups
+=======
+baseRollup
+>>>>>>>
+ + 
+<<<<<<<
+", baseRollup="
+=======
+", aggregatorDelay="
+>>>>>>>
+ + 
+<<<<<<<
+baseRollup
+=======
+aggregatorDelay
+>>>>>>>
+ + 
+<<<<<<<
+", aggregatorDelay="
+=======
+", authorizedTenants="
+>>>>>>>
+ + 
+<<<<<<<
+aggregatorDelay
+=======
+authorizedTenants
+>>>>>>>
+ + 
+<<<<<<<
+", aggregateBaseRollup="
+=======
+", allowAll="
+>>>>>>>
+ + 
+<<<<<<<
+aggregateBaseRollup
+=======
+allowAll
+>>>>>>>
+ + '}' ; }   private  List  < String >  authorizedTenants =  new  ArrayList  < >  ( ) ;   private boolean  allowAll = true ;   public  List  < String > getAuthorizedTenants  ( )  {  return authorizedTenants ; }   public void setAuthorizedTenants  (   List  < String > authorizedTenants )  {    this . authorizedTenants =  Objects . requireNonNullElseGet  ( authorizedTenants ,  ArrayList :: new ) ; }   public boolean isAllowAll  ( )  {  return allowAll ; }   public void setAllowAll  (  boolean allowAll )  {    this . allowAll = allowAll ; } }
