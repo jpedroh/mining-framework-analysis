0143b53accb17a7965e344ed1ba7bed75c8b96e6
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 07:02:05 +0000

    Mining Framework Analysis

--- src/test/java/com/ning/billing/recurly/model/TestModelBase.java
+++ src/test/java/com/ning/billing/recurly/model/TestModelBase.java
@@ -1,30 +1,24 @@
-/*
- * Copyright 2010-2012 Ning, Inc.
- *
- * Ning licenses this file to you under the Apache License, version 2.0
- * (the "License"); you may not use this file except in compliance with the
- * License.  You may obtain a copy of the License at:
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
- * License for the specific language governing permissions and limitations
- * under the License.
- */
-
 package com.ning.billing.recurly.model;
-
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import org.testng.annotations.BeforeMethod;
+import com.fasterxml.jackson.databind.introspect.AnnotationIntrospectorPair;
+import com.fasterxml.jackson.databind.type.TypeFactory;
 
 public abstract class TestModelBase {
+  protected XmlMapper xmlMapper;
 
-    protected XmlMapper xmlMapper;
+  @BeforeMethod(alwaysRun = true) public void setUp() throws Exception {
+    xmlMapper = RecurlyObject.newXmlMapper();
 
-    @BeforeMethod(alwaysRun = true)
-    public void setUp() throws Exception {
-        xmlMapper = RecurlyObject.newXmlMapper();
-    }
+<<<<<<< Unknown file: This is a bug in JDime.
+=======
+    final AnnotationIntrospector secondary = new JaxbAnnotationIntrospector(TypeFactory.defaultInstance());
+>>>>>>> /usr/src/app/output/killbilling/recurly-java-library/500781acecc44ce57590effaab9a7369c2358035/src/test/java/com/ning/billing/recurly/model/TestModelBase.java/right.java
+
+
+<<<<<<< Unknown file: This is a bug in JDime.
+=======
+    final AnnotationIntrospector pair = new AnnotationIntrospectorPair(primary, secondary);
+>>>>>>> /usr/src/app/output/killbilling/recurly-java-library/500781acecc44ce57590effaab9a7369c2358035/src/test/java/com/ning/billing/recurly/model/TestModelBase.java/right.java
+  }
 }
