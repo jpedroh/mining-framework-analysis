2e006c2ca70c98c17da9cd70ae75b354bceb68a6
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 07:11:31 +0000

    Mining Framework Analysis

--- src/test/java/org/kohsuke/file_leak_detector/TransformerTest.java
+++ src/test/java/org/kohsuke/file_leak_detector/TransformerTest.java
@@ -1,5 +1,4 @@
 package org.kohsuke.file_leak_detector;
-
 import org.apache.commons.io.IOUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,7 +8,6 @@ import org.kohsuke.asm5.ClassReader;
 import org.kohsuke.asm5.util.CheckClassAdapter;
 import org.kohsuke.file_leak_detector.transform.ClassTransformSpec;
 import org.kohsuke.file_leak_detector.transform.TransformerImpl;
-
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,40 +16,29 @@ import java.util.zip.ZipFile;
 /**
  * @author Kohsuke Kawaguchi
  */
-@RunWith(Parameterized.class)
-public class TransformerTest {
-    List<ClassTransformSpec> specs = AgentMain.createSpec();
+@RunWith(value = Parameterized.class) public class TransformerTest {
+  List<ClassTransformSpec> specs = AgentMain.createSpec();
 
-    Class<?> c;
-    
-    public TransformerTest(Class<?> c) {
-        this.c = c;
+  Class<?> c;
+
+  public TransformerTest(Class<?> c) {
+    this.c = c;
+  }
+
+  @Test public void testInstrumentations() throws Exception {
+    TransformerImpl t = new TransformerImpl(specs);
+    String name = c.getName().replace('.', '/');
+    byte[] data = IOUtils.toByteArray(getClass().getClassLoader().getResourceAsStream(name + ".class"));
+    byte[] data2 = t.transform(name, data);
+    CheckClassAdapter.verify(new ClassReader(data2), false, new PrintWriter(System.err));
+  }
+
+  @Parameters public static List<Object[]> specs() throws Exception {
+    List<Object[]> r = new ArrayList<Object[]>();
+    for (ClassTransformSpec s : AgentMain.createSpec()) {
+      Class<?> c = TransformerTest.class.getClassLoader().loadClass(s.name.replace('/', '.'));
+      r.add(new Object[] { c });
     }
-
-    @Test
-    public void testInstrumentations() throws Exception {
-        TransformerImpl t = new TransformerImpl(specs);
-
-        String name = c.getName().replace('.', '/');
-        byte[] data = IOUtils.toByteArray(getClass().getClassLoader().getResourceAsStream(name + ".class"));
-        byte[] data2 = t.transform(name,data);
-
-//        File classFile = new File("/tmp/" + name + ".class");
-//        classFile.getParentFile().mkdirs();
-//        FileOutputStream o = new FileOutputStream(classFile);
-//        o.write(data2);
-//        o.close();
-
-        CheckClassAdapter.verify(new ClassReader(data2), false, new PrintWriter(System.err));
-    }
-    
-    @Parameters
-    public static List<Object[]> specs() throws Exception {
-        List<Object[]> r = new ArrayList<Object[]>();
-        for (ClassTransformSpec s : AgentMain.createSpec()) {
-            Class<?> c = TransformerTest.class.getClassLoader().loadClass(s.name.replace('/', '.'));
-            r.add(new Object[]{c});
-        }
-        return r;
-    }
+    return r;
+  }
 }
