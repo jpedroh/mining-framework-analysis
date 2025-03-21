79f1320db6c56a755e2ae7c4c0af2e569203ca57
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 23:02:00 +0000

    Mining Framework Analysis

--- opennlp-morfologik-addon/src/test/java/opennlp/morfologik/tagdict/POSTaggerFactoryTest.java
+++ opennlp-morfologik-addon/src/test/java/opennlp/morfologik/tagdict/POSTaggerFactoryTest.java
@@ -1,22 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package opennlp.morfologik.tagdict;
-
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -24,10 +6,8 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
-
 import org.junit.Assert;
 import org.junit.Test;
-
 import opennlp.morfologik.builder.POSDictionayBuilderTest;
 import opennlp.tools.cmdline.CmdLineUtil;
 import opennlp.tools.postag.POSModel;
@@ -46,16 +26,10 @@ import opennlp.tools.util.model.ModelType;
  * Tests for the {@link POSTaggerFactory} class.
  */
 public class POSTaggerFactoryTest {
-
-  private static ObjectStream<POSSample> createSampleStream()
-      throws IOException {
-
+  private static ObjectStream<POSSample> createSampleStream() throws IOException {
     MarkableFileInputStreamFactory sampleDataIn;
     try {
-      sampleDataIn =
-              new MarkableFileInputStreamFactory(
-                      new File(POSTaggerFactory.class.getResource(
-                              "/AnnotatedSentences.txt").toURI().getPath()));
+      sampleDataIn = new MarkableFileInputStreamFactory(new File(POSTaggerFactory.class.getResource("/AnnotatedSentences.txt").toURI().getPath()));
     } catch (URISyntaxException e) {
       throw new IOException(e);
     }
@@ -65,43 +39,29 @@ public class POSTaggerFactoryTest {
     } catch (IOException ex) {
       CmdLineUtil.handleCreateObjectStreamError(ex);
     }
-
     return new WordTagSampleStream(lineStream);
   }
 
-  private static POSModel trainPOSModel(ModelType type, POSTaggerFactory factory)
-      throws IOException {
-    return POSTaggerME.train("en", createSampleStream(),
-        TrainingParameters.defaultParams(), factory);
+  private static POSModel trainPOSModel(ModelType type, POSTaggerFactory factory) throws IOException {
+    return POSTaggerME.train("en", createSampleStream(), TrainingParameters.defaultParams(), factory);
   }
 
-  @Test
-  public void testPOSTaggerWithCustomFactory() throws Exception {
-
+  @Test public void testPOSTaggerWithCustomFactory() throws Exception {
     Path dictionary = POSDictionayBuilderTest.createMorfologikDictionary();
     dictionary.toFile().deleteOnExit();
     POSTaggerFactory inFactory = new MorfologikPOSTaggerFactory();
     TagDictionary inDict = inFactory.createTagDictionary(dictionary.toFile());
     inFactory.setTagDictionary(inDict);
-
     POSModel posModel = trainPOSModel(ModelType.MAXENT, inFactory);
-
     POSTaggerFactory factory = posModel.getFactory();
     Assert.assertTrue(factory.getTagDictionary() instanceof MorfologikTagDictionary);
-
     factory = null;
-
     ByteArrayOutputStream out = new ByteArrayOutputStream();
     posModel.serialize(out);
     ByteArrayInputStream in = new ByteArrayInputStream(out.toByteArray());
-
     POSModel fromSerialized = new POSModel(in);
-
     factory = fromSerialized.getFactory();
     Assert.assertTrue(factory.getTagDictionary() instanceof MorfologikTagDictionary);
-
     Assert.assertEquals(2, factory.getTagDictionary().getTags("casa").length);
   }
-
 }
-
