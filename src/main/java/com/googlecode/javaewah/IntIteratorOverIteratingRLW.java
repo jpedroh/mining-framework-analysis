4e55ca1c9537b0a279e935fa982d6f7b9332bdb3
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 12:16:59 +0000

    Mining Framework Analysis

--- src/main/java/com/googlecode/javaewah/IntIteratorOverIteratingRLW.java
+++ src/main/java/com/googlecode/javaewah/IntIteratorOverIteratingRLW.java
@@ -1,5 +1,4 @@
 package com.googlecode.javaewah;
-
 import static com.googlecode.javaewah.EWAHCompressedBitmap.wordinbits;
 
 /**
@@ -7,90 +6,82 @@ import static com.googlecode.javaewah.EWAHCompressedBitmap.wordinbits;
  *
  */
 public class IntIteratorOverIteratingRLW implements IntIterator {
-	IteratingRLW parent;
-	private int position;
-	private int runningLength;
-	private long word;
-	private int wordPosition;
-	private int wordLength;
-	private int literalPosition;
-	private boolean hasnext;
+  IteratingRLW parent;
 
-	/**
+  private int position;
+
+  private int runningLength;
+
+  private long word;
+
+  private int wordPosition;
+
+  private int wordLength;
+
+  private int literalPosition;
+
+  private boolean hasnext;
+
+  /**
 	 * @param p iterator we wish to iterate over
 	 */
-	public IntIteratorOverIteratingRLW(final IteratingRLW p) {
-		parent = p;
-		position = 0;
-                setupForCurrentRunningLengthWord();
-                // can we assume that an empty bitmap can always be recognized like this...?
-                hasnext = (runningHasNext() || literalHasNext()); // ??
-	}
+  public IntIteratorOverIteratingRLW(final IteratingRLW p) {
+    parent = p;
+    position = 0;
+    setupForCurrentRunningLengthWord();
+    hasnext = (runningHasNext() || literalHasNext());
+  }
 
-	/**
+  /**
 	 * @return whether we could find another set bit
 	 */
-	private final boolean moveToNext() {
-            while (!runningHasNext() && !literalHasNext()) {
-                if (parent.next())
-                    setupForCurrentRunningLengthWord();
-                else return false;
-            }
-            return true;
-	}
+  private final boolean moveToNext() {
+    while (!runningHasNext() && !literalHasNext()) {
+      if (parent.next()) {
+        setupForCurrentRunningLengthWord();
+      } else {
+        return false;
+      }
+    }
+    return true;
+  }
 
-	public boolean hasNext() {
-		return this.hasnext;
-	}
+  public boolean hasNext() {
+    return this.hasnext;
+  }
 
-	public final int next() {
-		final int answer;
-		if (runningHasNext()) {
-			answer = this.position++;
-		} else {
-			final int bit = Long.numberOfTrailingZeros(this.word);
-			this.word ^= (1l << bit);
-			answer = this.literalPosition + bit;
-		}
-		this.hasnext = this.moveToNext();
-		return answer;
-	}
+  public final int next() {
+    final int answer;
+    if (runningHasNext()) {
+      answer = this.position++;
+    } else {
+      final int bit = Long.numberOfTrailingZeros(this.word);
+      this.word ^= (1l << bit);
+      answer = this.literalPosition + bit;
+    }
+    this.hasnext = this.moveToNext();
+    return answer;
+  }
 
-	private final void setupForCurrentRunningLengthWord() {
-		this.runningLength = wordinbits * (int) this.parent.getRunningLength()
-				+ this.position;
-		if (!parent.getRunningBit()) {
-			this.position = this.runningLength;
-		}
-		this.wordPosition = 0;  // ofk, was: this.parent.getNumberOfLiteralWords();
-		this.wordLength = // ofk , was: this.wordPosition
-				+ this.parent.getNumberOfLiteralWords();
-	}
+  private final void setupForCurrentRunningLengthWord() {
+    this.runningLength = wordinbits * (int) this.parent.getRunningLength() + this.position;
+    if (!parent.getRunningBit()) {
+      this.position = this.runningLength;
+    }
+    this.wordPosition = 0;
+    this.wordLength = +this.parent.getNumberOfLiteralWords();
+  }
 
-	// private final boolean eatRunningLengthWord() {
-	// 	this.runningLength = wordinbits * (int) this.parent.getRunningLength()
-	// 			+ this.position;
-        //         System.out.println("setting runningLength to "+runningLength+" since parent's running length is "+parent.getRunningLength());
-	// 	if (!parent.getRunningBit()) {
-	// 		this.position = this.runningLength;
-	// 	}
-	// 	this.wordPosition = 0;  // ofk, was: this.parent.getNumberOfLiteralWords();
-	// 	this.wordLength = // ofk , was: this.wordPosition
-	// 			+ this.parent.getNumberOfLiteralWords();
-        //         System.out.println("advancing parent to next");
-	// 	return this.parent.next();
-	// }
+  private final boolean runningHasNext() {
+    return this.position < this.runningLength;
+  }
 
-	private final boolean runningHasNext() {
-		return this.position < this.runningLength;
-	}
-
-	private final boolean literalHasNext() {
-		while (this.word == 0 && this.wordPosition < this.wordLength) {
-			this.word = this.parent.getLiteralWordAt(this.wordPosition++);
-			this.literalPosition = this.position;
-			this.position += wordinbits;
-		}
-		return this.word != 0;
-	}
+  private final boolean literalHasNext() {
+    while (this.word == 0 && this.wordPosition < this.wordLength) {
+      this.word = this.parent.getLiteralWordAt(this.wordPosition++);
+      this.literalPosition = this.position;
+      this.position += wordinbits;
+    }
+    return this.word != 0;
+  }
 }
--- src/main/java/com/googlecode/javaewah/IteratingRLW.java
+++ src/main/java/com/googlecode/javaewah/IteratingRLW.java
@@ -5,35 +5,41 @@ package com.googlecode.javaewah;
  *
  */
 public interface IteratingRLW {
-	 /**
+  /**
 	 * @return whether there is more
 	 */
-	public boolean next() ;
-	 /**
+  public boolean next();
+
+  /**
 	 * @param index where the literal word is
 	 * @return the literal word at the given index.
 	 */
-	public long getLiteralWordAt(int index);
-	 /**
+  public long getLiteralWordAt(int index);
+
+  /**
 	 * @return the number of literal (non-fill) words
 	 */
-	public int getNumberOfLiteralWords() ;
-	 /**
+  public int getNumberOfLiteralWords();
+
+  /**
 	 * @return the bit used for the fill bits
 	 */
-	public boolean getRunningBit() ;
-	  /**
+  public boolean getRunningBit();
+
+  /**
 	 * @return sum of getRunningLength() and getNumberOfLiteralWords() 
 	 */
-	public long size() ;
-	  /**
+  public long size();
+
+  /**
 	 * @return length of the run of fill words
 	 */
-	public long getRunningLength() ;
-	  /**
+  public long getRunningLength();
+
+  /**
 	 * @param x the number of words to discard
 	 */
-	public void discardFirstWords(long x);
+  public void discardFirstWords(long x);
 
-    public IteratingRLW clone() throws CloneNotSupportedException;
+  public IteratingRLW clone() throws CloneNotSupportedException;
 }
