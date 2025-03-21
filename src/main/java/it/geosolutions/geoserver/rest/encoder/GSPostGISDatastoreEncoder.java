c25e317c0a42989cee56ed45b2d73991902b841f
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 23:43:47 +0000

    Mining Framework Analysis

--- src/main/java/it/geosolutions/geoserver/rest/encoder/GSPostGISDatastoreEncoder.java
+++ src/main/java/it/geosolutions/geoserver/rest/encoder/GSPostGISDatastoreEncoder.java
@@ -1,325 +1,116 @@
-/*
- *  GeoServer-Manager - Simple Manager Library for GeoServer
- *  
- *  Copyright (C) 2007,2011 GeoSolutions S.A.S.
- *  http://www.geo-solutions.it
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
-package it.geosolutions.geoserver.rest.encoder;
-
-import it.geosolutions.geoserver.rest.encoder.utils.NestedElementEncoder;
-import it.geosolutions.geoserver.rest.encoder.utils.PropertyXMLEncoder;
-
-/**
- * Geoserver datastore XML encoder.
- *  
- * @author Eric Grosso
- * @author ETj
- * @author Carlo Cancellieri - carlo.cancellieri@geo-solutions.it
- */
-public class GSPostGISDatastoreEncoder extends PropertyXMLEncoder {
-
-    private NestedElementEncoder connectionParameters = new NestedElementEncoder("connectionParameters");
-
-    public GSPostGISDatastoreEncoder() {
-        super("dataStore");
-        addType("PostGIS"); // may be overwritten with e.g. "PostGIS (JNDI)"
-        addDatabaseType("postgis");
-        addContent(connectionParameters.getRoot());
-    }
-    
-    /**
-     * Set some initial defaults.
-     * <br/><br/>
-     * The default parameters are as follows: <ul>
-     * <li>maximum connections: 10, </li>
-     * <li>minimum connections: 1,</li>
-     * <li>fetch size: 1000, </li>
-     * <li>connection timeout: 20 seconds, </li>
-     * <li>loose BBox: true, </li>
-     * <li>prepared statements: false,</li>
-     * <li>maximum open prepared statements: 50.    </li>
-     * </ul>
-     */
-    public void defaultInit() {
-        setMinConnections(1);
-        setMaxConnections(10);
-        setFetchSize(1000);
-        setConnectionTimeout(20);
-        setLooseBBox(true);
-        setPreparedStatements(false);
-        setMaxOpenPreparedStatements(50);
-    }
-
-    /**
-	 * @deprecated will be set to protected in the next release
-	 */
-	public void addName(String name) {
-        add("name", name);
-    }
-    
-    public void setName(String name) {
-        set("name", name);
-    }
-
-    /**
-	 * @deprecated will be set to protected in the next release
-	 */
-	public void addDescription(String description) {
-        add("description", description);
-    }
-    
-    public void setDescription(String description) {
-        set("description", description);
-    }
-
-    /**
-     * @deprecated will be set to protected in the next release
-     */
-	public void addType(String type) {
-        add("type", type);
-    }
-    
-    public void setType(String type) {
-        set("type", type);
-    }
-
-    /**
-		* @deprecated will be set to protected in the next release
-		*/
-	public void addEnabled(boolean enabled) {
-        add("enabled", Boolean.toString(enabled));
-    }
-    
-    public void setEnabled(boolean enabled) {
-        set("enabled", Boolean.toString(enabled));
-    }
-    
-    /**
-	 * @deprecated will be set to protected in the next release
-	 */
-	public void addNamespace(String namespace) {
-        connectionParameters.add("namespace", namespace);
-    }
-    
-    public void setNamespace(String namespace) {
-        connectionParameters.set("namespace", namespace);
-    }
-    
-    /**
-	 * @deprecated will be set to protected in the next release
-	 */
-	public void addHost(String host) {
-        connectionParameters.add("host", host);
-    }
-    
-    public void setHost(String host) {
-        connectionParameters.set("host", host);
-    }
-
-    /**
-	 * @deprecated will be set to protected in the next release
-	 */
-	public void addPort(int port) {
-        connectionParameters.add("port", Integer.toString(port));
-    }
-    
-    public void setPort(int port) {
-        connectionParameters.set("port", Integer.toString(port));
-    }
-
-    /**
-	 * @deprecated will be set to protected in the next release
-	 */
-	public void addDatabase(String database) {
-        connectionParameters.add("database", database);
-    }
-    
-    public void setDatabase(String database) {
-        connectionParameters.set("database", database);
-    }
-
-    /**
-	 * @deprecated will be set to protected in the next release
-	 */
-	public void addSchema(String schema) {
-        connectionParameters.add("schema", schema);
-    }
-    
-    public void setSchema(String schema) {
-        connectionParameters.set("schema", schema);
-    }
-
-    /**
-	 * @deprecated will be set to protected in the next release
-	 */
-	public void addUser(String user) {
-        connectionParameters.add("user", user);
-    }
-    
-    public void setUser(String user) {
-        connectionParameters.set("user", user);
-    }
-
-    /**
-	 * @deprecated will be set to protected in the next release
-	 */
-	public void addPassword(String password) {
-        connectionParameters.add("passwd", password);
-    }
-    
-    public void setPassword(String password) {
-        connectionParameters.set("passwd", password);
-    }
-
-    /**
-	 * @deprecated will be set to protected in the next release
-	 */
-	public void addDatabaseType(String dbtype) {
-        connectionParameters.add("dbtype", dbtype);
-    }
-
-    public void setDatabaseType(String dbtype) {
-        connectionParameters.set("dbtype", dbtype);
-    }
-
-    /**
-	 * @deprecated will be set to protected in the next release
-	 */
-	public void addJndiReferenceName(String jndiReferenceName) {
-        connectionParameters.add("jndiReferenceName", jndiReferenceName);
-    }
-    
-    public void setJndiReferenceName(String jndiReferenceName) {
-        connectionParameters.set("jndiReferenceName", jndiReferenceName);
-    }
-
-    /**
-	 * @deprecated will be set to protected in the next release
-	 */
-	public void addExposePrimaryKeys(boolean exposePrimaryKeys) {
-    	connectionParameters.add("Expose primary keys", Boolean.toString(exposePrimaryKeys));
-    }
-    
-    public void setExposePrimaryKeys(boolean exposePrimaryKeys) {
-    	connectionParameters.set("Expose primary keys", Boolean.toString(exposePrimaryKeys));
-    }
-    
-    /**
-	 * @deprecated will be set to protected in the next release
-	 */
-	public void addMaxConnections(int maxConnections) {
-    	connectionParameters.add("max connections", Integer.toString(maxConnections));
-    }
-    
-    public void setMaxConnections(int maxConnections) {
-    	connectionParameters.set("max connections", Integer.toString(maxConnections));
-    }
-    
-    /**
-	 * @deprecated will be set to protected in the next release
-	 */
-	public void addMinConnections(int minConnections) {
-    	connectionParameters.add("min connections", Integer.toString(minConnections));
-    }
-    
-    public void setMinConnections(int minConnections) {
-    	connectionParameters.set("min connections", Integer.toString(minConnections));
-    }
-    
-    /**
-	 * @deprecated will be set to protected in the next release
-	 */
-	public void addFetchSize(int fetchSize) {
-    	connectionParameters.add("fetch size", Integer.toString(fetchSize));
-    }
-    
-    public void setFetchSize(int fetchSize) {
-    	connectionParameters.set("fetch size", Integer.toString(fetchSize));
-    }
-    
-    /**
-	 * @deprecated will be set to protected in the next release
-	 */
-	public void addConnectionTimeout(int seconds) {
-    	connectionParameters.add("Connection timeout", Integer.toString(seconds));
-    }
-    
-    public void setConnectionTimeout(int seconds) {
-    	connectionParameters.set("Connection timeout", Integer.toString(seconds));
-    }
-    
-    /**
-	 * @deprecated will be set to protected in the next release
-	 */
-	public void addValidateConnections(boolean validateConnections) {
-    	connectionParameters.add("validate connections", Boolean.toString(validateConnections));
-    }
-    
-    public void setValidateConnections(boolean validateConnections) {
-    	connectionParameters.set("validate connections", Boolean.toString(validateConnections));
-    }
-    
-    /**
-	 * @deprecated will be set to protected in the next release
-	 */
-	public void addPrimaryKeyMetadataTable(String primaryKeyMetadataTable) {
-    	connectionParameters.add("Primary key metadata table", primaryKeyMetadataTable);
-    }
-    
-    public void setPrimaryKeyMetadataTable(String primaryKeyMetadataTable) {
-    	connectionParameters.set("Primary key metadata table", primaryKeyMetadataTable);
-    }
-    
-    /**
-	 * @deprecated will be set to protected in the next release
-	 */
-	public void addLooseBBox(boolean looseBBox) {
-    	connectionParameters.add("Loose bbox", Boolean.toString(looseBBox));
-    }
-    
-    public void setLooseBBox(boolean looseBBox) {
-    	connectionParameters.set("Loose bbox", Boolean.toString(looseBBox));
-    }
-    
-    /**
-	 * @deprecated will be set to protected in the next release
-	 */
-	public void addPreparedStatements(boolean preparedStatements) {
-    	connectionParameters.add("preparedStatements", Boolean.toString(preparedStatements));
-    }
-    
-    public void setPreparedStatements(boolean preparedStatements) {
-    	connectionParameters.set("preparedStatements", Boolean.toString(preparedStatements));
-    }
-    
-    /**
-	 * @deprecated will be set to protected in the next release
-	 */
-	public void addMaxOpenPreparedStatements(int maxOpenPreparedStatements) {
-    	connectionParameters.add("Max open prepared statements", Integer.toString(maxOpenPreparedStatements));
-    }
-    
-    public void setMaxOpenPreparedStatements(int maxOpenPreparedStatements) {
-    	connectionParameters.set("Max open prepared statements", Integer.toString(maxOpenPreparedStatements));
-    }
-
-}
+  package     it . geosolutions . geoserver . rest . encoder ;   import       it . geosolutions . geoserver . rest . encoder . utils . NestedElementEncoder ;  import       it . geosolutions . geoserver . rest . encoder . utils . PropertyXMLEncoder ;   public class GSPostGISDatastoreEncoder  extends PropertyXMLEncoder  {   private NestedElementEncoder  connectionParameters =  new NestedElementEncoder  ( "connectionParameters" ) ;   public GSPostGISDatastoreEncoder  ( )  {  super  ( "dataStore" ) ;   addType  ( "PostGIS" ) ;   addDatabaseType  ( "postgis" ) ;   addContent  (  connectionParameters . getRoot  ( ) ) ; }   public void defaultInit  ( )  {   addMinConnections  ( 1 ) ;   addMaxConnections  ( 10 ) ;   addFetchSize  ( 1000 ) ;   addConnectionTimeout  ( 20 ) ;   addLooseBBox  ( true ) ;   addPreparedStatements  ( false ) ;   addMaxOpenPreparedStatements  ( 50 ) ; }   public void addName  (  String name )  {   add  ( "name" , name ) ; }   public void addDescription  (  String description )  {   add  ( "description" , description ) ; }   public void addType  (  String type )  {   add  ( "type" , type ) ; }   public void addEnabled  (  boolean enabled )  {   add  ( "enabled" ,  Boolean . toString  ( enabled ) ) ; }   public void addNamespace  (  String namespace )  {   connectionParameters . add  ( "namespace" , namespace ) ; }   public void addHost  (  String host )  {   connectionParameters . add  ( "host" , host ) ; }   public void addPort  (   int port )  {   connectionParameters . add  ( "port" ,  Integer . toString  ( port ) ) ; }   public void addDatabase  (  String database )  {   connectionParameters . add  ( "database" , database ) ; }   public void addSchema  (  String schema )  {   connectionParameters . add  ( "schema" , schema ) ; }   public void addUser  (  String user )  {   connectionParameters . add  ( "user" , user ) ; }   public void addPassword  (  String password )  {   connectionParameters . add  ( "passwd" , password ) ; }   public void addDatabaseType  (  String dbtype )  {   connectionParameters . add  ( "dbtype" , dbtype ) ; }   public void addJndiReferenceName  (  String jndiReferenceName )  {   connectionParameters . add  ( "jndiReferenceName" , jndiReferenceName ) ; }   public void addExposePrimaryKeys  (  boolean exposePrimaryKeys )  {   connectionParameters . add  ( "Expose primary keys" ,  Boolean . toString  ( exposePrimaryKeys ) ) ; }   public void addMaxConnections  (   int maxConnections )  {   connectionParameters . add  ( "max connections" ,  Integer . toString  ( maxConnections ) ) ; }   public void addMinConnections  (   int minConnections )  {   connectionParameters . add  ( "min connections" ,  Integer . toString  ( minConnections ) ) ; }   public void addFetchSize  (   int fetchSize )  {   connectionParameters . add  ( "fetch size" ,  Integer . toString  ( fetchSize ) ) ; }   public void addConnectionTimeout  (   int seconds )  {   connectionParameters . add  ( "Connection timeout" ,  Integer . toString  ( seconds ) ) ; }   public void addValidateConnections  (  boolean validateConnections )  {   connectionParameters . add  ( "validate connections" ,  Boolean . toString  ( validateConnections ) ) ; }   public void addPrimaryKeyMetadataTable  (  String primaryKeyMetadataTable )  {   connectionParameters . add  ( "Primary key metadata table" , primaryKeyMetadataTable ) ; }   public void addLooseBBox  (  boolean looseBBox )  {   connectionParameters . add  ( "Loose bbox" ,  Boolean . toString  ( looseBBox ) ) ; }   public void addPreparedStatements  (  boolean preparedStatements )  {   connectionParameters . add  ( "preparedStatements" ,  Boolean . toString  ( preparedStatements ) ) ; }   public void addMaxOpenPreparedStatements  (   int maxOpenPreparedStatements )  {   connectionParameters . add  ( "Max open prepared statements" ,  Integer . toString  ( maxOpenPreparedStatements ) ) ; } 
+<<<<<<<
+=======
+  public void setName  (  String name )  {   set  ( "name" , name ) ; }
+>>>>>>>
+ 
+<<<<<<<
+=======
+  public void setDescription  (  String description )  {   set  ( "description" , description ) ; }
+>>>>>>>
+ 
+<<<<<<<
+=======
+  public void setType  (  String type )  {   set  ( "type" , type ) ; }
+>>>>>>>
+ 
+<<<<<<<
+=======
+  public void setEnabled  (  boolean enabled )  {   set  ( "enabled" ,  Boolean . toString  ( enabled ) ) ; }
+>>>>>>>
+ 
+<<<<<<<
+=======
+  public void setNamespace  (  String namespace )  {   connectionParameters . set  ( "namespace" , namespace ) ; }
+>>>>>>>
+ 
+<<<<<<<
+=======
+  public void setHost  (  String host )  {   connectionParameters . set  ( "host" , host ) ; }
+>>>>>>>
+ 
+<<<<<<<
+=======
+  public void setPort  (   int port )  {   connectionParameters . set  ( "port" ,  Integer . toString  ( port ) ) ; }
+>>>>>>>
+ 
+<<<<<<<
+=======
+  public void setDatabase  (  String database )  {   connectionParameters . set  ( "database" , database ) ; }
+>>>>>>>
+ 
+<<<<<<<
+=======
+  public void setSchema  (  String schema )  {   connectionParameters . set  ( "schema" , schema ) ; }
+>>>>>>>
+ 
+<<<<<<<
+=======
+  public void setUser  (  String user )  {   connectionParameters . set  ( "user" , user ) ; }
+>>>>>>>
+ 
+<<<<<<<
+=======
+  public void setPassword  (  String password )  {   connectionParameters . set  ( "passwd" , password ) ; }
+>>>>>>>
+ 
+<<<<<<<
+=======
+  public void setDatabaseType  (  String dbtype )  {   connectionParameters . set  ( "dbtype" , dbtype ) ; }
+>>>>>>>
+ 
+<<<<<<<
+=======
+  public void setJndiReferenceName  (  String jndiReferenceName )  {   connectionParameters . set  ( "jndiReferenceName" , jndiReferenceName ) ; }
+>>>>>>>
+ 
+<<<<<<<
+=======
+  public void setExposePrimaryKeys  (  boolean exposePrimaryKeys )  {   connectionParameters . set  ( "Expose primary keys" ,  Boolean . toString  ( exposePrimaryKeys ) ) ; }
+>>>>>>>
+ 
+<<<<<<<
+=======
+  public void setMaxConnections  (   int maxConnections )  {   connectionParameters . set  ( "max connections" ,  Integer . toString  ( maxConnections ) ) ; }
+>>>>>>>
+ 
+<<<<<<<
+=======
+  public void setMinConnections  (   int minConnections )  {   connectionParameters . set  ( "min connections" ,  Integer . toString  ( minConnections ) ) ; }
+>>>>>>>
+ 
+<<<<<<<
+=======
+  public void setFetchSize  (   int fetchSize )  {   connectionParameters . set  ( "fetch size" ,  Integer . toString  ( fetchSize ) ) ; }
+>>>>>>>
+ 
+<<<<<<<
+=======
+  public void setConnectionTimeout  (   int seconds )  {   connectionParameters . set  ( "Connection timeout" ,  Integer . toString  ( seconds ) ) ; }
+>>>>>>>
+ 
+<<<<<<<
+=======
+  public void setValidateConnections  (  boolean validateConnections )  {   connectionParameters . set  ( "validate connections" ,  Boolean . toString  ( validateConnections ) ) ; }
+>>>>>>>
+ 
+<<<<<<<
+=======
+  public void setPrimaryKeyMetadataTable  (  String primaryKeyMetadataTable )  {   connectionParameters . set  ( "Primary key metadata table" , primaryKeyMetadataTable ) ; }
+>>>>>>>
+ 
+<<<<<<<
+=======
+  public void setLooseBBox  (  boolean looseBBox )  {   connectionParameters . set  ( "Loose bbox" ,  Boolean . toString  ( looseBBox ) ) ; }
+>>>>>>>
+ 
+<<<<<<<
+=======
+  public void setPreparedStatements  (  boolean preparedStatements )  {   connectionParameters . set  ( "preparedStatements" ,  Boolean . toString  ( preparedStatements ) ) ; }
+>>>>>>>
+ 
+<<<<<<<
+=======
+  public void setMaxOpenPreparedStatements  (   int maxOpenPreparedStatements )  {   connectionParameters . set  ( "Max open prepared statements" ,  Integer . toString  ( maxOpenPreparedStatements ) ) ; }
+>>>>>>>
+ }
--- src/main/java/it/geosolutions/geoserver/rest/encoder/metadata/GSDimensionInfoEncoder.java
+++ src/main/java/it/geosolutions/geoserver/rest/encoder/metadata/GSDimensionInfoEncoder.java
@@ -1,121 +1,24 @@
-/*
- *  GeoServer-Manager - Simple Manager Library for GeoServer
- *  
- *  Copyright (C) 2007,2011 GeoSolutions S.A.S.
- *  http://www.geo-solutions.it
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
-package it.geosolutions.geoserver.rest.encoder.metadata;
-
-import it.geosolutions.geoserver.rest.encoder.utils.XmlElement;
-
-import java.math.BigDecimal;
-
-/**
- * 
- * @author Carlo Cancellieri - carlo.cancellieri@geo-solutions.it
- *
- */
-public class GSDimensionInfoEncoder extends XmlElement{
-	public final static String DIMENSIONINFO="dimensionInfo";
-	
-	public final static String RESOLUTION="resolution";
-	public final static String PRESENTATION="presentation";
-	
-	private boolean enabled;
-	
-	/**
-	 * Enum for presentation mode
-	 */
-	public enum Presentation{
-		LIST,
-		CONTINUOUS_INTERVAL
-	}
-	/**
-	 * Enum for presentation mode which needs arguments
-	 */
-	public enum PresentationDiscrete{
-		DISCRETE_INTERVAL
-	}
-	
-	/**
-	 * Build a dimension
-	 * @param enabled enable dimension if true
-	 * @note a enabled dimension also need a presentation mode set.
-	 */
-	public GSDimensionInfoEncoder(final boolean enabled) {
-		super(DIMENSIONINFO);
-		add("enabled", (enabled)?"true":"false");
-		this.enabled=enabled;
-	}
-	
-	/**
-	 * build an not enabled dimension
-	 */
-	public GSDimensionInfoEncoder() {
-		super(DIMENSIONINFO);
-		add("enabled", "false");
-		this.enabled=Boolean.FALSE;
-	}
-	
-	public void setEnabled(final boolean enabled){
-		set("enabled", "true");
-		this.enabled=Boolean.TRUE;
-	}
-	
-	/**
-	 * @deprecated will be set to protected in the next release {@link setPresentation(final Presentation pres)}
-	 * @param pres
-	 */
-	public void addPresentation(final Presentation pres){
-		if (enabled){
-			add(PRESENTATION,pres.toString());
-		}
-	}
-	
-	public void setPresentation(final Presentation pres){
-		if (enabled){
-			set(PRESENTATION,pres.toString());
-			remove(RESOLUTION);
-		}
-	}
-	
-	/**
-	 * @param pres
-	 * @param interval
-	 * @deprecated will be set to protected in the next release {@link setPresentation(final PresentationDiscrete pres, final BigDecimal interval)}
-	 */
-	public void addPresentation(final PresentationDiscrete pres, final BigDecimal interval){
-		if (enabled){
-			add(PRESENTATION,pres.toString());
-			add(RESOLUTION,String.valueOf(interval));	
-		}
-	}
-	
-	public void setPresentation(final PresentationDiscrete pres, final BigDecimal interval){
-		if (enabled){
-			set(PRESENTATION,pres.toString());
-			set(RESOLUTION,String.valueOf(interval));	
-		}
-	}
-	
-	
-}
+  package      it . geosolutions . geoserver . rest . encoder . metadata ;   import       it . geosolutions . geoserver . rest . encoder . utils . XmlElement ;  import   java . math . BigDecimal ;  import   org . jdom . Element ;   public class GSDimensionInfoEncoder  extends XmlElement  {   private boolean  enabled ;   public enum Presentation  {  LIST ,  CONTINUOUS_INTERVAL }   public enum PresentationDiscrete  {  DISCRETE_INTERVAL }   public GSDimensionInfoEncoder  (   final boolean enabled )  {  super  ( DIMENSIONINFO ) ;   add  ( "enabled" ,   ( enabled ) ? "true" : "false" ) ;    this . enabled = enabled ; }   public GSDimensionInfoEncoder  ( )  {  super  ( DIMENSIONINFO ) ;   add  ( "enabled" , "false" ) ;    this . enabled =  Boolean . FALSE ; }   public void addPresentation  (   final Presentation pres )  {  if  ( enabled )  {   add  ( 
+<<<<<<<
+"presentation"
+=======
+PRESENTATION
+>>>>>>>
+ ,  pres . toString  ( ) ) ; } }   public void addPresentation  (   final PresentationDiscrete pres ,   final BigDecimal interval )  {  if  ( enabled )  {   add  ( 
+<<<<<<<
+"presentation"
+=======
+PRESENTATION
+>>>>>>>
+ ,  pres . toString  ( ) ) ;   add  ( 
+<<<<<<<
+"resolution"
+=======
+RESOLUTION
+>>>>>>>
+ ,  String . valueOf  ( interval ) ) ; } }   public void add  (  String nodename ,  String nodetext )  {   final Element  el =  new Element  ( nodename ) ;   el . setText  ( nodetext ) ;   this . addContent  ( el ) ; }   public final static String  DIMENSIONINFO = "dimensionInfo" ;   public final static String  RESOLUTION = "resolution" ;   public final static String  PRESENTATION = "presentation" ;   public void setEnabled  (   final boolean enabled )  {   set  ( "enabled" , "true" ) ;    this . enabled =  Boolean . TRUE ; } 
+<<<<<<<
+=======
+  public void setPresentation  (   final Presentation pres )  {  if  ( enabled )  {   set  ( PRESENTATION ,  pres . toString  ( ) ) ;   remove  ( RESOLUTION ) ; } }
+>>>>>>>
+   public void setPresentation  (   final PresentationDiscrete pres ,   final BigDecimal interval )  {  if  ( enabled )  {   set  ( PRESENTATION ,  pres . toString  ( ) ) ;   set  ( RESOLUTION ,  String . valueOf  ( interval ) ) ; } } }
