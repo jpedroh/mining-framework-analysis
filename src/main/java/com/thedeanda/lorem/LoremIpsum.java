0849639bb2514de1f49fff838534d7885cffedbc
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 23:10:52 +0000

    Mining Framework Analysis

--- src/main/java/com/thedeanda/lorem/LoremIpsum.java
+++ src/main/java/com/thedeanda/lorem/LoremIpsum.java
@@ -1,5 +1,4 @@
 package com.thedeanda.lorem;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -36,408 +35,269 @@ import java.util.Random;
  * 
  */
 public class LoremIpsum implements Lorem {
-	/*
-	 * this command was useful:
-	 * 
-	 * cat lorem.txt | sed -e 's/[,;.]//g' | sed -e 's/ /\n/g' | sed -e \
-	 * 'y/ABCDEFGHIJKLMNOPQRSTUVWXYZ/abcdefghijklmnopqrstuvwxyz/' | sort | \
-	 * uniq > lorem.txt.2
-	 */
-	private static LoremIpsum instance;
+  private static LoremIpsum instance;
 
-	private List<String> words = new ArrayList<String>();
-	private Random random = null;
-	private List<String> maleNames;
-	private List<String> femaleNames;
-	private List<String> surnames;
-	private List<String> firstNames;
-	private List<String> stateAbbr;
-	private List<String> stateFull;
-	private List<String> cities;
-	private List<String> countries;
+  private List<String> words = new ArrayList<String>();
 
-	private String[] URL_HOSTS = new String[] { "https://www.google.com/#q=%s",
-			"http://www.bing.com/search?q=%s",
-			"https://search.yahoo.com/search?p=%s",
-			"https://duckduckgo.com/?q=%s" };
+  private Random random = null;
 
-	public static LoremIpsum getInstance() {
-		if (instance == null) {
-			synchronized (LoremIpsum.class) {
-				if (instance == null) {
-					instance = new LoremIpsum(new Random());
-				}
-			}
-		}
-		return instance;
-	}
+  private List<String> maleNames;
 
-	public LoremIpsum() {
-		this(new Random());
-	}
-	
-	public LoremIpsum(Long seed) {
-		this(seed == null ? new Random() : new Random(seed));
-	}
+  private List<String> femaleNames;
 
-	public LoremIpsum(Random random) {
-		this.random = random;
-		
-		words = readLines("lorem.txt");
-		maleNames = readLines("male_names.txt");
-		femaleNames = readLines("female_names.txt");
-		surnames = readLines("surnames.txt");
-		firstNames = new ArrayList<String>();
-		firstNames.addAll(maleNames);
-		firstNames.addAll(femaleNames);
+  private List<String> surnames;
 
-		cities = readLines("cities.txt");
-		stateAbbr = readLines("state_abbr.txt");
-		stateFull = readLines("state_full.txt");
-		countries = readLines("countries.txt");
-	}
+  private List<String> firstNames;
 
-	private List<String> readLines(String file) {
-		List<String> ret = new ArrayList<String>();
-		BufferedReader br = null;
-		try {
-			br = new BufferedReader(new InputStreamReader(getClass()
-					.getResourceAsStream(file), "UTF-8"));
-			String line;
-			while ((line = br.readLine()) != null) {
-				ret.add(line.trim());
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			if (br != null) {
-				try {
-					br.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-		return ret;
-	}
+  private List<String> stateAbbr;
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.thedeanda.lorem.Lorem#getCity()
-	 */
-	@Override
-	public String getCity() {
-		return getRandom(cities);
-	}
+  private List<String> stateFull;
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.thedeanda.lorem.Lorem#getCountry()
-	 */
-	@Override
-	public String getCountry() {
-		return getRandom(countries);
-	}
+  private List<String> cities;
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.thedeanda.lorem.Lorem#getEmail()
-	 */
-	@Override
-	public String getEmail() {
-		StringBuilder sb = new StringBuilder();
+  private List<String> countries;
 
-		sb.append(getFirstName().toLowerCase());
-		sb.append(".");
-		sb.append(getLastName().toLowerCase());
-		sb.append("@example.com");
+  private String[] URL_HOSTS = new String[] { "https://www.google.com/#q=%s", "http://www.bing.com/search?q=%s", "https://search.yahoo.com/search?p=%s", "https://duckduckgo.com/?q=%s" };
 
-		return sb.toString().replace(' ', '.');
-	}
+  public static LoremIpsum getInstance() {
+    if (instance == null) {
+      synchronized (LoremIpsum.class) {
+        if (instance == null) {
+          instance = new LoremIpsum(new Random());
+        }
+      }
+    }
+    return instance;
+  }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.thedeanda.lorem.Lorem#getFirstName()
-	 */
-	@Override
-	public String getFirstName() {
-		return getRandom(firstNames);
-	}
+  public LoremIpsum() {
+    this(new Random());
+  }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.thedeanda.lorem.Lorem#getFirstNameMale()
-	 */
-	@Override
-	public String getFirstNameMale() {
-		return getRandom(maleNames);
-	}
+  public LoremIpsum(Long seed) {
+    this(seed == null ? new Random() : new Random(seed));
+  }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.thedeanda.lorem.Lorem#getFirstNameFemale()
-	 */
-	@Override
-	public String getFirstNameFemale() {
-		return getRandom(femaleNames);
-	}
+  public LoremIpsum(Random random) {
+    this.random = random;
+    words = readLines("lorem.txt");
+    maleNames = readLines("male_names.txt");
+    femaleNames = readLines("female_names.txt");
+    surnames = readLines("surnames.txt");
+    firstNames = new ArrayList<String>();
+    firstNames.addAll(maleNames);
+    firstNames.addAll(femaleNames);
+    cities = readLines("cities.txt");
+    stateAbbr = readLines("state_abbr.txt");
+    stateFull = readLines("state_full.txt");
+    countries = readLines("countries.txt");
+  }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.thedeanda.lorem.Lorem#getLastName()
-	 */
-	@Override
-	public String getLastName() {
-		return getRandom(surnames);
-	}
+  private List<String> readLines(String file) {
+    List<String> ret = new ArrayList<String>();
+    BufferedReader br = null;
+    try {
+      br = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream(file), "UTF-8"));
+      String line;
+      while ((line = br.readLine()) != null) {
+        ret.add(line.trim());
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    } finally {
+      if (br != null) {
+        try {
+          br.close();
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+    }
+    return ret;
+  }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.thedeanda.lorem.Lorem#getName()
-	 */
-	@Override
-	public String getName() {
-		return getFirstName() + " " + getLastName();
-	}
+  @Override public String getCity() {
+    return getRandom(cities);
+  }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.thedeanda.lorem.Lorem#getNameMale()
-	 */
-	@Override
-	public String getNameMale() {
-		return getFirstNameMale() + " " + getLastName();
-	}
+  @Override public String getCountry() {
+    return getRandom(countries);
+  }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.thedeanda.lorem.Lorem#getNameFemale()
-	 */
-	@Override
-	public String getNameFemale() {
-		return getFirstNameFemale() + " " + getLastName();
-	}
+  @Override public String getEmail() {
+    StringBuilder sb = new StringBuilder();
+    sb.append(getFirstName().toLowerCase());
+    sb.append(".");
+    sb.append(getLastName().toLowerCase());
+    sb.append("@example.com");
+    return sb.toString().replace(' ', '.');
+  }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.thedeanda.lorem.Lorem#getTitle(int)
-	 */
-	@Override
-	public String getTitle(int count) {
-		return getWords(count, count, true);
-	}
+  @Override public String getFirstName() {
+    return getRandom(firstNames);
+  }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.thedeanda.lorem.Lorem#getTitle(int, int)
-	 */
-	@Override
-	public String getTitle(int min, int max) {
-		return getWords(min, max, true);
-	}
+  @Override public String getFirstNameMale() {
+    return getRandom(maleNames);
+  }
 
-	private int getCount(int min, int max) {
-		if (min < 0)
-			min = 0;
-		if (max < min)
-			max = min;
-		int count = max != min ? random.nextInt(max - min) + min : min;
-		return count;
-	}
+  @Override public String getFirstNameFemale() {
+    return getRandom(femaleNames);
+  }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.thedeanda.lorem.Lorem#getHtmlParagraphs(int, int)
-	 */
-	@Override
-	public String getHtmlParagraphs(int min, int max) {
-		int count = getCount(min, max);
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < count; i++) {
-			sb.append("<p>");
-			sb.append(getParagraphs(1, 1));
-			sb.append("</p>");
-		}
-		return sb.toString().trim();
-	}
+  @Override public String getLastName() {
+    return getRandom(surnames);
+  }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.thedeanda.lorem.Lorem#getParagraphs(int, int)
-	 */
-	@Override
-	public String getParagraphs(int min, int max) {
-		int count = getCount(min, max);
-		StringBuilder sb = new StringBuilder();
-		for (int j = 0; j < count; j++) {
-			int sentences = random.nextInt(5) + 2; // 2 to 6
-			for (int i = 0; i < sentences; i++) {
-				String first = getWords(1, 1, false);
-				first = first.substring(0, 1).toUpperCase()
-						+ first.substring(1);
-				sb.append(first);
+  @Override public String getName() {
+    return getFirstName() + " " + getLastName();
+  }
 
-				sb.append(getWords(2, 20, false));
-				sb.append(". ");
-			}
-			sb.append("\n");
-		}
-		return sb.toString().trim();
-	}
+  @Override public String getNameMale() {
+    return getFirstNameMale() + " " + getLastName();
+  }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.thedeanda.lorem.Lorem#getUrl()
-	 */
-	@Override
-	public String getUrl() {
-		StringBuilder sb = new StringBuilder();
-		int hostId = random.nextInt(URL_HOSTS.length);
-		String host = String.format(URL_HOSTS[hostId], getWords(1));
-		sb.append(host);
-		return sb.toString();
-	}
+  @Override public String getNameFemale() {
+    return getFirstNameFemale() + " " + getLastName();
+  }
 
-	private String getWords(int min, int max, boolean title) {
-		int count = getCount(min, max);
-		return getWords(count, title);
-	}
+  @Override public String getTitle(int count) {
+    return getWords(count, count, true);
+  }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.thedeanda.lorem.Lorem#getWords(int)
-	 */
-	@Override
-	public String getWords(int count) {
-		return getWords(count, count, false);
-	}
+  @Override public String getTitle(int min, int max) {
+    return getWords(min, max, true);
+  }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.thedeanda.lorem.Lorem#getWords(int, int)
-	 */
-	@Override
-	public String getWords(int min, int max) {
-		return getWords(min, max, false);
-	}
+  private int getCount(int min, int max) {
+    if (min < 0) {
+      min = 0;
+    }
+    if (max < min) {
+      max = min;
+    }
+    int count = max != min ? random.nextInt(max - min) + min : min;
+    return count;
+  }
 
-	private String getWords(int count, boolean title) {
-		StringBuilder sb = new StringBuilder();
-		int size = words.size();
-		int wordCount = 0;
-		while (wordCount < count) {
-			String word = words.get(random.nextInt(size));
-			if (title) {
-				if (wordCount == 0 || word.length() > 3) {
-					word = word.substring(0, 1).toUpperCase()
-							+ word.substring(1);
-				}
-			}
-			sb.append(word);
-			sb.append(" ");
-			wordCount++;
-		}
-		return sb.toString().trim();
-	}
+  @Override public String getHtmlParagraphs(int min, int max) {
+    int count = getCount(min, max);
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < count; i++) {
+      sb.append("<p>");
+      sb.append(getParagraphs(1, 1));
+      sb.append("</p>");
+    }
+    return sb.toString().trim();
+  }
 
-	private String getRandom(List<String> list) {
-		int size = list.size();
-		return list.get(random.nextInt(size));
-	}
+  @Override public String getParagraphs(int min, int max) {
+    int count = getCount(min, max);
+    StringBuilder sb = new StringBuilder();
+    for (int j = 0; j < count; j++) {
+      int sentences = random.nextInt(5) + 2;
+      for (int i = 0; i < sentences; i++) {
+        String first = getWords(1, 1, false);
+        first = first.substring(0, 1).toUpperCase() + first.substring(1);
+        sb.append(first);
+        sb.append(getWords(2, 20, false));
+        sb.append(". ");
+      }
+      sb.append("\n");
+    }
+    return sb.toString().trim();
+  }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.thedeanda.lorem.Lorem#getPhone()
-	 */
-	@Override
-	public String getPhone() {
-		StringBuilder sb = new StringBuilder();
+  @Override public String getUrl() {
+    StringBuilder sb = new StringBuilder();
+    int hostId = random.nextInt(URL_HOSTS.length);
+    String host = String.format(URL_HOSTS[hostId], getWords(1));
+    sb.append(host);
+    return sb.toString();
+  }
 
-		sb.append("(");
-		sb.append(random.nextInt(9) + 1);
-		for (int i = 0; i < 2; i++) {
-			sb.append(random.nextInt(10));
-		}
-		sb.append(") ");
+  private String getWords(int min, int max, boolean title) {
+    int count = getCount(min, max);
+    return getWords(count, title);
+  }
 
-		sb.append(random.nextInt(9) + 1);
-		for (int i = 0; i < 2; i++) {
-			sb.append(random.nextInt(10));
-		}
-		sb.append("-");
+  @Override public String getWords(int count) {
+    return getWords(count, count, false);
+  }
 
-		for (int i = 0; i < 4; i++) {
-			sb.append(random.nextInt(10));
-		}
-		return sb.toString();
-	}
+  @Override public String getWords(int min, int max) {
+    return getWords(min, max, false);
+  }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.thedeanda.lorem.Lorem#getStateAbbr()
-	 */
-	@Override
-	public String getStateAbbr() {
-		return getRandom(stateAbbr);
-	}
+  private String getWords(int count, boolean title) {
+    StringBuilder sb = new StringBuilder();
+    int size = words.size();
+    int wordCount = 0;
+    while (wordCount < count) {
+      String word = words.get(random.nextInt(size));
+      if (title) {
+        if (wordCount == 0 || word.length() > 3) {
+          word = word.substring(0, 1).toUpperCase() + word.substring(1);
+        }
+      }
+      sb.append(word);
+      sb.append(" ");
+      wordCount++;
+    }
+    return sb.toString().trim();
+  }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.thedeanda.lorem.Lorem#getStateFull()
-	 */
-	@Override
-	public String getStateFull() {
-		return getRandom(stateFull);
-	}
+  private String getRandom(List<String> list) {
+    int size = list.size();
+    return list.get(random.nextInt(size));
+  }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.thedeanda.lorem.Lorem#getZipCode()
-	 */
-	@Override
-	public String getZipCode() {
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < 5; i++) {
-			sb.append(random.nextInt(10));
-		}
-		return sb.toString();
-	}
+  @Override public String getPhone() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("(");
+    sb.append(random.nextInt(9) + 1);
+    for (int i = 0; i < 2; i++) {
+      sb.append(random.nextInt(10));
+    }
+    sb.append(") ");
+    sb.append(random.nextInt(9) + 1);
+    for (int i = 0; i < 2; i++) {
+      sb.append(random.nextInt(10));
+    }
+    sb.append("-");
+    for (int i = 0; i < 4; i++) {
+      sb.append(random.nextInt(10));
+    }
+    return sb.toString();
+  }
 
-	@Override
-	public LocalDateTime getPriorDate(Duration maxDurationBeforeNow) {
-		LocalDateTime now = LocalDateTime.now();
-		long offset = (long) (maxDurationBeforeNow.getSeconds() * random.nextDouble());
-		LocalDateTime result = now.minusSeconds(offset);
-		return result;
-	}
+  @Override public String getStateAbbr() {
+    return getRandom(stateAbbr);
+  }
 
-	@Override
-	public LocalDateTime getFutureDate(Duration maxDurationFromNow) {
-		LocalDateTime now = LocalDateTime.now();
-		long offset = (long) (maxDurationFromNow.getSeconds() * random.nextDouble());
-		LocalDateTime result = now.plusSeconds(offset);
-		return result;
-	}
+  @Override public String getStateFull() {
+    return getRandom(stateFull);
+  }
 
+  @Override public String getZipCode() {
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < 5; i++) {
+      sb.append(random.nextInt(10));
+    }
+    return sb.toString();
+  }
+
+  @Override public LocalDateTime getPriorDate(Duration maxDurationBeforeNow) {
+    LocalDateTime now = LocalDateTime.now();
+    long offset = (long) (maxDurationBeforeNow.getSeconds() * random.nextDouble());
+    LocalDateTime result = now.minusSeconds(offset);
+    return result;
+  }
+
+  @Override public LocalDateTime getFutureDate(Duration maxDurationFromNow) {
+    LocalDateTime now = LocalDateTime.now();
+    long offset = (long) (maxDurationFromNow.getSeconds() * random.nextDouble());
+    LocalDateTime result = now.plusSeconds(offset);
+    return result;
+  }
 }
