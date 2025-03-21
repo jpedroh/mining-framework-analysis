857cf6c8c33e3c3fcbae37826fc78809fb97f3a6
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 10:57:54 +0000

    Mining Framework Analysis

--- src/main/java/com/nitnelave/CreeperHeal/block/CreeperBrick.java
+++ src/main/java/com/nitnelave/CreeperHeal/block/CreeperBrick.java
@@ -1,20 +1,7 @@
-package com.nitnelave.CreeperHeal.block;
-
-import org.bukkit.Material;
-import org.bukkit.block.BlockState;
-
-import com.nitnelave.CreeperHeal.config.CfgVal;
-import com.nitnelave.CreeperHeal.config.CreeperConfig;
-
-class CreeperBrick extends CreeperBlock
-{
-
-    CreeperBrick(BlockState blockState)
-    {
-        super(blockState);
-
-        if (CreeperConfig.getBool(CfgVal.CRACK_DESTROYED_BRICKS))
-            blockState.setType(Material.CRACKED_STONE_BRICKS);
-    }
-
-}
+  package    com . nitnelave . CreeperHeal . block ;   import   org . bukkit . Material ;  import    org . bukkit . block . BlockState ;  import     com . nitnelave . CreeperHeal . config . CfgVal ;  import     com . nitnelave . CreeperHeal . config . CreeperConfig ;  class CreeperBrick  extends CreeperBlock  {  CreeperBrick  (  BlockState blockState )  {  super  ( blockState ) ;  if  ( 
+<<<<<<<
+ CreeperConfig . getBool  (  CfgVal . CRACK_DESTROYED_BRICKS )
+=======
+  CreeperConfig . getBool  (  CfgVal . CRACK_DESTROYED_BRICKS ) &&   blockState . getRawData  ( ) ==  (  byte ) 0
+>>>>>>>
+ )   blockState . setType  (  Material . CRACKED_STONE_BRICKS ) ; } }
