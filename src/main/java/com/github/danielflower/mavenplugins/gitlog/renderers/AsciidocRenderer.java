ebbf92b2f3d1ee183cb05ca67b52a788c9a1c1c6
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 15:24:37 +0000

    Mining Framework Analysis

--- src/main/java/com/github/danielflower/mavenplugins/gitlog/renderers/AsciidocRenderer.java
+++ src/main/java/com/github/danielflower/mavenplugins/gitlog/renderers/AsciidocRenderer.java
@@ -1,12 +1,9 @@
 package com.github.danielflower.mavenplugins.gitlog.renderers;
-
 import org.apache.maven.plugin.logging.Log;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.revwalk.RevTag;
-
 import java.io.File;
 import java.io.IOException;
-
 import static com.github.danielflower.mavenplugins.gitlog.renderers.Formatter.NEW_LINE;
 
 /**
@@ -47,99 +44,97 @@ import static com.github.danielflower.mavenplugins.gitlog.renderers.Formatter.NE
  * |===
  */
 public class AsciidocRenderer extends FileRenderer {
+  private boolean previousWasTag = false;
 
-	private boolean previousWasTag = false;
-	private final boolean fullGitMessage;
-	protected final MessageConverter messageConverter;
-	private AsciidocLinkConverter asciidocLinkConverter;
-	private String asciidocHeading; // `=`
-	private boolean isAsciidocTableView;
-	private String asciidocTableViewHeader1; // Date
-	private String asciidocTableViewHeader2; // Commit
+  private final boolean fullGitMessage;
 
-	public AsciidocRenderer(Log log, File targetFolder, String filename, boolean fullGitMessage, MessageConverter messageConverter,
-							String asciidocHeading, boolean isAsciidocTableView, String asciidocTableViewHeader1, String asciidocTableViewHeader2) throws IOException {
-		super(log, targetFolder, filename, false);
-		this.fullGitMessage = fullGitMessage;
-		this.messageConverter = messageConverter;
-		this.asciidocHeading = ((asciidocHeading == null) ? "=" : asciidocHeading);
-		this.isAsciidocTableView = isAsciidocTableView;
-		this.asciidocTableViewHeader1 = ((asciidocTableViewHeader1 == null) ? "Date" : asciidocTableViewHeader1);
-		this.asciidocTableViewHeader2 = ((asciidocTableViewHeader2 == null) ? "Commit" : asciidocTableViewHeader2);
-		asciidocLinkConverter = new AsciidocLinkConverter(log);
-	}
+  protected final MessageConverter messageConverter;
 
+  private AsciidocLinkConverter asciidocLinkConverter;
 
-	public void renderHeader(String reportTitle) throws IOException {
+  private String asciidocHeading;
 
-		if (reportTitle != null && reportTitle.length() > 0) {
-			writer.write(this.asciidocHeading + " "); // MD Heading 1
-			writer.write(reportTitle);
-			writer.write(NEW_LINE);
-			writer.write(NEW_LINE);
-			if (isAsciidocTableView) {
-				writer.write("|===");
-				writer.write(NEW_LINE);
-				writer.write("|" + asciidocTableViewHeader1 + " | " + asciidocTableViewHeader2);
-				writer.write(NEW_LINE);
-			}
+  private boolean isAsciidocTableView;
 
-		}
-	}
+  private String asciidocTableViewHeader1;
 
-	public void renderTag(RevTag tag) throws IOException {
-		if (isAsciidocTableView) {
-			if (!previousWasTag) {
-				renderFooter();
-				renderHeader("*" + tag.getTagName() + "*" + " +");
-				previousWasTag = true;
-			}
+  private String asciidocTableViewHeader2;
 
-		} else {
-			if (!previousWasTag) {
-				writer.write(NEW_LINE);
-			}
-			writer.write("*"); // MD start bold
-			writer.write(tag.getTagName());
-			writer.write("*"); // MD end bold
-			writer.write(" +"); // MD line warp
-			writer.write(NEW_LINE);
-			previousWasTag = true;
-		}
-	}
+  public AsciidocRenderer(Log log, File targetFolder, String filename, boolean fullGitMessage, MessageConverter messageConverter, String asciidocHeading, boolean isAsciidocTableView, String asciidocTableViewHeader1, String asciidocTableViewHeader2) throws IOException {
+    super(log, targetFolder, filename, false);
+    this.fullGitMessage = fullGitMessage;
+    this.messageConverter = messageConverter;
+    this.asciidocHeading = ((asciidocHeading == null) ? "=" : asciidocHeading);
+    this.isAsciidocTableView = isAsciidocTableView;
+    this.asciidocTableViewHeader1 = ((asciidocTableViewHeader1 == null) ? "Date" : asciidocTableViewHeader1);
+    this.asciidocTableViewHeader2 = ((asciidocTableViewHeader2 == null) ? "Commit" : asciidocTableViewHeader2);
+    asciidocLinkConverter = new AsciidocLinkConverter(log);
+  }
 
-	public void renderCommit(RevCommit commit) throws IOException {
-		String message = null;
-		// use the message formatter to get a HTML hyperlink
-		if (fullGitMessage) {
-			message = messageConverter.formatCommitMessage(commit.getFullMessage());
-		} else {
-			message = messageConverter.formatCommitMessage(commit.getShortMessage());
-		}
-		// now convert the HTML hyperlink into an Asciidoc link
-		message = asciidocLinkConverter.formatCommitMessage(message);
-		writer.write(NEW_LINE);
-		if (isAsciidocTableView) {
-			writer.write("|");
-			writer.write(Formatter.formatDateTime(commit.getCommitTime()) + " |" + message);
-			writer.write(" " + Formatter.formatCommiter(commit.getCommitterIdent()));
-			writer.write(" +"); // MD line warp
-			writer.write(NEW_LINE);
-		} else {
-			writer.write(Formatter.formatDateTime(commit.getCommitTime()) + "     " + message);
-			writer.write(" (" + commit.getCommitterIdent().getName() + ")");
-			writer.write(" +"); // MD line warp
-			writer.write(NEW_LINE);
-		}
-		previousWasTag = false;
-	}
+  public void renderHeader(String reportTitle) throws IOException {
+    if (reportTitle != null && reportTitle.length() > 0) {
+      writer.write(this.asciidocHeading + " ");
+      writer.write(reportTitle);
+      writer.write(NEW_LINE);
+      writer.write(NEW_LINE);
+      if (isAsciidocTableView) {
+        writer.write("|===");
+        writer.write(NEW_LINE);
+        writer.write("|" + asciidocTableViewHeader1 + " | " + asciidocTableViewHeader2);
+        writer.write(NEW_LINE);
+      }
+    }
+  }
 
+  public void renderTag(RevTag tag) throws IOException {
+    if (isAsciidocTableView) {
+      if (!previousWasTag) {
+        renderFooter();
+        renderHeader("*" + tag.getTagName() + "*" + " +");
+        previousWasTag = true;
+      }
+    } else {
+      if (!previousWasTag) {
+        writer.write(NEW_LINE);
+      }
+      writer.write("*");
+      writer.write(tag.getTagName());
+      writer.write("*");
+      writer.write(" +");
+      writer.write(NEW_LINE);
+      previousWasTag = true;
+    }
+  }
 
-	public void renderFooter() throws IOException {
-		if (isAsciidocTableView) {
-			writer.write(NEW_LINE);
-			writer.write("|===");
-			writer.write(NEW_LINE);
-		}
-	}
+  public void renderCommit(RevCommit commit) throws IOException {
+    String message = null;
+    if (fullGitMessage) {
+      message = messageConverter.formatCommitMessage(commit.getFullMessage());
+    } else {
+      message = messageConverter.formatCommitMessage(commit.getShortMessage());
+    }
+    message = asciidocLinkConverter.formatCommitMessage(message);
+    writer.write(NEW_LINE);
+    if (isAsciidocTableView) {
+      writer.write("|");
+      writer.write(Formatter.formatDateTime(commit.getCommitTime()) + " |" + message);
+      writer.write(" " + Formatter.formatCommiter(commit.getCommitterIdent()));
+      writer.write(" +");
+      writer.write(NEW_LINE);
+    } else {
+      writer.write(Formatter.formatDateTime(commit.getCommitTime()) + "     " + message);
+      writer.write(" (" + commit.getCommitterIdent().getName() + ")");
+      writer.write(" +");
+      writer.write(NEW_LINE);
+    }
+    previousWasTag = false;
+  }
+
+  public void renderFooter() throws IOException {
+    if (isAsciidocTableView) {
+      writer.write(NEW_LINE);
+      writer.write("|===");
+      writer.write(NEW_LINE);
+    }
+  }
 }
--- src/test/java/com/github/danielflower/mavenplugins/gitlog/GeneratorTest.java
+++ src/test/java/com/github/danielflower/mavenplugins/gitlog/GeneratorTest.java
@@ -1,5 +1,4 @@
 package com.github.danielflower.mavenplugins.gitlog;
-
 import com.github.danielflower.mavenplugins.gitlog.filters.CommitFilter;
 import com.github.danielflower.mavenplugins.gitlog.filters.RegexpFilter;
 import com.github.danielflower.mavenplugins.gitlog.renderers.*;
@@ -7,125 +6,109 @@ import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.plugin.logging.SystemStreamLog;
 import org.junit.Ignore;
 import org.junit.Test;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-// Not unit tests as such, but a way to manually observe the output during maven test phase
 public class GeneratorTest {
-    private static final String THIS_PLUGIN_ISSUES = "https://github.com/danielflower/maven-gitlog-plugin/issues/";
-    private static final String TARGET_DIR = "target";
+  private static final String THIS_PLUGIN_ISSUES = "https://github.com/danielflower/maven-gitlog-plugin/issues/";
 
-    @Test
-    public void writeLogToStandardOutput() throws Exception {
-        Log log = new SystemStreamLog();
-        ChangeLogRenderer renderer = new MavenLoggerRenderer(log);
-        generateReport(log, renderer);
-    }
+  private static final String TARGET_DIR = "target";
 
-    @Test
-    public void writeLogToStandardOutputWithRegexpFilter() throws Exception {
-        Log log = new SystemStreamLog();
-        ChangeLogRenderer renderer = new MavenLoggerRenderer(log);
+  @Test public void writeLogToStandardOutput() throws Exception {
+    Log log = new SystemStreamLog();
+    ChangeLogRenderer renderer = new MavenLoggerRenderer(log);
+    generateReport(log, renderer);
+  }
 
-        String regexp = ".*\\b(without|Upgraded)\\b.*";
+  @Test public void writeLogToStandardOutputWithRegexpFilter() throws Exception {
+    Log log = new SystemStreamLog();
+    ChangeLogRenderer renderer = new MavenLoggerRenderer(log);
+    String regexp = ".*\\b(without|Upgraded)\\b.*";
+    List<CommitFilter> commitFilters = new ArrayList<CommitFilter>(Defaults.COMMIT_FILTERS);
+    commitFilters.add(new RegexpFilter(regexp));
+    generateReport(log, renderer, null, commitFilters);
+  }
 
-        List<CommitFilter> commitFilters = new ArrayList<CommitFilter>(Defaults.COMMIT_FILTERS);
-        commitFilters.add(new RegexpFilter(regexp));
-        generateReport(log, renderer, null, commitFilters);
-    }
+  @Test public void writePlainTextLogToFile() throws Exception {
+    Log log = new SystemStreamLog();
+    ChangeLogRenderer renderer = new PlainTextRenderer(log, new File(TARGET_DIR), "changelog.txt", false);
+    generateReport(log, renderer);
+  }
 
-    @Test
-    public void writePlainTextLogToFile() throws Exception {
-        Log log = new SystemStreamLog();
-        ChangeLogRenderer renderer = new PlainTextRenderer(log, new File(TARGET_DIR), "changelog.txt", false);
-        generateReport(log, renderer);
-    }
+  @Test public void writePlainTextFullLogToFile() throws Exception {
+    Log log = new SystemStreamLog();
+    ChangeLogRenderer renderer = new PlainTextRenderer(log, new File(TARGET_DIR), "changelogFull.txt", true);
+    generateReport(log, renderer);
+  }
 
-    @Test
-    public void writePlainTextFullLogToFile() throws Exception {
-        Log log = new SystemStreamLog();
-        ChangeLogRenderer renderer = new PlainTextRenderer(log, new File(TARGET_DIR), "changelogFull.txt", true);
-        generateReport(log, renderer);
-    }
+  @Ignore public void writePlainTextFullLogToFileCustomDir() throws Exception {
+    Log log = new SystemStreamLog();
+    ChangeLogRenderer renderer = new PlainTextRenderer(log, new File(TARGET_DIR), "changelogFullCustomDir.txt", true);
+    generateReport(log, renderer, new File("/home/myUser/myRepo"));
+  }
 
-    @Ignore
-    public void writePlainTextFullLogToFileCustomDir() throws Exception {
-        Log log = new SystemStreamLog();
-        ChangeLogRenderer renderer = new PlainTextRenderer(log, new File(TARGET_DIR), "changelogFullCustomDir.txt", true);
-        generateReport(log, renderer, new File("/home/myUser/myRepo"));
-    }
+  @Test public void writeSimpleHtmlLogToFile() throws Exception {
+    Log log = new SystemStreamLog();
+    GitHubIssueLinkConverter messageConverter = new GitHubIssueLinkConverter(log, THIS_PLUGIN_ISSUES);
+    ChangeLogRenderer renderer = new SimpleHtmlRenderer(log, new File(TARGET_DIR), "changelog.html", false, messageConverter, false);
+    generateReport(log, renderer);
+  }
 
-    @Test
-    public void writeSimpleHtmlLogToFile() throws Exception {
-        Log log = new SystemStreamLog();
-        GitHubIssueLinkConverter messageConverter = new GitHubIssueLinkConverter(log, THIS_PLUGIN_ISSUES);
-        ChangeLogRenderer renderer = new SimpleHtmlRenderer(log, new File(TARGET_DIR), "changelog.html", false, messageConverter, false);
-        generateReport(log, renderer);
-    }
+  @Test public void writeSimpleHtmlFullLogToFile() throws Exception {
+    Log log = new SystemStreamLog();
+    GitHubIssueLinkConverter messageConverter = new GitHubIssueLinkConverter(log, THIS_PLUGIN_ISSUES);
+    ChangeLogRenderer renderer = new SimpleHtmlRenderer(log, new File(TARGET_DIR), "changelogFullMessage.html", true, messageConverter, false);
+    generateReport(log, renderer);
+  }
 
-    @Test
-    public void writeSimpleHtmlFullLogToFile() throws Exception {
-        Log log = new SystemStreamLog();
-        GitHubIssueLinkConverter messageConverter = new GitHubIssueLinkConverter(log, THIS_PLUGIN_ISSUES);
-        ChangeLogRenderer renderer = new SimpleHtmlRenderer(log, new File(TARGET_DIR), "changelogFullMessage.html", true, messageConverter, false);
-        generateReport(log, renderer);
-    }
+  @Test public void writeHtmlTableOnlyLogToFile() throws Exception {
+    Log log = new SystemStreamLog();
+    GitHubIssueLinkConverter messageConverter = new GitHubIssueLinkConverter(log, THIS_PLUGIN_ISSUES);
+    ChangeLogRenderer renderer = new SimpleHtmlRenderer(log, new File(TARGET_DIR), "changelogtable.html", false, messageConverter, true);
+    generateReport(log, renderer);
+  }
 
-    @Test
-    public void writeHtmlTableOnlyLogToFile() throws Exception {
-        Log log = new SystemStreamLog();
-        GitHubIssueLinkConverter messageConverter = new GitHubIssueLinkConverter(log, THIS_PLUGIN_ISSUES);
-        ChangeLogRenderer renderer = new SimpleHtmlRenderer(log, new File(TARGET_DIR), "changelogtable.html", false, messageConverter, true);
-        generateReport(log, renderer);
-    }
+  @Test public void writeMarkdownLogToFile() throws Exception {
+    Log log = new SystemStreamLog();
+    GitHubIssueLinkConverter messageConverter = new GitHubIssueLinkConverter(log, THIS_PLUGIN_ISSUES);
+    ChangeLogRenderer renderer = new MarkdownRenderer(log, new File(TARGET_DIR), "changelog.md", false, messageConverter, false);
+    generateReport(log, renderer);
+  }
 
-    @Test
-    public void writeMarkdownLogToFile() throws Exception {
-        Log log = new SystemStreamLog();
-        GitHubIssueLinkConverter messageConverter = new GitHubIssueLinkConverter(log, THIS_PLUGIN_ISSUES);
-        ChangeLogRenderer renderer = new MarkdownRenderer(log, new File(TARGET_DIR), "changelog.md", false, messageConverter, false);
-        generateReport(log, renderer);
-    }
+  @Test public void writeAsciidocLogToFile() throws Exception {
+    Log log = new SystemStreamLog();
+    GitHubIssueLinkConverter messageConverter = new GitHubIssueLinkConverter(log, THIS_PLUGIN_ISSUES);
+    ChangeLogRenderer renderer = new AsciidocRenderer(log, new File(TARGET_DIR), "changelog.adoc", false, messageConverter, null, false, null, null);
+    generateReport(log, renderer);
+  }
 
-	@Test
-	public void writeAsciidocLogToFile() throws Exception {
-		Log log = new SystemStreamLog();
-		GitHubIssueLinkConverter messageConverter = new GitHubIssueLinkConverter(log, THIS_PLUGIN_ISSUES);
-		ChangeLogRenderer renderer = new AsciidocRenderer(log, new File(TARGET_DIR), "changelog.adoc", false, messageConverter, null, false,null, null);
-		generateReport(log, renderer);
-	}
-	@Test
-	public void writeAsciidocLogToFileJiraAsTableView() throws Exception {
-		Log log = new SystemStreamLog();
-		JiraIssueLinkConverter messageConverter = new JiraIssueLinkConverter(log, THIS_PLUGIN_ISSUES);
-		ChangeLogRenderer renderer = new AsciidocRenderer(log, new File(TARGET_DIR), "changelog-jira-asTableview.adoc", false, messageConverter, "==", true,"Date", "Commit");
-		generateReport(log, renderer);
-	}
+  @Test public void writeAsciidocLogToFileJiraAsTableView() throws Exception {
+    Log log = new SystemStreamLog();
+    JiraIssueLinkConverter messageConverter = new JiraIssueLinkConverter(log, THIS_PLUGIN_ISSUES);
+    ChangeLogRenderer renderer = new AsciidocRenderer(log, new File(TARGET_DIR), "changelog-jira-asTableview.adoc", false, messageConverter, "==", true, "Date", "Commit");
+    generateReport(log, renderer);
+  }
 
+  @Test public void writeJsonLogToFile() throws Exception {
+    Log log = new SystemStreamLog();
+    ChangeLogRenderer renderer = new JsonRenderer(log, new File(TARGET_DIR), "changelog.json", false);
+    generateReport(log, renderer);
+  }
 
-	@Test
-    public void writeJsonLogToFile() throws Exception {
-        Log log = new SystemStreamLog();
-        ChangeLogRenderer renderer = new JsonRenderer(log, new File(TARGET_DIR), "changelog.json", false);
-        generateReport(log, renderer);
-    }
+  private void generateReport(Log log, ChangeLogRenderer renderer) throws IOException, NoGitRepositoryException {
+    generateReport(log, renderer, null);
+  }
 
-    private void generateReport(Log log, ChangeLogRenderer renderer) throws IOException, NoGitRepositoryException {
-        generateReport(log, renderer, null);
-    }
+  private void generateReport(Log log, ChangeLogRenderer renderer, File gitdir) throws IOException, NoGitRepositoryException {
+    generateReport(log, renderer, null, Defaults.COMMIT_FILTERS);
+  }
 
-    private void generateReport(Log log, ChangeLogRenderer renderer, File gitdir) throws IOException, NoGitRepositoryException {
-        generateReport(log, renderer, null, Defaults.COMMIT_FILTERS);
-    }
-
-    private void generateReport(Log log, ChangeLogRenderer renderer, File gitdir, List<CommitFilter> filters) throws IOException, NoGitRepositoryException {
-        Generator generator = new Generator(Arrays.asList(renderer), filters, log);
-        generator.openRepository(gitdir);
-        generator.generate("Maven GitLog Plugin changelog");
-    }
-
+  private void generateReport(Log log, ChangeLogRenderer renderer, File gitdir, List<CommitFilter> filters) throws IOException, NoGitRepositoryException {
+    Generator generator = new Generator(Arrays.asList(renderer), filters, log);
+    generator.openRepository(gitdir);
+    generator.generate("Maven GitLog Plugin changelog");
+  }
 }
