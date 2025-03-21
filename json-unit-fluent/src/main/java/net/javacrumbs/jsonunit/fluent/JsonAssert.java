1cccb6112d2d83b9a89bd5cbfdba3d412eada6bc
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 05:04:43 +0000

    Mining Framework Analysis

--- json-unit-fluent/src/main/java/net/javacrumbs/jsonunit/fluent/JsonAssert.java
+++ json-unit-fluent/src/main/java/net/javacrumbs/jsonunit/fluent/JsonAssert.java
@@ -1,23 +1,6 @@
-/**
- * Copyright 2009-2013 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package net.javacrumbs.jsonunit.fluent;
-
 import org.codehaus.jackson.JsonNode;
 import net.javacrumbs.jsonunit.core.internal.Options;
-
 
 /**
  * Contains JSON related fluent assertions inspired by FEST or AssertJ. Typical usage is:
@@ -33,13 +16,12 @@ import net.javacrumbs.jsonunit.core.internal.Options;
  *
  * @deprecated use JsonFluentAssert instead
  */
-@Deprecated
-public class JsonAssert extends JsonFluentAssert {
-    protected JsonAssert(JsonNode actual, String path, String description, String ignorePlaceholder) {
-        super(actual, path, description, ignorePlaceholder, null, Options.empty());
-    }
+@Deprecated public class JsonAssert extends JsonFluentAssert {
+  protected JsonAssert(JsonNode actual, String path, String description, String ignorePlaceholder) {
+    super(actual, path, description, ignorePlaceholder, null, Options.empty());
+  }
 
-    public JsonAssert(JsonNode actual) {
-        super(actual);
-    }
+  public JsonAssert(JsonNode actual) {
+    super(actual);
+  }
 }
