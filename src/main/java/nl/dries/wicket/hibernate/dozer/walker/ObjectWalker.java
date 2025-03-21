03260d6453b9732b3f30bfc3ce1bc8470a9b114b
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 07:30:30 +0000

    Mining Framework Analysis

--- src/main/java/nl/dries/wicket/hibernate/dozer/walker/ObjectWalker.java
+++ src/main/java/nl/dries/wicket/hibernate/dozer/walker/ObjectWalker.java
@@ -1,5 +1,4 @@
 package nl.dries.wicket.hibernate.dozer.walker;
-
 import java.io.Serializable;
 import java.util.Collection;
 import java.util.HashSet;
@@ -7,7 +6,6 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
-
 import nl.dries.wicket.hibernate.dozer.SessionFinder;
 import nl.dries.wicket.hibernate.dozer.helper.HibernateCollectionType;
 import nl.dries.wicket.hibernate.dozer.helper.HibernateProperty;
@@ -16,7 +14,6 @@ import nl.dries.wicket.hibernate.dozer.helper.ReflectionHelper;
 import nl.dries.wicket.hibernate.dozer.properties.AbstractPropertyDefinition;
 import nl.dries.wicket.hibernate.dozer.properties.CollectionPropertyDefinition;
 import nl.dries.wicket.hibernate.dozer.properties.SimplePropertyDefinition;
-
 import org.hibernate.Hibernate;
 import org.hibernate.collection.PersistentCollection;
 import org.hibernate.engine.SessionFactoryImplementor;
@@ -33,107 +30,88 @@ import org.hibernate.type.Type;
  * 
  * @author schulten
  */
-public class ObjectWalker<T>
-{
-	/** Root */
-	private final T root;
+public class ObjectWalker<T extends java.lang.Object> {
+  /** Root */
+  private final T root;
 
-	/** */
-	private final SessionImplementor sessionImpl;
+  /** */
+  private final SessionImplementor sessionImpl;
 
-	/** */
-	private final SessionFactoryImplementor factory;
+  /** */
+  private final SessionFactoryImplementor factory;
 
-	/*** */
-	private final ModelCallback callback;
+  /*** */
+  private final ModelCallback callback;
 
-	/** Seen objects, to prevent never ending recursion etc */
-	private final Set<Object> seen;
+  /** Seen objects, to prevent never ending recursion etc */
+  private final Set<Object> seen;
 
-	/**
+  /**
 	 * @param root
 	 * @param sessionFinder
 	 * @param callback
 	 */
-	public ObjectWalker(T root, SessionFinder sessionFinder, ModelCallback callback)
-	{
-		this.root = root;
-		this.sessionImpl = (SessionImplementor) sessionFinder.getHibernateSession();
-		this.factory = sessionImpl.getFactory();
-		this.callback = callback;
-		this.seen = new HashSet<>();
-	}
+  public ObjectWalker(T root, SessionFinder sessionFinder, ModelCallback callback) {
+    this.root = root;
+    this.sessionImpl = (SessionImplementor) sessionFinder.getHibernateSession();
+    this.factory = sessionImpl.getFactory();
+    this.callback = callback;
+    this.seen = new HashSet<>();
+  }
 
-	/**
+  /**
 	 * Walk the object tree, handeling registering un-initialized proxies
 	 * 
 	 * @return the root object
 	 */
-	public T walk()
-	{
-		walk(deproxy(root));
-		return root;
-	}
+  public T walk() {
+    walk(deproxy(root));
+    return root;
+  }
 
-	/**
+  /**
 	 * Recursive walker
 	 * 
 	 * @param object
 	 *            current object
 	 */
-	private void walk(Object object)
-	{
-		Set<Object> toWalk = new HashSet<>();
-		ClassMetadata metadata = factory.getClassMetadata(object.getClass());
+  private void walk(Object object) {
+    Set<Object> toWalk = new HashSet<>();
+    ClassMetadata metadata = factory.getClassMetadata(object.getClass());
+    if (metadata != null) {
+      Serializable identifier = metadata.getIdentifier(object, sessionImpl);
+      for (String propertyName : metadata.getPropertyNames()) {
+        Type type = metadata.getPropertyType(propertyName);
+        if (type instanceof AssociationType) {
+          Object value = ReflectionHelper.getValue(object, propertyName);
+          if (value != null) {
+            if (!Hibernate.isInitialized(value)) {
+              handleProxy(object, identifier, propertyName, value);
+            } else {
+              if (value instanceof PersistentCollection) {
+                handleInitializedCollection(object, toWalk, convertToPlainCollection(object, propertyName, value));
+              } else {
+                if (value instanceof Collection<?>) {
+                  handlePlainCollection(toWalk, (Collection<?>) value);
+                } else {
+                  value = deproxy(value);
+                  if (!seen.contains(value)) {
+                    toWalk.add(value);
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    seen.add(object);
+    for (Iterator<Object> iter = toWalk.iterator(); iter.hasNext(); ) {
+      walk(iter.next());
+    }
+  }
 
-		if (metadata != null)
-		{
-			Serializable identifier = metadata.getIdentifier(object, sessionImpl);
-
-			for (String propertyName : metadata.getPropertyNames())
-			{
-				Type type = metadata.getPropertyType(propertyName);
-				if (type instanceof AssociationType)
-				{
-					Object value = ReflectionHelper.getValue(object, propertyName);
-
-					if (value != null)
-					{
-						if (!Hibernate.isInitialized(value))
-						{
-							handleProxy(object, identifier, propertyName, value);
-						}
-						else if (value instanceof PersistentCollection)
-						{
-							handleInitializedCollection(object, toWalk,
-								convertToPlainCollection(object, propertyName, value));
-						}
-						else if (value instanceof Collection<?>)
-						{
-							handlePlainCollection(toWalk, (Collection<?>) value);
-						}
-						else
-						{
-							value = deproxy(value);
-							if (!seen.contains(value))
-							{
-								toWalk.add(value);
-							}
-						}
-					}
-				}
-			}
-		}
-
-		seen.add(object);
-
-		for (Iterator<Object> iter = toWalk.iterator(); iter.hasNext();)
-		{
-			walk(iter.next());
-		}
-	}
-
-	/**
+  /**
 	 * Add a plain collection to the visitable objects
 	 * 
 	 * @param toWalk
@@ -141,20 +119,17 @@ public class ObjectWalker<T>
 	 * @param collection
 	 *            the collection to add
 	 */
-	public void handlePlainCollection(Set<Object> toWalk, Collection<?> collection)
-	{
-		Iterator<?> iter = collection.iterator();
-		while (iter.hasNext())
-		{
-			Object next = iter.next();
-			if (!seen.contains(next))
-			{
-				toWalk.add(next);
-			}
-		}
-	}
+  public void handlePlainCollection(Set<Object> toWalk, Collection<?> collection) {
+    Iterator<?> iter = collection.iterator();
+    while (iter.hasNext()) {
+      Object next = iter.next();
+      if (!seen.contains(next)) {
+        toWalk.add(next);
+      }
+    }
+  }
 
-	/**
+  /**
 	 * Handles a initialized Hibernate collection value
 	 * 
 	 * @param object
@@ -164,30 +139,25 @@ public class ObjectWalker<T>
 	 * @param value
 	 *            the collection value
 	 */
-	public void handleInitializedCollection(Object object, Set<Object> toWalk, Object value)
-	{
-		if (value instanceof Collection<?>)
-		{
-			Collection<?> collection = (Collection<?>) value;
-			Iterator<?> iter = collection.iterator();
-			while (iter.hasNext())
-			{
-				addObject(toWalk, iter.next());
-			}
-		}
-		else if (value instanceof Map<?, ?>)
-		{
-			for (Object obj : ((Map<?, ?>) value).entrySet())
-			{
-				Entry<?, ?> entry = (Entry<?, ?>) obj;
+  public void handleInitializedCollection(Object object, Set<Object> toWalk, Object value) {
+    if (value instanceof Collection<?>) {
+      Collection<?> collection = (Collection<?>) value;
+      Iterator<?> iter = collection.iterator();
+      while (iter.hasNext()) {
+        addObject(toWalk, iter.next());
+      }
+    } else {
+      if (value instanceof Map<?, ?>) {
+        for (Object obj : ((Map<?, ?>) value).entrySet()) {
+          Entry<?, ?> entry = (Entry<?, ?>) obj;
+          addObject(toWalk, entry.getKey());
+          addObject(toWalk, entry.getValue());
+        }
+      }
+    }
+  }
 
-				addObject(toWalk, entry.getKey());
-				addObject(toWalk, entry.getValue());
-			}
-		}
-	}
-
-	/**
+  /**
 	 * Add an object to the vistiable objects (only we haven't already seen the object)
 	 * 
 	 * @param toWalk
@@ -195,16 +165,14 @@ public class ObjectWalker<T>
 	 * @param object
 	 *            the object to add to the set
 	 */
-	public void addObject(Set<Object> toWalk, Object object)
-	{
-		object = deproxy(object);
-		if (!seen.contains(object))
-		{
-			toWalk.add(object);
-		}
-	}
+  public void addObject(Set<Object> toWalk, Object object) {
+    object = deproxy(object);
+    if (!seen.contains(object)) {
+      toWalk.add(object);
+    }
+  }
 
-	/**
+  /**
 	 * Convert Hibernate collection to a plain collection type
 	 * 
 	 * @param object
@@ -215,16 +183,14 @@ public class ObjectWalker<T>
 	 *            input collection
 	 * @return plain collection type
 	 */
-	private Object convertToPlainCollection(Object object, String propertyName, Object value)
-	{
-		PersistentCollection collection = (PersistentCollection) value;
-		Object plainCollection = HibernateCollectionType.determineType(collection).createPlainCollection(
-			collection);
-		ReflectionHelper.setValue(object, propertyName, plainCollection);
-		return plainCollection;
-	}
+  private Object convertToPlainCollection(Object object, String propertyName, Object value) {
+    PersistentCollection collection = (PersistentCollection) value;
+    Object plainCollection = HibernateCollectionType.determineType(collection).createPlainCollection(collection);
+    ReflectionHelper.setValue(object, propertyName, plainCollection);
+    return plainCollection;
+  }
 
-	/**
+  /**
 	 * Creates a mapping for a Hibernate proxy
 	 * 
 	 * @param object
@@ -236,33 +202,21 @@ public class ObjectWalker<T>
 	 * @param value
 	 *            its current value
 	 */
-	@SuppressWarnings("unchecked")
-	public void handleProxy(Object object, Serializable identifier, String propertyName, Object value)
-	{
-		final AbstractPropertyDefinition def;
+  @SuppressWarnings(value = { "unchecked" }) public void handleProxy(Object object, Serializable identifier, String propertyName, Object value) {
+    final AbstractPropertyDefinition def;
+    Class<? extends Serializable> objectClass = HibernateProxyHelper.getClassWithoutInitializingProxy(object);
+    if (value instanceof PersistentCollection) {
+      def = new CollectionPropertyDefinition(objectClass, identifier, propertyName, HibernateCollectionType.determineType((PersistentCollection) value));
+    } else {
+      LazyInitializer initializer = ((HibernateProxy) value).getHibernateLazyInitializer();
+      HibernateProperty property = new HibernateProperty(initializer.getPersistentClass(), initializer.getIdentifier());
+      def = new SimplePropertyDefinition(objectClass, identifier, propertyName, property);
+    }
+    callback.addDetachedProperty(object, def);
+    ReflectionHelper.setValue(object, propertyName, null);
+  }
 
-		Class<? extends Serializable> objectClass = HibernateProxyHelper.getClassWithoutInitializingProxy(object);
-
-		// Collection
-		if (value instanceof PersistentCollection)
-		{
-			def = new CollectionPropertyDefinition(objectClass, identifier, propertyName,
-				HibernateCollectionType.determineType((PersistentCollection) value));
-		}
-		// Other
-		else
-		{
-			LazyInitializer initializer = ((HibernateProxy) value).getHibernateLazyInitializer();
-			HibernateProperty property = new HibernateProperty(initializer.getPersistentClass(),
-				initializer.getIdentifier());
-			def = new SimplePropertyDefinition(objectClass, identifier, propertyName, property);
-		}
-
-		callback.addDetachedProperty(object, def);
-		ReflectionHelper.setValue(object, propertyName, null); // Reset to null
-	}
-
-	/**
+  /**
 	 * Deproxy a Hibernate enhanced object, only call when sure the object is initialized, otherwise (unwanted)
 	 * intialization wil take place
 	 * 
@@ -270,16 +224,12 @@ public class ObjectWalker<T>
 	 *            the input object
 	 * @return deproxied object
 	 */
-	@SuppressWarnings("unchecked")
-	protected <U> U deproxy(U object)
-	{
-		if (object instanceof HibernateProxy)
-		{
-			HibernateProxy hibernateProxy = (HibernateProxy) object;
-			LazyInitializer lazyInitializer = hibernateProxy.getHibernateLazyInitializer();
-
-			return (U) lazyInitializer.getImplementation();
-		}
-		return object;
-	}
+  @SuppressWarnings(value = { "unchecked" }) protected <U extends java.lang.Object> U deproxy(U object) {
+    if (object instanceof HibernateProxy) {
+      HibernateProxy hibernateProxy = (HibernateProxy) object;
+      LazyInitializer lazyInitializer = hibernateProxy.getHibernateLazyInitializer();
+      return (U) lazyInitializer.getImplementation();
+    }
+    return object;
+  }
 }
