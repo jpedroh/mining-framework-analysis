8370074f8d7a04760ae083c7cbacc5d2f02c13c3
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 10:01:17 +0000

    Mining Framework Analysis

--- src/main/java/org/kohsuke/github/GHBranch.java
+++ src/main/java/org/kohsuke/github/GHBranch.java
@@ -1,12 +1,10 @@
 package org.kohsuke.github;
-
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Collection;
-
 import static org.kohsuke.github.Previews.*;
 
 /**
@@ -14,165 +12,113 @@ import static org.kohsuke.github.Previews.*;
  *
  * @author Yusuke Kokubo
  */
-@SuppressFBWarnings(value = { "UWF_UNWRITTEN_PUBLIC_OR_PROTECTED_FIELD", "UWF_UNWRITTEN_FIELD", "NP_UNWRITTEN_FIELD",
-        "URF_UNREAD_FIELD" }, justification = "JSON API")
-public class GHBranch {
-    private GitHub root;
-    private GHRepository owner;
+@SuppressFBWarnings(value = { "UWF_UNWRITTEN_PUBLIC_OR_PROTECTED_FIELD", "UWF_UNWRITTEN_FIELD", "NP_UNWRITTEN_FIELD", "URF_UNREAD_FIELD" }, justification = "JSON API") public class GHBranch {
+  private GitHub root;
 
-    private String name;
-    private Commit commit;
-    @JsonProperty("protected")
-    private boolean protection;
-    private String protection_url;
+  private GHRepository owner;
 
-    @JsonCreator
-    GHBranch(@JsonProperty("name") String name) throws Exception{
-        if (name != null) {
-            this.name = name;
-        }
-        else{
-            throw new GHFileNotFoundException("Branch Not Found");
-        }
+  private String name;
+
+  private Commit commit;
+
+  @JsonProperty(value = "protected") private boolean protection;
+
+  private String protection_url;
+
+  @JsonCreator GHBranch(@JsonProperty(value = "name") String name) throws Exception {
+    if (name != null) {
+      this.name = name;
+    } else {
+      throw new GHFileNotFoundException("Branch Not Found");
     }
-    /**
-     * The type Commit.
+  }
+
+  public static class Commit {
+    String sha;
+
+    @SuppressFBWarnings(value = "UUF_UNUSED_FIELD", justification = "We don\'t provide it in API now") String url;
+  }
+
+  public GitHub getRoot() {
+    return root;
+  }
+
+  /**
+     * Repository that this branch is in.
      */
-    public static class Commit {
-        String sha;
+  public GHRepository getOwner() {
+    return owner;
+  }
 
-        @SuppressFBWarnings(value = "UUF_UNUSED_FIELD", justification = "We don't provide it in API now")
-        String url;
-    }
+  public String getName() {
+    return name;
+  }
 
-    /**
-     * Gets root.
-     *
-     * @return the root
+  /**
+     * Returns true if the push to this branch is restricted via branch protection.
      */
-    public GitHub getRoot() {
-        return root;
-    }
+  @Preview @Deprecated public boolean isProtected() {
+    return protection;
+  }
 
-    /**
-     * Gets owner.
-     *
-     * @return the repository that this branch is in.
+  /**
+     * Returns API URL that deals with the protection of this branch.
      */
-    public GHRepository getOwner() {
-        return owner;
-    }
+  @Preview @Deprecated public URL getProtectionUrl() {
+    return GitHub.parseURL(protection_url);
+  }
 
-    /**
-     * Gets name.
-     *
-     * @return the name
+  public GHBranchProtection getProtection() throws IOException {
+    return root.retrieve().to(protection_url, GHBranchProtection.class).wrap(this);
+  }
+
+  /**
+     * The commit that this branch currently points to.
      */
-    public String getName() {
-        return name;
-    }
+  public String getSHA1() {
+    return commit.sha;
+  }
 
-    /**
-     * Is protected boolean.
-     *
-     * @return true if the push to this branch is restricted via branch protection.
-     */
-    @Preview
-    @Deprecated
-    public boolean isProtected() {
-        return protection;
-    }
-
-    /**
-     * Gets protection url.
-     *
-     * @return API URL that deals with the protection of this branch.
-     */
-    @Preview
-    @Deprecated
-    public URL getProtectionUrl() {
-        return GitHub.parseURL(protection_url);
-    }
-
-    /**
-     * Gets protection.
-     *
-     * @return the protection
-     * @throws IOException
-     *             the io exception
-     */
-    public GHBranchProtection getProtection() throws IOException {
-        return root.retrieve().to(protection_url, GHBranchProtection.class).wrap(this);
-    }
-
-    /**
-     * Gets sha 1.
-     *
-     * @return The SHA1 of the commit that this branch currently points to.
-     */
-    public String getSHA1() {
-        return commit.sha;
-    }
-
-    /**
+  /**
      * Disables branch protection and allows anyone with push access to push changes.
-     *
-     * @throws IOException
-     *             if disabling protection fails
      */
-    public void disableProtection() throws IOException {
-        new Requester(root).method("DELETE").to(protection_url);
-    }
+  public void disableProtection() throws IOException {
+    new Requester(root).method("DELETE").to(protection_url);
+  }
 
-    /**
+  /**
      * Enables branch protection to control what commit statuses are required to push.
      *
-     * @return GHBranchProtectionBuilder for enabling protection
-     * @see GHCommitStatus#getContext() GHCommitStatus#getContext()
+     * @see GHCommitStatus#getContext()
      */
-    @Preview
-    @Deprecated
-    public GHBranchProtectionBuilder enableProtection() {
-        return new GHBranchProtectionBuilder(this);
-    }
+  @Preview @Deprecated public GHBranchProtectionBuilder enableProtection() {
+    return new GHBranchProtectionBuilder(this);
+  }
 
-    /**
-     * Enable protection.
-     *
-     * @param level
-     *            the level
-     * @param contexts
-     *            the contexts
-     * @throws IOException
-     *             the io exception
-     */
-    // backward compatibility with previous signature
-    @Deprecated
-    public void enableProtection(EnforcementLevel level, Collection<String> contexts) throws IOException {
-        switch (level) {
-        case OFF:
-            disableProtection();
-            break;
-        case NON_ADMINS:
-        case EVERYONE:
-            enableProtection().addRequiredChecks(contexts).includeAdmins(level == EnforcementLevel.EVERYONE).enable();
-            break;
-        }
+  @Deprecated public void enableProtection(EnforcementLevel level, Collection<String> contexts) throws IOException {
+    switch (level) {
+      case OFF:
+      disableProtection();
+      break;
+      case NON_ADMINS:
+      case EVERYONE:
+      enableProtection().addRequiredChecks(contexts).includeAdmins(level == EnforcementLevel.EVERYONE).enable();
+      break;
     }
+  }
 
-    String getApiRoute() {
-        return owner.getApiTailUrl("/branches/" + name);
-    }
+  String getApiRoute() {
+    return owner.getApiTailUrl("/branches/" + name);
+  }
 
-    @Override
-    public String toString() {
-        final String url = owner != null ? owner.getUrl().toString() : "unknown";
-        return "Branch:" + name + " in " + url;
-    }
+  @Override public String toString() {
+    final String url = owner != null ? owner.getUrl().toString() : "unknown";
+    return "Branch:" + name + " in " + url;
+  }
 
-    GHBranch wrap(GHRepository repo) {
-        this.owner = repo;
-        this.root = repo.root;
-        return this;
-    }
+  GHBranch wrap(GHRepository repo) {
+    this.owner = repo;
+    this.root = repo.root;
+    return this;
+  }
 }
