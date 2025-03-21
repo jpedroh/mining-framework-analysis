6d6dacf3209a9e551e684bf391d515b94bb7ec61
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 00:43:46 +0000

    Mining Framework Analysis

--- driver-core/src/main/java/com/datastax/driver/core/policies/WhiteListPolicy.java
+++ driver-core/src/main/java/com/datastax/driver/core/policies/WhiteListPolicy.java
@@ -1,29 +1,11 @@
-/*
- *      Copyright (C) 2012-2014 DataStax Inc.
- *
- *   Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
- */
 package com.datastax.driver.core.policies;
-
 import java.net.InetSocketAddress;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
-
 import com.google.common.collect.ImmutableSet;
-
 import com.datastax.driver.core.Cluster;
 import com.datastax.driver.core.Host;
 import com.datastax.driver.core.HostDistance;
@@ -48,10 +30,11 @@ import com.datastax.driver.core.Statement;
  * in particular.
  */
 public class WhiteListPolicy implements LoadBalancingPolicy {
-    private final LoadBalancingPolicy childPolicy;
-    private final Set<InetSocketAddress> whiteList;
+  private final LoadBalancingPolicy childPolicy;
 
-    /**
+  private final Set<InetSocketAddress> whiteList;
+
+  /**
      * Create a new policy that wraps the provided child policy but only "allow" hosts
      * from the provided while list.
      *
@@ -59,12 +42,12 @@ public class WhiteListPolicy implements LoadBalancingPolicy {
      * @param whiteList the white listed hosts. Only hosts from this list may get connected
      * to (whether they will get connected to or not depends on the child policy).
      */
-    public WhiteListPolicy(LoadBalancingPolicy childPolicy, Collection<InetSocketAddress> whiteList) {
-        this.childPolicy = childPolicy;
-        this.whiteList = ImmutableSet.copyOf(whiteList);
-    }
+  public WhiteListPolicy(LoadBalancingPolicy childPolicy, Collection<InetSocketAddress> whiteList) {
+    this.childPolicy = childPolicy;
+    this.whiteList = ImmutableSet.copyOf(whiteList);
+  }
 
-    /**
+  /**
      * Initialize this load balancing policy.
      *
      * @param cluster the {@code Cluster} instance for which the policy is created.
@@ -73,34 +56,31 @@ public class WhiteListPolicy implements LoadBalancingPolicy {
      * @throws IllegalArgumentException if none of the host in {@code hosts}
      * (which will correspond to the contact points) are part of the white list.
      */
-    @Override
-    public void init(Cluster cluster, Collection<Host> hosts) {
-        List<Host> whiteHosts = new ArrayList<Host>(hosts.size());
-        for (Host host : hosts)
-            if (whiteList.contains(host.getSocketAddress()))
-                whiteHosts.add(host);
-
-        if (whiteHosts.isEmpty())
-            throw new IllegalArgumentException(String.format("Cannot use WhiteListPolicy where the white list (%s) contains none of the contacts points (%s)", whiteList, hosts));
-
-        childPolicy.init(cluster, whiteHosts);
+  @Override public void init(Cluster cluster, Collection<Host> hosts) {
+    List<Host> whiteHosts = new ArrayList<Host>(hosts.size());
+    for (Host host : hosts) {
+      if (whiteList.contains(host.getSocketAddress())) {
+        whiteHosts.add(host);
+      }
     }
+    if (whiteHosts.isEmpty()) {
+      throw new IllegalArgumentException(String.format("Cannot use WhiteListPolicy where the white list (%s) contains none of the contacts points (%s)", whiteList, hosts));
+    }
+    childPolicy.init(cluster, whiteHosts);
+  }
 
-    /**
+  /**
      * Return the HostDistance for the provided host.
      *
      * @param host the host of which to return the distance of.
      * @return {@link HostDistance#IGNORED} if {@code host} is not part of the white list, the HostDistance
      * as returned by the wrapped policy otherwise.
      */
-    @Override
-    public HostDistance distance(Host host) {
-        return whiteList.contains(host.getSocketAddress())
-             ? childPolicy.distance(host)
-             : HostDistance.IGNORED;
-    }
+  @Override public HostDistance distance(Host host) {
+    return whiteList.contains(host.getSocketAddress()) ? childPolicy.distance(host) : HostDistance.IGNORED;
+  }
 
-    /**
+  /**
      * Returns the hosts to use for a new query.
      * <p>
      * It is guaranteed that only hosts from the white list will be returned.
@@ -111,41 +91,37 @@ public class WhiteListPolicy implements LoadBalancingPolicy {
      * the corresponding session has no keyspace logged in.
      * @param statement the query for which to build a plan.
      */
-    @Override
-    public Iterator<Host> newQueryPlan(String loggedKeyspace, Statement statement) {
-        // Just delegate to the child policy, since we filter the hosts not white
-        // listed upfront, the child policy will never see a host that is not white
-        // listed and thus can't return one.
-        return childPolicy.newQueryPlan(loggedKeyspace, statement);
-    }
+  @Override public Iterator<Host> newQueryPlan(String loggedKeyspace, Statement statement) {
+    return childPolicy.newQueryPlan(loggedKeyspace, statement);
+  }
 
-    @Override
-    public void onUp(Host host) {
-        if (whiteList.contains(host.getSocketAddress()))
-            childPolicy.onUp(host);
+  @Override public void onUp(Host host) {
+    if (whiteList.contains(host.getSocketAddress())) {
+      childPolicy.onUp(host);
     }
+  }
 
-    @Override
-    public void onSuspected(Host host) {
-        if (whiteList.contains(host.getSocketAddress()))
-            childPolicy.onSuspected(host);
+  @Override public void onSuspected(Host host) {
+    if (whiteList.contains(host.getSocketAddress())) {
+      childPolicy.onSuspected(host);
     }
+  }
 
-    @Override
-    public void onDown(Host host) {
-        if (whiteList.contains(host.getSocketAddress()))
-            childPolicy.onDown(host);
+  @Override public void onDown(Host host) {
+    if (whiteList.contains(host.getSocketAddress())) {
+      childPolicy.onDown(host);
     }
+  }
 
-    @Override
-    public void onAdd(Host host) {
-        if (whiteList.contains(host.getSocketAddress()))
-            childPolicy.onAdd(host);
+  @Override public void onAdd(Host host) {
+    if (whiteList.contains(host.getSocketAddress())) {
+      childPolicy.onAdd(host);
     }
+  }
 
-    @Override
-    public void onRemove(Host host) {
-        if (whiteList.contains(host.getSocketAddress()))
-            childPolicy.onRemove(host);
+  @Override public void onRemove(Host host) {
+    if (whiteList.contains(host.getSocketAddress())) {
+      childPolicy.onRemove(host);
     }
+  }
 }
