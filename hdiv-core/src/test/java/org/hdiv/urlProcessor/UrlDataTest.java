2ac6a83cd26e90d05c17c04b68c520c6a190950a
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 06:15:53 +0000

    Mining Framework Analysis

--- hdiv-core/src/test/java/org/hdiv/urlProcessor/UrlDataTest.java
+++ hdiv-core/src/test/java/org/hdiv/urlProcessor/UrlDataTest.java
@@ -1,3 +1,4 @@
+<<<<<<< /usr/src/app/output/hdiv/hdiv/c620070983d557e4ea74df520f8ebf28d39580e5/hdiv-core/src/test/java/org/hdiv/urlProcessor/UrlDataTest.java/left.java
 /**
  * Copyright 2005-2016 hdiv.org
  *
@@ -13,6 +14,24 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+||||||| /usr/src/app/output/hdiv/hdiv/c620070983d557e4ea74df520f8ebf28d39580e5/hdiv-core/src/test/java/org/hdiv/urlProcessor/UrlDataTest.java/base.java
+=======
+/**
+ * Copyright 2005-2015 hdiv.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+>>>>>>> /usr/src/app/output/hdiv/hdiv/c620070983d557e4ea74df520f8ebf28d39580e5/hdiv-core/src/test/java/org/hdiv/urlProcessor/UrlDataTest.java/right.java
 package org.hdiv.urlProcessor;
 
 import org.hdiv.util.Method;
