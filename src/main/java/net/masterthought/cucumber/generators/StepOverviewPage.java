65cbb057e5a13f7274402b594817eb55d8137cfd
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 03:13:38 +0000

    Mining Framework Analysis

--- src/main/java/net/masterthought/cucumber/generators/StepOverviewPage.java
+++ src/main/java/net/masterthought/cucumber/generators/StepOverviewPage.java
@@ -1,10 +1,8 @@
 package net.masterthought.cucumber.generators;
-
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Map;
-
 import net.masterthought.cucumber.ReportBuilder;
 import net.masterthought.cucumber.json.support.StepObject;
 import net.masterthought.cucumber.util.Util;
@@ -16,43 +14,47 @@ import net.masterthought.cucumber.util.Util;
  *
  */
 public class StepOverviewPage extends AbstractPage {
+  public StepOverviewPage(ReportBuilder reportBuilder) {
+    super(reportBuilder, "stepOverview.vm");
+  }
 
-    public StepOverviewPage(ReportBuilder reportBuilder) {
-        super(reportBuilder, "stepOverview.vm");
+  @Override public void generatePage() throws IOException {
+    super.generatePage();
+    contextMap.put("steps", sortStepsByDate());
+    int allOccurrences = 0;
+    long allDurations = 0;
+    for (StepObject stepObject : reportInformation.getAllSteps().values()) {
+      allOccurrences += stepObject.getTotalOccurrences();
+      allDurations += stepObject.getTotalDuration();
     }
+    contextMap.put("all_occurrences", allOccurrences);
+    contextMap.put("all_durations", allDurations);
+    contextMap.put("all_formatted_durations", Util.formatDuration(allDurations));
+    super.generateReport("step-overview.html");
+  }
 
-    @Override
-    public void generatePage() throws IOException {
-        super.generatePage();
+  private StepObject[] sortStepsByDate() {
+    Map<String, StepObject> steps = reportInformation.getStepObject();
+    StepObject[] array = new StepObject[steps.
+<<<<<<< /usr/src/app/output/masterthought/cucumber-reporting/9fa019efbb3e9219643a69303c549bdb7b6515a2/src/main/java/net/masterthought/cucumber/generators/StepOverviewPage.java/left.java
+    size()
+=======
+    getAllSteps().size()
+>>>>>>> /usr/src/app/output/masterthought/cucumber-reporting/9fa019efbb3e9219643a69303c549bdb7b6515a2/src/main/java/net/masterthought/cucumber/generators/StepOverviewPage.java/right.java
+    ];
+    Arrays.sort(steps.
+<<<<<<< /usr/src/app/output/masterthought/cucumber-reporting/9fa019efbb3e9219643a69303c549bdb7b6515a2/src/main/java/net/masterthought/cucumber/generators/StepOverviewPage.java/left.java
+    values().toArray(array)
+=======
+    getAllSteps().values().toArray(array)
+>>>>>>> /usr/src/app/output/masterthought/cucumber-reporting/9fa019efbb3e9219643a69303c549bdb7b6515a2/src/main/java/net/masterthought/cucumber/generators/StepOverviewPage.java/right.java
+    , new DurationCompator());
+    return array;
+  }
 
-        contextMap.put("all_steps", sortStepsByDuration());
-
-        int allOccurrences = 0;
-        long allDurations = 0;
-        for (StepObject stepObject : reportInformation.getAllSteps().values()) {
-            allOccurrences += stepObject.getTotalOccurrences();
-            allDurations += stepObject.getTotalDuration();
-        }
-        contextMap.put("all_occurrences", allOccurrences);
-        contextMap.put("all_durations", allDurations);
-        contextMap.put("all_formatted_durations", Util.formatDuration(allDurations));
-
-        super.generateReport("step-overview.html");
+  private static class DurationCompator implements Comparator<StepObject> {
+    @Override public int compare(StepObject o1, StepObject o2) {
+      return Long.signum(o2.getTotalDuration() - o1.getTotalDuration());
     }
-
-    private StepObject[] sortStepsByDuration() {
-        Map<String, StepObject> steps = reportInformation.getAllSteps();
-        StepObject[] array = new StepObject[steps.size()];
-        Arrays.sort(steps.values().toArray(array), new DurationCompator());
-
-        return array;
-    }
-
-    private static class DurationCompator implements Comparator<StepObject> {
-
-        @Override
-        public int compare(StepObject o1, StepObject o2) {
-            return Long.signum(o2.getTotalDuration() - o1.getTotalDuration());
-        }
-    }
+  }
 }
