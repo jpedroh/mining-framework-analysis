5b85d47cab6c7a1fdfa01127b65530ccf506f35f
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 10:29:02 +0000

    Mining Framework Analysis

--- jetty-embedded-9/src/main/java/org/jboss/arquillian/container/jetty/embedded_9/JettyEmbeddedContainer.java
+++ jetty-embedded-9/src/main/java/org/jboss/arquillian/container/jetty/embedded_9/JettyEmbeddedContainer.java
@@ -1,27 +1,9 @@
-/*
- * JBoss, Home of Professional Open Source
- * Copyright 2010, Red Hat Middleware LLC, and individual contributors
- * by the @authors tag. See the copyright.txt in the distribution for a
- * full listing of individual contributors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.jboss.arquillian.container.jetty.embedded_9;
-
 import java.io.File;
 import java.util.Collections;
 import java.util.Map;
 import java.util.Set;
 import java.util.logging.Logger;
-
 import org.eclipse.jetty.annotations.AnnotationConfiguration;
 import org.eclipse.jetty.deploy.App;
 import org.eclipse.jetty.deploy.AppLifeCycle;
@@ -62,7 +44,6 @@ import org.jboss.arquillian.core.api.annotation.ApplicationScoped;
 import org.jboss.arquillian.core.api.annotation.Inject;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.descriptor.api.Descriptor;
-
 import javax.servlet.ServletContext;
 
 /**
@@ -87,234 +68,177 @@ import javax.servlet.ServletContext;
  * @author Martin Kouba
  */
 public class JettyEmbeddedContainer implements DeployableContainer<JettyEmbeddedConfiguration> {
-    private static final Logger log = Logger.getLogger(JettyEmbeddedContainer.class.getName());
+  private static final Logger log = Logger.getLogger(JettyEmbeddedContainer.class.getName());
 
-    static {
-        // Make jetty's own logging use java.util.logging
-        org.eclipse.jetty.util.log.Log.setLog(new JavaUtilLog());
-    }
+  static {
+    org.eclipse.jetty.util.log.Log.setLog(new JavaUtilLog());
+  }
 
-    private Server server;
-    private String listeningHost;
-    private int listeningPort;
-    private DeploymentManager deployer;
-    private ArquillianAppProvider appProvider;
+  private Server server;
 
-    private JettyEmbeddedConfiguration containerConfig;
+  private String listeningHost;
 
-    @Inject
-    @DeploymentScoped
-    private InstanceProducer<App> webAppContextProducer;
+  private int listeningPort;
 
-    @Inject
-    @ApplicationScoped
-    private InstanceProducer<ServletContext> servletContextInstanceProducer;
+  private DeploymentManager deployer;
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.jboss.arquillian.spi.client.container.DeployableContainer#getConfigurationClass()
-     */
-    public Class<JettyEmbeddedConfiguration> getConfigurationClass() {
-        return JettyEmbeddedConfiguration.class;
-    }
+  private ArquillianAppProvider appProvider;
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.jboss.arquillian.spi.client.container.DeployableContainer#getDefaultProtocol()
-     */
-    public ProtocolDescription getDefaultProtocol() {
-        // Jetty 9 is a Servlet 3.1 container.
-        // However, Arquillian "Protocol" actuall means "Packaging"
-        return new ProtocolDescription("Servlet 3.0");
-    }
+  private JettyEmbeddedConfiguration containerConfig;
 
-    public void setup(JettyEmbeddedConfiguration containerConfig) {
-        this.containerConfig = containerConfig;
-    }
+  @Inject @DeploymentScoped private InstanceProducer<App> webAppContextProducer;
 
-    public void start() throws LifecycleException {
-        EnvUtil.assertMinimumJettyVersion(Server.getVersion(), "9.4");
+  @Inject @ApplicationScoped private InstanceProducer<ServletContext> servletContextInstanceProducer;
 
-        try {
-            server = new Server();
+  public Class<JettyEmbeddedConfiguration> getConfigurationClass() {
+    return JettyEmbeddedConfiguration.class;
+  }
 
-            // Use default configuration classes at the server level
-            ClassList serverConf = ClassList.setServerDefault(server);
+  public ProtocolDescription getDefaultProtocol() {
+    return new ProtocolDescription("Servlet 3.0");
+  }
 
-            String configuredConfigurationClasses = containerConfig.getConfigurationClasses();
-            if (configuredConfigurationClasses != null && configuredConfigurationClasses.trim().length() > 0) {
-                // User provided classlist, use it as-is.
-                serverConf.clear();
-                Collections.addAll(serverConf, configuredConfigurationClasses.split(","));
-            } else {
-                // Arquillian assumption is that all features of Servlet 3.1 are available.
-                // This means that annotation scanning is enabled by default.
-                // That means jetty-plus is mandatory.
+  public void setup(JettyEmbeddedConfiguration containerConfig) {
+    this.containerConfig = containerConfig;
+  }
 
-                // Applying equivalent of etc/jetty-annotations.xml
-                serverConf.addBefore(JettyWebXmlConfiguration.class.getName(),
-                    AnnotationConfiguration.class.getName());
-
-                // Applying equivalent of etc/jetty-plus.xml
-                serverConf.addAfter(FragmentConfiguration.class.getName()
-                    , EnvConfiguration.class.getName(),
-                    PlusConfiguration.class.getName());
-            }
-
-            // Setup HTTP Configuration
-            HttpConfiguration httpConfig = containerConfig.getHttpConfiguration();
-            if (httpConfig == null) {
-                httpConfig = new HttpConfiguration();
-                if (this.containerConfig.isHeaderBufferSizeSet()) {
-                    httpConfig.setRequestHeaderSize(containerConfig.getHeaderBufferSize());
-                    httpConfig.setResponseHeaderSize(containerConfig.getHeaderBufferSize());
-                }
-                if(this.containerConfig.getRequestCookieCompliance()!=null) {
-                    httpConfig.setRequestCookieCompliance(CookieCompliance.valueOf(containerConfig.getRequestCookieCompliance()));
-                }
-                if(this.containerConfig.getResponseCookieCompliance()!=null) {
-                    httpConfig.setResponseCookieCompliance(CookieCompliance.valueOf(containerConfig.getResponseCookieCompliance()));
-                }
-            }
-
-            ConnectionFactory connectionFactory = new HttpConnectionFactory(httpConfig);
-            // Setup Connector
-            ServerConnector connector = new ServerConnector(server, connectionFactory);
-
-            connector.setHost(containerConfig.getBindAddress());
-            connector.setPort(containerConfig.getBindHttpPort());
-            connector.setIdleTimeout(containerConfig.getIdleTimeoutMillis());
-            server.setConnectors(new Connector[] {connector});
-
-            // Handler Tree location for all webapps
-            ContextHandlerCollection contexts = new ContextHandlerCollection();
-
-            // Deployment Management
-            deployer = new DeploymentManager();
-            deployer.setContexts(contexts);
-            appProvider = new ArquillianAppProvider(containerConfig);
-            deployer.addAppProvider(appProvider);
-            server.addBean(deployer);
-
-            // Handler Tree
-            HandlerCollection handlers = new HandlerCollection();
-            handlers.addHandler(contexts);
-            handlers.addHandler(new DefaultHandler());
-            server.setHandler(handlers);
-
-            if (containerConfig.isRealmPropertiesFileSet()) {
-                String realmName = getRealmName();
-                HashLoginService hashUserRealm =
-                    new HashLoginService(realmName, containerConfig.getRealmProperties().getAbsolutePath());
-                server.addBean(hashUserRealm);
-            }
-
-            server.setDumpAfterStart(containerConfig.isDumpServerAfterStart());
-            log.info("Starting Jetty Embedded Server " + Server.getVersion() + " [id:" + server.hashCode() + "]");
-            server.start();
-
-            listeningHost = connector.getHost();
-            if (listeningHost == null) {
-                listeningHost = containerConfig.getBindAddress();
-            }
-            listeningPort = connector.getLocalPort();
-        } catch (Exception e) {
-            throw new LifecycleException("Could not start container", e);
+  public void start() throws LifecycleException {
+    EnvUtil.assertMinimumJettyVersion(Server.getVersion(), "9.4");
+    try {
+      server = new Server();
+      ClassList serverConf = ClassList.setServerDefault(server);
+      String configuredConfigurationClasses = containerConfig.getConfigurationClasses();
+      if (configuredConfigurationClasses != null && configuredConfigurationClasses.trim().length() > 0) {
+        serverConf.clear();
+        Collections.addAll(serverConf, configuredConfigurationClasses.split(","));
+      } else {
+        serverConf.addBefore(JettyWebXmlConfiguration.class.getName(), AnnotationConfiguration.class.getName());
+        serverConf.addAfter(FragmentConfiguration.class.getName(), EnvConfiguration.class.getName(), PlusConfiguration.class.getName());
+      }
+      HttpConfiguration httpConfig = containerConfig.getHttpConfiguration();
+      if (httpConfig == null) {
+        httpConfig = new HttpConfiguration();
+        if (this.containerConfig.isHeaderBufferSizeSet()) {
+          httpConfig.setRequestHeaderSize(containerConfig.getHeaderBufferSize());
+          httpConfig.setResponseHeaderSize(containerConfig.getHeaderBufferSize());
         }
-    }
-
-    private String getRealmName() {
-        File realmProperties = containerConfig.getRealmProperties();
-        String fileName = realmProperties.getName();
-        int index;
-        if ((index = fileName.indexOf('.')) > -1) {
-            fileName = fileName.substring(0, index);
+        if (this.containerConfig.getRequestCookieCompliance() != null) {
+          httpConfig.setRequestCookieCompliance(CookieCompliance.valueOf(containerConfig.getRequestCookieCompliance()));
         }
-        return fileName;
-    }
-
-    public void stop() throws LifecycleException {
-        try {
-            log.info("Stopping Jetty Embedded Server [id:" + server.hashCode() + "]");
-            server.stop();
-        } catch (Exception e) {
-            throw new LifecycleException("Could not stop container", e);
+        if (this.containerConfig.getResponseCookieCompliance() != null) {
+          httpConfig.setResponseCookieCompliance(CookieCompliance.valueOf(containerConfig.getResponseCookieCompliance()));
         }
+      }
+      ConnectionFactory connectionFactory = new HttpConnectionFactory(httpConfig);
+      ServerConnector connector = new ServerConnector(server, connectionFactory);
+      connector.setHost(containerConfig.getBindAddress());
+      connector.setPort(containerConfig.getBindHttpPort());
+      connector.setIdleTimeout(containerConfig.getIdleTimeoutMillis());
+      server.setConnectors(new Connector[] { connector });
+      ContextHandlerCollection contexts = new ContextHandlerCollection();
+      deployer = new DeploymentManager();
+      deployer.setContexts(contexts);
+      appProvider = new ArquillianAppProvider(containerConfig);
+      deployer.addAppProvider(appProvider);
+      server.addBean(deployer);
+      HandlerCollection handlers = new HandlerCollection();
+      handlers.addHandler(contexts);
+      handlers.addHandler(new DefaultHandler());
+      server.setHandler(handlers);
+      if (containerConfig.isRealmPropertiesFileSet()) {
+        String realmName = getRealmName();
+        HashLoginService hashUserRealm = new HashLoginService(realmName, containerConfig.getRealmProperties().getAbsolutePath());
+        server.addBean(hashUserRealm);
+      }
+      server.setDumpAfterStart(containerConfig.isDumpServerAfterStart());
+      log.info("Starting Jetty Embedded Server " + Server.getVersion() + " [id:" + server.hashCode() + "]");
+      server.start();
+      listeningHost = connector.getHost();
+      if (listeningHost == null) {
+        listeningHost = containerConfig.getBindAddress();
+      }
+      listeningPort = connector.getLocalPort();
+    } catch (Exception e) {
+      throw new LifecycleException("Could not start container", e);
     }
+  }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.jboss.arquillian.spi.client.container.DeployableContainer#deploy(org.jboss.shrinkwrap.descriptor.api.Descriptor)
-     */
-    public void deploy(Descriptor descriptor) throws DeploymentException {
-        throw new UnsupportedOperationException("Not implemented");
+  private String getRealmName() {
+    File realmProperties = containerConfig.getRealmProperties();
+    String fileName = realmProperties.getName();
+    int index;
+    if ((index = fileName.indexOf('.')) > -1) {
+      fileName = fileName.substring(0, index);
     }
+    return fileName;
+  }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.jboss.arquillian.spi.client.container.DeployableContainer#undeploy(org.jboss.shrinkwrap.descriptor.api.Descriptor)
-     */
-    public void undeploy(Descriptor descriptor) throws DeploymentException {
-        throw new UnsupportedOperationException("Not implemented");
+  public void stop() throws LifecycleException {
+    try {
+      log.info("Stopping Jetty Embedded Server [id:" + server.hashCode() + "]");
+      server.stop();
+    } catch (Exception e) {
+      throw new LifecycleException("Could not stop container", e);
     }
+  }
 
-    public ProtocolMetaData deploy(final Archive<?> archive) throws DeploymentException {
-        try {
-            App app = appProvider.createApp(archive);
-            deployer.removeApp(app);
-            WebAppContext webAppContext = getWebAppContext(app);
+  public void deploy(Descriptor descriptor) throws DeploymentException {
+    throw new UnsupportedOperationException("Not implemented");
+  }
 
-            if (containerConfig.areMimeTypesSet()) {
-                MimeTypes mimeTypes = getMimeTypes();
-                webAppContext.setMimeTypes(mimeTypes);
-            }
+  public void undeploy(Descriptor descriptor) throws DeploymentException {
+    throw new UnsupportedOperationException("Not implemented");
+  }
 
-            deployer.addApp(app);
-            deployer.requestAppGoal(app, AppLifeCycle.STARTED);
-
-            webAppContextProducer.set(app);
-            servletContextInstanceProducer.set(webAppContext.getServletContext());
-
-            HTTPContext httpContext = new HTTPContext(listeningHost, listeningPort);
-            ServletHandler servletHandler = webAppContext.getServletHandler();
-            for (ServletHolder servlet : servletHandler.getServlets()) {
-                httpContext.add(new Servlet(servlet.getName(), servlet.getContextPath()));
-            }
-            return new ProtocolMetaData().addContext(httpContext);
-        } catch (Exception e) {
-            throw new DeploymentException("Could not deploy " + archive.getName(), e);
-        }
+  public ProtocolMetaData deploy(final Archive<?> archive) throws DeploymentException {
+    try {
+      App app = appProvider.createApp(archive);
+      deployer.removeApp(app);
+      WebAppContext webAppContext = getWebAppContext(app);
+      if (containerConfig.areMimeTypesSet()) {
+        MimeTypes mimeTypes = getMimeTypes();
+        webAppContext.setMimeTypes(mimeTypes);
+      }
+      deployer.addApp(app);
+      deployer.requestAppGoal(app, AppLifeCycle.STARTED);
+      webAppContextProducer.set(app);
+      servletContextInstanceProducer.set(webAppContext.getServletContext());
+      HTTPContext httpContext = new HTTPContext(listeningHost, listeningPort);
+      ServletHandler servletHandler = webAppContext.getServletHandler();
+      for (ServletHolder servlet : servletHandler.getServlets()) {
+        httpContext.add(new Servlet(servlet.getName(), servlet.getContextPath()));
+      }
+      return new ProtocolMetaData().addContext(httpContext);
+    } catch (Exception e) {
+      throw new DeploymentException("Could not deploy " + archive.getName(), e);
     }
+  }
 
-    private WebAppContext getWebAppContext(App app) throws Exception {
-        ContextHandler handler = app.getContextHandler();
-        WebAppContext webAppContext;
-        if (handler instanceof WebAppContext) {
-            webAppContext = (WebAppContext) handler;
-        } else {
-            throw new DeploymentException("Deployment of raw ContextHandler's not supported by Arquillian");
-        }
-        return webAppContext;
+  private WebAppContext getWebAppContext(App app) throws Exception {
+    ContextHandler handler = app.getContextHandler();
+    WebAppContext webAppContext;
+    if (handler instanceof WebAppContext) {
+      webAppContext = (WebAppContext) handler;
+    } else {
+      throw new DeploymentException("Deployment of raw ContextHandler\'s not supported by Arquillian");
     }
+    return webAppContext;
+  }
 
-    private MimeTypes getMimeTypes() {
-        Map<String, String> configuredMimeTypes = containerConfig.getMimeTypes();
-        Set<Map.Entry<String, String>> entries = configuredMimeTypes.entrySet();
-        MimeTypes mimeTypes = new MimeTypes();
-        for (Map.Entry<String, String> entry : entries) {
-            mimeTypes.addMimeMapping(entry.getKey(), entry.getValue());
-        }
-        return mimeTypes;
+  private MimeTypes getMimeTypes() {
+    Map<String, String> configuredMimeTypes = containerConfig.getMimeTypes();
+    Set<Map.Entry<String, String>> entries = configuredMimeTypes.entrySet();
+    MimeTypes mimeTypes = new MimeTypes();
+    for (Map.Entry<String, String> entry : entries) {
+      mimeTypes.addMimeMapping(entry.getKey(), entry.getValue());
     }
+    return mimeTypes;
+  }
 
-    public void undeploy(Archive<?> archive) throws DeploymentException {
-        App app = webAppContextProducer.get();
-        if (app != null) {
-            deployer.requestAppGoal(app, AppLifeCycle.UNDEPLOYED);
-        }
+  public void undeploy(Archive<?> archive) throws DeploymentException {
+    App app = webAppContextProducer.get();
+    if (app != null) {
+      deployer.requestAppGoal(app, AppLifeCycle.UNDEPLOYED);
     }
+  }
 }
