5e21f897936ce5efdaab3bb96fd09e63bbb817c7
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 13:12:55 +0000

    Mining Framework Analysis

--- src/main/java/org/skyscreamer/jsonassert/JSONCompareResult.java
+++ src/main/java/org/skyscreamer/jsonassert/JSONCompareResult.java
@@ -4,119 +4,116 @@ package org.skyscreamer.jsonassert;
  * Bean for holding results from JSONCompare.
  */
 public class JSONCompareResult {
-    private boolean _success;
-    private String _message;
-    private String _field;
-    private Object _expected;
-    private Object _actual;
+  private boolean _success;
 
-    /**
+  private String _message;
+
+  private String _field;
+
+  private Object _expected;
+
+  private Object _actual;
+
+  /**
      * Default constructor.
      */
-    public JSONCompareResult() {
-        this(true, null);
-    }
+  public JSONCompareResult() {
+    this(true, null);
+  }
 
-    private JSONCompareResult(boolean success, String message) {
-        _success = success;
-        _message = message == null ? "" : message;
-    }
+  private JSONCompareResult(boolean success, String message) {
+    _success = success;
+    _message = message == null ? "" : message;
+  }
 
-    /**
+  /**
      * Did the comparison pass?
      * @return True if it passed
      */
-    public boolean passed() {
-        return _success;
-    }
+  public boolean passed() {
+    return _success;
+  }
 
-    /**
+  /**
      * Did the comparison fail?
      * @return True if it failed
      */
-    public boolean failed() {
-        return !_success;
-    }
+  public boolean failed() {
+    return !_success;
+  }
 
-    /**
+  /**
      * Result message
      * @return String explaining why if the comparison failed
      */
-    public String getMessage() {
-        return _message;
-    }
+  public String getMessage() {
+    return _message;
+  }
 
-    /**
+  /**
      * Actual field value
      * 
      * @return a {@code JSONObject}, {@code JSONArray} or other {@code Object}
      *         instance, or {@code null} if the comparison did not fail on a
      *         particular field
      */
-    public Object getActual() {
-        return _actual;
-    }
-    
-    /**
+  public Object getActual() {
+    return _actual;
+  }
+
+  /**
      * Expected field value
      * 
      * @return a {@code JSONObject}, {@code JSONArray} or other {@code Object}
      *         instance, or {@code null} if the comparison did not fail on a
      *         particular field
      */
-    public Object getExpected() {
-        return _expected;
-    }
-    
-    /**
+  public Object getExpected() {
+    return _expected;
+  }
+
+  /**
      * Check if comparison failed on a particular field
      */
-    public boolean isFailureOnField() {
-        return _field != null;
-    }
+  public boolean isFailureOnField() {
+    return _field != null;
+  }
 
-    /**
+  /**
      * Dot-separated path the the field that failed comparison
      * 
      * @return a {@code String} instance, or {@code null} if the comparison did
      *         not fail on a particular field
      */
-    public String getField() {
-        return _field;
-    }
-    
-    protected void fail(String message) {
-        _success = false;
-        if (_message.length() == 0) {
-            _message = message;
-        }
-        else {
-            _message += " ; " + message;
-        }
-    }
+  public String getField() {
+    return _field;
+  }
 
-    /**
-     * Identify that the comparison failed
-     * @param field Which field failed
-     * @param expected Expected result
-     * @param actual Actual result
-     */
-    protected JSONCompareResult fail(String field, Object expected, Object actual) {
-        this._field = field;
-        this._expected = expected;
-        this._actual = actual;
-        fail(formatFailureMessage(field, expected, actual));
-        return this;
+  protected void fail(String message) {
+    _success = false;
+    if (_message.length() == 0) {
+      _message = message;
+    } else {
+      _message += " ; " + message;
     }
+  }
 
-    private String formatFailureMessage(String field, Object expected, Object actual) {
-        StringBuffer message= new StringBuffer();
-        message.append(field);
-        message.append("\nExpected: ");
-        message.append(expected + "");
-        message.append("\n     got: ");
-        message.append(actual + "");
-        message.append("\n");
-        return message.toString();
-    }
+  protected JSONCompareResult fail(String field, Object expected, Object actual) {
+    this._field = field;
+    this._expected = expected;
+    this._actual = actual;
+    fail(formatFailureMessage(field, expected, actual));
+    return this;
+  }
+
+  private String formatFailureMessage(String field, Object expected, Object actual) {
+    StringBuffer message = new StringBuffer();
+    message.append(field);
+    message.append("\nExpected: ");
+    message.append(expected + "");
+    message.append("\n     got: ");
+    message.append(actual + "");
+    message.append("\n");
+    return message.toString();
+  }
 }
