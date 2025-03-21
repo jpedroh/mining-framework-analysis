f020d8ffd4cfcdff2c745e19f2fd8dc25aaa2ffa
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 12:11:33 +0000

    Mining Framework Analysis

--- modules/dmx-webclient/src/main/java/systems/dmx/webclient/migrations/Migration2.java
+++ modules/dmx-webclient/src/main/java/systems/dmx/webclient/migrations/Migration2.java
@@ -1,9 +1,6 @@
 package systems.dmx.webclient.migrations;
-
 import systems.dmx.core.AssociationType;
 import systems.dmx.core.service.Migration;
-
-
 
 /**
  * Add view configs to Core types.
@@ -12,40 +9,26 @@ import systems.dmx.core.service.Migration;
  * Runs ALWAYS
  */
 public class Migration2 extends Migration {
+  @Override public void run() {
+    addIconToTopicType("dmx.core.meta_type", "\uf10c");
+    addIconToTopicType("dmx.core.topic_type", "\uf10c");
+    addIconToTopicType("dmx.core.assoc_type", "\uf192");
+    addIconToTopicType("dmx.core.plugin", "\uf12e");
+    addColorToAssociationType("dmx.core.association", "hsl(0, 0%, 80%)");
+    addColorToAssociationType("dmx.core.composition", "hsl(5, 50%, 53%)");
+    addColorToAssociationType("dmx.core.composition_def", "hsl(210, 50%, 53%)");
+    addColorToAssociationType("dmx.core.instantiation", "hsl(180, 50%, 53%)");
+    addColorToAssociationType("dmx.core.sequence", "hsl(60, 80%, 53%)");
+    AssociationType compDef = dmx.getAssociationType("dmx.core.composition_def");
+    compDef.getAssocDef("dmx.core.cardinality").getViewConfig().setConfigValueRef("dmx.webclient.view_config", "dmx.webclient.widget", "dmx.webclient.select");
+    compDef.getAssocDef("dmx.core.assoc_type#dmx.core.custom_assoc_type").getViewConfig().setConfigValueRef("dmx.webclient.view_config", "dmx.webclient.widget", "dmx.webclient.select").setConfigValue("dmx.webclient.view_config", "dmx.webclient.clearable", true);
+  }
 
-    // -------------------------------------------------------------------------------------------------- Public Methods
+  private void addIconToTopicType(String topicTypeUri, String icon) {
+    setTopicTypeViewConfigValue(topicTypeUri, "icon", icon);
+  }
 
-    @Override
-    public void run() {
-        addIconToTopicType("dmx.core.meta_type",  "\uf10c");    // fa-circle-o
-        addIconToTopicType("dmx.core.topic_type", "\uf10c");    // fa-circle-o
-        addIconToTopicType("dmx.core.assoc_type", "\uf192");    // fa-dot-circle-o
-        addIconToTopicType("dmx.core.plugin",     "\uf12e");    // fa-puzzle-piece
-        // colors match dm5-color-picker
-        // Note: color values are not aligned by extra spaces. Cytoscape style parsing would fail.
-        addColorToAssociationType("dmx.core.association",     "hsl(0, 0%, 80%)");
-        addColorToAssociationType("dmx.core.composition",     "hsl(5, 50%, 53%)");
-        addColorToAssociationType("dmx.core.composition_def", "hsl(210, 50%, 53%)");
-        addColorToAssociationType("dmx.core.instantiation",   "hsl(180, 50%, 53%)");
-        addColorToAssociationType("dmx.core.sequence",        "hsl(60, 80%, 53%)");
-        //
-        AssociationType compDef = dmx.getAssociationType("dmx.core.composition_def");
-        compDef.getAssocDef("dmx.core.cardinality")
-            .getViewConfig()
-                .setConfigValueRef("dmx.webclient.view_config", "dmx.webclient.widget", "dmx.webclient.select");
-        compDef.getAssocDef("dmx.core.assoc_type#dmx.core.custom_assoc_type")
-            .getViewConfig()
-                .setConfigValueRef("dmx.webclient.view_config", "dmx.webclient.widget", "dmx.webclient.select")
-                .setConfigValue("dmx.webclient.view_config", "dmx.webclient.clearable", true);
-    }
-
-    // ------------------------------------------------------------------------------------------------- Private Methods
-
-    private void addIconToTopicType(String topicTypeUri, String icon) {
-        setTopicTypeViewConfigValue(topicTypeUri, "icon", icon);
-    }
-
-    private void addColorToAssociationType(String assocTypeUri, String color) {
-        setAssocTypeViewConfigValue(assocTypeUri, "color", color);
-    }
+  private void addColorToAssociationType(String assocTypeUri, String color) {
+    setAssocTypeViewConfigValue(assocTypeUri, "color", color);
+  }
 }
