b3374af633f47ac78b82502c413966b5a22eff99
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 15:02:33 +0000

    Mining Framework Analysis

--- jcseg-server/src/main/java/org/lionsoul/jcseg/server/JcsegServer.java
+++ jcseg-server/src/main/java/org/lionsoul/jcseg/server/JcsegServer.java
@@ -1,5 +1,4 @@
 package org.lionsoul.jcseg.server;
-
 import org.eclipse.jetty.server.HttpConfiguration;
 import org.eclipse.jetty.server.HttpConnectionFactory;
 import org.eclipse.jetty.server.Server;
@@ -20,135 +19,98 @@ import org.lionsoul.jcseg.server.core.StandardHandler;
  * 
  * @author chenxin<chenxin619315@gmail.com>
 */
-public class JcsegServer 
-{
-	/**
+public class JcsegServer {
+  /**
 	 * jcseg server config 
 	*/
-	private JcsegServerConfig config;
-	
-	/**
+  private JcsegServerConfig config;
+
+  /**
 	 * jetty server instance 
 	*/
-	private Server server;
-	
-	/**
+  private Server server;
+
+  /**
 	 * global resource pool 
 	*/
-	private GlobalResourcePool resourcePool = null;
-	
-	/**
+  private GlobalResourcePool resourcePool = null;
+
+  /**
 	 * construct method
 	 * 
 	 * @param	config
 	*/
-	public JcsegServer(JcsegServerConfig config)
-	{
-		this.config = config;
-		resourcePool = new GlobalResourcePool();
-		init();
-	}
-	
-	/**
+  public JcsegServer(JcsegServerConfig config) {
+    this.config = config;
+    resourcePool = new GlobalResourcePool();
+    init();
+  }
+
+  /**
 	 * initialize the server and register the basic context handler
 	*/
-	private void init()
-	{
-		//setup thread pool
-		QueuedThreadPool threadPool = new QueuedThreadPool();
-		threadPool.setMaxThreads(config.getMaxThreadPoolSize());
-		threadPool.setIdleTimeout(config.getThreadIdleTimeout());
-		
-		server = new Server(threadPool);
-		
-		//setup the http configuration
-		HttpConfiguration http_config = new HttpConfiguration();
-		http_config.setOutputBufferSize(config.getOutputBufferSize());
-		http_config.setRequestHeaderSize(config.getRequestHeaderSize());
-		http_config.setResponseHeaderSize(config.getRequestHeaderSize());
-		http_config.setSendServerVersion(false);
-		http_config.setSendDateHeader(false);
-		
-		//setup the connector
-		ServerConnector connector = new ServerConnector(
-			server, 
-			new HttpConnectionFactory(http_config)
-		);
-		connector.setPort(config.getPort());
-		connector.setHost(config.getHost());
-		connector.setIdleTimeout(config.getHttpIdleTimeout());
-		server.addConnector(connector);
-	}
-	
-	/**
+  private void init() {
+    QueuedThreadPool threadPool = new QueuedThreadPool();
+    threadPool.setMaxThreads(config.getMaxThreadPoolSize());
+    threadPool.setIdleTimeout(config.getThreadIdleTimeout());
+    server = new Server(threadPool);
+    HttpConfiguration http_config = new HttpConfiguration();
+    http_config.setOutputBufferSize(config.getOutputBufferSize());
+    http_config.setRequestHeaderSize(config.getRequestHeaderSize());
+    http_config.setResponseHeaderSize(config.getRequestHeaderSize());
+    http_config.setSendServerVersion(false);
+    http_config.setSendDateHeader(false);
+    ServerConnector connector = new ServerConnector(server, new HttpConnectionFactory(http_config));
+    connector.setPort(config.getPort());
+    connector.setHost(config.getHost());
+    connector.setIdleTimeout(config.getHttpIdleTimeout());
+    server.addConnector(connector);
+  }
+
+  /**
 	 * register handler service 
 	*/
-	public JcsegServer registerHandler()
-	{
-		String basePath = this.getClass().getPackage().getName()+".controller";
-		AbstractRouter router = new DynamicRestRouter(basePath, MainController.class);
-		router.addMapping("/extractor/keywords", KeywordsController.class);
-		router.addMapping("/extractor/keyphrase", KeyphraseController.class);
-		router.addMapping("/extractor/sentence", SentenceController.class);
-		router.addMapping("/extractor/summary", SummaryController.class);
-		
-		
-		// test for contextRouter
-//		AbstractRouter router = new ContextRouter( MainController.class); 
-//		router.addMapping("/extractor/keywords", KeywordsController.class);
-//		router.addMapping("/extractor/keyphrase", KeyphraseController.class);
-//		router.addMapping("/extractor/sentence/a", SentenceController.class);
-//		router.addMapping("/extractor/summary/*", SummaryController.class);
-		
-		/*
-		 * the rest of path and dynamic rest checking will handler it 
-		*/
-		//router.addMapping("/tokenizer/default", TokenizerController.class);
-		
-		/*
-		 * yet, i am going to rewrite the path to handler mapping mechanism
-		 * check the Router handler for more info 
-		*/
-		server.setHandler(new StandardHandler(resourcePool, router));
-		
-		return this;
-	}
+  public JcsegServer registerHandler() {
+    String basePath = this.getClass().getPackage().getName() + ".controller";
+    AbstractRouter router = new DynamicRestRouter(basePath, MainController.class);
+    router.addMapping("/extractor/keywords", KeywordsController.class);
+    router.addMapping("/extractor/keyphrase", KeyphraseController.class);
+    router.addMapping("/extractor/sentence", SentenceController.class);
+    router.addMapping("/extractor/summary", SummaryController.class);
+    server.setHandler(new StandardHandler(resourcePool, router));
+    return this;
+  }
 
-	/**
+  /**
 	 * start the server 
 	 * 
 	 * @throws Exception 
 	*/
-	public void start() throws Exception
-	{
-		if ( server != null ) {
-			server.start();
-			server.join();
-		}
-	}
-	
-	/**
+  public void start() throws Exception {
+    if (server != null) {
+      server.start();
+      server.join();
+    }
+  }
+
+  /**
 	 * stop the server 
 	 * 
 	 * @throws Exception 
 	*/
-	public void stop() throws Exception
-	{
-		if ( server != null ) {
-			server.stop();
-		}
-	}
+  public void stop() throws Exception {
+    if (server != null) {
+      server.stop();
+    }
+  }
 
-	public static void main(String[] args) 
-	{
-		JcsegServerConfig config = new JcsegServerConfig();
-		JcsegServer server = new JcsegServer(config);
-		
-		try {
-			server.registerHandler().start();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
+  public static void main(String[] args) {
+    JcsegServerConfig config = new JcsegServerConfig();
+    JcsegServer server = new JcsegServer(config);
+    try {
+      server.registerHandler().start();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
 }
