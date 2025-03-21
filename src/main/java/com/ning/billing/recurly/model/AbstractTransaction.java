367ba01e3240dc563460b8734190d5137dd6c838
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 05:59:01 +0000

    Mining Framework Analysis

--- src/main/java/com/ning/billing/recurly/model/AbstractTransaction.java
+++ src/main/java/com/ning/billing/recurly/model/AbstractTransaction.java
@@ -1,300 +1,258 @@
-/*
- * Copyright 2010-2014 Ning, Inc.
- * Copyright 2014-2015 The Billing Project, LLC
- *
- * The Billing Project licenses this file to you under the Apache License, version 2.0
- * (the "License"); you may not use this file except in compliance with the
- * License.  You may obtain a copy of the License at:
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
- * License for the specific language governing permissions and limitations
- * under the License.
- */
-
 package com.ning.billing.recurly.model;
-
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import java.util.Map;
 
-@XmlRootElement(name = "transaction")
-public class AbstractTransaction extends RecurlyObject {
+@XmlRootElement(name = "transaction") public class AbstractTransaction extends RecurlyObject {
+  public static class VerificationResult {
+    private String code;
 
-    public static class VerificationResult {
+    private String message;
 
-        private String code;
-
-        private String message;
-
-        public VerificationResult() {
-        }
-
-        public VerificationResult(final String code, final String message) {
-            this.code = code;
-            this.message = message;
-        }
-
-        public String getCode() {
-            return code;
-        }
-
-        public void setCode(final String code) {
-            this.code = code;
-        }
-
-        public String getMessage() {
-            return message;
-        }
-
-        public void setMessage(final String message) {
-            this.message = message;
-        }
-
-        public static VerificationResult as(final Object object) {
-            if (isNull(object)) {
-                return null;
-            }
-
-            if (object instanceof Map) {
-                final Map map = (Map) object;
-                return new VerificationResult(stringOrNull(map.get("code")), stringOrNull(map.get("")));
-            }
-
-            return new VerificationResult(null, object.toString());
-        }
+    public VerificationResult() {
     }
 
-    @XmlElement(name = "action")
-    protected String action;
-
-    @XmlElement(name = "amount_in_cents")
-    protected Integer amountInCents;
-
-    @XmlElement(name = "status")
-    protected String status;
-
-    @XmlElement(name = "reference")
-    protected String reference;
-
-    @XmlElement(name = "test")
-    protected Boolean test;
-
-    @XmlElement(name = "voidable")
-    protected Boolean voidable;
-
-    @XmlElement(name = "refundable")
-    protected Boolean refundable;
-
-    @XmlElement(name = "transaction_error")
-    private TransactionError transactionError;
-
-    @XmlElement(name = "source")
-    private String source;
-
-    @XmlElement(name = "ip_address")
-    private String ipAddress;
-
-    @XmlElement(name = "cvv_result")
-    private VerificationResult cvvResult;
-
-    @XmlElement(name = "avs_result")
-    private VerificationResult avsResult;
-
-    @XmlElement(name = "avs_result_street")
-    private String avsResultStreet;
-
-    @XmlElement(name = "avs_result_postal")
-    private String avsResultPostal;
-
-    public String getAction() {
-        return action;
+    public VerificationResult(final String code, final String message) {
+      this.code = code;
+      this.message = message;
     }
 
-    public void setAction(final Object action) {
-        this.action = stringOrNull(action);
+    public String getCode() {
+      return code;
     }
 
-    public Integer getAmountInCents() {
-        return amountInCents;
+    public void setCode(final String code) {
+      this.code = code;
     }
 
-    public void setAmountInCents(final Object amountInCents) {
-        this.amountInCents = integerOrNull(amountInCents);
+    public String getMessage() {
+      return message;
     }
 
-    public String getStatus() {
-        return status;
+    public void setMessage(final String message) {
+      this.message = message;
     }
 
-    public void setStatus(final Object status) {
-        this.status = stringOrNull(status);
+    public static VerificationResult as(final Object object) {
+      if (isNull(object)) {
+        return null;
+      }
+      if (object instanceof Map) {
+        final Map map = (Map) object;
+        return new VerificationResult(stringOrNull(map.get("code")), stringOrNull(map.get("")));
+      }
+      return new VerificationResult(null, object.toString());
     }
+  }
 
-    public String getReference() {
-        return reference;
+  @XmlElement(name = "action") protected String action;
+
+  @XmlElement(name = "amount_in_cents") protected Integer amountInCents;
+
+  @XmlElement(name = "status") protected String status;
+
+  @XmlElement(name = "reference") protected String reference;
+
+  @XmlElement(name = "test") protected Boolean test;
+
+  @XmlElement(name = "voidable") protected Boolean voidable;
+
+  @XmlElement(name = "refundable") protected Boolean refundable;
+
+  @XmlElement(name = "transaction_error") private TransactionError transactionError;
+
+  @XmlElement(name = "source") private String source;
+
+  @XmlElement(name = "ip_address") private String ipAddress;
+
+  @XmlElement(name = "cvv_result") private VerificationResult cvvResult;
+
+  @XmlElement(name = "avs_result") private VerificationResult avsResult;
+
+  @XmlElement(name = "avs_result_street") private String avsResultStreet;
+
+  @XmlElement(name = "avs_result_postal") private String avsResultPostal;
+
+  public String getAction() {
+    return action;
+  }
+
+  public void setAction(final Object action) {
+    this.action = stringOrNull(action);
+  }
+
+  public Integer getAmountInCents() {
+    return amountInCents;
+  }
+
+  public void setAmountInCents(final Object amountInCents) {
+    this.amountInCents = integerOrNull(amountInCents);
+  }
+
+  public String getStatus() {
+    return status;
+  }
+
+  public void setStatus(final Object status) {
+    this.status = stringOrNull(status);
+  }
+
+  public String getReference() {
+    return reference;
+  }
+
+  public void setReference(final Object reference) {
+    this.reference = stringOrNull(reference);
+  }
+
+  public Boolean getTest() {
+    return test;
+  }
+
+  public void setTest(final Object test) {
+    this.test = booleanOrNull(test);
+  }
+
+  public Boolean getVoidable() {
+    return voidable;
+  }
+
+  public void setVoidable(final Object voidable) {
+    this.voidable = booleanOrNull(voidable);
+  }
+
+  public Boolean getRefundable() {
+    return refundable;
+  }
+
+  public void setRefundable(final Object refundable) {
+    this.refundable = booleanOrNull(refundable);
+  }
+
+  public TransactionError getTransactionError() {
+    return transactionError;
+  }
+
+  public void setTransactionError(final TransactionError transactionError) {
+    this.transactionError = transactionError;
+  }
+
+  public String getSource() {
+    return source;
+  }
+
+  public void setSource(final Object source) {
+    this.source = stringOrNull(source);
+  }
+
+  public String getIpAddress() {
+    return ipAddress;
+  }
+
+  public void setIpAddress(final Object ipAddress) {
+    this.ipAddress = stringOrNull(ipAddress);
+  }
+
+  public VerificationResult getCvvResult() {
+    return cvvResult;
+  }
+
+  public void setCvvResult(final Object cvvResult) {
+    this.cvvResult = VerificationResult.as(cvvResult);
+  }
+
+  public VerificationResult getAvsResult() {
+    return avsResult;
+  }
+
+  public void setAvsResult(final Object avsResult) {
+    this.avsResult = VerificationResult.as(avsResult);
+  }
+
+  public String getAvsResultStreet() {
+    return avsResultStreet;
+  }
+
+  public void setAvsResultStreet(final Object avsResultStreet) {
+    this.avsResultStreet = stringOrNull(avsResultStreet);
+  }
+
+  public String getAvsResultPostal() {
+    return avsResultPostal;
+  }
+
+  public void setAvsResultPostal(final Object avsResultPostal) {
+    this.avsResultPostal = stringOrNull(avsResultPostal);
+  }
+
+  @Override public boolean equals(final Object o) {
+    if (this == o) {
+      return true;
     }
-
-    public void setReference(final Object reference) {
-        this.reference = stringOrNull(reference);
+    if (o == null || getClass() != o.getClass()) {
+      return false;
     }
-
-    public Boolean getTest() {
-        return test;
+    final AbstractTransaction that = (AbstractTransaction) o;
+    if (action != null ? !action.equals(that.action) : that.action != null) {
+      return false;
     }
-
-    public void setTest(final Object test) {
-        this.test = booleanOrNull(test);
+    if (amountInCents != null ? !amountInCents.equals(that.amountInCents) : that.amountInCents != null) {
+      return false;
     }
-
-    public Boolean getVoidable() {
-        return voidable;
+    if (reference != null ? !reference.equals(that.reference) : that.reference != null) {
+      return false;
     }
-
-    public void setVoidable(final Object voidable) {
-        this.voidable = booleanOrNull(voidable);
+    if (refundable != null ? !refundable.equals(that.refundable) : that.refundable != null) {
+      return false;
     }
-
-    public Boolean getRefundable() {
-        return refundable;
+    if (status != null ? !status.equals(that.status) : that.status != null) {
+      return false;
     }
-
-    public void setRefundable(final Object refundable) {
-        this.refundable = booleanOrNull(refundable);
+    if (test != null ? !test.equals(that.test) : that.test != null) {
+      return false;
     }
-
-    public TransactionError getTransactionError() {
-        return transactionError;
+    if (transactionError != null ? !transactionError.equals(that.transactionError) : that.transactionError != null) {
+      return false;
     }
-
-    public void setTransactionError(final TransactionError transactionError) {
-        this.transactionError = transactionError;
+    if (voidable != null ? !voidable.equals(that.voidable) : that.voidable != null) {
+      return false;
     }
-
-    public String getSource() {
-        return source;
+    if (source != null ? !source.equals(that.source) : that.source != null) {
+      return false;
     }
-
-    public void setSource(final Object source) {
-        this.source = stringOrNull(source);
+    if (ipAddress != null ? !ipAddress.equals(that.ipAddress) : that.ipAddress != null) {
+      return false;
     }
-
-    public String getIpAddress() {
-        return ipAddress;
+    if (avsResult != null ? !avsResult.equals(that.avsResult) : that.avsResult != null) {
+      return false;
     }
-
-    public void setIpAddress(final Object ipAddress) {
-        this.ipAddress = stringOrNull(ipAddress);
+    if (cvvResult != null ? !cvvResult.equals(that.cvvResult) : that.cvvResult != null) {
+      return false;
     }
-
-    public VerificationResult getCvvResult() {
-        return cvvResult;
+    if (avsResultStreet != null ? !avsResultStreet.equals(that.avsResultStreet) : that.avsResultStreet != null) {
+      return false;
     }
-
-    public void setCvvResult(final Object cvvResult) {
-        this.cvvResult = VerificationResult.as(cvvResult);
+    if (avsResultPostal != null ? !avsResultPostal.equals(that.avsResultPostal) : that.avsResultPostal != null) {
+      return false;
     }
+    return true;
+  }
 
-    public VerificationResult getAvsResult() {
-        return avsResult;
-    }
-
-    public void setAvsResult(final Object avsResult) {
-        this.avsResult = VerificationResult.as(avsResult);
-    }
-
-    public String getAvsResultStreet() {
-        return avsResultStreet;
-    }
-
-    public void setAvsResultStreet(final Object avsResultStreet) {
-        this.avsResultStreet = stringOrNull(avsResultStreet);
-    }
-
-    public String getAvsResultPostal() {
-        return avsResultPostal;
-    }
-
-    public void setAvsResultPostal(final Object avsResultPostal) {
-        this.avsResultPostal = stringOrNull(avsResultPostal);
-    }
-
-    @Override
-    public boolean equals(final Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-
-        final AbstractTransaction that = (AbstractTransaction) o;
-
-        if (action != null ? !action.equals(that.action) : that.action != null) {
-            return false;
-        }
-        if (amountInCents != null ? !amountInCents.equals(that.amountInCents) : that.amountInCents != null) {
-            return false;
-        }
-        if (reference != null ? !reference.equals(that.reference) : that.reference != null) {
-            return false;
-        }
-        if (refundable != null ? !refundable.equals(that.refundable) : that.refundable != null) {
-            return false;
-        }
-        if (status != null ? !status.equals(that.status) : that.status != null) {
-            return false;
-        }
-        if (test != null ? !test.equals(that.test) : that.test != null) {
-            return false;
-        }
-        if (transactionError != null ? !transactionError.equals(that.transactionError) : that.transactionError != null) {
-            return false;
-        }
-        if (voidable != null ? !voidable.equals(that.voidable) : that.voidable != null) {
-            return false;
-        }
-        if (source != null ? !source.equals(that.source) : that.source != null) {
-            return false;
-        }
-        if (ipAddress != null ? !ipAddress.equals(that.ipAddress) : that.ipAddress != null) {
-            return false;
-        }
-        if (avsResult != null ? !avsResult.equals(that.avsResult) : that.avsResult != null) {
-            return false;
-        }
-        if (cvvResult != null ? !cvvResult.equals(that.cvvResult) : that.cvvResult != null) {
-            return false;
-        }
-        if (avsResultStreet != null ? !avsResultStreet.equals(that.avsResultStreet) : that.avsResultStreet != null) {
-            return false;
-        }
-        if (avsResultPostal != null ? !avsResultPostal.equals(that.avsResultPostal) : that.avsResultPostal != null) {
-            return false;
-        }
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = action != null ? action.hashCode() : 0;
-        result = 31 * result + (amountInCents != null ? amountInCents.hashCode() : 0);
-        result = 31 * result + (status != null ? status.hashCode() : 0);
-        result = 31 * result + (reference != null ? reference.hashCode() : 0);
-        result = 31 * result + (test != null ? test.hashCode() : 0);
-        result = 31 * result + (voidable != null ? voidable.hashCode() : 0);
-        result = 31 * result + (refundable != null ? refundable.hashCode() : 0);
-        result = 31 * result + (transactionError != null ? transactionError.hashCode() : 0);
-        result = 31 * result + (source != null ? source.hashCode() : 0);
-        result = 31 * result + (ipAddress != null ? ipAddress.hashCode() : 0);
-        result = 31 * result + (cvvResult != null ? cvvResult.hashCode() : 0);
-        result = 31 * result + (avsResult != null ? avsResult.hashCode() : 0);
-        result = 31 * result + (avsResultStreet != null ? avsResultStreet.hashCode() : 0);
-        result = 31 * result + (avsResultPostal != null ? avsResultPostal.hashCode() : 0);
-        return result;
-    }
+  @Override public int hashCode() {
+    int result = action != null ? action.hashCode() : 0;
+    result = 31 * result + (amountInCents != null ? amountInCents.hashCode() : 0);
+    result = 31 * result + (status != null ? status.hashCode() : 0);
+    result = 31 * result + (reference != null ? reference.hashCode() : 0);
+    result = 31 * result + (test != null ? test.hashCode() : 0);
+    result = 31 * result + (voidable != null ? voidable.hashCode() : 0);
+    result = 31 * result + (refundable != null ? refundable.hashCode() : 0);
+    result = 31 * result + (transactionError != null ? transactionError.hashCode() : 0);
+    result = 31 * result + (source != null ? source.hashCode() : 0);
+    result = 31 * result + (ipAddress != null ? ipAddress.hashCode() : 0);
+    result = 31 * result + (cvvResult != null ? cvvResult.hashCode() : 0);
+    result = 31 * result + (avsResult != null ? avsResult.hashCode() : 0);
+    result = 31 * result + (avsResultStreet != null ? avsResultStreet.hashCode() : 0);
+    result = 31 * result + (avsResultPostal != null ? avsResultPostal.hashCode() : 0);
+    return result;
+  }
 }
--- src/main/java/com/ning/billing/recurly/model/push/payment/PushTransaction.java
+++ src/main/java/com/ning/billing/recurly/model/push/payment/PushTransaction.java
@@ -1,145 +1,110 @@
-/*
- * Copyright 2010-2014 Ning, Inc.
- * Copyright 2014-2015 The Billing Project, LLC
- *
- * The Billing Project licenses this file to you under the Apache License, version 2.0
- * (the "License"); you may not use this file except in compliance with the
- * License.  You may obtain a copy of the License at:
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
- * License for the specific language governing permissions and limitations
- * under the License.
- */
-
 package com.ning.billing.recurly.model.push.payment;
-
 import java.util.Map;
-
 import javax.xml.bind.annotation.XmlElement;
-
 import org.joda.time.DateTime;
-
 import com.ning.billing.recurly.model.AbstractTransaction;
 
 public class PushTransaction extends AbstractTransaction {
+  @XmlElement private String id;
 
-    @XmlElement
-    private String id;
+  @XmlElement(name = "invoice_id") private String invoiceId;
 
-    @XmlElement(name = "invoice_id")
-    private String invoiceId;
+  @XmlElement(name = "invoice_number") private Integer invoiceNumber;
 
-    @XmlElement(name = "invoice_number")
-    private Integer invoiceNumber;
+  @XmlElement(name = "subscription_id") private String subscriptionId;
 
-    @XmlElement(name = "subscription_id")
-    private String subscriptionId;
+  @XmlElement private DateTime date;
 
-    @XmlElement
-    private DateTime date;
+  @XmlElement private String message;
 
-    @XmlElement
-    private String message;
+  public String getId() {
+    return id;
+  }
 
-    public String getId() {
-        return id;
+  public void setId(final Object id) {
+    this.id = stringOrNull(id);
+  }
+
+  public String getInvoiceId() {
+    return invoiceId;
+  }
+
+  public void setInvoiceId(final Object invoiceId) {
+    this.invoiceId = stringOrNull(invoiceId);
+  }
+
+  public Integer getInvoiceNumber() {
+    return invoiceNumber;
+  }
+
+  public void setInvoiceNumber(final Object invoiceNumber) {
+    this.invoiceNumber = integerOrNull(invoiceNumber);
+  }
+
+  public String getSubscriptionId() {
+    return subscriptionId;
+  }
+
+  public void setSubscriptionId(final Object subscriptionId) {
+    this.subscriptionId = stringOrNull(subscriptionId);
+  }
+
+  public DateTime getDate() {
+    return date;
+  }
+
+  public void setDate(final Object date) {
+    this.date = dateTimeOrNull(date);
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(final Object message) {
+    this.message = stringOrNull(message);
+  }
+
+  @Override public boolean equals(final Object o) {
+    if (this == o) {
+      return true;
     }
-
-    public void setId(final Object id) {
-        this.id = stringOrNull(id);
+    if (!(o instanceof PushTransaction)) {
+      return false;
     }
-
-    public String getInvoiceId() {
-        return invoiceId;
+    if (!super.equals(o)) {
+      return false;
     }
-
-    public void setInvoiceId(final Object invoiceId) {
-        this.invoiceId = stringOrNull(invoiceId);
+    final PushTransaction that = (PushTransaction) o;
+    if (date != null ? !date.equals(that.date) : that.date != null) {
+      return false;
     }
-
-    public Integer getInvoiceNumber() {
-        return invoiceNumber;
+    if (id != null ? !id.equals(that.id) : that.id != null) {
+      return false;
     }
-
-    public void setInvoiceNumber(final Object invoiceNumber) {
-        this.invoiceNumber = integerOrNull(invoiceNumber);
+    if (invoiceId != null ? !invoiceId.equals(that.invoiceId) : that.invoiceId != null) {
+      return false;
     }
-
-    public String getSubscriptionId() {
-        return subscriptionId;
+    if (invoiceNumber != null ? !invoiceNumber.equals(that.invoiceNumber) : that.invoiceNumber != null) {
+      return false;
     }
-
-    public void setSubscriptionId(final Object subscriptionId) {
-        this.subscriptionId = stringOrNull(subscriptionId);
+    if (message != null ? !message.equals(that.message) : that.message != null) {
+      return false;
     }
-
-    public DateTime getDate() {
-        return date;
+    if (subscriptionId != null ? !subscriptionId.equals(that.subscriptionId) : that.subscriptionId != null) {
+      return false;
     }
+    return true;
+  }
 
-    public void setDate(final Object date) {
-        this.date = dateTimeOrNull(date);
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(final Object message) {
-        this.message = stringOrNull(message);
-    }
-
-
-
-    @Override
-    public boolean equals(final Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof PushTransaction)) {
-            return false;
-        }
-        if (!super.equals(o)) {
-            return false;
-        }
-
-        final PushTransaction that = (PushTransaction) o;
-
-        if (date != null ? !date.equals(that.date) : that.date != null) {
-            return false;
-        }
-        if (id != null ? !id.equals(that.id) : that.id != null) {
-            return false;
-        }
-        if (invoiceId != null ? !invoiceId.equals(that.invoiceId) : that.invoiceId != null) {
-            return false;
-        }
-        if (invoiceNumber != null ? !invoiceNumber.equals(that.invoiceNumber) : that.invoiceNumber != null) {
-            return false;
-        }
-        if (message != null ? !message.equals(that.message) : that.message != null) {
-            return false;
-        }
-        if (subscriptionId != null ? !subscriptionId.equals(that.subscriptionId) : that.subscriptionId != null) {
-            return false;
-        }
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = super.hashCode();
-        result = 31 * result + (id != null ? id.hashCode() : 0);
-        result = 31 * result + (invoiceId != null ? invoiceId.hashCode() : 0);
-        result = 31 * result + (invoiceNumber != null ? invoiceNumber.hashCode() : 0);
-        result = 31 * result + (subscriptionId != null ? subscriptionId.hashCode() : 0);
-        result = 31 * result + (date != null ? date.hashCode() : 0);
-        result = 31 * result + (message != null ? message.hashCode() : 0);
-        return result;
-    }
+  @Override public int hashCode() {
+    int result = super.hashCode();
+    result = 31 * result + (id != null ? id.hashCode() : 0);
+    result = 31 * result + (invoiceId != null ? invoiceId.hashCode() : 0);
+    result = 31 * result + (invoiceNumber != null ? invoiceNumber.hashCode() : 0);
+    result = 31 * result + (subscriptionId != null ? subscriptionId.hashCode() : 0);
+    result = 31 * result + (date != null ? date.hashCode() : 0);
+    result = 31 * result + (message != null ? message.hashCode() : 0);
+    return result;
+  }
 }
