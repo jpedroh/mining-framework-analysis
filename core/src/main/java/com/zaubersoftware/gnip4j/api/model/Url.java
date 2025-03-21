fdc05d5ce16f1d66faa581c233c64e42fee42cfe
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 11:18:28 +0000

    Mining Framework Analysis

--- core/src/main/java/com/zaubersoftware/gnip4j/api/model/Url.java
+++ core/src/main/java/com/zaubersoftware/gnip4j/api/model/Url.java
@@ -1,22 +1,5 @@
-/**
- * Copyright (c) 2011-2016 Zauber S.A. <http://flowics.com/>
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.zaubersoftware.gnip4j.api.model;
-
 import java.io.Serializable;
-
 import org.codehaus.jackson.annotate.JsonProperty;
 
 /**
@@ -27,59 +10,55 @@ import org.codehaus.jackson.annotate.JsonProperty;
  * @since May 28, 2011
  */
 public class Url implements Serializable {
-    private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-    private String url;
+  private String url;
 
-    @JsonProperty(value = "expanded_url")
-    private String expandedUrl;
+  @JsonProperty(value = "expanded_url") private String expandedUrl;
 
-    @JsonProperty(value = "expanded_status")
-    private Integer expandedStatus;
+  @JsonProperty(value = "expanded_status") private Integer expandedStatus;
 
-    @JsonProperty(value = "expanded_url_title")
-    private String expandedUrlTitle;
+  @JsonProperty(value = "expanded_url_title") private String expandedUrlTitle;
 
-    @JsonProperty(value = "expanded_url_description")
-    private String expandedUrlDescription;
+  @JsonProperty(value = "expanded_url_description") private String expandedUrlDescription;
 
-    public final String getUrl() {
-        return url;
-    }
+  public final String getUrl() {
+    return url;
+  }
 
-    public final void setUrl(final String url) {
-        this.url = url;
-    }
+  public final void setUrl(final String url) {
+    this.url = url;
+  }
 
-    public final String getExpandedUrl() {
-        return expandedUrl;
-    }
+  public final String getExpandedUrl() {
+    return expandedUrl;
+  }
 
-    public final void setExpandedUrl(final String expandedUrl) {
-        this.expandedUrl = expandedUrl;
-    }
+  public final void setExpandedUrl(final String expandedUrl) {
+    this.expandedUrl = expandedUrl;
+  }
 
-    public Integer getExpandedStatus() {
-        return expandedStatus;
-    }
+  public Integer getExpandedStatus() {
+    return expandedStatus;
+  }
 
-    public void setExpandedStatus(final Integer expandedStatus) {
-        this.expandedStatus = expandedStatus;
-    }
+  public void setExpandedStatus(final Integer expandedStatus) {
+    this.expandedStatus = expandedStatus;
+  }
 
-    public String getExpandedUrlTitle() {
-        return expandedUrlTitle;
-    }
+  public String getExpandedUrlTitle() {
+    return expandedUrlTitle;
+  }
 
-    public void setExpandedUrlTitle(final String expandedUrlTitle) {
-        this.expandedUrlTitle = expandedUrlTitle;
-    }
+  public void setExpandedUrlTitle(final String expandedUrlTitle) {
+    this.expandedUrlTitle = expandedUrlTitle;
+  }
 
-    public String getExpandedUrlDescription() {
-        return expandedUrlDescription;
-    }
+  public String getExpandedUrlDescription() {
+    return expandedUrlDescription;
+  }
 
-    public void setExpandedUrlDescription(final String expandedUrlDescription) {
-        this.expandedUrlDescription = expandedUrlDescription;
-    }
+  public void setExpandedUrlDescription(final String expandedUrlDescription) {
+    this.expandedUrlDescription = expandedUrlDescription;
+  }
 }
