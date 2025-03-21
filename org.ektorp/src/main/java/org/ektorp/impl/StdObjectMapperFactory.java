eb5a96babb18d39c0ef2778e5f8f08749284c5f8
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 09:00:42 +0000

    Mining Framework Analysis

--- org.ektorp/src/main/java/org/ektorp/impl/StdObjectMapperFactory.java
+++ org.ektorp/src/main/java/org/ektorp/impl/StdObjectMapperFactory.java
@@ -51,7 +51,11 @@ public class StdObjectMapperFactory implements ObjectMapperFactory {
 	protected void applyDefaultConfiguration(ObjectMapper om) {
 		om.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, this.writeDatesAsTimestamps);
 
+		// method was removed in jackson 2.9.0
 		om.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+
+		// method introduced in jackson 2.7.0
+		// om.getSerializationConfig().withPropertyInclusion(JsonInclude.Value.empty().withValueInclusion(JsonInclude.Include.NON_NULL));
 	}
 
 }
