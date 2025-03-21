51a8a64ba25f55aaefe37b4d3ad8fd3d84842dc1
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 09:12:09 +0000

    Mining Framework Analysis

--- src/main/java/se/fishtank/css/selectors/dom/internal/PseudoContainsSpecifierChecker.java
+++ src/main/java/se/fishtank/css/selectors/dom/internal/PseudoContainsSpecifierChecker.java
@@ -1,13 +1,7 @@
-/**
- * Copyright (c) 2014, John Heintz and Christer Sandberg
- */
 package se.fishtank.css.selectors.dom.internal;
-
 import java.util.LinkedHashSet;
 import java.util.Set;
-
 import org.w3c.dom.Node;
-
 import se.fishtank.css.selectors.NodeSelectorException;
 import se.fishtank.css.selectors.specifier.PseudoContainsSpecifier;
 import se.fishtank.css.util.Assert;
@@ -22,36 +16,33 @@ import se.fishtank.css.util.Assert;
  * @author Christer Sandberg
  */
 public class PseudoContainsSpecifierChecker extends NodeTraversalChecker {
-    
-    /** The pseudo-class specifier to check against. */
-    private final PseudoContainsSpecifier specifier;
-    
-    /**
+  /** The pseudo-class specifier to check against. */
+  private final PseudoContainsSpecifier specifier;
+
+  /**
      * Create a new instance.
      * 
      * @param specifier The pseudo-class specifier to check against.
      */
-    public PseudoContainsSpecifierChecker(PseudoContainsSpecifier specifier) {
-        Assert.notNull(specifier, "specifier is null!");
-        this.specifier = specifier;
-    }
+  public PseudoContainsSpecifierChecker(PseudoContainsSpecifier specifier) {
+    Assert.notNull(specifier, "specifier is null!");
+    this.specifier = specifier;
+  }
 
-    /**
+  /**
      * {@inheritDoc}
      */
-    @Override
-    public Set<Node> check(Set<Node> nodes, Node root) throws NodeSelectorException {
-        Assert.notNull(nodes, "nodes is null!");
-        Assert.notNull(root, "root is null!");
-        LinkedHashSet<Node> result = new LinkedHashSet<Node>();
-        String value = specifier.getValue();
-        for (Node node : nodes) {
-        	String textContent = node.getTextContent();
-			if (textContent!=null && textContent.contains(value)) {
-        		result.add(node);
-        	}
-        }
-        
-        return result;
+  @Override public Set<Node> check(Set<Node> nodes, Node root) throws NodeSelectorException {
+    Assert.notNull(nodes, "nodes is null!");
+    Assert.notNull(root, "root is null!");
+    LinkedHashSet<Node> result = new LinkedHashSet<Node>();
+    String value = specifier.getValue();
+    for (Node node : nodes) {
+      String textContent = node.getTextContent();
+      if (textContent != null && textContent.contains(value)) {
+        result.add(node);
+      }
     }
+    return result;
+  }
 }
