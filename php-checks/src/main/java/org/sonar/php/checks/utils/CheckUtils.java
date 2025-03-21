6a0d4be5270605dbef7c786ed32a7972a8a05700
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 11:09:16 +0000

    Mining Framework Analysis

--- php-checks/src/main/java/org/sonar/php/checks/utils/CheckUtils.java
+++ php-checks/src/main/java/org/sonar/php/checks/utils/CheckUtils.java
@@ -1,29 +1,8 @@
-/*
- * SonarQube PHP Plugin
- * Copyright (C) 2010-2020 SonarSource SA
- * mailto:info AT sonarsource DOT com
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3 of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- */
 package org.sonar.php.checks.utils;
-
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Iterables;
-
 import java.io.BufferedReader;
 import java.io.StringReader;
 import java.util.Arrays;
@@ -36,7 +15,6 @@ import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import javax.annotation.Nullable;
-
 import org.apache.commons.lang3.StringUtils;
 import org.sonar.php.tree.TreeUtils;
 import org.sonar.php.tree.impl.PHPTree;
@@ -65,26 +43,13 @@ import org.sonar.plugins.php.api.tree.statement.InlineHTMLTree;
 import org.sonar.plugins.php.api.visitors.PhpFile;
 
 public final class CheckUtils {
-
-  private static final Kind[] FUNCTION_KINDS_ARRAY = {
-    Kind.METHOD_DECLARATION,
-    Kind.FUNCTION_DECLARATION,
-    Kind.FUNCTION_EXPRESSION,
-    Kind.ARROW_FUNCTION_EXPRESSION};
+  private static final Kind[] FUNCTION_KINDS_ARRAY = { Kind.METHOD_DECLARATION, Kind.FUNCTION_DECLARATION, Kind.FUNCTION_EXPRESSION, Kind.ARROW_FUNCTION_EXPRESSION };
 
   public static final ImmutableList<Kind> FUNCTION_KINDS = ImmutableList.copyOf(FUNCTION_KINDS_ARRAY);
 
-  public static final ImmutableMap<String, String> SUPERGLOBALS_BY_OLD_NAME = ImmutableMap.<String, String>builder()
-    .put("$HTTP_SERVER_VARS", "$_SERVER")
-    .put("$HTTP_GET_VARS", "$_GET")
-    .put("$HTTP_POST_VARS", "$_POST")
-    .put("$HTTP_POST_FILES", "$_FILES")
-    .put("$HTTP_SESSION_VARS", "$_SESSION")
-    .put("$HTTP_ENV_VARS", "$_ENV")
-    .put("$HTTP_COOKIE_VARS", "$_COOKIE").build();
+  public static final ImmutableMap<String, String> SUPERGLOBALS_BY_OLD_NAME = ImmutableMap.<String, String>builder().put("$HTTP_SERVER_VARS", "$_SERVER").put("$HTTP_GET_VARS", "$_GET").put("$HTTP_POST_VARS", "$_POST").put("$HTTP_POST_FILES", "$_FILES").put("$HTTP_SESSION_VARS", "$_SESSION").put("$HTTP_ENV_VARS", "$_ENV").put("$HTTP_COOKIE_VARS", "$_COOKIE").build();
 
-  public static final ImmutableSet<String> SUPERGLOBALS = ImmutableSet.of(
-      "$GLOBALS", "$_SERVER", "$_GET", "$_POST", "$_FILES", "$_COOKIE", "$_SESSION", "$_REQUEST", "$_ENV");
+  public static final ImmutableSet<String> SUPERGLOBALS = ImmutableSet.of("$GLOBALS", "$_SERVER", "$_GET", "$_POST", "$_FILES", "$_COOKIE", "$_SESSION", "$_REQUEST", "$_ENV");
 
   private CheckUtils() {
   }
@@ -102,8 +67,10 @@ public final class CheckUtils {
   public static String getFunctionName(FunctionTree functionDec) {
     if (functionDec.is(Kind.FUNCTION_DECLARATION)) {
       return "\"" + ((FunctionDeclarationTree) functionDec).name().text() + "\"";
-    } else if (functionDec.is(Kind.METHOD_DECLARATION)) {
-      return "\"" + ((MethodDeclarationTree) functionDec).name().text() + "\"";
+    } else {
+      if (functionDec.is(Kind.METHOD_DECLARATION)) {
+        return "\"" + ((MethodDeclarationTree) functionDec).name().text() + "\"";
+      }
     }
     return "expression";
   }
@@ -111,16 +78,14 @@ public final class CheckUtils {
   /**
    * @return Returns function, static method's or known dynamic method's name, like "f" or "A::f". Warning, use case insensitive comparison of the result.
    */
-  @Nullable
-  public static String getFunctionName(FunctionCallTree functionCall) {
+  @Nullable public static String getFunctionName(FunctionCallTree functionCall) {
     return nameOf(functionCall.callee());
   }
 
   /**
    * @return Returns function, static method's or known dynamic method's lower case name, like "f" or "a::f".
    */
-  @Nullable
-  public static String getLowerCaseFunctionName(FunctionCallTree functionCall) {
+  @Nullable public static String getLowerCaseFunctionName(FunctionCallTree functionCall) {
     String name = getFunctionName(functionCall);
     return name != null ? name.toLowerCase(Locale.ROOT) : null;
   }
@@ -128,8 +93,7 @@ public final class CheckUtils {
   /**
    * @return Returns function or method's name without receiver, like "fooBar".
    */
-  @Nullable
-  public static String functionName(FunctionCallTree functionCall) {
+  @Nullable public static String functionName(FunctionCallTree functionCall) {
     ExpressionTree callee = functionCall.callee();
     if (callee.is(Kind.CLASS_MEMBER_ACCESS) || callee.is(Kind.OBJECT_MEMBER_ACCESS)) {
       return nameOf(((MemberAccessTree) callee).member());
@@ -140,14 +104,13 @@ public final class CheckUtils {
   /**
    * @return Returns function or method's lower case name without receiver, like "foobar".
    */
-  @Nullable
-  public static String lowerCaseFunctionName(FunctionCallTree functionCall) {
+  @Nullable public static String lowerCaseFunctionName(FunctionCallTree functionCall) {
     String name = functionName(functionCall);
     return name != null ? name.toLowerCase(Locale.ROOT) : null;
   }
 
   public static Set<String> lowerCaseSet(String... names) {
-    return Arrays.stream(names).map(name -> name.toLowerCase(Locale.ROOT)).collect(Collectors.toSet());
+    return Arrays.stream(names).map((name) -> name.toLowerCase(Locale.ROOT)).collect(Collectors.toSet());
   }
 
   public static String getClassName(ClassDeclarationTree classDeclaration) {
@@ -161,25 +124,30 @@ public final class CheckUtils {
   /**
    * @return Returns the name of a tree.
    */
-  @Nullable
-  public static String nameOf(Tree tree) {
+  @Nullable public static String nameOf(Tree tree) {
     if (tree.is(Tree.Kind.NAMESPACE_NAME)) {
       return ((NamespaceNameTree) tree).qualifiedName();
-    } else if (tree.is(Tree.Kind.NAME_IDENTIFIER)) {
-      return ((NameIdentifierTree) tree).text();
-    } else if (tree.is(Tree.Kind.CLASS_MEMBER_ACCESS) || tree.is(Tree.Kind.OBJECT_MEMBER_ACCESS)) {
-      MemberAccessTree memberAccess = (MemberAccessTree) tree;
-      String className = nameOf(memberAccess.object());
-      String memberName = nameOf(memberAccess.member());
-      if (className != null && memberName != null) {
-        return className + "::" + memberName;
-      }
-    } else if (tree.is(Tree.Kind.VARIABLE_IDENTIFIER)) {
-      VariableIdentifierTree variableIdentifier = (VariableIdentifierTree) tree;
-      if (variableIdentifier.text().equals("$this")) {
-        ClassDeclarationTree classDeclaration = (ClassDeclarationTree) TreeUtils.findAncestorWithKind(tree, EnumSet.of(Kind.CLASS_DECLARATION, Kind.TRAIT_DECLARATION));
-        if (classDeclaration != null) {
-          return nameOf(classDeclaration.name());
+    } else {
+      if (tree.is(Tree.Kind.NAME_IDENTIFIER)) {
+        return ((NameIdentifierTree) tree).text();
+      } else {
+        if (tree.is(Tree.Kind.CLASS_MEMBER_ACCESS) || tree.is(Tree.Kind.OBJECT_MEMBER_ACCESS)) {
+          MemberAccessTree memberAccess = (MemberAccessTree) tree;
+          String className = nameOf(memberAccess.object());
+          String memberName = nameOf(memberAccess.member());
+          if (className != null && memberName != null) {
+            return className + "::" + memberName;
+          }
+        } else {
+          if (tree.is(Tree.Kind.VARIABLE_IDENTIFIER)) {
+            VariableIdentifierTree variableIdentifier = (VariableIdentifierTree) tree;
+            if (variableIdentifier.text().equals("$this")) {
+              ClassDeclarationTree classDeclaration = (ClassDeclarationTree) TreeUtils.findAncestorWithKind(tree, EnumSet.of(Kind.CLASS_DECLARATION, Kind.TRAIT_DECLARATION));
+              if (classDeclaration != null) {
+                return nameOf(classDeclaration.name());
+              }
+            }
+          }
         }
       }
     }
@@ -209,8 +177,10 @@ public final class CheckUtils {
   public static boolean hasModifier(ClassMemberTree tree, String toFind) {
     if (tree.is(Kind.METHOD_DECLARATION)) {
       return hasModifier(((MethodDeclarationTree) tree).modifiers(), toFind);
-    } else if (tree.is(Kind.CLASS_PROPERTY_DECLARATION)) {
-      return hasModifier(((ClassPropertyDeclarationTree) tree).modifierTokens(), toFind);
+    } else {
+      if (tree.is(Kind.CLASS_PROPERTY_DECLARATION)) {
+        return hasModifier(((ClassPropertyDeclarationTree) tree).modifierTokens(), toFind);
+      }
     }
     return false;
   }
@@ -250,18 +220,15 @@ public final class CheckUtils {
     return expr;
   }
 
-  @Nullable
-  public static ExpressionTree getForCondition(ForStatementTree tree) {
+  @Nullable public static ExpressionTree getForCondition(ForStatementTree tree) {
     if (tree.condition().isEmpty()) {
       return null;
     }
-    // in a loop, all conditions are evaluated but only the last one is used as the result
     return tree.condition().get(tree.condition().size() - 1);
   }
 
-
   public static String trimQuotes(String value) {
-    if (value.length() > 1 && (value.startsWith("'") || value.startsWith("\""))) {
+    if (value.length() > 1 && (value.startsWith("\'") || value.startsWith("\""))) {
       return value.substring(1, value.length() - 1);
     }
     return value;
@@ -284,9 +251,7 @@ public final class CheckUtils {
   public static boolean isFalseValue(ExpressionTree tree) {
     if (tree.is(Tree.Kind.BOOLEAN_LITERAL, Kind.NUMERIC_LITERAL)) {
       String value = ((LiteralTree) tree).value();
-      return value.equalsIgnoreCase("false")
-        || value.equals("0")
-        || value.equals("0.0");
+      return value.equalsIgnoreCase("false") || value.equals("0") || value.equals("0.0");
     }
     if (tree.is(Kind.REGULAR_STRING_LITERAL)) {
       String value = trimQuotes(((LiteralTree) tree).value());
@@ -299,8 +264,7 @@ public final class CheckUtils {
    * @see #isFalseValue(ExpressionTree)
    */
   public static boolean isTrueValue(ExpressionTree tree) {
-    return tree.is(Kind.BOOLEAN_LITERAL, Kind.NUMERIC_LITERAL, Kind.REGULAR_STRING_LITERAL, Kind.NULL_LITERAL)
-      && !isFalseValue(tree);
+    return tree.is(Kind.BOOLEAN_LITERAL, Kind.NUMERIC_LITERAL, Kind.REGULAR_STRING_LITERAL, Kind.NULL_LITERAL) && !isFalseValue(tree);
   }
 
   public static boolean isStringLiteralWithValue(@Nullable Tree tree, String s) {
@@ -320,15 +284,12 @@ public final class CheckUtils {
 
   public static boolean hasAnnotation(Tree declaration, String annotation) {
     if (!annotation.startsWith("@")) {
-      annotation = "@"+annotation;
+      annotation = "@" + annotation;
     }
-
     List<SyntaxTrivia> trivias = ((PHPTree) declaration).getFirstToken().trivias();
-
     if (!trivias.isEmpty()) {
       return StringUtils.containsIgnoreCase(Iterables.getLast(trivias).text(), annotation);
     }
-
     return false;
   }
 
@@ -350,25 +311,17 @@ public final class CheckUtils {
    */
   public static Optional<CallArgumentTree> argument(FunctionCallTree call, String name, int position) {
     SeparatedList<CallArgumentTree> callArguments = call.callArguments();
-
-    CallArgumentTree argument = callArguments.stream()
-      .filter(a -> a.name() != null)
-      .filter(a -> a.name().text().equalsIgnoreCase(name))
-      .findFirst()
-      .orElse(null);
-
+    CallArgumentTree argument = callArguments.stream().filter((a) -> a.name() != null).filter((a) -> a.name().text().equalsIgnoreCase(name)).findFirst().orElse(null);
     if (argument != null) {
       return Optional.of(argument);
     }
-
     if (callArguments.size() >= position + 1 && callArguments.get(position).name() == null) {
       return Optional.of(callArguments.get(position));
     }
-
     return Optional.empty();
   }
 
   public static boolean hasNamedArgument(FunctionCallTree call) {
-    return call.callArguments().stream().anyMatch(arg -> arg.name() != null);
+    return call.callArguments().stream().anyMatch((arg) -> arg.name() != null);
   }
 }
