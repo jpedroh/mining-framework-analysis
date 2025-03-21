f59a7f1020aaa816b3e405ed4878d1d5a77c9ee2
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 13:27:47 +0000

    Mining Framework Analysis

--- src/test/java/de/uniluebeck/itm/spitfire/nCoap/communication/ObserveOptionAutoNotificationMaxAgeTest.java
+++ src/test/java/de/uniluebeck/itm/spitfire/nCoap/communication/ObserveOptionAutoNotificationMaxAgeTest.java
@@ -1,10 +1,8 @@
 package de.uniluebeck.itm.spitfire.nCoap.communication;
-
 import de.uniluebeck.itm.spitfire.nCoap.application.CoapServerApplication;
 import static junit.framework.Assert.*;
 import static de.uniluebeck.itm.spitfire.nCoap.message.options.OptionRegistry.OptionName.*;
 import static de.uniluebeck.itm.spitfire.nCoap.application.CoapServerApplication.DEFAULT_COAP_SERVER_PORT;
-
 import de.uniluebeck.itm.spitfire.nCoap.communication.utils.ObservableDummyWebService;
 import de.uniluebeck.itm.spitfire.nCoap.message.CoapMessage;
 import de.uniluebeck.itm.spitfire.nCoap.message.CoapRequest;
@@ -18,167 +16,118 @@ import java.util.Iterator;
 import java.util.SortedMap;
 import org.junit.BeforeClass;
 import org.junit.Test;
-
 import static de.uniluebeck.itm.spitfire.nCoap.testtools.ByteTestTools.*;
 
-
 /**
-* Tests if the server sends a new notification when Max-Age ends.
-*
-* @author Stefan Hueske
-*/
-public class ObserveOptionAutoNotificationMaxAgeTest extends AbstractCoapCommunicationTest{
+ * Tests if the server sends a new notification when Max-Age ends.
+ *
+ * @author Stefan Hueske
+ */
+public class ObserveOptionAutoNotificationMaxAgeTest extends AbstractCoapCommunicationTest {
+  private static CoapRequest coapRequest;
 
-    //registration requests
-    private static CoapRequest coapRequest;
+  private static CoapResponse expectedNotification1;
 
-    //notifications
-    private static CoapResponse expectedNotification1;
-    private static CoapResponse expectedNotification2;
+  private static CoapResponse expectedNotification2;
 
-    @Override
-    public void createTestScenario() throws Exception {
-        //define expected notifications
-        expectedNotification1 = new CoapResponse(Code.CONTENT_205);
-        expectedNotification1.setPayload("testpayload1".getBytes("UTF-8"));
-        expectedNotification1.setMaxAge(5);
 
-        expectedNotification2 = new CoapResponse(Code.CONTENT_205);
-        expectedNotification2.setPayload("testpayload2".getBytes("UTF-8"));
+<<<<<<< /usr/src/app/output/okleine/ncoap/0fe28d174e89355a76eb698dd4f13327180400a3/src/test/java/de/uniluebeck/itm/spitfire/nCoap/communication/ObserveOptionAutoNotificationMaxAgeTest.java/left.java
+  @BeforeClass public static void init() throws Exception {
+    testServer.reset();
+    Thread.sleep(150);
+    testReceiver.reset();
+    testReceiver.setReceiveEnabled(true);
+    String requestPath = "/testpath";
+    URI targetUri = new URI("coap://localhost:" + CoapServerDatagramChannelFactory.COAP_SERVER_PORT + requestPath);
+    regRequest = new CoapRequest(MsgType.CON, Code.GET, targetUri);
+    regRequest.getHeader().setMsgID(1111);
+    regRequest.setToken(new byte[] { 0x13, 0x53, 0x34 });
+    regRequest.setObserveOptionRequest();
+    (notification1 = new CoapResponse(Code.CONTENT_205)).setPayload("testpayload1tt".getBytes("UTF-8"));
+    notification1.setMaxAge(5);
+    (notification2 = new CoapResponse(Code.CONTENT_205)).setPayload("testpayload2".getBytes("UTF-8"));
+    ObservableDummyWebService observableDummyWebService = new ObservableDummyWebService(requestPath, true, 0, 0);
+    observableDummyWebService.addPreparedResponses(notification1, notification2);
+    testServer.registerService(observableDummyWebService);
+    testReceiver.writeMessage(regRequest, new InetSocketAddress("localhost", COAP_SERVER_PORT));
+    Thread.sleep(150);
+    Thread.sleep(5500);
+    CoapResponse rstMsg = new CoapResponse(Code.EMPTY);
+    rstMsg.getHeader().setMsgType(MsgType.RST);
+    rstMsg.setMessageID(notification2.getMessageID());
+    testReceiver.writeMessage(rstMsg, new InetSocketAddress("localhost", COAP_SERVER_PORT));
+    testReceiver.setReceiveEnabled(false);
+  }
+=======
+>>>>>>> Unknown file: This is a bug in JDime.
 
-        //register webservice on test server
-        registerObservableDummyService(0, 2000, 2000);
 
-        //create registration request
-        URI targetUri = new URI("coap://localhost:" + testServer.getServerPort() + OBSERVABLE_SERVICE_PATH);
-        coapRequest = new CoapRequest(MsgType.CON, Code.GET, targetUri);
-        coapRequest.getHeader().setMsgID(1111);
-        coapRequest.setToken(new byte[]{0x13, 0x53, 0x34});
-        coapRequest.setObserveOptionRequest();
+  @Override public void createTestScenario() throws Exception {
+    expectedNotification1 = new CoapResponse(Code.CONTENT_205);
+    expectedNotification1.setPayload("testpayload1".getBytes("UTF-8"));
+    expectedNotification1.setMaxAge(5);
+    expectedNotification2 = new CoapResponse(Code.CONTENT_205);
+    expectedNotification2.setPayload("testpayload2".getBytes("UTF-8"));
+    registerObservableDummyService(0, 2000, 2000);
+    URI targetUri = new URI("coap://localhost:" + testServer.getServerPort() + OBSERVABLE_SERVICE_PATH);
+    coapRequest = new CoapRequest(MsgType.CON, Code.GET, targetUri);
+    coapRequest.getHeader().setMsgID(1111);
+    coapRequest.setToken(new byte[] { 0x13, 0x53, 0x34 });
+    coapRequest.setObserveOptionRequest();
+    testReceiver.writeMessage(coapRequest, new InetSocketAddress("localhost", testServer.getServerPort()));
+    Thread.sleep(2500);
+    CoapResponse resetMessage = new CoapResponse(Code.EMPTY);
+    resetMessage.getHeader().setMsgType(MsgType.RST);
+    resetMessage.setMessageID(2222);
+    testReceiver.writeMessage(resetMessage, new InetSocketAddress("localhost", testServer.getServerPort()));
+    testReceiver.setReceiveEnabled(false);
+  }
 
-        //registration
-        testReceiver.writeMessage(coapRequest, new InetSocketAddress("localhost", testServer.getServerPort()));
+  @Test public void testReceiverReceived2Messages() {
+    String message = "Receiver did not receive 2 messages";
+    assertEquals(message, 2, testReceiver.getReceivedMessages().values().size());
+  }
 
-        //wait for Max-Age to end and resulting notification
-        Thread.sleep(2500);
+  @Test public void testReceivedMessageArrivedIn2secDelay() {
+    SortedMap<Long, CoapMessage> receivedMessages = testReceiver.getReceivedMessages();
+    Iterator<Long> timeKeys = receivedMessages.keySet().iterator();
+    Long msg1time = timeKeys.next();
+    Long msg2time = timeKeys.next();
+    long delay = msg2time - msg1time;
+    String message = "Scheduled Max-Age notification did not arrive after 5 seconds";
+    assertTrue(message, Math.abs(2000 - delay) < 200);
+  }
 
-        //send reset to remove observer
-        CoapResponse resetMessage = new CoapResponse(Code.EMPTY);
-        resetMessage.getHeader().setMsgType(MsgType.RST);
-        resetMessage.setMessageID(2222);
-        testReceiver.writeMessage(resetMessage, new InetSocketAddress("localhost", testServer.getServerPort()));
+  @Test public void testObserveOptionIsSetProperly() {
+    SortedMap<Long, CoapMessage> receivedMessages = testReceiver.getReceivedMessages();
+    Iterator<Long> timeKeys = receivedMessages.keySet().iterator();
+    CoapMessage recNotification1 = receivedMessages.get(timeKeys.next());
+    CoapMessage recNotification2 = receivedMessages.get(timeKeys.next());
+    Long observe1 = ((UintOption) recNotification1.getOption(OBSERVE_RESPONSE).get(0)).getDecodedValue();
+    Long observe2 = ((UintOption) recNotification2.getOption(OBSERVE_RESPONSE).get(0)).getDecodedValue();
+    String message = String.format("ObserveOption sequence is not set properly (1st: %d, 2nd: %d)", observe1, observe2);
+    assertTrue(message, observe1 < observe2);
+  }
 
-        testReceiver.setReceiveEnabled(false);
-    }
+  @Test public void testMessageType() {
+    SortedMap<Long, CoapMessage> receivedMessages = testReceiver.getReceivedMessages();
+    Iterator<Long> timeKeys = receivedMessages.keySet().iterator();
+    CoapMessage recNotification1 = receivedMessages.get(timeKeys.next());
+    CoapMessage recNotification2 = receivedMessages.get(timeKeys.next());
+    String message = "1st notification should be ACK";
+    assertEquals(message, MsgType.ACK, recNotification1.getMessageType());
+    message = "2nd notification should be CON";
+    assertEquals(message, MsgType.CON, recNotification2.getMessageType());
+  }
 
-//    @BeforeClass
-//    public static void init() throws Exception {
-//        //init
-//        testReceiver.reset();
-//        testServer.reset();
-//        testReceiver.setReceiveEnabled(true);
-//
-//        //Wireshark: https://dl.dropbox.com/u/10179177/Screenshot_2013.04.11-21.53.31.png
-//
-//        //create registration request
-//        String requestPath = "/testpath";
-//        URI targetUri = new URI("coap://localhost:" +
-//                CoapServerApplication.DEFAULT_COAP_SERVER_PORT + requestPath);
-//        coapRequest = new CoapRequest(MsgType.CON, Code.GET, targetUri);
-//        coapRequest.getHeader().setMsgID(1111);
-//        coapRequest.setToken(new byte[]{0x13, 0x53, 0x34});
-//        coapRequest.setObserveOptionRequest();
-//
-//        //create notifications
-//        (expectedNotification1 = new CoapResponse(Code.CONTENT_205)).setPayload("testpayload1tt".getBytes("UTF-8"));
-//        expectedNotification1.setMaxAge(5);
-//        (expectedNotification2 = new CoapResponse(Code.CONTENT_205)).setPayload("testpayload2".getBytes("UTF-8"));
-//
-//        ObservableDummyWebService observableDummyWebService = new ObservableDummyWebService(requestPath, true, 0, 0);
-//        observableDummyWebService.addPreparedResponses(expectedNotification1, expectedNotification2);
-//        testServer.registerService(observableDummyWebService);
-//
-//        //setup testServer
-////        testServer.registerDummyService(requestPath);
-//        //if both cancellations fail, the notification will be send six times
-////        testServer.addResponse(expectedNotification1, expectedNotification2);
-//
-//
-//        //run test sequence
-//
-//        //registration
-//        testReceiver.writeMessage(coapRequest, new InetSocketAddress("localhost", DEFAULT_COAP_SERVER_PORT));
-//        //wait for response
-//        Thread.sleep(150);
-//
-//        //wait for Max-Age to end and resulting notification
-//        Thread.sleep(5500);
-//
-//        //send reset to remove observer
-//        CoapResponse rstMsg = new CoapResponse(Code.EMPTY);
-//        rstMsg.getHeader().setMsgType(MsgType.RST);
-//        rstMsg.setMessageID(expectedNotification2.getMessageID());
-//        testReceiver.writeMessage(rstMsg, new InetSocketAddress("localhost", DEFAULT_COAP_SERVER_PORT));
-//
-//        testReceiver.setReceiveEnabled(false);
-//    }
-
-    @Test
-    public void testReceiverReceived2Messages() {
-        String message = "Receiver did not receive 2 messages";
-        assertEquals(message, 2, testReceiver.getReceivedMessages().values().size());
-    }
-
-    @Test
-    public void testReceivedMessageArrivedIn2secDelay() {
-        SortedMap<Long, CoapMessage> receivedMessages = testReceiver.getReceivedMessages();
-        Iterator<Long> timeKeys = receivedMessages.keySet().iterator();
-        Long msg1time = timeKeys.next();
-        Long msg2time = timeKeys.next();
-        long delay = msg2time - msg1time;
-
-        String message = "Scheduled Max-Age notification did not arrive after 5 seconds";
-        assertTrue(message, Math.abs(2000 - delay) < 200); //200ms tolerance
-    }
-
-    @Test
-    public void testObserveOptionIsSetProperly() {
-        SortedMap<Long, CoapMessage> receivedMessages = testReceiver.getReceivedMessages();
-        Iterator<Long> timeKeys = receivedMessages.keySet().iterator();
-        CoapMessage recNotification1 = receivedMessages.get(timeKeys.next());
-        CoapMessage recNotification2 = receivedMessages.get(timeKeys.next());
-
-        Long observe1 = ((UintOption)recNotification1.getOption(OBSERVE_RESPONSE).get(0)).getDecodedValue();
-        Long observe2 = ((UintOption)recNotification2.getOption(OBSERVE_RESPONSE).get(0)).getDecodedValue();
-
-        String message = String.format("ObserveOption sequence is not set properly (1st: %d, 2nd: %d)",
-                observe1, observe2);
-        assertTrue(message, observe1 < observe2);
-    }
-
-    @Test
-    public void testMessageType() {
-        SortedMap<Long, CoapMessage> receivedMessages = testReceiver.getReceivedMessages();
-        Iterator<Long> timeKeys = receivedMessages.keySet().iterator();
-        CoapMessage recNotification1 = receivedMessages.get(timeKeys.next());
-        CoapMessage recNotification2 = receivedMessages.get(timeKeys.next());
-
-        String message = "1st notification should be ACK";
-        assertEquals(message, MsgType.ACK, recNotification1.getMessageType());
-        message = "2nd notification should be CON";
-        assertEquals(message, MsgType.CON, recNotification2.getMessageType());
-    }
-
-    @Test
-    public void testMessagePayload() {
-        SortedMap<Long, CoapMessage> receivedMessages = testReceiver.getReceivedMessages();
-        Iterator<Long> timeKeys = receivedMessages.keySet().iterator();
-        CoapMessage recNotification1 = receivedMessages.get(timeKeys.next());
-        CoapMessage recNotification2 = receivedMessages.get(timeKeys.next());
-
-        String message = "1st notifications payload does not match";
-        assertEquals(message, expectedNotification1.getPayload(), recNotification1.getPayload());
-        message = "2nd notifications payload does not match";
-        assertEquals(message, expectedNotification2.getPayload(), recNotification2.getPayload());
-    }
+  @Test public void testMessagePayload() {
+    SortedMap<Long, CoapMessage> receivedMessages = testReceiver.getReceivedMessages();
+    Iterator<Long> timeKeys = receivedMessages.keySet().iterator();
+    CoapMessage recNotification1 = receivedMessages.get(timeKeys.next());
+    CoapMessage recNotification2 = receivedMessages.get(timeKeys.next());
+    String message = "1st notifications payload does not match";
+    assertEquals(message, expectedNotification1.getPayload(), recNotification1.getPayload());
+    message = "2nd notifications payload does not match";
+    assertEquals(message, expectedNotification2.getPayload(), recNotification2.getPayload());
+  }
 }
