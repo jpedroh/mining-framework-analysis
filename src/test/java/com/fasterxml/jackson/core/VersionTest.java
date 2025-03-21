20a88b4e5433927a364d3bb61e1e6095e15cd948
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-21 20:53:59 +0000

    Mining Framework Analysis

--- src/test/java/com/fasterxml/jackson/core/VersionTest.java
+++ src/test/java/com/fasterxml/jackson/core/VersionTest.java
@@ -2,19 +2,39 @@ package com.fasterxml.jackson.core;
 
 /**
  * Unit tests for class {@link Version}.
+<<<<<<< /usr/src/app/output/fasterxml/jackson-core/d708338d421df111ab3d4a36bb90b14900594ec0/src/test/java/com/fasterxml/jackson/core/VersionTest.java/left.java
  */
+||||||| /usr/src/app/output/fasterxml/jackson-core/d708338d421df111ab3d4a36bb90b14900594ec0/src/test/java/com/fasterxml/jackson/core/VersionTest.java/base.java
+ *
+ * @date 2017-08-01
+ * @see Version
+ *
+ **/
+=======
+ *
+ **/
+>>>>>>> /usr/src/app/output/fasterxml/jackson-core/d708338d421df111ab3d4a36bb90b14900594ec0/src/test/java/com/fasterxml/jackson/core/VersionTest.java/right.java
 public class VersionTest extends BaseTest
 {
+  
   public void testCompareToOne() {
       Version version = Version.unknownVersion();
+<<<<<<< /usr/src/app/output/fasterxml/jackson-core/d708338d421df111ab3d4a36bb90b14900594ec0/src/test/java/com/fasterxml/jackson/core/VersionTest.java/left.java
       Version versionTwo = new Version(0, -263, -1820, "", "", "");
+||||||| /usr/src/app/output/fasterxml/jackson-core/d708338d421df111ab3d4a36bb90b14900594ec0/src/test/java/com/fasterxml/jackson/core/VersionTest.java/base.java
+      Version versionTwo = new Version(0, (-263), (-1820), "");
+=======
+      Version versionTwo = new Version(0, (-263), (-1820), "",
+              "", "");
+>>>>>>> /usr/src/app/output/fasterxml/jackson-core/d708338d421df111ab3d4a36bb90b14900594ec0/src/test/java/com/fasterxml/jackson/core/VersionTest.java/right.java
 
       assertEquals(263, version.compareTo(versionTwo));
   }
 
   public void testCompareToReturningZero() {
       Version version = Version.unknownVersion();
-      Version versionTwo = new Version(0, 0, 0, "", "", "");
+      Version versionTwo = new Version(0, 0, 0, "",
+              "", "");
 
       assertEquals(0, version.compareTo(versionTwo));
   }
@@ -28,7 +48,14 @@ public class VersionTest extends BaseTest
 
   public void testCompareToTwo() {
       Version version = Version.unknownVersion();
+<<<<<<< /usr/src/app/output/fasterxml/jackson-core/d708338d421df111ab3d4a36bb90b14900594ec0/src/test/java/com/fasterxml/jackson/core/VersionTest.java/left.java
       Version versionTwo = new Version(-1, 0, 0, "SNAPSHOT", "groupId", "artifactId");
+||||||| /usr/src/app/output/fasterxml/jackson-core/d708338d421df111ab3d4a36bb90b14900594ec0/src/test/java/com/fasterxml/jackson/core/VersionTest.java/base.java
+      Version versionTwo = new Version(-1, 0, 0, "SNAPSHOT", "groupId");
+=======
+      Version versionTwo = new Version(-1, 0, 0, "SNAPSHOT", "groupId",
+              "", "");
+>>>>>>> /usr/src/app/output/fasterxml/jackson-core/d708338d421df111ab3d4a36bb90b14900594ec0/src/test/java/com/fasterxml/jackson/core/VersionTest.java/right.java
 
       int diff = version.compareTo(versionTwo);
       assertTrue("Diff should be negative, was: "+diff, diff < 0);
