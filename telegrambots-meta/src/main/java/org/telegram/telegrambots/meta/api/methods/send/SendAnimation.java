1b9e77f7de5013896062133dc4ffdd11bd2cb67d
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-22 05:38:44 +0000

    Mining Framework Analysis

--- telegrambots-meta/src/main/java/org/telegram/telegrambots/meta/api/methods/send/SendAnimation.java
+++ telegrambots-meta/src/main/java/org/telegram/telegrambots/meta/api/methods/send/SendAnimation.java
@@ -1,5 +1,4 @@
 package org.telegram.telegrambots.meta.api.methods.send;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
@@ -17,7 +16,6 @@ import org.telegram.telegrambots.meta.api.objects.MessageEntity;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboard;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiValidationException;
-
 import java.util.List;
 
 /**
@@ -28,58 +26,68 @@ import java.util.List;
  *
  * Bots can currently send animation files of up to 50 MB in size, this limit may be changed in the future.
  */
-@EqualsAndHashCode(callSuper = false)
-@Getter
-@Setter
-@ToString
-@RequiredArgsConstructor
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-public class SendAnimation extends SendMediaBotMethod<Message> {
-    public static final String PATH = "sendAnimation";
+@EqualsAndHashCode(callSuper = false) @Getter @Setter @ToString @RequiredArgsConstructor @NoArgsConstructor @AllArgsConstructor @Builder public class SendAnimation extends SendMediaBotMethod<Message> {
+  public static final String PATH = "sendAnimation";
 
-    public static final String CHATID_FIELD = "chat_id";
-    public static final String MESSAGETHREADID_FIELD = "message_thread_id";
-    public static final String ANIMATION_FIELD = "animation";
-    public static final String DURATION_FIELD = "duration";
-    public static final String WIDTH_FIELD = "width";
-    public static final String HEIGHT_FIELD = "height";
-    public static final String CAPTION_FIELD = "caption";
-    public static final String PARSEMODE_FIELD = "parse_mode";
-    public static final String DISABLENOTIFICATION_FIELD = "disable_notification";
-    public static final String REPLYTOMESSAGEID_FIELD = "reply_to_message_id";
-    public static final String REPLYMARKUP_FIELD = "reply_markup";
-    public static final String THUMB_FIELD = "thumb";
-    public static final String CAPTION_ENTITIES_FIELD = "caption_entities";
-    public static final String ALLOWSENDINGWITHOUTREPLY_FIELD = "allow_sending_without_reply";
-    public static final String PROTECTCONTENT_FIELD = "protect_content";
-    public static final String HASSPOILER_FIELD = "has_spoiler";
+  public static final String CHATID_FIELD = "chat_id";
 
-    @NonNull
-    private String chatId; ///< Unique identifier for the chat to send the message to (Or username for channels)
-    private Integer messageThreadId;
+  public static final String MESSAGETHREADID_FIELD = "message_thread_id";
 
-    /**
-     * Animation to send. Pass a file_id as String to send an animation that exists on the
-     * Telegram servers (recommended), pass an HTTP URL as a String for Telegram to get an animation
-     * from the Internet, or upload a new animation using multipart/form-data.
-     */
-    /**
+  public static final String ANIMATION_FIELD = "animation";
+
+  public static final String DURATION_FIELD = "duration";
+
+  public static final String WIDTH_FIELD = "width";
+
+  public static final String HEIGHT_FIELD = "height";
+
+  public static final String CAPTION_FIELD = "caption";
+
+  public static final String PARSEMODE_FIELD = "parse_mode";
+
+  public static final String DISABLENOTIFICATION_FIELD = "disable_notification";
+
+  public static final String REPLYTOMESSAGEID_FIELD = "reply_to_message_id";
+
+  public static final String REPLYMARKUP_FIELD = "reply_markup";
+
+  public static final String THUMB_FIELD = "thumb";
+
+  public static final String CAPTION_ENTITIES_FIELD = "caption_entities";
+
+  public static final String ALLOWSENDINGWITHOUTREPLY_FIELD = "allow_sending_without_reply";
+
+  public static final String PROTECTCONTENT_FIELD = "protect_content";
+
+  public static final String HASSPOILER_FIELD = "has_spoiler";
+
+  @NonNull private String chatId;
+
+  /**
      * Unique identifier for the target message thread (topic) of the forum;
      * for forum supergroups only
      */
-    @NonNull
-    private InputFile animation;
-    private Integer duration; ///< Optional. Duration of sent animation in seconds
-    private String caption; ///< Optional. Animation caption (may also be used when resending videos by file_id).
-    private Integer width; ///< Optional. Animation width
-    private Integer height; ///< Optional. Animation height
-    private Boolean disableNotification; ///< Optional. Sends the message silently. Users will receive a notification with no sound.
-    private Integer replyToMessageId; ///< Optional. If the message is a reply, ID of the original message
-    private ReplyKeyboard replyMarkup; ///< Optional. JSON-serialized object for a custom reply keyboard
-    private String parseMode; ///< Optional. Send Markdown or HTML, if you want Telegram apps to show bold, italic, fixed-width text or inline URLs in the media caption.
-    /**
+  private Integer messageThreadId;
+
+  @NonNull private InputFile animation;
+
+  private Integer duration;
+
+  private String caption;
+
+  private Integer width;
+
+  private Integer height;
+
+  private Boolean disableNotification;
+
+  private Integer replyToMessageId;
+
+  private ReplyKeyboard replyMarkup;
+
+  private String parseMode;
+
+  /**
      * Optional.
      * Thumbnail of the file sent. The thumbnail should be in JPEG format and less than 200 kB in size.
      * A thumbnail’s width and height should not exceed 320.
@@ -87,76 +95,68 @@ public class SendAnimation extends SendMediaBotMethod<Message> {
      * Thumbnails can’t be reused and can be only uploaded as a new file, so you can pass “attach://<file_attach_name>”
      * if the thumbnail was uploaded using multipart/form-data under <file_attach_name>.
      */
-    private InputFile thumb;
-    @Singular
-    private List<MessageEntity> captionEntities; ///< Optional. List of special entities that appear in the caption, which can be specified instead of parse_mode
-    private Boolean allowSendingWithoutReply; ///< Optional	Pass True, if the message should be sent even if the specified replied-to message is not found
-    private Boolean protectContent; ///< Optional. Protects the contents of sent messages from forwarding and saving
-    /**
+  private InputFile thumb;
+
+  @Singular private List<MessageEntity> captionEntities;
+
+  private Boolean allowSendingWithoutReply;
+
+  private Boolean protectContent;
+
+  /**
      * Optional.
      * Pass True if the animation must be covered with a spoiler animation
      */
-    private Boolean hasSpoiler;
+  private Boolean hasSpoiler;
 
-    @Tolerate
-    public void setChatId(@NonNull Long chatId) {
-        this.chatId = chatId.toString();
+  @Tolerate public void setChatId(@NonNull Long chatId) {
+    this.chatId = chatId.toString();
+  }
+
+  public void enableNotification() {
+    this.disableNotification = false;
+  }
+
+  public void disableNotification() {
+    this.disableNotification = true;
+  }
+
+  @Override public Message deserializeResponse(String answer) throws TelegramApiRequestException {
+    return deserializeResponse(answer, Message.class);
+  }
+
+  @Override public void validate() throws TelegramApiValidationException {
+    if (chatId.isEmpty()) {
+      throw new TelegramApiValidationException("ChatId parameter can\'t be empty", this);
     }
-
-    public void enableNotification() {
-        this.disableNotification = false;
+    if (parseMode != null && (captionEntities != null && !captionEntities.isEmpty())) {
+      throw new TelegramApiValidationException("Parse mode can\'t be enabled if Entities are provided", this);
     }
-
-    public void disableNotification() {
-        this.disableNotification = true;
+    animation.validate();
+    if (replyMarkup != null) {
+      replyMarkup.validate();
     }
-
-    @Override
-    public Message deserializeResponse(String answer) throws TelegramApiRequestException {
-        return deserializeResponse(answer, Message.class);
+    if (thumb != null) {
+      thumb.validate();
     }
+  }
 
-    @Override
-    public void validate() throws TelegramApiValidationException {
-        if (chatId.isEmpty()) {
-            throw new TelegramApiValidationException("ChatId parameter can't be empty", this);
-        }
+  @Override public String getMethod() {
+    return PATH;
+  }
 
-        if (parseMode != null && (captionEntities != null && !captionEntities.isEmpty()) ) {
-            throw new TelegramApiValidationException("Parse mode can't be enabled if Entities are provided", this);
-        }
+  @Override public InputFile getFile() {
+    return animation;
+  }
 
-        animation.validate();
+  @Override public String getFileField() {
+    return ANIMATION_FIELD;
+  }
 
-        if (replyMarkup != null) {
-            replyMarkup.validate();
-        }
-        if (thumb != null) {
-            thumb.validate();
-        }
+  public static class SendAnimationBuilder {
+    @Tolerate public SendAnimationBuilder chatId(@NonNull Long chatId) {
+      this.chatId = chatId.toString();
+      return this;
     }
-
-    @Override
-    public String getMethod() {
-        return PATH;
-    }
-
-    @Override
-    public InputFile getFile() {
-        return animation;
-    }
-
-    @Override
-    public String getFileField() {
-        return ANIMATION_FIELD;
-    }
-
-    public static class SendAnimationBuilder {
-
-        @Tolerate
-        public SendAnimationBuilder chatId(@NonNull Long chatId) {
-            this.chatId = chatId.toString();
-            return this;
-        }
-    }
+  }
 }
