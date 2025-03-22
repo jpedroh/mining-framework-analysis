efe36d131c26d50f14de0713719e3efef7f635f6
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-22 10:24:49 +0000

    Mining Framework Analysis

--- lang/java/protobuf/src/main/java/org/apache/avro/protobuf/ProtobufData.java
+++ lang/java/protobuf/src/main/java/org/apache/avro/protobuf/ProtobufData.java
@@ -1,22 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.apache.avro.protobuf;
-
 import java.util.List;
 import java.util.Arrays;
 import java.util.ArrayList;
@@ -24,17 +6,14 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.IdentityHashMap;
 import java.util.concurrent.ConcurrentHashMap;
-
 import java.io.IOException;
 import java.io.File;
-
 import org.apache.avro.Schema;
 import org.apache.avro.Schema.Field;
 import org.apache.avro.generic.GenericData;
 import org.apache.avro.specific.SpecificData;
 import org.apache.avro.io.DatumReader;
 import org.apache.avro.io.DatumWriter;
-
 import com.google.protobuf.ByteString;
 import com.google.protobuf.Message;
 import com.google.protobuf.Message.Builder;
@@ -45,7 +24,6 @@ import com.google.protobuf.Descriptors.EnumDescriptor;
 import com.google.protobuf.Descriptors.EnumValueDescriptor;
 import com.google.protobuf.Descriptors.FileDescriptor;
 import com.google.protobuf.DescriptorProtos.FileOptions;
-
 import org.apache.avro.util.internal.Accessor;
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.databind.JsonNode;
@@ -66,104 +44,94 @@ public class ProtobufData extends GenericData {
     return INSTANCE;
   }
 
-  @Override
-  public DatumReader createDatumReader(Schema schema) {
+  @Override public DatumReader createDatumReader(Schema schema) {
     return new ProtobufDatumReader(schema, schema, this);
   }
 
-  @Override
-  public DatumWriter createDatumWriter(Schema schema) {
+  @Override public DatumWriter createDatumWriter(Schema schema) {
     return new ProtobufDatumWriter(schema, this);
   }
 
-  @Override
-  public void setField(Object r, String n, int pos, Object o) {
+  @Override public void setField(Object r, String n, int pos, Object o) {
     setField(r, n, pos, o, getRecordState(r, getSchema(r.getClass())));
   }
 
-  @Override
-  public Object getField(Object r, String name, int pos) {
+  @Override public Object getField(Object r, String name, int pos) {
     return getField(r, name, pos, getRecordState(r, getSchema(r.getClass())));
   }
 
-  @Override
-  protected void setField(Object r, String n, int pos, Object o, Object state) {
+  @Override protected void setField(Object r, String n, int pos, Object o, Object state) {
     Builder b = (Builder) r;
     FieldDescriptor f = ((FieldDescriptor[]) state)[pos];
     switch (f.getType()) {
-    case MESSAGE:
+      case MESSAGE:
       if (o == null) {
         b.clearField(f);
         break;
       }
-    default:
+      default:
       b.setField(f, o);
     }
   }
 
-  @Override
-  protected Object getField(Object record, String name, int pos, Object state) {
+  @Override protected Object getField(Object record, String name, int pos, Object state) {
     Message m = (Message) record;
     FieldDescriptor f = ((FieldDescriptor[]) state)[pos];
     switch (f.getType()) {
-    case MESSAGE:
-      if (!f.isRepeated() && !m.hasField(f))
+      case MESSAGE:
+      if (!f.isRepeated() && !m.hasField(f)) {
         return null;
-    default:
+      }
+      default:
       return m.getField(f);
     }
   }
 
   private final Map<Descriptor, FieldDescriptor[]> fieldCache = new ConcurrentHashMap<>();
 
-  @Override
-  protected Object getRecordState(Object r, Schema s) {
+  @Override protected Object getRecordState(Object r, Schema s) {
     Descriptor d = ((MessageOrBuilder) r).getDescriptorForType();
     FieldDescriptor[] fields = fieldCache.get(d);
-    if (fields == null) { // cache miss
+    if (fields == null) {
       fields = new FieldDescriptor[s.getFields().size()];
-      for (Field f : s.getFields())
+      for (Field f : s.getFields()) {
         fields[f.pos()] = d.findFieldByName(f.name());
-      fieldCache.put(d, fields); // update cache
+      }
+      fieldCache.put(d, fields);
     }
     return fields;
   }
 
-  @Override
-  protected boolean isRecord(Object datum) {
+  @Override protected boolean isRecord(Object datum) {
     return datum instanceof Message;
   }
 
-  @Override
-  public Object newRecord(Object old, Schema schema) {
+  @Override public Object newRecord(Object old, Schema schema) {
     try {
       Class c = SpecificData.get().getClass(schema);
-      if (c == null)
-        return newRecord(old, schema); // punt to generic
-      if (c.isInstance(old))
-        return old; // reuse instance
+      if (c == null) {
+        return newRecord(old, schema);
+      }
+      if (c.isInstance(old)) {
+        return old;
+      }
       return c.getMethod("newBuilder").invoke(null);
-
     } catch (Exception e) {
       throw new RuntimeException(e);
     }
   }
 
-  @Override
-  protected boolean isArray(Object datum) {
+  @Override protected boolean isArray(Object datum) {
     return datum instanceof List;
   }
 
-  @Override
-  protected boolean isBytes(Object datum) {
+  @Override protected boolean isBytes(Object datum) {
     return datum instanceof ByteString;
   }
 
-  @Override
-  protected Schema getRecordSchema(Object record) {
+  @Override protected Schema getRecordSchema(Object record) {
     Descriptor descriptor = ((Message) record).getDescriptorForType();
     Schema schema = schemaCache.get(descriptor);
-
     if (schema == null) {
       schema = getSchema(descriptor);
       schemaCache.put(descriptor, schema);
@@ -176,18 +144,18 @@ public class ProtobufData extends GenericData {
   /** Return a record schema given a protobuf message class. */
   public Schema getSchema(Class c) {
     Schema schema = schemaCache.get(c);
-
-    if (schema == null) { // cache miss
+    if (schema == null) {
       try {
         Object descriptor = c.getMethod("getDescriptor").invoke(null);
-        if (c.isEnum())
+        if (c.isEnum()) {
           schema = getSchema((EnumDescriptor) descriptor);
-        else
+        } else {
           schema = getSchema((Descriptor) descriptor);
+        }
       } catch (Exception e) {
         throw new RuntimeException(e);
       }
-      schemaCache.put(c, schema); // update cache
+      schemaCache.put(c, schema);
     }
     return schema;
   }
@@ -196,30 +164,27 @@ public class ProtobufData extends GenericData {
 
   public Schema getSchema(Descriptor descriptor) {
     Map<Descriptor, Schema> seen = SEEN.get();
-    if (seen.containsKey(descriptor)) // stop recursion
+    if (seen.containsKey(descriptor)) {
       return seen.get(descriptor);
+    }
     boolean first = seen.isEmpty();
-
     Schema converted = schemaConversions.get(descriptor);
     if (converted != null) {
       return converted;
     }
-
     try {
-      Schema result = Schema.createRecord(descriptor.getName(), null,
-          getNamespace(descriptor.getFile(), descriptor.getContainingType()), false);
-
+      Schema result = Schema.createRecord(descriptor.getName(), null, getNamespace(descriptor.getFile(), descriptor.getContainingType()), false);
       seen.put(descriptor, result);
-
       List<Field> fields = new ArrayList<>();
-      for (FieldDescriptor f : descriptor.getFields())
+      for (FieldDescriptor f : descriptor.getFields()) {
         fields.add(Accessor.createField(f.getName(), getSchema(f), null, getDefault(f)));
+      }
       result.setFields(fields);
       return result;
-
-    } finally {
-      if (first)
+    }  finally {
+      if (first) {
         seen.clear();
+      }
     }
   }
 
@@ -267,48 +232,49 @@ public class ProtobufData extends GenericData {
 
   private Schema getSchema(FieldDescriptor f) {
     Schema s = getNonRepeatedSchema(f);
-    if (f.isRepeated())
+    if (f.isRepeated()) {
       s = Schema.createArray(s);
+    }
     return s;
   }
 
   private Schema getNonRepeatedSchema(FieldDescriptor f) {
     Schema result;
     switch (f.getType()) {
-    case BOOL:
+      case BOOL:
       return Schema.create(Schema.Type.BOOLEAN);
-    case FLOAT:
+      case FLOAT:
       return Schema.create(Schema.Type.FLOAT);
-    case DOUBLE:
+      case DOUBLE:
       return Schema.create(Schema.Type.DOUBLE);
-    case STRING:
+      case STRING:
       Schema s = Schema.create(Schema.Type.STRING);
       GenericData.setStringType(s, GenericData.StringType.String);
       return s;
-    case BYTES:
+      case BYTES:
       return Schema.create(Schema.Type.BYTES);
-    case INT32:
-    case UINT32:
-    case SINT32:
-    case FIXED32:
-    case SFIXED32:
+      case INT32:
+      case UINT32:
+      case SINT32:
+      case FIXED32:
+      case SFIXED32:
       return Schema.create(Schema.Type.INT);
-    case INT64:
-    case UINT64:
-    case SINT64:
-    case FIXED64:
-    case SFIXED64:
+      case INT64:
+      case UINT64:
+      case SINT64:
+      case FIXED64:
+      case SFIXED64:
       return Schema.create(Schema.Type.LONG);
-    case ENUM:
+      case ENUM:
       return getSchema(f.getEnumType());
-    case MESSAGE:
+      case MESSAGE:
       result = getSchema(f.getMessageType());
-      if (f.isOptional())
-        // wrap optional record fields in a union with null
+      if (f.isOptional()) {
         result = Schema.createUnion(Arrays.asList(NULL, result));
+      }
       return result;
-    case GROUP: // groups are deprecated
-    default:
+      case GROUP:
+      default:
       throw new RuntimeException("Unexpected type: " + f.getType());
     }
   }
@@ -322,17 +288,19 @@ public class ProtobufData extends GenericData {
   }
 
   private static final JsonFactory FACTORY = new JsonFactory();
+
   private static final ObjectMapper MAPPER = new ObjectMapper(FACTORY);
+
   private static final JsonNodeFactory NODES = JsonNodeFactory.instance;
 
   private JsonNode getDefault(FieldDescriptor f) {
-    if (f.isRequired() || f.isRepeated()) // no default
+    if (f.isRequired() || f.isRepeated()) {
       return null;
-
-    if (f.hasDefaultValue()) { // parse spec'd default value
+    }
+    if (f.hasDefaultValue()) {
       Object value = f.getDefaultValue();
       switch (f.getType()) {
-      case ENUM:
+        case ENUM:
         value = ((EnumValueDescriptor) value).getName();
         break;
       }
@@ -343,35 +311,33 @@ public class ProtobufData extends GenericData {
         throw new RuntimeException(e);
       }
     }
-
-    switch (f.getType()) { // generate default for type
-    case BOOL:
+    switch (f.getType()) {
+      case BOOL:
       return NODES.booleanNode(false);
-    case FLOAT:
-    case DOUBLE:
-    case INT32:
-    case UINT32:
-    case SINT32:
-    case FIXED32:
-    case SFIXED32:
-    case INT64:
-    case UINT64:
-    case SINT64:
-    case FIXED64:
-    case SFIXED64:
+      case FLOAT:
+      case DOUBLE:
+      case INT32:
+      case UINT32:
+      case SINT32:
+      case FIXED32:
+      case SFIXED32:
+      case INT64:
+      case UINT64:
+      case SINT64:
+      case FIXED64:
+      case SFIXED64:
       return NODES.numberNode(0);
-    case STRING:
-    case BYTES:
+      case STRING:
+      case BYTES:
       return NODES.textNode("");
-    case ENUM:
+      case ENUM:
       return NODES.textNode(f.getEnumType().getValues().get(0).getName());
-    case MESSAGE:
+      case MESSAGE:
       return NODES.nullNode();
-    case GROUP: // groups are deprecated
-    default:
+      case GROUP:
+      default:
       throw new RuntimeException("Unexpected type: " + f.getType());
     }
-
   }
 
   private Map<Descriptor, Schema> schemaConversions = new HashMap<>();
@@ -385,5 +351,4 @@ public class ProtobufData extends GenericData {
   public void addSchemaConversion(Descriptor descriptor, Schema avroSchema) {
     schemaConversions.put(descriptor, avroSchema);
   }
-
 }
--- lang/java/protobuf/src/test/java/org/apache/avro/protobuf/TestProtobuf.java
+++ lang/java/protobuf/src/test/java/org/apache/avro/protobuf/TestProtobuf.java
@@ -1,45 +1,21 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.apache.avro.protobuf;
-
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-
 import org.apache.avro.Schema;
 import org.apache.avro.io.DecoderFactory;
 import org.apache.avro.io.Encoder;
 import org.apache.avro.io.EncoderFactory;
 import org.apache.avro.specific.SpecificData;
-
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
-
 import com.google.protobuf.ByteString;
-
 import org.apache.avro.protobuf.noopt.Test.Foo;
 import org.apache.avro.protobuf.noopt.Test.A;
 import org.apache.avro.protobuf.noopt.Test.M.N;
 
 public class TestProtobuf {
-  @Test
-  public void testMessage() throws Exception {
-
+  @Test public void testMessage() throws Exception {
     System.out.println(ProtobufData.get().getSchema(Foo.class).toString(true));
     Foo.Builder builder = Foo.newBuilder();
     builder.setInt32(0);
@@ -61,64 +37,48 @@ public class TestProtobuf {
     builder.addIntArray(27);
     builder.addSyms(A.Y);
     Foo fooInner = builder.build();
-
     Foo fooInArray = builder.build();
     builder = Foo.newBuilder(fooInArray);
     builder.addFooArray(fooInArray);
-
     builder = Foo.newBuilder(fooInner);
     builder.setFoo(fooInner);
     Foo foo = builder.build();
-
     System.out.println(foo);
-
     ByteArrayOutputStream bao = new ByteArrayOutputStream();
     ProtobufDatumWriter<Foo> w = new ProtobufDatumWriter<>(Foo.class);
     Encoder e = EncoderFactory.get().binaryEncoder(bao, null);
     w.write(foo, e);
     e.flush();
-
-    Object o = new ProtobufDatumReader<>(Foo.class).read(null,
-        DecoderFactory.get().binaryDecoder(new ByteArrayInputStream(bao.toByteArray()), null));
-
+    Object o = new ProtobufDatumReader<>(Foo.class).read(null, DecoderFactory.get().binaryDecoder(new ByteArrayInputStream(bao.toByteArray()), null));
     assertEquals(foo, o);
   }
 
-  @Test
-  public void testNestedEnum() throws Exception {
+  @Test public void testNestedEnum() throws Exception {
     Schema s = ProtobufData.get().getSchema(N.class);
     assertEquals(N.class.getName(), SpecificData.get().getClass(s).getName());
   }
 
-  @Test
-  public void testNestedClassNamespace() throws Exception {
+  @Test public void testNestedClassNamespace() throws Exception {
     Schema s = ProtobufData.get().getSchema(Foo.class);
     assertEquals(org.apache.avro.protobuf.noopt.Test.class.getName(), s.getNamespace());
   }
 
-  @Test
-  public void testClassNamespaceInMultipleFiles() throws Exception {
-    Schema fooSchema = ProtobufData.get().getSchema(org.apache.avro.protobuf.multiplefiles.Foo.class);
-    assertEquals(org.apache.avro.protobuf.multiplefiles.Foo.class.getPackage().getName(), fooSchema.getNamespace());
-
-    Schema nSchema = ProtobufData.get().getSchema(org.apache.avro.protobuf.multiplefiles.M.N.class);
-    assertEquals(org.apache.avro.protobuf.multiplefiles.M.class.getName(), nSchema.getNamespace());
-  }
-
-  @Test
-  public void testGetNonRepeatedSchemaWithLogicalType() throws Exception {
+  @Test public void testGetNonRepeatedSchemaWithLogicalType() throws Exception {
     ProtoConversions.TimestampMillisConversion conversion = new ProtoConversions.TimestampMillisConversion();
-
-    // Don't convert to logical type if conversion isn't set
     ProtobufData instance1 = new ProtobufData();
     Schema s1 = instance1.getSchema(com.google.protobuf.Timestamp.class);
     assertNotEquals(conversion.getRecommendedSchema(), s1);
-
-    // Convert to logical type if conversion is set
     ProtobufData instance2 = new ProtobufData();
     instance2.addLogicalTypeConversion(conversion);
     instance2.addSchemaConversion(com.google.protobuf.Timestamp.getDescriptor(), conversion.getRecommendedSchema());
     Schema s2 = instance2.getSchema(com.google.protobuf.Timestamp.class);
     assertEquals(conversion.getRecommendedSchema(), s2);
+  }
+
+  @Test public void testClassNamespaceInMultipleFiles() throws Exception {
+    Schema fooSchema = ProtobufData.get().getSchema(org.apache.avro.protobuf.multiplefiles.Foo.class);
+    assertEquals(org.apache.avro.protobuf.multiplefiles.Foo.class.getPackage().getName(), fooSchema.getNamespace());
+    Schema nSchema = ProtobufData.get().getSchema(org.apache.avro.protobuf.multiplefiles.M.N.class);
+    assertEquals(org.apache.avro.protobuf.multiplefiles.M.class.getName(), nSchema.getNamespace());
   }
 }
