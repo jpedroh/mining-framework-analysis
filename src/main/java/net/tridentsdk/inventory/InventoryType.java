8dda8b287523c5a8a585ad823c62eb4e3599de95
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-22 00:47:44 +0000

    Mining Framework Analysis

--- src/main/java/net/tridentsdk/inventory/InventoryType.java
+++ src/main/java/net/tridentsdk/inventory/InventoryType.java
@@ -1,21 +1,4 @@
-/*
- * Trident - A Multithreaded Server Alternative
- * Copyright 2017 The TridentSDK Team
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package net.tridentsdk.inventory;
-
 import javax.annotation.concurrent.Immutable;
 
 /**
@@ -24,56 +7,91 @@ import javax.annotation.concurrent.Immutable;
  *
  * @author TridentSDK
  * @since 0.3-alpha-DP
- */
-@Immutable
-public enum InventoryType {
-    /**
-     * Represents a player inventory. This exists in code
-     * only, no explicit type for an inventory of this type
-     * may be created sent by the server.
-     */
-    PLAYER("Player"),
-    CONTAINER,
-    CHEST,
-    CRAFTING_TABLE,
-    FURNACE,
-    DISPENSER,
-    ENCHANTING_TABLE,
-    BREWING_STAND,
-    VILLAGER,
-    BEACON,
-    ANVIL,
-    HOPPER,
-    DROPPER,
-    SHULKER_BOX,
-    HORSE("EntityHorse");
+ */@Immutable public enum InventoryType {
+  CONTAINER,
+  PLAYER(
+<<<<<<< /usr/src/app/output/tridentsdk/tridentsdk/5f8547d9368c73ccbc2ba422b1efa3775af4ac61/src/main/java/net/tridentsdk/inventory/InventoryType.java/left.java
+  "player"
+=======
+  "Player"
+>>>>>>> /usr/src/app/output/tridentsdk/tridentsdk/5f8547d9368c73ccbc2ba422b1efa3775af4ac61/src/main/java/net/tridentsdk/inventory/InventoryType.java/right.java
+  ),
+  CHEST,
+  CRAFTING_TABLE,
+  FURNACE,
+  DISPENSER,
+  ENCHANTING_TABLE,
+  BREWING_STAND,
+  VILLAGER,
+  BEACON,
+  ANVIL,
+  HOPPER,
+  DROPPER,
+  SHULKER_BOX,
+  ENTITY_HORSE("EntityHorse"),
+  HORSE("EntityHorse")
+  ;
 
-    /**
-     * The raw string form of the inventory type
+  /**
+     * The raw name of the inventory as represented by the
+     * protocol.
      */
-    private final String name;
+  private final String 
+<<<<<<< /usr/src/app/output/tridentsdk/tridentsdk/5f8547d9368c73ccbc2ba422b1efa3775af4ac61/src/main/java/net/tridentsdk/inventory/InventoryType.java/left.java
+  raw
+=======
+  name
+>>>>>>> /usr/src/app/output/tridentsdk/tridentsdk/5f8547d9368c73ccbc2ba422b1efa3775af4ac61/src/main/java/net/tridentsdk/inventory/InventoryType.java/right.java
+  ;
 
-    /**
-     * Creates an inventory type that is the default
-     * prefixed by {@code minecraft:} and the enum name in
-     * lowercase.
+  /**
+     * Creates a new inventory type based on the enum name.
      */
-    InventoryType() {
-        this.name = "minecraft:" + this.name().toLowerCase();
-    }
+  InventoryType() {
+    this.
+<<<<<<< /usr/src/app/output/tridentsdk/tridentsdk/5f8547d9368c73ccbc2ba422b1efa3775af4ac61/src/main/java/net/tridentsdk/inventory/InventoryType.java/left.java
+    raw
+=======
+    name
+>>>>>>> /usr/src/app/output/tridentsdk/tridentsdk/5f8547d9368c73ccbc2ba422b1efa3775af4ac61/src/main/java/net/tridentsdk/inventory/InventoryType.java/right.java
+     = "minecraft:" + this.name().toLowerCase();
+  }
 
-    /**
-     * Creates a new inventory with the given custom name
-     * as an irregularity.
+  /**
+     * Creates a new inventory type based on the given raw
+     * inventory name.
      *
-     * @param name the name of the inventory
+     * @param raw the raw inventory name
      */
-    InventoryType(String name) {
-        this.name = name;
-    }
+  InventoryType(
+<<<<<<< /usr/src/app/output/tridentsdk/tridentsdk/5f8547d9368c73ccbc2ba422b1efa3775af4ac61/src/main/java/net/tridentsdk/inventory/InventoryType.java/left.java
+  String raw
+=======
+  String name
+>>>>>>> /usr/src/app/output/tridentsdk/tridentsdk/5f8547d9368c73ccbc2ba422b1efa3775af4ac61/src/main/java/net/tridentsdk/inventory/InventoryType.java/right.java
+  ) {
+    this.
+<<<<<<< /usr/src/app/output/tridentsdk/tridentsdk/5f8547d9368c73ccbc2ba422b1efa3775af4ac61/src/main/java/net/tridentsdk/inventory/InventoryType.java/left.java
+    raw
+=======
+    name
+>>>>>>> /usr/src/app/output/tridentsdk/tridentsdk/5f8547d9368c73ccbc2ba422b1efa3775af4ac61/src/main/java/net/tridentsdk/inventory/InventoryType.java/right.java
+     = 
+<<<<<<< /usr/src/app/output/tridentsdk/tridentsdk/5f8547d9368c73ccbc2ba422b1efa3775af4ac61/src/main/java/net/tridentsdk/inventory/InventoryType.java/left.java
+    raw
+=======
+    name
+>>>>>>> /usr/src/app/output/tridentsdk/tridentsdk/5f8547d9368c73ccbc2ba422b1efa3775af4ac61/src/main/java/net/tridentsdk/inventory/InventoryType.java/right.java
+    ;
+  }
 
-    @Override
-    public String toString() {
-        return this.name;
-    }
+  @Override public String toString() {
+    return this.
+<<<<<<< /usr/src/app/output/tridentsdk/tridentsdk/5f8547d9368c73ccbc2ba422b1efa3775af4ac61/src/main/java/net/tridentsdk/inventory/InventoryType.java/left.java
+    raw
+=======
+    name
+>>>>>>> /usr/src/app/output/tridentsdk/tridentsdk/5f8547d9368c73ccbc2ba422b1efa3775af4ac61/src/main/java/net/tridentsdk/inventory/InventoryType.java/right.java
+    ;
+  }
 }
--- src/main/java/net/tridentsdk/meta/ItemMeta.java
+++ src/main/java/net/tridentsdk/meta/ItemMeta.java
@@ -1,23 +1,5 @@
-/*
- * Trident - A Multithreaded Server Alternative
- * Copyright 2017 The TridentSDK Team
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package net.tridentsdk.meta;
-
 import net.tridentsdk.meta.nbt.TagCompound;
-
 import javax.annotation.Nullable;
 import javax.annotation.concurrent.ThreadSafe;
 
@@ -29,26 +11,23 @@ import javax.annotation.concurrent.ThreadSafe;
  * @author TridentSDK
  * @since 0.5-alpha
  */
-@ThreadSafe
-public class ItemMeta {
-    /**
+@ThreadSafe public class ItemMeta {
+  /**
      * The NBT data which contains modifications made to
      * this item.
      */
-    private final TagCompound nbt = new TagCompound();
+  private final TagCompound nbt = new TagCompound();
 
-    /**
-     * Obtains the NBT data of this particular item.
-     *
-     * @return the NBT data, or {@code null} if the item was
-     * not modified
-     */
-    @Nullable
-    public TagCompound toNbt() {
-        if (this.nbt.isEmpty()) {
-            return null;
-        }
-
-        return this.nbt;
+  @Nullable public TagCompound toNbt() {
+    if (this.nbt.isEmpty()) {
+      return null;
     }
+    return 
+<<<<<<< /usr/src/app/output/tridentsdk/tridentsdk/5f8547d9368c73ccbc2ba422b1efa3775af4ac61/src/main/java/net/tridentsdk/meta/ItemMeta.java/left.java
+    null
+=======
+    this.nbt
+>>>>>>> /usr/src/app/output/tridentsdk/tridentsdk/5f8547d9368c73ccbc2ba422b1efa3775af4ac61/src/main/java/net/tridentsdk/meta/ItemMeta.java/right.java
+    ;
+  }
 }
