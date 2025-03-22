10d01182f443eba325f7d268d332da4f10d6efce
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-22 10:04:48 +0000

    Mining Framework Analysis

--- wk-framework/src/main/java/cn/wizzer/framework/base/Result.java
+++ wk-framework/src/main/java/cn/wizzer/framework/base/Result.java
@@ -1,5 +1,4 @@
 package cn.wizzer.framework.base;
-
 import org.nutz.json.Json;
 import org.nutz.json.JsonFormat;
 import org.nutz.lang.Strings;
@@ -9,89 +8,85 @@ import org.nutz.mvc.Mvcs;
  * Created by wizzer on 2016/12/21.
  */
 public class Result {
+  private int code;
 
-    private int code;
-    private String msg;
-    private Object data;
+  private String msg;
 
-    public Result() {
+  private Object data;
 
+  public Result() {
+  }
+
+  public static Result NEW() {
+    return new Result();
+  }
+
+  public Result addCode(int code) {
+    this.code = code;
+    return this;
+  }
+
+  public Result addMsg(String msg) {
+    if (Strings.isBlank(msg) || Mvcs.getActionContext() == null || Mvcs.getActionContext().getRequest() == null || Mvcs.getMessage(Mvcs.getActionContext().getRequest(), msg) == null) {
+      this.msg = "";
+    } else {
+      this.msg = Mvcs.getMessage(Mvcs.getActionContext().getRequest(), msg);
     }
+    return this;
+  }
 
-    public static Result NEW() {
-        return new Result();
+  public Result addData(Object data) {
+    this.data = data;
+    return this;
+  }
+
+  public Result(int code, String msg, Object data) {
+    this.code = code;
+    if (Strings.isBlank(msg) || Mvcs.getActionContext() == null || Mvcs.getActionContext().getRequest() == null || Mvcs.getMessage(Mvcs.getActionContext().getRequest(), msg) == null) {
+      this.msg = "";
+    } else {
+      this.msg = Mvcs.getMessage(Mvcs.getActionContext().getRequest(), msg);
     }
+    this.data = data;
+  }
 
+  public static Result success(String content) {
+    return new Result(0, content, null);
+  }
 
-    public Result addCode(int code) {
-        this.code = code;
-        return this;
-    }
+  public static Result success(String content, Object data) {
+    return new Result(0, content, data);
+  }
 
-    public Result addMsg(String msg) {
-        if (Strings.isBlank(msg) || Mvcs.getActionContext() == null || Mvcs.getActionContext().getRequest() == null || Mvcs.getMessage(Mvcs.getActionContext().getRequest(), msg) == null) {
-            this.msg = "";
-        } else {
-            this.msg = Mvcs.getMessage(Mvcs.getActionContext().getRequest(), msg);
-        }
-        return this;
-    }
+  public static Result error(int code, String content) {
+    return new Result(code, content, null);
+  }
 
-    public Result addData(Object data) {
-        this.data = data;
-        return this;
-    }
+  public static Result error(String content) {
+    return new Result(1, content, null);
+  }
 
-    public Result(int code, String msg, Object data) {
-        this.code = code;
-        if (Strings.isBlank(msg) || Mvcs.getActionContext() == null || Mvcs.getActionContext().getRequest() == null || Mvcs.getMessage(Mvcs.getActionContext().getRequest(), msg) == null) {
-            this.msg = "";
-        } else {
-            this.msg = Mvcs.getMessage(Mvcs.getActionContext().getRequest(), msg);
-        }
-        this.data = data;
-    }
+  public static Result success() {
+    return new Result(0, "globals.result.success", null);
+  }
 
-    public static Result success(String content) {
-        return new Result(0, content, null);
-    }
+  public static Result error() {
+    return new Result(1, "globals.result.error", null);
+  }
 
-    public static Result success(String content, Object data) {
-        return new Result(0, content, data);
-    }
+  public int getCode() {
+    return code;
+  }
 
-    public static Result error(int code, String content) {
-        return new Result(code, content, null);
-    }
+  public String getMsg() {
+    return msg;
+  }
 
-    public static Result error(String content) {
-        return new Result(1, content, null);
-    }
+  public Object getData() {
+    return data;
+  }
 
-    public static Result success() {
-        return new Result(0, "globals.result.success", null);
-    }
-
-    public static Result error() {
-        return new Result(1, "globals.result.error", null);
-    }
-    
-    
-    public int getCode() {
-        return code;
-    }
-
-    public String getMsg() {
-        return msg;
-    }
-
-    public Object getData() {
-        return data;
-    }
-
-    @Override
-    public String toString() {
-        return Json.toJson(this, JsonFormat.compact());
-    }
-
+  @Override public String toString() {
+    return Json.toJson(this, JsonFormat.compact());
+  }
 }
