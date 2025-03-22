2cab41afe0eb8948a549abecceae661d980404d5
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-22 01:58:13 +0000

    Mining Framework Analysis

--- src/main/java/org/movsim/input/InputData.java
+++ src/main/java/org/movsim/input/InputData.java
@@ -1,123 +1,91 @@
-/**
- * Copyright (C) 2010, 2011 by Arne Kesting, Martin Treiber,
- *                             Ralph Germ, Martin Budden
- *                             <info@movsim.org>
- * ----------------------------------------------------------------------
- * 
- *  This file is part of 
- *  
- *  MovSim - the multi-model open-source vehicular-traffic simulator 
- *
- *  MovSim is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  MovSim is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with MovSim.  If not, see <http://www.gnu.org/licenses/> or
- *  <http://www.movsim.org>.
- *  
- * ----------------------------------------------------------------------
- */
 package org.movsim.input;
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import org.movsim.input.model.SimulationInput;
 import org.movsim.input.model.VehicleInput;
 import org.movsim.input.model.consumption.FuelConsumptionInput;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 public class InputData {
+  /** The Constant logger. */
+  final static Logger logger = LoggerFactory.getLogger(InputData.class);
 
-    /** The Constant logger. */
-    final static Logger logger = LoggerFactory.getLogger(InputData.class);
+  /** The vehicle input data. */
+  private List<VehicleInput> vehicleInputData;
 
-    /** The vehicle input data. */
-    private List<VehicleInput> vehicleInputData;
+  /** The simulation input. */
+  private SimulationInput simulationInput;
 
-    /** The simulation input. */
-    private SimulationInput simulationInput;
-    
-    /** The fuel consumption input. */
-    private FuelConsumptionInput fuelConsumptionInput;
+  /** The fuel consumption input. */
+  private FuelConsumptionInput fuelConsumptionInput;
 
-    private ProjectMetaData projectMetaData;
+  private ProjectMetaData projectMetaData;
 
-    
-    /**
+  /**
      * Instantiates a new input data.
      */
-    public InputData() {
-        projectMetaData = ProjectMetaData.getInstance();
-    }
+  public InputData() {
+    projectMetaData = ProjectMetaData.getInstance();
+  }
 
-    /**
+  /**
      * Sets the project name.
      * 
      * @param projectname
      *            the new project name
      */
-    public void setProjectName(String projectname) {
-        this.projectMetaData.setProjectName(projectname);
-    }
+  public void setProjectName(String projectname) {
+    this.projectMetaData.setProjectName(projectname);
+  }
 
-    /**
+  /**
      * Sets the vehicle input data.
      * 
      * @param vehicleInputData
      *            the new vehicle input data
      */
-    public void setVehicleInputData(List<VehicleInput> vehicleInputData) {
-        this.vehicleInputData = vehicleInputData;
-    }
+  public void setVehicleInputData(List<VehicleInput> vehicleInputData) {
+    this.vehicleInputData = vehicleInputData;
+  }
 
-    public List<VehicleInput> getVehicleInputData() {
-        return vehicleInputData;
-    }
+  public List<VehicleInput> getVehicleInputData() {
+    return vehicleInputData;
+  }
 
-    public static Map<String, VehicleInput> createVehicleInputDataMap(List<VehicleInput> vehicleInputData) {
-        final HashMap<String, VehicleInput> map = new HashMap<String, VehicleInput>();
-        for (final VehicleInput vehInput : vehicleInputData) {
-            final String keyName = vehInput.getLabel();
-            map.put(keyName, vehInput);
-        }
-        return map;
+  public static Map<String, VehicleInput> createVehicleInputDataMap(List<VehicleInput> vehicleInputData) {
+    final HashMap<String, VehicleInput> map = new HashMap<String, VehicleInput>();
+    for (final VehicleInput vehInput : vehicleInputData) {
+      final String keyName = vehInput.getLabel();
+      map.put(keyName, vehInput);
     }
+    return map;
+  }
 
-    public SimulationInput getSimulationInput() {
-        return simulationInput;
-    }
+  public SimulationInput getSimulationInput() {
+    return simulationInput;
+  }
 
-    /**
+  /**
      * Sets the simulation input.
      * 
      * @param simulationInput
      *            the new simulation input
      */
-    public void setSimulationInput(SimulationInput simulationInput) {
-        this.simulationInput = simulationInput;
-    }
+  public void setSimulationInput(SimulationInput simulationInput) {
+    this.simulationInput = simulationInput;
+  }
 
-    public ProjectMetaData getProjectMetaData() {
-        return projectMetaData;
-    }
+  public ProjectMetaData getProjectMetaData() {
+    return projectMetaData;
+  }
 
-    public void setFuelConsumptionInput(FuelConsumptionInput fuelConsumptionInput) {
-        this.fuelConsumptionInput = fuelConsumptionInput;
-    }
+  public void setFuelConsumptionInput(FuelConsumptionInput fuelConsumptionInput) {
+    this.fuelConsumptionInput = fuelConsumptionInput;
+  }
 
-    public FuelConsumptionInput getFuelConsumptionInput() {
-        return fuelConsumptionInput;
-    }
-
+  public FuelConsumptionInput getFuelConsumptionInput() {
+    return fuelConsumptionInput;
+  }
 }
