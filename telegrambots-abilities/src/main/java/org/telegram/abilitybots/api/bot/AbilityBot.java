7e7e219074b2c8a1bc26188490156fb8db2427ff
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-22 06:26:07 +0000

    Mining Framework Analysis

--- telegrambots-abilities/src/main/java/org/telegram/abilitybots/api/bot/AbilityBot.java
+++ telegrambots-abilities/src/main/java/org/telegram/abilitybots/api/bot/AbilityBot.java
@@ -1,44 +1,6 @@
-package org.telegram.abilitybots.api.bot;
-
-import org.telegram.abilitybots.api.db.DBContext;
-import org.telegram.telegrambots.api.objects.Update;
-import org.telegram.telegrambots.bots.DefaultBotOptions;
-import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
-import org.telegram.telegrambots.generics.LongPollingBot;
-import org.telegram.telegrambots.util.WebhookUtils;
-
-import static org.telegram.abilitybots.api.db.MapDBContext.onlineInstance;
-
-/**
- * The default AbilityBot class implements {@link LongPollingBot}. It delegates all updates to a {@link TelegramLongPollingBot} instance.
- *
- * @author Abbas Abou Daya
- */
-public abstract class AbilityBot extends BaseAbilityBot implements LongPollingBot {
-  protected AbilityBot(String botToken, String botUsername, DBContext db, DefaultBotOptions botOptions) {
-    super(botToken, botUsername, db, botOptions);
-  }
-
-  protected AbilityBot(String botToken, String botUsername, DBContext db) {
-    this(botToken, botUsername, db, new DefaultBotOptions());
-  }
-
-  protected AbilityBot(String botToken, String botUsername, DefaultBotOptions botOptions) {
-    this(botToken, botUsername, onlineInstance(botUsername), botOptions);
-  }
-
-  protected AbilityBot(String botToken, String botUsername) {
-    this(botToken, botUsername, onlineInstance(botUsername));
-  }
-
-  @Override
-  public void onUpdateReceived(Update update) {
-    super.onUpdateReceived(update);
-  }
-
-  @Override
-  public void clearWebhook() throws TelegramApiRequestException {
-    WebhookUtils.clearWebhook(this);
-  }
-}
+  package     org . telegram . abilitybots . api . bot ;   import      org . telegram . abilitybots . api . db . DBContext ;  import       org . telegram . telegrambots . meta . api . methods . GetFile ;  import        org . telegram . telegrambots . meta . api . methods . groupadministration . GetChatAdministrators ;  import        org . telegram . telegrambots . meta . api . methods . send . SendDocument ;  import       org . telegram . telegrambots . meta . api . objects . Message ;  import       org . telegram . telegrambots . meta . api . objects . Update ;  import       org . telegram . telegrambots . meta . api . objects . User ;  import     org . telegram . telegrambots . bots . DefaultBotOptions ;  import     org . telegram . telegrambots . bots . TelegramLongPollingBot ;  import      org . telegram . telegrambots . meta . exceptions . TelegramApiException ;  import      org . telegram . telegrambots . meta . logging . BotLogger ;  import static       org . telegram . abilitybots . api . db . MapDBContext . onlineInstance ;  import     org . telegram . telegrambots . exceptions . TelegramApiRequestException ;  import     org . telegram . telegrambots . generics . LongPollingBot ;  import     org . telegram . telegrambots . util . WebhookUtils ;    @ SuppressWarnings  (  { "WeakerAccess" , "UnusedReturnValue" , "ConfusingArgumentToVarargsMethod" , "ConstantConditions" } ) public abstract class AbilityBot  extends BaseAbilityBot  implements  LongPollingBot  {   protected AbilityBot  (  String botToken ,  String botUsername ,  DBContext db ,  DefaultBotOptions botOptions )  {  super  ( botToken , botUsername , db , botOptions ) ; }   protected AbilityBot  (  String botToken ,  String botUsername ,  DBContext db )  {  this  ( botToken , botUsername , db ,  new DefaultBotOptions  ( ) ) ; }   protected AbilityBot  (  String botToken ,  String botUsername ,  DefaultBotOptions botOptions )  {  this  ( botToken , botUsername ,  onlineInstance  ( botUsername ) , botOptions ) ; }   protected AbilityBot  (  String botToken ,  String botUsername )  {  this  ( botToken , botUsername ,  onlineInstance  ( botUsername ) ) ; }    @ Override public void onUpdateReceived  (  Update update )  {   super . onUpdateReceived  ( update ) ; } 
+<<<<<<<
+  public Ability backupDB  ( )  {  return        builder  ( ) . name  ( BACKUP ) . locality  ( USER ) . privacy  ( CREATOR ) . input  ( 0 ) . action  (  ctx ->  {  File  backup =  new File  ( "backup.json" ) ;  try  (  PrintStream printStream =  new PrintStream  ( backup ) )  {   printStream . print  (  db . backup  ( ) ) ;   sender . sendDocument  (    new SendDocument  ( ) . setDocument  ( backup ) . setChatId  (  ctx . chatId  ( ) ) ) ; }  catch (   FileNotFoundException e )  {   BotLogger . error  ( "Error while fetching backup" , TAG , e ) ; }  catch (   TelegramApiException e )  {   BotLogger . error  ( "Error while sending document/backup file" , TAG , e ) ; } } ) . build  ( ) ; }
+=======
+>>>>>>>
+    @ Override public void clearWebhook  ( )  throws TelegramApiRequestException  {   WebhookUtils . clearWebhook  ( this ) ; } }
