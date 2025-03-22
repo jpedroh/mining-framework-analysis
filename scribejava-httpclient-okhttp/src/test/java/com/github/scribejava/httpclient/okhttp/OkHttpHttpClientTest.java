e47c31bcbb4e84a51e1bf7ec47d7d55fb996afa9
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-22 10:31:07 +0000

    Mining Framework Analysis

--- scribejava-httpclient-okhttp/src/test/java/com/github/scribejava/httpclient/okhttp/OkHttpHttpClientTest.java
+++ scribejava-httpclient-okhttp/src/test/java/com/github/scribejava/httpclient/okhttp/OkHttpHttpClientTest.java
@@ -5,6 +5,12 @@ import com.github.scribejava.core.httpclient.HttpClient;
 import okhttp3.OkHttpClient;
 
 public class OkHttpHttpClientTest extends AbstractClientTest {
+<<<<<<< /usr/src/app/output/scribejava/scribejava/2f5d9b10ee3604387e16d355cfb95af096d3c51c/scribejava-httpclient-okhttp/src/test/java/com/github/scribejava/httpclient/okhttp/OkHttpHttpClientTest.java/left.java
+    private OAuthService oAuthService;
+||||||| /usr/src/app/output/scribejava/scribejava/2f5d9b10ee3604387e16d355cfb95af096d3c51c/scribejava-httpclient-okhttp/src/test/java/com/github/scribejava/httpclient/okhttp/OkHttpHttpClientTest.java/base.java
+    private OAuthService<?> oAuthService;
+=======
+>>>>>>> /usr/src/app/output/scribejava/scribejava/2f5d9b10ee3604387e16d355cfb95af096d3c51c/scribejava-httpclient-okhttp/src/test/java/com/github/scribejava/httpclient/okhttp/OkHttpHttpClientTest.java/right.java
 
     @Override
     protected HttpClient createNewClient() {
