9ef7682e20fd6987e7b0cf86567cc87f87835b6f
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-22 10:19:36 +0000

    Mining Framework Analysis

--- src/main/java/org/dasein/cloud/AbstractCloud.java
+++ src/main/java/org/dasein/cloud/AbstractCloud.java
@@ -1,24 +1,4 @@
-/**
- * Copyright (C) 2009-2014 Dell, Inc.
- * See annotations for authorship information
- *
- * ====================================================================
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * ====================================================================
- */
-
 package org.dasein.cloud;
-
 import org.dasein.cloud.admin.AdminServices;
 import org.dasein.cloud.ci.CIServices;
 import org.dasein.cloud.compute.ComputeServices;
@@ -26,7 +6,6 @@ import org.dasein.cloud.identity.IdentityServices;
 import org.dasein.cloud.network.NetworkServices;
 import org.dasein.cloud.platform.PlatformServices;
 import org.dasein.cloud.quotas.QuotaServices;
-
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
@@ -37,63 +16,47 @@ import javax.annotation.Nullable;
  * @since unknown
  */
 public abstract class AbstractCloud extends CloudProvider {
-    /**
+  /**
      * Constructs a cloud provider instance.
      */
-    public AbstractCloud() { }
+  public AbstractCloud() {
+  }
 
-    @Override
-    public @Nullable AdminServices getAdminServices() {
-        return null;
-    }
-    
-    @Override
-    public @Nullable ComputeServices getComputeServices() {
-        CloudProvider compute = getComputeCloud();
-        
-        return (compute == null ? null : compute.getComputeServices());
-    }
+  @Override public @Nullable AdminServices getAdminServices() {
+    return null;
+  }
 
-    @Override
-    public @Nonnull ContextRequirements getContextRequirements() {
-        return new ContextRequirements(
-                new ContextRequirements.Field("apiKeys", ContextRequirements.FieldType.KEYPAIR),
-                new ContextRequirements.Field("x509", ContextRequirements.FieldType.KEYPAIR, false)
-        );
-    }
+  @Override public @Nullable ComputeServices getComputeServices() {
+    CloudProvider compute = getComputeCloud();
+    return (compute == null ? null : compute.getComputeServices());
+  }
 
-    @Override
-    public @Nullable CIServices getCIServices() {
-        CloudProvider compute = getComputeCloud();
+  @Override public @Nonnull ContextRequirements getContextRequirements() {
+    return new ContextRequirements(new ContextRequirements.Field("apiKeys", ContextRequirements.FieldType.KEYPAIR), new ContextRequirements.Field("x509", ContextRequirements.FieldType.KEYPAIR, false));
+  }
 
-        return (compute == null ? null : compute.getCIServices());
-    }
-    
-    @Override
-    public @Nullable IdentityServices getIdentityServices() {
-        CloudProvider compute = getComputeCloud();
-        
-        return (compute == null ? null : compute.getIdentityServices());
-    }
+  @Override public @Nullable CIServices getCIServices() {
+    CloudProvider compute = getComputeCloud();
+    return (compute == null ? null : compute.getCIServices());
+  }
 
-    @Override
-    public @Nullable NetworkServices getNetworkServices() {
-        CloudProvider compute = getComputeCloud();
+  @Override public @Nullable IdentityServices getIdentityServices() {
+    CloudProvider compute = getComputeCloud();
+    return (compute == null ? null : compute.getIdentityServices());
+  }
 
-        return (compute == null ? null : compute.getNetworkServices());
-    }
+  @Override public @Nullable NetworkServices getNetworkServices() {
+    CloudProvider compute = getComputeCloud();
+    return (compute == null ? null : compute.getNetworkServices());
+  }
 
-    @Override
-    public @Nullable PlatformServices getPlatformServices() {
-        CloudProvider compute = getComputeCloud();
+  @Override public @Nullable PlatformServices getPlatformServices() {
+    CloudProvider compute = getComputeCloud();
+    return (compute == null ? null : compute.getPlatformServices());
+  }
 
-        return ( compute == null ? null : compute.getPlatformServices() );
-    }
-
-    @Override
-    public @Nullable QuotaServices getQuotaServices() {
-        CloudProvider compute = getComputeCloud();
-
-        return (compute == null ? null : compute.getQuotaServices());
-    }
+  @Override public @Nullable QuotaServices getQuotaServices() {
+    CloudProvider compute = getComputeCloud();
+    return (compute == null ? null : compute.getQuotaServices());
+  }
 }
