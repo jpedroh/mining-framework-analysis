8ec2729294c89e6cc63968e8e90b1d9cb137e60f
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-22 10:46:51 +0000

    Mining Framework Analysis

--- scribejava-apis/src/main/java/com/github/scribejava/apis/service/HHOAuthServiceImpl.java
+++ scribejava-apis/src/main/java/com/github/scribejava/apis/service/HHOAuthServiceImpl.java
@@ -1,20 +1,6 @@
-package com.github.scribejava.apis.service;
-
-import com.github.scribejava.core.builder.api.DefaultApi20;
-import com.github.scribejava.core.model.AbstractRequest;
-import com.github.scribejava.core.model.AccessToken;
-import com.github.scribejava.core.model.OAuthConfig;
-import com.github.scribejava.core.model.Token;
-import com.github.scribejava.core.oauth.OAuth20Service;
-
-public class HHOAuthServiceImpl extends OAuth20Service {
-
-    public HHOAuthServiceImpl(final DefaultApi20 api, final OAuthConfig config) {
-        super(api, config);
-    }
-
-    @Override
-    public void signRequest(final AccessToken accessToken, final AbstractRequest request) {
-        request.addHeader("Authorization", "Bearer " + accessToken.getToken());
-    }
-}
+  package     com . github . scribejava . apis . service ;   import       com . github . scribejava . core . builder . api . DefaultApi20 ;  import      com . github . scribejava . core . model . AbstractRequest ;  import      com . github . scribejava . core . model . AccessToken ;  import      com . github . scribejava . core . model . OAuthConfig ;  import      com . github . scribejava . core . model . Token ;  import      com . github . scribejava . core . oauth . OAuth20Service ;   public class HHOAuthServiceImpl  extends OAuth20Service  {   public HHOAuthServiceImpl  (   final DefaultApi20 api ,   final OAuthConfig config )  {  super  ( api , config ) ; }    @ Override public void signRequest  (  AccessToken accessToken ,  AbstractRequest request )  {   request . addHeader  ( "Authorization" ,  "Bearer " +  accessToken . getToken  ( ) ) ; } 
+<<<<<<<
+=======
+   @ Override public void signRequest  (   final Token accessToken ,   final AbstractRequest request )  {   request . addHeader  ( "Authorization" ,  "Bearer " +  accessToken . getToken  ( ) ) ; }
+>>>>>>>
+ }
--- scribejava-apis/src/main/java/com/github/scribejava/apis/service/TutByOAuthServiceImpl.java
+++ scribejava-apis/src/main/java/com/github/scribejava/apis/service/TutByOAuthServiceImpl.java
@@ -1,22 +1,6 @@
-package com.github.scribejava.apis.service;
-
-import com.github.scribejava.core.builder.api.DefaultApi20;
-import com.github.scribejava.core.model.AbstractRequest;
-import com.github.scribejava.core.model.AccessToken;
-import com.github.scribejava.core.model.OAuthConfig;
-import com.github.scribejava.core.model.OAuthConstants;
-import com.github.scribejava.core.model.Token;
-import com.github.scribejava.core.oauth.OAuth20Service;
-
-public class TutByOAuthServiceImpl extends OAuth20Service {
-
-    public TutByOAuthServiceImpl(final DefaultApi20 api, final OAuthConfig config) {
-        super(api, config);
-    }
-
-    @Override
-    public void signRequest(final AccessToken accessToken, final AbstractRequest request) {
-        request.addQuerystringParameter(OAuthConstants.TOKEN, accessToken.getToken());
-    }
-
-}
+  package     com . github . scribejava . apis . service ;   import       com . github . scribejava . core . builder . api . DefaultApi20 ;  import      com . github . scribejava . core . model . AbstractRequest ;  import      com . github . scribejava . core . model . AccessToken ;  import      com . github . scribejava . core . model . OAuthConfig ;  import      com . github . scribejava . core . model . OAuthConstants ;  import      com . github . scribejava . core . model . Token ;  import      com . github . scribejava . core . oauth . OAuth20Service ;   public class TutByOAuthServiceImpl  extends OAuth20Service  {   public TutByOAuthServiceImpl  (   final DefaultApi20 api ,   final OAuthConfig config )  {  super  ( api , config ) ; }    @ Override public void signRequest  (  AccessToken accessToken ,  AbstractRequest request )  {   request . addQuerystringParameter  (  OAuthConstants . TOKEN ,  accessToken . getToken  ( ) ) ; } 
+<<<<<<<
+=======
+   @ Override public void signRequest  (   final Token accessToken ,   final AbstractRequest request )  {   request . addQuerystringParameter  (  OAuthConstants . TOKEN ,  accessToken . getToken  ( ) ) ; }
+>>>>>>>
+ }
