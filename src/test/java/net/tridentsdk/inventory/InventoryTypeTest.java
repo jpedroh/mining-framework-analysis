eeef1e2c378f78cbd69284e4e34d850079fd1718
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-22 00:47:33 +0000

    Mining Framework Analysis

--- src/test/java/net/tridentsdk/inventory/InventoryTypeTest.java
+++ src/test/java/net/tridentsdk/inventory/InventoryTypeTest.java
@@ -1,45 +1,91 @@
-/*
- * Trident - A Multithreaded Server Alternative
- * Copyright 2017 The TridentSDK Team
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package net.tridentsdk.inventory;
-
-import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
-
-public class InventoryTypeTest {
-    @Test
-    public void testTypes() {
-        String types = "Player\n" +
-                "minecraft:container\n" +
-                "minecraft:chest\n" +
-                "minecraft:crafting_table\n" +
-                "minecraft:furnace\n" +
-                "minecraft:dispenser\n" +
-                "minecraft:enchanting_table\n" +
-                "minecraft:brewing_stand\n" +
-                "minecraft:villager\n" +
-                "minecraft:beacon\n" +
-                "minecraft:anvil\n" +
-                "minecraft:hopper\n" +
-                "minecraft:dropper\n" +
-                "minecraft:shulker_box\n" +
-                "EntityHorse";
-        for (InventoryType type : InventoryType.values()) {
-            assertEquals(types.split("\n")[type.ordinal()], type.toString());
-        }
-    }
-}
+  package   net . tridentsdk . inventory ;   import   org . junit . Test ;  import static    org . junit . Assert . assertEquals ;   public class InventoryTypeTest  {    @ Test public void testTypes  ( )  {  String  types =               
+<<<<<<<
+"minecraft:container\n"
+=======
+"Player\n"
+>>>>>>>
+ + 
+<<<<<<<
+"minecraft:chest\n"
+=======
+"minecraft:container\n"
+>>>>>>>
+ + 
+<<<<<<<
+"minecraft:crafting_table\n"
+=======
+"minecraft:chest\n"
+>>>>>>>
+ + 
+<<<<<<<
+"minecraft:furnace\n"
+=======
+"minecraft:crafting_table\n"
+>>>>>>>
+ + 
+<<<<<<<
+"minecraft:dispenser\n"
+=======
+"minecraft:furnace\n"
+>>>>>>>
+ + 
+<<<<<<<
+"minecraft:enchanting_table\n"
+=======
+"minecraft:dispenser\n"
+>>>>>>>
+ + 
+<<<<<<<
+"minecraft:brewing_stand\n"
+=======
+"minecraft:enchanting_table\n"
+>>>>>>>
+ + 
+<<<<<<<
+"minecraft:villager\n"
+=======
+"minecraft:brewing_stand\n"
+>>>>>>>
+ + 
+<<<<<<<
+"minecraft:beacon\n"
+=======
+"minecraft:villager\n"
+>>>>>>>
+ + 
+<<<<<<<
+"minecraft:anvil\n"
+=======
+"minecraft:beacon\n"
+>>>>>>>
+ + 
+<<<<<<<
+"minecraft:hopper\n"
+=======
+"minecraft:anvil\n"
+>>>>>>>
+ + 
+<<<<<<<
+"minecraft:dropper\n"
+=======
+"minecraft:hopper\n"
+>>>>>>>
+ + 
+<<<<<<<
+"minecraft:shulker_box\n"
+=======
+"minecraft:dropper\n"
+>>>>>>>
+ + 
+<<<<<<<
+"EntityHorse\n"
+=======
+"minecraft:shulker_box\n"
+>>>>>>>
+ + 
+<<<<<<<
+"player"
+=======
+"EntityHorse"
+>>>>>>>
+ ;  for ( InventoryType type :  InventoryType . values  ( ) )  {   assertEquals  (   types . split  ( "\n" ) [  type . ordinal  ( ) ] ,  type . toString  ( ) ) ; } } }
