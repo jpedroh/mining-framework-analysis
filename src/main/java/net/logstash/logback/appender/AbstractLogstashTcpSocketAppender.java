64086e5193191b07a4decb818f1009a66f663e4b
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-22 05:22:37 +0000

    Mining Framework Analysis

--- src/main/java/net/logstash/logback/appender/AbstractLogstashTcpSocketAppender.java
+++ src/main/java/net/logstash/logback/appender/AbstractLogstashTcpSocketAppender.java
@@ -928,7 +928,7 @@ public abstract class AbstractLogstashTcpSocketAppender<Event extends DeferredPr
      * The charset to use when writing the {@link #keepAliveMessage}.
      * Defaults to UTF-8.
      */
-    public void setKeepAliveCharset(Charset keepAliveCharset) {
+   public void setKeepAliveCharset(Charset keepAliveCharset) {
         this.keepAliveCharset = keepAliveCharset;
     }
 }
