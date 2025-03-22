affb13ad66768c920b8bf9a46ba0964e5f500149
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-22 00:12:08 +0000

    Mining Framework Analysis

--- yoga-integration/yoga-jaxrs/src/main/java/org/skyscreamer/yoga/jaxrs/view/AbstractSelectorMessageBodyWriter.java
+++ yoga-integration/yoga-jaxrs/src/main/java/org/skyscreamer/yoga/jaxrs/view/AbstractSelectorMessageBodyWriter.java
@@ -1,10 +1,8 @@
 package org.skyscreamer.yoga.jaxrs.view;
-
 import java.io.IOException;
 import java.io.OutputStream;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.WebApplicationException;
@@ -13,7 +11,6 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.MessageBodyWriter;
-
 import org.skyscreamer.yoga.listener.RenderingListenerRegistry;
 import org.skyscreamer.yoga.mapper.ResultTraverser;
 import org.skyscreamer.yoga.selector.CoreSelector;
@@ -22,74 +19,51 @@ import org.skyscreamer.yoga.util.ClassFinderStrategy;
 import org.skyscreamer.yoga.view.AbstractYogaView;
 import org.springframework.beans.factory.annotation.Autowired;
 
-public abstract class AbstractSelectorMessageBodyWriter implements MessageBodyWriter<Object>
-{
-    protected ResultTraverser _resultTraverser = new ResultTraverser();
+public abstract class AbstractSelectorMessageBodyWriter implements MessageBodyWriter<Object> {
+  protected ResultTraverser _resultTraverser = new ResultTraverser();
 
-    @Autowired
-    protected SelectorParser _selectorParser;
+  @Autowired protected SelectorParser _selectorParser;
 
-    @Autowired
-    protected RenderingListenerRegistry _renderingListenerRegistry = new RenderingListenerRegistry();
+  @Autowired protected RenderingListenerRegistry _renderingListenerRegistry = new RenderingListenerRegistry();
 
-    @Autowired
-    protected CoreSelector _selector;
-    
-    @Context
-    protected HttpServletRequest _request;
+  @Autowired protected CoreSelector _selector;
 
-    @Context
-    protected HttpServletResponse _response;
+  @Context protected HttpServletRequest _request;
 
-	protected ClassFinderStrategy _classFinderStrategy;
+  @Context protected HttpServletResponse _response;
 
-    @Autowired
-    public void setClassFinderStrategy( ClassFinderStrategy classFinderStrategy )
-    {
-        this._classFinderStrategy = classFinderStrategy;
-        _resultTraverser.setClassFinderStrategy( classFinderStrategy );
+  protected ClassFinderStrategy _classFinderStrategy;
+
+  @Autowired public void setClassFinderStrategy(ClassFinderStrategy classFinderStrategy) {
+    this._classFinderStrategy = classFinderStrategy;
+    _resultTraverser.setClassFinderStrategy(classFinderStrategy);
+  }
+
+  @Override public long getSize(Object arg0, Class<?> arg1, Type arg2, Annotation[] arg3, MediaType arg4) {
+    return -1;
+  }
+
+  @Override public boolean isWriteable(Class<?> arg0, Type arg1, Annotation[] arg2, MediaType arg3) {
+    return true;
+  }
+
+  @Override public void writeTo(Object t, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType, MultivaluedMap<String, Object> httpHeaders, OutputStream entityStream) throws IOException, WebApplicationException {
+    try {
+      AbstractYogaView view = getView();
+      view.setResultTraverser(_resultTraverser);
+      view.setSelectorParser(_selectorParser);
+      view.setSelector(_selector);
+      view.setRegistry(_renderingListenerRegistry);
+      view.render(_request, _response, t, entityStream);
+      if (_classFinderStrategy != null) {
+        view.setClassFinderStrategy(_classFinderStrategy);
+      }
+    } catch (RuntimeException e) {
+      throw e;
+    } catch (Exception e) {
+      throw new WebApplicationException(e, Response.Status.BAD_REQUEST);
     }
+  }
 
-    
-    @Override
-    public long getSize( Object arg0, Class<?> arg1, Type arg2, Annotation[] arg3, MediaType arg4 )
-    {
-        return -1;
-    }
-
-    @Override
-    public boolean isWriteable( Class<?> arg0, Type arg1, Annotation[] arg2, MediaType arg3 )
-    {
-        return true;
-    }
-
-    @Override
-    public void writeTo( Object t, Class<?> type, Type genericType, Annotation[] annotations,
-            MediaType mediaType, MultivaluedMap<String, Object> httpHeaders,
-            OutputStream entityStream ) throws IOException, WebApplicationException
-    {
-        try
-        {
-            AbstractYogaView view = getView();
-            view.setResultTraverser( _resultTraverser );
-            view.setSelectorParser( _selectorParser );
-            view.setSelector( _selector );
-            view.setRegistry( _renderingListenerRegistry );
-            view.render( _request, _response, t, entityStream );
-            if ( _classFinderStrategy != null )
-            {
-                view.setClassFinderStrategy( _classFinderStrategy );
-            }
-        }
-        catch (RuntimeException e)
-        {
-            throw e;
-        }
-        catch (Exception e)
-        {
-            throw new WebApplicationException( e, Response.Status.BAD_REQUEST );
-        }
-    }
-
-    protected abstract AbstractYogaView getView();
+  protected abstract AbstractYogaView getView();
 }
