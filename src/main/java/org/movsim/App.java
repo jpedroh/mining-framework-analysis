8f3b0bd03d2cad72a383ef20e410af2318826165
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-22 02:29:07 +0000

    Mining Framework Analysis

--- src/main/java/org/movsim/App.java
+++ src/main/java/org/movsim/App.java
@@ -1,35 +1,7 @@
-/**
- * Copyright (C) 2010, 2011 by Arne Kesting, Martin Treiber,
- *                             Ralph Germ, Martin Budden
- *                             <info@movsim.org>
- * ----------------------------------------------------------------------
- * 
- *  This file is part of 
- *  
- *  MovSim - the multi-model open-source vehicular-traffic simulator 
- *
- *  MovSim is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  MovSim is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with MovSim.  If not, see <http://www.gnu.org/licenses/> or
- *  <http://www.movsim.org>.
- *  
- * ----------------------------------------------------------------------
- */
 package org.movsim;
-
 import java.io.File;
 import java.net.URL;
 import java.util.Locale;
-
 import org.apache.log4j.PropertyConfigurator;
 import org.movsim.input.commandline.SimCommandLine;
 import org.movsim.input.commandline.impl.SimCommandLineImpl;
@@ -47,78 +19,60 @@ import org.slf4j.LoggerFactory;
  * The Class App.
  */
 public class App {
+  /** The Constant logger. */
+  final static Logger logger = LoggerFactory.getLogger(App.class);
 
-    // Define a static logger variable
-    // Logging with slf4j, a facade for log4j
-    /** The Constant logger. */
-    final static Logger logger = LoggerFactory.getLogger(App.class);
+  /** The Constant xmlDefault. */
+  final static String xmlDefault = "sim/onramp_IDM.xml";
 
-    /** The Constant xmlDefault. */
-    final static String xmlDefault = "sim/onramp_IDM.xml";
-
-    /**
+  /**
      * The main method.
      * 
      * @param args
      *            the arguments
      */
-    public static void main(String[] args) {
+  public static void main(String[] args) {
+    final App universalprogram = new App();
+    universalprogram.runAsAplication(args);
+  }
 
-        final App universalprogram = new App();
-        universalprogram.runAsAplication(args);
-
-    }
-
-    /**
+  /**
      * Run as aplication.
      * 
      * @param args
      *            the args
      */
-    private void runAsAplication(String[] args) {
-
-        initLocalizationAndLogger();
-
-        // CommandLine args options Parser
-        String xmlFilename;
-        final SimCommandLine cmdline = new SimCommandLineImpl(args);
-        if (cmdline.isWithXmlSimulationConfigFile()) {
-            xmlFilename = cmdline.getSimulationFilename();
-        } else {
-            xmlFilename = xmlDefault;
-        }
-
-        final InputDataImpl inputData = new InputDataImpl();
-
-        // parse xmlFile and set values
-        final XmlReaderSimInput xmlReader = new XmlReaderSimInput(xmlFilename, cmdline, inputData);
-
-        final Simulator simulator = new SimulatorImpl(cmdline.isGui(), inputData);
-        
-        // TODO raus für release
-        if (cmdline.isGui()) {
-            Controller controller = new SimulatorGUIController(simulator);
-        } else {
-            // commandline tool
-            final Controller controller = new SimulatorController(simulator); 
-        }
-
+  private void runAsAplication(String[] args) {
+    initLocalizationAndLogger();
+    String xmlFilename;
+    final SimCommandLine cmdline = new SimCommandLineImpl(args);
+    if (cmdline.isWithXmlSimulationConfigFile()) {
+      xmlFilename = cmdline.getSimulationFilename();
+    } else {
+      xmlFilename = xmlDefault;
     }
+    final InputDataImpl inputData = new InputDataImpl();
+    final XmlReaderSimInput xmlReader = new XmlReaderSimInput(xmlFilename, cmdline, inputData);
+    final Simulator simulator = new SimulatorImpl(cmdline.isGui(), inputData);
+    if (cmdline.isGui()) {
+      Controller controller = new SimulatorGUIController(simulator);
+    } else {
+      final Controller controller = new SimulatorController(simulator);
+    }
+  }
 
-    /**
+  /**
      * Inits the localization and logger.
      */
-    private static void initLocalizationAndLogger() {
-        Locale.setDefault(Locale.US);
-            final File file = new File("log4j.properties");
-            if (file.exists() && file.isFile()) {
-            PropertyConfigurator.configure("log4j.properties");
-        } else {
-            final URL log4jConfig = App.class.getResource("/sim/log4j.properties");
-            PropertyConfigurator.configure(log4jConfig);
-        }
-        // Log Levels: DEBUG < INFO < WARN < ERROR
-        logger.info("Copyright '\u00A9' by Arne Kesting, Martin Treiber, Ralph Germ and  Martin Budden (2010, 2011)");
+  private static void initLocalizationAndLogger() {
+    Locale.setDefault(Locale.US);
+    final File file = new File("log4j.properties");
+    if (file.exists() && file.isFile()) {
+      PropertyConfigurator.configure("log4j.properties");
+    } else {
+      final URL log4jConfig = App.class.getResource("/sim/log4j.properties");
+      PropertyConfigurator.configure(log4jConfig);
     }
-
+    logger.info("Copyright \'\u00a9\' by Arne Kesting, Martin Treiber, Ralph Germ and  Martin Budden (2010, 2011)");
+  }
 }
--- src/main/java/org/movsim/input/impl/XmlReaderSimInput.java
+++ src/main/java/org/movsim/input/impl/XmlReaderSimInput.java
@@ -1,39 +1,10 @@
-/**
- * Copyright (C) 2010, 2011 by Arne Kesting, Martin Treiber,
- *                             Ralph Germ, Martin Budden
- *                             <info@movsim.org>
- * ----------------------------------------------------------------------
- * 
- *  This file is part of 
- *  
- *  MovSim - the multi-model open-source vehicular-traffic simulator 
- *
- *  MovSim is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  MovSim is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with MovSim.  If not, see <http://www.gnu.org/licenses/> or
- *  <http://www.movsim.org>.
- *  
- * ----------------------------------------------------------------------
- */
 package org.movsim.input.impl;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.management.MXBean;
-
 import org.jdom.Document;
 import org.jdom.Element;
 import org.jdom.JDOMException;
@@ -57,38 +28,35 @@ import org.xml.sax.XMLReader;
 import org.xml.sax.helpers.DefaultHandler;
 import org.xml.sax.helpers.XMLReaderFactory;
 
-
-// TODO: Auto-generated Javadoc
 /**
  * The Class XmlReaderSimInput.
  * 
  * @author Arne Kesting, Ralph Germ
  */
 public class XmlReaderSimInput {
+  /** The Constant logger. */
+  final static Logger logger = LoggerFactory.getLogger(XmlReaderSimInput.class);
 
-    /** The Constant logger. */
-    final static Logger logger = LoggerFactory.getLogger(XmlReaderSimInput.class);
+  /** The is valid. */
+  private boolean isValid;
 
-    /** The is valid. */
-    private boolean isValid;
+  /** The input data. */
+  private final InputDataImpl inputData;
 
-    /** The input data. */
-    private final InputDataImpl inputData;
+  /** The xml filename. */
+  private final String xmlFilename;
 
-    /** The xml filename. */
-    private final String xmlFilename;
+  /** The xml filename. */
+  private final String filenameEnding = ".xml";
 
-    /** The xml filename. */
-    private final String filenameEnding = ".xml";
+  /** The doc. */
+  private Document doc;
 
-    /** The doc. */
-    private Document doc;
+  private SimCommandLine cmdline;
 
-    private SimCommandLine cmdline;
+  private String dtdFilename = "/sim/multiModelTrafficSimulatorInput.dtd";
 
-    private String dtdFilename = "/sim/multiModelTrafficSimulatorInput.dtd";
-
-    /**
+  /**
      * Instantiates a new xml reader to parse and validate the simulation input.
      * 
      * @param xmlFilename
@@ -97,223 +65,167 @@ public class XmlReaderSimInput {
      * @param inputData
      *            the input data
      */
-    public XmlReaderSimInput(String xmlFilename, SimCommandLine cmdline, InputDataImpl inputData) {
-        this.xmlFilename = xmlFilename;
-        this.cmdline = cmdline;
-        this.inputData = inputData;
-
-        if (!FileUtils.fileExists(xmlFilename)) {
-            logger.error("XML File does not exist. Exit Simulation.");
-            System.exit(1);
-        }
-
-        logger.info("Begin parsing: " + xmlFilename);
-        readAndValidateXml();
-
-        // write internal xml file:
-        if (cmdline.isWriteInternalXml()) {
-            String outFilename = xmlFilename + ".internal_xml";
-            writeInternalXmlToFile(doc, outFilename);
-            logger.info("internal xml output written to file {}. Exit.", outFilename);
-            System.exit(0);
-        }
-
-        fromDomToInternalDatastructure();
-        logger.info("End XmlReaderSimInput.");
+  public XmlReaderSimInput(String xmlFilename, SimCommandLine cmdline, InputDataImpl inputData) {
+    this.xmlFilename = xmlFilename;
+    this.cmdline = cmdline;
+    this.inputData = inputData;
+    if (!FileUtils.fileExists(xmlFilename)) {
+      logger.error("XML File does not exist. Exit Simulation.");
+      System.exit(1);
     }
+    logger.info("Begin parsing: " + xmlFilename);
+    readAndValidateXml();
+    if (cmdline.isWriteInternalXml()) {
+      String outFilename = xmlFilename + ".internal_xml";
+      writeInternalXmlToFile(doc, outFilename);
+      logger.info("internal xml output written to file {}. Exit.", outFilename);
+      System.exit(0);
+    }
+    fromDomToInternalDatastructure();
+    logger.info("End XmlReaderSimInput.");
+  }
 
-    /**
+  /**
      * Writes the internal xml after validation to file
      * 
      * @param doc2
      * @param outFilename
      *            the output file name
      */
-    private void writeInternalXmlToFile(Document doc2, String outFilename) {
-        PrintWriter writer = FileUtils.getWriter(outFilename);
-        XMLOutputter outputter = new XMLOutputter();
-        outputter.setFormat(Format.getPrettyFormat());
-        try {
-            logger.info("  write internal xml after validation to file \"" + outFilename + "\"");
-            outputter.output(doc, writer);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+  private void writeInternalXmlToFile(Document doc2, String outFilename) {
+    PrintWriter writer = FileUtils.getWriter(outFilename);
+    XMLOutputter outputter = new XMLOutputter();
+    outputter.setFormat(Format.getPrettyFormat());
+    try {
+      logger.info("  write internal xml after validation to file \"" + outFilename + "\"");
+      outputter.output(doc, writer);
+    } catch (Exception e) {
+      e.printStackTrace();
     }
+  }
 
-    /**
+  /**
      * From dom to internal data structure.
      */
-    @SuppressWarnings("unchecked")
-    private void fromDomToInternalDatastructure() {
-
-        inputData.setProjectName(xmlFilename.substring(0, xmlFilename.indexOf(filenameEnding)));
-
-        final Element root = doc.getRootElement();
-
-        // -------------------------------------------------------
-
-        final List<VehicleInput> vehicleInputData = new ArrayList<VehicleInput>();
-
-        final List<Element> vehicleElements = root.getChild(XmlElementNames.DriverVehicleUnits).getChildren();
-        for (final Element vehElem : vehicleElements) {
-            vehicleInputData.add(new VehicleInputImpl(vehElem));
-        }
-        inputData.setVehicleInputData(vehicleInputData);
-
-        // -------------------------------------------------------
-
-        final SimulationInput simInput = new SimulationInputImpl(root.getChild(XmlElementNames.Simulation));
-        inputData.setSimulationInput(simInput);
-        
-       
-        
+  @SuppressWarnings(value = { "unchecked" }) private void fromDomToInternalDatastructure() {
+    inputData.setProjectName(xmlFilename.substring(0, xmlFilename.indexOf(filenameEnding)));
+    final Element root = doc.getRootElement();
+    final List<VehicleInput> vehicleInputData = new ArrayList<VehicleInput>();
+    final List<Element> vehicleElements = root.getChild(XmlElementNames.DriverVehicleUnits).getChildren();
+    for (final Element vehElem : vehicleElements) {
+      vehicleInputData.add(new VehicleInputImpl(vehElem));
     }
+    inputData.setVehicleInputData(vehicleInputData);
+    final SimulationInput simInput = new SimulationInputImpl(root.getChild(XmlElementNames.Simulation));
+    inputData.setSimulationInput(simInput);
+  }
 
-    /**
+  /**
      * Read and validate xml.
      */
-    private void readAndValidateXml() {
-        validate(FileUtils.getInputSourceFromFilename(xmlFilename));
-        doc = getDocument(FileUtils.getInputSourceFromFilename(xmlFilename));
-    }
+  private void readAndValidateXml() {
+    validate(FileUtils.getInputSourceFromFilename(xmlFilename));
+    doc = getDocument(FileUtils.getInputSourceFromFilename(xmlFilename));
+  }
 
-    /**
+  /**
      * Gets the Document.
      * 
      * @param inputSource
      *            the input source
      * @return the document
      */
-    private Document getDocument(final InputSource inputSource) {
-        try {
-            final SAXBuilder builder = new SAXBuilder();
-            builder.setIgnoringElementContentWhitespace(true);
-            builder.setEntityResolver(new EntityResolver() {
-                
-                @Override
-                public InputSource resolveEntity(String publicId, String systemId) throws SAXException, IOException {
-                    InputStream is = XmlReaderSimInput.class.getResourceAsStream(dtdFilename);
-                    InputSource input = new InputSource(is);
-                    return input;
-                }
-            });
-            final Document doc = builder.build(inputSource);
-            return doc;
-        } catch (final JDOMException e) {
-            e.printStackTrace();
-            System.exit(-1);
-            return null;
-        } catch (final Exception e) {
-            e.printStackTrace();
-            return null;
+  private Document getDocument(final InputSource inputSource) {
+    try {
+      final SAXBuilder builder = new SAXBuilder();
+      builder.setIgnoringElementContentWhitespace(true);
+      builder.setEntityResolver(new EntityResolver() {
+        @Override public InputSource resolveEntity(String publicId, String systemId) throws SAXException, IOException {
+          InputStream is = XmlReaderSimInput.class.getResourceAsStream(dtdFilename);
+          InputSource input = new InputSource(is);
+          return input;
         }
+      });
+      final Document doc = builder.build(inputSource);
+      return doc;
+    } catch (final JDOMException e) {
+      e.printStackTrace();
+      System.exit(-1);
+      return null;
+    } catch (final Exception e) {
+      e.printStackTrace();
+      return null;
     }
+  }
 
-    /**
+  /**
      * Validates the Inputsource.
      * 
      * @param inputSource
      *            the input source
      */
-    private void validate(InputSource inputSource) {
-        // global flag !!! also used in errorHandler
-        isValid = true;
-        try {
-            logger.debug("validate input ... ");
-            final XMLReader myXMLReader = XMLReaderFactory.createXMLReader();
-            myXMLReader.setFeature("http://xml.org/sax/features/validation", true);
-            final DefaultHandler handler = new MyErrorHandler();
-            myXMLReader.setErrorHandler(handler);
-            
-            //overriding dtd source from xml file with internal dtd from jar
-            myXMLReader.setEntityResolver(new EntityResolver() {
-                
-                @Override
-                public InputSource resolveEntity(String publicId, String systemId) throws SAXException, IOException {
-                    InputStream is = XmlReaderSimInput.class.getResourceAsStream(dtdFilename);
-                    InputSource input = new InputSource(is);
-                    return input;
-                }
-            });
-            
-            myXMLReader.parse(inputSource);
-        } catch (final SAXException e) {
-            isValid = false;
-        } catch (final IOException e) {
-            isValid = false;
+  private void validate(InputSource inputSource) {
+    isValid = true;
+    try {
+      logger.debug("validate input ... ");
+      final XMLReader myXMLReader = XMLReaderFactory.createXMLReader();
+      myXMLReader.setFeature("http://xml.org/sax/features/validation", true);
+      final DefaultHandler handler = new MyErrorHandler();
+      myXMLReader.setErrorHandler(handler);
+      myXMLReader.setEntityResolver(new EntityResolver() {
+        @Override public InputSource resolveEntity(String publicId, String systemId) throws SAXException, IOException {
+          InputStream is = XmlReaderSimInput.class.getResourceAsStream(dtdFilename);
+          InputSource input = new InputSource(is);
+          return input;
         }
-        
-        if (!isValid) {
-            logger.error("xml input file {} is not well-formed or invalid ...Exit Simulation.", xmlFilename);
-            System.exit(0);
-        } else if (cmdline.isOnlyValidation()) {
-            logger.info("xml input file is well-formed and valid. Exit Simulation as requested.");
-            System.exit(0);
-        }
+      });
+      myXMLReader.parse(inputSource);
+    } catch (final SAXException e) {
+      isValid = false;
+    } catch (final IOException e) {
+      isValid = false;
+    }
+    if (!isValid) {
+      logger.error("xml input file {} is not well-formed or invalid ...Exit Simulation.", xmlFilename);
+      System.exit(0);
+    } else {
+      if (cmdline.isOnlyValidation()) {
+        logger.info("xml input file is well-formed and valid. Exit Simulation as requested.");
+        System.exit(0);
+      }
+    }
+  }
+
+  class MyErrorHandler extends DefaultHandler {
+    @Override public void warning(SAXParseException e) throws SAXException {
+      logger.warn(getInfo(e));
     }
 
+    @Override public void error(SAXParseException e) throws SAXException {
+      logger.error(getInfo(e));
+      isValid = false;
+    }
+
+    @Override public void fatalError(SAXParseException e) throws SAXException {
+      logger.error(getInfo(e));
+      isValid = false;
+    }
 
     /**
-     * The Inner Class MyErrorHandler.
-     * 
-     * uses global isValid flag
-     */
-    class MyErrorHandler extends DefaultHandler {
-
-        /*
-         * (non-Javadoc)
-         * 
-         * @see
-         * org.xml.sax.helpers.DefaultHandler#warning(org.xml.sax.SAXParseException
-         * )
-         */
-        @Override
-        public void warning(SAXParseException e) throws SAXException {
-            logger.warn(getInfo(e));
-        }
-
-        /*
-         * (non-Javadoc)
-         * 
-         * @see
-         * org.xml.sax.helpers.DefaultHandler#error(org.xml.sax.SAXParseException
-         * )
-         */
-        @Override
-        public void error(SAXParseException e) throws SAXException {
-            logger.error(getInfo(e));
-            isValid = false;
-        }
-
-        /*
-         * (non-Javadoc)
-         * 
-         * @see org.xml.sax.helpers.DefaultHandler#fatalError(org.xml.sax.
-         * SAXParseException)
-         */
-        @Override
-        public void fatalError(SAXParseException e) throws SAXException {
-            logger.error(getInfo(e));
-            isValid = false;
-        }
-
-        /**
          * Gets the info to the corresponding exception.
          * 
          * @param e
          *            the exception
          * @return the info
          */
-        private String getInfo(SAXParseException e) {
-            final StringBuilder stringb = new StringBuilder();
-            stringb.append("   Public ID: " + e.getPublicId());
-            stringb.append("   System ID: " + e.getSystemId());
-            stringb.append("   Line number: " + e.getLineNumber());
-            stringb.append("   Column number: " + e.getColumnNumber());
-            stringb.append("   Message: " + e.getMessage());
-            return stringb.toString();
-        }
+    private String getInfo(SAXParseException e) {
+      final StringBuilder stringb = new StringBuilder();
+      stringb.append("   Public ID: " + e.getPublicId());
+      stringb.append("   System ID: " + e.getSystemId());
+      stringb.append("   Line number: " + e.getLineNumber());
+      stringb.append("   Column number: " + e.getColumnNumber());
+      stringb.append("   Message: " + e.getMessage());
+      return stringb.toString();
     }
-
+  }
 }
