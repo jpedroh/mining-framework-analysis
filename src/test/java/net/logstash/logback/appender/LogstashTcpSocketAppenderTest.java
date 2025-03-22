c334a0ff47d74ecdf0b4f9de63d5c20d93536fba
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-22 06:00:29 +0000

    Mining Framework Analysis

--- src/test/java/net/logstash/logback/appender/LogstashTcpSocketAppenderTest.java
+++ src/test/java/net/logstash/logback/appender/LogstashTcpSocketAppenderTest.java
@@ -30,7 +30,6 @@ import static org.mockito.Mockito.inOrder;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.reset;
-import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.timeout;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
