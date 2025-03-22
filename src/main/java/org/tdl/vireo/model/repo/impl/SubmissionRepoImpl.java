0375ec56349628b15636762bbd7014a6c806a1ac
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-22 10:49:12 +0000

    Mining Framework Analysis

--- src/main/java/org/tdl/vireo/model/repo/impl/SubmissionRepoImpl.java
+++ src/main/java/org/tdl/vireo/model/repo/impl/SubmissionRepoImpl.java
@@ -1021,8 +1021,8 @@ public class SubmissionRepoImpl extends AbstractWeaverRepoImpl<Submission, Submi
     private StringBuilder buildSubmissionDateFieldString(String column, String filter) {
         if (filter.contains("|")) {
             String[] dates = filter.split(Pattern.quote("|"));
-        return new StringBuilder()
-            .append("s.").append(column)
+            return new StringBuilder()
+                .append("s.").append(column)
                 .append(" BETWEEN CAST('").append(dates[0])
                 .append("' AS DATE) AND CAST('").append(dates[1])
                 .append("' AS DATE)");
