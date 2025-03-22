656715bb7063dbe8cbd4ea28b4dd1ce0064dbe09
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-22 01:02:48 +0000

    Mining Framework Analysis

--- core/src/main/java/org/movsim/output/route/ConsumptionOnRoute.java
+++ core/src/main/java/org/movsim/output/route/ConsumptionOnRoute.java
@@ -1,5 +1,4 @@
 package org.movsim.output.route;
-
 import org.movsim.autogen.ConsumptionCalculation;
 import org.movsim.simulator.roadnetwork.RoadNetwork;
 import org.movsim.simulator.roadnetwork.RoadNetworkUtils;
@@ -9,64 +8,54 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ConsumptionOnRoute extends OutputOnRouteBase {
+  /** The Constant LOG. */
+  final static Logger LOG = LoggerFactory.getLogger(ConsumptionOnRoute.class);
 
-    /** The Constant LOG. */
-    final static Logger LOG = LoggerFactory.getLogger(ConsumptionOnRoute.class);
+  private final double tauEMA;
 
-    private final double tauEMA;
+  private final double beta;
 
-    private final double beta;
+  private final FileConsumptionOnRoute fileWriter;
 
-    private final FileConsumptionOnRoute fileWriter;
+  private double instantaneousConsumptionRate;
 
-    private double instantaneousConsumptionRate;
+  private double instConsumptionEMA;
 
-    private double instConsumptionEMA;
+  private double totalConsumption;
 
-    private double totalConsumption; // TODO unit liters or more general approach
+  private int numberOfVehicles;
 
-    private int numberOfVehicles;
+  public ConsumptionOnRoute(double simulationTimestep, ConsumptionCalculation fuelRouteInput, RoadNetwork roadNetwork, Route route, boolean writeOutput) {
+    super(roadNetwork, route);
+    this.tauEMA = fuelRouteInput.getTauEMA();
+    this.beta = Math.exp(-simulationTimestep / tauEMA);
+    fileWriter = (writeOutput) ? new FileConsumptionOnRoute(fuelRouteInput, route) : null;
+    totalConsumption = 0;
+  }
 
-    public ConsumptionOnRoute(double simulationTimestep, ConsumptionCalculation fuelRouteInput,
-            RoadNetwork roadNetwork, Route route, boolean writeOutput) {
-        super(roadNetwork, route);
-        this.tauEMA = fuelRouteInput.getTauEMA();
-        this.beta = Math.exp(-simulationTimestep / tauEMA);
-        fileWriter = (writeOutput) ? new FileConsumptionOnRoute(fuelRouteInput, route) : null;
-        totalConsumption = 0;
+  @Override public void timeStep(double dt, double simulationTime, long iterationCount) {
+    instantaneousConsumptionRate = RoadNetworkUtils.instantaneousFuelUsedLiters(route);
+    totalConsumption += instantaneousConsumptionRate * dt;
+    numberOfVehicles = RoadNetworkUtils.vehicleCount(route);
+    instConsumptionEMA = (simulationTime == 0) ? instantaneousConsumptionRate : ExponentialMovingAverage.calc(instantaneousConsumptionRate, instConsumptionEMA, beta);
+    if (fileWriter != null) {
+      fileWriter.write(simulationTime, this);
     }
+  }
 
-    @Override
-    public void timeStep(double dt, double simulationTime, long iterationCount) {
+  public double getInstantaneousConsumptionRate() {
+    return instantaneousConsumptionRate;
+  }
 
-        instantaneousConsumptionRate = RoadNetworkUtils.instantaneousFuelUsedLiters(route);
-        totalConsumption += instantaneousConsumptionRate * dt;
+  public double getInstantaneousConsumptionEMA() {
+    return instConsumptionEMA;
+  }
 
-        numberOfVehicles = RoadNetworkUtils.vehicleCount(route);
+  public double getTotalConsumption() {
+    return totalConsumption;
+  }
 
-        instConsumptionEMA = (simulationTime == 0) ? instantaneousConsumptionRate : ExponentialMovingAverage.calc(
-                instantaneousConsumptionRate, instConsumptionEMA, beta);
-
-        if (fileWriter != null) {
-            fileWriter.write(simulationTime, this);
-        }
-
-    }
-
-    public double getInstantaneousConsumptionRate() {
-        return instantaneousConsumptionRate;
-    }
-
-    public double getInstantaneousConsumptionEMA() {
-        return instConsumptionEMA;
-    }
-
-    public double getTotalConsumption() {
-        return totalConsumption;
-    }
-
-    public int getNumberOfVehicles() {
-        return numberOfVehicles;
-    }
-
+  public int getNumberOfVehicles() {
+    return numberOfVehicles;
+  }
 }
--- core/src/main/java/org/movsim/simulator/SimulationRun.java
+++ core/src/main/java/org/movsim/simulator/SimulationRun.java
@@ -1,51 +1,22 @@
-/*
- * Copyright (C) 2010, 2011, 2012 by Arne Kesting, Martin Treiber, Ralph Germ, Martin Budden
- * <movsim.org@gmail.com>
- * -----------------------------------------------------------------------------------------
- * 
- * This file is part of
- * 
- * MovSim - the multi-model open-source vehicular-traffic simulator.
- * 
- * MovSim is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * MovSim is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with MovSim. If not, see <http://www.gnu.org/licenses/>
- * or <http://www.movsim.org>.
- * 
- * -----------------------------------------------------------------------------------------
- */
 package org.movsim.simulator;
-
 import java.util.ArrayList;
 import java.util.List;
-
 import org.movsim.shutdown.ShutdownHooks;
-
 import com.google.common.base.Preconditions;
 
 public class SimulationRun {
-
-    public interface CompletionCallback {
-	/**
+  public interface CompletionCallback {
+    /**
 	 * Callback to inform the application that the simulation has run to
 	 * completion.
 	 * 
 	 * @param simulationTime
 	 */
-	public void simulationComplete(double simulationTime);
-    }
+    public void simulationComplete(double simulationTime);
+  }
 
-    public interface UpdateStatusCallback {
-	/**
+  public interface UpdateStatusCallback {
+    /**
 	 * Callback to allow the application to make updates to its state after
 	 * the vehicle positions etc have been updated, but before the repaint
 	 * is called.
@@ -53,97 +24,95 @@ public class SimulationRun {
 	 * @param simulationTime
 	 *            the current logical time in the simulation
 	 */
-	public void updateStatus(double simulationTime);
-    }
+    public void updateStatus(double simulationTime);
+  }
 
-    protected double dt; // timestep, seconds
+  protected double dt;
 
-    protected double duration; // duration, seconds
+  protected double duration;
 
-    protected double simulationTime; // Simulation time, seconds (reset to 0.0
-	                             // in each run)
+  protected double simulationTime;
 
-    protected long iterationCount;
+  protected long iterationCount;
 
-    protected long totalSimulationTime;
+  protected long totalSimulationTime;
 
-    protected final List<UpdateStatusCallback> updateStatusCallbacks = new ArrayList<>();
+  protected final List<UpdateStatusCallback> updateStatusCallbacks = new ArrayList<>();
 
-    protected CompletionCallback completionCallback;
+  protected CompletionCallback completionCallback;
 
-    // simulation is an object that implements the SimulationTimeStep interface.
-    protected final SimulationTimeStep simulation;
+  protected final SimulationTimeStep simulation;
 
-    /**
+  /**
      * Constructor, sets the simulation object.
      * 
      * @param simulation
      *            a simulation object that implements the SimulationTimeStep
      *            interface
      */
-    public SimulationRun(SimulationTimeStep simulation) {
-	assert simulation != null;
-	this.simulation = simulation;
-	initShutdownHook();
-    }
+  public SimulationRun(SimulationTimeStep simulation) {
+    assert simulation != null;
+    this.simulation = simulation;
+    initShutdownHook();
+  }
 
-    /**
+  /**
      * Returns the simulation timestep object.
      * 
      * @return the simulation timestep object
      */
-    public final SimulationTimeStep simulation() {
-	return simulation;
-    }
+  public final SimulationTimeStep simulation() {
+    return simulation;
+  }
 
-    /**
+  /**
      * Sets the simulation timestep.
      * 
      * @param dt
      *            simulation timestep in seconds
      */
-    public final void setTimeStep(double dt) {
-	this.dt = dt;
-    }
+  public final void setTimeStep(double dt) {
+    this.dt = dt;
+  }
 
-    /**
+  /**
      * Returns the simulation timestep
      * 
      * @return simulation timestep in seconds
      */
-    public final double timeStep() {
-	return dt;
-    }
+  public final double timeStep() {
+    return dt;
+  }
 
-    /**
+  /**
      * Sets the simulation duration.
      * 
      * @param duration
      *            simulation duration in seconds
      */
-    public final void setDuration(double duration) {
-	this.duration = duration;
-    }
+  public final void setDuration(double duration) {
+    this.duration = duration;
+  }
 
-    /**
+  /**
      * Returns the simulation duration.
      * 
      * @return simulation duration in seconds
      */
-    public final double duration() {
-	return duration;
-    }
+  public final double duration() {
+    return duration;
+  }
 
-    /**
+  /**
      * Returns the number of iterations executed.
      * 
      * @return number of iterations
      */
-    public final long iterationCount() {
-	return iterationCount;
-    }
+  public final long iterationCount() {
+    return iterationCount;
+  }
 
-    /**
+  /**
      * <p>
      * Returns the time for which the simulation has been running.
      * </p>
@@ -156,87 +125,79 @@ public class SimulationRun {
      * 
      * @return the simulation time
      */
-    public final double simulationTime() {
-	return simulationTime;
-    }
+  public final double simulationTime() {
+    return simulationTime;
+  }
 
-    /**
+  /**
      * Returns the total execution time of the simulation. Useful for order of
      * magnitude benchmarking.
      * 
      * @return total execution time of the simulation
      */
-    public final long totalSimulationTime() {
-	return totalSimulationTime;
-    }
+  public final long totalSimulationTime() {
+    return totalSimulationTime;
+  }
 
-    /**
+  /**
      * Resets the simulation instrumentation data.
      */
-    public void reset() {
-	simulationTime = 0.0;
-	iterationCount = 0;
-	totalSimulationTime = 0;
-    }
+  public void reset() {
+    simulationTime = 0.0;
+    iterationCount = 0;
+    totalSimulationTime = 0;
+  }
 
-    /**
+  /**
      * Adds a update status callback.
      * 
      * @param updateStatusCallback
      */
-    public void addUpdateStatusCallback(UpdateStatusCallback updateStatusCallback) {
-	updateStatusCallbacks.add(Preconditions.checkNotNull(updateStatusCallback));
-    }
+  public void addUpdateStatusCallback(UpdateStatusCallback updateStatusCallback) {
+    updateStatusCallbacks.add(Preconditions.checkNotNull(updateStatusCallback));
+  }
 
-    /**
+  /**
      * Sets the completion callback.
      * 
      * @param completionCallback
      */
-    public final void setCompletionCallback(CompletionCallback completionCallback) {
-	Preconditions.checkState(this.completionCallback == null, "it's a mistake if this is set twice");
-	this.completionCallback = completionCallback;
-    }
+  public final void setCompletionCallback(CompletionCallback completionCallback) {
+    Preconditions.checkState(this.completionCallback == null, "it\'s a mistake if this is set twice");
+    this.completionCallback = completionCallback;
+  }
 
-    /**
+  /**
      * Runs the simulation to completion and then calls the completion callback.
      */
-    public void runToCompletion() {
-        assert dt != 0.0;
-        assert duration != 0.0;
-        assert duration > 0.0;
-        reset();
-        final long timeBeforeSim_ms = System.currentTimeMillis();
-        final double timeLimit = duration + dt / 2.0; // allow for rounding errors
-        while (simulationTime <= timeLimit) {
-            // perform the timeStep for the road network
-            simulation.timeStep(dt, simulationTime, iterationCount);
-            for (final UpdateStatusCallback updateStatusCallback : updateStatusCallbacks) {
-                updateStatusCallback.updateStatus(simulationTime);
-            }
-            simulationTime += dt;
-            ++iterationCount;
+  public void runToCompletion() {
+    assert dt != 0.0;
+    assert duration != 0.0;
+    assert duration > 0.0;
+    reset();
+    final long timeBeforeSim_ms = System.currentTimeMillis();
+    final double timeLimit = duration + dt / 2.0;
+    while (simulationTime <= timeLimit) {
+      simulation.timeStep(dt, simulationTime, iterationCount);
+      for (final UpdateStatusCallback updateStatusCallback : updateStatusCallbacks) {
+        updateStatusCallback.updateStatus(simulationTime);
+      }
+      simulationTime += dt;
+      ++iterationCount;
+    }
+    totalSimulationTime = System.currentTimeMillis() - timeBeforeSim_ms;
+    if (completionCallback != null) {
+      completionCallback.simulationComplete(simulationTime);
+    }
+    ShutdownHooks.INSTANCE.onShutDown();
+  }
 
-            // TODO testwise
-            // if (iterationCount == 1000) {
-            // throw new RuntimeException("Dummy Exception to cause JVM to exit");
-            // }
-
-        }
-        totalSimulationTime = System.currentTimeMillis() - timeBeforeSim_ms;
-        if (completionCallback != null) {
-            completionCallback.simulationComplete(simulationTime);
-        }
+  private static void initShutdownHook() {
+    Runtime.getRuntime().addShutdownHook(new Thread() {
+      @Override public void run() {
+        System.out.println("Unexpected end of simulator: perform ShutdownHooks");
         ShutdownHooks.INSTANCE.onShutDown();
-    }
-
-    private static void initShutdownHook() {
-        Runtime.getRuntime().addShutdownHook(new Thread() {
-            @Override
-            public void run() {
-                System.out.println("Unexpected end of simulator: perform ShutdownHooks");
-                ShutdownHooks.INSTANCE.onShutDown();
-            }
-        });
-    }
+      }
+    });
+  }
 }
