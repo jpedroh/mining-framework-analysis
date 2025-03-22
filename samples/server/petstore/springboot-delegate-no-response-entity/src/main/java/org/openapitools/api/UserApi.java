992378342998278243e1878b8c466af532031ca8
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-22 09:00:35 +0000

    Mining Framework Analysis

--- samples/server/petstore/springboot-delegate-no-response-entity/src/main/java/org/openapitools/api/UserApi.java
+++ samples/server/petstore/springboot-delegate-no-response-entity/src/main/java/org/openapitools/api/UserApi.java
@@ -16,7 +16,6 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
 import javax.validation.constraints.*;
-import java.util.List;
 import java.util.Map;
 import javax.annotation.Generated;
 
