6b98c6482c8a2e1640ddf4b6a69ad112c90b9d91
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-22 01:55:12 +0000

    Mining Framework Analysis

--- src/main/java/org/jongo/query/BsonQueryFactory.java
+++ src/main/java/org/jongo/query/BsonQueryFactory.java
@@ -1,22 +1,4 @@
-/*
- * Copyright (C) 2011 Benoit GUEROUT <bguerout at gmail dot com>, Yves AMSELLEM <amsellem dot yves at gmail dot com>
- * and other contributors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.jongo.query;
-
 import com.mongodb.BasicDBList;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DBObject;
@@ -27,203 +9,164 @@ import org.bson.BSONObject;
 import org.jongo.bson.Bson;
 import org.jongo.marshall.Marshaller;
 import org.jongo.marshall.MarshallingException;
-
 import java.util.Collection;
 import java.util.List;
 
 public class BsonQueryFactory implements QueryFactory {
+  private static final String DEFAULT_TOKEN = "#";
 
-    private static final String DEFAULT_TOKEN = "#";
-    private static final String MARSHALL_OPERATOR = "$marshall";
-    private static final String PRECEDING_VALUE_PARAM = ": ,[\t\r\n";
+  private static final String MARSHALL_OPERATOR = "$marshall";
 
-    private final String token;
-    private final Marshaller marshaller;
+  private static final String PRECEDING_VALUE_PARAM = ": ,[\t\r\n";
 
-    private static class BsonQuery implements Query {
-        private final DBObject dbo;
+  private final String token;
 
-        public BsonQuery(DBObject dbo) {
-            this.dbo = dbo;
-        }
+  private final Marshaller marshaller;
 
-        public DBObject toDBObject() {
-            return dbo;
-        }
+  private static class BsonQuery implements Query {
+    private final DBObject dbo;
+
+    public BsonQuery(DBObject dbo) {
+      this.dbo = dbo;
     }
 
-    public BsonQueryFactory(Marshaller marshaller) {
-        this(marshaller, DEFAULT_TOKEN);
+    public DBObject toDBObject() {
+      return dbo;
     }
+  }
 
-    public BsonQueryFactory(Marshaller marshaller, String token) {
-        this.token = token;
-        this.marshaller = marshaller;
+  public BsonQueryFactory(Marshaller marshaller) {
+    this(marshaller, DEFAULT_TOKEN);
+  }
+
+  public BsonQueryFactory(Marshaller marshaller, String token) {
+    this.token = token;
+    this.marshaller = marshaller;
+  }
+
+  public Query createQuery(final String query, Object... parameters) {
+    if (query == null) {
+      return new BsonQuery((DBObject) JSON.parse(query));
     }
-
-    public Query createQuery(final String query, Object... parameters) {
-
-        if (query == null) {
-            return new BsonQuery((DBObject) JSON.parse(query));
+    if (parameters == null) {
+      parameters = new Object[] { null };
+    }
+    StringBuilder sb = new StringBuilder();
+    int paramIncrement = 0;
+    int paramPos = 0;
+    int start = 0;
+    int pos;
+    while ((pos = query.indexOf(token, start)) != -1) {
+      if (paramPos >= parameters.length) {
+        throw new IllegalArgumentException("Not enough parameters passed to query: " + query);
+      }
+      sb.append(query, start, pos);
+      boolean isValueParam = true;
+      int comaIndex = query.indexOf(",", pos);
+      int doubleDotIndex = query.indexOf(":", pos);
+      if (comaIndex == -1 && doubleDotIndex == -1) {
+        isValueParam = true;
+      } else {
+        if (comaIndex == -1 && doubleDotIndex != -1) {
+          isValueParam = false;
+        } else {
+          if (comaIndex < doubleDotIndex) {
+            isValueParam = true;
+          } else {
+            isValueParam = false;
+          }
         }
-        if (parameters == null) {
-            parameters = new Object[]{null};
-        }
+      }
+      if (isValueParam) {
+        sb.append("{\"").append(MARSHALL_OPERATOR).append("\":").append(paramIncrement).append("}");
+        paramIncrement = 0;
+      } else {
+        sb.append(parameters[paramPos]);
+        paramIncrement++;
+      }
+      paramPos++;
+      start = pos + token.length();
+    }
+    sb.append(query, start, query.length());
+    if (paramPos < parameters.length) {
+      throw new IllegalArgumentException("Too many parameters passed to query: " + query);
+    }
+    final Object[] params = parameters;
+    DBObject dbo;
+    try {
+      dbo = (DBObject) JSON.parse(sb.toString(), new JSONCallback() {
+        int paramPos = 0;
 
-        // We have two different cases:
-        //
-        // - tokens as property names "{scores.#: 1}": they must be expanded before going
-        //   through the JSON parser, and their toString() is inserted in the query
-        //
-        // - tokens as property values "{id: #}": they are resolved by the JSON parser and
-        //   therefore marshalled as DBObjects (actually LazyDBObjects).
-
-        StringBuilder sb = new StringBuilder();
-        int paramIncrement = 0; // how many params must be skipped by the next value param
-        int paramPos = 0;       // current position in the parameter list
-        int start = 0;          // start of the current string segment
-        int pos;                // position of the last token found
-        while ((pos = query.indexOf(token, start)) != -1) {
-            if (paramPos >= parameters.length) {
+        @Override public Object objectDone() {
+          String name = curName();
+          Object o = super.objectDone();
+          if (o instanceof BSONObject && !(o instanceof List<?>)) {
+            BSONObject dbo = (BSONObject) o;
+            Object marshallValue = dbo.get(MARSHALL_OPERATOR);
+            if (marshallValue != null) {
+              paramPos += ((Number) marshallValue).intValue();
+              if (paramPos >= params.length) {
                 throw new IllegalArgumentException("Not enough parameters passed to query: " + query);
+              }
+              o = marshallParameter(params[paramPos++]);
+              if (!isStackEmpty()) {
+                _put(name, o);
+              } else {
+                o = !BSON.hasDecodeHooks() ? o : BSON.applyDecodingHooks(o);
+                setRoot(o);
+              }
             }
-
-            // Insert chars before the token
-            sb.append(query, start, pos);
-
-            // Check if the character preceding the token is one that separates values.
-            // Otherwise, it's a property name substitution
-            boolean isValueParam = true;
-            int comaIndex = query.indexOf(",", pos);
-            int doubleDotIndex =query.indexOf(":", pos);
-            if (comaIndex == -1 && doubleDotIndex == -1) {
-                isValueParam = true;
-            } else if (comaIndex == -1 && doubleDotIndex != -1) {
-                isValueParam = false;
-            } else if (comaIndex <  doubleDotIndex) {
-                isValueParam = true;
-            } else {
-                isValueParam = false;
-            }
-            /*if (pos > 0) {
-                char c = query.charAt(pos - 1);
-                if (PRECEDING_VALUE_PARAM.indexOf(c) == -1) {
-                    isValueParam = false;
-                }
-            }*/
-
-            if (isValueParam) {
-                // Will be resolved by the JSON parser below
-                sb.append("{\"").append(MARSHALL_OPERATOR).append("\":").append(paramIncrement).append("}");
-                paramIncrement = 0;
-            } else {
-                // Resolve it now
-                sb.append(parameters[paramPos]);
-                paramIncrement++;
-            }
-
-            paramPos++;
-            start = pos + token.length();
+          }
+          if (isStackEmpty()) {
+          }
+          return o;
         }
-
-        // Add remaining chars
-        sb.append(query, start, query.length());
-
-        if (paramPos < parameters.length) {
-            throw new IllegalArgumentException("Too many parameters passed to query: " + query);
-        }
-
-
-        final Object[] params = parameters;
-
-        // Parse the query with a callback that will weave in marshalled parameters
-        DBObject dbo;
-        try {
-            dbo = (DBObject) JSON.parse(sb.toString(), new JSONCallback() {
-
-                int paramPos = 0;
-
-                @Override
-                public Object objectDone() {
-                    String name = curName();
-                    Object o = super.objectDone();
-
-                    if (o instanceof BSONObject && !(o instanceof List<?>)) {
-                        BSONObject dbo = (BSONObject) o;
-                        Object marshallValue = dbo.get(MARSHALL_OPERATOR);
-                        if (marshallValue != null) {
-                            paramPos += ((Number) marshallValue).intValue();
-                            if (paramPos >= params.length) {
-                                throw new IllegalArgumentException("Not enough parameters passed to query: " + query);
-                            }
-
-                            o = marshallParameter(params[paramPos++]);
-
-                            // Replace value set by super.objectDone()
-                            if (!isStackEmpty()) {
-                                _put(name, o);
-                            } else {
-                                o = !BSON.hasDecodeHooks() ? o : BSON.applyDecodingHooks(o);
-                                setRoot(o);
-                            }
-                        }
-                    }
-
-                    if (isStackEmpty()) {
-                        // End of object
-                    }
-
-                    return o;
-                }
-            });
-
-        } catch (Exception e) {
-            throw new IllegalArgumentException("Cannot parse query: " + query, e);
-        }
-
-        return new BsonQuery(dbo);
-
+      });
+    } catch (Exception e) {
+      throw new IllegalArgumentException("Cannot parse query: " + query, e);
     }
+    return new BsonQuery(dbo);
+  }
 
-    private Object marshallParameter(Object parameter) {
-        try {
-            if (parameter == null || Bson.isPrimitive(parameter)) {
-                return parameter;
-            }
-            if (parameter instanceof Enum) {
-                return ((Enum<?>) parameter).name();
-            }
-            if (parameter instanceof Collection) {
-                return marshallCollection((Collection<?>) parameter);
-            }
-            if (parameter instanceof Object[]) {
-                return marshallArray((Object[]) parameter);
-            }
-            return marshallDocument(parameter);
-        } catch (Exception e) {
-            String message = String.format("Unable to marshall parameter: %s", parameter);
-            throw new MarshallingException(message, e);
-        }
+  private Object marshallParameter(Object parameter) {
+    try {
+      if (parameter == null || Bson.isPrimitive(parameter)) {
+        return parameter;
+      }
+      if (parameter instanceof Enum) {
+        return ((Enum<?>) parameter).name();
+      }
+      if (parameter instanceof Collection) {
+        return marshallCollection((Collection<?>) parameter);
+      }
+      if (parameter instanceof Object[]) {
+        return marshallArray((Object[]) parameter);
+      }
+      return marshallDocument(parameter);
+    } catch (Exception e) {
+      String message = String.format("Unable to marshall parameter: %s", parameter);
+      throw new MarshallingException(message, e);
     }
+  }
 
-    private DBObject marshallArray(Object[] parameters) {
-        BasicDBList list = new BasicDBList();
-        for (int i = 0; i < parameters.length; i++) {
-            list.add(marshallParameter(parameters[i]));
-        }
-        return list;
+  private DBObject marshallArray(Object[] parameters) {
+    BasicDBList list = new BasicDBList();
+    for (int i = 0; i < parameters.length; i++) {
+      list.add(marshallParameter(parameters[i]));
     }
+    return list;
+  }
 
-    private DBObject marshallCollection(Collection<?> parameters) {
-        BasicDBList list = new BasicDBList();
-        for (Object param : parameters) {
-            list.add(marshallParameter(param));
-        }
-        return list;
+  private DBObject marshallCollection(Collection<?> parameters) {
+    BasicDBList list = new BasicDBList();
+    for (Object param : parameters) {
+      list.add(marshallParameter(param));
     }
+    return list;
+  }
 
-    private Object marshallDocument(Object parameter) {
-        DBObject wrapper = new BasicDBObject("wrapped", parameter);
-        return marshaller.marshall(wrapper).toDBObject().get("wrapped");
-    }
+  private Object marshallDocument(Object parameter) {
+    DBObject wrapper = new BasicDBObject("wrapped", parameter);
+    return marshaller.marshall(wrapper).toDBObject().get("wrapped");
+  }
 }
--- src/test/java/org/jongo/query/BsonQueryFactoryTest.java
+++ src/test/java/org/jongo/query/BsonQueryFactoryTest.java
@@ -1,21 +1,4 @@
-/*
- * Copyright (C) 2011 Benoit GUEROUT <bguerout at gmail dot com> and Yves AMSELLEM <amsellem dot yves at gmail dot com>
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.jongo.query;
-
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonSerializer;
@@ -29,166 +12,114 @@ import org.jongo.model.Friend;
 import org.jongo.util.ErrorObject;
 import org.junit.Before;
 import org.junit.Test;
-
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class BsonQueryFactoryTest {
+  private QueryFactory factory;
 
-    private QueryFactory factory;
+  @Before public void setUp() throws Exception {
+    factory = new BsonQueryFactory(new JacksonEngine(Mapping.defaultMapping()));
+  }
 
-    @Before
-    public void setUp() throws Exception {
-        factory = new BsonQueryFactory(new JacksonEngine(Mapping.defaultMapping()));
+  @Test(expected = IllegalArgumentException.class) public void shouldFailWithInvalidParameter() throws Exception {
+    factory.createQuery("{id:#}", new ErrorObject());
+  }
+
+  @Test(expected = IllegalArgumentException.class) public void shouldFailWhenNotEnoughParameters() throws Exception {
+    factory.createQuery("{id:#,id2:#}", "123");
+  }
+
+  @Test(expected = IllegalArgumentException.class) public void shouldFailWhenTooManyParameters() throws Exception {
+    factory.createQuery("{id:#}", 123, 456);
+  }
+
+  @Test public void shouldAllowToCreateNullQuery() throws Exception {
+    Query query = factory.createQuery(null);
+    assertThat(query.toDBObject()).isEqualTo(null);
+  }
+
+  @Test public void shouldBindOneParameter() throws Exception {
+    Query query = factory.createQuery("{id:#}", 123);
+    assertThat(query.toDBObject()).isEqualTo(new BasicDBObject("id", 123));
+  }
+
+  @Test public void shouldBindManyParameters() throws Exception {
+    Query query = factory.createQuery("{id:#, test:#}", 123, 456);
+    assertThat(query.toDBObject()).isEqualTo(QueryBuilder.start("id").is(123).and("test").is(456).get());
+  }
+
+  @Test public void shouldBindManyNames() throws Exception {
+    Query query = factory.createQuery("{#:123, #:456}", "id", "test");
+    assertThat(query.toDBObject()).isEqualTo(QueryBuilder.start("id").is(123).and("test").is(456).get());
+  }
+
+  @Test public void shouldBindNullParameter() throws Exception {
+    Query query = factory.createQuery("{id:#}", null);
+    assertThat(query.toDBObject()).isEqualTo(new BasicDBObject("id", null));
+  }
+
+  @Test public void shouldBindParameterWithCustomToken() throws Exception {
+    QueryFactory factoryWithToken = new BsonQueryFactory(new JacksonEngine(Mapping.defaultMapping()), "@");
+    Query query = factoryWithToken.createQuery("{id:@}", 123);
+    assertThat(query.toDBObject()).isEqualTo(new BasicDBObject("id", 123));
+  }
+
+  @Test public void shouldBindHashSign() throws Exception {
+    Query query = factory.createQuery("{id:#}", "string with # sign");
+    assertThat(query.toDBObject()).isEqualTo(new BasicDBObject("id", "string with # sign"));
+  }
+
+  @Test public void shouldBindQuotationMark() throws Exception {
+    Query query = factory.createQuery("{id:#}", "string with \" quotation mark");
+    assertThat(query.toDBObject()).isEqualTo(new BasicDBObject("id", "string with \" quotation mark"));
+  }
+
+  @Test public void shouldBindNestedQuery() throws Exception {
+    DBObject dbo = factory.createQuery("{ a: #, b: { c: #, d: #}, e: #}", 1, Long.valueOf(2), 3, "hello").toDBObject();
+    assertThat(dbo.get("a")).isEqualTo(Integer.valueOf(1));
+    assertThat(dbo.get("e")).isEqualTo("hello");
+    DBObject dbo2 = (DBObject) dbo.get("b");
+    assertThat(dbo2.get("c")).isEqualTo(Long.valueOf(2));
+    assertThat(dbo2.get("d")).isEqualTo(Integer.valueOf(3));
+  }
+
+  public static class AnObject {
+    public int aa;
+
+    public long bb;
+  }
+
+  @Test public void shouldBindNestedParameter() throws Exception {
+    AnObject ao = new AnObject();
+    ao.aa = 10;
+    ao.bb = 11;
+    DBObject dbo = factory.createQuery("{ a: #, b: { c: #, d: #}, e: #}", 1, Long.valueOf(2), ao, "hello").toDBObject();
+    assertThat(dbo.get("a")).isEqualTo(Integer.valueOf(1));
+    assertThat(dbo.get("e")).isEqualTo("hello");
+    DBObject dbo2 = (DBObject) dbo.get("b");
+    assertThat(dbo2.get("c")).isEqualTo(Long.valueOf(2));
+    DBObject dbo3 = (DBObject) dbo2.get("d");
+    assertThat(dbo3.get("aa")).isEqualTo(Integer.valueOf(10));
+    assertThat(dbo3.get("bb")).isEqualTo(Long.valueOf(11));
+  }
+
+  @Test public void canSaveAndUpdateBytes() throws Exception {
+    DBObject dbo = factory.createQuery("{bytes:#}", "test".getBytes(StandardCharsets.UTF_8)).toDBObject();
+    assertThat(new String((byte[]) dbo.get("bytes"))).isEqualTo("test");
+  }
+
+  @Test public void canHandleAObjectSerializedAsAPrimitive() throws Exception {
+    Mapping mapping = new Mapping.Builder().addSerializer(Friend.class, new PrimitiveJsonSerializer()).build();
+    factory = new BsonQueryFactory(new JacksonEngine(mapping));
+    DBObject query = factory.createQuery("{bytes:#}", new Friend("Robert")).toDBObject();
+    assertThat(query.get("bytes")).isEqualTo("Robert");
+  }
+
+  private static class PrimitiveJsonSerializer extends JsonSerializer<Friend> {
+    @Override public void serialize(Friend friend, JsonGenerator jgen, SerializerProvider provider) throws IOException {
+      jgen.writeString(friend.getName());
     }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void shouldFailWithInvalidParameter() throws Exception {
-
-        factory.createQuery("{id:#}", new ErrorObject());
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void shouldFailWhenNotEnoughParameters() throws Exception {
-
-        factory.createQuery("{id:#,id2:#}", "123");
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void shouldFailWhenTooManyParameters() throws Exception {
-
-        factory.createQuery("{id:#}", 123, 456);
-    }
-
-    @Test
-    public void shouldAllowToCreateNullQuery() throws Exception {
-
-        Query query = factory.createQuery(null);
-
-        assertThat(query.toDBObject()).isEqualTo(null);
-    }
-
-    @Test
-    public void shouldBindOneParameter() throws Exception {
-
-        Query query = factory.createQuery("{id:#}", 123);
-
-        assertThat(query.toDBObject()).isEqualTo(new BasicDBObject("id", 123));
-    }
-
-    @Test
-    public void shouldBindManyParameters() throws Exception {
-
-        Query query = factory.createQuery("{id:#, test:#}", 123, 456);
-
-        assertThat(query.toDBObject()).isEqualTo(QueryBuilder.start("id").is(123).and("test").is(456).get());
-    }
-
-    @Test
-    public void shouldBindManyNames() throws Exception {
-
-        Query query = factory.createQuery("{#:123, #:456}", "id", "test");
-
-        assertThat(query.toDBObject()).isEqualTo(QueryBuilder.start("id").is(123).and("test").is(456).get());
-    }
-
-    @Test
-    public void shouldBindNullParameter() throws Exception {
-
-        Query query = factory.createQuery("{id:#}", null);
-
-        assertThat(query.toDBObject()).isEqualTo(new BasicDBObject("id", null));
-    }
-
-    @Test
-    public void shouldBindParameterWithCustomToken() throws Exception {
-
-        QueryFactory factoryWithToken = new BsonQueryFactory(new JacksonEngine(Mapping.defaultMapping()), "@");
-
-        Query query = factoryWithToken.createQuery("{id:@}", 123);
-
-        assertThat(query.toDBObject()).isEqualTo(new BasicDBObject("id", 123));
-    }
-
-    @Test
-    public void shouldBindHashSign() throws Exception {
-
-        Query query = factory.createQuery("{id:#}", "string with # sign");
-
-        assertThat(query.toDBObject()).isEqualTo(new BasicDBObject("id", "string with # sign"));
-    }
-
-    @Test
-    public void shouldBindQuotationMark() throws Exception {
-
-        Query query = factory.createQuery("{id:#}", "string with \" quotation mark");
-
-        assertThat(query.toDBObject()).isEqualTo(new BasicDBObject("id", "string with \" quotation mark"));
-    }
-
-    @Test
-    public void shouldBindNestedQuery() throws Exception {
-
-        DBObject dbo = factory.createQuery("{ a: #, b: { c: #, d: #}, e: #}", 1, Long.valueOf(2), 3, "hello").toDBObject();
-
-        assertThat(dbo.get("a")).isEqualTo(Integer.valueOf(1));
-        assertThat(dbo.get("e")).isEqualTo("hello");
-
-        DBObject dbo2 = (DBObject) dbo.get("b");
-        assertThat(dbo2.get("c")).isEqualTo(Long.valueOf(2));
-        assertThat(dbo2.get("d")).isEqualTo(Integer.valueOf(3));
-    }
-
-    public static class AnObject {
-        public int aa;
-        public long bb;
-    }
-
-    @Test
-    public void shouldBindNestedParameter() throws Exception {
-        AnObject ao = new AnObject();
-        ao.aa = 10;
-        ao.bb = 11;
-
-        DBObject dbo = factory.createQuery("{ a: #, b: { c: #, d: #}, e: #}", 1, Long.valueOf(2), ao, "hello").toDBObject();
-
-        assertThat(dbo.get("a")).isEqualTo(Integer.valueOf(1));
-        assertThat(dbo.get("e")).isEqualTo("hello");
-
-        DBObject dbo2 = (DBObject) dbo.get("b");
-        assertThat(dbo2.get("c")).isEqualTo(Long.valueOf(2));
-
-        DBObject dbo3 = (DBObject) dbo2.get("d");
-        assertThat(dbo3.get("aa")).isEqualTo(Integer.valueOf(10));
-        assertThat(dbo3.get("bb")).isEqualTo(Long.valueOf(11));
-    }
-
-    @Test
-    public void canSaveAndUpdateBytes() throws Exception {
-
-        DBObject dbo = factory.createQuery("{bytes:#}", "test".getBytes(StandardCharsets.UTF_8)).toDBObject();
-
-        assertThat(new String((byte[]) dbo.get("bytes"))).isEqualTo("test");
-    }
-
-    @Test
-    public void canHandleAObjectSerializedAsAPrimitive() throws Exception {
-
-        Mapping mapping = new Mapping.Builder().addSerializer(Friend.class, new PrimitiveJsonSerializer()).build();
-        factory = new BsonQueryFactory(new JacksonEngine(mapping));
-
-        DBObject query = factory.createQuery("{bytes:#}", new Friend("Robert")).toDBObject();
-
-        assertThat(query.get("bytes")).isEqualTo("Robert");
-    }
-
-    private static class PrimitiveJsonSerializer extends JsonSerializer<Friend> {
-
-        @Override
-        public void serialize(Friend friend, JsonGenerator jgen, SerializerProvider provider) throws IOException {
-            jgen.writeString(friend.getName());
-        }
-    }
+  }
 }
