8bbd7adce77698dfca92bd49425d62ade7ba711e
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-22 06:10:39 +0000

    Mining Framework Analysis

--- src/main/java/net/logstash/logback/composite/AbstractPatternJsonProvider.java
+++ src/main/java/net/logstash/logback/composite/AbstractPatternJsonProvider.java
@@ -1,27 +1,9 @@
-/*
- * Copyright 2013-2021 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package net.logstash.logback.composite;
-
 import java.io.IOException;
 import java.util.Objects;
-
 import net.logstash.logback.pattern.AbstractJsonPatternParser;
 import net.logstash.logback.pattern.AbstractJsonPatternParser.JsonPatternException;
 import net.logstash.logback.pattern.NodeWriter;
-
 import ch.qos.logback.access.spi.IAccessEvent;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.spi.DeferredProcessingAware;
@@ -38,88 +20,79 @@ import com.fasterxml.jackson.core.JsonGenerator;
  *
  * @author <a href="mailto:dimas@dataart.com">Dmitry Andrianov</a>
  */
-public abstract class AbstractPatternJsonProvider<Event extends DeferredProcessingAware>
-        extends AbstractJsonProvider<Event> implements JsonFactoryAware {
+public abstract class AbstractPatternJsonProvider<Event extends DeferredProcessingAware> extends AbstractJsonProvider<Event> implements JsonFactoryAware {
+  private NodeWriter<Event> nodeWriter;
 
-    private NodeWriter<Event> nodeWriter;
-    
-    private String pattern;
+  private String pattern;
 
-    private JsonFactory jsonFactory;
+  private JsonFactory jsonFactory;
 
-    /**
+  /**
      * When {@code true}, fields whose values are considered empty ({@link AbstractJsonPatternParser#isEmptyValue(Object)}})
      * will be omitted from JSON output.
      */
-    private boolean omitEmptyFields;
+  private boolean omitEmptyFields;
 
-    @Override
-    public void writeTo(JsonGenerator generator, Event event) throws IOException {
-        if (nodeWriter != null) {
-            nodeWriter.write(generator, event);
-        }
+  @Override public void writeTo(JsonGenerator generator, Event event) throws IOException {
+    if (nodeWriter != null) {
+      nodeWriter.write(generator, event);
     }
-    
-    protected abstract AbstractJsonPatternParser<Event> createParser(JsonFactory jsonFactory);
-    
-    public String getPattern() {
-        return pattern;
-    }
+  }
 
-    public void setPattern(final String pattern) {
-        this.pattern = pattern;
-    }
-    
-    @Override
-    public void setJsonFactory(JsonFactory jsonFactory) {
-        this.jsonFactory = Objects.requireNonNull(jsonFactory);
-    }
+  protected abstract AbstractJsonPatternParser<Event> createParser(JsonFactory jsonFactory);
 
-    @Override
-    public void start() {
-        if (jsonFactory == null) {
-            throw new IllegalStateException("JsonFactory has not been set");
-        }
-        
-        try {
-            this.nodeWriter = initializeNodeWriter();
-        } catch (JsonPatternException e) {
-            this.nodeWriter = null;
-            addError("Invalid [pattern]: " + e.getMessage(), e);
-        }
-        
-        super.start();
+  public String getPattern() {
+    return pattern;
+  }
+
+  public void setPattern(final String pattern) {
+    this.pattern = pattern;
+  }
+
+  @Override public void setJsonFactory(JsonFactory jsonFactory) {
+    this.jsonFactory = Objects.requireNonNull(jsonFactory);
+  }
+
+  @Override public void start() {
+    if (jsonFactory == null) {
+      throw new IllegalStateException("JsonFactory has not been set");
     }
-    
-    
-    /**
+    try {
+      this.nodeWriter = initializeNodeWriter();
+    } catch (JsonPatternException e) {
+      this.nodeWriter = null;
+      addError("Invalid [pattern]: " + e.getMessage(), e);
+    }
+    super.start();
+  }
+
+  /**
      * Parses the pattern into a {@link NodeWriter}.
      * 
      * @return a {@link NodeWriter}
      * @throws JsonPatternException thrown in case of invalid pattern
      */
-    private NodeWriter<Event> initializeNodeWriter() throws JsonPatternException {
-        AbstractJsonPatternParser<Event> parser = createParser(this.jsonFactory);
-        parser.setOmitEmptyFields(omitEmptyFields);
-        return parser.parse(pattern);
-    }
-    
+  private NodeWriter<Event> initializeNodeWriter() throws JsonPatternException {
+    AbstractJsonPatternParser<Event> parser = createParser(this.jsonFactory);
+    parser.setOmitEmptyFields(omitEmptyFields);
+    return parser.parse(pattern);
+  }
 
-    /**
+  /**
      * When {@code true}, fields whose values are considered empty will be omitted from JSON output.
      * 
      * @return {@code true} if empty fields are omitted from JSON output, {@code false} otherwise.
      */
-    public boolean isOmitEmptyFields() {
-        return omitEmptyFields;
-    }
+  public boolean isOmitEmptyFields() {
+    return omitEmptyFields;
+  }
 
-    /**
+  /**
      * When {@code true}, fields whose values are considered empty will be omitted from JSON output.
      * 
      * @param omitEmptyFields whether empty fields are omitted or not
      */
-    public void setOmitEmptyFields(boolean omitEmptyFields) {
-        this.omitEmptyFields = omitEmptyFields;
-    }
+  public void setOmitEmptyFields(boolean omitEmptyFields) {
+    this.omitEmptyFields = omitEmptyFields;
+  }
 }
--- src/main/java/net/logstash/logback/pattern/AccessEventJsonPatternParser.java
+++ src/main/java/net/logstash/logback/pattern/AccessEventJsonPatternParser.java
@@ -1,20 +1,4 @@
-/*
- * Copyright 2013-2021 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package net.logstash.logback.pattern;
-
 import ch.qos.logback.access.PatternLayout;
 import ch.qos.logback.access.spi.IAccessEvent;
 import ch.qos.logback.core.Context;
@@ -25,27 +9,36 @@ import com.fasterxml.jackson.core.JsonFactory;
  * @author <a href="mailto:dimas@dataart.com">Dmitry Andrianov</a>
  */
 public class AccessEventJsonPatternParser extends AbstractJsonPatternParser<IAccessEvent> {
+  public AccessEventJsonPatternParser(final Context context, final JsonFactory jsonFactory) {
+    super(context, jsonFactory);
+    addOperation("nullNA", new NullNaValueOperation());
+  }
 
-    public AccessEventJsonPatternParser(final Context context, final JsonFactory jsonFactory) {
-        super(context, jsonFactory);
-        
-        addOperation("nullNA", new NullNaValueOperation());
+  protected class NullNaValueOperation extends 
+<<<<<<< Unknown file: This is a bug in JDime.
+=======
+  AbstractJsonPatternParser<IAccessEvent>.Operation<String>
+>>>>>>> /usr/src/app/output/logstash/logstash-logback-encoder/35759d0a000d8ea4bd238e736572bf94bc7891d9/src/main/java/net/logstash/logback/pattern/AccessEventJsonPatternParser.java/right.java
+   implements Operation<IAccessEvent, String> {
+
+<<<<<<< Unknown file: This is a bug in JDime.
+=======
+    public NullNaValueOperation() {
+      super(true);
     }
-    
-    protected class NullNaValueOperation implements Operation<IAccessEvent, String> {
-        @Override
-        public ValueGetter<String, IAccessEvent> createValueGetter(String data) {
-            return makeLayoutValueGetter(data).andThen(this::convert);
-        }
-        
-        private String convert(final String value) {
-            return "-".equals(value) ? null : value;
-        }
+>>>>>>> /usr/src/app/output/logstash/logstash-logback-encoder/35759d0a000d8ea4bd238e736572bf94bc7891d9/src/main/java/net/logstash/logback/pattern/AccessEventJsonPatternParser.java/right.java
+
+
+    @Override public ValueGetter<String, IAccessEvent> createValueGetter(String data) {
+      return makeLayoutValueGetter(data).andThen(this::convert);
     }
 
-    
-    @Override
-    protected PatternLayoutBase<IAccessEvent> createLayout() {
-        return new PatternLayout();
+    private String convert(final String value) {
+      return "-".equals(value) ? null : value;
     }
+  }
+
+  @Override protected PatternLayoutBase<IAccessEvent> createLayout() {
+    return new PatternLayout();
+  }
 }
