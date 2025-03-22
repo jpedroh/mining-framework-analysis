353ecf0f19fd43126e171c25569b2a0612373425
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-22 07:51:13 +0000

    Mining Framework Analysis

--- jgrapht-io/src/main/java/org/jgrapht/io/IntegerComponentNameProvider.java
+++ jgrapht-io/src/main/java/org/jgrapht/io/IntegerComponentNameProvider.java
@@ -1,24 +1,5 @@
-/*
- * (C) Copyright 2005-2017, by Trevor Harmon and Contributors.
- *
- * JGraphT : a free Java graph-theory library
- *
- * See the CONTRIBUTORS.md file distributed with this work for additional
- * information regarding copyright ownership.
- *
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License 2.0 which is available at
- * http://www.eclipse.org/legal/epl-2.0, or the
- * GNU Lesser General Public License v2.1 or later
- * which is available at
- * http://www.gnu.org/licenses/old-licenses/lgpl-2.1-standalone.html.
- *
- * SPDX-License-Identifier: EPL-2.0 OR LGPL-2.1-or-later
- */
 package org.jgrapht.io;
-
 import java.util.*;
-
 import org.jgrapht.nio.IntegerIdProvider;
 
 /**
@@ -29,64 +10,57 @@ import org.jgrapht.nio.IntegerIdProvider;
  * @param <T> the component type
  *
  * @author Trevor Harmon
- * @author Amr ALHOSSARY
+ * 
  * @deprecated Use {@link IntegerIdProvider} instead.
  */
-@Deprecated
-public class IntegerComponentNameProvider<T>
-    implements
-    ComponentNameProvider<T>
-{
-    private static final int DEFAULT_BASE = 1;
+@Deprecated public class IntegerComponentNameProvider<T extends java.lang.Object> implements ComponentNameProvider<T> {
+  private static final int DEFAULT_BASE = 1;
 
-    /**
+  /**
      * The first ID to use.
      */
-    private int base;
-    private int nextID;
-    private final Map<T, Integer> idMap = new HashMap<>();
+  private int base;
 
-    /**
+  private int nextID;
+
+  private final Map<T, Integer> idMap = new HashMap<>();
+
+  /**
      * Create a provider with the default base id (1).
      */
-    public IntegerComponentNameProvider()
-    {
-        this(DEFAULT_BASE);
-    }
+  public IntegerComponentNameProvider() {
+    this(DEFAULT_BASE);
+  }
 
-    /**
+  /**
      * Create a provider with a given arbitrary base
      * 
      * @param base the first Id to use.
      */
-    public IntegerComponentNameProvider(int base)
-    {
-        this.base = base;
-        clear();
-    }
+  public IntegerComponentNameProvider(int base) {
+    this.base = base;
+    clear();
+  }
 
-    /**
+  /**
      * Clears all cached identifiers, and resets the unique identifier counter.
      */
-    public void clear()
-    {
-        nextID = base;
-        idMap.clear();
-    }
+  public void clear() {
+    nextID = base;
+    idMap.clear();
+  }
 
-    /**
+  /**
      * Returns the string representation of a component.
      *
      * @param component the component to be named
      */
-    @Override
-    public String getName(T component)
-    {
-        Integer id = idMap.get(component);
-        if (id == null) {
-            id = nextID++;
-            idMap.put(component, id);
-        }
-        return id.toString();
+  @Override public String getName(T component) {
+    Integer id = idMap.get(component);
+    if (id == null) {
+      id = nextID++;
+      idMap.put(component, id);
     }
+    return id.toString();
+  }
 }
