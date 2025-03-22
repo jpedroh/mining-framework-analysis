7b9d7338f993fe37ab6f024028416edf9b915fab
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-22 07:44:30 +0000

    Mining Framework Analysis

--- src/main/java/org/vafer/jdeb/ant/Data.java
+++ src/main/java/org/vafer/jdeb/ant/Data.java
@@ -1,27 +1,10 @@
-/*
- * Copyright 2013 The jdeb developers.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.vafer.jdeb.ant;
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
-
 import org.apache.tools.ant.Project;
 import org.apache.tools.ant.types.PatternSet;
 import org.vafer.jdeb.DataConsumer;
@@ -36,89 +19,79 @@ import org.vafer.jdeb.producers.*;
  * @author Torsten Curdt
  */
 public final class Data extends PatternSet implements DataProducer {
+  private final Collection<Mapper> mapperWrapper = new ArrayList<Mapper>();
 
-    private final Collection<Mapper> mapperWrapper = new ArrayList<Mapper>();
+  private File src;
 
-    private File src;
+  private String type;
 
-    private String type;
+  private Boolean conffile;
 
-    private Boolean conffile;
+  private String destinationName;
 
-    private String destinationName;
+  public void setSrc(File src) {
+    this.src = src;
+  }
 
-    public void setSrc(File src) {
-        this.src = src;
+  public String getType() {
+    return type;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+
+  public void setConffile(Boolean conffile) {
+    this.conffile = conffile;
+  }
+
+  public Boolean getConffile() {
+    return this.conffile;
+  }
+
+  public void setDst(String destinationName) {
+    this.destinationName = destinationName;
+  }
+
+  public void addMapper(Mapper mapper) {
+    mapperWrapper.add(mapper);
+  }
+
+  public void produce(final DataConsumer pReceiver) throws IOException {
+    if (src == null || !src.exists()) {
+      throw new FileNotFoundException("Data source not found : " + src);
     }
-
-    public String getType() {
-        return type;
+    final org.vafer.jdeb.mapping.Mapper[] mappers = new org.vafer.jdeb.mapping.Mapper[mapperWrapper.size()];
+    final Iterator<Mapper> it = mapperWrapper.iterator();
+    for (int i = 0; i < mappers.length; i++) {
+      mappers[i] = it.next().createMapper();
     }
-
-    public void setType(String type) {
-        this.type = type;
+    final Project project = getProject();
+    ProducerFactory.KnownType knownType = ProducerFactory.KnownType.forString(type);
+    if (knownType == null) {
+      return;
     }
+    final DataProducer p = ProducerFactory.create(knownType, new ProducerFactory.Params() {
+      @Override public File getSource() {
+        return src;
+      }
 
-    public void setConffile(Boolean conffile) {
-        this.conffile = conffile;
-    }
-    
-    public Boolean getConffile() {
-        return this.conffile;
-    }
+      @Override public String getDestination() {
+        return destinationName;
+      }
 
-    public void setDst(String destinationName) {
-        this.destinationName = destinationName;
-    }
+      @Override public String[] getIncludePatterns() {
+        return Data.this.getIncludePatterns(project);
+      }
 
-    public void addMapper(Mapper mapper) {
-        mapperWrapper.add(mapper);
-    }
+      @Override public String[] getExcludePatterns() {
+        return Data.this.getExcludePatterns(project);
+      }
 
-    public void produce( final DataConsumer pReceiver ) throws IOException {
-
-        if (src == null || !src.exists()) {
-            throw new FileNotFoundException("Data source not found : " + src);
-        }
-
-        final org.vafer.jdeb.mapping.Mapper[] mappers = new org.vafer.jdeb.mapping.Mapper[mapperWrapper.size()];
-        final Iterator<Mapper> it = mapperWrapper.iterator();
-        for (int i = 0; i < mappers.length; i++) {
-            mappers[i] = it.next().createMapper();
-        }
-
-        final Project project = getProject();
-
-        ProducerFactory.KnownType knownType = ProducerFactory.KnownType.forString(type);
-
-        if (knownType == null) {
-            return;
-        }
-
-        final DataProducer p = ProducerFactory.create(knownType, new ProducerFactory.Params() {
-            @Override
-            public File getSource() {
-                return src;
-            }
-            @Override
-            public String getDestination() {
-                return destinationName;
-            }
-            @Override
-            public String[] getIncludePatterns() {
-                return Data.this.getIncludePatterns(project);
-            }
-
-            @Override
-            public String[] getExcludePatterns() {
-                return Data.this.getExcludePatterns(project);
-            }
-
-            @Override
-            public org.vafer.jdeb.mapping.Mapper[] getMappers() {
-                return mappers;
-            }
-        });
-        p.produce(pReceiver);
-    }
+      @Override public org.vafer.jdeb.mapping.Mapper[] getMappers() {
+        return mappers;
+      }
+    });
+    p.produce(pReceiver);
+  }
 }
--- src/main/java/org/vafer/jdeb/producers/DataProducerFile.java
+++ src/main/java/org/vafer/jdeb/producers/DataProducerFile.java
@@ -1,24 +1,7 @@
-/*
- * Copyright 2013 The jdeb developers.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.vafer.jdeb.producers;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-
 import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
 import org.vafer.jdeb.DataConsumer;
 import org.vafer.jdeb.DataProducer;
@@ -31,32 +14,26 @@ import org.vafer.jdeb.mapping.Mapper;
  * @author Torsten Curdt
  */
 public final class DataProducerFile extends AbstractDataProducer implements DataProducer {
+  private final File file;
 
-    private final File file;
+  private final String destinationName;
 
-    private final String destinationName;
+  public DataProducerFile(final File pFile, String pDestinationName, String[] pIncludes, String[] pExcludes, Mapper[] pMapper) {
+    super(pIncludes, pExcludes, pMapper);
+    file = pFile;
+    destinationName = pDestinationName;
+  }
 
-    public DataProducerFile( final File pFile, String pDestinationName, String[] pIncludes, String[] pExcludes, Mapper[] pMapper ) {
-        super(pIncludes, pExcludes, pMapper);
-        file = pFile;
-        destinationName = pDestinationName;
+  public void produce(final DataConsumer pReceiver) throws IOException {
+    String fileName;
+    if (destinationName != null && destinationName.trim().length() > 0) {
+      fileName = destinationName.trim();
+    } else {
+      fileName = file.getName();
     }
-
-    public void produce( final DataConsumer pReceiver ) throws IOException {
-        String fileName;
-        if (destinationName != null && destinationName.trim().length() > 0) {
-            fileName = destinationName.trim();
-        } else {
-            fileName = file.getName();
-        }
-
-        TarArchiveEntry entry = Producers.defaultFileEntryWithName(fileName);
-
-        entry = map(entry);
-
-        entry.setSize(file.length());
-
-        Producers.produceInputStreamWithEntry(pReceiver, new FileInputStream(file), entry);
-    }
-
+    TarArchiveEntry entry = Producers.defaultFileEntryWithName(fileName);
+    entry = map(entry);
+    entry.setSize(file.length());
+    Producers.produceInputStreamWithEntry(pReceiver, new FileInputStream(file), entry);
+  }
 }
