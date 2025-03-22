4ae74aae5cda7406c102b29989dc8207f7cae149
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-22 01:55:41 +0000

    Mining Framework Analysis

--- scribejava-apis/src/main/java/com/github/scribejava/apis/ConstantContactApi2.java
+++ scribejava-apis/src/main/java/com/github/scribejava/apis/ConstantContactApi2.java
@@ -1,69 +1,7 @@
-package com.github.scribejava.apis;
-
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import com.github.scribejava.core.builder.api.DefaultApi20;
-import com.github.scribejava.core.exceptions.OAuthException;
-import com.github.scribejava.core.extractors.AccessTokenExtractor;
-import com.github.scribejava.core.model.AccessToken;
-import com.github.scribejava.core.model.OAuth2AccessToken;
-import com.github.scribejava.core.model.OAuthConfig;
-import com.github.scribejava.core.model.OAuthConstants;
-import com.github.scribejava.core.model.Token;
-import com.github.scribejava.core.model.Verb;
-import com.github.scribejava.core.utils.OAuthEncoder;
-import com.github.scribejava.core.utils.Preconditions;
-
-public class ConstantContactApi2 extends DefaultApi20 {
-
-    private static final String AUTHORIZE_URL
-            = "https://oauth2.constantcontact.com/oauth2/oauth/siteowner/authorize?client_id=%s&response_type=code&redirect_uri=%s";
-    private static final AccessTokenExtractor ACCESS_TOKEN_EXTRACTOR = new AccessTokenExtractor() {
-
-        @Override
-        public AccessToken extract(final String response) {
-            Preconditions.checkEmptyString(response, "Response body is incorrect. Can't extract a token from an empty string");
-
-            final String regex = "\"access_token\"\\s*:\\s*\"([^&\"]+)\"";
-            final Matcher matcher = Pattern.compile(regex).matcher(response);
-            if (matcher.find()) {
-                final String token = OAuthEncoder.decode(matcher.group(1));
-                return new OAuth2AccessToken(token, response);
-            } else {
-                throw new OAuthException("Response body is incorrect. Can't extract a token from this: '" + response + "'", null);
-            }
-        }
-    };
-
-    private ConstantContactApi2() {
-    }
-
-    private static class InstanceHolder {
-        private static final ConstantContactApi2 INSTANCE = new ConstantContactApi2();
-    }
-
-    public static ConstantContactApi2 instance() {
-        return InstanceHolder.INSTANCE;
-    }
-
-    @Override
-    public String getAccessTokenEndpoint() {
-        return "https://oauth2.constantcontact.com/oauth2/oauth/token?grant_type=" + OAuthConstants.AUTHORIZATION_CODE;
-    }
-
-    @Override
-    public String getAuthorizationUrl(final OAuthConfig config) {
-        return String.format(AUTHORIZE_URL, config.getApiKey(), OAuthEncoder.encode(config.getCallback()));
-    }
-
-    @Override
-    public Verb getAccessTokenVerb() {
-        return Verb.POST;
-    }
-
-    @Override
-    public AccessTokenExtractor getAccessTokenExtractor() {
-        return ACCESS_TOKEN_EXTRACTOR;
-    }
-}
+  package    com . github . scribejava . apis ;   import    java . util . regex . Matcher ;  import    java . util . regex . Pattern ;  import       com . github . scribejava . core . builder . api . DefaultApi20 ;  import      com . github . scribejava . core . exceptions . OAuthException ;  import      com . github . scribejava . core . extractors . AccessTokenExtractor ;  import      com . github . scribejava . core . model . AccessToken ;  import      com . github . scribejava . core . model . OAuth2AccessToken ;  import      com . github . scribejava . core . model . OAuthConfig ;  import      com . github . scribejava . core . model . OAuthConstants ;  import      com . github . scribejava . core . model . Token ;  import      com . github . scribejava . core . model . Verb ;  import      com . github . scribejava . core . utils . OAuthEncoder ;  import      com . github . scribejava . core . utils . Preconditions ;   public class ConstantContactApi2  extends DefaultApi20  {   private static final String  AUTHORIZE_URL = "https://oauth2.constantcontact.com/oauth2/oauth/siteowner/authorize?client_id=%s&response_type=code&redirect_uri=%s" ;    @ Override public String getAccessTokenEndpoint  ( )  {  return  "https://oauth2.constantcontact.com/oauth2/oauth/token?grant_type=" +  OAuthConstants . AUTHORIZATION_CODE ; }    @ Override public String getAuthorizationUrl  (   final OAuthConfig config )  {  return  String . format  ( AUTHORIZE_URL ,  config . getApiKey  ( ) ,  OAuthEncoder . encode  (  config . getCallback  ( ) ) ) ; }    @ Override public Verb getAccessTokenVerb  ( )  {  return  Verb . POST ; }    @ Override public AccessTokenExtractor getAccessTokenExtractor  ( )  {  return 
+<<<<<<<
+ new AccessTokenExtractor  ( )  {   public AccessToken extract  (  String response )  {   Preconditions . checkEmptyString  ( response , "Response body is incorrect. Can't extract a token from an empty string" ) ;  String  regex = "\"access_token\"\\s*:\\s*\"([^&\"]+)\"" ;  Matcher  matcher =   Pattern . compile  ( regex ) . matcher  ( response ) ;  if  (  matcher . find  ( ) )  {  String  token =  OAuthEncoder . decode  (  matcher . group  ( 1 ) ) ;  return  new OAuth2AccessToken  ( token , response ) ; } else  {  throw  new OAuthException  (   "Response body is incorrect. Can't extract a token from this: '" + response + "'" , null ) ; } } }
+=======
+ACCESS_TOKEN_EXTRACTOR
+>>>>>>>
+ ; }   private static final AccessTokenExtractor  ACCESS_TOKEN_EXTRACTOR =  new AccessTokenExtractor  ( )  {    @ Override public Token extract  (   final String response )  {   Preconditions . checkEmptyString  ( response , "Response body is incorrect. Can't extract a token from an empty string" ) ;   final String  regex = "\"access_token\"\\s*:\\s*\"([^&\"]+)\"" ;   final Matcher  matcher =   Pattern . compile  ( regex ) . matcher  ( response ) ;  if  (  matcher . find  ( ) )  {   final String  token =  OAuthEncoder . decode  (  matcher . group  ( 1 ) ) ;  return  new Token  ( token , "" , response ) ; } else  {  throw  new OAuthException  (   "Response body is incorrect. Can't extract a token from this: '" + response + "'" , null ) ; } } } ;   private ConstantContactApi2  ( )  { }   private static class InstanceHolder  {   private static final ConstantContactApi2  INSTANCE =  new ConstantContactApi2  ( ) ; }   public static ConstantContactApi2 instance  ( )  {  return  InstanceHolder . INSTANCE ; } }
