3c8db84cef75d8b62cc4044565db73c6c4e423b6
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-22 06:50:19 +0000

    Mining Framework Analysis

--- jgrapht-core/src/main/java/org/jgrapht/GraphTests.java
+++ jgrapht-core/src/main/java/org/jgrapht/GraphTests.java
@@ -20,7 +20,10 @@ package org.jgrapht;
 import org.jgrapht.alg.connectivity.BiconnectivityInspector;
 import org.jgrapht.alg.connectivity.ConnectivityInspector;
 import org.jgrapht.alg.connectivity.KosarajuStrongConnectivityInspector;
-import org.jgrapht.alg.cycle.*;
+import org.jgrapht.alg.cycle.HierholzerEulerianCycle;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * A collection of utilities to test for various graph properties.
@@ -477,22 +480,6 @@ public abstract class GraphTests
     }
 
     /**
-     * Checks whether a graph is chordal. A <a href="https://en.wikipedia.org/wiki/Chordal_graph">
-     * chordal graph</a> is one in which all cycles of four or more vertices have a chord, which
-     * is an edge that is not part of the cycle but connects two vertices of the cycle.
-     *
-     * @param graph the input graph
-     * @param <V> the graph vertex type
-     * @param <E> the graph edge type
-     * @return true if the graph is chordal, false otherwise
-     * @see ChordalityInspector#isChordal()
-     */
-    public static <V, E> boolean isChordal(Graph<V, E> graph){
-        Objects.requireNonNull(graph, GRAPH_CANNOT_BE_NULL);
-        return new ChordalityInspector<>(graph).isChordal();
-    }
-
-    /**
      * Tests whether an undirected graph meets Ore's condition to be Hamiltonian.
      *
      * Let G be a (finite and simple) graph with $n \geq 3$ vertices. We denote by deg(v) the degree of a vertex v in G,
@@ -520,6 +507,22 @@ public abstract class GraphTests
                     return false;
 
         return true;
+    }
+
+    /**
+     * Checks whether a graph is chordal. A <a href="https://en.wikipedia.org/wiki/Chordal_graph">
+     * chordal graph</a> is one in which all cycles of four or more vertices have a chord, which
+     * is an edge that is not part of the cycle but connects two vertices of the cycle.
+     *
+     * @param graph the input graph
+     * @param <V> the graph vertex type
+     * @param <E> the graph edge type
+     * @return true if the graph is chordal, false otherwise
+     * @see ChordalityInspector#isChordal()
+     */
+    public static <V, E> boolean isChordal(Graph<V, E> graph){
+        Objects.requireNonNull(graph, GRAPH_CANNOT_BE_NULL);
+        return new ChordalityInspector<>(graph).isChordal();
     }
 
     /**
