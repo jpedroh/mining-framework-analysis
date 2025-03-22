3e0dc57c6d22310eb528ffc545947bd1367fbd94
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-22 07:15:13 +0000

    Mining Framework Analysis

--- sm-core/src/main/java/com/salesmanager/core/business/repositories/catalog/product/type/ProductTypeRepository.java
+++ sm-core/src/main/java/com/salesmanager/core/business/repositories/catalog/product/type/ProductTypeRepository.java
@@ -1,11 +1,1 @@
-package com.salesmanager.core.business.repositories.catalog.product.type;
-
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.salesmanager.core.model.catalog.product.type.ProductType;
-
-public interface ProductTypeRepository extends JpaRepository<ProductType, Long> {
-
-	ProductType findByCode(String code);
-
-}
+  package        com . salesmanager . core . business . repositories . catalog . product . type ;   import   java . util . List ;  import      org . springframework . data . jpa . repository . JpaRepository ;  import        com . salesmanager . core . model . catalog . product . type . ProductType ;   public interface ProductTypeRepository  extends   JpaRepository  < ProductType , Long >  {  ProductType findByCode  (  String code ) ; }
