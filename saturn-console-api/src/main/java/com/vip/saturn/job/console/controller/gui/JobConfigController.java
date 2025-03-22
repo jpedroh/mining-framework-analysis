b631d43aa1dffa73f51b90d1b2ccf93eefa38b68
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-22 07:34:51 +0000

    Mining Framework Analysis

--- saturn-console-api/src/main/java/com/vip/saturn/job/console/controller/gui/JobConfigController.java
+++ saturn-console-api/src/main/java/com/vip/saturn/job/console/controller/gui/JobConfigController.java
@@ -1,5 +1,4 @@
 package com.vip.saturn.job.console.controller.gui;
-
 import com.vip.saturn.job.console.aop.annotation.Audit;
 import com.vip.saturn.job.console.aop.annotation.AuditParam;
 import com.vip.saturn.job.console.aop.annotation.AuditType;
@@ -15,39 +14,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
 /**
- * Job config related operations.
- *
  * @author hebelala
  */
-@Controller
-@RequestMapping("/console/{namespace:.+}/jobs/{jobName}/config")
-public class JobConfigController extends AbstractGUIController {
+@Controller @RequestMapping(value = "/console/{namespace:.+}/jobs/{jobName}/config") public class JobConfigController extends AbstractGUIController {
+  @Resource private JobService jobService;
 
-	@Resource
-	private JobService jobService;
+  @ApiResponses(value = { @ApiResponse(code = 200, message = "Success/Fail", response = RequestResult.class) }) @GetMapping public SuccessResponseEntity getJobConfig(final HttpServletRequest request, @AuditParam(value = "namespace") @PathVariable String namespace, @AuditParam(value = "jobName") @PathVariable String jobName) throws SaturnJobConsoleException {
+    return new SuccessResponseEntity(jobService.getJobConfigVo(namespace, jobName));
+  }
 
-	@ApiResponses(value = {@ApiResponse(code = 200, message = "Success/Fail", response = RequestResult.class)})
-	@GetMapping
-	public SuccessResponseEntity getJobConfig(final HttpServletRequest request,
-			@AuditParam("namespace") @PathVariable String namespace,
-			@AuditParam("jobName") @PathVariable String jobName) throws SaturnJobConsoleException {
-		return new SuccessResponseEntity(jobService.getJobConfigVo(namespace, jobName));
-	}
-
-	@ApiResponses(value = {@ApiResponse(code = 200, message = "Success/Fail", response = RequestResult.class)})
-	@Audit
-	@PostMapping
-	public SuccessResponseEntity updateJobConfig(final HttpServletRequest request,
-			@AuditParam("namespace") @PathVariable String namespace,
-			@AuditParam("jobName") @PathVariable String jobName, JobConfig jobConfig)
-			throws SaturnJobConsoleException {
-		jobService.updateJobConfig(namespace, jobConfig);
-		return new SuccessResponseEntity();
-	}
-
+  @Audit @ApiResponses(value = { @ApiResponse(code = 200, message = "Success/Fail", response = RequestResult.class) }) @PostMapping public SuccessResponseEntity updateJobConfig(final HttpServletRequest request, @AuditParam(value = "namespace") @PathVariable String namespace, @AuditParam(value = "jobName") @PathVariable String jobName, JobConfig jobConfig) throws SaturnJobConsoleException {
+    jobService.updateJobConfig(namespace, jobConfig);
+    return new SuccessResponseEntity();
+  }
 }
