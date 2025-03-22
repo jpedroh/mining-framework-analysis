abac4f399773c5fb1f2c254cf2b04302b60c191e
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-22 00:16:19 +0000

    Mining Framework Analysis

--- yoga-integration/yoga-springmvc/src/main/java/org/skyscreamer/yoga/springmvc/view/XhtmlSelectorView.java
+++ yoga-integration/yoga-springmvc/src/main/java/org/skyscreamer/yoga/springmvc/view/XhtmlSelectorView.java
@@ -1,8 +1,6 @@
 package org.skyscreamer.yoga.springmvc.view;
-
 import java.io.IOException;
 import java.io.OutputStream;
-
 import org.dom4j.Element;
 import org.dom4j.dom.DOMDocument;
 import org.dom4j.dom.DOMElement;
@@ -12,52 +10,36 @@ import org.skyscreamer.yoga.mapper.XhtmlHierarchyModel;
 import org.skyscreamer.yoga.selector.Selector;
 import org.skyscreamer.yoga.util.NameUtil;
 
-public class XhtmlSelectorView extends AbstractYogaView
-{
-
-   @Override
-   public void render(OutputStream outputStream, Selector selector, Object value)
-         throws IOException
-   {
-      DOMDocument domDocument = new DOMDocument();
-      Element rootElement = new DOMElement( "html" );
-      domDocument.setRootElement( rootElement );
-      Element cssLink = rootElement.addElement( "head" ).addElement( "link" );
-      cssLink.addAttribute( "href", "/css/xhtml.css" );
-      cssLink.addAttribute( "rel", "stylesheet" );
-      Element body = rootElement.addElement( "body" );
-
-      if (value instanceof Iterable)
-      {
-         for (Object child : (Iterable<?>) value)
-         {
-            traverse( child, selector, resultTraverser, body );
-         }
+public class XhtmlSelectorView extends AbstractYogaView {
+  @Override public void render(OutputStream outputStream, Selector selector, Object value) throws IOException {
+    DOMDocument domDocument = new DOMDocument();
+    Element rootElement = new DOMElement("html");
+    domDocument.setRootElement(rootElement);
+    Element cssLink = rootElement.addElement("head").addElement("link");
+    cssLink.addAttribute("href", "/css/xhtml.css");
+    cssLink.addAttribute("rel", "stylesheet");
+    Element body = rootElement.addElement("body");
+    if (value instanceof Iterable) {
+      for (Object child : (Iterable<?>) value) {
+        traverse(child, selector, resultTraverser, body);
       }
-      else
-      {
-         traverse( value, selector, resultTraverser, body );
-      }
-      write( outputStream, domDocument );
-   }
+    } else {
+      traverse(value, selector, resultTraverser, body);
+    }
+    write(outputStream, domDocument);
+  }
 
-   public void traverse(Object value, Selector selector, ResultTraverser traverser, Element body)
-   {
-      String name = NameUtil.getName( traverser.getClass( value ) );
-      HierarchicalModel model = new XhtmlHierarchyModel( body.addElement( "div" ).addAttribute(
-            "class", name ) );
-      traverser.traverse( value, selector, model, getHrefSuffix() );
-   }
+  public void traverse(Object value, Selector selector, ResultTraverser traverser, Element body) {
+    String name = NameUtil.getName(traverser.getClass(value));
+    HierarchicalModel model = new XhtmlHierarchyModel(body.addElement("div").addAttribute("class", name));
+    traverser.traverse(value, selector, model, getHrefSuffix());
+  }
 
-   @Override
-   public String getContentType()
-   {
-      return "text/html";
-   }
+  @Override public String getContentType() {
+    return "text/html";
+  }
 
-   @Override
-   public String getHrefSuffix()
-   {
-      return "xhtml";
-   }
+  @Override public String getHrefSuffix() {
+    return "xhtml";
+  }
 }
