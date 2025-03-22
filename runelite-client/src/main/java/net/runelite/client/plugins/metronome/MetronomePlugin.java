09c6e05b6ba78203abbf7379a8d705205f7f5cb3
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-22 06:35:48 +0000

    Mining Framework Analysis

--- runelite-client/src/main/java/net/runelite/client/plugins/metronome/MetronomePlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/metronome/MetronomePlugin.java
@@ -1,30 +1,4 @@
-/*
- * Copyright (c) 2018, SomeoneWithAnInternetConnection
- * Copyright (c) 2018, oplosthee <https://github.com/oplosthee>
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice, this
- *    list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
 package net.runelite.client.plugins.metronome;
-
 import com.google.inject.Provides;
 import javax.inject.Inject;
 import net.runelite.api.Client;
@@ -36,88 +10,59 @@ import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.client.ui.overlay.OverlayManager;
 
-@PluginDescriptor(
-	name = "Metronome",
-	description = "Play a sound on a specified tick to aid in efficient skilling and prayer flicking",
-	tags = {"skilling", "tick", "timers", "prayer", "flick"},
-	enabledByDefault = false
-)
-public class MetronomePlugin extends Plugin
-{
-	@Inject
-	private Client client;
+@PluginDescriptor(name = "Metronome", description = "Play a sound on a specified tick to aid in efficient skilling and prayer flicking", tags = { "skilling", "tick", "timers", "prayer", "flick" }, enabledByDefault = false) public class MetronomePlugin extends Plugin {
+  @Inject private Client client;
 
-	@Inject
-	private MetronomePluginConfiguration config;
+  @Inject private MetronomePluginConfiguration config;
 
-	@Inject
-	private MetronomeOverlay ovr;
+  @Inject private MetronomeOverlay ovr;
 
-	@Inject
-	private OverlayManager overlayManager;
+  @Inject private OverlayManager overlayManager;
 
-	private int tickCounter = 0;
-	private boolean shouldTock = false;
-	private boolean currentColorGreen = false;
+  private int tickCounter = 0;
 
-	@Provides
-	MetronomePluginConfiguration provideConfig(ConfigManager configManager)
-	{
-		return configManager.getConfig(MetronomePluginConfiguration.class);
-	}
+  private boolean shouldTock = false;
 
-	@Override
-	protected void startUp() throws Exception
-	{
-		overlayManager.add(ovr);
-	}
+  private boolean currentColorGreen = false;
 
-	@Override
-	protected void shutDown() throws Exception
-	{
-		overlayManager.remove(ovr);
-	}
+  @Provides MetronomePluginConfiguration provideConfig(ConfigManager configManager) {
+    return configManager.getConfig(MetronomePluginConfiguration.class);
+  }
 
-	@Subscribe
-	public void onGameTick(GameTick tick)
-	{
-		currentColorGreen = !currentColorGreen;
+  @Override protected void startUp() throws Exception {
+    overlayManager.add(ovr);
+  }
 
-		if (config.tickCount() == 0)
-		{
-			return;
-		}
+  @Override protected void shutDown() throws Exception {
+    overlayManager.remove(ovr);
+  }
 
-		if (++tickCounter % config.tickCount() == 0)
-		{
-			// As playSoundEffect only uses the volume argument when the in-game volume isn't muted, sound effect volume
-			// needs to be set to the value desired for ticks or tocks and afterwards reset to the previous value.
-			int previousVolume = client.getSoundEffectVolume();
+  @Subscribe public void onGameTick(GameTick tick) {
+    currentColorGreen = !currentColorGreen;
+    if (config.tickCount() == 0) {
+      return;
+    }
+    if (++tickCounter % config.tickCount() == 0) {
+      int previousVolume = client.getSoundEffectVolume();
+      if (shouldTock && config.tockVolume() > 0) {
+        client.setSoundEffectVolume(config.tockVolume());
+        client.playSoundEffect(SoundEffectID.GE_DECREMENT_PLOP, config.tockVolume());
+      } else {
+        if (config.tickVolume() > 0) {
+          client.setSoundEffectVolume(config.tickVolume());
+          client.playSoundEffect(SoundEffectID.GE_INCREMENT_PLOP, config.tickVolume());
+        }
+      }
+      client.setSoundEffectVolume(previousVolume);
+      shouldTock = !shouldTock;
+    }
+  }
 
-			if (shouldTock && config.tockVolume() > 0)
-			{
-				client.setSoundEffectVolume(config.tockVolume());
-				client.playSoundEffect(SoundEffectID.GE_DECREMENT_PLOP, config.tockVolume());
-			}
-			else if (config.tickVolume() > 0)
-			{
-				client.setSoundEffectVolume(config.tickVolume());
-				client.playSoundEffect(SoundEffectID.GE_INCREMENT_PLOP, config.tickVolume());
-			}
+  public int getTickCounter() {
+    return tickCounter;
+  }
 
-			client.setSoundEffectVolume(previousVolume);
-
-			shouldTock = !shouldTock;
-		}
-	}
-
-	public int getTickCounter()
-	{
-		return tickCounter;
-	}
-
-	public boolean getIsCurrentColorGreen()
-	{
-		return currentColorGreen;
-	}
+  public boolean getIsCurrentColorGreen() {
+    return currentColorGreen;
+  }
 }
