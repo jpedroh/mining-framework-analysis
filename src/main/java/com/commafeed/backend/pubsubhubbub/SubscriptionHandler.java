d7125a4753ea4a3acb0eff487c1581b676919206
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-22 10:31:41 +0000

    Mining Framework Analysis

--- src/main/java/com/commafeed/backend/pubsubhubbub/SubscriptionHandler.java
+++ src/main/java/com/commafeed/backend/pubsubhubbub/SubscriptionHandler.java
@@ -1,10 +1,7 @@
 package com.commafeed.backend.pubsubhubbub;
-
 import java.util.List;
-
 import javax.inject.Inject;
 import javax.ws.rs.core.MediaType;
-
 import org.apache.commons.lang.StringUtils;
 import org.apache.http.HttpHeaders;
 import org.apache.http.HttpResponse;
@@ -16,8 +13,8 @@ import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import com.commafeed.backend.HttpGetter;
+import com.commafeed.backend.dao.FeedDAO;
 import com.commafeed.backend.feeds.FeedRefreshTaskGiver;
 import com.commafeed.backend.feeds.FeedUtils;
 import com.commafeed.backend.model.Feed;
@@ -25,77 +22,54 @@ import com.commafeed.backend.services.ApplicationSettingsService;
 import com.google.common.collect.Lists;
 
 public class SubscriptionHandler {
+  private static Logger log = LoggerFactory.getLogger(SubscriptionHandler.class);
 
-	private static Logger log = LoggerFactory
-			.getLogger(SubscriptionHandler.class);
+  @Inject ApplicationSettingsService applicationSettingsService;
 
-	@Inject
-	ApplicationSettingsService applicationSettingsService;
+  @Inject FeedRefreshTaskGiver taskGiver;
 
-	@Inject
-	FeedRefreshTaskGiver taskGiver;
-
-	public void subscribe(Feed feed) {
-
-		try {
-			// make sure the feed has been updated in the database so that the
-			// callback works
-			Thread.sleep(30000);
-		} catch (InterruptedException e1) {
-			// do nothing
-		}
-
-		String hub = feed.getPushHub();
-		String topic = feed.getPushTopic();
-		String publicUrl = FeedUtils
-				.removeTrailingSlash(applicationSettingsService.get()
-						.getPublicUrl());
-
-		log.debug("sending new pubsub subscription to {} for {}", hub, topic);
-
-		HttpPost post = new HttpPost(hub);
-		List<NameValuePair> nvp = Lists.newArrayList();
-		nvp.add(new BasicNameValuePair("hub.callback", publicUrl
-				+ "/rest/push/callback"));
-		nvp.add(new BasicNameValuePair("hub.topic", topic));
-		nvp.add(new BasicNameValuePair("hub.mode", "subscribe"));
-		nvp.add(new BasicNameValuePair("hub.verify", "async"));
-		nvp.add(new BasicNameValuePair("hub.secret", ""));
-		nvp.add(new BasicNameValuePair("hub.verify_token", ""));
-		nvp.add(new BasicNameValuePair("hub.lease_seconds", ""));
-
-		post.setHeader(HttpHeaders.USER_AGENT, "CommaFeed");
-		post.setHeader(HttpHeaders.CONTENT_TYPE,
-				MediaType.APPLICATION_FORM_URLENCODED);
-
-		HttpClient client = HttpGetter.newClient();
-		try {
-			post.setEntity(new UrlEncodedFormEntity(nvp));
-			HttpResponse response = client.execute(post);
-
-			int code = response.getStatusLine().getStatusCode();
-			if (code != 204 && code != 202 && code != 200) {
-				String message = EntityUtils.toString(response.getEntity());
-				String pushpressError = " is value is not allowed.  You may only subscribe to";
-				if (code == 400
-						&& StringUtils.contains(message, pushpressError)) {
-					String[] tokens = message.split(" ");
-					feed.setPushTopic(tokens[tokens.length - 1]);
-					taskGiver.giveBack(feed);
-					log.debug("handled pushpress subfeed {} : {}", topic,
-							feed.getPushTopic());
-				} else {
-					throw new Exception("Unexpected response code: " + code
-							+ " " + response.getStatusLine().getReasonPhrase()
-							+ " - " + message);
-				}
-			}
-			log.debug("subscribed to {} for {}", hub, topic);
-		} catch (Exception e) {
-			log.error("Could not subscribe to {} for {} : " + e.getMessage(),
-					hub, topic);
-		} finally {
-			client.getConnectionManager().shutdown();
-		}
-	}
+  public void subscribe(Feed feed) {
+    try {
+      Thread.sleep(30000);
+    } catch (InterruptedException e1) {
+    }
+    String hub = feed.getPushHub();
+    String topic = feed.getPushTopic();
+    String publicUrl = FeedUtils.removeTrailingSlash(applicationSettingsService.get().getPublicUrl());
+    log.debug("sending new pubsub subscription to {} for {}", hub, topic);
+    HttpPost post = new HttpPost(hub);
+    List<NameValuePair> nvp = Lists.newArrayList();
+    nvp.add(new BasicNameValuePair("hub.callback", publicUrl + "/rest/push/callback"));
+    nvp.add(new BasicNameValuePair("hub.topic", topic));
+    nvp.add(new BasicNameValuePair("hub.mode", "subscribe"));
+    nvp.add(new BasicNameValuePair("hub.verify", "async"));
+    nvp.add(new BasicNameValuePair("hub.secret", ""));
+    nvp.add(new BasicNameValuePair("hub.verify_token", ""));
+    nvp.add(new BasicNameValuePair("hub.lease_seconds", ""));
+    post.setHeader(HttpHeaders.USER_AGENT, "CommaFeed");
+    post.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED);
+    HttpClient client = HttpGetter.newClient();
+    try {
+      post.setEntity(new UrlEncodedFormEntity(nvp));
+      HttpResponse response = client.execute(post);
+      int code = response.getStatusLine().getStatusCode();
+      if (code != 204 && code != 202 && code != 200) {
+        String message = EntityUtils.toString(response.getEntity());
+        String pushpressError = " is value is not allowed.  You may only subscribe to";
+        if (code == 400 && StringUtils.contains(message, pushpressError)) {
+          String[] tokens = message.split(" ");
+          feed.setPushTopic(tokens[tokens.length - 1]);
+          taskGiver.giveBack(feed);
+          log.debug("handled pushpress subfeed {} : {}", topic, feed.getPushTopic());
+        } else {
+          throw new Exception("Unexpected response code: " + code + " " + response.getStatusLine().getReasonPhrase() + " - " + message);
+        }
+      }
+      log.debug("subscribed to {} for {}", hub, topic);
+    } catch (Exception e) {
+      log.error("Could not subscribe to {} for {} : " + e.getMessage(), hub, topic);
+    } finally {
+      client.getConnectionManager().shutdown();
+    }
+  }
 }
