c9fb0f884fa703b360147ea777a1c429fb7bcb14
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-22 06:39:48 +0000

    Mining Framework Analysis

--- jgrapht-core/src/main/java/org/jgrapht/alg/PathValidator.java
+++ jgrapht-core/src/main/java/org/jgrapht/alg/PathValidator.java
@@ -1,22 +1,4 @@
-/*
- * (C) Copyright 2016-2016, by Assaf Mizrachi and Contributors.
- *
- * JGraphT : a free Java graph-theory library
- *
- * This program and the accompanying materials are dual-licensed under
- * either
- *
- * (a) the terms of the GNU Lesser General Public License version 2.1
- * as published by the Free Software Foundation, or (at your option) any
- * later version.
- *
- * or (per the licensee's choosing)
- *
- * (b) the terms of the Eclipse Public License v1.0 as published by
- * the Eclipse Foundation.
- */
 package org.jgrapht.alg;
-
 import org.jgrapht.*;
 
 /**
@@ -31,26 +13,20 @@ import org.jgrapht.*;
  * @since July, 21, 2016
  *
  */
-public interface PathValidator<V, E>
-{
-
-    /**
+public interface PathValidator<V extends java.lang.Object, E extends java.lang.Object> {
+  /**
      * Checks if an edge can be added to a previous path element.
      * 
-<<<<<<< HEAD
      * @param partialPath the path from source vertex up to the current vertex.
      * @param edge the new edge to be added to the path.
      * 
      * @return <code>true</code> if edge can be added, <code>false</code> otherwise.
      */
-    public boolean isValidPath(GraphPath<V, E> partialPath, E edge);
-}
+  public boolean isValidPath(
+<<<<<<< /usr/src/app/output/jgrapht/jgrapht/b7c525bdcb82359ff37e9660cc48a8a13656e062/jgrapht-core/src/main/java/org/jgrapht/alg/PathValidator.java/left.java
+  GraphPath<V, E> partialPath
 =======
-     * @param prevPath the path from source vertex up to the new edge.
-     * @param edge the edge to be added to the path.
-     * 
-     * @return <code>true</code> if edge can be added, <code>false</code> otherwise.
-     */
-    public boolean isValidPath(GraphPath<V, E> prevPath, E edge);
+  GraphPath<V, E> prevPath
+>>>>>>> /usr/src/app/output/jgrapht/jgrapht/b7c525bdcb82359ff37e9660cc48a8a13656e062/jgrapht-core/src/main/java/org/jgrapht/alg/PathValidator.java/right.java
+  , E edge);
 }
->>>>>>> bb2518936fea6fc4262a23259462def8e83efae5
