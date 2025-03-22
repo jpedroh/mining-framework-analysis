c54c52c177d69f35f3c70103de162f1c17b1fdcf
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-22 05:43:06 +0000

    Mining Framework Analysis

--- jgrapht-core/src/test/java/org/jgrapht/alg/AllAlgTests.java
+++ jgrapht-core/src/test/java/org/jgrapht/alg/AllAlgTests.java
@@ -36,8 +36,8 @@ import org.junit.runners.*;
     FloydWarshallShortestPathsTest.class, GusfieldEquivalentFlowTreeTest.class, GusfieldGomoryHuCutTreeTest.class,
     HamiltonianCycleTest.class, KShortestPathCostTest.class,
     KShortestPathKValuesTest.class, KSPDiscardsValidPathsTest.class, KSPExampleTest.class,
-    NaiveLcaFinderTest.class, NeighborIndexTest.class, PadbergRaoOddMinimumCutsetTest.class,
-    PushRelabelMFImplTest.class, PushRelabelMinimumSTCutTest.class, StoerWagnerMinimumCutTest.class,
+    NaiveLcaFinderTest.class, NeighborIndexTest.class, PadbergRaoOddMinimumCutsetTest.class, PushRelabelMFImplTest.class,
+    PushRelabelMinimumSTCutTest.class, StoerWagnerMinimumCutTest.class,
     StrongConnectivityAlgorithmTest.class, TarjanLowestCommonAncestorTest.class,
     TransitiveClosureTest.class, VertexCoverTest.class, WeightedVertexCoverTest.class })
 public final class AllAlgTests
