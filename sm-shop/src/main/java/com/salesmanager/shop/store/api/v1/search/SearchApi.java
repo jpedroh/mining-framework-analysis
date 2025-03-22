0200ec0a6db20ab06a3a8b7a18e74dd410e5c5e8
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-22 06:11:14 +0000

    Mining Framework Analysis

--- sm-shop/src/main/java/com/salesmanager/shop/store/api/v1/search/SearchApi.java
+++ sm-shop/src/main/java/com/salesmanager/shop/store/api/v1/search/SearchApi.java
@@ -1,27 +1,44 @@
 package com.salesmanager.shop.store.api.v1.search;
 
-
-import static com.salesmanager.core.business.constants.Constants.DEFAULT_STORE;
-
 import io.swagger.annotations.ApiImplicitParam;
+
 import io.swagger.annotations.ApiImplicitParams;
+
 import javax.inject.Inject;
+
 import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
+
 import org.slf4j.LoggerFactory;
+
 import org.springframework.web.bind.annotation.PostMapping;
+
 import org.springframework.web.bind.annotation.RequestBody;
+
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import org.springframework.web.bind.annotation.ResponseBody;
+
 import org.springframework.web.bind.annotation.RestController;
+
 import com.salesmanager.core.model.merchant.MerchantStore;
+
 import com.salesmanager.core.model.reference.language.Language;
+
 import com.salesmanager.shop.model.ValueList;
+
 import com.salesmanager.shop.model.catalog.SearchProductList;
+
 import com.salesmanager.shop.model.catalog.SearchProductRequest;
+
 import com.salesmanager.shop.store.controller.search.facade.SearchFacade;
+
 import com.salesmanager.shop.store.controller.store.facade.StoreFacade;
+
 import com.salesmanager.shop.utils.LanguageUtils;
+
+import springfox.documentation.annotations.ApiIgnore;
 
 /**
  * Api for searching shopizer catalog based on search term when filtering products based on product
