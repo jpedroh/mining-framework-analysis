36e72f0049cd1e24dd2a299cac048e2b8d16ee25
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-22 07:53:11 +0000

    Mining Framework Analysis

--- src/test/java/com/michelboudreau/test/AlternatorTestSuite.java
+++ src/test/java/com/michelboudreau/test/AlternatorTestSuite.java
@@ -1,10 +1,13 @@
 package com.michelboudreau.test;
-
 import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-@RunWith(Suite.class)
-@Suite.SuiteClasses({ AlternatorTableTest.class , AlternatorItemTest.class , AlternatorQueryTest.class, AlternatorScanTest.class, AlternatorBatchItemTest.class, AlternatorSaveRestoreTest.class, AlternatorMapperTest.class})
-public class AlternatorTestSuite {
+@RunWith(value = Suite.class) @Suite.SuiteClasses(value = { AlternatorTableTest.class, AlternatorItemTest.class, AlternatorQueryTest.class, AlternatorScanTest.class, AlternatorBatchItemTest.class, 
+<<<<<<< /usr/src/app/output/mboudreau/alternator/9adf68eb8af7a41af55b72a8cd6a5b6191b7026c/src/test/java/com/michelboudreau/test/AlternatorTestSuite.java/left.java
+AlternatorSaveRestoreTest
+=======
+AlternatorMapperTest
+>>>>>>> /usr/src/app/output/mboudreau/alternator/9adf68eb8af7a41af55b72a8cd6a5b6191b7026c/src/test/java/com/michelboudreau/test/AlternatorTestSuite.java/right.java
+.class }) public class AlternatorTestSuite {
 }
