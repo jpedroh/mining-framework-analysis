99a8203fc25e40ad936ee0fe7faf43f82da93362
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-22 10:43:17 +0000

    Mining Framework Analysis

--- fluentlenium-core/src/main/java/org/fluentlenium/core/wait/FluentSizeBuilder.java
+++ fluentlenium-core/src/main/java/org/fluentlenium/core/wait/FluentSizeBuilder.java
@@ -1,115 +1,43 @@
-package org.fluentlenium.core.wait;
-
-import com.google.common.base.Predicate;
-import org.fluentlenium.core.Fluent;
-
-import static org.fluentlenium.core.wait.FluentWaitMessages.equalToMessage;
-import static org.fluentlenium.core.wait.FluentWaitMessages.greatherThanMessage;
-import static org.fluentlenium.core.wait.FluentWaitMessages.greatherThanOrEqualToMessage;
-import static org.fluentlenium.core.wait.FluentWaitMessages.lessThanMessage;
-import static org.fluentlenium.core.wait.FluentWaitMessages.lessThanOrEqualToMessage;
-import static org.fluentlenium.core.wait.FluentWaitMessages.notEqualToMessage;
-
-public class FluentSizeBuilder {
-
-    private AbstractWaitElementMatcher parent;
-    private String selection;
-    private FluentWait wait;
-
-    public FluentSizeBuilder(AbstractWaitElementMatcher parent, FluentWait fluentWait, String selection) {
-        this.parent = parent;
-        this.selection = selection;
-        this.wait = fluentWait;
-    }
-
-
-    /**
-     * Equals
-     *
-     * @param size size value
-     */
-    public void equalTo(final int size) {
-        Predicate<Fluent> isPresent = new com.google.common.base.Predicate<Fluent>() {
-            public boolean apply(Fluent fluent) {
-                return getSize() == size;
-            }
-        };
-        parent.until(wait, isPresent, equalToMessage(selection, size));
-    }
-
-    /**
-     * Not equals
-     *
-     * @param size size value
-     */
-    public void notEqualTo(final int size) {
-        Predicate<Fluent> isPresent = new com.google.common.base.Predicate<Fluent>() {
-            public boolean apply(Fluent fluent) {
-                return getSize() != size;
-            }
-        };
-        parent.until(wait, isPresent, notEqualToMessage(selection, size));
-    }
-
-    /**
-     * Less than
-     *
-     * @param size size value
-     */
-    public void lessThan(final int size) {
-        Predicate<Fluent> isPresent = new com.google.common.base.Predicate<Fluent>() {
-            public boolean apply(Fluent fluent) {
-                return getSize() < size;
-            }
-        };
-        parent.until(wait, isPresent, lessThanMessage(selection, size));
-    }
-
-    /**
-     * Less than or equals
-     *
-     * @param size size value
-     */
-    public void lessThanOrEqualTo(final int size) {
-        Predicate<Fluent> isPresent = new com.google.common.base.Predicate<Fluent>() {
-            public boolean apply(Fluent fluent) {
-                return getSize() <= size;
-            }
-        };
-        parent.until(wait, isPresent, lessThanOrEqualToMessage(selection, size));
-    }
-
-    /**
-     * Greater than
-     *
-     * @param size size value
-     */
-    public void greaterThan(final int size) {
-        Predicate<Fluent> isPresent = new com.google.common.base.Predicate<Fluent>() {
-            public boolean apply(Fluent fluent) {
-                return getSize() > size;
-            }
-        };
-        parent.until(wait, isPresent, greatherThanMessage(selection, size));
-    }
-
-    /**
-     * Greater than or equals
-     *
-     * @param size size value
-     */
-    public void greaterThanOrEqualTo(final int size) {
-        Predicate<Fluent> isPresent = new com.google.common.base.Predicate<Fluent>() {
-            public boolean apply(Fluent fluent) {
-                return getSize() >= size;
-            }
-        };
-        parent.until(wait, isPresent, greatherThanOrEqualToMessage(selection, size));
-    }
-
-    private int getSize() {
-        return parent.find().size();
-    }
-
-
-}
+  package    org . fluentlenium . core . wait ;   import     com . google . common . base . Predicate ;  import    org . fluentlenium . core . Fluent ;  import    org . openqa . selenium . By ;  import static      org . fluentlenium . core . wait . WaitMessage . equalToMessage ;  import static      org . fluentlenium . core . wait . WaitMessage . greatherThanMessage ;  import static      org . fluentlenium . core . wait . WaitMessage . greatherThanOrEqualToMessage ;  import static      org . fluentlenium . core . wait . WaitMessage . lessThanMessage ;  import static      org . fluentlenium . core . wait . WaitMessage . lessThanOrEqualToMessage ;  import static      org . fluentlenium . core . wait . WaitMessage . notEqualToMessage ;  import static      org . fluentlenium . core . wait . FluentWaitMessages . equalToMessage ;  import static      org . fluentlenium . core . wait . FluentWaitMessages . greatherThanMessage ;  import static      org . fluentlenium . core . wait . FluentWaitMessages . greatherThanOrEqualToMessage ;  import static      org . fluentlenium . core . wait . FluentWaitMessages . lessThanMessage ;  import static      org . fluentlenium . core . wait . FluentWaitMessages . lessThanOrEqualToMessage ;  import static      org . fluentlenium . core . wait . FluentWaitMessages . notEqualToMessage ;   public class FluentSizeBuilder  {   private By  locator ;   private FluentWait  wait ;   public FluentSizeBuilder  (  Search search ,  FluentWait fluentWait ,  By locator ,   List  < Filter > filters )  {    this . locator = locator ;    this . wait = fluentWait ;    this . search = search ;    this . filters = filters ; }   public void equalTo  (   final  int size )  {   Predicate  < Fluent >  isPresent =  new      com . google . common . base . Predicate  < Fluent >  ( )  {   public boolean apply  (  Fluent fluent )  {  return   getSize  ( ) == size ; } } ;   parent . until  ( wait , isPresent ,  equalToMessage  ( 
+<<<<<<<
+locator
+=======
+selection
+>>>>>>>
+ , size ) ) ; }   public void notEqualTo  (   final  int size )  {   Predicate  < Fluent >  isPresent =  new      com . google . common . base . Predicate  < Fluent >  ( )  {   public boolean apply  (  Fluent fluent )  {  return   getSize  ( ) != size ; } } ;   parent . until  ( wait , isPresent ,  notEqualToMessage  ( 
+<<<<<<<
+locator
+=======
+selection
+>>>>>>>
+ , size ) ) ; }   public void lessThan  (   final  int size )  {   Predicate  < Fluent >  isPresent =  new      com . google . common . base . Predicate  < Fluent >  ( )  {   public boolean apply  (  Fluent fluent )  {  return   getSize  ( ) < size ; } } ;   parent . until  ( wait , isPresent ,  lessThanMessage  ( 
+<<<<<<<
+locator
+=======
+selection
+>>>>>>>
+ , size ) ) ; }   public void lessThanOrEqualTo  (   final  int size )  {   Predicate  < Fluent >  isPresent =  new      com . google . common . base . Predicate  < Fluent >  ( )  {   public boolean apply  (  Fluent fluent )  {  return   getSize  ( ) <= size ; } } ;   parent . until  ( wait , isPresent ,  lessThanOrEqualToMessage  ( 
+<<<<<<<
+locator
+=======
+selection
+>>>>>>>
+ , size ) ) ; }   public void greaterThan  (   final  int size )  {   Predicate  < Fluent >  isPresent =  new      com . google . common . base . Predicate  < Fluent >  ( )  {   public boolean apply  (  Fluent fluent )  {  return   getSize  ( ) > size ; } } ;   parent . until  ( wait , isPresent ,  greatherThanMessage  ( 
+<<<<<<<
+locator
+=======
+selection
+>>>>>>>
+ , size ) ) ; }   public void greaterThanOrEqualTo  (   final  int size )  {   Predicate  < Fluent >  isPresent =  new      com . google . common . base . Predicate  < Fluent >  ( )  {   public boolean apply  (  Fluent fluent )  {  return   getSize  ( ) >= size ; } } ;   parent . until  ( wait , isPresent ,  greatherThanOrEqualToMessage  ( 
+<<<<<<<
+locator
+=======
+selection
+>>>>>>>
+ , size ) ) ; }   private  int getSize  ( )  { 
+<<<<<<<
+ if  (   filters . size  ( ) > 0 )  {  return   search . find  ( locator ,  (  Filter  [ ] )  filters . toArray  (  new Filter  [  filters . size  ( ) ] ) ) . size  ( ) ; } else  {  return   search . find  ( locator ) . size  ( ) ; }
+=======
+ return   parent . find  ( ) . size  ( ) ;
+>>>>>>>
+ }   private AbstractWaitElementMatcher  parent ;   private String  selection ;   public FluentSizeBuilder  (  AbstractWaitElementMatcher parent ,  FluentWait fluentWait ,  String selection )  {    this . parent = parent ;    this . selection = selection ;    this . wait = fluentWait ; } }
