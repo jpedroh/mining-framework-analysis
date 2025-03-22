46cd27a48dd5793ff5528174c456061afc315e78
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-22 01:09:07 +0000

    Mining Framework Analysis

--- core/src/main/java/org/movsim/simulator/roadnetwork/Lanes.java
+++ core/src/main/java/org/movsim/simulator/roadnetwork/Lanes.java
@@ -1,29 +1,3 @@
-/*
- * Copyright (C) 2010, 2011, 2012 by Arne Kesting, Martin Treiber, Ralph Germ, Martin Budden
- * <movsim.org@gmail.com>
- * -----------------------------------------------------------------------------------------
- * 
- * This file is part of
- * 
- * MovSim - the multi-model open-source vehicular-traffic simulator.
- * 
- * MovSim is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * MovSim is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with MovSim. If not, see <http://www.gnu.org/licenses/>
- * or <http://www.movsim.org>.
- * 
- * -----------------------------------------------------------------------------------------
- */
-
 package org.movsim.simulator.roadnetwork;
 
 /**
@@ -40,121 +14,90 @@ package org.movsim.simulator.roadnetwork;
  * </p>
  */
 public final class Lanes {
+  private Lanes() {
+  }
 
-    private Lanes() {
-        // private constructor
+  public static final int LANE1 = 1;
+
+  public static final int LANE2 = 2;
+
+  public static final int LANE3 = 3;
+
+  public static final int LANE4 = 4;
+
+  public static final int LANE5 = 5;
+
+  public static final int HARD_SHOULDER = -1;
+
+  public static final int NONE = -2;
+
+  public final static int TO_LEFT = -1;
+
+  public final static int TO_RIGHT = 1;
+
+  public final static int NO_CHANGE = 0;
+
+  public static final int MOST_INNER_LANE = LANE1;
+
+  /** laneIndex=0 is the internal overtaking lane (xodr reserves this for middlelane) */
+  public static final int OVERTAKING = 0;
+
+  public enum Type {
+    TRAFFIC("driving"),
+    ENTRANCE("mwyEntry"),
+    EXIT("mwyExit"),
+    SHOULDER("shoulder"),
+    RESTRICTED("restricted"),
+    BICYCLE("biking")
+    ;
+
+    private final String openDriveIdentifier;
+
+    Type(String keyword) {
+      this.openDriveIdentifier = keyword;
     }
 
+    public String getOpenDriveIdentifier() {
+      return openDriveIdentifier;
+    }
+  }
 
-    // from inner to outer lanes
-    public static final int LANE1 = 1;
-    public static final int LANE2 = 2;
-    public static final int LANE3 = 3;
-    public static final int LANE4 = 4;
-    public static final int LANE5 = 5;
-    public static final int HARD_SHOULDER = -1; // NOT DRIVABLE
-    public static final int NONE = -2; // NOT DRIVABLE
+  public enum LaneSectionType {
+    LEFT("-", true),
+    RIGHT("+", false)
+    ;
 
-    // TODO renaming: TO_OUTER, TO_INNER
-    public final static int TO_LEFT = -1; // TODO decrease index
-    public final static int TO_RIGHT = 1; // TODO increase index
-    public final static int NO_CHANGE = 0;
+    private final String idAppender;
 
-    public static final int MOST_INNER_LANE = LANE1;
+    private final boolean reverseDirection;
 
-    /** internal overtaking lane, works with mapping */
-    public static final int OVERTAKING = 0;
-
-    /**
-     * Lanes type.
-     * 
-     * <p>
-     * Mapping of OpenDRIVE laneIndex types.
-     */
-    public enum Type {
-        /**
-         * Lanes for normal traffic.
-         */
-        TRAFFIC("driving"),
-        /**
-         * Entrance (acceleration) laneIndex.
-         */
-        ENTRANCE("mwyEntry"),
-        /**
-         * Exit (deceleration) laneIndex.
-         */
-        EXIT("mwyExit"),
-        /**
-         * Shoulder laneIndex.
-         */
-        SHOULDER("shoulder"),
-        /**
-         * Restricted laneIndex, eg bus or multiple-occupancy vehicle laneIndex.
-         */
-        RESTRICTED("restricted"),
-        /**
-         * Bicycle laneIndex.
-         */
-        BICYCLE("biking");
-
-        private final String openDriveIdentifier;
-
-        Type(String keyword) {
-            this.openDriveIdentifier = keyword;
-        }
-
-        public String getOpenDriveIdentifier() {
-            return openDriveIdentifier;
-        }
+    private LaneSectionType(String idAppender, boolean reverseDirection) {
+      this.idAppender = idAppender;
+      this.reverseDirection = reverseDirection;
     }
 
-    /**
-     * center laneSection not supported
-     * 
-     * <br>
-     * created: Mar 30, 2013<br>
-     * 
-     */
-    public enum LaneSectionType {
-        LEFT("-", true), RIGHT("+", false);
-
-        private final String idAppender;
-
-        private final boolean reverseDirection;
-
-        private LaneSectionType(String idAppender, boolean reverseDirection) {
-            this.idAppender = idAppender;
-            this.reverseDirection = reverseDirection;
-        }
-
-        public String idAppender() {
-            return idAppender;
-        }
-
-        public boolean isReverseDirection() {
-            return reverseDirection;
-        }
+    public String idAppender() {
+      return idAppender;
     }
 
-    /**
-     * 
-     * 
-     * <br>
-     * created: Mar 30, 2013<br>
-     * 
-     */
-    public enum RoadLinkElementType {
-        ROAD("road"), JUNCTION("junction");
+    public boolean isReverseDirection() {
+      return reverseDirection;
+    }
+  }
 
-        private final String xodrIdentifier;
+  public enum RoadLinkElementType {
+    ROAD("road"),
+    JUNCTION("junction")
+    ;
 
-        RoadLinkElementType(String keyword) {
-            this.xodrIdentifier = keyword;
-        }
+    private final String xodrIdentifier;
 
-        public String xodrIdentifier() {
-            return xodrIdentifier;
-        }
+    RoadLinkElementType(String keyword) {
+      this.xodrIdentifier = keyword;
     }
 
+    public String xodrIdentifier() {
+      return xodrIdentifier;
+    }
+  }
 }
