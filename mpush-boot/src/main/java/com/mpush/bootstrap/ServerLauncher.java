f8d92675da12d64a2e811eb032d4c25e6425479c
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-22 04:51:42 +0000

    Mining Framework Analysis

--- mpush-boot/src/main/java/com/mpush/bootstrap/ServerLauncher.java
+++ mpush-boot/src/main/java/com/mpush/bootstrap/ServerLauncher.java
@@ -1,29 +1,9 @@
-/*
- * (C) Copyright 2015-2016 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * Contributors:
- *   ohun@live.cn (夜色)
- */
-
 package com.mpush.bootstrap;
-
-
 import com.mpush.bootstrap.job.*;
 import com.mpush.core.server.*;
-
-import static com.mpush.common.ServerNodes.*;
+import com.mpush.tools.config.CC;
+import static com.mpush.common.ServerNodes.CS;
+import static com.mpush.common.ServerNodes.GS;
 import static com.mpush.tools.config.CC.mp.net.udpGateway;
 import static com.mpush.tools.config.CC.mp.net.wsEnabled;
 
@@ -33,28 +13,23 @@ import static com.mpush.tools.config.CC.mp.net.wsEnabled;
  * @author ohun@live.cn
  */
 public final class ServerLauncher {
+  private final BootChain chain = BootChain.chain();
 
-    private final BootChain chain = BootChain.chain();
+  public ServerLauncher() {
+    chain.boot().setNext(new ServiceRegistryBoot()).setNext(new RedisBoot()).setNext(new ServerBoot(ConnectionServer.I(), CS)).setNext(
+<<<<<<< /usr/src/app/output/mpusher/mpush/684f038dad1d5b95b6f3f60674e12e4ef0b47922/mpush-boot/src/main/java/com/mpush/bootstrap/ServerLauncher.java/left.java
+    () -> new ServerBoot(WebSocketServer.I(), WS_NODE)
+=======
+    new ServerBoot(udpGateway() ? GatewayUDPConnector.I() : GatewayServer.I(), GS)
+>>>>>>> /usr/src/app/output/mpusher/mpush/684f038dad1d5b95b6f3f60674e12e4ef0b47922/mpush-boot/src/main/java/com/mpush/bootstrap/ServerLauncher.java/right.java
+    , wsEnabled()).setNext(new ServerBoot(udpGateway() ? GatewayUDPConnector.I() : GatewayServer.I(), GS_NODE)).setNext(new ServerBoot(AdminServer.I(), null)).setNext(new PushCenterBoot()).setNext(new HttpProxyBoot()).setNext(new MonitorBoot()).setNext(new LastBoot());
+  }
 
-    public ServerLauncher() {
-        chain.boot()
-                .setNext(new ServiceRegistryBoot())//1.启动ZK节点数据变化监听
-                .setNext(new RedisBoot())//2.注册redis sever 到ZK
-                .setNext(new ServerBoot(ConnectionServer.I(), CS))//3.启动长连接服务
-                .setNext(() -> new ServerBoot(WebSocketServer.I(), WS), wsEnabled())//4.启动websocket连接服务
-                .setNext(new ServerBoot(udpGateway() ? GatewayUDPConnector.I() : GatewayServer.I(), GS))//4.启动网关服务
-                .setNext(new ServerBoot(AdminServer.I(), null))//5.启动控制台服务
-                .setNext(new PushCenterBoot())//6.启动http代理服务，解析dns
-                .setNext(new HttpProxyBoot())//6.启动http代理服务，解析dns
-                .setNext(new MonitorBoot())//7.启动监控
-                .setNext(new LastBoot());//8.启动结束
-    }
+  public void start() {
+    chain.start();
+  }
 
-    public void start() {
-        chain.start();
-    }
-
-    public void stop() {
-        chain.stop();
-    }
+  public void stop() {
+    chain.stop();
+  }
 }
--- mpush-boot/src/main/java/com/mpush/bootstrap/job/ServerBoot.java
+++ mpush-boot/src/main/java/com/mpush/bootstrap/job/ServerBoot.java
@@ -1,24 +1,4 @@
-/*
- * (C) Copyright 2015-2016 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * Contributors:
- *   ohun@live.cn (夜色)
- */
-
 package com.mpush.bootstrap.job;
-
 import com.mpush.api.service.Listener;
 import com.mpush.api.service.Server;
 import com.mpush.api.spi.common.ServiceRegistryFactory;
@@ -31,49 +11,45 @@ import com.mpush.tools.log.Logs;
  * @author ohun@live.cn
  */
 public final class ServerBoot extends BootJob {
-    private final Server server;
-    private final ServiceNode node;
+  private final Server server;
 
-    public ServerBoot(Server server, ServiceNode node) {
-        this.server = server;
-        this.node = node;
-    }
+  private final ServiceNode node;
 
-    @Override
-    public void start() {
-        server.init();
-        server.start(new Listener() {
-            @Override
-            public void onSuccess(Object... args) {
-                Logs.Console.info("start {} success on:{}", server.getClass().getSimpleName(), args[0]);
-                if (node != null) {//注册应用到zk
-                    ServiceRegistryFactory.create().register(node);
-                    Logs.RSD.info("register {} to srd success.", node);
-                }
-                startNext();
-            }
+  public ServerBoot(Server server, ServiceNode node) {
+    this.server = server;
+    this.node = node;
+  }
 
-            @Override
-            public void onFailure(Throwable cause) {
-                Logs.Console.error("start {} failure, jvm exit with code -1", server.getClass().getSimpleName(), cause);
-                System.exit(-1);
-            }
-        });
-    }
-
-    @Override
-    protected void stop() {
+  @Override public void start() {
+    server.init();
+    server.start(new Listener() {
+      @Override public void onSuccess(Object... args) {
+        Logs.Console.info("start {} success on:{}", server.getClass().getSimpleName(), args[0]);
         if (node != null) {
-            ServiceRegistryFactory.create().deregister(node);
+          ServiceRegistryFactory.create().register(node);
+          Logs.RSD.info("register {} to srd success.", node);
         }
-        Logs.Console.info("try shutdown {}...", this.getClass().getSimpleName());
-        server.stop().join();
-        Logs.Console.info("{} shutdown success.", this.getClass().getSimpleName());
-        stopNext();
-    }
+        startNext();
+      }
 
-    @Override
-    protected String getName() {
-        return super.getName() + '(' + server.getClass().getSimpleName() + ')';
+      @Override public void onFailure(Throwable cause) {
+        Logs.Console.error("start {} failure, jvm exit with code -1", server.getClass().getSimpleName(), cause);
+        System.exit(-1);
+      }
+    });
+  }
+
+  @Override protected void stop() {
+    if (node != null) {
+      ServiceRegistryFactory.create().deregister(node);
     }
+    Logs.Console.info("try shutdown {}...", this.getClass().getSimpleName());
+    server.stop().join();
+    Logs.Console.info("{} shutdown success.", this.getClass().getSimpleName());
+    stopNext();
+  }
+
+  @Override protected String getName() {
+    return super.getName() + '(' + server.getClass().getSimpleName() + ')';
+  }
 }
