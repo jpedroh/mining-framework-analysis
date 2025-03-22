b725a239ade3abdd02f236cdaa4f5f0b9dc75453
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-22 06:17:39 +0000

    Mining Framework Analysis

--- src/main/java/com/thealgorithms/dynamicprogramming/KnapsackMemoization.java
+++ src/main/java/com/thealgorithms/dynamicprogramming/KnapsackMemoization.java
@@ -9,7 +9,13 @@ package com.thealgorithms.dynamicprogramming;
  */
 public class KnapsackMemoization {
 
+<<<<<<< /usr/src/app/output/thealgorithms/java/5b2622164831552c25a3c8ec56a8a05788448d30/src/main/java/com/thealgorithms/dynamicprogramming/KnapsackMemoization.java/left.java
+    int knapSack(int capacity, int[] wt, int[] val, int numOfItems) {
+||||||| /usr/src/app/output/thealgorithms/java/5b2622164831552c25a3c8ec56a8a05788448d30/src/main/java/com/thealgorithms/dynamicprogramming/KnapsackMemoization.java/base.java
+    int knapSack(int capacity, int wt[], int val[], int numOfItems) {
+=======
     int knapSack(int capacity, int[] weights, int[] profits, int numOfItems) {
+>>>>>>> /usr/src/app/output/thealgorithms/java/5b2622164831552c25a3c8ec56a8a05788448d30/src/main/java/com/thealgorithms/dynamicprogramming/KnapsackMemoization.java/right.java
 
         // Declare the table dynamically
         int[][] dpTable = new int[numOfItems + 1][capacity + 1];
@@ -25,9 +31,22 @@ public class KnapsackMemoization {
     }
 
     // Returns the value of maximum profit using recursive approach
+<<<<<<< /usr/src/app/output/thealgorithms/java/5b2622164831552c25a3c8ec56a8a05788448d30/src/main/java/com/thealgorithms/dynamicprogramming/KnapsackMemoization.java/left.java
+    int solveKnapsackRecursive(int capacity, int[] wt,
+||||||| /usr/src/app/output/thealgorithms/java/5b2622164831552c25a3c8ec56a8a05788448d30/src/main/java/com/thealgorithms/dynamicprogramming/KnapsackMemoization.java/base.java
+    int solveKnapsackRecursive(int capacity, int wt[],
+=======
     int solveKnapsackRecursive(int capacity, int[] weights,
-                               int[] profits, int numOfItems,
-                               int[][] dpTable) {
+>>>>>>> /usr/src/app/output/thealgorithms/java/5b2622164831552c25a3c8ec56a8a05788448d30/src/main/java/com/thealgorithms/dynamicprogramming/KnapsackMemoization.java/right.java
+<<<<<<< /usr/src/app/output/thealgorithms/java/5b2622164831552c25a3c8ec56a8a05788448d30/src/main/java/com/thealgorithms/dynamicprogramming/KnapsackMemoization.java/left.java
+                    int[] val, int numOfItems,
+||||||| /usr/src/app/output/thealgorithms/java/5b2622164831552c25a3c8ec56a8a05788448d30/src/main/java/com/thealgorithms/dynamicprogramming/KnapsackMemoization.java/base.java
+                    int val[], int numOfItems,
+=======
+                    int[] profits, int numOfItems,
+>>>>>>> /usr/src/app/output/thealgorithms/java/5b2622164831552c25a3c8ec56a8a05788448d30/src/main/java/com/thealgorithms/dynamicprogramming/KnapsackMemoization.java/right.java
+                    int[][] dpTable) {
+
         // Base condition
         if (numOfItems == 0 || capacity == 0) {
             return 0;
