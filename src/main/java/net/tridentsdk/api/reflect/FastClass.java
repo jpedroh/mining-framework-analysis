b627f225fe1287e8bfe01450b6fe44f0c76b1de4
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-22 01:15:05 +0000

    Mining Framework Analysis

--- src/main/java/net/tridentsdk/api/reflect/FastClass.java
+++ src/main/java/net/tridentsdk/api/reflect/FastClass.java
@@ -1,91 +1,74 @@
-/*
- *     TridentSDK - A Minecraft Server API
- *     Copyright (C) 2014, The TridentSDK Team
- *
- *     This program is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
- *
- *     This program is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
- *
- *     You should have received a copy of the GNU General Public License
- *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 package net.tridentsdk.api.reflect;
-
 import com.esotericsoftware.reflectasm.ConstructorAccess;
 import com.esotericsoftware.reflectasm.FieldAccess;
 import com.esotericsoftware.reflectasm.MethodAccess;
-
 import java.lang.reflect.Field;
 
 public class FastClass {
-    private final Class<?> cls;
+  private final Class<?> cls;
 
-    private final FieldAccess fieldAccess;
-    private final MethodAccess methodAccess;
-    private final ConstructorAccess constructorAccess;
+  private final FieldAccess fieldAccess;
 
-    private FastClass(Class<?> cls) {
-        this.cls = cls;
-        this.fieldAccess = FieldAccess.get(cls);
-        this.methodAccess = MethodAccess.get(cls);
-        this.constructorAccess = ConstructorAccess.get(cls);
-    }
+  private final MethodAccess methodAccess;
 
-    public static FastClass get(Class<?> cls) {
-        return new FastClass(cls);
-    }
+  private final ConstructorAccess constructorAccess;
 
-    public static FastClass get(Object obj) {
-        return get(obj.getClass());
-    }
+  private FastClass(Class<?> cls) {
+    this.cls = cls;
+    this.fieldAccess = FieldAccess.get(cls);
+    this.methodAccess = MethodAccess.get(cls);
+    this.constructorAccess = ConstructorAccess.get(cls);
+  }
 
-    /**
+  public static FastClass get(Class<?> cls) {
+    return new FastClass(cls);
+  }
+
+  public static FastClass get(Object obj) {
+    return get(obj.getClass());
+  }
+
+  /**
      * Get a field from the class
      *
+     * @param instance Instance of the class
      * @param name     Name of the field
      * @return FastField instance
      */
-    public FastField getField(String name) {
-        return new FastField(this, this.fieldAccess, name);
-    }
+  public FastField getField(String name) {
+    return new FastField(this, this.fieldAccess, name);
+  }
 
-    /**
+  /**
      * Get a method from the class
      *
+     * @param instance Instance of the class
      * @param name     Name of the method
      * @return FastMethod instance
      */
-    public FastMethod getMethod(String name) {
-        return new FastMethod(this.methodAccess, name);
+  public FastMethod getMethod(String name) {
+    return new FastMethod(this.methodAccess, name);
+  }
+
+  public FastField[] getFields() {
+    Field[] fields = this.cls.getDeclaredFields();
+    FastField[] fastFields = new FastField[fields.length];
+    for (int i = 0; i < fields.length; i += 1) {
+      fastFields[i] = new FastField(this, this.fieldAccess, fields[i].getName());
     }
+    return fastFields;
+  }
 
-    public FastField[] getFields() {
-        Field[] fields = this.cls.getDeclaredFields();
-        FastField[] fastFields = new FastField[fields.length];
-
-        for (int i = 0; i < fields.length; i += 1) {
-            fastFields[i] = new FastField(this, this.fieldAccess, fields[i].getName());
-        }
-
-        return fastFields;
-    }
-
-    /**
+  /**
      * Get the default constructor found
      *
      * @return the default FastConstructor
      */
-    public FastConstructor getConstructor() {
-        return new FastConstructor(this.constructorAccess);
-    }
+  public FastConstructor getConstructor() {
+    return new FastConstructor(this.constructorAccess);
+  }
 
-    public Class<?> toClass() {
-        return this.cls;
-    }
+  public Class<?> toClass() {
+    return this.cls;
+  }
 }
