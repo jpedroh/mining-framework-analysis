189be77e76102ca36eb1f7e35fb033ff2f6ed7d7
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-22 10:50:31 +0000

    Mining Framework Analysis

--- src/main/java/net/helpscout/api/model/ref/PersonRef.java
+++ src/main/java/net/helpscout/api/model/ref/PersonRef.java
@@ -1,12 +1,6 @@
-package net.helpscout.api.model.ref;
-
-import net.helpscout.api.cbo.PersonType;
-
-public interface PersonRef {
-
-	public int getId();
-	public String getFirstName();
-	public String getLastName();
-	public String getEmail();
-	public PersonType getType();
-}
+  package     net . helpscout . api . model . ref ;   import     net . helpscout . api . cbo . PersonType ;   public interface PersonRef  {   public  int getId  ( ) ;   public String getFirstName  ( ) ;   public String getLastName  ( ) ;   public String getEmail  ( ) ; 
+<<<<<<<
+  public PersonType getType  ( ) ;
+=======
+>>>>>>>
+ }
