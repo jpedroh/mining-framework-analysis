bcf148d3e65a10d6b28ec197189f67bfaa63e069
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-22 06:17:25 +0000

    Mining Framework Analysis

--- src/main/java/com/thealgorithms/dynamicprogramming/KnapsackMemoization.java
+++ src/main/java/com/thealgorithms/dynamicprogramming/KnapsackMemoization.java
@@ -1,50 +1,85 @@
-package com.thealgorithms.dynamicprogramming;
-
-/**
- * Recursive Solution for 0-1 knapsack with memoization
- * This method is basically an extension to the recursive approach so that we
- * can overcome the problem of calculating redundant cases and thus increased
- * complexity. We can solve this problem by simply creating a 2-D array that can
- * store a particular state (n, w) if we get it the first time.
- */
-public class KnapsackMemoization {
-
-    int knapSack(int capacity, int[] weights, int[] profits, int numOfItems) {
-
-        // Declare the table dynamically
-        int[][] dpTable = new int[numOfItems + 1][capacity + 1];
-
-        // Loop to initially fill the table with -1
-        for (int i = 0; i < numOfItems + 1; i++) {
-            for (int j = 0; j < capacity + 1; j++) {
-                dpTable[i][j] = -1;
-            }
-        }
-
-        return solveKnapsackRecursive(capacity, weights, profits, numOfItems, dpTable);
-    }
-
-    // Returns the value of maximum profit using recursive approach
-    int solveKnapsackRecursive(int capacity, int[] weights,
-                               int[] profits, int numOfItems,
-                               int[][] dpTable) {
-        // Base condition
-        if (numOfItems == 0 || capacity == 0) {
-            return 0;
-        }
-
-        if (dpTable[numOfItems][capacity] != -1) {
-            return dpTable[numOfItems][capacity];
-        }
-
-        if (weights[numOfItems - 1] > capacity) {
-            // Store the value of function call stack in table
-            dpTable[numOfItems][capacity] = solveKnapsackRecursive(capacity, weights, profits, numOfItems - 1, dpTable);
-            return dpTable[numOfItems][capacity];
-        } else {
-            // Return value of table after storing
-            return dpTable[numOfItems][capacity] = Math.max((profits[numOfItems - 1] + solveKnapsackRecursive(capacity - weights[numOfItems - 1], weights, profits, numOfItems - 1, dpTable)),
-                    solveKnapsackRecursive(capacity, weights, profits, numOfItems - 1, dpTable));
-        }
-    }
-}
+  package   com . thealgorithms . dynamicprogramming ;   public class KnapsackMemoization  {   int knapSack  (   int 
+<<<<<<<
+W
+=======
+capacity
+>>>>>>>
+ ,    int  [ ] 
+<<<<<<<
+wt
+=======
+weights
+>>>>>>>
+ ,    int  [ ] 
+<<<<<<<
+val
+=======
+profits
+>>>>>>>
+ ,   int 
+<<<<<<<
+N
+=======
+numOfItems
+>>>>>>>
+ )  {    int  [ ] [ ] 
+<<<<<<<
+ dp =  new  int  [  N + 1 ]  [  W + 1 ]
+=======
+ dpTable =  new  int  [  numOfItems + 1 ]  [  capacity + 1 ]
+>>>>>>>
+ ;  for (   int  i = 0 ;  i <  
+<<<<<<<
+N
+=======
+numOfItems
+>>>>>>>
+ + 1 ;  i ++ )  {  for (   int  j = 0 ;  j <  
+<<<<<<<
+W
+=======
+capacity
+>>>>>>>
+ + 1 ;  j ++ )  {     
+<<<<<<<
+dp
+=======
+dpTable
+>>>>>>>
+ [ i ] [ j ] =  - 1 ; } }  return  
+<<<<<<<
+knapSackRec
+=======
+solveKnapsackRecursive
+>>>>>>>
+  ( 
+<<<<<<<
+W
+=======
+capacity
+>>>>>>>
+ , 
+<<<<<<<
+wt
+=======
+weights
+>>>>>>>
+ , 
+<<<<<<<
+val
+=======
+profits
+>>>>>>>
+ , 
+<<<<<<<
+N
+=======
+numOfItems
+>>>>>>>
+ , 
+<<<<<<<
+dp
+=======
+dpTable
+>>>>>>>
+ ) ; }   int knapSackRec  (   int W ,    int  [ ] wt ,    int  [ ] val ,   int n ,    int  [ ] [ ] dp )  {  if  (   n == 0 ||  W == 0 )  {  return 0 ; }  if  (    dp [ n ] [ W ] !=  - 1 )  {  return   dp [ n ] [ W ] ; }  if  (   wt [  n - 1 ] > W )  {     dp [ n ] [ W ] =  knapSackRec  ( W , wt , val ,  n - 1 , dp ) ;  return   dp [ n ] [ W ] ; } else  {  return    dp [ n ] [ W ] =  Math . max  (  (   val [  n - 1 ] +  knapSackRec  (  W -  wt [  n - 1 ] , wt , val ,  n - 1 , dp ) ) ,  knapSackRec  ( W , wt , val ,  n - 1 , dp ) ) ; } }   int solveKnapsackRecursive  (   int capacity ,    int  [ ] weights ,    int  [ ] profits ,   int numOfItems ,    int  [ ] [ ] dpTable )  {  if  (   numOfItems == 0 ||  capacity == 0 )  {  return 0 ; }  if  (    dpTable [ numOfItems ] [ capacity ] !=  - 1 )  {  return   dpTable [ numOfItems ] [ capacity ] ; }  if  (   weights [  numOfItems - 1 ] > capacity )  {     dpTable [ numOfItems ] [ capacity ] =  solveKnapsackRecursive  ( capacity , weights , profits ,  numOfItems - 1 , dpTable ) ;  return   dpTable [ numOfItems ] [ capacity ] ; } else  {  return    dpTable [ numOfItems ] [ capacity ] =  Math . max  (  (   profits [  numOfItems - 1 ] +  solveKnapsackRecursive  (  capacity -  weights [  numOfItems - 1 ] , weights , profits ,  numOfItems - 1 , dpTable ) ) ,  solveKnapsackRecursive  ( capacity , weights , profits ,  numOfItems - 1 , dpTable ) ) ; } } }
