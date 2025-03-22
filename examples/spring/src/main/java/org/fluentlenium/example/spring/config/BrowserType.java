2179db8006ac9a836de8a601ce587316b53a7b38
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-22 10:35:48 +0000

    Mining Framework Analysis

--- examples/spring/src/main/java/org/fluentlenium/example/spring/config/BrowserType.java
+++ examples/spring/src/main/java/org/fluentlenium/example/spring/config/BrowserType.java
@@ -1,5 +1,4 @@
 package org.fluentlenium.example.spring.config;
-
 import java.net.MalformedURLException;
 import java.net.URL;
 import org.openqa.selenium.MutableCapabilities;
@@ -20,117 +19,97 @@ import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.safari.SafariOptions;
 
 public enum BrowserType {
-
-    @SuppressWarnings("unused")
-    CHROME() {
-        @Override
-        public WebDriver getWebDriver() {
-            return new ChromeDriver();
-        }
-
-        @Override
-        protected MutableCapabilities getBrowserCapabilities() {
-            return new ChromeOptions();
-        }
-
-        @Override
-        public String getDriverSystemPropertyName() {
-            return "webdriver.chrome.driver";
-        }
-    },
-    SAFARI() {
-        @Override
-        public WebDriver getWebDriver() {
-            return new SafariDriver();
-        }
-
-        @Override
-        protected MutableCapabilities getBrowserCapabilities() {
-            return new SafariOptions();
-        }
-
-        @Override
-        public String getDriverSystemPropertyName() {
-            return "webdriver.safari.driver";
-        }
-    },
-    FIREFOX() {
-        @Override
-        public WebDriver getWebDriver() {
-            return new FirefoxDriver();
-        }
-
-        @Override
-        protected MutableCapabilities getBrowserCapabilities() {
-            return new FirefoxOptions();
-        }
-
-        @Override
-        public String getDriverSystemPropertyName() {
-            return "webdriver.gecko.driver";
-        }
-    },
-    IE() {
-        @Override
-        public WebDriver getWebDriver() {
-            return new InternetExplorerDriver();
-        }
-
-        @Override
-        protected MutableCapabilities getBrowserCapabilities() {
-            return new InternetExplorerOptions();
-        }
-
-        @Override
-        public String getDriverSystemPropertyName() {
-            return "webdriver.ie.driver";
-        }
-    },
-    EDGE() {
-        @Override
-        public WebDriver getWebDriver() {
-            return new EdgeDriver();
-        }
-
-        @Override
-        protected MutableCapabilities getBrowserCapabilities() {
-            return new EdgeOptions();
-        }
-
-        @Override
-        public String getDriverSystemPropertyName() {
-            return "webdriver.edge.driver";
-        }
-    },
-    OPERA() {
-        @Override
-        public WebDriver getWebDriver() {
-            return new OperaDriver();
-        }
-
-        @Override
-        protected MutableCapabilities getBrowserCapabilities() {
-            return new OperaOptions();
-        }
-
-        @Override
-        public String getDriverSystemPropertyName() {
-            return "webdriver.opera.driver";
-        }
-    };
-
-    protected abstract WebDriver getWebDriver();
-
-    protected abstract MutableCapabilities getBrowserCapabilities();
-
-    public abstract String getDriverSystemPropertyName();
-
-    public WebDriver getRemoteWebDriver(String hubLocation) {
-        try {
-            return new Augmenter().augment(new RemoteWebDriver(new URL(hubLocation), getBrowserCapabilities()));
-        } catch (MalformedURLException e) {
-            throw new ConfigException("Invalid hub location: " + hubLocation, e);
-        }
+  @SuppressWarnings(value = { "unused" }) CHROME() {
+    @Override public WebDriver getWebDriver() {
+      return new ChromeDriver();
     }
 
+    @Override protected MutableCapabilities getBrowserCapabilities() {
+      return new ChromeOptions();
+    }
+
+    @Override public String getDriverSystemPropertyName() {
+      return "webdriver.chrome.driver";
+    }
+  },
+  SAFARI() {
+    @Override public WebDriver getWebDriver() {
+      return new SafariDriver();
+    }
+
+    @Override protected MutableCapabilities getBrowserCapabilities() {
+      return new SafariOptions();
+    }
+
+    @Override public String getDriverSystemPropertyName() {
+      return "webdriver.safari.driver";
+    }
+  },
+  FIREFOX() {
+    @Override public WebDriver getWebDriver() {
+      return new FirefoxDriver();
+    }
+
+    @Override protected MutableCapabilities getBrowserCapabilities() {
+      return new FirefoxOptions();
+    }
+
+    @Override public String getDriverSystemPropertyName() {
+      return "webdriver.gecko.driver";
+    }
+  },
+  IE() {
+    @Override public WebDriver getWebDriver() {
+      return new InternetExplorerDriver();
+    }
+
+    @Override protected MutableCapabilities getBrowserCapabilities() {
+      return new InternetExplorerOptions();
+    }
+
+    @Override public String getDriverSystemPropertyName() {
+      return "webdriver.ie.driver";
+    }
+  },
+  EDGE() {
+    @Override public WebDriver getWebDriver() {
+      return new EdgeDriver();
+    }
+
+    @Override protected MutableCapabilities getBrowserCapabilities() {
+      return new EdgeOptions();
+    }
+
+    @Override public String getDriverSystemPropertyName() {
+      return "webdriver.edge.driver";
+    }
+  },
+  OPERA() {
+    @Override public WebDriver getWebDriver() {
+      return new OperaDriver();
+    }
+
+    @Override protected MutableCapabilities getBrowserCapabilities() {
+      return new OperaOptions();
+    }
+
+    @Override public String getDriverSystemPropertyName() {
+      return "webdriver.opera.driver";
+    }
+  }
+  ;
+
+  protected abstract WebDriver getWebDriver();
+
+  protected abstract MutableCapabilities getBrowserCapabilities();
+
+  public abstract String getDriverSystemPropertyName();
+
+  public WebDriver getRemoteWebDriver(String hubLocation) {
+    try {
+      return new Augmenter().augment(new RemoteWebDriver(new URL(hubLocation), getBrowserCapabilities()));
+    } catch (MalformedURLException e) {
+      throw new ConfigException("Invalid hub location: " + hubLocation, e);
+    }
+  }
 }
--- examples/spring/src/main/java/org/fluentlenium/example/spring/config/SeleniumBrowserConfigProperties.java
+++ examples/spring/src/main/java/org/fluentlenium/example/spring/config/SeleniumBrowserConfigProperties.java
@@ -1,54 +1,50 @@
 package org.fluentlenium.example.spring.config;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-@Component
-public class SeleniumBrowserConfigProperties {
-    @Value("${selenium.browser.type}")
-    private BrowserType browserType;
-    @Value("${selenium.hub.enabled}")
-    private Boolean useHub;
-    @Value("${selenium.hub.location}")
-    private String hubLocation;
-    @Value("${selenium.get.url}")
-    private String pageUrl;
+@Component public class SeleniumBrowserConfigProperties {
+  @Value(value = "${selenium.browser.type}") private BrowserType browserType;
 
-    @Value("${firefoxdriver.path}")
-    private String firefoxDriverPath;
-    @Value("${chromedriver.path}")
-    private String chromeDriverPath;
-    @Value("${safaridriver.path}")
-    private String safariDriverPath;
-    @Value("${iedriver.path}")
-    private String ieDriverPath;
-    @Value("${edgedriver.path}")
-    private String edgeDriverPath;
-    @Value("${operadriver.path}")
-    private String operaDriverPath;
+  @Value(value = "${selenium.hub.enabled}") private Boolean useHub;
 
-    public BrowserConfig getBrowserConfig() {
-        return new BrowserConfig(browserType, useHub, hubLocation);
+  @Value(value = "${selenium.hub.location}") private String hubLocation;
+
+  @Value(value = "${selenium.get.url}") private String pageUrl;
+
+  @Value(value = "${firefoxdriver.path}") private String firefoxDriverPath;
+
+  @Value(value = "${chromedriver.path}") private String chromeDriverPath;
+
+  @Value(value = "${safaridriver.path}") private String safariDriverPath;
+
+  @Value(value = "${iedriver.path}") private String ieDriverPath;
+
+  @Value(value = "${edgedriver.path}") private String edgeDriverPath;
+
+  @Value(value = "${operadriver.path}") private String operaDriverPath;
+
+  public BrowserConfig getBrowserConfig() {
+    return new BrowserConfig(browserType, useHub, hubLocation);
+  }
+
+  public String getPageUrl() {
+    return pageUrl;
+  }
+
+  public String getDriverExecutablePath() {
+    switch (browserType) {
+      case SAFARI:
+      return safariDriverPath;
+      case FIREFOX:
+      return firefoxDriverPath;
+      case IE:
+      return ieDriverPath;
+      case EDGE:
+      return edgeDriverPath;
+      case OPERA:
+      return operaDriverPath;
+      default:
+      return chromeDriverPath;
     }
-
-    public String getPageUrl() {
-        return pageUrl;
-    }
-
-    public String getDriverExecutablePath() {
-        switch (browserType) {
-            case SAFARI:
-                return safariDriverPath;
-            case FIREFOX:
-                return firefoxDriverPath;
-            case IE:
-                return ieDriverPath;
-            case EDGE:
-                return edgeDriverPath;
-            case OPERA:
-                return operaDriverPath;
-            default:
-                return chromeDriverPath;
-        }
-    }
+  }
 }
