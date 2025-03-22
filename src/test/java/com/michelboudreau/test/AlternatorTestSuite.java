b0512f672ee7e13f762db3cd186208b3b7172f7f
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-22 07:47:19 +0000

    Mining Framework Analysis

--- src/test/java/com/michelboudreau/test/AlternatorTestSuite.java
+++ src/test/java/com/michelboudreau/test/AlternatorTestSuite.java
@@ -1,14 +1,1 @@
-package com.michelboudreau.test;
-
-import org.junit.BeforeClass;
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-
-@RunWith(Suite.class)
-@Suite.SuiteClasses({ AlternatorTableTest.class , AlternatorItemTest.class , AlternatorQueryTest.class, AlternatorScanTest.class, AlternatorBatchItemTest.class, AlternatorSaveRestoreTest.class, AlternatorMapperTest.class})
-public class AlternatorTestSuite {
-	@BeforeClass
-	public static void before() {
-		AlternatorTest.RUN_DB_AS_SERVICE = true;
-	}
-}
+  package   com . michelboudreau . test ;   import    org . junit . runner . RunWith ;  import    org . junit . runners . Suite ;  import   org . junit . BeforeClass ;    @ RunWith  (  Suite . class )  @  Suite . SuiteClasses  (  {  AlternatorTableTest . class ,  AlternatorItemTest . class ,  AlternatorQueryTest . class ,  AlternatorScanTest . class ,  AlternatorBatchItemTest . class ,  AlternatorSaveRestoreTest . class ,  AlternatorMapperTest . class } ) public class AlternatorTestSuite  {    @ BeforeClass public static void before  ( )  {    AlternatorTest . RUN_DB_AS_SERVICE = true ; } }
