0a73d8aa8b3635779d368c966d6b15f5ed381140
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-22 06:33:09 +0000

    Mining Framework Analysis

--- xxl-job-core/src/test/java/com/xxl/job/core/biz/impl/ExecutorBizImplTest.java
+++ xxl-job-core/src/test/java/com/xxl/job/core/biz/impl/ExecutorBizImplTest.java
@@ -1,5 +1,4 @@
 package com.xxl.job.core.biz.impl;
-
 import com.xxl.job.core.biz.ExecutorBiz;
 import com.xxl.job.core.biz.model.LogResult;
 import com.xxl.job.core.biz.model.ReturnT;
@@ -16,130 +15,91 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-
 import java.util.concurrent.TimeUnit;
 
-
 public class ExecutorBizImplTest {
+  public XxlJobExecutor xxlJobExecutor = null;
 
-    public XxlJobExecutor xxlJobExecutor = null;
-    public ExecutorBiz executorBiz = null;
+  public ExecutorBiz executorBiz = null;
 
-    @Before
-    public void before() throws Exception {
+  @Before public void before() throws Exception {
+    xxlJobExecutor = new XxlJobExecutor();
+    xxlJobExecutor.setAdminAddresses(null);
+    xxlJobExecutor.setAppName("xxl-job-executor-sample");
+    xxlJobExecutor.setIp(null);
+    xxlJobExecutor.setPort(9999);
+    xxlJobExecutor.setAccessToken(null);
+    xxlJobExecutor.setLogPath("/data/applogs/xxl-job/jobhandler");
+    xxlJobExecutor.setLogRetentionDays(-1);
+    xxlJobExecutor.start();
+    TimeUnit.SECONDS.sleep(3);
+    XxlRpcReferenceBean referenceBean = new XxlRpcReferenceBean();
+    referenceBean.setClient(NettyHttpClient.class);
+    referenceBean.setSerializer(HessianSerializer.class);
+    referenceBean.setCallType(CallType.SYNC);
+    referenceBean.setLoadBalance(LoadBalance.ROUND);
+    referenceBean.setIface(ExecutorBiz.class);
+    referenceBean.setVersion(null);
+    referenceBean.setTimeout(3000);
+    referenceBean.setAddress("127.0.0.1:9999");
+    referenceBean.setAccessToken(null);
+    referenceBean.setInvokeCallback(null);
+    referenceBean.setInvokerFactory(null);
+    executorBiz = (ExecutorBiz) referenceBean.getObject();
+  }
 
-        // init executor
-        xxlJobExecutor = new XxlJobExecutor();
-        xxlJobExecutor.setAdminAddresses(null);
-        xxlJobExecutor.setAppName("xxl-job-executor-sample");
-        xxlJobExecutor.setIp(null);
-        xxlJobExecutor.setPort(9999);
-        xxlJobExecutor.setAccessToken(null);
-        xxlJobExecutor.setLogPath("/data/applogs/xxl-job/jobhandler");
-        xxlJobExecutor.setLogRetentionDays(-1);
-
-        // start executor
-        xxlJobExecutor.start();
-
-        TimeUnit.SECONDS.sleep(3);
-
-        // init executor biz proxy
-        XxlRpcReferenceBean referenceBean = new XxlRpcReferenceBean();
-        referenceBean.setClient(NettyHttpClient.class);
-        referenceBean.setSerializer(HessianSerializer.class);
-        referenceBean.setCallType(CallType.SYNC);
-        referenceBean.setLoadBalance(LoadBalance.ROUND);
-        referenceBean.setIface(ExecutorBiz.class);
-        referenceBean.setVersion(null);
-        referenceBean.setTimeout(3000);
-        referenceBean.setAddress("127.0.0.1:9999");
-        referenceBean.setAccessToken(null);
-        referenceBean.setInvokeCallback(null);
-        referenceBean.setInvokerFactory(null);
-
-        executorBiz = (ExecutorBiz) referenceBean.getObject();
+  @After public void after() {
+    if (xxlJobExecutor != null) {
+      xxlJobExecutor.destroy();
     }
+  }
 
-    @After
-    public void after(){
-        if (xxlJobExecutor != null) {
-            xxlJobExecutor.destroy();
-        }
-    }
+  @Test public void beat() {
+    final ReturnT<String> retval = executorBiz.beat();
+    Assert.assertNotNull(retval);
+    Assert.assertNull(((ReturnT<String>) retval).getContent());
+    Assert.assertEquals(200, retval.getCode());
+    Assert.assertNull(retval.getMsg());
+  }
 
+  @Test public void idleBeat() {
+    final int jobId = 0;
+    final ReturnT<String> retval = executorBiz.idleBeat(jobId);
+    Assert.assertNotNull(retval);
+    Assert.assertNull(((ReturnT<String>) retval).getContent());
+    Assert.assertEquals(500, retval.getCode());
+    Assert.assertEquals("job thread is running or has trigger queue.", retval.getMsg());
+  }
 
-    @Test
-    public void beat() {
-        // Act
-        final ReturnT<String> retval = executorBiz.beat();
+  @Test public void kill() {
+    final int jobId = 0;
+    final ReturnT<String> retval = executorBiz.kill(jobId);
+    Assert.assertNotNull(retval);
+    Assert.assertNull(((ReturnT<String>) retval).getContent());
+    Assert.assertEquals(200, retval.getCode());
+    Assert.assertNull(retval.getMsg());
+  }
 
-        // Assert result
-        Assert.assertNotNull(retval);
-        Assert.assertNull(((ReturnT<String>) retval).getContent());
-        Assert.assertEquals(200, retval.getCode());
-        Assert.assertNull(retval.getMsg());
-    }
+  @Test public void log() {
+    final long logDateTim = 0L;
+    final long logId = 0;
+    final int fromLineNum = 0;
+    final ReturnT<LogResult> retval = executorBiz.log(logDateTim, logId, fromLineNum);
+    Assert.assertNotNull(retval);
+  }
 
-    @Test
-    public void idleBeat(){
-        final int jobId = 0;
-
-        // Act
-        final ReturnT<String> retval = executorBiz.idleBeat(jobId);
-
-        // Assert result
-        Assert.assertNotNull(retval);
-        Assert.assertNull(((ReturnT<String>) retval).getContent());
-        Assert.assertEquals(500, retval.getCode());
-        Assert.assertEquals("job thread is running or has trigger queue.", retval.getMsg());
-    }
-
-    @Test
-    public void kill(){
-        final int jobId = 0;
-
-        // Act
-        final ReturnT<String> retval = executorBiz.kill(jobId);
-
-        // Assert result
-        Assert.assertNotNull(retval);
-        Assert.assertNull(((ReturnT<String>) retval).getContent());
-        Assert.assertEquals(200, retval.getCode());
-        Assert.assertNull(retval.getMsg());
-    }
-
-    @Test
-    public void log(){
-        final long logDateTim = 0L;
-        final long logId = 0;
-        final int fromLineNum = 0;
-
-        // Act
-        final ReturnT<LogResult> retval = executorBiz.log(logDateTim, logId, fromLineNum);
-
-        // Assert result
-        Assert.assertNotNull(retval);
-    }
-
-    @Test
-    public void run(){
-        // trigger data
-        final TriggerParam triggerParam = new TriggerParam();
-        triggerParam.setJobId(1);
-        triggerParam.setExecutorHandler("demoJobHandler");
-        triggerParam.setExecutorParams(null);
-        triggerParam.setExecutorBlockStrategy(ExecutorBlockStrategyEnum.COVER_EARLY.name());
-        triggerParam.setGlueType(GlueTypeEnum.BEAN.name());
-        triggerParam.setGlueSource(null);
-        triggerParam.setGlueUpdatetime(System.currentTimeMillis());
-        triggerParam.setLogId(1);
-        triggerParam.setLogDateTime(System.currentTimeMillis());
-
-        // Act
-        final ReturnT<String> retval = executorBiz.run(triggerParam);
-
-        // Assert result
-        Assert.assertNotNull(retval);
-    }
-
+  @Test public void run() {
+    final TriggerParam triggerParam = new TriggerParam();
+    triggerParam.setJobId(1);
+    triggerParam.setExecutorHandler("demoJobHandler");
+    triggerParam.setExecutorParams(null);
+    triggerParam.setExecutorBlockStrategy(ExecutorBlockStrategyEnum.COVER_EARLY.name());
+    triggerParam.setGlueType(GlueTypeEnum.BEAN.name());
+    triggerParam.setGlueSource(null);
+    triggerParam.setGlueUpdatetime(System.currentTimeMillis());
+    triggerParam.setLogId(1);
+    triggerParam.setLogDateTime(System.currentTimeMillis());
+    final ReturnT<String> retval = executorBiz.run(triggerParam);
+    Assert.assertNotNull(retval);
+  }
 }
