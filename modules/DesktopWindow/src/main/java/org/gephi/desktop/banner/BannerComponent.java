5acd32f76f76ca28f0161d55ba3f0a08fc0e30ce
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-22 08:29:10 +0000

    Mining Framework Analysis

--- modules/DesktopWindow/src/main/java/org/gephi/desktop/banner/BannerComponent.java
+++ modules/DesktopWindow/src/main/java/org/gephi/desktop/banner/BannerComponent.java
@@ -1,47 +1,4 @@
-/*
-Copyright 2008-2010 Gephi
-Authors : Mathieu Bastian <mathieu.bastian@gephi.org>
-Website : http://www.gephi.org
-
-This file is part of Gephi.
-
-DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
-
-Copyright 2011 Gephi Consortium. All rights reserved.
-
-The contents of this file are subject to the terms of either the GNU
-General Public License Version 3 only ("GPL") or the Common
-Development and Distribution License("CDDL") (collectively, the
-"License"). You may not use this file except in compliance with the
-License. You can obtain a copy of the License at
-http://gephi.org/about/legal/license-notice/
-or /cddl-1.0.txt and /gpl-3.0.txt. See the License for the
-specific language governing permissions and limitations under the
-License.  When distributing the software, include this License Header
-Notice in each file and include the License files at
-/cddl-1.0.txt and /gpl-3.0.txt. If applicable, add the following below the
-License Header, with the fields enclosed by brackets [] replaced by
-your own identifying information:
-"Portions Copyrighted [year] [name of copyright owner]"
-
-If you wish your version of this file to be governed by only the CDDL
-or only the GPL Version 3, indicate your decision by adding
-"[Contributor] elects to include this software in this distribution
-under the [CDDL or GPL Version 3] license." If you do not indicate a
-single choice of license, a recipient has the option to distribute
-your version of this file under either the CDDL, the GPL Version 3 or
-to extend the choice of license to its licensees as provided above.
-However, if you add GPL Version 3 code and therefore, elected the GPL
-Version 3 license, then the option applies only if the new code is
-made subject to such option by the copyright holder.
-
-Contributor(s):
-
-Portions Copyrighted 2011 Gephi Consortium.
- */
-
 package org.gephi.desktop.banner;
-
 import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
@@ -59,108 +16,134 @@ import org.openide.util.Lookup;
  * @author Mathieu Bastian
  */
 public class BannerComponent extends javax.swing.JPanel {
+  private final transient PerspectiveController perspectiveController;
 
-    private final transient PerspectiveController perspectiveController;
-    private transient JToggleButton[] buttons;
-    // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JPanel buttonsPanel;
-    private javax.swing.JPanel groupsPanel;
-    private javax.swing.JPanel mainPanel;
-    private javax.swing.ButtonGroup perspectivesButtonGroup;
-    private javax.swing.JPanel workspacePanel;
-    // End of variables declaration//GEN-END:variables
+  private transient JToggleButton[] buttons;
 
-    public BannerComponent() {
-        initComponents();
+  private javax.swing.JPanel buttonsPanel;
 
-        //Init perspective controller
-        perspectiveController = Lookup.getDefault().lookup(PerspectiveController.class);
+  private javax.swing.JPanel groupsPanel;
 
-        addGroupTabs();
+  private javax.swing.JPanel mainPanel;
 
-        //This defines the height of the banner bar
-        setPreferredSize(new Dimension(100, 65));
+  private javax.swing.ButtonGroup perspectivesButtonGroup;
+
+  private javax.swing.JPanel workspacePanel;
+
+  public BannerComponent() {
+    initComponents();
+    perspectiveController = Lookup.getDefault().lookup(PerspectiveController.class);
+    addGroupTabs();
+    setPreferredSize(new Dimension(100, 65));
+  }
+
+  private void addGroupTabs() {
+    buttons = new JToggleButton[perspectiveController.getPerspectives().length];
+    int i = 0;
+    for (final Perspective perspective : perspectiveController.getPerspectives()) {
+      JToggleButton toggleButton = new JToggleButton(perspective.getDisplayName(), perspective.getIcon());
+      toggleButton.setFocusPainted(false);
+      toggleButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+      toggleButton.addActionListener((e) -> perspectiveController.selectPerspective(perspective));
+      perspectivesButtonGroup.add(toggleButton);
+      buttonsPanel.add(toggleButton);
+      buttons[i++] = toggleButton;
     }
+    perspectivesButtonGroup.setSelected(buttons[getSelectedPerspectiveIndex()].getModel(), true);
+  }
 
-    private void addGroupTabs() {
-        buttons = new JToggleButton[perspectiveController.getPerspectives().length];
-        int i = 0;
-
-        //Add tabs
-        for (final Perspective perspective : perspectiveController.getPerspectives()) {
-            JToggleButton toggleButton =
-                new JToggleButton(perspective.getDisplayName(), perspective.getIcon());
-            toggleButton.setFocusPainted(false);
-            toggleButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-
-            toggleButton.addActionListener(e -> perspectiveController.selectPerspective(perspective));
-            perspectivesButtonGroup.add(toggleButton);
-            buttonsPanel.add(toggleButton);
-            buttons[i++] = toggleButton;
-        }
-
-        //Set currently selected button
-        perspectivesButtonGroup.setSelected(buttons[getSelectedPerspectiveIndex()].getModel(), true);
+  public int getSelectedPerspectiveIndex() {
+    int i = 0;
+    for (Perspective p : perspectiveController.getPerspectives()) {
+      if (p.equals(perspectiveController.getSelectedPerspective())) {
+        return i;
+      }
+      i++;
     }
+    return -1;
+  }
 
-    public int getSelectedPerspectiveIndex() {
-        int i = 0;
-        for (Perspective p : perspectiveController.getPerspectives()) {
-            if (p.equals(perspectiveController.getSelectedPerspective())) {
-                return i;
-            }
-            i++;
-        }
-        return -1;
-    }
-
-    /**
+  /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
      * regenerated by the Form Editor.
      */
-    @SuppressWarnings("unchecked")
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
-    private void initComponents() {
-        java.awt.GridBagConstraints gridBagConstraints;
+  @SuppressWarnings(value = { "unchecked" }) private void initComponents() {
+    java.awt.GridBagConstraints gridBagConstraints;
+    perspectivesButtonGroup = new javax.swing.ButtonGroup();
+    mainPanel = new javax.swing.JPanel();
+    groupsPanel = new javax.swing.JPanel();
+    buttonsPanel = new javax.swing.JPanel();
+    workspacePanel = new org.gephi.desktop.banner.workspace.WorkspacePanel();
+    setBackground(new java.awt.Color(255, 255, 255));
+    setLayout(new java.awt.BorderLayout());
+    mainPanel.setBackground(new java.awt.Color(255, 255, 255));
+    mainPanel.setLayout(new java.awt.GridBagLayout());
 
-        perspectivesButtonGroup = new javax.swing.ButtonGroup();
-        mainPanel = new javax.swing.JPanel();
-        groupsPanel = new javax.swing.JPanel();
-        buttonsPanel = new javax.swing.JPanel();
-        workspacePanel = new org.gephi.desktop.banner.workspace.WorkspacePanel();
+<<<<<<< /usr/src/app/output/gephi/gephi/6d122ed8d5aa839543625e58cf7eb7235576d866/modules/DesktopWindow/src/main/java/org/gephi/desktop/banner/BannerComponent.java/left.java
+    logoButton.setIcon(ImageUtilities.loadImageIcon("org/gephi/desktop/banner/resources/logo_std.png", false));
+=======
+>>>>>>> Unknown file: This is a bug in JDime.
 
-        setBackground(new java.awt.Color(255, 255, 255));
-        setLayout(new java.awt.BorderLayout());
 
-        mainPanel.setBackground(new java.awt.Color(255, 255, 255));
-        mainPanel.setLayout(new java.awt.GridBagLayout());
+<<<<<<< /usr/src/app/output/gephi/gephi/6d122ed8d5aa839543625e58cf7eb7235576d866/modules/DesktopWindow/src/main/java/org/gephi/desktop/banner/BannerComponent.java/left.java
+    logoButton.setPressedIcon(ImageUtilities.loadImageIcon("org/gephi/desktop/banner/resources/logo_glow.png", false));
+=======
+>>>>>>> Unknown file: This is a bug in JDime.
 
-        groupsPanel.setLayout(new java.awt.GridBagLayout());
 
-        buttonsPanel.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        buttonsPanel.setOpaque(false);
-        buttonsPanel.setPreferredSize(new java.awt.Dimension(10, 50));
-        buttonsPanel.setLayout(new java.awt.FlowLayout(java.awt.FlowLayout.LEFT, 3, 3));
-        gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 0;
-        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
-        gridBagConstraints.anchor = java.awt.GridBagConstraints.FIRST_LINE_START;
-        gridBagConstraints.weightx = 1.0;
-        gridBagConstraints.weighty = 1.0;
-        groupsPanel.add(buttonsPanel, gridBagConstraints);
+<<<<<<< /usr/src/app/output/gephi/gephi/6d122ed8d5aa839543625e58cf7eb7235576d866/modules/DesktopWindow/src/main/java/org/gephi/desktop/banner/BannerComponent.java/left.java
+    logoButton.setRolloverIcon(ImageUtilities.loadImageIcon("org/gephi/desktop/banner/resources/logo_glow.png", false));
+=======
+>>>>>>> Unknown file: This is a bug in JDime.
 
-        gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 0;
-        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
-        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
-        gridBagConstraints.weightx = 1.0;
-        gridBagConstraints.weighty = 1.0;
-        mainPanel.add(groupsPanel, gridBagConstraints);
+    groupsPanel.setLayout(new java.awt.GridBagLayout());
+    buttonsPanel.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+    buttonsPanel.setOpaque(false);
+    buttonsPanel.setPreferredSize(new java.awt.Dimension(10, 50));
+    buttonsPanel.setLayout(new java.awt.FlowLayout(java.awt.FlowLayout.LEFT, 3, 3));
+    gridBagConstraints = new java.awt.GridBagConstraints();
+    gridBagConstraints.gridx = 0;
+    gridBagConstraints.gridy = 0;
+    gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+    gridBagConstraints.anchor = java.awt.GridBagConstraints.FIRST_LINE_START;
+    gridBagConstraints.weightx = 1.0;
+    gridBagConstraints.weighty = 1.0;
+    groupsPanel.add(buttonsPanel, gridBagConstraints);
 
-        add(mainPanel, java.awt.BorderLayout.CENTER);
-        add(workspacePanel, java.awt.BorderLayout.SOUTH);
-    }// </editor-fold>//GEN-END:initComponents
+<<<<<<< /usr/src/app/output/gephi/gephi/6d122ed8d5aa839543625e58cf7eb7235576d866/modules/DesktopWindow/src/main/java/org/gephi/desktop/banner/BannerComponent.java/left.java
+    bannerBackground.setIcon(ImageUtilities.loadImageIcon("org/gephi/desktop/banner/resources/bannerback.png", false));
+=======
+>>>>>>> Unknown file: This is a bug in JDime.
+
+    gridBagConstraints = new java.awt.GridBagConstraints();
+    gridBagConstraints.gridx = 0;
+    gridBagConstraints.gridy = 0;
+    gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+    gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
+    gridBagConstraints.weightx = 1.0;
+    gridBagConstraints.weighty = 1.0;
+    mainPanel.add(groupsPanel, gridBagConstraints);
+    add(mainPanel, java.awt.BorderLayout.CENTER);
+    add(workspacePanel, java.awt.BorderLayout.SOUTH);
+  }
+
+
+<<<<<<< /usr/src/app/output/gephi/gephi/6d122ed8d5aa839543625e58cf7eb7235576d866/modules/DesktopWindow/src/main/java/org/gephi/desktop/banner/BannerComponent.java/left.java
+  private static class JPerspectiveButton extends JToggleButton {
+    public JPerspectiveButton(String text, Icon icon) {
+      setText(text);
+      setBorder(null);
+      setBorderPainted(false);
+      setContentAreaFilled(false);
+      setFocusPainted(false);
+      setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+      setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+      setIcon(ImageUtilities.image2Icon(ImageUtilities.mergeImages(ImageUtilities.loadImage("org/gephi/desktop/banner/perspective/resources/aqua-enabled.png", false), ImageUtilities.icon2Image(icon), 6, 3)));
+      setRolloverIcon(ImageUtilities.image2Icon(ImageUtilities.mergeImages(ImageUtilities.loadImage("org/gephi/desktop/banner/perspective/resources/aqua-mouseover.png", false), ImageUtilities.icon2Image(icon), 6, 3)));
+      setSelectedIcon(ImageUtilities.image2Icon(ImageUtilities.mergeImages(ImageUtilities.loadImage("org/gephi/desktop/banner/perspective/resources/aqua-selected.png", false), ImageUtilities.icon2Image(icon), 6, 3)));
+    }
+  }
+=======
+>>>>>>> Unknown file: This is a bug in JDime.
 }
