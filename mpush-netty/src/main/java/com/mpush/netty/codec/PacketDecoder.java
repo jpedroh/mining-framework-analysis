075a1f78a3a9e85ff597c2646341297ea00b4442
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-22 05:27:33 +0000

    Mining Framework Analysis

--- mpush-netty/src/main/java/com/mpush/netty/codec/PacketDecoder.java
+++ mpush-netty/src/main/java/com/mpush/netty/codec/PacketDecoder.java
@@ -1,24 +1,4 @@
-/*
- * (C) Copyright 2015-2016 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * Contributors:
- *   ohun@live.cn (夜色)
- */
-
 package com.mpush.netty.codec;
-
 import com.mpush.api.protocol.Packet;
 import com.mpush.api.protocol.UDPPacket;
 import com.mpush.tools.config.CC;
@@ -27,7 +7,6 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.socket.DatagramPacket;
 import io.netty.handler.codec.ByteToMessageDecoder;
 import io.netty.handler.codec.TooLongFrameException;
-
 import java.util.List;
 
 /**
@@ -37,73 +16,66 @@ import java.util.List;
  * @author ohun@live.cn
  */
 public final class PacketDecoder extends ByteToMessageDecoder {
-    private static final int maxPacketSize = CC.mp.core.max_packet_size;
+  private static final int maxPacketSize = CC.mp.core.max_packet_size;
 
-    @Override
-    protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
-        decodeHeartbeat(in, out);
-        decodeFrames(in, out);
+  @Override protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
+    decodeHeartbeat(in, out);
+    decodeFrames(in, out);
+  }
+
+  private void decodeHeartbeat(ByteBuf in, List<Object> out) {
+    while (in.isReadable()) {
+      if (in.readByte() == Packet.HB_PACKET_BYTE) {
+        out.add(Packet.HB_PACKET);
+      } else {
+        in.readerIndex(in.readerIndex() - 1);
+        break;
+      }
     }
+  }
 
-    private void decodeHeartbeat(ByteBuf in, List<Object> out) {
-        while (in.isReadable()) {
-            if (in.readByte() == Packet.HB_PACKET_BYTE) {
-                out.add(Packet.HB_PACKET);
-            } else {
-                in.readerIndex(in.readerIndex() - 1);
-                break;
-            }
-        }
+  private void decodeFrames(ByteBuf in, List<Object> out) throws Exception {
+    if (in.readableBytes() >= Packet.HEADER_LEN) {
+      in.markReaderIndex();
+      Packet packet = decodeFrame(in);
+      if (packet != null) {
+        out.add(packet);
+      } else {
+        in.resetReaderIndex();
+      }
     }
+  }
 
-    private void decodeFrames(ByteBuf in, List<Object> out) throws Exception {
-        if (in.readableBytes() >= Packet.HEADER_LEN) {
-            //1.记录当前读取位置位置.如果读取到非完整的frame,要恢复到该位置,便于下次读取
-            in.markReaderIndex();
-
-            Packet packet = decodeFrame(in);
-            if (packet != null) {
-                out.add(packet);
-            } else {
-                //2.读取到不完整的frame,恢复到最近一次正常读取的位置,便于下次读取
-                in.resetReaderIndex();
-            }
-        }
+  private Packet decodeFrame(ByteBuf in) throws Exception {
+    int readableBytes = in.readableBytes();
+    int bodyLength = in.readInt();
+    if (readableBytes < (bodyLength + Packet.HEADER_LEN)) {
+      return null;
     }
+    return readPacket(new Packet(in.readByte()), in, bodyLength);
+  }
 
-    private Packet decodeFrame(ByteBuf in) throws Exception {
-        int readableBytes = in.readableBytes();
-        int bodyLength = in.readInt();
-        if (readableBytes < (bodyLength + Packet.HEADER_LEN)) {
-            return null;
-        }
-        return readPacket(new Packet(in.readByte()), in, bodyLength);
+  public static Packet decodeFrame(DatagramPacket datagram) throws Exception {
+    ByteBuf in = datagram.content();
+    int readableBytes = in.readableBytes();
+    int bodyLength = in.readInt();
+    if (readableBytes < (bodyLength + Packet.HEADER_LEN)) {
+      return null;
     }
+    return readPacket(new UDPPacket(in.readByte(), datagram.sender()), in, bodyLength);
+  }
 
-    public static Packet decodeFrame(DatagramPacket datagram) throws Exception {
-        ByteBuf in = datagram.content();
-        int readableBytes = in.readableBytes();
-        int bodyLength = in.readInt();
-        if (readableBytes < (bodyLength + Packet.HEADER_LEN)) {
-            return null;
-        }
-        return readPacket(new UDPPacket(in.readByte()
-                        , datagram.sender()), in, bodyLength);
+  private static Packet readPacket(Packet packet, ByteBuf in, int bodyLength) {
+    packet.cc = in.readShort();
+    packet.flags = in.readByte();
+    packet.sessionId = in.readInt();
+    packet.lrc = in.readByte();
+    if (bodyLength > 0) {
+      if (bodyLength > maxPacketSize) {
+        throw new TooLongFrameException("packet body length over limit:" + bodyLength);
+      }
+      in.readBytes(packet.body = new byte[bodyLength]);
     }
-
-    private static Packet readPacket(Packet packet, ByteBuf in, int bodyLength) {
-        packet.cc = in.readShort();//read cc
-        packet.flags = in.readByte();//read flags
-        packet.sessionId = in.readInt();//read sessionId
-        packet.lrc = in.readByte();//read lrc
-
-        //read body
-        if (bodyLength > 0) {
-            if (bodyLength > maxPacketSize) {
-                throw new TooLongFrameException("packet body length over limit:" + bodyLength);
-            }
-            in.readBytes(packet.body = new byte[bodyLength]);
-        }
-        return packet;
-    }
+    return packet;
+  }
 }
