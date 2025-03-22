6b47d4e36a3b1bdc06f18647fe75c55f9ce9395d
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-22 07:53:17 +0000

    Mining Framework Analysis

--- src/test/java/com/michelboudreau/test/AlternatorTestSuite.java
+++ src/test/java/com/michelboudreau/test/AlternatorTestSuite.java
@@ -5,6 +5,12 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ AlternatorTableTest.class , AlternatorItemTest.class , AlternatorQueryTest.class, AlternatorScanTest.class, AlternatorBatchItemTest.class, AlternatorSaveRestoreTest.class, AlternatorMapperTest.class})
+<<<<<<< /usr/src/app/output/mboudreau/alternator/9adf68eb8af7a41af55b72a8cd6a5b6191b7026c/src/test/java/com/michelboudreau/test/AlternatorTestSuite.java/left.java
+@Suite.SuiteClasses({ AlternatorTableTest.class , AlternatorItemTest.class , AlternatorQueryTest.class, AlternatorScanTest.class, AlternatorBatchItemTest.class, AlternatorSaveRestoreTest.class})
+||||||| /usr/src/app/output/mboudreau/alternator/9adf68eb8af7a41af55b72a8cd6a5b6191b7026c/src/test/java/com/michelboudreau/test/AlternatorTestSuite.java/base.java
+@Suite.SuiteClasses({ AlternatorTableTest.class , AlternatorItemTest.class , AlternatorQueryTest.class, AlternatorScanTest.class, AlternatorBatchItemTest.class})
+=======
+@Suite.SuiteClasses({ AlternatorTableTest.class , AlternatorItemTest.class , AlternatorQueryTest.class, AlternatorScanTest.class, AlternatorBatchItemTest.class, AlternatorMapperTest.class})
+>>>>>>> /usr/src/app/output/mboudreau/alternator/9adf68eb8af7a41af55b72a8cd6a5b6191b7026c/src/test/java/com/michelboudreau/test/AlternatorTestSuite.java/right.java
 public class AlternatorTestSuite {
 }
