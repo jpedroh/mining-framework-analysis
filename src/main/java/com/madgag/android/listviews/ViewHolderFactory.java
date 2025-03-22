0408cf7c257fddfb1936a00a454cdf2e26c1d376
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-22 00:45:04 +0000

    Mining Framework Analysis

--- src/main/java/com/madgag/android/listviews/ViewHolderFactory.java
+++ src/main/java/com/madgag/android/listviews/ViewHolderFactory.java
@@ -1,27 +1,14 @@
-/*
- * Copyright 2011 Roberto Tyley
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- */
-
 package com.madgag.android.listviews;
-
-
 import android.view.View;
 
-public interface ViewHolderFactory<T> {
+public interface ViewHolderFactory<T extends java.lang.Object> {
+  ViewHolder<T> createViewHolderFor(View view);
 
-    ViewHolder<T> createViewHolderFor(View view);
-
-    Class<? extends ViewHolder<T>> getHolderClass();
+  Class<
+<<<<<<< /usr/src/app/output/rtyley/android-viewholder-listviews/b5a40bb804382b21052c99890298fc5afcd4b6b1/src/main/java/com/madgag/android/listviews/ViewHolderFactory.java/left.java
+  ? extends ViewHolder<T>
+=======
+  ViewHolder<T>
+>>>>>>> /usr/src/app/output/rtyley/android-viewholder-listviews/b5a40bb804382b21052c99890298fc5afcd4b6b1/src/main/java/com/madgag/android/listviews/ViewHolderFactory.java/right.java
+  > getHolderClass();
 }
