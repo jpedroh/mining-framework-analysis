1aefa677386059b8e748383507ea984a2b40556b
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-22 01:09:35 +0000

    Mining Framework Analysis

--- src/main/java/net/tridentsdk/ui/tablist/TabList.java
+++ src/main/java/net/tridentsdk/ui/tablist/TabList.java
@@ -1,24 +1,6 @@
-/*
- * Trident - A Multithreaded Server Alternative
- * Copyright 2016 The TridentSDK Team
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package net.tridentsdk.ui.tablist;
-
 import net.tridentsdk.chat.ChatComponent;
 import net.tridentsdk.entity.living.Player;
-
 import java.util.Collection;
 
 /**
@@ -39,72 +21,72 @@ import java.util.Collection;
  * @since 0.5-alpha
  */
 public interface TabList {
-    /**
+  /**
      * Set an element of the tablist to a specific value.
      * If value is null, the element will be removed
      *
      * @param slot the slot number of the element
      * @param value the value of the element
      */
-    void setElement(int slot, ChatComponent value);
+  void setElement(int slot, ChatComponent value);
 
-    /**
+  /**
      * Obtains the value of the specified element
      *
      * @param slot the slot number of the element
      * @return the value of the element
      */
-    ChatComponent getElement(int slot);
+  ChatComponent getElement(int slot);
 
-    /**
+  /**
      * Change the header of the tablist.
      * If value is null, header will be removed.
      *
      * @param value the new header or null
      */
-    void setHeader(ChatComponent value);
+  void setHeader(ChatComponent value);
 
-    /**
+  /**
      * Obtain the current header of this tablist.
      *
      * @return the current header
      */
-    ChatComponent getHeader();
+  ChatComponent getHeader();
 
-    /**
+  /**
      * Change the footer of the tablist.
      * If value is null, footer will be removed.
      *
      * @param value the new footer or null
      */
-    void setFooter(ChatComponent value);
+  void setFooter(ChatComponent value);
 
-    /**
+  /**
      * Obtain the current footer of this tablist.
      *
      * @return the current footer
      */
-    ChatComponent getFooter();
+  ChatComponent getFooter();
 
-    /**
+  /**
      * Return a list of all players that currently use
      * this tablist.
      *
      * @return list of tablist users
      */
-    Collection<Player> getUserList();
+  Collection<Player> getUserList();
 
-    /**
+  /**
      * Add a player as a user of this tablist
      *
      * @param player the player to add
      */
-    void addUser(Player player);
+  void addUser(Player player);
 
-    /**
+  /**
      * Remove a player as a user of this tablist
      *
      * @param player the player to remove
      */
-    void removeUser(Player player);
+  void removeUser(Player player);
 }
--- src/main/java/net/tridentsdk/ui/tablist/TabListManager.java
+++ src/main/java/net/tridentsdk/ui/tablist/TabListManager.java
@@ -1,21 +1,4 @@
-/*
- * Trident - A Multithreaded Server Alternative
- * Copyright 2016 The TridentSDK Team
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package net.tridentsdk.ui.tablist;
-
 import net.tridentsdk.Impl;
 
 /**
@@ -29,26 +12,26 @@ import net.tridentsdk.Impl;
  * @since 0.5-alpha
  */
 public interface TabListManager {
-    /**
+  /**
      * Obtain an instance of the TabList Manager
      *
      * @return an instance of TabList Manager
      */
-    static TabListManager getInstance(){
-        return Impl.get().tabListManager();
-    }
+  static TabListManager getInstance() {
+    return Impl.get().tabListManager();
+  }
 
-    /**
+  /**
      * Obtain the global tablist shared between all players.
      *
      * @return the global tablist
      */
-    TabList getGlobalTabList();
+  TabList getGlobalTabList();
 
-    /**
+  /**
      * Obtain a new tablist instance.
      *
      * @return a new tablist instance.
      */
-    TabList newTabList();
+  TabList newTabList();
 }
