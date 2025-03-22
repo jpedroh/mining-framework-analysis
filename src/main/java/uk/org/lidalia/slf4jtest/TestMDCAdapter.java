f973647956f7122a754946f5e128a5e96bd49957
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-22 00:58:07 +0000

    Mining Framework Analysis

--- src/main/java/uk/org/lidalia/slf4jtest/TestMDCAdapter.java
+++ src/main/java/uk/org/lidalia/slf4jtest/TestMDCAdapter.java
@@ -1,46 +1,38 @@
 package uk.org.lidalia.slf4jtest;
-
 import java.util.HashMap;
 import java.util.Map;
-
 import org.slf4j.spi.MDCAdapter;
-
 import com.google.common.base.Function;
 import com.google.common.collect.FluentIterable;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Maps;
-
 import uk.org.lidalia.lang.ThreadLocal;
-
 import static com.google.common.base.Optional.fromNullable;
 
 public class TestMDCAdapter implements MDCAdapter {
+  private final ThreadLocal<Map<String, String>> value = new ThreadLocal<Map<String, String>>(Suppliers.<String, String>makeEmptyMutableMap());
 
-    private final ThreadLocal<Map<String, String>> value = new ThreadLocal<Map<String, String>>(
-            Suppliers.<String, String>makeEmptyMutableMap());
+  public void put(final String key, final String val) {
+    value.get().put(key, fromNullable(val).or("null"));
+  }
 
-    public void put(final String key, final String val) {
-        value.get().put(key, fromNullable(val).or("null"));
-    }
+  public String get(final String key) {
+    return value.get().get(key);
+  }
 
-    public String get(final String key) {
-        return value.get().get(key);
-    }
+  public void remove(final String key) {
+    value.get().remove(key);
+  }
 
-    public void remove(final String key) {
-        value.get().remove(key);
-    }
+  public void clear() {
+    value.get().clear();
+  }
 
-    public void clear() {
-        value.get().clear();
-    }
+  public ImmutableMap<String, String> getCopyOfContextMap() {
+    return ImmutableMap.copyOf(value.get());
+  }
 
-    public ImmutableMap<String, String> getCopyOfContextMap() {
-        return ImmutableMap.copyOf(value.get());
-    }
-
-    @SuppressWarnings("unchecked")
-    public void setContextMap(final Map contextMap) {
-        value.set(new HashMap<String, String>(contextMap));
-    }
+  @SuppressWarnings(value = { "unchecked" }) public void setContextMap(final Map contextMap) {
+    value.set(new HashMap<String, String>(contextMap));
+  }
 }
