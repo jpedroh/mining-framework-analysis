f5d6b2b52563f64648e7e4506ad5c551afa18267
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-22 06:11:07 +0000

    Mining Framework Analysis

--- sm-shop/src/main/java/com/salesmanager/shop/store/api/v1/search/SearchApi.java
+++ sm-shop/src/main/java/com/salesmanager/shop/store/api/v1/search/SearchApi.java
@@ -1,8 +1,4 @@
 package com.salesmanager.shop.store.api.v1.search;
-
-
-import static com.salesmanager.core.business.constants.Constants.DEFAULT_STORE;
-
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import javax.inject.Inject;
@@ -13,8 +9,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 import com.salesmanager.core.model.merchant.MerchantStore;
+import org.springframework.web.bind.annotation.RestController;
 import com.salesmanager.core.model.reference.language.Language;
 import com.salesmanager.shop.model.ValueList;
 import com.salesmanager.shop.model.catalog.SearchProductList;
@@ -22,6 +18,7 @@ import com.salesmanager.shop.model.catalog.SearchProductRequest;
 import com.salesmanager.shop.store.controller.search.facade.SearchFacade;
 import com.salesmanager.shop.store.controller.store.facade.StoreFacade;
 import com.salesmanager.shop.utils.LanguageUtils;
+import springfox.documentation.annotations.ApiIgnore;
 
 /**
  * Api for searching shopizer catalog based on search term when filtering products based on product
@@ -29,10 +26,7 @@ import com.salesmanager.shop.utils.LanguageUtils;
  *
  * @author c.samson
  */
-@RestController
-@RequestMapping("/api/v1")
-public class SearchApi {
-
+@RestController @RequestMapping(value = "/api/v1") public class SearchApi {
   private static final Logger LOGGER = LoggerFactory.getLogger(SearchApi.class);
 
   @Inject private SearchFacade searchFacade;
@@ -48,29 +42,11 @@ public class SearchApi {
    * @param request
    * @return
    */
-  @PostMapping("/search")
-  @ApiImplicitParams({
-      @ApiImplicitParam(name = "store", dataType = "String", defaultValue = "DEFAULT"),
-      @ApiImplicitParam(name = "lang", dataType = "String", defaultValue = "en")
-  })
-  public @ResponseBody SearchProductList search(
-      @RequestBody SearchProductRequest searchRequest,
-      @ApiIgnore MerchantStore merchantStore,
-      @ApiIgnore Language language,
-      HttpServletRequest request) {
+  @PostMapping(value = "/search") @ApiImplicitParams(value = { @ApiImplicitParam(name = "store", dataType = "String", defaultValue = "DEFAULT"), @ApiImplicitParam(name = "lang", dataType = "String", defaultValue = "en") }) public @ResponseBody SearchProductList search(@RequestBody SearchProductRequest searchRequest, @ApiIgnore MerchantStore merchantStore, @ApiIgnore Language language, HttpServletRequest request) {
     return searchFacade.search(merchantStore, language, searchRequest);
   }
 
-  @PostMapping("/search/autocomplete")
-  @ApiImplicitParams({
-      @ApiImplicitParam(name = "store", dataType = "String", defaultValue = "DEFAULT"),
-      @ApiImplicitParam(name = "lang", dataType = "String", defaultValue = "en")
-  })
-  public @ResponseBody ValueList autocomplete(
-      @RequestBody SearchProductRequest searchRequest,
-      @ApiIgnore MerchantStore merchantStore,
-      @ApiIgnore Language language,
-      HttpServletRequest request) {
+  @PostMapping(value = "/search/autocomplete") @ApiImplicitParams(value = { @ApiImplicitParam(name = "store", dataType = "String", defaultValue = "DEFAULT"), @ApiImplicitParam(name = "lang", dataType = "String", defaultValue = "en") }) public @ResponseBody ValueList autocomplete(@RequestBody SearchProductRequest searchRequest, @ApiIgnore MerchantStore merchantStore, @ApiIgnore Language language, HttpServletRequest request) {
     return searchFacade.autocompleteRequest(searchRequest.getQuery(), merchantStore, language);
   }
 }
