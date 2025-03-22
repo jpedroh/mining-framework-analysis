7a8dbf6e1e12e91f33c8834e33f4701e83792caf
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-22 05:46:04 +0000

    Mining Framework Analysis

--- src/main/java/net/logstash/logback/LogstashFormatter.java
+++ src/main/java/net/logstash/logback/LogstashFormatter.java
@@ -1,30 +1,13 @@
-/**
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package net.logstash.logback;
-
 import java.io.IOException;
-import java.util.Iterator;
-import java.util.Map;
-
-import org.apache.commons.lang.time.FastDateFormat;
-import org.slf4j.Marker;
-
 import ch.qos.logback.classic.spi.ILoggingEvent;
+import java.util.Iterator;
 import ch.qos.logback.classic.spi.IThrowableProxy;
+import java.util.Map;
 import ch.qos.logback.classic.spi.ThrowableProxyUtil;
+import org.apache.commons.lang.time.FastDateFormat;
 import ch.qos.logback.core.Context;
-
+import org.slf4j.Marker;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -32,142 +15,132 @@ import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
 /**
- * 
+ *
  */
 public class LogstashFormatter {
-    
-    private static final ObjectMapper MAPPER = new ObjectMapper().configure(JsonGenerator.Feature.ESCAPE_NON_ASCII, true);
-    private static final FastDateFormat ISO_DATETIME_TIME_ZONE_FORMAT_WITH_MILLIS = FastDateFormat.getInstance("yyyy-MM-dd'T'HH:mm:ss.SSSZZ");
-    private static final StackTraceElement DEFAULT_CALLER_DATA = new StackTraceElement("", "", "", 0);
-    
-    private boolean includeCallerInfo;
-    private JsonNode customFields;
-    
-    public LogstashFormatter() {
-        this(false);
+  private static final ObjectMapper MAPPER = new ObjectMapper().configure(JsonGenerator.Feature.ESCAPE_NON_ASCII, true);
+
+  private static final FastDateFormat ISO_DATETIME_TIME_ZONE_FORMAT_WITH_MILLIS = FastDateFormat.getInstance("yyyy-MM-dd\'T\'HH:mm:ss.SSSZZ");
+
+  private static final StackTraceElement DEFAULT_CALLER_DATA = new StackTraceElement("", "", "", 0);
+
+  private boolean includeCallerInfo;
+
+  private JsonNode customFields;
+
+  public LogstashFormatter(boolean includeCallerInfo) {
+    this.includeCallerInfo = includeCallerInfo;
+  }
+
+  public LogstashFormatter(boolean includeCallerInfo, JsonNode customFields) {
+    this.includeCallerInfo = includeCallerInfo;
+    this.customFields = customFields;
+  }
+
+  public LogstashFormatter() {
+    this(false);
+  }
+
+  public byte[] writeValueAsBytes(ILoggingEvent event, Context context) throws IOException {
+    return MAPPER.writeValueAsBytes(eventToNode(event, context));
+  }
+
+  public String writeValueAsString(ILoggingEvent event, Context context) throws IOException {
+    return MAPPER.writeValueAsString(eventToNode(event, context));
+  }
+
+  private ObjectNode eventToNode(ILoggingEvent event, Context context) {
+    ObjectNode eventNode = MAPPER.createObjectNode();
+    eventNode.put("@timestamp", ISO_DATETIME_TIME_ZONE_FORMAT_WITH_MILLIS.format(event.getTimeStamp()));
+    eventNode.put("@version", 1);
+    eventNode.put("message", event.getFormattedMessage());
+    createFields(event, context, eventNode);
+    eventNode.put("tags", createTags(event));
+    return eventNode;
+  }
+
+  private void createFields(ILoggingEvent event, Context context, ObjectNode eventNode) {
+    eventNode.put("logger_name", event.getLoggerName());
+    eventNode.put("thread_name", event.getThreadName());
+    eventNode.put("level", event.getLevel().toString());
+    eventNode.put("level_value", event.getLevel().toInt());
+    if (includeCallerInfo) {
+      StackTraceElement callerData = extractCallerData(event);
+      eventNode.put("caller_class_name", callerData.getClassName());
+      eventNode.put("caller_method_name", callerData.getMethodName());
+      eventNode.put("caller_file_name", callerData.getFileName());
+      eventNode.put("caller_line_number", callerData.getLineNumber());
     }
-    
-    public LogstashFormatter(boolean includeCallerInfo) {
-        this.includeCallerInfo = includeCallerInfo;
+    IThrowableProxy throwableProxy = event.getThrowableProxy();
+    if (throwableProxy != null) {
+      eventNode.put("stack_trace", ThrowableProxyUtil.asString(throwableProxy));
     }
-    
-    public LogstashFormatter(boolean includeCallerInfo, JsonNode customFields) {
-        this.includeCallerInfo = includeCallerInfo;
-        this.customFields = customFields;
+    if (context != null) {
+      addPropertiesAsFields(eventNode, context.getCopyOfPropertyMap());
     }
-    
-    public byte[] writeValueAsBytes(ILoggingEvent event, Context context) throws IOException {
-        return MAPPER.writeValueAsBytes(eventToNode(event, context));
-    }
-    
-    public String writeValueAsString(ILoggingEvent event, Context context) throws IOException {
-        return MAPPER.writeValueAsString(eventToNode(event, context));
-    }
-    
-    private ObjectNode eventToNode(ILoggingEvent event, Context context) {
-        ObjectNode eventNode = MAPPER.createObjectNode();
-        eventNode.put("@timestamp", ISO_DATETIME_TIME_ZONE_FORMAT_WITH_MILLIS.format(event.getTimeStamp()));
-        eventNode.put("@version", 1);
-        eventNode.put("message", event.getFormattedMessage());
-        createFields(event, context, eventNode);
-        eventNode.put("tags", createTags(event));
-        return eventNode;
-    }
-    
-    private void createFields(ILoggingEvent event, Context context, ObjectNode eventNode) {
-        
-        eventNode.put("logger_name", event.getLoggerName());
-        eventNode.put("thread_name", event.getThreadName());
-        eventNode.put("level", event.getLevel().toString());
-        eventNode.put("level_value", event.getLevel().toInt());
-        
-        if (includeCallerInfo) {
-            StackTraceElement callerData = extractCallerData(event);
-            eventNode.put("caller_class_name", callerData.getClassName());
-            eventNode.put("caller_method_name", callerData.getMethodName());
-            eventNode.put("caller_file_name", callerData.getFileName());
-            eventNode.put("caller_line_number", callerData.getLineNumber());
+    addPropertiesAsFields(eventNode, event.getMDCPropertyMap());
+    addCustomFields(eventNode);
+  }
+
+  private ArrayNode createTags(ILoggingEvent event) {
+    ArrayNode node = null;
+    final Marker marker = event.getMarker();
+    if (marker != null) {
+      node = MAPPER.createArrayNode();
+      node.add(marker.getName());
+      if (marker.hasReferences()) {
+        final Iterator<?> i = event.getMarker().iterator();
+        while (i.hasNext()) {
+          Marker next = (Marker) i.next();
+          node.add(next.getName());
         }
-        
-        IThrowableProxy throwableProxy = event.getThrowableProxy();
-        if (throwableProxy != null) {
-            eventNode.put("stack_trace", ThrowableProxyUtil.asString(throwableProxy));
-        }
-        
-        if (context != null) {
-            addPropertiesAsFields(eventNode, context.getCopyOfPropertyMap());
-        }
-        addPropertiesAsFields(eventNode, event.getMDCPropertyMap());
-        
-        addCustomFields(eventNode);
-        
+      }
     }
-    
-    private ArrayNode createTags(ILoggingEvent event) {
-        ArrayNode node = null;
-        final Marker marker = event.getMarker();
-        
-        if (marker != null) {
-            node = MAPPER.createArrayNode();
-            node.add(marker.getName());
-            
-            if (marker.hasReferences()) {
-                final Iterator<?> i = event.getMarker().iterator();
-                
-                while (i.hasNext()) {
-                    Marker next = (Marker) i.next();
-                    
-                    // attached markers will never be null as provided by the MarkerFactory.
-                    node.add(next.getName());
-                }
-            }
-        }
-        
-        return node;
+    return node;
+  }
+
+  private void addPropertiesAsFields(final ObjectNode fieldsNode, final Map<String, String> properties) {
+    if (properties != null) {
+      for (Map.Entry<String, String> entry : properties.entrySet()) {
+        String key = entry.getKey();
+        String value = entry.getValue();
+        fieldsNode.put(key, value);
+      }
     }
-    
-    private void addPropertiesAsFields(final ObjectNode fieldsNode, final Map<String, String> properties) {
-        if (properties != null) {
-            for (Map.Entry<String, String> entry : properties.entrySet()) {
-                String key = entry.getKey();
-                String value = entry.getValue();
-                fieldsNode.put(key, value);
-            }
-        }
+  }
+
+  private StackTraceElement extractCallerData(final ILoggingEvent event) {
+    final StackTraceElement[] ste = event.getCallerData();
+    if (ste == null || ste.length == 0) {
+      return DEFAULT_CALLER_DATA;
     }
-    
-    private StackTraceElement extractCallerData(final ILoggingEvent event) {
-        final StackTraceElement[] ste = event.getCallerData();
-        if (ste == null || ste.length == 0) {
-            return DEFAULT_CALLER_DATA;
-        }
-        return ste[0];
+    return ste[0];
+  }
+
+  private void addCustomFields(ObjectNode eventNode) {
+    if (customFields != null) {
+      Iterator<String> i = customFields.fieldNames();
+      while (i.hasNext()) {
+        String k = i.next();
+        JsonNode v = customFields.get(k);
+        eventNode.put(k, v);
+      }
     }
-    
-    private void addCustomFields(ObjectNode eventNode) {
-        if (customFields != null) {
-            Iterator<String> i = customFields.fieldNames();
-            while (i.hasNext()) {
-                String k = i.next();
-                JsonNode v = customFields.get(k);
-                eventNode.put(k, v);
-            }
-        }
-    }
-    
-    public boolean isIncludeCallerInfo() {
-        return includeCallerInfo;
-    }
-    
-    public void setIncludeCallerInfo(boolean includeCallerInfo) {
-        this.includeCallerInfo = includeCallerInfo;
-    }
-    
-    public void setCustomFields(JsonNode customFields) {
-        this.customFields = customFields;
-    }
-    
-    public JsonNode getCustomFields() {
-        return this.customFields;
-    }
+  }
+
+  public boolean isIncludeCallerInfo() {
+    return includeCallerInfo;
+  }
+
+  public void setIncludeCallerInfo(boolean includeCallerInfo) {
+    this.includeCallerInfo = includeCallerInfo;
+  }
+
+  public void setCustomFields(JsonNode customFields) {
+    this.customFields = customFields;
+  }
+
+  public JsonNode getCustomFields() {
+    return this.customFields;
+  }
 }
