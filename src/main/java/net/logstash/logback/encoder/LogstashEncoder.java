c732ac11a1e8f69ca9f1292714a0a9589db58569
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-22 05:38:08 +0000

    Mining Framework Analysis

--- src/main/java/net/logstash/logback/encoder/LogstashEncoder.java
+++ src/main/java/net/logstash/logback/encoder/LogstashEncoder.java
@@ -1,24 +1,8 @@
-/**
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package net.logstash.logback.encoder;
-
 import static org.apache.commons.io.IOUtils.LINE_SEPARATOR;
 import static org.apache.commons.io.IOUtils.write;
-
 import java.io.IOException;
 import java.util.Map;
-
 import net.logstash.logback.LogstashFormatter;
 import net.logstash.logback.marker.Markers;
 import ch.qos.logback.classic.spi.ILoggingEvent;
@@ -26,53 +10,47 @@ import ch.qos.logback.core.CoreConstants;
 import ch.qos.logback.core.encoder.EncoderBase;
 
 public class LogstashEncoder extends EncoderBase<ILoggingEvent> {
-    
-    private boolean immediateFlush = true;
-    
-    private final LogstashFormatter formatter = new LogstashFormatter();
-    
-    @Override
-    public void doEncode(ILoggingEvent event) throws IOException {
-        
-        formatter.writeValueToOutputStream(event, context, outputStream);
-        write(CoreConstants.LINE_SEPARATOR, outputStream);
-        
-        if (immediateFlush) {
-            outputStream.flush();
-        }
-        
+  private boolean immediateFlush = true;
+
+  private final LogstashFormatter formatter = new LogstashFormatter();
+
+  @Override public void doEncode(ILoggingEvent event) throws IOException {
+    formatter.writeValueToOutputStream(event, context, outputStream);
+    write(CoreConstants.LINE_SEPARATOR, outputStream);
+    if (immediateFlush) {
+      outputStream.flush();
     }
-    
-    @Override
-    public void close() throws IOException {
-        write(LINE_SEPARATOR, outputStream);
-    }
-    
-    public boolean isImmediateFlush() {
-        return immediateFlush;
-    }
-    
-    public void setImmediateFlush(boolean immediateFlush) {
-        this.immediateFlush = immediateFlush;
-    }
-    
-    public boolean isIncludeCallerInfo() {
-        return formatter.isIncludeCallerInfo();
-    }
-    
-    public void setIncludeCallerInfo(boolean includeCallerInfo) {
-        formatter.setIncludeCallerInfo(includeCallerInfo);
-    }
-    
-    public void setCustomFields(String customFields) {
-        formatter.setCustomFieldsFromString(customFields, this);
-    }
-    
-    public String getCustomFields() {
-        return formatter.getCustomFields().toString();
-    }
-    
-    /**
+  }
+
+  @Override public void close() throws IOException {
+    write(LINE_SEPARATOR, outputStream);
+  }
+
+  public boolean isImmediateFlush() {
+    return immediateFlush;
+  }
+
+  public void setImmediateFlush(boolean immediateFlush) {
+    this.immediateFlush = immediateFlush;
+  }
+
+  public boolean isIncludeCallerInfo() {
+    return formatter.isIncludeCallerInfo();
+  }
+
+  public void setIncludeCallerInfo(boolean includeCallerInfo) {
+    formatter.setIncludeCallerInfo(includeCallerInfo);
+  }
+
+  public void setCustomFields(String customFields) {
+    formatter.setCustomFieldsFromString(customFields, this);
+  }
+
+  public String getCustomFields() {
+    return formatter.getCustomFields().toString();
+  }
+
+  /**
      * <p>
      * If set to true the encoder will search logging event array and if the last item is a Map, entries will be included in the message.
      * </p>
@@ -97,22 +75,16 @@ public class LogstashEncoder extends EncoderBase<ILoggingEvent> {
      * </p>
      * 
      * @param enableContextMap <code>true</code> to enable context map
-     * @deprecated When logging, prefer using a {@link Markers#appendEntries(Map)} marker instead.
      */
-    @Deprecated
-    public void setEnableContextMap(boolean enableContextMap) {
-        formatter.setEnableContextMap(enableContextMap);
-    }
-    
-    /**
-     * @deprecated When logging, prefer using a {@link Markers#appendEntries(Map)} marker instead.
-     */
-    @Deprecated
-    public boolean isEnableContextMap() {
-        return formatter.isEnableContextMap();
-    }
-    
-    protected LogstashFormatter getFormatter() {
-		return formatter;
-	}
+  @Deprecated public void setEnableContextMap(boolean enableContextMap) {
+    formatter.setEnableContextMap(enableContextMap);
+  }
+
+  @Deprecated public boolean isEnableContextMap() {
+    return formatter.isEnableContextMap();
+  }
+
+  protected LogstashFormatter getFormatter() {
+    return formatter;
+  }
 }
