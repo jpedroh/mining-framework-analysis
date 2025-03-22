19eb03571e7ec4bd9c485d6dd0884b86c3737a65
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-22 10:30:58 +0000

    Mining Framework Analysis

--- scribejava-httpclient-okhttp/src/test/java/com/github/scribejava/httpclient/okhttp/OkHttpHttpClientTest.java
+++ scribejava-httpclient-okhttp/src/test/java/com/github/scribejava/httpclient/okhttp/OkHttpHttpClientTest.java
@@ -1,13 +1,6 @@
-package com.github.scribejava.httpclient.okhttp;
-
-import com.github.scribejava.core.AbstractClientTest;
-import com.github.scribejava.core.httpclient.HttpClient;
-import okhttp3.OkHttpClient;
-
-public class OkHttpHttpClientTest extends AbstractClientTest {
-
-    @Override
-    protected HttpClient createNewClient() {
-        return new OkHttpHttpClient(new OkHttpClient());
-    }
-}
+  package     com . github . scribejava . httpclient . okhttp ;   import      com . github . scribejava . core . httpclient . HttpClient ;  import  okhttp3 . OkHttpClient ;  import     com . github . scribejava . core . AbstractClientTest ;   public class OkHttpHttpClientTest  extends AbstractClientTest  { 
+<<<<<<<
+  private OAuthService  oAuthService ;
+=======
+>>>>>>>
+    @ Override protected HttpClient createNewClient  ( )  {  return  new OkHttpHttpClient  (  new OkHttpClient  ( ) ) ; } }
