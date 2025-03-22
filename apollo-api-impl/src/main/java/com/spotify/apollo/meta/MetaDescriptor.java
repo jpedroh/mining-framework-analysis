a3dd8768374ef818563dcc05888d293736b0ff85
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-22 06:15:06 +0000

    Mining Framework Analysis

--- apollo-api-impl/src/main/java/com/spotify/apollo/meta/MetaDescriptor.java
+++ apollo-api-impl/src/main/java/com/spotify/apollo/meta/MetaDescriptor.java
@@ -1,27 +1,6 @@
-/*
- * -\-\-
- * Spotify Apollo API Implementations
- * --
- * Copyright (C) 2013 - 2015 Spotify AB
- * --
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * -/-/-
- */
 package com.spotify.apollo.meta;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.io.IOException;
 import java.net.URL;
 import java.util.Enumeration;
@@ -30,12 +9,12 @@ import java.util.jar.Attributes;
 import java.util.jar.Manifest;
 
 public class MetaDescriptor {
-
   private static final Logger LOG = LoggerFactory.getLogger(MetaDescriptor.class);
 
   private static final String IMPL_VERSION = "Implementation-Version";
 
   private final Descriptor descriptor;
+
   private final String apolloVersion;
 
   MetaDescriptor(Descriptor descriptor, String apolloVersion) {
@@ -51,23 +30,17 @@ public class MetaDescriptor {
     return apolloVersion;
   }
 
-  public static MetaDescriptor readMetaDescriptor(String serviceName, ClassLoader classLoader)
-      throws IOException {
+  public static MetaDescriptor readMetaDescriptor(String serviceName, ClassLoader classLoader) throws IOException {
     String apolloVersion;
     String version;
-
     try {
       apolloVersion = loadApolloVersion(classLoader);
     } catch (IOException e) {
       apolloVersion = "0.0.0-UNKNOWN";
     }
-
     version = loadVersion(classLoader);
     version = version != null ? version : "0.0.0-UNKNOWN";
-
-    return new MetaDescriptor(
-        Descriptor.create(serviceName, version),
-        apolloVersion);
+    return new MetaDescriptor(Descriptor.create(serviceName, version), apolloVersion);
   }
 
   protected static String loadApolloVersion(ClassLoader classLoader) throws IOException {
@@ -87,23 +60,19 @@ public class MetaDescriptor {
   private static String loadVersion(ClassLoader classLoader) throws IOException {
     try {
       Enumeration<URL> resources = classLoader.getResources("META-INF/MANIFEST.MF");
-
       while (resources.hasMoreElements()) {
         final URL url = resources.nextElement();
         final Manifest manifest = new Manifest(url.openStream());
         final Attributes mainAttributes = manifest.getMainAttributes();
         final String value = mainAttributes.getValue(IMPL_VERSION);
-
         if (value != null) {
           return value;
         }
       }
-
     } catch (IOException e) {
       LOG.error("Failed to read manifest", e);
       throw new IOException("Failed to find manifest", e);
     }
-
     return null;
   }
 }
--- apollo-api-impl/src/main/java/com/spotify/apollo/meta/MetaInfoTracker.java
+++ apollo-api-impl/src/main/java/com/spotify/apollo/meta/MetaInfoTracker.java
@@ -1,24 +1,4 @@
-/*
- * -\-\-
- * Spotify Apollo API Implementations
- * --
- * Copyright (C) 2013 - 2015 Spotify AB
- * --
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * -/-/-
- */
 package com.spotify.apollo.meta;
-
 import com.spotify.apollo.Request;
 import com.spotify.apollo.dispatch.Endpoint;
 import com.spotify.apollo.dispatch.EndpointInfo;
@@ -31,25 +11,15 @@ import com.spotify.apollo.meta.model.MetaGatherer.EndpointGatherer;
 import com.spotify.apollo.meta.model.MetaInfoBuilder;
 import com.spotify.apollo.meta.model.Model;
 import com.typesafe.config.Config;
-
 import java.util.List;
 import java.util.Optional;
-
 import static java.util.Collections.singletonList;
 
 public class MetaInfoTracker {
-
   private final MetaGatherer gatherer;
 
-  public MetaInfoTracker(
-      Descriptor descriptor,
-      String containerVersion,
-      Config configNode) {
-    final Model.MetaInfo metaInfo = new MetaInfoBuilder()
-        .buildVersion(descriptor.serviceName() + ' ' + descriptor.version())
-        .containerVersion(containerVersion)
-        .build();
-
+  public MetaInfoTracker(Descriptor descriptor, String containerVersion, Config configNode) {
+    final Model.MetaInfo metaInfo = new MetaInfoBuilder().buildVersion(descriptor.serviceName() + ' ' + descriptor.version()).containerVersion(containerVersion).build();
     gatherer = Meta.createGatherer(metaInfo, configNode);
   }
 
@@ -61,21 +31,12 @@ public class MetaInfoTracker {
     CallsGatherer callsGatherer = gatherer.getServiceCallsGatherer();
     for (E endpoint : endpoints) {
       EndpointInfo info = endpoint.info();
-
       String uri = info.getUri();
       String endpointDocString = formatDocString(info.getDocString());
-
-      EndpointGatherer endpointGatherer =
-          callsGatherer.uriMethodsEndpointGatherer(uri, singletonList(info.getRequestMethod()));
-
+      EndpointGatherer endpointGatherer = callsGatherer.uriMethodsEndpointGatherer(uri, singletonList(info.getRequestMethod()));
       endpointGatherer.setUri(uri);
       endpointGatherer.addMethod(info.getRequestMethod());
       endpointGatherer.setDocstring(endpointDocString);
-
-      // see https://github.com/spotify/apollo/issues/13
-//      endpointGatherer.setResponseContentType(endpoint.getContentType());
-
-      // query parameters are not available
     }
   }
 
@@ -83,14 +44,11 @@ public class MetaInfoTracker {
     if (!docString.isPresent()) {
       return "Lacks DocString annotation.";
     }
-
     return docString.get().summary() + "\n\n" + docString.get().description();
   }
 
   private class IncomingGatherer implements IncomingCallsGatherer {
-
-    @Override
-    public void gatherIncomingCall(OngoingRequest ongoingRequest, Endpoint endpoint) {
+    @Override public void gatherIncomingCall(OngoingRequest ongoingRequest, Endpoint endpoint) {
       Request message = ongoingRequest.request();
       String fromService = message.service().orElse(null);
       String method = message.method();
@@ -113,13 +71,8 @@ public class MetaInfoTracker {
   }
 
   private class OutgoingGatherer implements OutgoingCallsGatherer {
-
     public void gatherOutgoingCall(String toService, Request request) {
-      @SuppressWarnings("UnusedDeclaration")
-      CallsGatherer callsGatherer = gatherer.getOutgoingCallsGatherer(toService);
-      // Don't know how to convert outgoing uri:s to a useful limited set of endpoints, so
-      // not too useful to collect any info other than that the external service is called.
-      // The request method, content type and query parameters are available.
+      @SuppressWarnings(value = { "UnusedDeclaration" }) CallsGatherer callsGatherer = gatherer.getOutgoingCallsGatherer(toService);
     }
   }
 
