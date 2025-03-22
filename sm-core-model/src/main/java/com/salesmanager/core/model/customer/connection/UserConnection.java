b0819a30827b406af9cdf0d70118495e9df4a2b4
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-22 06:26:01 +0000

    Mining Framework Analysis

--- sm-core-model/src/main/java/com/salesmanager/core/model/customer/connection/UserConnection.java
+++ sm-core-model/src/main/java/com/salesmanager/core/model/customer/connection/UserConnection.java
@@ -1,15 +1,1 @@
-package com.salesmanager.core.model.customer.connection;
-
-import javax.persistence.Entity;
-
-@Deprecated
-@Entity
-public class UserConnection extends AbstractUserConnectionWithCompositeKey {
-
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-
-
-}
+  fatal :  path  'sm-core-model/src/main/java/com/salesmanager/core/model/customer/connection/UserConnection.java' exists on  disk ,  but   not in '4ce8226c52271034398dc68fdc137b6f230ac31e'    @ Deprecated  @ Entity public class UserConnection  extends AbstractUserConnectionWithCompositeKey  {   private static final  long  serialVersionUID = 1L ; }
--- sm-core/src/main/java/com/salesmanager/core/business/configuration/CoreApplicationConfiguration.java
+++ sm-core/src/main/java/com/salesmanager/core/business/configuration/CoreApplicationConfiguration.java
@@ -1,22 +1,6 @@
-package com.salesmanager.core.business.configuration;
-
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
-
-@Configuration
-@EnableAutoConfiguration
-/**
- * added in shopizer-core-config.xml
- * 
- * @author c.samson
- *
- */
-@ComponentScan({"com.salesmanager.core.business"})
-@ImportResource("classpath:/spring/shopizer-core-context.xml")
-public class CoreApplicationConfiguration {
-
-
-
-}
+  package     com . salesmanager . core . business . configuration ;   import     org . springframework . boot . autoconfigure . EnableAutoConfiguration ;  import     org . springframework . context . annotation . ComponentScan ;  import     org . springframework . context . annotation . Configuration ;  import     org . springframework . context . annotation . ImportResource ;    @ Configuration  @ EnableAutoConfiguration  @ ComponentScan  (  { "com.salesmanager.core.business" } )  @ ImportResource  ( "classpath:/spring/shopizer-core-context.xml" ) 
+<<<<<<<
+ @ Import  (  {  DroolsConfiguration . class ,  DataConfiguration . class } )
+=======
+>>>>>>>
+ public class CoreApplicationConfiguration  { }
