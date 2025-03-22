06b4e7f81a94abd0407b61d5e2677e82fcabfacc
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-22 09:59:38 +0000

    Mining Framework Analysis

--- src/main/java/org/dasein/cloud/network/VLANSupport.java
+++ src/main/java/org/dasein/cloud/network/VLANSupport.java
@@ -236,6 +236,16 @@ public interface VLANSupport extends AccessControlledService {
     public abstract @Nonnull VLAN createVlan(@Nonnull VlanCreateOptions options) throws InternalException, CloudException;
 
     /**
+     * Creates a new vlan based on the specified creation options.
+     * @param options the options to be used in creating the vlan
+     * @return the unique provider ID identifying the newly created vlan
+     * @throws CloudException an error occurred with the cloud provider while performing the operation
+     * @throws InternalException an error occurred locally independent of any events in the cloud
+     * @throws UnsupportedOperationException this cloud doesn't support vlan creation using the specified options
+     */
+    public abstract @Nonnull VLAN createVlan(@Nonnull VlanCreateOptions options) throws InternalException, CloudException;
+
+    /**
      * Detaches the specified network interface from any virtual machine it might be attached to.
      * @param nicId the unique ID of the network interface to be detached
      * @throws CloudException an error occurred with the cloud provider while detaching the network interface
