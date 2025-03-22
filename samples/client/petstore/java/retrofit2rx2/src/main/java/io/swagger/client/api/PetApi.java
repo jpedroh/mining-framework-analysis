564b1e64e15662e8696822c5e4d51a3de7bcec21
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-22 10:47:10 +0000

    Mining Framework Analysis

--- samples/client/petstore/java/retrofit2rx2/src/main/java/io/swagger/client/api/PetApi.java
+++ samples/client/petstore/java/retrofit2rx2/src/main/java/io/swagger/client/api/PetApi.java
@@ -1,18 +1,13 @@
 package io.swagger.client.api;
-
 import io.swagger.client.CollectionFormats.*;
-
 import io.reactivex.Observable;
 import io.reactivex.Completable;
 import retrofit2.http.*;
-
 import okhttp3.RequestBody;
 import okhttp3.ResponseBody;
-
 import java.io.File;
 import io.swagger.client.model.ModelApiResponse;
 import io.swagger.client.model.Pet;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -23,76 +18,51 @@ public interface PetApi {
    * Add a new pet to the store
    * 
    * @param body Pet object that needs to be added to the store (required)
-   * @return Completable
+   * @return Call&lt;Void&gt;
    */
-  @Headers({
-    "Content-Type:application/json"
-  })
-  @POST("pet")
-  Completable addPet(
-    @retrofit2.http.Body Pet body
-  );
+  @Headers(value = { "Content-Type:application/json" }) @POST(value = "pet") Completable addPet(@retrofit2.http.Body Pet body);
 
   /**
    * Deletes a pet
    * 
    * @param petId Pet id to delete (required)
    * @param apiKey  (optional)
-   * @return Completable
+   * @return Call&lt;Void&gt;
    */
-  @DELETE("pet/{petId}")
-  Completable deletePet(
-    @retrofit2.http.Path("petId") Long petId, @retrofit2.http.Header("api_key") String apiKey
-  );
+  @DELETE(value = "pet/{petId}") Completable deletePet(@retrofit2.http.Path(value = "petId") Long petId, @retrofit2.http.Header(value = "api_key") String apiKey);
 
   /**
    * Finds Pets by status
    * Multiple status values can be provided with comma separated strings
    * @param status Status values that need to be considered for filter (required)
-   * @return Observable&lt;List&lt;Pet&gt;&gt;
+   * @return Call&lt;List&lt;Pet&gt;&gt;
    */
-  @GET("pet/findByStatus")
-  Observable<List<Pet>> findPetsByStatus(
-    @retrofit2.http.Query("status") CSVParams status
-  );
+  @GET(value = "pet/findByStatus") Observable<List<Pet>> findPetsByStatus(@retrofit2.http.Query(value = "status") CSVParams status);
 
   /**
    * Finds Pets by tags
    * Multiple tags can be provided with comma separated strings. Use tag1, tag2, tag3 for testing.
    * @param tags Tags to filter by (required)
-   * @return Observable&lt;List&lt;Pet&gt;&gt;
+   * @return Call&lt;List&lt;Pet&gt;&gt;
    * @deprecated
    */
-  @Deprecated
-  @GET("pet/findByTags")
-  Observable<List<Pet>> findPetsByTags(
-    @retrofit2.http.Query("tags") CSVParams tags
-  );
+  @Deprecated @GET(value = "pet/findByTags") Observable<List<Pet>> findPetsByTags(@retrofit2.http.Query(value = "tags") CSVParams tags);
 
   /**
    * Find pet by ID
    * Returns a single pet
    * @param petId ID of pet to return (required)
-   * @return Observable&lt;Pet&gt;
+   * @return Call&lt;Pet&gt;
    */
-  @GET("pet/{petId}")
-  Observable<Pet> getPetById(
-    @retrofit2.http.Path("petId") Long petId
-  );
+  @GET(value = "pet/{petId}") Observable<Pet> getPetById(@retrofit2.http.Path(value = "petId") Long petId);
 
   /**
    * Update an existing pet
    * 
    * @param body Pet object that needs to be added to the store (required)
-   * @return Completable
+   * @return Call&lt;Void&gt;
    */
-  @Headers({
-    "Content-Type:application/json"
-  })
-  @PUT("pet")
-  Completable updatePet(
-    @retrofit2.http.Body Pet body
-  );
+  @Headers(value = { "Content-Type:application/json" }) @PUT(value = "pet") Completable updatePet(@retrofit2.http.Body Pet body);
 
   /**
    * Updates a pet in the store with form data
@@ -100,13 +70,9 @@ public interface PetApi {
    * @param petId ID of pet that needs to be updated (required)
    * @param name Updated name of the pet (optional)
    * @param status Updated status of the pet (optional)
-   * @return Completable
+   * @return Call&lt;Void&gt;
    */
-  @retrofit2.http.FormUrlEncoded
-  @POST("pet/{petId}")
-  Completable updatePetWithForm(
-    @retrofit2.http.Path("petId") Long petId, @retrofit2.http.Field("name") String name, @retrofit2.http.Field("status") String status
-  );
+  @retrofit2.http.FormUrlEncoded @POST(value = "pet/{petId}") Completable updatePetWithForm(@retrofit2.http.Path(value = "petId") Long petId, @retrofit2.http.Field(value = "name") String name, @retrofit2.http.Field(value = "status") String status);
 
   /**
    * uploads an image
@@ -114,12 +80,7 @@ public interface PetApi {
    * @param petId ID of pet to update (required)
    * @param additionalMetadata Additional data to pass to server (optional)
    * @param file file to upload (optional)
-   * @return Observable&lt;ModelApiResponse&gt;
+   * @return Call&lt;ModelApiResponse&gt;
    */
-  @retrofit2.http.Multipart
-  @POST("pet/{petId}/uploadImage")
-  Observable<ModelApiResponse> uploadFile(
-    @retrofit2.http.Path("petId") Long petId, @retrofit2.http.Part("additionalMetadata") String additionalMetadata, @retrofit2.http.Part("file\"; filename=\"file") RequestBody file
-  );
-
+  @retrofit2.http.Multipart @POST(value = "pet/{petId}/uploadImage") Observable<ModelApiResponse> uploadFile(@retrofit2.http.Path(value = "petId") Long petId, @retrofit2.http.Part(value = "additionalMetadata") String additionalMetadata, @retrofit2.http.Part(value = "file\"; filename=\"file") RequestBody file);
 }
