3c94a432831b0f9f07a4fd9b006c5e2cf94c689c
Joao Pedro <joao.pedro.hsd@gmail.com>
2025-03-22 00:15:08 +0000

    Mining Framework Analysis

--- yoga-core/src/main/java/org/skyscreamer/yoga/populator/DefaultFieldPopulatorRegistry.java
+++ yoga-core/src/main/java/org/skyscreamer/yoga/populator/DefaultFieldPopulatorRegistry.java
@@ -1,5 +1,4 @@
 package org.skyscreamer.yoga.populator;
-
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.util.HashMap;
@@ -10,39 +9,31 @@ import java.util.Map;
  * Created by IntelliJ IDEA.
  * User: corby
  */
-public class DefaultFieldPopulatorRegistry implements FieldPopulatorRegistry
-{
-    private Map<Class<?>,FieldPopulator<?>> _registry = new HashMap<Class<?>, FieldPopulator<?>>();
+public class DefaultFieldPopulatorRegistry implements FieldPopulatorRegistry {
+  private Map<Class<?>, FieldPopulator<?>> _registry = new HashMap<Class<?>, FieldPopulator<?>>();
 
-    @SuppressWarnings("rawtypes")
-	public DefaultFieldPopulatorRegistry( List<FieldPopulator<?>> fieldPopulators )
-    {
-        for ( FieldPopulator<?> fieldPopulator : fieldPopulators )
-        {
-            Class<? extends FieldPopulator> fieldPopulatorClass = fieldPopulator.getClass();
-            for ( Type genericInterface : fieldPopulatorClass.getGenericInterfaces() )
-            {
-                if ( genericInterface.toString().contains( FieldPopulator.class.getName() ) )
-                {
-                    ParameterizedType parameterizedType = (ParameterizedType)genericInterface;
-                    Class<?> clazz = (Class<?>)parameterizedType.getActualTypeArguments()[0];
-                    _registry.put( clazz, fieldPopulator );
-                    break;
-                }
-            }
-            Type genericSuperclass = fieldPopulatorClass.getGenericSuperclass();
-            if ( genericSuperclass.toString().contains( FieldPopulatorSupport.class.getName() ) )
-            {
-                ParameterizedType parameterizedType = (ParameterizedType)genericSuperclass;
-                Class<?> clazz = (Class<?>)parameterizedType.getActualTypeArguments()[0];
-                _registry.put( clazz, fieldPopulator );
-                break;
-            }
+  @SuppressWarnings(value = { "rawtypes" }) public DefaultFieldPopulatorRegistry(List<FieldPopulator<?>> fieldPopulators) {
+    for (FieldPopulator<?> fieldPopulator : fieldPopulators) {
+      Class<? extends FieldPopulator> fieldPopulatorClass = fieldPopulator.getClass();
+      for (Type genericInterface : fieldPopulatorClass.getGenericInterfaces()) {
+        if (genericInterface.toString().contains(FieldPopulator.class.getName())) {
+          ParameterizedType parameterizedType = (ParameterizedType) genericInterface;
+          Class<?> clazz = (Class<?>) parameterizedType.getActualTypeArguments()[0];
+          _registry.put(clazz, fieldPopulator);
+          break;
         }
+      }
+      Type genericSuperclass = fieldPopulatorClass.getGenericSuperclass();
+      if (genericSuperclass.toString().contains(FieldPopulatorSupport.class.getName())) {
+        ParameterizedType parameterizedType = (ParameterizedType) genericSuperclass;
+        Class<?> clazz = (Class<?>) parameterizedType.getActualTypeArguments()[0];
+        _registry.put(clazz, fieldPopulator);
+        break;
+      }
     }
+  }
 
-    public FieldPopulator<?> getFieldPopulator( Class<?> clazz )
-    {
-        return _registry.get( clazz );
-    }
+  public FieldPopulator<?> getFieldPopulator(Class<?> clazz) {
+    return _registry.get(clazz);
+  }
 }
